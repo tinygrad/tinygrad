420217ddbf150dacd86e06bd2b4301051564f74e
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-10-19 09:49:37 -0500

    simplify lock, fix read

--- tinygrad/utils.py
+++ tinygrad/utils.py
@@ -15,6 +15,7 @@ mnist_filename = "mnist.npz"
 mnist_url = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/" + mnist_filename
 mnist_md5_hash = "8a61469f7ea1b51cbae51d4f78837e45"
 mnist_fp = os.path.join("/tmp", mnist_md5_hash)
+
 mnist_lock = FileLock(mnist_fp + ".lock")
 
 
@@ -22,19 +23,19 @@ def fetch_mnist():
   # Cache within session
   if hasattr(fetch_mnist, "data"):
     return fetch_mnist.data
-  data = None
-  # Cache across sessions
-  if os.path.exists(mnist_fp):
-    with open(mnist_fp, "rb") as f:
-      data = f.read()
-      if md5(data).hexdigest() != mnist_md5_hash:
-        data = None
-  if data is None:
-    data = requests.get(mnist_url, timeout=10).content
-    with mnist_lock, open(mnist_fp, "wb") as f:
-      f.write(data)
-  with np.load(mnist_fp, allow_pickle=True) as f:
-    X_train, Y_train = f['x_train'], f['y_train']
-    X_test, Y_test = f['x_test'], f['y_test']
+  with mnist_lock:
+    download = True
+    # Cache across sessions
+    if os.path.exists(mnist_fp):
+      with open(mnist_fp, "rb") as f:
+        if md5(f.read()).hexdigest() == mnist_md5_hash:
+          download = False
+    if download:
+      data = requests.get(mnist_url, timeout=10).content
+      with open(mnist_fp, "wb") as f:
+        f.write(data)
+    with np.load(mnist_fp, allow_pickle=True) as f:
+      X_train, Y_train = f['x_train'], f['y_train']
+      X_test, Y_test = f['x_test'], f['y_test']
   fetch_mnist.data = (X_train, Y_train, X_test, Y_test)
   return X_train, Y_train, X_test, Y_test
