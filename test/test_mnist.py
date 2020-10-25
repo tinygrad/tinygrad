ea41f5e1c1106a00a55a7874761868032c2a32be
George Hotz <geohot@gmail.com>
2020-10-25 16:40:37 -0700

    seems more generic

--- test/test_mnist.py
+++ test/test_mnist.py
@@ -39,7 +39,7 @@ class TinyConvNet:
 
 def train(model, optim, steps, BS=128):
   losses, accuracies = [], []
-  for i in (t := trange(steps, disable=os.getenv('GITHUB_ACTIONS') is not None)):
+  for i in (t := trange(steps, disable=os.getenv('CI') is not None)):
     samp = np.random.randint(0, X_train.shape[0], size=(BS))
     
     x = Tensor(X_train[samp].reshape((-1, 28*28)).astype(np.float32))
