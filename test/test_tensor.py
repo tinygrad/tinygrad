9fc117caee5f0754522f443a6aeaa3511b347c8c
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-10-26 21:42:26 +0300

    test new adam

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -2,11 +2,26 @@ import numpy as np
 import torch
 import unittest
 from tinygrad.tensor import Tensor
+from tinygrad.optim import Adam
 from tinygrad.gradcheck import numerical_jacobian, jacobian, gradcheck
 
 x_init = np.random.randn(1,3).astype(np.float32)
 W_init = np.random.randn(3,3).astype(np.float32)
 m_init = np.random.randn(1,3).astype(np.float32)
+
+class OldAdam(Adam):
+  def __init__(self, params, lr=0.001, b1=0.9, b2=0.999, eps=1e-8):
+    super(OldAdam, self).__init__(params, lr, b1, b2, eps)
+
+  def step(self):
+    self.t += 1
+    for i,t in enumerate(self.params):
+      self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * t.grad
+      self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * np.square(t.grad)
+      mhat = self.m[i] / (1. - self.b1**self.t)
+      vhat = self.v[i] / (1. - self.b2**self.t)
+      t.data -= self.lr * mhat / (np.sqrt(vhat) + self.eps)
+
 
 class TestTinygrad(unittest.TestCase):
   def test_backward_pass(self):
@@ -31,6 +46,37 @@ class TestTinygrad(unittest.TestCase):
       return out.detach().numpy(), x.grad, W.grad
 
     for x,y in zip(test_tinygrad(), test_pytorch()):
+      np.testing.assert_allclose(x, y, atol=1e-5)
+
+  def test_adam_step(self):
+    n_step = 10
+    def test_new_adam():
+      x = Tensor(x_init)
+      W = Tensor(W_init)
+      optim = Adam([W, x])
+      m = Tensor(m_init)
+      for _ in range(n_step):
+        out = x.dot(W).relu()
+        out = out.logsoftmax()
+        out = out.mul(m).add(m).sum()
+        out.backward()
+        optim.step()
+      return x.data, W.data
+
+    def test_old_adam():
+      x = Tensor(x_init)
+      W = Tensor(W_init)
+      optim = OldAdam([W, x])
+      m = Tensor(m_init)
+      for _ in range(n_step):
+        out = x.dot(W).relu()
+        out = out.logsoftmax()
+        out = out.mul(m).add(m).sum()
+        out.backward()
+        optim.step()
+      return x.data, W.data
+
+    for x,y in zip(test_new_adam(), test_old_adam()):
       np.testing.assert_allclose(x, y, atol=1e-5)
 
   def test_jacobian(self):
