cc9bd45b44c397cb8b686464f816336c81a38e30
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-10-26 18:19:50 +0300

    add setup.py and change imports to relative

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='tinygrad',
+      version='1.0.0',
+      description='You like pytorch? You like micrograd? You love tinygrad! heart',
+      author='George Hotz',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages = ['tinygrad'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License"
+      ],
+      install_requires=['numpy', 'requests'],
+      python_requires='>=3.6',
+      include_package_data=True)
---
+++ tinygrad/__init__.py
@@ -0,0 +1,4 @@
+from .optim import *
+from .ops import *
+from .tensor import *
+from .utils import *
