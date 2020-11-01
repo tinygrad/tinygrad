02a5e8d16208d623246e0b603b6f39370fc85312
marcelbischoff <mrclbschff@gmail.com>
2020-11-01 16:48:43 -0500

    got rid of print

--- examples/efficientnet.py
+++ examples/efficientnet.py
@@ -148,8 +148,6 @@ if __name__ == "__main__":
   lbls = fetch("https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt")
   lbls = ast.literal_eval(lbls.decode('utf-8'))
 
-  print(lbls)
-
   # run the net
   import time
   st = time.time()
