111f4e44abb6201c47fc93a644c1e18b753e4648
dustcollector12 <44067540+dustcollector12@users.noreply.github.com>
2020-11-09 00:10:19 +0100

    pygame is fine, cv2 can also do the trick

--- examples/efficientnet.py
+++ examples/efficientnet.py
@@ -171,24 +171,18 @@ if __name__ == "__main__":
   from PIL import Image
   url = sys.argv[1]
   if url == 'webcam':
-    import pygame
-    pygame.init()
-    SCALE = 3
-    screen = pygame.display.set_mode((224*SCALE, 224*SCALE))
-    pygame.display.set_caption("capture")
-
     import cv2
     cap = cv2.VideoCapture(0)
     while 1:
       ret, frame = cap.read()
-      frame = Image.fromarray(frame[:, :, [2,1,0]])
-      out, retimg = infer(model, frame)
-      simg = cv2.resize(retimg, (224*SCALE, 224*SCALE))
-      pygame.surfarray.blit_array(screen, np.array(simg).swapaxes(0,1))
-      pygame.display.update()
-      for e in pygame.event.get():
-        pass
+      cv2.imshow('capture', frame)
+      img = Image.fromarray(frame[:, :, [2,1,0]])
+      out, retimg = infer(model, img)
       print(np.argmax(out.data), np.max(out.data), lbls[np.argmax(out.data)])
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cap.release()
+    cv2.destroyAllWindows()
   else:
     img = Image.open(io.BytesIO(fetch(url)))
     st = time.time()
