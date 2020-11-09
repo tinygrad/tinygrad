29c92ab484b7911551ee3b7e276aadcc9736148d
holonomicjl <58403584+holonomicjl@users.noreply.github.com>
2020-11-09 23:19:25 +0000

    Pad2d backward pass on GPU

--- tinygrad/opsgpu.py
+++ tinygrad/opsgpu.py
@@ -301,14 +301,14 @@ class Pad2D(Function):
     prg = clbuild(ctx.cl_ctx, """
     __kernel void pad2d(
         __global const float *input, __global float *output,
-        int cin, int py, int px, int oy, int ox, int iy, int ix
+        int cin, int py, int px, int ipy, int ipx, int oy, int ox, int iy, int ix
       )
     {
       int B = get_global_id(0);
       int C = get_global_id(1);
       int Y = get_global_id(2);
 
-      int iptr = B*cin*iy*ix + C*iy*ix + Y*ix;
+      int iptr = B*cin*iy*ix + C*iy*ix + (Y+ipy)*ix + ipx;
       int optr = B*cin*oy*ox + C*oy*ox + (Y+py)*ox + px;
 
       for (int x = 0; x < ix; x++) {
@@ -316,16 +316,26 @@ class Pad2D(Function):
       }
     }
     """)
+    ctx.save_for_backward(prg, padding)
     prg.pad2d(ctx.cl_queue, [bs, cin, iy], None,
         x, ret,
-        np.int32(cin), np.int32(padding[0]), np.int32(padding[2]),
+        np.int32(cin), np.int32(padding[0]), np.int32(padding[2]), np.int32(0), np.int32(0),
         np.int32(oy), np.int32(ox), np.int32(iy), np.int32(ix)
       )
     return ret
 
   @staticmethod
   def backward(ctx, grad_output):
-    raise Exception("write this")
+    prg, padding = ctx.saved_tensors
+    bs, cin, iy, ix = grad_output.shape
+    oy, ox = iy - padding[0] - padding[1], ix - padding[2] - padding[3]
+    ret = buffer_zeros(ctx, (bs, cin, oy, ox))
+    prg.pad2d(ctx.cl_queue, [bs, cin, iy], None,
+              grad_output, ret,
+              np.int32(cin), np.int32(0), np.int32(0), np.int32(padding[0]), np.int32(padding[2]),
+              np.int32(oy), np.int32(ox), np.int32(iy), np.int32(ix)
+              )
+    return ret
 register('pad2d', Pad2D, gpu=True)
 
 class Reshape(Function):
