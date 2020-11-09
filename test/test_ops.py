870b84a89370118d34c3dcc159d59b831df7bee9
George Hotz <geohot@gmail.com>
2020-11-09 15:50:37 -0800

    test pad2d backward on GPU

--- test/test_ops.py
+++ test/test_ops.py
@@ -63,7 +63,7 @@ class TestOps(unittest.TestCase):
     helper_test_op([(45,65)], lambda x: torch.nn.LogSoftmax(dim=1)(x), Tensor.logsoftmax, atol=1e-7, grad_atol=1e-7, gpu=self.gpu)
 
   def test_pad2d(self):
-    helper_test_op([(3,3,3,3)], lambda x: torch.nn.functional.pad(x, (1,1,1,1)), lambda x: x.pad2d(padding=(1,1,1,1)), gpu=self.gpu, forward_only=self.gpu)
+    helper_test_op([(3,3,3,3)], lambda x: torch.nn.functional.pad(x, (1,1,1,1)), lambda x: x.pad2d(padding=(1,1,1,1)), gpu=self.gpu)
 
   def test_conv2d(self):
     for bs in [1,8]:
