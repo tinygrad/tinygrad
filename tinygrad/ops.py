eef2f24a42cf8b19f68440607bac80d674f8dfef
Kartik Sharma <kartik.sharma@claimgenius.com>
2020-11-10 21:03:28 +0530

    :hammer: clean up

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -1,6 +1,7 @@
 import sys
 import warnings
 import numpy as np
+import itertools
 from .tensor import Function, register
 
 # ************* basic ops *************
@@ -224,8 +225,7 @@ class Conv2D(Function):
 
     # needs to be optimized
     gdx = np.zeros((bs,ctx.groups,cin,OY,OX), dtype=tx.dtype)
-    for Y in range(grad_output.shape[2]):
-      for X in range(grad_output.shape[3]):
+    for Y, X in itertools.product(range(grad_output.shape[2]),range(grad_output.shape[3])):
         iY,iX = Y*ys, X*xs
         #gdx[:,:,: , iY:iY+H, iX:iX+W] += np.einsum('igk,gkjyx->igjyx', ggg[:,:,:,Y,X], tw)
         for g in range(ctx.groups):
@@ -238,29 +238,24 @@ register('conv2d', Conv2D)
 
 # ************* pooling ops *************
 
-def stack_for_pool(x, py, px):
-  my, mx = (x.shape[2]//py)*py, (x.shape[3]//px)*px
-  stack = []
-  xup = x[:, :, :my, :mx]
-  for Y in range(py):
-    for X in range(px):
-      stack.append(xup[:, :, Y::py, X::px][None])
-  return np.concatenate(stack, axis=0)
-
-def unstack_for_pool(fxn, s, py, px):
+def pool(x, py, px, fxn=None):
+  s = x if fxn else x.shape
   my, mx = (s[2]//py)*py, (s[3]//px)*px
-  for Y in range(py):
-    for X in range(px):
+  stack, xup = (None,None) if fxn else (list([]),x[:,:,:my,:mx])
+  for Y, X in itertools.product(range(py), range(px)):
+    if fxn:
       ll = fxn(Y*px+X)
       if X == 0 and Y == 0:
         ret = np.zeros(s, dtype=ll.dtype)
       ret[:, :, Y:my:py, X:mx:px] = ll
-  return ret
+    else:
+      stack.append(xup[:, :, Y::py, X::px][None])
+  return ret if fxn else np.concatenate(stack, axis=0)
 
 class MaxPool2D(Function):
   @staticmethod
   def forward(ctx, x, kernel_size=(2, 2)):
-    stack = stack_for_pool(x, *kernel_size)
+    stack = pool(x, *kernel_size)
     idxs = np.argmax(stack, axis=0)
     ctx.save_for_backward(idxs, x.shape)
     return np.max(stack, axis=0)
@@ -268,24 +263,23 @@ class MaxPool2D(Function):
   @staticmethod
   def backward(ctx, grad_output):
     idxs,s = ctx.saved_tensors
-    return unstack_for_pool(
-      lambda idx: grad_output * (idxs == idx),
-      s, *ctx.kernel_size)
+    return pool(s, *ctx.kernel_size,
+      fxn = lambda idx: grad_output * (idxs == idx)
+      )
 register('max_pool2d', MaxPool2D)
 
 class AvgPool2D(Function):
   @staticmethod
   def forward(ctx, x, kernel_size=(2, 2)):
-    stack = stack_for_pool(x, *kernel_size)
+    stack = pool(x, *kernel_size)
     ctx.save_for_backward(x.shape)
     return np.mean(stack, axis=0)
 
   @staticmethod
   def backward(ctx, grad_output):
     s, = ctx.saved_tensors
-    py, px = ctx.kernel_size
-    return unstack_for_pool(
-      lambda idx: grad_output/py/px,
-      s, py, px)
+    return pool(s, *ctx.kernel_size,
+      fxn=lambda idx: grad_output/py/px
+      )
 register('avg_pool2d', AvgPool2D)
 
