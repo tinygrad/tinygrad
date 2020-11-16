fd8c7f9340ff5621a455e27888bfef137c50c2a5
dustcollector12 <44067540+dustcollector12@users.noreply.github.com>
2020-11-16 22:02:45 +0100

    tensor implementation for rmsprop and adam

--- tinygrad/optim.py
+++ tinygrad/optim.py
@@ -23,36 +23,39 @@ class SGD(Optimizer):
 class RMSprop(Optimizer):
   def __init__(self, params, lr=0.001, decay=0.9, eps=1e-8):
     super(RMSprop, self).__init__(params)
-    self.lr = lr
-    self.decay = decay
-    self.eps = eps
+    self.lr = Tensor([lr], gpu=params[0].gpu)
+    self.decay = Tensor([decay], gpu=params[0].gpu)
+    self.eps = Tensor([eps], gpu=params[0].gpu)
 
-    self.v = [np.zeros_like(t.data) for t in self.params]
+    self.v = [Tensor(np.zeros(t.shape, dtype=np.float32), gpu=params[0].gpu) for t in self.params]
+
+    self.one = Tensor([1], gpu=self.params[0].gpu)
+    self.two = Tensor([2], gpu=self.params[0].gpu)
 
   def step(self):
     for i, t in enumerate(self.params):
-      self.v[i] = self.decay * self.v[i] + (1 - self.decay) * np.square(t.grad.data)
-      t.data -= self.lr / (np.sqrt(self.v[i]) + self.eps) * t.grad.data
+      self.v[i] = self.decay * self.v[i] + (self.one - self.decay) * t.grad.pow(self.two)
+      t -= self.lr.div(self.v[i].sqrt() + self.eps) * t.grad
 
 class Adam(Optimizer):
   def __init__(self, params, lr=0.001, b1=0.9, b2=0.999, eps=1e-8):
     super(Adam, self).__init__(params)
-    self.lr = lr
-    self.b1 = b1
-    self.b2 = b2
-    self.eps = eps
-    self.t = 0
+    self.lr = Tensor([lr], gpu=params[0].gpu)
+    self.b1 = Tensor([b1], gpu=params[0].gpu)
+    self.b2 = Tensor([b2], gpu=params[0].gpu)
+    self.eps = Tensor([eps], gpu=params[0].gpu)
+    self.t = Tensor([0], gpu=params[0].gpu)
 
-    self.m = [np.zeros_like(t.data) for t in self.params]
-    self.v = [np.zeros_like(t.data) for t in self.params]
+    self.m = [Tensor(np.zeros(t.shape, dtype=np.float32), gpu=params[0].gpu) for t in self.params]
+    self.v = [Tensor(np.zeros(t.shape, dtype=np.float32), gpu=params[0].gpu) for t in self.params]
+
+    self.one = Tensor([1], gpu=self.params[0].gpu)
+    self.two = Tensor([2], gpu=self.params[0].gpu)
 
   def step(self):
-    self.t += 1
-    a = self.lr * (
-      np.sqrt(1 - np.power(self.b2, self.t)) /
-      (1 - np.power(self.b1, self.t)))
+    self.t = self.t + self.one
+    a = self.lr * (self.one - self.b2.pow(self.t)).sqrt().div(self.one - self.b1.pow(self.t))
     for i,t in enumerate(self.params):
-      self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * t.grad.data
-      self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * np.square(t.grad.data)
-      t.data -= a * self.m[i] / (np.sqrt(self.v[i]) + self.eps)
-
+      self.m[i] = self.b1 * self.m[i] + (self.one - self.b1) * t.grad
+      self.v[i] = self.b2 * self.v[i] + (self.one - self.b2) * t.grad.pow(self.two)
+      t -= a * self.m[i].div(self.v[i].sqrt() + self.eps)
