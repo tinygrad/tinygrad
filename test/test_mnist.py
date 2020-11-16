a43c80584c1a18ca16830c8c71c1bddda96399d5
dustcollector12 <44067540+dustcollector12@users.noreply.github.com>
2020-11-16 22:57:33 +0100

    test_mnist.py extended to cover sgd, rmsprop and adam on cpu and gpu

--- test/test_mnist.py
+++ test/test_mnist.py
@@ -99,7 +99,7 @@ class TestMNIST(unittest.TestCase):
     np.random.seed(1337)
     model = TinyConvNet()
     [x.cuda_() for x in model.parameters()]
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     train(model, optimizer, steps=1000, gpu=True)
     evaluate(model, gpu=True)
 
@@ -125,6 +125,15 @@ class TestMNIST(unittest.TestCase):
     optimizer = optim.SGD(model.parameters(), lr=0.001)
     train(model, optimizer, steps=1000)
     evaluate(model)
+
+  @unittest.skipUnless(GPU, "Requires GPU")
+  def test_rmsprop_gpu(self):
+    np.random.seed(1337)
+    model = TinyBobNet()
+    [x.cuda_() for x in model.parameters()]
+    optimizer = optim.RMSprop(model.parameters(), lr=0.0002)
+    train(model, optimizer, steps=1000, gpu=True)
+    evaluate(model, gpu=True)
 
   def test_rmsprop(self):
     np.random.seed(1337)
