c60abb79c6edfaadc6b9918dcfe91f0af6273af9
George Hotz <geohot@gmail.com>
2020-11-28 10:01:10 -0600

    remove hwx before compile, weights are converted to float16

--- ane/2_compile/compile.sh
+++ ane/2_compile/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcc compile.m -F /System/Library/PrivateFrameworks/ -framework ANECompiler -framework CoreFoundation
+rm -f model.hwx
 ./a.out
 log show --process a.out --last 1m --info --debug
 
