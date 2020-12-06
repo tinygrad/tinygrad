6ba25834ee15da45a6fe598c0521092573751f73
George Hotz <geohot@gmail.com>
2020-12-06 15:57:28 -0800

    found some plists

--- ane/1_build/coreml_ane.py
+++ ane/1_build/coreml_ane.py
@@ -4,13 +4,17 @@ import coremltools as ct
 from coremltools.models.neural_network import datatypes, NeuralNetworkBuilder
 
 input_features = [('image', datatypes.Array(3))]
-output_features = [('probs', datatypes.Array(2))]
+output_features = [('probs', datatypes.Array(3))]
 
-weights = np.zeros((3, 2)) + 3
-bias = np.ones(2)
+weights = np.zeros((3, 3)) + 3
+bias = np.ones(3)
 
 builder = NeuralNetworkBuilder(input_features, output_features)
-builder.add_inner_product(name='ip_layer', W=weights, b=bias, input_channels=3, output_channels=2, has_bias=True, input_name='image', output_name='probs')
+builder.add_inner_product(name='ip_layer', W=weights, b=None, input_channels=3, output_channels=3, has_bias=False, input_name='image', output_name='med')
+#builder.add_inner_product(name='ip_layer_2', W=weights, b=None, input_channels=3, output_channels=3, has_bias=False, input_name='med', output_name='probs')
+#builder.add_elementwise(name='element', input_names=['med', 'med'], output_name='probs', mode='ADD')
+#builder.add_bias(name='bias', b=bias, input_name='med', output_name='probs', shape_bias=(3,))
+builder.add_activation(name='act_layer', non_linearity='SIGMOID', input_name='med', output_name='probs')
 
 # compile the spec
 mlmodel = ct.models.MLModel(builder.spec)
