4e56ad9b8cb3f67154bd60d647c252c2db747fb3
Marcel Bischoff <mrclbschff@gmail.com>
2020-12-06 23:33:54 -0500

    needs work

--- test/test_nn.py
+++ test/test_nn.py
@@ -8,33 +8,40 @@ class TestNN(unittest.TestCase):
   def test_batchnorm2d(self):
     sz = 4
 
-    # create in tinygrad
-    bn = BatchNorm2D(sz, eps=1e-5)
-    bn.weight = Tensor.randn(sz)
-    bn.bias = Tensor.randn(sz)
-    bn.running_mean = Tensor.randn(sz)
-    bn.running_var = Tensor.randn(sz)
-    bn.running_var.data[bn.running_var.data < 0] = 0
+    for training in [False, True]:
+      # create in tinygrad
+      bn = BatchNorm2D(sz, eps=1e-5, training=training)
+      bn.weight = Tensor.randn(sz)
+      bn.bias = Tensor.randn(sz)
+      bn.running_mean = Tensor.randn(sz)
+      bn.running_var = Tensor.randn(sz)
+      bn.running_var.data[bn.running_var.data < 0] = 0
+      #debug
+      bn.running_mean = Tensor.zeros(sz)
+      bn.running_var = Tensor.zeros(sz)
 
-    # create in torch
-    with torch.no_grad():
-      tbn = torch.nn.BatchNorm2d(sz).eval()
-      tbn.weight[:] = torch.tensor(bn.weight.data)
-      tbn.bias[:] = torch.tensor(bn.bias.data)
-      tbn.running_mean[:] = torch.tensor(bn.running_mean.data)
-      tbn.running_var[:] = torch.tensor(bn.running_var.data)
+      # create in torch
+      #with torch.no_grad():
+      if True:
+        tbn = torch.nn.BatchNorm2d(sz,momentum=None,eps=1e-5).eval()
+        tbn.training=training
+        tbn.track_running_stats=False
+        tbn.weight[:] = torch.tensor(bn.weight.data) #, requires_grad=True)
+        tbn.bias[:] = torch.tensor(bn.bias.data) #,requires_grad=True)
+        tbn.running_mean[:] = torch.tensor(bn.running_mean.data)
+        tbn.running_var[:] = torch.tensor(bn.running_var.data)
 
-    # trial
-    inn = Tensor.randn(2, sz, 3, 3)
+      # trial
+      inn = Tensor.randn(2, sz, 3, 3)
 
-    # in tinygrad
-    outt = bn(inn)
+      # in tinygrad
+      outt = bn(inn)
 
-    # in torch
-    toutt = tbn(torch.tensor(inn.data))
+      # in torch
+      toutt = tbn(torch.tensor(inn.data))
 
-    # close
-    np.testing.assert_allclose(outt.data, toutt.detach().numpy(), rtol=1e-5)
+      # close
+      np.testing.assert_allclose(outt.data, toutt.detach().numpy(), rtol=1e-5)
 
 
 if __name__ == '__main__':
--- tinygrad/nn.py
+++ tinygrad/nn.py
@@ -1,26 +1,36 @@
-import numpy as np
 from tinygrad.tensor import Tensor
 
 class BatchNorm2D:
-  def __init__(self, sz, eps=0.001):
+  def __init__(self, sz, eps=1e-5, track_running_stats=False, training=True):
     self.eps = Tensor([eps], requires_grad=False)
     self.two = Tensor([2], requires_grad=False)
     self.weight = Tensor.ones(sz)
     self.bias = Tensor.zeros(sz)
+    self.track_running_stats = track_running_stats
+    self.training = training
 
     self.running_mean = Tensor.zeros(sz, requires_grad=False)
     self.running_var = Tensor.ones(sz, requires_grad=False)
     self.num_batches_tracked = Tensor.zeros(1, requires_grad=False)
 
-  def __call__(self, x, training = False):
-    if training: #how to determine?
-      [bs, sz], m = x.shape[:2], x.shape[2]*x.shape[3]
-      div =  Tensor(np.array([1/bs/m], dtype=np.float32),  gpu=x.gpu, requires_grad=False)
-      crow =  Tensor.ones(1,bs,  gpu=x.gpu, requires_grad=False)
-      ccol =  Tensor.ones(m,1, gpu=x.gpu, requires_grad=False)
-      self.running_mean = crow.dot(x.reshape(shape=[bs,sz*m])).reshape(shape=[sz,m]).dot(ccol).reshape(shape=[sz]).mul(div)
-      y = pow((x - self.running_mean.reshape(shape=[1, -1, 1, 1])).reshape(shape=[-1]), self.two) 
-      self.running_var = crow.dot(y.reshape(shape=[bs,sz*m])).reshape(shape=[sz,m]).dot(ccol).reshape(shape=[sz]).mul(div)
+  def __call__(self, x):
+    [bs, sz], m = x.shape[:2], x.shape[2]*x.shape[3]
+    div =  Tensor([1/bs/m],  gpu=x.gpu, requires_grad=False)   #we are reimplementing means by contracting with 1's
+    crow =  Tensor.ones(1,bs,  gpu=x.gpu, requires_grad=False) #we are reimplementing means by contracting with 1's
+    ccol =  Tensor.ones(m,1, gpu=x.gpu, requires_grad=False)   #we are reimplementing means by contracting with 1's
+    batch_mean = crow.dot(x.reshape(shape=[bs,sz*m])).reshape(shape=[sz,m]).dot(ccol).reshape(shape=[sz]).mul(div)
+    y = pow((x - self.running_mean.reshape(shape=[1, -1, 1, 1])), self.two) 
+    #y = (x - self.running_mean.reshape(shape=[1, -1, 1, 1])).mul(x - self.running_mean.reshape(shape=[1, -1, 1, 1]))#, self.two) 
+    batch_var = crow.dot(y.reshape(shape=[bs,sz*m])).reshape(shape=[sz,m]).dot(ccol).reshape(shape=[sz]).mul(div)
+    if self.track_running_stats: #needs momentum
+        self.running_mean = self.running_mean.mul(self.num_batches_tracked).add(batch_mean)
+        self.running_var = self.running_var.mul(self.num_batches_tracked).add(batch_var)
+        self.num_batches_tracked = self.num_batches_tracked.add(Tensor.ones(1, requires_grad=False))
+        self.running_mean = self.running_mean.mul(self.num_batches_tracked).div(self.num_batches_tracked)
+        self.running_var = self.running_var.mul(self.num_batches_tracked).div(self.num_batches_tracked)
+    elif self.training: #just use the batch mean and variance  
+        self.running_mean = batch_mean
+        self.running_var = batch_var
 
     x = x.sub(self.running_mean.reshape(shape=[1, -1, 1, 1]))
     x = x.mul(self.weight.reshape(shape=[1, -1, 1, 1]))
