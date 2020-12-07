6e793e96c36d061e3da264642cfc78bc90c2b495
George Hotz <geohot@gmail.com>
2020-12-06 17:49:17 -0800

    deeebug

--- ane/2_compile/compile.m
+++ ane/2_compile/compile.m
@@ -22,10 +22,22 @@ int main(int argc, char* argv[]) {
   CFMutableDictionaryRef flagsDictionary = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
 
   CFDictionaryAddValue(optionsDictionary, CFSTR("InputNetworks"), array);
-  CFDictionaryAddValue(optionsDictionary, CFSTR("OutputFileName"), CFSTR("model.hwx"));
   CFDictionaryAddValue(optionsDictionary, CFSTR("OutputFilePath"), CFSTR("./"));
+  //CFDictionaryAddValue(optionsDictionary, CFSTR("OptionsFilePath"), CFSTR("good.options"));
 
+  // h11 (or anything?) works here too, and creates different outputs that don't run
   CFDictionaryAddValue(flagsDictionary, CFSTR("TargetArchitecture"), CFSTR("h13"));
+
+  if (argc > 2) {
+    CFDictionaryAddValue(optionsDictionary, CFSTR("OutputFileName"), CFSTR("debug/model.hwx"));
+    CFDictionaryAddValue(flagsDictionary, CFSTR("DebugDetailPrint"), kCFBooleanTrue);
+    int debug_mask = 0x7fffffff;
+    CFDictionaryAddValue(flagsDictionary, CFSTR("DebugMask"), CFNumberCreate(kCFAllocatorDefault, 3, &debug_mask));
+  } else {
+    CFDictionaryAddValue(optionsDictionary, CFSTR("OutputFileName"), CFSTR("model.hwx"));
+  }
+  //CFDictionaryAddValue(flagsDictionary, CFSTR("DisableMergeScaleBias"), kCFBooleanTrue);
+  //CFDictionaryAddValue(flagsDictionary, CFSTR("Externs"), CFSTR("swag"));
 
   //CFShow(optionsDictionary);
   //CFShow(flagsDictionary);
