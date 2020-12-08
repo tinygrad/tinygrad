24d688c184bf4038681d0fc19bad0d228f472a2f
Daulet <daulet@users.noreply.github.com>
2020-12-08 22:18:45 +0000

    win more lines for core library (#158)
    ...and sacrifice test speed

--- test/test_gc.py
+++ test/test_gc.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python
+import gc
 import unittest
 from tinygrad.tensor import Tensor, GPU
 
+def tensors_allocated():
+    return sum([isinstance(x, Tensor) for x in gc.get_objects()])
+    
 class TestGC(unittest.TestCase):
   gpu = False
+
   def test_gc(self):
     a = Tensor.zeros(4,4, gpu=self.gpu)
     b = Tensor.zeros(4,4, gpu=self.gpu)
     (a*b).mean().backward()
-    assert(Tensor.allocated > 0)
+    assert(tensors_allocated() > 0)
     del a,b
-    assert(Tensor.allocated == 0)
+    assert(tensors_allocated() == 0)
 
   def test_gc_complex(self):
     a = Tensor.zeros(4,4, gpu=self.gpu)
     b = Tensor.zeros(4,4, gpu=self.gpu)
-    assert(Tensor.allocated == 2)
+    assert(tensors_allocated() == 2)
     (a*b).mean().backward()
-    assert(Tensor.allocated == 4)
+    assert(tensors_allocated() == 4)
     del b
-    assert(Tensor.allocated == 2)
+    assert(tensors_allocated() == 2)
     b = Tensor.zeros(4,4, gpu=self.gpu)
-    print(Tensor.allocated)
+    print(tensors_allocated())
     (a*b).mean().backward()
-    print(Tensor.allocated)
-    assert(Tensor.allocated == 4)
+    print(tensors_allocated())
+    assert(tensors_allocated() == 4)
     del b
-    assert(Tensor.allocated == 2)
+    assert(tensors_allocated() == 2)
 
     
 
