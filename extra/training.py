7fc54587ce3154a40842b09457938dcad2de6790
Marcel Bischoff <mrclbschff@gmail.com>
2020-12-12 09:50:53 -0500

    num_classes

--- extra/training.py
+++ extra/training.py
@@ -4,17 +4,18 @@ from tqdm import trange
 from tinygrad.utils import fetch, get_parameters
 from tinygrad.tensor import Tensor, GPU
 
-def train(model, X_train, Y_train, optim, steps, BS=128, gpu=False, lossfn = lambda out,y: out.mul(y).mean()):
+def train(model, X_train, Y_train, optim, steps, num_classes=None, BS=128, gpu=False, lossfn = lambda out,y: out.mul(y).mean()):
   if gpu is True: [x.cuda_() for x in get_parameters([model, optim])]
+  if num_classes is None: num_classes = Y_train.max().astype(int)+1
   losses, accuracies = [], []
   for i in (t := trange(steps, disable=os.getenv('CI') is not None)):
     samp = np.random.randint(0, X_train.shape[0], size=(BS))
 
     x = Tensor(X_train[samp].reshape((-1, 28*28)).astype(np.float32), gpu=gpu)
     Y = Y_train[samp]
-    y = np.zeros((len(samp),10), np.float32)
+    y = np.zeros((len(samp),num_classes), np.float32)
     # correct loss for NLL, torch NLL loss returns one per row
-    y[range(y.shape[0]),Y] = -10.0
+    y[range(y.shape[0]),Y] = -1.0*num_classes
     y = Tensor(y, gpu=gpu)
 
     # network
@@ -35,9 +36,10 @@ def train(model, X_train, Y_train, optim, steps, BS=128, gpu=False, lossfn = lam
     accuracies.append(accuracy)
     t.set_description("loss %.2f accuracy %.2f" % (loss, accuracy))
 
-def evaluate(model, X_test, Y_test, gpu=False, BS=128):
+def evaluate(model, X_test, Y_test, num_classes=None, gpu=False, BS=128):
   def numpy_eval():
-    Y_test_preds_out = np.zeros((len(Y_test),10))
+    if num_classes is None: num_classes = Y_train.max().astype(int)+1
+    Y_test_preds_out = np.zeros((len(Y_test),num_classes))
     for i in trange(len(Y_test)//BS, disable=os.getenv('CI') is not None):
       Y_test_preds_out[i*BS:(i+1)*BS] = model.forward(Tensor(X_test[i*BS:(i+1)*BS].reshape((-1, 28*28)).astype(np.float32), gpu=gpu)).cpu().data
     Y_test_preds = np.argmax(Y_test_preds_out, axis=1)
