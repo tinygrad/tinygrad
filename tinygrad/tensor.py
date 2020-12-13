cf2e2dc46e9d40bde1078f87ca6f3c4affff1628
Kartik Sharma <kartik4949@gmail.com>
2020-12-13 21:23:59 +0530

    refactor

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -87,23 +87,18 @@ class Tensor:
   def __repr__(self):
     return f"Tensor {self.data!r} with grad {(self.grad.data if self.grad else None)!r}"
 
-  def _calc_strides(self, shape):
-    stride = [np.prod(shape[i:]) for i in range(1, len(shape))]
-    stride.append(1)
-    return stride
-
   def _gpu_index(self, index):
+    shape = (1,)
     if isinstance(index, int):
       data = self.data.cl.get_sub_region(index*4, 4)
-      shape = (1,)
     elif isinstance(index , slice):
       data = self.data.cl.get_sub_region(index.start*4, index.stop*4) #4 - float32
       shape = (index.stop - index.start,)
     elif isinstance(index, tuple):
-      _strides = self._calc_strides(self.shape)
+      _strides = [np.prod(shape[i:]) for i in range(1, len(self.shape))]
+      _strides.append(1)
       _origin = sum(map(lambda x: x[0]*x[1]*4, zip(_strides, index)))
       data = self.data.cl.get_sub_region(_origin, 4)
-      shape = (1,)
     gbuffer = GPUBuffer(shape)
     gbuffer.cl = data
     return Tensor(gbuffer, gpu=True, requires_grad=self.requires_grad)
