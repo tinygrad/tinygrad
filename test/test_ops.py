cf3ccafca1bf8c64990973da332f9b7cdc81d7e4
marcelbischoff <mrclbschff@gmail.com>
2020-12-29 00:33:46 -0500

    transpose

--- test/test_ops.py
+++ test/test_ops.py
@@ -121,7 +121,6 @@ class TestOps(unittest.TestCase):
   def test_pad2d(self):
     helper_test_op([(3,3,3,3)], lambda x: torch.nn.functional.pad(x, (1,2,3,4)), lambda x: x.pad2d(padding=(1,2,3,4)), device=self.device)
 
-  @cpu_only
   def test_transpose(self):
     helper_test_op([(3,3,3)], lambda x: x.transpose(1,2), lambda x: x.transpose(order=(0,2,1)), device=self.device)
 
