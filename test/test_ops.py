ea240fbe7cdca09a3041260f52a4175390f82c6f
Kartik Sharma <kartik4949@gmail.com>
2021-01-03 15:22:47 +0530

    no need of device in testing

--- test/test_ops.py
+++ test/test_ops.py
@@ -63,13 +63,13 @@ class TestOps(unittest.TestCase):
   def test_exp(self):
     helper_test_op([(45,65)], lambda x: torch.exp(x), Tensor.exp)
   def test_sigmoid(self):
-    helper_test_op([(45,65)], lambda x: x.sigmoid(), Tensor.sigmoid, device=self.device)
+    helper_test_op([(45,65)], lambda x: x.sigmoid(), Tensor.sigmoid)
   def test_softplus(self):
-    helper_test_op([(45,65)], lambda x: torch.nn.functional.softplus(x), Tensor.softplus, device=self.device, atol=1e-6, grad_atol=1e-6)
+    helper_test_op([(45,65)], lambda x: torch.nn.functional.softplus(x), Tensor.softplus, atol=1e-6, grad_atol=1e-6)
   def test_mish(self):
     def _mish_pytorch(x):
       return x*torch.tanh(torch.nn.functional.softplus(x))
-    helper_test_op([(45,65)], _mish_pytorch, Tensor.mish, device=self.device, atol=1e-6, grad_atol=1e-6)
+    helper_test_op([(45,65)], _mish_pytorch, Tensor.mish, atol=1e-6, grad_atol=1e-6)
   def test_dot(self):
     helper_test_op([(45,65), (65,100)], lambda x,y: x.matmul(y), Tensor.dot)
   def test_multidot(self):
