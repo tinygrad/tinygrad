b87cf34e5dd4dcdb81593555971d91517c67b5cd
George Hotz <geohot@gmail.com>
2021-05-30 17:57:27 -0700

    gucci gang

--- fpga/riscv.sh
+++ fpga/riscv.sh
@@ -2,7 +2,7 @@
 cd out
 riscv64-unknown-elf-as ../src/riscv.asm
 riscv64-unknown-elf-objdump -d a.out
-riscv64-unknown-elf-objcopy -O binary a.out firmware.bin
+riscv64-unknown-elf-objcopy -O binary a.out a.asm
 xxd a.asm
 python -c 'import struct; dat = open("a.asm", "rb").read(); print("\n".join(["%08x" % c for c in struct.unpack("I"*(len(dat)//4), dat)]))' > ../src/firmware.hex
 
--- fpga/src/riscv.asm
+++ fpga/src/riscv.asm
@@ -1,11 +1,16 @@
-li s0, 0x1
+restart:
+li s0, 3 
 lui s1, 0x2000
-li s3, 0x100
+/*li s3, 0x100*/
 
 addr_c:
-addi s0,s0,0x1
+sll s0,s0,1
+and s0,s0,0xFF
+beq s0,zero,restart
+
+/*addi s0,s0,0x1
 blt s0,s3,addr_18
-li s0, 0x2
+li s0, 2 
 
 addr_18:
 li s2, 0x2
@@ -17,7 +22,7 @@ mv a1, s2
 jal ra, mod_or_something
 beq a0, zero, addr_40
 addi s2, s2, 0x1
-j addr_1c
+j addr_1c*/
 
 addr_38:
 sw s0, 0x0(s1)
@@ -27,17 +32,17 @@ addr_40:
 j addr_c
 
 
-mod_or_something:
+/*mod_or_something:
 li t0,0x1
 
 addr_48:
 sub a0,a0,a1
 bge a0,t0,addr_48
 
-ret
+ret*/
 
 sleep_fixed:
-lui t0,0x44b
+lui t0,0x100
 addi t0,t0,-0x5e0
 addr_5c:
 addi t0,t0,-0x1
