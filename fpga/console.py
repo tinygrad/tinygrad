6aa6b3c117ab56f0b26b860c78805d6cc663779f
George Hotz <geohot@gmail.com>
2021-05-30 19:17:41 -0700

    hello

--- fpga/console.py
+++ fpga/console.py
@@ -6,7 +6,7 @@ port = pyftdi.serialext.serial_for_url('ftdi://ftdi:2232h/2', baudrate=115200)
 print(port)
 
 while 1:
-  port.write(b'a')
+  #port.write(b'a')
   data = port.read(1)
   print(data)
   time.sleep(0.01)
--- fpga/src/main.c
+++ fpga/src/main.c
@@ -10,7 +10,18 @@ int main() {
   reg_uart_clkdiv = 434;
   while (1) {
     for (int i = 1; i < 0x100; i <<= 1) {
-      if (i == 4) reg_uart_data = 'u';
+      if (i == 4) {
+        sdelay();
+        reg_uart_data = 'h';
+        sdelay();
+        reg_uart_data = 'e';
+        sdelay();
+        reg_uart_data = 'l';
+        sdelay();
+        reg_uart_data = 'l';
+        sdelay();
+        reg_uart_data = 'o';
+      }
       reg_leds = i;
       delay();
     }
@@ -22,6 +33,14 @@ void __attribute__ ((noinline)) delay() {
        "lop:"
        "addi t0,t0,-0x1\n"
        "bne t0,zero,lop\n"
+       ::);
+}
+
+void __attribute__ ((noinline)) sdelay() {
+  asm ("lui t0, 0x10\n"
+       "lop2:"
+       "addi t0,t0,-0x1\n"
+       "bne t0,zero,lop2\n"
        ::);
 }
 
