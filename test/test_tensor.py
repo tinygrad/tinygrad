3a91d5434f5b2807f93416b62244b6855ad2c64b
Jacky Lee <39754370+jla524@users.noreply.github.com>
2021-06-19 08:49:13 -0700

    Add dropout test (#265)
    * Add dropout test
    
    * Remove condition where training is false
    
    * Skip dropout test when on GPU
    
    * Revert changes to tensor.py and fix test case
    
    * Revert change on whitespace
    
    * Convert Tensor to cpu for testing
    
    * Fix whitespace in tensor.py

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -64,6 +64,14 @@ class TestTinygrad(unittest.TestCase):
     for x,y in zip(test_tinygrad(), test_pytorch()):
       np.testing.assert_allclose(x, y, atol=1e-5)
 
+  def test_dropout(self):
+    Tensor.training = True
+    n, rate = 1_000_000, 0.1
+    w = Tensor.ones(n).dropout(rate)
+    non_zeros = np.count_nonzero(w.cpu().data)
+    expected = n * (1 - rate)
+    np.testing.assert_allclose(non_zeros, expected, rtol=1e-3)
+
   @unittest.skipUnless(not DEFAULT_DEVICE, "float64 not supported on GPU")
   def test_jacobian(self):
     W = np.random.RandomState(1337).random((10, 5))
