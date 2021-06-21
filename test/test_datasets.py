a330754b960ffcbce1a20d2d510309e2d12cebb4
guglielmocamporese <guglielmocamporese@gmail.com>
2021-06-22 00:31:23 +0200

    improved datasets test

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -6,42 +6,27 @@ from datasets import *
 class TestDatasets(unittest.TestCase):
   def test_mnist(self):
     ds = MNIST(root='./data', train=True, download=True)
-    x, y = next(iter(ds))
-    C, H, W = x.shape
-    assert x.min() >= 0 
-    assert x.max() <= 1.0
-    assert C == 1 
-    assert H == 28 
-    assert W == 28
-    assert x.dtype == np.float32
-    assert y.dtype == np.int32
-    self.__test_dataloader(ds, sample_shape=(C, H, W))
+    self.__test_dataset(ds, sample_shape=(1, 28, 28))
+    self.__test_dataloader(ds, sample_shape=(1, 28, 28))
 
   def test_cifar10(self):
     ds = CIFAR10(root='./data', train=True, download=True)
-    x, y = next(iter(ds))
-    C, H, W = x.shape
-    assert x.min() >= 0 
-    assert x.max() <= 1.0
-    assert C == 3
-    assert H == 32
-    assert W == 32
-    assert x.dtype == np.float32
-    assert y.dtype == np.int32
-    self.__test_dataloader(ds, sample_shape=(C, H, W))
+    self.__test_dataset(ds, sample_shape=(3, 32, 32))
+    self.__test_dataloader(ds, sample_shape=(3, 32, 32))
 
   def test_cifar100(self):
     ds = CIFAR100(root='./data', train=True, download=True)
-    x, y = next(iter(ds))
-    C, H, W = x.shape
+    self.__test_dataset(ds, sample_shape=(3, 32, 32))
+    self.__test_dataloader(ds, sample_shape=(3, 32, 32))
+
+  def __test_dataset(self, dataset, sample_shape):
+    x, y = next(iter(dataset))
+    dims = x.shape
     assert x.min() >= 0 
     assert x.max() <= 1.0
-    assert C == 3
-    assert H == 32
-    assert W == 32
+    assert list(dims) == list(sample_shape)
     assert x.dtype == np.float32
     assert y.dtype == np.int32
-    self.__test_dataloader(ds, sample_shape=(C, H, W))
 
   def __test_dataloader(self, dataset, sample_shape):
     batch_size = 4
@@ -49,10 +34,9 @@ class TestDatasets(unittest.TestCase):
     x, y = next(iter(dl))
     B, *dims = x.shape
     assert B == batch_size
-    assert list(sample_shape) == list(dims)
+    assert list(dims) == list(sample_shape)
     assert x.dtype == np.float32
     assert y.dtype == np.int32
-
 
 if __name__ == '__main__':
   unittest.main()
