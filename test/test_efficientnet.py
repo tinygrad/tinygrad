8113eec4cff35322fc333b900432953b5bc47617
Sebastian Kreft <sk-@users.noreply.github.com>
2021-10-30 19:53:51 -0300

    feat: add efficientnet test (#285)
    Simple test using the Chicken example from https://upload.wikimedia.org/wikipedia/commons/4/41/Chicken.jpg and the image preprocessing from example/efficientnet.py
    
    Note that EfficientNet loads the weights from the internet so running the tests may be slow the first time. We could speed up the tests by caching the /tmp folder.
    
    Fixes #234

---
+++ test/test_efficientnet.py
@@ -0,0 +1,53 @@
+import ast
+import pathlib
+import sys
+import unittest
+
+import numpy as np
+from PIL import Image
+
+from models.efficientnet import EfficientNet
+from tinygrad.tensor import Tensor
+
+
+def _load_labels() -> list[str]:
+  labels_filename = pathlib.Path(__file__).parent / 'efficientnet/imagenet1000_clsidx_to_labels.txt'
+  return ast.literal_eval(labels_filename.read_text())
+
+
+_LABELS = _load_labels()
+
+
+def _infer(model: EfficientNet, img) -> str:
+  # preprocess image
+  aspect_ratio = img.size[0] / img.size[1]
+  img = img.resize((int(224*max(aspect_ratio,1.0)), int(224*max(1.0/aspect_ratio,1.0))))
+
+  img = np.array(img)
+  y0, x0 =(np.asarray(img.shape)[:2] - 224) // 2
+  img = img[y0: y0 + 224, x0: x0 + 224]
+
+  # low level preprocess
+  img = np.moveaxis(img, [2, 0, 1], [0, 1, 2])
+  img = img.astype(np.float32)[:3].reshape(1, 3, 224, 224)
+  img /= 255.0
+  img -= np.array([0.485, 0.456, 0.406]).reshape((1, -1, 1, 1))
+  img /= np.array([0.229, 0.224, 0.225]).reshape((1, -1, 1, 1))
+
+  # run the net
+  out = model.forward(Tensor(img)).cpu()
+  class_id = np.argmax(out.data)
+  return _LABELS[np.argmax(out.data)]
+
+
+class TestEfficientNet(unittest.TestCase):
+  def test_chicken(self):
+    chicken_img = Image.open(pathlib.Path(__file__).parent / 'efficientnet/Chicken.jpg')
+    model = EfficientNet(number=0)
+    model.load_weights_from_torch()
+    label = _infer(model, chicken_img)
+    self.assertEqual(label, "hen", f"Expected hen but got {label} for number=0")
+
+
+if __name__ == '__main__':
+  unittest.main()
