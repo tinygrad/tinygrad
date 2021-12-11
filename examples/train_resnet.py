32d42c2667ce7e7231fc9197f2975eafb2663935
Colman Glagovich <cjg91@pitt.edu>
2021-12-11 13:52:57 -0500

    moved ResNet depth specifics into the class

--- examples/train_resnet.py
+++ examples/train_resnet.py
@@ -7,7 +7,7 @@ from PIL import Image
 from tinygrad.tensor import Device
 from extra.utils import get_parameters
 from extra.training import train, evaluate
-from models.resnet import ResNet18, ResNet34, ResNet50
+from models.resnet import ResNet
 from tinygrad.optim import Adam
 from datasets import fetch_mnist
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
   TRANSFER = os.getenv('TRANSFER') is not None
   NUM = int(os.getenv('NUM', '18'))
-  model = eval(f'ResNet{NUM}(num_classes={classes})')
+  model = ResNet(NUM, num_classes=classes)
   if TRANSFER:
     model.load_from_pretrained()
 
