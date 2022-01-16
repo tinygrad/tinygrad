52918fbf789d65ca4f1e1d4f396a92cff2c39cd0
George Hotz <george@comma.ai>
2022-01-15 19:02:14 -0800

    cuda stub

---
+++ accel/cuda/ops_cuda.py
@@ -0,0 +1,27 @@
+# pip3 install pycuda
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+import numpy as np
+
+class CudaBuffer:
+  def __init__(self, shape, hostbuf=None):
+    # TODO: these are generic
+    self.shape = shape
+    self.sz = int(np.prod(shape)*4)
+
+    self.buf = cuda.mem_alloc(self.sz)
+    if hostbuf is not None:
+      cuda.memcpy_htod(self.buf, hostbuf)
+
+  # TODO: this is generic
+  @staticmethod
+  def fromCPU(data):
+    return CudaBuffer(data.shape, data)
+
+  def toCPU(self):
+    ret = numpy.empty(self.shape)
+    cuda.memcpy_dtoh(ret, self.buf)
+    return ret
+
+
