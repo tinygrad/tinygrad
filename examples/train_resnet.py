7025c9bbeb55486ba93d29afca5845988d3522fa
cjg91 <90344000+cjg91@users.noreply.github.com>
2022-01-15 20:22:10 -0800

    Transfer learning for ResNet (#295)
    * Transfer learning for ResNet
    
    * moved ResNet depth specifics into the class

--- examples/train_resnet.py
+++ examples/train_resnet.py
@@ -7,9 +7,9 @@ from PIL import Image
 from tinygrad.tensor import Device
 from extra.utils import get_parameters
 from extra.training import train, evaluate
-from models.resnet import ResNet18, ResNet34, ResNet50
+from models.resnet import ResNet
 from tinygrad.optim import Adam
-from test.test_mnist import fetch_mnist
+from datasets import fetch_mnist
 
 from tinygrad.optim import Adam
 
@@ -23,11 +23,17 @@ class ComposeTransforms:
     return x
 
 if __name__ == "__main__":
-  model = ResNet18(num_classes=10, pretrained=True)
-
   X_train, Y_train, X_test, Y_test = fetch_mnist()
   X_train = X_train.reshape(-1, 28, 28).astype(np.uint8)
   X_test = X_test.reshape(-1, 28, 28).astype(np.uint8)
+  classes = 10
+
+  TRANSFER = os.getenv('TRANSFER') is not None
+  NUM = int(os.getenv('NUM', '18'))
+  model = ResNet(NUM, num_classes=classes)
+  if TRANSFER:
+    model.load_from_pretrained()
+
   lr = 5e-5
   transform = ComposeTransforms([
     lambda x: [Image.fromarray(xx, mode='L').resize((64, 64)) for xx in x],
@@ -40,4 +46,4 @@ if __name__ == "__main__":
     train(model, X_train, Y_train, optim, 50, BS=32, transform=transform)
     acc, Y_test_preds = evaluate(model, X_test, Y_test, num_classes=10, return_predict=True, transform=transform)
     lr /= 1.2
-    print(f'reducing lr to {lr:.4f}')
+    print(f'reducing lr to {lr:.7f}')
--- models/resnet.py
+++ models/resnet.py
@@ -52,17 +52,35 @@ class Bottleneck:
     return out
 
 class ResNet:
-  def __init__(self, block, num_blocks, num_classes=10, url=None):
-    self.url = url
+  # def __init__(self, block, num_blocks, num_classes=10, url=None):
+  def __init__(self, num, num_classes):
+    self.num = num
+
+    self.block = {
+      18: BasicBlock,
+      34: BasicBlock,
+      50: Bottleneck,
+      101: Bottleneck,
+      152: Bottleneck
+    }[num]
+
+    self.num_blocks = {
+      18: [2,2,2,2],
+      34: [3,4,6,3],
+      50: [3,4,6,3],
+      101: [3,4,23,3],
+      152: [3,8,36,3]
+    }[num]
+
     self.in_planes = 64
 
     self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, bias=False, padding=3)
     self.bn1 = nn.BatchNorm2D(64)
-    self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=2)
-    self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-    self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-    self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-    self.fc = {"weight": Tensor.uniform(512 * block.expansion, num_classes), "bias": Tensor.zeros(num_classes)}
+    self.layer1 = self._make_layer(self.block, 64, self.num_blocks[0], stride=2)
+    self.layer2 = self._make_layer(self.block, 128, self.num_blocks[1], stride=2)
+    self.layer3 = self._make_layer(self.block, 256, self.num_blocks[2], stride=2)
+    self.layer4 = self._make_layer(self.block, 512, self.num_blocks[3], stride=2)
+    self.fc = {"weight": Tensor.uniform(512 * self.block.expansion, num_classes), "bias": Tensor.zeros(num_classes)}
 
   def _make_layer(self, block, planes, num_blocks, stride):
     strides = [stride] + [1] * (num_blocks-1)
@@ -86,18 +104,32 @@ class ResNet:
     return self.forward(x)
 
   def load_from_pretrained(self):
-    # TODO: replace with fake torch load
+    # TODO replace with fake torch load
+  
+    model_urls = {
+      18: 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+      34: 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+      50: 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+      101: 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+      152: 'https://download.pytorch.org/models/resnet152-b121ed2d.pth'
+    }
+
+    self.url = model_urls[self.num]
+
     from torch.hub import load_state_dict_from_url
     state_dict = load_state_dict_from_url(self.url, progress=True)
     for k, v in state_dict.items():
       obj = get_child(self, k)
       dat = v.detach().numpy().T if "fc.weight" in k else v.detach().numpy()
-      assert obj.shape == dat.shape
+
+      if 'fc.' in k and obj.shape != dat.shape:
+        continue # Skip FC if transfer learning
+
+      assert obj.shape == dat.shape, (k, obj.shape, dat.shape)
       obj.assign(dat)
 
-ResNet18 = lambda: ResNet(BasicBlock, [2,2,2,2], 1000, 'https://download.pytorch.org/models/resnet18-5c106cde.pth')
-ResNet34 = lambda: ResNet(BasicBlock, [3,4,6,3], 1000, 'https://download.pytorch.org/models/resnet34-333f7ec4.pth')
-ResNet50 = lambda: ResNet(Bottleneck, [3,4,6,3], 1000, 'https://download.pytorch.org/models/resnet50-19c8e357.pth')
-ResNet101 = lambda: ResNet(Bottleneck, [3,4,23,3], 1000, 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
-ResNet101 = lambda: ResNet(Bottleneck, [3,8,36,3], 1000, 'https://download.pytorch.org/models/resnet152-b121ed2d.pth')
-
+ResNet18 = lambda num_classes=1000: ResNet(18, num_classes=num_classes)
+ResNet34 = lambda num_classes=1000: ResNet(34, num_classes=num_classes)
+ResNet50 = lambda num_classes=1000: ResNet(50, num_classes=num_classes)
+ResNet101 = lambda num_classes=1000: ResNet(101, num_classes=num_classes)
+ResNet152 = lambda num_classes=1000: ResNet(152, num_classes=num_classes)
