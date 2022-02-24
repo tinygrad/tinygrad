0f1b4eeca277abe41ac8517009586b211424c277
Rylan Justice <98350503+rylan-justice@users.noreply.github.com>
2022-02-24 15:40:45 -0500

    Update build.sh

--- accel/ane/3_run/build.sh
+++ accel/ane/3_run/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 clang++ test.mm -F /System/Library/PrivateFrameworks/ -framework ANEServices -framework IOSurface -framework Foundation -framework IOKit
 codesign --entitlements entitlements.xml -s "Taylor Swift Child" a.out
-
