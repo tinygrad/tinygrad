4cc64aef0b51b9220cc6158b6364fb68e2ec82c1
Rylan Justice <98350503+rylan-justice@users.noreply.github.com>
2022-02-24 14:51:41 -0500

    Update efficientnet.py

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -133,7 +133,6 @@ class EfficientNet:
       7: "https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b7-dcc49843.pth"
     }
 
-
     b0 = fake_torch_load(fetch(model_urls[self.number]))
     for k,v in b0.items():
       for cat in ['_conv_head', '_conv_stem', '_depthwise_conv', '_expand_conv', '_fc', '_project_conv', '_se_reduce', '_se_expand']:
@@ -151,4 +150,3 @@ class EfficientNet:
         mv.assign(vnp)
       else:
         print("MISMATCH SHAPE IN %s, %r %r" % (k, mv.shape, vnp.shape))
-
