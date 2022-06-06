d8ee8a39ace9fc5655cf4443f50e060cd50c95a9
George Hotz <geohot@gmail.com>
2022-06-06 09:45:37 -0700

    sgd threestep graph is so pretty

--- test/test_mnist.py
+++ test/test_mnist.py
@@ -51,6 +51,12 @@ class TestMNIST(unittest.TestCase):
     optimizer = optim.SGD(model.parameters(), lr=0.001)
     train(model, X_train, Y_train, optimizer, BS=69, steps=1)
 
+  def test_sgd_threestep(self):
+    np.random.seed(1337)
+    model = TinyBobNet()
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    train(model, X_train, Y_train, optimizer, BS=69, steps=3)
+
   def test_adam_onestep(self):
     np.random.seed(1337)
     model = TinyBobNet()
