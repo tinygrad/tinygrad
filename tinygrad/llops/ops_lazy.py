80bd24bbfca60751a732f36b3de7a7a591e6b911
George Hotz <george@comma.ai>
2022-06-16 19:20:44 -0700

    shape correct

--- tinygrad/llops/ops_lazy.py
+++ tinygrad/llops/ops_lazy.py
@@ -1,6 +1,7 @@
 from matplotlib.pyplot import isinteractive
 from tinygrad.shapetracker import ShapeTracker
 import numpy as np
+from copy import deepcopy
 
 # TODO: these aren't really ops
 from tinygrad.ops import BinaryOps, MovementOps, UnaryOps, log_op
@@ -11,6 +12,7 @@ class LazyBuffer:
     self.dtype = np.float32
 
     # if we got a movement op, push it above any UnaryOp/BinaryOp
+    # this is done on construction
     if isinstance(op, MovementOps):
       if isinstance(src[0].op, BinaryOps) or isinstance(src[0].op, UnaryOps):
         mop = op
@@ -23,6 +25,7 @@ class LazyBuffer:
 
   def realize(self):
     if self.did_realize or self.op is None: return
+    self.did_realize = True
     for s in self.src:
       s.realize()
     log_op(self.op, self, self.src)
@@ -41,4 +44,5 @@ class Ops:
   def binary_op(ctx, op, x, y): return LazyBuffer(x.shape, op, [x,y])
   def reduce_op(ctx, op, x, new_shape): return LazyBuffer(new_shape).op(op, [x], new_shape)
   def movement_op(ctx, op, x, arg): return LazyBuffer(ShapeTracker(*x.shape).movement_op(op, arg).shape, op, [x], arg)
-  def processing_op(ctx, op, x, w, C): return LazyBuffer((C.bs, C.cout, C.oy, C.ox), op, [x,w], C)
+  #def processing_op(ctx, op, x, w, C): return LazyBuffer((C.bs, C.cout, C.oy, C.ox), op, [x,w], C)
+  def processing_op(ctx, op, x, w, C): return LazyBuffer((C.bs*C.oy, C.ox*C.cout//4, 4), op, [x,w], C)
