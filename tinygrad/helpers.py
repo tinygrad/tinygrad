5a710ff609d9f9392d1a2e02348eb1d480b66ca2
Gabriel <73286903+korwi@users.noreply.github.com>
2022-06-17 22:39:52 +0100

    Update helpers.py

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -3,8 +3,7 @@ from collections import namedtuple
 
 def prod(x): return int(np.prod(x))
 
-def reduce_shape(shape, axis):
-  return [1 if i in axis else shape[i] for i in range(len(shape))]
+def reduce_shape(shape, axis): return [1 if i in axis else shape[i] for i in range(len(shape))]
 
 def get_conv_args(x_shape, w_shape, stride=1, groups=1, padding=0, dilation=1):
   # TODO: https://docs.nvidia.com/deeplearning/performance/dl-performance-convolutional/index.html#tensor-layout
@@ -17,8 +16,7 @@ def get_conv_args(x_shape, w_shape, stride=1, groups=1, padding=0, dilation=1):
   bs,cin_,iy,ix = x_shape
   # TODO: should be easy to support asymmetric padding by changing output size
   # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html describes these sizes well
-  oy = (iy + 2*py - dy * (H-1) - 1)//ys + 1
-  ox = (ix + 2*px - dx * (W-1) - 1)//xs + 1
+  oy, ox = (iy + 2*py - dy * (H-1) - 1)//ys + 1, (ix + 2*px - dx * (W-1) - 1)//xs + 1
   if cin*groups != cin_: raise Exception(f"Input Tensor shape {x_shape} does not match the shape of the weights {w_shape}. ({cin*groups} vs. {cin_})")
   assert cout % groups == 0
   return conv_args(H, W, groups, cout//groups, cin, oy, ox, iy, ix, ys, xs, bs, cout, py, px, dy, dx)
