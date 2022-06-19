4c00457de895fa5274f6072faaa372b407d11b36
George Hotz <geohot@gmail.com>
2022-06-19 08:33:38 -0700

    improve logging

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -23,7 +23,7 @@ if GRAPH:
   atexit.register(save_graph_exit)
 
 global_num_max = 0
-def log_op(op, ret, inp):
+def log_op(optype, op, ret, inp):
   if DEBUG: print(f"{op} : {', '.join([str(x.shape) for x in inp])} -> {ret.shape}")
   if GRAPH:
     def nm(x):
@@ -33,27 +33,28 @@ def log_op(op, ret, inp):
         global_num_max += 1
       return f"<<< {x.global_num} >>>"
 
-    top,sop = str(op).split(".")
-    top_colors = {"UnaryOps": "#c0c0c0", "ReduceOps": "#8080ff", "BinaryOps": "#c0c0c0", "MovementOps": "#80ff80", "ProcessingOps": "#ff8080"}
+    _,sop = str(op).split(".")
+    top_colors = {LoadOps: "#ffffff", UnaryOps: "#c0c0c0", ReduceOps: "#8080ff", BinaryOps: "#c0c0c0", MovementOps: "#80ff80", ProcessingOps: "#ff8080"}
 
     for x in inp:
       G.add_edge(nm(x), nm(ret), label=sop)
       if 'label' not in G.nodes[nm(x)]: G.nodes[nm(x)]['label'] = str(x.shape)
+    if nm(ret) not in G.nodes: G.add_node(nm(ret))
     G.nodes[nm(ret)]['label'] = str(ret.shape)
-    G.nodes[nm(ret)]['fillcolor'] = top_colors[top]
+    G.nodes[nm(ret)]['fillcolor'] = top_colors[optype]
     G.nodes[nm(ret)]['style'] = 'filled'
 
 class Ops:
   def unary_op(ctx, op:UnaryOps, x):
     ret = ctx.op.unary_op(op, x)
-    log_op(op, ret, [x])
+    if 'LAZY' not in ctx.device: log_op(UnaryOps, op, ret, [x])
     assert isinstance(ret, ctx.buffer)
     assert ret.shape == x.shape
     return ret
 
   def reduce_op(ctx, op:ReduceOps, x, new_shape):
     ret = ctx.op.reduce_op(op, x, tuple(new_shape))
-    log_op(op, ret, [x])
+    if 'LAZY' not in ctx.device: log_op(ReduceOps, op, ret, [x])
     assert isinstance(ret, ctx.buffer)
     assert ret.shape == tuple(new_shape)
     return ret
@@ -61,21 +62,21 @@ class Ops:
   def binary_op(ctx, op:BinaryOps, x, y):
     assert x.shape == y.shape
     ret = ctx.op.binary_op(op, x, y)
-    log_op(op, ret, [x, y])
+    if 'LAZY' not in ctx.device: log_op(BinaryOps, op, ret, [x, y])
     assert isinstance(ret, ctx.buffer)
     assert ret.shape == x.shape
     return ret
 
   def movement_op(ctx, op:MovementOps, x, arg):
     ret = ctx.op.movement_op(op, x, tuple(arg))
-    log_op(op, ret, [x])
+    if 'LAZY' not in ctx.device: log_op(MovementOps, op, ret, [x])
     assert isinstance(ret, ctx.buffer)
     assert ret.shape == ShapeTracker(*x.shape).movement_op(op, arg).shape
     return ret
 
   def processing_op(ctx, op:ProcessingOps, x, y, C):
     ret = ctx.op.processing_op(op, x, y, C)
-    log_op(op, ret, [x, y])
+    if 'LAZY' not in ctx.device: log_op(ProcessingOps, op, ret, [x, y])
     assert isinstance(ret, ctx.buffer)
     assert ret.shape == (C.bs, C.cout, C.oy, C.ox)
     return ret
