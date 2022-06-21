21c4205613fbed17cf3e99700915ce4798139f64
George Hotz <geohot@gmail.com>
2022-06-21 15:52:53 -0700

    runs, timing

--- openpilot/run_thneed.cc
+++ openpilot/run_thneed.cc
@@ -1,8 +1,15 @@
 #include "thneed.h"
+#include "common/timing.h"
 
 int main(int argc, char *argv[]) {
   auto thneed = new Thneed(true);
   thneed->record = false;
   thneed->load(argv[1]);
-  thneed->clexec();
+
+  for (int i = 0; i < 5; i++) {
+    uint64_t sb = nanos_since_boot();
+    thneed->clexec();
+    uint64_t et = nanos_since_boot();
+    printf("run in %.2f ms\n", (et-sb)*1e-6);
+  }
 }
--- openpilot/test.sh
+++ openpilot/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 ./compile.py
-THNEED_DEBUG=1 ./run_thneed /tmp/output.thneed
+#THNEED_DEBUG=1 ./run_thneed /tmp/output.thneed
+./run_thneed /tmp/output.thneed
