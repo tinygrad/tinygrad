51756360a91e52a194ba7068a8bdfb38ddd6ec47
George Hotz <geohot@gmail.com>
2022-06-21 10:23:01 -0700

    tests pass

--- accel/opencl/ops_opencl.py
+++ accel/opencl/ops_opencl.py
@@ -1,50 +1,46 @@
-# this is focused on speed
-# it may not run everything
+from __future__ import annotations
+import pyopencl as cl
+from tinygrad.llops.ops_gpu import GPUBuffer, get_cl_ctx, get_cl_queue, CLProgram
+from tinygrad.ops import ProcessingOps
+from tinygrad.helpers import prod, ConvArgs
+from typing import List, Tuple, Optional
+import numpy as np
 
 import pathlib
-import numpy as np
-from tinygrad.ops import MovementOps, ProcessingOps
-from tinygrad.llops.ops_gpu import require_init_gpu, clbuild, get_cl_queue, get_cl_ctx
-from tinygrad.llops.ops_gpu import contiguous
-from tinygrad.llops.ops_gpu import unary_op as unary_op_gpu, binary_op as binary_op_gpu, reduce_op as reduce_op_gpu
-from tinygrad.helpers import prod
-from tinygrad.shapetracker import ShapeTracker
-import pyopencl as cl
-from copy import deepcopy
+def load(x):
+   with open(x) as f:
+     ret = f.read()
+   return ret
+CONV_SRC = load(pathlib.Path(__file__).parent.parent.parent / 'accel/opencl/conv.cl')
+
+def get_replacements(prg_src, opencl_type):
+  middle_code = []
+  vv = "xyzw"
+  for i in range(4):
+    acc = f"outputValues[i].{vv[i%4]}"
+    args = [x.split(" ")[-1].replace("*", "") for x in opencl_type]
+    args = [f"(outputRow * get_image_width(output) + outputLocation.x)*4+{i}", acc]+args
+    middle_code.append(f"{acc} = _ewop("+', '.join(args)+");\n")
+
+  replacements = {}
+  if len(opencl_type) != 0:
+    replacements["//PREFIX"] = prg_src
+    replacements["//ARGS"] = ","+','.join(opencl_type)
+    replacements["//BINOP"] = ''.join(middle_code)
+  return replacements
 
 def roundup(x, n=4): return (x+(n-1))//n * n
-def flip(x): return (x[1], x[0])
-class OpenCLBuffer:
-  def __init__(self, shape, hostbuf=None, _buf=None, _image=None):
-    require_init_gpu()
-    self.shapetracker = deepcopy(shape) if isinstance(shape, ShapeTracker) else ShapeTracker(*shape)
-    self._buf = _buf
-    self._image = _image
-    self.dtype = np.float32
-    if hostbuf is not None:
-      # TODO: lazy?
-      self._buf = cl.Buffer(get_cl_ctx(), cl.mem_flags.READ_WRITE, 4*roundup(prod(shape)))
-      cl.enqueue_copy(get_cl_queue(), self._buf, hostbuf.astype(np.float32).ravel())
-
-  def clone(self):
-    return OpenCLBuffer(self.shapetracker, _buf=self._buf, _image=self._image)
-
-  @property
-  def shape(self): return self.shapetracker.shape
+class OpenCLBuffer(GPUBuffer):
+  def __init__(self, shape, hostbuf:Optional[OpenCLBuffer]=None):
+    super().__init__(shape, hostbuf)
+    self._image = hostbuf._image if hostbuf is not None else None
 
   @staticmethod
   def fromCPU(x):
-    return OpenCLBuffer(x.shape, x)
-
-  def toCPU(self):
-    data = np.empty(self.shape, dtype=np.float32)
-    if self.shapetracker.contiguous == False:
-      tmp = OpenCLBuffer(self.shapetracker.shape)
-      contiguous(None, self, self.shapetracker, tmp)
-    else:
-      tmp = self
-    cl.enqueue_copy(get_cl_queue(), data, tmp.cl, is_blocking=True)
-    return data
+    ret = OpenCLBuffer(x.shape)
+    # TODO: this is blocking even though we told it not to
+    cl.enqueue_copy(get_cl_queue(), ret.cl, x.view(np.ndarray).astype(np.float32).ravel(), is_blocking=False)
+    return ret
 
   @property
   def cl(self):
@@ -52,8 +48,8 @@ class OpenCLBuffer:
       self._buf = cl.Buffer(get_cl_ctx(), cl.mem_flags.READ_WRITE, 4*roundup(prod(self.shape)))
       if self._image is not None:
         assert prod(self.shape) == prod(self._image.shape)*4
-        print(f"converting {self.shape} back to buffer, image shape is {self._image.shape}")
-        clbuild("from_image", """
+        #print(f"converting {self.shape} back to buffer, image shape is {self._image.shape}")
+        CLProgram("from_image", """
           __kernel void from_image(
               read_only image2d_t in,
               __global float4 *out) {
@@ -73,11 +69,11 @@ class OpenCLBuffer:
     if self._image is None:
       assert self.shape[2] == 4 and len(self.shape) == 3
       fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.FLOAT)
-      self._image = cl.Image(get_cl_ctx(), cl.mem_flags.READ_WRITE, fmt, shape=flip(self.shape))
+      self._image = cl.Image(get_cl_ctx(), cl.mem_flags.READ_WRITE, fmt, shape=(self.shape[1], self.shape[0]))
       if self._buf is not None:
         assert prod(self.shape) == prod(self._image.shape)*4
-        print(f"converting {self.shape} to image with shape {self._image.shape}")
-        clbuild("to_image", """
+        #print(f"converting {self.shape} to image with shape {self._image.shape}")
+        CLProgram("to_image", """
           __kernel void to_image(
               __global const float4 *in,
               write_only image2d_t out) {
@@ -91,67 +87,45 @@ class OpenCLBuffer:
       self._buf = None
     return self._image
 
-def unary_op(ctx, op, x):
-  # TODO: this doesn't actually have to be contiguous
-  x = contiguous(ctx, x, x.shapetracker) if not x.shapetracker.contiguous else x
-  return unary_op_gpu(ctx, op, x)
+  def movement_op(x, op, arg):
+    x.cl
+    # TODO: call super after x.cl
+    ret = type(x)(x.st, x)
+    ret.shape = ret.st.movement_op(op, arg).shape
+    return ret
 
-def binary_op(ctx, op, x, y):
-  x = contiguous(ctx, x, x.shapetracker) if not x.shapetracker.contiguous else x
-  y = contiguous(ctx, y, y.shapetracker) if not y.shapetracker.contiguous else y
-  return binary_op_gpu(ctx, op, x, y)
+  def processing_op(x, op, w, C:ConvArgs):
+    assert op == ProcessingOps.CONV, f"{op} isn't supported"
+    return type(x)(C.out_shape)._processing_op_cl([("input", x), ("weight", w)], "acc", C)
 
-def reduce_op(ctx, op, x, new_shape):
-  x = contiguous(ctx, x, x.shapetracker) if not x.shapetracker.contiguous else x
-  return reduce_op_gpu(ctx, op, x, new_shape)
+  def _processing_op_cl(ret, bufs: List[Tuple[str, GPUBuffer]]=[], code:str="acc", C=None):
+    assert bufs[0][0] == "input" and bufs[1][0] == "weight"
+    x,w = bufs[0][1], bufs[1][1]
+    ewbufs = bufs[2:]
 
-def movement_op(ctx, op, x, arg=None):
-  xc = x.clone()
-  # convert from image if the buffer can change shape
-  if op in [MovementOps.EXPAND, MovementOps.SLICE]: xc.cl
-  xc.shapetracker.movement_op(op, arg)
-  if not xc.shapetracker.contiguous: return contiguous(ctx, xc, xc.shapetracker)
-  else: return xc
+    elementwise_prefix = '\n'.join([buf.contiguous_view(name) for name, buf in ewbufs])+ \
+      "inline float _ewop("+','.join(["int gid", "float acc"]+[f"__global const float *{name}_g" for name, _ in ewbufs])+") {"+ \
+      '\n'.join([f"float {name} = get_{name}({name}_g, gid);" for name, _ in ewbufs])+ \
+      f"return {code}; }}"
 
-def load(x):
-  with open(x) as f:
-    ret = f.read()
-  return ret
+    replacements = get_replacements(elementwise_prefix, [f"__global const float *{name}_g" for name, _ in ewbufs] )
 
-def conv(x,w,ret,C):
-  print(x.shapetracker.expr(), w.shapetracker.expr())
-  print(x.shape, w.shape, ret.shape)
-  options = []
-  if C.cin == 1: options.append("-DDEPTHWISE")
-  if C.bs > 1:
-    options.append("-DBATCH")
-    assert C.py == 0, "batched conv doesn't work with y-padding"
-  conv_prg = clbuild("conv", load(pathlib.Path(__file__).parent.parent.parent / 'accel/opencl/conv.cl'), tuple(options))
-  assert C.cout%4 == 0
-  kernel_args = [C.cout//4, (C.ox+3)//4, C.bs*C.oy]
-  conv_args = [max(1, C.cin//4), C.groups*C.cin//4, max(1, C.rcout//4), C.cout//4, C.ox, C.oy, C.iy, C.W, C.H, C.px, C.py, C.xs, C.ys, C.dx, C.dy]
-  print(conv_args, kernel_args)
-  conv_prg(kernel_args, None, x.image, w.image, ret.image, *[np.int16(x) for x in conv_args])
+    x, w = x.contiguous_op(), w.contiguous_op()
+    options = []
+    if C.cin == 1: options.append("-DDEPTHWISE")
+    if C.bs > 1:
+      options.append("-DBATCH")
+      assert C.py == 0, "batched conv doesn't work with y-padding"
+    assert C.cout%4 == 0
+    conv_src = CONV_SRC
+    for k,v in replacements.items():
+      conv_src = conv_src.replace(k, v)
+    #print(conv_src)
+    conv_prg = CLProgram("image_conv", conv_src,
+      options=tuple(options),
+      argdtypes=tuple([None, None, None] + [np.int16]*15 + [None]*len(ewbufs))
+    )
+    conv_args = [max(1, C.cin//4), C.groups*C.cin//4, max(1, C.rcout//4), C.cout//4, C.ox, C.oy, C.iy, C.W, C.H, C.px, C.py, C.xs, C.ys, C.dx, C.dy]
+    conv_prg([C.cout//4, (C.ox+3)//4, C.bs*C.oy], None, x.image, w.image, ret.image, *conv_args, *[buf.cl for _, buf in ewbufs])
 
-
-def processing_op(ctx,op,x,w,out_shape,C):
-  assert op == ProcessingOps.CONV, f"{op} isn't supported"
-  ret = ctx.buffer((C.bs*C.oy, C.ox*C.cout//4, 4))
-  conv(x, w, ret, C)
-  return ret
-
-
-def test_image():
-  hostbuf = np.random.randn(5,8,4).astype(np.float32)
-  x = OpenCLBuffer((5,8,4), hostbuf)
-  assert np.allclose(x.toCPU(), hostbuf)
-  print(x.image)
-  assert np.allclose(x.toCPU(), hostbuf)
-
-if __name__ == "__main__":
-  test_image()
-
-
-
-
-
+    return ret
