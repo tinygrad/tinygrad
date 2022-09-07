5a76e652b874008ef145356735c0d61ebe95c061
George Hotz <george@comma.ai>
2022-09-06 17:27:33 -0700

    simpler movement op

--- tinygrad/llops/ops_torch.py
+++ tinygrad/llops/ops_torch.py
@@ -4,7 +4,7 @@ from tinygrad.ops import ProcessingOps
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class TorchBuffer(torch.Tensor):
-  def custompad(x, padding): return torch.nn.functional.pad(x, [item for sublist in padding[::-1] for item in sublist])
+  def pad(x, padding): return torch.nn.functional.pad(x, [item for sublist in padding[::-1] for item in sublist])
 
   @staticmethod
   def fromCPU(data): return TorchBuffer(torch.from_numpy(data).requires_grad_(False)).to(device)
