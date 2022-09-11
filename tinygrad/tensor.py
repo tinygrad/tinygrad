ef6c80616b46115e59de0d84977d4917a36c31cb
Ollin Boer Bohan <madebyollin@gmail.com>
2022-09-11 14:46:01 -0700

    OPT is reverted; use original mish

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -270,7 +270,7 @@ class Tensor:
   def gelu(self): return 0.5 * self * (1 + (self * 0.7978845608 * (1 + 0.044715 * self * self)).tanh())
   def quick_gelu(self): return self * (self * 1.702).sigmoid()
   def leakyrelu(self, neg_slope=0.01): return self.relu() - (-neg_slope*self).relu()
-  def mish(self): return self - (2.0 * self) * (2.0 * self.exp() + (2.0 * self).exp() + 2.0).reciprocal()
+  def mish(self): return self * self.softplus().tanh()
   def softplus(self, limit=20, beta=1): return (1/beta) * (1 + (self*beta).exp()).log()
 
   # ***** broadcasted binary ops *****
