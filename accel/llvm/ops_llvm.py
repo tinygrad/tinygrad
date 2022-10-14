64078162e80076fbc34f978a25514ccc20204689
George Hotz <george@comma.ai>
2022-10-13 17:27:00 -0700

    progress

--- accel/llvm/ops_llvm.py
+++ accel/llvm/ops_llvm.py
@@ -1,4 +1,5 @@
 # https://github.com/numba/llvmlite/blob/main/llvmlite/ir/builder.py
+from __future__ import annotations
 from typing import Tuple, Union
 from tinygrad.helpers import prod
 from tinygrad.shapetracker import ShapeTracker
@@ -6,6 +7,7 @@ import ctypes
 import numpy as np
 from llvmlite import ir
 from ctypes import CFUNCTYPE, c_double
+from tinygrad.ops import DEBUG, UnaryOps, BinaryOps, ReduceOps, MovementOps
 
 import llvmlite.binding as llvm
 
@@ -14,11 +16,19 @@ class LLVMProgram:
     pass
 
 # TODO: write this
+# TODO: Refactor LLVMBuffer and GPUBuffer into ShapeTrackedBuffer
 class LLVMBuffer:
-  def __init__(self, shape:Union[ShapeTracker, Tuple[int, ...]]):
+  def __init__(self, shape:Union[ShapeTracker, Tuple[int, ...]], hostbuf=None):
     self.st = shape if isinstance(shape, ShapeTracker) else ShapeTracker(tuple(shape))
     self.shape = self.st.shape
-    self._buf = (ctypes.c_float * prod(self.shape))()
+    self._buf = (ctypes.c_float * prod(self.shape))() if hostbuf is None else hostbuf._buf
+
+  # copied from GPUBuffer
+  def movement_op(x, op:MovementOps, arg): return type(x)(ShapeTracker(x.st).movement_op(op, arg), x)
+  def contiguous_op(x): return x if x.st.contiguous else x.unary_op(UnaryOps.NOOP)
+
+  def unary_op(x, op:UnaryOps): return type(x)(x.shape)._dumb_processing_op([x], op)
+  def binary_op(x, op:BinaryOps, y): return type(x)(x.shape)._dumb_processing_op([x, y], op)
 
   @staticmethod
   def fromCPU(x):
@@ -26,14 +36,11 @@ class LLVMBuffer:
     ctypes.memmove(ret._buf, x.ctypes.data, prod(ret.shape)*4)
     return ret
   
-  def toCPU(x): return np.ctypeslib.as_array(x._buf).reshape(x.shape)
+  def toCPU(x): return np.ctypeslib.as_array(x.contiguous_op()._buf).reshape(x.shape)
 
-  def binary_op(x, op, y):
-    ret = LLVMBuffer(x.shape)
-    #cnt = prod(x.shape)
-    #typ = ir.ArrayType(ir.FloatType(), cnt)
+  def _dumb_processing_op(ret, bufs, op):
     typ = ir.PointerType(ir.FloatType())
-    fnty = ir.FunctionType(ir.VoidType(), (typ, typ, typ))
+    fnty = ir.FunctionType(ir.VoidType(), [typ]*(1+len(bufs)))
     module = ir.Module(name=__file__)
     func = ir.Function(module, fnty, name="fpadd")
 
@@ -41,26 +48,32 @@ class LLVMBuffer:
     block = func.append_basic_block(name="inner_loop")
     exit_block = func.append_basic_block(name="exit")
     builder = ir.IRBuilder(block)
+    start_builder = ir.IRBuilder(start_block)
+    start_builder.branch(block)
+    exit_builder = ir.IRBuilder(exit_block)
+    exit_builder.ret_void()
 
-    r, a, b = func.args
-
-    #start = builder.alloca(ir.IntType(32))
     start = ir.Constant(ir.IntType(32), 0)
-    end = ir.Constant(ir.IntType(32), prod(x.shape))
-
+    end = ir.Constant(ir.IntType(32), prod(ret.shape))
     idx = builder.phi(ir.IntType(32))
     idx.add_incoming(start, start_block)
 
-    ap = builder.load(builder.gep(a, [idx]))
-    bp = builder.load(builder.gep(b, [idx]))
-    val = builder.fadd(ap, bp)
-    builder.store(val, builder.gep(r, [idx]))
+    if op == BinaryOps.ADD:
+      r, a, b = func.args
+      ap = builder.load(builder.gep(a, [idx]))
+      bp = builder.load(builder.gep(b, [idx]))
+      val = builder.fadd(ap, bp)
+      builder.store(val, builder.gep(r, [idx]))
+    elif op == UnaryOps.NOOP:
+      r, a = func.args
+      ap = builder.load(builder.gep(a, [idx]))
+      val = ap
+      builder.store(val, builder.gep(r, [idx]))
 
     idx_new = builder.add(idx, ir.Constant(ir.IntType(32), 1))
     idx.add_incoming(idx_new, block)
 
     builder.cbranch(builder.icmp_unsigned("==", idx, end), exit_block, block)
-
 
 
     #idx = builder.add(idx, ir.Constant(ir.IntType(32), 1))
@@ -73,16 +86,24 @@ class LLVMBuffer:
     llvm_ir = str(module)
     print(llvm_ir)
 
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()  # yes, even this one
+
     mod = llvm.parse_assembly(llvm_ir)
     mod.verify()
+    target_machine = llvm.Target.from_default_triple().create_target_machine()
+    print(target_machine.emit_assembly(mod))
 
-    engine = llvm.create_mcjit_compiler(llvm.parse_assembly(""), llvm.Target.from_default_triple().create_target_machine())
+    engine = llvm.create_mcjit_compiler(llvm.parse_assembly(""), target_machine)
     engine.add_module(mod)
     engine.finalize_object()
     engine.run_static_constructors()
 
+
     func_ptr = engine.get_function_address("fpadd")
-    cfunc = CFUNCTYPE(ctypes.c_float * prod(x.shape), ctypes.c_float * prod(x.shape), ctypes.c_float * prod(x.shape))(func_ptr)
-    cfunc(ret._buf, x._buf, y._buf)
+    bufs = [ret] + bufs
+    cfunc = CFUNCTYPE(*[ctypes.c_float * prod(x.shape) for x in bufs])(func_ptr)
+    cfunc(*[x._buf for x in bufs])
 
     return ret
