0d5a450017d1679327fc4e0e98ae3e0ea5a72b18
George Hotz <geohot@gmail.com>
2022-10-16 17:13:38 -0700

    less lines and more powerful

--- accel/llvm/ops_llvm.py
+++ accel/llvm/ops_llvm.py
@@ -26,16 +26,17 @@ def idx_deref(builder, buf, ptr, idx):
         valid = ir.Constant(ir.IntType(1), 1)
       acc = 1
       for s,(x,y) in list(zip(v.old_shape, v.arg))[::-1]:
-        lr = idx
-        if acc != 1:
-          lr = builder.sdiv(lr, int_const(acc))
-        lr = builder.srem(lr, int_const(y-x))
-        if x != 0:
-          lr = builder.add(lr, int_const(x))
-        if x < 0:
-          valid = builder.and_(valid, builder.icmp_signed(">=", lr, int_const(0)))
-        if y > s:
-          valid = builder.and_(valid, builder.icmp_signed("<", lr, int_const(s)))
+        if x < 0 or y > s:
+          lr = idx
+          if acc != 1:
+            lr = builder.sdiv(lr, int_const(acc))
+          lr = builder.srem(lr, int_const(y-x))
+          if x != 0:
+            lr = builder.add(lr, int_const(x))
+          if x < 0:
+            valid = builder.and_(valid, builder.icmp_signed(">=", lr, int_const(0)))
+          if y > s:
+            valid = builder.and_(valid, builder.icmp_signed("<", lr, int_const(s)))
         acc *= y-x
     else:
       acc = 1
@@ -94,15 +95,15 @@ class LLVMBuffer:
   def movement_op(x, op:MovementOps, arg): return type(x)(ShapeTracker(x.st).movement_op(op, arg), x)
 
   # universal
-  def unary_op(x, op:UnaryOps): return type(x)(x.shape).exec_ast(LazyOp(op=op, src=[x]))
-  def binary_op(x, op:BinaryOps, y): return type(x)(x.shape).exec_ast(LazyOp(op=op, src=[x, y]))
-  def reduce_op(x, op:ReduceOps, new_shape:Tuple[int, ...]): return type(x)(new_shape).exec_ast(LazyOp(op=op, src=[x]))
+  def unary_op(x, op:UnaryOps): return type(x)(x.shape).exec_ast(LazyOp(op=op, src=(x,)))
+  def binary_op(x, op:BinaryOps, y): return type(x)(x.shape).exec_ast(LazyOp(op=op, src=(x, y)))
+  def reduce_op(x, op:ReduceOps, new_shape:Tuple[int, ...]): return type(x)(new_shape).exec_ast(LazyOp(op=op, src=(x,)))
   def contiguous_op(x): return x if x.st.contiguous else x.unary_op(UnaryOps.NOOP)
 
   @staticmethod
   def fromCPU(x):
     ret = LLVMBuffer(x.shape)
-    ctypes.memmove(ret._buf, x.ctypes.data, prod(ret.shape)*4)
+    ctypes.memmove(ret._buf, x.astype(np.float32).ctypes.data, prod(ret.shape)*4)
     return ret
   
   def toCPU(x): return np.ctypeslib.as_array(x.contiguous_op()._buf)[:prod(x.shape)].reshape(x.shape).copy()
@@ -111,6 +112,9 @@ class LLVMBuffer:
   def exec_ast(ret, ast:Union[LLVMBuffer, LazyOp]):
     # get the real buffers from the ast
     bufs = get_buffers(ast)
+    reduceops = [x for x in get_lazyops(ast) if isinstance(x.op, ReduceOps)]
+    assert len(reduceops) <= 1, "max one reduce op in an ast"
+    earlybufs = get_buffers(reduceops[0]) if len(reduceops) > 0 else []
 
     if engine is None:
       init_llvm()
@@ -128,34 +132,45 @@ class LLVMBuffer:
     reduce_builder = ir.IRBuilder(func.append_basic_block(name="reduce_loop"))
     store_builder = ir.IRBuilder(func.append_basic_block(name="store_block"))
 
-    if isinstance(ast.op, ReduceOps):
-      red = prod([s for s,n in zip(bufs[0].shape, ret.shape) if n == 1])
+    def ast_parse(builder, x, idx, reduce_result=None):
+      if not isinstance(x, LazyOp):
+        return idx_deref(builder, x, func.args[bufs.index(x)+1], idx)
+      if isinstance(x.op, ReduceOps):
+        if reduce_result is None:
+          raise Exception("no reduce")
+        return reduce_result
+      values = [ast_parse(builder, v, idx, reduce_result) for v in x.src]
+      return LLVMBuffer.op_lookup[x.op](builder, *values)
+
+    if len(reduceops) > 0:
+      red = prod([s for s,n in zip(earlybufs[0].shape, ret.shape) if n == 1])
       red_idx_start = body_builder.mul(idx, int_const(red))
       red_idx_end = body_builder.add(red_idx_start, int_const(red-1))
       red_idx = reduce_builder.phi(ir.IntType(64))
-      red_idx.add_incoming(red_idx_start, body_builder._block)
-
       val = reduce_builder.phi(ir.FloatType())
-      tval = idx_deref(reduce_builder, bufs[0], func.args[1], red_idx)
-      if ast.op == ReduceOps.SUM:
+      red_idx.add_incoming(red_idx_start, body_builder._block)
+      reduce_input = ast_parse(reduce_builder, reduceops[0].src[0], red_idx)
+
+      if reduceops[0].op == ReduceOps.SUM:
         val.add_incoming(ir.Constant(ir.FloatType(), 0), body_builder._block)
-        new_val = reduce_builder.fadd(tval, val)
-      elif ast.op == ReduceOps.MAX:
+        reduce_result = reduce_builder.fadd(reduce_input, val)
+      elif reduceops[0].op == ReduceOps.MAX:
         val.add_incoming(ir.Constant(ir.FloatType(), -math.inf), body_builder._block)
-        new_val = reduce_builder.call(ir.Function(module, ir.FunctionType(ir.FloatType(), [ir.FloatType(), ir.FloatType()]), name="llvm.maxnum.f32"), [tval, val])
-      val.add_incoming(new_val, reduce_builder._block)
-      val = new_val
+        reduce_result = reduce_builder.call(ir.Function(module, ir.FunctionType(ir.FloatType(), [ir.FloatType(), ir.FloatType()]), name="llvm.maxnum.f32"), [reduce_input, val])
+      else:
+        raise Exception(f"unknown ReduceOps {ast.op}")
+      val.add_incoming(reduce_result, reduce_builder._block)
 
       red_idx_p1 = reduce_builder.add(red_idx, int_const(1))
       red_idx.add_incoming(red_idx_p1, reduce_builder._block)
       reduce_builder.cbranch(reduce_builder.icmp_unsigned("==", red_idx, red_idx_end), store_builder._block, reduce_builder._block)
     else:
-      values = [idx_deref(body_builder, buf, ptr, idx) for buf, ptr in zip(bufs, func.args[1:])]
-      val = LLVMBuffer.op_lookup[ast.op](body_builder, *values)
+      reduce_result = None
       reduce_builder.branch(store_builder._block)
 
     body_builder.branch(reduce_builder._block)
-    store_builder.store(val, store_builder.gep(func.args[0], [idx]))
+    result = ast_parse(store_builder, ast, idx, reduce_result)
+    store_builder.store(result, store_builder.gep(func.args[0], [idx]))
     idx_new = store_builder.add(idx, ir.Constant(ir.IntType(64), 1))
     idx.add_incoming(idx_new, store_builder._block)
 
