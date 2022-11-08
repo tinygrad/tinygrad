a7025a01586b8409f67d0b5e99ba094dfd8756af
Jacky Lee <jla524@sfu.ca>
2022-11-08 14:49:52 -0800

    A bit cleaner

--- tinygrad/nn/__init__.py
+++ tinygrad/nn/__init__.py
@@ -63,8 +63,7 @@ class Linear:
 class GroupNorm:
   def __init__(self, num_groups, num_channels, eps=1e-5, affine=True):
     self.num_groups, self.num_channels, self.eps, self.affine = num_groups, num_channels, eps, affine
-    if self.affine:
-      self.weight, self.bias = Tensor.ones(num_channels), Tensor.zeros(num_channels)
+    self.weight, self.bias = (Tensor.ones(num_channels), Tensor.zeros(num_channels)) if affine else (None, None)
 
   def __call__(self, x):
     # reshape for layernorm to work as group norm
@@ -74,8 +73,6 @@ class GroupNorm:
     else:
       x = x.reshape(x.shape[0], self.num_groups, -1).layernorm(eps=self.eps).reshape(x.shape)
 
-    if not self.affine:
-      return x
     # elementwise_affine on channels
     if len(x.shape) == 4:
       # HACK for channels in conv
