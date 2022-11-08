5de593251262310bd3ef4e5a58b6ca65f8e9b22b
uzaaft <muaf@nmbu.no>
2022-11-08 10:47:32 +0100

    fix(extra): Swap list comprehension with sum, and fix indexing

--- extra/introspection.py
+++ extra/introspection.py
@@ -9,7 +9,7 @@ from tinygrad.llops.ops_gpu import CL, GPUBuffer
 def print_objects():
   gc.collect()
   tensors = [x for x in gc.get_objects() if isinstance(x, Tensor)]
-  tensor_ram_used = sum([prod(x.shape)*4 for x in tensors])
+  tensor_ram_used = sum(prod(x.shape)*4 for x in tensors)
   lazybuffers = [x for x in gc.get_objects() if isinstance(x, LazyBuffer)]
   gpubuffers = [x for x in gc.get_objects() if isinstance(x, GPUBuffer)]
   realized_buffers = [x.realized for x in lazybuffers if x.realized]
@@ -24,9 +24,9 @@ def print_objects():
     bb = gc.get_referrers(tb)
     for b in bb:
       if b is not gpubuffers and b is not gpubuffers_orphaned:
-        print(tb, "reference", type(b),len(b), str(b)[0:150])
+        print(tb, "reference", type(b), len(b), str(b)[:150])
         for x in gc.get_referrers(b):
-          print(str(x)[0:100])
+          print(str(x)[:100])
     if cnt == 10:
       break
     cnt += 1
