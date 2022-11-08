2adf7d540f2702060663cee5c01b69883fa8e4bf
uzaaft <muaf@nmbu.no>
2022-11-08 18:31:19 +0100

    chore: fix issue with indentation

--- test/test_gc.py
+++ test/test_gc.py
@@ -6,34 +6,34 @@ from tinygrad.tensor import Tensor
 
 
 def tensors_allocated():
-    return sum(isinstance(x, Tensor) for x in gc.get_objects())
+  return sum(isinstance(x, Tensor) for x in gc.get_objects())
 
 
 class TestGC(unittest.TestCase):
-    def test_gc(self):
-        a = Tensor.zeros(4, 4, requires_grad=True)
-        b = Tensor.zeros(4, 4, requires_grad=True)
-        (a * b).mean().backward()
-        assert tensors_allocated() > 0
-        del a, b
-        assert tensors_allocated() == 0
+  def test_gc(self):
+    a = Tensor.zeros(4, 4, requires_grad=True)
+    b = Tensor.zeros(4, 4, requires_grad=True)
+    (a * b).mean().backward()
+    assert tensors_allocated() > 0
+    del a, b
+    assert tensors_allocated() == 0
 
-    def test_gc_complex(self):
-        a = Tensor.zeros(4, 4, requires_grad=True)
-        b = Tensor.zeros(4, 4, requires_grad=True)
-        assert tensors_allocated() == 2
-        (a * b).mean().backward()
-        assert tensors_allocated() == 4
-        del b
-        assert tensors_allocated() == 2
-        b = Tensor.zeros(4, 4, requires_grad=True)
-        print(tensors_allocated())
-        (a * b).mean().backward()
-        print(tensors_allocated())
-        assert tensors_allocated() == 4
-        del b
-        assert tensors_allocated() == 2
+  def test_gc_complex(self):
+    a = Tensor.zeros(4, 4, requires_grad=True)
+    b = Tensor.zeros(4, 4, requires_grad=True)
+    assert tensors_allocated() == 2
+    (a * b).mean().backward()
+    assert tensors_allocated() == 4
+    del b
+    assert tensors_allocated() == 2
+    b = Tensor.zeros(4, 4, requires_grad=True)
+    print(tensors_allocated())
+    (a * b).mean().backward()
+    print(tensors_allocated())
+    assert tensors_allocated() == 4
+    del b
+    assert tensors_allocated() == 2
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
