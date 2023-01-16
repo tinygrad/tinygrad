dc956878d30e0cfb79f7d175a82d837efa86c26a
George Hotz <geohot@gmail.com>
2023-01-15 16:10:41 -0800

    more tests

--- test/test_symbolic.py
+++ test/test_symbolic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import unittest
-from tinygrad.symbolic import Variable
+from tinygrad.symbolic import Variable, SumNode
 
 class TestSymbolic(unittest.TestCase):
   def test_mul_0(self):
@@ -18,6 +18,21 @@ class TestSymbolic(unittest.TestCase):
   def test_div_1(self):
     ret = Variable("a", 0, 8)//1
     assert str(ret) == "a"
+
+  def test_add_min_max(self):
+    ret = Variable("a", 0, 8) * 2 + 12
+    assert ret.min == 12
+    assert ret.max == 16+12
+
+  def test_div_min_max(self):
+    ret = Variable("a", 0, 7) // 2
+    assert ret.min == 0
+    assert ret.max == 3
+
+  def test_sum_div_min_max(self):
+    ret = SumNode([Variable("a", 0, 7), Variable("b", 0, 3)]) // 2
+    assert ret.min == 0
+    assert ret.max == 5
 
 if __name__ == '__main__':
   unittest.main()
