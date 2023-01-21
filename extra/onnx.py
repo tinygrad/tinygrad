d38fa9dce52aac57aa9b1b1f1809792feee23935
George Hotz <geohot@gmail.com>
2023-01-21 08:48:09 -0800

    insource TENSOR_TYPE_TO_NP_TYPE

--- extra/onnx.py
+++ extra/onnx.py
@@ -1,10 +1,9 @@
 import os
 import numpy as np
-import functools
-from onnx.mapping import TENSOR_TYPE_TO_NP_TYPE
 from tinygrad.tensor import Tensor
 from tinygrad.helpers import prod
 from tinygrad.ops import DEBUG
+TENSOR_TYPE_TO_NP_TYPE = {1:np.float32, 10:np.float16, 7:np.int64}  # from onnx.mapping import TENSOR_TYPE_TO_NP_TYPE
 
 ONNXLIMIT = int(os.getenv("ONNXLIMIT", "-1"))
 
