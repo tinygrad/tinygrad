1557590a931a9419ccd70f49bb82afe3f12b650f
George Hotz <geohot@gmail.com>
2023-01-21 08:57:41 -0800

    move imports out

--- test/lib_test_ast.py
+++ test/lib_test_ast.py
@@ -1,15 +1,15 @@
 import sys
+import numpy as np
 from typing import Dict
 from tinygrad.ast import ASTKernel
+from tinygrad.llops.ops_cpu import CPUBuffer
+from tinygrad.ops import DeviceBuffer
+from tinygrad.lazy import realize_buffers
 
 in_test = False
 def test_ast(k:ASTKernel):
   global in_test
   if in_test: return
-  import numpy as np
-  from tinygrad.llops.ops_cpu import CPUBuffer
-  from tinygrad.ops import DeviceBuffer
-  from tinygrad.lazy import realize_buffers
   in_test = True
   print("testing AST")
   cpubufs : Dict[DeviceBuffer, CPUBuffer] = {x:CPUBuffer.fromCPU(x.toCPU()) for x in k.bufs}
