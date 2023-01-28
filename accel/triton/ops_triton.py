ac2e9746debcc8a22c7571f4dd42ffe2dd265da1
George Hotz <geohot@gmail.com>
2023-01-27 17:32:29 -0800

    ops triton working

--- accel/triton/ops_triton.py
+++ accel/triton/ops_triton.py
@@ -3,22 +3,76 @@ import torch
 import hashlib
 import numpy as np
 
-from typing import Union, Tuple, Optional
-from tinygrad.ops import ExplicitExecAST, LazyOp
+from typing import Union, Tuple, Optional, Dict
+from tinygrad.ops import UnaryOps, BinaryOps, ReduceOps, LazyOp, Op, ExplicitExecAST, DEBUG
 from tinygrad.shape import ShapeTracker
 from tinygrad.helpers import prod
 from tinygrad.ast import ASTKernel
 
+from tinygrad.shape import View, ZeroView
+from tinygrad.shape.symbolic import Variable
+
 class TritonASTKernel(ASTKernel):
+  code_for_op : Dict[Op, str] = {
+    UnaryOps.NOOP: "(A)", UnaryOps.NEG: "(-(A))", UnaryOps.RELU: "max(A, (float)0.)", UnaryOps.SIGN: "sign(A)",
+    UnaryOps.EXP: "exp(A)", UnaryOps.LOG: "log(A)", UnaryOps.RECIPROCAL: "((float)1.0/A)",
+    BinaryOps.ADD: "(A+B)", BinaryOps.SUB: "(A-B)", BinaryOps.MUL: "(A*B)",
+    BinaryOps.DIV: "(A/B)", BinaryOps.POW: "pow(A,B)", BinaryOps.CMPEQ: "(A==B)",
+    ReduceOps.SUM: "A+=B", ReduceOps.MAX: "A=max(A,B)"
+  }
+  start_for_op = {ReduceOps.SUM: "0.0", ReduceOps.MAX: "-INFINITY"}
+
+  # TODO: move to shapetracker
+  def compute_buf_index_symbolic(self, st, buf_index, offset=0):
+    view = View(self.shapes[buf_index], self.strides[buf_index], self.offsets[buf_index] + offset)
+    idx = view.expr_idxs([f"idx{i}" for i in range(self.shape_len)])
+    valid = Variable.num(1)
+    for v in st.views[0:-1][::-1]:
+      if isinstance(v, ZeroView): valid = v.expr_node(valid, idx)
+      else: idx = v.expr_node(idx)
+    return idx, valid
+
+  def ast_parse(self, x:Union[TritonBuffer, LazyOp], acc:str, do_reduce=False) -> str:
+    if not isinstance(x, LazyOp):
+      # this is a load
+      buf_index = self.bufs.index(x)
+      if buf_index not in self.loaded:
+        # TODO: valid
+        idx, valid = self.compute_buf_index_symbolic(self.bufs[buf_index].st, buf_index)
+        self.kernel.append(f"  val{buf_index} = tl.load(data{buf_index} + {idx})")
+        self.loaded.add(buf_index)
+      return f"val{buf_index}"
+
+    values = ([acc] if isinstance(x.op, ReduceOps) else []) + [self.ast_parse(v, acc, do_reduce) for v in x.src]
+
+    code = TritonASTKernel.code_for_op[x.op]  # TODO: replace this with a function
+    code = code.replace("A", values[0])
+    if len(values) == 2: code = code.replace("B", values[1])
+    return code
+
   def codegen(self):
     self.process()
+    self.loaded = set()
     self.kernel = ["import triton", "import triton.language as tl", "@triton.jit"]
     self.kernel.append("def fxn("+','.join(f"data{i}" for i in range(len(self.bufs)))+"):")
 
-    self.kernel.append("  idx = tl.program_id(0)")
-    self.kernel.append("  x = tl.load(data1 + idx)")
-    self.kernel.append("  y = tl.load(data2 + idx)")
-    self.kernel.append("  tl.store(data0 + idx, x+y)")
+    self.output_shape = list(self.shapes[0][:self.first_reduce])
+
+    # copied from ops_gpu
+    MAX_OUTPUT_SHAPE = 3
+    self.kernel += [f"  idx{len(self.output_shape)-1-i} = tl.program_id({i})" for i in range(min(MAX_OUTPUT_SHAPE, len(self.output_shape)))]
+    if len(self.output_shape) > MAX_OUTPUT_SHAPE:
+      final_dimension = len(self.output_shape)-MAX_OUTPUT_SHAPE
+      for i in range(final_dimension-1, -1, -1):
+        self.kernel += [f"  idx{i} = idx{final_dimension} % {self.output_shape[i]}", f"  idx{final_dimension} = idx{final_dimension} // {self.output_shape[i]}"]
+      self.output_shape = [prod(self.output_shape[0:final_dimension+1])] + list(self.output_shape[final_dimension+1:])
+      if DEBUG >= 3: print(f"replaced output shape with {self.output_shape}")
+
+    code = self.ast_parse(self.ast, "<acc>")
+
+    # store
+    idx, valid = self.compute_buf_index_symbolic(self.bufs[0].st, 0)
+    self.kernel.append(f"  tl.store(data0 + {idx}, {code})")
 
     # Torch inductor seems to write out files too!
     hash = hashlib.md5(self.key.encode('utf-8')).hexdigest()
@@ -30,8 +84,8 @@ class TritonASTKernel(ASTKernel):
     exec(codeObject, globals())
     program = globals()['fxn']
 
-    return lambda *bufs: program[(prod(self.bufs[0].shape),)](*[x.torch for x in bufs])
-
+    def runner(*bufs): return program[tuple(self.output_shape[::-1])](*[x.torch for x in bufs])
+    return runner
 
 class TritonBuffer(ExplicitExecAST):
   def __init__(self, shape:Union[ShapeTracker, Tuple[int, ...]], hostbuf:Optional[TritonBuffer]=None, backing:Optional[np.ndarray]=None, force_create=False):
@@ -49,7 +103,7 @@ class TritonBuffer(ExplicitExecAST):
 
   @staticmethod
   def fromCPU(x): return TritonBuffer(x.shape, backing=x.view(np.ndarray).astype(np.float32).ravel())
-  def toCPU(x): return x.torch.cpu().numpy().reshape(x.shape)
+  def toCPU(x): return x.contiguous().torch.cpu().numpy().reshape(x.shape)
 
   @classmethod
   def exec_ast(cls, ast:LazyOp):
