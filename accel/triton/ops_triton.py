9be4ad9cf5fff45337d4e130c905f8a19b5056c6
George Hotz <geohot@gmail.com>
2023-01-27 17:05:33 -0800

    write out file

--- accel/triton/ops_triton.py
+++ accel/triton/ops_triton.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 import torch
+import hashlib
 import numpy as np
-import triton
-import triton.language as tl
 
 from typing import Union, Tuple, Optional
-from tinygrad.ops import ExplicitExecAST
+from tinygrad.ops import ExplicitExecAST, LazyOp
 from tinygrad.shape import ShapeTracker
 from tinygrad.helpers import prod
 from tinygrad.ast import ASTKernel
@@ -13,13 +12,23 @@ from tinygrad.ast import ASTKernel
 class TritonASTKernel(ASTKernel):
   def codegen(self):
     self.process()
+    self.kernel = ["import triton", "import triton.language as tl", "@triton.jit"]
+    self.kernel.append("def fxn("+','.join(f"data{i}" for i in range(len(self.bufs)))+"):")
 
-    @triton.jit
-    def program(b0, b1, b2):
-      idx = tl.program_id(0)
-      x = tl.load(b1 + idx)
-      y = tl.load(b2 + idx)
-      tl.store(b0 + idx, x+y)
+    self.kernel.append("  idx = tl.program_id(0)")
+    self.kernel.append("  x = tl.load(data1 + idx)")
+    self.kernel.append("  y = tl.load(data2 + idx)")
+    self.kernel.append("  tl.store(data0 + idx, x+y)")
+
+    # Torch inductor seems to write out files too!
+    hash = hashlib.md5(self.key.encode('utf-8')).hexdigest()
+    fn = f"/tmp/{hash}.py"
+    kernel = '\n'.join(self.kernel)
+    print(kernel)
+    with open(fn, "w") as f: f.write(kernel)
+    codeObject = compile(kernel, fn, "exec")
+    exec(codeObject, globals())
+    program = globals()['fxn']
 
     return lambda *bufs: program[(prod(self.bufs[0].shape),)](*[x.torch for x in bufs])
 
