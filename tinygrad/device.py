c0450006436ceed85f6b855b64ec30406922c5e6
George Hotz <geohot@gmail.com>
2023-02-07 17:45:54 -0600

    no gpu hack

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -5,7 +5,6 @@ def get_available_llops():
   _buffers, DEFAULT = {}, "CPU"
   for op in [os.path.splitext(x)[0] for x in sorted(os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "llops"))) if x.startswith("ops_")]:
     name = op[len("ops_"):].upper()
-    if name != "GPU": continue
     DEFAULT = name if os.environ.get(name, 0) == "1" else DEFAULT
     try:
       _buffers[name] = [cls for cname, cls in inspect.getmembers(importlib.import_module('tinygrad.llops.'+op), inspect.isclass) if (cname.upper() == name + "BUFFER")][0]
