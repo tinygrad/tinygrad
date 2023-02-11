62943e5ebc39a2c5b94594e1189dc5d8ff57ed4b
Tom Finet <tom.codeninja@gmail.com>
2023-02-11 21:55:47 +0100

    add momentum/nesterov

--- test/test_optim.py
+++ test/test_optim.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("/home/tomfinet/projects/tinygrad")
 import numpy as np
 import torch
 import unittest
@@ -47,12 +49,12 @@ def helper_test_optim(steps, tinyoptim, torchoptim, tinyargs={}, torchargs={}):
   for _ in range(steps):
     step(tinynet, tinyoptim, tinyargs)
     step(torchnet, torchoptim, torchargs)
-    x1, y1 = tinynet.x.cpu().data, tinynet.W.cpu().data
+    x1, y1 = tinynet.x.detach().numpy(), tinynet.W.detach().numpy()
     x2, y2 = torchnet.x.detach().numpy(), torchnet.W.detach().numpy()
     np.testing.assert_allclose(x1, x2, atol=1e-4)
     np.testing.assert_allclose(y1, y2, atol=1e-4)
 
-steps = 3
+steps = 10
 class TestOptim(unittest.TestCase):
 
   def test_adam(self):
@@ -61,6 +63,16 @@ class TestOptim(unittest.TestCase):
   def test_sgd(self):
     helper_test_optim(steps, SGD, torch.optim.SGD, 
                       tinyargs={'lr': 0.001}, torchargs={'lr': 0.001})
+
+  def test_sgd_momentum(self):
+    helper_test_optim(steps, SGD, torch.optim.SGD, 
+                      tinyargs={'lr': 0.001, 'momentum': 0.9}, 
+                      torchargs={'lr': 0.001, 'momentum': 0.9})
+
+  def test_sgd_momentum_nesterov(self):
+    helper_test_optim(steps, SGD, torch.optim.SGD, 
+                      tinyargs={'lr': 0.001, 'momentum': 0.9, 'nesterov': True}, 
+                      torchargs={'lr': 0.001, 'momentum': 0.9, 'nesterov': True})
 
   def test_rmsprop(self):
     helper_test_optim(steps, RMSprop, torch.optim.RMSprop, 
