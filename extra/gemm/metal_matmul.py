f0e96fd9a19c633c44e1d8e63603a3c9aeebded0
George Hotz <geohot@gmail.com>
2023-02-17 18:36:55 -0800

    6.7 TFLOPS

--- extra/gemm/metal_matmul.py
+++ extra/gemm/metal_matmul.py
@@ -25,37 +25,45 @@ FLOPS = N*N*N*2
 
 prog = CLProgram("test", f"""
 #include <metal_simdgroup_matrix>
-#pragma METAL internals : enable
 using namespace metal;
 kernel void test(device float *a, device float *data1, device float *data2, uint3 gid [[thread_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]]) {{
   int idx = gid.x/32;
-  int pos_x = (idx%{N//16}) * 16;
-  int pos_y = (idx/{N//16}) * 16;
-  simdgroup_float8x8 acc[2][2];
-  acc[0][0] = simdgroup_float8x8(0);
-  acc[0][1] = simdgroup_float8x8(0);
-  acc[1][0] = simdgroup_float8x8(0);
-  acc[1][1] = simdgroup_float8x8(0);
-  simdgroup_float8x8 A[2];
-  simdgroup_float8x8 B[2];
+  int pos_x = (idx%{N//32}) * 32;
+  int pos_y = (idx/{N//32}) * 32;
+  simdgroup_float8x8 acc[4][4];
+  for (uint i = 0; i < 4; i++) {{
+    for (uint j = 0; j < 4; j++) {{
+      acc[i][j] = simdgroup_float8x8(0);
+    }}
+  }}
+  simdgroup_float8x8 A[4];
+  simdgroup_float8x8 B[4];
   //__metal_get_null_simdgroup_event
   //__metal_simdgroup_async_copy_2d
   for (uint k = 0; k < {N}; k+=8) {{
     simdgroup_load(A[0], data1, {N}, ulong2(k, pos_x));
     simdgroup_load(A[1], data1, {N}, ulong2(k, pos_x+8));
+    simdgroup_load(A[2], data1, {N}, ulong2(k, pos_x+16));
+    simdgroup_load(A[3], data1, {N}, ulong2(k, pos_x+24));
     simdgroup_load(B[0], data2, {N}, ulong2(pos_y, k));
     simdgroup_load(B[1], data2, {N}, ulong2(pos_y+8, k));
-    simdgroup_multiply_accumulate(acc[0][0], A[0], B[0], acc[0][0]);
-    simdgroup_multiply_accumulate(acc[0][1], A[1], B[0], acc[0][1]);
-    simdgroup_multiply_accumulate(acc[1][0], A[0], B[1], acc[1][0]);
-    simdgroup_multiply_accumulate(acc[1][1], A[1], B[1], acc[1][1]);
+    simdgroup_load(B[2], data2, {N}, ulong2(pos_y+16, k));
+    simdgroup_load(B[3], data2, {N}, ulong2(pos_y+24, k));
+
+    for (uint i = 0; i < 4; i++) {{
+      simdgroup_multiply_accumulate(acc[i][0], A[0], B[i], acc[i][0]);
+      simdgroup_multiply_accumulate(acc[i][1], A[1], B[i], acc[i][1]);
+      simdgroup_multiply_accumulate(acc[i][2], A[2], B[i], acc[i][2]);
+      simdgroup_multiply_accumulate(acc[i][3], A[3], B[i], acc[i][3]);
+    }}
   }}
-  simdgroup_store(acc[0][0], a, {N}, ulong2(pos_y, pos_x));
-  simdgroup_store(acc[0][1], a, {N}, ulong2(pos_y, pos_x+8));
-  simdgroup_store(acc[1][0], a, {N}, ulong2(pos_y+8, pos_x));
-  simdgroup_store(acc[1][1], a, {N}, ulong2(pos_y+8, pos_x+8));
+  for (uint i = 0; i < 4; i++) {{
+    for (uint j = 0; j < 4; j++) {{
+      simdgroup_store(acc[i][j], a, {N}, ulong2(pos_y+i*8, pos_x+j*8));
+    }}
+  }}
 }}""")
-tm = mb(lambda: prog([N*N//8], [32], a._cl, b._cl, c._cl))
+tm = mb(lambda: prog([N*N//(2*4*4)], [512], a._cl, b._cl, c._cl))
 na = a.toCPU().reshape(N,N)
 comp = nb@nc
 if N <= 32:
