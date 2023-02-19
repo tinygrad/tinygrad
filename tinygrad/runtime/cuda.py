3ade0523cd12f0a205831d306d141d2cb3d43cc3
George Hotz <geohot@gmail.com>
2023-02-19 11:45:58 -0800

    runtime fixups

--- tinygrad/runtime/cuda.py
+++ tinygrad/runtime/cuda.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import pycuda.autoprimaryctx # type: ignore # pylint: disable=unused-import # noqa: F401
 import pycuda.driver as cuda # type: ignore
-from pycuda.compiler import SourceModule # type: ignore
+from pycuda.compiler import compile # type: ignore
 import numpy as np
 from tinygrad.ops import DEBUG, GlobalCounters
 
@@ -19,10 +19,13 @@ class CLProgram:
   smem_prefix = "__shared__ "
   barrier = "__syncthreads();"
   gid = [f'blockDim.{chr(120+i)}*blockIdx.{chr(120+i)}+threadIdx.{chr(120+i)}' for i in range(3)]
-  def __init__(self, name:str, prg:str, op_estimate:int=0, mem_estimate:int=0):
-    self.name, self.op_estimate, self.mem_estimate = name, op_estimate, mem_estimate
-    if DEBUG >= 4: print("CUDA compile", prg)
-    self.prg = SourceModule(prg).get_function(name)
+  lid = [f'threadIdx.{chr(120+i)}' for i in range(3)]
+  def __init__(self, name:str, prg:str, binary=False, shared=0, op_estimate:int=0, mem_estimate:int=0):
+    self.name, self.op_estimate, self.mem_estimate, self.shared = name, op_estimate, mem_estimate, shared
+    if DEBUG >= 4 and not binary: print("CUDA compile", prg)
+    if not binary: prg = compile(prg, target="ptx", no_extern_c=True).decode('utf-8')
+    if DEBUG >= 5: print(prg)
+    self.prg = cuda.module_from_buffer(prg.encode('utf-8')).get_function(prg.split(".visible .entry ")[1].split("(")[0])
 
   def __call__(self, global_size, local_size, *args):
     local_size = (local_size + [1] * (3 - len(local_size))) if local_size is not None else (1,1,1)
@@ -30,5 +33,5 @@ class CLProgram:
     assert all(x%y == 0 for x,y in zip(global_size, local_size)), f"local:{local_size} must divide global:{global_size}"
     global_size = [x//y for x,y in zip(global_size, local_size)]
     if DEBUG >= 2: print("CUDA launch", global_size, local_size)
-    self.prg(*args, block=tuple(local_size), grid=tuple(global_size))
+    self.prg(*args, block=tuple(local_size), grid=tuple(global_size), shared=self.shared)
     GlobalCounters.log_kernel(self.op_estimate, self.mem_estimate)
--- tinygrad/runtime/metal.py
+++ tinygrad/runtime/metal.py
@@ -3,7 +3,10 @@ import Metal # type: ignore
 import numpy as np
 from typing import List, Any
 from tinygrad.ops import DEBUG, GlobalCounters
-from tinygrad.helpers import prod
+from tinygrad.helpers import prod, getenv
+import subprocess
+
+METAL_XCODE = getenv("METAL_XCODE")
 
 device = Metal.MTLCreateSystemDefaultDevice()
 mtl_queue = device.newCommandQueue()
@@ -35,26 +38,53 @@ class CLProgram:
   smem_prefix = "threadgroup "
   barrier = "threadgroup_barrier(mem_flags::mem_threadgroup);"
   gid = [f"gid.{chr(120+i)}" for i in range(3)]
+  lid = [f"lid.{chr(120+i)}" for i in range(3)]
   def __init__(self, name:str, prg:str, op_estimate:int=0, mem_estimate:int=0):
     self.name, self.op_estimate, self.mem_estimate = name, op_estimate, mem_estimate
-    options = Metal.MTLCompileOptions.alloc().init()
     if DEBUG >= 4: print("Metal compile", prg)
-    self.library = device.newLibraryWithSource_options_error_(prg, options, None)
+    if DEBUG >= 6:  # dump llvm
+      air = subprocess.check_output(['xcrun', '-sdk', 'macosx', 'metal', '-x', 'metal', '-c', '-', '-o', '-'], input=prg.encode('utf-8'))
+      dis = subprocess.check_output(['/Users/kafka/Downloads/clang+llvm-15.0.7-arm64-apple-darwin22.0/bin/llvm-dis'], input=air)
+      print(dis.decode('utf-8'))
+    if METAL_XCODE:
+      air = subprocess.check_output(['xcrun', '-sdk', 'macosx', 'metal', '-x', 'metal', '-c', '-', '-o', '-'], input=prg.encode('utf-8'))
+      lib = subprocess.check_output(['xcrun', '-sdk', 'macosx', 'metallib', '-', '-o', '-'], input=air)
+      # newLibraryWithData needs a dispatch_data_t
+      with open("/tmp/prog.metallib", "wb") as f:
+        f.write(lib)
+      import Cocoa
+      self.library = device.newLibraryWithURL_error_(Cocoa.NSURL.URLWithString_("file:///tmp/prog.metallib"), None)
+    else:
+      options = Metal.MTLCompileOptions.alloc().init()
+      self.library = device.newLibraryWithSource_options_error_(prg, options, None)
     assert self.library[0] is not None, str(self.library)
     self.fxn = self.library[0].newFunctionWithName_(name)
-    self.pipeline_state = device.newComputePipelineStateWithFunction_error_(self.fxn, None)
-    assert self.pipeline_state[0] is not None, str(self.pipeline_state)
+    # hacks to disassemble shader
+    if DEBUG >= 5:
+      arc, err = device.newBinaryArchiveWithDescriptor_error_(Metal.MTLBinaryArchiveDescriptor.alloc().init(), None)
+      assert err is None, str(err)
+      desc = Metal.MTLComputePipelineDescriptor.alloc().init()
+      desc.setComputeFunction_(self.fxn)
+      _, err = arc.addComputePipelineFunctionsWithDescriptor_error_(desc, None)
+      assert err is None, str(err)
+      import Cocoa
+      _, err = arc.serializeToURL_error_(Cocoa.NSURL.URLWithString_("file:///tmp/shader.bin"), None)
+      assert err is None, str(err)
+      # https://github.com/dougallj/applegpu.git
+      import os
+      os.system("cd /Users/kafka/fun/m1/applegpu && python3 compiler_explorer.py /tmp/shader.bin")
+    self.pipeline_state, err = device.newComputePipelineStateWithFunction_error_(self.fxn, None)
+    assert err is None, str(err)
 
   def __call__(self, global_size, local_size, *args):
     global_size += [1] * (3-len(global_size))
     if local_size is None: local_size = [32]
     local_size += [1] * (3-len(local_size))
 
-    assert prod(local_size) <= self.pipeline_state[0].maxTotalThreadsPerThreadgroup(), f"local size {local_size} bigger than {self.pipeline_state[0].maxTotalThreadsPerThreadgroup()} with exec width {self.pipeline_state[0].threadExecutionWidth()} memory length {self.pipeline_state[0].staticThreadgroupMemoryLength()}"
+    assert prod(local_size) <= self.pipeline_state.maxTotalThreadsPerThreadgroup(), f"local size {local_size} bigger than {self.pipeline_state.maxTotalThreadsPerThreadgroup()} with exec width {self.pipeline_state.threadExecutionWidth()} memory length {self.pipeline_state.staticThreadgroupMemoryLength()}"
     command_buffer = mtl_queue.commandBuffer()
     encoder = command_buffer.computeCommandEncoder()
-    
-    encoder.setComputePipelineState_(self.pipeline_state[0])
+    encoder.setComputePipelineState_(self.pipeline_state)
     for i,a in enumerate(args):
       encoder.setBuffer_offset_atIndex_(a, 0, i)
     encoder.dispatchThreads_threadsPerThreadgroup_(Metal.MTLSize(*global_size), Metal.MTLSize(*local_size))
