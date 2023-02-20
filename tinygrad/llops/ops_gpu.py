353e81fa8fa347f34d1b05f385d412f4d41d3548
George Hotz <geohot@gmail.com>
2023-02-20 10:18:19 -0800

    need hand opt with IMAGE==2

--- tinygrad/llops/ops_gpu.py
+++ tinygrad/llops/ops_gpu.py
@@ -231,7 +231,7 @@ class CLASTKernel(ASTKernel):
   def codegen(self) -> Callable:
     self.process()
     self.upcast_in_mid_reduce = False
-    if not KOPT: self.hand_coded_optimizations()
+    if not KOPT or IMAGE == 2: self.hand_coded_optimizations()
 
     # add a local buffer for multistage reduce
     if len(self.group_for_reduce):
