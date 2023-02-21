9670bf1fd1f8f158d80ab32118aeabaa83f455dc
Connor Henderson <connor.henderson@talkiatry.com>
2023-02-20 23:14:59 -0500

    Add unsqueeze (#574)
    * Add unsqueeze
    
    * remove UNSQUEEZE from llops part of readme
    
    * make it an hlop

--- test/test_ops.py
+++ test/test_ops.py
@@ -214,6 +214,12 @@ class TestOps(unittest.TestCase):
     helper_test_op([(4,3,6,6)], lambda x: torch.flip(x, (0,1)), lambda x: x.flip(axis=(0,1)))
     helper_test_op([(4,3,6,6)], lambda x: torch.flip(x, (0,1,3)), lambda x: x.flip(axis=(0,1,3)))
     helper_test_op([(4,3,6,6)], lambda x: torch.flip(x, (3,)), lambda x: x.flip(axis=(3,)))
+  
+  def test_unsqueeze(self):
+    helper_test_op([(4,3,6,6)], lambda x: torch.unsqueeze(x, 0), lambda x: x.unsqueeze(dim=0))
+    helper_test_op([(4,3,6,6)], lambda x: torch.unsqueeze(x, 4), lambda x: x.unsqueeze(dim=4)) 
+    helper_test_op([(4,3,6,6)], lambda x: torch.unsqueeze(x, -1), lambda x: x.unsqueeze(dim=-1))
+    helper_test_op([(4,3,6,6)], lambda x: torch.unsqueeze(x, -3), lambda x: x.unsqueeze(dim=-3))
 
   def test_flatten(self):
     for axis in range(3):
