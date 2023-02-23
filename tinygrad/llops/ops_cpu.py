30dcf62bd13d62ae9139f22f2f9db0d8cbcc41c8
George Hotz <geohot@gmail.com>
2023-02-23 15:34:59 -0800

    refactor einsum_mulacc

--- tinygrad/llops/ops_cpu.py
+++ tinygrad/llops/ops_cpu.py
@@ -2,7 +2,7 @@ import numpy as np
 import operator
 from typing import ClassVar, Callable, Dict
 from tinygrad.ops import UnaryOps, BinaryOps, MovementOps, ReduceOps, ProcessingOps, GenericExecAST, Op
-from tinygrad.helpers import shape_to_axis, einsum_mulacc
+from tinygrad.helpers import shape_to_axis
 
 base_fxn_for_op : Dict[Op, Callable] = {
   UnaryOps.NOOP: lambda x: x[:], UnaryOps.NEG: lambda x: -x, UnaryOps.RECIPROCAL: lambda x: 1.0/x,
@@ -22,9 +22,14 @@ def numpy_conv(x,w,C):
   out = np.einsum("nGhwCHW, GkCHW -> nGkhw", tx.ravel().reshape(tx.shape), tw.ravel().reshape(tw.shape))
   return out.reshape(C.bs, C.groups*C.rcout, C.oy, C.ox)
 
-def mulacc(a, b, new_shape):
-  subscripts, a_slices, b_slices = einsum_mulacc(a.shape, a.strides, b.shape, b.strides, new_shape)
-  return np.einsum(subscripts, a[a_slices].copy(), b[b_slices].copy()).reshape(new_shape)
+def einsum_mulacc(einsum, get_strides):
+  def einscripts(x): return ''.join(["abcdefghijklmnopqrstuvwxyz"[i] for i in x])
+  def axes_slice(strides): return [i for i in range(len(strides)) if strides[i] != 0], tuple(slice(None) if strides[i] != 0 else 0 for i in range(len(strides)))
+  def mulacc(a, b, new_shape):
+    (a_axes, a_slices), (b_axes, b_slices) = axes_slice(get_strides(a)), axes_slice(get_strides(b))
+    out = [i for i in range(len(new_shape)) if a.shape[i] == new_shape[i] and (i in a_axes or i in b_axes)]
+    return einsum(f"{einscripts(a_axes)}, {einscripts(b_axes)} -> {einscripts(out)}", a[a_slices], b[b_slices]).reshape(new_shape)
+  return mulacc
 
 numpy_fxn_for_op : Dict[Op, Callable] = {**base_fxn_for_op, **{
   UnaryOps.RELU: lambda x: np.maximum(x, 0), UnaryOps.EXP: lambda x: np.exp(x), UnaryOps.LOG: lambda x: np.log(x),
@@ -32,7 +37,7 @@ numpy_fxn_for_op : Dict[Op, Callable] = {**base_fxn_for_op, **{
   MovementOps.FLIP: lambda x, axis: np.flip(x, axis), MovementOps.PERMUTE: lambda x, order: x.transpose(order),
   MovementOps.PAD: lambda x, padding: np.pad(x, padding), MovementOps.EXPAND: lambda x, new_shape: np.broadcast_to(x, new_shape),
   MovementOps.STRIDED: numpy_strided, ProcessingOps.CONV: numpy_conv,
-  ProcessingOps.MULACC: mulacc
+  ProcessingOps.MULACC: einsum_mulacc(lambda s,a,b: np.einsum(s, a.copy(), b.copy()), lambda x: x.strides)
 }}
 
 class CPUBuffer(GenericExecAST):
--- tinygrad/llops/ops_torch.py
+++ tinygrad/llops/ops_torch.py
@@ -1,12 +1,8 @@
 import torch
 from typing import ClassVar, Final, Dict, Callable
 from tinygrad.ops import UnaryOps, BinaryOps, MovementOps, ProcessingOps, GenericExecAST, Op
-from tinygrad.helpers import getenv, einsum_mulacc
-from tinygrad.llops.ops_cpu import base_fxn_for_op
-
-def mulacc(a, b, new_shape):
-  subscripts, a_slices, b_slices = einsum_mulacc(a.shape, a.stride(), b.shape, b.stride(), new_shape)
-  return torch.einsum(subscripts, a[a_slices], b[b_slices]).reshape(new_shape)
+from tinygrad.helpers import getenv
+from tinygrad.llops.ops_cpu import base_fxn_for_op, einsum_mulacc
 
 torch_fxn_for_op : Dict[Op, Callable] = {**base_fxn_for_op, **{
   UnaryOps.RELU: lambda x: x.relu(), UnaryOps.EXP: lambda x: x.exp(), UnaryOps.LOG: lambda x: x.log(),
@@ -14,7 +10,7 @@ torch_fxn_for_op : Dict[Op, Callable] = {**base_fxn_for_op, **{
   MovementOps.PAD: lambda x, padding: torch.nn.functional.pad(x, [item for sublist in padding[::-1] for item in sublist]),
   MovementOps.STRIDED: lambda x, arg: x.contiguous().as_strided([y[0] for y in arg], [y[1] for y in arg]),
   ProcessingOps.CONV: lambda x,w,C: C.px == C.px_ and C.py == C.py_ and torch.conv2d(x, w, stride=(C.sy, C.sx), groups=C.groups, dilation=(C.dy, C.dx), padding=(C.py, C.px)),
-  ProcessingOps.MULACC: mulacc,
+  ProcessingOps.MULACC: einsum_mulacc(torch.einsum, lambda x: x.stride())
 }}
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else ("mps" if getenv("MPS", 0) else "cpu"))
