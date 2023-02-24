85d952a9629bed1bf302deeffda8f78425279052
George Hotz <geohot@gmail.com>
2023-02-23 16:34:54 -0800

    _image_conv2d

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -7,6 +7,8 @@ from typing import List, Tuple, Callable, Optional, ClassVar, Type, Union
 from tinygrad.lazy import Device, LazyBuffer
 from tinygrad.helpers import DEBUG
 
+IMAGE = getenv("IMAGE", 0)
+
 # An instantiation of the Function is the Context
 class Function:
   def __init__(self, device:str, *tensors:Tensor):
@@ -311,7 +313,58 @@ class Tensor:
   def avg_pool2d(self, kernel_size=(2,2), stride=None): return self._pool2d(*make_pair(kernel_size), *make_pair(stride if stride is not None else kernel_size)).mean(axis=(2,4))
   def max_pool2d(self, kernel_size=(2,2), stride=None): return self._pool2d(*make_pair(kernel_size), *make_pair(stride if stride is not None else kernel_size)).max(axis=(2,4))
 
-  def conv2d(self, weight:Tensor, bias:Optional[Tensor]=None, stride=1, groups=1, dilation=1, padding=0):
+  # TODO: does this belong in Tensor?
+  def _image_conv2d(self, weight:Tensor, bias:Optional[Tensor]=None, stride=1, groups=1, dilation=1, padding=0):
+    (bs,_,iy,ix), (cout,cin,H,W) = self.shape, weight.shape
+    rcout = cout//groups
+    x, w = self, weight.reshape(groups, rcout, cin, H, W)
+
+    # hack for non multiples of 4 on cin
+    if cin % 4 != 0 and not (cin == 1 and groups%4 == 0):
+      x = x.reshape(bs, groups, cin, iy, ix)   # do this always?
+      added_input_channels = 4 - (cin % 4)
+      cin = cin + added_input_channels
+      w = w.slice(tuple((0, cin) if i == 2 else (0, w.shape[i]) for i in range(len(w.shape))))
+      x = x.slice(tuple((0, cin) if i == 2 else (0, x.shape[i]) for i in range(len(x.shape))))
+      x = x.reshape(bs, groups*cin, iy, ix)
+
+    # hack for non multiples of 4 on rcout
+    added_output_channels = 0
+    if rcout % 4 != 0 and not (rcout == 1 and groups%4 == 0):
+      added_output_channels = 4 - (rcout % 4)
+      rcout += added_output_channels
+      cout = groups * rcout
+      w = w.slice(tuple((0, rcout) if i == 1 else (0, w.shape[i]) for i in range(len(w.shape))))
+
+    # packed
+    x = x.permute(0,2,3,1).reshape(bs * iy, ix * groups * cin//4, 4)
+    if cin == 1: w = w.reshape(cout//4,4,H*W).permute(0,2,1)
+    else: w = w.reshape(cout//4,4,cin//4,4,H,W).permute(0,4,2,5,1,3).reshape(cout//4, H*cin*W, 4)
+
+    # contiguous creates the image, and early realize static weights (TODO: don't always realize)
+    x, w = x.contiguous(), w.contiguous().realize()
+
+    # put it back for normal conv
+    x = x.reshape(bs, iy, ix, groups*cin).permute(0,3,1,2)
+    w = w.reshape(cout//4, H, cin//4 if cin >= 4 else 1, W, 4, 4 if cin >= 4 else 1).permute(0,4,2,5,1,3).reshape(cout, cin, H, W)
+
+    # run normal conv
+    ret = x._conv2d(w, bias, stride, groups, dilation, padding)
+
+    # make image sized
+    oy, ox = ret.shape[2:]
+    ret = ret.permute(0,2,3,1).reshape(bs*oy, ox*cout//4, 4).contiguous()
+
+    # undo hack for non multiples of 4 on C.rcout
+    if added_output_channels != 0:
+      ret = ret.reshape(bs, oy, ox, groups, rcout)[:, :, :, :, :-added_output_channels]
+      rcout -= added_output_channels
+      cout = groups * rcout
+
+    # NCHW output
+    return ret.reshape(bs, oy, ox, cout).permute(0,3,1,2)
+
+  def _conv2d(self, weight:Tensor, bias:Optional[Tensor]=None, stride=1, groups=1, dilation=1, padding=0):
     #ret = mlops.Conv2D.apply(self, weight, stride=stride, groups=groups, dilation=dilation, padding=padding)
     #return ret if bias is None else ret.add(bias.reshape(shape=[1, -1, 1, 1]))
 
@@ -335,6 +388,8 @@ class Tensor:
     # conv! broadcasted to (bs, groups, rcout, oy, ox, cin, H, W)
     ret = (x*w).sum((-3, -2, -1)).reshape(bs, cout, oy, ox)
     return ret if bias is None else ret.add(bias.reshape(1, -1, 1, 1))
+
+  conv2d = _image_conv2d if IMAGE >= 1 else _conv2d
 
   # ***** math functions (unary) *****
 
