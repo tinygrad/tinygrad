75ed4e72a14cb6c3c34098fd7c6d4ef3e1cff40e
Jacky Lee <jla524@sfu.ca>
2023-02-25 20:00:37 -0800

    Test without cv2

--- test/test_yolo.py
+++ test/test_yolo.py
@@ -2,17 +2,24 @@ import io
 import unittest
 from pathlib import Path
 
-import cv2
 import numpy as np
+from PIL import Image
 
 from tinygrad.tensor import Tensor
 from extra.utils import fetch
-from examples.yolov3 import Darknet, infer, show_labels
+from examples.yolov3 import Darknet, show_labels
 
-chicken_img = cv2.imread(str(Path(__file__).parent / 'efficientnet/Chicken.jpg'))
-car_img = cv2.imread(str(Path(__file__).parent / 'efficientnet/car.jpg'))
+chicken_img = Image.open(str(Path(__file__).parent / 'efficientnet/Chicken.jpg'))
+car_img = Image.open(str(Path(__file__).parent / 'efficientnet/car.jpg'))
 dog_url = "https://github.com/ayooshkathuria/pytorch-yolo-v3/raw/master/dog-cycle-car.png"
-dog_img = cv2.imdecode(np.frombuffer(io.BytesIO(fetch(dog_url)).read(), np.uint8), 1)
+dog_img = Image.open(io.BytesIO(fetch(dog_url)))
+
+def infer(model, img):
+  img = np.array(img.resize((608, 608)))
+  img = img[:,:,::-1].transpose((2,0,1))
+  img = img[np.newaxis,:,:,:]/255.0
+  prediction = model.forward(Tensor(img.astype(np.float32)))
+  return prediction
 
 class TestYOLO(unittest.TestCase):
   @classmethod
@@ -27,16 +34,16 @@ class TestYOLO(unittest.TestCase):
     del cls.model
 
   def test_chicken(self):
-    labels = show_labels(infer(self.model, chicken_img))
-    self.assertEqual(", ".join(labels), "person, bird")
+    labels = show_labels(infer(self.model, chicken_img), confidence=0.8)
+    self.assertEqual(labels, ["bird"])
 
   def test_car(self):
-    labels = show_labels(infer(self.model, car_img))
-    self.assertEqual(", ".join(labels), "car")
+    labels = show_labels(infer(self.model, car_img), confidence=0.8)
+    self.assertEqual(labels, ["car"])
 
   def test_dog(self):
-    labels = show_labels(infer(self.model, dog_img))
-    self.assertEqual(", ".join(labels), "bicycle, truck, dog")
+    labels = show_labels(infer(self.model, dog_img), confidence=0.8)
+    self.assertEqual(labels, ["bicycle", "truck", "dog"])
 
 if __name__ == '__main__':
   unittest.main()
