201d9a2d5836ffd62645f16c60e5c19e666d6178
George Hotz <geohot@gmail.com>
2023-03-01 21:19:44 -0800

    remove extra copy on output

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -87,7 +87,7 @@ class Tensor:
     return self
 
   def detach(self): return Tensor(self.lazydata, device=self.device, requires_grad=False)
-  def numpy(self) -> np.ndarray: return np.array(self.lazydata.toCPU())
+  def numpy(self) -> np.ndarray: return self.lazydata.toCPU()
 
   # TODO: if things are realized this won't work
   def to_(self, device:str):
