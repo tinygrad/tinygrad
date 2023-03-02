343811b743c6d7ac3d922577f0e241abffd8ad0a
Diogo Da Cruz <dgcruz983@gmail.com>
2023-03-02 10:58:00 -0500

    added number upcast tests

--- test/test_ops.py
+++ test/test_ops.py
@@ -61,18 +61,27 @@ class TestOps(unittest.TestCase):
     helper_test_op([], lambda: torch.arange(10), lambda: Tensor.arange(10), forward_only=True)
   
   def test_eq(self):
+    helper_test_op([(3, 4, 5)], lambda x: x==2, lambda x: x.eq(2), forward_only=True)
     for x in [[(3,4, 5), (3, 4, 5)], [(3, 4, 5), (5,)], [(5,), (3, 4, 5)]]:
       helper_test_op(x, torch.eq, lambda x,y: x.eq(y), forward_only=True)
   def test_gt(self):
+    helper_test_op([(3, 4, 5)], lambda x: x>2, lambda x: x>2, forward_only=True)
+    helper_test_op([(3, 4, 5)], lambda x: 2>x, lambda x: 2>x, forward_only=True)
     for x in [[(3,4, 5), (3, 4, 5)], [(3, 4, 5), (5,)], [(5,), (3, 4, 5)]]:
       helper_test_op(x, torch.gt, lambda x,y: x>y, forward_only=True)
   def test_gte(self):
+    helper_test_op([(3, 4, 5)], lambda x: x>=2, lambda x: x>=2, forward_only=True)
+    helper_test_op([(3, 4, 5)], lambda x: 2>=x, lambda x: 2>=x, forward_only=True)
     for x in [[(3,4, 5), (3, 4, 5)], [(3, 4, 5), (5,)], [(5,), (3, 4, 5)]]:
       helper_test_op(x, torch.ge, lambda x,y: x>=y, forward_only=True)
   def test_lt(self):
+    helper_test_op([(3, 4, 5)], lambda x: x<2, lambda x: x<2, forward_only=True)
+    helper_test_op([(3, 4, 5)], lambda x: 2<x, lambda x: 2<x, forward_only=True)
     for x in [[(3,4, 5), (3, 4, 5)], [(3, 4, 5), (5,)], [(5,), (3, 4, 5)]]:
       helper_test_op(x, torch.lt, lambda x,y: x<y, forward_only=True)
   def test_lte(self):
+    helper_test_op([(3, 4, 5)], lambda x: x<=2, lambda x: x<=2, forward_only=True)
+    helper_test_op([(3, 4, 5)], lambda x: 2<=x, lambda x: 2<=x, forward_only=True)
     for x in [[(3,4, 5), (3, 4, 5)], [(3, 4, 5), (5,)], [(5,), (3, 4, 5)]]: 
       helper_test_op(x, torch.le, lambda x,y: x<=y, forward_only=True)
 
