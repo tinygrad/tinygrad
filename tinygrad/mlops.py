5a39fee1969a415d48c151a3ccf9d9bf123c1244
Diogo Da Cruz <dgcruz983@gmail.com>
2023-03-01 19:35:18 -0500

    raise runtime error

--- tinygrad/mlops.py
+++ tinygrad/mlops.py
@@ -55,11 +55,15 @@ class CompareLess(Function):
   def forward(self, x, y):
     self.ret = x.binary_op(BinaryOps.CMPLT, y)
     return self.ret
+
+  def backward(self): raise RuntimeError("no grad function")
   
 class CompareEqual(Function):
   def forward(self, x, y):
     return x.binary_op(BinaryOps.CMPEQ, y)
 
+  def backward(self): raise RuntimeError("no grad function")
+  
 class Maximum(Function):
   def forward(self, x, y):
     self.y, self.ret = y, x.binary_op(BinaryOps.MAX, y)
