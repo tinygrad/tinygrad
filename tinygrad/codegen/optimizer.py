76357bc1c817e6386aab0aa9ef3a94e51686bbbc
George Hotz <geohot@gmail.com>
2023-03-04 17:28:20 -0800

    depth 2

--- tinygrad/codegen/optimizer.py
+++ tinygrad/codegen/optimizer.py
@@ -12,35 +12,41 @@ def optimize(runtime_type, codegen_type, ast, output_buffer):
     k.simplify_ones()
 
   k = get()
-  if k.key in opt_key:
-    axes = opt_key[k.key]
-  else:
-    valid_axes = [i for i,s in enumerate(k.full_shape) if s < 4 or s%4 == 0 and not k.group_for_reduce]
-    print(f"opt {k.full_shape} with {valid_axes}")
+  if k.key not in opt_key:
+    print(f"opt {k.full_shape}")
+    axes = []
 
-    ops = None
-    best, axis = None, None
-    for i in [None]+valid_axes:
-      k = get()
-      if i != None:
-        apply(k, i)
-      colorshape = k.colorshape()
-      try:
-        prg = k.codegen().build(runtime_type)
-        if i == None: ops = prg.op_estimate
-        et = prg(prg.lower(k.bufs), wait=True, silent=True)
-        if best is None or et < best:
-          best = et
-          axis = i
-      except Exception as e:
-        et = float('nan')
-        raise e
-      print(f"{str(i):8s} : {colorshape} : {et*1e6:7.2f} us")
-    axes = [] if axis is None else [axis]
+    for _ in range(2):
+      ops = None
+      best, new_axis = None, None
+      valid_axes = [None]
+      while len(valid_axes):
+        i = valid_axes.pop(0)
+        k = get()
+        for axis in axes: apply(k, axis)
+        if i is None:
+          valid_axes += [i for i,s in enumerate(k.full_shape) if s < 4 or s%4 == 0 and not k.group_for_reduce and k.first_reduce >= 2]
+          print(k.full_shape, valid_axes)
+        else:
+          apply(k, i)
+        colorshape = k.colorshape()
+        try:
+          prg = k.codegen().build(runtime_type)
+          if i == None: ops = prg.op_estimate
+          et = prg(prg.lower(k.bufs), wait=True, silent=True)
+          if best is None or et < best:
+            best = et
+            new_axis = i
+        except Exception as e:
+          et = float('nan')
+          raise e
+        print(f"{str(axes)+' '+str(i):8s} : {colorshape} : {et*1e6:7.2f} us")
+      axes += [] if new_axis is None else [new_axis]
+      if new_axis is None: break
+
     opt_key[k.key] = axes
-
     print(f"best is {axes} in {et*1e6:.2f} us with {ops/et*1e-9:.2f} GFLOPS")
     k = get()
 
-  for axis in axes: apply(k, axis)
+  for axis in opt_key[k.key]: apply(k, axis)
   return k
