53fd1bea1b3ee807359b2e760e3f6a3802ab833a
Ethan <127143581+ethagrove@users.noreply.github.com>
2023-03-09 23:17:13 -0500

    use pathlib over os.path (#670)

--- examples/llama.py
+++ examples/llama.py
@@ -6,6 +6,7 @@ os.environ["METAL"] = "1"  # metal is best choice for llama
 import sys, argparse, math
 import numpy as np
 np.set_printoptions(linewidth=200)
+from pathlib import Path
 from hexdump import hexdump
 from typing import Optional
 from extra.helpers import Timing
@@ -137,19 +138,21 @@ class Transformer:
     return self.output(self.norm(h)[:, -1, :])
 
 # **** files and arguments ****
-
-TOKENIZER_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../weights/LLaMA/tokenizer.model")
+# examples/llama.py/../.. = tinygrad
+p = Path(__file__).parent.parent
+llama_dir = (p / 'weights' / 'LLaMA')
+TOKENIZER_FILENAME = (llama_dir / "7B" / "tokenizer.model").absolute().as_posix()
 VOCAB_SIZE = 32000
 
 args_small = {"dim": 512, "multiple_of": 256, "n_heads": 8, "n_layers": 8, "norm_eps": 1e-05, "vocab_size": VOCAB_SIZE}
 
 args_7B = {"dim": 4096, "multiple_of": 256, "n_heads": 32, "n_layers": 32, "norm_eps": 1e-06, "vocab_size": VOCAB_SIZE}
-WEIGHTS_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../weights/LLaMA/7B/consolidated.00.pth")
+WEIGHTS_FILENAME = (llama_dir / "7B" / "consolidated.00.pth")
 
 # TODO: make this model work
 args_13B = {"dim": 5120, "multiple_of": 256, "n_heads": 40, "n_layers": 40, "norm_eps": 1e-06, "vocab_size": VOCAB_SIZE}
-WEIGHTS0_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../weights/LLaMA/13B/consolidated.00.pth")
-WEIGHTS1_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../weights/LLaMA/13B/consolidated.01.pth")
+WEIGHTS0_FILENAME = (llama_dir / "13B" / "consolidated.00.pth")
+WEIGHTS1_FILENAME = (llama_dir / "13B" / "consolidated.01.pth")
 
 # **** helper functions ****
 
