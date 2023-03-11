a518b2cd8c6f14fe95b63a728e3bcadca02794fc
croumegous <cyril.roumegous@gmail.com>
2023-03-11 17:16:11 +0100

    fix mypy compile shape file not found

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mypyc --check-untyped-defs --explicit-package-bases --warn-unreachable tinygrad/shape/__init__.py tinygrad/shape/symbolic.py \
+mypyc --check-untyped-defs --explicit-package-bases --warn-unreachable tinygrad/shape/shapetracker.py tinygrad/shape/symbolic.py \
   tinygrad/nn/__init__.py tinygrad/helpers.py tinygrad/mlops.py tinygrad/tensor.py tinygrad/graph.py \
   #tinygrad/codegen/ast.py tinygrad/codegen/gpu.py tinygrad/ops.py tinygrad/runtime/ops_metal.py
   #tinygrad/runtime/ops_metal.py tinygrad/shape/__init__.py tinygrad/ops.py tinygrad/codegen/ast.py \
