fba63c60db5ac7f0f1b3047295ee9d98c13f2d9b
George Hotz <geohot@gmail.com>
2023-03-10 22:15:08 -0800

    default ENABLE_METHOD_CACHE

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mypyc --explicit-package-bases tinygrad/shape/__init__.py
+mypyc --check-untyped-defs --explicit-package-bases --warn-unreachable tinygrad/shape/__init__.py
   #tinygrad/runtime/ops_metal.py tinygrad/shape/__init__.py tinygrad/ops.py tinygrad/codegen/ast.py \
   #tinygrad/helpers.py tinygrad/mlops.py tinygrad/nn/__init__.py tinygrad/graph.py tinygrad/lazy.py tinygrad/tensor.py
 
