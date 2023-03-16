ff94f82ba0c189e640c9c22436bb0bbb3e090546
George Hotz <geohot@gmail.com>
2023-03-15 21:31:09 -0700

    fix graph test

--- test/unit/test_graph.py
+++ test/unit/test_graph.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import unittest
 import networkx as nx  # type: ignore
-import numpy as np
+from tinygrad.tensor import Tensor
 from tinygrad.graph import G, log_op, prune_graph
-from tinygrad.runtime.ops_cpu import CPUBuffer
 from tinygrad.ops import BinaryOps, LazyOp, MovementOps, ReduceOps
+
+def buf(*shp): return Tensor.ones(*shp, device="CPU").lazydata
 
 class TestGraph(unittest.TestCase):
   def setUp(self):
@@ -14,10 +15,10 @@ class TestGraph(unittest.TestCase):
     assert nx.is_isomorphic(G, RG, node_match=lambda x,y: x["label"] == y["label"], edge_match=lambda x,y: x["label"] == y["label"] if "label" in y else True)
 
   def test_add_graph(self):
-    a = CPUBuffer(np.ones((4,4), dtype=np.float32))
-    b = CPUBuffer(np.ones((4,4), dtype=np.float32))
+    a = buf(4,4)
+    b = buf(4,4)
     ast = LazyOp(BinaryOps.ADD, (a,b))
-    ret = CPUBuffer(np.ones((4,4), dtype=np.float32))
+    ret = buf(4,4)
 
     RG = nx.DiGraph()
     RG.add_node(0, label="(4, 4)")
@@ -30,12 +31,12 @@ class TestGraph(unittest.TestCase):
     self.helper_compare_graph(RG)
 
   def test_add_sum_graph(self):
-    a = CPUBuffer(np.ones((4,4), dtype=np.float32))
-    b = CPUBuffer(np.ones((1,1), dtype=np.float32))
+    a = buf(4,4)
+    b = buf(1,1)
     op0 = LazyOp(MovementOps.RESHAPE, (b,), (4, 4))
     op1 = LazyOp(BinaryOps.ADD, (a,op0))
     ast = LazyOp(ReduceOps.SUM, (op1,), (1,1))
-    ret = CPUBuffer(np.ones((1,1), dtype=np.float32))
+    ret = buf(1,1)
 
     RG = nx.DiGraph()
     RG.add_node(0, label="(4, 4)")
@@ -48,14 +49,14 @@ class TestGraph(unittest.TestCase):
     self.helper_compare_graph(RG)
 
   def test_add_graph_prune(self):
-    a = CPUBuffer(np.ones((1,1), dtype=np.float32))
+    a = buf(1,1)
     ast = LazyOp(MovementOps.RESHAPE, (a,), (4, 4))
-    ret = CPUBuffer(np.ones((4,4), dtype=np.float32))
+    ret = buf(4,4)
     log_op(ret, ast, show_graph=True)
 
-    b = CPUBuffer(np.ones((4,4), dtype=np.float32))
+    b = buf(4,4)
     ast = LazyOp(BinaryOps.ADD, (ret,b))
-    ret = CPUBuffer(np.ones((4,4), dtype=np.float32))
+    ret = buf(4,4)
     log_op(ret, ast, show_graph=True)
     prune_graph()
 
