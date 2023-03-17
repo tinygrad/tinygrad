7c613d55310746e6461fb5dccb5dcd0281c629e7
George Hotz <geohot@gmail.com>
2023-03-17 10:42:32 -0700

    fix lna

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -34,7 +34,7 @@ class DType(NamedTuple):
 
 class LazyNumpyArray:
   def __init__(self, fxn, shape, dtype): self.fxn, self.shape, self.dtype = fxn, shape, dtype
-  def __call__(self): return self.fxn(self) if callable(self.fxn) else self.fxn
+  def __call__(self) -> np.ndarray: return np.ascontiguousarray(self.fxn(self) if callable(self.fxn) else self.fxn).reshape(self.shape).astype(self.dtype)
   def reshape(self, new_shape): return LazyNumpyArray(self.fxn, new_shape, self.dtype)
   def copy(self): return self if callable(self.fxn) else LazyNumpyArray(self.fxn.copy(), self.shape, self.dtype)
   def astype(self, typ): return LazyNumpyArray(self.fxn, self.shape, typ)
