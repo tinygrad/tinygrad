21ec5f0d36415c49bc4a0f00d4564ba09b449348
Mattis Megevand <megevand.mattis@gmail.com>
2023-05-06 20:37:35 +0200

    lr scheduler test moved + integration test

--- test/extra/test_lr_scheduler.py
+++ test/extra/test_lr_scheduler.py
@@ -1,10 +1,35 @@
 import numpy as np
 import torch
 import unittest
-from tinygrad.nn.optim import SGD
+from tinygrad.tensor import Tensor
+from tinygrad.nn.optim import SGD, get_parameters
 from extra.lr_scheduler import MultiStepLR, ReduceLROnPlateau, CosineAnnealingLR
+from extra.training import train, evaluate
+from datasets import fetch_mnist
 
 np.random.seed(1337)
+
+X_train, Y_train, X_test, Y_test = fetch_mnist()
+
+class TinyBobNet:
+  def __init__(self):
+    self.l1 = Tensor.scaled_uniform(784, 128)
+    self.l2 = Tensor.scaled_uniform(128, 10)
+
+  def parameters(self):
+    return get_parameters(self)
+
+  def forward(self, x):
+    return x.dot(self.l1).relu().dot(self.l2).log_softmax()
+
+def lr_scheduler_training(uses_sched):
+  model = TinyBobNet()
+  optim = SGD(model.parameters(), lr=0.005)
+  if uses_sched: sched = MultiStepLR(optim, milestones=[3, 6, 8], gamma=0.5)
+  for _ in range(10):
+    train(model, X_train, Y_train, optim, 100)
+    if uses_sched: sched.step()
+  return evaluate(model, X_test, Y_test)
 
 def current_lr(optim): return optim.param_groups[0]['lr'] if hasattr(optim, 'param_groups') else optim.lr
 def get_lrs(optim, sched, epochs, steps=1, accs=None):
@@ -27,15 +52,15 @@ class TestLrScheduler(unittest.TestCase):
 
     np.testing.assert_allclose(tinygrad_lrs, torch_lrs, atol=atol, rtol=rtol)
 
-  def _test_multisteplr(self, epochs, opts, atol, rtol): 
+  def _test_multisteplr(self, epochs, opts, atol, rtol):
     self._test_lr_scheduler(MultiStepLR, torch.optim.lr_scheduler.MultiStepLR, epochs, opts, atol, rtol)
-  def _test_reducelronplateau(self, epochs, opts, atol, rtol): 
+  def _test_reducelronplateau(self, epochs, opts, atol, rtol):
     opts['accs'] = np.random.randn(epochs)
     self._test_lr_scheduler(ReduceLROnPlateau, torch.optim.lr_scheduler.ReduceLROnPlateau, epochs, opts, atol, rtol)
-  def _test_cosineannealinglr(self, epochs, opts, atol, rtol): 
+  def _test_cosineannealinglr(self, epochs, opts, atol, rtol):
     opts['T_max'] = epochs
     self._test_lr_scheduler(CosineAnnealingLR, torch.optim.lr_scheduler.CosineAnnealingLR, epochs, opts, atol, rtol)
-  
+
   def test_multisteplr(self): self._test_multisteplr(10, {'milestones': [1, 2, 7]}, 1e-6, 1e-6)
   def test_multisteplr_gamma(self): self._test_multisteplr(10, {'milestones': [1, 2, 7], 'gamma': 0.1337}, 1e-6, 1e-6)
 
@@ -48,6 +73,11 @@ class TestLrScheduler(unittest.TestCase):
 
   def test_cosineannealinglr(self): self._test_cosineannealinglr(100, {}, 1e-6, 1e-6)
   def test_cosineannealinglr_eta_min(self): self._test_cosineannealinglr(100, {'eta_min': 0.001}, 1e-6, 1e-6)
+
+  def test_lr_scheduler_training(self):
+    without = lr_scheduler_training(False)   # usually around 40% accuracy
+    with_sched = lr_scheduler_training(True) # usually around 85% accuracy
+    assert with_sched > without
 
 if __name__ == '__main__':
   unittest.main()
