1c9a1f11a7f7eff4e0d58ca17dc26f2d42698040
Jacky Lee <jla524@sfu.ca>
2023-05-10 00:35:05 -0700

    Reduce number of Epochs

--- examples/train_resnet.py
+++ examples/train_resnet.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     lambda x: x / 255.0,
     lambda x: np.tile(np.expand_dims(x, 1), (1, 3, 1, 1)).astype(np.float32),
   ])
-  for _ in range(10):
-    optimizer = optim.SGD(optim.get_parameters(model), lr=lr, momentum=0.875)
+  for _ in range(5):
+    optimizer = optim.SGD(optim.get_parameters(model), lr=lr, momentum=0.9)
     train(model, X_train, Y_train, optimizer, 100, BS=32, transform=transform)
     evaluate(model, X_test, Y_test, num_classes=classes, transform=transform)
     lr /= 1.2
