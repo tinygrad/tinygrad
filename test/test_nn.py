858513f9326dfcea242cf6187309e7f669ed7aa8
Jacky Lee <jla524@sfu.ca>
2023-05-23 22:08:49 -0700

    Test instance norm 2d and 3d

--- test/test_nn.py
+++ test/test_nn.py
@@ -149,7 +149,7 @@ class TestNN(unittest.TestCase):
     torch_x = torch.tensor(x.cpu().numpy())
     torch_z = torch_layer(torch_x.permute(0,2,3,1)).permute(0,3,1,2)
 
-  def test_instancenorm(self):
+  def test_instancenorm_2d(self):
     N, C, H, W = 20, 5, 10, 10
 
     # create in tinygrad
@@ -163,6 +163,26 @@ class TestNN(unittest.TestCase):
 
     # test
     x = Tensor.randn(N, C, H, W)
+    z = layer(x)
+    torch_x = torch.tensor(x.cpu().numpy())
+    torch_z = torch_layer(torch_x)
+    np.testing.assert_allclose(z.numpy(), torch_z.detach().numpy(), atol=5e-3, rtol=5e-3)
+    np.testing.assert_allclose(z.numpy(), torch_z.detach().numpy(), atol=5e-3, rtol=5e-3)
+
+  def test_instancenorm_3d(self):
+    N, C, D, H, W = 20, 5, 3, 10, 10
+
+    # create in tinygrad
+    layer = InstanceNorm(C)
+
+    # create in torch
+    with torch.no_grad():
+      torch_layer = torch.nn.InstanceNorm3d(C, affine=True).eval()
+      torch_layer.weight[:] = torch.tensor(layer.weight.numpy(), dtype=torch.float32)
+      torch_layer.bias[:] = torch.tensor(layer.bias.numpy(), dtype=torch.float32)
+
+    # test
+    x = Tensor.randn(N, C, D, H, W)
     z = layer(x)
     torch_x = torch.tensor(x.cpu().numpy())
     torch_z = torch_layer(torch_x)
