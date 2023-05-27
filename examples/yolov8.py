92f07a778a571bd41bf3b45f35303f5a97d6aaa0
r3sist-uniq <amanmatreja@gmail.com>
2023-05-27 10:50:04 +0530

    checked c2f

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -5,27 +5,27 @@ from tinygrad.nn import Conv2d,BatchNorm2d
 # Model architecture from https://github.com/ultralytics/ultralytics/issues/189
   
 class SPPF:
-    def __init__(self, c1, c2, k=5):
-        c_ = c1 // 2  # hidden channels
-        self.cv1 = Conv_Block(c1, c_, k, 1)
-        self.cv2 = Conv_Block(c_ * 4, c2, k, 1)
-        self.maxpool = lambda x : x.pad2d((k // 2, k // 2, k // 2, k // 2)).max_pool2d(kernel_size=5, stride=1)
+  def __init__(self, c1, c2, k=5):
+    c_ = c1 // 2  # hidden channels
+    self.cv1 = Conv_Block(c1, c_, k, 1)
+    self.cv2 = Conv_Block(c_ * 4, c2, k, 1)
+    self.maxpool = lambda x : x.pad2d((k // 2, k // 2, k // 2, k // 2)).max_pool2d(kernel_size=5, stride=1)
         
-    def forward(self, x):
-        x = self.cv1(x)
-        x2 = self.maxpool(x)
-        x3 = self.maxpool(x2)
-        x4 = self.maxpool(x3)
-        return self.cv2(x.cat(x2, x3, x4, dim=1))
-    
+  def forward(self, x):
+    x = self.cv1(x)
+    x2 = self.maxpool(x)
+    x3 = self.maxpool(x2)
+    x4 = self.maxpool(x3)
+    return self.cv2(x.cat(x2, x3, x4, dim=1))
+  
 # this function is from the original implementation
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
-    """Pad to 'same' shape outputs."""
-    if d > 1:
-        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
-    if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
-    return p
+  """Pad to 'same' shape outputs."""
+  if d > 1:
+      k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+  if p is None:
+      p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+  return p
 
 class Conv_Block:
   def __init__(self, c1, c2, kernel_size=1, stride=1, groups=1, dilation=1, padding=None):
@@ -49,29 +49,28 @@ class Bottleneck:
 
 # TODO: test this
 class C2f:
-    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
-            self.c = int(c2 * e)  # hidden channels
-            self.cv1 = Conv_Block(c1, 2 * self.c, 1, 1)
-            self.cv2 = Conv_Block((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-            self.bottleneck = [Bottleneck(self.c, self.c, shortcut, g, k=[(3, 3), (3, 3)], e=1.0) for _ in range(n)]
+  def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+    self.c = int(c2 * e)  # hidden channels
+    self.cv1 = Conv_Block(c1, 2 * self.c, 1, 1)
+    self.cv2 = Conv_Block((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+    self.bottleneck = [Bottleneck(self.c, self.c, shortcut, g, k=[(3, 3), (3, 3)], e=1.0) for _ in range(n)]
 
-    def forward(self, x):
-            """Forward pass through C2f layer."""
-            y = self.cv1(x)
-            # TODO: maybe can use 'Tensor.chunck' here
-            y_chunks = [y[:, :self.c], y[:, self.c:]]
-            y2 = [m(y_chunks[-1]) for m in self.bottleneck]
-            concatenated = tuple([y] + y_chunks + y2)
-            return self.cv2(concatenated)
+  def forward(self, x):
+    y = self.cv1(x)
+    # TODO: maybe can use 'Tensor.chunck' here
+    y_chunks = [y[:, :self.c], y[:, self.c:]]
+    y2 = [m(y_chunks[-1]) for m in self.bottleneck]
+    concatenated = tuple([y] + y_chunks + y2)
+    return self.cv2(concatenated)
 
 #  ****** incomplete and probably doesn't work yet*******
 # class Detect():
 #     """YOLOv8 Detect head for detection models."""
-#     dynamic = False  # force grid reconstruction
-#     export = False  # export mode
-#     shape = None
-#     anchors = Tensor.empty(0)  # init
-#     strides = Tensor.empty(0)  # init
+# dynamic = False  # force grid reconstruction
+# export = False  # export mode
+# shape = None
+# anchors = Tensor.empty(0)  # init
+# strides = Tensor.empty(0)  # init
 
 #     def __init__(self, nc=80, ch=()):  # detection layer
 #         super().__init__()
