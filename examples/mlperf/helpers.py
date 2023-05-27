e001499ce0ad49c1856bf8925e7739416d960588
Woze Parrot <wozeparrot@gmail.com>
2023-05-27 19:43:28 +0000

    clean+fix: cleaner + working

--- examples/mlperf/helpers.py
+++ examples/mlperf/helpers.py
@@ -1,4 +1,4 @@
-import collections
+from collections import OrderedDict
 import unicodedata
 
 def _get_best_indices(logits, n_best_size):
@@ -6,11 +6,19 @@ def _get_best_indices(logits, n_best_size):
   return list(map(lambda x: x[0], index_and_score))[:n_best_size]
 
 def _is_punctuation(char):
-  cp = ord(char)
-  if cp in range(33, 48) or cp in range(58, 65) or cp in range(91, 97) or cp in range(123, 127):
+  if (cp := ord(char)) in range(33, 48) or cp in range(58, 65) or cp in range(91, 97) or cp in range(123, 127):
     return True
-  cat = unicodedata.category(char)
-  return cat.startswith("P")
+  return unicodedata.category(char).startswith("P")
+
+def _is_whitespace(char):
+  if char == " " or char == "\t" or char == "\n" or char == "\r":
+    return True
+  return unicodedata.category(char) == "Zs"
+
+def _is_control(char):
+  if char == "\t" or char == "\n" or char == "\r":
+    return False
+  return unicodedata.category(char).startswith("C")
 
 def _run_split_on_punc(text):
   if text in ("[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]"):
@@ -28,10 +36,24 @@ def _run_split_on_punc(text):
       output[-1].append(char)
   return ["".join(x) for x in output]
 
-def get_final_text(pred_text, orig_text):
+def _run_strip_accents(text):
+  output = []
+  for char in unicodedata.normalize("NFD", text):
+    if unicodedata.category(char) != "Mn":
+      output.append(char)
+  return "".join(output)
+
+def _clean_text(text):
+  output = []
+  for char in text:
+    if not ((cp := ord(char)) == 0 or cp == 0xfffd or _is_control(char)):
+      output.append(" " if _is_whitespace(char) else char)
+  return "".join(output)
+
+def _get_final_text(pred_text, orig_text):
   def _strip_spaces(text):
     ns_text = ""
-    ns_to_s_map = collections.OrderedDict()
+    ns_to_s_map = OrderedDict()
     for i, c in enumerate(text):
       if c == " ":
         continue
@@ -39,11 +61,12 @@ def get_final_text(pred_text, orig_text):
       ns_text += c
     return ns_text, ns_to_s_map
 
-  orig_tokens = orig_text.strip().split()
+  orig_tokens = _clean_text(orig_text).strip().split()
   split_tokens = []
   for token in orig_tokens:
     if token not in ("[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]"):
       token = token.lower()
+      token = _run_strip_accents(token)
     split_tokens.extend(_run_split_on_punc(token))
 
   tok_text = " ".join(" ".join(split_tokens).strip().split())
@@ -96,16 +119,16 @@ def get_bert_qa_prediction(features, example, start_end_logits):
           "start_logit": start_end_logits[i][0, start_index],
           "end_logit": start_end_logits[i][1, end_index]
         })
-  prelim_predictions = sorted(prelim_predictions, key=lambda x: (x["start_logit"] + x["end_logit"]), reverse=True)
+  predictions = sorted(prelim_predictions, key=lambda x: (x["start_logit"] + x["end_logit"]), reverse=True)
 
-  for pred in prelim_predictions:
-    feature = features[pred["feature_index"]]
-    tok_tokens = feature["tokens"][pred["start_index"]:(pred["end_index"] + 1)]
-    orig_doc_start = feature["token_to_orig_map"][pred["start_index"]]
-    orig_doc_end = feature["token_to_orig_map"][pred["end_index"]]
+  if len(predictions) > 0:
+    feature = features[predictions[0]["feature_index"]]
+    tok_tokens = feature["tokens"][predictions[0]["start_index"]:(predictions[0]["end_index"] + 1)]
+    orig_doc_start = feature["token_to_orig_map"][predictions[0]["start_index"]]
+    orig_doc_end = feature["token_to_orig_map"][predictions[0]["end_index"]]
     orig_tokens = example["context"][orig_doc_start:(orig_doc_end + 1)]
     tok_text = " ".join(tok_tokens).replace(" ##", "").replace("##", "")
     tok_text = " ".join(tok_text.strip().split())
     orig_text = " ".join(orig_tokens)
-    return get_final_text(tok_text, orig_text)
+    return _get_final_text(tok_text, orig_text)
   return "empty"
