21e67902f36a5511be5ba2cab6bd51dfb5c32f38
r3sist-uniq <amanmatreja@gmail.com>
2023-05-27 18:12:57 +0530

    fixed dfl

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -63,25 +63,21 @@ class C2f:
     concatenated = tuple([y] + y_chunks + y2)
     return self.cv2(concatenated)
 
+
+
+# not finished 
 class DFL():
-    """
-    Integral module of Distribution Focal Loss (DFL).
-    Proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
-    """
-    def __init__(self, c1=16):
-        """Initialize a convolutional layer with a given number of input channels."""
-        self.conv = Conv2d(c1, 1, 1, bias=False).requires_grad(False)
-        self.conv = Conv2d(c1, 1 , 1, bias=False)
-
-        # x = torch.arange(c1, dtype=torch.float)
-        # self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
-        # self.c1 = c1
-
-    def forward(self, x):
-        """Applies a transformer layer on input tensor 'x' and returns a tensor."""
-        b, c, a = x.shape  # batch, channels, anchors
-        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+  
+  def __init__(self, c1=16):
+    self.conv = Conv2d(c1, 1, 1, bias=False)
+    self.conv = Conv2d(c1, 1 , 1, bias=False)
+    # x = torch.arange(c1, dtype=torch.float)
+    # self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
+    # self.c1 = c1
+  def forward(self, x):
+    b, c, a = x.shape  # batch, channels, anchors
+    return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+    # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
 
 
 
