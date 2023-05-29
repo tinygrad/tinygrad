99062210a9da71c238436e7f6ff6e2095502a1d0
Sehaj <pasrichasehaj@gmail.com>
2023-05-29 15:13:14 +0530

    structure fixx

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -30,49 +30,7 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
     stride_tensor.append(np.full((h * w, 1), stride))
   return np.concatenate(anchor_points), np.concatenate(stride_tensor)
 
-def clip_boxes(boxes, shape):
-  if isinstance(boxes, Tensor):  # TODO: maybe tensor.clip can be used here.
-   boxes[..., 0] = boxes[..., 0].maximum(0).minimum(shape[1])  # x1
-   boxes[..., 1] = boxes[..., 1].maximum(0).minimum(shape[0])  # y1
-   boxes[..., 2] = boxes[..., 2].maximum(0).minimum(shape[1])  # x2
-   boxes[..., 3] = boxes[..., 3].maximum(0).minimum(shape[0])  # y2
-  else:  # np.array 
-    boxes[..., [0, 2]] = np.clip(boxes[..., [0, 2]], 0, shape[1])  # x1, x2
-    boxes[..., [1, 3]] = np.clip(boxes[..., [1, 3]], 0, shape[0])  # y1, y2
 
-
-def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
-  if ratio_pad is None:  # calculate from img0_shape
-    gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
-    pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
-  else:
-    gain = ratio_pad[0][0]
-    pad = ratio_pad[1]
-  boxes[..., [0, 2]] -= pad[0]  # x padding
-  boxes[..., [1, 3]] -= pad[1]  # y padding
-  boxes[..., :4] /= gain
-  clip_boxes(boxes, img0_shape)
-  return boxes
-
-# TODO: remove clone 
-def xywh2xyxy(x):
-  y = x.clone() if isinstance(x, Tensor) else np.copy(x)
-  y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
-  y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
-  y[..., 2] = x[..., 0] + x[..., 2] / 2  # bottom right x
-  y[..., 3] = x[..., 1] + x[..., 3] / 2  # bottom right y
-  return y
-
-# TODO: use prod 
-def box_iou(box1, box2):
-  (a1, a2), (b1, b2) = box1[:, None].chunk(2, 2), box2.chunk(2, 1)
-  intersection = (a2.minimum(b2) - a1.maximum(b1)).maximum(0).prod(2)
-  # IoU = intersection / (area1 + area2 - intersection)
-  box1 = box1.T
-  box2 = box2.T
-  area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
-  area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
-  return intersection / (area1[:, None] + area2 - intersection)
 
 # this function is from the original implementation
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
@@ -81,6 +39,7 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
   if p is None:
     p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
   return p
+
 # MODULE Definitions
 class SPPF:
   def __init__(self, c1, c2, k=5):
@@ -143,11 +102,10 @@ class DFL():
     return self.conv(x.reshape(b, 4, self.c1, a).transpose(2, 1).softmax(1)).reshape(b, 4, a)
 
   
-# incomplete and untested
+# int_bias untested
 class DetectionHead():
 
   def __init__(self, nc=80, filters=()):
-    super().__init__()
     self.ch = 16  # DFL channels
     self.nc = nc  # number of classes
     self.nl = len(filters)  # number of detection layers
@@ -156,7 +114,7 @@ class DetectionHead():
     c1 = max(filters[0], self.nc)
     c2 = max((filters[0] // 4, self.ch * 4))
 
-    self.dfl = DFL(self.ch) #FIX this
+    self.dfl = DFL(self.ch) 
     self.cls = [[Conv_Block(x, c1, 3), Conv_Block(c1, c1, 3), Conv2d(c1, self.nc, 1)] for x in filters]
     self.box = [[Conv_Block(x, c2, 3), Conv_Block(c2, c2, 3), Conv2d(c2, 4 * self.ch, 1)] for x in filters]
     
@@ -171,13 +129,64 @@ class DetectionHead():
     dbox = dist2bbox(self.dfl.forward(box), Tensor(self.anchors).unsqueeze(0), xywh=True, dim=1) * Tensor(self.strides)
     z = dbox.cat(cls.sigmoid(), dim=1)
     return (z, x)
-
-
-
-# (a. non max suppression b. scale boxes (done) c. clip boxes (done) d. xywh2xyxy e. box_iou - ops.py) (c. Result d. predict.cli e. init - basepredictor)
-
-
   
+  # def initialize_biases(self):
+  #   # Initialize biases
+  #   # WARNING: requires stride availability
+  #   m = self
+  #   for a, b, s in zip(m.box, m.cls, m.stride):
+#       a[-1].bias = 1.0  # box
+#       # cls (.01 objects, 80 classes, 640 img)
+#       b[-1].bias[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)
+
+
+# DETECTION PREDICTOR CLASS FOR PROCESSING RAW OUTPUTS FROM detection head "https://github.com/ultralytics/ultralytics/blob/dada5b73c4340671ac67b99e8c813bf7b16c34ce/ultralytics/yolo/v8/detect/predict.py"
+# (a. non max suppression b. scale boxes (done) c. clip boxes (done) d. xywh2xyxy e. box_iou - ops.py) (f. [Result - individial class] g. predict.cli h. init inherit i. steam_inference- basepredictor parent class)
+# TODO: xywh2xyxy + box_iou --> NMS --> test NMS (others implict)
+
+def clip_boxes(boxes, shape):
+  if isinstance(boxes, Tensor):  # TODO: maybe tensor.clip can be used here.
+   boxes[..., 0] = boxes[..., 0].maximum(0).minimum(shape[1])  # x1
+   boxes[..., 1] = boxes[..., 1].maximum(0).minimum(shape[0])  # y1
+   boxes[..., 2] = boxes[..., 2].maximum(0).minimum(shape[1])  # x2
+   boxes[..., 3] = boxes[..., 3].maximum(0).minimum(shape[0])  # y2
+  else:  # np.array 
+    boxes[..., [0, 2]] = np.clip(boxes[..., [0, 2]], 0, shape[1])  # x1, x2
+    boxes[..., [1, 3]] = np.clip(boxes[..., [1, 3]], 0, shape[0])  # y1, y2
+
+
+def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
+  if ratio_pad is None:  # calculate from img0_shape
+    gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+    pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+  else:
+    gain = ratio_pad[0][0]
+    pad = ratio_pad[1]
+  boxes[..., [0, 2]] -= pad[0]  # x padding
+  boxes[..., [1, 3]] -= pad[1]  # y padding
+  boxes[..., :4] /= gain
+  clip_boxes(boxes, img0_shape)
+  return boxes
+
+# # TODO: remove clone 
+# def xywh2xyxy(x):
+#   y = x.clone() if isinstance(x, Tensor) else np.copy(x)
+#   y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
+#   y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
+#   y[..., 2] = x[..., 0] + x[..., 2] / 2  # bottom right x
+#   y[..., 3] = x[..., 1] + x[..., 3] / 2  # bottom right y
+#   return y
+
+# # TODO: fix prod and see about clamp
+# def box_iou(box1, box2):
+#   (a1, a2), (b1, b2) = box1[:, None].chunk(2, 2), box2.chunk(2, 1)
+#   intersection = (a2.minimum(b2) - a1.maximum(b1)).maximum(0).prod(2)
+#   # IoU = intersection / (area1 + area2 - intersection)
+#   box1 = box1.T
+#   box2 = box2.T
+#   area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+#   area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+#   return intersection / (area1[:, None] + area2 - intersection)
     
 
 
