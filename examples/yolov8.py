13fda5f1357be0e856b130997e9727a40b25cfa7
Sehaj <pasrichasehaj@gmail.com>
2023-05-29 12:30:36 +0530

    untested blocks a. scale_boxes b. clip_boxes c. xywh2xyxy d. box_iou

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -1,14 +1,13 @@
 from tinygrad.nn import Conv2d,BatchNorm2d
 from tinygrad.tensor import Tensor
 from tinygrad.nn import Conv2d,BatchNorm2d
-from tinygrad.helpers import dtypes
+from tinygrad.helpers import dtypes, prod
 import numpy as np
 import math
 # Model architecture from https://github.com/ultralytics/ultralytics/issues/189
 
 
 # UTIL FUNCTIONS
-
 def dist2bbox(distance, anchor_points, xywh=True, dim=-1):
   lt, rb = distance.chunk(2, dim)
   x1y1 = anchor_points - lt
@@ -31,15 +30,58 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
     stride_tensor.append(np.full((h * w, 1), stride))
   return np.concatenate(anchor_points), np.concatenate(stride_tensor)
 
+def clip_boxes(boxes, shape):
+  if isinstance(boxes, Tensor):  # TODO: maybe tensor.clip can be used here.
+   boxes[..., 0] = boxes[..., 0].maximum(0).minimum(shape[1])  # x1
+   boxes[..., 1] = boxes[..., 1].maximum(0).minimum(shape[0])  # y1
+   boxes[..., 2] = boxes[..., 2].maximum(0).minimum(shape[1])  # x2
+   boxes[..., 3] = boxes[..., 3].maximum(0).minimum(shape[0])  # y2
+  else:  # np.array 
+    boxes[..., [0, 2]] = np.clip(boxes[..., [0, 2]], 0, shape[1])  # x1, x2
+    boxes[..., [1, 3]] = np.clip(boxes[..., [1, 3]], 0, shape[0])  # y1, y2
+
+
+def scale_boxes(img1_shape, boxes, img0_shape, ratio_pad=None):
+  if ratio_pad is None:  # calculate from img0_shape
+    gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+    pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+  else:
+    gain = ratio_pad[0][0]
+    pad = ratio_pad[1]
+
+  boxes[..., [0, 2]] -= pad[0]  # x padding
+  boxes[..., [1, 3]] -= pad[1]  # y padding
+  boxes[..., :4] /= gain
+  clip_boxes(boxes, img0_shape)
+  return boxes
+
+# TODO: remove clone 
+def xywh2xyxy(x):
+    y = x.clone() if isinstance(x, Tensor) else np.copy(x)
+    y[..., 0] = x[..., 0] - x[..., 2] / 2  # top left x
+    y[..., 1] = x[..., 1] - x[..., 3] / 2  # top left y
+    y[..., 2] = x[..., 0] + x[..., 2] / 2  # bottom right x
+    y[..., 3] = x[..., 1] + x[..., 3] / 2  # bottom right y
+    return y
+
+# TODO: use prod 
+def box_iou(box1, box2):
+    (a1, a2), (b1, b2) = box1[:, None].chunk(2, 2), box2.chunk(2, 1)
+    intersection = (a2.minimum(b2) - a1.maximum(b1)).maximum(0).prod(2)
+    # IoU = intersection / (area1 + area2 - intersection)
+    box1 = box1.T
+    box2 = box2.T
+    area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    return intersection / (area1[:, None] + area2 - intersection)
+
 # this function is from the original implementation
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
   if d > 1:
-      k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
   if p is None:
-      p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
   return p
-
-
 # MODULE Definitions
 class SPPF:
   def __init__(self, c1, c2, k=5):
@@ -90,7 +132,6 @@ class C2f:
     concatenated = tuple([y] + y_chunks + y2)
     return self.cv2(concatenated)
 
-# TODO: test this. for some reason DFL isn't working with 3d tensors inputs, and it's exactly how it should work.
 class DFL():
   def __init__(self, c1=16):
     self.conv = Conv2d(c1, 1, 1, bias=False)
@@ -119,7 +160,7 @@ class DetectionHead():
     c1 = math.max(filters[0], self.nc)
     c2 = math.max((filters[0] // 4, self.ch * 4))
 
-    self.dfl = DFL(self.ch) #FIX this
+    self.dfl = DFL(self.ch) 
     self.cls = [[Conv_Block(x, c1, 3), Conv_Block(c1, c1, 3), Conv2d(c1, self.nc, 1)] for x in filters]
     self.box = [[Conv_Block(x, c2, 3), Conv_Block(c2, c2, 3), Conv2d(c2, 4 * self.ch, 1)] for x in filters]
     
@@ -134,11 +175,22 @@ class DetectionHead():
     b = self.anchors.unsqueeze(0) + b
     box = Tensor.stack(((a + b) / 2, b - a), dim=1)
     return Tensor.stack((box * self.strides, cls.sigmoid()), dim=1)
+  
+  def bias_init(self):
+      """Initialize Detect() biases, WARNING: requires stride availability."""
+      m = self  # self.model[-1]  # Detect() module
+      # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+      # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+      for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+          a[-1].bias.data[:] = 1.0  # box
+          b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
 
 
 
-      
-    
+# (a. non max suppression b. scale boxes (done) c. clip boxes (done) d. xywh2xyxy e. box_iou - ops.py) (c. Result d. predict.cli e. init - basepredictor)
+
+
+  
     
 
 
