ae204e40c8c7e0ffbbadaad803404d4f68545446
George Hotz <geohot@gmail.com>
2023-05-29 09:21:18 -0700

    move line counter to python

--- sz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# switched to cloc due to https://github.com/boyter/scc/issues/379
-cloc --by-file tinygrad/* | grep "tinygrad" && echo
-# also some sloccount for a dir summary
-sloccount tinygrad | grep "python" && echo
-# token count (as defined by Python tokenize)
-./count_tokens.py
