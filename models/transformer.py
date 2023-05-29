e00ed66bef7ab4c286102e743185ae7b796548c6
Jude Davis <judedavis92@gmail.com>
2023-05-29 11:10:11 +0100

    default to 0.1

--- models/transformer.py
+++ models/transformer.py
@@ -2,9 +2,9 @@ import numpy as np
 from tinygrad.tensor import Tensor
 
 class TransformerBlock:
-  def __init__(self, embed_dim, num_heads, ff_dim, prenorm=False, act=lambda x: x.relu(), dropout=0.):
+  def __init__(self, embed_dim, num_heads, ff_dim, prenorm=False, act=lambda x: x.relu(), dropout=0.1):
     assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
-    
+
     self.num_heads = num_heads
     self.head_size = embed_dim // num_heads
     self.prenorm, self.act = prenorm, act
