65d09031f228962f53147125ff3575150c87ff2c
Sohaib <errabii.sohaib@gmail.com>
2023-05-29 03:20:16 +0000

    add retinanet with resnet backbone (#813)
    * add retinanet with resnet backbone
    
    * adds resnext to support loading retinanet pretrained on openimages
    * object detection post processing with numpy
    * data is downloaded and converted to coco format with fiftyone
    * data loading and mAP evaluation with pycocotools
    
    * remove fiftyone dep
    
    * * eval freq
    
    * fix model timing
    
    * del jit for last batch
    
    * faster accumulate

--- examples/mlperf/model_eval.py
+++ examples/mlperf/model_eval.py
@@ -42,6 +42,64 @@ def eval_resnet():
     print(f"****** {n}/{d}  {n*100.0/d:.2f}%")
     st = time.perf_counter()
 
+def eval_retinanet():
+  # RetinaNet with ResNeXt50_32X4D
+  from models.resnet import ResNeXt50_32X4D
+  from models.retinanet import RetinaNet
+  mdl = RetinaNet(ResNeXt50_32X4D())
+  mdl.load_from_pretrained()
+
+  input_mean = Tensor([0.485, 0.456, 0.406]).reshape(1, -1, 1, 1)
+  input_std = Tensor([0.229, 0.224, 0.225]).reshape(1, -1, 1, 1)
+  def input_fixup(x):
+    x = x.permute([0,3,1,2]) / 255.0
+    x -= input_mean
+    x /= input_std
+    return x
+
+  from datasets.openimages import openimages, iterate
+  from pycocotools.coco import COCO
+  from pycocotools.cocoeval import COCOeval
+  from contextlib import redirect_stdout
+  coco = COCO(openimages())
+  coco_eval = COCOeval(coco, iouType="bbox")
+  coco_evalimgs, evaluated_imgs, ncats, narea = [], [], len(coco_eval.params.catIds), len(coco_eval.params.areaRng)
+
+  from tinygrad.jit import TinyJit
+  mdlrun = TinyJit(lambda x: mdl(input_fixup(x)).realize())
+
+  n, bs = 0, 8
+  st = time.perf_counter()
+  for x, targets in iterate(coco, bs):
+    dat = Tensor(x.astype(np.float32))
+    mt = time.perf_counter()
+    if dat.shape[0] == bs:
+      outs = mdlrun(dat).numpy()
+    else:
+      mdlrun.jit_cache = None
+      outs =  mdl(input_fixup(dat)).numpy()
+    et = time.perf_counter()
+    predictions = mdl.postprocess_detections(outs, input_size=dat.shape[1:3], orig_image_sizes=[t["image_size"] for t in targets])
+    ext = time.perf_counter()
+    n += len(targets)
+    print(f"[{n}/{len(coco.imgs)}] == {(mt-st)*1000:.2f} ms loading data, {(et-mt)*1000:.2f} ms to run model, {(ext-et)*1000:.2f} ms for postprocessing")
+    img_ids = [t["image_id"] for t in targets]
+    coco_results  = [{"image_id": targets[i]["image_id"], "category_id": label, "bbox": box, "score": score} 
+      for i, prediction in enumerate(predictions) for box, score, label in zip(*prediction.values())]
+    with redirect_stdout(None):
+      coco_eval.cocoDt = coco.loadRes(coco_results)
+      coco_eval.params.imgIds = img_ids
+      coco_eval.evaluate()
+    evaluated_imgs.extend(img_ids)
+    coco_evalimgs.append(np.array(coco_eval.evalImgs).reshape(ncats, narea, len(img_ids)))
+    st = time.perf_counter()
+
+  coco_eval.params.imgIds = evaluated_imgs
+  coco_eval._paramsEval.imgIds = evaluated_imgs
+  coco_eval.evalImgs = list(np.concatenate(coco_evalimgs, -1).flatten())
+  coco_eval.accumulate()
+  coco_eval.summarize()
+
 def eval_rnnt():
   # RNN-T
   from models.rnnt import RNNT
