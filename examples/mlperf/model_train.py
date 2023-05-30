63bdbbee4db1726ca1a724d9c8c562df8ea898d0
shauray8 <shauray.209309012@muj.manipal.edu>
2023-05-31 02:33:04 +0530

    training loop

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -5,13 +5,27 @@ from tinygrad.nn import optim
 def train_resnet():
   # TODO: Resnet50-v1.5
   # cosine LR or OneCycleLR, use linear warmup for bigger batch sizes, weight decay, 250 epochs (best possible accuracy) 
-  # use mixup regularization
+  # use mixup regularization, write cross entropy loss
   # for data augmentation - Norm, random croped, horizontal flip
   from models.resnet import ResNet50
-  model = ResNet50()
-  optimizer = optim.SGD(optim.get_parameters(model), lr=1e-4, momentum = .875)
+  #from datasets.imagenet import iterate
+  from extra.lr_scheduler import CosineAnnealingLR
+  from examples.mlperf.metrics import f1_score
 
-  pass
+  model = ResNet50()
+  optimizer = optim.SGD(optim.get_parameters(model), lr=1e-4, momentum = .875, weight_decay = 1/2**15)
+  scheduler = CosineAnnealingLR(optimizer, 250)
+
+  print("here")
+  for epoch in (r := trange(args_epoch)):
+    for image, label in iterate(val=False):
+      for p in model.parameters():
+        p.grad = None
+      out = model(Tensor(image))
+      loss = f1_score(out, label)
+      loss.backwards()
+      optimizer.step()
+    r.set_description("some data")
 
 def train_retinanet():
   # TODO: Retinanet
