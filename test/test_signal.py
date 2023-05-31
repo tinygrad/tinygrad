0b300f52a24c128a460f08e555e5d26ffaf71cc4
radenmuaz <raden.m.muaz@gmail.com>
2023-05-31 23:11:39 +0800

    removed mfcc

--- test/test_signal.py
+++ test/test_signal.py
@@ -7,15 +7,15 @@ import numpy as np
 class TestSignal(unittest.TestCase):
   def test_stft_reconstruction(self):
     x = Tensor.ones(2,300)
-    stft_layer = STFT(n_fft=128, win_length=128, hop_length=64)
-    y = stft_layer(x)
-    x_hat = stft_layer(y, inverse=True)
+    stft = STFT(n_fft=128, win_length=128, hop_length=64)
+    y = stft(x)
+    x_hat = stft(y, inverse=True)
     np.testing.assert_allclose(x[:, :x_hat.shape[1]].numpy(), x_hat.numpy(), atol=1e-3) # TODO: improve precision
 
   def test_stft_spectrogram(self):
     x = Tensor.ones(2,2000)
-    stft_layer = STFT(n_fft=512, window="hann", hop_length=160, win_length=320)
-    spec = stft_layer(x, return_spec=True)
+    stft = STFT(n_fft=512, window="hann", hop_length=160, win_length=320)
+    spec = stft(x, return_spec=True)
     np.testing.assert_((spec.numpy()>0).all())
   
   def test_log_mel_spectrogram(self):
