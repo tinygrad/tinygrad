2dd4e31f64a7df202e1b12d065de973f87640a07
deefi <dee7ine@gmail.com>
2023-05-31 18:39:13 +0200

    fixed missing numel call in test_numel

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -188,7 +188,7 @@ class TestTinygrad(unittest.TestCase):
   def test_numel(self):
     assert Tensor.randn(10, 10).numel() == 100
     assert Tensor.randn(1,2,5).numel() == 10
-    assert Tensor.randn(1,1,1,1,1,1) == 1
+    assert Tensor.randn(1,1,1,1,1,1).numel() == 1
     assert Tensor([]).numel() == 0
     # assert Tensor.randn(1,0,2,5) == 0 # TODO: fix empty tensors
 
