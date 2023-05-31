b14028f4d2022deb8ed72b3cf7ce2ba917d0a58f
Kunwar Raj Singh <kunwar31@pop-os.localdomain>
2023-05-31 13:28:37 +0530

    drouput using tinygrad randomness

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -515,8 +515,7 @@ class Tensor:
 
   def dropout(self, p=0.5) -> Tensor:
     if not Tensor.training: return self
-    return self * Tensor(Tensor._rng.binomial(1, 1.0 - p, size=self.shape), requires_grad=False, device=self.device, dtype=self.dtype) * (1 / (1.0 - p))
-
+    return self * (Tensor.rand(*self.shape) > p) * (1/(1.0 - p))
   # ***** cast ops *****
 
   def cast(self, dtype:DType) -> Tensor: return mlops.Cast.apply(self, dtype=dtype) if self.dtype != dtype else self
