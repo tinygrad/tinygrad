5b3a45bcca53165c32760488f76d5a05ac9f94b6
Jacky Lee <jla524@sfu.ca>
2023-06-02 14:31:46 -0700

    Revert "Revert "Skip a bunch of tests""
    This reverts commit c419e246a5d8a67a2a9f2d435aa6ed3750552443.

--- test/models/test_efficientnet.py
+++ test/models/test_efficientnet.py
@@ -8,6 +8,7 @@ from PIL import Image
 
 from tinygrad.helpers import getenv
 from tinygrad.tensor import Tensor
+from tinygrad.lazy import Device
 from models.efficientnet import EfficientNet
 from models.vit import ViT
 from models.resnet import ResNet50
@@ -112,4 +113,5 @@ class TestResNet(unittest.TestCase):
     self.assertEqual(label, "sports car, sport car")
 
 if __name__ == '__main__':
-  unittest.main()
+  if not getenv("CI", "") or Device.DEFAULT != "METAL":
+    unittest.main()
