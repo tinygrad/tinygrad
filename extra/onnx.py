301f7b54c6d02b9329c19e94f14a8292db8fa780
Steven Anderson <34435120+stevenandersonz@users.noreply.github.com>
2023-06-02 07:34:25 -0700

    ConstantOfShape ONNX test fixed. (#890)
    * ConstantOfShape ONNX test fixed.
    
    * removed redundant if statement
    
    * value is optional and should default to a float32 tensor with value of 0
    
    * fixed: default parameters are created at function definition, bad for mutable objects.

--- extra/onnx.py
+++ extra/onnx.py
@@ -38,7 +38,7 @@ def get_run_onnx(onnx_model: ModelProto):
       elif len(inp.int64_data) > 0:
         ret = Tensor(np.array(inp.int64_data, dtype=np.float32).reshape(inp.dims), requires_grad=False)
       elif len(inp.int32_data) > 0:
-        ret = Tensor(np.array(inp.int32_data, dtype=np.float32).reshape(inp.dims), requires_grad=False)
+        ret = Tensor(np.array(inp.int32_data, dtype=np.int32).reshape(inp.dims), requires_grad=False)
       else:
         ret = Tensor(np.frombuffer(inp.raw_data, dtype=tensor_dtype_to_np_dtype(inp.data_type)).reshape(inp.dims).astype(np.float32).copy(), requires_grad=False)
     else:
