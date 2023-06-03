8643ff3a2785d076c4db0b3b6b497ebf946dc8c4
Jacky Lee <jla524@sfu.ca>
2023-06-03 01:04:41 -0700

    Fix default value for CI getenv

--- test/models/test_mnist.py
+++ test/models/test_mnist.py
@@ -96,7 +96,7 @@ class TestMNIST(unittest.TestCase):
     train(model, X_train, Y_train, optimizer, steps=100)
     assert evaluate(model, X_test, Y_test) > 0.94   # torch gets 0.9415 sometimes
 
-  @unittest.skipIf(getenv("CI") and Device.DEFAULT == "METAL", "broken on some CI runners")
+  @unittest.skipIf(getenv("CI", "") and Device.DEFAULT == "METAL", "broken on some CI runners")
   def test_conv_with_bn(self):
     np.random.seed(1337)
     model = TinyConvNet(has_batchnorm=True)
