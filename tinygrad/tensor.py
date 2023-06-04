fe0eef8676b40cb1e350dc9a36306843764575c2
Peter Ross <pross@xvid.org>
2023-06-04 19:00:18 +1000

    ensure negate operation preserves type information when CPU=1
    simple test case:
    
    x = Tensor([42], dtype=dtypes.int32)
    x = -x
    print(x.numpy())

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -475,7 +475,7 @@ class Tensor:
   def tan(self): return self.sin() / self.cos()
   # ***** math functions (unary) *****
 
-  def __neg__(self): return 0.0-self
+  def __neg__(self): return Tensor(0, dtype=self.dtype)-self
   def sqrt(self): return self.pow(0.5)
   def rsqrt(self): return self.pow(-0.5)
   def square(self): return self*self
