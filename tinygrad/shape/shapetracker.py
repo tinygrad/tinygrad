7a0bd3ae20cf7aa40bb946c7959ecdef0d9f00bd
Uzair Aftab <uzaaft@outlook.com>
2023-06-04 12:16:53 +0200

    fix(shape): Simplify

--- tinygrad/shape/shapetracker.py
+++ tinygrad/shape/shapetracker.py
@@ -18,7 +18,7 @@ def check_no_mul(test, var):
 @functools.lru_cache(maxsize=None)
 def to_shape_strides(shape:Tuple[int, ...], strides:Tuple[int, ...]) -> List[Tuple[int, int]]:
   assert len(shape) == len(strides)
-  ret = [(shape[0], strides[0])] if len(shape) > 0 else []
+  ret = [(shape[0], strides[0])] if shape else []
   for i in range(1, len(shape)):
     if (strides[i] != 0 and ret[-1][1] == shape[i]*strides[i]) or ret[-1][0] == 1 or (strides[i] == 0 and ret[-1][1] == 0):
       ret[-1] = (ret[-1][0] * shape[i], strides[i])
