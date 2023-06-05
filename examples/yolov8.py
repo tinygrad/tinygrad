024388ee0a168f75f3060401c038152a9e90e8ad
Sehaj <pasrichasehaj@gmail.com>
2023-06-05 15:22:34 +0530

    added letterbox and pre_tranform for pre_process function

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -7,7 +7,7 @@ from itertools import chain
 from extra.utils import download_file, get_child
 from pathlib import Path
 import torch
-
+import cv2
 
 #Model architecture from https://github.com/ultralytics/ultralytics/issues/189
 #the upsampling class has been taken from this pull request https://github.com/geohot/tinygrad/pull/784 by dc-dc-dc. Now 2 models use upsampling. (retinet and this)
@@ -73,7 +73,7 @@ def xywh2xyxy(x):
   result = np.concatenate((xy1, xy2), axis=-1)
   return Tensor(result) if isinstance(x, Tensor) else result
 
-
+# right now this works for tensors only
 def box_iou(box1, box2, eps=1e-7):  
   # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
   a1, a2 =  box1.unsqueeze(1).chunk(2, 2)
@@ -88,7 +88,6 @@ def box_iou(box1, box2, eps=1e-7):
 
 def prepare_boxes_scores_for_nms(prediction, conf_thres=0.25, agnostic=False, nc=0, max_wh=7680):
   prediction_np = prediction.cpu().numpy()
-  print(prediction_np.shape)
   nc = nc or (prediction_np.shape[1] - 4)  # number of classes
   mi = 4 + nc  # mask start index
   xc = np.amax(prediction_np[:, 4:mi], axis=1) > conf_thres    
@@ -116,6 +115,62 @@ def prepare_boxes_scores_for_nms(prediction, conf_thres=0.25, agnostic=False, nc
     output.append((boxes,scores))
   return output
   
+class LetterBox:
+  """taken from: https://github.com/ultralytics/ultralytics/blob/dada5b73c4340671ac67b99e8c813bf7b16c34ce/ultralytics/yolo/data/augment.py#L531"""
+  def __init__(self, new_shape=(640, 640), auto=False, scaleFill=False, scaleup=True, stride=32):
+    """Initialize LetterBox object with specific parameters."""
+    self.new_shape = new_shape
+    self.auto = auto
+    self.scaleFill = scaleFill
+    self.scaleup = scaleup
+    self.stride = stride
+
+  def __call__(self, labels=None, image=None):
+    """Return updated labels and image with added border."""
+    if labels is None:
+        labels = {}
+    img = labels.get('img') if image is None else image
+    shape = img.shape[:2]  # current shape [height, width]
+    new_shape = labels.pop('rect_shape', self.new_shape)
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not self.scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if self.auto:  # minimum rectangle
+        dw, dh = np.mod(dw, self.stride), np.mod(dh, self.stride)  # wh padding
+    elif self.scaleFill:  # stretch
+      dw, dh = 0.0, 0.0
+      new_unpad = (new_shape[1], new_shape[0])
+      ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+    if labels.get('ratio_pad'):
+        labels['ratio_pad'] = (labels['ratio_pad'], (dw, dh))  # for evaluation
+
+    if shape[::-1] != new_unpad:  # resize
+      img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT,
+                                value=(114, 114, 114))  # add border
+    return img
+
+
+def pre_transform(im, imgsz=640, model_stride=32, model_pt=True):
+  im = im.cpu().numpy() if isinstance(im, Tensor) else im
+  same_shapes = all(x.shape == im[0].shape for x in im)
+  auto = same_shapes and model_pt
+  return [LetterBox(imgsz, auto=auto, stride=model_stride)(image=x) for x in im]
+
   
 #this is taken from https://github.com/geohot/tinygrad/pull/784/files by dc-dc-dc (Now 2 models use upsampling)
 class Upsample:
