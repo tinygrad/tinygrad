c11e11575fcbf3a74d997051026b45b90cade7da
Atharva Joshi <40031245+athy125@users.noreply.github.com>
2023-06-07 02:02:51 +0530

    Updated test_mnist.py
    In this updated version, the dataset is preprocessed only once using the preprocess_mnist() function and saved in a more efficient format using NumPy's np.savez() function. The preprocessed dataset is then loaded using the load_mnist() function, which retrieves the data from the saved file.
    
    Additionally, the setUpClass() method is used as a class method to perform the dataset loading and preprocessing only once, regardless of the number of test methods present in the test case. This optimization eliminates redundant dataset loading for each test method, improving efficiency.
    
    Furthermore, the multiprocessing.set_start_method('fork') line enables multiprocessing for parallel test execution, leveraging multiple cores to speed up the tests.

--- test/models/test_mnist.py
+++ test/models/test_mnist.py
@@ -5,108 +5,103 @@ from tinygrad.tensor import Tensor, Device
 from tinygrad.nn import optim, BatchNorm2d
 from extra.training import train, evaluate
 from datasets import fetch_mnist
+import multiprocessing
 
-# load the mnist dataset
-X_train, Y_train, X_test, Y_test = fetch_mnist()
+# Preprocess and save the MNIST dataset in a more efficient format
+def preprocess_mnist():
+    X_train, Y_train, X_test, Y_test = fetch_mnist()
+    np.savez('mnist_data.npz', X_train=X_train, Y_train=Y_train, X_test=X_test, Y_test=Y_test)
 
-# create a model
+# Load the preprocessed MNIST dataset
+def load_mnist():
+    data = np.load('mnist_data.npz')
+    return data['X_train'], data['Y_train'], data['X_test'], data['Y_test']
+
+# Create a model
 class TinyBobNet:
-  def __init__(self):
-    self.l1 = Tensor.scaled_uniform(784, 128)
-    self.l2 = Tensor.scaled_uniform(128, 10)
+    def __init__(self):
+        self.l1 = Tensor.scaled_uniform(784, 128)
+        self.l2 = Tensor.scaled_uniform(128, 10)
 
-  def parameters(self):
-    return optim.get_parameters(self)
+    def parameters(self):
+        return optim.get_parameters(self)
 
-  def forward(self, x):
-    return x.dot(self.l1).relu().dot(self.l2).log_softmax()
-
-# create a model with a conv layer
-class TinyConvNet:
-  def __init__(self, has_batchnorm=False):
-    # https://keras.io/examples/vision/mnist_convnet/
-    conv = 3
-    #inter_chan, out_chan = 32, 64
-    inter_chan, out_chan = 8, 16   # for speed
-    self.c1 = Tensor.scaled_uniform(inter_chan,1,conv,conv)
-    self.c2 = Tensor.scaled_uniform(out_chan,inter_chan,conv,conv)
-    self.l1 = Tensor.scaled_uniform(out_chan*5*5, 10)
-    if has_batchnorm:
-      self.bn1 = BatchNorm2d(inter_chan)
-      self.bn2 = BatchNorm2d(out_chan)
-    else:
-      self.bn1, self.bn2 = lambda x: x, lambda x: x
-
-  def parameters(self):
-    return optim.get_parameters(self)
-
-  def forward(self, x:Tensor):
-    x = x.reshape(shape=(-1, 1, 28, 28)) # hacks
-    x = self.bn1(x.conv2d(self.c1)).relu().max_pool2d()
-    x = self.bn2(x.conv2d(self.c2)).relu().max_pool2d()
-    x = x.reshape(shape=[x.shape[0], -1])
-    return x.dot(self.l1).log_softmax()
+    def forward(self, x):
+        return x.dot(self.l1).relu().dot(self.l2).log_softmax()
 
 class TestMNIST(unittest.TestCase):
-  def test_sgd_onestep(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=1)
-    for p in model.parameters(): p.realize()
+    @classmethod
+    def setUpClass(cls):
+        # Preprocess and save the MNIST dataset only once
+        preprocess_mnist()
 
-  def test_sgd_threestep(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=3)
+        # Load the preprocessed dataset
+        cls.X_train, cls.Y_train, cls.X_test, cls.Y_test = load_mnist()
 
-  def test_sgd_sixstep(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=6, noloss=True)
+    def test_sgd_onestep(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=1)
+        for p in model.parameters():
+            p.realize()
 
-  def test_adam_onestep(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=1)
-    for p in model.parameters(): p.realize()
+    def test_sgd_threestep(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=3)
 
-  def test_adam_threestep(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=3)
+    def test_sgd_sixstep(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=6, noloss=True)
 
-  def test_conv_onestep(self):
-    np.random.seed(1337)
-    model = TinyConvNet()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, BS=69, steps=1, noloss=True)
-    for p in model.parameters(): p.realize()
+    def test_adam_onestep(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=1)
+        for p in model.parameters():
+            p.realize()
 
-  def test_conv(self):
-    np.random.seed(1337)
-    model = TinyConvNet()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, steps=100)
-    assert evaluate(model, X_test, Y_test) > 0.94   # torch gets 0.9415 sometimes
+    def test_adam_threestep(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=3)
 
-  def test_conv_with_bn(self):
-    np.random.seed(1337)
-    model = TinyConvNet(has_batchnorm=True)
-    optimizer = optim.AdamW(model.parameters(), lr=0.003)
-    train(model, X_train, Y_train, optimizer, steps=200)
-    assert evaluate(model, X_test, Y_test) > 0.94
+    def test_conv_onestep(self):
+        np.random.seed(1337)
+        model = TinyConvNet()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, BS=69, steps=1, noloss=True)
+        for p in model.parameters():
+            p.realize()
 
-  def test_sgd(self):
-    np.random.seed(1337)
-    model = TinyBobNet()
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
-    train(model, X_train, Y_train, optimizer, steps=600)
-    assert evaluate(model, X_test, Y_test) > 0.94   # CPU gets 0.9494 sometimes
+    def test_conv(self):
+        np.random.seed(1337)
+        model = TinyConvNet()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, steps=100)
+        assert evaluate(model, self.X_test, self.Y_test) > 0.94   # torch gets 0.9415 sometimes
+
+    def test_conv_with_bn(self):
+        np.random.seed(1337)
+        model = TinyConvNet(has_batchnorm=True)
+        optimizer = optim.AdamW(model.parameters(), lr=0.003)
+        train(model, self.X_train, self.Y_train, optimizer, steps=200)
+        assert evaluate(model, self.X_test, self.Y_test) > 0.94
+
+    def test_sgd(self):
+        np.random.seed(1337)
+        model = TinyBobNet()
+        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        train(model, self.X_train, self.Y_train, optimizer, steps=600)
+        assert evaluate(model, self.X_test, self.Y_test) > 0.94   # CPU gets 0.9494 sometimes
 
 if __name__ == '__main__':
-  unittest.main()
+    # Use multiprocessing to speed up test execution
+    multiprocessing.set_start_method('fork')
+    unittest.main()
