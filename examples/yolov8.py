c2b72ce62c213ae3f6019200a1f99a244d7d05a4
Sehaj <pasrichasehaj@gmail.com>
2023-06-06 12:05:13 +0530

    fixed letterbox, pre_transform and added preprocess function

--- examples/yolov8.py
+++ examples/yolov8.py
@@ -115,62 +115,51 @@ def prepare_boxes_scores_for_nms(prediction, conf_thres=0.25, agnostic=False, nc
     output.append((boxes,scores))
   return output
   
-class LetterBox:
-  """taken from: https://github.com/ultralytics/ultralytics/blob/dada5b73c4340671ac67b99e8c813bf7b16c34ce/ultralytics/yolo/data/augment.py#L531"""
-  def __init__(self, new_shape=(640, 640), auto=False, scaleFill=False, scaleup=True, stride=32):
-    """Initialize LetterBox object with specific parameters."""
-    self.new_shape = new_shape
-    self.auto = auto
-    self.scaleFill = scaleFill
-    self.scaleup = scaleup
-    self.stride = stride
+'''TAKEN FROM: https://github.com/ultralytics/ultralytics/blob/dada5b73c4340671ac67b99e8c813bf7b16c34ce/ultralytics/yolo/data/augment.py#L531'''
+def letterbox(image, new_shape=(640, 640), auto=False, scaleFill=False, scaleup=True, stride=32):
+  shape = image.shape[:2]  # current shape [height, width]
+  if isinstance(new_shape, int):
+    new_shape = (new_shape, new_shape)
 
-  def __call__(self, labels=None, image=None):
-    """Return updated labels and image with added border."""
-    if labels is None:
-        labels = {}
-    img = labels.get('img') if image is None else image
-    shape = img.shape[:2]  # current shape [height, width]
-    new_shape = labels.pop('rect_shape', self.new_shape)
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
+  # Scale ratio (new / old)
+  r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+  if not scaleup:  # only scale down, do not scale up (for better val mAP)
+    r = min(r, 1.0)
 
-    # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not self.scaleup:  # only scale down, do not scale up (for better val mAP)
-        r = min(r, 1.0)
+  # Compute padding
+  ratio = r, r  # width, height ratios
+  new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+  dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+  if auto:  # minimum rectangle
+    dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+  elif scaleFill:  # stretch
+    dw, dh = 0.0, 0.0
+    new_unpad = (new_shape[1], new_shape[0])
+    ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-    # Compute padding
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if self.auto:  # minimum rectangle
-        dw, dh = np.mod(dw, self.stride), np.mod(dh, self.stride)  # wh padding
-    elif self.scaleFill:  # stretch
-      dw, dh = 0.0, 0.0
-      new_unpad = (new_shape[1], new_shape[0])
-      ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+  dw /= 2  # divide padding into 2 sides
+  dh /= 2
 
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-    if labels.get('ratio_pad'):
-        labels['ratio_pad'] = (labels['ratio_pad'], (dw, dh))  # for evaluation
-
-    if shape[::-1] != new_unpad:  # resize
-      img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                value=(114, 114, 114))  # add border
-    return img
-
+  if shape[::-1] != new_unpad:  # resize
+    image = cv2.resize(image, new_unpad, interpolation=cv2.INTER_LINEAR)
+  top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+  left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+  image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(114, 114, 114))  # add border
+  return image
 
 def pre_transform(im, imgsz=640, model_stride=32, model_pt=True):
   im = im.cpu().numpy() if isinstance(im, Tensor) else im
   same_shapes = all(x.shape == im[0].shape for x in im)
   auto = same_shapes and model_pt
-  return [LetterBox(imgsz, auto=auto, stride=model_stride)(image=x) for x in im]
+  return [letterbox(x, new_shape=imgsz, auto=auto, stride=model_stride) for x in im]
 
+def preprocess(im, device=None, model=None):
+  im = np.stack(pre_transform(im))
+  im = im[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW, (n, 3, h, w)
+  im = np.ascontiguousarray(im)  # contiguous
+  img = im.astype(np.float32)  # uint8 to float32
+  img /= 255  # 0 - 255 to 0.0 - 1.0
+  return img
   
 #this is taken from https://github.com/geohot/tinygrad/pull/784/files by dc-dc-dc (Now 2 models use upsampling)
 class Upsample:
