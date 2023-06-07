faf1eef6f9af0ab0de3b5822eb84721999d3a69b
Woze Parrot <wozeparrot@gmail.com>
2023-06-07 04:41:23 +0000

    feat: slightly more correct tokenization

--- datasets/wikipedia.py
+++ datasets/wikipedia.py
@@ -3,10 +3,139 @@ import pickle
 from pathlib import Path
 from transformers import BertTokenizer
 import numpy as np
+from tqdm import tqdm
 import random
-from tqdm import tqdm, trange
+import unicodedata
 
 BASEDIR = Path(__file__).parent.parent / "datasets/wikipedia"
+
+def _is_punctuation(char):
+  if (cp := ord(char)) in range(33, 48) or cp in range(58, 65) or cp in range(91, 97) or cp in range(123, 127):
+    return True
+  return unicodedata.category(char).startswith("P")
+
+def _is_whitespace(char):
+  if char == " " or char == "\t" or char == "\n" or char == "\r":
+    return True
+  return unicodedata.category(char) == "Zs"
+
+def _is_control(char):
+  if char == "\t" or char == "\n" or char == "\r":
+    return False
+  return unicodedata.category(char).startswith("C")
+
+def _run_split_on_punc(text):
+  if text in ("[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]"):
+    return [text]
+  start_new_word = True
+  output = []
+  for i in range(len(text)):
+    if _is_punctuation(char := text[i]):
+      output.append([char])
+      start_new_word = True
+    else:
+      if start_new_word:
+        output.append([])
+      start_new_word = False
+      output[-1].append(char)
+  return ["".join(x) for x in output]
+
+def _run_strip_accents(text):
+  output = []
+  for char in unicodedata.normalize("NFD", text):
+    if unicodedata.category(char) != "Mn":
+      output.append(char)
+  return "".join(output)
+
+def _clean_text(text):
+  output = []
+  for char in text:
+    if not ((cp := ord(char)) == 0 or cp == 0xfffd or _is_control(char)):
+      output.append(" " if _is_whitespace(char) else char)
+  return "".join(output)
+
+def _is_chinese_char(cp):
+    return cp in range(0x4E00, 0x9FFF+1) or cp in range(0x3400, 0x4DBF+1) or cp in range(0x20000, 0x2A6DF+1) or cp in range(0x2A700, 0x2B73F+1) or cp in range(0x2B740, 0x2B81F+1) or cp in range(0x2B820, 0x2CEAF+1) or cp in range(0xF900, 0xFAFF+1) or cp in range(0x2F800, 0x2FA1F+1)
+
+def _tokenize_chinese_chars(text):
+  output = []
+  for char in text:
+    cp = ord(char)
+    if _is_chinese_char(cp):
+      output.append(" ")
+      output.append(char)
+      output.append(" ")
+    else:
+      output.append(char)
+  return "".join(output)
+
+def _wordpiece_tokenize(text, vocab):
+  text = text.decode("utf-8", "ignore") if isinstance(text, bytes) else text
+  output_tokens = []
+  for token in text.strip().split():
+    chars = list(token)
+    if len(chars) > 200:
+      output_tokens.append("[UNK]")
+      continue
+
+    is_bad = False
+    start = 0
+    sub_tokens = []
+    while start < len(chars):
+      end = len(chars)
+      cur_substr = None
+      while start < end:
+        substr = "".join(chars[start:end])
+        if start > 0:
+          substr = "##" + substr
+        if substr in vocab:
+          cur_substr = substr
+          break
+        end -= 1
+      if cur_substr is None:
+        is_bad = True
+        break
+      sub_tokens.append(cur_substr)
+      start = end
+
+    if is_bad:
+      output_tokens.append("[UNK]")
+    else:
+      output_tokens.extend(sub_tokens)
+  return output_tokens
+
+class Tokenizer:
+  def __init__(self, vocab_file):
+    self.vocab = {}
+    with open(vocab_file) as f:
+      for line in f:
+        line = line.decode("utf-8", "ignore") if isinstance(line, bytes) else line
+        if (token := line.strip()) and token not in self.vocab:
+          self.vocab[token] = len(self.vocab)
+    self.inv_vocab = {v: k for k, v in self.vocab.items()}
+
+  def tokenize(self, text):
+    text = _clean_text(text.decode("utf-8", "ignore") if isinstance(text, bytes) else text)
+    text = _tokenize_chinese_chars(text)
+
+    # BasicTokenizer
+    split_tokens = []
+    for token in text.strip().split():
+      split_tokens.extend(_run_split_on_punc(_run_strip_accents(token.lower())))
+    split_tokens = " ".join(split_tokens).strip().split()
+
+    # WordpieceTokenizer
+    tokens = []
+    for token in split_tokens:
+      tokens.extend(_wordpiece_tokenize(token, self.vocab))
+
+    return tokens
+
+  def convert_tokens_to_ids(self, tokens):
+    return [self.vocab[token] for token in tokens]
+
+  def convert_ids_to_tokens(self, ids):
+    return [self.inv_vocab[id] for id in ids]
 
 def truncate_seq_pair(tokens_a, tokens_b, max_num_tokens, rng):
   while True:
@@ -22,7 +151,7 @@ def truncate_seq_pair(tokens_a, tokens_b, max_num_tokens, rng):
     else:
       trunc_tokens.pop()
 
-def create_masked_lm_predictions(tokens, rng, tokenizer):
+def create_masked_lm_predictions(tokens, rng, vocab_words):
   cand_indices = []
   for i, token in enumerate(tokens):
     if token == "[CLS]" or token == "[SEP]":
@@ -49,7 +178,7 @@ def create_masked_lm_predictions(tokens, rng, tokenizer):
       if rng.random() < 0.5:
         masked_token = tokens[index]
       else:
-        masked_token = tokenizer.convert_tokens_to_ids([tokens[index]])[0]
+        masked_token = vocab_words[rng.randint(0, len(tokenizer.vocab) - 1)]
 
     output_tokens[index] = masked_token
     masked_lms.append((index, tokens[index]))
@@ -67,11 +196,9 @@ def get_documents(rng, tokenizer, fn):
   documents = [[]]
   with open(BASEDIR / "raw" / fn) as f:
     for line in tqdm(f.readlines()):
-      if not line: break
-      if not (line := line.strip()):
-        documents.append([])
-      tokens = tokenizer.tokenize(line)
-      if tokens: documents[-1].append(tokens)
+      if not (line := line.decode("utf-8", "ignore") if isinstance(line, bytes) else line): break
+      if not (line := line.strip()): documents.append([])
+      if (tokens := tokenizer.tokenize(line)): documents[-1].append(tokens)
   documents = [x for x in documents if x]
   rng.shuffle(documents)
   return documents
@@ -142,7 +269,7 @@ def create_instances_from_document(rng, tokenizer, doc, di, documents):
         tokens.append("[SEP]")
         segment_ids.append(1)
 
-        tokens, masked_lm_positions, masked_lm_labels = create_masked_lm_predictions(tokens, rng, tokenizer)
+        tokens, masked_lm_positions, masked_lm_labels = create_masked_lm_predictions(tokens, rng, list(tokenizer.vocab.keys()))
         instances.append({
           "tokens": tokens,
           "segment_ids": segment_ids,
@@ -157,7 +284,7 @@ def create_instances_from_document(rng, tokenizer, doc, di, documents):
 
 def get_instances(rng, tokenizer, documents):
   instances = []
-  for i in range(10):
+  for i in range(1):
     for di, doc in tqdm(enumerate(documents), desc=f"dupe {i}", total=len(documents)):
       instances.extend(create_instances_from_document(rng, tokenizer, doc, di, documents))
   rng.shuffle(instances)
@@ -191,12 +318,10 @@ def process_iterate(tokenizer, val=True):
 
       masked_lm_positions = instance["masked_lm_positions"]
       masked_lm_ids = tokenizer.convert_tokens_to_ids(instance["masked_lm_labels"])
-      masked_lm_weights = [1.0] * len(masked_lm_ids)
 
       while len(masked_lm_positions) < 76:
         masked_lm_positions.append(0)
         masked_lm_ids.append(0)
-        masked_lm_weights.append(0.0)
 
       next_sentence_label = 1 if instance["is_random_next"] else 0
 
@@ -204,10 +329,9 @@ def process_iterate(tokenizer, val=True):
         "input_ids": np.expand_dims(np.array(input_ids, dtype=np.float32), 0),
         "input_mask": np.expand_dims(np.array(input_mask, dtype=np.float32), 0),
         "segment_ids": np.expand_dims(np.array(segment_ids, dtype=np.float32), 0),
-        "masked_lm_positions": masked_lm_positions,
-        "masked_lm_ids": masked_lm_ids,
-        "masked_lm_weights": masked_lm_weights,
-        "next_sentence_labels": np.array([next_sentence_label], dtype=np.float32),
+        "masked_lm_positions": np.expand_dims(np.array(masked_lm_positions, dtype=np.float32), 0),
+        "masked_lm_ids": np.expand_dims(np.array(masked_lm_ids, dtype=np.float32), 0),
+        "next_sentence_labels": np.expand_dims(np.array([next_sentence_label], dtype=np.float32), 0),
       }
 
       yield features, instances[i]
@@ -215,20 +339,53 @@ def process_iterate(tokenizer, val=True):
 def get_val_files():
   return sorted(list((BASEDIR / "eval").glob("*.pkl")))
 
-def iterate(start=0, val=True):
+def iterate(bs=1, start=0, val=True):
   if val:
     # scan directory for files
     files = get_val_files()
-    for i in range(start, len(files)):
-      with open(files[i], "rb") as f:
-        yield pickle.load(f)
+    for i in range(start, len(files), bs):
+      input_ids = []
+      input_mask = []
+      segment_ids = []
+      masked_lm_positions = []
+      masked_lm_ids = []
+      next_sentence_labels = []
+      instances = []
+      for j in range(bs):
+        with open(files[i + j], "rb") as f:
+          features, instance = pickle.load(f)
+          input_ids.append(features["input_ids"])
+          input_mask.append(features["input_mask"])
+          segment_ids.append(features["segment_ids"])
+          masked_lm_positions.append(features["masked_lm_positions"])
+          masked_lm_ids.append(features["masked_lm_ids"])
+          next_sentence_labels.append(features["next_sentence_labels"])
+          instances.append(instance)
+
+      yield {
+        "input_ids": np.concatenate(input_ids, axis=0),
+        "input_mask": np.concatenate(input_mask, axis=0),
+        "segment_ids": np.concatenate(segment_ids, axis=0),
+        "masked_lm_positions": np.concatenate(masked_lm_positions, axis=0),
+        "masked_lm_ids": np.concatenate(masked_lm_ids, axis=0),
+        "next_sentence_labels": np.concatenate(next_sentence_labels, axis=0),
+      }, instances
 
 if __name__ == "__main__":
-  tokenizer = BertTokenizer(str(Path(__file__).parent.parent / "weights/bert_vocab.txt"), do_lower_case=True)
+  # tokenizer = BertTokenizer(str(Path(__file__).parent.parent / "weights/bert_vocab.txt"), do_lower_case=True)
+  tokenizer = Tokenizer(Path(__file__).parent.parent / "weights/bert_vocab.txt")
 
   if len(sys.argv) <= 1:
     X, Y = next(iterate())
-    print(" ".join(map(str, Y["tokens"])))
+    print(X["input_ids"])
+    print(tokenizer.convert_ids_to_tokens(X["input_ids"][0]))
+    print(X["masked_lm_ids"])
+    print(tokenizer.convert_ids_to_tokens(X["masked_lm_ids"][0]))
+
+    # fill in the blanks
+    for i in range(76):
+      X["input_ids"][0][int(X["masked_lm_positions"][0][i])] = X["masked_lm_ids"][0][i]
+    print(" ".join(tokenizer.convert_ids_to_tokens(X["input_ids"][0])))
   else:
     if sys.argv[1] == "pre-eval":
       for i, (X, Y) in tqdm(enumerate(process_iterate(tokenizer)), total=10000):
