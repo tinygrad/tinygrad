b731275f8b2d7e78ced49526fe62bfbf1d728e8c
cloud11665 <cloud11665@gmail.com>
2023-06-12 19:48:48 +0200

    use dict

--- tinygrad/codegen/cstyle.py
+++ tinygrad/codegen/cstyle.py
@@ -141,10 +141,8 @@ def uops_to_cstyle(uops:List[UOp], bufs:List[Union[LocalBuffer,LazyBuffer]], lan
       # NOTE: if min and max are both 0, it should be a CONST in the Linearizer
       if args.valid.min == 1: kk(f"{newvar.render(True)} = {val};")
       else:
-        if   newvar.ltype == LocalTypes.float4: expr = f"({val}) : {lang.float4}(0.0f, 0.0f, 0.0f, 0.0f);"
-        elif newvar.ltype == LocalTypes.half:   expr = f"(half)({val}) : (half)(0.0f);"
-        else:                                   expr = f"(float)({val}) : 0.0f;"
-        kk(f"{newvar.render(True)} = ({args.valid.render(render_cl)}) ? {expr}")
+        casts = {LocalTypes.float4: ("", f"{lang.float4}(0.0f, 0.0f, 0.0f, 0.0f)"), LocalTypes.half: ("(half)", "(half)(0.0f)"), LocalTypes.float: ("(float)", "0.0f")}[newvar.ltype]
+        kk(f"{newvar.render(True)} = ({args.valid.render(render_cl)}) ? {casts[0]}({val}) : {casts[1]};")
     elif uop == UOps.STORE and (vin[0].ltype == LocalTypes.float or (vin[0].ltype == LocalTypes.float4 and vin[0].offset is not None)):
       assert not isinstance(bufs[args.i].dtype, ImageDType), "image store must be float4"
       assert args.valid.min == 1, "store must be valid"
