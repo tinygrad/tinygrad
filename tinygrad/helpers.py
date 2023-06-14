0629791cbd37e1b885911a78791d983b4a36b931
Diogo <dgcruz983@gmail.com>
2023-06-14 00:31:31 -0400

    F64 support (#976)
    * initial commit
    
    * added osx check for opencl
    
    * added llvm f64 conversions
    
    * typo in llvmir
    
    * more tests and modified unsupported error
    
    * fixed linting error
    
    * added pragma fp64
    
    * simplified exclusion for OSX
    
    * fixed device check and also added it to cast func
    
    * added ifdef check for fp16 in ops_gpu
    
    * Revert "added ifdef check for fp16 in ops_gpu"
    
    This reverts commit 92de754d48cba19c04ef20b3d4a1c3003046a9d0.
    
    * f64 prekernel signature match f16
    
    * moved condition to buffer init

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
+import platform
 from dataclasses import dataclass, asdict
 import os, math, functools, time, re
 import numpy as np
 from typing import Tuple, Union, List, NamedTuple, Final, Iterator, ClassVar, Optional, Callable, Any
 ShapeType = Tuple[int, ...]
 # NOTE: helpers is not allowed to import from anything else in tinygrad
+OSX = platform.system() == "Darwin"
 
 def dedup(x): return list(dict.fromkeys(x))   # retains list order
 def prod(x:Union[List[int], Tuple[int, ...]]) -> int: return math.prod(x)
@@ -71,7 +73,7 @@ class dtypes:
   @staticmethod # static methds on top, or bool in the type info will refer to dtypes.bool
   def is_int(x: DType)-> bool: return x in (dtypes.int8, dtypes.uint8, dtypes.int32, dtypes.int64)
   @staticmethod
-  def is_float(x: DType) -> bool: return x in (dtypes.float16, dtypes.float32)
+  def is_float(x: DType) -> bool: return x in (dtypes.float16, dtypes.float32, dtypes.float64)
   @staticmethod
   def is_unsigned(x: DType) -> bool: return x in (dtypes.uint8, dtypes.uint32, dtypes.uint64)
   @staticmethod
@@ -79,6 +81,7 @@ class dtypes:
   bool: Final[DType] = DType(0, 1, "bool", bool)
   float16: Final[DType] = DType(0, 2, "half", np.float16)
   float32: Final[DType] = DType(4, 4, "float", np.float32)
+  float64: Final[DType] = DType(5, 8, "double", np.float64)
   int8: Final[DType] = DType(0, 1, "char", np.int8)
   int32: Final[DType] = DType(1, 4, "int", np.int32)
   int64: Final[DType] = DType(2, 8, "int64", np.int64)
