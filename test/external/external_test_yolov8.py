0f631ec915a2817cf7d7ce26310de6435b59b565
r3sist-uniq <amanmatreja@gmail.com>
2023-06-15 14:13:21 +0530

    updated tests

--- test/external/external_test_yolov8.py
+++ test/external/external_test_yolov8.py
@@ -2,42 +2,35 @@ import numpy as np
 from extra.utils import fetch, download_file, get_child
 from examples.yolov8 import YOLOv8, get_variant_multiples, preprocess, postprocess, label_predictions
 from pathlib import Path
-import torch
 import unittest
-import io
-import sys
-from tinygrad.nn import Tensor
-import cv2
+import io, cv2, os
 import onnxruntime as ort
-import os
 import ultralytics
 
 class TestYOLOv8(unittest.TestCase):
   def test_all_load_weights(self):
     for variant in ['n', 's', 'm', 'l', 'x']:
       weights_location = Path(__file__).parent.parent.parent / "weights" / f'yolov8{variant}.npz'
-      weights_location_pt = Path(__file__).parent.parent.parent / "weights" / f'yolov8{variant}.pt'
       download_file(f'https://gitlab.com/r3sist/yolov8_weights/-/raw/master/yolov8{variant}.npz', weights_location)
-      download_file(f'https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8{variant}.pt', weights_location_pt)
       
       depth, width, ratio = get_variant_multiples(variant) 
       TinyYolov8 = YOLOv8(w=width, r=ratio, d=depth, num_classes=80) 
       TinyYolov8.load_weights(weights_location, variant)
       
-      state_dict = torch.load(weights_location_pt)
-      weights = state_dict['model'].state_dict().items()
+      loaded_npz = np.load(weights_location)
       all_trainable_weights = TinyYolov8.return_all_trainable_modules()
       
-      for k, v in weights:
+      for k in loaded_npz.files:
+        v = loaded_npz[k]
         k = k.split('.')
         for i in all_trainable_weights:
           if int(k[1]) in i and k[-1] != "num_batches_tracked":
             child_key = '.'.join(k[2:]) if k[2] != 'm' else 'bottleneck.' + '.'.join(k[3:])
             obj = get_child(i[1], child_key)
-            weight = v.numpy().astype(np.float32)
-            assert obj.shape == weight.shape
-            np.testing.assert_allclose(v.numpy(), obj.cpu().numpy(), atol=5e-4, rtol=1e-5)
-
+            weight = v.astype(np.float32)
+            assert obj.shape == weight.shape, (k, obj.shape, weight.shape)
+            np.testing.assert_allclose(v, obj.cpu().numpy(), atol=5e-4, rtol=1e-5)
+              
   def test_predictions(self):
     test_image_urls = ['https://raw.githubusercontent.com/ultralytics/yolov5/master/data/images/bus.jpg', 'https://www.aljazeera.com/wp-content/uploads/2022/10/2022-04-28T192650Z_1186456067_UP1EI4S1I0P14_RTRMADP_3_SOCCER-ENGLAND-MUN-CHE-REPORT.jpg']
     variant = 'n'
