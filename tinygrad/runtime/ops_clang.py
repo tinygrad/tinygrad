96810c6637771a349bf003193707b9ed7ec747c7
bl-ke <bhwhitworth@gmail.com>
2023-06-14 23:14:59 -0700

    added platform config for clang runtime and tempfile dir for xplatform /tmp

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,16 +1,24 @@
-import os, time, ctypes, hashlib, subprocess, platform
+import os, time, ctypes, hashlib, subprocess, platform, tempfile
 from tinygrad.ops import Compiled
 from tinygrad.runtime.lib import RawMallocBuffer
 from tinygrad.codegen.cstyle import CStyleCodegen, CStyleLanguage
+
+cfg = {
+  # NOTE: --rtlib=compiler-rt fixes float16 on Linux, it defines __gnu_h2f_ieee and __gnu_f2h_ieee
+  'base'   : ['clang', '-shared', '-O2', '-Wall', '-Werror', '--rtlib=compiler-rt', '-x', 'c'],
+  'Windows': {'cflags':[''], 'ext':'dll', 'export':'__declspec(dllexport)'},
+  'Linux'  : {'cflags':['-lm', '-fPIC'], 'ext':'so', 'export':'__attribute__((visibility("default")))'},
+  'Darwin' : {'cflags':[''], 'ext':'dylib', 'export':''}
+}
+plat_cfg = cfg[platform.system()]
 
 class ClangProgram:
   def __init__(self, name:str, prg:str):
     prg = "#include <math.h>\n#define max(x,y) ((x>y)?x:y)\n#define int64 long\n#define half __fp16\n#define uchar unsigned char\n#define bool uchar\n" + prg
     # TODO: is there a way to not write this to disk?
-    fn = f"/tmp/clang_{hashlib.md5(prg.encode('utf-8')).hexdigest()}.{'dylib' if platform.system() == 'Darwin' else 'so'}"
-    # NOTE: --rtlib=compiler-rt fixes float16 on Linux, it defines __gnu_h2f_ieee and __gnu_f2h_ieee
+    fn = f"{tempfile.gettempdir()}/clang_{hashlib.md5(prg.encode('utf-8')).hexdigest()}.{plat_cfg['ext']}"
     if not os.path.exists(fn):
-      subprocess.check_output(['clang', '-shared', '-O2', '-Wall','-Werror', '-lm', '--rtlib=compiler-rt', '-fPIC', '-x', 'c', '-', '-o', fn+".tmp"], input=prg.encode('utf-8'))
+      subprocess.check_output(cfg['base'] + plat_cfg['cflags'] + ['-o', fn+".tmp", '-'], input=prg.encode('utf-8'))
       os.rename(fn+".tmp", fn)
     self.lib = ctypes.CDLL(fn)
     self.fxn = self.lib[name]
@@ -21,7 +29,7 @@ class ClangProgram:
     if wait: return time.monotonic()-st
 
 class ClangCodegen(CStyleCodegen):
-  lang = CStyleLanguage(buffer_suffix=" restrict")
+  lang = CStyleLanguage(kernel_prefix=plat_cfg['export'], buffer_suffix=" restrict")
   supports_float4: bool = False
 
 ClangBuffer = Compiled(RawMallocBuffer, ClangCodegen, ClangProgram)
