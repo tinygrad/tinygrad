0ec417ca2f68a05b2b0d487f9e37468c56fc25a2
cloud11665 <cloud11665@gmail.com>
2023-06-15 23:01:53 +0200

    use find_library

--- extra/cudacpu/__init__.py
+++ extra/cudacpu/__init__.py
@@ -1,16 +1,13 @@
 from ctypes import CDLL, c_char_p, c_void_p, c_int, POINTER, cast
+from ctypes.util import find_library
 
-lib = CDLL("./extra/cudacpu/libcudacpu.so")
-
+lib = CDLL(find_library("cudacpu"))
 lib.ptx_kernel_create.argtypes = [c_char_p]
 lib.ptx_kernel_create.restype = c_void_p
 lib.ptx_kernel_destroy.argtypes = [c_void_p]
 lib.ptx_call.argtypes = [c_void_p,  c_int, POINTER(c_void_p), c_int, c_int, c_int, c_int, c_int, c_int]
 
 class PTXKernel:
-    def __init__(self, source: bytes):
-        self.kernel = lib.ptx_kernel_create(c_char_p(source))
-    def __call__(self, *args, block, grid):
-        lib.ptx_call(self.kernel, len(args), (c_void_p * len(args))(*[cast(x, c_void_p) for x in args]), *block, *grid)
-    def __del__(self):
-        lib.ptx_kernel_destroy(self.kernel)
+    def __init__(self, source: bytes): self.kernel = lib.ptx_kernel_create(c_char_p(source))
+    def __call__(self, *args, block, grid): lib.ptx_call(self.kernel, len(args), (c_void_p * len(args))(*[cast(x, c_void_p) for x in args]), *block, *grid)
+    def __del__(self): lib.ptx_kernel_destroy(self.kernel)
