e4a35057231c63bac4c8d608ffc48a67bc7c0ba1
cloud11665 <cloud11665@gmail.com>
2023-06-18 02:01:18 +0200

    add cstyle.header, don't run fp16 tests on emulator

--- tinygrad/runtime/ops_cuda.py
+++ tinygrad/runtime/ops_cuda.py
@@ -57,14 +57,13 @@ class CUDACodegen(CStyleCodegen):
     kernel_prefix = "__global__", smem_prefix = "__shared__ ", barrier = "__syncthreads();", float4 = "make_float4",
     gid = [f'blockDim.{chr(120+i)}*blockIdx.{chr(120+i)}+threadIdx.{chr(120+i)}' for i in range(3)],
     lid = [f'threadIdx.{chr(120+i)}' for i in range(3)],
+    header = "typedef unsigned char uchar; typedef long long int64;"
     half_prekernel = """
       #include <cuda_fp16.h>
       struct __align__(8) half4 {
         half2 x, y;
         __device__ __forceinline__ explicit operator float4() const {return make_float4(__half2float(x.x), __half2float(x.y), __half2float(y.x), __half2float(y.y)); }
       };
-      typedef unsigned char uchar;
-      typedef long long int64;
     """)
   supports_float4_alu = False
 CUDABuffer = Compiled(RawMallocBuffer, CUDACodegen, CUDAProgram) if EMULATING else Compiled(RawCUDABuffer, PTXCodegen if getenv("PTX") else CUDACodegen, CUDAProgram, cuda.Context.synchronize)
