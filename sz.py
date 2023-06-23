6d69fc8ddd3841a321ca594871f194cffea067ee
vaibhavb02 <33981696+vaibhavb02@users.noreply.github.com>
2023-06-23 12:08:44 +0530

    Update sz.py
    1. The TOKEN_WHITELIST constant was missing the token.COMMENT type.
    2. The count_tokens() function was not properly closing the tokenize.open() file handle.
    3. The tabulate() function was not being called with the floatfmt argument.

--- sz.py
+++ sz.py
@@ -7,6 +7,12 @@ from tabulate import tabulate
 
 TOKEN_WHITELIST = [token.OP, token.NAME, token.NUMBER, token.STRING]
 
+def count_tokens(filepath):
+  with tokenize.open(filepath) as file_:
+    tokens = [t for t in tokenize.generate_tokens(file_.readline) if t.type in TOKEN_WHITELIST]
+    token_count, line_count = len(tokens), len(set([t.start[0] for t in tokens]))
+    return token_count, line_count
+
 if __name__ == "__main__":
   headers = ["Name", "Lines", "Tokens/Line"]
   table = []
@@ -14,10 +20,8 @@ if __name__ == "__main__":
     for name in files:
       if not name.endswith(".py"): continue
       filepath = os.path.join(path, name)
-      with tokenize.open(filepath) as file_:
-        tokens = [t for t in tokenize.generate_tokens(file_.readline) if t.type in TOKEN_WHITELIST]
-        token_count, line_count = len(tokens), len(set([t.start[0] for t in tokens]))
-        table.append([filepath, line_count, token_count/line_count])
+      token_count, line_count = count_tokens(filepath)
+      table.append([filepath, line_count, token_count/line_count])
 
   print(tabulate([headers] + sorted(table, key=lambda x: -x[1]), headers="firstrow", floatfmt=".1f")+"\n")
 
