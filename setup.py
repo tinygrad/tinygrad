f9c22cbfdb7b86f13ff472d749761bc7161cd3f8
KARTHIK KUMAR GUPTA <137091753+KARTHIK07GUPTA@users.noreply.github.com>
2023-06-24 22:56:46 +0530

    Update setup.py
    These changes help improve the readability and maintainability of the code while preserving its functionality.

--- setup.py
+++ setup.py
@@ -5,23 +5,32 @@ from setuptools import setup
 
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
+    long_description = f.read()
 
-setup(name='tinygrad',
-      version='0.6.0',
-      description='You like pytorch? You like micrograd? You love tinygrad! <3',
-      author='George Hotz',
-      license='MIT',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages = ['tinygrad', 'tinygrad.codegen', 'tinygrad.nn', 'tinygrad.runtime', 'tinygrad.shape'],
-      classifiers=[
+setup(
+    name='tinygrad',
+    version='0.6.0',
+    description='You like pytorch? You like micrograd? You love tinygrad! <3',
+    author='George Hotz',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['tinygrad', 'tinygrad.codegen', 'tinygrad.nn', 'tinygrad.runtime', 'tinygrad.shape'],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
-      ],
-      install_requires=['numpy', 'requests', 'pillow', 'tqdm', 'networkx', 'pyopencl', 'PyYAML'],
-      python_requires='>=3.8',
-      extras_require={
+    ],
+    install_requires=[
+        'numpy',
+        'requests',
+        'pillow',
+        'tqdm',
+        'networkx',
+        'pyopencl',
+        'PyYAML',
+    ],
+    python_requires='>=3.8',
+    extras_require={
         'llvm': ["llvmlite"],
         'cuda': ["pycuda"],
         'triton': ["triton>=2.0.0.dev20221202"],
@@ -43,5 +52,6 @@ setup(name='tinygrad',
             "safetensors",
             "types-PyYAML",
         ],
-      },
-      include_package_data=True)
+    },
+    include_package_data=True,
+)
