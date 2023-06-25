6b4f65ae2a62003abbc8f965db2a4800d139284d
George Hotz <geohot@gmail.com>
2023-06-25 14:46:01 -0700

    uses the real device codegen

--- tinygrad/runtime/ops_fake.py
+++ tinygrad/runtime/ops_fake.py
@@ -1,9 +1,8 @@
-# used for compilation speed tests of CLCodegen
+# used for compilation only speed tests
 import numpy as np
-from tinygrad.helpers import dtypes, prod, DEBUG
+from tinygrad.helpers import dtypes, prod
 from tinygrad.ops import Compiled
 from tinygrad.runtime.lib import RawBuffer
-from tinygrad.runtime.ops_gpu import CLCodegen
 
 class RawFakeBuffer(RawBuffer):
   @classmethod
@@ -11,8 +10,8 @@ class RawFakeBuffer(RawBuffer):
   def toCPU(self): return np.empty(self.size, dtype=self.dtype.np)
 
 class FakeProgram:
-  def __init__(self, name:str, prg:str):
-    if DEBUG >= 3: print(prg)
+  def __init__(self, name:str, prg:str): pass
   def __call__(self, global_size, local_size, *args, wait=False): pass
 
-FakeBuffer = Compiled(RawFakeBuffer, CLCodegen, FakeProgram)
+# NOTE: you have to set a codegen to use this
+FakeBuffer = Compiled(RawFakeBuffer, None, FakeProgram)
