e234bf2298fbe525c1752726e944357cf945ae63
George Hotz <geohot@gmail.com>
2023-06-28 19:54:33 +0000

    hip matmul : add K support

--- extra/gemm/hip_matmul.py
+++ extra/gemm/hip_matmul.py
@@ -3,8 +3,9 @@ import numpy as np
 from tinygrad.helpers import dtypes, getenv
 from tinygrad.runtime.ops_hip import RawHIPBuffer, HIPProgram
 
-N = getenv("N", 1024)
-assert N%16 == 0, "multiple of 16"
+N = getenv("N", 64)
+K = getenv("K", 1)
+assert N%(16*K) == 0, f"N must be multiple of {16*K}"
 FLOPS = N*N*N*2
 BW = N*N*3*4
 
@@ -22,29 +23,38 @@ extern "C" __global__ void test(float* c, __half* a, __half* b) {{
   const int gx = blockIdx.x;
   const int gy = blockIdx.y;
 
-  c += gx*16*{N} + gy*16;
-  a += gx*16*{N};
-  b += gy*16;
+  c += gx*{K*16}*{N} + gy*{K*16};
+  a += gx*{K*16}*{N};
+  b += gy*{K*16};
 
   const int lIdx = threadIdx.x;
-  const int lane = lIdx % 16;
+  const int lane = lIdx%16;
 
-  half16 a_frag;
-  half16 b_frag;
-  float8 c_frag = {{}};
+  half16 a_frag[{K}];
+  half16 b_frag[{K}];
+  float8 c_frag[{K}][{K}] = {{}};
 
   for (int k = 0; k < {N}; k += 16) {{
     for (int ele = 0; ele < 16; ++ele) {{
-      a_frag[ele] = a[{N}*lane + (k+ele)];
-      b_frag[ele] = b[(k+ele)*{N} + lane];
+      for (int x = 0; x < {K}; x++) {{
+        a_frag[x][ele] = a[{N}*lane + (k+ele) + x*{16*N}];
+        b_frag[x][ele] = b[(k+ele)*{N} + lane + x*16];
+      }}
     }}
-
-    c_frag = __builtin_amdgcn_wmma_f32_16x16x16_f16_w32(a_frag, b_frag, c_frag);
+    for (int y = 0; y < {K}; y++) {{
+      for (int x = 0; x < {K}; x++) {{
+        c_frag[y][x] = __builtin_amdgcn_wmma_f32_16x16x16_f16_w32(a_frag[x], b_frag[y], c_frag[y][x]);
+      }}
+    }}
   }}
 
   for (int ele = 0; ele < 8; ++ele) {{
     const int r = ele * 2 + (lIdx / 16);
-    c[{N}*r + lane] = c_frag[ele];
+    for (int y = 0; y < {K}; y++) {{
+      for (int x = 0; x < {K}; x++) {{
+        c[{N}*r + lane + x*16 + y*{16*N}] = c_frag[x][y][ele];
+      }}
+    }}
   }}
 }}""")
 
@@ -55,7 +65,7 @@ def timeit(fxn):
   #print(f"{ret*1e6:.2f} us")
   return ret
 
-tm = min([timeit(lambda: prog([N//16, N//16, 1], [32, 1, 1], a, b, c, wait=True)) for _ in range(20)])
+tm = min([timeit(lambda: prog([N//(K*16), N//(K*16), 1], [32, 1, 1], a, b, c, wait=True)) for _ in range(20)])
 na = a.toCPU().reshape(N,N)
 comp = nb.astype(np.float32) @ nc.astype(np.float32)
 print(f"{N*N:10d} {tm*1e6:9.2f} us, would be {FLOPS*1e-9/tm:9.2f} GFLOPS matmul, {BW*1e-9/tm:.2f} GB/s")
