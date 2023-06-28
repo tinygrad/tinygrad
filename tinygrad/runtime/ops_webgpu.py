c9765d41ee8f9fcdf05f590b6a53a1dacf30cc31
dc-dc-dc <dgcruz983@gmail.com>
2023-06-28 13:08:40 -0400

    refactor

--- tinygrad/runtime/ops_webgpu.py
+++ tinygrad/runtime/ops_webgpu.py
@@ -4,20 +4,14 @@ from tinygrad.codegen.linearizer import Linearizer, LocalBuffer, UOps
 from tinygrad.helpers import DType, dtypes
 from tinygrad.ops import Compiled, UnaryOps, BinaryOps, ASTRunner, FusedOps
 from tinygrad.shape.symbolic import NumNode, render_python, DivNode, AndNode, Variable
+from tinygrad.codegen.cstyle import render_cl
 import math
 import wgpu
+
 device = get_default_device()
 
-render_cl = render_python.copy()
-render_cl[DivNode] = lambda self,ops,ctx: f"({self.a.render(ops, ctx)}/{self.b})"
-render_cl[AndNode] = lambda self,ops,ctx: f"({'&&'.join(sorted([x.render(ops,ctx) for x in self.nodes]))})"
-
 class WebGPUProgram:
-  def __init__(self, name: str, prg: str):
-    # if DEBUG >= 5: print(prg)
-    print("NAME", name)
-    self.name,self.prg = name,device.create_shader_module(code=prg)
-
+  def __init__(self, name: str, prg: str): self.name,self.prg = name,device.create_shader_module(code=prg)
   def __call__(self, global_size, local_size, *bufs, wait=False):
     binding_layouts = [{"binding": i, "visibility": wgpu.ShaderStage.COMPUTE, "buffer": {"type": wgpu.BufferBindingType.storage}} for i in range(len(bufs))]
     bindings = [{"binding": i, "resource": {"buffer": x._buf, "offset": 0, "size": x._buf.size}} for i, x in enumerate(bufs)]
@@ -28,8 +22,7 @@ class WebGPUProgram:
     command_encoder = device.create_command_encoder()
     compute_pass = command_encoder.begin_compute_pass()
     compute_pass.set_pipeline(compute_pipeline)
-    # last 2 elements not used
-    compute_pass.set_bind_group(0, bind_group, [], 0, 999999)
+    compute_pass.set_bind_group(0, bind_group, [], 0, 999999) # last 2 not used
     compute_pass.dispatch_workgroups(*global_size)  # x y z
     compute_pass.end()
     device.queue.submit([command_encoder.finish()])
@@ -52,15 +45,12 @@ class WebGpuCodegen(Linearizer):
     self.limit_global_dims(3)
     self.linearize()
 
-    kernel = []
-    global_size = []
-    local_size = []
+    kernel,global_size,local_size = [],[],[]
     depth = 0
     def kk(s): kernel.append(" "*depth+s)
     bufnames = ["temp" if isinstance(b, LocalBuffer) else f"data{i}" for i,b in enumerate(self.bufs)]
     depth += 1
-    gid = [f"gindex.{'xyz'[x]}" for x in range(3)]
-    lid = []
+    gid,lid = [f"gindex.{'xyz'[x]}" for x in range(3)],[]
     pend_close = None
     for uop,newvar,vin,args in self.uops:
       if uop == UOps.LOOP:
@@ -99,47 +89,31 @@ class WebGpuCodegen(Linearizer):
         else:
           val = f"{bufnames[args.i]}[{args.idx.render(render_cl)}]"
         if args.valid.min == 1: kk(f"let {newvar.render()} = {val};")
-        # elif args.valid.min == 0: kk(f"let {newvar.render()} = 0.0f;")
         else: kk(f"var {newvar.render()} = select(0.0f, {val}, bool({args.valid.render(render_cl)}));")
       elif uop == UOps.ALU:
         assert newvar is not None
-        if newvar in vin:
-          kk(f"{newvar.render()} = {code_for_op[args](*[x.render() for x in vin])};")
-        else:
-          kk(f"let {newvar.render()} = {code_for_op[args](*[x.render() for x in vin])};")
+        if newvar in vin: kk(f"{newvar.render()} = {code_for_op[args](*[x.render() for x in vin])};")
+        else: kk(f"let {newvar.render()} = {code_for_op[args](*[x.render() for x in vin])};")
       elif uop == UOps.STORE:
         val = vin[0].render()
         if vin[0].dtype != self.bufs[args.i].dtype:
           val = f"{type_map[self.bufs[args.i].dtype]}({val})"
-        
         kk(f"{bufnames[args.i]}[{args.idx.render(render_cl)}] = {val};")
       elif uop == UOps.CONST:
         assert newvar is not None
-        if args == -math.inf:
-          kk(f"var {newvar.render()} = 1.17549435082228750797e-38f;")
-        else:
-          kk(f"var {newvar.render()} = {args};")
-      elif uop == UOps.DEFINE_LOCAL:
-        kk(f"var {args[0]} = array<f32,{args[1]}>();")
-      elif uop == UOps.BARRIER:
-        kk("workgroupBarrier();")
-      else:
-        raise RuntimeError(f"failed to render {uop}")
-
+        if args == -math.inf: kk(f"var {newvar.render()} = 1.17549435082228750797e-38f;")
+        else: kk(f"var {newvar.render()} = {args};")
+      elif uop == UOps.DEFINE_LOCAL: kk(f"var {args[0]} = array<f32,{args[1]}>();")
+      elif uop == UOps.BARRIER: kk("workgroupBarrier();")
+      else: raise RuntimeError(f"failed to render {uop}")
     prg = "\n".join([f"@group(0) @binding({i}) var<storage,read_write> data{i}: array<{type_map[x.dtype]}>;" for i,x in enumerate(self.bufs) if not isinstance(x, LocalBuffer) and not isinstance(x.realized, RawConst)])
-    # TODO: revert local_size {','.join([str(x) for x in local_size])} once bug is fixed
-    prg += f"\n@compute @workgroup_size(1) fn {self.function_name}(@builtin(global_invocation_id) gindex: vec3<u32>, @builtin(local_invocation_id) lindex: vec3<u32>) {{\n" + "\n".join(kernel) + "\n}"
+    
+    prg += f"\n@compute @workgroup_size(1) fn {self.function_name}(@builtin(global_invocation_id) gindex: vec3<u32>, @builtin(local_invocation_id) lindex: vec3<u32>) {{\n" + "\n".join(kernel) + "\n}" # TODO: revert local_size {','.join([str(x) for x in local_size])} once bug is fixed
     return ASTRunner(self.function_name, prg, global_size[::-1] if len(global_size) else [1], local_size[::-1] if len(local_size) else [1])
-
-
+  
 class RawWebGPUBuffer(RawBufferMapped):
-    def __init__(self, size, dtype: DType): super().__init__(size, dtype, device.create_buffer(
-        size=size*dtype.itemsize, usage=wgpu.BufferUsage.STORAGE | wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.COPY_SRC))
-
+    def __init__(self, size, dtype: DType): super().__init__(size, dtype, device.create_buffer(size=size*dtype.itemsize, usage=wgpu.BufferUsage.STORAGE | wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.COPY_SRC))
     def _copyin(self, x) -> None: device.queue.write_buffer(self._buf, 0, x)
-
-    def _buffer(
-        self) -> memoryview: return device.queue.read_buffer(self._buf, 0)
-
+    def _buffer(self) -> memoryview: return device.queue.read_buffer(self._buf, 0)
 
 WebGpuBuffer = Compiled(RawWebGPUBuffer, WebGpuCodegen, WebGPUProgram)
