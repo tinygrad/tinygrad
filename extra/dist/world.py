800c48d03c25424bb947099985eee74f8a1a08dc
Woze Parrot <wozeparrot@gmail.com>
2023-06-29 17:37:31 -0400

    feat: send and recv are now functions

--- extra/dist/world.py
+++ extra/dist/world.py
@@ -1,31 +1,56 @@
 from extra import dist
-from tinygrad.tensor import Tensor
-from tinygrad.lazy import Device
+from math import prod
 from multiprocessing import shared_memory
+from tinygrad.lazy import Device, LazyBuffer
+from tinygrad.ops import LoadOps
+from tinygrad.tensor import Tensor, Function
 
-# sends a tensor from our rank to the target rank
+# sends a lazybuffer from our rank to the target rank
 # transfer method differs depending on if this is a cross or intra node transfer
-def send(tensor:Tensor, target_rank, **kwargs):
+def send_lb(x:LazyBuffer, target_rank, **kwargs) -> None:
   # assuming intra node transfer so we just use shared memory
   # cache the shared memory so we don't have to create it every time
   cache_id = kwargs.get("cache_id", None)
-  if cache_id in send.shared_memory_cache:
-    shm_name = send.shared_memory_cache[cache_id]
+  if cache_id in send_lb.shared_memory_cache:
+    shm_name = send_lb.shared_memory_cache[cache_id]
   else:
-    shm_name = (s := shared_memory.SharedMemory(create=True, size=tensor.nbytes())).name
+    shm_name = (s := shared_memory.SharedMemory(create=True, size=prod(x.shape) * x.dtype.itemsize)).name
     s.close()
-    if cache_id is not None: send.shared_memory_cache[cache_id] = shm_name
-  tensor.to(f"shm:{shm_name}").realize()
-  dist.OOB.send((tensor.shape, tensor.dtype, (shm_name, cache_id is not None)), target_rank)
-setattr(send, "shared_memory_cache", {})
+    if cache_id is not None: send_lb.shared_memory_cache[cache_id] = shm_name
+  # we instantly realize here to force the copy into shared memory
+  LazyBuffer.loadop(LoadOps.FROM, x.shape, x.dtype, f"shm:{shm_name}", src=x).realize()
+  dist.OOB.send((x.shape, x.dtype, (shm_name, cache_id is not None)), target_rank)
+setattr(send_lb, "shared_memory_cache", {})
 
-def recv(into=None):
+# TODO: use lazybuffer directly instead of tensor
+def recv_lb() -> LazyBuffer:
   shape, dtype, extra = dist.OOB.recv()
   # intra node transfer so we just use shared memory
   tensor = Tensor.empty(*shape, dtype=dtype, device=f"shm:{extra[0]}").to(Device.DEFAULT).realize()
-  if into is not None: into.assign(tensor)
   # delete the shared memory if we're not caching it
   if not extra[1]:
     (s := shared_memory.SharedMemory(name=extra[0])).close()
     s.unlink()
-  return tensor
+  return tensor.lazydata
+
+class Send(Function):
+  def forward(self, x:LazyBuffer, target_rank, **kwargs) -> LazyBuffer:
+    self.target_rank, self.kwargs = target_rank, kwargs
+    send_lb(x, target_rank, **kwargs)
+    return x
+  def backward(self, _:LazyBuffer) -> LazyBuffer:
+    return recv_lb()
+
+class Recv(Function):
+  def forward(self, _:LazyBuffer, target_rank, **kwargs) -> LazyBuffer:
+    self.target_rank, self.kwargs = target_rank, kwargs
+    return recv_lb()
+  def backward(self, grad_output:LazyBuffer) -> LazyBuffer:
+    send_lb(grad_output, self.target_rank, **self.kwargs)
+    return grad_output
+
+def send(x:Tensor, target_rank, **kwargs) -> Tensor:
+  return Send.apply(x, target_rank=target_rank, **kwargs)
+
+def recv(x:Tensor, target_rank, **kwargs) -> Tensor:
+  return Recv.apply(x, target_rank=target_rank, **kwargs)
