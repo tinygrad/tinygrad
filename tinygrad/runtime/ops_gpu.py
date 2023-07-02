5a598277f0a387fd508b280037655985185a9eab
rezaarezvan <rezvanr2378@gmail.com>
2023-07-02 23:30:45 +0200

    Refactor: Remove unused imports

--- tinygrad/runtime/ops_gpu.py
+++ tinygrad/runtime/ops_gpu.py
@@ -3,7 +3,7 @@ import pathlib
 import numpy as np
 import pyopencl as cl  # type: ignore
 from typing import Optional, List
-from tinygrad.helpers import DEBUG, getenv, prod, ImageDType, OSX, dtypes, fromimport
+from tinygrad.helpers import DEBUG, getenv, prod, ImageDType, OSX, fromimport
 from tinygrad.ops import Compiled
 from tinygrad.runtime.lib import RawBufferCopyInOut
 from tinygrad.codegen.cstyle import CStyleCodegen, CStyleLanguage
