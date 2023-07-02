a239b6cd743997b2414b1f49d5fc262d38698a31
alex newman <posix4e@alexs-Air.webpass.net>
2023-07-02 13:42:35 -0700

    Removing untested code

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -5,7 +5,6 @@ import unittest
 import itertools
 from tinygrad.tensor import Tensor, Device
 from tinygrad.helpers import dtypes
-from extra.gradcheck import numerical_jacobian, jacobian, gradcheck
 
 x_init = np.random.randn(1,3).astype(np.float32)
 U_init = np.random.randn(3,3).astype(np.float32)
@@ -104,41 +103,6 @@ class TestTinygrad(unittest.TestCase):
     non_zeros = np.count_nonzero(w.cpu().numpy())
     expected = n * (1 - rate)
     np.testing.assert_allclose(non_zeros, expected, rtol=2e-3)
-
-  #@unittest.skipUnless(Device.DEFAULT == Device.CPU, "float64 not supported on GPU")
-  @unittest.skip("float64 support broken")
-  def test_jacobian(self):
-    W = np.random.RandomState(1337).random((10, 5))
-    x = np.random.RandomState(7331).random((1, 10)) - 0.5
-
-    torch_x = torch.tensor(x, requires_grad=True)
-    torch_W = torch.tensor(W, requires_grad=True)
-    torch_func = lambda x: torch.nn.functional.log_softmax(x.matmul(torch_W).relu(), dim=1)
-    PJ = torch.autograd.functional.jacobian(torch_func, torch_x).squeeze().numpy()
-
-    tiny_x = Tensor(x)
-    tiny_W = Tensor(W)
-    tiny_func = lambda x: x.dot(tiny_W).relu().log_softmax()
-    J = jacobian(tiny_func, tiny_x)
-    NJ = numerical_jacobian(tiny_func, tiny_x)
-
-    np.testing.assert_allclose(PJ, J, atol = 1e-5)
-    np.testing.assert_allclose(PJ, NJ, atol = 1e-5)
-
-  #@unittest.skipUnless(Device.DEFAULT == Device.CPU, "float64 not supported on GPU")
-  @unittest.skip("float64 support broken")
-  def test_gradcheck(self):
-    W = np.random.RandomState(1337).random((10, 5))
-    x = np.random.RandomState(7331).random((1, 10)) - 0.5
-
-    tiny_x = Tensor(x)
-    tiny_W = Tensor(W)
-    tiny_func = lambda x: x.dot(tiny_W).relu().log_softmax()
-
-    self.assertTrue(gradcheck(tiny_func, tiny_x))
-
-    # coarse approx. since a "big" eps and the non-linearities of the model
-    self.assertFalse(gradcheck(tiny_func, tiny_x, eps = 0.1))
 
   def test_random_fns_are_deterministic_with_seed(self):
     for random_fn in [Tensor.randn, Tensor.uniform, Tensor.scaled_uniform, Tensor.glorot_uniform]:
