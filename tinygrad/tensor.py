ccc1b99cec7957d074298e0dd0bee86545a56dde
Mike <ovyan13@gmail.com>
2023-07-03 21:43:44 +0300

    py moment

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -302,15 +302,7 @@ class Tensor:
       new_shape = new_shape[::2]
       final_slice = reduce(operator.add, (((0, sh), (0, 1)) for sh in new_shape), ())
       sliced_tensor = reshaped_tensor.shrink(final_slice)
-    final_shape = []
-    it_shape = iter(new_shape)
-    for i in orig_slices:
-      if isinstance(i, (int, slice)):
-        dim_shape = next(it_shape)
-        if isinstance(i, slice): final_shape.append(dim_shape)
-      else: # i is None
-        final_shape.append(1)
-    return sliced_tensor.reshape(tuple(final_shape))  # Reshape
+    return sliced_tensor.reshape(tuple([next(iter(new_shape)) if isinstance(i, slice) else 1 for i in orig_slices if isinstance(i, (int, slice))]))  # Reshape
 
   def cat(self, *args, dim=0):
     dim = (dim + len(self.shape)) if dim < 0 else dim
