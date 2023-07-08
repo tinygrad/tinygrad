31b139ab644ae7918915be16a5b3be58480906cf
Carson Radtke <nosrac925@gmail.com>
2023-07-08 01:22:53 -0500

    fix abstractions + mypy

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -61,8 +61,8 @@ class Tensor:
     if data.__class__ is list:
       data = np.array(data, dtype=(dtype or Tensor.default_type).np)
 
-    if isinstance(data, (int, float)) or data.__class__ is np.ndarray and data.size == 1:
-      self.lazydata = LazyBuffer.loadop(LoadOps.CONST, data.shape if data.__class__ is np.ndarray else tuple(), dtype or Tensor.default_type, device, data)
+    if isinstance(data, (int, float)) or data.__class__ is np.ndarray and cast(np.ndarray, data).size == 1:
+      self.lazydata = LazyBuffer.loadop(LoadOps.CONST, cast(np.ndarray, data).shape if data.__class__ is np.ndarray else tuple(), dtype or Tensor.default_type, device, data)
       return
 
     if data.__class__ is np.ndarray:
