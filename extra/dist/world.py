2fa1126d54eae2c10fb4ff088e542dc4be212265
Woze Parrot <wozeparrot@gmail.com>
2023-07-08 00:02:43 -0400

    feat: measure bandwidth

--- extra/dist/world.py
+++ extra/dist/world.py
@@ -4,12 +4,14 @@ from multiprocessing import shared_memory
 from tinygrad.lazy import Device, LazyBuffer
 from tinygrad.ops import LoadOps
 from tinygrad.tensor import Tensor, Function
+import time
 
 # sends a lazybuffer from our rank to the target rank
 # transfer method differs depending on if this is a cross or intra node transfer
 def send_lb(x:LazyBuffer, target_rank, **kwargs) -> None:
   # assuming intra node transfer so we just use shared memory
   # cache the shared memory so we don't have to create it every time
+  st = time.perf_counter()
   cache_id = kwargs.get("cache_id", None)
   if cache_id in send_lb.shared_memory_cache:
     shm_name = send_lb.shared_memory_cache[cache_id]
@@ -19,19 +21,20 @@ def send_lb(x:LazyBuffer, target_rank, **kwargs) -> None:
     if cache_id is not None: send_lb.shared_memory_cache[cache_id] = shm_name
   # we instantly realize here to force the copy into shared memory
   LazyBuffer.loadop(LoadOps.FROM, x.shape, x.dtype, f"shm:{shm_name}", src=x).realize()
-  dist.OOB.send((x.shape, x.dtype, (shm_name, cache_id is not None)), target_rank)
+  dist.OOB.send((x.shape, x.dtype, (shm_name, cache_id is not None), st), target_rank)
 setattr(send_lb, "shared_memory_cache", {})
 
-# TODO: use lazybuffer directly instead of tensor
-def recv_lb() -> LazyBuffer:
-  shape, dtype, extra = dist.OOB.recv()
+def recv_lb(target_rank) -> LazyBuffer:
+  shape, dtype, extra, st = dist.OOB.recv(target_rank)
   # intra node transfer so we just use shared memory
-  tensor = Tensor.empty(*shape, dtype=dtype, device=f"shm:{extra[0]}").to(Device.DEFAULT).realize()
+  lb = LazyBuffer.loadop(LoadOps.FROM, shape, dtype, Device.DEFAULT, src=LazyBuffer.loadop(LoadOps.EMPTY, shape, dtype, f"shm:{extra[0]}"))
   # delete the shared memory if we're not caching it
   if not extra[1]:
     (s := shared_memory.SharedMemory(name=extra[0])).close()
     s.unlink()
-  return tensor.lazydata
+  rt = time.perf_counter()
+  print(f"took: {rt - st}s, bandwidth: {(prod(shape) * dtype.itemsize) / (rt - st) / 1e9} GB/s")
+  return lb
 
 # these aren't true lazyop adding functions
 class Send(Function):
@@ -40,12 +43,12 @@ class Send(Function):
     send_lb(x, target_rank, **kwargs)
     return x
   def backward(self, _:LazyBuffer) -> LazyBuffer:
-    return recv_lb()
+    return recv_lb(self.target_rank)
 
 class Recv(Function):
   def forward(self, _:LazyBuffer, target_rank, **kwargs) -> LazyBuffer:
     self.target_rank, self.kwargs = target_rank, kwargs
-    return recv_lb()
+    return recv_lb(target_rank)
   def backward(self, grad_output:LazyBuffer) -> LazyBuffer:
     send_lb(grad_output, self.target_rank, **self.kwargs)
     return grad_output
