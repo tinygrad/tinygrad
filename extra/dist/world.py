9ce219c302e0dd6694dff4a72fae522f5acad290
Woze Parrot <wozeparrot@gmail.com>
2023-07-09 01:57:15 -0400

    feat: blazing fast shared memory

--- extra/dist/world.py
+++ extra/dist/world.py
@@ -11,7 +11,6 @@ import time
 def send_lb(x:LazyBuffer, target_rank, **kwargs) -> None:
   # assuming intra node transfer so we just use shared memory
   # cache the shared memory so we don't have to create it every time
-  st = time.perf_counter()
   cache_id = kwargs.get("cache_id", None)
   if cache_id in send_lb.shared_memory_cache:
     shm_name = send_lb.shared_memory_cache[cache_id]
@@ -20,20 +19,18 @@ def send_lb(x:LazyBuffer, target_rank, **kwargs) -> None:
     s.close()
     if cache_id is not None: send_lb.shared_memory_cache[cache_id] = shm_name
   # we instantly realize here to force the copy into shared memory
-  LazyBuffer.loadop(LoadOps.FROM, x.shape, x.dtype, f"shm:{shm_name}", src=x).realize()
-  dist.OOB.send((x.shape, x.dtype, (shm_name, cache_id is not None), st), target_rank)
+  LazyBuffer.loadop(LoadOps.FROM, x.shape, x.dtype, f"shm:{shm_name},{cache_id}", src=x).realize()
+  dist.OOB.send((x.shape, x.dtype, (shm_name, cache_id)), target_rank)
 setattr(send_lb, "shared_memory_cache", {})
 
 def recv_lb(target_rank) -> LazyBuffer:
-  shape, dtype, extra, st = dist.OOB.recv(target_rank)
+  shape, dtype, extra = dist.OOB.recv(target_rank)
   # intra node transfer so we just use shared memory
-  lb = LazyBuffer.loadop(LoadOps.FROM, shape, dtype, Device.DEFAULT, src=LazyBuffer.loadop(LoadOps.EMPTY, shape, dtype, f"shm:{extra[0]}"))
+  lb = LazyBuffer.loadop(LoadOps.FROM, shape, dtype, Device.DEFAULT, src=LazyBuffer.loadop(LoadOps.EMPTY, shape, dtype, f"shm:{extra[0]},{extra[1]}")).realize()
   # delete the shared memory if we're not caching it
-  if not extra[1]:
+  if extra[1] is None:
     (s := shared_memory.SharedMemory(name=extra[0])).close()
     s.unlink()
-  rt = time.perf_counter()
-  print(f"took: {rt - st}s, bandwidth: {(prod(shape) * dtype.itemsize) / (rt - st) / 1e9} GB/s")
   return lb
 
 # these aren't true lazyop adding functions
--- tinygrad/runtime/ops_shm.py
+++ tinygrad/runtime/ops_shm.py
@@ -1,15 +1,24 @@
-from multiprocessing import shared_memory
+import os, mmap, _posixshmem
 from typing import Callable, Dict
 from tinygrad.helpers import DType
 from tinygrad.runtime.lib import RawBufferMapped
 from tinygrad.ops import Interpreted, Op, UnaryOps, MovementOps
 
+SHM_CACHE: Dict[str, mmap.mmap] = {}
+
 class RawShmBuffer(RawBufferMapped):
   def __init__(self, size, dtype:DType, device:str):
-    super().__init__(size, dtype, shared_memory.SharedMemory(name=device))
-    setattr(self._buf, "shape", (size,))
-  def __del__(self): self._buf.close()
-  def _buffer(self): return self._buf.buf
+    device, cache_id = device.split(",")[0], None if "," not in device else device.split(",")[1]
+
+    if cache_id in SHM_CACHE: shm = SHM_CACHE[cache_id]
+    else:
+      fd = _posixshmem.shm_open(device, os.O_RDWR, 0o600)
+      shm = mmap.mmap(fd, size * dtype.itemsize, flags=mmap.MAP_SHARED | 0x2000 | 0x008000)
+      os.close(fd)
+      if cache_id is not None: SHM_CACHE[cache_id] = shm
+
+    super().__init__(size, dtype, shm)
+  def _buffer(self): return memoryview(self._buf)
 
 shm_fxn_for_op: Dict[Op, Callable] = { UnaryOps.NOOP: lambda x:x, MovementOps.RESHAPE: lambda x,_:x }
 ShmBuffer = Interpreted(RawShmBuffer, shm_fxn_for_op, to_underlying=lambda x:x, from_underlying=lambda x:x)
