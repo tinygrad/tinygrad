b2188e59e057d783dbe122ef4b6bc85ce1db9d1b
FrostyTheSouthernSnowman <92753960+FrostyTheSouthernSnowman@users.noreply.github.com>
2023-07-10 17:12:00 -0400

    Added back type matching tests

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -116,6 +116,7 @@ class TestInt32Dtype(unittest.TestCase):
   def test_int32_add_upcast_int64(self): _test_add_upcast(Tensor([1,2,3,4], dtype=dtypes.int32), Tensor([1,2,3,4], dtype=dtypes.int64), dtypes.int64, [2,4,6,8])
   def test_int32_mul_upcast_int64(self): _test_mul_upcast(Tensor([1,2,3,4], dtype=dtypes.int32), Tensor([1,2,3,4], dtype=dtypes.int64), dtypes.int64, [1,4,9,16])
   def test_int32_matmul_upcast_int64(self): _test_matmul_upcast(Tensor([[1,2],[3,4]], dtype=dtypes.int32), Tensor.eye(2, dtype=dtypes.int64), dtypes.int64, [[1,2],[3,4]])
+"""
 
 class TestTypeMatching(unittest.TestCase):
   def test_type_matching_same(self):
@@ -134,7 +135,6 @@ class TestTypeMatching(unittest.TestCase):
 
     assert a_match.dtype == np.float32
     assert b_match.dtype == np.float32
-"""
 
 if __name__ == '__main__':
   unittest.main()
