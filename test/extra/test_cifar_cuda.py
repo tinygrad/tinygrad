a576cda8053a70fed057529d6e5eb00c919738fb
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-07-10 07:40:19 -0500

    minimum example to fix cuda on cifar

---
+++ test/extra/test_cifar_cuda.py
@@ -0,0 +1,32 @@
+import time
+import numpy as np
+from tinygrad import nn
+from tinygrad.nn import optim
+from tinygrad.tensor import Tensor
+from tinygrad.jit import TinyJit
+from tinygrad.state import get_parameters
+
+class Net:
+    def __init__(self):
+        self.conv = nn.Conv2d(3, 64, kernel_size=1)
+        self.linear= nn.Linear(32, 10, bias=False)        
+
+    def __call__(self, x):
+        x = self.conv(x)
+        print(x)
+        x = self.linear(x)
+        return x.log_softmax()
+    
+if __name__=="__main__":
+    x = Tensor.ones((256,3,32,32))
+    y = Tensor.ones((256,10))
+    net = Net()
+    # did get_parameters add grad????
+    optim = optim.SGD(get_parameters(net), lr=0.001)
+
+    out = net(x)
+    loss = out.mul(y).mean()
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+
