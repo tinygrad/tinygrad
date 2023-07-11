ed02edb8b3e0c67a95f1f6c120394f38ae6fa973
Christopher Milan <chrismilan@ucla.edu>
2023-07-11 14:43:42 -0700

    change monkeypatch format

--- test/conftest.py
+++ test/conftest.py
@@ -3,5 +3,4 @@ from tinygrad.helpers import getenv
 
 @pytest.fixture(autouse=getenv("CUDACPU", 0) and getenv("TRITON", 0))
 def mock_torch(monkeypatch):
-  import triton.runtime.jit
-  monkeypatch.setattr(triton.runtime.jit, "get_current_device", lambda: 0)
+  monkeypatch.setattr("triton.runtime.jit.get_current_device", lambda: 0)
