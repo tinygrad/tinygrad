8dfaff2cdfcabf9e8190269fd05feeccbb8e7bd7
Michael Lojkovic <mikelojkovic@gmail.com>
2023-07-11 18:37:08 -0500

    Updates compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # note: if we compile tinygrad/nn/__init__.py __dict__ no longer works, and optimizers will silently fail
-mypyc --check-untyped-defs --explicit-package-bases --warn-unreachable tinygrad/shape/shapetracker.py tinygrad/shape/symbolic.py \
-  tinygrad/helpers.py tinygrad/mlops.py tinygrad/tensor.py tinygrad/graph.py \
+mypyc --check-untyped-defs --explicit-package-bases --warn-unreachable tinygrad/shape/shapetracker.py \
+  tinygrad/shape/symbolic.py tinygrad/helpers.py tinygrad/mlops.py tinygrad/tensor.py tinygrad/graph.py \
+  tinygrad/DType.py tinygrad/dtypes.py tinygrad/ImageDType.py tinygrad/GlobalCounters.py tinygrad/GRAPH_MACROS.py tinygrad/LightWeakSet.py \
+  tinygrad/LightWeakValueDictionary.py \
   #tinygrad/codegen/gpu.py tinygrad/runtime/ops_metal.py
   #tinygrad/codegen/ast.py
   #tinygrad/nn/__init__.py
