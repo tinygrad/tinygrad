36a2d200c554da4a83e58b656782d64abe017c1b
dc-dc-dc <dgcruz983@gmail.com>
2023-07-10 20:57:36 -0400

    one more linter issue

--- tinygrad/codegen/wgsl.py
+++ tinygrad/codegen/wgsl.py
@@ -31,7 +31,7 @@ class WGSLLanguage(CStyleLanguage):
     else: val = f"{x}" + ("" if dtypes.is_int(var_dtype) else "f")
     return self.render_cast([val]*var_dtype.sz, var_dtype) if var_dtype.sz > 1 else val
   
-  def render_kernel(self, kernel: List[str], bufs: List[LocalBuffer | LazyBuffer], bufnames: List[str], local_size: List[int], prekernel: List[str]) -> str:
+  def render_kernel(self, kernel: List[str], bufs: List[Union[LocalBuffer,LazyBuffer]], bufnames: List[str], local_size: List[int], prekernel: List[str]) -> str:
     local_size = local_size if len(local_size) else [1]
     bind_it = iter(range(len(bufs)))
     prg = "\n".join(prekernel+[f"@group(0) @binding({next(bind_it)}) var<storage,read_write> data{i}: array<{type_map[x.dtype]}>;" for i,x in enumerate(bufs) if not isinstance(x, LocalBuffer) and not isinstance(x.realized, RawConst)])
