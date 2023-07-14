e3113a0d93e3b8452afe1b364bc5e7b4298b6b84
Kunwar Raj Singh <kunwar31@pop-os.localdomain>
2023-07-14 07:13:37 +0530

    fix test

--- test/test_speed_v_torch.py
+++ test/test_speed_v_torch.py
@@ -101,7 +101,7 @@ def helper_test_generic(name, f1, f1_args, f2, f2_args):
   mem = save_mem*1e-6
   print(f"{prefix}{name:42s} {et_torch:7.2f} ms ({flops/et_torch:8.2f} GFLOPS {mem/et_torch:8.2f} GB/s) in torch, {et_tinygrad:7.2f} ms ({flops/et_tinygrad:8.2f} GFLOPS {mem/et_tinygrad:8.2f} GB/s) in tinygrad, {colorize_float(et_tinygrad/et_torch)} {desc} {flops:10.2f} MOPS {mem:8.2f} MB")
   prefix = " "
-  np.testing.assert_allclose(val_tinygrad, val_torch.half() if HALF else val_torch, atol=1e-1 if HALF else 1e-4, rtol=1e-1 if HALF else 1e-3)
+  np.testing.assert_allclose(val_tinygrad, val_torch.astype(np.float16) if HALF else val_torch, atol=1e-1 if HALF else 1e-4, rtol=1e-1 if HALF else 1e-3)
 
 def helper_test_conv(bs, in_chans, out_chans, kernel_size, img_size_y, img_size_x):
   torch.manual_seed(0)
