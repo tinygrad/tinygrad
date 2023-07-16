9c2539baaca54b18d30b2252ac2ef80f5defb53a
Dosier <stanvdbend@gmail.com>
2023-07-16 02:37:14 +0200

    Moved mkdir to diff line
    .mkdir does not return the actual directory being created.

--- extra/utils.py
+++ extra/utils.py
@@ -39,7 +39,9 @@ def download_file(url, fp, skip_if_exists=True):
   r = requests.get(url, stream=True)
   assert r.status_code == 200
   progress_bar = tqdm(total=int(r.headers.get('content-length', 0)), unit='B', unit_scale=True, desc=url)
-  with tempfile.NamedTemporaryFile(dir=pathlib.Path(fp).parent.mkdir(parents=True, exist_ok=True), delete=False) as f:
+  path = pathlib.Path(fp).parent
+  path.mkdir(parents=True, exist_ok=True)
+  with tempfile.NamedTemporaryFile(dir=path, delete=False) as f:
     for chunk in r.iter_content(chunk_size=16384):
       progress_bar.update(f.write(chunk))
     f.close()
