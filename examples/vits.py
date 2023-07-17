4856e787f44f9d6282654ba02a806e23d3a78f36
Dosier <stanvdbend@gmail.com>
2023-07-17 03:01:44 +0200

    Removed `Tensor.Training=False` (not needed and wrong oop)

--- examples/vits.py
+++ examples/vits.py
@@ -695,7 +695,7 @@ if __name__ == '__main__':
   text_mapper = TextMapper(apply_cleaners=True, symbols=symbols)
 
   # Load the model.
-  Tensor.no_grad, Tensor.Training = True, False
+  Tensor.no_grad = True
   if args.seed is not None:
     Tensor.manual_seed(args.seed)
     np.random.seed(args.seed)
