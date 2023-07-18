4ec39d0dd6f4cafefb8f70c3129af3848a05b4f3
David Hou <david.hou314@gmail.com>
2023-07-18 16:46:45 -0700

    Revert "minimize the test"
    This reverts commit e0c092959636109f745d1c8a73f2db90c75fe3c1.

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -6,6 +6,7 @@ from tinygrad.ops import GlobalCounters, Compiled
 from tinygrad.tensor import Tensor
 
 class TestLinearizer(unittest.TestCase):
+  @unittest.skipUnless(isinstance(Device[Device.DEFAULT], Compiled), "Only Compiled supports cache")
   def test_arg_dedup(self):
     a, b = Tensor.randn(4), Tensor.randn(4)
 
