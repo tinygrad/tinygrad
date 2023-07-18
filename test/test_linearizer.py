e0c092959636109f745d1c8a73f2db90c75fe3c1
David Hou <david.hou314@gmail.com>
2023-07-18 16:46:10 -0700

    minimize the test

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -6,7 +6,6 @@ from tinygrad.ops import GlobalCounters, Compiled
 from tinygrad.tensor import Tensor
 
 class TestLinearizer(unittest.TestCase):
-  @unittest.skipUnless(isinstance(Device[Device.DEFAULT], Compiled), "Only Compiled supports cache")
   def test_arg_dedup(self):
     a, b = Tensor.randn(4), Tensor.randn(4)
 
