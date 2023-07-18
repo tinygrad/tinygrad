2e719e830381c21cc1eb6c4d0b1268c2c4e975f5
David Hou <david.hou314@gmail.com>
2023-07-18 16:46:45 -0700

    Revert "minimize the test"
    This reverts commit 88240551b13403b21a81765043d5736103a49293.

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -9,6 +9,7 @@ class TestLinearizer(unittest.TestCase):
   @unittest.skipUnless(isinstance(Device[Device.DEFAULT], Compiled), "Only Compiled supports cache")
   def test_arg_dedup(self):
     a, b = Tensor.randn(4), Tensor.randn(4)
+    c = ((a.shrink(((0, 2),)) - a.shrink(((2, 4),)))).realize()
 
 if __name__ == '__main__':
   unittest.main()
