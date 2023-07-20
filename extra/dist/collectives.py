41892232c23aeded477e1358153424fd633e155f
Woze Parrot <wozeparrot@gmail.com>
2023-07-20 13:33:37 -0400

    feat: working allreduce

--- extra/dist/collectives.py
+++ extra/dist/collectives.py
@@ -1,17 +1,30 @@
 from tinygrad.tensor import Tensor
+from tinygrad.helpers import getenv
 
-from extra.dist import WORLD_SIZE, RANK
 from extra.dist import world
 
 def allreduce(t:Tensor) -> Tensor:
+  RANK, WORLD_SIZE = getenv("RANK"), getenv("WORLD_SIZE")
+
   chunks = t.chunk(WORLD_SIZE, dim=0)
-  bucket = chunks[RANK]
+  reduced = chunks[RANK]
 
   next_rank = (RANK + 1) % WORLD_SIZE
-  prev_rank = (RANK - 1) % WORLD_SIZE
+  prev_rank = ((RANK - 1) + WORLD_SIZE) % WORLD_SIZE
 
-  world.send(bucket, next_rank)
+  current_chunk_index = RANK
+  for _ in range(WORLD_SIZE - 1):
+    world.send(reduced, next_rank)
+    current_chunk_index = ((current_chunk_index - 1) + WORLD_SIZE) % WORLD_SIZE
+    got = world.recv(chunks[current_chunk_index], prev_rank)
+    reduced = got + chunks[current_chunk_index]
 
-  for i in range(WORLD_SIZE - 1):
-    bucket = bucket + world.recv(bucket, prev_rank)
-    world.send(bucket, next_rank)
+  chunks[current_chunk_index] = reduced
+  current_chunk_index = (RANK + 1) % WORLD_SIZE
+  for _ in range(WORLD_SIZE - 1):
+    world.send(reduced, next_rank)
+    current_chunk_index = ((current_chunk_index - 1) + WORLD_SIZE) % WORLD_SIZE
+    chunks[current_chunk_index] = reduced = world.recv(chunks[current_chunk_index], prev_rank)
+
+  return Tensor.cat(*chunks, dim=0)
+
