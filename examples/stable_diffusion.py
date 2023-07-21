35bc46289c7419a7bf210e33a442837596feaaf7
George Hotz <geohot@gmail.com>
2023-07-20 19:25:12 -0700

    stable diffusion: use new tinygrad primitives

--- examples/stable_diffusion.py
+++ examples/stable_diffusion.py
@@ -11,7 +11,7 @@ import numpy as np
 from tqdm import tqdm
 
 from tinygrad.tensor import Tensor
-from tinygrad.nn import Conv2d, Linear, GroupNorm, LayerNorm
+from tinygrad.nn import Conv2d, Linear, GroupNorm, LayerNorm, Embedding
 from extra.utils import download_file
 from tinygrad.state import torch_load, load_state_dict
 
@@ -272,10 +272,9 @@ class Upsample:
 
 def timestep_embedding(timesteps, dim, max_period=10000):
   half = dim // 2
-  freqs = np.exp(-math.log(max_period) * np.arange(0, half, dtype=np.float32) / half)
+  freqs = (-math.log(max_period) * Tensor.arange(half) / half).exp()
   args = timesteps * freqs
-  embedding = np.concatenate([np.cos(args), np.sin(args)])
-  return Tensor(embedding).reshape(1, -1)
+  return Tensor.cat(args.cos(), args.sin()).reshape(1, -1)
 
 class UNetModel:
   def __init__(self):
@@ -436,19 +435,11 @@ class CLIPEncoder:
 
 class CLIPTextEmbeddings:
   def __init__(self):
-    #self.position_ids = Tensor.empty(1, 77)  # what is this?
-    self.token_embedding = {"weight": Tensor.empty(49408, 768)}
-    self.position_embedding = {"weight": Tensor.empty(77, 768)}
+    self.token_embedding = Embedding(49408, 768)
+    self.position_embedding = Embedding(77, 768)
 
   def __call__(self, input_ids, position_ids):
-    # TODO: actually support batches
-    inputs = np.zeros((1, len(input_ids), 49408), dtype=np.float32)
-    positions = np.zeros((1, len(position_ids), 77), dtype=np.float32)
-    for i,x in enumerate(input_ids): inputs[0][i][x] = 1
-    for i,x in enumerate(position_ids): positions[0][i][x] = 1
-    inputs_embeds = Tensor(inputs, device=self.token_embedding['weight'].device) @ self.token_embedding['weight']
-    position_embeddings = Tensor(positions, device=self.position_embedding['weight'].device) @ self.position_embedding['weight']
-    return inputs_embeds + position_embeddings
+    return self.token_embedding(input_ids) + self.position_embedding(position_ids)
 
 class CLIPTextTransformer:
   def __init__(self):
@@ -457,10 +448,8 @@ class CLIPTextTransformer:
     self.final_layer_norm = LayerNorm(768)
 
   def __call__(self, input_ids):
-    x = self.embeddings(input_ids, list(range(len(input_ids))))
-    causal_attention_mask = np.triu(np.ones((1,1,77,77), dtype=np.float32) * -np.inf, k=1)
-    x = self.encoder(x, Tensor(causal_attention_mask, device=x.device))
-    # x = self.encoder(x, Tensor.full((1, 1, 77, 77), float("-inf")).triu(1)) # TODO: Pending(#942)
+    x = self.embeddings(input_ids, Tensor.arange(input_ids.shape[1]).reshape(1, -1))
+    x = self.encoder(x, Tensor.full((1, 1, 77, 77), float("-inf")).triu(1))
     return self.final_layer_norm(x)
 
 # Clip tokenizer, taken from https://github.com/openai/CLIP/blob/main/clip/simple_tokenizer.py (MIT license)
@@ -618,21 +607,21 @@ if __name__ == "__main__":
 
   # run through CLIP to get context
   tokenizer = ClipTokenizer()
-  prompt = tokenizer.encode(args.prompt)
+  prompt = Tensor([tokenizer.encode(args.prompt)])
   context = model.cond_stage_model.transformer.text_model(prompt).realize()
   print("got CLIP context", context.shape)
 
-  prompt = tokenizer.encode("")
+  prompt = Tensor([tokenizer.encode("")])
   unconditional_context = model.cond_stage_model.transformer.text_model(prompt).realize()
   print("got unconditional CLIP context", unconditional_context.shape)
 
   # done with clip model
   del model.cond_stage_model
 
-  def get_model_output(latent, timesteps):
+  def get_model_output(latent, timestep):
     # put into diffuser
-    unconditional_latent = model.model.diffusion_model(latent, timesteps, unconditional_context).realize()
-    latent = model.model.diffusion_model(latent, timesteps, context).realize()
+    unconditional_latent = model.model.diffusion_model(latent, timestep, unconditional_context).realize()
+    latent = model.model.diffusion_model(latent, timestep, context).realize()
 
     unconditional_guidance_scale = 7.5
     e_t = unconditional_latent + unconditional_guidance_scale * (latent - unconditional_latent)
@@ -666,7 +655,7 @@ if __name__ == "__main__":
   # this is diffusion
   for index, timestep in (t:=tqdm(list(enumerate(timesteps))[::-1])):
     t.set_description("%3d %3d" % (index, timestep))
-    e_t = get_model_output(latent, timestep)
+    e_t = get_model_output(latent, Tensor([timestep]))
     x_prev, pred_x0 = get_x_prev_and_pred_x0(latent, e_t, index)
     #e_t_next = get_model_output(x_prev)
     #e_t_prime = (e_t + e_t_next) / 2
