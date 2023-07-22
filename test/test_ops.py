c89fba93aa332c733d6167d40fe5d83a9066f6c8
Finn Cairns <fjamescairns@gmail.com>
2023-07-22 14:31:04 -0500

    suppress warnings

--- test/test_ops.py
+++ test/test_ops.py
@@ -7,6 +7,10 @@ from tinygrad.tensor import Tensor
 from tinygrad.helpers import getenv, IMAGE, DEBUG, CI
 from tinygrad.lazy import Device
 
+if CI:
+  import warnings
+  warnings.filterwarnings("ignore", message="Non-empty compiler output encountered")
+
 FORWARD_ONLY = getenv("FORWARD_ONLY", 0)
 PRINT_TENSORS = getenv("PRINT_TENSORS", 0)
 def helper_test_op(shps, torch_fxn, tinygrad_fxn=None, atol=1e-6, rtol=1e-3, grad_atol=1e-4, grad_rtol=1e-3, forward_only=False, vals=None, a=-0.5, b=3):
