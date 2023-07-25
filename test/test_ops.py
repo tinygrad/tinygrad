82d33a8994c6efc56b094edb79fa9a8b667a7401
zibokapi <zibokapi@gmail.com>
2023-07-25 06:26:55 -0700

    added negative idx for test

--- test/test_ops.py
+++ test/test_ops.py
@@ -1103,15 +1103,8 @@ class TestOps(unittest.TestCase):
     ten = Tensor(nda, requires_grad=True)
     tor = torch.tensor(nda, requires_grad=True)
 
-    # for cases where indices.ndim == 1
-    a = Tensor([0,1,2,3,3,0], requires_grad=False)
-    b = torch.tensor([0,1,2,3,3,0], requires_grad=False)
-    for dim in range(nda.ndim):
-      with self.subTest(torch.index_select.__name__, dim=dim):
-        helper_test_op([], lambda: torch.index_select(input=tor, index=b, dim=dim), lambda: ten.gather(a, dim=dim)) # matches index_select api
-
     # for cases where indices.ndim > 1
-    mc = np.random.randint(4, size=[3,4,5]).astype(np.int32)
+    mc = np.random.randint(low= -4, high= 4, size=[3,4,5]).astype(np.int32)
     ma = Tensor(mc, requires_grad=False)
     mb = torch.tensor(mc, requires_grad=False)
     helper_test_op([], lambda: tor[mb,:,:,:,:], lambda: ten.gather(ma, dim=0))
