375d3d4bb43a9c6e60f1d1621ea0590b3b15961c
Umut Zengin <70948490+ZenginU@users.noreply.github.com>
2023-07-26 22:35:38 +0300

    small reformat

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -322,7 +322,7 @@ class Tensor:
   def cat(self, *args, dim=0):
     dim = (dim + len(self.shape)) if dim < 0 else dim
     assert all(len(y.shape) == len(self.shape) and all(y.shape[i] == s for i,s in enumerate(self.shape) if i != dim) for y in args)
-    catargs = [self] + list(args)
+    catargs = [self, *args]
     assert all(t.shape for t in catargs), "zero-dimensional tensor cannot be concatenated"
     shapes = [s.shape[dim] for s in catargs]
     shape_cumsum = [0, *accumulate(shapes)]
