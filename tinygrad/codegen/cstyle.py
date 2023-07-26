0c4015f84892e5a16599cb680533c5955e944ee0
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-07-26 12:05:32 -0500

    add check for global dim limit but need refactor

--- tinygrad/codegen/cstyle.py
+++ tinygrad/codegen/cstyle.py
@@ -195,7 +195,7 @@ class CStyleCodegen(Linearizer):
 
   def codegen(self):
     self.process()
-    if self.lang.global_max: self.limit_global_dims(len(self.lang.gid))  # NOTE: this is optional now
+    if self.lang.global_max: self.limit_global_dims(len(self.lang.gid), self.lang.global_max)  # NOTE: this is optional now
     self.linearize()
 
     prg, global_size, local_size = uops_to_cstyle(self.uops, self.lang)
