770f79c25e310ca128995ff8a9f17d9441c2a37d
Jacob Pradels <pradelsjacob@gmail.com>
2023-08-02 11:09:23 -0700

    Removed skip on to_half

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -85,9 +85,8 @@ class TestBFloat16DType(unittest.TestCase):
 @unittest.skipIf((getenv("CI", "") != "" and Device.DEFAULT in ["LLVM"]) or Device.DEFAULT == "WEBGPU", "float16 broken in some CI backends")
 class TestHalfDtype(unittest.TestCase):
   def test_float16_to_np(self): _test_to_np(Tensor([1,2,3,4], dtype=dtypes.float16), np.float16, [1,2,3,4])
-  @unittest.skipIf(Device.DEFAULT == "METAL", "Metal Backend does not support float16")
   def test_casts_to_half(self): _test_casts_to([1,2,3,4], source_dtypes=[dtypes.float32, dtypes.int8, dtypes.uint8], target_dtype=dtypes.float16)
-  @unittest.skipIf(Device.DEFAULT == "METAL", "Metal Backend does not support float16")
+  @unittest.skipIf(Device.DEFAULT == "METAL", "TODO")
   def test_casts_from_half(self): _test_casts_from([1,2,3,4], source_dtype=dtypes.float16, target_dtypes=[dtypes.int8, dtypes.uint8, dtypes.float32, dtypes.int32, dtypes.int64])
   def test_half_upcast_ops(self): _test_ops(a_dtype=dtypes.float16, b_dtype=dtypes.float32, target_dtype=dtypes.float32)
   def test_upcast_to_half_ops(self): _test_ops(a_dtype=dtypes.int8, b_dtype=dtypes.float16, target_dtype=dtypes.float16)
