5e5e023972830b9a098946874747d54e241e271e
Joshua Eason <jeason15@gmail.com>
2023-08-01 23:05:44 -0500

    tackled the TODO in Token class, slight refactor

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -56,47 +56,40 @@ class Token(NamedTuple):
     return self.name+"."+"xyzw"[int(self.offset)]
   def __repr__(self): return f"<{self.name}>" if self.offset is None and self.dtype == dtypes.float32 else f"<{self.name}:{self.dtype.name}:{self.offset}>"
 
-# TODO: the next three functions are poorly written
-def get_grouped_float4_idxs(acc:List[Token]) -> Optional[List[int]]:
-  idxs: Optional[List[int]] = []
-  for i,a in enumerate(acc):
-    if idxs is None: break
-    if i in idxs: continue
-    if a.dtype.sz > 1 and a.offset == 0:
-      idxs.append(i)
-      friends: List[int] = []
-      for j,b in enumerate(acc):
-        if len(friends) == 3: break
-        if j in idxs: continue
-        if a.name == b.name and b.dtype.sz > 1 and b.offset == len(friends)+1:
-          friends.append(j)
-      if len(friends) == 3: idxs += friends
-      else: idxs = None
-    else:
-      idxs = None
-  return idxs
+def get_grouped_float4_idxs(acc: List[Token]) -> Optional[List[int]]:
+    idx_to_token_group = {}
+    for idx, token in enumerate(acc):
+        if token.dtype != 'float4' or token.offset > 3:
+            continue
+        if token.name not in idx_to_token_group:
+            idx_to_token_group[token.name] = []
+        idx_to_token_group[token.name].append(idx)
+    idx_to_token_group = {k: v for k, v in idx_to_token_group.items() if len(v) == 4}
+    if idx_to_token_group:
+        return [idx for group in idx_to_token_group.values() for idx in group]
+    return None
 
-def to_float4(x:List[Token]) -> Optional[Token]:
-  if all_same(x): return x[0]
-  if all_same([y.name for y in x]) and all(y.dtype == dtypes._float4 and y.offset == i for i,y in enumerate(x)):
-    return Token(x[0].name, dtypes._float4)
-  return None
+def to_float4(x: List[Token]) -> Optional[Token]:
+    if len(x) != 4 or any(token.dtype != 'float4' for token in x):
+        return None
+    return x[0]
 
-def get_grouped_maybe_float4(*values:List[Token], grouping_allowed=True):
-  assert all_same([len(x) for x in values]), f"all values are not the same length {values}"
-  # these use accumulators, we can only fold if the acc is a float4
-  idxs = get_grouped_float4_idxs(values[-1]) if grouping_allowed else None
-  if idxs is not None:
-    new_idxs = []
-    new_values = []
-    for i in range(0, len(idxs), 4):
-      nv = [to_float4([v[j] for j in idxs[i:i+4]]) for v in values]
-      if any(x is None for x in nv): break
-      new_idxs.append(idxs[i:i+4])
-      new_values.append(nv)
-    if len(new_values) == len(idxs)//4:
-      return zip(new_idxs, new_values)
-  return zip([[i] for i in range(len(values[0]))], zip(*values))
+def get_grouped_maybe_float4(*values: List[Token], grouping_allowed=True):
+    assert all(len(value) == len(values[0]) for value in values), f"all values are not the same length {values}"
+    idxs = get_grouped_float4_idxs(values[-1]) if grouping_allowed else None
+    if idxs is not None:
+        new_idxs = []
+        new_values = []
+        for i in range(0, len(idxs), 4):
+            group_values = [[v[idx] for v in values] for idx in idxs[i:i+4]]
+            new_values_group = [to_float4(value) for value in group_values]
+            if None in new_values_group:
+                break
+            new_idxs.append(idxs[i:i+4])
+            new_values.append(new_values_group)
+        if len(new_values) == len(idxs) // 4:
+            return zip(new_idxs, new_values)
+    return zip([[i] for i in range(len(values[0]))], zip(*values))
 
 # TODO: generic visitor pattern?
 def expand_node(idx:Node) -> List[Node]:
