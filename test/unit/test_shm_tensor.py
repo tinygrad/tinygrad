91e4c0e97a5c40686f07a7349240c3d014f7105b
Woze Parrot <wozeparrot@gmail.com>
2023-08-03 14:32:34 -0400

    feat: testing

--- test/unit/test_shm_tensor.py
+++ test/unit/test_shm_tensor.py
@@ -20,9 +20,14 @@ class TestRawShmBuffer(unittest.TestCase):
     assert np.allclose(t.numpy(), t2.numpy())
     s.unlink()
 
-  @unittest.skipIf(CI, "CI doesn't like big mmaps")
+  # @unittest.skipIf(CI, "CI doesn't like big mmaps")
   def test_e2e_big(self):
-    t = Tensor.randn(2048, 2048, 8).realize()
+    # print output of lsipc
+    import subprocess
+    s = subprocess.check_output(["lsipc"])
+    print(s.decode("utf-8"))
+
+    t = Tensor.randn(2).realize()
 
     # copy to shm
     shm_name = (s := shared_memory.SharedMemory(create=True, size=t.nbytes())).name
