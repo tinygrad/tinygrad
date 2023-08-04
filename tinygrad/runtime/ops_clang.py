3809fd78ca9100a730624b3209185010261b4228
Lachlan Greve <lachlan.greve@webstercare.com.au>
2023-08-04 14:16:17 +1000

    use processing lock instead

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,4 +1,4 @@
-import os, time, ctypes, hashlib, subprocess, platform, tempfile, threading
+import os, time, ctypes, hashlib, subprocess, platform, tempfile, multiprocessing
 from tinygrad.ops import Compiled
 from tinygrad.runtime.lib import RawMallocBuffer
 from tinygrad.codegen.cstyle import CStyleCodegen, CStyleLanguage
@@ -9,24 +9,24 @@ args = {
   'Darwin': {'cflags':'-lm -fPIC --rtlib=compiler-rt ', 'ext':'dylib', 'exp':''}
 }[platform.system()]
 CLANG_PROGRAM_HEADER = '#include <math.h>\n#define max(x,y) ((x>y)?x:y)\n#define int64 long\n#define half __fp16\n#define uchar unsigned char\n#define bool uchar\n'
-file_mutex = threading.Lock()
-file_condition = threading.Condition()
+file_lock = multiprocessing.Lock()
+compiled_event = multiprocessing.Event()
 class ClangProgram:
   def __init__(self, name:str, prg:str):
     prg = CLANG_PROGRAM_HEADER + prg
     # TODO: is there a way to not write this to disk?
     fn = f"{tempfile.gettempdir()}/clang_{hashlib.md5(prg.encode('utf-8')).hexdigest()}.{args['ext']}"
-    with file_mutex: 
+    with file_lock: 
       if not os.path.exists(fn):
         subprocess.check_output(args=('clang -shared -O2 -Wall -Werror -x c '+args['cflags']+' - -o '+fn+'.tmp').split(), input=prg.encode('utf-8'))
         os.rename(fn+'.tmp', fn)
-    with file_condition: file_condition.notify_all()
+    compiled_event.set()
     self.lib = ctypes.CDLL(fn)
     self.fxn = self.lib[name]
 
   def __call__(self, global_size, local_size, *args, wait=False):
     if wait: st = time.monotonic()
-    with file_condition: file_condition.wait()
+    compiled_event.wait()
     self.fxn(*[x._buf for x in args])
     if wait: return time.monotonic()-st
 
