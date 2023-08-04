6559d1b66e32331e4f214f8a41da6cf16cbfb460
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-08-04 14:29:51 +0000

    remove space

--- tinygrad/nn/__init__.py
+++ tinygrad/nn/__init__.py
@@ -122,4 +122,3 @@ class Embedding:
   def __call__(self, idx:Tensor) -> Tensor:
     vocab_counter = Tensor.arange(self.vocab_size, requires_grad=False).reshape(1, 1, self.vocab_size).expand(*idx.shape, self.vocab_size)
     return (vocab_counter == idx.unsqueeze(2).expand(*idx.shape, self.vocab_size)) @ self.weight
-
