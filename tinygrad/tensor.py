0bf9671db53281c5064e4d6379486e115ccfec44
zibokapi <zibokapi@gmail.com>
2023-08-05 12:21:00 -0700

    I understand how maximum and minimum works now

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -332,7 +332,7 @@ class Tensor:
     if tensor_found:
       for i,s in enumerate(sub): tensor_found[i] = (tensor_found[i][0]+s, tensor_found[i][1])
       dim = [i[0] for i in tensor_found]
-      idx = [-i[1].sign().minimum(0) * ret.shape[i[0]] + i[1] for i in tensor_found]
+      idx = [i[1].sign().__neg__().maximum(0) * ret.shape[i[0]] + i[1] for i in tensor_found]
       max_dim = max(idx, key=lambda i: i.ndim).ndim
       idx = [i if i.ndim == max_dim else i.reshape(*[1]*(max_dim-i.ndim), *i.shape) for i in idx]
       sum_dim = [d if n==0 else d+i.ndim-n for n,(d,i) in enumerate(zip(dim,idx))]
