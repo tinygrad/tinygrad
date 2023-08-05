bae0e116c4f78b7af8a02e537aeeb637c9378fe4
ojaffe <oliver.jaffe@hotmail.co.uk>
2023-08-05 20:40:36 +0100

    dummy train and eval loops

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -24,6 +24,29 @@ def train_resnet():
     y = y.reshape(list(Y.shape)+[num_classes])
     y = Tensor(y)
     return (1 - label_smoothing) * out.mul(y).mean() + (-1 * label_smoothing * out.mean())
+  
+  def train_step(X, Y):
+    optimizer.zero_grad()
+    out = model.forward(X)
+    loss = cross_entropy(out, Y, label_smoothing=0.1)
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+    return loss, out
+  
+  def eval_step(X, Y):
+    out = model.forward(X)
+    loss = cross_entropy(out, Y, label_smoothing=0.1)
+    return loss, out
+  
+  def calculate_accuracy(out, Y, top_n):
+    out_top_n = np.argpartition(out.cpu().numpy(), -top_n, axis=-1)[:, -top_n:]
+    YY = np.expand_dims(Y, axis=1)
+    YY = np.repeat(YY, top_n, axis=1)
+
+    eq_elements = np.equal(out_top_n, YY)
+    top_n_acc = np.count_nonzero(eq_elements) / eq_elements.size * top_n
+    return top_n_acc
 
   seed = getenv('SEED', 42)
   Tensor.manual_seed(seed)
@@ -43,40 +66,47 @@ def train_resnet():
   scheduler = CosineAnnealingLR(optimizer, epochs)
   print(f"training with batch size {BS} for {epochs} epochs")
 
-  def train_step(X, Y):
-    optimizer.zero_grad()
-    out = model.forward(X)
-    loss = cross_entropy(out, Y, label_smoothing=0.1)
-    loss.backward()
-    optimizer.step()
-    scheduler.step()
-    return loss, out
-  
-  def calculate_accuracy(out, Y, top_n):
-    out_top_n = np.argpartition(out.cpu().numpy(), -top_n, axis=-1)[:, -top_n:]
-    YY = np.expand_dims(Y, axis=1)
-    YY = np.repeat(YY, top_n, axis=1)
-
-    eq_elements = np.equal(out_top_n, YY)
-    top_n_acc = np.count_nonzero(eq_elements) / eq_elements.size * top_n
-    return top_n_acc
-
-  Tensor.training = True
-  steps_in_epoch = math.floor(len(get_val_files()) / BS)
-  for epoch in range(epochs):
-    for X, Y in (t := tqdm(iterate(bs=BS, val=True), total=steps_in_epoch)):
+  steps_in_train_epoch = math.floor(len(get_val_files()) / BS)
+  steps_in_val_epoch = math.floor(len(get_val_files()) / BS)
+  for _ in range(epochs):
+    # train loop
+    Tensor.training = True
+    for X, Y in (t := tqdm(iterate(bs=BS, val=True), total=steps_in_train_epoch)):
       X = Tensor(X, requires_grad=False)
       st = time.time()
       loss, out = train_step(X, Y)
       et = time.time()
 
       t.set_description(f"loss: {loss.numpy().item():.3f}")
-      top_1_acc = calculate_accuracy(out, Y, 1)
-      wandb.log({"loss": loss.numpy().item(), 
-                 "top_1_acc": top_1_acc,
+      wandb.log({"train/loss": loss.numpy().item(), 
+                 "train/forward_time": et - st,
                  "lr": scheduler.get_lr().cpu().numpy().item(),
-                 "forward_time": et - st,}
-      )
+      })
+    
+    # "eval" loop
+    eval_loss = []
+    eval_times = []
+    eval_top_1_acc = []
+    eval_top_5_acc = []
+    Tensor.training = False
+    for X, Y in (t := tqdm(iterate(bs=BS, val=True), total=steps_in_val_epoch)):
+      X = Tensor(X, requires_grad=False)
+      st = time.time()
+      loss, out = eval_step(X, Y)
+      et = time.time()
+
+      top_1_acc = calculate_accuracy(out, Y, 1)
+      top_5_acc = calculate_accuracy(out, Y, 5)
+      eval_loss.append(loss.numpy().item())
+      eval_times.append(et - st)
+      eval_top_1_acc.append(top_1_acc)
+      eval_top_5_acc.append(top_5_acc)
+
+    wandb.log({"eval/loss": sum(eval_loss) / len(eval_loss),
+               "eval/forward_time": sum(eval_times) / len(eval_times),
+               "eval/top_1_acc": sum(eval_top_1_acc) / len(eval_top_1_acc),
+               "eval/top_5_acc": sum(eval_top_5_acc) / len(eval_top_5_acc),
+    })
 
 def train_retinanet():
   # TODO: Retinanet
