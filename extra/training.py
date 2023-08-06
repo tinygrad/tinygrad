67c9a95feddfdd69e6ca61da5dc85d88f0d435c3
Guy Leroy <g.m.leroy@outlook.com>
2023-08-06 22:39:51 +0100

    Revert changes on sparse_categorical_crossentropy

--- extra/training.py
+++ extra/training.py
@@ -5,9 +5,12 @@ from tinygrad.tensor import Tensor
 
 def sparse_categorical_crossentropy(out, Y):
   num_classes = out.shape[-1]
-  y_counter = Tensor.arange(num_classes, requires_grad=False, dtype=dtypes.uint8).unsqueeze(0).expand(Y.size, num_classes)
-  y = (y_counter == Y.flatten().reshape(-1, 1)).where(-1.0 * num_classes, 0)
-  y = y.reshape(*Y.shape, num_classes)
+  YY = Y.flatten().astype(np.int32)
+  y = np.zeros((YY.shape[0], num_classes), np.float32)
+  # correct loss for NLL, torch NLL loss returns one per row
+  y[range(y.shape[0]),YY] = -1.0*num_classes
+  y = y.reshape(list(Y.shape)+[num_classes])
+  y = Tensor(y)
   return out.mul(y).mean()
 
 def lr_warmup(optim, init_lr, lr, current_epoch, warmup_epochs):
