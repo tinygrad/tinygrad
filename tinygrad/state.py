056ed651be8bacf84402c87af0456460f6e006ff
George Hotz <geohot@gmail.com>
2023-08-06 20:25:05 -0700

    that

--- tinygrad/state.py
+++ tinygrad/state.py
@@ -49,7 +49,7 @@ def load_state_dict(model, state_dict, strict=True):
     model_state_dict = get_state_dict(model)
     if DEBUG >= 1 and len(state_dict) > len(model_state_dict): print("WARNING: unused weights in state_dict", sorted(list(state_dict.keys() - model_state_dict.keys())))
     for k,v in (t := tqdm(model_state_dict.items(), disable=CI)):
-      t.set_description(f"ram used: {GlobalCounters.mem_used/1e9:5.2f} GB, {k:50s}")
+      if not CI: t.set_description(f"ram used: {GlobalCounters.mem_used/1e9:5.2f} GB, {k:50s}")
       if k not in state_dict and not strict:
         if DEBUG >= 1: print(f"WARNING: not loading {k}")
         continue
