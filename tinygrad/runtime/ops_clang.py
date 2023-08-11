f301cb40ad265c85729411ff070d447aad1b45de
Steven Anderson <sdandersonz@icloud.com>
2023-08-11 00:03:55 -0400

    passing test

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -21,14 +21,13 @@ mock_lm = {"sinf": np.sin, "sqrtf": np.sqrt, "exp2f": np.exp2, "log2f": np.log2}
 def hook_code(fn, uc, address, size, user_data):
   s_in = struct.unpack('f', struct.pack('I', uc.reg_read(getattr(arm64_const, f'UC_ARM64_REG_S{fn[2][1:]}'))))[0]
   uc.reg_write(getattr(arm64_const, f'UC_ARM64_REG_S{fn[1][1:]}'), struct.unpack('I', struct.pack('f', mock_lm[fn[0]](s_in)))[0])  # type: ignore
-
+def hook_print(uc, address, size, user_data): print(address)
 class ClangProgram:
   def __init__(self, name:str, prg:str, binary:bool=False, var_size:int=0):
     # TODO: is there a way to not write this to disk?
     fn = f"{tempfile.gettempdir()}/clang_{hashlib.md5(prg.encode('utf-8')).hexdigest()}.{args['ext']}"
     if not binary:
       prg = CLANG_PROGRAM_HEADER + prg
-      # TODO: is there a way to not write this to disk?
       if not os.path.exists(fn):
         subprocess.check_output(args=('clang -shared -O2 -Wall -Werror -x c '+args['cflags']+' - -o '+fn+'.tmp').split(), input=prg.encode('utf-8'))
         os.rename(fn+'.tmp', fn)
@@ -51,22 +50,22 @@ class ClangProgram:
   def __call__(self, global_size, local_size, *args, wait=False):
     if wait: st = time.monotonic()
     if CI and getenv('ARM64'):
-      try:
-        mu = Uc(UC_ARCH_ARM64, UC_MODE_ARM)
-        total_mem = align(reduce(lambda total, arg: total + arg.size * arg.dtype.itemsize, args, len(self.prg)+self.varsize))
-        mu.mem_map(ADDRESS, total_mem)
-        for k, fn in self.ext_calls.items(): mu.hook_add(UC_HOOK_CODE, partial(hook_code, fn), begin=k, end=k)
-        mu.mem_write(ADDRESS, self.prg + b''.join(bytes(arg._buf) for arg in args))
-        addr = ADDRESS + len(self.prg)
-        for i, arg in enumerate(args):
-          if i<=7: mu.reg_write(getattr(arm64_const, f'UC_ARM64_REG_X{i}'), addr)
-          else: mu.mem_write(ADDRESS + total_mem - (len(args[8:])+1)*8 + 8*(i-8), addr.to_bytes(8, 'little'))
-          addr += arg.size * arg.dtype.itemsize
-        mu.reg_write(arm64_const.UC_ARM64_REG_SP, ADDRESS + total_mem - (len(args[8:])+1)*8)
-        mu.emu_start(ADDRESS, ADDRESS + len(self.prg))
-        args[0]._buf = mu.mem_read(mu.reg_read(arm64_const.UC_ARM64_REG_X0), args[0].size * args[0].dtype.itemsize)
-      except UcError as e:
-        print("ERROR: %s" % e)
+      mu = Uc(UC_ARCH_ARM64, UC_MODE_ARM)
+      total_mem = align(reduce(lambda total, arg: total + arg.size * arg.dtype.itemsize, args, len(self.prg)+self.varsize))
+      mu.mem_map(ADDRESS, total_mem)
+      for k, fn in self.ext_calls.items(): mu.hook_add(UC_HOOK_CODE, partial(hook_code, fn), begin=k, end=k)
+      mu.mem_write(ADDRESS, self.prg + b''.join(bytes(arg._buf) for arg in args))
+      addr = ADDRESS + len(self.prg)
+      for i, arg in enumerate(args):
+        if i<=7:
+          mu.reg_write(getattr(arm64_const, f'UC_ARM64_REG_X{i}'), addr)
+        else:
+          # NOTE: In ARM, args beyond the first 8 are placed on the stack it also account for the stack red zone.
+          mu.mem_write(ADDRESS + total_mem - (len(args[8:])+2)*8 + 8*(i-8), addr.to_bytes(8, 'little'))
+        addr += arg.size * arg.dtype.itemsize
+      mu.reg_write(arm64_const.UC_ARM64_REG_SP, ADDRESS + total_mem - (len(args[8:])+2)*8)
+      mu.emu_start(ADDRESS, ADDRESS + len(self.prg))
+      args[0]._buf = mu.mem_read(mu.reg_read(arm64_const.UC_ARM64_REG_X0), args[0].size * args[0].dtype.itemsize)
     else:
       self.fxn(*[x._buf for x in args])
     if wait: return time.monotonic()-st
