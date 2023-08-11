b70bee8bbc26a269b1939b49aa4c1801f20c3cc1
Jakob Spahn <jakob@craalse.de>
2023-08-11 22:26:35 +0200

    tribute to linter lords

--- test/models/test_bert.py
+++ test/models/test_bert.py
@@ -27,9 +27,9 @@ class TestBert(unittest.TestCase):
     from transformers import BertConfig
 
     config = {
-      'vocab_size' : 30522, 'hidden_size' : 1024, 'num_hidden_layers' : 24, 'num_attention_heads' : 16, 
-      'intermediate_size' : 4096, 'hidden_dropout_prob' : 0.1, 'attention_probs_dropout_prob' : 0.1,
-      'max_position_embeddings' : 512, 'type_vocab_size' : 2
+      'vocab_size':30522, 'hidden_size':1024, 'num_hidden_layers':24, 'num_attention_heads':16, 
+      'intermediate_size':4096, 'hidden_dropout_prob':0.1, 'attention_probs_dropout_prob':0.1,
+      'max_position_embeddings':512, 'type_vocab_size':2
       }
 
     # Create in tinygrad
