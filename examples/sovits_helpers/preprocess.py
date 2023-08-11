d0e0367012e8f2b131eeb3fbfb4ad7a96dcc281c
Jakob Spahn <jakob@craalse.de>
2023-08-11 11:39:17 +0200

    clean up preprocess as well

--- examples/sovits_helpers/preprocess.py
+++ examples/sovits_helpers/preprocess.py
@@ -7,15 +7,9 @@ import soundfile
 import numpy as np
 import parselmouth
 
-# from https://github.com/svc-develop-team/so-vits-svc/
-class PMF0Predictor:
+class PMF0Predictor:  # from https://github.com/svc-develop-team/so-vits-svc/
   def __init__(self,hop_length=512,f0_min=50,f0_max=1100,sampling_rate=44100):
-    self.hop_length = hop_length
-    self.f0_min = f0_min
-    self.f0_max = f0_max
-    self.sampling_rate = sampling_rate
-    self.name = "pm"
-
+    self.hop_length, self.f0_min, self.f0_max, self.sampling_rate, self.name = hop_length, f0_min, f0_max, sampling_rate, "pm"
   def interpolate_f0(self,f0):
     vuv_vector = np.zeros_like(f0, dtype=np.float32)
     vuv_vector[f0 > 0.0] = 1.0
@@ -25,29 +19,23 @@ class PMF0Predictor:
     nzindex = nzindex.astype(np.float32)
     time_org = self.hop_length / self.sampling_rate * nzindex
     time_frame = np.arange(f0.shape[0]) * self.hop_length / self.sampling_rate
-    if data.shape[0] <= 0:
-      return np.zeros(f0.shape[0], dtype=np.float32),vuv_vector
-    if data.shape[0] == 1:
-      return np.ones(f0.shape[0], dtype=np.float32) * f0[0],vuv_vector
+    if data.shape[0] <= 0: return np.zeros(f0.shape[0], dtype=np.float32),vuv_vector
+    if data.shape[0] == 1: return np.ones(f0.shape[0], dtype=np.float32) * f0[0],vuv_vector
     f0 = np.interp(time_frame, time_org, data, left=data[0], right=data[-1])
     return f0,vuv_vector
-
   def compute_f0(self,wav,p_len=None):
     x = wav
-    if p_len is None:
-      p_len = x.shape[0]//self.hop_length
-    else:
-      assert abs(p_len-x.shape[0]//self.hop_length) < 4, "pad length error"
+    if p_len is None: p_len = x.shape[0]//self.hop_length
+    else: assert abs(p_len-x.shape[0]//self.hop_length) < 4, "pad length error"
     time_step = self.hop_length / self.sampling_rate * 1000
-    f0 = parselmouth.Sound(x, self.sampling_rate).to_pitch_ac(
-      time_step=time_step / 1000, voicing_threshold=0.6,
-      pitch_floor=self.f0_min, pitch_ceiling=self.f0_max).selected_array['frequency']
+    f0 = parselmouth.Sound(x, self.sampling_rate) \
+                    .to_pitch_ac(time_step=time_step / 1000, voicing_threshold=0.6,pitch_floor=self.f0_min, pitch_ceiling=self.f0_max) \
+                    .selected_array['frequency']
     pad_size=(p_len - len(f0) + 1) // 2
     if(pad_size>0 or p_len - len(f0) - pad_size>0):
       f0 = np.pad(f0,[[pad_size,p_len - len(f0) - pad_size]], mode='constant')
     f0,uv = self.interpolate_f0(f0)
     return f0
-
   def compute_f0_uv(self,wav,p_len=None):
     x = wav
     if p_len is None: p_len = x.shape[0]//self.hop_length
@@ -62,15 +50,8 @@ class PMF0Predictor:
     f0,uv = self.interpolate_f0(f0)
     return f0,uv
 
-# from https://github.com/svc-develop-team/so-vits-svc/
-class Slicer:
-  def __init__(self,
-               sr: int,
-               threshold: float = -40.,
-               min_length: int = 5000,
-               min_interval: int = 300,
-               hop_size: int = 20,
-               max_sil_kept: int = 5000):
+class Slicer:  # from https://github.com/svc-develop-team/so-vits-svc/
+  def __init__(self, sr: int, threshold: float = -40., min_length: int = 5000, min_interval: int = 300, hop_size: int = 20, max_sil_kept: int = 5000):
     if not min_length >= min_interval >= hop_size:
       raise ValueError('The following condition must be satisfied: min_length >= min_interval >= hop_size')
     if not max_sil_kept >= hop_size:
@@ -82,47 +63,29 @@ class Slicer:
     self.min_length = round(sr * min_length / 1000 / self.hop_size)
     self.min_interval = round(min_interval / self.hop_size)
     self.max_sil_kept = round(sr * max_sil_kept / 1000 / self.hop_size)
-
   def _apply_slice(self, waveform, begin, end):
-    if len(waveform.shape) > 1:
-      return waveform[:, begin * self.hop_size: min(waveform.shape[1], end * self.hop_size)]
-    else:
-      return waveform[begin * self.hop_size: min(waveform.shape[0], end * self.hop_size)]
-
+    if len(waveform.shape) > 1: return waveform[:, begin * self.hop_size: min(waveform.shape[1], end * self.hop_size)]
+    else: return waveform[begin * self.hop_size: min(waveform.shape[0], end * self.hop_size)]
   def slice(self, waveform):
-    if len(waveform.shape) > 1:
-      samples = librosa.to_mono(waveform)
-    else:
-      samples = waveform
-    if samples.shape[0] <= self.min_length:
-      return {"0": {"slice": False, "split_time": f"0,{len(waveform)}"}}
+    samples = librosa.to_mono(waveform) if len(waveform.shape) > 1 else waveform
+    if samples.shape[0] <= self.min_length: return {"0": {"slice": False, "split_time": f"0,{len(waveform)}"}}
     rms_list = librosa.feature.rms(y=samples, frame_length=self.win_size, hop_length=self.hop_size).squeeze(0)
-    sil_tags = []
-    silence_start = None
-    clip_start = 0
+    sil_tags, silence_start, clip_start = [], None, 0
     for i, rms in enumerate(rms_list):
-      # Keep looping while frame is silent.
-      if rms < self.threshold:
-        # Record start of silent frames.
-        if silence_start is None:
+      if rms < self.threshold:  # Keep looping while frame is silent.
+        if silence_start is None:  # Record start of silent frames.
           silence_start = i
         continue
-      # Keep looping while frame is not silent and silence start has not been recorded.
-      if silence_start is None:
-        continue
+      if silence_start is None: continue  # Keep looping while frame is not silent and silence start has not been recorded.
       # Clear recorded silence start if interval is not enough or clip is too short
       is_leading_silence = silence_start == 0 and i > self.max_sil_kept
       need_slice_middle = i - silence_start >= self.min_interval and i - clip_start >= self.min_length
       if not is_leading_silence and not need_slice_middle:
         silence_start = None
         continue
-      # Need slicing. Record the range of silent frames to be removed.
-      if i - silence_start <= self.max_sil_kept:
+      if i - silence_start <= self.max_sil_kept:  # Need slicing. Record the range of silent frames to be removed.
         pos = rms_list[silence_start: i + 1].argmin() + silence_start
-        if silence_start == 0:
-          sil_tags.append((0, pos))
-        else:
-          sil_tags.append((pos, pos))
+        sil_tags.append((0, pos) if silence_start == 0 else (pos, pos))
         clip_start = pos
       elif i - silence_start <= self.max_sil_kept * 2:
         pos = rms_list[i - self.max_sil_kept: silence_start + self.max_sil_kept + 1].argmin()
@@ -138,38 +101,26 @@ class Slicer:
       else:
         pos_l = rms_list[silence_start: silence_start + self.max_sil_kept + 1].argmin() + silence_start
         pos_r = rms_list[i - self.max_sil_kept: i + 1].argmin() + i - self.max_sil_kept
-        if silence_start == 0:
-          sil_tags.append((0, pos_r))
-        else:
-          sil_tags.append((pos_l, pos_r))
+        sil_tags.append((0, pos_r) if silence_start == 0 else (pos_l, pos_r))
         clip_start = pos_r
       silence_start = None
-    # Deal with trailing silence.
     total_frames = rms_list.shape[0]
-    if silence_start is not None and total_frames - silence_start >= self.min_interval:
+    if silence_start is not None and total_frames - silence_start >= self.min_interval:  # Deal with trailing silence.
       silence_end = min(total_frames, silence_start + self.max_sil_kept)
       pos = rms_list[silence_start: silence_end + 1].argmin() + silence_start
       sil_tags.append((pos, total_frames + 1))
-    # Apply and return slices.
-    if len(sil_tags) == 0:
-      return {"0": {"slice": False, "split_time": f"0,{len(waveform)}"}}
-    else:
-      chunks = []
-      if sil_tags[0][0]:
-        chunks.append(
-          {"slice": False, "split_time": f"0,{min(waveform.shape[0], sil_tags[0][0] * self.hop_size)}"})
-      for i in range(0, len(sil_tags)):
-        if i:
-          chunks.append({"slice": False,
-                         "split_time": f"{sil_tags[i - 1][1] * self.hop_size},{min(waveform.shape[0], sil_tags[i][0] * self.hop_size)}"})
-        chunks.append({"slice": True,
-                       "split_time": f"{sil_tags[i][0] * self.hop_size},{min(waveform.shape[0], sil_tags[i][1] * self.hop_size)}"})
-      if sil_tags[-1][1] * self.hop_size < len(waveform):
-        chunks.append({"slice": False, "split_time": f"{sil_tags[-1][1] * self.hop_size},{len(waveform)}"})
-      chunk_dict = {}
-      for i in range(len(chunks)):
-        chunk_dict[str(i)] = chunks[i]
-      return chunk_dict
+    if len(sil_tags) == 0: return {"0": {"slice": False, "split_time": f"0,{len(waveform)}"}}  # Apply and return slices.
+    chunks = []
+    if sil_tags[0][0]:
+      chunks.append({"slice": False, "split_time": f"0,{min(waveform.shape[0], sil_tags[0][0] * self.hop_size)}"})
+    for i in range(0, len(sil_tags)):
+      if i: chunks.append({"slice": False, "split_time": f"{sil_tags[i - 1][1] * self.hop_size},{min(waveform.shape[0], sil_tags[i][0] * self.hop_size)}"})
+      chunks.append({"slice": True, "split_time": f"{sil_tags[i][0] * self.hop_size},{min(waveform.shape[0], sil_tags[i][1] * self.hop_size)}"})
+    if sil_tags[-1][1] * self.hop_size < len(waveform):
+      chunks.append({"slice": False, "split_time": f"{sil_tags[-1][1] * self.hop_size},{len(waveform)}"})
+    chunk_dict = {}
+    for i in range(len(chunks)): chunk_dict[str(i)] = chunks[i]
+    return chunk_dict
 
 # sinc_interp_hann audio resampling
 class Resample:
@@ -217,12 +168,7 @@ def sinc_interp_resample(x:Tensor, orig_freq:int=16000, new_freq:int=1600, lowpa
 
 def cut(audio_path, db_thresh=-30, min_len=5000):
   audio, sr = librosa.load(audio_path, sr=None)
-  slicer = Slicer(
-      sr=sr,
-      threshold=db_thresh,
-      min_length=min_len,
-      #max_sil_kept=300  # TODO
-  )
+  slicer = Slicer(sr=sr, threshold=db_thresh, min_length=min_len)
   chunks = slicer.slice(audio)
   return chunks
 
