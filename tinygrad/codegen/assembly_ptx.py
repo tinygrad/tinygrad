f9f4e5dacab124711dfa003b0524d3208232c943
kawpuh <e.r.sorrell@gmail.com>
2023-08-15 19:51:27 -0500

    initial merge fixes

--- tinygrad/codegen/assembly_ptx.py
+++ tinygrad/codegen/assembly_ptx.py
@@ -1,6 +1,6 @@
 from typing import List
 import struct
-from extra.assembly.assembly import uops_to_asmstyle, AssemblyLanguage
+from tinygrad.codegen.assembly import uops_to_asmstyle, AssemblyLanguage
 from tinygrad.codegen.linearizer import UOps, UOp, ConstOp
 from tinygrad.helpers import dtypes
 from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
@@ -16,24 +16,24 @@ class PTXLanguage(AssemblyLanguage):
   supports_constant_folding: bool = True
 
 def specialize_to_ptx(lang, function_name, asm):
-  ins = [".version 8.2", ".target " + arch(), ".address_size 64",
-         f".visible .entry {function_name}({', '.join(f'.param .u64 data{i}' for i in range(len(lang.bufs)))}) {{"]
-
+  param_cnt = 0
+  registers = []
+  ins = []
   alu = {BinaryOps.ADD: "add", BinaryOps.SUB: "sub", BinaryOps.MUL: "mul", BinaryOps.DIV: "div", BinaryOps.MAX: "max",
          BinaryOps.MOD: "rem", BinaryOps.CMPLT: "setp.lt", UnaryOps.SQRT: "sqrt.approx",
          UnaryOps.NOOP: "mov", UnaryOps.SIN: "sin.approx", UnaryOps.LOG2: "lg2.approx", UnaryOps.EXP2: "ex2.approx.ftz",
          TernaryOps.MULACC: "fma.rn", TernaryOps.WHERE: "selp"}
-
   for uop, out, vin, arg in asm:
     if uop == UOps.ENDLOOP:
-      if arg in ["local", "global+local"]:
-        ins.append("bar.sync 0;")
+      ins.append("bar.sync 0;")
     elif uop == UOps.DEFINE_REGISTER:
-      ins.append(f".reg .{dtype_to_nvtype[arg[0][0]]} %{arg[1]}<{arg[2]}>;",)
+      registers += [arg]
+      # ins.append(f".reg .{dtype_to_nvtype[arg[0][0]]} %{arg[1]}<{arg[2]}>;",)
     elif uop == UOps.DEFINE_LOCAL:
       ins.append(f".shared .align 4 .b8 {arg[0]}[{arg[1]*4}];")
     elif uop == UOps.SPECIAL:
       if arg.startswith('data'):
+        param_cnt += 1
         ins.append(f"ld.param.u64 {out}, [{arg}];")
         # TODO: we sometimes want this to be local, nvcc converts to global most of the time, not sure when we would need to?
         # ins.append(f"cvta.to.global.u64 {out}, {out};")
@@ -46,14 +46,18 @@ def specialize_to_ptx(lang, function_name, asm):
         ins.append(f"and.pred {out}, {', '.join(str(x) for x in vin)};")
       else:
         otype = vin[0].dtype if arg in [BinaryOps.CMPLT] else out.dtype
+        # if arg == TernaryOps.WHERE: vin[0], vin[2] = vin[2], vin[0]
         ins.append(f"{alu[arg]}{'.lo' if arg == BinaryOps.MUL and out.dtype != dtypes.float32 else ''}{'.rn' if arg == BinaryOps.DIV and out.dtype == dtypes.float32 else ''}.{dtype_to_nvtype[otype]} {out}, {', '.join(str(x) for x in vin)};")
     elif uop == UOps.LOAD:
-      if isinstance(arg, ConstOp):
-        ins.append(f"mov.{dtype_to_nvtype[out.dtype]} {out}, {'0f'+float_to_hex(arg.value) if dtypes.is_float(out.dtype) else arg.value};")
-      else: # memop
+      if arg.__class__ in (int, float):
+        ins.append(f"mov.{dtype_to_nvtype[out.dtype]} {out}, {'0f'+float_to_hex(arg) if dtypes.is_float(out.dtype) else arg};")
+      elif arg[2] is not None:
         ins.append(f"ld.{arg[1]}.{dtype_to_nvtype[arg[2]]} {out}, [{vin[0]}{f'+{arg[0]}' if arg[0] is not None else ''}];")
+        # cast after load
+      else:
+        ins.append(f"ld.{arg[1]}.{dtype_to_nvtype[dtypes.float]} {out}, [{vin[0]}{f'+{arg[0]}' if arg[0] is not None else ''}];")
     elif uop == UOps.STORE:
-      ins.append(f"st.{arg[1]}.{dtype_to_nvtype[arg[2]]} [{vin[0]}{f'+{arg[0]}' if arg[0] is not None else ''}], {vin[1]};")
+      ins.append(f"st.{arg[1]}.{dtype_to_nvtype[dtypes.float if arg[2] is None else arg[2]]} [{vin[0]}{f'+{arg[0]}' if arg[0] is not None else ''}], {vin[1]};")
     elif uop == UOps.CAST:
       if vin[0].dtype == dtypes.bool and (dtypes.is_float(out.dtype) or dtypes.is_int(out.dtype)):
         ins.append(f"selp.{dtype_to_nvtype[out.dtype]} {out}, {'0f3F800000, 0f00000000' if dtypes.is_float(out.dtype) else '1, 0'}, {vin[0]};")
@@ -67,10 +71,16 @@ def specialize_to_ptx(lang, function_name, asm):
     elif uop == UOps.COND_BRANCH:
       ins.append(f"@{'!' if not arg[1] else ''}{vin[0]} bra {arg[0]};")
 
+  ins_prefix = [".version 8.2", ".target " + arch(), ".address_size 64",
+                f".visible .entry {function_name}({', '.join(f'.param .u64 data{i}' for i in range(param_cnt))}) {{"]
+  for arg in registers: ins_prefix.append(f".reg .{dtype_to_nvtype[arg[0][0]]} %{arg[1]}<{arg[2]}>;",)
+
+  ins = ins_prefix + ins
+
   ins += ["ret;", "}"]
   return '\n'.join(ins)
 
 def uops_to_ptx_asm(function_name:str, uops:List[UOp]):
   lang = PTXLanguage()
-  global_size, local_size = uops_to_asmstyle(lang, function_name, uops, is_ptx=True)
-  return specialize_to_ptx(lang, function_name, lang.ins), global_size[::-1], local_size[::-1]
+  global_size, local_size = uops_to_asmstyle(lang, function_name, uops)
+  return specialize_to_ptx(lang, function_name, lang.ins), global_size[::-1], local_size[::-1], True
