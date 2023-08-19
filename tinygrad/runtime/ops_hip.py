f2f93696988abe650566774b91a218b6ac7a800a
George Hotz <geohot@gmail.com>
2023-08-19 17:22:10 +0000

    fix

--- tinygrad/runtime/ops_hip.py
+++ tinygrad/runtime/ops_hip.py
@@ -67,7 +67,7 @@ __device__ float4 sin(float4 x) { return float4(sin(x.x), sin(x.y), sin(x.z), si
 typedef float float8 __attribute__((ext_vector_type(8)));
 typedef _Float16 half16 __attribute__((ext_vector_type(16)));
 extern "C" __global__
-  """, kernel_modifier="__launch_bounds__ (128, 1)",  # TODO: is this okay all the time?
+  """, launch_bounds=True,
   smem_prefix = "__shared__ ", barrier = "__syncthreads();", float4 = "make_float4", uses_vload=True, uses_ptr_arithmetic=True,
   half_prekernel = "#include <hip/hip_fp16.h>\nusing half4 = HIP_vector_type<half, 4>;" + """
 __device__ float vload_half(size_t offset, const half *p) { return (float)*(p + offset); }
