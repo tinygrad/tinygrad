d4a771d94a812c3f3119ef01a202cf47cbb560d7
George Hotz <george@comma.ai>
2023-08-22 15:02:13 -0700

    print mem bw

--- examples/gpt2.py
+++ examples/gpt2.py
@@ -179,7 +179,9 @@ class GPT2:
       GlobalCounters.reset()
       if args.timing: print("")
       st = GlobalCounters.time_sum_s
-      with Timing(f"ran model in ", on_exit=(lambda et: f", {(GlobalCounters.time_sum_s-st)*1e3:.2f} ms on GPU, {GlobalCounters.global_ops*1e-9:.2f} GOPS, {GlobalCounters.global_mem*1e-9:.2f} GB") if DEBUG else None, enabled=timing):
+      with Timing(f"ran model in ", on_exit=(lambda et: f", {(GlobalCounters.time_sum_s-st)*1e3:.2f} ms on GPU"+
+                  f", {GlobalCounters.global_ops*1e-9:.2f} GOPS, {GlobalCounters.global_mem*1e-9:.2f} GB"+
+                  f", {GlobalCounters.global_mem*1e-9/(GlobalCounters.time_sum_s-st):.2f} GB/s") if DEBUG else None, enabled=timing):
         probs = self.model(Tensor([toks[start_pos:]]), start_pos, temperature)
       with Timing("sync in ", enabled=timing):
         probs_np = probs.numpy()
