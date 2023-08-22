cfabc5ddc33f647dd43a733713bf2462a207eab2
ZenginU <umutzengin00@gmail.com>
2023-08-22 05:43:00 +0300

    update argmax

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -446,9 +446,9 @@ class Tensor:
 
   def argmax(self, axis=None, keepdim=False):
     if axis is None: return prod(self.shape) - ((self == self.max(axis)).flatten() * Tensor.arange(prod(self.shape)-1,-1,-1)).max() - 1
-    axis = axis + self.ndim if axis < 0 else axis
-    m = self == (self.max(axis=axis, keepdim=keepdim) if keepdim else self.max(axis=axis, keepdim=keepdim).unsqueeze(axis))
-    idx = m * Tensor.arange(self.shape[axis]-1,-1,-1).reshape(*[1]*axis, self.shape[axis], *[1]*(self.ndim-(axis+1)))
+    axis = axis + len(self.shape) if axis < 0 else axis
+    m = self == self.max(axis=axis, keepdim=True)
+    idx = m * Tensor.arange(self.shape[axis]-1,-1,-1).reshape(self.shape[axis], *[1]*(self.ndim-axis-1))
     return self.shape[axis]-idx.max(axis=axis, keepdim=keepdim)-1
   def argmin(self, axis=None, keepdim=False): return (-self).argmax(axis=axis, keepdim=keepdim)
 
