c41dea3796963974c7517c2a2ac4cbbeb4783ee2
Szymon Ożóg <szymon.ozog@gmail.com>
2023-08-24 09:01:20 +0200

    Implemented bool dtype

--- tinygrad/renderer/triton.py
+++ tinygrad/renderer/triton.py
@@ -29,8 +29,8 @@ def uops_to_triton(function_name:str, uops:List[UOp]):
     TernaryOps.MULACC: lambda x,y,z: f"(({x}*{y})+{z})",
     TernaryOps.WHERE: lambda x,y,z: f"tl.where({x},{y},{z})",
   }
-  triton_dtypes = {dtypes.double: "tl.float64", dtypes.float32: "tl.float32", dtypes.float16: "tl.float16", dtypes.int8: "tl.int8", dtypes.uint8: "tl.uint8", dtypes.int32: "tl.int32", dtypes.int64: "tl.int64"}
-  signature_dtypes = {dtypes.double: "*fp64",dtypes.float32: "*fp32", dtypes.float16: "*fp16", dtypes.int8: "*i8", dtypes.uint8: "*u8", dtypes.int32: "*i32", dtypes.int64: "*i64"}
+  triton_dtypes = {dtypes.double: "tl.float64", dtypes.float32: "tl.float32", dtypes.float16: "tl.float16", dtypes.bool: "tl.int8", dtypes.int8: "tl.int8", dtypes.uint8: "tl.uint8", dtypes.int32: "tl.int32", dtypes.int64: "tl.int64"}
+  signature_dtypes = {dtypes.double: "*fp64",dtypes.float32: "*fp32", dtypes.float16: "*fp16", dtypes.bool: "*i8", dtypes.int8: "*i8", dtypes.uint8: "*u8", dtypes.int32: "*i32", dtypes.int64: "*i64"}
   for uop,newvar,vin,args in uops:
     if uop == UOps.LOOP:
       for i,var in enumerate(args[0]):
