f7dadafdd5a3e7bedc3a621f4b1636260b99f046
zibokapi <zibokapi@gmail.com>
2023-08-24 16:45:01 -0700

    revert prev refactor: go back to using sum_dim for first iteration

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -320,11 +320,11 @@ class Tensor:
       max_dim = max(i.ndim for i in idx)
       idx = [i.reshape(*[1]*(max_dim-i.ndim), *i.shape) for i in idx] # TODO this reshape is removable. Just add it to args of other reshapes, but makes args more unreadable
       # compute sum_dim
-      sum_dim = [d+max_dim-n for n,d in enumerate(dim)]
+      sum_dim = [d if n==0 else d+i.ndim-n for n,(d,i) in enumerate(zip(dim,idx))]
       # first iteration
-      new_idx = idx[0].reshape(*[1]*dim[0], 1,*idx[0].shape, *[1]*(ret.ndim-dim[0]-1))
-      arange = Tensor.arange(ret.shape[dim[0]], dtype=dtypes.int32, requires_grad=False, device=self.device).reshape(*[1]*dim[0], ret.shape[dim[0]], *[1]*idx[0].ndim, *[1]*(ret.ndim-dim[0]-1))
-      ret = (ret.reshape(*ret.shape[:dim[0]+1], *[1]*idx[0].ndim, *ret.shape[dim[0]+1:]) * (arange == new_idx)).sum(dim[0])
+      new_idx = idx[0].reshape(*[1]*sum_dim[0], 1,*idx[0].shape, *[1]*(ret.ndim-sum_dim[0]-1))
+      arange = Tensor.arange(ret.shape[sum_dim[0]], dtype=dtypes.int32, requires_grad=False, device=self.device).reshape(*[1]*sum_dim[0], ret.shape[sum_dim[0]], *[1]*idx[0].ndim, *[1]*(ret.ndim-sum_dim[0]-1))
+      ret = (ret.reshape(*ret.shape[:sum_dim[0]+1], *[1]*idx[0].ndim, *ret.shape[sum_dim[0]+1:]) * (arange == new_idx)).sum(sum_dim[0])
       # consecutive iterations
       for idx_,d in zip(idx[1:],sum_dim[1:]):
         new_idx = idx_.reshape(*[1]*dim[0], *idx_.shape, *[1]*(ret.ndim-dim[0]-idx_.ndim))
