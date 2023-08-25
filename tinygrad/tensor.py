b4351c6b1232d44e24a7fb5d5070c1b3c8db2235
zibokapi <zibokapi@gmail.com>
2023-08-25 13:18:09 -0700

    lol

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -314,13 +314,11 @@ class Tensor:
     if tensors:
       # normalize idx
       idx = [t.sign().contiguous().__neg__().contiguous().relu() * ret.shape[d] + t for d,t in zip(dim, tensors)] # TODO first contiguous fixes torch+cpu_only CI, but it causes llvm to fail. Second one fixes llvm
-      # reshape to same ndim when idxs have different ndim
-      max_dim = max(i.ndim for i in idx)
-      idx = [i.reshape(*[1]*(max_dim-i.ndim), *i.shape) for i in idx] # NOTE this reshape is removable. Just add it to args of other reshapes, but makes args more unreadable
       # compute sum_dim, arange, and idx
+      max_dim = max(i.ndim for i in idx)
       sum_dim = [d if n==0 else d+max_dim-n for n,d in enumerate(dim)]
       arange = [Tensor.arange(ret.shape[d], dtype=dtypes.int32, requires_grad=False, device=self.device).reshape(*[1]*sd, ret.shape[d], *[1]*(((ret.ndim + max_dim) if n==0 else (ret.ndim + max_dim - n)) - sd - 1)) for n,(sd,d) in enumerate(zip(sum_dim, dim))]
-      idx = [i.reshape(*[1]*sum_dim[0], *((1, *i.shape) if n == 0 else i.shape), *[1]*((ret.ndim - sd - 1) if n==0 else (ret.ndim - sum_dim[0] - n))) for n,(sd,i) in enumerate(zip(sum_dim, idx))]
+      idx = [i.reshape(*[1]*sum_dim[0], *((1, *[1]*(max_dim-i.ndim), *i.shape, *[1]*(ret.ndim - sd - 1)) if n == 0 else (*[1]*(max_dim-i.ndim), *i.shape, *[1]*(ret.ndim - sum_dim[0] - n)))) for n,(sd,i) in enumerate(zip(sum_dim, idx))]
       ret = ret.reshape(*ret.shape[:sum_dim[0]+1], *[1]*max_dim, *ret.shape[sum_dim[0]+1:])
       for a,i,sd in zip(arange, idx, sum_dim): ret = (a==i).mul(ret).sum(sd)
       # special permute case
