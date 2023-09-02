a5369f08c085d0fef9228b99854fc49c6d694361
Szymon Ożóg <szymon.ozog@gmail.com>
2023-09-02 09:39:14 +0200

    update to new uops

--- tinygrad/renderer/triton.py
+++ tinygrad/renderer/triton.py
@@ -1,7 +1,8 @@
-from typing import Dict, List, Final, Callable
+from typing import Dict, List, Final, Callable, DefaultDict
+from collections import defaultdict
 from tinygrad.ops import UnaryOps, BinaryOps, TernaryOps, Op
 from tinygrad.helpers import dtypes, ImageDType, DEBUG, getenv
-from tinygrad.codegen.linearizer import  UOp, UOps, ConstOp
+from tinygrad.codegen.linearizer import  UOp, UOps
 from tinygrad.shape.symbolic import NumNode
 from triton.compiler import compile as triton_compile
 import hashlib
@@ -33,6 +34,12 @@ def uops_to_triton(function_name:str, uops:List[UOp]):
   bufs = []
   signatures = []
   dims = []
+  c: DefaultDict[str, int] = defaultdict(int)
+  def ssa(prefix="t"):
+    nonlocal c
+    c[prefix] += 1
+    return f"{prefix}{c[prefix]-1}"
+  r: Dict[UOp, str] = {}
   def kk(s): kernel.append("  "*depth+s)  
   gid = [f"tl.program_id({i})" for i in range(3)]
   code_for_op: Final[Dict[Op, Callable]] = {
@@ -50,7 +57,10 @@ def uops_to_triton(function_name:str, uops:List[UOp]):
   }
   triton_dtypes = {dtypes.double: "tl.float64", dtypes.float32: "tl.float32", dtypes.float16: "tl.float16", dtypes.bool: "tl.int8", dtypes.int8: "tl.int8", dtypes.uint8: "tl.uint8", dtypes.int32: "tl.int32", dtypes.int64: "tl.int64"}
   signature_dtypes = {dtypes.double: "*fp64",dtypes.float32: "*fp32", dtypes.float16: "*fp16", dtypes.bool: "*i8", dtypes.int8: "*i8", dtypes.uint8: "*u8", dtypes._arg_int32: "i32", dtypes.int32: "*i32", dtypes.int64: "*i64"}
-  for uop,newvar,vin,args in uops:
+
+  
+  for u in uops:
+    uop,newvar,vin,args,_ = u
     if uop == UOps.LOOP:
       for i,var in enumerate(args[0]):
         if isinstance(var, NumNode): continue # python doesnt have block scope
@@ -72,25 +82,32 @@ def uops_to_triton(function_name:str, uops:List[UOp]):
         kk(f"# end {args[1]}")
     elif uop == UOps.ALU:
       assert newvar is not None
-      kk(f"{newvar.render()} = ({code_for_op[args](*[x.render() for x in vin])}).to({triton_dtypes[newvar.dtype]})")
+      r[u] = ssa("alu")
+      kk(f"{r[u]} = ({code_for_op[args](*[r[x] for x in vin])}).to({triton_dtypes[newvar]})")
     elif uop == UOps.LOAD:
       assert newvar is not None
-      if isinstance(args, ConstOp):
-        val = ('-' if math.isinf(args.value) and args.value<0 else'') + ('float("inf")' if math.isinf(args.value) else str(args.value))
-        if math.isnan(args.value): val = "float('nan')"
+      r[u] = ssa("val")
+      if args.valid.min == 1: kk(f"{r[u]} = tl.load({args.name} + {args.idx.render()}, mask = {render_valid(args.idx)}).to({triton_dtypes[args.memory_dtype]})")
+      else: kk(f"{r[u]} = tl.where({args.valid.render()}, tl.load({args.name}+{fill_dims_for_idx(args.idx,dims)} , mask={args.valid.render()}), 0.0).to({triton_dtypes[args.memory_dtype]})")
+    elif uop == UOps.CONST or uop == UOps.DEFINE_ACC:
+        r[u] = ssa("const" if uop == UOps.CONST else "acc")
+        val = ('-' if math.isinf(args) and args<0 else'') + ('float("inf")' if math.isinf(args) else str(args))
+        if math.isnan(args): val = "float('nan')"
         if len(local_size) > 0:
-          kk(f"{newvar.render()} = tl.where({args.valid.render()},tl.full(({','.join([str(next_power_of_2(x)) for x in local_size])},),{val}, dtype={triton_dtypes[newvar.dtype]}),tl.full(({','.join([str(next_power_of_2(x)) for x in local_size])},),{args.invalid_value}, dtype={triton_dtypes[newvar.dtype]}))") 
+          kk(f"{r[u]} = tl.full(({','.join([str(next_power_of_2(x)) for x in local_size])},),{val}, dtype={triton_dtypes[newvar]})") 
         else:
-          kk(f"{newvar.render()} = tl.where({args.valid.render()},{val},{args.invalid_value}).to({triton_dtypes[newvar.dtype]})")
-      elif args.valid.min == 1: kk(f"{newvar.render()} = tl.load({args.name} + {args.idx.render()}, mask = {render_valid(args.idx)}).to({triton_dtypes[args.memory_dtype]})")
-      else: kk(f"{newvar.render()} = tl.where({args.valid.render()}, tl.load({args.name}+{fill_dims_for_idx(args.idx,dims)} , mask={args.valid.render()}), 0.0).to({triton_dtypes[args.memory_dtype]})")
+          kk(f"{r[u]} = tl.where(1, {val}, {val}).to({triton_dtypes[newvar]})")
     elif uop == UOps.STORE:
-      assert not isinstance(args.memory_dtype, ImageDType), "unimplemented: image store"
-      assert args.valid.min == 1, "store must be valid"
-      kk(f"tl.store({args.name} + {args.idx.render()}, {vin[0].render()}, mask = {render_valid(args.idx)}) ")
+      assert not isinstance(newvar, ImageDType), "unimplemented: image store"
+      if args is None:
+        kk(f"{r[vin[0]]} =  {r[vin[1]]}")
+      else:
+        kk(f"tl.store({args.name} + {args.idx.render()}, {r[vin[0]]}, mask = {render_valid(args.idx)}) ")
     elif uop == UOps.DEFINE_GLOBAL:
       bufs.append(args)
       signatures.append(signature_dtypes[args[1]])
+    elif uop == UOps.SPECIAL:
+      r[u] = args.expr
     elif uop == UOps.CAST: raise NotImplementedError("unimplemented: cast")
     else:
       raise NotImplementedError(f"unimplemented: {uop}")  
