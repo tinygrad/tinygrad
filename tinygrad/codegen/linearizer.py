d3821502c26ba176e1cfcaa2928717147a79214a
David Hou <david.hou314@gmail.com>
2023-09-05 01:53:31 -0700

    try to fix bug?

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -105,7 +105,7 @@ class Linearizer(OptimizedKernel):
     for _idx in _idxs:
       substitute = {a: b for a, b in zip(fake_idxs, _idx) if isinstance(a, Variable)}
       if amt > 1:
-        float4_substitute = {**substitute, fake_idxs[dim]: Variable.num(fake_idxs[dim].min)}
+        float4_substitute = {**substitute, fake_idxs[dim]: expanded_nodes[dim][0]}
         idx, valid = g_idx.substitute(float4_substitute), g_valid.substitute(float4_substitute)
         localtype = dtypes._float4 if amt == 4 else dtypes._float2
         if idx.render() != ((idx//amt)*amt).render():
