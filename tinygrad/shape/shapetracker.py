0d30fcf0729517bd987baba96180890b9528c9d6
Roelof van Dijk <roelof.van.dijk@vitestro.com>
2023-09-10 22:28:46 +0200

    fix: conditional assert

--- tinygrad/shape/shapetracker.py
+++ tinygrad/shape/shapetracker.py
@@ -234,8 +234,7 @@ class ShapeTracker:
     if self.views[-1].shape == new_shape: return self
     assert all(is_sym_int(x) and x > 0 for x in new_shape), f"shape must be symbolic ints and can't contain 0 or negative numbers {new_shape}"
     # only check size for int shapes. we don't check symbolic here as long as the reshape itself can be done
-    if all(isinstance(s, int) for s in self.shape) and all(isinstance(s, int) for s in new_shape):
-      assert prod(self.shape) == prod(new_shape), f"can't reshape {self.shape} -> {new_shape}" # type: ignore  # mypy cannot resolve, all ints here
+    assert not (all(isinstance(s, int) for s in self.shape) and all(isinstance(s, int) for s in new_shape)) or prod(self.shape) == prod(new_shape), f"can't reshape {self.shape} -> {new_shape}" # type: ignore  # mypy cannot resolve, all ints here
     new_view, extra = _reshape(self.views[-1], new_shape)
     if extra: self.views.append(new_view)
     else: self.views[-1] = new_view
