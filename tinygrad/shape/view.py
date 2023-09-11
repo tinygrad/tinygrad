f135e044333a0158ff999d94795a69ee394cf3d0
Roelof van Dijk <roelof.van.dijk@vitestro.com>
2023-09-11 11:38:35 +0200

    perf: assert behind if is not optimized

--- tinygrad/shape/view.py
+++ tinygrad/shape/view.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import functools
 from typing import Tuple, List, Optional, NamedTuple, Union
-from tinygrad.helpers import prod
+from tinygrad.helpers import prod, all_int
 from tinygrad.shape.symbolic import Variable, Node, is_sym_int
 
 @functools.lru_cache(maxsize=None)
@@ -125,9 +125,7 @@ class View(NamedTuple):
 
     assert all(is_sym_int(x) and x > 0 for x in new_shape), f"shape must be symbolic ints and can't contain 0 or negative numbers {new_shape}"
     # only check size for int shapes. we don't check symbolic here as long as the reshape itself can be done
-    if all(isinstance(s, int) for s in self.shape) and all(isinstance(s, int) for s in new_shape):
-      assert prod(self.shape) == prod(new_shape), f"can't reshape {self.shape} -> {new_shape}" # type: ignore  # mypy cannot resolve, all ints here
-
+    assert prod(self.shape) == prod(new_shape) if all_int(self.shape + new_shape) else True, f"can't reshape {self.shape=} -> {new_shape=}"
     # after the asserts, it's okay to check contiguous
     if self.contiguous: return View.create(new_shape)
 
