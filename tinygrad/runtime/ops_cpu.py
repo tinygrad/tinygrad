b981f744f688508a1d60671755bbdaacf316f09d
George Hotz <geohot@gmail.com>
2023-09-10 22:40:34 -0700

    minor cleanups

--- tinygrad/runtime/ops_cpu.py
+++ tinygrad/runtime/ops_cpu.py
@@ -47,17 +47,19 @@ class RawNumpyBuffer(RawBuffer):
   @classmethod
   def fromCPU(cls, x): return cls(x.size, dtypes.from_np(x.dtype), x)
   def toCPU(self): return self._buf
-def apply_st(x):
+def apply_shapetracker(x) -> RawNumpyBuffer:
   real, st = x.realized, x.st
-  st.simplify()
+  st.simplify()   # TODO: this is generic
   ret = real._buf
   for v in st.views:
-    real_buf = np.require(ret, requirements='C')  # if this is non contig, how did it happen?
-    real_strides = tuple(x*real.dtype.itemsize for x in v.strides)
-    real_shape = tuple(y-x for x,y in v.mask) if v.mask else v.shape
-    real_offset = v.offset * real.dtype.itemsize
-    if v.mask is not None: real_offset += sum(x*st for (x,_),st in zip(v.mask, real_strides))
-    ret = np.ndarray(real_shape, buffer=real_buf, dtype=real.dtype.np, offset=real_offset, strides=real_strides)
-    if v.mask is not None: ret = np.pad(ret, tuple((x,s-y) for (x,y),s in zip(v.mask, v.shape)))
+    real_buf = np.require(ret, requirements='C')  # if this is non contig on the first one, how did it happen?
+    if v.mask is not None:
+      ret = np.ndarray(tuple(y-x for x,y in v.mask), buffer=real_buf, dtype=real.dtype.np,
+                       offset=(v.offset + sum(x*st for (x,_),st in zip(v.mask, v.strides)))*real.dtype.itemsize,
+                       strides=tuple(x*real.dtype.itemsize for x in v.strides))
+      ret = np.pad(ret, tuple((x,s-y) for (x,y),s in zip(v.mask, v.shape)))
+    else:
+      ret = np.ndarray(v.shape, buffer=real_buf, dtype=real.dtype.np,
+                       offset=v.offset*real.dtype.itemsize, strides=tuple(x*real.dtype.itemsize for x in v.strides))
   return RawNumpyBuffer.fromCPU(ret)
-CPUBuffer = Interpreted(RawNumpyBuffer, numpy_fxn_for_op, from_lazybuffer=apply_st, from_underlying=RawNumpyBuffer.fromCPU)
+CPUBuffer = Interpreted(RawNumpyBuffer, numpy_fxn_for_op, from_lazybuffer=apply_shapetracker, from_underlying=RawNumpyBuffer.fromCPU)
