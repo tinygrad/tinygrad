2e05b31b149de4e09ff3dad33cf72548224a043c
madt2709 <theodore.dias@hotmail.co.uk>
2023-09-12 22:02:54 -0700

    Add github action for sz diff

--- sz.py
+++ sz.py
@@ -8,9 +8,8 @@ from tabulate import tabulate
 
 TOKEN_WHITELIST = [token.OP, token.NAME, token.NUMBER, token.STRING]
 
-if __name__ == "__main__":
-  headers = ["Name", "Lines", "Tokens/Line"]
-  table = []
+def file_sz():
+  table = {}
   for path, subdirs, files in os.walk("tinygrad"):
     for name in files:
       if not name.endswith(".py"): continue
@@ -18,11 +17,20 @@ if __name__ == "__main__":
       with tokenize.open(filepath) as file_:
         tokens = [t for t in tokenize.generate_tokens(file_.readline) if t.type in TOKEN_WHITELIST]
         token_count, line_count = len(tokens), len(set([t.start[0] for t in tokens]))
-        table.append([filepath.as_posix(), line_count, token_count/line_count])
+        table[filepath.as_posix()] = [line_count, token_count/line_count]
 
-  print(tabulate([headers] + sorted(table, key=lambda x: -x[1]), headers="firstrow", floatfmt=".1f")+"\n")
+  return table
 
-  for dir_name, group in itertools.groupby(sorted([(x[0].rsplit("/", 1)[0], x[1]) for x in table]), key=lambda x:x[0]):
-    print(f"{dir_name:30s} : {sum([x[1] for x in group]):6d}")
+if __name__ == "__main__":
+  headers = ["Name", "Lines", "Tokens/Line"]
+  if os.getenv("CI") == 1: 
+    print(file_sz())
+  else:
+    table = [[key] + value for key, value in file_sz().items()]
 
-  print(f"\ntotal line count: {sum([x[1] for x in table])}")
+    print(tabulate([headers] + sorted(table, key=lambda x: -x[1]), headers="firstrow", floatfmt=".1f")+"\n")
+
+    for dir_name, group in itertools.groupby(sorted([(x[0].rsplit("/", 1)[0], x[1]) for x in table]), key=lambda x:x[0]):
+      print(f"{dir_name:30s} : {sum([x[1] for x in group]):6d}")
+
+    print(f"\ntotal line count: {sum([x[1] for x in table])}")
