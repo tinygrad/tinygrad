6b224228b3c6a2fe13ec1767c18c7560ccd9d4a7
Gijs Koning <gijs-koning@live.nl>
2023-09-15 14:10:44 +0200

    Also fix constant folding

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -79,22 +79,21 @@ class TestLinearizer(unittest.TestCase):
     num_ops = len([uop for uop in k.uops if uop.uop == UOps.ALU])
     assert num_ops == 0, "more alu uops than needed"
 
-  @unittest.skip("constant folding not supported yet")
   def test_constant_fold(self):
     if not isinstance(Device[Device.DEFAULT], Compiled):
       self.skipTest("Only Compiled uses linearizer")
 
-    a, b = Tensor(2), Tensor(3)
-    r = a * b
-    ast = r.lazydata.op
-    r = r.realize()  # realize an output buffer
-    k = Linearizer(ast, r.lazydata, Device[Device.DEFAULT].linearizer_opts)
-    k.process()
-    k.linearize()
-    num_ops = len([uop for uop in k.uops if uop.uop in [UOps.LOAD, UOps.ALU]])
-    assert num_ops == 0, "more load or alu uops than needed"
+    fs = [lambda a,b: a*b,lambda a,b:a-b,lambda a,b: a/b,lambda a,b: a<b] # tensor.__mod__ is not yet implemented
 
-  def test_max_fold(self):
+    for f in fs:
+      a, b = 2,3
+      r = f(Tensor(a), Tensor(b))
+      k = linearize(r)
+      num_ops = len([uop for uop in k.uops if uop.uop in [UOps.LOAD, UOps.ALU]])
+      assert num_ops == 0, "more load or alu uops than needed"
+      np.testing.assert_allclose(r.numpy(), f(a,b)), f"result is wrong"
+
+  def test_max_constant_fold(self):
     if not isinstance(Device[Device.DEFAULT], Compiled):
       self.skipTest("Only Compiled uses linearizer")
 
@@ -106,21 +105,21 @@ class TestLinearizer(unittest.TestCase):
     np.testing.assert_allclose(r.numpy(), 0.), f"result is wrong"
     assert num_ops == 0, "more load or alu uops than needed"
 
-  def test_sub_fold(self):
+  def test_equal_fold(self):
     if not isinstance(Device[Device.DEFAULT], Compiled):
       self.skipTest("Only Compiled uses linearizer")
 
-    a, b = Tensor(2), Tensor(2)
-    r = a - b
+    a = Tensor.rand(1)
+    r = a - a
     k = linearize(r)
 
     num_ops = len([uop for uop in k.uops if uop.uop in [UOps.LOAD, UOps.ALU]])
     np.testing.assert_allclose(r.numpy(), 0.), f"result is wrong"
     assert num_ops == 0, "more load or alu uops than needed"
 
-  def test_sub_fold_array(self):
+  def test_equal_sub_fold_array(self):
     if not isinstance(Device[Device.DEFAULT], Compiled):
-      self.skipTest("Only Compiled uses linearizer")
+      self.skipTest("Onlyuops Compiled uses linearizer")
 
     a, b, c = Tensor.ones(2), Tensor.ones(2), Tensor([3, 3])
     r = (a + c) - (b + c)
@@ -129,7 +128,7 @@ class TestLinearizer(unittest.TestCase):
     np.testing.assert_allclose(r.numpy(), np.zeros(2)), f"result is wrong"
     assert num_ops == 0, "more load or alu uops than needed"
 
-  def test_compare_fold(self): # doesnt work for LLVM only if lazy is adapted for constants
+  def test_compare_fold(self):  # doesnt work for LLVM only if lazy is adapted for constants
     if not isinstance(Device[Device.DEFAULT], Compiled):
       self.skipTest("Only Compiled uses linearizer")
 
@@ -141,17 +140,20 @@ class TestLinearizer(unittest.TestCase):
     np.testing.assert_allclose(r.numpy(), 0.), f"result is wrong"
     assert num_ops == 0, "more load or alu uops than needed"
 
+
 def helper_linearizer_opt(r: Tensor, opts=[]):
   wanna_output = None
   realized_ast = None
 
   # HACK to get real ast.
   real_dev_exec_ast = Device[Device.DEFAULT].exec_ast
+
   def fake_exec_ast(ast, output=None, **kwargs):
     nonlocal realized_ast
     x = real_dev_exec_ast(ast, output, **kwargs)
-    if not(ast.op in MovementOps and ast.src[0].__class__ is not LazyOp and ast.src[0].realized): realized_ast = ast # get last executed
+    if not (ast.op in MovementOps and ast.src[0].__class__ is not LazyOp and ast.src[0].realized): realized_ast = ast  # get last executed
     return x
+
   Device[Device.DEFAULT].exec_ast = fake_exec_ast
   r = r.realize()  # realize an output buffer
   assert realized_ast is not None
@@ -162,7 +164,7 @@ def helper_linearizer_opt(r: Tensor, opts=[]):
     k.process()
     k.apply_auto_opt(x)
     prg = to_prg(k)
-    k.bufs[0].realized = k.bufs[0].realized.fromCPU(np.zeros(k.bufs[0].shape, dtype=k.bufs[0].dtype.np)) # Zero to check that all values are filled
+    k.bufs[0].realized = k.bufs[0].realized.fromCPU(np.zeros(k.bufs[0].shape, dtype=k.bufs[0].dtype.np))  # Zero to check that all values are filled
     prg.exec(k.bufs, force_wait=True)
     np.testing.assert_allclose(wanna_output, k.bufs[0].toCPU(), atol=1e-4, rtol=1e-4)
 
@@ -177,11 +179,12 @@ def helper_linearizer_opt(r: Tensor, opts=[]):
   k = Linearizer(realized_ast, r.lazydata, Device[Device.DEFAULT].linearizer_opts)
   k.hand_coded_optimizations()
   prg = Device[Device.DEFAULT].to_program(k)
-  k.bufs[0].realized = k.bufs[0].realized.fromCPU(np.zeros(k.bufs[0].shape, dtype=k.bufs[0].dtype.np)) # Zero to check that all values are filled
+  k.bufs[0].realized = k.bufs[0].realized.fromCPU(np.zeros(k.bufs[0].shape, dtype=k.bufs[0].dtype.np))  # Zero to check that all values are filled
   prg.exec(k.bufs, force_wait=True)
   np.testing.assert_allclose(wanna_output, k.bufs[0].toCPU(), atol=1e-4, rtol=1e-4)
-  for x in opts: # Check custom transformations if any.
+  for x in opts:  # Check custom transformations if any.
     check_opt(x, lambda: Linearizer(realized_ast, r.lazydata, Device[Device.DEFAULT].linearizer_opts), Device[Device.DEFAULT].to_program)
+
 
 class TestLinearizerOpts(unittest.TestCase):
   def test_local_and_grouped_reduce(self):
@@ -192,12 +195,13 @@ class TestLinearizerOpts(unittest.TestCase):
     Tensor.manual_seed(1882)
     a = Tensor.rand(4, 4, N, N)
     b = Tensor.rand(4, 4, N)
-    r = (b.sqrt() + ((a+1).sum(axis=3).exp()))
+    r = (b.sqrt() + ((a + 1).sum(axis=3).exp()))
     helper_linearizer_opt(r, [
-      [(0, 2, 'L')], [(0, 8, 'L')], [(0, 16, 'L')], # Checking how it works with locals
-      [(0, 2, 'G')], [(0, 32, 'G')], [(0, 64, 'G')], # Checking how it works with grouped reduce
-      [(0, 2, 'L'), (0, 2, 'G')], [(0, 16, 'L'), (0, 16, 'G')], [(0, 32, 'L'), (0, 2, 'G')], [(0, 2, 'L'), (0, 64, 'G')], # Checking how it works with locals + grouped reduce
-      [(0, 2, 'L'), (0, 2, 'G'), (0, 8, 'U'), (0, 4, 'R')], # Checking how it works with locals + grouped reduce + upcasts
+      [(0, 2, 'L')], [(0, 8, 'L')], [(0, 16, 'L')],  # Checking how it works with locals
+      [(0, 2, 'G')], [(0, 32, 'G')], [(0, 64, 'G')],  # Checking how it works with grouped reduce
+      [(0, 2, 'L'), (0, 2, 'G')], [(0, 16, 'L'), (0, 16, 'G')], [(0, 32, 'L'), (0, 2, 'G')], [(0, 2, 'L'), (0, 64, 'G')],
+      # Checking how it works with locals + grouped reduce
+      [(0, 2, 'L'), (0, 2, 'G'), (0, 8, 'U'), (0, 4, 'R')],  # Checking how it works with locals + grouped reduce + upcasts
     ])
 
   def test_upcasts(self):
@@ -208,9 +212,9 @@ class TestLinearizerOpts(unittest.TestCase):
     Tensor.manual_seed(1772)
     a = Tensor.rand(N, N)
     b = Tensor.rand(N, N)
-    r = (a+b).sqrt() * ((a+1).exp())
+    r = (a + b).sqrt() * ((a + 1).exp())
     helper_linearizer_opt(r, [
-      [(0, 2, 'U')], [(0, 4, 'U')], [(0, 8, 'U')], # Checking how it works with upcasts
+      [(0, 2, 'U')], [(0, 4, 'U')], [(0, 8, 'U')],  # Checking how it works with upcasts
     ])
 
   def test_full_upcast(self):
@@ -220,9 +224,9 @@ class TestLinearizerOpts(unittest.TestCase):
     Tensor.manual_seed(1772)
     a = Tensor.rand(4)
     b = Tensor.rand(4)
-    r = (a+b).sqrt() * ((a+1).exp())
+    r = (a + b).sqrt() * ((a + 1).exp())
     helper_linearizer_opt(r, [
-      [(0, 4, 'U')], # Checking how it works with upcasts
+      [(0, 4, 'U')],  # Checking how it works with upcasts
     ])
 
   def test_matmul(self):
@@ -233,14 +237,15 @@ class TestLinearizerOpts(unittest.TestCase):
     Tensor.manual_seed(1552)
     a = Tensor.rand(N, N)
     b = Tensor.rand(N, N)
-    r = a@b
+    r = a @ b
     helper_linearizer_opt(r, [
-      [(0, 2, 'U')], [(0, 4, 'U'), (1, 4, 'U')], # Checking how it works with upcasts
-      [(0, 2, 'L')], [(1, 32, 'L')], [(0, 4, 'L'), (1, 4, 'L')], [(0, 4, 'L'), (1, 32, 'L')], [(0, 16, 'L'), (1, 8, 'L')], # Checking how it works with locals
-      [(0, 2, 'G')], [(0, 32, 'G')], [(0, 32, 'G'), (0, 4, 'R')], # Checking how it works with grouped_reduce
-      [(0, 2, 'L'), (1, 2, 'L'), (0, 32, 'G')], [(0, 16, 'L'), (0, 32, 'G')], [(0, 16, 'L'), (0, 8, 'L'), (0, 4, 'G')], # Checking how it works with local+grouped_reduce
-      [(0, 4, 'L'), (0, 4, 'L'), (0, 16, 'G'), (0, 4, 'R'), (0, 4, 'U'), (1, 2, 'U')], # Checking all together
-      [(0, 4, 'L'), (0, 4, 'L'), (0, 16, 'G'), (0, 4, 'R'), (0, 8, 'U')], # Full global upcast + local
+      [(0, 2, 'U')], [(0, 4, 'U'), (1, 4, 'U')],  # Checking how it works with upcasts
+      [(0, 2, 'L')], [(1, 32, 'L')], [(0, 4, 'L'), (1, 4, 'L')], [(0, 4, 'L'), (1, 32, 'L')], [(0, 16, 'L'), (1, 8, 'L')],  # Checking how it works with locals
+      [(0, 2, 'G')], [(0, 32, 'G')], [(0, 32, 'G'), (0, 4, 'R')],  # Checking how it works with grouped_reduce
+      [(0, 2, 'L'), (1, 2, 'L'), (0, 32, 'G')], [(0, 16, 'L'), (0, 32, 'G')], [(0, 16, 'L'), (0, 8, 'L'), (0, 4, 'G')],
+      # Checking how it works with local+grouped_reduce
+      [(0, 4, 'L'), (0, 4, 'L'), (0, 16, 'G'), (0, 4, 'R'), (0, 4, 'U'), (1, 2, 'U')],  # Checking all together
+      [(0, 4, 'L'), (0, 4, 'L'), (0, 16, 'G'), (0, 4, 'R'), (0, 8, 'U')],  # Full global upcast + local
     ])
 
   def test_double_reduce(self):
@@ -250,14 +255,17 @@ class TestLinearizerOpts(unittest.TestCase):
     N = 128
     Tensor.manual_seed(1552)
     a = Tensor.rand(8, N, 8, N)
-    r = a.sum(axis=(1,3))
+    r = a.sum(axis=(1, 3))
     helper_linearizer_opt(r, [
-      [(0, 2, 'G')], [(0, 32, 'G')], [(1, 2, 'G')], [(1, 32, 'G')], # Checking how it works with 1 grouped_reduce.
-      [(0, 2, 'G'), (1, 2, 'G')], [(0, 16, 'G'), (1, 2, 'G')], [(0, 4, 'G'), (1, 64, 'G')], # Checking how it works with 2 grouped_reduces.
-      [(0, 16, 'G'), (1, 2, 'G'), (1, 4, 'R')], [(0, 2, 'G'), (1, 32, 'G'), (1, 4, 'R')], # Checking how it works with 2 grouped_reduces + upcasts.
-      [(0, 4, 'L'), (1, 4, 'L'), (0, 8, 'G'), (1, 4, 'G')], [(0, 4, 'L'), (1, 4, 'L'), (0, 2, 'G'), (1, 32, 'G'), (1, 4, 'R')], # Checking how it works with 2 grouped_reduces + upcasts + locals.
-      [(0, 2, 'L'), (1, 2, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U')], [(0, 2, 'L'), (1, 2, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U'), (0, 4, 'R'), (1, 4, 'R')], # Checking how it works with 2 grouped_reduces + upcasts + locals.
-      [(0, 4, 'L'), (1, 4, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U'), (1, 2, 'U')], # No globals
+      [(0, 2, 'G')], [(0, 32, 'G')], [(1, 2, 'G')], [(1, 32, 'G')],  # Checking how it works with 1 grouped_reduce.
+      [(0, 2, 'G'), (1, 2, 'G')], [(0, 16, 'G'), (1, 2, 'G')], [(0, 4, 'G'), (1, 64, 'G')],  # Checking how it works with 2 grouped_reduces.
+      [(0, 16, 'G'), (1, 2, 'G'), (1, 4, 'R')], [(0, 2, 'G'), (1, 32, 'G'), (1, 4, 'R')],  # Checking how it works with 2 grouped_reduces + upcasts.
+      [(0, 4, 'L'), (1, 4, 'L'), (0, 8, 'G'), (1, 4, 'G')], [(0, 4, 'L'), (1, 4, 'L'), (0, 2, 'G'), (1, 32, 'G'), (1, 4, 'R')],
+      # Checking how it works with 2 grouped_reduces + upcasts + locals.
+      [(0, 2, 'L'), (1, 2, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U')],
+      [(0, 2, 'L'), (1, 2, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U'), (0, 4, 'R'), (1, 4, 'R')],
+      # Checking how it works with 2 grouped_reduces + upcasts + locals.
+      [(0, 4, 'L'), (1, 4, 'L'), (0, 8, 'G'), (1, 4, 'G'), (0, 2, 'U'), (1, 2, 'U')],  # No globals
     ])
 
 
--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -415,6 +415,14 @@ class Linearizer(OptimizedKernel):
       if arg == BinaryOps.ADD and vin[1].uop == UOps.ALU and vin[1].arg == UnaryOps.NEG: return self.uop(UOps.ALU, dtype, (vin[0], vin[1].vin[0]), BinaryOps.SUB, cachable=cachable)
       # constant folding
       if arg == UnaryOps.NEG and vin[0].uop == UOps.CONST: return self.const(-vin[0].arg, dtype)
+      if arg in BinaryOps and vin[0].uop == UOps.CONST and vin[0].uop == UOps.CONST:
+        if arg == BinaryOps.ADD: return self.const(vin[0].arg + vin[1].arg, dtype)
+        if arg == BinaryOps.SUB: return self.const(vin[0].arg - vin[1].arg, dtype)
+        if arg == BinaryOps.MUL: return self.const(vin[0].arg * vin[1].arg, dtype)
+        if arg == BinaryOps.DIV: return self.const(vin[0].arg / vin[1].arg, dtype)
+        if arg == BinaryOps.MAX: return self.const(max(vin[0].arg, vin[1].arg), dtype)
+        if arg == BinaryOps.CMPLT: return self.const(int(vin[0].arg < vin[1].arg), dtype)
+        if arg == BinaryOps.MOD: return self.const(vin[0].arg % vin[1].arg, dtype)
       # zero folding
       for x in [0,1]:
         if arg == BinaryOps.ADD and vin[x].uop == UOps.CONST and vin[x].arg == 0.0: return vin[1-x]
@@ -423,9 +431,9 @@ class Linearizer(OptimizedKernel):
       if arg == BinaryOps.SUB and vin[1].uop == UOps.CONST and vin[1].arg == 0.0: return vin[0]
       if arg == BinaryOps.DIV and vin[1].uop == UOps.CONST and vin[1].arg == 1.0: return vin[0]
       # equal folding
-      if arg == BinaryOps.MAX and vin[0] == vin[1]: return vin[0]
       if arg == BinaryOps.SUB and vin[0] == vin[1]: return self.const(0, dtype)
       if arg == BinaryOps.CMPLT and vin[0] == vin[1]: return self.const(0, dtype)
+      # BinaryOps.Max not needed is only used for relu. Which is handled in constant folding
 
     if cachable and key in self.saved_exprs: return self.saved_exprs[key]
     self.uops.append(UOp(uop, dtype, vin, arg, len(self.uops)))
