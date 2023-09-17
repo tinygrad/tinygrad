2eef3d3a830fb00ce4e598d1c1767fd9b2cc11f6
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-09-17 23:02:00 +0000

    ema is outputting the same results as normal

--- examples/hlb_cifar10.py
+++ examples/hlb_cifar10.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 # tinygrad implementation of https://github.com/tysam-code/hlb-CIFAR10/blob/main/main.py
 # https://myrtle.ai/learn/how-to-train-your-resnet-8-bag-of-tricks/
 # https://siboehm.com/articles/22/CUDA-MMM
-import random, time, copy
+import random, time, copy, wandb
 import numpy as np
 from extra.datasets import fetch_cifar, cifar_mean, cifar_std
 from tinygrad import nn
@@ -22,219 +22,225 @@ from extra.lr_scheduler import OneCycleLR
 from tinygrad.jit import TinyJit
 from extra.dist import collectives
 
-BS, EVAL_BS, STEPS = getenv("BS", 512), getenv('EVAL_BS', 500), getenv("STEPS", 1000)
-
-# hyper-parameters were exactly the same as the original repo
-bias_scaler = 55
-hyp = {
-  'opt': {
-    'bias_lr':            1.70 * bias_scaler/512,
-    'non_bias_lr':        1.70 / 512,
-    'bias_decay':         1.08 * 6.45e-4 * BS/bias_scaler,
-    'non_bias_decay':     1.08 * 6.45e-4 * BS,
-    'final_lr_ratio':     0.025,
-    'initial_div_factor': 1e15, 
-    'label_smoothing':    0.20,
-    'momentum':           0.85,
-    'percent_start':      0.23,
-    'scaling_factor':     1./9,
-    'loss_scale_scaler':  1./512,   # (range: ~1/512 - 16+, 1/128 w/ FP16)
-  },
-  'net': {
-      'kernel_size': 2,             # kernel size for the whitening layer
-      'batch_norm_momentum': .85,
-      'cutmix_size': 3,
-      'cutmix_steps': 599, 
-      'pad_amount': 2
-  },
-  'ema': {
-      'steps': 199,
-      'decay_base': .95,
-      'decay_pow': 2.,
-      'every_n_steps': 5,
-  }
-}
-
-# ========== Model ==========
-def whitening(X, kernel_size=hyp['net']['kernel_size']):
-  def _cov(X):
-    X = X/np.sqrt(X.shape[0] - 1)
-    return X.T @ X
-
-  def _patches(data, patch_size=(kernel_size,kernel_size)):
-    h, w = patch_size
-    c = data.shape[1]
-    return np.lib.stride_tricks.sliding_window_view(data, window_shape=(h,w), axis=(2,3)).transpose((0,3,2,1,4,5)).reshape((-1,c,h,w))
-
-  def _eigens(patches):
-    n,c,h,w = patches.shape
-    Σ = _cov(patches.reshape(n, c*h*w))
-    Λ, V = np.linalg.eigh(Σ, UPLO='U')
-    return np.flip(Λ, 0), np.flip(V.T.reshape(c*h*w, c, h, w), 0)
-
-  Λ, V = _eigens(_patches(X.numpy()))
-
-  return Tensor(V/np.sqrt(Λ+1e-2)[:,None,None,None], requires_grad=False)
-
-class BatchNorm(nn.BatchNorm2d):
-  def __init__(self, num_features):
-    super().__init__(num_features, track_running_stats=False, eps=1e-12, momentum=hyp['net']['batch_norm_momentum'], affine=True)
-    self.weight.requires_grad = False
-    self.bias.requires_grad = True
-
-class ConvGroup:
-  def __init__(self, channels_in, channels_out):
-    self.conv1 = nn.Conv2d(channels_in,  channels_out, kernel_size=3, padding=1, bias=False)
-    self.conv2 = nn.Conv2d(channels_out, channels_out, kernel_size=3, padding=1, bias=False)
-
-    self.norm1 = BatchNorm(channels_out)
-    self.norm2 = BatchNorm(channels_out)
-
-  def __call__(self, x):
-    x = self.conv1(x)
-    x = x.max_pool2d(2)
-    x = self.norm1(x)
-    x = x.gelu()
-    residual = x
-    x = self.conv2(x)
-    x = self.norm2(x)
-    x = x.gelu()
-
-    return x + residual
-
-class SpeedyResNet:
-  def __init__(self, W):
-    self.whitening = W
-    self.net = [
-      nn.Conv2d(12, 32, kernel_size=1, bias=False),
-      lambda x: x.gelu(),
-      ConvGroup(32, 64),
-      ConvGroup(64, 256),
-      ConvGroup(256, 512),
-      lambda x: x.max((2,3)),
-      nn.Linear(512, 10, bias=False),
-      lambda x: x.mul(hyp['opt']['scaling_factor'])
-    ]
-  def __call__(self, x, training=True):
-    # pad to 32x32 because whitening conv creates 31x31 images that are awfully slow to compute with
-    forward = lambda x: x.conv2d(self.whitening).pad2d((1,0,0,1)).sequential(self.net)
-    return forward(x) if training else forward(x)*0.5 + forward(x[..., ::-1])*0.5
-
-# ========== Loss ==========
-def cross_entropy(x:Tensor, y:Tensor, reduction:str='mean', label_smoothing:float=0.0) -> Tensor:
-  y = (1 - label_smoothing)*y + label_smoothing / y.shape[1]
-  if reduction=='none': return -x.log_softmax(axis=1).mul(y).sum(axis=1)
-  if reduction=='sum': return -x.log_softmax(axis=1).mul(y).sum(axis=1).sum()
-  return -x.log_softmax(axis=1).mul(y).sum(axis=1).mean()
-
-# ========== Preprocessing ==========
-# TODO currently this only works for RGB in format of NxCxHxW and pads the HxW
-# implemented in recursive fashion but figuring out how to switch indexing dim
-# during the loop was a bit tricky
-def pad_reflect(X, size=2) -> Tensor:
-  padding = ((0,0),(0,0),(size,size),(size,size))
-  p = padding[3]
-  s = X.shape[3]
-
-  X_lr = X[...,:,1:1+p[0]].flip(3).pad(((0,0),(0,0),(0,0),(0,s+p[0]))) + X[...,:,-1-p[1]:-1].flip(3).pad(((0,0),(0,0),(0,0),(s+p[1],0)))
-  X = X.pad(((0,0),(0,0),(0,0),p)) + X_lr
-
-  p = padding[2]
-  s = X.shape[2]
-  X_lr = X[...,1:1+p[0],:].flip(2).pad(((0,0),(0,0),(0,s+p[0]),(0,0))) + X[...,-1-p[1]:-1,:].flip(2).pad(((0,0),(0,0),(s+p[1],0),(0,0)))
-  X = X.pad(((0,0),(0,0),p,(0,0))) + X_lr
-
-  return X
-
-# return a binary mask in the format of BS x C x H x W where H x W contains a random square mask
-def make_square_mask(shape, mask_size):
-  is_even = int(mask_size % 2 == 0)
-  center_max = shape[-2]-mask_size//2-is_even
-  center_min = mask_size//2-is_even
-  center_x = (Tensor.rand(shape[0])*(center_max-center_min)+center_min).floor()
-  center_y = (Tensor.rand(shape[0])*(center_max-center_min)+center_min).floor()
-  d_x = Tensor.arange(0, shape[-1]).reshape((1,1,1,shape[-1])) - center_x.reshape((-1,1,1,1))
-  d_y = Tensor.arange(0, shape[-2]).reshape((1,1,shape[-2],1)) - center_y.reshape((-1,1,1,1))
-  d_x =(d_x >= -(mask_size // 2) + is_even) * (d_x <= mask_size // 2)
-  d_y =(d_y >= -(mask_size // 2) + is_even) * (d_y <= mask_size // 2)
-  mask = d_y * d_x
-  return mask
-
-def random_crop(X, crop_size=32):
-  mask = make_square_mask(X.shape, crop_size)
-  mask = mask.repeat((1,3,1,1))
-  X_cropped = Tensor(X.flatten().numpy()[mask.flatten().numpy().astype(bool)])
-
-  return X_cropped.reshape((-1, 3, crop_size, crop_size))
-
-def cutmix(X, Y, mask_size=3):
-  # fill the square with randomly selected images from the same batch
-  mask = make_square_mask(X.shape, mask_size)
-  order = list(range(0, X.shape[0]))
-  random.shuffle(order)
-  X_patch = Tensor(X.numpy()[order,...])
-  Y_patch = Tensor(Y.numpy()[order])
-  X_cutmix = Tensor.where(mask, X_patch, X)
-  mix_portion = float(mask_size**2)/(X.shape[-2]*X.shape[-1])
-  Y_cutmix = mix_portion * Y_patch + (1. - mix_portion) * Y
-  return X_cutmix, Y_cutmix
-
-# the operations that remain inside batch fetcher is the ones that involves random operations
-def fetch_batches(X_in, Y_in, BS, is_train):
-  step = 0
-  while True:
-    # set_seed(seed)
-    X, Y = X_in, Y_in
-    order = list(range(0, X.shape[0]))
-    random.shuffle(order)
-    if is_train:
-      X = random_crop(X, crop_size=32)
-      X = Tensor.where(Tensor.rand(X.shape[0],1,1,1) < 0.5, X[..., ::-1], X) # flip LR
-      if step >= hyp['net']['cutmix_steps']: X, Y = cutmix(X, Y, mask_size=hyp['net']['cutmix_size'])
-    X, Y = X.numpy(), Y.numpy()
-    for i in range(0, X.shape[0], BS):
-      # pad the last batch
-      batch_end = min(i+BS, Y.shape[0])
-      x = Tensor(X[order[batch_end-BS:batch_end],:])
-      y = Tensor(Y[order[batch_end-BS:batch_end]])
-      step += 1
-      yield x, y
-
-    if not is_train: break
-    # seed += 1
-
-transform = [
-  lambda x: x / 255.0,
-  lambda x: (x - Tensor(cifar_mean).repeat((1024,1)).T.reshape(1,-1))/ Tensor(cifar_std).repeat((1024,1)).T.reshape(1,-1),
-  lambda x: x.reshape((-1,3,32,32))
-]
-
-class modelEMA():
-  def __init__(self, w, net):
-    # self.model_ema = copy.deepcopy(net) # won't work for opencl due to unpickeable pyopencl._cl.Buffer
-    self.net_ema = SpeedyResNet(w)
-    for net_ema_param, net_param in zip(get_state_dict(self.net_ema).values(), get_state_dict(net).values()):
-      net_ema_param.requires_grad = False
-      net_ema_param.assign(net_param.detach().realize())
-
-  def __call__(self, x:Tensor, training=False):
-    return self.net_ema(x)
-
-  @TinyJit
-  def update(self, net_current, decay):
-    # TODO with Tensor.no_grad()
-    Tensor.no_grad = True
-    for net_ema_param, (param_name, net_current_param) in zip(get_state_dict(self.net_ema).values(), get_state_dict(net_current).items()):
-      # batchnorm currently is not being tracked 
-      if not ("num_batches_tracked" in param_name) and not ("running" in param_name):
-        net_ema_param.assign(net_ema_param.detach()*decay + net_current_param.detach()*(1.-decay)).realize()
-        if not ('norm' in param_name and 'weight' in param_name) and not 'whiten' in param_name:
-          net_current_param.requires_grad = False
-          net_current_param.assign(net_ema_param.detach())
-          net_current_param.requires_grad = True
-    Tensor.no_grad = False
+BS, EVAL_BS, STEPS = getenv("BS", 512), getenv('EVAL_BS', 500), getenv("STEPS", 100)
 
 def train_cifar():
+
+  # hyper-parameters were exactly the same as the original repo
+  bias_scaler = 58
+  hyp = {
+    'seed' : 1000,
+    'opt': {
+      'bias_lr':            1.76 * bias_scaler/512,
+      'non_bias_lr':        1.76 / 512,
+      'bias_decay':         1.08 * 6.45e-4 * BS/bias_scaler,
+      'non_bias_decay':     1.08 * 6.45e-4 * BS,
+      'final_lr_ratio':     0.025,
+      'initial_div_factor': 1e16, 
+      'label_smoothing':    0.20,
+      'momentum':           0.85,
+      'percent_start':      0.23,
+      'scaling_factor':     1./9,
+      'loss_scale_scaler':  1./512,   # (range: ~1/512 - 16+, 1/128 w/ FP16)
+    },
+    'net': {
+        'kernel_size': 2,             # kernel size for the whitening layer
+        'batch_norm_momentum': .85,
+        'cutmix_size': 3,
+        'cutmix_steps': 499, 
+        'pad_amount': 2
+    },
+    'ema': {
+        'steps': 199,
+        'decay_base': .95,
+        'decay_pow': 2.,
+        'every_n_steps': 5,
+    }
+  }
+
+  def set_seed(seed):
+    Tensor.manual_seed(getenv('SEED', seed))
+    random.seed(getenv('SEED', seed))
+
+  # ========== Model ==========
+  def whitening(X, kernel_size=hyp['net']['kernel_size']):
+    def _cov(X):
+      X = X/np.sqrt(X.shape[0] - 1)
+      return X.T @ X
+
+    def _patches(data, patch_size=(kernel_size,kernel_size)):
+      h, w = patch_size
+      c = data.shape[1]
+      return np.lib.stride_tricks.sliding_window_view(data, window_shape=(h,w), axis=(2,3)).transpose((0,3,2,1,4,5)).reshape((-1,c,h,w))
+
+    def _eigens(patches):
+      n,c,h,w = patches.shape
+      Σ = _cov(patches.reshape(n, c*h*w))
+      Λ, V = np.linalg.eigh(Σ, UPLO='U')
+      return np.flip(Λ, 0), np.flip(V.T.reshape(c*h*w, c, h, w), 0)
+
+    Λ, V = _eigens(_patches(X.numpy()))
+
+    return Tensor(V/np.sqrt(Λ+1e-2)[:,None,None,None], requires_grad=False)
+
+  class BatchNorm(nn.BatchNorm2d):
+    def __init__(self, num_features):
+      super().__init__(num_features, track_running_stats=False, eps=1e-12, momentum=hyp['net']['batch_norm_momentum'], affine=True)
+      self.weight.requires_grad = False
+      self.bias.requires_grad = True
+
+  class ConvGroup:
+    def __init__(self, channels_in, channels_out):
+      self.conv1 = nn.Conv2d(channels_in,  channels_out, kernel_size=3, padding=1, bias=False)
+      self.conv2 = nn.Conv2d(channels_out, channels_out, kernel_size=3, padding=1, bias=False)
+
+      self.norm1 = BatchNorm(channels_out)
+      self.norm2 = BatchNorm(channels_out)
+
+    def __call__(self, x):
+      x = self.conv1(x)
+      x = x.max_pool2d(2)
+      x = self.norm1(x)
+      x = x.gelu()
+      residual = x
+      x = self.conv2(x)
+      x = self.norm2(x)
+      x = x.gelu()
+
+      return x + residual
+
+  class SpeedyResNet:
+    def __init__(self, W):
+      self.whitening = W
+      self.net = [
+        nn.Conv2d(12, 32, kernel_size=1, bias=False),
+        lambda x: x.gelu(),
+        ConvGroup(32, 64),
+        ConvGroup(64, 256),
+        ConvGroup(256, 512),
+        lambda x: x.max((2,3)),
+        nn.Linear(512, 10, bias=False),
+        lambda x: x.mul(hyp['opt']['scaling_factor'])
+      ]
+    def __call__(self, x, training=True):
+      # pad to 32x32 because whitening conv creates 31x31 images that are awfully slow to compute with
+      forward = lambda x: x.conv2d(self.whitening).pad2d((1,0,0,1)).sequential(self.net)
+      return forward(x) if training else forward(x)*0.5 + forward(x[..., ::-1])*0.5
+
+  # ========== Loss ==========
+  def cross_entropy(x:Tensor, y:Tensor, reduction:str='mean', label_smoothing:float=0.0) -> Tensor:
+    y = (1 - label_smoothing)*y + label_smoothing / y.shape[1]
+    if reduction=='none': return -x.log_softmax(axis=1).mul(y).sum(axis=1)
+    if reduction=='sum': return -x.log_softmax(axis=1).mul(y).sum(axis=1).sum()
+    return -x.log_softmax(axis=1).mul(y).sum(axis=1).mean()
+
+  # ========== Preprocessing ==========
+  # TODO currently this only works for RGB in format of NxCxHxW and pads the HxW
+  # implemented in recursive fashion but figuring out how to switch indexing dim
+  # during the loop was a bit tricky
+  def pad_reflect(X, size=2) -> Tensor:
+    padding = ((0,0),(0,0),(size,size),(size,size))
+    p = padding[3]
+    s = X.shape[3]
+
+    X_lr = X[...,:,1:1+p[0]].flip(3).pad(((0,0),(0,0),(0,0),(0,s+p[0]))) + X[...,:,-1-p[1]:-1].flip(3).pad(((0,0),(0,0),(0,0),(s+p[1],0)))
+    X = X.pad(((0,0),(0,0),(0,0),p)) + X_lr
+
+    p = padding[2]
+    s = X.shape[2]
+    X_lr = X[...,1:1+p[0],:].flip(2).pad(((0,0),(0,0),(0,s+p[0]),(0,0))) + X[...,-1-p[1]:-1,:].flip(2).pad(((0,0),(0,0),(s+p[1],0),(0,0)))
+    X = X.pad(((0,0),(0,0),p,(0,0))) + X_lr
+
+    return X
+
+  # return a binary mask in the format of BS x C x H x W where H x W contains a random square mask
+  def make_square_mask(shape, mask_size):
+    is_even = int(mask_size % 2 == 0)
+    center_max = shape[-2]-mask_size//2-is_even
+    center_min = mask_size//2-is_even
+    center_x = (Tensor.rand(shape[0])*(center_max-center_min)+center_min).floor()
+    center_y = (Tensor.rand(shape[0])*(center_max-center_min)+center_min).floor()
+    d_x = Tensor.arange(0, shape[-1]).reshape((1,1,1,shape[-1])) - center_x.reshape((-1,1,1,1))
+    d_y = Tensor.arange(0, shape[-2]).reshape((1,1,shape[-2],1)) - center_y.reshape((-1,1,1,1))
+    d_x =(d_x >= -(mask_size // 2) + is_even) * (d_x <= mask_size // 2)
+    d_y =(d_y >= -(mask_size // 2) + is_even) * (d_y <= mask_size // 2)
+    mask = d_y * d_x
+    return mask
+
+  def random_crop(X, crop_size=32):
+    mask = make_square_mask(X.shape, crop_size)
+    mask = mask.repeat((1,3,1,1))
+    X_cropped = Tensor(X.flatten().numpy()[mask.flatten().numpy().astype(bool)])
+
+    return X_cropped.reshape((-1, 3, crop_size, crop_size))
+
+  def cutmix(X, Y, mask_size=3):
+    # fill the square with randomly selected images from the same batch
+    mask = make_square_mask(X.shape, mask_size)
+    order = list(range(0, X.shape[0]))
+    random.shuffle(order)
+    X_patch = Tensor(X.numpy()[order,...])
+    Y_patch = Tensor(Y.numpy()[order])
+    X_cutmix = Tensor.where(mask, X_patch, X)
+    mix_portion = float(mask_size**2)/(X.shape[-2]*X.shape[-1])
+    Y_cutmix = mix_portion * Y_patch + (1. - mix_portion) * Y
+    return X_cutmix, Y_cutmix
+
+  # the operations that remain inside batch fetcher is the ones that involves random operations
+  def fetch_batches(X_in, Y_in, BS, is_train):
+    step = 0
+    while True:
+      X, Y = X_in, Y_in
+      order = list(range(0, X.shape[0]))
+      random.shuffle(order)
+      if is_train:
+        X = random_crop(X, crop_size=32)
+        X = Tensor.where(Tensor.rand(X.shape[0],1,1,1) < 0.5, X[..., ::-1], X) # flip LR
+        if step >= hyp['net']['cutmix_steps']: X, Y = cutmix(X, Y, mask_size=hyp['net']['cutmix_size'])
+      X, Y = X.numpy(), Y.numpy()
+      for i in range(0, X.shape[0], BS):
+        # pad the last batch
+        batch_end = min(i+BS, Y.shape[0])
+        x = Tensor(X[order[batch_end-BS:batch_end],:])
+        y = Tensor(Y[order[batch_end-BS:batch_end]])
+        step += 1
+        yield x, y
+
+      if not is_train: break
+
+  transform = [
+    lambda x: x / 255.0,
+    lambda x: (x - Tensor(cifar_mean).repeat((1024,1)).T.reshape(1,-1))/ Tensor(cifar_std).repeat((1024,1)).T.reshape(1,-1),
+    lambda x: x.reshape((-1,3,32,32))
+  ]
+
+  class modelEMA():
+    def __init__(self, w, net):
+      # self.model_ema = copy.deepcopy(net) # won't work for opencl due to unpickeable pyopencl._cl.Buffer
+      self.net_ema = SpeedyResNet(w)
+      for net_ema_param, net_param in zip(get_state_dict(self.net_ema).values(), get_state_dict(net).values()):
+        net_ema_param.requires_grad = False
+        net_ema_param.assign(net_param.detach().realize())
+
+    def __call__(self, x:Tensor, training=False):
+      return self.net_ema(x, training=False)
+
+    @TinyJit
+    def update(self, net_current, decay):
+      # TODO with Tensor.no_grad()
+      Tensor.no_grad = True
+      for net_ema_param, (param_name, net_current_param) in zip(get_state_dict(self.net_ema).values(), get_state_dict(net_current).items()):
+        # batchnorm currently is not being tracked
+        if not ("num_batches_tracked" in param_name) and not ("running" in param_name):
+          net_ema_param.assign(net_ema_param.detach()*decay + net_current_param.detach()*(1.-decay)).realize()
+          # if not ('norm' in param_name and 'weight' in param_name) and not 'whiten' in param_name:
+          #   net_current_param.requires_grad = False
+          #   net_current_param.assign(net_ema_param.detach())
+          #   net_current_param.requires_grad = True
+      Tensor.no_grad = False
+
+  set_seed(hyp['seed'])
+
   # this import needs to be done here because this is running in a subprocess
   from extra.dist import OOB
   Tensor.training = True
@@ -307,13 +313,13 @@ def train_cifar():
       lr_scheduler[1].step()
     return loss.realize()
 
-  def eval_step_jit(model, X, Y):
+  def eval_step(model, X, Y):
     out = model(X, training=False)
     loss = cross_entropy(out, Y, reduction='mean')
     correct = out.argmax(axis=1) == Y.argmax(axis=1)
     return correct.realize(), loss.realize()
-  eval_jitted     = TinyJit(eval_step_jit)
-  eval_ema_jitted = TinyJit(eval_step_jit)
+  eval_step_jitted     = TinyJit(eval_step)
+  eval_step_ema_jitted = TinyJit(eval_step)
 
   # 97 steps in 2 seconds = 20ms / step  Tensor.training = True
 
@@ -342,13 +348,13 @@ def train_cifar():
         if getenv("DIST"):
           Xt, Yt = Xt.chunk(min(world_size, 5), 0)[min(rank, 4)], Yt.chunk(min(world_size, 5), 0)[min(rank, 4)]
 
-        correct, loss = eval_jitted(model, Xt, Yt)
+        correct, loss = eval_step_jitted(model, Xt, Yt)
         losses.append(loss.numpy().tolist())
         corrects.extend(correct.numpy().tolist())
         if model_ema:
-          correct, loss = eval_ema_jitted(model_ema, Xt, Yt)
-          losses_ema.append(loss.numpy().tolist())
-          corrects_ema.extend(correct.numpy().tolist())
+          correct_ema, loss_ema = eval_step(model_ema, Xt, Yt)
+          losses_ema.append(loss_ema.numpy().tolist())
+          corrects_ema.extend(correct_ema.numpy().tolist())
 
       # collect accuracy across ranks
       correct_sum, correct_len = sum(corrects), len(corrects)
@@ -390,9 +396,13 @@ def train_cifar():
     print(f"{i:3d} {(cl-st)*1000.0:7.2f} ms run, {(et-st)*1000.0:7.2f} ms python, {(cl-et)*1000.0:7.2f} ms CL, {loss_cpu:7.2f} loss, {opt_non_bias.lr.numpy()[0]:.6f} LR, {GlobalCounters.mem_used/1e9:.2f} GB used, {GlobalCounters.global_ops*1e-9/(cl-st):9.2f} GFLOPS")
     i += 1
 
+  return acc
+
 if __name__ == "__main__":
+
   if not getenv("DIST"):
     train_cifar()
+
   else: # distributed
     from tinygrad.runtime.ops_gpu import CL
     devices = [f"gpu:{i}" for i in range(len(CL.devices))]
