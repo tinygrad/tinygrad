457ace8a32347a4c33c55df7d1f94aa0e72d0c46
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-09-28 18:23:11 +0000

    remove the rest TODOs

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -43,7 +43,7 @@ class Tensor:
       self.prev = Tensor.training
       Tensor.training = True
     def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any):
-      Tensor.training = self.prev  
+      Tensor.training = self.prev
   no_grad: ClassVar[bool] = False
   default_type: ClassVar[DType] = dtypes.float32
   def __init__(self, data:Union[int, float, list, LazyBuffer, np.ndarray], device:Optional[str]=None, dtype:Optional[DType]=None, requires_grad:Optional[bool]=None):
