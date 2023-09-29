0a86ccfdc5f4243b3f8535156ca226a5388df922
qazal <qazal.software@gmail.com>
2023-09-29 16:13:40 +0300

    q4_0 vectorized dequantization

--- extra/gguf.py
+++ extra/gguf.py
@@ -1,13 +1,14 @@
 import enum, os, struct, io
 import numpy as np
 from typing import Any, Dict, List, Tuple
+
+from numpy.core.multiarray import where
 from tinygrad.helpers import dtypes, prod
 from tinygrad.ops import Device
 from tinygrad.tensor import Tensor
 import sys
 
 Device.DEFAULT = "GPU"
-INDEX = int(sys.argv[1])
 
 # --- Compatibility junk
 class GgmlDType(enum.Enum): F32 = 0; F16 = 1; Q4_0 = 2; Q4_1 = 3; Q5_0 = 6; Q5_1 = 7; Q8_0 = 8; Q8_1 = 9; Q2K = 10; Q3K = 11; Q4K = 12; Q5K = 13; Q6K = 14; Q8K = 15
@@ -59,17 +60,18 @@ def read_data_offset(reader: io.BufferedReader, params: Dict) -> int: # This is 
 # Vectorized version of https://github.com/ggerganov/llama.cpp/blob/468ea24fb4633a0d681f7ac84089566c1c6190cb/ggml.c#L1525
 def dequantize_q4_0_tensor(t: Tensor, n_blocks, shape):
   qs_size = 16; d_size = 2; block_size = d_size + qs_size
-  d = t.cast(dtypes.float16).to("GPU").reshape(n_blocks, block_size).reshape(-1, 9)[:, :d_size-1]
+  d = t.cast(dtypes.float16).to("GPU")[::9][:n_blocks].reshape(-1, 1)
   qs = t.cast(dtypes.uint8).to("GPU").reshape(n_blocks, block_size)[:, d_size:]
-  print(d.numpy()[INDEX], qs.numpy()[INDEX])
-  """
   x0 = qs - (qs / 16).floor() * 16 - 8 # qs & 0x0F - 8
   x1 = (qs / 16).floor() - 8 # (qs >> 4) - 8
   ret =  Tensor.stack([x0 * d, x1 * d]).transpose(0, 1).reshape(shape)
   return ret
-  """
+
+def dequantize_q6k_tensor(t: Tensor, n_blocks, shape):
+  pass
 
 def tinygrad_tensor_from_gguf(disk_tensor: Tensor, name: str, shape: Tuple, ggml_dtype: GgmlDType, offset: int, data_offset: int) -> Tensor:
+  print(name, ggml_dtype)
   itemsize, block_size = ggml_sizes[ggml_dtype]
   size_in_bytes = int(prod(shape) * itemsize // block_size)
   init_offset = data_offset + offset
@@ -77,6 +79,7 @@ def tinygrad_tensor_from_gguf(disk_tensor: Tensor, name: str, shape: Tuple, ggml
   fxn_for_dtype = {
       GgmlDType.F32: lambda ret: ret[:n_blocks].cast(dtypes.float32),
       GgmlDType.Q4_0: lambda ret: dequantize_q4_0_tensor(ret, n_blocks, shape),
+      GgmlDType.Q6K: lambda ret: dequantize_q6k_tensor(ret, n_blocks, shape),
   }
   return fxn_for_dtype[ggml_dtype](disk_tensor[init_offset:init_offset+size_in_bytes])
 
@@ -90,11 +93,7 @@ def gguf_load(fn:str):
     params = {k: v for k, v in [read_model_params(f) for _ in range(param_count)]}
     tensor_info = [read_tensor_info(f) for _ in range(tensor_count)]
     data_offset = read_data_offset(f, params)
-    # return [tinygrad_tensor_from_gguf(t, *info, data_offset) for info in tensor_info], params TODO uncomment once dequantize is fast
-    tinygrad_tensor_from_gguf(t, *tensor_info[0], data_offset)
-    """
-    np_tensor = tensor.numpy()
-    flat = np_tensor.flatten()
-    idxs = np.argwhere(np.isnan(flat))
-    print(flat[INDEX])
-    """
+    # return [tinygrad_tensor_from_gguf(t, *info, data_offset) for info in tensor_info], params
+    name = "output.weight"
+    info = [i for i in tensor_info if i[0] == name][0]
+    tinygrad_tensor_from_gguf(t, *info, data_offset)
