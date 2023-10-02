511caf00beb1aeb523f9dbac586e037e35145800
madt2709 <theodore.dias@hotmail.co.uk>
2023-10-02 14:44:12 -0700

    Actually make it readable

--- tinygrad/shape/view.py
+++ tinygrad/shape/view.py
@@ -96,12 +96,16 @@ class View:
     # after the asserts, it's okay to check contiguous
     if self.contiguous: return View.create(new_shape)
 
-    # check if this is adding or removing 1s with [x for x in self.shape if x != 1] == [x for x in new_shape if x != 1].
+    # check if this is adding or removing 1s
     # extend the above to include cases like (3,4) --> (3,i) where we add or remove 1s and add/remove a "dummy" variable.
     # NOTE: this is optional, but removes most calls to (expensive!) merge_views (with mask, not optional)
-    diff_between_shapes = [(a,b) for a,b in zip_longest([x for x in self.shape if x != 1],[x for x in new_shape if x != 1]) if a != b]
-    variable_added_or_removed = (len(diff_between_shapes) == 1) and any(isinstance(x, Node) for x in diff_between_shapes[0])
-    if ([x for x in self.shape if x != 1] == [x for x in new_shape if x != 1]) or variable_added_or_removed:
+    filtered_shape = [x for x in self.shape if x != 1]
+    filtered_new_shape = [x for x in new_shape if x != 1]
+    # if all the values in both shapes are ints, the number of differences between shapes must be even. 
+    # this is due to the product of shapes being equal.
+    # therefore when we only get 1 diff, we must be in a symbolic case where a variable has either been added, removed or replaced.
+    diff_count_filtered_shapes = sum([1 for a,b in zip_longest(filtered_shape, filtered_new_shape) if a != b])
+    if (filtered_shape == filtered_new_shape) or (diff_count_filtered_shapes == 1):
       new_strides: List[sint] = [y for x,y in zip(self.shape, self.strides) if x != 1]
       new_strides_tuple: Tuple[sint, ...] = tuple([0 if x == 1 else new_strides.pop(0) for x in new_shape])
       new_mask_tuple: Optional[Tuple[Tuple[sint, sint], ...]] = None
