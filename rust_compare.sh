b7d5d91285023721da44237be38c899b97ecbb03
qazal <qazal.software@gmail.com>
2023-10-02 13:13:57 +0300

    remove compare script

--- rust_compare.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/env bash
-
-echo "Inspecting tensor at index $1"
-echo "🦀 ========== "
-cd ~/gg-rs && cargo run $1
-echo "🐍 ========== "
-cd ~/tinygrad && source venv/bin/activate && python gg.py $1
