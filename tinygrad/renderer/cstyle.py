52f96c61759c10c89ba4729f41b25dfcb97db85c
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2023-10-03 16:57:06 +0200

    User render_cast intead of introducing new method

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -40,6 +40,7 @@ class CStyleLanguage(NamedTuple):
 
   # returns a str expression of the casted xs with the given type
   def render_cast(self, x:List[str], var_dtype:DType) -> str:
+    if len(x) == 1: return f"({var_dtype.name})({x[0]})"
     assert len(x) == var_dtype.sz, f"cast is wrong size {len(x)} != {var_dtype.sz}"
     assert self.float4 is not None, "cast is not supported on this platform"
     if var_dtype == dtypes._float4: return f"{self.float4}({','.join(x)})"
@@ -47,9 +48,6 @@ class CStyleLanguage(NamedTuple):
     if var_dtype == dtypes._int2: return f"{self.float4.replace('float4', 'int2')}({','.join(x)})"
     raise NotImplementedError(f"no cast for {var_dtype}")
 
-  def render_cast_scalar(self, x:str, var_dtype:DType) -> str:
-    return f"({var_dtype.name})({x})"
-  
   # returns a str expression of the const with the given type
   def render_const(self, x:Union[float,int], var_dtype) -> str:
     if math.isnan(x): val = "NAN"
@@ -71,7 +69,7 @@ class CStyleLanguage(NamedTuple):
     else:
       out_val = f"*({buf_name}+{idx})" if self.uses_ptr_arithmetic else f"{buf_name}[{idx}]"
 
-    return self.render_cast_scalar(out_val, output_dtype) if output_dtype != buf_dtype else out_val
+    return self.render_cast([out_val], output_dtype) if output_dtype != buf_dtype else out_val
 
   def render_local(self, name:str, size:int):
     return self.smem_prefix + f"float {name}[{size}];"
