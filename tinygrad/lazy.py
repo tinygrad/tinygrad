debec0b7b758247b93fee0c5ca44ebfc0f5e752e
sahamrit <amritsahu.iitkgp@gmail.com>
2023-10-08 09:54:02 +0530

    allow push of intermediate movement ops

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -255,7 +255,7 @@ class LazyBuffer:
   # *** movement ops ***
 
   def _movement_op(self, st: ShapeTracker, op: MovementOps, arg: Union[Tuple[sint, ...], Tuple[Tuple[sint, sint], ...]]) -> LazyBuffer:
-    if SHUFFLE_MOVEMENT_OPS and not self.realized and self.optype == BinaryOps and not self.children and op is not MovementOps.EXPAND and (op is not MovementOps.PAD or all(x.op not in UNSAFE_PAD_OPS for x in self.op.get_lazyops())) and not all(x.realized or len(x.op.src) == 0 for x in self.op.buffers):
+    if SHUFFLE_MOVEMENT_OPS and not self.realized and self.optype == BinaryOps and not self.children and op is not MovementOps.EXPAND and (op is not MovementOps.PAD or (SHUFFLE_PAD_OPS and all(x.op not in UNSAFE_PAD_OPS for x in self.op.get_lazyops()))) and not all(x.realized or len(x.op.src) == 0 for x in self.op.buffers):
       return self.op.replace_with_movement_ops([(op, arg)])
     if REMOVE_MOVEMENT_NOPS and not self.realized and st.contiguous:
       # MovementOps aren't stacked any more, they each have one parent, find the root
@@ -336,7 +336,7 @@ def _push_movement_ops(srcs:Tuple[LazyBuffer, ...]) -> Tuple[LazyBuffer, ...]:
     mops: List[Tuple[MovementOps, Any]] = []
     bx = x
     # backwalk all the movement ops. don't push PAD or EXPAND
-    while not bx.realized and bx.optype is MovementOps and bx.op.op is not MovementOps.EXPAND and (SHUFFLE_PAD_OPS or bx.op.op is not MovementOps.PAD) and len(bx.children) <= 1 and not all(y.realized or len(y.op.src) == 0 for y in bx.op.buffers):
+    while not bx.realized and bx.optype is MovementOps and bx.op.op is not MovementOps.EXPAND and (SHUFFLE_PAD_OPS or bx.op.op is not MovementOps.PAD) and len(bx.children) <= 1:
       assert isinstance(bx.op.op, MovementOps)
       mops.append((bx.op.op, bx.op.arg))
       assert isinstance(bx.op.src[0], LazyBuffer)
