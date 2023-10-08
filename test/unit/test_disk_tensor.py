a2d8c3cc6892d12b3d01f3292b796f9155dd8412
qazal <qazal.software@gmail.com>
2023-10-08 20:45:47 +0300

    testing with the test_ops pattern

--- test/unit/test_disk_tensor.py
+++ test/unit/test_disk_tensor.py
@@ -105,6 +105,12 @@ class TestSafetensors(unittest.TestCase):
     import json
     assert json.loads(dat[8:8+sz])['__metadata__']['hello'] == 'world'
 
+def helper_test_disk_tensor(fn, data, np_fxn, tinygrad_fxn):
+  pathlib.Path(temp(fn)).unlink(missing_ok=True)
+  tinygrad_tensor = Tensor(data, device="CPU").to(f"disk:{temp(fn)}")
+  numpy_arr = np.array(data)
+  np.testing.assert_allclose(tinygrad_fxn(tinygrad_tensor).numpy(), np_fxn(numpy_arr))
+
 class TestDiskTensor(unittest.TestCase):
   def test_empty(self):
     pathlib.Path(temp("dt1")).unlink(missing_ok=True)
@@ -128,22 +134,6 @@ class TestDiskTensor(unittest.TestCase):
     out = reloaded.numpy()
     assert np.all(out == 1.)
 
-  def test_slice(self):
-    pathlib.Path(temp("dt3")).unlink(missing_ok=True)
-    Tensor.arange(10, device="CPU").to(f"disk:{temp('dt3')}").realize()
-
-    slice_me = Tensor.empty(10, device=f"disk:{temp('dt3')}")
-    print(slice_me)
-    is_3 = slice_me[3:4].cpu()
-    assert is_3.numpy()[0] == 3
-
-  def test_slice_2d(self):
-    pathlib.Path(temp("dt5")).unlink(missing_ok=True)
-    Tensor.arange(100, device="CPU").to(f"disk:{temp('dt5')}").realize()
-    slice_me = Tensor.empty(10, 10, device=f"disk:{temp('dt5')}")
-    tst = slice_me[1].numpy()
-    print(tst)
-    np.testing.assert_allclose(tst, np.arange(10, 20))
 
   def test_assign_slice(self):
     pathlib.Path(temp("dt4")).unlink(missing_ok=True)
@@ -154,6 +144,13 @@ class TestDiskTensor(unittest.TestCase):
     cc[3:5].assign([13, 12]).realize()
     print(cc.numpy())
 
+  def test_reshape(self):
+    helper_test_disk_tensor("dt6", [1,2,3,4,5], lambda x: x.reshape((1,5)), lambda x: x.reshape((1,5)))
+    helper_test_disk_tensor("dt6", [1,2,3,4], lambda x: x.reshape((2,2)), lambda x: x.reshape((2,2)))
+
+  def test_permute(self):
+    helper_test_disk_tensor("dt7", [[1,2,3,4,5],[6,7,8,9,10]], lambda x: np.transpose(x, (0,1)), lambda x: x.permute(0,1))
+    # TODO permute 2D broken helper_test_disk_tensor("dt7", [[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]], lambda x: np.transpose(x, (0,2,1)), lambda x: x.permute(0,2,1))
+
 if __name__ == "__main__":
   unittest.main()
-
