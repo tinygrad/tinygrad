3c68d6f2e9ac0273d25bfdcb274ae7500d5343ca
George Hotz <geohot@gmail.com>
2023-10-08 08:58:47 -0700

    needs inf

--- extra/optimization/go.py
+++ extra/optimization/go.py
@@ -8,6 +8,7 @@ from tinygrad.ops import LazyOp, TernaryOps, BinaryOps, UnaryOps, ReduceOps, Buf
 from tinygrad.helpers import dtypes
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.view import View
+inf = float('inf')
 
 if __name__ == "__main__":
   asts = [eval(x) for x in dedup(open(sys.argv[1]).read().strip().split("\n"))]
@@ -16,6 +17,7 @@ if __name__ == "__main__":
   for ast in tqdm(asts):
     lin = Linearizer(ast)
     lin.hand_coded_optimizations()
-    print(lin.colored_shape())
+    #print(lin.colored_shape())
     lin.linearize()
+    #for u in lin.uops: print(u)
 
