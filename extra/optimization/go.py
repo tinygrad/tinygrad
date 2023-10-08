44fb956204bd05addb0aea649f9906e77324371b
George Hotz <geohot@gmail.com>
2023-10-08 07:37:11 -0700

    fix dtype printing

---
+++ extra/optimization/go.py
@@ -0,0 +1,21 @@
+import sys
+from tqdm import tqdm
+from tinygrad.helpers import dedup
+from tinygrad.codegen.linearizer import Linearizer
+
+# stuff needed to unpack a kernel
+from tinygrad.ops import LazyOp, TernaryOps, BinaryOps, UnaryOps, ReduceOps, BufferOps, MemBuffer, ConstBuffer
+from tinygrad.helpers import dtypes
+from tinygrad.shape.shapetracker import ShapeTracker
+from tinygrad.shape.view import View
+
+if __name__ == "__main__":
+  asts = [eval(x) for x in dedup(open(sys.argv[1]).read().strip().split("\n"))]
+  print(f"loaded {len(asts)} kernels")
+
+  for ast in tqdm(asts):
+    lin = Linearizer(ast)
+    lin.hand_coded_optimizations()
+    print(lin.colored_shape())
+    lin.linearize()
+
