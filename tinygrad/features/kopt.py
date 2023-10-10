ca30c1a7043986b7bea3870a859d4cc1e80b2fe8
David Hou <david.hou314@gmail.com>
2023-10-09 17:05:43 -0700

    Output KOPT config from hand_coded

--- tinygrad/features/kopt.py
+++ tinygrad/features/kopt.py
@@ -4,25 +4,32 @@ from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.helpers import DEBUG, prod, getenv
 from tinygrad.lazy import var_vals_from_ast
 
-def get_divisors(n, min_div = 1, max_div = 512):
+def get_divisors(n, min_div = 1, max_div = 512, extra=None):
   if min_div > 1: yield 1
-  for d in range(min_div, min(max_div, n//2) + 1):
+  if extra is not None and extra < min_div and extra != 1 and n % extra == 0: yield extra
+  for d in range(min_div, max_div + 1):
     if n % d == 0: yield d
+  if extra is not None and extra > max_div and extra != 1 and n % extra == 0: yield extra
 
-def kernel_optimize_opts(k:Linearizer):
+def kernel_optimize_opts(k:Linearizer, suggestion):
   import nevergrad as ng
+  if suggestion is None: suggestion = {}
   opts = []
   for i in range(k.first_reduce):
     # TODO: the upcast always happen first, you might want to reverse this?
     # TODO: the order of the locals might improve things too
-    opts.append(ng.p.TransitionChoice([(i,s,"U") for s in get_divisors(k.full_shape[i], max_div=8)]))
-    opts.append(ng.p.TransitionChoice([(i,s,"L") for s in get_divisors(k.full_shape[i], min_div=4)]))
-  for i in range(k.shape_len-k.first_reduce):
-    opts.append(ng.p.TransitionChoice([(i,s,"R") for s in get_divisors(k.full_shape[k.first_reduce+i], max_div=8)]))
-    opts.append(ng.p.TransitionChoice([(i,s,"G") for s in get_divisors(k.full_shape[k.first_reduce+i], min_div=4) if all(st.shape[k.first_reduce+i] % s == 0 or st.shape[k.first_reduce+i] == 1 for st in k.sts)]))
-  return opts
+    opts.append([(i,s,"U") for s in get_divisors(k.full_shape[i], max_div=8, extra=suggestion.get(("U", i)))])
+    opts.append([(i,s,"L") for s in get_divisors(k.full_shape[i], min_div=4, extra=suggestion.get(("L", i)))])
+  for i in range(k.first_reduce, k.shape_len):
+    opts.append([(i,s,"R") for s in get_divisors(k.full_shape[i], max_div=8, extra=suggestion.get(("R", i)))])
+    opts.append([(i,s,"G") for s in get_divisors(k.full_shape[i], min_div=4, extra=suggestion.get(("G", i))) if all(st.shape[i] % s == 0 or st.shape[i] == 1 for st in k.sts)])
+  # nevergrad parameters, default parameter choice
+  return [ng.p.TransitionChoice(opt) for opt in opts], [opt[0] for opt in opts]
 
-def kernel_optimize_search(k:Linearizer, create_k:Callable[[], Linearizer], to_prg, baseline, bufs, var_vals):
+def normalize_suggestion(default_opt, suggestion):
+  return tuple([(i, s, typ) for (typ, i), s in ({(typ, i): s for i,s,typ in default_opt} | suggestion).items()])  # maintain ordering from default_opt
+
+def kernel_optimize_search(k:Linearizer, create_k:Callable[[], Linearizer], to_prg, baseline, bufs, var_vals, suggestion):
   import nevergrad as ng
   def opt(x):
     try:
@@ -38,12 +45,13 @@ def kernel_optimize_search(k:Linearizer, create_k:Callable[[], Linearizer], to_p
         import traceback
         traceback.print_exc()
       return 10000_000   # 10000 seconds is infinity
-  opts = kernel_optimize_opts(k)
+  opts, default_opt = kernel_optimize_opts(k, suggestion)
   if not opts: return "BASELINE"
   search_space = prod([len(x.choices) for x in opts])
   st = time.perf_counter()
   budget = getenv("BUDGET", 200)
   optimizer = ng.optimizers.NGOpt(parametrization=ng.p.Tuple(*opts), budget=min(search_space, budget))
+  if suggestion is not None: optimizer.suggest(normalize_suggestion(default_opt, suggestion))
   recommendation = optimizer.minimize(opt)
   et = time.perf_counter() - st
   if DEBUG >= 1: print(f"optimizer({et:6.2f} s to search) space {search_space:8d} with tm {recommendation.loss:5.2f} ms vs baseline {baseline:5.2f} ms, a {baseline/recommendation.loss:5.2f}x gain : {k.colored_shape()}")
@@ -70,10 +78,11 @@ def kernel_optimize(k:Linearizer, create_k:Callable[[], Linearizer], to_prg, buf
     # get baseline
     def get_baseline():
       k = create_k()
-      k.hand_coded_optimizations()
+      suggestion = k.hand_coded_optimizations()
       prg = to_prg(k)
-      return min([prg.exec(bufs, var_vals, force_wait=True, optimizing=True) for _ in range(5)])*1000
-    choice = kernel_optimize_search(k, create_k, to_prg, get_baseline(), bufs, var_vals)
+      return min([prg.exec(bufs, var_vals, force_wait=True, optimizing=True) for _ in range(5)])*1000, suggestion
+    baseline, suggestion = get_baseline()
+    choice = kernel_optimize_search(k, create_k, to_prg, baseline, bufs, var_vals, suggestion)
     if global_db is not None:
       global_db[skey] = choice
       global_db.sync()
