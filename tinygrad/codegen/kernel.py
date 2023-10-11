9d4b6e17fd1b9e6ec4bbff603223cf298cbf6a4b
Francis Lam <flam@alum.mit.edu>
2023-10-11 14:05:27 -0700

    wmma: refactor tensor core

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -27,6 +27,7 @@ tensor_cores: Dict[str, List[TensorCore]] = {
   ],
   "HIP": [
     TensorCore(device="HIP", dims=[16,16,16], dtype_in=dtypes.half, dtype_out=dtypes.float, threads=[16,2], thread_local_sizes=[16,16,8], thread_local_aliases=[ [[-1], [0], [1]], [[-1], [1], [0]], [[0], [1], [2, -1]] ]),
+    TensorCore(device="HIP", dims=[16,16,16], dtype_in=dtypes.half, dtype_out=dtypes.half,  threads=[16,2], thread_local_sizes=[16,16,8], thread_local_aliases=[ [[-1], [0], [1]], [[-1], [1], [0]], [[0], [1], [2, -1]] ]),
   ]
 }
 
@@ -76,9 +77,7 @@ class Kernel:
     self.upcasted: int = 0
     self.local_dims: int = 0
     self.local_alias: Dict[int, LocalBuffer] = {}
-    self.use_tensor_cores: bool = False
-    self.exclude_local_upcast: int = 0
-    self.reverse_upcast_dir: bool = False
+    self.tensor_core: Optional[TensorCore] = None
 
     self.global_size: Optional[List[int]] = None
     self.local_size: Optional[List[int]] = None
@@ -139,6 +138,7 @@ class Kernel:
   # white  -- reduce-late upcasted dim (self.upcast_in_mid_reduce_axes)
   # red    -- reduce loops
   #  *** self.upcasted
+  # cyan   -- WWMA
   # purple -- reduce upcasted
   # yellow -- normal upcasted dimensions
   def colors(self) -> List[str]:
@@ -150,8 +150,8 @@ class Kernel:
     colors += ["white" if i in self.upcast_in_mid_reduce_axes else "green" for i in range(self.first_reduce, self.first_reduce + len(self.group_for_reduce))]
     # between first_reduce + group_for_reduce and upcasted, they are reduce (red)
     colors += ["red"] * ((self.shape_len-self.upcasted) - (self.first_reduce + len(self.group_for_reduce)))
-    # upcasted dimensions are reduce (magenta) or normal (yellow)
-    colors += ["magenta" if self.full_shape[i] != self.sts[0].shape[i] else "yellow" for i in range(self.shape_len-self.upcasted, self.shape_len)]
+    # upcasted dimensions are cyan (WMMA), reduce (magenta) or normal (yellow)
+    colors += ["cyan"] * (3 if self.tensor_core else 0) + ["magenta" if self.full_shape[i] != self.sts[0].shape[i] else "yellow" for i in range(self.shape_len-self.upcasted+(3 if self.tensor_core else 0), self.shape_len)]
     assert len(colors) == self.shape_len, "colors size mismatch"
     return colors
 
