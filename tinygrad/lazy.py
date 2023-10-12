59db795f2471fd18f89024d7e623c09bbb769632
sahamrit <amritsahu.iitkgp@gmail.com>
2023-10-13 03:23:02 +0530

    disable PAD solves convnext kernel count increase

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -260,11 +260,9 @@ class LazyBuffer:
     if SHUFFLE_MOVEMENT_OPS and not self.realized and self.optype == BinaryOps and not self.children:
       base_bufs = (x.base for x in self.op.buffers)
       # don't push if all ast buffers (.base) are realized or sourceless
-      if not all(x.realized or len(x.op.src) == 0 for x in base_bufs):
-        push_pad_safe = (SHUFFLE_PAD_OPS and all(x.op not in UNSAFE_PAD_OPS for x in self.op.get_lazyops()))
-        push_reshape_safe = (self.op.op in UnaryOps) or any(isinstance(x, LazyOp) or not x.children for x in self.op.src)
-        if (op is not MovementOps.EXPAND) and (op is not MovementOps.PAD or push_pad_safe) and (op is not MovementOps.RESHAPE or push_reshape_safe):
-          return self.op.replace_with_movement_ops([(op, arg)])
+      push_reshape_safe = (self.op.op in UnaryOps) or (any(isinstance(x, LazyOp) or not x.children for x in self.op.src) and not all(x.realized or len(x.op.src) == 0 for x in base_bufs))
+      if (op is not MovementOps.EXPAND) and (op is not MovementOps.PAD) and (op is not MovementOps.RESHAPE or push_reshape_safe):
+        return self.op.replace_with_movement_ops([(op, arg)])
     if REMOVE_MOVEMENT_NOPS and not self.realized and st.contiguous:
       # MovementOps aren't stacked any more, they each have one parent, find the root
       root = get_movementroot(self)
