0905dc81e35f3fde35c789cd657891cb8797d748
root <20CS021@charusat.edu.in>
2023-10-12 18:30:24 +0000

    get the list of all function calls that lead to the current function from the stack

---
+++ tinygrad/get_function_stack.py
@@ -0,0 +1,5 @@
+import inspect
+
+# get the list of all function calls that lead to the current function from the stack
+def generate_stack():
+  for i in list(reversed(inspect.stack())): print(f"Function = {i.function} called from {i.filename} at line number ={i.lineno}")
