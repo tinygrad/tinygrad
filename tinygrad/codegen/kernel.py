409edd44ab3175a0a46abd8e82cbd720c63084af
George Hotz <geohot@gmail.com>
2023-10-13 07:45:12 -0700

    expose exclude_local_upcast

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -144,8 +144,10 @@ class Kernel:
   def colors(self) -> List[str]:
     # up to first_reduce, they are all global (blue)
     colors = ["blue"] * self.global_dims
+    # some special local_dims are excluded from the local upcast
+    colors += ["CYAN"] * self.exclude_local_upcast
     # except the local_dims, these are non-reduce locals (cyan)
-    colors += ["cyan"] * self.local_dims
+    colors += ["cyan"] * (self.local_dims - self.exclude_local_upcast)
     # between first_reduce and first_reduce + group_for_reduce, they are either local (cyan), or late upcasted (green)
     colors += ["white" if i in self.upcast_in_mid_reduce_axes else "green" for i in range(self.first_reduce, self.first_reduce + len(self.group_for_reduce))]
     # between first_reduce + group_for_reduce and upcasted, they are reduce (red)
