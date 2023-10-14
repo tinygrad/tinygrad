91168a28c42dc99af72f56de8ca387841a771956
mmmkkaaayy <18418787+mmmkkaaayy@users.noreply.github.com>
2023-10-13 17:13:35 -0700

    whisper: make file transcription work, add basic CI test (#2042)

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(name='tinygrad',
             "nevergrad",
             "sentencepiece",
             "tiktoken",
+            "librosa"
         ],
       },
       include_package_data=True)
