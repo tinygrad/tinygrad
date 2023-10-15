1d0bc352f6c579cdca4661de5dcbcf5e02900e04
ZenginU <umutzengin00@gmail.com>
2023-10-15 05:04:09 +0300

    revert

--- tinygrad/shape/symbolic.py
+++ tinygrad/shape/symbolic.py
@@ -295,7 +295,8 @@ class SumNode(RedNode):
         # NOTE: gcd in python 3.8 takes exactly 2 args
         mul_gcd = b
         for x in muls: mul_gcd = gcd(mul_gcd, x.b)  # type: ignore  # mypy cannot tell x.b is int here
-        if mul_gcd != 1 and Variable.sum(others) // mul_gcd == 0:
+        all_others = Variable.sum(others)
+        if mul_gcd != 1 and all_others.min >= 0 and all_others.max < mul_gcd:
           lhs, b = Variable.sum([mul//mul_gcd for mul in muls]), b//mul_gcd
     return Node.__lt__(lhs, b)
 
