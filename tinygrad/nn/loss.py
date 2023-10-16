0d915d58f80af892c8e6cd35573e5d7b4c353cd0
hi-sushanta <iamchi@skiff.com>
2023-10-16 06:56:15 +0530

    I have added a new loss file, which currently only contains a simple MSE loss function

---
+++ tinygrad/nn/loss.py
@@ -0,0 +1,18 @@
+import tinygrad as tg
+from tinygrad.tensor import Tensor
+from typing import List
+
+def mse_loss(y_pred:List[Tensor], y_true:List[Tensor]):
+  """
+  Computes the mean squared error between two tensors.
+
+  Args:
+    y_pred (`Tensor`): A tensor of predicted values.
+    y_true (`Tensor`): A tensor of ground truth values.
+
+  Returns:
+    A tensor of the mean squared error between the two tensors.
+  """
+
+  loss = (y_pred - y_true)**2
+  return loss.mean()
