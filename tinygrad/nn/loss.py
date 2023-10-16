68110d36b3124c7e03e8502978049824aff47ebf
Chi <iamchi@skiff.com>
2023-10-16 20:07:19 +0530

    Update loss.py
    Remove unnecessary comments and add mae loss function.

--- tinygrad/nn/loss.py
+++ tinygrad/nn/loss.py
@@ -2,16 +2,9 @@ from tinygrad.tensor import Tensor
 from typing import List
 
 def mse_loss(y_pred:List[Tensor], y_true:List[Tensor]):
-  """
-  Computes the mean squared error between two tensors.
-
-  Args:
-    y_pred (`Tensor`): A tensor of predicted values.
-    y_true (`Tensor`): A tensor of ground truth values.
-
-  Returns:
-    A tensor of the mean squared error between the two tensors.
-  """
-
   loss = (y_pred - y_true)**2
+  return loss.mean()
+
+def mae_loss(y_pred:List[Tensor], y_true:List[Tensor]):
+  loss = (y_pred - y_true).abs()
   return loss.mean()
