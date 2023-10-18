95e17ff0d4bc5d4ed8299410292d84f18b2edd5c
David Hou <david.hou314@gmail.com>
2023-10-18 16:54:48 -0700

    fix wino mask upcast calculation (#2057)
    * fix wino mask upcast calculation
    
    * add tests for wino upcast hcopt
    
    * add info to note
    
    * real world wino hcopt test
    
    * wino backward test
    
    * whitespace

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -254,6 +254,73 @@ class TestFloat4(unittest.TestCase):
 
     assert TestFloat4.count_float4(k) == (1, 1)
 
+class TestHandCodedOpts(unittest.TestCase):
+  def setUp(self):
+    if not isinstance(Device[Device.DEFAULT], Compiled):
+      self.skipTest("Device does not use linearizer")
+
+  def test_masked_upcast(self):
+    layer_1 = Tensor.cat(*[Tensor.rand(5) for _ in range(4)])
+    layer_2 = Tensor.cat(layer_1.unsqueeze(0), Tensor.rand(6, 20))
+
+    s = layer_2.lazydata.schedule()[-1]
+    k = Linearizer(s.ast)
+    k.hand_coded_optimizations()
+    assert len(k.bufs) == 6  # make sure all ops are done in one kernel
+    # masked upcast should upcast masked axis of size 7
+    # masked upcast should not upcast large (20) last axis
+    # float4/other hcopt shouldn't upcast last axis, since we already have 7 upcast, and the last axis is not very contiguous
+    assert k.upcasted == 1 and k.full_shape[-1] == 7
+
+  def test_masked_upcast_wino(self):
+    monster = Tensor.stack([Tensor.stack([Tensor.rand(16) for _ in range(6)]) for _ in range(6)])
+
+    s = monster.lazydata.schedule()[-1]
+    k = Linearizer(s.ast)
+    k.hand_coded_optimizations()
+    assert len(k.bufs) == 37  # make sure all ops are done in one kernel
+    # should upcast the two Tensor.stacks
+    assert k.upcasted >= 2 and k.full_shape[k.shape_len-k.upcasted:k.shape_len].count(6) == 2
+
+  def test_masked_upcast_wino_full(self):
+    old_wino = Tensor.wino
+    Tensor.wino = True
+    x,w = Tensor.rand(1,4,9,9, requires_grad=True).realize(), Tensor.rand(4,4,3,3, requires_grad=True).realize()
+    out = Tensor.conv2d(x,w, padding=1)
+    upcasts = []
+    # collect upcasts of tile transform kernels
+    for i, si in enumerate(out.lazydata.schedule()):
+      k = Linearizer(si.ast)
+      k.hand_coded_optimizations()
+      if k.reduceop is not None: continue  # not a tile transform kernel (there is a gemm reduce kernel)
+      if len(k.bufs) < 100: continue  # not a tile transform kernel (there's a permute kernel at the end)
+      upcasts.append(tuple(k.full_shape[k.shape_len - k.upcasted:k.shape_len]))
+    assert len(upcasts) == 3  # 3 transformation matrices
+    assert upcasts.count((6, 6)) == 2 and upcasts.count((4, 4)) == 1
+
+    out.mean().backward()
+    for si in x.grad.lazydata.schedule() + w.grad.lazydata.schedule():
+      k = Linearizer(si.ast)
+      k.hand_coded_optimizations()
+      k.linearize()
+      if len(k.bufs) < 20: continue  # not a tile transform kernel
+      # heuristic number to make sure that at least some upcasts but not too many upcasts are being done
+      assert 6 <= prod(k.full_shape[k.shape_len - k.upcasted:k.shape_len]) <= 49
+
+    Tensor.wino = old_wino
+
+  def test_masked_upcast_many(self):
+    layer_1 = Tensor.cat(Tensor.rand(3, 4), Tensor.rand(4, 4))
+    layer_2 = Tensor.cat(layer_1.unsqueeze(0), Tensor.rand(6, 7, 4))
+    layer_3 = Tensor.cat(layer_2.unsqueeze(0), Tensor.rand(6, 7, 7, 4))
+
+    s = layer_3.lazydata.schedule()[-1]
+    k = Linearizer(s.ast)
+    k.hand_coded_optimizations()
+    assert len(k.bufs) == 5  # make sure all ops are done in one kernel
+    # check that we don't do too many upcasts
+    assert prod(k.full_shape[k.shape_len-k.upcasted:k.shape_len]) <= 49
+
 
 if __name__ == '__main__':
   unittest.main()
