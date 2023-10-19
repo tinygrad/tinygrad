8eed617ceb1362e97889e8fef000182bcf08d700
qazal <qazal.software@gmail.com>
2023-10-18 22:56:44 -0400

    dont use device.default in decorator

--- test/test_search.py
+++ test/test_search.py
@@ -5,8 +5,10 @@ from tinygrad.features.search import time_linearizer
 from tinygrad.ops import Compiled, Device, LoadOps
 from tinygrad.tensor import Tensor
 
-@unittest.skipIf(not isinstance(Device[Device.DEFAULT], Compiled), "only test for compiled backends")
 class TestTimeLinearizer(unittest.TestCase):
+  def setUp(self) -> None:
+    if not isinstance(Device[Device.DEFAULT], Compiled): raise unittest.SkipTest("only test for compiled backends")
+
   def test_reasonable_time(self):
     si = [si for si in Tensor([1,2,3,4]).add(1).lazydata.schedule() if si.ast.op not in LoadOps][0]
     rawbufs = [Device[Device.DEFAULT].buffer(si.out.st.size(), si.out.dtype)] + [Device[Device.DEFAULT].buffer(x.st.size(), x.dtype) for x in si.inputs]
