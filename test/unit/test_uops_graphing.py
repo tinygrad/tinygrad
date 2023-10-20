253e97120046e6ea2ae68e860f1806930f8dba2e
George Hotz <geohot@gmail.com>
2023-10-19 21:29:50 -0700

    naming

--- test/unit/test_uops_graphing.py
+++ test/unit/test_uops_graphing.py
@@ -4,9 +4,10 @@ from tinygrad.tensor import Tensor
 from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.renderer.opencl import OpenCLRenderer
 from tinygrad.graph import graph_uops
+from tinygrad.nn import Conv2d
 
-class TestUopsFlopCounter(unittest.TestCase):
-  def test_flops_matmul(self):
+class TestUopsGraph(unittest.TestCase):
+  def test_matmul(self):
     N = 1024
     a = Tensor.rand(N,N)
     b = Tensor.rand(N,N)
@@ -19,7 +20,7 @@ class TestUopsFlopCounter(unittest.TestCase):
     for u in uops: print(u)
     print(OpenCLRenderer("matmul", uops)[0])
 
-  def test_flops_reduce(self):
+  def test_reduce(self):
     a = Tensor.rand(1024*1024)
     si = a.sum().lazydata.schedule()[-1]
     lin = Linearizer(si.ast)
@@ -27,6 +28,17 @@ class TestUopsFlopCounter(unittest.TestCase):
     uops = lin.linearize().uops
     graph_uops(uops)
     #print(OpenCLRenderer("reduce", uops)[0])
+
+  def test_conv(self):
+    x = Tensor.rand(1,3,16,16)
+    c = Conv2d(3, 16, (3,3))
+    si = c(x).elu().lazydata.schedule()[-1]
+    lin = Linearizer(si.ast)
+    lin.hand_coded_optimizations()
+    uops = lin.linearize().uops
+    graph_uops(uops)
+    print(lin.colored_shape())
+    print(OpenCLRenderer("conv", uops)[0])
 
 if __name__ == '__main__':
   unittest.main()
