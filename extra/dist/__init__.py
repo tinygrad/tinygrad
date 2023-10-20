03ec6ed410af9070f401b037005fe7a7137a9bd9
Woze Parrot <wozeparrot@gmail.com>
2023-10-20 17:41:01 -0400

    feat: set this

--- extra/dist/__init__.py
+++ extra/dist/__init__.py
@@ -46,7 +46,7 @@ def _process_wrap(rank:int, device:str, oob:_OOB, fn:Callable, args=()):
     from tinygrad.runtime.ops_gpu import CL
     CL.post_init(device_num)
   elif "HIP" in device:
-    os.environ["HIP_DEFAULT_DEVICE"] = str(device_num)
+    os.environ["HIP_DEFAULT_DEVICE"] = os.environ["HIP_VISIBLE_DEVICES"] = str(device_num)
   if DEBUG >= 1: print(f"distributed process {rank} initialized runtime for device {device}")
 
   # convert device to be process specific
