6c58bf3e9cd2684557f982ae54e9977c645da6c9
chenyu <chenyu@fastmail.com>
2023-10-28 13:17:41 -0400

    in time_linearizer, allocate a scratch buffer if output buffer is also input (#2152)
    * in time_linearizer, allocate a scratch buffer if output buffer is also input
    
    * move scratch buffer creation outside search

--- extra/optimization/test_beam_search.py
+++ extra/optimization/test_beam_search.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 
 from tinygrad.helpers import BEAM
 from tinygrad.shape.symbolic import Variable
@@ -14,3 +15,10 @@ class TestBeamSearch(unittest.TestCase):
   def test_variable_ast_no_beam(self):
     a = Tensor.rand(3, 3).reshape((Variable("a", 1, 10).bind(3), 3))
     a = (a+1).realize()
+
+  def test_no_mutate_rawbuffers(self):
+    a = Tensor.rand(3, 3).realize()
+    desired = a.numpy() + 1
+    a.assign(a+1)
+    actual = a.numpy()
+    np.testing.assert_allclose(actual, desired)
