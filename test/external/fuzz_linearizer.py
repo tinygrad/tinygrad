a254e3407c6c8eb021feb3d3f6a71786aa9d56e0
chenyu <chenyu@fastmail.com>
2023-10-31 18:24:12 -0400

    work

--- test/external/fuzz_linearizer.py
+++ test/external/fuzz_linearizer.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from collections import Counter
 from extra.optimization.helpers import load_worlds, ast_str_to_lin
 from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.features.search import get_linearizer_actions
@@ -23,25 +24,27 @@ def fuzz_linearizer(lin: Linearizer):
     idx, valid = buf.st.expr_idxs()
     # TODO: image type and variable shape
     size = idx.max+1
-    rawbuf = device.buffer.fromCPU(np.random.rand(size).astype(buf.dtype.np))
+    # TODO: different range for int type v.s. float type
+    # TODO: assert based on L2 distance not elementwise
+    rawbuf = device.buffer.fromCPU(np.random.uniform(low=-1.0, high=1.0, size=size).astype(buf.dtype.np))
     rawbufs.append(rawbuf)
-
-  prg = device.to_program(lin.copy())
-  prg.exec(rawbufs, force_wait=True)
-  ground_truth = rawbufs[0].toCPU()
-  # print(f"{ground_truth=}")
 
   # NOTE: copied from beam_search
   def tuplize_uops(uops): return tuple([(x.uop, x.dtype, tuple(x.num for x in x.vin), x.arg) for x in uops])
   seen_uops = {}
 
+  output = None
   while 1:
+    if len(seen_uops) >= 20:
+      # enough for this kernel
+      break
     # TODO: if this is too slow, we can reject sample until first valid action, instead of getting all actions first
     actions = get_linearizer_actions(lin.copy(), include_0=False)
     if not actions: break
     lin = random.choice(list(actions.values()))
+    if lin.applied_opts: print(f"last action: {lin.applied_opts[-1]}")
 
-    # TODO: why is there a noop action?
+    # TODO: why is there a noop action? local a local can permute and have a loop
     tuops = tuplize_uops(lin.copy().linearize().uops)
     if tuops in seen_uops:
       break
@@ -49,28 +52,39 @@ def fuzz_linearizer(lin: Linearizer):
 
     print(lin.colored_shape())
     # get a new output buffer
-    outputbuffer = device.buffer(size=prod(lin.membufs[0].st.shape), dtype=lin.membufs[0].dtype)
-    rawbufs[0] = outputbuffer
+    rawbufs[0] = device.buffer(size=prod(lin.membufs[0].st.shape), dtype=lin.membufs[0].dtype)
 
+    # TODO: Interpreted backend
     try:
       prg = device.to_program(lin.copy())
+    except:
+      import traceback; traceback.print_exc()
+      print("COMPILE FAILED!!")
+      return "COMPILE_ERROR"
+
+    try:
       prg.exec(rawbufs, force_wait=True)
     except:
-      print("FAIL!!")
-      return False
+      print("EXEC FAILED!!")
+      return "EXEC_ERROR"
+
     result = rawbufs[0].toCPU()
-    # print(result)
-    np.testing.assert_allclose(result, ground_truth, rtol=1e-4, atol=1e-4)
-  return True
+    if output is None:
+      output = result
+    elif not np.allclose(result, output, rtol=1e-4, atol=1e-4):
+      return "NOT_ALLCLOSE"
+  return "PASS"
 
 if __name__ == "__main__":
   device = Device[Device.DEFAULT]
   ast_strs = load_worlds()
   print(f"{len(ast_strs)=}")
-  tested = passed = 0
+  tested = 0
+  c = Counter()
   # TODO: ast_strs[0] output contains nan?
   for ast in ast_strs[:20]:
     tested += 1
     lin = ast_str_to_lin(ast)
-    passed += int(fuzz_linearizer(lin))
-  print(f"{passed} / {tested} = {passed/tested:.4f}")
+    c[fuzz_linearizer(lin)] += 1
+  print(f"{tested=}")
+  print(c.most_common())
