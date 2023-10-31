f01aaa245da0398d88cd36c939507ec840fadbda
George Hotz <geohot@gmail.com>
2023-10-31 10:49:55 -0700

    fix tanh onnx

--- openpilot/compile2.py
+++ openpilot/compile2.py
@@ -22,11 +22,12 @@ from tinygrad.ops import LoadOps, Device, ScheduleItem
 from tinygrad.features.image import fix_schedule_for_images
 Device.DEFAULT = "GPU"
 
-def get_schedule(onnx_model) -> Tuple[List[ScheduleItem], List[ScheduleItem]]:
+def get_schedule(onnx_data) -> Tuple[List[ScheduleItem], List[ScheduleItem]]:
   Tensor.no_grad = True
   Tensor.training = False
 
   # load the model
+  onnx_model = onnx.load(io.BytesIO(onnx_data))
   run_onnx = get_run_onnx(onnx_model)
   input_shapes = {inp.name:tuple(x.dim_value for x in inp.type.tensor_type.shape.dim) for inp in onnx_model.graph.input}
 
@@ -81,38 +82,64 @@ def schedule_to_thneed(schedule, output_fn):
   runtime = t.run()
   print(f"network using {used_ops/1e9:.2f} GOPS with runtime {runtime*1e3:.2f} ms that's {used_ops/runtime*1e-9:.2f} GFLOPS")
 
-def thneed_test_onnx(output_fn, onnx_model):
+def thneed_test_onnx(onnx_data, output_fn):
+  import onnx
   import pyopencl as cl
   from tinygrad.runtime.ops_gpu import CL
   import numpy as np
-  from test.models.test_onnx import run_onnx_torch
   from extra.thneed import Thneed
+  onnx_model = onnx.load(io.BytesIO(onnx_data))
 
-  # load thneed and try that
   input_shapes = {inp.name:tuple(x.dim_value for x in inp.type.tensor_type.shape.dim) for inp in onnx_model.graph.input}
   inputs = {k:Tensor.randn(*shp, requires_grad=False)*8 for k,shp in input_shapes.items()}
   new_np_inputs = {k:v.realize().numpy() for k,v in inputs.items()}
-  new_torch_out = run_onnx_torch(onnx_model, new_np_inputs).numpy()
-  nt = Thneed()
-  nt.load(output_fn)
 
-  # inputs
-  for k,v in nt.inputs.items():
-    cl.enqueue_copy(CL.cl_queue[0], v, new_np_inputs[k], is_blocking=True)
+  if getenv("ORT"):
+    # test with onnxruntime
+    import onnxruntime as ort
+    onnx_session = ort.InferenceSession(onnx_data)
+    onnx_output = onnx_session.run([onnx_model.graph.output[0].name], {k:v.astype(np.float16) for k,v in new_np_inputs.items()})
+    new_torch_out = onnx_output[0]
+  else:
+    # test with torch
+    from test.models.test_onnx import run_onnx_torch
+    new_torch_out = run_onnx_torch(onnx_model, new_np_inputs).numpy()
 
-  nt.run()
-  new_thneed_out = np.empty((nt.outputs[0].size//4,), dtype=np.float32).reshape(new_torch_out.shape)
-  cl.enqueue_copy(CL.cl_queue[0], new_thneed_out, nt.outputs[0], is_blocking=True)
+  if output_fn is None:
+    # non thneed
+    run_onnx = get_run_onnx(onnx_model)
+    new_tinygrad_out = next(iter(run_onnx(inputs).values())).cast(dtypes.float32).numpy()
+    for i,(x,y) in enumerate(zip(new_torch_out.flatten().tolist(), new_tinygrad_out.flatten().tolist())):
+      if abs(x-y) > 100:
+        print(i, x, y)
 
-  # compare torch to thneed
-  np.testing.assert_allclose(new_torch_out, new_thneed_out, atol=1e-4, rtol=1e-2)
-  print("thneed self-test passed!")
+    np.testing.assert_allclose(new_torch_out, new_tinygrad_out, atol=1e-4, rtol=1e-2)
+    print("classic self-test passed!")
+  else:
+    # load thneed and try that
+    nt = Thneed()
+    nt.load(output_fn)
+
+    # inputs
+    for k,v in nt.inputs.items():
+      cl.enqueue_copy(CL.cl_queue[0], v, new_np_inputs[k], is_blocking=True)
+
+    nt.run()
+    new_thneed_out = np.empty((nt.outputs[0].size//4,), dtype=np.float32).reshape(new_torch_out.shape)
+    cl.enqueue_copy(CL.cl_queue[0], new_thneed_out, nt.outputs[0], is_blocking=True)
+
+    # compare torch to thneed
+    np.testing.assert_allclose(new_torch_out, new_thneed_out, atol=1e-4, rtol=1e-2)
+    print("thneed self-test passed!")
 
 if __name__ == "__main__":
-  dat = fetch(sys.argv[1] if len(sys.argv) > 1 else OPENPILOT_MODEL)
-  onnx_model = onnx.load(io.BytesIO(dat))
+  onnx_data = fetch(sys.argv[1] if len(sys.argv) > 1 else OPENPILOT_MODEL)
 
-  schedule, schedule_independent, inputs = get_schedule(onnx_model)
+  # quick test for ONNX issues
+  #thneed_test_onnx(onnx_data, None)
+  #exit(0)
+
+  schedule, schedule_independent, inputs = get_schedule(onnx_data)
   schedule, schedule_input = partition(schedule, lambda x: x.ast.op not in LoadOps)
   print(f"{len(schedule_input)} inputs")
   schedule = fix_schedule_for_images(schedule)
@@ -135,7 +162,7 @@ if __name__ == "__main__":
   FLOAT16 = getenv("FLOAT16", 0)
   if FLOAT16 == 0:
     try:
-      thneed_test_onnx(output_fn, onnx_model)
+      thneed_test_onnx(onnx_data, output_fn)
     except ModuleNotFoundError as e:
       print(f"TEST NOT HAPPENING {e}")
 
