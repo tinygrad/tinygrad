6aeae61e4198013fab26345fda4ebce27643f50a
pythonlearner1025 <mjsong2021@gmail.com>
2023-11-03 02:47:49 -0400

    recover cache opt

--- tinygrad/codegen/optimizer.py
+++ tinygrad/codegen/optimizer.py
@@ -135,6 +135,8 @@ class OptimizedKernel(Kernel):
         assert max(global_max) >= max(self.full_shape[:global_dims]), f"device max allocation {max(self.full_shape[:global_dims])} exceeds global dim maximum {max(global_max)}"
       print(len(self.full_shape), len(global_max), global_dims)
       print(self.full_shape,global_max)
+      if len(global_max) < global_dims-1:
+        global_dims-=1
       for i in range(global_dims-1):
         if self.full_shape[i] > global_max[i]:
           order = list(range(len(self.full_shape)))
