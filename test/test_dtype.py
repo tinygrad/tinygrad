12ad914b3ffa0ad1200b51a5fdd7374fda678289
duyle-py <duyltk.cs@gmail.com>
2023-11-06 23:56:04 +0700

    remove METAL skip test

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -11,7 +11,7 @@ def is_dtype_supported(dtype: DType):
   # for LLVM, it segfaults because it can't link to the casting function
   if dtype == dtypes.half: return not (CI and Device.DEFAULT in ["GPU", "LLVM"]) and Device.DEFAULT != "WEBGPU" and getenv("CUDACPU") != 1
   if dtype == dtypes.bfloat16: return False # numpy doesn't support bf16, tested separately in TestBFloat16DType
-  if dtype == dtypes.float64: return Device.DEFAULT not in ["WEBGPU", "METAL"] and not OSX
+  if dtype == dtypes.float64: return Device.DEFAULT not in ["WEBGPU"] and not OSX
   if dtype in [dtypes.int8, dtypes.uint8]: return Device.DEFAULT not in ["WEBGPU"]
   if dtype in [dtypes.int16, dtypes.uint16]: return Device.DEFAULT not in ["WEBGPU", "TORCH"]
   if dtype == dtypes.uint32: return Device.DEFAULT not in ["TORCH"]
