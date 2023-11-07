0bf2dd35ed2f2b162a88146d8b0019b30526a1b9
chenyu <chenyu@fastmail.com>
2023-11-07 14:42:29 -0800

    just Exception

--- test/test_linearizer_failures.py
+++ test/test_linearizer_failures.py
@@ -22,7 +22,7 @@ class TestLinearizerFailures(unittest.TestCase):
   def test_failure_2(self):
     ast = LazyOp(op=ReduceOps.MAX, src=(LazyOp(op=BufferOps.MEM, src=(), arg=MemBuffer(idx=1, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(32, 2, 111, 27), strides=(6160, 3080, 28, 1), offset=0, mask=((0, 32), (0, 2), (0, 110), (0, 27)), contiguous=False), View(shape=(32, 2, 37, 9, 2, 2), strides=(5994, 2997, 81, 3, 27, 1), offset=0, mask=None, contiguous=False))))),), arg=(32, 2, 37, 9, 1, 1))
     lin = Linearizer(ast)
-    assert isinstance(fuzz_linearizer(lin), ValueError)
+    assert isinstance(fuzz_linearizer(lin), Exception)
 
   @unittest.skipUnless(Device.DEFAULT in ["METAL", "GPU", "CLANG"], "fails on these backends")
   def test_failure_3(self):
