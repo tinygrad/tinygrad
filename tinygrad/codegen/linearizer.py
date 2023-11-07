b3fb982ef8e1be41d82abce8f9c8d72e2951a365
qazal <qazal.software@gmail.com>
2023-11-07 02:49:45 -0500

    get rid of parse_cast

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -408,16 +408,11 @@ class Linearizer(Kernel):
     if cachable: self.saved_exprs[key] = self.uops[-1]
     return self.uops[-1]
 
-  def parse_cast(self, uop: UOp, dtype:DType, bitcast:bool) -> UOp:
-    if isinstance(dtype, ImageDType): return uop
-    if uop.uop == UOps.LOAD and uop.vin[0].dtype != uop.dtype: uop = self.uop(UOps.CAST, uop.vin[0].dtype, (uop,), (uop.vin[0].dtype, False))
-    return self.uop(UOps.CAST, dtype, (uop,), (dtype,bitcast))
-
   def ast_parse(self, x, acc, offs, loaded_buffers, do_reduce=False) -> List[UOp]:
     if x.__class__ is not LazyOp: return loaded_buffers[x]    # for LOCAL_BUFFER
     if x.op in BufferOps: return loaded_buffers[x.arg]
     if x.op == UnaryOps.NOOP: return self.ast_parse(x.src[0], acc, offs, loaded_buffers)
-    if x.op == UnaryOps.CAST: return [self.parse_cast(val, *x.arg) for val in self.ast_parse(x.src[0], acc, offs, loaded_buffers)]
+    if x.op == UnaryOps.CAST: return [self.uop(UOps.CAST, x.arg[0], (u,), x.arg) if not isinstance(x.arg[0], ImageDType) else u for u in self.ast_parse(x.src[0], acc, offs, loaded_buffers)]
     if x.op in ReduceOps and not do_reduce:
       assert offs is None, "not available if we aren't doing reduce"
       return acc
