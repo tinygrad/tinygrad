0afde540098ace57a03d6b9c79ec0832c2761c56
George Hotz <geohot@gmail.com>
2023-11-11 07:42:41 -0800

    more beautiful

--- examples/beautiful_mnist.py
+++ examples/beautiful_mnist.py
@@ -10,11 +10,10 @@ class Model:
     self.c1 = nn.Conv2d(1, 8, (3,3))
     self.c2 = nn.Conv2d(8, 16, (3,3))
     self.l1 = nn.Linear(400, 10)
-  def __call__(self, x):
+  def __call__(self, x:Tensor) -> Tensor:
     x = self.c1(x).relu().max_pool2d()
     x = self.c2(x).relu().max_pool2d()
-    x = self.l1(x.reshape(x.shape[0], -1))
-    return x.log_softmax()   # TODO: there's a second one of these in sparse_categorical_crossentropy
+    return self.l1(x.flatten(1)).log_softmax()   # TODO: there's a second one of these in sparse_categorical_crossentropy
 
 if __name__ == "__main__":
   X_train, Y_train, X_test, Y_test = fetch_mnist()
@@ -22,17 +21,19 @@ if __name__ == "__main__":
   model = Model()
   opt = nn.optim.Adam(nn.state.get_parameters(model))
 
+  # TODO: there's a compiler error if you comment out TinyJit since randint isn't being realized and there's something weird with int
   @TinyJit
-  def train_step(samples):
+  def train_step(samples:Tensor) -> Tensor:
     opt.zero_grad()
     loss = model(X_train[samples]).sparse_categorical_crossentropy(Y_train[samples]).backward()
     opt.step()
-    return loss.realize()
+    return loss.realize()  # TODO: should the jit do this automatically? i think yes
 
   for i in (t:=trange(200)):
+    # TODO: should the TinyJit auto reset this? this is only for debugging
     GlobalCounters.reset()
     # TODO: put this in the JIT when rand is fixed (also shouldn't need the realize)
-    samples = Tensor.randint(32, low=0, high=X_train.shape[0]).realize()
+    samples = Tensor.randint(32, high=X_train.shape[0])
     loss = train_step(samples)
     t.set_description(f"loss: {loss.item():.2f}")
 
