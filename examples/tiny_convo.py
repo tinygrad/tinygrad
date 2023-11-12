bc67df4eff44fe5c98bcc2d206c747ea2ac5f65e
Francis Lata <francisalbertlata@gmail.com>
2023-11-11 22:09:22 -0500

    rewrite listening and whisper decoding

--- examples/tiny_convo.py
+++ examples/tiny_convo.py
@@ -1,74 +1,83 @@
 from tinygrad.helpers import getenv
 from tinygrad.tensor import Tensor
 from examples.whisper import init_whisper, prep_audio, Whisper
+from tqdm import tqdm
 
 import multiprocessing
 import numpy as np
 import pyaudio
 import tiktoken
+import os
 
 SAMPLE_RATE = 16000
 N_FRAME_CHUNK = 1600
+RECORD_SECONDS = 10
+NUM_RUNS = int(SAMPLE_RATE / N_FRAME_CHUNK * RECORD_SECONDS)
 
-class AudioListener:
-  def __init__(self, sample_rate: int = SAMPLE_RATE, n_frame_chunk: int = N_FRAME_CHUNK):
-    self.sample_rate = sample_rate
-    self.n_frame_chunk = n_frame_chunk
+# TODO: move this somewhere because process cannot pickle this
 
-  def start(self, audio_queue: multiprocessing.Queue):
-    self.process = multiprocessing.Process(target=self.listen, args=(audio_queue,))
-    self.process.daemon = True
-    self.process.start()
-
-  def listen(self, audio_queue: multiprocessing.Queue):
-    paudio = pyaudio.PyAudio()
-    stream = paudio.open(
+def listen(q: multiprocessing.Queue):
+  p = pyaudio.PyAudio()
+  stream = p.open(
       format=pyaudio.paInt16,
       channels=1,
-      rate=self.sample_rate,
+      rate=SAMPLE_RATE,
       input=True,
-      frames_per_buffer=self.n_frame_chunk
-    )
+      frames_per_buffer=N_FRAME_CHUNK
+  )
+  print("Start listening...")
 
-    while True:
-      # TODO: make audio settings as ArgumentParser arg
-      audio = stream.read(self.n_frame_chunk)
-      audio = ((np.frombuffer(audio, np.int16)/32768).astype(np.float32)*3)
+  for _ in range(0, NUM_RUNS):
+    au_data = stream.read(N_FRAME_CHUNK)
+    au = ((np.frombuffer(au_data, np.int16)/32768).astype(np.float32)*3)
+    q.put(au)
 
-      audio_queue.put(audio)
+  print("Done listening!")
 
+  stream.close()
+  p.terminate()
 
 if __name__ == "__main__":
-  # TODO: either refactor whisper example or put it on some helper class
+  q = multiprocessing.Queue()
+
+  p = multiprocessing.Process(target=listen, args=(q,))
+  p.daemon = True
+
+  p.start()
+
   model, enc = init_whisper("small.en" if getenv("SMALL") else "tiny.en")
-
-  # TODO: make this take arguments later
-  audio_listener = AudioListener()
-  audio_queue = multiprocessing.Queue()
-
-  audio_listener.start(audio_queue)
-
   lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
-  audio_buffer = None
-  did_read = False
+  au_buffer = None
 
+  # listen
   while True:
-    while not audio_queue.empty():
-      audio = audio_queue.get()
-      if audio_buffer is None:
-        audio_buffer = audio
-      else:
-        audio_buffer = np.concatenate([audio_buffer, audio])
+    au = q.get()
 
-    if audio_buffer is not None:
-      log_spec = prep_audio(audio_buffer)
-      encoded_audio = model.encoder(Tensor(log_spec)).realize()
+    if au_buffer is None:
+      au_buffer = au
+    else:
+      au_buffer = np.concatenate([au_buffer, au])
 
-      out = model.decoder(Tensor([lst]), encoded_audio).realize()
-      idx = int(out[0,-1].argmax().numpy().item())
-      lst.append(idx)
-      dec = enc.decode(lst)
+    if au_buffer.shape[0] == N_FRAME_CHUNK * NUM_RUNS:
+      break
 
-      print(dec) # DO NOT REMOVE PRINT. IT'S VERY IMPORTANT
-      if dec.endswith("<|endoftext|>"):
-        lst.pop()
+  # decode what user is saying
+  for _ in range(NUM_RUNS):
+    log_spec = prep_audio(au_buffer)
+    encoded_audio = model.encoder(Tensor(log_spec)).realize()
+
+    out = model.decoder(Tensor([lst]), encoded_audio).realize()
+    idx = int(out[0,-1].argmax().numpy().item())
+    lst.append(idx)
+    dec = enc.decode(lst)
+
+    print(dec)
+
+    if dec.endswith("<|endoftext|>"):
+      lst.pop()
+      break
+
+  # TODO: remove special tokens
+  # ask LLaMA
+
+  # use VITS for response
