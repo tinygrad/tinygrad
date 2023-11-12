e5a324375117bc5bf87ac295ab9faf427eac4f31
Francis Lata <francisalbertlata@gmail.com>
2023-11-12 01:03:19 -0500

    add LLaMA decoding of user prompt

--- examples/tiny_convo.py
+++ examples/tiny_convo.py
@@ -1,20 +1,29 @@
 from tinygrad.helpers import getenv
 from tinygrad.tensor import Tensor
-from examples.whisper import init_whisper, prep_audio, Whisper
-from tqdm import tqdm
+from tinygrad.shape.symbolic import Variable
+from examples.whisper import init_whisper, prep_audio
+from pathlib import Path
+from examples.llama import LLaMa
 
 import multiprocessing
 import numpy as np
 import pyaudio
-import tiktoken
-import os
+import sys
 
+# Whisper
 SAMPLE_RATE = 16000
 N_FRAME_CHUNK = 1600
-RECORD_SECONDS = 10
+RECORD_SECONDS = 3
 NUM_RUNS = int(SAMPLE_RATE / N_FRAME_CHUNK * RECORD_SECONDS)
 
-# TODO: move this somewhere because process cannot pickle this
+# LLaMA
+# TODO: reuse it from examples/llama.py file
+LLAMA_SUFFIX = {"1": "", "2": "-2", "code": "-code"}["1"]
+MODEL_PATH = Path(__file__).parents[1] / f"weights/LLaMA{LLAMA_SUFFIX}/7B"
+TOKENIZER_PATH = (MODEL_PATH if MODEL_PATH.is_dir() else MODEL_PATH.parent) / "tokenizer.model"
+N_LLAMA_COUNT = 1000
+MAX_CONTEXT = 1024
+TEMP = 0.7
 
 def listen(q: multiprocessing.Queue):
   p = pyaudio.PyAudio()
@@ -37,7 +46,39 @@ def listen(q: multiprocessing.Queue):
   stream.close()
   p.terminate()
 
+def whisper_decode(au_buffer: np.ndarray) -> str:
+  model, enc = init_whisper("small.en" if getenv("SMALL") else "tiny.en")
+  lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
+  idx_2_spec_toks = {v: k for k, v in enc._special_tokens.items()}
+  output_history = ""
+
+  for _ in range(NUM_RUNS):
+    log_spec = prep_audio(au_buffer)
+    encoded_audio = model.encoder(Tensor(log_spec)).realize()
+
+    out = model.decoder(Tensor([lst]), encoded_audio).realize()
+    idx = int(out[0,-1].argmax().numpy().item())
+    lst.append(idx)
+    
+    lst_no_special_tokens = [token for token in lst if token not in idx_2_spec_toks]
+
+    unmod_dec = enc.decode(lst)
+    dec = enc.decode(lst_no_special_tokens)
+
+    sys.stdout.write(dec[len(output_history):])
+    sys.stdout.flush()
+
+    output_history = dec
+
+    if unmod_dec.endswith("<|endoftext|>"):
+      break
+
+  return dec
+
+
 if __name__ == "__main__":
+  Tensor.no_grad = True
+
   q = multiprocessing.Queue()
 
   p = multiprocessing.Process(target=listen, args=(q,))
@@ -45,8 +86,6 @@ if __name__ == "__main__":
 
   p.start()
 
-  model, enc = init_whisper("small.en" if getenv("SMALL") else "tiny.en")
-  lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
   au_buffer = None
 
   # listen
@@ -62,22 +101,25 @@ if __name__ == "__main__":
       break
 
   # decode what user is saying
-  for _ in range(NUM_RUNS):
-    log_spec = prep_audio(au_buffer)
-    encoded_audio = model.encoder(Tensor(log_spec)).realize()
+  user_prompt = whisper_decode(au_buffer)
 
-    out = model.decoder(Tensor([lst]), encoded_audio).realize()
-    idx = int(out[0,-1].argmax().numpy().item())
-    lst.append(idx)
-    dec = enc.decode(lst)
-
-    print(dec)
-
-    if dec.endswith("<|endoftext|>"):
-      lst.pop()
-      break
-
-  # TODO: remove special tokens
   # ask LLaMA
+  outputted = user_prompt
+  llama = LLaMa.build(MODEL_PATH, TOKENIZER_PATH, model_gen="1", model_size="7B", quantize=False)
+  llama_tokens = [llama.tokenizer.bos_id()] + llama.tokenizer.encode(user_prompt)
+  start_pos = 0
+
+  for i in range(N_LLAMA_COUNT):
+    probs = llama.model(Tensor([llama_tokens[start_pos:]]), Variable("start_pos", 1 if start_pos else 0, MAX_CONTEXT).bind(start_pos), TEMP).realize()
+    probs_np = probs.numpy()
+    tok = int(np.random.choice(len(probs_np), p=probs_np))
+
+    start_pos = len(llama_tokens)
+    llama_tokens.append(tok)
+
+    cur = llama.tokenizer.decode(llama_tokens)
+    sys.stdout.write(cur[len(outputted):])
+    sys.stdout.flush()
+    outputted = cur
 
   # use VITS for response
