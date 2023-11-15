a98e9b6116f4145dfa2edc49bce1b9cd15f9be20
ananddtyagi <anand.deep.tyagi@gmail.com>
2023-11-15 00:48:36 -0800

    retain setting a none offset if the file is not seekable as in the initial implementation

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -110,7 +110,7 @@ def torch_load(fn:str):
     for n in myzip.namelist():
       if n.startswith(f'{base_name}/data/'):
         with myzip.open(n) as myfile:
-          if myfile.seekable(): offsets[n.split("/")[-1]] = myfile.tell()
+          offsets[n.split("/")[-1]] = myfile.tell() if myfile.seekable() else None
     with myzip.open(f'{base_name}/data.pkl') as myfile:
       return TorchPickle(myfile).load()
   else:
