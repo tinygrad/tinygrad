fd6479b7544d6fc21473fa9fe52b61b432530148
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2023-11-16 00:56:57 +0100

    typing

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -25,7 +25,7 @@ def make_pair(x:Union[int, Tuple[int, ...]], cnt=2) -> Tuple[int, ...]: return (
 def flatten(l:Union[List, Iterator]): return [item for sublist in l for item in sublist]
 def fromimport(mod, frm): return getattr(__import__(mod, fromlist=[frm]), frm)
 def strip_parens(fst): return fst[1:-1] if fst[0] == '(' and fst[-1] == ')' and fst[1:-1].find('(') <= fst[1:-1].find(')') else fst
-def merge_dicts(ds:Iterable[Dict]) -> Dict:
+def merge_dicts(ds:Iterable[Dict[T]]) -> Dict[T]:
   assert len(kvs:=set([(k,v) for d in ds for k,v in d.items()])) == len(set(kv[0] for kv in kvs)), f"cannot merge, {kvs} contains different values for the same key"
   return {k:v for d in ds for k,v in d.items()}
 def partition(lst, fxn):
