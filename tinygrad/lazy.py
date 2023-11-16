7afa248592aade478ab573ed48fbc8ba804d4ffa
dkormann <dominikormann@outlook.com>
2023-11-16 13:04:15 +0100

    cleanup

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -98,8 +98,6 @@ def create_lazybuffer(device:str, st:ShapeTracker, optype:OpType, op:LazyOp, dty
 
 UNSAFE_PAD_OPS = {BinaryOps.DIV, BinaryOps.CMPLT, UnaryOps.LOG2, UnaryOps.EXP2, UnaryOps.RECIP}
 
-from temp.utils import Timing
-
 class LazyBuffer:
   __deletable__ = ('op',)
   def __init__(self, device:str, st:ShapeTracker, optype:OpType, op:Optional[LazyOp], dtype:DType, src:Optional[RawBuffer]=None, base:Optional[LazyBuffer]=None):
