0aaed2930ba6f4a2461ad68a2e1845cfb5c081d8
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2023-11-16 01:28:41 +0100

    trigger actions

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:  # TODO: remove this and import TypeGuard from typing once min
 
 T = TypeVar("T")
 U = TypeVar("U")
+
 # NOTE: it returns int 1 if x is empty regardless of the type of x
 def prod(x:Iterable[T]) -> Union[T,int]: return functools.reduce(operator.__mul__, x, 1)
 
