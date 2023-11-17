6ead4f0bb148e1b7bf0f915efabbda97873e7641
Francis Lata <francisalbertlata@gmail.com>
2023-11-17 17:09:32 -0500

    add continuous conversation with tinygrad

--- examples/tiny_convo.py
+++ examples/tiny_convo.py
@@ -6,11 +6,14 @@ from pathlib import Path
 from examples.llama import LLaMa
 from examples.vits import MODELS, download_if_not_present, get_hparams_from_file, TextMapper, VITS_PATH, load_model
 from examples.tinyconvo.helpers import audio_stream
+from typing import Tuple
 
 import argparse
 import multiprocessing
 import numpy as np
+import re
 import sys
+import time
 
 # Whisper
 SAMPLE_RATE = 16000
@@ -23,7 +26,7 @@ NUM_RUNS = int(SAMPLE_RATE / N_FRAME_CHUNK * RECORD_SECONDS)
 LLAMA_SUFFIX = {"1": "", "2": "-2", "code": "-code"}["2"]
 MODEL_PATH = Path(__file__).parents[1] / f"weights/LLaMA{LLAMA_SUFFIX}/7B"
 TOKENIZER_PATH = (MODEL_PATH if MODEL_PATH.is_dir() else MODEL_PATH.parent) / "tokenizer.model"
-N_LLAMA_COUNT = 100
+N_LLAMA_COUNT = 30
 MAX_CONTEXT = 1024
 TEMP = 0.7
 
@@ -36,48 +39,26 @@ def listen(q: multiprocessing.Queue, n_frame_chunk: int):
   with audio_stream(True, SAMPLE_RATE, n_frame_chunk) as stream:
     print("Start listening...")
 
-    for _ in range(0, NUM_RUNS):
+    while True:
       au_data = stream.read(n_frame_chunk)
       au = ((np.frombuffer(au_data, np.int16)/32768).astype(np.float32)*3)
       q.put(au)
 
-    print("Done listening!")
-
-def whisper_decode(au_buffer: np.ndarray) -> str:
-  model, enc = init_whisper("small.en" if getenv("SMALL") else "tiny.en")
-  lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
-  idx_2_spec_toks = {v: k for k, v in enc._special_tokens.items()}
-  output_history = ""
-
-  log_spec = prep_audio(au_buffer)
-  encoded_audio = model.encoder(Tensor(log_spec)).realize()
-
-  for _ in range(NUM_RUNS):
-    out = model.decoder(Tensor([lst]), encoded_audio).realize()
-    idx = int(out[0,-1].argmax().numpy().item())
-    lst.append(idx)
-
-    # decode a version with no special tokens as input to LLaMA
-    dec = enc.decode([token for token in lst if token not in idx_2_spec_toks])
-
-    sys.stdout.write(dec[len(output_history):])
-    sys.stdout.flush()
-
-    output_history = dec
-
-    if lst[-1] == enc._special_tokens["<|endoftext|>"]:
-      return dec
+def whisper_decode(encoded_audio: Tensor, model, enc, lst) -> Tuple[str, int]:
+  out = model.decoder(Tensor([lst]), 0, encoded_audio, streaming=True).realize()
+  idx = int(out[0,-1].argmax().numpy().item())
+  lst.append(idx)
+  dec = enc.decode([token for token in lst if token not in {v: k for k, v in enc._special_tokens.items()}])
+  return dec, lst[-1]
 
 # TODO: refactor LLaMA example to expose API to perform decoding
-def llama_response(prompt: str) -> str:
+def llama_response(llama, prompt: str) -> str:
   outputted = prompt
-  llama = LLaMa.build(MODEL_PATH, TOKENIZER_PATH, model_gen="1", model_size="7B", quantize=False)
   llama_tokens = [llama.tokenizer.bos_id()] + llama.tokenizer.encode(prompt)
   start_pos = 0
 
   llama_response = ""
 
-  print("Getting response...")
   for i in range(N_LLAMA_COUNT):
     probs = llama.model(Tensor([llama_tokens[start_pos:]]), start_pos, TEMP).realize()
     probs_np = probs.numpy()
@@ -93,21 +74,11 @@ def llama_response(prompt: str) -> str:
     sys.stdout.flush()
     outputted = cur
 
-  print("Here's the response:")
-
+  print("\n")
   return llama_response
 
 # TODO: refactor VITS example to expose API to perform TTS
-def tts(text_to_synthesize: str) -> np.ndarray:
-  model_config = MODELS["mmts-tts"]
-  config_path = model_config[0]
-  download_if_not_present(config_path, model_config[2])
-  hps = get_hparams_from_file(config_path)
-
-  symbols = [x.replace("\n", "") for x in open(download_if_not_present(VITS_PATH / "vocab_mmts-tts.txt", "https://huggingface.co/facebook/mms-tts/raw/main/full_models/eng/vocab.txt"), encoding="utf-8").readlines()]
-  text_mapper = TextMapper(apply_cleaners=True, symbols=symbols)
-  net_g = load_model(text_mapper.symbols, hps, model_config)
-
+def tts(text_to_synthesize: str, net_g, text_mapper) -> np.ndarray:
   text_to_synthesize = text_mapper.filter_oov(text_to_synthesize.lower())
   stn_tst = text_mapper.get_text(text_to_synthesize, hps.data.add_blank, hps.data.text_cleaners)
   x_tst, x_tst_lengths = stn_tst.unsqueeze(0), Tensor([stn_tst.shape[0]], dtype=dtypes.int64)
@@ -132,6 +103,24 @@ if __name__ == "__main__":
 
   Tensor.no_grad = True
 
+  # whisper
+  model, enc = init_whisper("small.en" if getenv("SMALL") else "tiny.en")
+  lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
+
+  # llama
+  llama = LLaMa.build(MODEL_PATH, TOKENIZER_PATH, model_gen="1", model_size="7B", quantize=False)
+
+  # VITS
+  model_config = MODELS["mmts-tts"]
+  config_path = model_config[0]
+  download_if_not_present(config_path, model_config[2])
+  hps = get_hparams_from_file(config_path)
+
+  symbols = [x.replace("\n", "") for x in open(download_if_not_present(VITS_PATH / "vocab_mmts-tts.txt", "https://huggingface.co/facebook/mms-tts/raw/main/full_models/eng/vocab.txt"), encoding="utf-8").readlines()]
+  text_mapper = TextMapper(apply_cleaners=True, symbols=symbols)
+  net_g = load_model(text_mapper.symbols, hps, model_config)
+
+  # start listening process
   q = multiprocessing.Queue()
 
   p = multiprocessing.Process(target=listen, args=(q, args.n_frame_chunk))
@@ -140,28 +129,47 @@ if __name__ == "__main__":
   p.start()
 
   au_buffer = None
+  prompt_buffer = ""
 
   # listen
-  while True:
-    au = q.get()
+  try:
+    while True:
+      while not q.empty():
+        au = q.get()
 
-    if au_buffer is None:
-      au_buffer = au
-    else:
-      au_buffer = np.concatenate([au_buffer, au])
+        if au_buffer is None:
+          au_buffer = au
+        else:
+          au_buffer = np.concatenate([au_buffer, au])
 
-    if au_buffer.shape[0] == args.n_frame_chunk * NUM_RUNS:
-      break
+      if au_buffer is not None:
+        log_spec = prep_audio(au_buffer.reshape(1, -1), batch_size=1)
+        encoded_audio = model.encoder(Tensor(log_spec)).realize()
+        prompt, last_tok = whisper_decode(encoded_audio, model, enc, lst)
 
-  # decode what user is saying
-  user_prompt = whisper_decode(au_buffer)
+        sys.stdout.write(prompt[len(prompt_buffer):])
+        sys.stdout.flush()
 
-  # ask LLaMA
-  response = llama_response(user_prompt)
+        prompt_buffer = prompt
 
-  # use VITS to read response
-  audio_response = tts(response)
+        if last_tok == enc._special_tokens["<|endoftext|>"]:
+          print("\nDone listening")
+          print("Getting response...")
 
-  print("Reading the response...")
-  play_audio(audio_response)
-  print("Done!")
+          prompt = re.sub(re.compile(r'\\[.*?\\]'), "", prompt)
+          prompt = re.sub(re.compile(r'\\(.*?\\)'), "", prompt)
+          prompt = re.sub(re.compile(r"[^a-zA-Z0-9\\.,\\?!\\s\\:\\'\\-]"), " ", prompt)
+          response = llama_response(llama, prompt)
+          
+          print("Generating audio response...")
+          audio_response = tts(response, net_g, text_mapper)
+
+          print("Playing audio response")
+          play_audio(audio_response)
+
+          lst = [enc._special_tokens["<|startoftranscript|>"], enc._special_tokens["<|notimestamps|>"]]
+          au_buffer = None
+          prompt_buffer = ""
+
+  except KeyboardInterrupt:
+    sys.exit(0)
