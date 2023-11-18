482ed4f5fae02545c6e8c66324fc3c66ea7bd196
George Hotz <geohot@gmail.com>
2023-11-17 16:39:54 -0800

    use actor critic

--- examples/beautiful_cartpole.py
+++ examples/beautiful_cartpole.py
@@ -1,24 +1,29 @@
 from typing import Tuple
-from tinygrad import Tensor, TinyJit, nn, Variable
+from tinygrad import Tensor, TinyJit, nn
 from tinygrad.helpers import dtypes  # TODO: wouldn't need this if argmax returned the right dtype
 import gymnasium as gym
 from tqdm import trange
 import numpy as np  # TODO: remove numpy import
 
-class Actor:
+class ActorCritic:
   def __init__(self, in_features, out_features, hidden_state=32):
     self.l1 = nn.Linear(in_features, hidden_state)
     self.l2 = nn.Linear(hidden_state, out_features)
 
-  def __call__(self, x:Tensor) -> Tensor:
-    x = self.l1(x).relu()
-    return self.l2(x).log_softmax()
+    self.c1 = nn.Linear(in_features, hidden_state)
+    self.c2 = nn.Linear(hidden_state, 1)
 
-def evaluate(actor:Actor, test_env:gym.Env) -> float:
+  def __call__(self, obs:Tensor) -> Tensor:
+    x = self.l1(obs).relu()
+    act = self.l2(x).log_softmax()
+    x = self.c1(obs).relu()
+    return act, self.c2(x)
+
+def evaluate(model:ActorCritic, test_env:gym.Env) -> float:
   (obs, _), terminated, truncated = test_env.reset(), False, False
   total_rew = 0.0
   while not terminated and not truncated:
-    act = actor(Tensor(obs)).argmax().cast(dtypes.int32).item()
+    act = model(Tensor(obs))[0].argmax().cast(dtypes.int32).item()
     obs, rew, terminated, truncated, _ = test_env.step(act)
     total_rew += rew
   return total_rew
@@ -27,25 +32,26 @@ def evaluate(actor:Actor, test_env:gym.Env) -> float:
 if __name__ == "__main__":
   env = gym.make('CartPole-v1')
 
-  actor = Actor(env.observation_space.shape[0], int(env.action_space.n))
-  opt = nn.optim.Adam(nn.state.get_parameters(actor), lr=1e-2)
+  model = ActorCritic(env.observation_space.shape[0], int(env.action_space.n))
+  opt = nn.optim.Adam(nn.state.get_parameters(model), lr=1e-2)
 
   @TinyJit
-  def train_step(x:Tensor, y:Tensor) -> Tuple[Tensor, Tensor]:
-    log_dist = actor(x)
-    action_loss = -(log_dist * y).sum(-1).mean()
+  def train_step(x:Tensor, rtg:Tensor, mask:Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+    log_dist, value = model(x)
+    advantage = rtg - value
+    action_loss = -(log_dist * mask * advantage.detach()).sum(-1).mean()
     entropy_loss = (log_dist.exp() * log_dist).sum(-1).mean()   # this encourages diversity
-    (action_loss + entropy_loss*0.001).backward()
+    critic_loss = advantage.square().mean()
+    (action_loss + entropy_loss*0.001 + critic_loss).backward()
     opt.step()
-    return action_loss.realize(), entropy_loss.realize()
+    return action_loss.realize(), entropy_loss.realize(), critic_loss.realize()
 
   @TinyJit
-  def get_action_dist(obs:Tensor) -> Tensor: return actor(obs).exp().realize()
+  def get_action_dist(obs:Tensor) -> Tensor: return model(obs)[0].exp().realize()
 
   BS = 128
-  max_shape = Variable("max_shape", 0, 500)
   for i in (t:=trange(100)):
-    Xn, Yn = [], []
+    Xn, Rn, Mn = [], [], []
     ep_rews = []
     get_action_dist.reset()   # NOTE: if you don't reset the jit here it captures the wrong model on the first run through
     while len(Xn) < BS:
@@ -65,7 +71,7 @@ if __name__ == "__main__":
 
         obs, rew, terminated, truncated, _ = env.step(act)
         rews.append(rew)
-      ep_rews.append(sum(acts))
+      ep_rews.append(sum(rews))
 
       # reward to go
       # TODO: move this into tinygrad
@@ -73,16 +79,17 @@ if __name__ == "__main__":
         rew, discount = 0, 1.0
         for r in rews[i:]:
           rew += r * discount
-          discount *= 0.999
+          discount *= 0.9
+        Rn.append([rew])
         act_mask = np.zeros((env.action_space.n), dtype=np.float32)
-        act_mask[act] = rew
-        Yn.append(act_mask)
+        act_mask[act] = 1.0
+        Mn.append(act_mask)
 
     # TODO: this shouldn't be numpy
-    X, Y = np.array(Xn, dtype=np.float32), np.array(Yn, dtype=np.float32)
+    X, R, M = np.array(Xn, dtype=np.float32), np.array(Rn, dtype=np.float32), np.array(Mn, dtype=np.float32)
     samples = Tensor.randint(BS, high=X.shape[0], device="CPU").numpy()
-    action_loss, entropy_loss = train_step(Tensor(X[samples]), Tensor(Y[samples]))
-    t.set_description(f"action_loss: {action_loss.item():6.2f} entropy_loss: {entropy_loss.item():6.2f} ep_count: {len(ep_rews):2d} avg_ep_rew: {sum(ep_rews)/len(ep_rews):6.2f}")
+    action_loss, entropy_loss, critic_loss = train_step(Tensor(X[samples]), Tensor(R[samples]), Tensor(M[samples]))
+    t.set_description(f"action_loss: {action_loss.item():6.2f} entropy_loss: {entropy_loss.item():6.2f} critic_loss: {critic_loss.item():6.2f} ep_count: {len(ep_rews):2d} avg_ep_rew: {sum(ep_rews)/len(ep_rews):6.2f}")
 
-  test_rew = evaluate(actor, gym.make('CartPole-v1', render_mode='human'))
+  test_rew = evaluate(model, gym.make('CartPole-v1', render_mode='human'))
   print(f"test reward: {test_rew}")
