14a85d9fc2d10c01e9e4475b986e0efb1bbaf2d2
George Hotz <geohot@gmail.com>
2023-11-18 09:12:01 -0800

    add correctness check to disk tensor

--- tinygrad/runtime/ops_disk.py
+++ tinygrad/runtime/ops_disk.py
@@ -4,6 +4,7 @@ from typing import Callable, Dict, Tuple
 from tinygrad.helpers import prod, DType
 from tinygrad.runtime.lib import RawBufferMapped
 from tinygrad.ops import Interpreted, Op, MovementOps, UnaryOps, BufferOps
+from tinygrad.shape.view import strides_for_shape
 
 class RawDiskBuffer(RawBufferMapped):
   def __init__(self, size, dtype:DType, device:Optional[str]=None, buf=None, shape=None, offset=0):  # pylint: disable=super-init-not-called
@@ -23,7 +24,7 @@ class RawDiskBuffer(RawBufferMapped):
     if self._buf[2] == 0: self._buf[0].close()
   def cast(self, arg:Tuple[DType, bool]): return RawDiskBuffer(self.size, arg[0], buf=self._buf, shape=self.shape, offset=self.offset)
   def as_strided(self, arg):
-    # TOOD: test the correctness of this stride
+    assert strides_for_shape(arg[0]) == arg[1], "disk tensors don't support strides"
     return RawDiskBuffer(prod(arg[0]), self.dtype, buf=self._buf, offset=self.offset+arg[2]*self.dtype.itemsize, shape=arg[0])
 
   def _buffer(self): return memoryview(self._buf[1])[self.offset:self.offset+self.size*self.dtype.itemsize]
