2b001a23b7ec2875260b564ac9ea7f6a532b1652
chenyu <chenyu@fastmail.com>
2023-11-17 20:21:18 -0800

    update fixed linearizer test

--- test/test_linearizer_failures.py
+++ test/test_linearizer_failures.py
@@ -25,11 +25,11 @@ def helper_test_lin(lin: Linearizer, opts, fixed_platforms):
     return run_linearizer(lin) != "PASS"
 
 class TestLinearizerFailures(unittest.TestCase):
-  @unittest.skip("this is currently failing")
   def test_failure_1(self):
+    # fixed
     ast = LazyOp(op=BinaryOps.ADD, src=(LazyOp(op=BinaryOps.ADD, src=(LazyOp(op=ReduceOps.SUM, src=(LazyOp(op=BufferOps.MEM, src=(), arg=MemBuffer(idx=1, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(32, 16, 16), strides=(16, 1, 0), offset=0, mask=None, contiguous=False),)))),), arg=(32, 16, 1)), LazyOp(op=BufferOps.MEM, src=(), arg=MemBuffer(idx=2, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(32, 16, 1), strides=(0, 1, 0), offset=0, mask=None, contiguous=False),))))), arg=None), LazyOp(op=BufferOps.MEM, src=(), arg=MemBuffer(idx=1, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(32, 16, 1), strides=(16, 1, 0), offset=0, mask=None, contiguous=True),))))), arg=None)
     lin = Linearizer(ast)
-    prg = Device[Device.DEFAULT].to_program(lin)
+    assert run_linearizer(lin) == "PASS"
 
   # NOTE: test cases from fuzzer run. if you fixed something and it no longer fails, add platform to fixed_platforms list in helper_test_lin().
 
