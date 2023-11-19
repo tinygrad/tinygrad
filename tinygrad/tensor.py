28faea8e5773341694f74ef012ee5d7fdc07264f
chenyu <chenyu@fastmail.com>
2023-11-19 11:57:35 -0800

    single sum

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -216,7 +216,7 @@ class Tensor:
     assert 1 <= self.ndim <= 2 and num_samples > 0, f"{self.ndim=} must be 1 or 2 dim, {num_samples=} must be positive"
     assert replacement or num_samples == 1, "no replacement only supports num_samples = 1"
     weight = self.unsqueeze(0) if self.ndim == 1 else self
-    cdf = (weight / weight.sum(1, keepdim=True)).cumsum(1)
+    cdf = (cw := weight.cumsum(1)) / cw[:, -1].unsqueeze(1)
     unif_samples = Tensor.rand(num_samples, cdf.shape[0], 1)
     indices = (unif_samples.expand((-1, -1, cdf.shape[1])) >= cdf).sum(2).permute((1, 0))
     return (indices.squeeze(0) if self.ndim == 1 else indices).cast(dtypes.int32)
