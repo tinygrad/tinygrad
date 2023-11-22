ca86d720d9199a03b3dca2363a814fd724059aef
dxlnr <daniel.illner@outlook.de>
2023-11-22 18:27:49 +0100

    init unet3d

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -1,5 +1,9 @@
+from tqdm import tqdm
 from tinygrad.tensor import Tensor
 from tinygrad.helpers import getenv
+from tinygrad.nn.state import get_parameters
+from tinygrad.ops import Device
+from extra.lr_scheduler import MultiStepLR
 
 def train_resnet():
   # TODO: Resnet50-v1.5
@@ -10,8 +14,61 @@ def train_retinanet():
   pass
 
 def train_unet3d():
-  # TODO: Unet3d
-  pass
+  device = Device.DEFAULT
+
+  train_loader = None
+  rank, world_size = getenv("RANK"), getenv("WORLD_SIZE", 1)
+
+  flags = {"epochs": 1, "ga_steps": 1, "warmup_step": 4, "batch_size": 2, "optimizer": "sgd", "init_lr": 1e-4, "lr": 0.1, "lr_decay_epochs": [], "weight_decay": 0.0, "momentum": 0.9, "verbose": False}
+
+  def get_optimizer(params, flags: dict):
+    from tinygrad.nn.optim import Adam, SGD
+    if flags["optimizer"] == "adam":
+      optim = Adam(params, lr=flags["lr"], weight_decay=flags["weight_decay"])
+    elif flags["optimizer"] == "sgd":
+      optim = SGD(params, lr=flags["lr"], momentum=flags["momentum"], nesterov=True, weight_decay=flags["weight_decay"])
+    elif flags["optimizer"] == "lamb":
+      pass
+      # import apex
+      # optim = apex.optimizers.FusedLAMB(params, lr=flags.learning_rate, betas=flags.lamb_betas, weight_decay=flags.weight_decay)
+    else:
+      raise ValueError("Optimizer {} unknown.".format(flags["optimizer"]))
+    return optim
+
+  def lr_warmup(optimizer, init_lr, lr, current_epoch, warmup_epochs):
+    scale = current_epoch / warmup_epochs
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = init_lr + (lr - init_lr) * scale
+
+  # model init
+  from extra.models.unet3d import UNet3D
+  mdl = UNet3D()
+
+  optimizer = get_optimizer(get_parameters(mdl), flags)
+  if flags["lr_decay_epochs"]:
+    scheduler = MultiStepLR(optimizer, milestones=flags["lr_decay_epochs"], gamma=flags["lr_decay_factor"])
+  # scaler = GradScaler()
+
+  for epoch in range(1, flags["epochs"] + 1):
+    cumulative_loss = []
+
+    loss_value = None
+    optimizer.zero_grad()
+    for i, batch in enumerate(tqdm(train_loader, disable=(rank != 0) or not flags["verbose"])):
+      im, label = batch
+      im, label = im.to(device), label.to(device)
+      # for callback in callbacks:
+      #   callback.on_batch_start()
+
+#       with autocast(enabled=flags["amp"]):
+      out = mdl(im)
+      loss_value = loss_fn(out, label)
+      loss_value /= flags.ga_steps
+
+      if flags["amp"]:
+        scaler.scale(loss_value).backward()
+      else:
+        loss_value.backward()
 
 def train_rnnt():
   # TODO: RNN-T
@@ -32,5 +89,3 @@ if __name__ == "__main__":
       if nm in globals():
         print(f"training {m}")
         globals()[nm]()
-
-
