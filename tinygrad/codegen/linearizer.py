85d9414b6455d7cc70f2715acf9262d47fab8654
chenyu <chenyu@fastmail.com>
2023-11-22 17:54:06 -0800

    rebase artifact

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -130,7 +130,7 @@ class Linearizer(Kernel):
         amt = len(out_tokens)
         idx, valid = self.sts[i].expr_idxs(k)
         assert idx.render() == ((idx//amt)*amt).render(), "float4 stores are always aligned"
-        store_offset_new[k] = self.uop(UOps.CAST, dtypes._float4 if amt == 4 else dtypes._float2, tuple(out_tokens))
+        store_offset_new[k] = self.uop(UOps.CAST, dtypes.float.vec(amt), tuple(out_tokens))
       store_offset = store_offset_new
 
     stores = []
