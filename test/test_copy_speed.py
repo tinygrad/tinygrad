8307f8bc0d0075a509714709edb4b7a9fffc5bdf
George Hotz <geohot@gmail.com>
2023-11-23 08:45:50 -0800

    s.name

--- test/test_copy_speed.py
+++ test/test_copy_speed.py
@@ -13,7 +13,7 @@ class TestCopySpeed(unittest.TestCase):
     #t = Tensor.empty(N, N, device="disk:/dev/shm/test_X").realize()
     s = shared_memory.SharedMemory(name="test_X", create=True, size=N*N*4)
     s.close()
-    t = Tensor.empty(N, N, device="disk:shm:test_X").realize()
+    t = Tensor.empty(N, N, device=f"disk:shm:{s.name}").realize()
     for _ in range(3):
       with Timing("sync:  ", on_exit=lambda ns: f" @ {t.nbytes()/ns:.2f} GB/s"):
         with Timing("queue: "):
