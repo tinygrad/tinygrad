4744411bc7dfaa4697bf2fd90d5faa76bdd9d507
George Hotz <geohot@gmail.com>
2023-11-24 12:02:16 -0800

    support not supporting ImageDType/PtrDType

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -173,17 +173,20 @@ class TestBoolDtype(TestDType): DTYPE = dtypes.bool
 
 class TestEqStrDType(unittest.TestCase):
   def test_image_ne(self):
+    if ImageDType is None: raise unittest.SkipTest("no ImageDType support")
     assert dtypes.float == dtypes.float32, "float doesn't match?"
     assert dtypes.imagef((1,2,4)) != dtypes.imageh((1,2,4)), "different image dtype doesn't match"
     assert dtypes.imageh((1,2,4)) != dtypes.imageh((1,4,2)), "different shape doesn't match"
     assert dtypes.imageh((1,2,4)) == dtypes.imageh((1,2,4)), "same shape matches"
     assert isinstance(dtypes.imageh((1,2,4)), ImageDType)
   def test_ptr_ne(self):
+    if PtrDType is None: raise unittest.SkipTest("no PtrDType support")
     # TODO: is this the wrong behavior?
     assert PtrDType(dtypes.float32) == dtypes.float32
     #assert PtrDType(dtypes.float32) == PtrDType(dtypes.float32)
     #assert PtrDType(dtypes.float32) != dtypes.float32
   def test_strs(self):
+    if PtrDType is None: raise unittest.SkipTest("no PtrDType support")
     self.assertEqual(str(dtypes.imagef((1,2,4))), "dtypes.imagef((1, 2, 4))")
     self.assertEqual(str(PtrDType(dtypes.float32)), "ptr.dtypes.float")
 
