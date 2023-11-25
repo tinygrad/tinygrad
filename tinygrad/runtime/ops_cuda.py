553fef6d6b73dcedcd60f21df27349eef3b0a7f1
Leo Prokh <lenyaprokh@gmail.com>
2023-11-26 00:33:40 +0300

    try6

--- tinygrad/runtime/ops_cuda.py
+++ tinygrad/runtime/ops_cuda.py
@@ -24,18 +24,17 @@ if getenv("CUDACPU", 0) == 1:
   lib = ctypes.CDLL(ctypes.util.find_library("gpuocelot"))
   lib.ptx_run.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(ctypes.c_void_p), ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
   class cuda_class:
-    cuDeviceComputeCapability = _ = lambda x: (3,5)
-    cuEventRecord = cuEventDestroy = cuModuleLoadData = cuModuleUnload = lambda x: x
-    cuModuleGetFunction = _ = lambda x, y: x
-    cuLaunchKernel = _ = lambda src, gx, gy, gz, lx, ly, lz, shared, stream, args: lib.ptx_run(src, len(args), (ctypes.c_void_p * len(args))(*[ctypes.cast(x, ctypes.c_void_p) for x in args]), lx, ly, lz, gx, gy, gz, shared)
-    cuEventCreate = _ = lambda: time.perf_counter()
-    cuEventElapsedTime = _ = lambda x, y: y - x
+    cuDeviceComputeCapability = lambda x: (3,5) # noqa: E731
+    cuEventRecord = cuEventDestroy = cuModuleLoadData = cuModuleUnload = lambda x: x # noqa: E731
+    cuModuleGetFunction = lambda x, y: x # noqa: E731
+    cuLaunchKernel = lambda src, gx, gy, gz, lx, ly, lz, shared, stream, args: lib.ptx_run(src, len(args), (ctypes.c_void_p * len(args))(*[ctypes.cast(x, ctypes.c_void_p) for x in args]), lx, ly, lz, gx, gy, gz, shared)  # noqa: E731
+    cuEventCreate = lambda: time.perf_counter() # noqa: E731
+    cuEventElapsedTime = lambda x, y: y - x # noqa: E731
+    cuCtxSynchronize = lambda: None # noqa: E731
 
     nvrtcCreateProgram = cuda.nvrtcCreateProgram
     nvrtcCompileProgram = cuda.nvrtcCompileProgram
     nvrtcGetPTX = cuda.nvrtcGetPTX
-
-    cuCtxSynchronize = _ = lambda: None
 
   cuda: Any = cuda_class # type: ignore
   RawCUDABuffer = RawMallocBuffer
