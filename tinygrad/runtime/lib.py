b69cf6a8ede031a0ba3b47c80901725c9c002989
qtkite <kite@uwu.icu>
2023-11-26 23:47:45 +1100

    Remove the rawbuffer copy in runtime/lib.py on line 44

--- tinygrad/runtime/lib.py
+++ tinygrad/runtime/lib.py
@@ -41,7 +41,7 @@ class RawBufferMapped(RawBufferCopyIn):
   def _buffer(self) -> memoryview: raise NotImplementedError("must be implemented")
   # NOTE: this metadata prevents the backing buffer from being freed. hack can be removed with PEP688
   def buffer_view(self) -> np.ndarray: return np.frombuffer(self._buffer(), dtype=np.dtype(self.dtype.np, metadata={"backing": self}), count=self.size)  # type: ignore
-  def toCPU(self) -> np.ndarray: return self.buffer_view().astype(self.dtype.np, copy=True) # Need a copy (for now), since jit will write to the same buffer.
+  def toCPU(self) -> np.ndarray: return self.buffer_view()
   def _copyin(self, x:np.ndarray) -> None: np.copyto(self.buffer_view(), x.reshape(-1))
 
   @classmethod
