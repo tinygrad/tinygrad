3dc50ad5400f2b64d22f8b9f9d022097c8fc7d02
qtkite <kite@uwu.icu>
2023-11-28 00:00:21 +1100

    buffer behavior as is

--- test/test_jit.py
+++ test/test_jit.py
@@ -240,8 +240,7 @@ class TestJit(unittest.TestCase):
     assert len(cache.good_jitted.jit_cache) == 1
     assert len(cache.bad_jitted.jit_cache) == 1
 
-  @unittest.skip("Will fail as undefined JIT behavior")
-  def test_jit_delayed_cpu(self):
+  def test_jit_buffer_behavior(self):
     @TinyJit
     def foo(x) -> Tensor: return x.sum().realize()
 
@@ -250,7 +249,7 @@ class TestJit(unittest.TestCase):
     result_3 = foo(Tensor([3] * 2))
 
     np.testing.assert_allclose(result_1.numpy(), [2], atol=1e-4, rtol=1e-5)
-    np.testing.assert_allclose(result_2.numpy(), [4], atol=1e-4, rtol=1e-5)
+    np.testing.assert_allclose(result_2.numpy(), [6], atol=1e-4, rtol=1e-5)
     np.testing.assert_allclose(result_3.numpy(), [6], atol=1e-4, rtol=1e-5)
 
 if __name__ == '__main__':
