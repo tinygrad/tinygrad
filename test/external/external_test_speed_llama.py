77fa10031c2e9aedc2b7a92dddd633b3f163d357
George Hotz <geohot@gmail.com>
2023-11-27 10:22:15 -0800

    fix tests

--- test/external/external_test_speed_llama.py
+++ test/external/external_test_speed_llama.py
@@ -5,7 +5,7 @@ from examples.llama import Transformer, MODEL_PARAMS
 from tinygrad.tensor import Tensor
 from tinygrad import Device
 from tinygrad.nn.state import get_state_dict
-from tinygrad.ops import Compiled
+from tinygrad.device import Compiled
 from tinygrad.helpers import dtypes, prod, Profiling
 from tinygrad.runtime.lib import RawBuffer
 
