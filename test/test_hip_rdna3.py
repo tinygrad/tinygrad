2b0de9ffaf0563152fbd171eb8f3c965aa20da7d
Davi Silva <blueridanus@gmail.com>
2023-11-27 14:51:24 +0700

    gate the test properly

--- test/test_hip_rdna3.py
+++ test/test_hip_rdna3.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import unittest
 from tinygrad import Tensor, nn
+from tinygrad.helpers import getenv
 from tinygrad.ops import LoadOps
 from tinygrad.codegen.kernel import LinearizerOptions
 from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.renderer.hip import HIPRenderer
 from examples.beautiful_mnist import Model
 import extra.hip_wrapper as hip
-class TestAssign(unittest.TestCase):
+
+
+class TestHIPCompilationRDNA(unittest.TestCase):
   def test_compile_hip(self):
+    if not getenv("HIP"): raise unittest.SkipTest("testing HIP->rdna3 compilation needs HIP=1")
+
     model = Model()
 
     input = Tensor.rand(512,1,28,28, device="CPU").numpy()
