937d98a5cadc676c7f5a479894f499095c228241
Paul Gustafson <paul.gustafson@gmail.com>
2023-11-27 17:12:34 -0800

    Switch from pyobj to metalcompute

--- setup.py
+++ setup.py
@@ -20,8 +20,7 @@ setup(name='tinygrad',
         "License :: OSI Approved :: MIT License"
       ],
       install_requires=["numpy", "tqdm", "pyopencl",
-                        "pyobjc-framework-Metal; platform_system=='Darwin'",
-                        "pyobjc-framework-libdispatch; platform_system=='Darwin'"],
+                        "metalcompute; platform_system=='Darwin'"],
       python_requires='>=3.8',
       extras_require={
         'llvm': ["llvmlite"],
