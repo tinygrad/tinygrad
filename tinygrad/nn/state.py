4f8fa8ea34cb74317b7704cd1e9409754004e402
rodfer0x80 <rodrigolf080@gmail.com>
2023-11-28 23:14:51 +0000

    revert loops

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -8,10 +8,7 @@ from tinygrad.ops import Device
 
 safe_dtypes = {"F16": dtypes.float16, "F32": dtypes.float32, "U8": dtypes.uint8, "I8": dtypes.int8, "I32": dtypes.int32, "I64": dtypes.int64, "BF16": dtypes.bfloat16}
 inverse_safe_dtypes = {v:k for k,v in safe_dtypes.items()}
-<<<<<<< HEAD
 def cast_bfloat16(t:Union[Tensor, Any]) -> Union[Tensor, Any]: return t.bitcast(dtypes.uint16).to("CPU").cast(dtypes.uint32).mul(1<<16).bitcast(dtypes.float32).to(Device.DEFAULT).half()
-=======
->>>>>>> parent of f1198c66 (abstracted cast bf16 for loading f$)
 
 def safe_load_metadata(fn:Union[Tensor,str]) -> Tuple[Tensor, int, Any]:
   t = fn if isinstance(fn, Tensor) else Tensor.empty(os.stat(fn).st_size, dtype=dtypes.uint8, device=f"disk:{fn}")
@@ -19,26 +16,8 @@ def safe_load_metadata(fn:Union[Tensor,str]) -> Tuple[Tensor, int, Any]:
   return (t, json_len, json.loads(t[8:8+json_len].numpy().tobytes()))
 
 def safe_load(fn:Union[Tensor,str]) -> Dict[str, Tensor]:
-<<<<<<< HEAD
   t, json_len, metadata = safe_load_metadata(fn)
-  ret = {}
-  for k,v in metadata.items():
-    if k != "__metadata__":
-      ret[k] = t[8+json_len+v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape'])
-      if v['dtype'] == "BF16":
-        ret[k] = cast_bfloat16(ret[k])
-  return ret
-=======
-  ts, json_len, metadata = safe_load_metadata(fn)
-  def build_tensor(t, v, dtype) -> Union[str, Tensor]:
-    return t[8+json_len+v['data_offsets'][0]:].cast(dtype)[:prod(v['shape'])].reshape(v['shape'])
-  def cast_bf16(t):
-    return t.bitcast(dtypes.uint16).to("CPU").cast(dtypes.uint32).mul(1<<16).bitcast(dtypes.float32).to(Device.DEFAULT).half()
-  def process_weights(v):
-    t = build_tensor(ts, v, safe_dtypes[v['dtype']])
-    return cast_bf16(t) if v['dtype'] == "BF16" else t
-  return {k: process_weights(v) for k, v in metadata.items() if k != "__metadata__"}
->>>>>>> parent of f1198c66 (abstracted cast bf16 for loading f$)
+  return {k:cast_bfloat16(t[8+json_len+v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape'])) if v['dtype'] == "BF16" else t[8+json_len+v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape']) for k,v in metadata.items() if k != "__metadata__"}
 
 def safe_save(tensors:Dict[str, Tensor], fn:str, metadata:Optional[Dict[str, Any]]=None):
   headers, offset = {}, 0
