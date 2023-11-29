c0f8009e4659a85eaf33570d40ef7e6b61023893
George Hotz <72895+geohot@users.noreply.github.com>
2023-11-28 21:08:39 -0800

    realize cleanup (#2496)
    * move that logic
    
    * revert that change
    
    * clean up transfer and asserts
    
    * what's that junk

--- tinygrad/realize.py
+++ tinygrad/realize.py
@@ -4,7 +4,7 @@ from tinygrad.ops import ScheduleItem, LazyOp, LoadOps, BufferOps
 from tinygrad.device import Device
 from tinygrad.graph import log_schedule_item, print_tree
 from tinygrad.lazy import LazyBuffer
-from tinygrad.helpers import DEBUG, prod, all_int, IMAGE
+from tinygrad.helpers import DEBUG, prod, all_int, IMAGE, getenv
 from tinygrad.features.image import fix_schedule_for_images
 
 def run_schedule(schedule:List[ScheduleItem], disable_logging=False):
@@ -16,24 +16,24 @@ def run_schedule(schedule:List[ScheduleItem], disable_logging=False):
     si = schedule.pop(0)
     if not disable_logging: log_schedule_item(si)
     assert all(x.realized for x in si.inputs), "can't run schedule, some inputs aren't realized"
+    assert all(si.out.device == x.device for x in si.inputs) or si.ast.op is LoadOps.FROM, f"all devices must be the same, {si.out.device} != {[x.device for x in si.inputs]} {print_tree(si.ast) or ''}"
+    # check if we can reuse the output buffer
+    # if it's aliased, don't use it
+    # TODO: this is pretty wrong actually, who knows where else this buffer is used?
+    # TODO: what if an assign is required? this silently is wrong
+    # TODO: this logic doesn't belong here, it should be checked in assign or at least schedule
+    if si.out.output_buffer is not None:
+      for i,a in enumerate(si.inputs):
+        # TODO: if this is contiguous it's fine
+        if a.realized == si.out.output_buffer:
+          if any(not x.arg.st.contiguous for x in si.ast.get_lazyops() if x.op == BufferOps.LOAD and x.arg.idx == i+1):
+            si.out.output_buffer = None
+            break
     if si.ast.op in LoadOps:
       # confirm the LoadOps are contiguous and in order
       for i,s in enumerate(si.ast.src): assert isinstance(s, LazyOp) and s.op == BufferOps.LOAD and s.arg.idx == i+1 and s.arg.st.contiguous, f"bad LoadOps src {i}: {s}"
       LOAD_OPS_DISPATCHER[cast(LoadOps, si.ast.op)](si.out, *si.inputs)
     else:
-      assert all(si.out.device == x.device for x in si.inputs), f"all devices must be the same, {si.out.device} != {[x.device for x in si.inputs]} {print_tree(si.ast) or ''}"
-      # check if we can reuse the output buffer
-      # if it's aliased, don't use it
-      # TODO: this is pretty wrong actually, who knows where else this buffer is used?
-      # TODO: what if an assign is required? this silently is wrong
-      # TODO: this logic doesn't belong here, it should be checked in assign or at least schedule
-      if si.out.output_buffer is not None:
-        for i,a in enumerate(si.inputs):
-          # TODO: if this is contiguous it's fine
-          if a.realized == si.out.output_buffer:
-            if any(not x.arg.st.contiguous for x in si.ast.get_lazyops() if x.op == BufferOps.LOAD and x.arg.idx == i+1):
-              si.out.output_buffer = None
-              break
       # we don't have an output buffer, we have to create it, and create to max size if it has symbolic shape
       si.out.realized = si.out.output_buffer if si.out.output_buffer is not None else \
         Device[si.out.device].buffer(prod((s if isinstance(s, int) else s.max for s in si.out.shape)), si.out.dtype, **si.out._device_extra_args())
@@ -48,24 +48,36 @@ def run_schedule(schedule:List[ScheduleItem], disable_logging=False):
 # *** zero op LoadOps ***
 
 def _realize_empty(buffer: LazyBuffer) -> None:
-  assert all_int(buffer.shape), "does not support symbolic shape"
+  assert all_int(buffer.shape), "LoadOps do not support symbolic shape"
   if DEBUG >= 2: print(f"***     empty {buffer.device}                              shape {str(buffer.shape):23s} dtype {buffer.dtype}")
   buffer.realized = Device[buffer.device].buffer(prod(buffer.shape), buffer.dtype, **buffer._device_extra_args())
 
 # TODO: remove this and write the RNG in tinygrad
 def _realize_rand(buffer: LazyBuffer) -> None:
-  assert all_int(buffer.shape), "does not support symbolic shape"
+  assert all_int(buffer.shape), "LoadOps do not support symbolic shape"
   if DEBUG >= 2: print(f"***      rand {buffer.device}    seed {buffer.op.arg:<10d}  shape {str(buffer.shape):23s} dtype {buffer.dtype}")
   rng = np.random.default_rng(buffer.op.arg)
   buffer.realized = Device[buffer.device].buffer.fromCPU(rng.random(size=prod(buffer.shape), dtype=np.float32).astype(dtype=buffer.dtype.np, copy=False), **buffer._device_extra_args())
 
 # *** one op LoadOps ***
 
+from tinygrad.runtime.lib import RawBufferMapped, RawBufferTransfer
+from tinygrad.runtime.ops_disk import RawDiskBuffer
 def _realize_from(buffer: LazyBuffer, src: LazyBuffer) -> None:
+  assert all_int(buffer.shape), "LoadOps do not support symbolic shape"
   assert src.realized.size == buffer.st.size(), f"size mismatch on FROM {src.realized.size} != {buffer.st.size()}"
   assert src.st.contiguous and buffer.st.contiguous, "all must be contiguous for from"
   if DEBUG >= 2: print(f"***      copy {buffer.device} <- {src.device} size {src.realized.size:<16d} shape {str(buffer.shape):23s} dtype {src.realized.dtype}")
-  buffer.realized = Device[buffer.device].buffer.fromBuffer(src.realized, buffer.shape, buffer.dtype, **buffer._device_extra_args())
+  # TODO: make this generic
+  if isinstance(src.realized, RawDiskBuffer) and issubclass(Device[buffer.device].buffer, RawBufferMapped):
+    buffer.realized = Device[buffer.device].buffer(prod(buffer.shape), buffer.dtype, **buffer._device_extra_args())
+    src.realized.readinto(cast(RawBufferMapped, buffer.realized)._buffer())
+  elif isinstance(src.realized, RawBufferTransfer) and issubclass(Device[buffer.device].buffer, RawBufferTransfer) and getenv("P2P", 0) >= 1:
+    buffer.realized = Device[buffer.device].buffer(prod(buffer.shape), buffer.dtype, **buffer._device_extra_args())
+    cast(RawBufferTransfer, buffer.realized)._transfer(src.realized)
+  else:
+    # TODO: schedule this as FROM to go to CPU, and a FROM to go to device
+    buffer.realized = Device[buffer.device].buffer.fromCPU(src.realized.toCPU(), **buffer._device_extra_args())
 
 # *** n op LoadOps ***
 
