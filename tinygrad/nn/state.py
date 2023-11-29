911c06360cf8878a84863a6f25c909715736e886
rodfer0x80 <rodrigolf080@gmail.com>
2023-11-29 00:45:10 +0000

    back speedy

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -20,16 +20,11 @@ def safe_load_metadata(fn:Union[Tensor,str]) -> Tuple[Tensor, int, Any]:
   return (t, json_len, json.loads(t[8:8+json_len].numpy().tobytes()))
 
 def safe_load(fn:Union[Tensor,str]) -> Dict[str, Tensor]:
-  t, json_len, metadata = safe_load_metadata(fn)
-  return {k: t[v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape']) 
-          if safe_dtypes[v['dtype']] != dtypes.bfloat16 
-          else cast_bfloat16(t[v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape'])) 
-          for k,v in metadata.items() if k != "__metadata__"}
-
-  def rebuild_tensor(v):
-    x = xs[8+json_len+v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape'])
-    return cast_bfloat16(x) if safe_dtypes[v['dtype']] == dtypes.bfloat16 else x
-  return {k: rebuild_tensor(v) for k,v in metadata.items() if k != "__metadata__"}
+  ts, json_len, metadata = safe_load_metadata(fn)
+  def build_tensor(v) -> Tensor:
+    t = ts[8+json_len+v['data_offsets'][0]:].cast(safe_dtypes[v['dtype']])[:prod(v['shape'])].reshape(v['shape'])
+    return cast_bfloat16(t) if safe_dtypes[v['dtype']] == dtypes.bfloat16 else t
+  return {k: build_tensor(v) for k, v in metadata.items() if k != "__metadata__"}
 
 def safe_save(tensors:Dict[str, Tensor], fn:str, metadata:Optional[Dict[str, Any]]=None):
   headers, offset = {}, 0
