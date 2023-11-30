914dbe1186363cf38be6b93414e271c18774d2bd
Davi Silva <blueridanus@gmail.com>
2023-12-01 02:31:16 +0700

    comma AI

--- examples/llama.py
+++ examples/llama.py
@@ -24,77 +24,77 @@ MODEL_PARAMS = {
   "1": {
     "7B": {
       "args": {"dim": 4096, "n_heads": 32, "n_layers": 32, "norm_eps": 1e-06, "vocab_size": 32000, "hidden_dim": 11008},
-      "files": 1
+      "files": 1,
     },
     "13B": {
       "args": {"dim": 5120, "n_heads": 40, "n_layers": 40, "norm_eps": 1e-06, "vocab_size": 32000, "hidden_dim": 13824},
-      "files": 2
+      "files": 2,
     },
     "30B": {
       "args": {"dim": 6656, "n_heads": 52, "n_layers": 60, "norm_eps": 1e-06, "vocab_size": 32000, "hidden_dim": 17920},
-      "files": 4
+      "files": 4,
     },
     "65B": {
       "args": {"dim": 8192, "n_heads": 64, "n_layers": 80, "norm_eps": 1e-05, "vocab_size": 32000, "hidden_dim": 22016},
-      "files": 8
-    }
+      "files": 8,
+    },
   },
   "2": {
     "7B": {
       "args": {"dim": 4096, "n_heads": 32, "n_layers": 32, "norm_eps": 1e-05, "vocab_size": 32000, "hidden_dim": 11008},
-      "files": 1
+      "files": 1,
     },
     "13B": {
       "args": {"dim": 5120, "n_heads": 40, "n_layers": 40, "norm_eps": 1e-05, "vocab_size": 32000, "hidden_dim": 13824},
-      "files": 2
+      "files": 2,
     },
     "70B": {
       "args": {"dim": 8192, "n_heads": 64, "n_kv_heads": 8, "n_layers": 80, "norm_eps": 1e-05, "vocab_size": 32000, "hidden_dim": 24576},
-      "files": 8
-    }
+      "files": 8,
+    },
   },
   "code": {
     "7B": {
       "args": {"dim": 4096, "n_layers": 32, "n_heads": 32, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32016, "hidden_dim": 11008},
-      "files": 1
+      "files": 1,
     },
     "7B-Python": {
       "args": {"dim": 4096, "n_layers": 32, "n_heads": 32, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32000, "hidden_dim": 11008},
-      "files": 1
+      "files": 1,
     },
     "7B-Instruct": {
       "args": {"dim": 4096, "n_layers": 32, "n_heads": 32, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32016, "hidden_dim": 11008},
-      "files": 1
+      "files": 1,
     },
     "13B": {
       "args": {"dim": 5120, "n_layers": 40, "n_heads": 40, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32016, "hidden_dim": 13824},
-      "files": 2
+      "files": 2,
     },
     "13B-Python": {
       "args": {"dim": 5120, "n_layers": 40, "n_heads": 40, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32000, "hidden_dim": 13824},
-      "files": 2
+      "files": 2,
     },
     "13B-Instruct": {
       "args": {"dim": 5120, "n_layers": 40, "n_heads": 40, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32016, "hidden_dim": 13824},
-      "files": 2
+      "files": 2,
     },
     "34B": {
       "args": {"dim": 8192, "n_layers": 48, "n_heads": 64, "n_kv_heads": 8, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32000, "hidden_dim": 22016},
-      "files": 4
+      "files": 4,
     },
     "34B-Python": {
       "args": {"dim": 8192, "n_layers": 48, "n_heads": 64, "n_kv_heads": 8, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32000, "hidden_dim": 22016},
-      "files": 4
+      "files": 4,
     },
     "34B-Instruct": {
       "args": {"dim": 8192, "n_layers": 48, "n_heads": 64, "n_kv_heads": 8, "norm_eps": 1e-05, "rope_theta": 1000000, "vocab_size": 32000, "hidden_dim": 22016},
-      "files": 4
-    }
+      "files": 4,
+    },
   },
   "tiny": {
     "1B": {
       "args": {"dim": 2048, "n_layers": 22, "n_heads": 32, "n_kv_heads": 4, "norm_eps": 1e-05, "vocab_size": 32000, "hidden_dim": 5632},
-      "files": 1
+      "files": 1,
     }
   }
 }
