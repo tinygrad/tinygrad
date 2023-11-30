25a249394274defeefbb21f2a097a122585b4926
Joey Donovan <jsphdnvn@gmail.com>
2023-11-30 09:33:47 -0500

    revert triton changes

--- extra/triton/triton.py
+++ extra/triton/triton.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Final, Callable, DefaultDict, Any
+from typing import Dict, List, Final, Callable, DefaultDict
 from collections import defaultdict
 from tinygrad.ops import UnaryOps, BinaryOps, TernaryOps, Op
 from tinygrad.helpers import DType, dtypes, ImageDType, DEBUG, getenv
@@ -42,11 +42,7 @@ def define_scalar(local_size, dtype, args):
 def uops_to_triton(function_name:str, uops:List[UOp]):
   local_size: List[int] = []
   depth = 1
-  dims: List[Any] = []
-  bufs: List[Any] = []
-  valid: List[str] = []
-  signatures: List[str] = []
-  kernel: List[str] = []
+  signatures, dims, bufs, kernel, valid = [], [], [], [], [] #type: ignore
 
   c: DefaultDict[str, int] = defaultdict(int)
   r: Dict[UOp, str] = {}
