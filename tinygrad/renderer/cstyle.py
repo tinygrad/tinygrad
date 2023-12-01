93ebfdecfa186eb8b5afd0c0e5c2d65287b80d41
qazal <qazal.software@gmail.com>
2023-12-01 07:17:54 +0000

    render the values correctly in cstyle. If a constant number is rendered in cstyle, it always defaults to the 32 bit version and we need to cast if it's any other size

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -52,8 +52,8 @@ class CStyleLanguage(NamedTuple):
   def render_const(self, x:Union[float,int], var_dtype) -> str:
     if math.isnan(x): val = "NAN"
     elif math.isinf(x): val = ("-" if x < 0 else "") + "INFINITY"
-    else: val = f"{float(x)}f" if dtypes.is_float(var_dtype) else f"{int(x)}"
-    return self.render_cast([val]*var_dtype.sz, var_dtype) if var_dtype.sz > 1 else val
+    else: val = f"{float(x)}f" if dtypes.is_float(var_dtype) else f"{int(x)}" if dtypes.is_int(var_dtype) else f"{bool(x)}".lower()
+    return self.render_cast([val]*var_dtype.sz, var_dtype) if var_dtype.sz > 1 or var_dtype not in [dtypes.float, dtypes.int, dtypes.bool] else val
 
   # returns a str expression of the loaded value with the output type
   def render_load(self, output_dtype, buf_name, buf_dtype, idx, local=False) -> str:
