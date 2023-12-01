79bcce863b6aad8b4d52af8d0ef9cc48c720b144
George Hotz <geohot@gmail.com>
2023-12-01 09:32:07 -0800

    apt clean

--- tinygrad/runtime/ops_hip.py
+++ tinygrad/runtime/ops_hip.py
@@ -68,7 +68,7 @@ class HIPDevice(Compiled):
   def __init__(self, device:str):
     self.device = int(device.split(":")[1]) if ":" in device else 0
     if self.device == 0 and not MOCKHIP: HIPDevice.default_arch_name = init_c_var(hip.hipDeviceProp_t(), lambda x: check(hip.hipGetDeviceProperties(x, self.device))).gcnArchName.decode()
-    
+
     from tinygrad.runtime.graph.hip import HIPGraph
     super().__init__(MallocAllocator if MOCKHIP else HIPAllocator(self.device), LinearizerOptions(device="HIP"), HIPRenderer, compile_hip, functools.partial(HIPProgram, self.device), HIPGraph)
   def synchronize(self): hip.hipDeviceSynchronize()
