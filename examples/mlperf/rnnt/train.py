ca0d7edacd5c2a5b1d93dcdb6ab65438be59f5ad
dkormann <dominikormann@outlook.com>
2023-12-01 16:08:47 +0100

    mask for different seq_length

--- examples/mlperf/rnnt/train.py
+++ examples/mlperf/rnnt/train.py
@@ -1,23 +1,20 @@
 # %%
-from tinygrad.shape.shapetracker import ShapeTracker
-from tinygrad.lazy import LazyBuffer
-from tinygrad.ops  import ConstBuffer
-from tinygrad.ops import UnaryOps, LoadOps
-from tinygrad.helpers import dtypes
 from tinygrad.graph import print_tree
-from tinygrad.tensor import Tensor, Function
+from tinygrad.helpers import dtypes
 from tinygrad.jit import TinyJit
-from tinygrad.nn.optim import Adam
-from matplotlib import pyplot as plt
 from tinygrad.nn import Linear, Embedding
+from tinygrad.nn.optim import Adam
 from tinygrad.nn.state import get_parameters
+from tinygrad.ops  import ConstBuffer, UnaryOps, LoadOps
+from tinygrad.tensor import Tensor, Function
+from tinygrad.lazy import LazyBuffer
+from tinygrad.shape.shapetracker import ShapeTracker
+
+from matplotlib import pyplot as plt
 import numpy as np
 
 # %%
-X = 20
-Y = 22
-B = 5
-C = 6
+X,Y,B,C = 20,22,5,6
 labels = Tensor(np.random.randint(0,C,size=(B,Y)))
 
 # %%
@@ -57,18 +54,16 @@ def shear(d:Tensor,value = 0):
     d = d.pad(((0,0),(0,Y),(0,0),(0,0)),value=value)
     d = d.transpose(1,2).reshape((B,-1,C))
     d = d[:,:(X+Y-1)*Y,:].realize()
-    d = d.reshape((B,Y,X+Y-1,C)).transpose(1,2)
-    return d
+    return d.reshape((B,Y,X+Y-1,C)).transpose(1,2)
 
 def unshear(x:Tensor):
     B,X,Y = x.shape
     x = x.reshape((B,-1,))
     x = x.pad(((0,0),(0,X),))
     x = x.reshape((B,X,Y+1))
-    x = x.shrink(((0,B),(0,X),(0,Y+1-X)))
-    return x
+    return x.shrink(((0,B),(0,X),(0,Y+1-X)))
 
-class Loss(Function):
+class TransducerLoss(Function):
 
     def forward(self, d:Tensor, labels:Tensor):
         self.B,self.X,self.Y,self.C = d.shape
@@ -83,20 +78,15 @@ class Loss(Function):
             Tensor(np.arange(self.B).reshape((-1,1,1))),
             Tensor(np.arange(self.X).reshape((1,-1,1))),
             Tensor(np.arange(self.Y).reshape((1,1,-1))),
-            self.labels.reshape((self.B,1,-1))
-        ].log()
+            self.labels.reshape((self.B,1,-1))].log()
 
         assert self.p.shape == (self.B, self.X, self.Y)
         self.a = [Tensor([0]*self.B).reshape(-1,1).pad(((0,0),(0,self.Y-1),),-inf).realize()]
 
         for x in range(0,self.X-1):
-            self.a.append(
-                logsumexp(
-                (self.a[-1] + self.skip[:,x,:]).realize(),
-                (self.a[-1][:,:-1].pad(((0,0),(1,0),),-inf).realize() + self.p[:,x,:-1].pad(((0,0),(1,0),),-inf)).realize()
-            ))
-        loss = -(self.a[-1][:,-1] + self.skip[:,-1,-1]).sum()
-        return loss.lazydata
+            self.a.append(logsumexp((self.a[-1] + self.skip[:,x,:]).realize(), (self.a[-1][:,:-1].pad(((0,0),(1,0),),-inf).realize() + self.p[:,x,:-1].pad(((0,0),(1,0),),-inf)).realize()))
+
+        return (-self.a[-1][:,-1] - self.skip[:,-1,-1]).sum().lazydata
     
     def backward(self, g):
         self.b = [None] * (self.X-1) + [Tensor([0]*self.B).reshape(-1,1).pad(((0,0),(self.Y-1,0),),-inf).realize()]
@@ -107,37 +97,19 @@ class Loss(Function):
                 self.b[x+1][:,1:].pad(((0,0),(0,1),),-inf).realize() + self.p[:,x,:].realize()
              )).realize()
 
-        self.skip_grad = None
-        self.p_grad = None
-        for a,b in zip(self.a[:-1], self.b[1:]):
+        self.skg, self.p_grad = None, None
 
+        for a,b in zip(self.a[:-1], self.b[1:]):
             sg = (a + b).reshape(self.B, 1,-1)
-            self.skip_grad = sg if self.skip_grad is None else self.skip_grad.cat(sg,dim=1).realize()
+            self.skg = sg if self.skg is None else self.skg.cat(sg,dim=1).realize()
             pg = a.unsqueeze(1) + b[:,1:].pad(((0,0),(0,1),),-inf).unsqueeze(1)
             self.p_grad = pg if self.p_grad is None else self.p_grad.cat(pg,dim=1).realize()
-        
 
-        self.skip_grad = Tensor.cat(self.skip_grad,(self.a[-1] + self.b[-1]).reshape(self.B, 1,-1),dim=1).realize()
-        self.skip_grad = self.skip_grad.transpose(1,2)
+        self.skg = (unshear(Tensor.cat(self.skg,(self.a[-1] + self.b[-1]).reshape(self.B, 1,-1),dim=1).realize().transpose(1,2)) - self.b[0][:,0].unsqueeze(1).unsqueeze(1)).exp().realize()
+        self.p_grad = (unshear(self.p_grad.pad(((0,0),(0,1),(0,0))).transpose(1,2)) +Tensor([1]*(self.Y-1) + [-inf]).unsqueeze(-1) - self.b[0][:,0].realize().unsqueeze(1).unsqueeze(1)).exp().realize()
+        self.p_grad = self.p_grad.unsqueeze(-1).mul(Tensor.eye(self.C-1)[self.labels].unsqueeze(2))
 
-        
-        # print(self.skip_grad[0].numpy())
-        # plt.show()
-
-        self.skip_grad = unshear(self.skip_grad) 
-        self.skip_grad = self.skip_grad - self.b[0][:,0].unsqueeze(1).unsqueeze(1)
-        self.skip_grad = Tensor.exp(self.skip_grad).realize()
-
-        self.p_grad = unshear(self.p_grad.pad(((0,0),(0,1),(0,0))).transpose(1,2))
-        self.p_grad = self.p_grad +Tensor([1]*(self.Y-1) + [-inf]).unsqueeze(-1)
-
-        self.p_grad -= self.b[0][:,0].realize().unsqueeze(1).unsqueeze(1)
-        self.p_grad =  Tensor.exp(self.p_grad).realize()
-
-        idx = Tensor.eye(self.C-1)[self.labels].unsqueeze(2)
-        self.p_grad = self.p_grad.unsqueeze(-1).mul(idx)
-
-        return (-Tensor.cat(self.p_grad,self.skip_grad.unsqueeze(-1), dim=-1)).transpose(1,2).realize().lazydata,None
+        return (-Tensor.cat(self.p_grad,self.skg.unsqueeze(-1), dim=-1)).transpose(1,2).realize().lazydata,None
 
 # %%
 def setup():
@@ -159,7 +131,7 @@ def analyse():
     d = model.distribution(labels)
     imshow(d[0])
     opt.zero_grad()
-    l = Loss.apply(d.softmax(-1),labels)
+    l = TransducerLoss.apply(d.softmax(-1),labels)
     ctx = l._ctx
     l.backward()
     imshow(d.grad[0])
@@ -171,7 +143,7 @@ def analyse():
 @TinyJit
 def step(model,labels):
     d = model.distribution(labels)
-    L = Loss.apply(d.softmax(-1),labels)
+    L = TransducerLoss.apply(d.softmax(-1),labels)
     opt.zero_grad()
     L.backward()
     opt.step()
@@ -186,3 +158,124 @@ for i in range(10):
 
 # %%
 analyse()
+
+# %% [markdown]
+# ## LSTM
+
+# %%
+from extra.models.rnnt import RNNT, LSTM
+from tinygrad.nn import Linear, Embedding
+from temp.lstm import LSTM
+from tinygrad.helpers import Timing
+
+# %%
+emb = Embedding(C,10)
+H = 100
+
+class RNN:
+    def __init__(self,dim:int):
+        self.dim = dim
+        self.layers = [
+            LSTM(dim,H,1,0),
+            LSTM(H,H,10,0),
+            LSTM(H,dim,1,0)]
+
+    def __call__(self,x:Tensor):
+        for l in self.layers:
+            x,_ = l(x,None)
+        return x
+
+rnn = RNN(10)
+opt = Adam(get_parameters(rnn))
+
+# %%
+x = emb(labels)
+# x = Tensor.uniform(20,4,10)
+y = x[:,1:].contiguous().realize()
+x = x[:,:-1].contiguous().realize()
+
+# %%
+@TinyJit
+def rnn_step(rnn,x,y):
+    p = rnn(x.T)
+    # loss = Tensor.sparse_categorical_crossentropy(p.T,y)
+    loss = (p.T-y).square().mean()
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    return loss.realize(),p.realize()
+
+# %%
+with Timing("all: "):
+    for i in range (500):
+        l,p = rnn_step(rnn,x,y)
+        if (i+1)%10==0:
+            print(f"\r {i}",l.numpy(),end="")
+    print()
+
+# %% [markdown]
+# ## RNNT
+
+# %%
+from examples.mlperf.rnnt.model import RNNT
+from tinygrad import Tensor
+
+# %%
+rnnt = RNNT()
+
+# %%
+Tensor.randint(5,15,low=0,high=29).max().numpy()
+
+# %%
+X = Tensor.uniform(20,5,240)
+X_lens = Tensor.ones(5)*20
+Y = Tensor.randint(5,15,high=29)
+
+# %%
+
+def dis(data_item):
+    enc, xlens = rnnt.encoder(X,X_lens)
+    preds,ylens = rnnt.prediction.__call__(Y,None,1)
+    d = rnnt.joint(enc,preds).softmax(-1)
+    return d, xlens,ylens,labels
+
+# %% [markdown]
+# ## real data
+
+# %%
+from data import iterate
+
+# %%
+i2c = list("abcdefghijklmnopqrstuvwxyz' ")+["<pad>"]
+c2i = dict(map(reversed,enumerate(i2c)))
+C = len(i2c) # the last index stands for either the skip or pad. thesee are different.
+def text_encode(s):
+    if type(s[0]) == str: s = [s]
+    lens = list(map(len,s))
+    maxlen = max(lens)
+    encs = [list(map(c2i.__getitem__,e)) + (maxlen-l) * [c2i['<pad>']] for e,l in zip(s,lens)]
+    encs = np.array(encs)
+    return encs,lens
+
+# %%
+X,Y = next(iterate(4))
+X,X_lens = X
+Y,Y_lens = text_encode(Y)
+X,X_lens = rnnt.encoder.__call__(Tensor(X),X_lens) # LSTM expects (N,B,D)
+Y,_ = rnnt.prediction(Tensor(Y),None,1)
+Y = Y.pad(((0,0),(1,0),(0,0)))
+d = rnnt.joint(X,Y).softmax(-1)
+
+maxx = max(X_lens)
+ar = Tensor.arange(maxx)
+mask = (ar.unsqueeze(1) < Tensor(X_lens)).T
+d = d * mask.unsqueeze(-1).unsqueeze(-1)
+skip_mask = Tensor.eye(128)[Tensor(Y_lens)].unsqueeze(1).mul(1-mask.unsqueeze(-1))
+skip_mask = skip_mask.unsqueeze(-1).mul(Tensor([0]*(C-1)+[1]).reshape((1,1,1,C)))
+d = d + skip_mask
+X,Y,d
+
+# %%
+imshow(d[0,:,:,-1])
+
+
