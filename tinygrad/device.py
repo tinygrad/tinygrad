16ad3e66c3ec8d311fc732fafd4bc508c30c607d
George Hotz <geohot@gmail.com>
2023-12-01 10:58:51 -0800

    order + webgpu test

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -47,7 +47,7 @@ class Buffer:
   def __repr__(self): return f"<buf device:{self.device} size:{self.size}>"
   def copy_(self, src:Buffer):
     assert self.size == src.size and self.dtype == src.dtype, "buffer copy size/dtype mismatch"
-    if type(self.allocator) is type(src.allocator) and hasattr(self.allocator, 'transfer'):
+    if hasattr(self.allocator, 'transfer') and type(self.allocator) is type(src.allocator):
       # fast path, used on HIP between GPUs
       self.allocator.transfer(self._buf, src._buf, self.size*self.dtype.itemsize)
       return
