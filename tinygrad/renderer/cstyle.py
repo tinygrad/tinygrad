c1f43af2df73632f9b18b0b4ea3e527042b4527e
qazal <qazal.software@gmail.com>
2023-12-03 02:51:55 +0200

    share const render

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -313,10 +313,9 @@ class WGSLLanguage(CStyleLanguage):
     return f"var<workgroup> {name}: array<f32,{size}>;"
 
   def render_const(self, x:Union[float,int], var_dtype) -> str:
-    if math.isnan(x): val = "nan()"
-    elif math.isinf(x): val = ("-" if x < 0 else "") + "0x1.fffffep+127f"
-    else: val = f"({x}" + ("" if dtypes.is_int(var_dtype) else "f") + ")"
-    return self.render_cast([val]*var_dtype.sz, var_dtype) if var_dtype.sz > 1 else val
+    if math.isnan(x): return "nan()"
+    elif math.isinf(x): return ("-" if x < 0 else "") + "0x1.fffffep+127f"
+    return f"({super().render_const(x, var_dtype)})"
 
   def render_kernel(self, function_name:str, kernel:List[str], bufs:List[Tuple[str,DType]], local_size:List[int], prekernel:List[str]) -> str:
     local_size = local_size[::-1] if local_size else [1]
