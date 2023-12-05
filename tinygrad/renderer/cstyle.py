43bc71c6004a7158b5373be60fde2117191f02dd
Steven Anderson <sdandersonz@icloud.com>
2023-12-05 13:35:33 -0800

    deleting A and B shared memory

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -159,21 +159,13 @@ def uops_to_cstyle(lang:CStyleLanguage, function_name:str, uops:List[UOp]) -> Tu
         kk("nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::row_major> b_frag;")
         kk("nvcuda::wmma::fragment<nvcuda::wmma::accumulator, 16, 16, 16, float> c_frag;")
         kk("nvcuda::wmma::fill_fragment(c_frag, 0.0f);")
-        kk("__shared__ half a[16*16];")
-        kk("__shared__ half b[16*16];")
         kk("__shared__ float c[16*16];")
-        for i in range(16*16):
-          kk(f"a[lidx1*16+{i%16}] = {r[vin[(i%16)]]};")
-        for i in range(16*16):
-          kk(f"b[lidx1+{i%16*16}] = {r[vin[(i%16)+16]]};")
-        kk("__syncthreads();")
-        kk("nvcuda::wmma::load_matrix_sync(a_frag, a, 16);")
-        kk("nvcuda::wmma::load_matrix_sync(b_frag, b, 16);")
+        kk("nvcuda::wmma::load_matrix_sync(a_frag, data1, 16);")
+        kk("nvcuda::wmma::load_matrix_sync(b_frag, data2, 16);")
         kk("nvcuda::wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);")
         kk("nvcuda::wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);")
         for i in range(8):
           kk(f"{output}[{i}] = c[lidx1+(lidx0*16)+{i*32}];")
-        kk("__syncthreads();")
         kk("}")
       else:
         raise NotImplementedError(f"WMMA not implemented for {args}")
