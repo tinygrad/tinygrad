a5dd7678d179917ad944dd85e76b78785d25a88b
Leo Prokh <lenyaprokh@gmail.com>
2023-12-05 23:25:21 +0300

    revert this

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -8,7 +8,7 @@ from tinygrad.renderer.cstyle import uops_to_cstyle, CStyleLanguage
 CLANG_PROGRAM_HEADER = '#include <math.h>\n#define max(x,y) ((x>y)?x:y)\n#define int64 long\n#define half __fp16\n#define uchar unsigned char\n#include <stdbool.h>\n'
 
 @diskcache
-def compile_clang(prg:str, header=CLANG_PROGRAM_HEADER) -> bytes:
+def compile_clang(prg:str, header:str=CLANG_PROGRAM_HEADER) -> bytes:
   # TODO: remove file write. sadly clang doesn't like the use of /dev/stdout here
   with tempfile.NamedTemporaryFile(delete=True) as output_file:
     subprocess.check_output(args=('clang -shared -O2 -Wall -Werror -x c -lm -fPIC --rtlib=compiler-rt - -o '+str(output_file.name)).split(), input=(header+prg).encode('utf-8'))
