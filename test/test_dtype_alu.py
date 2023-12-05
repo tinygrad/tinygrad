2b6c83e0959cbee95077cd08bfbd0f088ec26ccf
George Hotz <geohot@gmail.com>
2023-12-04 20:40:18 -0800

    floats

--- test/test_dtype_alu.py
+++ test/test_dtype_alu.py
@@ -14,17 +14,16 @@ MAX_EXAMPLES = 200
 def universal_test(a, b, dtype, op):
   tensor_value = (op(Tensor([a], dtype=dtype), Tensor([b], dtype=dtype))).numpy()
   numpy_value = op(np.array([a]).astype(dtype.np), np.array([b]).astype(dtype.np))
-  np.testing.assert_equal(tensor_value, numpy_value)
+  if dtype in dtypes_float: np.testing.assert_almost_equal(tensor_value, numpy_value)
+  else: np.testing.assert_equal(tensor_value, numpy_value)
 
 class TestDTypeALU(unittest.TestCase):
-  @unittest.skip("flaky")
   @settings(max_examples=MAX_EXAMPLES, deadline=None)
-  @given(st.floats(width=32), st.floats(width=32), st.sampled_from(binary_operations))
+  @given(st.floats(width=32, allow_subnormal=False), st.floats(width=32, allow_subnormal=False), st.sampled_from(binary_operations))
   def test_float32(self, a, b, op): universal_test(a, b, dtypes.float32, op)
 
-  @unittest.skip("flaky")
   @settings(max_examples=MAX_EXAMPLES, deadline=None)
-  @given(st.floats(width=16), st.floats(width=16), st.sampled_from(binary_operations))
+  @given(st.floats(width=16, allow_subnormal=False), st.floats(width=16, allow_subnormal=False), st.sampled_from(binary_operations))
   def test_float16(self, a, b, op): universal_test(a, b, dtypes.float16, op)
 
   @settings(max_examples=MAX_EXAMPLES, deadline=None)
@@ -53,6 +52,23 @@ class TestDTypeALU(unittest.TestCase):
   @given(st.integers(-2147483648, 2147483647), st.integers(-2147483648, 2147483647), st.sampled_from(binary_operations))
   def test_int32(self, a, b, op): universal_test(a, b, dtypes.int32, op)
 
+  @settings(max_examples=MAX_EXAMPLES, deadline=None)
+  @given(st.integers(-2147483648, 2147483647), st.integers(-2147483648, 2147483647), st.floats(width=32, allow_subnormal=False), st.sampled_from(binary_operations), st.sampled_from(binary_operations))
+  def test_int32_midcast_float(self, a, b, c, op1, op2):
+    at, bt, ct = Tensor([a], dtype=dtypes.int32), Tensor([b], dtype=dtypes.int32), Tensor([c], dtype=dtypes.float32)
+    an, bn, cn = np.array([a]).astype(np.int32), np.array([b]).astype(np.int32), np.array([c]).astype(np.float32)
+    tensor_value = op2(op1(at, bt).cast(dtypes.float32), ct).numpy()
+    numpy_value = op2(op1(an, bn).astype(np.float32), cn)
+    np.testing.assert_almost_equal(tensor_value, numpy_value)
+
+  @settings(max_examples=MAX_EXAMPLES, deadline=None)
+  @given(st.floats(width=32, allow_subnormal=False), st.floats(width=32, allow_subnormal=False), st.integers(-2147483648, 2147483647), st.sampled_from(binary_operations), st.sampled_from(binary_operations))
+  def test_float_midcast_int32(self, a, b, c, op1, op2):
+    at, bt, ct = Tensor([a], dtype=dtypes.float32), Tensor([b], dtype=dtypes.float32), Tensor([c], dtype=dtypes.int32)
+    an, bn, cn = np.array([a]).astype(np.float32), np.array([b]).astype(np.float32), np.array([c]).astype(np.int32)
+    tensor_value = op2(op1(at, bt).cast(dtypes.int32), ct).numpy()
+    numpy_value = op2(op1(an, bn).astype(np.int32), cn)
+    np.testing.assert_equal(tensor_value, numpy_value)
 
 if __name__ == '__main__':
   unittest.main()
