046febb4413cf9e31ace6e7f2fa0e171b11f18da
Francis Lata <francisalbertlata@gmail.com>
2023-12-06 18:57:18 +0000

    move losses to main model file - will refactor after

--- extra/models/mask_rcnn.py
+++ extra/models/mask_rcnn.py
@@ -2,12 +2,13 @@ import re
 import math
 import os
 import numpy as np
+import torch
 from pathlib import Path
 from tinygrad import nn
 from tinygrad.tensor import Tensor
 from tinygrad.helpers import dtypes, get_child, fetch
 from tinygrad.nn.state import torch_load
-from examples.mask_rcnn.losses import FastRCNNLossComputation
+from typing import Tuple
 from extra.models.resnet import ResNet
 from extra.models.retinanet import nms as _box_nms
 
@@ -278,6 +279,17 @@ class BoxList:
 
   def __len__(self):
     return self.bbox.shape[0]
+  
+  def copy_with_fields(self, fields, skip_missing=False):
+    bbox = BoxList(self.bbox, self.size, self.mode)
+    if not isinstance(fields, (list, tuple)):
+      fields = [fields]
+    for field in fields:
+      if self.has_field(field):
+        bbox.add_field(field, self.get_field(field))
+      elif not skip_missing:
+        raise KeyError("Field '{}' not found in {}".format(field, self))
+    return bbox
 
 
 def cat_boxlist(bboxes):
@@ -1121,8 +1133,11 @@ class RoIBoxHead:
         box_coder=BoxCoder(weights=(10., 10., 5., 5.)),
         cls_agnostic_bbox_reg=False
     )
-    # TODO: implement required properties
-    self.loss_evaluator = FastRCNNLossComputation()
+
+    matcher = Matcher(0.5, 0.5, allow_low_quality_matches=False)
+    box_coder = BoxCoder((10., 10., 5., 5.))
+    fg_bg_sampler = BalancedPositiveNegativeSampler(512, 0.25)
+    self.loss_evaluator = FastRCNNLossComputation(matcher, fg_bg_sampler, box_coder, cls_agnostic_bbox_reg=False)
 
   def __call__(self, features, proposals, targets=None):
     if Tensor.training:
@@ -1136,8 +1151,9 @@ class RoIBoxHead:
       result = self.post_processor((class_logits, box_regression), proposals)
       return x, result, {}
     
-    loss_classifier, loss_box_reg = self.loss_evaluator([class_logits], [box_regression])
-    return x, proposals, dict(loss_classifier=loss_classifier, loss_box_reg=loss_box_reg)
+    # loss_classifier, loss_box_reg = self.loss_evaluator([class_logits], [box_regression])
+    # return x, proposals, dict(loss_classifier=loss_classifier, loss_box_reg=loss_box_reg)
+    return x, proposals, {}
 
 
 class MaskPostProcessor:
@@ -1237,7 +1253,381 @@ def to_image_list(tensors, size_divisible=32):
     return ImageList(batched_imgs, image_sizes)
   else:
     raise TypeError("Unsupported type for to_image_list: {}".format(type(tensors)))
+  
+class Matcher(object):
+  """
+  This class assigns to each predicted "element" (e.g., a box) a ground-truth
+  element. Each predicted element will have exactly zero or one matches; each
+  ground-truth element may be assigned to zero or more predicted elements.
 
+  Matching is based on the MxN match_quality_matrix, that characterizes how well
+  each (ground-truth, predicted)-pair match. For example, if the elements are
+  boxes, the matrix may contain box IoU overlap values.
+
+  The matcher returns a tensor of size N containing the index of the ground-truth
+  element m that matches to prediction n. If there is no match, a negative value
+  is returned.
+  """
+
+  BELOW_LOW_THRESHOLD = -1
+  BETWEEN_THRESHOLDS = -2
+
+  def __init__(self, high_threshold, low_threshold, allow_low_quality_matches=False):
+    """
+    Args:
+        high_threshold (float): quality values greater than or equal to
+            this value are candidate matches.
+        low_threshold (float): a lower quality threshold used to stratify
+            matches into three levels:
+            1) matches >= high_threshold
+            2) BETWEEN_THRESHOLDS matches in [low_threshold, high_threshold)
+            3) BELOW_LOW_THRESHOLD matches in [0, low_threshold)
+        allow_low_quality_matches (bool): if True, produce additional matches
+            for predictions that have only low-quality match candidates. See
+            set_low_quality_matches_ for more details.
+    """
+    assert low_threshold <= high_threshold
+    self.high_threshold = high_threshold
+    self.low_threshold = low_threshold
+    self.allow_low_quality_matches = allow_low_quality_matches
+
+  def __call__(self, match_quality_matrix):
+    """
+    Args:
+        match_quality_matrix (Tensor[float]): an MxN tensor, containing the
+        pairwise quality between M ground-truth elements and N predicted elements.
+
+    Returns:
+        matches (Tensor[int64]): an N tensor where N[i] is a matched gt in
+        [0, M - 1] or a negative value indicating that prediction i could not
+        be matched.
+    """
+    if match_quality_matrix.numel() == 0:
+      # empty targets or proposals not supported during training
+      if match_quality_matrix.shape[0] == 0:
+          raise ValueError(
+              "No ground-truth boxes available for one of the images "
+              "during training")
+      else:
+          raise ValueError(
+              "No proposal boxes available for one of the images "
+              "during training")
+
+    # match_quality_matrix is M (gt) x N (predicted)
+    # Max over gt elements (dim 0) to find best gt candidate for each prediction
+    matched_vals, matches = torch.from_numpy(match_quality_matrix.numpy()).max(axis=0)
+    # matched_vals, matches = Tensor(matched_vals.numpy()), Tensor(matches.numpy())
+    if self.allow_low_quality_matches:
+        raise NotImplementedError
+        # all_matches = matches.clone()
+
+    # Assign candidate matches with low quality to negative (unassigned) values
+    below_low_threshold = matched_vals < self.low_threshold
+    between_thresholds = (matched_vals >= self.low_threshold) & (
+        matched_vals < self.high_threshold
+    )
+    matches[below_low_threshold] = Matcher.BELOW_LOW_THRESHOLD
+    matches[between_thresholds] = Matcher.BETWEEN_THRESHOLDS
+
+    if self.allow_low_quality_matches:
+        raise NotImplementedError
+        # self.set_low_quality_matches_(matches, all_matches, match_quality_matrix)
+
+    return Tensor(matches.numpy())
+
+  def set_low_quality_matches_(self, matches, all_matches, match_quality_matrix):
+    """
+    Produce additional matches for predictions that have only low-quality matches.
+    Specifically, for each ground-truth find the set of predictions that have
+    maximum overlap with it (including ties); for each prediction in that set, if
+    it is unmatched, then match it to the ground-truth with which it has the highest
+    quality value.
+    """
+    # For each gt, find the prediction with which it has highest quality
+    highest_quality_foreach_gt, _ = match_quality_matrix.max(dim=1)
+    # Find highest quality match available, even if it is low, including ties
+    gt_pred_pairs_of_highest_quality = torch.nonzero(
+        match_quality_matrix == highest_quality_foreach_gt[:, None]
+    )
+    # Example gt_pred_pairs_of_highest_quality:
+    #   tensor([[    0, 39796],
+    #           [    1, 32055],
+    #           [    1, 32070],
+    #           [    2, 39190],
+    #           [    2, 40255],
+    #           [    3, 40390],
+    #           [    3, 41455],
+    #           [    4, 45470],
+    #           [    5, 45325],
+    #           [    5, 46390]])
+    # Each row is a (gt index, prediction index)
+    # Note how gt items 1, 2, 3, and 5 each have two ties
+
+    pred_inds_to_update = gt_pred_pairs_of_highest_quality[:, 1]
+    matches[pred_inds_to_update] = all_matches[pred_inds_to_update]
+
+class BalancedPositiveNegativeSampler(object):
+  """
+  This class samples batches, ensuring that they contain a fixed proportion of positives
+  """
+
+  def __init__(self, batch_size_per_image, positive_fraction):
+    """
+    Arguments:
+        batch_size_per_image (int): number of elements to be selected per image
+        positive_fraction (float): percentage of positive elements per batch
+    """
+    self.batch_size_per_image = batch_size_per_image
+    self.positive_fraction = positive_fraction
+
+  def __call__(self, matched_idxs):
+    """
+    Arguments:
+        matched idxs: list of tensors containing -1, 0 or positive values.
+            Each tensor corresponds to a specific image.
+            -1 values are ignored, 0 are considered as negatives and > 0 as
+            positives.
+
+    Returns:
+        pos_idx (list[tensor])
+        neg_idx (list[tensor])
+
+    Returns two lists of binary masks for each image.
+    The first list contains the positive elements that were selected,
+    and the second list the negative example.
+    """
+    pos_idx = []
+    neg_idx = []
+    # TODO: optimize some of the ops to be tinygrad native
+    for matched_idxs_per_image in matched_idxs:
+      positive = torch.nonzero(torch.from_numpy(matched_idxs_per_image.numpy()) >= 1).squeeze(1)
+      negative = torch.nonzero(torch.from_numpy(matched_idxs_per_image.numpy()) == 0).squeeze(1)
+
+      num_pos = int(self.batch_size_per_image * self.positive_fraction)
+      # protect against not enough positive examples
+      num_pos = min(positive.numel(), num_pos)
+      num_neg = self.batch_size_per_image - num_pos
+      # protect against not enough negative examples
+      num_neg = min(negative.numel(), num_neg)
+
+      # randomly select positive and negative examples
+      perm1 = torch.randperm(positive.numel(), device=positive.device)[:num_pos]
+      perm2 = torch.randperm(negative.numel(), device=negative.device)[:num_neg]
+
+      pos_idx_per_image = positive[perm1]
+      neg_idx_per_image = negative[perm2]
+
+      # create binary mask from indices
+      pos_idx_per_image_mask = torch.zeros_like(
+          torch.from_numpy(matched_idxs_per_image.numpy()), dtype=torch.bool
+      )
+      neg_idx_per_image_mask = torch.zeros_like(
+          torch.from_numpy(matched_idxs_per_image.numpy()), dtype=torch.bool
+      )
+      pos_idx_per_image_mask[pos_idx_per_image] = 1
+      neg_idx_per_image_mask[neg_idx_per_image] = 1
+
+      pos_idx.append(Tensor(pos_idx_per_image_mask.numpy()))
+      neg_idx.append(Tensor(neg_idx_per_image_mask.numpy()))
+
+    return pos_idx, neg_idx
+
+class FastRCNNLossComputation:
+  def __init__(self, proposal_matcher, fg_bg_sampler, box_coder, cls_agnostic_bbox_reg=False):
+    self.proposal_matcher = proposal_matcher
+    self.fg_bg_sampler = fg_bg_sampler
+    self.box_coder = box_coder
+    self.cls_agnostic_bbox_reg = cls_agnostic_bbox_reg
+
+  def match_targets_to_proposals(self, proposal, target):
+    match_quality_matrix = boxlist_iou(target, proposal)
+    matched_idxs = self.proposal_matcher(match_quality_matrix)
+    # Fast RCNN only need "labels" field for selecting the targets
+    target = target.copy_with_fields("labels")
+    # get the targets corresponding GT for each proposal
+    # NB: need to clamp the indices because we can have a single
+    # GT in the image, and matched_idxs can be -2, which goes
+    # out of bounds
+    matched_targets = target[matched_idxs.maximum(0)]
+    return matched_targets, matched_idxs
+  
+  def prepare_targets(self, proposals, targets):
+    labels, regression_targets = [], []
+    for proposals_per_image, targets_per_image in zip(proposals, targets):
+      matched_targets, matched_idxs = self.match_targets_to_proposals(proposals_per_image, targets_per_image)
+
+      labels_per_images = matched_targets.get_field("labels").cast(dtypes.int64)
+
+      # Label background (below the low threshold)
+      labels_per_images = (matched_idxs == -1).where(0, labels_per_images)
+
+      # Label ignore proposals (between low and high thresholds)
+      labels_per_images = (matched_idxs == -2).where(-1, labels_per_images)
+
+      # compute regression targets
+      regression_targets_per_image = self.box_coder.encode(matched_targets.bbox, proposals_per_image.bbox)
+
+      labels.append(labels_per_images)
+      regression_targets.append(regression_targets_per_image)
+
+    return labels, regression_targets
+  
+  def subsample(self, proposals, targets):
+    labels, regression_targets = self.prepare_targets(proposals, targets)
+    sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
+
+    proposals = list(proposals)
+    # add corresponding label and regression_targets information to the bounding boxes
+    for labels_per_image, regression_targets_per_image, proposals_per_image in zip(labels, regression_targets, proposals):
+      proposals_per_image.add_field("labels", labels_per_image)
+      proposals_per_image.add_field("regression_targets", regression_targets_per_image)
+
+    # distributed sampled proposals, that were obtained on all feature maps
+    # concatenated via the fg_bg_sampler, into individual feature map levels
+    for img_idx, (pos_inds_img, neg_inds_img) in enumerate(zip(sampled_pos_inds, sampled_neg_inds)):
+      # TODO: optimize this to be in tinygrad
+      pos_inds_img, neg_inds_img = torch.from_numpy(pos_inds_img.numpy()), torch.from_numpy(neg_inds_img.numpy())
+      img_sampled_inds = Tensor(torch.nonzero(pos_inds_img | neg_inds_img).squeeze(1).numpy())
+      proposals_per_image = proposals[img_idx][img_sampled_inds]
+      proposals[img_idx] = proposals_per_image
+
+    self._proposals = proposals
+    return proposals
+  
+  def __call__(self, class_logits, box_regression):
+    class_logits = Tensor.cat(*class_logits)
+    box_regression = Tensor.cat(*box_regression)
+    device = class_logits.device
+
+    if not hasattr(self, "_proposals"):
+      raise RuntimeError("subsample needs to be called before")
+    
+    proposals = self._proposals
+
+    labels = Tensor.cat(*[proposal.get_field("labels") for proposal in proposals])
+    regression_targets = Tensor.cat(*[proposal.get_field("regression_targets") for proposal in proposals])
+
+    classification_loss = Tensor.binary_crossentropy(class_logits, labels)
+
+    # get indices that correspond to the regression targets for
+    # the corresponding ground truth labels, to be used with
+    # advanced indexing
+    sampled_pos_inds_subset = nonzero(Tensor(labels > 0)).squeeze(1)
+    labels_pos = labels[sampled_pos_inds_subset]
+    if self.cls_agnostic_bbox_reg:
+      map_inds = Tensor([4, 5, 6, 7], device=device)
+    else:
+      map_inds = 4 * labels_pos[:, None] + Tensor([0, 1, 2, 3], device=device)
+
+    box_loss = smooth_l1_loss(
+      box_regression[sampled_pos_inds_subset[:, None], map_inds],
+      regression_targets[sampled_pos_inds_subset],
+      beta=1,
+      size_average=False
+    )
+    box_loss = box_loss / labels.numel()
+    return classification_loss, box_loss
+
+class RPNLossComputation:
+  def __init__(self, proposal_matcher, fg_bg_sampler, box_coder, generate_labels_func):
+    self.proposal_matcher = proposal_matcher
+    self.fg_bg_sampler = fg_bg_sampler
+    self.box_coder = box_coder
+    self.generate_labels_func = generate_labels_func
+    self.discard_cases = ["not_visibility", "between_thresholds"]
+
+  def __call__(self, anchors, objectness, box_regression, targets) -> Tuple[Tensor, ...]:
+    anchors = [cat_boxlist(anchors_per_image) for anchors_per_image in anchors]
+    labels, regression_targets = self.prepare_targets(anchors, targets)
+    sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
+    sampled_pos_inds, sampled_neg_inds = nonzero(Tensor.cat(sampled_pos_inds)), nonzero(Tensor.cat(sampled_neg_inds))
+    sampled_inds = sampled_pos_inds.cat(sampled_neg_inds)
+
+    objectness, box_regression = concat_box_prediction_layers(objectness, box_regression)
+    objectness = objectness.squeeze()
+    labels, regression_targets = Tensor.cat(*labels), Tensor.cat(*regression_targets)
+
+    box_loss = smooth_l1_loss(box_regression[sampled_pos_inds], regression_targets[sampled_pos_inds], size_average=False) / (sampled_inds.numel())
+    objectness_loss = objectness[sampled_inds].binary_crossentropy_logits(labels[sampled_inds])
+    return objectness_loss, box_loss
+
+  def match_targets_to_anchors(self, anchor, target):
+    match_quality_matrix = boxlist_iou(target, anchor)
+    matched_idxs = self.proposal_matcher(match_quality_matrix)
+    matched_targets = target[matched_idxs.maximum(0)]
+    return matched_targets, matched_idxs
+
+  def prepare_targets(self, anchors, targets):
+    labels, regression_targets = [], []
+    for anchors_per_image, targets_per_image in zip(anchors, targets):
+      matched_targets, matched_idxs = self.match_targets_to_anchors(anchors_per_image, targets_per_image)
+
+      labels_per_images = self.generate_labels_func(matched_idxs)
+      labels_per_images = labels_per_images.cast(dtypes.float32)
+      labels_per_images = (matched_idxs == -1).where(0, labels_per_images)
+
+      if "not_visibility" in self.discard_cases:
+        mask = tilde(anchors_per_image.bbox).cast(dtypes.bool)
+        labels_per_images = mask.where(-1, labels_per_images)
+
+      if "between_thresholds" in self.discard_cases:
+        labels_per_images = (matched_idxs == -2).where(-1, labels_per_images)
+
+      regression_targets_per_image = self.box_coder.encode(matched_targets.bbox, anchors_per_image.bbox)
+      labels.append(labels_per_images)
+      regression_targets.append(regression_targets_per_image)
+
+    return labels, regression_targets
+
+def smooth_l1_loss(self:Tensor, Y:Tensor, beta:float = 1./9, size_average:bool = True) -> Tensor:
+  n = (self-Y).abs()
+  cond = n < beta
+  loss = cond.where(0.5 * n ** 2 / beta, n - 0.5 * beta)
+  if size_average: return loss.mean()
+  return loss.sum()
+
+def boxlist_iou(boxlist1:BoxList, boxlist2:BoxList) -> Tensor:
+  assert boxlist1.size == boxlist2.size, "boxlists should have the same size"
+  area1, area2 = boxlist1.area(), boxlist2.area()
+  box1, box2 = boxlist1.bbox, boxlist2.bbox
+  lt = Tensor.maximum(box1[:, None, :2], box2[:, :2])
+  rb = Tensor.minimum(box1[:, None, 2:], box2[:, 2:])
+  TO_REMOVE = 1
+  wh = (rb - lt + TO_REMOVE).maximum(0)
+  inter = wh[:, :, 0] * wh[:, :, 1]
+  return inter / (area1[:, None] + area2 - inter)
+
+def concat_box_prediction_layers(box_cls, box_regression) -> Tuple[Tensor, ...]:
+  box_cls_flattened, box_regression_flattened = [], []
+  # for each feature level, permute the outputs to make them be in the
+  # same format as the labels. Note that the labels are computed for
+  # all feature levels concatenated, so we keep the same representation
+  # for the objectness and the box_regression
+  for box_cls_per_level, box_regression_per_level in zip(box_cls, box_regression):
+    N, AxC, H, W = box_cls_per_level.shape
+    Ax4 = box_regression_per_level.shape[1]
+    A = Ax4 // 4
+    C = AxC // A
+    box_cls_per_level = permute_and_flatten(box_cls_per_level, N, A, C, H, W)
+    box_cls_flattened.append(box_cls_per_level)
+
+    box_regression_per_level = permute_and_flatten(box_regression_per_level, N, A, 4, H, W)
+    box_regression_flattened.append(box_regression_per_level)
+  # concatenate on the first dimension (representing the feature levels), to
+  # take into account the way the labels were generated (with all feature maps
+  # being concatenated as well)
+  box_cls = Tensor.cat(*box_cls_flattened, dim=1).reshape(-1, C)
+  box_regression = Tensor.cat(*box_regression_flattened, dim=1).reshape(-1, 4)
+  return box_cls, box_regression
+
+def generate_rpn_labels(matched_idxs): return matched_idxs >= 0
+
+# NOTE: implement this in tinygrad
+def nonzero(self:Tensor) -> Tensor: return Tensor(torch.from_numpy(self.numpy()).nonzero().squeeze(1).numpy())
+
+def tilde(x: Tensor) -> Tensor:
+  if x.dtype == dtypes.bool: return (1 - x).cast(dtypes.bool)
+  return (x + 1) * -1  # this seems to be what the ~ operator does in pytorch for non bool
 
 class MaskRCNN:
   def __init__(self, backbone: ResNet):
