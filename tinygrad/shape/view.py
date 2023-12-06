259ca5c0e7570b655f724d2bb8361172a24e69a6
sahamrit <amritsahu.iitkgp@gmail.com>
2023-12-06 13:35:08 +0530

    check what cases fail by removing this

--- tinygrad/shape/view.py
+++ tinygrad/shape/view.py
@@ -64,10 +64,6 @@ def _reshape_mask(view: View, new_shape:Tuple[sint, ...]) -> Tuple[Optional[Tupl
       if next_mask != (0, 1) and mask != (0, 1) and (next_mask[1] - next_mask[0] == 1): off += next_mask[0] * old_dim
       mask, old_dim = (next_mask[0] * old_dim + l, (next_mask[1] - 1) * old_dim + r), old_dim * next(r_shape, 1)
 
-  # TODO: can this be removed?
-  for mask in r_masks: # if the old shape has leading 1s, need to make sure their mask is (0,1)
-    if mask != (0, 1): return ((0, 0),) * len(new_shape), None, False
-
   return tuple(reversed(new_mask)), tuple(offsets), False
 
 @dataclass(frozen=True)
