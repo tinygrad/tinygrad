556a46fb6c4b164dd0a9939b443ffaaa2a778ae5
zibokapi <zibokapi@gmail.com>
2023-12-07 21:06:41 +0800

    no cap

--- strip_whitespace.sh
+++ strip_whitespace.sh
@@ -3,8 +3,8 @@
 # find all Python files in the staging area using git diff
 # and filter for Added or Modified files (AM) that match the "*.py" pattern
 git diff --cached --name-only --diff-filter=AM | grep '\.py$' | while read -r file; do
-    # Strip trailing whitespace from the file
+    # strip trailing whitespace from the file
     sed -i '' 's/ *$//' "$file"
-    # Stage the modified file
+    # stage the modified file
     git add "$file"
 done
