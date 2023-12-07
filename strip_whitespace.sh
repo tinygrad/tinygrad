80c138f2fbf24621fc487edcf96e3992e133afb1
zibokapi <zibokapi@gmail.com>
2023-12-07 20:42:06 +0800

    fix something

--- strip_whitespace.sh
+++ strip_whitespace.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-find tinygrad -type f -name "*.py" -exec sed -i '' 's/ *$//' '{}' ';'
+
+# find all Python files in the staging area using git diff
+# and filter for added or modified files that match the "*.py" pattern
+git diff --cached --name-only --diff-filter=AM | grep '\.py$' | while read -r file; do
+    # Strip trailing whitespace from the file
+    sed -i '' 's/ *$//' "$file"
+    # Stage the modified file
+    git add "$file"
+done
+
+# #!/bin/bash
+# find tinygrad -type f -name "*.py" -exec sed -i '' 's/ *$//' '{}' ';'
