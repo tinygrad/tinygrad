65d5c6739ab93c9b3e91fd181253f40323778467
qazal <qazal.software@gmail.com>
2023-12-08 10:27:01 +0200

    skip CUDA

--- test/unit/test_disk_tensor.py
+++ test/unit/test_disk_tensor.py
@@ -27,7 +27,8 @@ class TestTorchLoad(unittest.TestCase):
 
   # for GPU, cl_khr_fp16 isn't supported
   # for LLVM, it segfaults because it can't link to the casting function
-  @unittest.skipIf(Device.DEFAULT in ["GPU", "LLVM"] and CI, "fp16 broken in some backends")
+  # CUDACPU architecture is sm_35 but we need at least sm_70 to run fp16 ALUs
+  @unittest.skipIf(Device.DEFAULT in ["GPU", "LLVM", "CUDA"] and CI, "fp16 broken in some backends")
   @unittest.skipIf(Device.DEFAULT == "TORCH", "torch doesn't support the way we load bfloat (cast to uint32)")
   def test_load_llama2bfloat(self): compare_weights_both("https://huggingface.co/qazalin/bf16-lightweight/resolve/main/consolidated.00.pth?download=true")
 
