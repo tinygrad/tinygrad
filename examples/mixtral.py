edbd80b854679a41b1f36dc7d4a4db7f58d2df13
George Hotz <geohot@gmail.com>
2023-12-09 19:07:08 +0000

    simpler

--- examples/mixtral.py
+++ examples/mixtral.py
@@ -1,8 +1,8 @@
-from typing import Union, Optional
+import functools
 from tqdm import tqdm
-from tinygrad import Tensor, Variable, nn, Device, dtypes
+from tinygrad import Tensor, nn, Device, dtypes
 from tinygrad.nn.state import torch_load, get_state_dict
-from extra.models.llama import Attention, FeedForward, RMSNorm, Transformer
+from extra.models.llama import FeedForward, Transformer
 
 def fake_bfloat16_to_float16(x, device):
   return x.to("CLANG").contiguous().to(device).cast(dtypes.uint32).mul(1<<16).contiguous().bitcast(dtypes.float32).half()
@@ -25,20 +25,9 @@ class MixtureFeedForward:
     ret = e1(x.to(e1_dev)).to(x.device) * (top[0][1]/norm) + e2(x.to(e2_dev)).to(x.device) * (top[1][1]/norm)
     return ret
 
-class MixtureTransformerBlock:
-  def __init__(self, dim:int, hidden_dim:int, n_heads:int, n_kv_heads:int, norm_eps:float, max_context:int, linear=nn.Linear):
-    self.attention = Attention(dim, n_heads, n_kv_heads, max_context, linear)
-    self.attention_norm = RMSNorm(dim, norm_eps)
-    self.ffn_norm = RMSNorm(dim, norm_eps)
-    self.feed_forward = MixtureFeedForward(8, dim, hidden_dim, linear)
-
-  def __call__(self, x:Tensor, start_pos:Union[Variable,int], freqs_cis:Tensor, mask:Optional[Tensor]):
-    h = x + self.attention(self.attention_norm(x), start_pos, freqs_cis, mask)
-    return (h + self.feed_forward(self.ffn_norm(h))).realize()
-
 if __name__ == "__main__":
   state = torch_load("/home/tiny/Downloads/mixtral-8x7b-32kseqlen/consolidated.00.pth.b")
-  model = Transformer(n_layers=32, dim=4096, hidden_dim=14336, n_heads=32, n_kv_heads=8, norm_eps=1e-5, vocab_size=32000, tf_block=MixtureTransformerBlock, jit=False)
+  model = Transformer(n_layers=32, dim=4096, hidden_dim=14336, n_heads=32, n_kv_heads=8, norm_eps=1e-5, vocab_size=32000, feed_forward=functools.partial(MixtureFeedForward, 8), jit=False)
   model_state_dict = get_state_dict(model)
 
   for k in (t := tqdm(state)):
