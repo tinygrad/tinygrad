6a7940326d4a7a98c41616cc7d8abd2787f6c37f
rodfer0x80 <rodrigolf080@gmail.com>
2023-12-10 01:05:03 +0000

    delete pink square for linter

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -85,7 +85,7 @@ def torch_load(fn:str):
     # upstream LLaMA also does this conversion:
     # https://github.com/facebookresearch/llama/blob/6c7fe276574e78057f917549435a2554000a876d/llama/generation.py#L95
     # TODO: should this be done in the example instead? or maybe we don't need this anymore with better bfloat16 support
-    if storage[1] == dtypes.bfloat16: ret = cast_bfloat16(ret) 
+    if storage[1] == dtypes.bfloat16: ret = cast_bfloat16(ret)
     else: ret = ret.cast(storage[1])
 
     # 7 lines to deal with permuted tensors. NOTE: this currently requires reading off the disk
