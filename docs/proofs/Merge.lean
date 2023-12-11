81d11aa30709fd3330564c1d060c7638031e4943
Paul Gustafson <paul.gustafson@theambrusgroup.com>
2023-12-10 23:07:02 -0800

    masks

--- docs/proofs/Merge.lean
+++ docs/proofs/Merge.lean
@@ -1,20 +1,33 @@
 def Tuple (α : Type) (n : Nat) :=
   { as : List α // as.length = n }
 
-
 structure View (n : Nat) where
-  shape : Tuple Nat n
---  strides : Tuple Nat n
---  mask : Tuple Nat n
   offset : Nat
+  shape : Tuple Nat n
+  min : Tuple Nat n
+  max : Tuple Nat n
+  stride : Tuple Nat n
+
 
 def size {n : Nat} (v : View n) : Nat :=
   List.foldl (λ x y ↦ x * y) 1 v.shape.val
 
-def idxs {n : Nat} (v : View n) : Type :=
-  { n : Nat // n >= v.offset ∧ n < v.offset + size v }
 
+def proj {n : Nat} (v : View n) (i : Fin n)  (j : Nat) : Nat :=
+  (j - v.offset) % (List.get v.stride.val (Fin.mk i sorry))
+
+
+def valid {n : Nat} (v : View n) (i : Fin n) (j : Nat) : Prop :=
+  (proj v i j) >= List.get v.min.val (Fin.mk i sorry)
+  ∧ (proj v i j) <= List.get v.max.val (Fin.mk i sorry)
+
+
+def idxs {n : Nat} (v : View n) : Type :=
+  { j : Nat
+    // j >= v.offset
+    ∧  j < v.offset + size v
+    ∧  forall i : Fin n, valid v i j}
 
 def reshapeable {n m : Nat} (v : View n) (w: View m)
   (f : idxs v → idxs w) (g : idxs w → idxs v) : Prop :=
-  (size v = size w) → (f ∘ g = id) ∧ (g ∘ f = id)
+  (f ∘ g = id) ∧ (g ∘ f = id)
