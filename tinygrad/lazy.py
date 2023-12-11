cd5700708ee4f9902a6b3a8a93b6056c67f53613
George Hotz <geohot@gmail.com>
2023-12-11 14:49:45 -0800

    new lazy is doing something

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -8,26 +8,39 @@ from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.device import Buffer
 
 class LazyBuffer:
-  def __init__(self, device:str, st:ShapeTracker, dtype:DType, op:Optional[Op]=None, arg:Any=None, src:Tuple[LazyBuffer, ...]=(), realized:Optional[Buffer]=None, base:Optional[LazyBuffer]=None):
+  def __init__(self, device:str, st:ShapeTracker, dtype:DType, op:Optional[Op]=None, arg:Any=None, srcs:Tuple[LazyBuffer, ...]=(), realized:Optional[Buffer]=None, base:Optional[LazyBuffer]=None):
     self.device, self.st, self.dtype = device, st, dtype
     self.shape = self.st.shape
+    assert base is None or base.base == base
     self._base = base
-    self.op, self.arg, self.src = op, arg, src
-    self.realized = realized
+    self.op, self.arg, self.srcs = op, arg, srcs  # this is a LazyOp, except the src is LazyBuffers and not LazyOps
+    self._realized = realized
+    self.output_buffer: Optional[Buffer] = None
+
+  def __repr__(self) -> str:
+    return f"<LB {self.device} {self.shape} contig:{self.st.contiguous} {self.op if self.op else self.realized}>"
 
   @property
   def base(self) -> LazyBuffer: return self._base if self._base is not None else self
+
+  @property
+  def realized(self): return self.base._realized
+
+  def const(self, val:Union[float, int]) -> LazyBuffer: return LazyBuffer(self.device, ShapeTracker.from_shape(self.shape), self.dtype.scalar(), LoadOps.CONST, val)
+
+  # NOTE: this no longer breaks the graph
+  def contiguous(self): return self if self.st.contiguous else self.e(LoadOps.CONTIGUOUS)
 
   def is_unrealized_const(self): return not self.realized and self.base.op == LoadOps.CONST
   def is_unrealized_contiguous_const(self): return not self.realized and self.op == LoadOps.CONST
 
   @staticmethod
   def fromCPU(x: np.ndarray) -> LazyBuffer:
-    return LazyBuffer(["CPU"], ShapeTracker.from_shape(x.shape), dtypes.from_np(x.dtype), realized=[Buffer("CPU", prod(x.shape), dtypes.from_np(x.dtype), x.flatten())])
+    return LazyBuffer("CPU", ShapeTracker.from_shape(x.shape), dtypes.from_np(x.dtype), realized=Buffer("CPU", prod(x.shape), dtypes.from_np(x.dtype), x.flatten()))
 
   def copy_to_device(self, device:str) -> LazyBuffer:
-    out = self.e(LoadOps.CONTIGUOUS)
-    return LazyBuffer(device, out.st, out.dtype, LoadOps.COPY, src=(out,))    # TODO: rename to LoadOps.COPY
+    out = self.contiguous()
+    return LazyBuffer(device, out.st, out.dtype, LoadOps.COPY, srcs=(out,))    # TODO: rename to LoadOps.COPY
 
   def e(self:LazyBuffer, op:Union[LoadOps, UnaryOps, BinaryOps, TernaryOps], *srcs:LazyBuffer, arg:Optional[Any]=None) -> LazyBuffer:
     srcs = (self,)+srcs
@@ -36,9 +49,9 @@ class LazyBuffer:
   def r(self:LazyBuffer, op:ReduceOps, new_shape:Tuple[sint, ...]) -> LazyBuffer:
     return LazyBuffer(self.device, ShapeTracker.from_shape(new_shape), self.dtype, op, new_shape, (self,))
 
-  def reshape(self:LazyBuffer, arg:Tuple[sint, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.reshape(arg), self.dtype, base=self)
-  def pad(self:LazyBuffer, arg:Tuple[Tuple[int, int], ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.pad(arg), self.dtype, base=self)
-  def expand(self: LazyBuffer, arg:Tuple[sint, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.expand(arg), self.dtype, base=self)
-  def permute(self: LazyBuffer, arg:Tuple[int, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.permute(arg), self.dtype, base=self)
-  def shrink(self:LazyBuffer, arg:Tuple[Tuple[sint, sint], ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.shrink(arg), self.dtype, base=self)
-  def stride(self:LazyBuffer, arg:Tuple[int, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.stride(arg), self.dtype, base=self)
+  def reshape(self:LazyBuffer, arg:Tuple[sint, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.reshape(arg), self.dtype, base=self.base)
+  def pad(self:LazyBuffer, arg:Tuple[Tuple[int, int], ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.pad(arg), self.dtype, base=self.base)
+  def expand(self: LazyBuffer, arg:Tuple[sint, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.expand(arg), self.dtype, base=self.base)
+  def permute(self: LazyBuffer, arg:Tuple[int, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.permute(arg), self.dtype, base=self.base)
+  def shrink(self:LazyBuffer, arg:Tuple[Tuple[sint, sint], ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.shrink(arg), self.dtype, base=self.base)
+  def stride(self:LazyBuffer, arg:Tuple[int, ...]) -> LazyBuffer: return LazyBuffer(self.device, self.st.stride(arg), self.dtype, base=self.base)
--- tinygrad/realize.py
+++ tinygrad/realize.py
@@ -1,9 +1,10 @@
-from typing import List, Dict, Optional
-from tinygrad.ops import LoadOps, ScheduleItem
+from typing import List, Dict, Optional, Set, Tuple, Any
+from tinygrad.ops import LoadOps, ScheduleItem, LazyOp, BufferOps, MemBuffer, ConstBuffer, vars_from_ast, get_lazyop_info
 from tinygrad.device import Device, Buffer, BufferCopy, JITRunner
 from tinygrad.graph import log_schedule_item, print_tree
-from tinygrad.helpers import prod
+from tinygrad.helpers import prod, dedup, merge_dicts, ImageDType, DEBUG
 from tinygrad.shape.symbolic import Variable
+from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.lazy import LazyBuffer
 
 class CustomOp(JITRunner):
@@ -29,14 +30,117 @@ def run_schedule(schedule:List[ScheduleItem], disable_logging=False):
     prg = lower_schedule_item(si)
 
     # we don't have an output buffer, we have to create it, and create to max size if it has symbolic shape
-    si.out.realized = si.out.output_buffer if si.out.output_buffer is not None else \
+    assert si.out._base is None, "no setting of non-base LazyBuffers"
+    si.out._realized = si.out.output_buffer if si.out.output_buffer is not None else \
       Buffer(si.out.device, prod((s if isinstance(s, int) else s.max for s in si.out.shape)), si.out.dtype)
     del si.out.op
-    for v in si.out.views: del v.op
+    #for v in si.out.views: del v.op
 
     # run the function (put it in JIT)
     if prg: prg.exec([si.out.realized] + [x.realized for x in si.inputs], si.var_vals)
 
-def create_schedule(out:List[LazyBuffer]) -> List[ScheduleItem]:
-  print(out)
-  pass
+def _recursive_get_lazyop(buf:LazyBuffer, inputs:List[LazyBuffer], first=True) -> LazyOp:
+  if not buf.realized:
+    if buf.base.op == LoadOps.CONST: return LazyOp(BufferOps.CONST, (), ConstBuffer(float(buf.base.arg), buf.dtype, buf.st.simplify().unbind()))
+    if buf.op == LoadOps.CONTIGUOUS and first: return _recursive_get_lazyop(buf.srcs[0], inputs, True)
+    if buf.op == LoadOps.COPY and first:
+      assert buf.st.contiguous
+      inputs.append(buf.srcs[0].base)
+      return LazyOp(LoadOps.COPY, (), buf.srcs[0].base)
+  #if buf.realized or not buf.st.contiguous or buf.base.op in LoadOps:  # no mop pushing
+  if buf.realized or buf != buf.base or buf.base.op in LoadOps:
+    if buf.base not in inputs: inputs.append(buf.base)
+    op = LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.index(buf.base)+1, buf.dtype, buf.st.simplify().unbind()))
+  else:
+    assert buf.op is not None
+    op = LazyOp(buf.op, tuple(_recursive_get_lazyop(x, inputs, False) for x in buf.srcs), buf.arg)
+  #print("store", first, buf.realized, buf.op, op)
+  return LazyOp(BufferOps.STORE, (op, ), MemBuffer(0, buf.dtype, buf.st)) if first else op
+
+#def _recursive_get_lazyop(op:LazyOp, srcs:Tuple[LazyBuffer], arg:Any) -> LazyOp:
+#  if src.realized: return LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.index(x.base)+1, x.dtype, st))
+#  if not src.realized:
+#  return LazyOp(op, )
+
+def _schedule_one(out:LazyBuffer, seen:Optional[Set[LazyBuffer]]) -> List[ScheduleItem]:
+  if out in seen or out.realized or out.is_unrealized_const(): return []
+  seen.add(out)
+  if out.base is not out: return _schedule_one(out.base, seen)
+  if out.op == LoadOps.CONTIGUOUS: return _schedule_one(out.srcs[0], seen)
+
+  #print(out)
+
+  inputs: List[LazyBuffer] = []
+  op = _recursive_get_lazyop(out, inputs)
+  ret: List[ScheduleItem] = []
+  for x in inputs:
+    assert x.base == x, f"all inputs must be base, {x} isn't"
+    ret += _schedule_one(x, seen)
+
+  from tinygrad.graph import print_tree
+  print_tree(op)
+  return ret + [ScheduleItem(op, out, tuple(inputs), {})]
+
+
+  print(op)
+
+
+  # schedule the past
+  ret:List[ScheduleItem] = []
+  inputs: List[LazyBuffer] = dedup([x.base for x in out.src if not x.is_unrealized_const()])
+  for x in inputs: ret += _schedule_one(x, seen)
+
+  # replace srcs
+  srcs_op: List[LazyOp] = []
+  for x in out.src:
+    st = x.st.simplify().unbind()
+    if x.base in inputs:
+      srcs_op.append(LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.index(x.base)+1, x.dtype, st)))
+    elif not x.realized and x.base.op.op == LoadOps.CONST:
+      srcs_op.append(LazyOp(BufferOps.CONST, (), ConstBuffer(float(x.base.op.arg), x.dtype, st)))
+    else:
+      raise NotImplementedError(f"not handled {x}")
+  op = LazyOp(out.op, tuple(srcs_op), out.arg)
+
+  # check if we can reuse the output buffer
+  # if it's aliased, don't use it
+  # TODO: this is pretty wrong actually, who knows where else this buffer is used?
+  # TODO: what if an assign is required? this silently is wrong
+  # NOTE: this has been moved to schedule, as this is only an issue if buffers are already realized
+  """
+  if out.output_buffer is not None:
+    for i,a in enumerate(inputs):
+      # TODO: if this is contiguous it's fine
+      if a.realized == out.output_buffer:
+        if any(not x.arg.st.contiguous for x in op.get_lazyops() if x.op == BufferOps.LOAD and x.arg.idx == i+1):
+          out.output_buffer = None
+          break
+  """
+
+  if op.op not in LoadOps:
+    # add the store
+    info = get_lazyop_info(op)
+    assert info.dtype == out.dtype or isinstance(out.dtype, ImageDType), f"dtype mismatch {info.dtype=} != {out.dtype=}"
+
+    #if isinstance(self.dtype, ImageDType) and (prod(self.shape) != prod(self.dtype.shape) or not any(self.shape[x]%4 == 0 for x in self.st.unit_stride_axes())):
+    #  if DEBUG >= 3: print(f"forcing image {self.dtype} to float32")
+    #  self.dtype = dtypes.float32  # NOTE; this is what makes the dtype above not match
+    #  op = LazyOp(UnaryOps.CAST, (op, ), (dtypes.float32, False))
+
+    # TODO: why doesn't this match?
+    #assert info.shape == self.shape, f"shape mismatch {info.shape=} != {self.shape=}"
+    op = LazyOp(BufferOps.STORE, (op, ), MemBuffer(0, out.dtype, ShapeTracker.from_shape(info.shape)))
+  else:
+    # check loadop validity of bufferops
+    for i,s in enumerate(op.src): assert isinstance(s, LazyOp) and s.op == BufferOps.LOAD and s.arg.idx == i+1 and s.arg.st.contiguous, f"bad LoadOps src {i}: {s}"
+
+  # return scheduleitem
+  var_vals = merge_dicts([out.st.var_vals] + [buf.st.var_vals for buf in inputs])
+  ret.append(ScheduleItem(op, out, tuple(inputs), {k:var_vals[k] for k in vars_from_ast(op)}))
+  return ret
+
+def create_schedule(outs:List[LazyBuffer]) -> List[ScheduleItem]:
+  ret = []
+  seen = set()
+  for out in outs: ret += _schedule_one(out, seen)
+  return ret
