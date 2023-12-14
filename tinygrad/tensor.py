149d8f935980ee7d4aa9292580ba513b0cc24b7e
Kevin Herro <kvherro@gmail.com>
2023-12-14 11:31:42 -0800

    match tensor.split api

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -448,12 +448,12 @@ class Tensor:
     final_shape = [r*s for r,s in zip(repeats, base_shape)]
     return self.reshape(new_shape).expand(expand_shape).reshape(final_shape)
 
-  def split(self, split_sizes:Union[int, List[int]], dim:int=0) -> Tuple[Tensor, ...]:
+  def split(self, split_size_or_sections:Union[int, List[int]], dim:int=0) -> Tuple[Tensor, ...]:
     if self.ndim < 1: raise RuntimeError("split expects at least a 1-dimensional tensor")
     if not -self.ndim <= dim < self.ndim: raise IndexError(f"Dimension out of range (expected to be in range of [{-self.ndim if self.ndim > 0 else self.ndim-1}, {self.ndim-1 if self.ndim > 0 else self.ndim}], but got {dim})") # noqa: E501
-    if isinstance(split_sizes, int): return self._split_with_size(split_sizes, dim)
-    if isinstance(split_sizes, list) and all(isinstance(x, int) for x in split_sizes): return self._split_with_sizes(split_sizes, dim)
-    raise TypeError(f"Expected split_sizes to be int or list of ints, got {type(split_sizes)}")
+    if isinstance(split_size_or_sections, int): return self._split_with_size(split_size_or_sections, dim)
+    if isinstance(split_size_or_sections, list) and all(isinstance(x, int) for x in split_size_or_sections): return self._split_with_sections(split_size_or_sections, dim) # noqa: E501
+    raise TypeError(f"Expected split_size_or_sections to be int or list of ints, got {type(split_size_or_sections)}")
 
   def _split_with_size(self, split_sizes:int, dim:int=0) -> Tuple[Tensor, ...]:
     assert all_int(self.shape), f"does not support symbolic shape {self.shape}"
@@ -461,11 +461,11 @@ class Tensor:
     if split_sizes == 0 and self.shape[dim] > 0: raise RuntimeError(f"split_sizes can only be 0 if dimension size is 0, but got dimension size of {self.shape[dim]}") # noqa: E501
     return tuple(self.chunk(math.ceil(self.shape[dim]/split_sizes)))
 
-  def _split_with_sizes(self, split_sizes:List[int], dim:int=0) -> Tuple[Tensor, ...]:
+  def _split_with_sections(self, sections:List[int], dim:int=0) -> Tuple[Tensor, ...]:
     assert all_int(self.shape), f"does not support symbolic shape {self.shape}"
-    if sum(split_sizes) != self.shape[dim]: raise RuntimeError(f"_split_with_sizes expects split_sizes to sum exactly to {self.shape[dim]} (input tensor's size at dimension {dim}), but got split_sizes={split_sizes}") # noqa: E501
-    if any(x < 0 for x in split_sizes): raise RuntimeError(f"_split_with_sizes expects split_sizes have only non-negative entries, but got split_sizes={split_sizes}") # noqa: E501
-    slice_indices = [sum(split_sizes[:i]) for i in range(len(split_sizes) + 1)]
+    if sum(sections) != self.shape[dim]: raise RuntimeError(f"_split_with_sections expects sections to sum exactly to {self.shape[dim]} (input tensor's size at dimension {dim}), but got sections={sections}") # noqa: E501
+    if any(x < 0 for x in sections): raise RuntimeError(f"_split_with_sections expects sections have only non-negative entries, but got sections={sections}") # noqa: E501
+    slice_indices = [sum(sections[:i]) for i in range(len(sections) + 1)]
     slice_params = [[slice(None)]*dim + [slice(slice_indices[i], slice_indices[i + 1])] for i in range(len(slice_indices) - 1)]
     return tuple(self.__getitem__(tuple(sl)) for sl in slice_params)
 
