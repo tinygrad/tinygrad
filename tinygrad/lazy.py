42df634aad0968fb8dfd5d2ea93686cdd12f936f
George Hotz <geohot@gmail.com>
2023-12-16 07:58:36 -0800

    bugs w/o this

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -230,6 +230,7 @@ def _recurse_lb(buf:LazyBuffer, realizes:Set[LazyBuffer], allbufs:Dict[LazyBuffe
 UNSAFE_PAD_OPS = {BinaryOps.DIV, BinaryOps.CMPLT, UnaryOps.LOG2, UnaryOps.EXP2, UnaryOps.RECIP}
 def _is_padding_okay(buf:LazyBuffer, realizes:Set[LazyBuffer]) -> bool:
   if buf in realizes or buf.realized: return True
+  return False
   if buf.op in UNSAFE_PAD_OPS: return False
   return all(_is_padding_okay(x.base, realizes) for x in buf.srcs)
 
