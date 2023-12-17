5d6403a1ca0d3e8a026561209426c658f8e0b447
George Hotz <geohot@gmail.com>
2023-12-16 20:20:07 -0800

    parsing command buffers

--- extra/qcom_gpu_driver/opencl_ioctl.py
+++ extra/qcom_gpu_driver/opencl_ioctl.py
@@ -4,6 +4,17 @@ from tinygrad.runtime.ops_gpu import CLDevice, CLAllocator
 import pathlib, sys
 sys.path.append(pathlib.Path(__file__).parent.parent.parent.as_posix())
 
+ops = {}
+import xml.etree.ElementTree as ET
+xml = ET.parse(pathlib.Path(__file__).parent / "adreno_pm4.xml")
+for child in xml.getroot():
+  if 'name' in child.attrib and child.attrib['name'] == "adreno_pm4_type3_packets":
+    for sc in child:
+      if 'name' in sc.attrib and ('variants' not in sc.attrib or sc.attrib['variants'] != "A2XX"):
+        ops[int(sc.attrib['value'], 0x10)] = sc.attrib['name']
+#print(ops)
+#exit(0)
+
 from extra.qcom_gpu_driver import msm_kgsl
 def ioctls_from_header():
   hdr = (pathlib.Path(__file__).parent.parent.parent / "extra/qcom_gpu_driver/msm_kgsl.h").read_text().replace("\\\n", "")
@@ -34,22 +45,37 @@ def get_mem(addr, vlen):
     if k <= addr and addr < k+len(v):
       return v[addr-k:addr-k+vlen]
 
+def hprint(vals):
+  ret = []
+  for v in vals:
+    if v > 31: ret.append(f"{v:#x}")
+    else: ret.append(f"{v}")
+  return f"({','.join(ret)})"
+
 def parse_cmd_buf(dat):
   ptr = 0
   while ptr < len(dat):
     cmd = struct.unpack("I", dat[ptr:ptr+4])[0]
     if (cmd>>24) == 0x70:
+      # packet with opcode and opcode specific payload (replace pkt3)
       opcode, size = ((cmd>>16)&0x7F), cmd&0x3FFF
-      print(f"{ptr:3X} -- typ 7: {size=:3d}, {opcode=:X}")
+      vals = struct.unpack("I"*size, dat[ptr+4:ptr+4+4*size])
+      print(f"{ptr:3X} -- typ 7: {size=:3d}, {opcode=:#x} {ops[opcode]}", hprint(vals))
+      if ops[opcode] == "CP_LOAD_STATE6_FRAG":
+        from disassemblers.adreno import disasm_raw
+        # pointers @ 0x140 in first call
+        #if vals[0] == 0x40364000: hexdump(get_mem(((vals[2] << 32) | vals[1]) + 0x140, 0x40))
+        if vals[0] == 0x40364000: hexdump(get_mem(((vals[2] << 32) | vals[1]), 0x180))
+        if vals[0] == 0xb60000: disasm_raw(get_mem(((vals[2] << 32) | vals[1]), 0x180))
+        #hexdump(get_mem((vals[2] << 32) | vals[1], 0x180))
+        pass
       ptr += 4*size
     elif (cmd>>28) == 0x4:
+      # write one or more registers (replace pkt0)
       offset, size = ((cmd>>8)&0x7FFFF), cmd&0x7F
-      print(f"{ptr:3X} -- typ 4: {size=:3d}, {offset=:X}")
+      vals = struct.unpack("I"*size, dat[ptr+4:ptr+4+4*size])
+      print(f"{ptr:3X} -- typ 4: {size=:3d}, {offset=:#x}", hprint(vals))
       ptr += 4*size
-    #elif (cmd>>30) == 0:
-    #  offset, size = (cmd&0x7FFF), ((cmd>>16)&0x3FFF)+1
-    #  print(f"typ 0: {size=:2d}, {offset=:X}")
-    #  ptr += 4*size
     else:
       print("unk", hex(cmd))
     ptr += 4
@@ -74,6 +100,7 @@ def ioctl(fd, request, argp):
       for i in range(s.numobjs):
         obj = get_struct(s.objlist+s.objsize*i, msm_kgsl.struct_kgsl_command_object)
         print(f"obj {i}:", format_struct(obj))
+        print(format_struct(msm_kgsl.struct_kgsl_cmdbatch_profiling_buffer.from_buffer_copy(get_mem(obj.gpuaddr, obj.size))))
         #hexdump(get_mem(obj.gpuaddr, obj.size))
   else:
     #print(f"ioctl({fd=}, (dir:{idir}, size:0x{size:3X}, type:{itype:d}, nr:0x{nr:2X}), {argp=:X}) = {ret=}")
@@ -98,30 +125,40 @@ def install_hook(c_function, python_function):
 libc = ctypes.CDLL(ctypes.util.find_library("libc"))
 install_hook(libc.ioctl, ioctl)
 
+"""
 print("***** init device")
 dev = CLDevice()
 print("***** alloc")
 alloc = CLAllocator(dev)
 a = alloc._alloc(16)
 #alloc._alloc(0x2000)
-print("***** copyin")
-alloc.copyin(a, memoryview(bytearray(b"hello")))
+ba = bytearray(b"hello")
+print(f"***** copyin {ctypes.addressof((ctypes.c_char * len(ba)).from_buffer(ba)):#x}")
+alloc.copyin(a, memoryview(ba))
 dev.synchronize()
-print("***** done")
+print("***** copyout")
+mv2 = memoryview(bytearray(b"nopeo"))
+alloc.copyout(mv2, a)
+dev.synchronize()
+print("***** done", bytes(mv2))
 exit(0)
+"""
 
 print("***** import tinygrad")
 from tinygrad import Tensor, Device, TinyJit
 print("***** access GPU")
 dev = Device["GPU"]
 print("***** create tensor a")
-a = Tensor([1.,2.]).realize()
+a = Tensor([1.,2.]*200).realize()
 print("***** create tensor b")
-b = Tensor([3.,4.]).realize()
+b = Tensor([3.,4.]*200).realize()
 @TinyJit
 def add(a, b): return (a+b).realize()
 for i in range(4):
   print(f"***** add tensors {i}")
   c = add(a, b)
-print("***** copy out")
-print(c.numpy())
+  #dev.synchronize()
+  c = add(b, a)
+  dev.synchronize()
+#print("***** copy out")
+#print(c.numpy())
