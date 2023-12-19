48a3443aaca127dda3ccd2a0740feaa0bc480de6
Woze Parrot <wozeparrot@gmail.com>
2023-12-19 08:26:40 +0000

    feat: store

--- test/unit/test_disk_tensor.py
+++ test/unit/test_disk_tensor.py
@@ -124,7 +124,7 @@ class TestSafetensors(unittest.TestCase):
     for dtype in dtypes.fields().values():
       if dtype in [dtypes.bfloat16]: continue # not supported in numpy
       path = temp("ones.safetensors")
-      ones = Tensor.rand((10,10), dtype=dtype)
+      ones = Tensor(np.random.rand(10,10), dtype=dtype)
       safe_save(get_state_dict(ones), path)
       assert ones == list(safe_load(path).values())[0]
 
