b6d71b131e758939c9aa43949c2c968077e8c764
George Hotz <geohot@gmail.com>
2023-12-18 21:08:42 -0800

    hotfix: push broken tests

--- test/unit/test_shapetracker_math.py
+++ test/unit/test_shapetracker_math.py
@@ -14,6 +14,15 @@ class TestShapeTrackerBasics(unittest.TestCase):
     a = a.shrink(((0,10), (0,11)))
     assert len(a.views) == 1 and a.views[-1].mask is not None
 
+  @unittest.skip("reshape is broken")
+  def test_reshape_makes_same(self):
+    a = ShapeTracker.from_shape((2, 5, 5))
+    x = a.pad( ((2, 0), (0, 0)) )
+    x = x.reshape( (2, 2, 5) )
+    x1 = x.reshape( (4, 5) )
+    x1 = x1.reshape( (2, 2, 5) )
+    assert x == x1
+
 class TestShapeTrackerAdd(unittest.TestCase):
   def test_simple_add_reshape(self):
     a = ShapeTracker.from_shape((10, 10))
@@ -67,6 +76,15 @@ class TestShapeTrackerInvert(unittest.TestCase):
     a = ShapeTracker.from_shape((10, 10))
     x = a.stride((2,2))
     assert x.invert(a.shape) is None
+
+  @unittest.skip("reshape is broken")
+  def test_invert_failure(self):
+    a = ShapeTracker.from_shape((2, 5, 5))
+    x = a.pad( ((2, 0), (0, 0)) )
+    x = x.reshape( (2, 2, 5) )
+    x = x.reshape( (4, 5) )
+    ap = ShapeTracker.from_shape(x.shape) + x.invert(a.shape)
+    assert ap == a, f"{ap} != {a}"
 
 if __name__ == '__main__':
   unittest.main()
