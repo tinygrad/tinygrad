adba838a505387ad3a8cb2a7e95c2b3f13694a58
ildarkashaev <kashaev99@icloud.com>
2023-12-19 03:07:16 +0300

    Lint file

--- examples/cc/tinygrad/tiny_model.h
+++ examples/cc/tinygrad/tiny_model.h
@@ -6,7 +6,7 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
- 
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
@@ -31,26 +31,27 @@ SOFTWARE.
 namespace tinygrad {
 
 class TinyModel {
- public:
-  TinyModel(const std::string& weights_file, const std::string& arch_file, cl::Context *context, cl::CommandQueue *command_queue);
+public:
+  TinyModel(const std::string &weights_file, const std::string &arch_file,
+            cl::Context *context, cl::CommandQueue *command_queue);
 
   ~TinyModel() = default;
 
-  void SetInputs(const std::vector<void*>& inputs);
-  
+  void SetInputs(const std::vector<void *> &inputs);
+
   void Inference();
 
-  void GetOutputs(std::vector<void*>& outputs);
-  
-  const std::string& backend() { return backend_; }
+  void GetOutputs(std::vector<void *> &outputs);
 
-  const std::vector<nlohmann::json>& inputs() { return inputs_; }
+  const std::string &backend() { return backend_; }
+
+  const std::vector<nlohmann::json> &inputs() { return inputs_; }
 
   const std::size_t NumInputs() { return inputs_.size(); }
 
   const std::size_t NumOutputs() { return outputs_.size(); }
 
- private:
+private:
   std::vector<nlohmann::json> statements_;
   std::string backend_;
   std::vector<nlohmann::json> inputs_;
@@ -60,9 +61,9 @@ class TinyModel {
   std::shared_ptr<cl::CommandQueue> command_queue_ = nullptr;
   std::unique_ptr<cl::Program> program_ = nullptr;
   std::unordered_map<std::string, cl::Buffer> buffers_;
-  std::vector<void*> output_buffers_;
+  std::vector<void *> output_buffers_;
 };
 
-}  // namespace tinygrad
+} // namespace tinygrad
 
-#endif  // TINYGRAD_TINY_MODEL_H_
+#endif // TINYGRAD_TINY_MODEL_H_
