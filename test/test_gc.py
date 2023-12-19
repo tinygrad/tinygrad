47a82cb9a0d543d5e066ffe1f94e7e06795bb80c
Woze Parrot <wozeparrot@gmail.com>
2023-12-19 07:27:47 +0000

    feat: match master

--- test/test_gc.py
+++ test/test_gc.py
@@ -10,28 +10,28 @@ def tensors_allocated():
 class TestGC(unittest.TestCase):
 
   def test_gc(self):
-    a = Tensor.rand(4, 4, requires_grad=True)
+    a = Tensor.zeros(4, 4, requires_grad=True)
     b = Tensor.zeros(4, 4, requires_grad=True)
     (a*b).mean().backward()
     assert(tensors_allocated() > 0)
     del a,b
-    assert(tensors_allocated() == 1) # one for Tensor._rng_counter
+    assert(tensors_allocated() == 0)
 
   def test_gc_complex(self):
     a = Tensor(np.zeros((4, 4), dtype=np.float32), requires_grad=True)
-    b = Tensor.rand(4, 4, requires_grad=True)
-    assert(tensors_allocated() == 3)
+    b = Tensor(np.zeros((4, 4), dtype=np.float32), requires_grad=True)
+    assert(tensors_allocated() == 2)
     (a*b).mean().backward()
-    assert(tensors_allocated() == 5)
+    assert(tensors_allocated() == 4)
     del b
-    assert(tensors_allocated() == 3)
+    assert(tensors_allocated() == 2)
     b = Tensor(np.zeros((4, 4), dtype=np.float32), requires_grad=True)
     print(tensors_allocated())
     (a*b).mean().backward()
     print(tensors_allocated())
-    assert(tensors_allocated() == 5)
+    assert(tensors_allocated() == 4)
     del b
-    assert(tensors_allocated() == 3)
+    assert(tensors_allocated() == 2)
 
 if __name__ == '__main__':
   unittest.main()
