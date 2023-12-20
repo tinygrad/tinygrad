9b2b89a52c1a768d40de01af8e34e147aa5e3b5b
Barış Özmen <barisozmen@Barss-MacBook-Pro.local>
2023-12-20 00:06:02 -0800

    revert fix

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -114,7 +114,7 @@ class Kernel:
   def copy(self):
     ret = deepcopy(self)
     # uncached since linearize didn't run
-    ret.applied_opts_cache, ret.local_alias = None, self.local_alias.copy()
+    ret.applied_opts_cache = None
     return ret
 
   @property
@@ -198,8 +198,10 @@ class Kernel:
   def reshape_and_permute(self, new_shape_fxn, axis):
     new_sts = []
     for st in self.sts:
-      if new_shape_fxn is not None: st = st.reshape(tuple(new_shape_fxn(st.shape)))
-      if axis is not None: st = st.permute(tuple(axis))
+      if new_shape_fxn is not None: 
+        st = st.reshape(tuple(new_shape_fxn(st.shape)))
+      if axis is not None: 
+        st = st.permute(tuple(axis))
       new_sts.append(st)
     self.sts = new_sts
 
