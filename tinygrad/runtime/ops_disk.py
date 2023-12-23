da5fe1d83b92ef01d78d3ee9710eecafa185d3ea
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-12-23 20:37:59 +0300

    don't load underlying disk buffer until it's actually needed

--- tinygrad/runtime/ops_disk.py
+++ tinygrad/runtime/ops_disk.py
@@ -6,9 +6,24 @@ from tinygrad.ops import Op, MovementOps, UnaryOps
 from tinygrad.shape.view import strides_for_shape
 
 class UnderlyingDiskBuffer:
-  def __init__(self, fd, mem): self.fd, self.mem = fd, mem
-  def __del__(self):
-    if self.fd: self.fd.close()
+  def __init__(self, device:str, size:int): self.device, self.size, self._fd, self._mem = device, size, None, None
+  def __load(self):
+    if self._mem is not None: return self
+    if str(self.device).startswith("shm:"):
+      fd = _posixshmem.shm_open("/"+self.device[4:].lstrip("/"), os.O_RDWR, 0o600)
+      self._mem = mmap.mmap(fd, self.size, flags=mmap.MAP_SHARED | MAP_POPULATE | MAP_LOCKED)
+      if (hp := getattr(mmap, "MADV_HUGEPAGE", None)) is not None: self._mem.madvise(hp) # type: ignore
+      os.close(fd)
+    else:
+      self._fd = open(self.device, "a+b")
+      if os.path.getsize(self.device) < self.size: os.ftruncate(self._fd.fileno(), self.size)
+      self._mem = mmap.mmap(self._fd.fileno(), self.size)
+    return self
+  @property
+  def fd(self): return self.__load()._fd
+  @property
+  def mem(self): return self.__load()._mem
+  def __del__(self): self._fd.close() if self._fd is not None else None
 
 class DiskBuffer:
   def __init__(self, ud:UnderlyingDiskBuffer, size:int, dtype:DType=dtypes.uint8, offset=0):
@@ -28,18 +43,7 @@ disk_fxn_for_op: Dict[Op, Callable] = { UnaryOps.CAST: DiskBuffer.cast, Movement
 MAP_LOCKED, MAP_POPULATE = 0 if OSX else 0x2000, getattr(mmap, "MAP_POPULATE", 0 if OSX else 0x008000)
 class DiskAllocator(Allocator):
   def __init__(self, device): self.device = device
-  def _alloc(self, size):
-    if str(self.device).startswith("shm:"):
-      fd = _posixshmem.shm_open("/"+self.device[4:].lstrip("/"), os.O_RDWR, 0o600)
-      shm = mmap.mmap(fd, size, flags=mmap.MAP_SHARED | MAP_POPULATE | MAP_LOCKED)
-      if (hp := getattr(mmap, "MADV_HUGEPAGE", None)) is not None: shm.madvise(hp) # type: ignore
-      os.close(fd)
-      buf = UnderlyingDiskBuffer(None, shm)
-    else:
-      f = open(self.device, "a+b")
-      if os.path.getsize(self.device) < size: os.ftruncate(f.fileno(), size)
-      buf = UnderlyingDiskBuffer(f, mmap.mmap(f.fileno(), size))
-    return DiskBuffer(buf, size)
+  def _alloc(self, size): return DiskBuffer(UnderlyingDiskBuffer(self.device, size), size)
   def as_buffer(self, src:DiskBuffer): return src._buf()
   def copyin(self, dest:DiskBuffer, src:memoryview): dest._buf()[:] = src
   def copyout(self, dest:memoryview, src:DiskBuffer):
