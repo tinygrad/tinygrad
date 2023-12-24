8de1fc2539a2a186f55fa14e788ff01cae68162a
Isalia20 <65120973+Isalia20@users.noreply.github.com>
2023-12-24 10:23:27 +0400

    Einsum space fix (#2927)
    * space removal in formula and a single test to cover it
    
    * space in torch einsum as well
    
    * replacing spaces in a var formula to support truncating all the spaces

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -526,6 +526,7 @@ class Tensor:
   @staticmethod
   def einsum(formula:str, *raw_xs) -> Tensor:
     xs:Tuple[Tensor] = argfix(*raw_xs)
+    formula = formula.replace(" ", "")
     inputs_str, output = formula.split("->") if "->" in formula else (formula, sorted(formula))
     inputs = [x for x in cast(str,inputs_str).split(',')]
     assert len(xs) == len(inputs), f"number of inputs doesn't match number of operands in formula, expected {len(inputs)}, got {len(xs)}"
