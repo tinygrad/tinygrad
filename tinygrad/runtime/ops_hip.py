be8bdcd639d3501040fca42b358213fa081ad97f
Maksym Sobolyev <sobomax@sippysoft.com>
2024-01-02 16:04:56 -0800

    Don't emit --offload-arch argiment if arch is unavailable.

--- tinygrad/runtime/ops_hip.py
+++ tinygrad/runtime/ops_hip.py
@@ -17,28 +17,28 @@ def check(status):
 # TODO: remove these helpers, they increase complexity
 def hip_time_execution(cb, enable=False): return time_execution_cuda_style(cb, hip.hipEvent_t, hip.hipEventCreate, hip.hipEventRecord, hip.hipEventSynchronize, hip.hipEventDestroy, hip.hipEventElapsedTime, enable=enable)  # noqa: E501
 
-def compile_hip(prg) -> bytes: return compile_cuda_style(prg, [f'--offload-arch={HIPDevice.default_arch_name}', '-I/opt/rocm/include'], hip.hiprtcProgram, hip.hiprtcCreateProgram, hip.hiprtcCompileProgram, hip.hiprtcGetCode, hip.hiprtcGetCodeSize, hip.hiprtcGetProgramLog, hip.hiprtcGetProgramLogSize, check)  # noqa: E501
+def compile_hip(prg) -> bytes: return compile_cuda_style(prg, *HIPDevice._comp_args)
+def alloc_call(f, *args): return init_c_var(f.argtypes[0]._type_(), lambda x: check(f(ctypes.byref(x), *args)))
 
 class HIPProgram:
-  def __init__(self, device:int, name:str, lib:bytes):
-    self.device, self.name, self.lib = device, name, lib
+  def __init__(self, device:HIPDevice, name:str, lib:bytes):
+    self.hip_scall, self.name, self.lib = device.hip_scall, name, lib
 
     if DEBUG >= 6:
       asm = subprocess.check_output(["/opt/rocm/llvm/bin/llvm-objdump", '-d', '-'], input=lib)
       print('\n'.join([x for x in asm.decode('utf-8').split("\n") if 's_code_end' not in x]))
 
     if MOCKHIP: return
-    check(hip.hipSetDevice(self.device))
-    self.module = init_c_var(hip.hipModule_t(), lambda x: check(hip.hipModuleLoadData(ctypes.byref(x), lib)))
-    self.prg = init_c_var(hip.hipFunction_t(), lambda x: check(hip.hipModuleGetFunction(ctypes.byref(x), self.module, name.encode("utf-8"))))
+    self.module = self.hip_scall(alloc_call, hip.hipModuleLoadData, lib, chk=False)
+    self.prg = alloc_call(hip.hipModuleGetFunction, self.module, name.encode("utf-8"))
 
   def __del__(self):
     if hasattr(self, 'module'): check(hip.hipModuleUnload(self.module))
 
   def __call__(self, *args, global_size:Tuple[int,int,int], local_size:Tuple[int,int,int], vals:Tuple[int, ...]=(), wait=False):
     if MOCKHIP: return float("inf")
-    check(hip.hipSetDevice(self.device))
-    return hip_time_execution(lambda: check(hip.hipModuleLaunchKernel(self.prg, *global_size, *local_size, 0, None, None, encode_args_cuda_style(args, vals, hip.hipDeviceptr_t, marks=(1,2,3))[0])), enable=wait)  # noqa: E501
+    self.hip_scall(hip_time_execution, lambda: check(hip.hipModuleLaunchKernel(self.prg, *global_size, *local_size, 0, None, None,
+                  encode_args_cuda_style(args, vals, hip.hipDeviceptr_t, marks=(1,2,3))[0])), wait, chk=False)
 
 T = TypeVar("T")
 CHUNK_SIZE, PAGE_SIZE = 256*1024*1024, 0x1000
@@ -46,11 +46,9 @@ class HIPAllocator(LRUAllocator):
   def __init__(self, device:HIPDevice):
     self.device = device
     super().__init__()
-  def _alloc(self, size:int):
-    check(hip.hipSetDevice(self.device.device))
-    return init_c_var(hip.hipDeviceptr_t(), lambda x: check(hip.hipMalloc(ctypes.byref(x), size)))
+  def _alloc(self, size:int): return self.device.hip_scall(alloc_call, hip.hipMalloc, size, chk=False)
   def _free(self, opaque:T): check(hip.hipFree(opaque))
-  def _hostalloc(self, size:int): return init_c_var(hip.hipDeviceptr_t(), lambda x: check(hip.hipHostMalloc(ctypes.byref(x), size, 0)))
+  def _hostalloc(self, size:int): return alloc_call(hip.hipHostMalloc, size, 0) # <- watchout, this one does not set the device
   def copy_from_fd(self, dest, fd, offset, size):
     check(hip.hipSetDevice(self.device.device))
     if not hasattr(self, 'hb'): self.hb = [self._hostalloc(CHUNK_SIZE) for _ in range(2)]
@@ -67,31 +65,30 @@ class HIPAllocator(LRUAllocator):
       self.hb = self.hb[1:] + [self.hb[0]]
       minor_offset = 0 # only on the first
   def copyin(self, dest:T, src: memoryview):
-    check(hip.hipSetDevice(self.device.device))
-    host_mem = self._hostalloc(len(src))
+    host_mem = self.device.hip_scall(self._hostalloc, len(src), chk=False)
     self.device.pending_copyin.append(host_mem)
     ctypes.memmove(host_mem, from_mv(src), len(src))
     check(hip.hipMemcpyAsync(dest, host_mem, len(src), hip.hipMemcpyHostToDevice, None))
-  def copyout(self, dest:memoryview, src:T):
-    check(hip.hipSetDevice(self.device.device))
-    check(hip.hipMemcpy(from_mv(dest), src, len(dest), hip.hipMemcpyDeviceToHost))
-  def transfer(self, dest:T, src:T, sz:int):
-    check(hip.hipSetDevice(self.device.device))
-    # TODO: hipMemcpyAsync, but you have to track the "src" buffer to not free it
-    check(hip.hipMemcpy(dest, src, sz, hip.hipMemcpyDeviceToDevice))
+  def copyout(self, dest:memoryview, src:T): self.device.hip_scall(hip.hipMemcpy, from_mv(dest), src, len(dest), hip.hipMemcpyDeviceToHost)
+  # TODO: hipMemcpyAsync, but you have to track the "src" buffer to not free it
+  def transfer(self, dest:T, src:T, sz:int): self.device.hip_scall(hip.hipMemcpy, dest, src, sz, hip.hipMemcpyDeviceToDevice)
 
 class HIPDevice(Compiled):
   default_arch_name = "gfx1100"
+  _comp_args = ([], hip.hiprtcProgram, hip.hiprtcCreateProgram, hip.hiprtcCompileProgram, hip.hiprtcGetCode,
+           hip.hiprtcGetCodeSize, hip.hiprtcGetProgramLog, hip.hiprtcGetProgramLogSize, check) # type: ignore[var-annotated]
   def __init__(self, device:str=""):
-    self.device = int(device.split(":")[1]) if ":" in device else 0
+    self.device, bas = int(device.split(":")[1]) if ":" in device else 0, (None, '', 'unavailable',)
     self.pending_copyin: List[hip.hipDeviceptr_t] = []
-    if self.device == 0 and not MOCKHIP: HIPDevice.default_arch_name = init_c_var(hip.hipDeviceProp_t(), lambda x: check(hip.hipGetDeviceProperties(x, self.device))).gcnArchName.decode()  # noqa: E501
+
+    if self.device == 0 and not MOCKHIP: HIPDevice.default_arch_name = alloc_call(hip.hipGetDeviceProperties, self.device).gcnArchName.decode()
+    if not (o:=HIPDevice._comp_args[0]) and (a:=HIPDevice.default_arch_name) not in bas: o.extend(('-I/opt/rocm/include', f'--offload-arch={a}'))
 
     from tinygrad.runtime.graph.hip import HIPGraph
     super().__init__(MallocAllocator if MOCKHIP else HIPAllocator(self), LinearizerOptions("HIP"), HIPRenderer,
-                     compile_hip, functools.partial(HIPProgram, self.device), HIPGraph)
+                     compile_hip, functools.partial(HIPProgram, self), HIPGraph)
   def synchronize(self):
-    check(hip.hipSetDevice(self.device))
-    check(hip.hipDeviceSynchronize())
+    self.hip_scall(hip.hipDeviceSynchronize)
     for opaque in self.pending_copyin: check(hip.hipFree(opaque))
     self.pending_copyin.clear()
+  def hip_scall(self, f, *args, chk=True): return (check(hip.hipSetDevice(self.device)), check(f(*args)) if chk else f(*args))[1]
