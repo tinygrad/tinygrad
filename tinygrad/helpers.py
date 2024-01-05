9a7807789d9044ce7195bd66ac83c17c238b8104
George Hotz <geohot@gmail.com>
2024-01-05 10:26:47 -0800

    bump to 6k lines

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -32,7 +32,7 @@ def round_up(num, amt:int): return (num+amt-1)//amt * amt
 def merge_dicts(ds:Iterable[Dict[T,U]]) -> Dict[T,U]:
   assert len(kvs:=set([(k,v) for d in ds for k,v in d.items()])) == len(set(kv[0] for kv in kvs)), f"cannot merge, {kvs} contains different values for the same key"  # noqa: E501
   return {k:v for d in ds for k,v in d.items()}
-def partition(lst:List[T], fxn:Callable[[T],bool]):
+def partition(lst:List[T], fxn:Callable[[T],bool]) -> Tuple[List[T], List[T]]:
   a:List[T] = []
   b:List[T] = []
   for s in lst: (a if fxn(s) else b).append(s)
