0fb2c58bf5ec6d136a4af6598d2287fd3ec4320c
George Hotz <geohot@gmail.com>
2024-01-05 16:20:22 -0800

    add test that the compiler isn't used

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -285,6 +285,8 @@ class Compiled:
   def synchronize(self): pass  # override this in your device
 
   def to_program(self, k:Linearizer) -> CompiledASTRunner:
+    from tinygrad.graph import print_tree
+    assert self.compiler is not None, f"compiler is None, can't build {print_tree(k.ast)}"
     k.linearize()
     src = self.renderer(to_function_name(k.name), k.uops)
     if getenv("DISABLE_COMPILER_CACHE") or '<' in self.compiler.__name__:
