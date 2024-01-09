4ef086dce53b2e0d357a5ed5c642da408969bdf0
George Hotz <geohot@gmail.com>
2024-01-09 10:59:15 -0800

    base doesn't have to be a function

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -43,19 +43,17 @@ class LazyBuffer:
       self.output_buffer: Optional[Buffer] = None
       self.forced_realize = False
       self.contiguous_child: Optional[Tuple[ReferenceType[LazyBuffer], ShapeTracker]] = None
+      self.base = self
     else:
       # properties on view
       assert base.base == base, "base must be a base itself"
-      self._base = base
+      self.base = base
 
   def __del__(self):
     if self.cache_key in lazycache: del lazycache[self.cache_key]
 
   def __repr__(self) -> str:
-    return f"<LB {self.device} {self.shape} contig:{self.st.contiguous} {self.st if hasattr(self, '_base') else (self.op, self.realized)}>"
-
-  @property
-  def base(self) -> LazyBuffer: return self._base if hasattr(self, '_base') else self
+    return f"<LB {self.device} {self.shape} contig:{self.st.contiguous} {self.st if self.base != self else (self.op, self.realized)}>"
 
   @staticmethod
   def loadop(op, shape:Tuple[sint,...], dtype:DType, device:str, arg=None, src:Optional[LazyBuffer]=None) -> LazyBuffer:
