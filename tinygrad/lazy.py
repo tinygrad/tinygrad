56f185b2c4b50e696d269fe5de67dfaa4c143d5f
George Hotz <72895+geohot@users.noreply.github.com>
2024-01-09 17:42:33 -0800

    speedtweaks3: apply shouldn't use the tensor constructor (#3065)
    * speedtweaks3: apply shouldn't use the tensor constructor
    
    * replace 0 size with CONST, not 0 in shape

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -19,7 +19,7 @@ lazycache: Dict[Any, ReferenceType[LazyBuffer]] = {}
 def create_lazybuffer(device:str, st:ShapeTracker, dtype:DType,
                       op:Optional[Op]=None, arg:Any=None, srcs:Tuple[LazyBuffer, ...]=(),
                       base:Optional[LazyBuffer]=None, enable_cache=bool(getenv("LAZYCACHE", 1))):
-  if 0 in st.shape: st, op, arg, srcs, base = ShapeTracker.from_shape(st.shape), LoadOps.CONST, 0, (), None
+  if st.size == 0: st, op, arg, srcs, base = ShapeTracker.from_shape(st.shape), LoadOps.CONST, 0, (), None
 
   cache_key = (device, st, dtype, op, arg, tuple(ref(x) for x in srcs)) if base is None else (st, ref(base))
   if (rret := lazycache.get(cache_key, None)): return cast(LazyBuffer, rret())  # NOTE: this should always be a live reference
