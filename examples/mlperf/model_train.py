b2fc16f01b337edbb420327f267b060f08825b6f
Francis Lata <francisalbertlata@gmail.com>
2024-01-18 20:48:27 +0000

    add gradient acc

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -24,12 +24,14 @@ def train_unet3d():
   MOMENTUM = getenv("MOMENTUM", 0.9)
   LR_WARMUP_EPOCHS = getenv("LR_WARMUP_EPOCHS", 200)
   LR_WARMUP_INIT_LR = getenv("LR_WARMUP_INIT_LR", 0.0001)
+  GA_STEPS = getenv("GA_STEPS", 1)
 
   GPUS = [f"{Device.DEFAULT}:{i}" for i in range(getenv("GPUS", 1))]
   assert BS % len(GPUS) == 0, f"{BS=} is not a multiple of {len(GPUS)=}"
   for x in GPUS: Device[x]
 
   model = UNet3D()
+  model_run = TinyJit(lambda x: model(x).realize())
   if len(GPUS) > 1: model.shard_(GPUS)
 
   optim = SGD(get_parameters(model), lr=LR, momentum=MOMENTUM, nesterov=True)
@@ -38,26 +40,29 @@ def train_unet3d():
     scale = current_epoch / warmup_epochs
     optim.lr.assign(Tensor([init_lr + (lr - init_lr) * scale]))
 
-  @TinyJit
   def _train_step(x, y):
-    y_hat = model(x)
+    y_hat = model_run(x)
     loss = dice_ce_loss(y_hat, y, gpus=GPUS)
-
-    optim.zero_grad()
-    loss.backward()
-    optim.step()
-
-    return loss.realize()
+    return loss
 
   for epoch in range(1, NUM_EPOCHS + 1):
     if epoch <= LR_WARMUP_EPOCHS and LR_WARMUP_EPOCHS > 0:
       _lr_warm_up(optim, LR_WARMUP_INIT_LR, LR, epoch, LR_WARMUP_EPOCHS)
 
-    for x, y in (t:=tqdm(iterate(val=False, shuffle=True, bs=BS), desc=f"[Epoch {epoch}]", total=len(get_train_files()) // BS)):
+    optim.zero_grad()
+
+    for iter, (x, y) in enumerate((t:=tqdm(iterate(val=False, shuffle=True, bs=BS), desc=f"[Epoch {epoch}]", total=len(get_train_files()) // BS))):
       x, y = Tensor(x, dtype=dtypes.float32), Tensor(y, dtype=dtypes.uint8)
       if len(GPUS) > 1: x, y = x.shard(GPUS, axis=0), y.shard(GPUS, axis=0)
 
-      loss = _train_step(x, y)
+      loss = _train_step(x, y, iter)
+      loss = loss / GA_STEPS
+      loss.backward()
+
+      if (iter + 1) % GA_STEPS == 0:
+        optim.step()
+        optim.zero_grad()
+
       t.set_description(f"[Epoch {epoch}][Loss: {loss.item():.3f}]")
 
 def train_rnnt():
