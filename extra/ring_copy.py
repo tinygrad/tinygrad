e16c1e02ef65cc8ad4b7e0f667903288cafe903c
chenyu <chenyu@fastmail.com>
2024-01-19 20:11:26 -0800

    use ones for init

--- extra/ring_copy.py
+++ extra/ring_copy.py
@@ -4,12 +4,12 @@ from tinygrad.helpers import Timing
 N = 512
 GPUS = 5
 ds = tuple([f"{Device.DEFAULT}:{i+1}" for i in range(GPUS)])
-t = [Tensor.rand(N, N, N, device=d).realize() for d in ds]
+t = [Tensor.ones(N, N, N, device=d).contiguous().realize() for d in ds]
 
 for _ in range(10):
   GlobalCounters.reset()
-  with Timing("timing  "):
+  with Timing():
     for ti in t:
-      # ti.to_(ds[(ds.index(ti.device)+1+len(ds))%len(ds)])
-      ti.to_(ds[(ds.index(ti.device)-1+len(ds))%len(ds)]) # reversed
+      ti.to_(ds[(ds.index(ti.device)+1+len(ds))%len(ds)])
+      # ti.to_(ds[(ds.index(ti.device)-1+len(ds))%len(ds)]) # reversed order
       ti.realize()
