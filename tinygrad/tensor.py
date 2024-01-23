ed186cad2fce26b63d237caac20a44bb2392b13e
zibokapi <zibokapi@gmail.com>
2024-01-23 15:57:57 +0800

    revert: nvm mul(0.5) and div(2) has the same kernel for backward

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -836,9 +836,9 @@ class Tensor:
   def xor(self, x:Tensor, reverse=False) -> Tensor: return mlops.Xor.apply(*self._broadcasted(x, reverse))
 
   def maximum(self, x:Union[Tensor, Scalar]) -> Tensor:
-    return (self<x).detach().where(x, (self>x).detach().where(self, (self+x)*0.5) if self.requires_grad and dtypes.is_float(self.dtype) else self)
+    return (self<x).detach().where(x, (self>x).detach().where(self, (self+x)/2) if self.requires_grad and dtypes.is_float(self.dtype) else self)
   def minimum(self, x:Union[Tensor, Scalar]) -> Tensor:
-    return (self>x).detach().where(x, (self<x).detach().where(self, (self+x)*0.5) if self.requires_grad and dtypes.is_float(self.dtype) else self)
+    return (self>x).detach().where(x, (self<x).detach().where(self, (self+x)/2) if self.requires_grad and dtypes.is_float(self.dtype) else self)
 
   def where(self:Tensor, input_:Union[Tensor, Scalar], other:Union[Tensor, Scalar]):
     if isinstance(input_, Tensor): input_, other = input_._broadcasted(other)
