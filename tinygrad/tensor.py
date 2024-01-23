b6dbd26ce8589a6f1512e6195e6553641b8cdb3b
zibokapi <zibokapi@gmail.com>
2024-01-23 15:14:52 +0800

    fix: how about this? tighter check

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -836,9 +836,9 @@ class Tensor:
   def xor(self, x:Tensor, reverse=False) -> Tensor: return mlops.Xor.apply(*self._broadcasted(x, reverse))
 
   def maximum(self, x:Union[Tensor, Scalar]) -> Tensor:
-    return (self<x).detach().where(x, self if not dtypes.is_float(self.dtype) else (self>x).detach().where(self, (self+x)/2))
+    return (self<x).detach().where(x, self if not (self.requires_grad and dtypes.is_float(self.dtype)) else (self>x).detach().where(self, (self+x)/2))
   def minimum(self, x:Union[Tensor, Scalar]) -> Tensor:
-    return (self>x).detach().where(x, self if not dtypes.is_float(self.dtype) else (self<x).detach().where(self, (self+x)/2))
+    return (self>x).detach().where(x, self if not (self.requires_grad and dtypes.is_float(self.dtype)) else (self<x).detach().where(self, (self+x)/2))
 
   def where(self:Tensor, input_:Union[Tensor, Scalar], other:Union[Tensor, Scalar]):
     if isinstance(input_, Tensor): input_, other = input_._broadcasted(other)
