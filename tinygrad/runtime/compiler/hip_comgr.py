e59478a7a73ae98199f81ff0babd93d68ad94927
George Hotz <geohot@gmail.com>
2024-01-26 01:57:03 +0000

    cleaner style

--- tinygrad/runtime/compiler/hip_comgr.py
+++ tinygrad/runtime/compiler/hip_comgr.py
@@ -1,6 +1,5 @@
 import ctypes
 import gpuctypes.comgr as comgr
-from tinygrad.helpers import init_c_var
 
 def check(status):
   if status != 0:
@@ -15,19 +14,20 @@ def _get_comgr_data(data_set, data_type):
 
 # AMD_COMGR_SAVE_TEMPS=1 AMD_COMGR_REDIRECT_LOGS=stdout AMD_COMGR_EMIT_VERBOSE_LOGS=1
 def compile_hip(prg:str, arch="gfx1100") -> bytes:
-  rprg = prg.encode()
-  action_info = init_c_var(comgr.amd_comgr_action_info_t(), lambda x: check(comgr.amd_comgr_create_action_info(ctypes.byref(x))))
-  data_src = init_c_var(comgr.amd_comgr_data_t(), lambda x: check(comgr.amd_comgr_create_data(comgr.AMD_COMGR_DATA_KIND_SOURCE, ctypes.byref(x))))
-  data_set_src = init_c_var(comgr.amd_comgr_data_set_t(), lambda x: check(comgr.amd_comgr_create_data_set(ctypes.byref(x))))
-  data_set_bc = init_c_var(comgr.amd_comgr_data_set_t(), lambda x: check(comgr.amd_comgr_create_data_set(ctypes.byref(x))))
-  data_set_reloc = init_c_var(comgr.amd_comgr_data_set_t(), lambda x: check(comgr.amd_comgr_create_data_set(ctypes.byref(x))))
-  data_set_exec = init_c_var(comgr.amd_comgr_data_set_t(), lambda x: check(comgr.amd_comgr_create_data_set(ctypes.byref(x))))
-  check(comgr.amd_comgr_set_data(data_src, len(rprg), rprg))
-  check(comgr.amd_comgr_set_data_name(data_src, b"<null>"))
-  check(comgr.amd_comgr_data_set_add(data_set_src, data_src))
+  check(comgr.amd_comgr_create_action_info(ctypes.byref(action_info := comgr.amd_comgr_action_info_t())))
   check(comgr.amd_comgr_action_info_set_language(action_info, comgr.AMD_COMGR_LANGUAGE_HIP))
   check(comgr.amd_comgr_action_info_set_isa_name(action_info, b"amdgcn-amd-amdhsa--" + arch.encode()))
   check(comgr.amd_comgr_action_info_set_logging(action_info, True))
+
+  check(comgr.amd_comgr_create_data(comgr.AMD_COMGR_DATA_KIND_SOURCE, ctypes.byref(data_src := comgr.amd_comgr_data_t())))
+  check(comgr.amd_comgr_create_data_set(ctypes.byref(data_set_src := comgr.amd_comgr_data_set_t())))
+  check(comgr.amd_comgr_create_data_set(ctypes.byref(data_set_bc := comgr.amd_comgr_data_set_t())))
+  check(comgr.amd_comgr_create_data_set(ctypes.byref(data_set_reloc := comgr.amd_comgr_data_set_t())))
+  check(comgr.amd_comgr_create_data_set(ctypes.byref(data_set_exec := comgr.amd_comgr_data_set_t())))
+
+  check(comgr.amd_comgr_set_data(data_src, len(rprg := prg.encode()), rprg))
+  check(comgr.amd_comgr_set_data_name(data_src, b"<null>"))
+  check(comgr.amd_comgr_data_set_add(data_set_src, data_src))
   # -include hiprtc_runtime.h was removed
   check(comgr.amd_comgr_action_info_set_options(action_info, b"-O3 -mcumode --hip-version=6.0.32830 -DHIP_VERSION_MAJOR=6 -DHIP_VERSION_MINOR=0 -DHIP_VERSION_PATCH=32830 -D__HIPCC_RTC__ -std=c++14 -nogpuinc -Wno-gnu-line-marker -Wno-missing-prototypes --offload-arch=gfx1100 -I/opt/rocm/include -Xclang -disable-llvm-passes")) # noqa: E501
   status = comgr.amd_comgr_do_action(comgr.AMD_COMGR_ACTION_COMPILE_SOURCE_WITH_DEVICE_LIBS_TO_BC, action_info, data_set_src, data_set_bc)
