5b46b0ff3d2e90fbc70a7d39834760fc5227f499
qazal <77887910+Qazalin@users.noreply.github.com>
2024-01-30 20:39:28 +0200

    Simple RDNA3 emulator (#2974)
    * mockhip->hipcpu
    
    * allocate buffers
    
    * launch a kernel
    
    read_asm api
    
    * run remu in CI
    
    * remu 0.0.2, real test ops
    
    * simple driver
    
    * 0.0.3, all test_ops
    
    * run the latest emulator
    
    * 9 minutes is way too long, drop backprop in CI
    
    * bring back the backward pass
    
    * Revert "bring back the backward pass"
    
    This reverts commit 3781e1bc56fc06b424e7c7bed1224f819247fb8f.
    
    * Print slowest tests
    
    * emulated device directly in ops_hip
    
    * fix ruff, override mypy for specific rules
    
    * test in the same code path
    
    - hip backend env variables
    
    - install packages and verify autogen
    
    - run certain tests
    
    - remove the other hip tests path
    
    - verify Device.DEFAULT
    
    * remove the emulated hip in extra
    
    ---------
    
    Co-authored-by: George Hotz <72895+geohot@users.noreply.github.com>

--- test/test_dtype_alu.py
+++ test/test_dtype_alu.py
@@ -143,9 +143,10 @@ class TestDTypeALU(unittest.TestCase):
   @given(ht.int32, ht.int32, ht.float32, strat.sampled_from(integer_binary_operations), strat.sampled_from(binary_operations))
   def test_int32_midcast_float(self, a, b, c, op1, op2): universal_test_midcast(a, b, c, op1, op2, dtypes.int32, dtypes.float32)
 
-  # Metal and CUDACPU behave differently than numpy in CI for overflows
-  @given(strat.floats(width=32, min_value=0, max_value=10.0) if CI and (Device.DEFAULT == "METAL" or getenv("CUDACPU")) else ht.float32,
-         strat.floats(width=32, min_value=0, max_value=10.0) if CI and (Device.DEFAULT == "METAL" or getenv("CUDACPU")) else ht.float32,
+  # Metal and CUDACPU and HIP behave differently than numpy in CI for overflows
+  skip_overflow = CI and (Device.DEFAULT in ["METAL","HIP"] or getenv("CUDACPU"))
+  @given(strat.floats(width=32, min_value=0, max_value=10.0) if skip_overflow else ht.float32,
+         strat.floats(width=32, min_value=0, max_value=10.0) if skip_overflow else ht.float32,
          ht.int32, strat.sampled_from(binary_operations), strat.sampled_from(integer_binary_operations))
   def test_float_midcast_int32(self, a, b, c, op1, op2): universal_test_midcast(a, b, c, op1, op2, dtypes.float32, dtypes.int32)
 
