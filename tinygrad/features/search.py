cf6f478901389a7ab6f47987a55eb6792f776478
David Hou <david.hou314@gmail.com>
2024-02-02 00:13:12 -0800

    limit group_for_reduce bufs to 32kb (#3299)
    hipcc crashes for buffers that are too large

--- tinygrad/features/search.py
+++ tinygrad/features/search.py
@@ -83,7 +83,7 @@ def get_linearizer_actions(lin:Linearizer, include_0=True) -> Dict[int, Lineariz
       for s,c in zip(lin2.full_shape, lin2.colors()):
         if c in {"magenta", "yellow"}: up *= s
         if c in {"cyan", "green", "white"}: lcl *= s
-      if up > 256 or lcl > 256: continue
+      if up > 256 or lcl > 256 or ("green" in lin2.colors() and up*lcl > 2 ** 15): continue
       acted_lins[i+1] = lin2
     except Exception:
       pass
