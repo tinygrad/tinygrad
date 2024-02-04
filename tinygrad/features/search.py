cd224521cf44e71ebfeffdacfaad01ba5087133f
David Hou <david.hou314@gmail.com>
2024-02-03 22:49:53 -0800

    rename

--- tinygrad/features/search.py
+++ tinygrad/features/search.py
@@ -104,7 +104,7 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
       _compile_fn = functools.partial(_try_compile_linearized_w_idx, compiler=cast(Compiled, Device[lin.opts.device]).compiler)
       for i,proc in (map(_compile_fn, enumerate(acted_lins)) if beam_pool is None else beam_pool.imap_unordered(_compile_fn, enumerate(acted_lins))):
         if proc is None: continue
-        lib, global_size, local_size = proc.binary, proc.global_size, proc.local_size
+        lib, global_size, local_size = proc.lib, proc.global_size, proc.local_size
         if lib in seen_libs: continue
         seen_libs.add(lib)
         tms = _time_program(lin.ast, dev, lib, global_size, local_size, var_vals, rawbufs, early_stop=beam[0][1]*3 if len(beam) else 1.0)
