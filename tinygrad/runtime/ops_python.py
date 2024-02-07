efe1c80245a483cf322465a04e38006586ab9c02
George Hotz <geohot@gmail.com>
2024-02-07 06:52:05 -0800

    more ops

--- tinygrad/runtime/ops_python.py
+++ tinygrad/runtime/ops_python.py
@@ -1,8 +1,9 @@
 # a python uops emulator
 # works to test the tensor cores, and all the uops in general
 # this is the (living) definition of uops
-from typing import Tuple, List
-import pickle, base64, itertools
+from typing import Tuple, List, Optional, Any
+import pickle, base64, itertools, time
+from tinygrad.dtype import DType
 from tinygrad.device import Compiled, Allocator, Compiler
 from tinygrad.codegen.kernel import LinearizerOptions
 from tinygrad.codegen.uops import UOp, UOps
@@ -10,44 +11,59 @@ from tinygrad.ops import UnaryOps, BinaryOps, TernaryOps
 
 class PythonProgram:
   def __init__(self, name:str, lib:bytes):
-    self.uops: List[UOp] = pickle.loads(lib)
+    self.uops: List[Tuple[UOp, Optional[DType], List[int], Any]] = pickle.loads(lib)
   def __call__(self, *bufs, global_size:Tuple[int,int,int]=(1,1,1), local_size:Tuple[int,int,int]=(1,1,1), vals:Tuple[int, ...]=(), wait=False):
+    st = time.perf_counter()
     # TODO: abstract this out so it can be used for constant folding
     for idxs in itertools.product(*[range(x) for x in global_size[::-1]+local_size[::-1]]):
       ul = {}
       pbufs: List[memoryview] = list(bufs)
-      for i,u in enumerate(self.uops):
-        inp = [ul[self.uops.index(v)] for v in u.vin]
-        if u.uop is UOps.DEFINE_GLOBAL: ul[i] = pbufs.pop(0).cast(u.dtype.fmt)
-        elif u.uop is UOps.SPECIAL:
-          if u.arg[1][0] == 'g': ul[i] = idxs[2-u.arg[0]]
-          elif u.arg[1][0] == 'l': ul[i] = idxs[5-u.arg[0]]
-        elif u.uop is UOps.CONST: ul[i] = u.arg
-        elif u.uop is UOps.CAST:
-          assert u.dtype.sz > 1
-          ul[i] = inp
-        elif u.uop is UOps.STORE:
+      for i, (uop, dtype, idp, arg) in enumerate(self.uops):
+        inp = [ul[v] for v in idp]
+        if uop is UOps.DEFINE_GLOBAL:
+          ul[i] = pbufs.pop(0).cast(dtype.fmt)
+        elif uop is UOps.SPECIAL:
+          if arg[1][0] == 'g': ul[i] = idxs[2-arg[0]]
+          elif arg[1][0] == 'l': ul[i] = idxs[5-arg[0]]
+        elif uop is UOps.CONST: ul[i] = arg
+        elif uop is UOps.CAST:
+          if dtype.sz > 1:
+            ul[i] = inp
+          else:
+            # TODO: add real cast
+            ul[i] = inp[0]
+        elif uop is UOps.STORE:
           if isinstance(inp[2], list):
             for j,val in enumerate(inp[2]):
               inp[0][inp[1] + j] = val
           else:
             inp[0][inp[1]] = inp[2]
-        elif u.uop is UOps.LOAD:
-          if u.dtype.sz > 1:
-            ul[i] = [inp[0][inp[1]+j] for j in range(u.dtype.sz)]
+        elif uop is UOps.LOAD:
+          if dtype.sz > 1:
+            ul[i] = [inp[0][inp[1]+j] for j in range(dtype.sz)]
           else:
             ul[i] = inp[0][inp[1]]
-        elif u.uop is UOps.GEP:
-          ul[i] = inp[0][u.arg]
-        elif u.uop is UOps.ALU:
-          if u.arg == BinaryOps.MUL:
+        elif uop is UOps.GEP:
+          ul[i] = inp[0][arg]
+        elif uop is UOps.ALU:
+          if arg == BinaryOps.MUL:
             ul[i] = inp[0] * inp[1]
-          elif u.arg == BinaryOps.ADD:
+          elif arg == BinaryOps.ADD:
             ul[i] = inp[0] + inp[1]
+          elif arg == BinaryOps.MAX:
+            ul[i] = max(inp[0], inp[1])
+          elif arg == BinaryOps.CMPLT:
+            ul[i] = inp[0] < inp[1]
+          elif arg == UnaryOps.NEG:
+            ul[i] = -inp[0]
+        assert uop in {UOps.STORE} or i in ul, (uop, dtype, arg)
+    return time.perf_counter() - st
 
 class PythonCompiler(Compiler):
   linearizer_opts = LinearizerOptions()
-  def render(self, name:str, uops) -> str: return base64.b64encode(pickle.dumps(uops))
+  def render(self, name:str, uops:List[UOp]) -> str:
+    lops = [(u.uop, u.dtype, [uops.index(v) for v in u.vin], u.arg) for u in uops]
+    return base64.b64encode(pickle.dumps(lops))
   def compile(self, src:str) -> bytes: return base64.b64decode(src)
 
 class PythonAllocator(Allocator):
