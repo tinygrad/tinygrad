df339ebe8b58a1a5f726b11a2e8e2d95ba62353a
Obada Khalili <obada.akhalili@gmail.com>
2024-02-13 17:50:46 +0200

    avoid accessing outputs for void uops

--- tinygrad/runtime/ops_python.py
+++ tinygrad/runtime/ops_python.py
@@ -61,8 +61,10 @@ class PythonProgram:
       loop_ends: Dict[int, int] = {}
       while i < len(self.uops):
         uop, dtype, idp, arg = self.uops[i]
-        inp = [ul[v] for v in idp]
-        dtp = [dl[v] for v in idp]
+        # is the below skip in loop right?
+        void_ops = {UOps.STORE, UOps.ENDLOOP, UOps.BARRIER, UOps.IF, UOps.ENDIF}
+        inp = [ul[v] for v in idp if self.uops[v][0] not in void_ops]
+        dtp = [dl[v] for v in idp if self.uops[v][0] not in void_ops]
         if getenv("TRACE"): print(i, uop, dtype, arg, inp, dtp)
         if uop is UOps.STORE:
           assert len(inp) <= 3, "gated stores not supported yet"
