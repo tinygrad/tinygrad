3c4c8c9e16d87b291d05e1cab558124cc339ac46
chenyu <chenyu@fastmail.com>
2024-02-14 10:13:18 -0500

    bump db version to 11 (#3398)
    followup after disabled fast math on metal.

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -125,7 +125,7 @@ _cache_dir: str = getenv("XDG_CACHE_HOME", os.path.expanduser("~/Library/Caches"
 CACHEDB: str = getenv("CACHEDB", os.path.abspath(os.path.join(_cache_dir, "tinygrad", "cache.db")))
 CACHELEVEL = getenv("CACHELEVEL", 2)
 
-VERSION = 10
+VERSION = 11
 _db_connection = None
 def db_connection():
   global _db_connection
