170108e683d32792a5c470ee5a965731537ef147
George Hotz <geohot@gmail.com>
2024-02-15 11:57:10 +0100

    don't allow zero copy with as buffer

--- tinygrad/runtime/ops_ext.py
+++ tinygrad/runtime/ops_ext.py
@@ -3,7 +3,6 @@ from tinygrad.device import Compiled, Allocator
 
 # the Any is an arbitrary object that's kept in scope with the memoryview
 class ExtAllocator(Allocator):
-  def as_buffer(self, src:Tuple[memoryview, Any]) -> memoryview: return src[0]
   def copyin(self, dest:Tuple[memoryview, Any], src:memoryview): dest[0][:] = src
   def copyout(self, dest:memoryview, src:Tuple[memoryview, Any]): dest[:] = src[0]
 
