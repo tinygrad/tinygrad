c70c069b012cb3a878d1804dbbfb501ef5ce7345
George Hotz <geohot@gmail.com>
2024-02-15 11:49:38 +0100

    ops_ext to replace cpu import

---
+++ tinygrad/runtime/ops_ext.py
@@ -0,0 +1,11 @@
+from typing import Tuple, Any
+from tinygrad.device import Compiled, Allocator
+
+# the Any is an arbitrary object that's kept in scope with the memoryview
+class ExtAllocator(Allocator):
+  def as_buffer(self, src:Tuple[memoryview, Any]) -> memoryview: return src[0]
+  def copyin(self, dest:Tuple[memoryview, Any], src:memoryview): dest[0][:] = src
+  def copyout(self, dest:memoryview, src:Tuple[memoryview, Any]): dest[:] = src[0]
+
+class ExtDevice(Compiled):
+  def __init__(self, device:str): super().__init__(device, ExtAllocator(), None, None)
