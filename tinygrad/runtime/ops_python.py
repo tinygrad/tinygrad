0dacd444338b7ac64fe997d7d19c8b1ad3e5f2fa
Patrick Tsai <patosai@users.noreply.github.com>
2024-02-19 18:42:15 -0500

    lol

--- tinygrad/runtime/ops_python.py
+++ tinygrad/runtime/ops_python.py
@@ -134,7 +134,7 @@ class PythonProgram:
               ul[i] = list(struct.unpack(unpack_format, struct.pack(pack_format, *inp[0])))
             else:
               casted = [float(x) if dtypes.is_float(dtype) else int(x) if dtypes.is_int(dtype) else x for x in inp[0]]
-              overflowed = [(x % 2**(dtype.itemsize*8)) - (dtype.itemsize*8-1)*dtypes.is_unsigned(dtype) if dtypes.is_int(dtype)
+              overflowed = [(x % 2**(dtype.itemsize*8)) - 2**(dtype.itemsize*8-1)*(not dtypes.is_unsigned(dtype))*(x<0) if dtypes.is_int(dtype)
                             else x for x in casted]
               packed = struct.pack(pack_format if (dtypes.is_int(dtype) and dtypes.is_int(dtp[0]) and dtype.itemsize == dtp[0].itemsize)
                                    else unpack_format, *overflowed)
