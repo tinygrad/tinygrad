a2f5b7f53f11b9915e38b5ef1fbfe06c060d49bd
qazal <qazal.software@gmail.com>
2024-02-19 17:03:29 +0200

    schedule api
    1f284a31 these are mini versions of their bigger abstractions
    f43d2159 add unit test
    653abd43 realize the output buffer
    fa0fc12f basic scheduler
    fc3e88cd sched start

--- linearizer2.py
+++ linearizer2.py
@@ -1,17 +1,21 @@
-import graphlib, unittest
-from typing import Any, Dict, List, Tuple, Union
+import numpy as np
+from dataclasses import dataclass
+import graphlib, unittest, copy
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
 from tinygrad.codegen.kernel import LocalBuffer
 from tinygrad.codegen.uops import UOp, UOps
+from tinygrad.device import Buffer, BufferCopy, Compiled, Compiler, Device, JITRunner
 from tinygrad.dtype import PtrDType, dtypes
 from tinygrad.features.graph import print_tree
-from tinygrad.ops import BinaryOps, BufferOps, ConstBuffer, LazyOp, MemBuffer, ReduceOps
+from tinygrad.lazy import LazyBuffer
+from tinygrad.ops import BinaryOps, BufferOps, ConstBuffer, LazyOp, LoadOps, MemBuffer, Op, ReduceOps
 from tinygrad.renderer.cstyle import MetalRenderer
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.symbolic import Variable
 from tinygrad.tensor import Tensor
 from verify import verify, f32_to_bits
 from tinygrad.helpers import panic
-from tinygrad.codegen.linearizer import Linearizer as LinearizerOld
+from tinygrad.codegen.linearizer import MiniLinearizer as LinearizerOld
 
 def create_graph(outs: List[LazyOp]):
   ts = graphlib.TopologicalSorter()
@@ -21,7 +25,69 @@ def create_graph(outs: List[LazyOp]):
   for out in outs: _recursive_add(out)
   return tuple(ts.static_order())
 
-class Linearizer:
+class ASTRunner(JITRunner):
+  def __init__(self, ast: Tuple[LazyOp,...]):
+    self.ast = ast
+    self.device, self.compiler = cast(Compiled, Device[Device.DEFAULT]), cast(Compiler, Device[Device.DEFAULT].compiler)
+    super().__init__()
+  def __call__(self, rawbufs: List[Buffer], var_vals, wait=False, jit=False):
+    lin = MiniLinearizer(self.ast)
+    lin.linearize()
+    lib = self.compiler.compile(self.compiler.render("test", lin.uops))
+    prg = self.device.runtime("test", lib)
+    prg(*[x._buf for x in rawbufs])
+
+@dataclass(frozen=True)
+class MiniScheduleItem:
+  runner: JITRunner
+  rawbufs: List[Buffer]
+
+class Scheduler:
+  def __init__(self) -> None:
+    self.sched: List[MiniScheduleItem] = []
+    self.rawbufs: List[Buffer] = []
+    self.host_bufs: List[Buffer] = []
+
+  def create_schedule(self, lb:LazyBuffer):
+    if lb.base.op == LoadOps.COPY:
+      host_buf, device_buf = cast(Buffer,lb.base.srcs[0].realized), Buffer(lb.device, lb.size, lb.dtype)
+      self.sched.append(MiniScheduleItem(BufferCopy(), [device_buf, host_buf]))
+      self.rawbufs.append(device_buf)
+      self.host_bufs.append(host_buf)
+    else:
+      ast = graphlib.TopologicalSorter()
+      for src in lb.srcs: self.create_schedule(src)
+      src = self._recursive_lazyop(lb, ast)
+      assert src is not None
+      op = LazyOp(BufferOps.STORE, (src, ), MemBuffer(len(self.rawbufs), lb.dtype, lb.st.simplify().unbind()[0]))
+      store_buf = Buffer(lb.device, lb.size, lb.dtype)
+      lb.realized = store_buf
+      self.rawbufs.append(store_buf)
+      ast.add(op, src)
+      output_ast = tuple(ast.static_order())
+      self.sched.append(MiniScheduleItem(ASTRunner(output_ast), self.rawbufs))
+
+  def _recursive_lazyop(self, lb:LazyBuffer, ast) -> LazyOp:
+    # these ops have special sources
+    if lb.base.op == LoadOps.EMPTY: # LoadOps.EMPTY "defines" the input MemBuffer in the AST
+      assert isinstance(lb.realized, Buffer)
+      unbound_st, st_var_vals = lb.st.simplify().unbind()
+      assert st_var_vals == {}, "variables not supported yet"
+      return LazyOp(BufferOps.LOAD, (), MemBuffer(self.host_bufs.index(lb.realized), lb.dtype, unbound_st))
+    if lb.base.op == LoadOps.CONST: # Consts are always generated
+      return LazyOp(BufferOps.CONST, src=(), arg=ConstBuffer(val=lb.base.arg, dtype=lb.base.dtype, st=lb.st.simplify().unbind()[0]))
+    elif lb.base.op == LoadOps.COPY: return self._recursive_lazyop(lb.srcs[0], ast)
+
+    srcs: List[LazyOp] = []
+    for src in lb.base.srcs:
+      src_op = self._recursive_lazyop(src, ast)
+      if src_op is not None:
+        srcs.append(src_op)
+    op = LazyOp(cast(Op,lb.base.op), src=tuple(srcs))
+    ast.add(op, *srcs)
+    return op
+
+class MiniLinearizer:
   def __init__(self, ast):
     self.ast = ast
     self.uops: List[UOp] = []
@@ -84,6 +150,21 @@ class Linearizer:
     return self.uops
 
 class TestLinearizer2(unittest.TestCase):
+  def _new_realize(self, x):
+    scheduler = Scheduler()
+    scheduler.create_schedule(x.lazydata)
+    for si in scheduler.sched:
+      si.runner(si.rawbufs, var_vals={})
+    ret = x.numpy()
+    x.lazydata.realized = None
+    return ret
+
+  def test_add_simple(self):
+    x = Tensor([1]) + Tensor([67])
+    ret = self._new_realize(x)
+    expected = x.numpy()
+    np.testing.assert_equal(ret, expected)
+
   def test_multi_output_simple(self):
     a = LazyOp(BufferOps.LOAD, src=(), arg=MemBuffer(idx=0, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     b = LazyOp(BufferOps.LOAD, src=(), arg=MemBuffer(idx=1, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
@@ -91,7 +172,7 @@ class TestLinearizer2(unittest.TestCase):
     out0 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.ADD, src=(a,c)),), arg=MemBuffer(idx=2, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     out1 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.MUL, src=(a,b)),), arg=MemBuffer(idx=3, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     graph = create_graph([out0, out1])
-    uops = Linearizer(graph).linearize()
+    uops = MiniLinearizer(graph).linearize()
     alloc_data, init_outputs, prg, get_outputs = verify(uops)
     a, b = alloc_data([1]), alloc_data([2])
     outs = init_outputs([1,1])
@@ -107,7 +188,7 @@ class TestLinearizer2(unittest.TestCase):
     out2 = LazyOp(BufferOps.STORE, src=(LazyOp(ReduceOps.SUM, src=(a,)),), arg=MemBuffer(idx=4, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     out3 = LazyOp(BufferOps.STORE, src=(LazyOp(ReduceOps.MAX, src=(b,)),), arg=MemBuffer(idx=5, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     graph = create_graph([out0, out1, out2, out3])
-    uops = Linearizer(graph).linearize()
+    uops = MiniLinearizer(graph).linearize()
     alloc_data, init_outputs, prg, get_outputs = verify(uops)
     a, b = alloc_data([4,3,4]), alloc_data([10,20,3])
     outs = init_outputs([1,1,1,1])
@@ -121,7 +202,7 @@ class TestLinearizer2(unittest.TestCase):
     out0 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.ADD, src=(c,b)),), arg=MemBuffer(idx=2, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     out1 = LazyOp(BufferOps.STORE, src=(c,), arg=MemBuffer(idx=3, dtype=dtypes.int, st=ShapeTracker.from_shape((1,))))
     graph = create_graph([out0, out1])
-    uops = Linearizer(graph).linearize()
+    uops = MiniLinearizer(graph).linearize()
     alloc_data, init_outputs, prg, get_outputs = verify(uops)
     a, b = alloc_data([1,1,1,1]), alloc_data([2])
     outs = init_outputs([1,1])
@@ -135,7 +216,7 @@ class TestLinearizer2(unittest.TestCase):
     out0 = LazyOp(BufferOps.STORE, src=(reduce,), arg=MemBuffer(idx=2, dtype=dtypes.int, st=ShapeTracker.from_shape((1,1))))
     out1 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.MUL, src=(b,reduce), arg=((1,1))),), arg=MemBuffer(idx=3, dtype=dtypes.int, st=ShapeTracker.from_shape((1,1))))
     graph = create_graph([out0, out1])
-    uops = Linearizer(graph).linearize()
+    uops = MiniLinearizer(graph).linearize()
     alloc_data, init_outputs, prg, get_outputs = verify(uops)
     a, b = alloc_data(list(range(16))), alloc_data([2])
     outs = init_outputs([1,1])
@@ -148,9 +229,10 @@ class TestLinearizer2(unittest.TestCase):
     c = LazyOp(BufferOps.CONST, src=(), arg=ConstBuffer(val=4.0, dtype=dtypes.float, st=ShapeTracker.from_shape((1,))))
     out0 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.ADD, src=(a,b)),), arg=MemBuffer(idx=2, dtype=dtypes.float, st=ShapeTracker.from_shape((1,))))
     out1 = LazyOp(BufferOps.STORE, src=(LazyOp(BinaryOps.MUL, src=(a,c)),), arg=MemBuffer(idx=3, dtype=dtypes.float, st=ShapeTracker.from_shape((1,))))
-    uops = Linearizer(create_graph([out0, out1])).linearize()
+    uops = MiniLinearizer(create_graph([out0, out1])).linearize()
     alloc_data, init_outputs, prg, get_outputs = verify(uops)
     a, b = alloc_data([f32_to_bits(1)]), alloc_data([f32_to_bits(2)])
     outs = init_outputs([1,1])
     prg(a, b, *outs, global_size=(1,1,1), local_size=(1,1,1))
+
     assert get_outputs(outs, "f") == [[3.0], [4.0]]
