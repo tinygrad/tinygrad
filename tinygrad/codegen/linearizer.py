8a59741df922f7315694d7abc02b14375eddfb52
zibokapi <zibokapi@gmail.com>
2024-02-25 17:23:51 +0800

    revert extra/ changes

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -417,6 +417,7 @@ class Linearizer(Kernel):
         if u.uop is UOps.PHI and len(u.vin) == 3:
           # if the parents of the PHI node don't have the LOOP in their parents, it can be folded
           # TODO: ADD becomes a MUL, MAX can just become nothing
+          # TODO: del lol this is probably not the right fix or figure out why ADD cannot be folded into mul here
           if all(x.uop is not UOps.LOOP for x in get_recursive_parents(UOp(u.uop, u.dtype, u.vin[0:2], u.arg))) \
           and u.vin[1].arg is BinaryOps.ADD and u.vin[1].vin[0].arg is not BinaryOps.MUL:
             if DEBUG >= 4: print(f"removing PHI node {u}")
