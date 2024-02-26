a041fa25651539de4f5ef03596d35dc8f16f188f
Anurag Lamsal <vizculus@gmail.com>
2024-02-26 13:14:13 +0545

    creating device object in compile function rather than beam search function

--- tinygrad/features/search.py
+++ tinygrad/features/search.py
@@ -48,7 +48,8 @@ def _compile_linearizer(compiler:Compiler, lin:Linearizer, name:Optional[str]=No
   src = compiler.render(name if name is not None else to_function_name(lin.name), lin.uops)   # NOTE: these all have the same name for deduping
   return compiler.compile(src), lin.global_size, lin.local_size
 
-def _try_compile_linearized_w_idx(x, compiler:Compiler):
+def _try_compile_linearized_w_idx(x, device:str):
+  compiler = cast(Compiler, Device[device].compiler)
   try: return (x[0], _compile_linearizer(compiler, x[1], "test"))
   except Exception:
     if DEBUG >= 4: traceback.print_exc()
@@ -101,7 +102,7 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
   beam: List[Tuple[Linearizer, float]] = []
   seen_libs = set()
 
-  default_parallel = 1 if lin.opts.device in {"CUDA", "HIP"} else 0
+  default_parallel = 1 if lin.opts.device in {"CUDA", "HIP", "GPU"} else 0
   if beam_pool is None and getenv("PARALLEL", default_parallel): beam_pool = multiprocessing.Pool(multiprocessing.cpu_count(), _init_worker)
 
   try:
@@ -112,7 +113,7 @@ def beam_search(lin:Linearizer, rawbufs, amt:int, allow_test_size=True) -> Linea
     while not exiting:
       acted_lins = flatten([get_linearizer_actions(lin, include_0=False).values() for lin,_ in beam]) if len(beam) else [lin]
       timed_lins: List[Tuple[Linearizer, float]] = []
-      _compile_fn = functools.partial(_try_compile_linearized_w_idx, compiler=Device[lin.opts.device].compiler)
+      _compile_fn = functools.partial(_try_compile_linearized_w_idx, device=lin.opts.device)
       for i,proc in (map(_compile_fn, enumerate(acted_lins)) if beam_pool is None else beam_pool.imap_unordered(_compile_fn, enumerate(acted_lins))):
         if proc is None: continue
         lib, global_size, local_size = proc
