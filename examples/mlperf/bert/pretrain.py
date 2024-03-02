8b7a61d19a9ea7b78ac4ec9a2e7f07199fd172bc
Elias Wahl <Eliasruben2204@gmail.com>
2024-03-02 16:34:21 +0100

    multigpu support

--- examples/mlperf/bert/pretrain.py
+++ examples/mlperf/bert/pretrain.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 
 from tinygrad.helpers import getenv
+from tinygrad.device import Device
 from tinygrad.features.jit import TinyJit
 from tinygrad.ops import GlobalCounters
 from tinygrad.nn import Linear, optim
@@ -12,8 +13,10 @@ from extra.lr_scheduler import OneCycleLR
 from extra.models.bert import Bert
 from extra.datasets.wikipedia import iterate
 
-BS, EVAL_BS, STEPS, MAX_EVAL_STEPS, WARMUP_STEPS, EPOCH, MAX_LR  = getenv("BS", 32), getenv('EVAL_BS', 8), getenv("STEPS", 100000), getenv("MAX_EVAL_STEPS", 100), getenv("WARMUP_STEPS", 10000), getenv("EPOCHS", 30), getenv('MAX_LR', 2.0)
+BS, EVAL_BS, STEPS, MAX_EVAL_STEPS, WARMUP_STEPS, EPOCH, MAX_LR = getenv("BS", 32), getenv('EVAL_BS', 8), getenv("STEPS", 100000), getenv("MAX_EVAL_STEPS", 100), getenv("WARMUP_STEPS", 10000), getenv("EPOCHS", 30), getenv('MAX_LR', 2.0)
 EVAL_STEP_FREQ = int(math.floor(0.05 * (230.23 * BS + 3000000) / 25000) * 25000 / BS)
+GPUS = [f"{Device.DEFAULT}:{i}" for i in range(getenv('GPUS', 1))]
+for x in GPUS: Device[x]
 
 if getenv('WANDB', 0): 
   import wandb
@@ -89,8 +92,8 @@ def get_model(config_path:str):
 
 def save_model(model: BertMLperf, path:str = "/tmp/model"): safe_save(get_state_dict(model), path)
 def load_model(model:BertMLperf, path:str = "/tmp/model"): get_state_dict(model, safe_load(path))
-# ************ Actual training ************
 
+# ************ Actual training ************
 def pretrain():
   model = get_model(Path(__file__).parent.parents[2] / "extra" / "datasets" / "wiki" / "bert_config.json")
   optimizer = optim.LAMB(get_parameters(model), 1 / WARMUP_STEPS, eps=1e-6, wd=0.01, adam=True) # TODO: Keep in FP32?, Exclude LayerNorm, and bias from weight decay
@@ -120,6 +123,10 @@ def pretrain():
   train_batcher = iterate(bs=BS, val=False)
   eval_batcher = iterate(bs=EVAL_BS, val=True)
 
+  if len(GPUS) > 1:
+    for x in get_parameters(model):
+      x.to_(GPUS)
+
   epoch = 1
   wallclock_start = time.monotonic()
   accuracy_achieved = False
@@ -127,8 +134,9 @@ def pretrain():
     step = 0
     while step < STEPS:
       if step % EVAL_STEP_FREQ == 0 and step > 0 and not getenv('DISABLE_EVAL', 0):
+        train_step_jitted.reset()
         Tensor.train = False
-        accu = Tensor.zeros(1)
+        accu = Tensor.zeros(1, requires_grad=False)
         for _ in range(MAX_EVAL_STEPS):
           X, Y = next(eval_batcher)
           accu += eval_step_jitted(Tensor(X["input_ids"]), Tensor(X["segment_ids"]), Tensor(X["input_mask"]), Tensor(X["masked_lm_positions"]), Tensor(Y["masked_lm_ids"])).numpy()
@@ -139,7 +147,7 @@ def pretrain():
           wallclock_end = time.monotonic()
           hours, minutes = divmod((wallclock_end - wallclock_start) / 3600, 1)
           print(f"MLM accuracy achieved in {int(hours)} hours and {int(minutes * 60)} minutes.")
-          save_model(model, getenv('SAVE_PATH', "/tmp/bert_mlperf.safetensors"))
+          save_model(model, getenv('SAVE_PATH', getenv("SAVE_PATH", "/raid/tmp/")) + "final.safetensors")
           accuracy_achieved = True
           break
       
@@ -147,15 +155,25 @@ def pretrain():
 
       st = time.monotonic()
       X, Y = next(train_batcher) 
+      input_ids, segment_ids, input_mask, masked_lm_positions, masked_lm_ids, next_sentence_labels = Tensor(X["input_ids"], dtype=dtypes.default_float), Tensor(X["segment_ids"], dtype=dtypes.default_float), Tensor(X["input_mask"], dtype=dtypes.default_float), Tensor(X["masked_lm_positions"], dtype=dtypes.default_float), Tensor(Y["masked_lm_ids"], dtype=dtypes.default_float), Tensor(Y["next_sentence_labels"], dtype=dtypes.default_float)
+      if len(GPUS) > 1:
+        input_ids.shard_(GPUS, axis=0)
+        segment_ids.shard_(GPUS, axis=0)
+        input_mask.shard_(GPUS, axis=0)
+        masked_lm_positions.shard_(GPUS, axis=0)
+        masked_lm_ids.shard_(GPUS, axis=0)
+        next_sentence_labels.shard_(GPUS, axis=0)
+
       mem = time.monotonic()
       GlobalCounters.reset()
 
-      loss = train_step_jitted(Tensor(X["input_ids"]).realize(), Tensor(X["segment_ids"]).realize(), Tensor(X["input_mask"]).realize(), Tensor(X["masked_lm_positions"]).realize(), Tensor(Y["masked_lm_ids"]).realize(), Tensor(Y["next_sentence_labels"]).realize())
+      loss = train_step_jitted(input_ids, segment_ids, input_mask, masked_lm_positions, masked_lm_ids, next_sentence_labels)
       et = time.monotonic()
       loss_cpu = loss.numpy()
       cl = time.monotonic()
+      device_str = loss.device if isinstance(loss.device, str) else f"{loss.device[0]} * {len(loss.device)}"
 
-      print(f"{step:3d} {(cl-st)*1000.0:7.2f} ms run, {(mem-st)*1000.0:7.2f} ms fetch, {(et-mem)*1000.0:7.2f} ms python, {(cl-et)*1000.0:7.2f} ms CL, {loss_cpu:7.2f} loss, {(lr := optimizer.lr.numpy()[0]):.6f} LR, {GlobalCounters.mem_used/1e9:.2f} GB used, {GlobalCounters.global_ops*1e-9/(cl-st):9.2f} GFLOPS")
+      print(f"{step:3d} {(cl-st)*1000.0:7.2f} ms run, {(mem-st)*1000.0:7.2f} ms fetch, {(et-mem)*1000.0:7.2f} ms python, {(cl-et)*1000.0:7.2f} ms {device_str}, {loss_cpu:7.2f} loss, {(lr := optimizer.lr.numpy()[0]):.6f} LR, {GlobalCounters.mem_used/1e9:.2f} GB used, {GlobalCounters.global_ops*1e-9/(cl-st):9.2f} GFLOPS")
       wandb.log({"Loss": loss_cpu, "LR": lr}) if getenv('WANDB', 0) else None
       st = cl
       step += 1
