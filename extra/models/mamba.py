4dc020bd689fcc8d7a2d0a713451a198d6c247f2
Caleb Bunch <rcbcg123@gmail.com>
2024-03-01 22:24:08 -0500

    mamba file running with no torch and no errors, bugs though

--- extra/models/mamba.py
+++ extra/models/mamba.py
@@ -1,134 +1,342 @@
-# https://arxiv.org/pdf/2312.00752.pdf
-# https://medium.com/ai-insights-cobet/building-mamba-from-scratch-a-comprehensive-code-walkthrough-5db040c28049
+"""Simple, minimal implementation of Mamba in one file of PyTorch.
 
-from tinygrad import Tensor, nn, dtypes
+Suggest reading the following before/while reading the code:
+    [1] Mamba: Linear-Time Sequence Modeling with Selective State Spaces (Albert Gu and Tri Dao)
+        https://arxiv.org/abs/2312.00752
+    [2] The Annotated S4 (Sasha Rush and Sidd Karamcheti)
+        https://srush.github.io/annotated-s4
 
-#TODO: put these params in main and pass to classes
-device = "CUDA"
+Glossary:
+    b: batch size                       (`B` in Mamba paper [1] Algorithm 2)
+    l: sequence length                  (`L` in [1] Algorithm 2)
+    d or d_model: hidden dim
+    n or d_state: latent state dim      (`N` in [1] Algorithm 2)
+    expand: expansion factor            (`E` in [1] Section 3.4)
+    d_in or d_inner: d * expand         (`D` in [1] Algorithm 2)
+    A, B, C, D: state space parameters  (See any state space representation formula)
+                                        (B, C are input-dependent (aka selective, a key innovation in Mamba); A, D are not)
+    Δ or delta: input-dependent step size
+    dt_rank: rank of Δ                  (See [1] Section 3.6 "Parameterization of ∆")
 
-DIFF_H = 0
-d_model = 8
-state_size = 128  
-seq_len = 100  
-batch_size = 256 
-last_batch_size = 81 # TODO: len(data_set) % batch_size
-current_batch_size = batch_size
-different_batch_size = False
-h_new = None
-temp_buffer = None
+"""
 
+import math
+import json
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+from dataclasses import dataclass
+# from einops import rearrange, repeat, einsum
+from typing import Union, Dict, List
+from tinygrad import Tensor, Device, nn, dtypes
+from tinygrad.helpers import Timing
+from transformers.utils.hub import cached_file
+import numpy as np
 
-class S6Layer():
-  def __init__(self, seq_len, d_model, state_size, device):
-    self.fc1 = nn.Linear(d_model, d_model)
-    self.fc2 = nn.Linear(d_model, state_size)
-    self.fc3 = nn.Linear(d_model, state_size)
-
-    self.seq_len = seq_len
-    self.d_model = d_model
-    self.state_size = state_size
-
-    # NOTE: this is the same as functional.normalize(torch.ones)
-    self.A = Tensor.full((d_model, state_size), 0.378, device=device)
-    self.B = Tensor.zeros(batch_size, self.seq_len, self.state_size, device=device)
-    self.C = Tensor.zeros(batch_size, self.seq_len, self.state_size, device=device)
-    self.delta = Tensor.zeros(batch_size, self.seq_len, self.d_model, device=device)
-    self.dA = Tensor.zeros(batch_size, self.seq_len, self.d_model, self.state_size, device=device)
-    self.dB = Tensor.zeros(batch_size, self.seq_len, self.d_model, self.state_size, device=device)
-    self.h = Tensor.zeros(batch_size, self.seq_len, self.d_model, self.state_size, device=device)
-    self.y = Tensor.zeros(batch_size, self.seq_len, self.d_model, device=device)
-
-  def discretization(self):
-    # https://arxiv.org/pdf/2312.00752.pdf (p. 28)
-    self.dB = Tensor.einsum("bld,bln->bldn", (self.delta, self.B))
-    self.dA = Tensor.einsum("bld,dn->bldn", (self.delta, self.A)).exp()
-    return self.dA, self.dB
-
-  def __call__(self, x):
-    self.B = self.fc2(x)
-    self.C = self.fc3(x)
-    self.delta = self.fc1(x).softplus()
-    self.discretization()
-
-    if DIFF_H:  
-      global current_batch_size
-      current_batch_size = x.shape[0]
-      if self.h.shape[0] != current_batch_size:
-        different_batch_size = True
-        h_new = self.dA.einsum("bldn,bldn->bldn", self.h[:current_batch_size, ...]) + x.reshape(current_batch_size, x.shape[1], 1) * self.dB
-      else:
-        different_batch_size = False
-        h_new = self.dA.einsum("bldn,bldn->bldn", self.h) + x.reshape(current_batch_size, x.shape[1], x.shape[2], 1)
-      self.y = self.C.einsum("bln,bldn->bld", h_new)
-      global temp_buffer
-      # TODO: do we need to clone/copy?
-      temp_buffer = h_new.detach() if not self.h.requires_grad else h_new()
-      return self.y
-    else: 
-      h = Tensor.zeros(x.shape[0], self.seq_len, self.d_model, self.state_size, device=x.device)
-      y = Tensor.zeros_like(x)
-      h = Tensor.einsum("bldn,bldn->bldn", (self.dA, h)) + x.reshape(current_batch_size, x.shape[1], x.shape[2], 1) * self.dB
-      y = Tensor.einsum("bln,bldn->bld", (self.C, h))
-      return y
-
-
-class Block():
-  def __init__(self, seq_len, d_model, state_size, device):
-    self.inp_proj = nn.Linear(d_model, 2*d_model)
-    self.out_proj = nn.Linear(2*d_model, d_model)
-    self.D = nn.Linear(d_model, 2*d_model)
-
-    self.out_proj.bias = Tensor.ones(self.out_proj.bias)
-    self.S6 = S6Layer(seq_len, 2*d_model, state_size, device)
-    self.conv = nn.Conv1d(seq_len, seq_len, kernel_size=3, padding=1)
-    self.conv_linear = nn.Linear(2*d_model, 2*d_model)
-    self.norm = RMSNorm(d_model, device=device)
-
-  def __call__(self, x):
-    x = self.norm(x)
-    x_proj = self.inp_proj(x)
-    x_conv = self.conv(x_proj)
-    x_conv_act = x_conv.silu()
-    x_conv_out = self.conv_linear(x_conv_act)
-    x_ssm = self.S6(x_conv_out)
-    x_act = x_ssm.silu()  
-    x_residual = self.D(x).silu()
-    x_combined = x_act * x_residual
-    x_out = self.out_proj(x_combined)
-    return x_out
+@dataclass
+class ModelArgs:
+  d_model: int
+  n_layer: int
+  vocab_size: int
+  d_state: int = 16
+  expand: int = 2
+  dt_rank: Union[int, str] = "auto"
+  d_conv: int = 4 
+  pad_vocab_size_multiple: int = 8
+  conv_bias: bool = True
+  bias: bool = False
     
-    
+  def __post_init__(self):
+    self.d_inner = int(self.expand * self.d_model)
+    if self.dt_rank == "auto": self.dt_rank = math.ceil(self.d_model / 16)
+    if self.vocab_size % self.pad_vocab_size_multiple != 0:
+      self.vocab_size += (self.pad_vocab_size_multiple - self.vocab_size % self.pad_vocab_size_multiple)
+
+
 class Mamba():
-  def __init__(self, seq_len, d_model, state_size, device):
-    self.mb1 = Block(seq_len, d_model, state_size, device)
-    self.mb2 = Block(seq_len, d_model, state_size, device)
-    self.mb3 = Block(seq_len, d_model, state_size, device)
+  def __init__(self, args: ModelArgs):
+    """Full Mamba model."""
+    self.args = args
+    self.embedding = nn.Embedding(args.vocab_size, args.d_model)
+    # TODO: does nn.state.get_parameters catch these?
+    self.layers: List[ResidualBlock] = [ResidualBlock(args) for _ in range(args.n_layer)]
+    self.norm_f = RMSNorm(args.d_model)
+    self.lm_head = nn.Linear(args.d_model, args.vocab_size, bias=False)
+    self.lm_head.weight = self.embedding.weight
 
-  def __call__(self, x):
-    x = self.mb1(x)
-    x = self.mb2(x)
-    x = self.mb3(x)
-    return x
+  def __call__(self, input_ids):
+    """
+    Args:
+        input_ids (long tensor): shape (b, l)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+    Returns:
+        logits: shape (b, l, vocab_size)
+
+    Official Implementation:
+        class MambaLMHeadModel, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/models/mixer_seq_simple.py#L173
+
+    """
+    x = self.embedding(input_ids)
+    for layer in self.layers: x = layer(x)    
+    x = self.norm_f(x)
+    logits = self.lm_head(x)
+    return logits
+  
+  @staticmethod
+  def from_pretrained(pretrained_model_name: str):
+    """Load pretrained weights from HuggingFace into model.
+    Args:
+        pretrained_model_name: One of
+            * "state-spaces/mamba-2.8b-slimpj"
+            * "state-spaces/mamba-2.8b"
+            * "state-spaces/mamba-1.4b"
+            * "state-spaces/mamba-790m"
+            * "state-spaces/mamba-370m"
+            * "state-spaces/mamba-130m"
+                        
+    Returns:
+        model: Mamba model with weights loaded
+
+    """
+
+    def load_config_hf(model_name: str):
+      resolved_archive_file = cached_file(model_name, "config.json", _raise_exceptions_for_missing_entries=False)
+      return json.load(open(resolved_archive_file))
     
-class RMSNorm():
-  def __init__(self, d_model: int, eps:float=1e-5, device="CUDA"):
-    self.eps = eps
-    # TODO: does this get counted in nn.state.get_parameters()
-    self.weight = Tensor.ones(d_model, device=device)
+    
+    def load_state_dict_hf(model_name: str) -> Dict[str, Tensor]:
+      resolved_archive_file = cached_file(model_name, "pytorch_model.bin", _raise_exceptions_for_missing_entries=False)
+      return nn.state.torch_load(resolved_archive_file)
+    
+    config_data = load_config_hf(pretrained_model_name)
+    args = ModelArgs(
+      d_model=config_data["d_model"],
+      n_layer=config_data["n_layer"],
+      vocab_size=config_data["vocab_size"]
+    )
+    
+    model = Mamba(args)
+    state_dict = load_state_dict_hf(pretrained_model_name)
+    # new_state_dict = {}
+    # for key in state_dict:
+    #   new_key = key.replace("backbone.", "")
+    #   new_state_dict[new_key] = state_dict[key]
+    with Timing("weights -> model: "):
+      # nn.state.load_state_dict(model, fix_bf16(convert_from_huggingface(part1, model, 32, 8)), strict=False)
+      nn.state.load_state_dict(model, state_dict, strict=False)
+
+    return model
+
+
+class ResidualBlock():
+  def __init__(self, args: ModelArgs):
+    """Simple block wrapping Mamba block with normalization and residual connection."""
+
+    self.args = args
+    self.mixer = MambaBlock(args)
+    self.norm = RMSNorm(args.d_model)
 
   def __call__(self, x):
-    output = x * ((x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight).rsqrt()
+    """
+    Args:
+        x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+    Returns:
+        output: shape (b, l, d)
+
+    Official Implementation:
+        Block.forward(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L297
+        
+        Note: the official repo chains residual blocks that look like
+            [Add -> Norm -> Mamba] -> [Add -> Norm -> Mamba] -> [Add -> Norm -> Mamba] -> ...
+        where the first Add is a no-op. This is purely for performance reasons as this
+        allows them to fuse the Add->Norm.
+
+        We instead implement our blocks as the more familiar, simpler, and numerically equivalent
+            [Norm -> Mamba -> Add] -> [Norm -> Mamba -> Add] -> [Norm -> Mamba -> Add] -> ....
+        
+    """
+    output = self.mixer(self.norm(x)) + x
+    return output
+            
+
+class MambaBlock():
+  def __init__(self, args: ModelArgs):
+    """A single Mamba block, as described in Figure 3 in Section 3.4 in the Mamba paper [1]."""
+    self.args = args
+    self.in_proj = nn.Linear(args.d_model, args.d_inner * 2, bias=args.bias)
+    self.conv1d = nn.Conv1d(
+      in_channels=args.d_inner,
+      out_channels=args.d_inner,
+      bias=args.conv_bias,
+      kernel_size=args.d_conv,
+      groups=args.d_conv,
+      padding=args.d_conv - 1
+    )
+    # x_proj takes in `x` and outputs the input-specific Δ, B, C
+    self.x_proj = nn.Linear(args.d_inner, args.dt_rank + args.d_state * 2, bias=False)
+    # dt_proj projects Δ from dt_rank to d_in
+    self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
+    A = Tensor.arange(1, args.d_state + 1).repeat([args.d_inner, 1])
+    self.A_log = A.log()
+    self.D = Tensor.ones(args.d_inner)
+    self.out_proj = nn.Linear(args.d_inner, args.d_model, bias=args.bias)
+    
+  def __call__(self, x: Tensor):
+    """Mamba block forward. This looks the same as Figure 3 in Section 3.4 in the Mamba paper [1].
+
+    Args:
+        x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+    Returns:
+        output: shape (b, l, d)
+    
+    Official Implementation:
+        class Mamba, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L119
+        mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
+        
+    """
+    (b, l, d) = x.shape
+    x_and_res = self.in_proj(x)  # shape (b, l, 2 * d_in)
+    (x, res) = x_and_res.split(sizes=[self.args.d_inner, self.args.d_inner], dim=-1)
+    x = x.reshape((x.shape[0], x.shape[2], x.shape[1]))
+    x = self.conv1d(x)[:, :, :l]
+    x = x.reshape((x.shape[0], x.shape[2], x.shape[1]))
+    x = x.silu()
+    y = self.ssm(x)
+    y = y * res.silu()
+    output = self.out_proj(y)
+    return output
+  
+  def ssm(self, x):
+    """Runs the SSM. See:
+        - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+        - run_SSM(A, B, C, u) in The Annotated S4 [2]
+
+    Args:
+        x: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+    Returns:
+        output: shape (b, l, d_in)
+
+    Official Implementation:
+        mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
+        
+    """
+    (d_in, n) = self.A_log.shape
+
+    # Compute ∆ A B C D, the state space parameters.
+    #     A, D are input independent (see Mamba paper [1] Section 3.5.2 "Interpretation of A" for why A isn"t selective)
+    #     ∆, B, C are input-dependent (this is a key difference between Mamba and the linear time invariant S4,
+    #                                  and is why Mamba is called **selective** state spaces)
+    
+    A = -self.A_log.float().exp() # shape (d_in, n)
+    D = self.D.float()
+    x_dbl = self.x_proj(x)  # (b, l, dt_rank + 2*n)
+    (delta, B, C) = x_dbl.split(sizes=[self.args.dt_rank, n, n], dim=-1)  # delta: (b, l, dt_rank). B, C: (b, l, n)
+    delta = self.dt_proj(delta).softplus()  # (b, l, d_in)
+    y = self.selective_scan(x, delta, A, B, C, D)  # This is similar to run_SSM(A, B, C, u) in The Annotated S4 [2]
+    return y
+
+    
+  def selective_scan(self, u, delta, A, B, C, D):
+    """Does selective scan algorithm. See:
+        - Section 2 State Space Models in the Mamba paper [1]
+        - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+        - run_SSM(A, B, C, u) in The Annotated S4 [2]
+
+    This is the classic discrete state space formula:
+        x(t + 1) = Ax(t) + Bu(t)
+        y(t)     = Cx(t) + Du(t)
+    except B and C (and the step size delta, which is used for discretization) are dependent on the input x(t).
+
+    Args:
+        u: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
+        delta: shape (b, l, d_in)
+        A: shape (d_in, n)
+        B: shape (b, l, n)
+        C: shape (b, l, n)
+        D: shape (d_in,)
+
+    Returns:
+        output: shape (b, l, d_in)
+
+    Official Implementation:
+        selective_scan_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L86
+        Note: I refactored some parts out of `selective_scan_ref` out, so the functionality doesn"t match exactly.
+        
+    """
+    (b, l, d_in) = u.shape
+    n = A.shape[1]
+    
+    # Discretize continuous parameters (A, B)
+    # - A is discretized using zero-order hold (ZOH) discretization (see Section 2 Equation 4 in the Mamba paper [1])
+    # - B is discretized using a simplified Euler discretization instead of ZOH. From a discussion with authors:
+    #   "A is the more important term and the performance doesn"t change much with the simplification on B"
+    deltaA = Tensor.einsum("bld,dn->bldn", delta, A).exp()
+    deltaB_u = Tensor.einsum("bld,bln,bld->bldn", delta, B, u)
+    # Perform selective scan (see scan_SSM() in The Annotated S4 [2])
+    # Note that the below is sequential, while the official implementation does a much faster parallel scan that
+    # is additionally hardware-aware (like FlashAttention).
+    x = Tensor.zeros((b, d_in, n), device=deltaA.device)
+    ys = []    
+    for i in range(l):
+      x = deltaA[:, i] * x + deltaB_u[:, i]
+      y = Tensor.einsum("bdn,bn->bd", x, C[:, i, :])
+      ys.append(y)
+    y = Tensor.stack(ys, dim=1)  # shape (b, l, d_in)
+    y = y + u * D
+    return y
+
+
+class RMSNorm():
+  def __init__(self, d_model: int, eps: float = 1e-5):
+    self.eps = eps
+    self.weight = Tensor.ones(d_model)
+
+  def __call__(self, x: Tensor):
+    output = x * (x.pow(2).mean(axis=-1, keepdim=True) + self.eps).rsqrt() * self.weight
     return output
 
-def main():
-  x = Tensor.rand(batch_size, seq_len, d_model, device=device)
-  mamba = Mamba(seq_len, d_model, state_size, device)
-  rmsnorm = RMSNorm(d_model)
-  x = rmsnorm(x)
-  output = mamba(x)
-  print(f"shape: {output.shape}")
 
+def generate(model, tokenizer, prompt: str, gen_length: int = 50, sample: bool = True, top_k: int = 40):
+  inp = Tensor(tokenizer(prompt, max_length=gen_length, truncation=True, return_tensors="np")["input_ids"])
+  for tok in range(gen_length):
+    indices = inp
+    next_logits = model(indices)[:, -1]   
+    probs = next_logits.softmax(axis=-1)
+    (batch, vocab_size) = probs.shape     
+    if top_k is not None:
+      # TODO: do not convert to np - Tensor probably has something that can do this
+      probs = probs.numpy()
+      target_indices = np.argpartition(probs, -top_k, axis=1)[:, -top_k:]
+      values = np.take_along_axis(probs, target_indices, axis=1)
+      probs[probs < values[:, -1: None]] = 0
+      probs = Tensor(probs)
+      probs /= probs.sum(axis=1, keepdim=True)
+
+    nxt = probs.multinomial(num_samples=1) if sample else probs.argmax(axis=-1)[:, None] 
+    inp = inp.cat(nxt, dim=1)
+
+    out = [tokenizer.decode(output.numpy().tolist()) for output in inp][0]
+    return out
+
+
+def main():
+    # TODO: add device=device support
+    from transformers import AutoTokenizer
+    # One of:
+    #     "state-spaces/mamba-2.8b-slimpj"
+    #     "state-spaces/mamba-2.8b"
+    #     "state-spaces/mamba-1.4b"
+    #     "state-spaces/mamba-790m"
+    #     "state-spaces/mamba-370m"
+    #     "state-spaces/mamba-130m"
+    pretrained_model_name = "state-spaces/mamba-370m"
+
+    model = Mamba.from_pretrained(pretrained_model_name)
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+    print(generate(model, tokenizer, "Mamba is the:"))
 
 if __name__ == "__main__":
   main()
-
