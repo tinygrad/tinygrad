fffe459408f79afef3281a35f567b29463428fad
Zaffer <jameswiles@gmail.com>
2024-03-03 21:03:43 -0500

    Remove LLaMa benchmark and add HSA specific test

---
+++ test/external/external_test_hsa_backend.py
@@ -0,0 +1,33 @@
+import unittest
+import subprocess
+import os
+
+
+class TestHSABackend(unittest.TestCase):
+  def test_llama_with_hsa(self):
+    result = subprocess.run(
+      [
+        "python3",
+        "examples/llama.py",
+        "--temperature=0",
+        "--count=50",
+        '--prompt="Hello."',
+        "--timing",
+        "--shard",
+        "2",
+        "--size",
+        "70B",
+        "--gen",
+        "2",
+      ],
+      capture_output=True,
+      text=True,
+    )
+    self.assertEqual(result.returncode, 0)
+
+    # Optional: Check output for expected values
+    # self.assertIn("expected output", result.stdout)
+
+
+if __name__ == "__main__":
+    unittest.main()
