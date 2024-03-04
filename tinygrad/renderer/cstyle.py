378f26189111524a1b2c3aca19b8e8ec268679c2
Zaffer <51871197+Zaffer@users.noreply.github.com>
2024-03-04 11:57:13 -0500

    Update cstyle.py

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -309,6 +309,5 @@ class HIPLanguage(CStyleLanguage):
   uses_ptr_arithmetic = True
   type_map = {dtypes.bfloat16: "hip_bfloat16"}
   def render_cast(self, x: List[str], var_dtype: DType, bitcast=False) -> str:
-    return (f"static_cast<{self.type_map.get(var_dtype)}>(static_cast<unsigned short>("
-      f"reinterpret_cast<const unsigned int*>(&{x[0]})[0] >> 16))" if var_dtype == dtypes.bfloat16 else super().render_cast(x, var_dtype, bitcast))
+    return (f"__float_as_uint({x[0]}) >> 16" if var_dtype == dtypes.bfloat16 else super().render_cast(x, var_dtype, bitcast))
 HIPRenderer = functools.partial(uops_to_cstyle, HIPLanguage())
