8a234dd2430920ea4c57ee574ed1467d1ca17c3d
reddyn <nikidsniper@gmail.com>
2024-03-04 15:59:13 -0500

    skip model test in CI and add CLANG fix

--- test/models/test_mamba.py
+++ test/models/test_mamba.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer
 PROMPT = 'Why is gravity '
 TOKENIZER = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
-@unittest.skipIf(CI and Device.DEFAULT in ["CLANG"], ".softmax().argmax() broken in CLANG")
+@unittest.skipIf(CI, "model is slow for CI")
 class TestMamba(unittest.TestCase):
   def test_mamba_130M(self):
     OUT_130M = '''Why is gravity \nnot a good idea?\n\nA:'''
