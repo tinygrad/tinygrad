25b4658c2494cec3eca337f61bfae80a85b6c653
Zaffer <jameswiles@gmail.com>
2024-03-04 22:10:22 -0500

    Refactor casting logic in HIPLanguage class

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -309,5 +309,5 @@ class HIPLanguage(CStyleLanguage):
   uses_ptr_arithmetic = True
   type_map = {dtypes.bfloat16: "hip_bfloat16"}
   def render_cast(self, x: List[str], var_dtype: DType, bitcast=False) -> str:
-    return (f"static_cast<hip_bfloat16>(__float_as_uint({x[0]}) >> 16)" if var_dtype == dtypes.bfloat16 else super().render_cast(x, var_dtype, bitcast))
+    return (f"float_to_bfloat16({x[0]}f)" if var_dtype == dtypes.bfloat16 else super().render_cast(x, var_dtype, bitcast))
 HIPRenderer = functools.partial(uops_to_cstyle, HIPLanguage())
