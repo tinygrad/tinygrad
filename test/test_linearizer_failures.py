1b3d70e65821f9c3e8c0ea18d5f19859edeace3a
chenyu <chenyu@fastmail.com>
2024-03-08 09:08:06 -0500

    test_failure_21 LLVM PADTO

--- test/test_linearizer_failures.py
+++ test/test_linearizer_failures.py
@@ -165,5 +165,11 @@ class TestLinearizerFailures(unittest.TestCase):
     opts = [Opt(op=OptOps.UPCAST, axis=1, amt=0), Opt(op=OptOps.UPCAST, axis=0, amt=0)]
     helper_test_lin(Linearizer(ast), opts, failed_platforms=[])
 
+  def test_failure_21(self):
+    ast = LazyOp(op=BufferOps.STORE, src=(LazyOp(op=BufferOps.CONST, src=(), arg=ConstBuffer(val=1.0, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(45, 65), strides=(0, 0), offset=0, mask=None, contiguous=False),)))),), arg=MemBuffer(idx=0, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(45, 65), strides=(65, 1), offset=0, mask=None, contiguous=True),))))
+    opts = [Opt(op=OptOps.PADTO, axis=0, amt=32)]
+    # LLVM: RuntimeError: PHINode should have one entry for each predecessor of its parent basic block!
+    helper_test_lin(Linearizer(ast), opts, failed_platforms=["LLVM"])
+
 if __name__ == '__main__':
   unittest.main()
