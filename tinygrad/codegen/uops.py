a6f4405c6ed1975de9037cdf24e18f921164f904
Patrick Tsai <patosai@users.noreply.github.com>
2024-03-10 00:47:58 -0500

    Lint

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -208,7 +208,8 @@ class UOpGraph:
     modified_phis = set()
     allowed_phi_parents = {UOps.CONST, UOps.SPECIAL, UOps.ALU, UOps.LOOP, UOps.DEFINE_ACC}
     for phi in [op for op in self.uops if op.uop is UOps.PHI]:
-      if any([op for op in get_recursive_parents(phi) if op.uop not in allowed_phi_parents]) or len([op for op in get_recursive_parents(phi) if op.uop is UOps.DEFINE_ACC]) > 1: continue
+      if (any([op for op in get_recursive_parents(phi) if op.uop not in allowed_phi_parents])
+        or len([op for op in get_recursive_parents(phi) if op.uop is UOps.DEFINE_ACC]) > 1): continue
       for where in sorted([op for op in get_recursive_parents(phi) if op.arg == TernaryOps.WHERE], key=lambda x: self.uops.index(x)):
         loop_op = phi.vin[2]
         comp, comp_lt, comp_gt = where.vin[0], where.vin[0].vin[0], where.vin[0].vin[1]
