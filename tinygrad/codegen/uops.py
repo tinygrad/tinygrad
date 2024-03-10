3a592bf3117b320346f62d8ec85bd59be44eebc9
George Hotz <geohot@gmail.com>
2024-03-10 15:33:41 -0700

    more wrong logic

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -113,7 +113,8 @@ class UOpGraph:
                 if vin[1-x].vin[y].uop is UOps.CONST:
                   computed_val = exec_alu(ops, dtype, [vin[x].arg, vin[1-x].vin[y].arg])
                   const = self.add(UOps.CONST, dtype, arg=computed_val, insert_before=insert_before)
-                  return self.add(UOps.ALU, dtype, (vin[1-x].vin[1-y], const), ops, insert_before=insert_before)
+                  return self.add(UOps.ALU, dtype, (vin[1-x].vin[1-y], const), ops,
+                                  insert_before=insert_before+1 if insert_before is not None else None)
           # (x+c0)*c1 = x*c1 + (c0*c1)
           if arg is BinaryOps.MUL and vin[x].uop is UOps.CONST and vin[1-x].uop is UOps.ALU and vin[1-x].arg is BinaryOps.ADD:
             for y in [0,1]:
