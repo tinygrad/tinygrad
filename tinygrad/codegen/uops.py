95e334b1fd458a54a85971d85d8abc54a47e1370
Patrick Tsai <patosai@users.noreply.github.com>
2024-03-11 02:56:19 -0400

    accurate var names

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -185,7 +185,6 @@ class UOpGraph:
     self.uops.remove(old)
 
   def simplify_phi_loops(self, get_recursive_parents):
-    seen_vars: Dict[UOp,str] = {}
     def alu_opposite(arg, x, y):
       if arg == BinaryOps.ADD: return x - y
       elif arg == BinaryOps.MUL: return Node.__floordiv__(x, y, False)
@@ -193,7 +192,7 @@ class UOpGraph:
     def to_symbolic(u: UOp):
       if u.uop == UOps.CONST: return NumNode(int(u.arg))
       elif u.uop in {UOps.LOOP, UOps.SPECIAL}:
-        if u not in seen_vars: seen_vars[u] = "var{}".format(len(seen_vars))
+        if u not in seen_vars: seen_vars[u] = u.arg[1] if u.uop is UOps.SPECIAL else "loop{}".format(len(seen_vars))
         return Variable(seen_vars[u], u.vin[0].arg, u.vin[1].arg-1) if u.uop is UOps.LOOP else Variable(seen_vars[u], 0, u.arg[2]-1)
       elif u.uop == UOps.ALU and u.arg == BinaryOps.ADD: return to_symbolic(u.vin[0]) + to_symbolic(u.vin[1])
       elif u.uop == UOps.ALU and u.arg == BinaryOps.MUL: return to_symbolic(u.vin[0]) * to_symbolic(u.vin[1])
@@ -208,15 +207,16 @@ class UOpGraph:
     def const(x): return self.add(UOps.CONST, dtypes.default_int, tuple(), x)
     def neg(x): return self.add(UOps.ALU, dtypes.default_int, (x,), UnaryOps.NEG)
     def max(x, y): return self.add(UOps.ALU, dtypes.default_int, (x, y), BinaryOps.MAX)
+    def uop_alu_idx(a: UOp, b, op, dtype=dtypes.default_int):
+      render_b: UOp = cast(UOp, (NumNode(b) if not isinstance(b, Node) else b).render(render_ops))
+      return self.add(UOps.ALU, dtype, (a, render_b), op)
+    seen_vars: Dict[UOp,str] = {}
     render_ops = {Variable: lambda self, ops, _: next(op for op, name in seen_vars.items() if name == self.expr),
                   NumNode: lambda self, ops, _: const(self.b),
                   MulNode: lambda self, ops, _: uop_alu_idx(self.a.render(ops, self), self.b, BinaryOps.MUL),
                   DivNode: lambda self, ops, _: uop_alu_idx(self.a.render(ops, self), self.b, BinaryOps.DIV),
                   SumNode: lambda self, ops, _:
                   functools.reduce(lambda a, b: uop_alu_idx(a, b, BinaryOps.ADD), self.nodes[1:], self.nodes[0].render(ops, self))}
-    def uop_alu_idx(a: UOp, b, op, dtype=dtypes.default_int):
-      render_b: UOp = cast(UOp, (NumNode(b) if not isinstance(b, Node) else b).render(render_ops))
-      return self.add(UOps.ALU, dtype, (a, render_b), op)
 
     allowed_ops = {UOps.CONST, UOps.SPECIAL, UOps.ALU, UOps.LOOP, UOps.DEFINE_ACC}
     allowed_alus = {BinaryOps.MUL, BinaryOps.ADD, BinaryOps.CMPLT, TernaryOps.WHERE}
