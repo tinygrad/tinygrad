60751f178f1edc43cdb09f3c55dddbd53260682d
a <a@a.aa>
2024-03-11 17:43:48 +0000

    fix indent

--- examples/stable_diffusion.py
+++ examples/stable_diffusion.py
@@ -514,10 +514,10 @@ class ClipTokenizer:
 
 
 def get_alphas_cumprod(beta_start=0.00085, beta_end=0.0120, n_training_steps=1000):
-    betas = np.linspace(beta_start ** 0.5, beta_end ** 0.5, n_training_steps, dtype=np.float32) ** 2
-    alphas = 1.0 - betas
-    alphas_cumprod = np.cumprod(alphas, axis=0)
-    return Tensor(alphas_cumprod)
+  betas = np.linspace(beta_start ** 0.5, beta_end ** 0.5, n_training_steps, dtype=np.float32) ** 2
+  alphas = 1.0 - betas
+  alphas_cumprod = np.cumprod(alphas, axis=0)
+  return Tensor(alphas_cumprod)
 
 class StableDiffusion:
   def __init__(self):
