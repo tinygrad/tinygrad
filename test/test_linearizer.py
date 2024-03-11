9b3488b651c08d9992abff1a7a8a96c910e5832e
qazal <qazal.software@gmail.com>
2024-03-11 20:05:58 +0200

    multi store op linearizer

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -160,7 +160,7 @@ class TestLinearizer(unittest.TestCase):
                    ConstBuffer(42, dtypes.float, ShapeTracker(views=(View(shape=(), strides=(), offset=0, mask=None, contiguous=True),))))
       ast = LazyOp(BufferOps.STORE, (ast,),
                    MemBuffer(0, dtypes.float, ShapeTracker(views=(View(shape=(), strides=(), offset=0, mask=None, contiguous=True),))))
-      lin = Linearizer(ast=ast) # this is a dummy ast
+      lin = Linearizer(ast) # this is a dummy ast
 
       lin.uops = []
       return lin.uop(uop, dtype, vin, arg, cachable=False)
