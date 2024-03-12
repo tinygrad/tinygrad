9980c755d21eefad856bed648a1ead4c39edb337
George Hotz <geohot@gmail.com>
2024-03-12 09:18:51 -0700

    speedup

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -82,21 +82,22 @@ def _match(uop:UOp, pattern:Dict[str, Any], store:Dict[str, UOp]) -> bool:
   return True
 
 class PatternMatcher:
-  def __init__(self, patterns):
+  def __init__(self, patterns:List[Tuple[Dict[str, Any], Any]]):
     self.patterns = patterns
+    self.pdict = defaultdict(list)
+    # uop is required, arg is optional
+    for p,fxn in self.patterns: self.pdict[(p.get("uop"), p.get("arg", None))].append((p, fxn))
 
   def rewrite(self, uop:UOp) -> Optional[UOp]:
-    # TODO: write a universal data structure to not have to loop this
-    for p,fxn in self.patterns:
+    for p,fxn in self.pdict[(uop.uop, None)] + self.pdict[(uop.uop, uop.arg)]:
       store: Dict[str, UOp] = {}
-      if _match(uop, p, store):
-        return fxn(**store)
+      if _match(uop, p, store): return fxn(**store)
     return None
 
 constant_folder = PatternMatcher([
   # const rules
   ({"__name__": "root", "uop": UOps.GEP, "vin": ({"__name__": "c", "uop": UOps.CONST},)}, lambda root, c: UOp.const(root.dtype, c.arg)),
-  ({"__name__": "root", "uop": UOps.CAST, "vin": {"__name__": "c", "uop": UOps.CONST}}, lambda root,c: UOp.const(root.dtype, c.arg)),
+  ({"__name__": "root", "uop": UOps.CAST, "vin": {"__name__": "c", "uop": UOps.CONST}}, lambda root, c: UOp.const(root.dtype, c.arg)),
   # a phi without loops (len(vin)==2) is a noop
   ({"uop": UOps.PHI, "vin": ({}, {"__name__": "x"})}, lambda x: x),
   # x+-y -> x-y
