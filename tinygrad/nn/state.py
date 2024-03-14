8d194aa76b57fd32c71766b519e8a3dbc07d3177
George Hotz <geohot@gmail.com>
2024-03-14 10:38:05 -0700

    add Tensor.replace

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -68,7 +68,7 @@ def load_state_dict(model, state_dict:Dict[str, Tensor], strict=True, verbose=Tr
       if k not in state_dict and not strict:
         if DEBUG >= 1: print(f"WARNING: not loading {k}")
         continue
-      v.assign(state_dict[k].shard(mlb.device, mlb.axis) if isinstance((mlb:=v.lazydata), MultiLazyBuffer) else state_dict[k].to(v.device)).realize()
+      v.replace(state_dict[k].shard(mlb.device, mlb.axis) if isinstance((mlb:=v.lazydata), MultiLazyBuffer) else state_dict[k].to(v.device)).realize()
       if consume: del state_dict[k]
 
 # torch support!
