9e8643609b914fbab30a863812bea8e07aa39f41
qazal <qazal.software@gmail.com>
2024-03-14 10:50:03 +0000

    exclude HSA graph

--- test/helpers.py
+++ test/helpers.py
@@ -2,6 +2,7 @@ from tinygrad.device import JITRunner
 from tinygrad.nn.state import get_parameters
 from tinygrad import Tensor
 from tinygrad.helpers import Context
+from tinygrad.runtime.graph.hsa import HSAGraph
 
 def derandomize_model(model):
   with Context(GRAPH=0):
@@ -11,7 +12,7 @@ def derandomize_model(model):
 
 def assert_jit_cache_len(fxn, expected_len):
   assert len(fxn.jit_cache) > 0
-  if issubclass(type(fxn.jit_cache[0].prg), JITRunner):
+  if issubclass(type(fxn.jit_cache[0].prg), JITRunner) and not isinstance(fxn.jit_cache[0].prg, HSAGraph):
     assert len(fxn.jit_cache) == expected_len
   else:
     assert len(fxn.jit_cache) == 1
