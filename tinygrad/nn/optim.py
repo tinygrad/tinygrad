9df19c8941bab0f5e6ce4fbc1c56ac4ff02e41ef
David Hou <david.hou314@gmail.com>
2024-03-15 22:51:10 -0700

    Revert "remove t.grad to try"
    This reverts commit 05ec2d3b89b55b784020e3823aaf05451730c48c.

--- tinygrad/nn/optim.py
+++ tinygrad/nn/optim.py
@@ -36,7 +36,7 @@ class SGD(Optimizer):
       assert t.grad is not None
       # contiguous is needed since the grads can allegedly form a "diamond"
       # TODO: fix this in lazy.py
-      g = t.grad + self.wd * t.detach()
+      g = t.grad.contiguous() + self.wd * t.detach()
       if self.momentum:
         self.b[i].assign(self.momentum * self.b[i] + g)  # NOTE: self.b[i] is zero on the first run, no if required
         g = (g + self.momentum * self.b[i]) if self.nesterov else self.b[i]
