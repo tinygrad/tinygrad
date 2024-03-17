f206860003359bda909e5a7ead4dfe51687bc29e
George Hotz <geohot@gmail.com>
2024-03-17 16:32:24 -0700

    pickle unrealized tensor

--- test/test_pickle.py
+++ test/test_pickle.py
@@ -3,13 +3,19 @@ import numpy as np
 from tinygrad import Tensor
 
 class TestPickle(unittest.TestCase):
-  def test_pickle_tensor(self):
+  def test_pickle_realized_tensor(self):
     t = Tensor.rand(10, 10).realize()
     st = pickle.dumps(t)
     t2:Tensor = pickle.loads(st)
     np.testing.assert_equal(t.numpy(), t2.numpy())
 
-  #def test_pickle_jit(self): pass
+  def test_pickle_unrealized_tensor(self):
+    t = Tensor.ones(10, 10)
+    #import dill
+    #with dill.detect.trace(): dill.dumps(t)
+    st = pickle.dumps(t)
+    t2:Tensor = pickle.loads(st)
+    np.testing.assert_equal(t.numpy(), t2.numpy())
 
 if __name__ == '__main__':
   unittest.main()
