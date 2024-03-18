ac866eaf5a0c952bcd513a7b29fd640b89dd51df
chenyu <chenyu@fastmail.com>
2024-03-18 19:25:26 -0400

    disable simplify_phi_loops (#3812)
    * disble simplify_phi_loops
    
    this breaks BEAM search GPT2.
    
    * skip that

--- test/test_linearizer_failures.py
+++ test/test_linearizer_failures.py
@@ -190,7 +190,7 @@ class TestLinearizerFailures(unittest.TestCase):
   def test_failure_25(self):
     ast = LazyOp(op=BufferOps.STORE, src=(LazyOp(op=BinaryOps.ADD, src=(LazyOp(op=ReduceOps.SUM, src=(LazyOp(op=BufferOps.CONST, src=(), arg=ConstBuffer(val=1, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1025, 2047), strides=(0, 0), offset=0, mask=((0, 1025), (1023, 2047)), contiguous=False), View(shape=(1024, 1024), strides=(1, 2048), offset=0, mask=None, contiguous=False))))),), arg=(1,)), LazyOp(op=BufferOps.CONST, src=(), arg=ConstBuffer(val=-1, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1024, 1), strides=(0, 0), offset=0, mask=None, contiguous=False),))))), arg=None),), arg=MemBuffer(idx=0, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1024, 1), strides=(1, 0), offset=0, mask=None, contiguous=True),))))
     opts = [Opt(op=OptOps.GROUP, axis=0, amt=16), Opt(op=OptOps.UNROLL, axis=0, amt=4)]
-    helper_test_lin(Linearizer(ast), opts, failed_platforms=["METAL", "GPU", "HSA", "CUDA"])
+    helper_test_lin(Linearizer(ast), opts, failed_platforms=[])
 
 if __name__ == '__main__':
   unittest.main()
