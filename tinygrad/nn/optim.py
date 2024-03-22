f1fc2f564396c10b2a7dad59fcf6c3446605e92c
George Hotz <geohot@gmail.com>
2024-03-22 11:37:43 -0700

    remove terrible optimizer hack

--- tinygrad/nn/optim.py
+++ tinygrad/nn/optim.py
@@ -19,11 +19,7 @@ class Optimizer:
     for param in self.params: param.grad = None
 
   def realize(self, extra=None):
-    # NOTE: in extra is too late for most of the params due to issues with assign
-    #Tensor.corealize(extra + self.params + self.buffers if extra is not None else self.params + self.buffers)
-    # TODO: SUPER BROKEN THAT THIS FIXES IT
-    if extra is not None: Tensor.corealize(extra)
-    Tensor.corealize(self.params + self.buffers)
+    Tensor.corealize(extra + self.params + self.buffers if extra is not None else self.params + self.buffers)
 
   def step(self) -> None: raise NotImplementedError
 
