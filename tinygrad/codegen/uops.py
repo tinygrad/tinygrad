a22b37566208baca466a6e931b4cc6dc8c6f9803
jaredeh <jaredeh@gmail.com>
2024-03-21 17:27:49 -0700

    Add size to DEFINE_GLOBAL's in the UOps graph (#1)
    * Add size field to args of DEFINE_GLOBAL node in the UOps graph for Rust to size input/output arrays
    * Adapt dedup key to ignore size, apparently we can get redundant buffers in the graph.
    * Fixed test

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -152,7 +152,7 @@ class UOpGraph:
     if simplify and (rewritten:=constant_folder.rewrite(ret)) is not None:
       if rewritten in self.uops: return rewritten  # ignore cachable
       ret = rewritten
-    key = (ret.uop, ret.dtype, ret.vin, ret.arg)
+    key = (ret.uop, ret.dtype, ret.vin, ret.arg if ret.uop is not UOps.DEFINE_GLOBAL else (ret.arg[0],ret.arg[1],ret.arg[2]))
     if insert_before is None: insert_before = len(self.uops)
     # check if the cached expr is valid with the given insert place.
     if cachable and (expr:=self.saved_exprs.get(key, None)) is not None and self.uops.index(expr) <= insert_before: return expr
