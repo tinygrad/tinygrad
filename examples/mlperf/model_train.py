9a9cac58f96e9263979ebf8b6f45b8f594b6ab4a
wozeparrot <wozeparrot@gmail.com>
2024-03-24 11:43:12 -0400

    add lars to nn (#3750)
    * feat: add lars
    
    * feat: don't remove this comment
    
    * clean: smaller diff
    
    * clean: shorter line
    
    * feat: remove mlperf lars, switch resnet
    
    * fix: fully remove mlperf lars
    
    * clean: comment
    
    * feat: contiguous
    
    * feat: no weight decay on skip params
    
    * feat: optimizergroup
    
    * feat: classic momentum
    
    * fix: pylint
    
    * clean: move comment
    
    * fix: correct algo
    
    * feat: lrschedulergroup
    
    * feat: skip list tests
    
    * feat: :| forgot that params are a thing
    
    * feat: remove skip_list params from main params
    
    * feat: set moment
    
    ---------
    
    Co-authored-by: chenyu <chenyu@fastmail.com>

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -6,7 +6,9 @@ from tqdm import tqdm
 from tinygrad import Device, GlobalCounters, Tensor, TinyJit, dtypes
 from tinygrad.helpers import getenv, BEAM, WINO
 from tinygrad.nn.state import get_parameters, get_state_dict, safe_load, safe_save
+from tinygrad.nn.optim import LARS, SGD, OptimizerGroup
 
+from extra.lr_scheduler import LRSchedulerGroup
 from examples.mlperf.helpers import get_training_state, load_training_state
 
 def train_resnet():
@@ -60,20 +62,26 @@ def train_resnet():
   config["SYNCBN"]  = getenv("SYNCBN")
 
   # ** Optimizer **
-  from examples.mlperf.optimizers import LARS
-  skip_list = {v for k, v in get_state_dict(model).items() if "bn" in k or "bias" in k or "downsample.1" in k}
-  optimizer = LARS(parameters, base_lr, momentum=.9, weight_decay=decay, skip_list=skip_list)
+  skip_list = [v for k, v in get_state_dict(model).items() if "bn" in k or "bias" in k or "downsample.1" in k]
+  parameters = [x for x in parameters if x not in set(skip_list)]
+  optimizer = LARS(parameters, base_lr, momentum=.9, weight_decay=decay)
+  optimizer_skip = SGD(skip_list, base_lr, momentum=.9, weight_decay=0.0, classic=True)
+  optimizer_group = OptimizerGroup(optimizer, optimizer_skip)
 
   # ** LR scheduler **
   scheduler = PolynomialDecayWithWarmup(optimizer, initial_lr=base_lr, end_lr=1e-4,
                                         train_steps=epochs * steps_in_train_epoch,
                                         warmup=lr_warmup_epochs * steps_in_train_epoch)
+  scheduler_skip = PolynomialDecayWithWarmup(optimizer_skip, initial_lr=base_lr, end_lr=1e-4,
+                                             train_steps=epochs * steps_in_train_epoch,
+                                             warmup=lr_warmup_epochs * steps_in_train_epoch)
+  scheduler_group = LRSchedulerGroup(scheduler, scheduler_skip)
   print(f"training with batch size {BS} for {epochs} epochs")
 
   # ** resume from checkpointing **
   start_epoch = 0
   if ckpt:=getenv("RESUME", ""):
-    load_training_state(model, optimizer, scheduler, safe_load(ckpt))
+    load_training_state(model, optimizer_group, scheduler_group, safe_load(ckpt))
     start_epoch = int(scheduler.epoch_counter.numpy().item() / steps_in_train_epoch)
     print(f"resuming from {ckpt} at epoch {start_epoch}")
 
@@ -93,14 +101,14 @@ def train_resnet():
   def normalize(x): return x.permute([0, 3, 1, 2]) - input_mean
   @TinyJit
   def train_step(X, Y):
-    optimizer.zero_grad()
+    optimizer_group.zero_grad()
     X = normalize(X)
     out = model.forward(X)
     loss = out.sparse_categorical_crossentropy(Y, label_smoothing=0.1)
     top_1 = (out.argmax(-1) == Y).sum()
     loss.backward()
-    optimizer.step()
-    scheduler.step()
+    optimizer_group.step()
+    scheduler_group.step()
     return loss.realize(), top_1.realize()
   @TinyJit
   def eval_step(X, Y):
@@ -214,7 +222,7 @@ def train_resnet():
         else:
           fn = f"./ckpts/{time.strftime('%Y%m%d_%H%M%S')}_e{e}.safe"
         print(f"saving ckpt to {fn}")
-        safe_save(get_training_state(model, optimizer, scheduler), fn)
+        safe_save(get_training_state(model, optimizer_group, scheduler_group), fn)
 
 def train_retinanet():
   # TODO: Retinanet
