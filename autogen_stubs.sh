ccce76ce4db0304a0788085a006f39bd8fa0c838
George Hotz <geohot@gmail.com>
2024-03-26 19:05:48 +0000

    add amd_signal_t

--- autogen_stubs.sh
+++ autogen_stubs.sh
@@ -76,6 +76,7 @@ generate_hsa() {
   clang2py \
     /opt/rocm/include/hsa/hsa.h \
     /opt/rocm/include/hsa/hsa_ext_amd.h \
+    /opt/rocm/include/hsa/amd_hsa_signal.h \
     /opt/rocm/include/hsa/hsa_ext_finalize.h /opt/rocm/include/hsa/hsa_ext_image.h \
     --clang-args="-I/opt/rocm/include" \
     -o $BASE/hsa.py -l /opt/rocm/lib/libhsa-runtime64.so
