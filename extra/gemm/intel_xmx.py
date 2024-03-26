40e62f3b2d96f86c3248909e7d6b15980c431b69
George Hotz <geohot@gmail.com>
2024-03-26 05:28:37 +0000

    intel xmx

--- extra/gemm/intel_xmx.py
+++ extra/gemm/intel_xmx.py
@@ -15,10 +15,10 @@ device = Device["GPU"]
 
 # NOTE: only the subgroup type 8 ones work
 prog = CLProgram(device, "test", CLCompiler(device, "test").compile(f"""
-__attribute__((reqd_sub_group_size(8)))
+__attribute__((intel_reqd_sub_group_size(8)))
 __kernel void test(__global float* data0, const __global int* data1, const __global int8* data2) {{
   int lidx0 = get_local_id(0);
-  int a = data1[lidx0/2];
+  int a = data1[lidx0];
   int8 b = data2[lidx0];
   float out = intel_sub_group_f16_f16_matrix_mad_k16(a, b, 0.0f);
   data0[lidx0] = out;
@@ -30,45 +30,8 @@ a = Buffer("GPU", 8, dtypes.float32)
 b = Buffer("GPU", 0x10, dtypes.float16)
 c = Buffer("GPU", 8*0x10, dtypes.float16)
 
-row = np.array([0]*0x10, np.float16)
-mat = np.zeros((8, 0x10), np.float16)
-#row[0] = 1
-#row[1] = 22
-#row[2] = 3
-#row[3] = 3
-# 2-3 don't work
-#row[4] = 3
-#row[5] = 4
-#row[7] = 4
-# 6-7 don't work
-#row[8] = 3
-
-#row[3] = 4
-mat[0][0] = 2
-mat[0][1] = 3
-mat[0][2] = 4
-mat[0][4] = 9
-mat[0][4] = 9
-mat[0][5] = 8
-
-mat[1][0] = 7
-mat[1][1] = 9
-mat[1][2] = 11
-#mat[0][2] = 1
-#mat += 1
-#mat[0] += 1
-#mat[1] += 1
-#mat[0][0] = 2
-#mat[0][1] = 3
-#mat[1][0] = 8
-#mat[1][1] = 9
-#mat[0][2] = 6
-#mat[0][2] = 6
-#mat[1][0] = 4
-
 row = np.array([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8], np.float16)
 mat = np.random.random((8, 0x10)).astype(np.float16)
-mat[:, 8:] = 0
 
 b.copyin(row.data)
 c.copyin(mat.data)
@@ -78,8 +41,3 @@ out = np.frombuffer(a.as_buffer(), np.float32)
 real = row.astype(np.float32)@mat.T.astype(np.float32)
 print("out:", out)
 print("real", real)
-
-
-#b.copyin(np.zeros(0x10, np.float16).data)
-#c.copyin(np.zeros(0x10, np.float16).data)
-#print(a.as_buffer().cast('f').tolist())
