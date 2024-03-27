8be22726f861257258bf918a3e83019a8ae3bf47
George Hotz <geohot@gmail.com>
2024-03-27 11:10:59 -0700

    cq work

--- tinygrad/engine/commandqueue.py
+++ tinygrad/engine/commandqueue.py
@@ -2,12 +2,15 @@
 from __future__ import annotations
 from typing import List, Dict, TypeVar, Generic, Callable, Optional
 import functools
-from tinygrad.ops import ScheduleItem, LazyBuffer
+from tinygrad.helpers import getenv, all_same
+from tinygrad.ops import ScheduleItem, LoadOps
+from tinygrad.lazy import LazyBuffer
 from tinygrad.shape.symbolic import Variable
 from tinygrad.device import Buffer
-from tinygrad.tensor import Tensor
-from tinygrad.nn.state import get_parameters
+#from tinygrad.tensor import Tensor
+#from tinygrad.nn.state import get_parameters
 
+"""
 ReturnType = TypeVar('ReturnType')
 class TinyJit(Generic[ReturnType]):
   active_jit: Optional[TinyJit] = None
@@ -36,7 +39,6 @@ class TinyJit(Generic[ReturnType]):
       self.cnt += 1
       return
 
-    """
     # all inputs (except const) are realized
     input_tensors: Dict[Union[int, str], Union[LazyBuffer, MultiLazyBuffer]] = { cast(Union[int, str], k):v.realize().lazydata for k,v in itertools.chain(enumerate(args), sorted(kwargs.items())) if v.__class__ is Tensor }  # noqa: E501
     expected_name_sts_dtype_device = tuple([(k, v.st.unbind()[0] if isinstance(v, LazyBuffer) else ShapeTracker.from_shape(v.shape), v.dtype, v.device) for k,v in input_tensors.items()]) #noqa: E501
@@ -49,11 +51,34 @@ class TinyJit(Generic[ReturnType]):
     # get variables: they can either be in Tensors or passed in as arguments, and all must be bound. these are all global
     var_vals: Dict[Variable, int] = merge_dicts([arg.st.var_vals for arg in lbs] + [dict(x.unbind() for x in itertools.chain(args, kwargs.values()) if isinstance(x, Variable))])  # noqa: E501
     expected_vals = tuple(var_vals.keys())
-    """
+"""
 
 # this will interface with HWCommandQueue to replace Graph
+logops = open(getenv("LOGOPS", ""), "a") if getenv("LOGOPS", "") else None
 class CommandQueue:
   def __init__(self, schedule:List[ScheduleItem], outs:List[LazyBuffer]):
+    # loop through the schedule, find (real) inputs, add assign outputs, and split into different devices
+    schedule_per_device = {}
+    while len(schedule):
+      si = schedule.pop(0)
+      assert len(set(x.device for x in si.outputs+si.inputs)) == 1 or (si.ast[0].op in {LoadOps.COPY, LoadOps.WAIT} and len(si.outputs) == 1)
+      if (device:=si.outputs[0].device) not in schedule_per_device: schedule_per_device[device] = []
+      schedule_per_device[device].append(si)
+
+    # do memory planning and allocations across the devices
+    for k,v in schedule_per_device.items():
+      print("*****", k)
+      for si in v:
+        print(si)
+
+
+
+
+    #while len(schedule):
+    #  si = schedule.pop(0)
+    #  if logops and si.ast[0].op not in LoadOps and not any(i.device.startswith("DISK:") for i in si.inputs): logops.write(str(si.ast)+"\n")
+
+
     pass
     # loop through the ScheduleItem
     # NOTE: that list will be emptied
@@ -72,7 +97,7 @@ class CommandQueue:
 
   def __call__(self):
     # this should be callable if we discover a full lazy graph has the same hash
-    if TinyJit.active_jit is not None: TinyJit.active_jit.cqs.append(self)
+    #if TinyJit.active_jit is not None: TinyJit.active_jit.cqs.append(self)
 
     pass
 
