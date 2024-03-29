6b6461122e963e090f361ad828239809a5c7ddcc
chenyu <chenyu@fastmail.com>
2024-03-29 14:51:02 -0400

    test case Tensor.randn should be finite (#3994)
    * test case Tensor.randn should be finite
    
    there's a hack to fix float16, need a generic solution that works with bf16 and threefry
    
    * skip not supported
    
    * bfloat16 local is wrong
    
    * skip RHIP

--- test/test_randomness.py
+++ test/test_randomness.py
@@ -1,12 +1,15 @@
-import math
-import unittest
+import unittest, math
 from functools import partial
 
 import numpy as np
 import torch
-from tinygrad import nn, dtypes, Tensor
+from tinygrad import nn, dtypes, Tensor, Device
 from tinygrad.helpers import THREEFRY
 from test.helpers import is_dtype_supported
+from hypothesis import given, settings, strategies as strat
+
+settings.register_profile("my_profile", max_examples=200, deadline=None)
+settings.load_profile("my_profile")
 
 # https://gist.github.com/devries/11405101
 def ksprob(a):
@@ -99,6 +102,22 @@ class TestRandomness(unittest.TestCase):
   def test_randn(self):
     self.assertTrue(normal_test(Tensor.randn))
     self.assertTrue(equal_distribution(Tensor.randn, torch.randn, lambda x: np.random.randn(*x)))
+
+  @given(strat.sampled_from([dtypes.float, dtypes.float16]))#, dtypes.bfloat16]))  # TODO: add bfloat16
+  @unittest.skipIf(Device.DEFAULT=="RHIP", "broken in HIP CI")
+  def test_randn_finite(self, default_float):
+    if not is_dtype_supported(default_float): return
+    old_default_float = dtypes.default_float
+    # low precision can result in inf from randn
+    dtypes.default_float = default_float
+    t = Tensor.randn(1024, 1024)
+    mx = t.max().numpy().item()
+    mn = t.min().numpy().item()
+    if default_float == dtypes.float or (default_float == dtypes.float16 and not THREEFRY.value):
+      print(f"testing with {default_float=}")
+      assert math.isfinite(mx), mx
+      assert math.isfinite(mn), mn
+    dtypes.default_float = old_default_float
 
   def test_randint(self):
     self.assertFalse(normal_test(Tensor.randint))
