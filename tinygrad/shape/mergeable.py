e4d7536c812dc0e813d635f33ea6e00635d0e069
Paul Gustafson <paul.gustafson@theambrusgroup.com>
2024-04-02 21:32:06 -0700

    cleanup; add masks

--- tinygrad/shape/mergeable.py
+++ tinygrad/shape/mergeable.py
@@ -2,65 +2,49 @@
 # Alternates between incorrect and correct behavior when cache is enabled on M1 Mac...
 
 from tinygrad.shape.shapetracker import ShapeTracker
-from tinygrad.shape.view import View
-from tinygrad.shape.symbolic import Node, NumNode
+from tinygrad.shape.view import View, un1d
+from tinygrad.shape.symbolic import Node, NumNode, Variable
 from tinygrad import Tensor
+from typing import Tuple, Iterable
 
-
-def to_tensor(exp: Node, shape, vars = None):
-    if vars is None:
-        vars = list(exp.vars())
-
-    if len(vars) == 0:
-        return None
-
+def to_tensor(exp: Node, shape: Tuple[int], vars: Iterable[Variable] = None):
+    if vars is None: vars = exp.vars()
+    if len(vars) == 0: return None
+    vars = sorted(list(vars), key=str)
     indices = [list() for _ in vars]
-
-    for i, v in enumerate(vars):
-        if i == 0:
-            indices[0] = [[x] for x in list(range(v.min, v.max + 1))]
-            continue
-
+    indices[0] = [[x] for x in list(range(vars[0].min, vars[0].max + 1))]
+    for i, v in enumerate(vars[1:]):
         for j in range(v.min, v.max + 1):
-            indices[i] += [x + [j] for x in indices[i-1]]
-            
+            indices[i+1] += [x + [j] for x in indices[i]]
     indices[-1].sort()
-
     ans = []
     for i in range(len(indices[-1])):
         ans.append(exp.substitute(dict(zip(vars, map(NumNode, indices[-1][i])))).b)
+    return Tensor(ans).reshape(shape)
 
-    return Tensor(ans).reshape(shape).numpy()
-
-
-
-def is_single_view(s: ShapeTracker, lower_corner=None):
+def is_single_view(s: ShapeTracker, lower_corner: Tuple[int]=None):
     x = to_tensor(s.expr_idxs()[0], s.shape, s.expr_idxs()[0].vars())
-
-    lower_corner = None
-    N = len(x.shape)
-    if lower_corner is None:
-        lower_corner = tuple([0]*N)
-
+    m = to_tensor(s.expr_idxs()[1], s.shape, s.expr_idxs()[0].vars())
+    u = m
+    for i in range(N := len(s.shape)):
+        u = u.cumsum(axis=i)
+    lower_corner = tuple(un1d(m.shape, (u == 1).argmax().item()))
+    mask = tuple(zip(lower_corner, [c + 1 for c in un1d(m.shape, u.argmax().item())]))
     strides = []
-    for i in range(len(x.shape)):
-        strides.append(x[tuple([lower_corner[i] if j != i else lower_corner[i] + 1 for j in range(N)])] - x[lower_corner])
-
-    offset = int((Tensor(strides) * Tensor(list(lower_corner))).sum().numpy())
-    s2 = ShapeTracker(views=(View.create(shape=x.shape, strides=tuple(strides), offset=offset),))
-
-    try:
-        return (to_tensor(s.expr_idxs()[0], s.shape, s.expr_idxs()[0].vars()) == to_tensor(s2.expr_idxs()[0], s2.shape, s2.expr_idxs()[0].vars())).all()
-    except Exception as e:
-        #print(e)
-        return False
-
-def is_rectangular(x: Tensor):
-    return True  #TODO
+    for i in range(N):
+        strides.append((x[tuple([lower_corner[i] if j != i else lower_corner[i] + 1 for j in range(N)])] - x[lower_corner]).item())
+    s2 = ShapeTracker(views=(View(shape=x.shape, strides=tuple(strides), offset=s.views[-1].offset, mask=mask, contiguous=False),))   # TODO: check if offset is right
+    return (x == to_tensor(s2.expr_idxs()[0], s2.shape, s2.expr_idxs()[0].vars())).numpy().all()
 
 
 
-s = ShapeTracker.from_shape((2,4))
+s = ShapeTracker.from_shape((2,4)).permute((1,0)).reshape((2,4))
+s1 = ShapeTracker.from_shape((2,4))
+s2 = ShapeTracker(views=(View.create(shape=(3,3,3), strides=(9, 3,1), mask=((1,3), (1,3), (1,3)), offset=0),))
+s3 = ShapeTracker(views=(View.create(shape=(3,3,3), strides=(9, 3,1), mask=None, offset=99),))
+
 print(is_single_view(s))
-print(is_single_view(ShapeTracker.from_shape((2,4)).permute((1,0)).reshape((2,4))))
+print(is_single_view(s1))
+print(is_single_view(s2))
+print(is_single_view(s3))
 
