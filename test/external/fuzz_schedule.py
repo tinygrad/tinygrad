d3f542e1e5da7d98037a49072d2c77d34445850c
qazal <qazal.software@gmail.com>
2024-04-12 22:30:47 +0300

    simpler

--- test/external/fuzz_schedule.py
+++ test/external/fuzz_schedule.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 import numpy as np
-from typing import DefaultDict, List, Set, TypeVar, Dict
+from typing import DefaultDict, List, Set, TypeVar
 from tinygrad.buffer import Buffer
 from tinygrad.engine.realize import run_schedule
 from tinygrad.helpers import DEBUG, GlobalCounters, colored, getenv
@@ -11,38 +11,35 @@ from tinygrad.tensor import Tensor
 
 def fuzz_schedule(outs: List[LazyBuffer]):
   graph, in_degree, prescheduled = _graph_schedule(outs, seen:=set())
-  sorts = find_all_sorts(graph, in_degree)
-  if DEBUG >= 2: print(colored(f"fuzzing {len(sorts)} toposorts", "yellow"))
+  toposorts = find_all_toposorts(graph, in_degree)
+  if DEBUG >= 2: print(colored(f"fuzzing {len(toposorts)} toposorts", "yellow"))
 
-  schedules: List[List[ScheduleItem]] = [[] for _ in sorts]
+  schedules: List[List[ScheduleItem]] = [[] for _ in toposorts]
   fuzz_outputs: DefaultDict[LazyBuffer, List[Buffer]] = defaultdict(list)
-  for i, s in enumerate(sorts):
-    scheduled_bufs: Dict[LazyBuffer, Buffer] = {}
-    for key in s:
-      for buf in (ps:=prescheduled[key]).outputs: seen.add(buf)
-      for x in ps.outputs: scheduled_bufs[x] = Buffer(x.device, x.size, x.dtype) if i > 0 else x.buffer
-      inputs = tuple(x.buffer if hasattr(x.buffer, "_buf") else scheduled_bufs[x] for x in ps.inputs if x.size != 0)
-      schedules[i].append(ScheduleItem(ps.ast, tuple(scheduled_bufs[x] for x in ps.outputs if x.size != 0), inputs))
-      for out in ps.outputs: fuzz_outputs[out].append(scheduled_bufs[out])
+  for i, ts in enumerate(toposorts):
+    for key in ts:
+      for out in (ps:=prescheduled[key]).outputs:
+        seen.add(out)
+        # the first ts changes the LazyBuffer's .buffer
+        fuzz_outputs[out].append(out.buffer if i == 0 else Buffer(out.device, out.size, out.dtype))
+      inputs = (x.buffer if hasattr(x.buffer, "_buf") else fuzz_outputs[x][i] for x in ps.inputs if x.size != 0)
+      schedules[i].append(ScheduleItem(ps.ast, tuple(fuzz_outputs[x][i] for x in ps.outputs if x.size != 0), tuple(inputs)))
 
   # seed is the same between runs
   seed = Tensor._seed
   for i, schedule in enumerate(schedules):
     Tensor.manual_seed(seed)
     if DEBUG >= 2: print(f"toposort permutation {i}")
-    run_schedule(schedule.copy())
+    run_schedule(schedule)
     GlobalCounters.reset()
 
   all_passed = True
-  for lb, bufs in fuzz_outputs.items():
-    for i, buf in enumerate(bufs):
-      ground_truth, output = np.frombuffer(bufs[0].as_buffer(), bufs[0].dtype.np), np.frombuffer(buf.as_buffer(), buf.dtype.np)
+  for lb, rawbufs in fuzz_outputs.items():
+    for i, buf in enumerate(rawbufs):
+      ground_truth, output = (np.frombuffer(buf.as_buffer(), buf.dtype.np) for buf in [rawbufs[0], buf])
       try: np.testing.assert_allclose(output, ground_truth, atol=1e-2, rtol=1e2)
       except AssertionError as e:
-        print(f"COMPARE FAILED FOR REALIZE {lb}")
-        curr_si = [si for si in schedules[i] if buf in si.outputs][0]
-        baseline_si = [si for si in schedules[0] if bufs[0] in si.outputs][0]
-        print(f"ground truth realized at {schedules[0].index(baseline_si)}, this realize was at {schedules[i].index(curr_si)}")
+        print(f"COMPARE FAILED FOR REALIZE {lb} at permutation {i}")
         print(e)
         all_passed = False
 
@@ -50,7 +47,7 @@ def fuzz_schedule(outs: List[LazyBuffer]):
   if DEBUG >= 2: print(colored("all toposorts all_passed", "green"))
 
 T = TypeVar("T")
-def find_all_sorts(graph:DefaultDict[T, List[T]], in_degree:DefaultDict[T, int]) -> List[List[T]]:
+def find_all_toposorts(graph:DefaultDict[T, List[T]], in_degree:DefaultDict[T, int]) -> List[List[T]]:
   visited: Set[T] = set()
   ret: List[List[T]] = []
   path: List[T] = []
@@ -73,6 +70,4 @@ def find_all_sorts(graph:DefaultDict[T, List[T]], in_degree:DefaultDict[T, int])
   if len(ret) == 0: raise RuntimeError("detected cycle in the graph")
   # verify all paths are unique
   assert len(ret) == len(set(map(tuple, ret)))
-  # backtrack cleanup
-  for v in in_degree: in_degree[v] = 0
   return ret
