114fb1bbbbe5a0293d4148b440673dfa285a6312
c-nnr <cdtmc@icloud.com>
2024-04-15 14:47:43 +0100

    simplest way to drop in mnist() for fetch_mnist(tensors=True)

--- test/external/external_test_mnist_data_select.py
+++ test/external/external_test_mnist_data_select.py
@@ -1,8 +1,8 @@
 #!/bin/bash
 from tinygrad import Tensor
-from extra.datasets import fetch_mnist
+from tinygrad.features.datasets import mnist
 
 if __name__ == "__main__":
-  X_train, Y_train, X_test, Y_test = fetch_mnist(tensors=True)
+  X_train, Y_train, X_test, Y_test = mnist()
   samples = Tensor.randint(512, high=X_train.shape[0])
   select = X_train[samples].realize()
