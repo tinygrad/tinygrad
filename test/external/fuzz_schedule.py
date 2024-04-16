335b28652083569aa28245202b9f2812e0234f3c
qazal <qazal.software@gmail.com>
2024-04-16 14:07:07 +0300

    teeny cleanups

--- test/external/fuzz_schedule.py
+++ test/external/fuzz_schedule.py
@@ -15,6 +15,7 @@ def fuzz_schedule(outs: List[LazyBuffer]):
 
   # setup ground truth
   ground_truth: Dict[LazyBuffer, memoryview] = {}
+  # IMPORTANT: freeze prerealized bufs before ScheduleItem exec
   prerealized: Dict[LazyBuffer, memoryview] = {}
   seed = Tensor._seed
   for key in toposorts[0]:
@@ -25,13 +26,10 @@ def fuzz_schedule(outs: List[LazyBuffer]):
     for x in ps.inputs:
       if x not in ground_truth and x.device != "NPY": prerealized[x] = x.buffer.as_buffer()
     si = ScheduleItem(ps.ast, tuple(x.buffer for x in ps.outputs if x.size != 0), tuple(x.buffer for x in ps.inputs if x.size != 0))
-    ei = ExecItem(lower_schedule_item(si), list(si.outputs+si.inputs))
-    if len(capturing): capturing[0].add(ei)
-    if isinstance(ei.prg, CustomOp): Tensor._seed = seed
-    ei.run()
+    _exec_si(si, seed)
     for out in ps.outputs: ground_truth[out] = out.buffer.as_buffer()
 
-  # create new Buffers for each permutation
+  # exec and validate each permutation with new Buffers
   for i, ts in enumerate(toposorts[1:]):
     if DEBUG >= 1: print(colored(f"testing permutation {i}", "yellow"))
     rawbufs: Dict[LazyBuffer, Buffer] = {}
@@ -45,17 +43,19 @@ def fuzz_schedule(outs: List[LazyBuffer]):
           # copy the pre realized input
           else: rawbufs[x] = Buffer(x.buffer.device, x.buffer.size, x.buffer.dtype, initial_value=prerealized[x])
       si = ScheduleItem(ps.ast, tuple(rawbufs[x] for x in ps.outputs if x.size != 0), tuple(rawbufs[x] for x in ps.inputs if x.size != 0))
-      ei = ExecItem(lower_schedule_item(si), list(si.outputs+si.inputs))
-      if len(capturing): capturing[0].add(ei)
-      if isinstance(ei.prg, CustomOp): Tensor._seed = seed
-      ei.run()
+      _exec_si(si, seed)
       for out in ps.outputs:
-        gt = np.frombuffer(ground_truth[out], out.dtype.np)
         outbuf = np.frombuffer(rawbufs[out].as_buffer(), out.dtype.np)
-        try: np.testing.assert_allclose(gt, outbuf, atol=1e-2, rtol=1e-2)
+        try: np.testing.assert_allclose(outbuf, np.frombuffer(ground_truth[out], out.dtype.np), atol=1e-2, rtol=1e-2)
         except Exception as e:
           print(f"FAILED FOR {out}")
           raise e
+
+def _exec_si(si: ScheduleItem, seed:int):
+  ei = ExecItem(lower_schedule_item(si), list(si.outputs+si.inputs))
+  if len(capturing): capturing[0].add(ei)
+  if isinstance(ei.prg, CustomOp): Tensor._seed = seed
+  ei.run()
 
 T = TypeVar("T")
 def find_all_toposorts(graph:DefaultDict[T, List[T]], in_degree:DefaultDict[T, int]) -> List[List[T]]:
@@ -70,7 +70,7 @@ def find_all_toposorts(graph:DefaultDict[T, List[T]], in_degree:DefaultDict[T, i
       path.append(v)
       visited.add(v)
       recurse_paths(path)
-      if len(ret) >= getenv("FUZZ_SCHEDULE_MAX_PATHS", 5): return
+      if len(ret) >= getenv("FUZZ_SCHEDULE_MAX_PATHS", 10): return
       # backtrack
       for u in graph[v]: in_degree[u] += 1
       path.pop()
