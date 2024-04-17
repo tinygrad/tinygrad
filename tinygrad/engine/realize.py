49d92606ee65679c19c3f85d03e6370a2255c706
George Hotz <geohot@gmail.com>
2024-04-17 06:30:38 +0400

    typo + use central memory planner in openpilot

--- tinygrad/engine/realize.py
+++ tinygrad/engine/realize.py
@@ -43,7 +43,7 @@ def lower_schedule(schedule:List[ScheduleItem]) -> Generator[ExecItem, None, Non
 
 capturing: List = []  # put classes with an add method in here
 
-# NOTE: currently this behavior is the same as PlaceHolder, however, we can be more aggressive size with different size buffers
+# NOTE: currently this behavior is the same as PlaceHolder, however, we can be more aggressive here with different size buffers
 def central_memory_planner(schedule:List[ScheduleItem]) -> List[ScheduleItem]:
   # all unallocated unparented buffers are fair game to replace
   unallocated = [[x for x in (si.outputs+si.inputs) if not x.is_allocated() and x.lb_refcount == 0] for si in schedule]
@@ -64,7 +64,7 @@ def central_memory_planner(schedule:List[ScheduleItem]) -> List[ScheduleItem]:
         local_cache[key].append(assigned[buf])
 
   if DEBUG >= 1 and len(ak:=dedup(assigned.keys())) != len(av:=dedup(assigned.values())):
-    print(f"memory reduced from {sum([x.nbytes for x in ak])/1e9:.2f} GB to {sum([x.nbytes for x in av])/1e9:.2f} GB")
+    print(f"memory reduced from {sum([x.nbytes for x in ak])/1e6:.2f} MB to {sum([x.nbytes for x in av])/1e6:.2f} MB")
 
   # do the buffer replacements in the schedule
   return [ScheduleItem(si.ast, tuple(assigned.get(x, x) for x in si.outputs),
