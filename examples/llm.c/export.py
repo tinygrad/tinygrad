084541bfc60499009fc241618b7b0e4db0094ec6
George Hotz <geohot@gmail.com>
2024-04-18 12:48:40 +0400

    print nicely

--- examples/llm.c/export.py
+++ examples/llm.c/export.py
@@ -4,27 +4,32 @@ os.environ["NOOPT"] = "1"
 from tinygrad import Device, nn, Tensor, dtypes
 #Device.DEFAULT = "CLANG"
 from train_gpt2 import GPT, GPTConfig
-from tinygrad.helpers import dedup, to_function_name, flatten
+from tinygrad.helpers import dedup, to_function_name, flatten, getenv, GRAPH, GlobalCounters, ansilen
 from tinygrad.engine.schedule import create_schedule
 from tinygrad.engine.realize import memory_planner, run_schedule
 from tinygrad.ops import BufferOps, LoadOps
 from tinygrad.runtime.ops_clang import CLANG_PROGRAM_HEADER
 
 if __name__ == "__main__":
-  model = GPT(GPTConfig(n_layer=2, n_head=12, n_embd=768))
-  #model.load_pretrained()
+  model = GPT(GPTConfig(n_layer=getenv("NLAYER", 12), n_head=12, n_embd=768))
+  model.load_pretrained()
   seen = set()
   early_sched = create_schedule([x.lazydata for x in nn.state.get_parameters(model)], seen)
   print(f"built model {len(early_sched)}")
 
   optimizer = nn.optim.Adam(nn.state.get_parameters(model), lr=1e-4)
   for i in range(3):  # TODO: why does it take three and not two to stablize
+    if i == 2: GRAPH.value = getenv("LATEGRAPH")
+    GlobalCounters.reset()
     X = Tensor.empty(4, 64, dtype=dtypes.int)
     Y = Tensor.empty(4, 64, dtype=dtypes.int)
     _, loss = model(X, Y)
     optimizer.zero_grad()
-    loss.backward()
-    tensors = optimizer.schedule_step()
+    if getenv("BACKWARD", 1):
+      loss.backward()
+      tensors = optimizer.schedule_step()
+    else:
+      tensors = []
     sched = create_schedule([loss.lazydata] + [x.lazydata for x in tensors], seen)
     print(f"calls {i}:", len(sched))
     #run_schedule(sched[:])
@@ -38,13 +43,15 @@ if __name__ == "__main__":
     src = Device["CLANG"].compiler.render(to_function_name(k.name), k.uops).strip(CLANG_PROGRAM_HEADER)
     srcs[ast] = (k.name, src)
   print("functions:", len(srcs))
-  all_buffers = dedup(flatten([si.outputs+si.inputs for si in sched]))
-  numbered_bufs = {x:i for i,x in enumerate(all_buffers)}
+  used_buffers = dedup(flatten([si.outputs+si.inputs for si in sched]))
+  numbered_bufs = {x:i for i,x in enumerate(used_buffers)}
   print("buffers:", len(numbered_bufs))
 
   state_dict = nn.state.get_state_dict(model)
   state_dict.update({'X': X, 'Y': Y, 'loss': loss})
-  state_dict.update({'adam_b1': optimizer.b1, 'adam_b2': optimizer.b2, 'adam_t': optimizer.t})
+  for k,v in state_dict.items():
+    if v.lazydata.base.buffer not in used_buffers: print(f"UNUSED: {k}")
+  state_dict.update({'adam_b1': optimizer.b1, 'adam_b2': optimizer.b2, 'adam_t': optimizer.t, 'adam_lr': optimizer.lr})
   inverse_state_dict = {v:k for k,v in state_dict.items()}
   for p,m,v in zip(optimizer.params, optimizer.m, optimizer.v):
     nm = inverse_state_dict[p]
@@ -52,10 +59,12 @@ if __name__ == "__main__":
     state_dict["adam_v_"+nm] = v
   named_buffers = {v.lazydata.base.buffer:k.replace(".", "_") for k,v in state_dict.items()}
 
-  for si in sched:
+  for i,si in enumerate(sched):
     bufs = [named_buffers.get(b, f"b{numbered_bufs[b]}") for b in si.outputs+si.inputs]
     if si.ast[0].op is not BufferOps.STORE:
       print(f"// {si.ast[0].op}", bufs)
     else:
-      print(f"{srcs[si.ast][0]}({', '.join(bufs)})")
+      call = f"{srcs[si.ast][0]}({', '.join(bufs)})"
+      call += " "*(80-ansilen(call))
+      print(f"{call} // {i+1}")
       #print(srcs[si.ast][1])
