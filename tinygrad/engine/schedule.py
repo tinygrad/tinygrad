43422408983003a38e8a050da57d088d16914096
qazal <qazal.software@gmail.com>
2024-04-20 17:03:20 +0300

    track visits

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -215,19 +215,22 @@ def _graph_schedule(outs:List[LazyBuffer], seen:Set[LazyBuffer]) -> Tuple[Defaul
     if r in reduce_for_op or r.op in ReduceOps or r in group_for_output: continue
     r_parents = deque([r])
     realized_parents: Set[LazyBuffer] = set()
+    visited: Set[LazyBuffer] = set()
     while r_parents:
-      if (p:=r_parents.pop()).realized or (p.op in LoadOps and p.op is not LoadOps.ASSIGN): continue
+      if (p:=r_parents.pop()).realized or (p.op in LoadOps and p.op is not LoadOps.ASSIGN) or p in visited: continue
+      visited.add(p)
       if p in realizes and p is not r:
         if not p.forced_realize and p not in reduce_for_op and p.shape == r.shape: realized_parents.add(p)
         continue
       for next_p in p.srcs: r_parents.append(next_p.base)
-    if not realized_parents: continue
+    visited.clear()
 
     for rp in realized_parents:
       can_group = True
       rp_children = deque(children[rp])
       while rp_children and can_group:
-        if (c:=rp_children.pop()).realized or c.op is LoadOps.CONST: continue
+        if (c:=rp_children.pop()).realized or c.op is LoadOps.CONST or c in visited: continue
+        visited.add(c)
         if c in realizes and c is not r:
           can_group = False
           break
