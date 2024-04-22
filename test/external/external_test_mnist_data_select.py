cf9900f19d8330d9bd0141dc8455cd4bd511ba2e
Alex Litzenberger <alex@agot.ai>
2024-04-22 13:54:47 -0400

    Remove conflicts

--- test/external/external_test_mnist_data_select.py
+++ test/external/external_test_mnist_data_select.py
@@ -4,8 +4,5 @@ from extra.datasets import fetch_mnist
 
 if __name__ == "__main__":
   X_train, Y_train, X_test, Y_test = fetch_mnist(tensors=True)
-  # samples = Tensor.randint(512, high=X_train.shape[0])
-  samples = Tensor.arange(10)
-  select = X_train[samples].realize();
-  # print(select.shape)
-  print(select[:10, 0, 10, 10].numpy())
+  samples = Tensor.randint(512, high=X_train.shape[0])
+  select = X_train[samples].realize()
