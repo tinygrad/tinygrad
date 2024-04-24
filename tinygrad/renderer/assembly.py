6548d00284f1505b6aa75a4dccafc39287828608
szymonozog <szymon.ozog@gmail.com>
2024-04-24 05:52:02 +0200

    Remove float4 pred and alt

--- tinygrad/renderer/assembly.py
+++ tinygrad/renderer/assembly.py
@@ -213,10 +213,7 @@ def uops_to_asm(lang:AssemblyLanguage, function_name:str, uops:UOpGraph) -> str:
         assert vin[1].dtype is not None
         if dtype.count > 1:
           r[u] = [ssa('val', dtype=lang.types[dtype.scalar()]) for _ in range(dtype.count)]
-          if(len(vin)>3):
-            for v in r[u]: kk(f"mov.{lang.mem_type(dtype.scalar())} {v}, {render_val(0, dtype.scalar())};")
-          kk((f"@{r[vin[2]]}"if len(vin) > 3 else "")
-            + f" ld{u.arg}.v{dtype.count}.{lang.mem_type(dtype.scalar())} {{{', '.join(r[u])}}}, [{r[vin[0]]}+{vin[1].arg}];")
+          kk(f"ld{u.arg}.v{dtype.count}.{lang.mem_type(dtype.scalar())} {{{', '.join(r[u])}}}, [{r[vin[0]]}+{vin[1].arg}];")
         else:
           kk(*lang.render_load(r[vin[0]], ssa('val', u), dtype, gate=r[vin[2]] if len(vin) > 3 else None,
                               alt=r[vin[3]] if len(vin) > 3 else None, ss=u.arg, offset=vin[1].arg))
