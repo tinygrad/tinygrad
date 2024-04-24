f155bece2c4480f0e57f24f3eba09107eb7992c4
Francis Lata <francisalbertlata@gmail.com>
2024-04-23 23:37:11 -0400

    update unet3d model_eval script

--- examples/mlperf/model_eval.py
+++ examples/mlperf/model_eval.py
@@ -63,13 +63,13 @@ def eval_resnet():
 def eval_unet3d():
   # UNet3D
   from extra.models.unet3d import UNet3D
-  from extra.datasets.kits19 import iterate, sliding_window_inference
+  from extra.datasets.kits19 import iterate, sliding_window_inference, get_val_files
   from examples.mlperf.metrics import dice_score
   mdl = UNet3D()
   mdl.load_from_pretrained()
   s = 0
   st = time.perf_counter()
-  for i, (image, label) in enumerate(iterate(), start=1):
+  for i, (image, label) in enumerate(iterate(get_val_files()), start=1):
     mt = time.perf_counter()
     pred, label = sliding_window_inference(mdl, image, label)
     et = time.perf_counter()
