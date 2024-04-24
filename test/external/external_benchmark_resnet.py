3577b324965f4fcaca54723b258bb24f981566b2
David Hou <david.hou314@gmail.com>
2024-04-23 20:04:32 -0700

    E501

--- test/external/external_benchmark_resnet.py
+++ test/external/external_benchmark_resnet.py
@@ -95,7 +95,8 @@ class BenchmarkResnetTrain(unittest.TestCase):
       if kernels is None: kernels = GlobalCounters.kernel_count // JITCNT
       tm = (et-st) / JITCNT
       if best_tm is None or tm < best_tm: best_tm = tm
-    print(f"\r{name:42s}: {best_tm * 1000:>9.2f} ms, {flops / 10**12 / best_tm:>7.2f} tflops, {mem_used / 10**9: 7.2f} GB used, {kernels:>6d} kernels")
+    print(f"\r{name:42s}: {best_tm * 1000:>9.2f} ms, {flops / 10**12 / best_tm:>7.2f} tflops,"
+          f"{mem_used / 10**9: 7.2f} GB used, {kernels:>6d} kernels")
     return best_tm, flops, mem, kernels
 
   def test_layer1_1(self): self._est(*self._test_layer(*self._get_layer(0, 0)), 1)
