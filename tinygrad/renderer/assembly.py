fc5341369b9e0f9b8d873cbe6b319176066de355
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-04-26 16:18:27 +0200

    Update assembly.py

--- tinygrad/renderer/assembly.py
+++ tinygrad/renderer/assembly.py
@@ -107,9 +107,7 @@ def uops_to_asm(lang:AssemblyLanguage, function_name:str, _uops:UOpGraph) -> str
   for pointer_op in list(filter(lambda uop: uop.uop in [UOps.LOAD, UOps.STORE], uops.uops)): ptr_ar(pointer_op, uops)
   uops.remove_childless(set(x for x in uops if x.uop in {UOps.DEFINE_GLOBAL, UOps.PHI, UOps.ENDIF, UOps.ENDLOOP, UOps.STORE}))
   uops.optimize_loops()
-  uops.print()
   optimize_gated_loads(uops)
-  uops.print()
 
   def kk(*s: str): kernel.append("\n".join(s))
 
