28b933d4eb54c9a3fb4c39f584122f501c791d27
qazal <qazal.software@gmail.com>
2024-04-28 17:48:38 +0300

    remu dev

--- tinygrad/runtime/ops_rhip.py
+++ tinygrad/runtime/ops_rhip.py
@@ -1,8 +1,17 @@
 import ctypes
-from tinygrad.device import Compiled, MallocAllocator
-from tinygrad.runtime.ops_hsa import HSACompiler
+from tinygrad.device import Compiled, Compiler, CompilerOptions, MallocAllocator
+from tinygrad.renderer.cstyle import HIPRenderer
+from tinygrad.runtime.driver.hip_comgr import compile_hip
 
-rhip = ctypes.CDLL("/usr/local/lib/libremu.so")
+class RHIPCompiler(Compiler):
+  compiler_opts = CompilerOptions("HSA", has_tensor_cores=True, shared_max=65536)
+  def __init__(self, arch:str):
+    self.arch = arch
+    super().__init__(f"compile_hip_{self.arch}")
+  def render(self, name:str, uops) -> str: return HIPRenderer(name, uops)
+  def compile(self, src:str) -> bytes: return compile_hip(src, self.arch)
+
+rhip = ctypes.CDLL("/Users/qazal/code/tinygrad/remu/target/release/libremu.dylib")
 class RHIPProgram:
   def __init__(self, name:str, lib:bytes):
     self.name, self.lib = name, lib
@@ -14,4 +23,4 @@ class RHIPProgram:
 class RHIPDevice(Compiled):
   def __init__(self, device:str=""):
     self.device = int(device.split(":")[1]) if ":" in device else 0
-    super().__init__(device, MallocAllocator, HSACompiler("gfx1100"), RHIPProgram)
+    super().__init__(device, MallocAllocator, RHIPCompiler("gfx1100"), RHIPProgram)
