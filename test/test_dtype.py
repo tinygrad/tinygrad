fix mean underflow for half tensor (#4377)
* fix mean underflow for half tensor

divide only the reduce factor. added unit test and non-nan assertion in resnet training. also added a failed test cast for symbolic shape var

* skip for python backend

test/test_dtype.py
@@ -621,9 +621,17 @@ class TestAutoCastType(unittest.TestCase):
     t.reshape(2, 1).expand(2, 10001).max().backward()
     np.testing.assert_allclose(t.grad.numpy(), [1, 0])
 
+  @unittest.skipIf(Device.DEFAULT=="PYTHON", "very slow")
+  @unittest.skipUnless(is_dtype_supported(dtypes.half), "need half")
+  def test_mean_half_precision_underflow(self):
+    N = 10000
+    x = 0.001
+    t = Tensor([[x]], dtype=dtypes.half, requires_grad=True).expand(N, N).contiguous()
+    np.testing.assert_allclose(t.mean(axis=1).numpy(), np.array([x] * N, dtype=np.float16), rtol=1e-3)
+
   @unittest.skip("TODO: fix this")
   @unittest.skipUnless(is_dtype_supported(dtypes.half), "need half")
-  def test_mean_half_precision(self):
+  def test_mean_half_precision_overflow(self):
     t = Tensor([60000, 60000, 60000], dtype=dtypes.half, requires_grad=True)
     np.testing.assert_allclose(t.mean().numpy(), 60000)
     t.square().mean().backward()