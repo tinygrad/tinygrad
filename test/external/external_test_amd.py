3e5550cbb7b0e0fc263b502f180c464bd347ae6e
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-05-06 13:52:46 +0000

    no need to round there

--- test/external/external_test_amd.py
+++ test/external/external_test_amd.py
@@ -1,6 +1,5 @@
 import unittest
 from tinygrad import Device, Tensor
-from tinygrad.helpers import round_up
 from tinygrad.engine.schedule import create_schedule
 from tinygrad.runtime.ops_amd import AMDDevice
 
@@ -15,7 +14,7 @@ class TestAMD(unittest.TestCase):
     TestAMD.b.lazydata.buffer.allocate()
 
   def test_amd_ring_64bit_doorbell(self):
-    TestAMD.d0.pm4_write_pointer[0] = TestAMD.d0.pm4_write_pointer[0] + round_up((2 << 32) - TestAMD.d0.pm4_ring.size // 4, TestAMD.d0.pm4_ring.size // 4)
+    TestAMD.d0.pm4_write_pointer[0] = TestAMD.d0.pm4_write_pointer[0] + (2 << 32) - TestAMD.d0.pm4_ring.size // 4
     for _ in range(2000):
       TestAMD.d0_runner.clprg(TestAMD.b.lazydata.buffer._buf, TestAMD.a.lazydata.buffer._buf,
                               global_size=TestAMD.d0_runner.global_size, local_size=TestAMD.d0_runner.local_size)
