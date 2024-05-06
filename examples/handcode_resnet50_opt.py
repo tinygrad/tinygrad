f4e49a7c1a9cb3ac1ab54ef5a8c6767e3885a494
George Hotz <72895+geohot@users.noreply.github.com>
2024-05-06 08:01:26 -0700

    resnet 50 opt: correct loop + LARS (#4449)
    * correct loop + LARS
    
    * ops

--- examples/handcode_resnet50_opt.py
+++ examples/handcode_resnet50_opt.py
@@ -9,6 +9,7 @@ from tinygrad.helpers import ansilen, DEBUG, getenv
 from tinygrad.shape.symbolic import sym_infer
 from tinygrad.dtype import dtypes
 from tinygrad.engine.schedule import create_schedule
+from tinygrad.features.graph import print_tree
 
 if __name__ == "__main__":
   if getenv("HALF"):
@@ -19,21 +20,19 @@ if __name__ == "__main__":
 
   # the device we are optimizing for
   device: Compiled = Device[Device.DEFAULT]
+  if getenv("BACKWARD"): optim = (nn.optim.LARS if getenv("LARS") else nn.optim.SGD)(nn.state.get_parameters(mdl))
   print(f"optimizing for {Device.DEFAULT}")
 
-  # first model run to init the weights, they are saved in seen
-  create_schedule([mdl(Tensor.empty(64, 3, 224, 224)).lazydata], seen)
-
-  # run model again to get only what changes, these are the kernels of the model
-  x = Tensor.empty(64, 3, 224, 224)
-  if getenv("BACKWARD"): optim = nn.optim.SGD(nn.state.get_parameters(mdl))
-  out = mdl(x)
-  targets = [out.lazydata]
-  if getenv("BACKWARD"):
-    optim.zero_grad()
-    out.mean().backward()
-    targets += [x.lazydata for x in optim.schedule_step()]
-  sched = create_schedule(targets, seen)
+  # run model twice to get only what changes, these are the kernels of the model
+  for i in range(2):
+    out = mdl(Tensor.empty(64, 3, 224, 224))
+    targets = [out.lazydata]
+    if getenv("BACKWARD"):
+      optim.zero_grad()
+      out.sparse_categorical_crossentropy(Tensor.empty(64, dtype=dtypes.int)).backward()
+      targets += [x.lazydata for x in optim.schedule_step()]
+    sched = create_schedule(targets, seen)
+    print(f"schedule length {len(sched)}")
   sched = [x for x in sched if x.ast[0].op not in LoadOps]
 
   # focus on one kernel
@@ -43,6 +42,11 @@ if __name__ == "__main__":
   total_tm = 0
   running_gflops = 0
   for i,si in enumerate(sched):
+    ops = sum(get_lazyop_info(ast).flops for ast in si.ast)
+
+    if DEBUG >= 2:
+      for ast in si.ast: print_tree(ast)
+
     rawbufs = bufs_from_lin(Linearizer(*si.ast))
 
     # "linearize" the op into uops in different ways
@@ -68,7 +72,7 @@ if __name__ == "__main__":
     choices = []
     for lin in lins:
       tm = time_linearizer(lin, rawbufs, allow_test_size=False, cnt=10)
-      gflops = sym_infer(get_lazyop_info(lin.ast[0]).flops, {k:k.min for k in lin.ast[0].vars()})*1e-9/tm
+      gflops = sym_infer(ops, {k:k.min for k in lin.ast[0].vars()})*1e-9/tm
       choices.append((tm, gflops, lin.linearize()))
 
       # print all kernels
