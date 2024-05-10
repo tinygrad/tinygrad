bbb4d4966636f6f9311c9da7aa6c680af0db43fd
George Hotz <geohot@gmail.com>
2024-05-09 18:14:09 -0700

    minor compiler cleanup

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import multiprocessing
 from collections import defaultdict
 from dataclasses import dataclass, replace
-from typing import TYPE_CHECKING, Any, List, Optional, Dict, Tuple, ClassVar, cast
+from typing import TYPE_CHECKING, Any, List, Optional, Dict, Tuple, ClassVar
 import importlib, inspect, functools, pathlib, time, ctypes, os
 from tinygrad.helpers import prod, getenv, colored, all_int, to_function_name, from_mv, flat_mv, diskcache_get, diskcache_put, DEBUG, BEAM, NOOPT
 from tinygrad.shape.symbolic import Variable, sym_infer, sint
@@ -180,10 +180,6 @@ class Program:
   @functools.cached_property
   def function_name(self) -> str: return to_function_name(self.name)
 
-  def compile(self, cached=True) -> bytes:
-    compiler = cast(Compiler, Device[self.dname].compiler)
-    return compiler.compile_cached(self.src) if cached else compiler.compile(self.src)
-
   def launch_dims(self, var_vals:Dict[Variable, int]):
     global_size = [sym_infer(sz, var_vals) for sz in self.global_size] if self.global_size is not None else None
     local_size = [sym_infer(sz, var_vals) for sz in self.local_size] if self.local_size is not None else None
@@ -193,7 +189,7 @@ class CompiledRunner(Runner):
   def __init__(self, p:Program, precompiled:Optional[bytes]=None):
     if DEBUG >= 4: print(p.src)
     self.p:Program = p
-    self.lib:bytes = precompiled if precompiled is not None else self.p.compile()
+    self.lib:bytes = precompiled if precompiled is not None else Device[p.dname].compiler.compile_cached(p.src)
     self.clprg = Device[p.dname].runtime(p.function_name, self.lib)
     super().__init__(p.name, p.dname, p.op_estimate, p.mem_estimate)
 
@@ -219,15 +215,12 @@ method_cache: Dict[Tuple[str, Tuple[LazyOp, ...], int, bool], CompiledRunner] = 
 logkerns, logkerns_level = open(getenv("LOGKERNS", ""), "a") if getenv("LOGKERNS", "") else None, getenv("LOGKERNS_LEVEL", 1)
 class Compiled:
   def __init__(self, device:str, allocator:Allocator, compiler:Optional[Compiler], runtime, graph=None):
-    self.dname, self.allocator, self.compiler, self.runtime, self.graph = device, allocator, compiler, runtime, graph
+    self.dname, self.allocator, self.compiler, self.runtime, self.graph = device, allocator, compiler if compiler else Compiler(), runtime, graph
   def synchronize(self): pass  # override this in your device
 
-  def to_runner(self, k:Linearizer) -> CompiledRunner:
-    assert self.compiler is not None, "compiler is required to run AST"
-    return CompiledRunner(self.compiler.to_program(k, override_device=self.dname))
+  def to_runner(self, k:Linearizer) -> CompiledRunner: return CompiledRunner(self.compiler.to_program(k, override_device=self.dname))
 
   def get_linearizer(self, *ast:LazyOp) -> Linearizer:
-    assert self.compiler is not None, "compiler is required to build AST"
     if DEBUG >= 3:
       from tinygrad.features.graph import print_tree
       for op in ast: print_tree(op)
