1217c0cefbbae34a2f4a35be3b6d7828c673ca01
George Hotz <geohot@gmail.com>
2024-05-11 16:55:40 -0700

    precise exp2

--- tinygrad/runtime/driver/tinymath.h
+++ tinygrad/runtime/driver/tinymath.h
@@ -1,5 +1,34 @@
 // https://github.com/JuliaMath/openlibm/blob/master/src/e_log2f.c
 
+#define NAN (0.0f/0.0f)
+#define INFINITY (1.0f/0.0f)
+#define sqrt __builtin_sqrtf
+#define uint32_t unsigned int
+#define int32_t int
+
+typedef union
+{
+  double value;
+  struct
+  {
+    unsigned int msw;
+    unsigned int lsw;
+  } parts;
+  struct
+  {
+    unsigned long w;
+  } xparts;
+} ieee_double_shape_type;
+
+#define INSERT_WORDS(d,ix0,ix1)         \
+do {                \
+  ieee_double_shape_type iw_u;          \
+  iw_u.parts.msw = (ix0);         \
+  iw_u.parts.lsw = (ix1);         \
+  (d) = iw_u.value;           \
+} while (0)
+
+#define STRICT_ASSIGN(type, lval, rval) ((lval) = (rval))
 #define GET_FLOAT_WORD(i,f) i = *((int*)&(f))
 #define SET_FLOAT_WORD(f,i) { int i2 = i; f = *((float*)&(i2)); }
 
@@ -67,15 +96,84 @@ inline float log2(float x) {
 }
 
 // ***** exp2 *****
-// https://stackoverflow.com/questions/65554112/fast-double-exp2-function-in-c
+// https://android.googlesource.com/platform/bionic/+/a27d2baa/libm/src/s_exp2f.c
 
-inline float exp2(float p) {
-  if(p<-126.f) p= -126.f;
-  int w=(int)p;
-  float z=p-(float)w;
-  if(p<0.f) z+= 1.f;
-  union {unsigned int i; float f;} v={(unsigned int)((1<<23)*(p+121.2740575f+27.7280233f/(4.84252568f -z)-1.49012907f * z)) };
-  return v.f;
+#define	TBLBITS	4
+#define	TBLSIZE	(1 << TBLBITS)
+
+const float
+    huge    = 0x1p100f,
+    twom100 = 0x1p-100f,
+    redux   = 0x1.8p23f / TBLSIZE,
+    P1	    = 0x1.62e430p-1f,
+    P2	    = 0x1.ebfbe0p-3f,
+    P3	    = 0x1.c6b348p-5f,
+    P4	    = 0x1.3b2c9cp-7f;
+const double exp2ft[TBLSIZE] = {
+	0x1.6a09e667f3bcdp-1,
+	0x1.7a11473eb0187p-1,
+	0x1.8ace5422aa0dbp-1,
+	0x1.9c49182a3f090p-1,
+	0x1.ae89f995ad3adp-1,
+	0x1.c199bdd85529cp-1,
+	0x1.d5818dcfba487p-1,
+	0x1.ea4afa2a490dap-1,
+	0x1.0000000000000p+0,
+	0x1.0b5586cf9890fp+0,
+	0x1.172b83c7d517bp+0,
+	0x1.2387a6e756238p+0,
+	0x1.306fe0a31b715p+0,
+	0x1.3dea64c123422p+0,
+	0x1.4bfdad5362a27p+0,
+	0x1.5ab07dd485429p+0,
+};
+
+inline float exp2(float x) {
+  double tv;
+	float r, z;
+	volatile float t;	/* prevent gcc from using too much precision */
+	uint32_t hx, hr, ix, i0;
+	int32_t k;
+	/* Filter out exceptional cases. */
+	GET_FLOAT_WORD(hx,x);
+	ix = hx & 0x7fffffff;		/* high word of |x| */
+	if(ix >= 0x43000000) {			/* |x| >= 128 */
+		if(ix >= 0x7f800000) {
+			if ((ix & 0x7fffff) != 0 || (hx & 0x80000000) == 0)
+				return (x); 	/* x is NaN or +Inf */
+			else
+				return (0.0);	/* x is -Inf */
+		}
+		if(x >= 0x1.0p7f)
+			return (huge * huge);	/* overflow */
+		if(x <= -0x1.2cp7f)
+			return (twom100 * twom100); /* underflow */
+	} else if (ix <= 0x33000000) {		/* |x| <= 0x1p-25 */
+		return (1.0f + x);
+	}
+	/* Reduce x, computing z, i0, and k. */
+	t = x + redux;
+	GET_FLOAT_WORD(i0, t);
+	i0 += TBLSIZE / 2;
+	k = (i0 >> TBLBITS) << 23;
+	i0 &= TBLSIZE - 1;
+	t -= redux;
+	z = x - t;
+	/* Compute r = exp2(y) = exp2ft[i0] * p(z). */
+	tv = exp2ft[i0];
+	r = tv + tv * (z * (P1 + z * (P2 + z * (P3 + z * P4))));
+	/* Scale by 2**(k>>23). */
+	if(k >= -(125 << 23)) {
+		if (k != 0) {
+			GET_FLOAT_WORD(hr, r);
+			SET_FLOAT_WORD(r, hr + k);
+		}
+		return (r);
+	} else {
+		GET_FLOAT_WORD(hr, r);
+		SET_FLOAT_WORD(r, hr + (k + (100 << 23)));
+		return (r * twom100);
+	}
 }
 
 // ***** sin *****
@@ -97,7 +195,3 @@ inline float sin(float x) {
   s = z*x;
   return (x + s*(S1+z*S2)) + s*w*r;
 }
-
-#define NAN (0.0f/0.0f)
-#define INFINITY (1.0f/0.0f)
-#define sqrt __builtin_sqrtf
