203eb820c4b4c1b96d6307198e66e08950eb00d7
Tzepesh <vlad.striletchi@airportlabs.com>
2024-05-12 13:39:20 +0300

    finetune terms

--- tinygrad/function.py
+++ tinygrad/function.py
@@ -51,7 +51,7 @@ class Sin(Function):
   # return res
 
   def taylor_sin(self, x:LazyBuffer) -> LazyBuffer:
-    terms = 12
+    terms = 14
     res = x.const(0)
     sgn = x.const(1)
     x_pow = x
