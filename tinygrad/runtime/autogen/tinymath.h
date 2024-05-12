680b897b43c05200756bab6c94f2fd8c796af0cf
uuuvn <uuuvn@proton.me>
2024-05-12 17:11:44 +0300

    beginnings of single-header tinymath.h

--- tinygrad/runtime/autogen/tinymath.h
+++ tinygrad/runtime/autogen/tinymath.h
@@ -1,17 +1,38 @@
+// tinymath - a tiny single-header math library for tinygrad
+// a bunch of stuff here is taken from musl
 #include <stdint.h> // only used for int8_t instead of char which is buggy for some reason
+
+#define NAN __builtin_nanf("")
+#define INFINITY __builtin_inff()
 
 double sin(double);
 float sinf(float);
-double sqrt(double);
-float sqrtf(float);
+
+double sqrt(double x) {
+#if defined(__aarch64__) || defined(_M_ARM64)
+  __asm__("fsqrt %d0, %d1" : "=w"(x) : "w"(x));
+#elif defined(__x86_64__) || defined(_M_X64)
+  __asm__("sqrtsd %1, %0" : "=x"(x) : "x"(x));
+#else
+#error only aarch64 and x86 is supported
+#endif
+  return x;
+}
+
+float sqrtf(float x) {
+#if defined(__aarch64__) || defined(_M_ARM64)
+  __asm__("fsqrt %s0, %s1" : "=w"(x) : "w"(x));
+#elif defined(__x86_64__) || defined(_M_X64)
+  __asm__("sqrtss %1, %0" : "=x"(x) : "x"(x));
+#else
+#error only aarch64 and x86 is supported
+#endif
+  return x;
+}
 double exp2(double);
 float exp2f(float);
 double log2(double);
 float log2f(float);
-
-// taken from musl's headers
-#define NAN __builtin_nanf("")
-#define INFINITY __builtin_inff()
 
 #define __IS_FP(x) (sizeof((x) + 1ULL) == sizeof((x) + 1.0f))
 
@@ -30,10 +51,10 @@ float log2f(float);
 #define __tg_real(fun, x) (__RETCAST(x) __tg_real_nocast(fun, x))
 
 #undef sin
-#define sin(x) __tg_real(sin, (x))
 #undef sqrt
-#define sqrt(x) __tg_real(sqrt, (x))
 #undef exp2
-#define exp2(x) __tg_real(exp2, (x))
 #undef log2
+#define sin(x) __tg_real(sin, (x))
+#define sqrt(x) __tg_real(sqrt, (x))
+#define exp2(x) __tg_real(exp2, (x))
 #define log2(x) __tg_real(log2, (x))
--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -10,13 +10,9 @@ class ClangCompiler(Compiler):
   def compile(self, src:str) -> bytes:
     platspec = ('-ffixed-x18',) if platform.machine() == "arm64" else ('-Xclang=-fnative-half-type', '-Xclang=-fnative-half-arguments-and-returns')
     return subprocess.check_output(('clang', '-x', 'c', '-c', '-target', f'{platform.machine()}-none-unknown-elf', '-march=native', '-fPIC', '-O2',
-                                    '-fno-builtin', '-ffreestanding', '-Wall', '-Werror', '-include',
+                                    '-fno-builtin', '-ffreestanding', '-nostdlib', '-Wall', '-Werror', '-include',
                                     f'{os.path.dirname(__file__)}/autogen/tinymath.h', '-', '-o', '-')+platspec,
                                     input=src.encode('utf-8'))
-
-with open(f'{os.path.dirname(__file__)}/autogen/tinymath.c') as fd:
-  syms = loader.load_elf(ClangCompiler().compile(fd.read()))
-  loader.link({k:addrof(v) for k,v in syms.items()})
 
 class ClangProgram:
   def __init__(self, name:str, lib:bytes):
