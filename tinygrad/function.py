6ba01dde4d06297084257051f54b515c50a1b1b2
Tzepesh <vlad.striletchi@airportlabs.com>
2024-05-12 14:46:54 +0300

    increase terms

--- tinygrad/function.py
+++ tinygrad/function.py
@@ -51,7 +51,7 @@ class Sin(Function):
   # return res
 
   def taylor_sin(self, x:LazyBuffer) -> LazyBuffer:
-    no_terms = 7
+    no_terms = 10
     res = x.const(0)
     x_pow = x
     fact = x.const(1)
