50cced24722e9d1c5b7493d77d09505805334662
George Hotz <geohot@gmail.com>
2024-05-12 14:36:28 -0700

    it's way slower

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Tuple, Any, Dict, List, DefaultDict
+from typing import Optional, Tuple, Any, Dict, List, DefaultDict, Set
 from collections import deque, defaultdict
 from enum import Enum, auto
 from dataclasses import dataclass
@@ -46,6 +46,16 @@ class UOpGraph:
     for i,u in enumerate(self):
       print(f"{i:4d} {str(u.uop):20s}: {str(u.dtype) if u.dtype is not None else '':25s} " f"{str([self._uops.index(x) for x in u.vin]):32s} {u.arg}")
 
+  def get_recursive_children(self, x:UOp) -> Set[UOp]:
+    deps = set([x])
+    ssize = 0
+    while ssize != len(deps):
+      ssize = len(deps)
+      for u in self.uops:
+        if len(deps.intersection([x for x in u.vin if x.uop is not UOps.PHI])):
+          deps.add(u)
+    return deps
+
   def linearize(self):
     # BFS toposort
     graph: DefaultDict[UOp, List[UOp]] = defaultdict(list)
@@ -57,18 +67,31 @@ class UOpGraph:
 
     self._uops = []
     queue = deque(u for u in self.nodes.values() if in_degree[u] == 0)
-    print(len(queue))
+    loops_pending = []
+    ifs_pending = []
     while queue:
       x = queue.popleft()
-      print(x)
+      if x.uop is UOps.LOOP:
+        # start loops as late as possible
+        if len(queue) and not all(x.uop is UOps.LOOP for x in queue):
+          queue.append(x)
+          continue
+        loops_pending.append(x)
+      if x.uop is UOps.IF:
+        ifs_pending.append(x)
       self._uops.append(x)
       for c in graph[x]:
         in_degree[c] -= 1
-        if in_degree[c] == 0:
-          queue.append(c)
+        if in_degree[c] == 0: queue.append(c)
+
+    for u in loops_pending[::-1]:
+      insert_before = self.uops.index(sorted(list(self.get_recursive_children(u)), key=self.uops.index)[-1])+1
+      self._uops.insert(insert_before, UOp(UOps.ENDLOOP, None, (u,)))
+    for u in ifs_pending[::-1]: self._uops.append(UOp(UOps.ENDIF, None, (u,)))
 
   def add(self, uop:UOps, dtype:Optional[DType]=None, vin:Tuple[UOp, ...]=tuple(), arg:Any=None,
           cachable=True, insert_before=None, simplify=True) -> UOp:
+    if uop is UOps.CONST: arg = dtypes.as_const(arg, dtype) # TODO: this doesn't belong here
     if found:=self.nodes.get(key:=(uop, dtype, vin, arg)): return found
     self.nodes[key] = ret = UOp(*key)
     return ret
