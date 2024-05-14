64345f0623360a610aea6951a8fc804b04b181a9
qazal <qazal.software@gmail.com>
2024-05-14 20:51:31 +0300

    update example

--- examples/llm.c/export.py
+++ examples/llm.c/export.py
@@ -6,7 +6,7 @@ Device.DEFAULT = "CLANG"
 from train_gpt2 import GPT, GPTConfig
 from tinygrad.helpers import dedup, to_function_name, flatten, getenv, GRAPH, GlobalCounters, ansilen, to_function_name
 from tinygrad.engine.schedule import create_schedule
-from tinygrad.engine.realize import run_schedule
+from tinygrad.engine.realize import get_linearizer, run_schedule
 from tinygrad.engine.memory import memory_planner
 from tinygrad.ops import BufferOps, LoadOps
 
@@ -16,6 +16,7 @@ if __name__ == "__main__":
   model = GPT(GPTConfig(n_layer=getenv("NLAYER", 12), n_head=12, n_embd=768))
   #model.load_pretrained()
   for p in nn.state.get_parameters(model): p.replace(Tensor.empty(p.shape, dtype=p.dtype)) # fake load pretrained
+  Tensor.training = True
 
   seen = set()
   #early_sched = create_schedule([x.lazydata for x in nn.state.get_parameters(model)], seen)
@@ -46,7 +47,7 @@ if __name__ == "__main__":
   ast_dedup = dedup([si.ast for si in sched if si.ast[0].op is BufferOps.STORE])
   srcs = {}
   for ast in ast_dedup:
-    k = Device["CLANG"].get_linearizer(*ast)
+    k = get_linearizer(Device["CLANG"].renderer, ast)
     k.linearize()
     src = Device["CLANG"].renderer.render(to_function_name(k.name), k.uops)
     srcs[ast] = (k.name, src)
@@ -62,7 +63,7 @@ if __name__ == "__main__":
     if v.lazydata.base.buffer not in used_buffers: print(f"UNUSED: {k}")
     if v.grad is not None: grad_state_dict['grad_'+k] = v.grad
   state_dict.update(grad_state_dict)
-  state_dict.update({'adam_b1': optimizer.b1, 'adam_b2': optimizer.b2, 'adam_t': optimizer.t, 'adam_lr': optimizer.lr})
+  state_dict.update({'adam_b1': optimizer.b1_t, 'adam_b2': optimizer.b2_t, 'adam_lr': optimizer.lr})
   inverse_state_dict = {v:k for k,v in state_dict.items()}
   for p,m,v in zip(optimizer.params, optimizer.m, optimizer.v):
     nm = inverse_state_dict[p]
