08edc06c2078b775003236172e6177bc947cc733
qazal <qazal.software@gmail.com>
2024-05-15 19:24:16 +0300

    wasnt this always false

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -189,7 +189,8 @@ def _graph_schedule(outs:List[LazyBuffer], seen:Set[LazyBuffer]) -> Tuple[Defaul
   # find all reduces, and pair them to a elementwise op. if they can't be cleanly paired, force realize the reduce (or a contig child)
   reduce_for_op: Dict[LazyBuffer, LazyBuffer] = {}
   for r in allbufs:
-    if r != r.base or r.op not in ReduceOps or r in realizes: continue
+    assert r is r.base
+    if r.op not in ReduceOps or r in realizes: continue
 
     group: Set[LazyBuffer] = set()
     _recursive_group(r, r.st, r, children, realizes, reduce_for_op, group)
