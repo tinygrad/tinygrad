e90a28d8dd79c97838f3570245533bdee4d7b34a
qazal <qazal.software@gmail.com>
2024-05-18 15:55:42 +0300

    find paths

--- test/external/fuzz_uops.py
+++ test/external/fuzz_uops.py
@@ -3,24 +3,40 @@ from typing import DefaultDict, Dict, List, Set, Tuple
 from tinygrad.codegen.uops import UOp, UOps
 
 def fuzz_uops(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int], queue:List[Tuple[int, UOp]], push, loops_children:Dict[UOp, Set[UOp]]):
-  uops: List[UOp] = []
+  uops = find_all_topsorts(graph, in_degree, queue, push, loops_children)[-1]
+  return uops
+
+MAX_PATHS = 2
+def find_all_topsorts(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int], queue:List[Tuple[int, UOp]], push, loops_children:Dict[UOp, Set[UOp]]):
+  visited: Set[UOp] = set()
+  ret: List[Tuple[UOp, ...]] = []
+  path: List[UOp] = []
   ifs: List[UOp] = []
+  global_bufs: List[UOp] = []
+
+  # find a path
   while queue:
     _,x = heapq.heappop(queue)
-    if x.uop is UOps.DEFINE_ACC and len(x.vin):
-      idx = min([uops.index(l) for l in x.vin])
-      uops.insert(idx, x)
-    else: uops.append(x)
-    if x.uop is UOps.IF: ifs.append(x)
-    for u, ss in loops_children.items():
-      if x in ss:
-        ss.remove(x)
-        if len(ss) == 0: uops.append(UOp(UOps.ENDLOOP, None, (u,)))
+    if in_degree[x] != 0 or x in visited: continue
+    # insert to path
+    path.append(x)
+    visited.add(x)
     for u in graph[x]:
       in_degree[u] -= 1
       if in_degree[u] == 0: push(u)
 
-  assert uops[-1].uop is UOps.SINK, f"didn't end with SINK, ended with {uops[-1]}"
-  uops = uops[:-1]
-  for u in ifs[::-1]: uops.append(UOp(UOps.ENDIF, None, (u,)))
-  return uops
+  # modify the path
+  for x in path:
+    if x.uop is UOps.DEFINE_ACC and len(x.vin):
+      path.remove(x)
+      path.insert(min(path.index(l) for l in x.vin), x)
+    elif x.uop is UOps.IF: path.insert(len(path)-1, UOp(UOps.ENDIF, None, (x,)))
+    for u, ss in loops_children.items():
+      if x in ss:
+        ss.remove(x)
+        if len(ss) == 0: path.insert(len(path)-1, UOp(UOps.ENDLOOP, None, (u,)))
+
+  # add to paths
+  assert path[-1].uop is UOps.SINK, f"didn't end with SINK, ended with {path[-1]}"
+  ret.append(tuple(path[:-1]))
+  return ret
