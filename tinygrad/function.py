27147fcee10335005554044fbfe9f3b979d90ba1
Tzepesh <vlad.striletchi@airportlabs.com>
2024-05-18 16:07:18 +0300

    re-introduce correction

--- tinygrad/function.py
+++ tinygrad/function.py
@@ -83,22 +83,22 @@ class Sin(Function):
         # res = self._sin(x)
         # return res
 
-        # cf1 = x.const(-0.002)
-        # cf2 = x.const(-0.0075)
-        # cf3 = x.const(-0.05)
-        # cf4 = x.const(-0.05)
+        cf1 = x.const(-0.002)
+        cf2 = x.const(-0.0075)
+        cf3 = x.const(-0.05)
+        cf4 = x.const(-0.05)
         # Choose correction factor based on x magnitude
-        # cf = self._abs(x).e(BinaryOps.CMPLT, x.const(1e14)).e(TernaryOps.WHERE, cf1, cf2)
-        # cf = self._abs(x) .e(BinaryOps.CMPLT, x.const(153e12)) .e(TernaryOps.WHERE, cf, cf3)
-        # cf = self._abs(x).e(BinaryOps.CMPLT, x.const(1e15)).e(TernaryOps.WHERE, cf, cf4)
+        cf = self._abs(x).e(BinaryOps.CMPLT, x.const(1e14)).e(TernaryOps.WHERE, cf1, cf2)
+        cf = self._abs(x) .e(BinaryOps.CMPLT, x.const(153e12)) .e(TernaryOps.WHERE, cf, cf3)
+        cf = self._abs(x).e(BinaryOps.CMPLT, x.const(1e15)).e(TernaryOps.WHERE, cf, cf4)
 
         # cf = x.const(-0.03)
         # cf = x.const(-0.00)
-        # correction = self._sin(
-        #     x.e(BinaryOps.ADD, x.const(math.pi / 2).e(BinaryOps.MUL, xsign))
-        # ).e(BinaryOps.MUL, cf)
+        correction = self._sin(
+            x.e(BinaryOps.ADD, x.const(math.pi / 2).e(BinaryOps.MUL, xsign))
+        ).e(BinaryOps.MUL, cf)
 
-        # res = x.e(BinaryOps.CMPLT, x.const(1e14)).e(TernaryOps.WHERE, res, res.e(BinaryOps.ADD, correction))
+        res = x.e(BinaryOps.CMPLT, x.const(1e13)).e(TernaryOps.WHERE, res, res.e(BinaryOps.ADD, correction))
         # res = x.e(BinaryOps.CMPLT, x.const(1e1)).e(TernaryOps.WHERE, res, res.e(BinaryOps.ADD, correction))
         # print("SIN: ")
         # print(__import__('tinygrad').Tensor(res).numpy())
