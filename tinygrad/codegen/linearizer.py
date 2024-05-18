a723b695e46998d2e2232449e17d8bc9896884dc
qazal <qazal.software@gmail.com>
2024-05-18 09:17:18 +0300

    nit: dont need assign

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -55,11 +55,10 @@ class Linearizer(Kernel):
     return self.uops.add(UOps.CONST, dtype, tuple(), dtypes.as_const(b, dtype))
 
   def get_reduce_acc(self, reduceop:LazyOp):
-    dtype = reduceop.dtype
-    if reduceop.op is ReduceOps.SUM: return 0.0 if dtypes.is_float(dtype) else 0
+    if reduceop.op is ReduceOps.SUM: return 0.0 if dtypes.is_float(reduceop.dtype) else 0
     elif reduceop.op is ReduceOps.MAX:
-      if dtypes.is_int(dtype): return 0 if dtypes.is_unsigned(dtype) else -2**(dtype.itemsize*8-1)
-      return -math.inf if dtypes.is_float(dtype) else False
+      if dtypes.is_int(reduceop.dtype): return 0 if dtypes.is_unsigned(reduceop.dtype) else -2**(reduceop.dtype.itemsize*8-1)
+      return -math.inf if dtypes.is_float(reduceop.dtype) else False
 
   # NOTE: once images are loaded, we uop them as their base float
   def get_base_dtype(self, dt:DType): return dt.base if isinstance(dt, ImageDType) else dt
