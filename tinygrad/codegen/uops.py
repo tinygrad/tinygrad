b574c8b5b662c56b1d1ae2b0c30f45900b192a9c
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-05-18 10:10:09 +0200

    Fix flop coutning for mulacc

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -378,7 +378,7 @@ class UOpGraph:
       elif u.uop is UOps.ENDLOOP:
         mults = mult_stack.pop(-1)
       elif u.uop is UOps.ALU:
-        flops += mults
+        flops += mults * (2 if u.arg == TernaryOps.MULACC else 1)
       elif u.uop is UOps.LOAD:
         assert u.dtype is not None
         mem += u.dtype.itemsize * mults
