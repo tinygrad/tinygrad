15e31e8c905286b52e955b22ae1c0e086a90feb1
VicKnightDR <vicderidder@hotmail.com>
2024-06-05 15:03:48 +0200

    import_statement

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import os, functools, platform, time, re, contextlib, operator, hashlib, pickle, sqlite3, cProfile, pstats, tempfile, pathlib, string, ctypes
 import itertools, urllib.request, subprocess
+import tqdm
 from typing import Dict, Tuple, Union, List, ClassVar, Optional, Iterable, Any, TypeVar, TYPE_CHECKING, Callable, Sequence
 if TYPE_CHECKING:  # TODO: remove this and import TypeGuard from typing once minimum python supported version is 3.10
   from typing_extensions import TypeGuard
