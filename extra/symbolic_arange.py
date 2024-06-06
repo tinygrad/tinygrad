4d02a1148e1738ae30b410bc820daa6bf2c2bf17
chenyu <chenyu@fastmail.com>
2024-06-06 18:04:31 -0400

    better example maybe

--- extra/symbolic_arange.py
+++ extra/symbolic_arange.py
@@ -2,21 +2,11 @@ from tinygrad import Variable, Tensor
 
 def sym_arange(N):
   v = Variable("v", 1, 10).bind(N)
-  t = Tensor.ones(v)
-  # pad left and pool
-  t = t.pad(((v-1, 0),)).expand(v, -1).flatten().pad(((0, v),)).reshape((v, v*2))
-  # take first v and sum
-  t = t.shrink(((0, v),)).sum(0) - 1
-  return t.realize().reshape(N)
-
-# this generates the same kernel
-def sym_arange2(N):
-  v = Variable("v", 1, 10).bind(N)
   t = Tensor.ones(v, v)
   # pad left and pool
-  t = t.pad((None, (v-1, 0))).flatten().pad(((0, v),)).reshape((v, v*2))
+  t = t.pad((None, (v, 0))).flatten().pad(((0, v),)).reshape((v, v*2+1))
   # take first v and sum
-  t = t.shrink(((0, v),)).sum(0) - 1
+  t = t.shrink(((0, v),)).sum(0)
   return t.realize().reshape(N)
 
 if __name__ == "__main__":
