4f657a1936bc3d1ab7ec71c248e95aa67071218e
Jhenner Tigreros <jtigreros16@gmail.com>
2024-06-08 14:17:24 -0500

    Fix formatting text

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -25,7 +25,7 @@ class CStyleLanguage(Renderer):
   type_map: Dict[DType, str] = {}
   code_for_op: Dict = {
     UnaryOps.NEG: lambda x,dtype: f"(!{x})" if dtype == dtypes.bool else f"(-{x})", UnaryOps.SQRT: lambda x,dtype: f"sqrt({x})",
-    UnaryOps.RECIP: lambda x,dtype: f"({"(half)"if dtype == dtypes.half else ""}1/{x})",
+    UnaryOps.RECIP: lambda x,dtype: f"({'(half)'if dtype == dtypes.half else ''}1/{x})",
     UnaryOps.EXP2: lambda x,dtype: f"exp2({x})", UnaryOps.LOG2: lambda x,dtype: f"log2({x})", UnaryOps.SIN: lambda x,dtype: f"sin({x})",
     BinaryOps.ADD: lambda a,b,dtype: f"({a}+{b})", BinaryOps.SUB: lambda a,b,dtype: f"({a}-{b})", BinaryOps.MUL: lambda a,b,dtype: f"({a}*{b})",
     BinaryOps.DIV: lambda a,b,dtype: f"({a}/{b})", BinaryOps.MAX: lambda a,b,dtype: f"max({a},{b})", BinaryOps.MOD: lambda a,b,dtype: f"({a}%{b})",
