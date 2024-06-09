b645ee445f8931a894612740d7684156c91755e6
snakeonex <romakcz@gmail.com>
2024-06-09 19:31:47 +0200

    clean

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -211,23 +211,6 @@ def fetch(url:str, name:Optional[Union[pathlib.Path, str]]=None, allow_caching=n
         pathlib.Path(f.name).rename(fp)
   return fp
 
-from tqdm import tqdm
-def fetch2(url:str, name:Optional[Union[pathlib.Path, str]]=None, allow_caching=not getenv("DISABLE_HTTP_CACHE")) -> pathlib.Path:
-  if url.startswith(("/", ".")): return pathlib.Path(url)
-  fp = pathlib.Path(name) if name is not None and (isinstance(name, pathlib.Path) or '/' in name) else pathlib.Path(_cache_dir) / "tinygrad" / "downloads" / (name if name else hashlib.md5(url.encode('utf-8')).hexdigest())  # noqa: E501
-  if not fp.is_file() or not allow_caching:
-    with urllib.request.urlopen(url, timeout=10) as r:
-      assert r.status == 200
-      total_length = int(r.headers.get('content-length', 0))
-      progress_bar = tqdm(total=total_length, unit='B', unit_scale=True, desc=f"{url}: ", disable=False)
-      (path := fp.parent).mkdir(parents=True, exist_ok=True)
-      with tempfile.NamedTemporaryFile(dir=path, delete=False) as f:
-        while chunk := r.read(16384): progress_bar.update(f.write(chunk))
-        f.close()
-        if (file_size:=os.stat(f.name).st_size) < total_length: raise RuntimeError(f"fetch size incomplete, {file_size} < {total_length}")
-        pathlib.Path(f.name).rename(fp)
-  return fp
-
 # *** Exec helpers
 
 def cpu_time_execution(cb, enable):
