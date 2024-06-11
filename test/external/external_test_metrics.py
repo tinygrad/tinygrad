5d50821c4c65905d8a259c036f1fd9ed64958daa
Jhenner Tigreros <jtigreros16@gmail.com>
2024-06-11 15:56:18 -0500

    Reduce margins

--- test/external/external_test_metrics.py
+++ test/external/external_test_metrics.py
@@ -10,7 +10,7 @@ class ExternalTestMetrics(unittest.TestCase):
   def _test_metrics(self, tinygrad_metrics, orig_metrics, pred, label):
     tinygrad_metrics_res = tinygrad_metrics(Tensor(pred), Tensor(label)).squeeze().numpy()
     orig_metrics_res = orig_metrics(torch.from_numpy(pred), torch.from_numpy(label)).numpy()
-    np.testing.assert_allclose(tinygrad_metrics_res, orig_metrics_res, atol=1e-6, rtol=1e-3)
+    np.testing.assert_allclose(tinygrad_metrics_res, orig_metrics_res, atol=1e-8, rtol=1e-7)
 
   def test_dice(self):
     pred, label = np.random.rand(1, 3, 128, 128, 128).astype(np.float32), np.ones((1, 1, 128, 128, 128)).astype(np.uint8)
