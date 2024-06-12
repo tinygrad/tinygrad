957d977fcace06822f3c03ff6ce761a1deff52d1
George Hotz <geohot@gmail.com>
2024-06-12 12:23:39 +0200

    rebase

--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -268,13 +268,8 @@ class Linearizer(Kernel):
     # end the local loop, do the local reduce
     if self.group_for_reduces:
       fake_global_idxs = [x*0 for x in global_idxs]
-<<<<<<< HEAD
       stores = self.global_store(out_buf, fake_global_idxs+local_idxs+fake_reduce_idxs+upcast_idxs, accs[reduceop])  # store accumulators
-      barrier = self.uops.add(UOps.BARRIER, None, tuple(stores))
-=======
-      stores = self.global_store(-1, fake_global_idxs+local_idxs+fake_reduce_idxs+upcast_idxs, accs[reduceop])  # store accumulators
       barrier = UOp(UOps.BARRIER, None, tuple(stores))
->>>>>>> ec83533c (don't use uops.add while constructing)
       if self.opts.has_local:
         fake_idxs = [NumNode(0)]*len(self.sts[-1].shape)
         fake_idxs[self.global_dims+self.local_dims:self.global_dims+len(local_idxs)] = local_idxs[self.local_dims:]
