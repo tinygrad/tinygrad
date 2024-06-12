f24a779b8f5e7aef81a8eea96c63598d4b39bab8
Jhenner Tigreros <jtigreros16@gmail.com>
2024-06-11 19:38:32 -0500

    Remove E501

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Iterator, Optional, Tuple, Any, Dict, List, DefaultDict, Set, Callable, Union, cast, TypeVar
-import functools, itertools, heapq, math
+import functools, itertools, heapq
+from math import isnan
 from collections import defaultdict
 from enum import Enum, auto
 from dataclasses import dataclass, field
@@ -199,7 +200,7 @@ constant_folder = PatternMatcher([
   # ** zero folding **
   #x*0 -> 0 or 0*x -> 0
   #if x is nan it should render the nan value.
-  (UPat(UOps.ALU, BinaryOps.MUL, [UPat(name="x"), UPat(UOps.CONST, 0, name="c")]), lambda x, c: x if isinstance(x.arg, float) and math.isnan(x.arg) else c),
+  (UPat(UOps.ALU, BinaryOps.MUL, [UPat(name="x"), UPat(UOps.CONST, 0, name="c")]), lambda x,c: x if isinstance(x.arg, float) and isnan(x.arg) else c),
   (UPat(UOps.ALU, BinaryOps.SUB, (UPat(name="x"), UPat(name="x"))), lambda x: UOp.const(x.dtype, 0)),   # x-x -> 0
   # ** load/store folding **
   (UPat(UOps.STORE, vin=(UPat(name="buf"), UPat(name="idx"),
