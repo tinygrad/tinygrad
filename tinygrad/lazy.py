d5a92b9b83eb76e5e66aa849aec2982d861ec670
George Hotz <72895+geohot@users.noreply.github.com>
2024-06-14 05:16:05 -0700

    sort the axis in reduce op [run_process_replay] (#4956)

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -170,7 +170,7 @@ class LazyBuffer:
 
   def _reduce_op(self, op:ReduceOps, axis:Tuple[int, ...]) -> LazyBuffer:
     assert all(0 <= x < len(self.shape) for x in axis), f"axis args {axis} out of range for shape {self.shape}"
-    axis = tuple(x for x in axis if self.shape[x] != 1)
+    axis = tuple(sorted([x for x in axis if self.shape[x] != 1]))
     if len(axis) == 0: return self
     new_shape = tuple(1 if i in axis else s for i,s in enumerate(self.shape))
     return create_lazybuffer(self.device, ShapeTracker.from_shape(new_shape), self.dtype, op, axis, (self,))
