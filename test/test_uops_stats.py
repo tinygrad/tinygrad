63a8add2c26a084d53a945a376bcd317e21907d4
George Hotz <72895+geohot@users.noreply.github.com>
2024-06-14 03:52:37 -0700

    move uops add logic to linearize (#4952)
    * move logic to linearize
    
    * idk how this should work
    
    * empty

--- test/test_uops_stats.py
+++ test/test_uops_stats.py
@@ -63,7 +63,7 @@ class TestUOpsStats(unittest.TestCase):
     u3 = UOp(UOps.CONST, dtypes.int, tuple(), 3)
     u4 = UOp(UOps.ALU, dtypes.int, (u1,u2), BinaryOps.MUL)
     u5 = UOp(UOps.ALU, dtypes.int, (u4,u3), BinaryOps.ADD)
-    uops = UOpGraph([UOp(UOps.SINK, None, (u5,))])
+    uops = UOpGraph([u5])
 
     globl = UOp(UOps.DEFINE_GLOBAL, dtypes.int, tuple())
     o1 = UOp(UOps.CONST, dtypes.int, tuple(), 1)
@@ -72,7 +72,7 @@ class TestUOpsStats(unittest.TestCase):
     u2 = UOp(UOps.LOAD, dtypes.int, (globl, o2))
     u3 = UOp(UOps.CONST, dtypes.int, tuple(), 3)
     u4 = UOp(UOps.ALU, dtypes.int, (u1,u2,u3), TernaryOps.MULACC)
-    uops_fma = UOpGraph([UOp(UOps.SINK, None, (u4,))])
+    uops_fma = UOpGraph([u4])
 
     self.assertEqual(uops.flops_mem(), uops_fma.flops_mem())
 
