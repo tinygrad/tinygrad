5b96c614d27ccefc53a7653a06c055f05c4bf96e
samm393 <martin.samarthur@gmail.com>
2024-06-15 11:29:00 +0100

    removing sub from dtype test

--- test/test_dtype_alu.py
+++ test/test_dtype_alu.py
@@ -19,7 +19,8 @@ print(settings.default)
 dtypes_float = (dtypes.float32, dtypes.float16)
 dtypes_int = (dtypes.int8, dtypes.int16, dtypes.int32, dtypes.int64, dtypes.uint8, dtypes.uint16, dtypes.uint32, dtypes.uint64)
 dtypes_bool = (dtypes.bool,)
-binary_operations = [operator.add, operator.sub, operator.mul, operator.lt, operator.eq]
+binary_operations = [operator.add, operator.mul, operator.lt, operator.eq]
+#binary_operations = [operator.add, operator.sub, operator.mul, operator.lt, operator.eq]
 
 # TODO: LLVM comparing with nan is incorrect
 if Device.DEFAULT == "LLVM":
