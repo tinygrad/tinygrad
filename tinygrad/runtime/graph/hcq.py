cf05d89f19a43d88f7cb999315db866e8a9050fc
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-06-17 21:56:24 +0000

    fix sync

--- tinygrad/runtime/graph/hcq.py
+++ tinygrad/runtime/graph/hcq.py
@@ -59,7 +59,8 @@ class HCQGraph(MultiGraphRunner):
     # Schedule signals
     for j,ji in enumerate(self.jit_cache):
       if isinstance(ji.prg, CompiledRunner):
-        deps = self.access_resources(ji.bufs[(outs:=ji.prg.p.outcount):], ji.bufs[:outs], (self.comp_signal[dev:=ji.prg.device], sig_val:=j+1))
+        deps = self.access_resources(ji.bufs[(outs:=ji.prg.p.outcount):], ji.bufs[:outs], (self.comp_signal[(dev:=ji.prg.device)], sig_val:=j+1))
+        if (val:=self.comp_signal_val[dev]) > 0: deps = [x for x in deps if id(x[0]) != id(self.comp_signal[dev])] + [(self.comp_signal[dev], val)]
 
         # Remove self-dependency for AMD or NV with only 1 same-queue dep, since NV chains 2+ kernels in this case, eliminating dep need.
         if (dname:=dev.dname.split(":", 1)[0]) == "AMD" or (dname == "NV" and len(deps) == 1 and id(deps[0][0]) == id(self.comp_signal[dev])):
@@ -68,7 +69,8 @@ class HCQGraph(MultiGraphRunner):
         signal_scheduling[j] = (deps, None) # on compute kernels, we can skip signals by default, since no dependencies might be there.
         self.comp_signal_val[dev] = sig_val
       elif isinstance(ji.prg, BufferXfer):
-        deps = self.access_resources([src:=ji.bufs[0]], [dest:=ji.bufs[1]], (self.copy_signal[Device[src.device]], sig_val:=j+1))
+        dest, src = [cast(Buffer, x) for x in ji.bufs[0:2]]
+        deps = self.access_resources([src], [dest], (self.copy_signal[Device[src.device]], sig_val:=j+1))
         deps = [x for x in deps if id(x[0]) != id(self.copy_signal[Device[src.device]])]
         signal_scheduling[j] = (deps, j+1)
         self.copy_signal_val[Device[src.device]] = sig_val
