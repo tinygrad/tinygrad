7bfa9101c07e6a8019b7b1ea064d61dab4b34158
Elias Wahl <82230675+Eliulm@users.noreply.github.com>
2024-06-18 14:16:41 +0200

    Float in scaled dot product attention (#4985)
    * Monkeypatch scaled-dot-product-attention
    
    * Use dot instead of matmul
    
    * new api
    
    * imports
    
    * least_upper_dtype

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -2704,8 +2704,8 @@ class Tensor:
     assert all_int(self.shape), f"does not support symbolic shape {self.shape}"
     if is_causal: attn_mask = Tensor.ones(self.shape[-2], key.shape[-2], requires_grad=False, device=self.device).tril(0).cast(dtypes.bool)
     if attn_mask is not None and attn_mask.dtype == dtypes.bool: attn_mask = (attn_mask == 0).where(-float("inf"), 0)
-    qk = self @ key.transpose(-2,-1) / math.sqrt(self.shape[-1])
-    return ((qk+attn_mask) if attn_mask is not None else qk).softmax(-1).dropout(dropout_p) @ value
+    qk = self.matmul(key.transpose(-2,-1), acc_dtype=least_upper_dtype(self.dtype, key.dtype, dtypes.float32)) / math.sqrt(self.shape[-1])
+    return ((qk+attn_mask) if attn_mask is not None else qk).softmax(-1).cast(dtypes.default_float).dropout(dropout_p) @ value
 
   def binary_crossentropy(self, y:Tensor) -> Tensor:
     """
