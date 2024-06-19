54d25c68cdc491387669c7011eed8881a1114623
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-06-19 20:39:15 +0000

    fix hcq sync

--- tinygrad/runtime/graph/hcq.py
+++ tinygrad/runtime/graph/hcq.py
@@ -46,48 +46,71 @@ class HCQGraph(MultiGraphRunner):
     self.copy_signal_val = {dev: 0 for dev in self.devices}
 
     self.kickoff_signal = self.devices[0]._get_signal(value=0)
+    self.copy_kickoff_signal = {dev: dev._get_signal(value=0) for dev in self.devices}
     self.kickoff_value = 0
-    self.graph_timeline = {dev: 0 for dev in self.devices}
 
     self.signal_sched: Dict[int, Tuple[List, Optional[int], Optional[Tuple]]] = {} # Dict[ji_idx, (deps, output sigval, (prof_st_sig, prof_en_sig))]
     self.exec_ptrs: Dict[int, Tuple[Any, int]] = {}
     self.copy_to_devs: Dict[Compiled, Set[Compiled]] = {dev: set() for dev in self.devices}
+    self.graph_timeline = {dev: 0 for dev in self.devices}
+    signal_save_devs: Dict[int, Set] = collections.defaultdict(set)
+    self.queue_last_prg = collections.defaultdict(lambda : None)
 
     # Schedule dependencies
     for j,ji in enumerate(self.jit_cache):
+      enqueue_queue = self.copy_queues[Device[ji.bufs[1].device]] if isinstance(ji.prg, BufferXfer) else self.comp_queues[ji.prg.device] #type:ignore
       if isinstance(ji.prg, CompiledRunner):
-        deps = self.access_resources(ji.bufs[(outs:=ji.prg.p.outcount):], ji.bufs[:outs], (self.comp_signal[(dev:=ji.prg.device)], sig_val:=j+1))
-        if (val:=self.comp_signal_val[dev]) > 0: deps = [x for x in deps if id(x[0]) != id(self.comp_signal[dev])] + [(self.comp_signal[dev], val)]
+        deps = self.access_resources(ji.bufs[(outs:=ji.prg.p.outcount):], ji.bufs[:outs], (self.comp_signal[(dev:=ji.prg.device)], j + 1))
+        if (last_j:=self.queue_last_prg[self.comp_queues[dev]]) is not None:
+          deps = [x for x in deps if id(x[0]) != id(self.comp_signal[dev])] + [(self.comp_signal[dev], last_j + 1)]
 
         # Remove self-dependency for AMD or NV with only 1 same-queue dep, since NV chains 2+ execs in this case, eliminating dep need.
         if (dname:=dev.dname.split(":", 1)[0]) == "AMD" or (dname == "NV" and len(deps) == 1 and id(deps[0][0]) == id(self.comp_signal[dev])):
           deps = [x for x in deps if id(x[0]) != id(self.comp_signal[dev])]
-
-        self.comp_signal_val[dev] = sig_val
       elif isinstance(ji.prg, BufferXfer):
         dest, src = [cast(Buffer, x) for x in ji.bufs[0:2]]
-        deps = self.access_resources([src], [dest], (self.copy_signal[(dev:=Device[src.device])], sig_val:=j+1))
+        deps = self.access_resources([src], [dest], (self.copy_signal[(dev:=Device[src.device])], j + 1))
         deps = [x for x in deps if id(x[0]) != id(self.copy_signal[Device[src.device]])]
-        self.copy_signal_val[Device[src.device]] = sig_val
-        self.copy_to_devs[Device[dest.device]].add(Device[src.device])
 
       # When running compute, set up lazy signals, since no dependencies might be there. Copies always have signals to sync.
+      signal_save_devs[j].update(((list(signal_save_devs[prev_j])+[dev]) if (prev_j:=self.queue_last_prg[enqueue_queue]) is not None else [dev]))
+      for sig, val in deps: signal_save_devs[j].update(signal_save_devs[val - 1])
+
+      # Update previous schedules in case we need thier signal now.
+      for sig, val in deps: self.signal_sched[val - 1] = (self.signal_sched[val - 1][0], val, self.signal_sched[val - 1][2])
+
+      # Transfer op accesses 2 buffers, src buffer is save, since it's on the device where op in enqueued, but also
+      # need to be sure that dest buffer is save access.
+      if isinstance(ji.prg, BufferXfer) and (dest_dev:=Device[ji.bufs[0].device]) not in signal_save_devs[j]:
+        deps = [(self.copy_kickoff_signal[dest_dev], self.kickoff_value)] + deps
+        signal_save_devs[j].update([dest_dev])
+        # print(signal_save_devs[j])
+      
+      # Fill current signal sched entry.
       prof_ji_desc = ji.prg.clprg.name if isinstance(ji.prg, CompiledRunner) else f"{ji.bufs[1].device} -> {ji.bufs[0].device}" # type: ignore
       prof_info = (dev._get_signal(), dev._get_signal(), dev, prof_ji_desc, isinstance(ji.prg, BufferXfer)) if PROFILE else None
       self.signal_sched[j] = (deps, None if isinstance(ji.prg, CompiledRunner) else j + 1, prof_info)
-      for sig, val in deps: self.signal_sched[val - 1] = (self.signal_sched[val - 1][0], val, self.signal_sched[val - 1][2])
+
+      self.queue_last_prg[enqueue_queue] = j
 
     # Building hardware queues
+    self.exec_ptrs: Dict[int, Tuple[Any, int]] = {}
+    self.copy_to_devs: Dict[Compiled, Set[Compiled]] = {dev: set() for dev in self.devices}
+    self.waits_cmds: Dict[Compiled, List] = {self.copy_queues[dev]: [0] for dev in self.devices}
+
     for dev in self.devices:
-      self.comp_queues[dev].memory_barrier().wait(dev.timeline_signal, dev.timeline_value - 1).wait(self.kickoff_signal, self.kickoff_value)
-      self.copy_queues[dev].wait(dev.timeline_signal, dev.timeline_value - 1).wait(self.kickoff_signal, self.kickoff_value)
+      self.comp_queues[dev].memory_barrier().wait(dev.timeline_signal, dev.timeline_value - 1).wait(self.kickoff_signal, self.kickoff_value) \
+                           .signal(self.copy_kickoff_signal[dev], self.kickoff_value)
+      self.copy_queues[dev].wait(self.copy_kickoff_signal[dev], self.kickoff_value)
 
     for j,ji in enumerate(self.jit_cache):
       deps, signal_value, prof_info = self.signal_sched[j]
       enqueue_queue = self.copy_queues[Device[ji.bufs[1].device]] if isinstance(ji.prg, BufferXfer) else self.comp_queues[ji.prg.device] #type:ignore
 
       # Encode waits and start profile timestamp (if needed).
-      for sig, val in deps: enqueue_queue.wait(sig, val)
+      for sig, val in deps:
+        enqueue_queue.wait(sig, val)
+        if id(sig) in [id(x) for x in self.copy_kickoff_signal.values()]: self.waits_cmds[enqueue_queue].append(len(enqueue_queue) - 1)
       if prof_info: enqueue_queue.timestamp(prof_info[0])
 
       # Encode main commands based on ji type.
@@ -105,12 +128,13 @@ class HCQGraph(MultiGraphRunner):
       if prof_info: enqueue_queue.timestamp(prof_info[1])
 
     for dev in self.devices:
-      if self.copy_signal_val[dev] > 0: self.comp_queues[dev].wait(self.copy_signal[dev], self.copy_signal_val[dev])
-      for dep_dev in self.copy_to_devs[dev]: self.comp_queues[dev].wait(self.copy_signal[dep_dev], self.copy_signal_val[dep_dev])
+      for dep_dev in list(self.copy_to_devs[dev]) + [dev]:
+        if (last_j:=self.queue_last_prg[self.copy_queues[dep_dev]]) is None: continue
+        self.comp_queues[dev].wait(self.copy_signal[dep_dev], self.signal_sched[last_j][1])
 
       self.comp_queues[dev].signal(dev.timeline_signal, dev.timeline_value)
       if hasattr(self.comp_queues[dev], 'bind'): self.comp_queues[dev].bind(dev)
-      if hasattr(self.copy_queues[dev], 'bind') and self.copy_signal_val[dev] > 0: self.copy_queues[dev].bind(dev)
+      if hasattr(self.copy_queues[dev], 'bind') and self.queue_last_prg[self.copy_queues[dev]] is not None: self.copy_queues[dev].bind(dev)
 
   def __call__(self, input_rawbuffers: List[Buffer], var_vals: Dict[Variable, int], wait=False) -> Optional[float]:
     # Wait and restore signals
@@ -138,13 +162,17 @@ class HCQGraph(MultiGraphRunner):
 
     for dev in self.devices:
       self.comp_queues[dev].update_wait(1, dev.timeline_signal, dev.timeline_value - 1).update_wait(2, value=self.kickoff_value) \
+                           .update_signal(3, value=self.kickoff_value) \
                            .update_signal(len(self.comp_queues[dev]) - 1, dev.timeline_signal, dev.timeline_value).submit(dev)
 
-      if self.copy_signal_val[dev] > 0:
-        self.copy_queues[dev].update_wait(0, dev.timeline_signal, dev.timeline_value - 1).update_wait(1, value=self.kickoff_value).submit(dev)
+      if self.queue_last_prg[(cp_queue:=self.copy_queues[dev])] is not None:
+        # print(dev.device_id, self.copy_waits_cmds[dev])
+        for cmd_idx in self.waits_cmds[cp_queue]: cp_queue.update_wait(cmd_idx, value=self.kickoff_value)
+        cp_queue.submit(dev)
 
       self.graph_timeline[dev] = dev.timeline_value
       dev.timeline_value += 1
+    # print()
 
     if wait:
       st = time.perf_counter()
