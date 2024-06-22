ba0c73c2a5b1718dd2207a214ec07b53d35fca45
Mesozoic Egg <mesozoic.egg@proton.me>
2024-06-22 18:04:41 +0800

    Repurpose uops graph test to compare bottom up and top down rewrite, intentially keep arange failure case

--- test/test_uop_graph.py
+++ test/test_uop_graph.py
@@ -152,7 +152,105 @@ class TestBottomupVsTopdownRewrite(TestUOps):
       UOp(UOps.CONST, dtypes.float, arg=3.0),
     )))
   
-  @unittest.skip("Skip until patterns rule return node that cannot further be simplified")
+  def test_where_const_fold(self):
+    def factory():
+      bf = UOp(UOps.CONST, dtypes.bool, arg=False)
+      c1 = UOp(UOps.CONST, dtypes.float, arg=1.0)
+      c2 = UOp(UOps.CONST, dtypes.float, arg=2.0)
+      out = UOp(UOps.ALU, dtypes.float, (bf, c1, c2), TernaryOps.WHERE)
+      return out
+    self.setup_and_assert(factory)
+
+  def test_add_constant_fold(self):
+    def factory():
+      c1 = UOp(UOps.CONST, dtypes.float, arg=1.0)
+      c2 = UOp(UOps.CONST, dtypes.float, arg=2.0)
+      out = UOp(UOps.ALU, dtypes.float, (c1, c2), BinaryOps.ADD)
+      return out
+    self.setup_and_assert(factory)
+
+  def test_where_same_fold(self):
+    def factory():
+      v = UOp(UOps.DEFINE_VAR, dtypes.int, arg=Variable('tmp', 0, 1))
+      c0 = UOp(UOps.CONST, dtypes.int, arg=0)
+      vc = UOp(UOps.ALU, dtypes.bool, (v, c0), BinaryOps.CMPNE)
+      c1 = UOp(UOps.CONST, dtypes.float, arg=1.0)
+      out = UOp(UOps.ALU, dtypes.float, (vc, c1, c1), TernaryOps.WHERE)
+      return out
+    self.setup_and_assert(factory)
+
+  def test_where_const_fold(self):
+    bf = UOp(UOps.CONST, dtypes.bool, arg=False)
+    c1 = UOp(UOps.CONST, dtypes.float, arg=1.0)
+    c2 = UOp(UOps.CONST, dtypes.float, arg=2.0)
+    out = UOp(UOps.ALU, dtypes.float, (bf, c1, c2), TernaryOps.WHERE)
+    g = UOpGraph([out])
+    self.assertEqual(len(g.uops), 1)
+    out = g.uops[-1]
+    self.assertEqual(out.op, UOps.CONST)
+    self.assertEqual(out.arg, 2.0)
+
+  def test_const_cast(self):
+    def factory():
+      bf = UOp(UOps.CONST, dtypes.bool, arg=False)
+      out = UOp(UOps.CAST, dtypes.int, (bf,))
+      return out
+    self.setup_and_assert(factory)
+  def test_cast_vectorized_fold(self):
+    def factory():
+      d0 = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=(0, True))
+      idx = UOp(UOps.CONST, dtypes.int, arg=0)
+      ld = UOp(UOps.LOAD, dtypes.float.vec(2), (d0, idx))
+      cast = UOp(UOps.CAST, dtypes.float.vec(2), (ld,))
+      x = UOp(UOps.GEP, dtypes.float, (cast, ), arg=0)
+      alu = UOp(UOps.ALU, dtypes.float, (x, ), UnaryOps.SQRT)
+      out = UOp(UOps.STORE, dtypes.float, (d0, idx, alu))
+      return out
+    self.setup_and_assert(factory)
+
+  def test_depth_2_const_fold(self):
+    def factory():
+      v = UOp(UOps.DEFINE_VAR, dtypes.int, arg=Variable('tmp', 0, 1))
+      c2 = UOp(UOps.CONST, dtypes.int, arg=2)
+      c4 = UOp(UOps.CONST, dtypes.int, arg=4)
+      vc = UOp(UOps.ALU, dtypes.int, (v, c2), BinaryOps.ADD)
+      out = UOp(UOps.ALU, dtypes.int, (vc, c4), BinaryOps.ADD)
+      return out
+    self.setup_and_assert(factory)
+
+  def test_fold_gated_load(self):
+    def factory():
+      glbl0 = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), (0, True))
+      glbl1 = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), (1, False))
+      glbl2 = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), (2, False))
+      idx = UOp.const(dtypes.int, 0)
+      ld0 = UOp(UOps.LOAD, dtypes.int, (glbl1, idx, UOp.const(dtypes.bool, False), UOp.const(dtypes.int, 2)))
+      ld1 = UOp(UOps.LOAD, dtypes.int, (glbl2, idx, UOp.const(dtypes.bool, True), UOp.const(dtypes.int, 3)))
+      out = UOp(UOps.STORE, None, (glbl0, idx, ld0+ld1))
+      return out
+    self.setup_and_assert(factory)
+  def test_fold_gated_load_local(self):
+    def factory():
+      glbl0 = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), (0, True))
+      smem = UOp(UOps.DEFINE_LOCAL, PtrDType(dtypes.int), (), ("temp", 1))
+      lidx = UOp(UOps.SPECIAL, dtypes.int, (), (0, "lidx1", 16))
+      st = UOp(UOps.STORE, None, (smem, lidx, UOp.load(glbl0, lidx, dtype=dtypes.int)))
+      barrier = UOp(UOps.BARRIER, None, (st, ))
+      ld0 = UOp(UOps.LOAD, dtypes.int, (smem, lidx+1, UOp.const(dtypes.bool, False), UOp.const(dtypes.int, 2), barrier))
+      ld1 = UOp(UOps.LOAD, dtypes.int, (smem, lidx+2, UOp.const(dtypes.bool, True), UOp.const(dtypes.int, 3), barrier))
+      store = UOp(UOps.STORE, None, (glbl0, lidx, ld0+ld1))
+      return store
+    self.setup_and_assert(factory)
+
+  def test_fold_gated_store(self):
+    def factory():
+      glbl = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), (0, True))
+      idx0 = UOp.const(dtypes.int, 0)
+      val = UOp.const(dtypes.int, 42)
+      store = UOp(UOps.STORE, None, (glbl, idx0, val, UOp.const(dtypes.bool, False)))
+      return store
+    self.setup_and_assert(factory)
+  
   def test_arange(self):
     def factory():
       const_0 = UOp(UOps.CONST, dtypes.float, (), 0.0)
