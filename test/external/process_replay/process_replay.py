1d3eaf2fe15ab342a8b0ac3439a530951f9be7bb
DKormann <dominikormann@outlook.com>
2024-06-22 23:05:30 +0200

    integrate in error structure

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # compare kernels created by HEAD against master
-import difflib, pickle, shutil
+import difflib, pickle
 from tinygrad.codegen.linearizer import Linearizer, time_lin
 from tinygrad.helpers import colored, db_connection, VERSION, getenv, to_function_name, tinytqdm
 
@@ -14,16 +14,13 @@ for offset in tinytqdm(range(0, row_count, page_size)):
     compare_k, compare_src, time_baseline = pickle.loads(row[0])
     k = Linearizer(*compare_k.ast, opts=compare_k.opts)
     for opt in compare_k.applied_opts: k.apply_opt(opt)
-
     good_src = k.opts.render(to_function_name(compare_k.name), k.linearize().uops)
-
     time_ = time_lin(k)
-    if (time_-time_baseline)/max(time_baseline, 1e8) > 0.2:
-      info = f"\rPerf: {time_baseline*1e-6:6.2f} -> {time_*1e-6:6.2f} ms {compare_k.name}"
-      print('\r'+' '*(shutil.get_terminal_size().columns) +info )
-    try: assert compare_src == good_src
+    try:
+      assert compare_src == good_src, "PROCESS REPLAY DETECTED CHANGE"
+      assert (time_-time_baseline)/max(time_baseline, 1e8) < 0.2, f"TIME REPLAY DETECTED CHANGE {time_baseline} ->{time_}"
     except AssertionError as e:
-      print("PROCESS REPLAY DETECTED CHANGE")
+      print(e.args[0])
       print(compare_k.ast)
       print(compare_k.applied_opts)
       diff = list(difflib.unified_diff(good_src.splitlines(), compare_src.splitlines()))
