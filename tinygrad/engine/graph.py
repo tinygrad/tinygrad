cc9fb7be39bb6eb16bda872d372ac4800a901f4a
DKormann <dominikormann@outlook.com>
2024-06-24 22:58:13 +0200

    fix and extend print_tree

--- tinygrad/engine/graph.py
+++ tinygrad/engine/graph.py
@@ -77,12 +77,12 @@ def log_lazybuffer(lb:'LazyBuffer', scheduled=False):
 
 def _tree(luop:Union[LazyOp,UOp], cycles, cnt, prefix=""):
   cnt[0] += 1
-  if len(luop.src) == 0: return [f"━━ {prefix}{luop.op.name} {luop.arg if luop.arg else ''}"]
+  if luop.src is None or len(luop.src) == 0: return [f"━━ {prefix}{luop.op.name if luop.op else '?'} {'?' if luop.arg is None else luop.arg}"]
   if (lid := id(luop)) in cycles and cycles[lid][1] > (tcnt := getenv("TREE_CYCLE_CNT", 5)) and tcnt >= 0:
-    return [f"━⬆︎ goto {cycles[id(luop)][0]}: {luop.op.name}"]
+    return [f"━⬆︎ goto {cycles[id(luop)][0]}: {luop.op.name if luop.op else '?'}"]
   cycles[lid] = (cnt[0], 1 if lid not in cycles else cycles[lid][1]+1)
-  lines = [f"━┳ {prefix}{luop.op.name} {luop.arg if luop.arg else ''}"]
-  childs = [_tree(c, cycles, cnt) for c in luop.src[:]]
+  lines = [f"━┳ {prefix}{luop.op.name if luop.op else '?'} {luop.arg if luop.arg else ''}"]
+  childs = [] if luop.src is None else [_tree(c, cycles, cnt) for c in luop.src[:]]
   for c in childs[:-1]: lines += [f" ┣{c[0]}"] + [f" ┃{l}" for l in c[1:]]
   return lines + [" ┗"+childs[-1][0]] + ["  "+l for l in childs[-1][1:]]
 
