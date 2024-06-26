6bcff1f95806c73522eef2e83c641e3efea21103
qazal <qazal.software@gmail.com>
2024-06-26 12:42:40 +0300

    delete uop from the list

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -12,7 +12,7 @@ row_count = cur.execute(f"select count(*) from 'process_replay_{VERSION}'").fetc
 for offset in tqdm(range(0, row_count, page_size)):
   cur.execute(f"SELECT val FROM 'process_replay_{VERSION}' LIMIT ? OFFSET ?", (page_size, offset))
   for row in cur.fetchall():
-    name, ast, device, applied_opts, compare_uops, compare_src = pickle.loads(row[0])
+    name, ast, device, applied_opts, compare_src = pickle.loads(row[0])
     k = Linearizer(*ast, opts=Device[device].renderer)
     for opt in applied_opts: k.apply_opt(opt)
     good_src = k.opts.render(name, k.linearize().uops)
--- tinygrad/codegen/linearizer.py
+++ tinygrad/codegen/linearizer.py
@@ -520,7 +520,7 @@ class Linearizer(Kernel):
     self.linearize()
     info = get_lazyop_info(self.ast[0])
     src = self.opts.render(name:=to_function_name(self.name), self.uops)
-    if getenv("RUN_PROCESS_REPLAY"): diskcache_put("process_replay", id(self), (name, self.ast, self.opts.device, self.applied_opts, self.uops, src))
+    if getenv("RUN_PROCESS_REPLAY"): diskcache_put("process_replay", id(self), (name, self.ast, self.opts.device, self.applied_opts, src))
     ops, mem = self.uops.flops_mem()
     run_count = prod((self.global_size if self.global_size else []) + (self.local_size if self.local_size else []))
     # NOTE: we use min here to ignore the indexing FLOPS
