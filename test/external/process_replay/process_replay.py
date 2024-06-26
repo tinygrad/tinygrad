702302b1f80b1f340ce2e53cac1feca920d93b63
DKormann <dominikormann@outlook.com>
2024-06-26 15:01:19 +0200

    timecompare

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -16,7 +16,7 @@ for offset in tqdm(range(0, row_count, page_size)):
     k = Linearizer(*compare_k.ast, opts=compare_k.opts)
     for opt in compare_k.applied_opts: k.apply_opt(opt)
     good_src = k.opts.render(to_function_name(compare_k.name), k.linearize().uops)
-    time_ = timeit(UOpGraph(k.uops).linearize)
+    time_ = timeit(UOpGraph(k.uops.sinks).linearize)
     if (time_ - time_baseline) / max(time_baseline, .1) > 0.1: print(colored(f"PERF: {time_baseline:.2f} -> {time_:.2f}",'red'))
     try: assert compare_src == good_src, "PROCESS REPLAY DETECTED CHANGE"
     except AssertionError as e:
