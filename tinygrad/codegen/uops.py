de0b277c69ffc4eb008d561a3e16e62600eae0e9
DKormann <dominikormann@outlook.com>
2024-06-26 22:33:12 +0200

    flatten out graph rewrite

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -311,21 +311,21 @@ class UOpGraph:
     # recursive rewrite
     changed = getenv("UOPS_REWRITE", 1)
     run_cnt = 0
+    @functools.lru_cache(None)
+    def rewrite(u:UOp) -> UOp:
+      nonlocal changed
+      recurse_cnt = 0
+      up = u
+      # locally recursively rewrite
+      while (rewritten := pm.rewrite(up)):
+        assert recurse_cnt < 100, f"recursive_rewrite looped {up} <--> {rewritten}"
+        up = rewritten
+        recurse_cnt += 1
+      changed += recurse_cnt
+      up = UOp(up.op, up.dtype, tuple(rewrite(x) for x in up.src), up.arg)
+      return self.nodes.setdefault(up.tuple(), up) # replace with cached nodes
     while changed:
       changed = 0
-      @functools.lru_cache(None)
-      def rewrite(u:UOp) -> UOp:
-        nonlocal changed
-        recurse_cnt = 0
-        up = u
-        # locally recursively rewrite
-        while (rewritten := pm.rewrite(up)):
-          assert recurse_cnt < 100, f"recursive_rewrite looped {up} <--> {rewritten}"
-          up = rewritten
-          recurse_cnt += 1
-        changed += recurse_cnt
-        up = UOp(up.op, up.dtype, tuple(rewrite(x) for x in up.src), up.arg)
-        return self.nodes.setdefault(up.tuple(), up) # replace with cached nodes
       sink = rewrite(sink)
       run_cnt += 1
       assert run_cnt < 100, "exceeded 100 rewrite loops!"
