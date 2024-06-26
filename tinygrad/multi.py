666a9c1448d5c4916cd132f8d6601939c291f87f
David Hou <david.hou314@gmail.com>
2024-06-25 20:19:09 -0700

    don't view origin buffer when sharding (#5122)
    * make buffer view optional with a flag
    
    * do not view when sharding to save memory

--- tinygrad/multi.py
+++ tinygrad/multi.py
@@ -73,7 +73,7 @@ class MultiLazyBuffer:
   def from_sharded(lb:LazyBuffer, devices:Tuple[str, ...], axis:Optional[int]=None):
     lbs = [lb.contiguous() if lb.base != lb and not lb.is_unrealized_unmasked_const() else lb] * len(devices)
     sharded_lbs = [lb.copy_to_device(d) for lb,d in zip(to_sharded(lbs, axis) if axis is not None else lbs, devices)]
-    return MultiLazyBuffer([lb if lb.is_unrealized_unmasked_const() else lb.contiguous() for lb in sharded_lbs], axis)
+    return MultiLazyBuffer([lb if lb.is_unrealized_unmasked_const() else lb.contiguous(allow_buffer_view=False) for lb in sharded_lbs], axis)
 
   def copy_to_device(self, device:str) -> LazyBuffer:
     if self.axis is None:
