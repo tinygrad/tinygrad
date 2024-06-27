315606789efdff1e806fb6af9595927228b57d6b
reddyn12 <nikhilr.ssm@gmail.com>
2024-06-26 21:59:22 -0400

    mvp

--- extra/models/resnet.py
+++ extra/models/resnet.py
@@ -147,14 +147,9 @@ class ResNet:
         print("skipping fully connected layer")
         continue # Skip FC if transfer learning
 
-      if dat.shape == ():
-        assert obj.shape == (1,), obj.shape
-        dat = dat.reshape(1)
-      if len(obj.shape)==2 and len(dat.shape)==1 and obj.shape[1]==dat.shape[0]:
-        dat = dat.reshape(1, obj.shape[1])
-        dat = np.broadcast_to(dat, obj.shape)
-      assert obj.shape == dat.shape, (k, obj.shape, dat.shape)
-      obj.assign(dat)
+      if 'bn' not in k and 'downsample' not in k: assert obj.shape == dat.shape, (k, obj.shape, dat.shape)
+      elif len(obj.shape)==2: dat = np.broadcast_to(dat, obj.shape)
+      obj.assign(dat.reshape(obj.shape))
 
 ResNet18 = lambda num_classes=1000: ResNet(18, num_classes=num_classes)
 ResNet34 = lambda num_classes=1000: ResNet(34, num_classes=num_classes)
