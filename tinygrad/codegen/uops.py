d25f37cb33cdc533a0cd529a65778066c2af8d5e
Mesozoic Egg <mesozoic.egg@proton.me>
2024-06-28 14:22:31 +0800

    loop collapse should return flag for further processing

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -163,7 +163,7 @@ def loop_collapse(loop_start, loop_end, compval, idx, mval, multconst, rng):
     if DEBUG >= 1: print(f"WARNING, NOT FOLDING: mval:{mval.arg} loop_start:{loop_start.arg}")
     return None
   comprange = UOp.min(loop_end, UOp.max(UOp.alu(BinaryOps.IDIV, idx-compval-mval, mval) + (loop_end-loop_start), loop_start))
-  return UOp(UOps.UNMUL, multconst.dtype, (comprange.cast(multconst.dtype) * multconst, loop_end-loop_start))
+  return UOp(UOps.UNMUL, multconst.dtype, (comprange.cast(multconst.dtype) * multconst, loop_end-loop_start)), True
 
 
 # this is symbolic 2.0
