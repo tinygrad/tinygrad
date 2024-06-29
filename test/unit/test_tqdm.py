3d959b873d332f56c0c415b9fbc2eb0badb6179d
chenyu <chenyu@fastmail.com>
2024-06-29 15:33:02 -0400

    tqdm add back unit scale e2e test
    no longer flaky after unit scale fix. it was removed
    https://github.com/tinygrad/tinygrad/pull/4846/commits/d392cc931cdf811f47894cb571b42cfe117ccd15

--- test/unit/test_tqdm.py
+++ test/unit/test_tqdm.py
@@ -185,6 +185,43 @@ class TestProgressBar(unittest.TestCase):
         tqdm_output = tqdm.format_meter(n=n, total=0, elapsed=elapsed, ncols=ncols, prefix="Test")
         self.assertEqual(tinytqdm_output, tqdm_output)
 
+  @patch('sys.stderr', new_callable=StringIO)
+  @patch('shutil.get_terminal_size')
+  @patch('time.perf_counter')
+  def test_tqdm_output_custom_e2e_unit_scale(self, mock_perf_counter, mock_terminal_size, mock_stderr):
+    for _ in range(30):
+      total, ncols, dt = random.randint(10, 100000), random.randint(80, 120), 0.1
+
+      def time_gen():
+        cnt = 0
+        while True:
+          yield dt * cnt
+          cnt += 1
+
+      mock_perf_counter.side_effect = time_gen()
+      mock_terminal_size.return_value = namedtuple(field_names='columns', typename='terminal_size')(ncols)
+      mock_stderr.truncate(0)
+      n = 0
+      i = 1
+
+      bar = tinytqdm(total=total, desc="Test", unit="B", unit_scale=True)
+      tinytqdm_output = mock_stderr.getvalue().split("\r")[-1].rstrip()
+      tqdm_output = tqdm.format_meter(n=n, total=total, unit="B", unit_scale=True, elapsed=i*dt, ncols=ncols, prefix="Test")
+      self._compare_bars(tinytqdm_output, tqdm_output)
+
+      while n < total:
+        i += 1
+        incr = (total // 10) + random.randint(0, 100)
+        if n + incr > total: incr = total - n
+        bar.update(incr, close=n+incr==total)
+        n += incr
+        if bar.i % bar.skip != 0: continue
+
+        tinytqdm_output = mock_stderr.getvalue().split("\r")[-1].rstrip()
+        tqdm_output = tqdm.format_meter(n=n, total=total, unit="B", unit_scale=True, elapsed=i*dt, ncols=ncols, prefix="Test")
+        print(f"{tinytqdm_output}\n{tqdm_output}\n")
+        self._compare_bars(tinytqdm_output, tqdm_output)
+
   def test_tqdm_perf(self):
     st = time.perf_counter()
     for _ in tqdm(range(100)): time.sleep(0.01)
