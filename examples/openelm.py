62e29d9130519add963e309a04c316d447759ca8
George Hotz <geohot@gmail.com>
2024-06-30 14:21:26 -0700

    output is good now

--- examples/openelm.py
+++ examples/openelm.py
@@ -1,8 +1,7 @@
 import json, pprint
-from typing import Optional
 from tinygrad import fetch, nn, Tensor
+from tinygrad.helpers import DEBUG
 from extra.models.llama import RMSNorm    # TODO: move to nn
-from extra.models.llama import apply_rotary_emb, precompute_freqs_cis
 
 class FeedForward:
   def __init__(self, model_dim, intermediate_dim):
@@ -14,6 +13,14 @@ class FeedForward:
     y_1, y_2 = y_12.chunk(2, dim=-1)
     return self.proj_2(y_1.silu() * y_2)
 
+# NOTE: this RoPE doesn't match LLaMA's?
+def _rotate_half(x: Tensor) -> Tensor:
+  x1, x2 = x.chunk(2, dim=-1)
+  return Tensor.cat(-x2, x1, dim=-1)
+
+def _apply_rotary_pos_emb(x: Tensor, pos_sin: Tensor, pos_cos: Tensor) -> Tensor:
+  return (x * pos_cos) + (_rotate_half(x) * pos_sin)
+
 class Attention:
   def __init__(self, model_dim, num_query_heads, num_kv_heads, head_dim):
     self.qkv_proj = nn.Linear(model_dim, (num_query_heads + num_kv_heads*2) * head_dim, bias=False)
@@ -22,28 +29,32 @@ class Attention:
     self.q_norm = RMSNorm(head_dim)
     self.k_norm = RMSNorm(head_dim)
     self.out_proj = nn.Linear(num_query_heads * head_dim, model_dim, bias=False)
-    self.freqs_cis: Optional[Tensor] = None
 
   def __call__(self, x:Tensor) -> Tensor:
     start_pos = 0
     batch_size, seq_len, embed_dim = x.shape
     qkv = self.qkv_proj(x)
-    qkv = qkv.reshape(batch_size, seq_len, self.num_query_heads+self.num_kv_heads*2, self.head_dim) #.transpose(1, 2)
-    xq,xk,xv = qkv.split([self.num_query_heads, self.num_kv_heads, self.num_kv_heads], dim=2)
+    qkv = qkv.reshape(batch_size, seq_len, self.num_query_heads+self.num_kv_heads*2, self.head_dim).transpose(1, 2)
+    xq,xk,xv = qkv.split([self.num_query_heads, self.num_kv_heads, self.num_kv_heads], dim=1)
     xq = self.q_norm(xq)
     xk = self.k_norm(xk)
 
-    # Add positional embedding (NOTE: jit avoid independent would avoid this None hack)
-    if self.freqs_cis is None: self.freqs_cis = precompute_freqs_cis(self.head_dim, 4096, 10000, dtype=xq.dtype) # rope_max_length, rope_freq_constant
-    xq, xk = apply_rotary_emb(xq, xk, self.freqs_cis.shrink((None, (start_pos, start_pos+seq_len), None, None, None)))
+    # add positional embedding
+    freq_constant = 10000
+    inv_freq = 1.0 / (freq_constant ** (Tensor.arange(0, self.head_dim, 2) / self.head_dim))
+    pos_index_theta = Tensor.einsum("i,j->ij", Tensor.arange(seq_len), inv_freq)
+    emb = Tensor.cat(pos_index_theta, pos_index_theta, dim=-1)
+    cos_emb, sin_emb = emb.cos()[None, None, :, :], emb.sin()[None, None, :, :]
+    xq = _apply_rotary_pos_emb(xq, sin_emb, cos_emb)
+    xk = _apply_rotary_pos_emb(xk, sin_emb, cos_emb)
 
     # grouped-query attention
     num_groups = self.num_query_heads // self.num_kv_heads
-    xk = xk.repeat_interleave(num_groups, dim=2)
-    xv = xv.repeat_interleave(num_groups, dim=2)
+    xk = xk.repeat_interleave(num_groups, dim=1)
+    xv = xv.repeat_interleave(num_groups, dim=1)
 
     mask = Tensor.full((1, 1, seq_len, start_pos+seq_len), float("-inf"), dtype=xq.dtype, device=xq.device).triu(start_pos+1)
-    attn_output = xq.transpose(1, 2).scaled_dot_product_attention(xk.transpose(1, 2), xv.transpose(1, 2), mask).transpose(1, 2)
+    attn_output = xq.scaled_dot_product_attention(xk, xv, mask).transpose(1, 2)
     attn_output = attn_output.reshape(batch_size, seq_len, self.num_query_heads * self.head_dim)
     return self.out_proj(attn_output)
 
@@ -67,7 +78,7 @@ def make_divisible(v, divisor):
 
 class Transformer:
   def __init__(self, cfg):
-    #pprint.pp(cfg)
+    if DEBUG >= 3: pprint.pp(cfg)
     self.layers = [Layer(cfg['model_dim'], make_divisible(int(cfg["model_dim"] * cfg['ffn_multipliers'][i]), cfg['ffn_dim_divisor']),
                          cfg['num_query_heads'][i], cfg['num_kv_heads'][i], cfg['head_dim']) for i in range(cfg['num_transformer_layers'])]
     self.norm = RMSNorm(cfg['model_dim'])
@@ -84,7 +95,8 @@ if __name__ == "__main__":
   model_name = "OpenELM-270M"  # fp32?
   model = Transformer(json.loads(fetch(f"https://huggingface.co/apple/{model_name}/resolve/main/config.json?download=true").read_bytes()))
   weights = nn.state.safe_load(fetch(f"https://huggingface.co/apple/{model_name}/resolve/main/model.safetensors?download=true"))
-  #for k, v in weights.items(): print(k, v.shape)
+  if DEBUG >= 3:
+    for k, v in weights.items(): print(k, v.shape)
   nn.state.load_state_dict(model, {k.removeprefix("transformer."):v for k,v in weights.items()})
 
   from sentencepiece import SentencePieceProcessor
@@ -95,12 +107,10 @@ if __name__ == "__main__":
     out = model(ttoks).realize()
     t0 = out[0].argmax(axis=-1).tolist()
     toks.append(t0[-1])
-    print(tokenizer.decode(t0))
+    # hmmm...passthrough still doesn't match
     print(tokenizer.decode(toks))
-    print(t0)
-    print(toks)
-    #print(t0, toks, tokenizer.decode(toks))
-
-    #print(toks, tokenizer.decode(toks))
+    #print(tokenizer.decode(t0))
+    #print(t0)
+    #print(toks)
 
 
