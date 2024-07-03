24fc2f8d75db6f9a7a94d675e1968a38fd0fc633
neilfoxy <neilthenerd1@gmail.com>
2024-07-02 23:20:24 -0500

    Fix indentation issues

--- tinygrad/linear_with_named_parameters.py
+++ tinygrad/linear_with_named_parameters.py
@@ -1,7 +1,6 @@
 from tinygrad.tensor import Tensor
 import math
-
-class Linear:
+  class Linear:
     def __init__(self, in_features, out_features, bias=True):
         self.weight = Tensor.kaiming_uniform(out_features, in_features, a=math.sqrt(5))
         self.weight.requires_grad = True  # Ensure weight requires gradients
@@ -9,11 +8,9 @@ class Linear:
         self.bias = Tensor.uniform(out_features, low=-bound, high=bound) if bias else None
         if self.bias is not None:
             self.bias.requires_grad = True  # Ensure bias requires gradients
-
-    def __call__(self, x: Tensor):
+      def __call__(self, x: Tensor):
         return x.linear(self.weight.transpose(), self.bias)
-
-    def named_parameters(self):
+      def named_parameters(self):
         params = [('weight', self.weight)]
         if self.bias is not None:
             params.append(('bias', self.bias))
