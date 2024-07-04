d8f776d3c6f5df0931c0eca69e4ff0dac6be8f8e
DKormann <dominikormann@outlook.com>
2024-07-05 01:04:30 +0200

    init

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -333,7 +333,7 @@ def get_children_dfs(u:UOp, children:Dict[UOp, List[UOp]], in_degree:Dict[UOp, i
     children[x].append(u)
   in_degree[u] = len(u.src)
 
-def graph_rewrite(sink:UOp, pm:PatternMatcher) -> UOp:
+def graph_rewrite(sink:UOp, pm:PatternMatcher=constant_folder) -> UOp:
   nodes: Dict[Tuple, UOp] = {}
   replace: Dict[UOp, UOp] = {}
   def __inner_rewrite(n:UOp) -> UOp:
@@ -369,7 +369,7 @@ class UOpGraph:
     for i,u in enumerate(self):
       print(f"{i:4d} {str(u.op):20s}: {str(u.dtype) if u.dtype is not None else '':25s} " f"{str([self.uops.index(x) for x in u.src]):32s} {u.arg}")
 
-  def linearize(self, extra_pm:Optional[PatternMatcher]=None, do_type_verify=True):
+  def linearize(self, extra_pm:Optional[List]=None, do_type_verify=True):
     # NOTE: relinearizering should be okay
     #assert self._uops is None, "already linearized"
 
@@ -386,8 +386,7 @@ class UOpGraph:
     sink = UOp(UOps.SINK, None, tuple(self.sinks))
 
     # dedup all nodes and do graph rewrite
-    sink = graph_rewrite(sink, constant_folder)
-    if extra_pm: sink = graph_rewrite(sink, PatternMatcher(constant_folder.patterns+extra_pm.patterns))
+    sink = graph_rewrite(sink, PatternMatcher(constant_folder.patterns + extra_pm) if extra_pm else constant_folder)
 
     # filter nodes that don't link to a sink
     # BFS toposort
