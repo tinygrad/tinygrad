7d1937d7c3d0952d70669f0f71ec1e019e8ef980
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-07-04 12:14:01 +0000

    mpre comments

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -332,15 +332,17 @@ def create_schedule(outs:List[LazyBuffer], seen:Optional[Set[LazyBuffer]]=None) 
 
 # *** memory planning ***
 
-def _internal_memory_planner(buffers:List[Union[List[Buffer], Tuple[Buffer, ...]]], debug_prefix="", keep_buffers=None) -> Dict[Buffer, Buffer]:
+def _internal_memory_planner(buffers:List[Union[List[Buffer], Tuple[Buffer, ...]]], noopt_buffers=None, debug_prefix="") -> Dict[Buffer, Buffer]:
   if getenv("NO_MEMORY_PLANNER"): return {}
   first_appearance, last_appearance = {}, {}
   for i,u in enumerate(buffers):
     for buf in u:
-      if buf.is_allocated() or buf.lb_refcount > 0 or (keep_buffers is not None and buf.base in keep_buffers): continue
+      if buf.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers): continue
       if buf.base not in first_appearance: first_appearance[buf.base] = i
       last_appearance[buf.base] = i
 
+  # Sort buffers by size in descending order, prioritizing largest buffers for allocation first.
+  # Track free segments, each containing (start, stop, and buffer that could be reused on this segment).
   free_segments: Dict[Tuple, List[Tuple[int, int, Buffer]]] = defaultdict(list) # Dict[buffer key, Tuple[start, end, buffer to reuse on the seg]]
   def find_replace_buffer(buf, st, en):
     seg_st, seg_en, seg_buf = 0, len(buffers) - 1, buf # return the buffer itself if the replace one is not found.
@@ -361,7 +363,7 @@ def _internal_memory_planner(buffers:List[Union[List[Buffer], Tuple[Buffer, ...]
 
   for i,u in enumerate(buffers):
     for buf in u:
-      if buf.is_allocated() or buf.lb_refcount > 0 or (keep_buffers is not None and buf.base in keep_buffers): continue
+      if buf.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers): continue
       if buf._base is not None: assigned[buf] = Buffer(buf.device, buf.size, buf.dtype, base=assigned.get(buf.base, buf.base).base, offset=buf.offset)
       else: assigned[buf] = assigned.get(buf, buf)
 
@@ -371,6 +373,6 @@ def _internal_memory_planner(buffers:List[Union[List[Buffer], Tuple[Buffer, ...]
   return assigned
 
 def memory_planner(schedule:List[ScheduleItem]) -> List[ScheduleItem]:
-  # Do not optimize buffers which are copies (or similar) to keep parallelism level.
-  assigned = _internal_memory_planner([si.bufs for si in schedule], keep_buffers=[b for si in schedule if si.ast[0].op in LoadOps for b in si.bufs])
+  # Exclude buffers involved in load ops (e.g transfers) to preserve parallelism in graphs.
+  assigned = _internal_memory_planner([si.bufs for si in schedule], noopt_buffers=[b for si in schedule if si.ast[0].op in LoadOps for b in si.bufs])
   return [ScheduleItem(si.ast, tuple(assigned.get(x, x) for x in si.bufs)) for si in schedule]
