de9281523d98358e7504d447459fc1fc71243360
hikettei <hikettei@gmail.com>
2024-07-09 12:58:07 +0900

    TRANSCENDENTAL as a ContextVar, removed old test of cody waite reduction, added assertions, et al.

--- test/test_transcendental.py
+++ test/test_transcendental.py
@@ -1,36 +1,31 @@
 import unittest
-from tinygrad import dtypes
 from tinygrad.tensor import Tensor
-from test.helpers import is_dtype_supported
+from tinygrad.helpers import Context
 from test.test_schedule import check_schedule
 
 class TestTranscendentalSchedule(unittest.TestCase):
-  # w/o payne_hanek_reduction (fp16)
-  @unittest.skipUnless(is_dtype_supported(dtypes.half), "need half")
-  def test_transcendental_sin_fp16_fusion(self):
-    a = Tensor.empty(10, dtype=dtypes.float16)
-    b = Tensor.empty(10, dtype=dtypes.float16)
-    c = a.sin() + b.sin()
-    c = c.sin()
-    check_schedule(c, 1)
   # w/ payne_hanek_reduction (fp32)
   def test_transcendental_sin_fusion(self):
-    a = Tensor.empty(10)
-    b = Tensor.empty(10)
-    c = a.sin() + b.sin()
-    c = c.sin()
-    check_schedule(c, 1)
+    with Context(TRANSCENDENTAL=2):
+      a = Tensor.empty(10)
+      b = Tensor.empty(10)
+      c = a.sin() + b.sin()
+      c = c.sin()
+      check_schedule(c, 1)
 
   def test_transcendental_log2_fusion(self):
-    a = Tensor.empty(10)
-    b = Tensor.empty(10)
-    c = a.log2() + b.log2()
-    c = c.log2()
-    check_schedule(c, 1)
+    with Context(TRANSCENDENTAL=2):
+      a = Tensor.empty(10)
+      b = Tensor.empty(10)
+      c = a.log2() + b.log2()
+      c = c.log2()
+      check_schedule(c, 1)
 
   def test_transcendental_exp2_fusion(self):
-    a = Tensor.empty(10)
-    b = Tensor.empty(10)
-    c = a.exp2() + b.exp2()
-    c = c.exp2()
-    check_schedule(c, 1)
+    with Context(TRANSCENDENTAL=2):
+      a = Tensor.empty(10)
+      b = Tensor.empty(10)
+      c = a.exp2() + b.exp2()
+      c = c.exp2()
+      check_schedule(c, 1)
+
--- tinygrad/transcendental.py
+++ tinygrad/transcendental.py
@@ -241,6 +241,7 @@ def xsin(d:LazyBuffer, fast:bool=False, switch_over:float=39800.0) -> LazyBuffer
   - fast=True assumes x <= switch_over.
   - switch_over is the threshold for switching to payne_hanek_reduction.
   """
+  assert is_dtype_transcendental_supported(d.dtype)
   if 0 in d.shape: return d
   if d.dtype == dtypes.float16:
     switch_over = 9500.0
@@ -269,6 +270,7 @@ def xexp2(x:LazyBuffer) -> LazyBuffer:
   Implements a 1.0 ULP approximation for UnaryOps.EXP2
   - Paper: https://arxiv.org/pdf/2001.09258
   """
+  assert is_dtype_transcendental_supported(x.dtype)
   if 0 in x.shape: return x
   fp64_p = x.dtype == dtypes.float64
   # mask +=inf/nan as zero.
@@ -299,6 +301,7 @@ def xlog2(d:LazyBuffer) -> LazyBuffer:
   Implements a 1.0 ULP approximation for UnaryOps.LOG2
   Paper: https://arxiv.org/pdf/2001.09258
   """
+  assert is_dtype_transcendental_supported(d.dtype)
   if 0 in d.shape: return d
   fp64_p = d.dtype == dtypes.float64
   FLT_MIN = d.const(1e-6 if d.dtype == dtypes.float16 else 1e-4)
