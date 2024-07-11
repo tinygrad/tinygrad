9f0b04ad75c5dc2a8bfc035fd286987b14a5b69a
qazal <qazal.software@gmail.com>
2024-07-11 22:35:30 +0300

    test_var_multireduce

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -9,7 +9,7 @@ from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.codegen.lowerer import get_grouped_dims
 from tinygrad.codegen.uops import UOp, UOps
 from tinygrad.device import Device, Buffer
-from tinygrad.ops import BinaryOps, BufferOps, MemBuffer, ConstBuffer, LazyOp, LoadOps, TernaryOps, ReduceOps, UnaryOps
+from tinygrad.ops import BinaryOps, BufferOps, MemBuffer, ConstBuffer, LazyOp, LoadOps, TernaryOps, ReduceOps, UnaryOps, verify_lazyop
 from tinygrad.renderer import TensorCore
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.view import View
@@ -103,7 +103,6 @@ class TestLinearizer(unittest.TestCase):
     assert [u.arg[0] for u in mutable_bufs] == [0, 1]
 
   @unittest.skipIf(CI and Device.DEFAULT == "AMD", "remu doesn't have multiple wave syncs yet")
-  @unittest.skip("still wrong")
   def test_var_multireduce(self):
     Tensor.manual_seed(0)
     x = Tensor.randn(3, 27, 32).realize()
@@ -117,7 +116,11 @@ class TestLinearizer(unittest.TestCase):
     squares = (second_x-mean)*(second_x-mean)
     squares_sum = LazyOp(ReduceOps.SUM, (squares,), (2,))
     store = LazyOp(BufferOps.STORE, (squares_sum,), MemBuffer(0, dtypes.float, ShapeTracker.from_shape((3, 27, 1, 1))))
-    helper_linearizer_ast((store, ), [x])
+    # verify_lazyop is correct here
+    verify_lazyop(store)
+    # TODO: fixup_ast merges the wrong axes
+    with self.assertRaises(Exception):
+      helper_linearizer_ast((store, ), [x])
     # tinygrad ref
     y_tiny = x.var(axis=2, correction=0)
     np.testing.assert_allclose(y_tiny.numpy(), x.numpy().var(axis=2, ddof=0), atol=1e-4, rtol=1e-4)
