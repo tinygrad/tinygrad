9712d9ffb66ea6ce8ddd13c2afb1d2da6a00d3b5
qazal <77887910+Qazalin@users.noreply.github.com>
2024-07-12 07:09:12 +0800

    pass lowering errors if not asserting process replay (#5395)
    * pass lowering errors if not asserting process replay
    
    * ProcessReplayError

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -4,6 +4,8 @@ import difflib, pickle
 from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.helpers import Context, ContextVar, colored, db_connection, VERSION, getenv, tqdm
 
+class ProcessReplayError(Exception): pass
+
 page_size = 100
 conn = db_connection()
 cur = conn.cursor()
@@ -13,15 +15,23 @@ for offset in tqdm(range(0, row_count, page_size)):
   for row in cur.fetchall():
     ast, opts, applied_opts, name, compare_src, ctx = pickle.loads(row[0])
     with Context(**{k:v for k,v in ctx.items() if k in ContextVar._cache}):
-      k = Linearizer(*ast, opts=opts)
-      for opt in applied_opts: k.apply_opt(opt)
-      good_src = k.opts.render(name, k.linearize().uops)
-      try: assert compare_src == good_src
-      except AssertionError as e:
-        print("PROCESS REPLAY DETECTED CHANGE")
+      try:
+        k = Linearizer(*ast, opts=opts)
+        for opt in applied_opts: k.apply_opt(opt)
+        good_src = k.opts.render(name, k.linearize().uops)
+        try: assert compare_src == good_src
+        except AssertionError:
+          print("PROCESS REPLAY DETECTED CHANGE")
+          print(ast)
+          print(applied_opts)
+          diff = list(difflib.unified_diff(good_src.splitlines(), compare_src.splitlines()))
+          for line in diff:
+            print(colored(line, "red" if line.startswith("-") else "green" if line.startswith("+") else None))
+          if getenv("ASSERT_PROCESS_REPLAY", 1): raise ProcessReplayError()
+      except ProcessReplayError as e: raise e
+      except Exception as e:
+        print("FAILED TO RECREATE KERNEL")
         print(ast)
         print(applied_opts)
-        diff = list(difflib.unified_diff(good_src.splitlines(), compare_src.splitlines()))
-        for line in diff:
-          print(colored(line, "red" if line.startswith("-") else "green" if line.startswith("+") else None))
+        print(e)
         if getenv("ASSERT_PROCESS_REPLAY", 1): raise e
