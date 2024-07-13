64cb9e0ea10d683f5955a47f12ddcd8ceb71cc11
David González Martínez <me@davidgonmar.me>
2024-07-14 01:05:19 +0200

    dont assert throws on test'

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -242,7 +242,6 @@ class TestUint8DType(TestDType):
 @unittest.skipIf(Device.DEFAULT == "WEBGL", "No bitcast on WebGL")
 class TestBitCast(unittest.TestCase):
   def test_shape_change_bitcast(self):
-    with self.assertRaises(RuntimeError):
       _test_bitcast(Tensor([100000], dtype=dtypes.float32), dtypes.uint8, [100000])
 
   def test_bitcast_float_to_int32(self):
