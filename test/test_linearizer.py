1cd993279bc3e5a1599f93943145b7b2030317a3
qazal <qazal.software@gmail.com>
2024-07-15 20:14:09 +0300

    skip arange expanded too

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -123,6 +123,7 @@ class TestLinearizer(unittest.TestCase):
     np.testing.assert_allclose(y_tiny.numpy(), wanna_output, atol=1e-4, rtol=1e-4)
 
   # *** buildup to fused indexing
+  @unittest.skipIf(CI and Device.DEFAULT in {"PTX", "AMD", "NV"}, "very slow")
   def test_arange_expanded(self):
     # Tensor.arange(16384) expanded such that output shape is (4, 16384, 256, 1)
     # basically it's pushing the expand through this reduce:
