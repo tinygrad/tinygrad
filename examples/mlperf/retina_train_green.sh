235d9c2f028cb0e0f9aefa4319582c37b245f3fb
reddyn12 <nikhilr.ssm@gmail.com>
2024-07-15 22:29:36 +0000

    make scripts for red and green

--- examples/mlperf/retina_train_green.sh
+++ examples/mlperf/retina_train_green.sh
@@ -8,6 +8,9 @@ export TRAIN_BEAM=3 IGNORE_JIT_FIRST_BEAM=1 BEAM_UOPS_MAX=1500 BEAM_UPCAST_MAX=6
 export DATAPATH='/raid/datasets/open-images'
 export WANDB=1
 
+# Download dataset if missing
+python3 extra/datasets/openimages.py
+
 CUDA=1 TRAIN_ONLY=1 python3 examples/mlperf/model_train.py
 
 CUDA=1 EVAL_ONLY=1 python3 examples/mlperf/model_train.py
