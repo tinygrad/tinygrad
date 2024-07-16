86a7aa8b376456271eebfeb129163c97703a85d6
DKormann <dominikormann@outlook.com>
2024-07-16 16:10:06 +0200

    min diff

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -91,7 +91,12 @@ class LazyOp:
   def __add__(self, x:LazyOp): return LazyOp(BinaryOps.ADD, (self, x))
   def __sub__(self, x:LazyOp): return LazyOp(BinaryOps.ADD, (self, -x))
   def __mul__(self, x:LazyOp): return LazyOp(BinaryOps.MUL, (self, x))
+  def ne(self, x:LazyOp): return LazyOp(BinaryOps.CMPNE, (self, x))
+  def eq(self, x:LazyOp): return -self.ne(x)
   def __neg__(self): return LazyOp(UnaryOps.NEG, (self,))
+  @staticmethod
+  def const(val, dtype:DType, shape:Tuple[sint, ...]):
+    return LazyOp(BufferOps.CONST, (), ConstBuffer(val, dtype, ShapeTracker.from_shape(()).reshape((1,)*len(shape)).expand(shape)))
 
 # **************** independent FlopCounter ****************
 
