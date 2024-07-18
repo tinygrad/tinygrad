5c4cdb168bf67a9a517f06a1e7f34cc51cf56494
DKormann <dominikormann@outlook.com>
2024-07-18 15:19:35 +0200

    fix

--- test/test_pattern_matcher.py
+++ test/test_pattern_matcher.py
@@ -176,7 +176,8 @@ class TestPatternMatcher(TestUOps):
     self.assert_equiv_uops(e3, uops.uops[2])
 
   def _assert_eq_upat(self, a:UPat, b:UPat):
-    assert (sorted(a.op), sorted(a.dtype), a.arg, a.allow_any_len) == (sorted(b.op), sorted(b.dtype), b.arg, b.allow_any_len)
+    assert (sorted(map(str,a.op)) if a.op else [] == (sorted(map(str,b.op)) if b.op else []))
+    assert (sorted(a.dtype) if a.dtype else [] == (sorted(b.dtype) if b.dtype else []))
     assert (a.name, type(a.src)) == (b.name, type(b.src))
     def simple_src(u:UPat):
       if u.src is None: return []
