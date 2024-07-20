4b42b7e9811a48829045f87cf91d205a690e789b
George Hotz <geohot@gmail.com>
2024-07-19 21:29:52 -0700

    mcts in extra

--- examples/handcode_opt.py
+++ examples/handcode_opt.py
@@ -1,12 +1,12 @@
 from typing import List
 from extra.models.resnet import ResNet50
+from extra.mcts_search import mcts_search
 from examples.mlperf.helpers import get_mlperf_bert_model
 from tinygrad import Tensor, Device, dtypes, nn
 from tinygrad.codegen.kernel import Kernel
 from tinygrad.device import Compiled
 from tinygrad.engine.schedule import create_schedule
 from tinygrad.engine.search import time_linearizer, beam_search, bufs_from_lin
-from tinygrad.engine.mcts import mcts_search
 from tinygrad.helpers import DEBUG, ansilen, getenv
 from tinygrad.ops import MetaOps
 from tinygrad.shape.symbolic import sym_infer
