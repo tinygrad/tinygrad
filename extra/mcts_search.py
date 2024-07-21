3b376047e3141aa16f349dee6a8b629c5009eb49
George Hotz <geohot@gmail.com>
2024-07-20 20:36:06 -0700

    usable graph

--- extra/mcts_search.py
+++ extra/mcts_search.py
@@ -10,6 +10,7 @@ class MCTSNode:
     self.kernel = kernel
     self.t = math.inf
     self.n = 0
+    self.tm = math.inf
     self.i = -1
     self.parents: List[MCTSNode] = [parent] if parent is not None else []
     self.children: Optional[List[MCTSNode]] = None
@@ -19,8 +20,10 @@ def expand_node(node:MCTSNode):
   node.children = [MCTSNode(x, node) for x in get_kernel_actions(node.kernel, include_0=False).values()]
   random.shuffle(node.children)
 
+graph_mcts_cnt = 0
 C = math.sqrt(2)
 def mcts_search(lin:Kernel, rawbufs:List[Buffer], amt:int) -> Kernel:
+  global graph_mcts_cnt
   # TODO: copied from BEAM
   key = {"ast": lin.ast.key, "amt": amt, "device": lin.opts.device, "suffix": lin.opts.suffix}
   if not getenv("IGNORE_MCTS_CACHE") and CACHELEVEL >= 1 and (val:=diskcache_get("mcts_search", key)) is not None:
@@ -64,7 +67,7 @@ def mcts_search(lin:Kernel, rawbufs:List[Buffer], amt:int) -> Kernel:
     else:
       p, lib, _ = compile_ret
       if (sibling_node:=seen_libs.get(lib, None)) is not None:
-        # TODO: add parents to sibling_node?
+        # TODO: add parents to sibling_node? it was forming cycles
         remove_node(node)
         tm = sibling_node.t
       else:
@@ -72,6 +75,7 @@ def mcts_search(lin:Kernel, rawbufs:List[Buffer], amt:int) -> Kernel:
         try: tm = statistics.median(_time_program(p, lib, var_vals, rawbufs, cnt=5, early_stop=best_tm*10/1e6))*1e6
         except RuntimeError:
           tm = math.inf
+        node.tm = tm
 
     if tm < best_tm: best, best_idx, best_tm = node.kernel, i, tm
     if DEBUG>=2: print(f"\r{time.perf_counter() - st:7.2f}s: {tm:12.2f} us     best: {best_tm:12.2f} us @ {best_idx+1:4d}        {i+1:4d}/{amt:4d}         {node.kernel.colored_shape()}\033[K", end="")  # noqa: E501
@@ -93,12 +97,12 @@ def mcts_search(lin:Kernel, rawbufs:List[Buffer], amt:int) -> Kernel:
       for parent in node.parents: G.add_edge(parent, node)
       gopts = node.kernel.applied_opts
       edge_lbl = f"{str(gopts[-1].op)[7:]} {gopts[-1].axis} {gopts[-1].amt}" if len(gopts) else "ROOT"
-      G.add_node(node, label=f"{node.i}\n{edge_lbl}\nt {node.t:.2f}\nn {node.n}", fillcolor="#80ff8080", style='filled' if node.t == best_tm else '')
+      G.add_node(node, label=f"{node.i}\n{node.tm:.2f} us\n{edge_lbl}\nt {node.t:.2f}\nn {node.n}", fillcolor="#80ff8080", style='filled' if node.t == best_tm else '')
       if node.children is not None:
         for child in node.children: add_node(child)
     add_node(root)
-    save_graph(G, f"{GRAPHPATH}.mcts", '-Grankdir=LR')
-    exit(0)
+    save_graph(G, f"{GRAPHPATH}.{graph_mcts_cnt}.mcts", '-Grankdir=LR')
+    graph_mcts_cnt += 1
 
   if CACHELEVEL >= 1: diskcache_put("mcts_search", key, best.applied_opts)
   return best
