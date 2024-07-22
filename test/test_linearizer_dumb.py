f2d2afdaa4084d00e7b4cde4f9f16975e768c1da
chenyu <chenyu@fastmail.com>
2024-07-22 18:37:26 -0400

    dumb linearizer example that max is not simplified (#5644)
    * dumb linearizer example that max is not simplified
    
    this might just get fix once basic mod simplification is done
    
    * need local

--- test/test_linearizer_dumb.py
+++ test/test_linearizer_dumb.py
@@ -32,5 +32,28 @@ class TestLinearizerDumb(unittest.TestCase):
     prg.uops.print()
     print(prg.src)
 
+  @unittest.skipUnless(Device[Device.DEFAULT].renderer.has_local, "need local")
+  def test_max_simplify_and_cancel(self):
+    ast = LazyOp(MetaOps.KERNEL, arg=None, src=(
+      LazyOp(BufferOps.STORE, arg=MemBuffer(idx=0, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1000, 1), strides=(1, 0), offset=0, mask=None, contiguous=True),))), src=(
+        LazyOp(BinaryOps.MUL, arg=None, src=(
+          LazyOp(UnaryOps.CAST, arg=dtypes.int, src=(
+            LazyOp(BinaryOps.CMPNE, arg=None, src=(
+              LazyOp(BinaryOps.CMPNE, arg=None, src=(
+                LazyOp(BufferOps.LOAD, arg=MemBuffer(idx=1, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(1000, 1), strides=(1, 0), offset=0, mask=None, contiguous=True),))), src=()),
+                LazyOp(BufferOps.LOAD, arg=MemBuffer(idx=2, dtype=dtypes.float, st=ShapeTracker(views=(View(shape=(1000, 1), strides=(0, 0), offset=0, mask=None, contiguous=False),))), src=()),)),
+              LazyOp(BufferOps.CONST, arg=ConstBuffer(val=True, dtype=dtypes.bool, st=ShapeTracker(views=(View(shape=(1000, 1), strides=(0, 0), offset=0, mask=None, contiguous=False),))), src=()),)),)),
+          LazyOp(BinaryOps.ADD, arg=None, src=(
+            LazyOp(ReduceOps.SUM, arg=(1,), src=(
+              LazyOp(BufferOps.CONST, arg=ConstBuffer(val=-1, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1001, 1999), strides=(0, 0), offset=0, mask=((0, 1001), (999, 1999)), contiguous=False), View(shape=(1000, 1000), strides=(1, 2000), offset=0, mask=None, contiguous=False)))), src=()),)),
+            LazyOp(BufferOps.CONST, arg=ConstBuffer(val=1000, dtype=dtypes.int, st=ShapeTracker(views=(View(shape=(1000, 1), strides=(0, 0), offset=0, mask=None, contiguous=False),))), src=()),)),)),)),))
+    opts = [Opt(op=OptOps.UNROLL, axis=0, amt=4), Opt(op=OptOps.LOCAL, axis=0, amt=8)]
+    k = Kernel(ast, opts=Device[Device.DEFAULT].renderer)
+    k.required_optimizations()
+    for opt in opts: k.apply_opt(opt)
+    prg = k.to_program()
+    prg.uops.print()
+    print(prg.src)
+
 if __name__ == '__main__':
   unittest.main()
