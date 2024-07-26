cea9f8af0ef12935b9f312fce0d8ef4528c2f7bc
chenyu <chenyu@fastmail.com>
2024-07-26 14:33:15 -0700

    UOp._min_max for MUL and MOD

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -110,6 +110,9 @@ class UOp:
         return self.const(-self.src[0].vmax.arg), self.const(-self.src[0].vmin.arg)
       if self.arg is BinaryOps.ADD:
         return self.const(self.src[0].vmin.arg+self.src[1].vmin.arg), self.const(self.src[0].vmax.arg+self.src[1].vmax.arg)
+      if self.arg is BinaryOps.MUL and self.src[0].vmin.arg >= 0 and self.src[1].vmin.arg >= 0:
+        return self.const(self.src[0].vmin.arg*self.src[1].vmin.arg), self.const(self.src[0].vmax.arg*self.src[1].vmax.arg)
+      if self.arg is BinaryOps.MOD and self.src[1].op is UOps.CONST: return self.const(0), self.const(self.src[1].arg-1)
     return None, None
 
 @dataclass(frozen=True, repr=False)  # reuse repr from UOp
