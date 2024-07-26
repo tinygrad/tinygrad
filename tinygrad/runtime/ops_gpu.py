9838c1a6ff895eefd64df90d06bedef276cf7651
chenyu <chenyu@fastmail.com>
2024-07-26 14:00:23 -0400

    update import style in runtime (#5735)

--- tinygrad/runtime/ops_gpu.py
+++ tinygrad/runtime/ops_gpu.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Tuple, Optional, List, cast
 import ctypes, functools, hashlib
-import tinygrad.runtime.autogen.opencl as cl
+from tinygrad.runtime.autogen import opencl as cl
 from tinygrad.helpers import init_c_var, to_char_p_p, from_mv, OSX, DEBUG
 from tinygrad.renderer.cstyle import OpenCLRenderer
 from tinygrad.device import BufferOptions, LRUAllocator, Compiled, Compiler, CompileError
