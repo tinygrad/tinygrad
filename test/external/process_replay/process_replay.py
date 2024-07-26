b837656aa9f6d10ddf37a163acd0bf4ed7644a72
qazal <qazal.software@gmail.com>
2024-07-26 21:26:20 +0300

    add kernel_changed

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -19,7 +19,7 @@ SKIP_PROCESS_REPLAY = (k:="[skip_process_replay]") in os.getenv("COMMIT_MESSAGE"
 early_stop = multiprocessing.Event()
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-def diff_kernel(offset:int, ref_schedule:List[LazyOp]):
+def diff_kernel(offset:int, ref_schedule:List[LazyOp], kernel_changed):
   if early_stop.is_set(): return
   conn = db_connection()
   cur = conn.cursor()
@@ -39,6 +39,7 @@ def diff_kernel(offset:int, ref_schedule:List[LazyOp]):
       logging.info(ast)
       logging.info(applied_opts)
       logging.info(e)
+      kernel_changed.value = True
       if ASSERT_DIFF: raise e
       continue
     # try compare
@@ -55,6 +56,7 @@ def diff_kernel(offset:int, ref_schedule:List[LazyOp]):
       diff = list(difflib.unified_diff(good_src.splitlines(), compare_src.splitlines()))
       for line in diff:
         logging.info(colored(line, "red" if line.startswith("-") else "green" if line.startswith("+") else None))
+      kernel_changed.value = True
       if ASSERT_DIFF: raise e
       if changed > MAX_DIFF_PCT:
         logging.warning(f"detected changes in over {MAX_DIFF_PCT}% of kernels. skipping further diff generation.")
@@ -109,10 +111,12 @@ def process_replay():
   conn.commit()
   cur.close()
   processes = []
+  changed = multiprocessing.Manager().Value('b', False)
   for i in tqdm(range(0, row_count, PAGE_SIZE)):
-    processes.append(p:=multiprocessing.Process(target=diff_kernel, args=(i, ref_schedule)))
+    processes.append(p:=multiprocessing.Process(target=diff_kernel, args=(i, ref_schedule, changed)))
     p.start()
   for p in processes: p.join()
+  if changed.value and ASSERT_DIFF: raise Exception("process replay detected changes")
 
 if __name__ == "__main__":
   if SKIP_PROCESS_REPLAY:
