1c007d3c8e023c300f3414b34f6806bf6617c766
qazal <qazal.software@gmail.com>
2024-07-26 22:23:54 +0300

    one ci job is fine

--- test/external/process_replay/test_process_replay.sh
+++ test/external/process_replay/test_process_replay.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # should assert
-sed -i '' 's/temp/temp1/g' ./tinygrad/codegen/lowerer.py
+sed -i 's/temp/temp1/g' ./tinygrad/codegen/lowerer.py
 ASSERT_PROCESS_REPLAY=1 python3 test/external/process_replay/process_replay.py &> /dev/null
 if [[ $? -eq 0 ]]; then
   echo "PROCESS REPLAY IS WRONG."
   exit 1
 fi
-
 # should NOT assert
 git stash > /dev/null
 ASSERT_PROCESS_REPLAY=1 python3 test/external/process_replay/process_replay.py &> /dev/null
-exit 0
-echo "process replay tests passed."
