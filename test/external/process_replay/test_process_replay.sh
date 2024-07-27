c40d38ee2d443abaf95f8fd1ff5df244eb4d4d49
qazal <qazal.software@gmail.com>
2024-07-27 11:58:02 +0300

    send to /dev/null

--- test/external/process_replay/test_process_replay.sh
+++ test/external/process_replay/test_process_replay.sh
@@ -2,7 +2,7 @@
 
 # should assert
 sed -i 's/temp/temp1/g' ./tinygrad/codegen/lowerer.py
-ASSERT_PROCESS_REPLAY=1 python3 test/external/process_replay/process_replay.py
+ASSERT_PROCESS_REPLAY=1 python3 test/external/process_replay/process_replay.py &> /dev/null
 if [[ $? -eq 0 ]]; then
   echo "PROCESS REPLAY IS WRONG."
   exit 1
