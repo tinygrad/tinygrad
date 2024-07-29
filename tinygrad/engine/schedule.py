dc1e2e145e731317f3e75e8e2cd4094da1332aa9
qazal <qazal.software@gmail.com>
2024-07-29 15:12:50 +0300

    exit_schedule

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -356,7 +356,7 @@ def create_schedule_with_vars(outs:List[LazyBuffer], seen:Optional[Set[LazyBuffe
   if any(degree != 0 for degree in in_degree.values()) or len(prescheduled) != len(schedule):
     raise RuntimeError(f"cycle detected in graph, prescheduled {len(prescheduled)} but only scheduled {len(schedule)}")
   if DEBUG >= 1 and len(schedule) >= 10: print(f"scheduled {len(schedule)} kernels")
-  #if len(schedule) > 1000: exit(0)
+  if len(schedule) > getenv("EXIT_SCHEDULE", 0): exit(0)
   return schedule, var_vals
 
 def create_schedule(outs:List[LazyBuffer], seen:Optional[Set[LazyBuffer]]=None) -> List[ScheduleItem]:
