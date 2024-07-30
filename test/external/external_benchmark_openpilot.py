35a7b8c1d988e0b548ff0d0fc6b2afa80cba07ed
Woze Parrot <wozeparrot@gmail.com>
2024-07-30 22:56:01 +0000

    feat: no verification for now

--- test/external/external_benchmark_openpilot.py
+++ test/external/external_benchmark_openpilot.py
@@ -1,7 +1,6 @@
 import time, sys
 import onnx
 from onnx.helper import tensor_dtype_to_np_dtype
-import onnxruntime as ort
 from extra.onnx import get_run_onnx
 from tinygrad import Tensor, dtypes, TinyJit
 from tinygrad.helpers import colored, fetch
@@ -39,14 +38,7 @@ if __name__ == "__main__":
     tms.append(time.perf_counter_ns() - st)
   print(f"jitted: {min(tms)*1e-6:7.2f} ms")
 
-  # validate
-  ort_options = ort.SessionOptions()
-  ort_options.log_severity_level = 3
-  onnx_session = ort.InferenceSession(onnx_path, ort_options)
-  onnx_output = onnx_session.run([onnx_model.graph.output[0].name], new_inputs_np)
-  ort_out = onnx_output[0]
-
   tinygrad_out = next(iter(run_onnx(new_inputs).values())).cast(dtypes.float32).numpy()
 
-  np.testing.assert_allclose(ort_out, tinygrad_out, atol=2e-3, rtol=1e-2)
-  print(colored("outputs validated!", "green"))
+  # np.testing.assert_allclose(ort_out, tinygrad_out, atol=2e-3, rtol=1e-2)
+  # print(colored("outputs validated!", "green"))
