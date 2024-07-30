c4ef0500bb485b344d1fffee0528467ef4211fb0
qazal <qazal.software@gmail.com>
2024-07-30 20:29:09 +0300

    all consts can go away

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -361,7 +361,7 @@ def do_expand(root:UOp):
     new_srcs = [new_srcs[_expand_arg_to_idx(old_args, rpk)].src[_expand_arg_to_idx(root.arg, rpk)] for rpk in _choices_from_args(expand_args)]
   if root.op is UOps.IF:
     # merge ifs into an or
-    conditions = functools.reduce(lambda x,y: x|y, dedup(x.src[0] for x in new_srcs if x.src[0].op is not UOps.CONST or x.src[0].arg != 0))
+    conditions = functools.reduce(lambda x,y: x|y, dedup(x.src[0] for x in new_srcs if x.src[0].op is not UOps.CONST))
     barriers = tuple(set(x.src[1] for x in new_srcs))
     new_srcs = [UOp(UOps.IF, src=(conditions,)+barriers) for _ in new_srcs]
   assert prod([x[1] for x in expand_args]) == len(new_srcs)
