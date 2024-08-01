b327a873bc8255bc784bb57a0c353d3f90006182
Woze Parrot <wozeparrot@gmail.com>
2024-08-01 19:04:08 +0000

    feat: known good outputs

--- test/external/external_benchmark_openpilot.py
+++ test/external/external_benchmark_openpilot.py
@@ -1,20 +1,21 @@
-import time, sys
+import time, sys, hashlib
+from pathlib import Path
 import onnx
 from onnx.helper import tensor_dtype_to_np_dtype
 import onnxruntime as ort
 from extra.onnx import get_run_onnx
 from tinygrad import Tensor, dtypes, TinyJit
-from tinygrad.helpers import GlobalCounters, fetch, colored
+from tinygrad.helpers import GlobalCounters, fetch, colored, getenv
 from tinygrad.tensor import _from_np_dtype
 import numpy as np
 
-OPENPILOT_MODEL = "https://github.com/commaai/openpilot/raw/v0.9.4/selfdrive/modeld/models/supercombo.onnx"
+OPENPILOT_MODEL = sys.argv[1] if len(sys.argv) > 1 else "https://github.com/commaai/openpilot/raw/v0.9.4/selfdrive/modeld/models/supercombo.onnx"
 
 if __name__ == "__main__":
   Tensor.no_grad = True
   Tensor.training = False
 
-  onnx_model = onnx.load(onnx_path := fetch(sys.argv[1] if len(sys.argv) > 1 else OPENPILOT_MODEL))
+  onnx_model = onnx.load(onnx_path := fetch(OPENPILOT_MODEL))
   run_onnx = get_run_onnx(onnx_model)
 
   Tensor.manual_seed(100)
@@ -38,13 +39,16 @@ if __name__ == "__main__":
     print(f"jitted:  {(time.perf_counter_ns() - st)*1e-6:7.4f} ms")
 
   # validate
-  ort_options = ort.SessionOptions()
-  ort_options.log_severity_level = 3
-  onnx_session = ort.InferenceSession(onnx_path, ort_options)
-  onnx_output = onnx_session.run([onnx_model.graph.output[0].name], new_inputs_np)
-  ort_out = onnx_output[0]
+  if getenv("SAVE_OUTPUT"):
+    ort_options = ort.SessionOptions()
+    ort_options.log_severity_level = 3
+    onnx_session = ort.InferenceSession(onnx_path, ort_options)
+    onnx_output = onnx_session.run([onnx_model.graph.output[0].name], new_inputs_np)
+    ort_out = onnx_output[0]
+    np.save(Path(__file__).parent / "openpilot" / f"{hashlib.md5(OPENPILOT_MODEL.encode()).hexdigest()}.npy", ort_out)
+  else:
+    ort_out = np.load(Path(__file__).parent / "openpilot" / f"{hashlib.md5(OPENPILOT_MODEL.encode()).hexdigest()}.npy")
+    tinygrad_out = next(iter(run_onnx_jit(new_inputs).values())).cast(dtypes.float32).numpy()
 
-  tinygrad_out = next(iter(run_onnx_jit(new_inputs).values())).cast(dtypes.float32).numpy()
-
-  np.testing.assert_allclose(ort_out, tinygrad_out, atol=2e-3, rtol=1e-2)
-  print(colored("outputs validated!", "green"))
+    np.testing.assert_allclose(ort_out, tinygrad_out, atol=2e-3, rtol=1e-2)
+    print(colored("outputs validated!", "green"))
