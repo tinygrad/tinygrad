2b4fb7cbd3c39dfe37d649ee3ff8b715af3b852c
reddyn12 <nikhilr.ssm@gmail.com>
2024-07-31 22:24:48 -0400

    make it loopable

--- extra/junk/cuda_bool_index.py
+++ extra/junk/cuda_bool_index.py
@@ -15,7 +15,8 @@ np.random.shuffle(nx)
 
 nx = nx.astype(bool)
 ny = ny.astype(np.int32)
-print('ARG_DTYPES:', nx.dtype, ny.dtype)
+zero = np.zeros((1)).astype(np.uint32)
+print('ARG_DTYPES:', nx.dtype, ny.dtype, zero.dtype)
 idx_real = np.sort(np.nonzero(nx)[0])
 
 device = CUDADevice("cuda:0")
@@ -23,21 +24,21 @@ cudaalloc = CUDAAllocator(device)
 
 x = cudaalloc.alloc(N*4)
 y = cudaalloc.alloc(N*4)
+cnt_ptr = cudaalloc.alloc(4)
 
 cudaalloc.copyin(x, bytearray(nx))
 cudaalloc.copyin(y, bytearray(ny))
+cudaalloc.copyin(cnt_ptr, bytearray(zero))
 
 print(device.arch)
 compiler = CUDACompiler(device.arch)
 
 prog = CUDAProgram(device, "boolidx_example", compiler.compile(f"""
 
-__device__ unsigned int count=0;
-
-extern "C" __global__ void boolidx_example(bool *x, int *y){{
+extern "C" __global__ void boolidx_example(bool *x, int *y, unsigned int *cnt_ptr){{
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if(x[i]==1){{
-    int y_ind = atomicAdd(&count, 1);
+    int y_ind = atomicAdd(cnt_ptr, 1);
     y[y_ind] = i;
   }}
 }}
@@ -45,11 +46,16 @@ extern "C" __global__ void boolidx_example(bool *x, int *y){{
 """))
 
 global_size, local_size = [10000,1,1], [1000, 1, 1]
-st = time.perf_counter()
-prog(x, y, global_size=global_size, local_size=local_size, wait=True)
-et = time.perf_counter()
+times, runs = 0, 100
+for i in range(runs):
+  st = time.perf_counter()
+  prog(x, y, cnt_ptr, global_size=global_size, local_size=local_size, wait=True)
+  cudaalloc.copyin(cnt_ptr, bytearray(zero))
+  et = time.perf_counter()
+  times+=(et-st)
 
-print(f'Kernel runtime {(et-st)*1000} ms')
+print(f'Ran {runs} times || Kernel AVG Runtime {times/runs*1000} ms')
 cudaalloc.copyout(flat_mv(ny.data), y)
 np.testing.assert_equal(np.sort(ny[:TRUE_CNT]), idx_real)
 print(f'NUM OF TRUES MATCH: {(ny>=0).sum()==TRUE_CNT}')
+print(f'Rest of array == -1: {ny[TRUE_CNT:].mean()==-1}')
