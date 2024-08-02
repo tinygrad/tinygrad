c2fd71e53238909280123df214dc0713706a11f1
ez2rok <69025426+ez2rok@users.noreply.github.com>
2024-08-02 00:58:40 -0400

    use np testing

--- test/test_nn.py
+++ test/test_nn.py
@@ -425,11 +425,8 @@ class TestNN(unittest.TestCase):
     # test negative and out of bound indices
     x = Tensor([-29, -28, -1, 28, 29])
     z = layer(x)
-    assert np.isnan(z.numpy()).all()
-    # torch_x = torch.tensor(x.numpy())
-    # torch_z = torch_layer(torch_x)
-    # np.testing.assert_allclose(z.numpy(), torch_z.detach().numpy(), atol=1e-8, rtol=1e-8)
-
+    np.testing.assert_equal(z, Tensor.nan(len(x), embed_size))
+    
     # test with jit enabled
     @TinyJit
     def layer_jit(x):
