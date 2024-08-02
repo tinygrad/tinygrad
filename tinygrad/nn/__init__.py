54cfa1bbb1706efe6def79a82387fbee6f1ccb98
ez2rok <69025426+ez2rok@users.noreply.github.com>
2024-08-02 00:52:09 -0400

    handle nans

--- tinygrad/nn/__init__.py
+++ tinygrad/nn/__init__.py
@@ -317,5 +317,5 @@ class Embedding:
     if idx.numel() == 0: return Tensor.empty(idx.shape+(self.embed_sz,), device=self.weight.device)
     arange_shp, weight_shp, big_shp = (1, 1, self.vocab_sz, 1), (1, 1, self.vocab_sz, self.embed_sz), idx.shape+(self.vocab_sz, self.embed_sz,)
     if not hasattr(self, 'arange'): self.arange = Tensor.arange(self.vocab_sz, requires_grad=False, device=self.weight.device).reshape(arange_shp)
-    arange, idx, vals = self.arange.expand(big_shp), idx.reshape(idx.shape+(1, 1,)).expand(big_shp), self.weight.reshape(weight_shp).expand(big_shp)
+    arange, idx, vals = self.arange.expand(big_shp), (idx >= self.vocab_sz).where(float('nan'), idx).reshape(idx.shape+(1, 1,)).expand(big_shp), self.weight.reshape(weight_shp).expand(big_shp)
     return (arange == idx).mul(vals).sum(2)
