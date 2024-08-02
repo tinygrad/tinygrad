9be7c2e429d045873eaf7838da302565b59a12d2
George Hotz <geohot@gmail.com>
2024-08-01 19:01:49 -0700

    move ops lds estimate to Program [run_process_replay]

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -13,7 +13,7 @@ from tinygrad.helpers import all_same, colored, ansilen, dedup, getenv, prod, DE
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.symbolic import sint
 from tinygrad.shape.view import strides_for_shape
-from tinygrad.codegen.uops import UOps, flops_mem
+from tinygrad.codegen.uops import UOps
 from tinygrad.codegen.uopgraph import UOpGraph
 from tinygrad.codegen.lowerer import lazyop_to_uop
 from enum import Enum, auto
@@ -760,10 +760,9 @@ class Kernel:
     else:
       global_size, local_size = None, None
 
-    ops, mem = flops_mem(self.uops.uops, ignore_indexing=True)
     # group non-local MemBuffers by the op type (LOAD or STORE) and the buffer arg. take the max access of that buffer in bytes
     # TODO: these max and min don't work on symbolic, and results are very wrong.
     mem_bytes = sum(max(x.arg.dtype.itemsize * x.arg.st.real_size() for x in group) for _, group in
       itertools.groupby([x for x in self.ast.lazyops if x.op in BufferOps and isinstance(x.arg, MemBuffer) and x.arg.idx >= 0],
                         key=lambda x: (x.op, x.arg.idx)))
-    return Program(ansiname, src, self.opts.device, global_size, local_size, self.uops.uops, ops, min(mem, mem_bytes), mem)
+    return Program(ansiname, src, self.opts.device, global_size, local_size, self.uops.uops, mem_bytes)
