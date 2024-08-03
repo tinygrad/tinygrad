da61dea1b2ca886b3de07e309efde2a78ac5682a
chenyu <chenyu@fastmail.com>
2024-08-03 14:27:23 -0400

    simple failed UOp sub symbolic test case (#5894)

--- test/unit/test_uop_symbolic.py
+++ test/unit/test_uop_symbolic.py
@@ -152,6 +152,11 @@ class TestSymbolic(unittest.TestCase):
   def test_sub_num_1(self):
     self.helper_test_variable(Variable("a", 0, 8)-NumNode(1), -1, 7, {"(-1+a)", "(a+(-1))"})
 
+  @unittest.expectedFailure
+  def test_sub_self(self):
+    a = Variable("a", 0, 8)
+    self.helper_test_variable(a*3-a, 0, 16, "(a*2)")
+
   def test_mul_0(self):
     self.helper_test_variable(Variable("a", 0, 8)*0, 0, 0, "0")
 
