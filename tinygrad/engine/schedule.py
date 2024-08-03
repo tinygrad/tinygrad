88af8e187e089f1f0514be9c1b5c5494e84c3b51
qazal <qazal.software@gmail.com>
2024-08-03 19:40:33 +0300

    update axis

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -119,8 +119,11 @@ def _recurse_reduceops(buf:LazyBuffer, st:ShapeTracker, realizes:Dict[LazyBuffer
         reduce_info[top_reduce] = (top_reduce_input_st, top_reduce_axes+axis)
         return None
       # reshape this reduceop based on the top reduce
-      #input_st = input_st.reshape(tuple(1 if i in top_reduce_axes else s for i,s in enumerate(top_reduce_input_st.shape)))
+      new_shape = tuple(1 if i in top_reduce_axes else s for i,s in enumerate(top_reduce_input_st.shape))
+      axis = tuple(i for i in range(len(new_shape)) if (st.shape[i] if i < len(st.shape) else 1) != new_shape[i])
+      input_st = input_st.reshape(new_shape)
     st = st.reshape(reduce_st(input_st, axis))
+
     reduce_info[(buf, st)] = (input_st, axis)
     return (buf, st)
   return top_reduce
