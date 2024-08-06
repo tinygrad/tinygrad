146b4af5c80155c15cd48428a4036eeb399ed0fd
reddyn12 <nikhilr.ssm@gmail.com>
2024-08-06 01:15:08 +0000

    add SYNCBN to training scripts

--- examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/retinanet/implementations/tinybox_red/retina_train_red.sh
+++ examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/retinanet/implementations/tinybox_red/retina_train_red.sh
@@ -4,6 +4,7 @@ export TINY_TYPE='tinyred'
 export PYTHONPATH='.'
 export MODEL='retinanet'
 export DEFAULT_FLOAT='HALF' GPUS=6 BS=96 BS_EVAL=36
+export SYNCBN=0
 export LAZYCACHE=0
 export TRAIN_BEAM=3 IGNORE_JIT_FIRST_BEAM=1 BEAM_UOPS_MAX=1500 BEAM_UPCAST_MAX=64 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=10 BEAM_PADTO=0
 export DATAPATH='/raid/datasets/open-images'
@@ -16,7 +17,7 @@ python3 extra/datasets/openimages.py
 
 TRAIN_ONLY=1 python3 examples/mlperf/model_train.py
 
-EVAL_ONLY=1 CHKPT_PATH='ckpts/retinanet_6xtinyred_B96_E1.safe' python3 examples/mlperf/model_train.py
-EVAL_ONLY=1 CHKPT_PATH='ckpts/retinanet_6xtinyred_B96_E2.safe' python3 examples/mlperf/model_train.py
-EVAL_ONLY=1 CHKPT_PATH='ckpts/retinanet_6xtinyred_B96_E3.safe' python3 examples/mlperf/model_train.py
-EVAL_ONLY=1 CHKPT_PATH='ckpts/retinanet_6xtinyred_B96_E4.safe' python3 examples/mlperf/model_train.py
+EVAL_ONLY=1 CHKPT_PATH="ckpts/retinanet_6x${TINY_TYPE}_B96_S${SYNCBN}_E1_SYNC.safe" python3 examples/mlperf/model_train.py
+EVAL_ONLY=1 CHKPT_PATH="ckpts/retinanet_6x${TINY_TYPE}_B96_S${SYNCBN}_E2_SYNC.safe" python3 examples/mlperf/model_train.py
+EVAL_ONLY=1 CHKPT_PATH="ckpts/retinanet_6x${TINY_TYPE}_B96_S${SYNCBN}_E3_SYNC.safe" python3 examples/mlperf/model_train.py
+EVAL_ONLY=1 CHKPT_PATH="ckpts/retinanet_6x${TINY_TYPE}_B96_S${SYNCBN}_E4_SYNC.safe" python3 examples/mlperf/model_train.py
