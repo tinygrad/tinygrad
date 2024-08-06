d490c387f1f0785ac0b62bec08b3216a963abf24
Elias Wahl <eliasruben2204@gmail.com>
2024-08-06 17:06:13 +0000

    note to todo

--- examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
+++ examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
@@ -8,8 +8,8 @@ export DEFAULT_FLOAT="HALF" GPUS=6 BS=84 EVAL_BS=6
 export BEAM=4
 export BASEDIR="/raid/datasets/wiki"
 
-echo "NOTE: DISABLING DROPOUT - UNSET FOR REAL SUBMISSION RUN"
-export DISABLE_DROPOUT=1 # NOTE: Unset flag for real submission run.
+echo "TODO: DISABLING DROPOUT - UNSET FOR REAL SUBMISSION RUN"
+export DISABLE_DROPOUT=1 # TODO: Unset flag for real submission run.
 
 # pip install -e ".[mlperf]"
 export LOGMLPERF=1
