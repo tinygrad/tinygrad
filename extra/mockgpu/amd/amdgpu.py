34aad31977da09c6e81330a6beb66a062cd4690e
Woze Parrot <wozeparrot@gmail.com>
2024-08-06 00:16:12 +0000

    feat: look for remu in different locations allows overriding location with standard linux features of `LD_LIBRARY_PATH`

--- extra/mockgpu/amd/amdgpu.py
+++ extra/mockgpu/amd/amdgpu.py
@@ -19,11 +19,17 @@ WAIT_REG_MEM_FUNCTION_ALWAYS = 0
 WAIT_REG_MEM_FUNCTION_EQ = 3 # ==
 WAIT_REG_MEM_FUNCTION_GEQ = 5 # >=
 
-try:
-  remu = ctypes.CDLL("/usr/local/lib/libremu.so")
-  remu.run_asm.restype = ctypes.c_int32
-  remu.run_asm.argtypes = [ctypes.c_void_p, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_void_p]
-except Exception: pass
+REMU_PATHS = ["libremu.so", "/usr/local/lib/libremu.so"]
+def _try_dlopen_remu():
+  for path in REMU_PATHS:
+    try:
+      remu = ctypes.CDLL(path)
+      remu.run_asm.restype = ctypes.c_int32
+      remu.run_asm.argtypes = [ctypes.c_void_p, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_uint32, ctypes.c_void_p]
+    except OSError: pass
+    else: return remu
+  raise RuntimeError("libremu not found")
+remu = _try_dlopen_remu()
 
 def create_sdma_packets():
   # TODO: clean up this, if we want to keep it
