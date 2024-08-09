743d7acfe11be04f94562afbf9d53809a315c63f
Comma Device <device@comma.ai>
2024-08-09 10:29:04 -0700

    compilation works

--- extra/dsp/compile.py
+++ extra/dsp/compile.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
+import os, ctypes, time
 import llvmlite.binding as llvm
+from tinygrad.helpers import getenv
 from tinygrad.runtime.support.elf import elf_loader
 from hexdump import hexdump
+if getenv("IOCTL"): import run # noqa: F401 # pylint: disable=unused-import
 
-src = """
-; ModuleID = "/Users/diane/tinygrad/tinygrad/renderer/llvmir.py"
-target triple = "unknown-unknown-unknown"
-target datalayout = ""
-
-define void @"E_3"(float* noalias %".1", float* noalias %".2", float* noalias %".3")
-{
-entry:
-  br label %"loop_body_0"
-loop_body_0:
-  %"loop0" = phi  i32 [0, %"entry"], [%".13", %"loop_body_0"]
-  %".6" = getelementptr inbounds float, float* %".2", i32 %"loop0"
-  %".7" = load float, float* %".6"
-  %".8" = getelementptr inbounds float, float* %".3", i32 %"loop0"
-  %".9" = load float, float* %".8"
-  %".10" = fadd nsz arcp contract afn reassoc float %".7", %".9"
-  %".11" = getelementptr inbounds float, float* %".1", i32 %"loop0"
-  store float %".10", float* %".11"
-  %".13" = add i32 %"loop0", 1
-  %".14" = icmp ult i32 %".13", 3
-  br i1 %".14", label %"loop_body_0", label %"loop_exit_0"
-loop_exit_0:
-  ret void
-}
-"""
+adsp = ctypes.CDLL(ctypes.util.find_library("adsprpc"))
+import adsprpc
 
 if __name__ == "__main__":
-  llvm.initialize()
-  llvm.initialize_all_targets()
-  llvm.initialize_all_asmprinters()
-  target_machine: llvm.targets.TargetMachine = llvm.Target.from_triple("hexagon").create_target_machine(opt=2)
-  mod = llvm.parse_assembly(src)
-  mod.verify()
-  print(target_machine.emit_assembly(mod))
-  obj = target_machine.emit_object(mod)
-  img, sections, relocs = elf_loader(obj, elf32=True)
-  hexdump(img)
+  from tinygrad.runtime.ops_clang import ClangCompiler
+  cc = ClangCompiler(["--target=hexagon", "-mcpu=hexagonv65", "-fuse-ld=lld", "-nostdlib"])
+
+  obj = cc.compile("""
+    struct remote_buf { void *pv; unsigned int len; };
+    int entry(unsigned long long handle, unsigned int sc, struct remote_buf* pra) {
+      if (sc>>24 == 1) {
+        ((char*)pra[0].pv)[0] = 55;
+        // NOTE: you have to return 0 for outbufs to work
+        return 0;
+      }
+      return 0;
+    }
+  """)
+  with open("/tmp/swag.so", "wb") as f: f.write(obj)
+
+  handle = ctypes.c_int64(-1)
+  adsp.remote_handle64_open(ctypes.create_string_buffer(b"file:////tmp/swag.so?entry&_modver=1.0&_dom=cdsp"), ctypes.byref(handle))
+  print(handle.value)
+  print(adsp.remote_handle64_invoke(handle, 0, None))
+
+  arg_2 = ctypes.c_int64(-1)
+  pra = (adsprpc.union_remote_arg64 * 3)()
+  pra[0].buf.pv = ctypes.addressof(arg_2)
+  pra[0].buf.len = 8
+  print(adsp.remote_handle64_invoke(handle, (1<<24) | (1<<8), pra))
+  print(hex(arg_2.value), arg_2.value)
