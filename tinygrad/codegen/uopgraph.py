63a8bc29d4e82626c1f579d33ef94a8b0b2822bd
chenyu <chenyu@fastmail.com>
2024-08-09 20:09:05 -0400

    addition divisor in UOp div_folding (#6002)
    in addition to try gcd of all terms, also try least common divisor of all MULs

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -100,7 +100,7 @@ def div_folding(x:UOp, c:int) -> Optional[UOp]:
   # simple cancel div case
   if 0 <= x.vmin.arg and x.vmax.arg < c: return x.const(0)
 
-  quotient, remainder, rem_const, something_changed, gcd = [], [], 0, False, c
+  quotient, remainder, rem_const, something_changed, gcd, divisor = [], [], 0, False, c, 1
   for u in _get_add_chain(x):
     if u.op is UOps.CONST:
       # add all const together first
@@ -110,6 +110,8 @@ def div_folding(x:UOp, c:int) -> Optional[UOp]:
       if factor: quotient.append(u.divides(c))
       something_changed = True
     else:
+      # divisor is the smallest common divisor of all MULs
+      if u.op is UOps.ALU and u.arg is BinaryOps.MUL and factor > 1 and c % factor == 0 and (divisor == 1 or divisor > factor): divisor = factor
       remainder.append(u)
       gcd = math.gcd(gcd, factor)
 
@@ -120,11 +122,14 @@ def div_folding(x:UOp, c:int) -> Optional[UOp]:
     rem_const = rem_const%c
   if rem_const != 0: remainder.append(x.const(rem_const))
 
-  if not something_changed: return newx//(c//gcd) if 1 < gcd < c and (newx:=div_folding(x, gcd)) is not None else None
+  # x // c -> quotient + (remainder // div) // (c // div)
+  div = gcd if gcd > 1 else divisor
+
+  if not something_changed: return newx//(c//div) if 1 < div < c and (newx:=div_folding(x, div)) is not None else None
   rem:Optional[UOp] = functools.reduce(operator.add, remainder) if remainder else None
   quo:Optional[UOp] = functools.reduce(operator.add, quotient) if quotient else None
-  if quo is None: return x.const(0) if rem is None else cast(UOp, div_folding(rem, gcd))//(c//gcd)
-  return quo if rem is None else cast(UOp, div_folding(rem, gcd))//(c//gcd)+quo
+  if quo is None: return x.const(0) if rem is None else cast(UOp, div_folding(rem, div))//(c//div)
+  return quo if rem is None else cast(UOp, div_folding(rem, div))//(c//div)+quo
 
 # ***** transcendental *****
 
