bd996cae2e7175e7daf39ce2484024ace7462ec5
p4sscode <p4ssenger.developer@gmail.com>
2024-08-12 10:32:06 -0300

    no longer expect compiler error

--- test/test_renderer_failures.py
+++ test/test_renderer_failures.py
@@ -36,12 +36,8 @@ class TestCStyleFailures(unittest.TestCase):
     alu = ld.alu(BinaryOps.MAX, UOp.const(dtypes.int, dtypes.min(dtypes.int)))
     store = UOp.store(a, idx, alu)
     # CLANG doesn't use the max function
-    if Device.DEFAULT in ["CLANG"]:
-      ret = _test_uop_result([Tensor([1])], [store])[0]
-      assert ret[0] == 1
-    # call to 'max' is ambiguous
-    else:
-      with self.assertRaises(CompileError): _test_uop_result([Tensor([1])], [store])
+    ret = _test_uop_result([Tensor([1])], [store])[0]
+    self.assertEqual(ret[0], 1)
 
 if __name__ == '__main__':
   unittest.main()
