e4b8d1ddc6d1e9dc8be75d78125c238f380be219
Ian Paul <iandouglaspaul@gmail.com>
2024-08-15 11:21:04 +0700

    Add tests for logic change

--- test/test_renderer_failures.py
+++ test/test_renderer_failures.py
@@ -12,19 +12,22 @@ from tinygrad.renderer import Program
 from tinygrad.tensor import Tensor, _to_np_dtype
 from tinygrad.lazy import LazyBuffer
 
-def _test_uop_result(inputs:List[Tensor], stores:List[UOp]):
+def _test_uop_result(inputs:List[Tensor], uops:List[UOp]):
   for x in inputs: x.realize()
-  assert all(x.op is UOps.STORE for x in stores)
-  # NOTE: we only toposort the stores
-  uops: List[UOp] = []
-  def _recursive_add(uop:UOp) -> List[UOp]: return flatten([_recursive_add(x) for x in uop.src])+[uop]
-  uops = dedup(flatten(_recursive_add(st) for st in stores))
   outbufs = [Buffer(Device.DEFAULT, 1, cast(DType,u.src[2].dtype)).allocate() for u in uops if u.op is UOps.STORE]
   inbufs = [cast(LazyBuffer,x.lazydata).base.buffer for x in inputs]
   src = Device[Device.DEFAULT].renderer.render("test", uops)
   ei = CompiledRunner(Program("test", src, Device.DEFAULT, uops=uops))
   ei.exec(outbufs+inbufs)
   return [np.frombuffer(x.as_buffer(), _to_np_dtype(x.dtype)) for x in outbufs]
+
+def _get_uops_from_stores(stores:List[UOp]) -> List[UOp]:
+  assert all(x.op is UOps.STORE for x in stores)
+  # NOTE: we only toposort the stores
+  uops: List[UOp] = []
+  def _recursive_add(uop:UOp) -> List[UOp]: return flatten([_recursive_add(x) for x in uop.src])+[uop]
+  uops = dedup(flatten(_recursive_add(st) for st in stores))
+  return uops
 
 @unittest.skipIf(not isinstance(Device[Device.DEFAULT].renderer, CStyleLanguage), "uops are for cstyle")
 class TestCStyleFailures(unittest.TestCase):
@@ -36,8 +39,28 @@ class TestCStyleFailures(unittest.TestCase):
     alu = ld.alu(BinaryOps.MAX, UOp.const(dtypes.int, dtypes.min(dtypes.int)))
     store = UOp.store(a, idx, alu)
     # CLANG doesn't use the max function
-    ret = _test_uop_result([Tensor([1])], [store])[0]
+    ret = _test_uop_result([Tensor([1])], _get_uops_from_stores([store]))[0]
     self.assertEqual(ret[0], 1)
+
+  # simplified version of test_padto_where_multireduce
+  def test_oos_fails(self):
+    g = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0)
+    c0 = UOp.const(dtypes.int, 0)
+    c4 = UOp.const(dtypes.int, 4)
+    acc0 = c4.alu(BinaryOps.ADD, c4)
+    r0 = UOp(UOps.RANGE, dtypes.int, (c0, c4), (1, 0, False))
+    alu0 = UOp(UOps.ALU, dtypes.int, (r0, acc0), BinaryOps.ADD)
+    phi0 = UOp(UOps.PHI, dtypes.int, (acc0, alu0))
+    cast0 = UOp(UOps.CAST, dtypes.int64, (acc0,))
+    er0 = UOp(UOps.ENDRANGE, None, (r0,))
+    gate0 = UOp(UOps.ALU, dtypes.bool, (acc0, c0), BinaryOps.CMPNE)
+    if0 = UOp(UOps.IF, None, (gate0, cast0))
+    store0 = UOp.store(g, c0, cast0)
+    eif0 = UOp(UOps.ENDIF, None, (if0,))
+
+    uops = [g, c0, c4, acc0, r0, alu0, phi0, cast0, er0, gate0, if0, store0, eif0]
+    ret = _test_uop_result([Tensor([1])], uops)[0]
+    self.assertEqual(ret[0], 14)
 
 if __name__ == '__main__':
   unittest.main()
