0a8774b51cb4712eb4bda0d77e8d56c9c97a25fc
zibokapi <zibokapi@gmail.com>
2024-08-17 23:44:58 +0800

    oops spaces

--- test/external/external_test_image_resize.py
+++ test/external/external_test_image_resize.py
@@ -6,18 +6,18 @@ from tinygrad import Tensor
 
 pil_resize_modes = {'nearest-exact': Image.NEAREST, 'bilinear': Image.BILINEAR}
 def compare_resize(image:np.ndarray, size, mode):
-    torch_image = torch.tensor(image).permute(2,0,1).unsqueeze(0)
-    tiny_image = Tensor(image).permute(2,0,1).unsqueeze(0)
+  torch_image = torch.tensor(image).permute(2,0,1).unsqueeze(0)
+  tiny_image = Tensor(image).permute(2,0,1).unsqueeze(0)
 
-    torch_image = torch.nn.functional.interpolate(torch_image, size, mode=mode).permute(0,2,3,1).squeeze(0).numpy()
-    tiny_image = tiny_image.interpolate(size, mode).permute(0,2,3,1).squeeze(0).numpy()
-    pil_image = np.array(Image.fromarray(image).resize((size[1], size[0]), pil_resize_modes[mode]))
+  torch_image = torch.nn.functional.interpolate(torch_image, size, mode=mode).permute(0,2,3,1).squeeze(0).numpy()
+  tiny_image = tiny_image.interpolate(size, mode).permute(0,2,3,1).squeeze(0).numpy()
+  pil_image = np.array(Image.fromarray(image).resize((size[1], size[0]), pil_resize_modes[mode]))
 
-    np.testing.assert_allclose(tiny_image, torch_image)
-    np.testing.assert_allclose(torch_image, pil_image)
-    np.testing.assert_allclose(tiny_image, pil_image)
+  np.testing.assert_allclose(torch_image, pil_image)
+  np.testing.assert_allclose(tiny_image, pil_image)
 
-for _ in range(100):
+if __name__ == "__main__":
+  for _ in range(100):
     image = np.random.randint(0, 256, (128, 128, 3), dtype=np.uint8)
     new_size = np.random.randint(32, 256, size=2).tolist()
     mode = np.random.choice(['nearest-exact'])
