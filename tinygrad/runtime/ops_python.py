3b764f2b5341bcfb3fe0146f71fdd37ec3d565ca
p4sscode <p4ssenger.developer@gmail.com>
2024-08-17 14:06:24 -0300

    fix ops_python bug

--- tinygrad/runtime/ops_python.py
+++ tinygrad/runtime/ops_python.py
@@ -179,7 +179,7 @@ class PythonProgram:
             def c_map(lane, elem): return (lane, elem)
             ul[i] = wmma_helper(8, 16, 16, 16, 8, a_elem, b_elem, c_map)
           elif arg[4] == "CLANG":
-            wmma_sz = [prod(x[1] for x in l) for l in arg[5]]
+            wmma_sz = [prod(x[1] for x in l) for l in arg[6]]
             def elem(x, i, j, _): return x[i+j][0]
             def c_map(_, elem): return (elem%wmma_sz[0], elem//wmma_sz[0])
             ul[i] = wmma_helper(1, 1, wmma_sz[0], wmma_sz[1], wmma_sz[2], elem, elem, c_map)
