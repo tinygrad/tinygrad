bc2a517ec22d77a883f5ee24bb0847e1dc932048
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-08-19 15:26:33 +0000

    no lib

--- tinygrad/runtime/ops_dsp.py
+++ tinygrad/runtime/ops_dsp.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 from typing import Tuple, List
-import ctypes, fcntl, os, mmap, tempfile
+import ctypes, fcntl, os, mmap, tempfile, pathlib, functools, array
 from tinygrad.device import BufferOptions, LRUAllocator, Compiled, Allocator
-from tinygrad.helpers import from_mv, getenv, DEBUG
+from tinygrad.helpers import from_mv, getenv, DEBUG, round_up, mv_address, to_mv
 from tinygrad.runtime.ops_clang import ClangCompiler
 from tinygrad.renderer.cstyle import CStyleLanguage
 from tinygrad.dtype import DType
@@ -17,29 +17,60 @@ from tinygrad.runtime.autogen import libc, ion, msm_ion, adsprpc, qcom_dsp
 
 if getenv("IOCTL"): import extra.dsp.run # noqa: F401 # pylint: disable=unused-import
 
-adsp = ctypes.CDLL(ctypes.util.find_library("adsprpc"))
+# adsp = ctypes.CDLL(ctypes.util.find_library("adsprpc"))
+
+def rpc_invoke(rpcfd, handle, method, ins=None, outs=None):
+  if ins or outs:
+    ins = ins or list()
+    outs = outs or list()
+    pra = (qcom_dsp.union_remote_arg * (len(ins) + len(outs)))()
+    for i,mv in enumerate(ins + outs):
+      if isinstance(mv, memoryview):
+        pra[i].buf.pv = mv_address(mv) if mv.nbytes > 0 else 0
+        pra[i].buf.len = mv.nbytes
+      else: assert False, "not supported"
+    # pra = (qcom_dsp.union_remote_arg * (len(ins) + len(outs))).from_address(ctypes.addressof(pra))
+  else:
+    pra = None
+    ins = ins or list()
+    outs = outs or list()
+
+  sc = (method << 24) | (len(ins) << 16) | (len(outs) << 8)
+  return qcom_dsp.FASTRPC_IOCTL_INVOKE(rpcfd, handle=handle, sc=sc, pra=pra)
 
 class DSPProgram:
-  def __init__(self, name:str, lib:bytes):
+  def __init__(self, device, name:str, lib:bytes):
+    self.device = device
+
     with tempfile.NamedTemporaryFile(delete=True) as output_file:
-      output_file.write(lib)
+      output_file.write(lib) # TODO: not need to write this
       output_file.flush()
       if DEBUG >= 6: os.system(f"llvm-objdump -mhvx -d {output_file.name}")
-      self.handle = ctypes.c_int64(-1)
-      fp = f"file:///{output_file.name}?entry&_modver=1.0&_dom=cdsp"
-      adsp.remote_handle64_open(ctypes.create_string_buffer(fp.encode()), ctypes.byref(self.handle))
-      # print("OPEN", self.handle.value)
-      assert self.handle.value != -1, "load failed"
+      # self.handle = ctypes.c_int64(-1)
+      fp = f"file:///{output_file.name}?entry&_modver=1.0&_dom=cdsp\0"
 
-  def __del__(self):
-    if self.handle.value != -1:
-      x = adsp.remote_handle64_close(self.handle)
-      assert x == 0, "CLOSE failed"
+      a1 = memoryview(array.array('I', [len(fp), 0xff]))
+      a2 = memoryview(bytearray(f"{fp}".encode()))
+      o1 = memoryview(bytearray(0x8))
+      o2 = memoryview(bytearray(0xff))
+      z = rpc_invoke(self.device.rpc_fd, handle=0, method=0, ins=[a1, a2], outs=[o1, o2])
+
+      self.handle = o1.cast('I')[0]
+      print("Handle", hex(self.handle))
+
+      # adsp.remote_handle64_open(ctypes.create_string_buffer(fp.encode()), ctypes.byref(self.handle))
+      # print("OPEN", self.handle.value)
+      # assert self.handle.value != -1, "load failed"
+
+  def __del__(self): pass # TODO: fix this
+    # if self.handle.value != -1:
+    #   x = adsp.remote_handle64_close(self.handle)
+    #   assert x == 0, "CLOSE failed"
 
   def __call__(self, *bufs, vals:Tuple[int, ...]=(), wait=False):
     assert len(vals) == 0
     assert len(bufs) < 16
-    pra = (adsprpc.union_remote_arg64 * (2+len(bufs)))()
+    pra = (qcom_dsp.union_remote_arg * (2+len(bufs)))()
     test = (ctypes.c_int32 * len(bufs))()
     time_est = ctypes.c_uint64(0)
     pra[0].buf.pv = ctypes.addressof(test)
@@ -50,7 +81,15 @@ class DSPProgram:
       test[i] = b.size
       pra[i+2].dma.fd = b.share_info.fd
       pra[i+2].dma.len = b.size
-    ret = adsp.remote_handle64_invoke(self.handle, (1<<24) | (1<<16) | (1<<8) | len(bufs), pra)
+
+    fds = (ctypes.c_int32 * (2+len(bufs)))(-1, -1, *[pra[i+2].dma.fd for i in range(len(bufs))])
+    attrs = (ctypes.c_uint32 * (2+len(bufs)))(0, 0, *[1 for i in range(len(bufs))])
+
+    # print([-1, -1, *[pra[i+2].dma.fd for i in range(len(bufs))]])
+
+    qcom_dsp.FASTRPC_IOCTL_INVOKE_ATTRS(self.device.rpc_fd, inv=qcom_dsp.struct_fastrpc_ioctl_invoke(handle=self.handle, sc=(1<<24) | (1<<16) | (1<<8) | len(bufs), pra=pra), fds=fds, attrs=attrs)
+
+    # ret = adsp.remote_handle64_invoke(self.handle, (1<<24) | (1<<16) | (1<<8) | len(bufs), pra)
     # print("invoke", ret)
     assert ret == 0, f"!!! invoke returned {ret}"
     # if ret != 0:
@@ -164,6 +203,117 @@ class DSPDevice(Compiled):
     self.ion_fd = os.open('/dev/ion', os.O_RDONLY)
     self.rpc_fd = os.open('/dev/adsprpc-smd', os.O_RDONLY | os.O_NONBLOCK)
 
+    qcom_dsp.FASTRPC_IOCTL_GETINFO(self.rpc_fd, 3)
+
+    fastrpc_shell = memoryview(bytearray(pathlib.Path('/dsp/cdsp/fastrpc_shell_3').read_bytes()))
+    shell_mem = qcom_dsp.ION_IOC_ALLOC(self.ion_fd, len=round_up(fastrpc_shell.nbytes, 0x1000), align=0x1000, heap_id_mask=0x2000000, flags=0x1)
+    shell_mapped = qcom_dsp.ION_IOC_MAP(self.ion_fd, handle=shell_mem.handle)
+    fastrpc_shell_addr = libc.mmap(0, shell_mem.len, mmap.PROT_READ|mmap.PROT_WRITE, mmap.MAP_SHARED, shell_mapped.fd, 0)
+    ctypes.memmove(fastrpc_shell_addr, mv_address(fastrpc_shell), fastrpc_shell.nbytes)
+
+    qcom_dsp.FASTRPC_IOCTL_CONTROL(self.rpc_fd, req=0x3)
+    qcom_dsp.FASTRPC_IOCTL_INIT(self.rpc_fd, flags=0x1, file=fastrpc_shell_addr, filelen=fastrpc_shell.nbytes, filefd=shell_mapped.fd)
+
+    qcom_dsp.FASTRPC_IOCTL_INVOKE(self.rpc_fd, handle=0x3, sc=(0x3 << 24)) # TODO: remove hardcoded 0x3
+
+    # Start a serve thread for now
+    from threading import Thread
+    self.worker = Thread(target=self.listner_worker)
+    self.worker.start()
+
     compiler_args = ["--target=hexagon", "-mcpu=hexagonv65", "-fuse-ld=lld", "-nostdlib", "-mhvx=v65", "-mhvx-length=128b",
                      "-I/data/home/nimlgen/tinygrad/extra/dsp/include"]
-    super().__init__(device, DSPAllocator(self), DSPRenderer(), ClangCompiler("compile_dsp", args=compiler_args), DSPProgram)
+    super().__init__(device, DSPAllocator(self), DSPRenderer(), ClangCompiler("compile_dsp", args=compiler_args), functools.partial(DSPProgram, self))
+
+  def listner_worker(self):
+    # TODO: cleanup
+    context = 0
+    handle = 0xffffffff
+    msg_send = memoryview(bytearray(0x10)).cast('I')
+    msg_recv = memoryview(bytearray(0x10)).cast('I')
+    out_buf = memoryview(bytearray(0x1000)).cast('I')
+    in_buf = memoryview(bytearray(0x1000)).cast('I')
+
+    prev_res = 0xffffffff
+    out_buf_size = 0
+
+    req_args = (qcom_dsp.union_remote_arg * 4)()
+    req_args[0].buf = qcom_dsp.struct_remote_buf(pv=mv_address(msg_send), len=0x10)
+    req_args[1].buf = qcom_dsp.struct_remote_buf(pv=mv_address(out_buf), len=0x1000)
+    req_args[2].buf = qcom_dsp.struct_remote_buf(pv=mv_address(msg_recv), len=0x10)
+    req_args[3].buf = qcom_dsp.struct_remote_buf(pv=mv_address(in_buf), len=0x1000)
+
+    while True:
+      msg_send[0] = context
+      msg_send[1] = prev_res
+      msg_send[2] = out_buf_size
+      msg_send[3] = 0x1000
+
+      req_args[1].buf.len = out_buf_size
+      qcom_dsp.FASTRPC_IOCTL_INVOKE(self.rpc_fd, handle=0x3, sc=0x04020200, pra=req_args) # listener
+
+      # print("dai")
+
+      context = msg_recv[0]
+      handle = msg_recv[1]
+      sc = msg_recv[2]
+      inbufs = (sc >> 16) & 0xff
+      outbufs = (sc >> 8) & 0xff
+
+      in_args, out_args = [], []
+      ptr = mv_address(in_buf)
+      for i in range(inbufs):
+        sz = to_mv(ptr, 4).cast('I')[0]
+        obj_ptr = round_up(ptr + 4, 8)
+        in_args.append(to_mv(obj_ptr, sz))
+        ptr = obj_ptr + sz
+
+      ctypes.memset(mv_address(out_buf), 0, 0x1000)
+      ptr_out = mv_address(out_buf)
+      for i in range(outbufs):
+        sz = to_mv(ptr, 4).cast('I')[0]
+        ptr += 4
+
+        to_mv(ptr_out, 4).cast('I')[0] = sz
+        obj_ptr = round_up(ptr_out + 4, 8)
+
+        out_args.append(to_mv(obj_ptr, sz))
+        ptr_out = obj_ptr + sz
+
+      out_buf_size = ptr_out - mv_address(out_buf)
+
+      if sc == 0x20200: # greating?
+        prev_res = 0
+      elif sc == 0x13050100: # open
+        # for a in in_args: hexdump(a)
+        try:
+          fd = os.open(in_args[3].tobytes()[:-1].decode(), os.O_RDONLY)
+          out_args[0].cast('I')[0] = fd
+          prev_res = 0
+        except: prev_res = 2
+      elif sc == 0x9010000: # seek
+        res = os.lseek(in_args[0].cast('I')[0], in_args[0].cast('I')[1], in_args[0].cast('I')[2])
+        prev_res = 0 if res >= 0 else res
+      elif sc == 0x4010200: # read
+        buf = os.read(in_args[0].cast('I')[0], in_args[0].cast('I')[1])
+        out_args[1][:len(buf)] = buf
+        out_args[0].cast('I')[0] = len(buf)
+        out_args[0].cast('I')[1] = int(len(buf) == 0)
+        prev_res = 0
+      elif sc == 0x3010000: # close
+        os.close(in_args[0].cast('I')[0])
+        prev_res = 0
+      elif sc == 0x1f020100: # stat
+        # try:
+        stat = os.stat(in_args[1].tobytes()[:-1].decode())
+        out_stat = out_args[0].cast('Q')
+        out_stat[1] = stat.st_dev
+        out_stat[2] = stat.st_ino
+        out_stat[3] = stat.st_mode | (stat.st_nlink << 32)
+        out_stat[4] = stat.st_rdev
+        out_stat[5] = stat.st_size
+        # print(stat, stat.st_rdev)
+        # assert False
+        prev_res = 0
+        # except: prev_res = 2
+      else: raise RuntimeError(f"Unknown {sc=:X}")
