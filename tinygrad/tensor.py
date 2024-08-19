556536df1cea1804590e57f95009a2481bab49f1
George Hotz <geohot@gmail.com>
2024-08-19 12:14:08 -0700

    master tensor

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -1524,7 +1524,6 @@ class Tensor:
   def std_mean(self, axis:Optional[Union[int, Sequence[int]]]=None, keepdim=False, correction=1):
     """
     Calculates the standard deviation and mean over the dimensions specified by dim.
-
     Syntactic sugar around `Tensor.std` and `Tensor.mean` to match `torch.std_mean`.
 
     ```python exec="true" source="above" session="tensor" result="python"
@@ -3013,15 +3012,6 @@ class Tensor:
     ```
     """
     return (self.maximum(0) - y * self + (1 + self.abs().neg().exp()).log()).mean()
-
-  def cross_entropy(self, y:Tensor, reduction:str='mean', label_smoothing:float=0.0) -> Tensor:
-    """
-    Compute the cross entropy loss between input logits and target.
-    """
-    y = (1 - label_smoothing)*y + label_smoothing / cast(int, y.shape[1])
-    ret = -self.log_softmax(axis=1).mul(y).sum(axis=1)
-    do_reduction: Dict[str, Callable[[Tensor], Tensor]] = {"mean": Tensor.mean, "sum": Tensor.sum, "none": lambda x: x}
-    return do_reduction[reduction](ret)
 
   def sparse_categorical_crossentropy(self, Y:Tensor, ignore_index=-1, label_smoothing=0.0) -> Tensor:
     """
