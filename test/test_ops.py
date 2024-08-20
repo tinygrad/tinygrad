fd3004105c25c25ecf38a14fa73a582400ae14dd
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-08-19 20:37:23 -0500

    uint8 bilinear on LLVM with atol=1

--- test/test_ops.py
+++ test/test_ops.py
@@ -2100,7 +2100,6 @@ class TestOpsUint8(unittest.TestCase):
       lambda x: torch.nn.functional.interpolate((10*x).relu().type(torch.uint8), size=out_sz, mode="nearest-exact"),
       lambda x: Tensor.interpolate((10*x).relu().cast('uint8'), size=out_sz, mode="nearest-exact"), forward_only=True)
 
-  @unittest.skipIf(Device.DEFAULT in ["NV", "LLVM"], "incorrect result")
   def test_interpolate_bilinear(self):
     out_sz = (10, 10)
     # NOTE: atol=1 due to torch using native uint8 interpolate on cpu
--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -2038,7 +2038,7 @@ class Tensor:
     assert isinstance(size, (tuple,list)) and all_int(size) and 0 < len(size) <= self.ndim, f"invalid {size=}"
     assert mode in ["linear", "nearest", "nearest-exact"], "only supports linear, nearest, and nearest-exact interpolate"
     if mode != "linear" and align_corners: raise ValueError("align_corners can only be set with interpolate mode linear")
-    x, expand = self, list(self.shape)
+    x, expand = self.cast(dtypes.float32), list(self.shape)
     for i in range(-len(size), 0):
       scale = (self.shape[i] - int(align_corners)) / (size[i] - int(align_corners))
       arr, reshape = Tensor.arange(size[i], dtype=dtypes.float32, device=self.device), [1] * self.ndim
