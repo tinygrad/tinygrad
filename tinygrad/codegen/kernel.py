66aad9c3ca94b436041c144147e503bb7a9163b6
p4sscode <p4ssenger.developer@gmail.com>
2024-08-23 09:58:25 -0300

    turing tc working!

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -334,7 +334,7 @@ class Kernel:
           for (tc_dim, tc_amt) in tc.threads: self.apply_opt(Opt(OptOps.LOCAL, tc_opts.axes[tc_dim], tc_amt), append_opt=False)
         elif self.opts.device in {"CUDA_80", "CUDA_75", "NV"}:
           if int(self.opts.device[-2:]) == 75:
-            self.apply_opt(Opt(OptOps.UNROLL, tc_opts.axes[2]-self.first_reduce, 8), append_opt=False)
+            self.apply_opt(Opt(OptOps.UNROLL, tc_opts.axes[2]-self.first_reduce, 4), append_opt=False)
             self.apply_opt(Opt(OptOps.UNROLL, tc_opts.axes[2]-self.first_reduce, 2), append_opt=False)
             # NOTE: LOCALS and UPCAST can be swapped here. it doesn't seem faster
             self.apply_opt(Opt(OptOps.UPCAST, tc_opts.axes[1], 2), append_opt=False)
@@ -371,7 +371,6 @@ class Kernel:
     if not self.opts.tensor_cores and use_tensor_cores != 2: return False
     try: # check TC first and apply hand-coded opts if successful
       self.apply_opt(Opt(OptOps.TC, axis, tc_opt))
-      return True
 
       if (tc_opts:=self.tensor_core_opts) is not None:
         if extra_opts is not None:
@@ -689,10 +688,10 @@ class Kernel:
               #   ((1,1), (1,0), (0,0), (0,1)), ((0,4), (0,2), (1,4), (0,3), (1,3), (1,2)))
               
               reduce_axes, upcast_axes = [0, 1], [[(0, 4)], [(3, 2)], [(2, 2), (3, 2)]]
-              fix_st1 = functools.partial(fix_st, (2,2,2,2,2), (8,2,2,2), (2,2,2,2,2,2),
-                ((1,1), (1,0), (0,2), (0,3), (0,4)), ((1,3), (1,4), (1,2), (0,0), (0,1), (1,5)))
-              fix_st2 = functools.partial(fix_st, (2,2,2,2,2), (8,2,2,2), (2,2,2,2,2,2),
-                ((1,1), (1,0), (1,5), (0,0), (0,1)), ((0,4), (0,2), (1,4), (0,3), (1,3), (1,2)))
+              fix_st1 = functools.partial(fix_st, (2,2,2,2,2), (4,2,2,2), (2,2,2,2,2),
+                ((1,1), (1,0), (0,2), (0,3), (0,4)), ((1,3), (1,2), (0,0), (0,1), (1,4)))
+              fix_st2 = functools.partial(fix_st, (2,2,2,2,2), (4,2,2,2), (2,2,2,2,2),
+                ((1,1), (1,0), (1,4), (0,0), (0,1)), ((0,4), (0,2), (0,3), (1,3), (1,2)))
               # fix_st1 = fix_st2 = None
               # fix_st1 = functools.partial(fix_st, (2,2,2,), (4,2,2), (2,2,2,2),
               #   ((1,1), (1,0), (0,2)), ((1,3), (1,2), (0,0), (0,1)))
