bb692a4a03e1362ce7ba99ed9fa27be7c2fb822e
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-08-25 13:47:56 +0000

    run2

--- extra/dsp/run.py
+++ extra/dsp/run.py
@@ -185,7 +185,7 @@ def install_hook(c_function, python_function):
   return orig_func
 
 libc = ctypes.CDLL(ctypes.util.find_library("libc"))
-install_hook(libc.ioctl, ioctl)
+# install_hook(libc.ioctl, ioctl)
 adsp = ctypes.CDLL(ctypes.util.find_library("adsprpc"))
 # print(adsp)
 
