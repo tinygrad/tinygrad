9ba674a12611609e98ff6a4e9a046bb80563d647
Pratyush Shukla <ps4534@nyu.edu>
2024-08-26 01:25:33 +0530

    add test

--- test/test_ops.py
+++ test/test_ops.py
@@ -1770,6 +1770,12 @@ class TestOps(unittest.TestCase):
       lambda x: torch.nn.functional.avg_pool2d(x, kernel_size=(111,28)),
       lambda x: Tensor.avg_pool2d(x, kernel_size=(111,28)), rtol=1e-5)
 
+  def test_interpolate_nearest(self):
+    for in_sz, out_sz in [((52,),(29,)), ((29,),(52,))]:
+      helper_test_op([(2,3)+in_sz],
+          lambda x: torch.nn.functional.interpolate(x, size=out_sz, mode="nearest"),
+          lambda x: Tensor.interpolate(x, size=out_sz, mode="nearest"))
+
   def test_interpolate_linear(self):
     for in_sz, out_sz in [((52,),(29,)), ((29,),(52,))]:
       helper_test_op([(2,3)+in_sz],
