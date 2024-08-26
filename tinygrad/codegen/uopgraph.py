9accbe1af644629bd5fb69be5b8f9362a3e5c21a
Ian Paul <iandouglaspaul@gmail.com>
2024-08-26 16:56:39 +0800

    include proper srcs in IF from the get-go

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -413,16 +413,13 @@ def no_vectorized_alu(alu):
 def create_gate(root:UOp) -> Optional[UOp]:
   @functools.lru_cache(None)
   def _gate_srcs(u:UOp, gate:UOp) -> UOp:
-    # print(f"checking gate {u.op} {len(u.src)}, {gate.op} {len(gate.src)}")
-    if u.op is UOps.BARRIER: return u
+    if u.op in {UOps.BARRIER, UOps.EXPAND}: return u
+    # if u.op is UOps.BARRIER: return u
     if u.op is UOps.LOAD and u.src[-1].op is UOps.BARRIER:
       # NOTE: gate could already be wrapped in an IF, so only take IF's src[0] in that case. Will join IFs in delete_redundant_gates
       return UOp(u.op, u.dtype, u.src[:-1] + (UOp(UOps.IF, None, (gate if gate.op is not UOps.IF else gate.src[0], u.src[-1])),), u.arg)
-    if u.op is UOps.STORE and len(u.src) == 4 and u.src[-1].op is not UOps.IF:
-      # print(f"gated store {u.src[3].op} with ref to {u.src[2].op}")
-      if u.src[-1].op is UOps.EXPAND and u.src[-1].src[0].op is UOps.IF: return u
-      # return UOp(u.op, u.dtype, u.src[:-1] + (UOp(UOps.IF, None, (u.src[3],)),), u.arg)
-      return UOp(u.op, u.dtype, u.src[:-1] + (UOp(UOps.IF, None, (u.src[3], u.src[2])),), u.arg)
+    if u.op is UOps.STORE and len(u.src) == 4 and u.src[-1].op in {UOps.ALU, UOps.CAST}:
+      return UOp(u.op, u.dtype, u.src[:-1] + (UOp(UOps.IF, None, (gate, u.src[2])),), u.arg)
     return u if (replace_source:=tuple(_gate_srcs(x, gate) for x in u.src)) == u.src else UOp(u.op, u.dtype, replace_source, u.arg)
   return None if len(root.src) == 3 or (ret:=_gate_srcs(root, root.src[3])) is root else ret
 
