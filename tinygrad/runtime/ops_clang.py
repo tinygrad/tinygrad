089d2e6f00bf4b2858abf9be8f3b79a454cf57ce
Christopher Milan <chrismilan@ucla.edu>
2024-08-30 11:23:28 -0700

    no-jump-tables fixes some stuff

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -7,16 +7,16 @@ from tinygrad.runtime.autogen import libc, mac
 
 class ClangCompiler(Compiler):
   def compile(self, src:str) -> bytes:
+    triple = f"{platform.machine()}-none-unknown-elf"
+    asm = subprocess.check_output(['clang', '-march=native', '-O2', '-Wall', '-Werror', '-x', 'c', '-S', '-ffreestanding', '-nostdlib', '-target',
+                                   triple, '-fno-jump-tables', '-fno-math-errno', '-', '-o', '-'], input=src.encode('utf-8'))
+    lines, data, func = asm.decode('utf-8').split('\n'), [], []
+    for l in lines:
+      if '.globl' in l: func.append(l)
+      elif len(func) == 0 and '.section' not in l: data.append(l)
+      elif len(func): func.append(l.replace('adrp', 'adr')) # this is really stupid
     # TODO: remove file write. sadly clang doesn't like the use of /dev/stdout here
     with tempfile.NamedTemporaryFile(delete=True) as output_file:
-      asm = subprocess.check_output(['clang', '-march=native', '-O2', '-Wall', '-Werror', '-x', 'c', '-S', '-ffreestanding', '-nostdlib', '-target',
-                                     triple:=f'{platform.machine()}-none-unknown-elf', '-fno-math-errno', '-', '-o', '-'], input=src.encode('utf-8'))
-      lines, data, func = asm.decode('utf-8').split('\n'), [], []
-      for l in lines:
-        if '.globl' in l: func.append(l)
-        elif len(func) == 0 and '.section' not in l: data.append(l)
-        elif len(func) and '.section' in l: break
-        elif len(func): func.append(l.replace('adrp', 'adr')) # this is really stupid
       subprocess.check_output(['clang', '-target', triple, '-x', 'assembler', '-c', '-', '-o', str(output_file.name)],
                               input=('\n'.join(func+data)+'\n').encode('utf-8'))
       return pathlib.Path(output_file.name).read_bytes()
@@ -24,7 +24,6 @@ class ClangCompiler(Compiler):
 class ClangProgram:
   def __init__(self, name:str, lib:bytes):
     if DEBUG >= 6: cpu_objdump(lib)
-    with open('test.o', 'wb') as f: f.write(lib)
     self.name, self.lib = name, lib
     image = elf_loader(lib)[0]
     addr = libc.mmap(0, len(image), mmap.PROT_READ | mmap.PROT_WRITE, mmap.MAP_ANON | mmap.MAP_PRIVATE, -1, 0)
