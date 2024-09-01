dab98bd6b8a2c648356a1d1c7405a885883f672f
Jovan Sardinha <jovan.sardinha@gmail.com>
2024-08-31 17:32:07 -0700

    tests to check behavior of _time_program

--- test/test_search.py
+++ test/test_search.py
@@ -4,7 +4,7 @@ from tinygrad.codegen.kernel import Opt, OptOps
 from tinygrad.codegen.kernel import Kernel
 from tinygrad.ops import UOp, UOps, BinaryOps
 from tinygrad.engine.schedule import create_schedule
-from tinygrad.engine.search import time_linearizer, bufs_from_lin, actions, beam_search
+from tinygrad.engine.search import time_linearizer, bufs_from_lin, actions, beam_search, _try_compile_linearized_w_idx
 from tinygrad.device import Device, Buffer
 from tinygrad.tensor import Tensor
 from tinygrad.dtype import dtypes, PtrDType
@@ -12,12 +12,44 @@ from tinygrad.helpers import Context, GlobalCounters
 from tinygrad.engine.realize import capturing
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.view import View
-from unittest.mock import Mock, patch
-from tinygrad.engine.search import _time_program, _get_test_global_size
-from tinygrad.engine.realize import CompiledRunner
-from tinygrad.shape.symbolic import Variable
-from dataclasses import dataclass, field
-from functools import reduce
+from tinygrad.engine.search import _time_program
+import math
+
+
+def get_ast_helper():
+  # taken from https://github.com/tinygrad/tinygrad/issues/4612
+  ast = UOp(UOps.SINK, None, arg=None, src=(
+    UOp(UOps.STORE, None, arg=None, src=(
+      UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=0, src=()),
+      UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(1, 1, 256), strides=(0, 0, 1), offset=0, mask=None, contiguous=True),)), src=()), # noqa: E501
+      UOp(UOps.REDUCE_AXIS, dtypes.float, arg=(BinaryOps.MAX, (1,)), src=(
+        UOp(UOps.ALU, dtypes.float, arg=BinaryOps.MUL, src=(
+          UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
+            UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
+              UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
+                UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
+                  UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
+                    UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+                      UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=1, src=()),
+                      UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=0, mask=((0, 64128),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)), # noqa: E501
+                    UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+                      UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=2, src=()),
+                      UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-64128, mask=((64128, 128256),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
+                  UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+                    UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=3, src=()),
+                    UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-128256, mask=((128256, 192384),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
+                UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+                  UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=4, src=()),
+                  UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-192384, mask=((192384, 256512),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
+              UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+                UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=5, src=()),
+                UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-256512, mask=((256512, 320640),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
+            UOp(UOps.LOAD, dtypes.float, arg=None, src=(
+              UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=6, src=()),
+              UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-320640, mask=((320640, 384768),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
+          UOp(UOps.CONST, dtypes.float, arg=1.4285714285714286, src=(
+            UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(1, 501, 256), strides=(0, 0, 0), offset=0, mask=None, contiguous=False),)), src=()),)),)),)),)),)) # noqa: E501
+  return ast
 
 class TestTimeLinearizer(unittest.TestCase):
   def test_reasonable_time(self):
@@ -107,38 +139,7 @@ class TestBEAM(unittest.TestCase):
       assert len([x for x in lins if x.applied_opts[0] == Opt(OptOps.GROUPTOP, axis=0, amt=3)]) == 0, "did not de-dup GROUPTOP"
 
   def test_filter_global_buffer(self):
-    # taken from https://github.com/tinygrad/tinygrad/issues/4612
-    ast = UOp(UOps.SINK, None, arg=None, src=(
-      UOp(UOps.STORE, None, arg=None, src=(
-        UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=0, src=()),
-        UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(1, 1, 256), strides=(0, 0, 1), offset=0, mask=None, contiguous=True),)), src=()), # noqa: E501
-        UOp(UOps.REDUCE_AXIS, dtypes.float, arg=(BinaryOps.MAX, (1,)), src=(
-          UOp(UOps.ALU, dtypes.float, arg=BinaryOps.MUL, src=(
-            UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
-              UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
-                UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
-                  UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
-                    UOp(UOps.ALU, dtypes.float, arg=BinaryOps.ADD, src=(
-                      UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                        UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=1, src=()),
-                        UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=0, mask=((0, 64128),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)), # noqa: E501
-                      UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                        UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=2, src=()),
-                        UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-64128, mask=((64128, 128256),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
-                    UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                      UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=3, src=()),
-                      UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-128256, mask=((128256, 192384),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
-                  UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                    UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=4, src=()),
-                    UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-192384, mask=((192384, 256512),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
-                UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                  UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=5, src=()),
-                  UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-256512, mask=((256512, 320640),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
-              UOp(UOps.LOAD, dtypes.float, arg=None, src=(
-                UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.float), arg=6, src=()),
-                UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(384768,), strides=(1,), offset=-320640, mask=((320640, 384768),), contiguous=False), View(shape=(1, 501, 256), strides=(0, 1, 501), offset=256512, mask=None, contiguous=False))), src=()),)),)), # noqa: E501
-            UOp(UOps.CONST, dtypes.float, arg=1.4285714285714286, src=(
-              UOp(UOps.SHAPETRACKER, None, arg=ShapeTracker(views=(View(shape=(1, 501, 256), strides=(0, 0, 0), offset=0, mask=None, contiguous=False),)), src=()),)),)),)),)),)) # noqa: E501
+    ast = get_ast_helper()
     lin = Kernel(ast)
 
     bufs = bufs_from_lin(lin)
@@ -159,129 +160,31 @@ class TestBEAM(unittest.TestCase):
     beam_search(lin, bufs, 3, disable_cache=True)
     self.assertEqual(kcount, len(Kernel.kernel_cnt))
 
-@dataclass
-class MockProgram:
-  global_size: list
-  dname: str
-  uops: list = None
-  globals: list = field(default_factory=list)
-
-class MockDevice:
-  def __init__(self, has_invalidate=True):
-    if has_invalidate:
-      self.invalidate_caches = self._invalidate_caches
-
-  def _invalidate_caches(self):
-    pass
-
 class TestTimeProgram(unittest.TestCase):
   def setUp(self):
-    self.mock_compiled_runner = Mock(spec=CompiledRunner)
-    self.mock_compiled_runner.p = MockProgram(
-        global_size=None, dname="MockDevice", globals=[0])
-    self.mock_device = MockDevice()
-    self.mock_tensor = Mock(spec=Tensor)
+    ast = get_ast_helper()
+    k = Kernel(ast)
+    dev = Device[k.opts.device]
+    _, proc = _try_compile_linearized_w_idx((0, k), dev.compiler)
 
-  def test_time_program_basic(self):
-    p = MockProgram(global_size=None, dname="MockDevice", globals=[0])
-    lib = b"mock_lib"
-    var_vals = {}
-    rawbufs = [Mock()]
+    self.p, self.lib, self.compile_et = proc
+    self.var_vals = {k:(k.max+k.min)//2 for k in k.ast.variables()}
+    self.rawbufs = bufs_from_lin(k)
 
-    with patch('tinygrad.engine.search.CompiledRunner', return_value=self.mock_compiled_runner):
-      self.mock_compiled_runner.return_value = 0.1
-      result = _time_program(p, lib, var_vals, rawbufs, cnt=3)
+  def test_reasonable_time(self):
+    for i in range(10):
+      results = _time_program(self.p, self.lib, self.var_vals, self.rawbufs, cnt=i)
+      self.assertEqual(len(results), i)
+      self.assertTrue(all(isinstance(x, float) for x in results))
 
-    self.assertEqual(len(result), 3)
-    self.assertTrue(all(t == 0.1 for t in result))
+  def test_time_early_stop_compile_error(self):
+    invalid_lib = b'bplist00\xd4\x01\x02\x03\x04\x05'
+    for i in range(10):
+      with self.assertRaises(Exception):
+        results = _time_program(self.p, invalid_lib, self.var_vals, self.rawbufs, cnt=i)
+        self.assertEqual(len(results), i)
+        self.assertTrue(all(x == math.inf for x in results))
 
-  def test_time_program_early_stop(self):
-    p = MockProgram(global_size=None, dname="MockDevice", globals=[0])
-    lib = b"mock_lib"
-    var_vals = {}
-    rawbufs = [Mock()]
-
-    with patch('tinygrad.engine.search.CompiledRunner', return_value=self.mock_compiled_runner):
-      self.mock_compiled_runner.return_value = 0.2
-      result = _time_program(p, lib, var_vals, rawbufs, early_stop=0.1, cnt=3)
-
-    self.assertEqual(len(result), 1)
-    self.assertEqual(result[0], 0.2)
-
-  def test_time_program_adjust_global_size(self):
-    p = MockProgram(global_size=[1000, 1000], dname="MockDevice", globals=[0])
-    lib = b"mock_lib"
-    var_vals = {Variable("x", 1, 1000): 500}
-    rawbufs = [Mock()]
-
-    with patch('tinygrad.engine.search.CompiledRunner', return_value=self.mock_compiled_runner), \
-            patch('tinygrad.engine.search._get_test_global_size', return_value=([500, 500], 2)):
-      self.mock_compiled_runner.return_value = 0.1
-      result = _time_program(p, lib, var_vals, rawbufs,
-                             max_global_size=65536, cnt=1)
-
-    self.assertEqual(len(result), 1)
-    self.assertEqual(result[0], 0.2)  # 0.1 * 2 (factor)
-
-  def test_time_program_clear_cache_with_invalidate(self):
-    self._test_time_program_clear_cache(True, True)
-
-  def test_time_program_clear_cache_without_invalidate(self):
-    self._test_time_program_clear_cache(True, False)
-
-  def test_time_program_no_clear_cache(self):
-    self._test_time_program_clear_cache(False, True)
-
-  def _test_time_program_clear_cache(self, clear_l2, has_invalidate):
-    p = MockProgram(global_size=None, dname="MockDevice", globals=[0])
-    lib = b"mock_lib"
-    var_vals = {}
-    rawbufs = [Mock()]
-
-    mock_device = MockDevice(has_invalidate=has_invalidate)
-
-    with patch('tinygrad.engine.search.CompiledRunner', return_value=self.mock_compiled_runner), \
-            patch('tinygrad.engine.search.Device', {p.dname: mock_device}), \
-            patch('tinygrad.engine.search.Context') as mock_context:
-
-      self.mock_compiled_runner.return_value = 0.1
-      _time_program(p, lib, var_vals, rawbufs, clear_l2=clear_l2, cnt=1)
-
-      if clear_l2:
-        if has_invalidate:
-          self.assertTrue(hasattr(mock_device, 'invalidate_caches'))
-        else:
-          mock_context.assert_called_once_with(DEBUG=0, BEAM=0, CAPTURING=0)
-      else:
-        if has_invalidate:
-          self.assertTrue(hasattr(mock_device, 'invalidate_caches'))
-        mock_context.assert_not_called()
-
-  def test_time_program_compilation_error(self):
-    p = MockProgram(global_size=None, dname="MockDevice", globals=[0])
-    lib = b"mock_lib"
-    var_vals = {}
-    rawbufs = [Mock()]
-
-    with patch('tinygrad.engine.search.CompiledRunner', side_effect=AssertionError):
-      result = _time_program(p, lib, var_vals, rawbufs, cnt=3)
-
-    self.assertEqual(len(result), 3)
-    self.assertTrue(all(t == float('inf') for t in result))
-
-  def test_get_test_global_size(self):
-    global_size = [1000, 1000, 1000]
-    max_global_size = 1000000
-    var_vals = {Variable("x", 1, 1000): 500}
-
-    result_size, factor = _get_test_global_size(
-        global_size, max_global_size, var_vals)
-
-    self.assertEqual(len(result_size), 3)
-    self.assertTrue(all(size <= 1000 for size in result_size))
-    self.assertGreaterEqual(factor, 1)
-    self.assertLessEqual(
-        reduce(lambda a, b: a*b, result_size), max_global_size)
 
 if __name__ == '__main__':
   unittest.main()
