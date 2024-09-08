e3bb20bda322a3dfde6e914f778e2d4efdbe9fb6
qazal <qazal.software@gmail.com>
2024-09-08 10:29:12 +0200

    switch to lidx

--- test/test_renderer_failures.py
+++ test/test_renderer_failures.py
@@ -12,17 +12,17 @@ from tinygrad.renderer import Program
 from tinygrad.tensor import Tensor, _to_np_dtype
 from tinygrad.lazy import LazyBuffer
 
-def _test_uop_result(inputs:List[Tensor], stores:List[UOp], global_size=(1, 1, 1)):
+def _test_uop_result(inputs:List[Tensor], stores:List[UOp], local_size=(1, 1, 1)):
   for x in inputs: x.realize()
   # NOTE: we only toposort the stores
   uops: List[UOp] = []
   def _recursive_add(uop:UOp) -> List[UOp]: return flatten([_recursive_add(x) for x in uop.src])+[uop]
   uops = dedup(flatten(_recursive_add(st) for st in stores))
-  outbufs = [Buffer(Device.DEFAULT, prod(global_size), cast(DType,u.src[2].dtype), \
-      initial_value=np.zeros(prod(global_size), dtype=_to_np_dtype(cast(DType,u.src[2].dtype))).data) for u in uops if u.op is UOps.STORE]
+  outbufs = [Buffer(Device.DEFAULT, prod(local_size), cast(DType,u.src[2].dtype), \
+      initial_value=np.zeros(prod(local_size), dtype=_to_np_dtype(cast(DType,u.src[2].dtype))).data) for u in uops if u.op is UOps.STORE]
   inbufs = [cast(LazyBuffer,x.lazydata).base.buffer for x in inputs]
   src = Device[Device.DEFAULT].renderer.render("test", uops)
-  ei = CompiledRunner(Program("test", src, Device.DEFAULT, uops=uops, global_size=list(global_size)))
+  ei = CompiledRunner(Program("test", src, Device.DEFAULT, uops=uops, local_size=list(local_size)))
   ei.exec(outbufs+inbufs)
   return [np.frombuffer(x.as_buffer(), _to_np_dtype(x.dtype)) for x in outbufs]
 
@@ -39,25 +39,26 @@ class TestCStyleFailures(unittest.TestCase):
     ret = _test_uop_result([Tensor([1])], [store])[0]
     self.assertEqual(ret[0], 1)
 
+@unittest.skipUnless(Device[Device.DEFAULT].renderer.has_local, "need local")
 class TestPTXFailures(unittest.TestCase):
   def test_gated_store_with_alu(self):
     a = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0)
-    gate_alu = (gidx0:=UOp(UOps.SPECIAL, dtypes.int, (), ('gidx0', 4))).ne(0)
-    gated_alu_store = UOp(UOps.STORE, None, (a, gidx0, UOp.const(dtypes.int, 1), gate_alu))
+    gate_alu = (lidx0:=UOp(UOps.SPECIAL, dtypes.int, (), ('lidx0', 4))).ne(0)
+    gated_alu_store = UOp(UOps.STORE, None, (a, lidx0, UOp.const(dtypes.int, 1), gate_alu))
     sink = UOp(UOps.SINK, None, (gated_alu_store,))
     uops = linearize_uop(full_graph_rewrite(sink, Device[Device.DEFAULT].renderer))
-    ret = _test_uop_result([], uops, global_size=(4, 1, 1))[0]
+    ret = _test_uop_result([], uops, local_size=(4, 1, 1))[0]
     np.testing.assert_equal(ret, [0, 1, 1, 1])
 
   def test_gated_store_with_if(self):
     a = UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0)
-    gate_alu = (gidx0:=UOp(UOps.SPECIAL, dtypes.int, (), ('gidx0', 4))).ne(0)
+    gate_alu = (lidx0:=UOp(UOps.SPECIAL, dtypes.int, (), ('lidx0', 4))).ne(0)
     val = UOp.const(dtypes.int, 1)
     if_uop = UOp(UOps.IF, None, (gate_alu, val))
-    gated_alu_store = UOp(UOps.STORE, None, (a, gidx0, val, if_uop))
+    gated_alu_store = UOp(UOps.STORE, None, (a, lidx0, val, if_uop))
     sink = UOp(UOps.SINK, None, (gated_alu_store,))
     uops = linearize_uop(full_graph_rewrite(sink, Device[Device.DEFAULT].renderer))
-    ret = _test_uop_result([], uops, global_size=(4, 1, 1))[0]
+    ret = _test_uop_result([], uops, local_size=(4, 1, 1))[0]
 
     if getenv("PTX"):
       with self.assertRaises(AssertionError):
