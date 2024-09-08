bf22b44ae35172715fc66b8f4c2698bc403e9bb1
Francis Lata <francisalbertlata@gmail.com>
2024-09-07 21:38:48 -0700

    update kits19 dataset setup script

--- examples/mlperf/scripts/setup_kits19_dataset.sh
+++ examples/mlperf/scripts/setup_kits19_dataset.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/neheller/kits19
-cd kits19
-pip3 install -r requirements.txt
-python3 -m starter_code.get_imaging
-cd ..
-mv kits19 extra/datasets/
+if [ -z $BASEDIR ]; then
+  echo "Ensure that BASEDIR is set before running the script"
+else
+  cd $BASEDIR
+  if [ -d "kits19" ]; then
+    echo "kits19 dataset is already available"
+  else
+    echo "Downloading and preparing kits19 dataset at $BASEDIR"
+
+    git clone https://github.com/neheller/kits19
+    cd kits19
+    pip3 install -r requirements.txt
+    python3 -m starter_code.get_imaging
+
+    echo "Done"
+  fi
+fi
