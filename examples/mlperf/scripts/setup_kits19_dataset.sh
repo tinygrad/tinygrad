b15608082b458993ff1a068972474f1ab126168f
Francis Lata <francisalbertlata@gmail.com>
2024-09-08 05:51:08 -0700

    set default value for BASEDIR

--- examples/mlperf/scripts/setup_kits19_dataset.sh
+++ examples/mlperf/scripts/setup_kits19_dataset.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [ -z $BASEDIR ]; then
-  echo "Ensure that BASEDIR is set before running the script"
+  export BASEDIR="./extra/datasets/"
+fi
+
+cd $BASEDIR
+if [ -d "kits19" ]; then
+  echo "kits19 dataset is already available"
 else
-  cd $BASEDIR
-  if [ -d "kits19" ]; then
-    echo "kits19 dataset is already available"
-  else
-    echo "Downloading and preparing kits19 dataset at $BASEDIR"
+  echo "Downloading and preparing kits19 dataset at $BASEDIR"
 
-    git clone https://github.com/neheller/kits19
-    cd kits19
-    pip3 install -r requirements.txt
-    python3 -m starter_code.get_imaging
+  git clone https://github.com/neheller/kits19
+  cd kits19
+  pip3 install -r requirements.txt
+  python3 -m starter_code.get_imaging
 
-    echo "Done"
-  fi
+  echo "Done"
 fi
