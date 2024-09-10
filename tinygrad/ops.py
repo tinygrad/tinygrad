a7eda9aad6e8e2492cc32e9110b866652e9b16d7
chenyu <chenyu@fastmail.com>
2024-09-11 06:41:16 +0800

    fix UOp const_factor for ADD [run_process_replay]
    currently not used, fixed for completeness

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -411,7 +411,7 @@ class UOp(MathTrait):
     """largest known int that divides self"""
     if self.op is UOps.CONST: return self.arg
     if self.op is UOps.ALU:
-      if self.arg is BinaryOps.ADD: return math.gcd(self.src[0].const_factor(), self.src[0].const_factor())
+      if self.arg is BinaryOps.ADD: return math.gcd(self.src[0].const_factor(), self.src[1].const_factor())
       if self.arg is BinaryOps.MUL: return self.src[0].arg if self.src[0].op is UOps.CONST else self.src[1].arg if self.src[1].op is UOps.CONST else 1
     return 1
   def divides(self, v) -> Optional[UOp]:
