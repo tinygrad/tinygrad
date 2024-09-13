7b16b9f000c7e87c496e709efbc406d3d49d4eb6
George Hotz <geohot@gmail.com>
2024-09-13 16:07:21 +0800

    start viz tool

---
+++ viz/serve.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import pickle, json, re, os, sys, time, threading
+from tinygrad.ops import UOp
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+def reloader():
+  mtime = os.stat(__file__).st_mtime
+  while 1:
+    if mtime != os.stat(__file__).st_mtime:
+      print("reloading server...")
+      os.execv(sys.executable, [sys.executable] + sys.argv)
+    time.sleep(0.1)
+
+def uop_to_json(x:UOp):
+  assert isinstance(x, UOp)
+  ret = {}
+  for u in x.sparents: ret[id(u)] = (u.op, str(u.dtype), [id(x) for x in u.src], str(u.arg))
+  return json.dumps(ret).encode()
+
+class Handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    print(f"GET {self.path}")
+    if self.path == "/":
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      with open("index.html", "rb") as f:
+        ret = f.read()
+    elif re.search(r'/\d+', self.path):
+      self.send_response(200)
+      self.send_header('Content-type', 'application/json')
+      self.end_headers()
+      ret = uop_to_json(uops[int(self.path.split("/")[-1])][0])
+    else:
+      self.send_response(404)
+      ret = b""
+    return self.wfile.write(ret)
+
+if __name__ == "__main__":
+  threading.Thread(target=reloader).start()
+  with open("/tmp/rewrites.pkl", "rb") as f:
+    uops = pickle.load(f)
+  #print(uop_to_json(uops[0][0]))
+  print("serving at port 8000")
+  HTTPServer(('', 8000), Handler).serve_forever()
