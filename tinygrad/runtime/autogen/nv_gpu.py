d21b47a4e20721429b2c557fa2fc9c4ea0ed5790
terafo <terafo@protonmail.com>
2024-09-15 17:27:49 +0300

    Other NV fixes (it passes now)

--- tinygrad/runtime/autogen/nv_gpu.py
+++ tinygrad/runtime/autogen/nv_gpu.py
@@ -52,6 +52,7 @@ class AsDictMixin:
 
 
 class Structure(ctypes.Structure, AsDictMixin):
+    va_addr, size = None, None
 
     def __init__(self, *args, **kwds):
         # We don't want to use positional arguments fill PADDING_* fields
