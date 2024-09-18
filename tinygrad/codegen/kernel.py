b4364c6b529d8ab79724f1f50e7b879253ccde1d
p4sscode <p4ssenger.developer@gmail.com>
2024-09-18 11:23:49 -0300

    hotfix: hand-coded tc bug

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -351,8 +351,9 @@ class Kernel:
         else:
           if (self.opts.device == "CLANG" and AMX): return True # skip hand-coded TC opts if AMX, upcasting will make kernel slower
           # hand-coded TC opts
-          for dim in [tc_opts.axes[tc_dim] for tc_dim in [1, 0] if tc_opts.axes_exist[tc_dim]]: # attempt to upcast M and N
-            if (upc_sz := [sz for sz in [5,4,3,2] if self.full_shape[dim]%sz == 0]): self.apply_opt(Opt(OptOps.UPCAST, dim, upc_sz[0]))
+          for tc_dim in [tc_dim for tc_dim in [1,0] if tc_opts.axes_exist[tc_dim]]:
+            szs = [sz for sz in [5,4,3,2] if self.full_shape[tc_opts.axes[tc_dim]]%sz==0]
+            if szs: self.apply_opt(Opt(OptOps.UPCAST, tc_opts.axes[tc_dim], szs[0]))
 
           if self.tensor_core and tc_opts.axes_exist[0]: # attempt to local N
             for upc in [4,2]:
