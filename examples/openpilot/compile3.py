d02bb270b728ed38b74d1def6594b40a02f2e36b
George Hotz <72895+geohot@users.noreply.github.com>
2024-09-18 16:06:20 +0800

    add copyin copyout for image on GPU [run_process_replay] (#6580)
    * add copyin copyout for image on GPU [run_process_replay]
    
    * add timing
    
    * enqueue vs total run
    
    * it's failing but that's fine

--- examples/openpilot/compile3.py
+++ examples/openpilot/compile3.py
@@ -1,12 +1,12 @@
-import os, sys, pickle
+import os, sys, pickle, time
+import numpy as np
 if "FLOAT16" not in os.environ: os.environ["FLOAT16"] = "1"
 if "IMAGE" not in os.environ: os.environ["IMAGE"] = "2"
 if "NOLOCALS" not in os.environ: os.environ["NOLOCALS"] = "1"
 
 from tinygrad import fetch, Tensor, TinyJit, Device, Context, GlobalCounters
-from tinygrad.helpers import OSX, DEBUG
+from tinygrad.helpers import OSX, DEBUG, Timing
 from tinygrad.tensor import _from_np_dtype
-Device.DEFAULT = "GPU"   # should be QCOM on comma device
 
 import onnx
 from onnx.helper import tensor_dtype_to_np_dtype
@@ -14,7 +14,6 @@ from extra.onnx import get_run_onnx   # TODO: port to main tinygrad
 
 OPENPILOT_MODEL = sys.argv[1] if len(sys.argv) > 1 else "https://github.com/commaai/openpilot/raw/v0.9.7/selfdrive/modeld/models/supercombo.onnx"
 OUTPUT = "/tmp/openpilot.pkl"
-
 
 def compile():
   # hack to fix GPU on OSX: max doesn't work on half, see test/external/external_gpu_fail_osx.py
@@ -31,10 +30,10 @@ def compile():
   run_onnx = get_run_onnx(onnx_model)
   print("loaded model")
 
-  Tensor.manual_seed(100)
   input_shapes = {inp.name:tuple(x.dim_value for x in inp.type.tensor_type.shape.dim) for inp in onnx_model.graph.input}
   input_types = {inp.name: tensor_dtype_to_np_dtype(inp.type.tensor_type.elem_type) for inp in onnx_model.graph.input}
-  new_inputs = {k:Tensor.randn(*shp, dtype=_from_np_dtype(input_types[k])).mul(8).realize() for k,shp in input_shapes.items()}
+  Tensor.manual_seed(100)
+  new_inputs = {k:Tensor.randn(*shp, dtype=_from_np_dtype(input_types[k])).mul(8).realize() for k,shp in sorted(input_shapes.items())}
   print("created tensors")
 
   run_onnx_jit = TinyJit(lambda **kwargs: run_onnx(kwargs), prune=True)
@@ -43,7 +42,10 @@ def compile():
     print(f"run {i}")
     with Context(DEBUG=max(DEBUG.value, 2 if i == 2 else 1)):
       ret = next(iter(run_onnx_jit(**new_inputs).values())).cast('float32').numpy()
+    if i == 0: test_val = np.copy(ret)
   print(f"captured {len(run_onnx_jit.captured.jit_cache)} kernels")
+  np.testing.assert_equal(test_val, ret)
+  print("jit run validated")
 
   with open(OUTPUT, "wb") as f:
     pickle.dump(run_onnx_jit, f)
@@ -52,17 +54,26 @@ def compile():
   print(f"mdl size is {mdl_sz/1e6:.2f}M")
   print(f"pkl size is {pkl_sz/1e6:.2f}M")
   print("**** compile done ****")
+  return test_val
 
-def test():
+def test(test_val):
   with open(OUTPUT, "rb") as f:
     run = pickle.load(f)
-  new_inputs = {nm:Tensor.randn(*st.shape, dtype=dtype) for nm, (st, _, dtype, _) in
-                zip(run.captured.expected_names, run.captured.expected_st_vars_dtype_device)}
-  out = run(**new_inputs)
-  val = out['outputs'].numpy()
+  Tensor.manual_seed(100)
+  new_inputs = {nm:Tensor.randn(*st.shape, dtype=dtype).mul(8).realize() for nm, (st, _, dtype, _) in
+                sorted(zip(run.captured.expected_names, run.captured.expected_st_vars_dtype_device))}
+  for _ in range(20):
+    st = time.perf_counter()
+    out = run(**new_inputs)
+    mt = time.perf_counter()
+    val = out['outputs'].numpy()
+    et = time.perf_counter()
+    print(f"enqueue {(mt-st)*1e3:6.2f} ms -- total run {(et-st)*1e3:6.2f} ms")
   print(out, val.shape, val.dtype)
+  np.testing.assert_equal(test_val, val)
+  print("**** test done ****")
 
 if __name__ == "__main__":
-  compile()
-  test()
+  test_val = compile()
+  test(test_val)
 
