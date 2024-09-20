76aa6416d7caf924fee20e218a790c5535f4c11d
Comma Device <device@comma.ai>
2024-09-20 07:04:28 +0000

    qcom: add disassembler with DEBUG >= 5

--- tinygrad/runtime/ops_qcom.py
+++ tinygrad/runtime/ops_qcom.py
@@ -6,7 +6,7 @@ from tinygrad.device import BufferOptions, HCQBuffer, HWComputeQueue, HCQProgram
 from tinygrad.runtime.autogen import kgsl, adreno, libc
 from tinygrad.runtime.ops_gpu import CLCompiler, CLDevice
 from tinygrad.renderer.cstyle import QCOMRenderer
-from tinygrad.helpers import getenv, from_mv, mv_address, to_mv, round_up, data64_le, prod
+from tinygrad.helpers import getenv, from_mv, mv_address, to_mv, round_up, data64_le, prod, DEBUG, fromimport
 if getenv("IOCTL"): import extra.qcom_gpu_driver.opencl_ioctl  # noqa: F401  # pylint: disable=unused-import
 
 def _qreg_exec(reg, __val=0, **kwargs):
@@ -219,6 +219,7 @@ class QCOMArgsState(HCQArgsState):
 class QCOMProgram(HCQProgram):
   def __init__(self, device: QCOMDevice, name: str, lib: bytes):
     self.device, self.name, self.lib = device, name, lib
+    if DEBUG >= 5: fromimport('extra.disassemblers.adreno', 'disasm')(lib)
     self._parse_lib()
 
     self.lib_gpu = self.device.allocator.alloc(self.image_size, options=BufferOptions(cpu_access=True, nolru=True))
