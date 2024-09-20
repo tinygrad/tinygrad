e811e93d3df1a4328d4a75711d7c1d8faaef928e
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-09-20 04:17:43 +0000

    qcom texs refactor

--- tinygrad/runtime/ops_qcom.py
+++ tinygrad/runtime/ops_qcom.py
@@ -123,39 +123,39 @@ class QCOMComputeQueue(HWComputeQueue):
              qreg.a6xx_sp_cs_pvt_mem_size(totalpvtmemsize=prg.pvtmem_size_total))
 
     self.cmd(adreno.CP_LOAD_STATE6_FRAG, qreg.cp_load_state6_0(state_type=adreno.ST_CONSTANTS, state_src=adreno.SS6_INDIRECT,
-                                                               state_block=adreno.SB6_CS_SHADER, num_unit=prg.kernargs_alloc_size // 4),
+                                                               state_block=adreno.SB6_CS_SHADER, num_unit=1024 // 4),
              *data64_le(args_state.ptr))
     self.cmd(adreno.CP_LOAD_STATE6_FRAG, qreg.cp_load_state6_0(state_type=adreno.ST_SHADER, state_src=adreno.SS6_INDIRECT,
                                                                state_block=adreno.SB6_CS_SHADER, num_unit=round_up(prg.image_size, 128) // 128),
              *data64_le(prg.lib_gpu.va_addr))
 
     self.reg(adreno.REG_A6XX_HLSQ_CONTROL_2_REG, 0xfcfcfcfc, 0xfcfcfcfc, 0xfcfcfcfc, 0xfc,
-             qreg.a6xx_hlsq_cs_cntl(constlen=prg.kernargs_alloc_size // 4, enabled=True))
+             qreg.a6xx_hlsq_cs_cntl(constlen=1024 // 4, enabled=True))
 
     self.reg(adreno.REG_A6XX_SP_CS_PVT_MEM_HW_STACK_OFFSET, qreg.a6xx_sp_cs_pvt_mem_hw_stack_offset(prg.hw_stack_offset))
     self.reg(adreno.REG_A6XX_SP_CS_INSTRLEN, qreg.a6xx_sp_cs_instrlen(prg.image_size // 4))
 
-    if hasattr(args_state, 'samplers_ptr'):
+    if args_state.prg.samp_cnt > 0:
       self.cmd(adreno.CP_LOAD_STATE6_FRAG, qreg.cp_load_state6_0(state_type=adreno.ST_SHADER, state_src=adreno.SS6_INDIRECT,
-                                                                 state_block=adreno.SB6_CS_TEX, num_unit=args_state.samplers_cnt),
-               *data64_le(args_state.samplers_ptr.va_addr))
-      self.reg(adreno.REG_A6XX_SP_CS_TEX_SAMP, *data64_le(args_state.samplers_ptr.va_addr))
+                                                                 state_block=adreno.SB6_CS_TEX, num_unit=args_state.prg.samp_cnt),
+               *data64_le(args_state.ptr + args_state.prg.samp_off))
+      self.reg(adreno.REG_A6XX_SP_CS_TEX_SAMP, *data64_le(args_state.ptr + args_state.prg.samp_off))
       self.reg(adreno.REG_A6XX_SP_PS_TP_BORDER_COLOR_BASE_ADDR, *data64_le(prg.device._border_color_base()))
 
-    if hasattr(args_state, 'descriptors_ptr'):
+    if args_state.prg.tex_cnt > 0:
       self.cmd(adreno.CP_LOAD_STATE6_FRAG, qreg.cp_load_state6_0(state_type=adreno.ST_CONSTANTS, state_src=adreno.SS6_INDIRECT,
-                                                                 state_block=adreno.SB6_CS_TEX, num_unit=args_state.descriptors_cnt),
-               *data64_le(args_state.descriptors_ptr.va_addr))
-      self.reg(adreno.REG_A6XX_SP_CS_TEX_CONST, *data64_le(args_state.descriptors_ptr.va_addr))
+                                                                 state_block=adreno.SB6_CS_TEX, num_unit=args_state.prg.tex_cnt),
+               *data64_le(args_state.ptr + args_state.prg.tex_off))
+      self.reg(adreno.REG_A6XX_SP_CS_TEX_CONST, *data64_le(args_state.ptr + args_state.prg.tex_off))
 
-    if hasattr(args_state, 'ibos_ptr'):
+    if args_state.prg.ibo_cnt > 0:
       self.cmd(adreno.CP_LOAD_STATE6_FRAG, qreg.cp_load_state6_0(state_type=adreno.ST6_IBO, state_src=adreno.SS6_INDIRECT,
-                                                                 state_block=adreno.SB6_CS_SHADER, num_unit=args_state.ibos_cnt),
-               *data64_le(args_state.ibos_ptr.va_addr))
-      self.reg(adreno.REG_A6XX_SP_CS_IBO, *data64_le(args_state.ibos_ptr.va_addr))
+                                                                 state_block=adreno.SB6_CS_SHADER, num_unit=args_state.prg.ibo_cnt),
+               *data64_le(args_state.ptr + args_state.prg.ibo_off))
+      self.reg(adreno.REG_A6XX_SP_CS_IBO, *data64_le(args_state.ptr + args_state.prg.ibo_off))
 
     self.reg(adreno.REG_A6XX_SP_CS_CONFIG,
-             qreg.a6xx_sp_cs_config(enabled=True, nsamp=args_state.samplers_cnt, ntex=args_state.descriptors_cnt, nibo=args_state.ibos_cnt))
+             qreg.a6xx_sp_cs_config(enabled=True, nsamp=args_state.prg.samp_cnt, ntex=args_state.prg.tex_cnt, nibo=args_state.prg.ibo_cnt))
     self.cmd(adreno.CP_RUN_OPENCL, 0)
     self._cache_flush(write_back=True, invalidate=False, sync=False, memsync=False)
 
@@ -175,46 +175,54 @@ class QCOMComputeQueue(HWComputeQueue):
 class QCOMArgsState(HCQArgsState):
   def __init__(self, ptr:int, prg:QCOMProgram, bufs:Tuple[HCQBuffer, ...], vals:Tuple[int, ...]=()):
     super().__init__(ptr, prg, bufs, vals=vals)
-    self.ibos_cnt, self.descriptors_cnt, self.samplers_cnt = 0, 0, 0
-    ctypes.memset(ptr, 0, 1024)
+    # self.ibos_cnt, self.descriptors_cnt, self.samplers_cnt = 0, 0, 0
+    ctypes.memset(self.ptr, 0, 1024)
 
-    if len(bufs) + len(vals) != len(prg.buffs_info): raise RuntimeError(f'incorrect args size given={len(bufs)} != want={len(prg.buffs_info)}')
-    self.boffs, self.aoffs = prg.buffs_info[:len(bufs)], prg.buffs_info[len(bufs):]
-    for i, v in enumerate(vals): self.update_var(i, v)
+    if len(bufs) + len(vals) != len(prg.buf_info): raise RuntimeError(f'incorrect args size given={len(bufs)+len(vals)} != want={len(prg.buf_info)}')
+    # self.boffs, self.aoffs = prg.buffs_info[:len(bufs)], prg.buffs_info[len(bufs):]
+
+    self.buf_info, self.args_info = prg.buf_info[:len(bufs)], prg.buf_info[len(bufs):]
+
     for cnst_val, cnst_off, cnst_sz in prg.consts_info:
       ctypes.memmove(self.ptr + cnst_off, (ctypes.c_int8 * cnst_sz).from_buffer_copy(cnst_val.to_bytes(cnst_sz, byteorder='little')), cnst_sz)
 
-    samplers: List[Any] = []
-    descriptors: List[Any] = []
-    ibos: List[Any] = []
-    self.i2descr: Dict[int, int] = {}
-    self.i2ibo: Dict[int, int] = {}
+    if prg.samp_cnt > 0: to_mv(self.ptr + prg.samp_off, len(prg.samplers) * 4).cast('I')[:] = array.array('I', prg.samplers)
     for i, b in enumerate(bufs):
-      if not hasattr(b, 'samplers') and not hasattr(b, 'descriptor') and not hasattr(b, 'ibo'): self.update_buffer(i, b)
-      elif self.boffs[i][1]: ibos, self.i2ibo = [*ibos, *getattr(b, 'ibo')], {**self.i2ibo, i: len(ibos)}
-      else:
-        samplers, descriptors = [*samplers, *getattr(b, 'samplers')], [*descriptors, *getattr(b, 'descriptor')]
-        self.i2descr[i] = len(descriptors) - 1
+      if prg.buf_info[i].is_texture: to_mv(self.ptr + prg.buf_info[i].offset, len(b.descriptor) * 4).cast('I')[:] = array.array('I', b.descriptor)
+      elif prg.buf_info[i].is_ibo: to_mv(self.ptr + prg.buf_info[i].offset, len(b.ibo) * 4).cast('I')[:] = array.array('I', b.ibo)
+      else: self.update_buffer(i, b)
+    for i, v in enumerate(vals): self.update_var(i, v)
 
-    def alloc_tex_gpu(data, chunk_size) -> Tuple[HCQBuffer, int]:
-      tex_gpu = self.prg.device.allocator.alloc(len(data) * 4, BufferOptions(nolru=True, cpu_access=True))
-      to_mv(tex_gpu.va_addr, len(data) * 4).cast('I')[:] = array.array('I', data)
-      return tex_gpu, len(data) // chunk_size
+    # samplers: List[Any] = []
+    # descriptors: List[Any] = []
+    # ibos: List[Any] = []
+    # self.i2descr: Dict[int, int] = {}
+    # self.i2ibo: Dict[int, int] = {}
+    # for i, b in enumerate(bufs):
+    #   if not hasattr(b, 'samplers') and not hasattr(b, 'descriptor') and not hasattr(b, 'ibo'): self.update_buffer(i, b)
+    #   elif self.boffs[i][1]: ibos, self.i2ibo = [*ibos, *getattr(b, 'ibo')], {**self.i2ibo, i: len(ibos)}
+    #   else:
+    #     samplers, descriptors = [*samplers, *getattr(b, 'samplers')], [*descriptors, *getattr(b, 'descriptor')]
+    #     self.i2descr[i] = len(descriptors) - 1
 
-    if len(samplers): self.samplers_ptr, self.samplers_cnt = alloc_tex_gpu(samplers, 4)
-    if len(descriptors): self.descriptors_ptr, self.descriptors_cnt = alloc_tex_gpu(descriptors, 16)
-    if len(ibos): self.ibos_ptr, self.ibos_cnt = alloc_tex_gpu(ibos, 16)
+    # def alloc_tex_gpu(data, chunk_size) -> Tuple[HCQBuffer, int]:
+    #   tex_gpu = self.prg.device.allocator.alloc(len(data) * 4, BufferOptions(nolru=True, cpu_access=True))
+    #   to_mv(tex_gpu.va_addr, len(data) * 4).cast('I')[:] = array.array('I', data)
+    #   return tex_gpu, len(data) // chunk_size
 
-  def __del__(self):
-    for ptr in ('samplers_ptr', 'descriptors_ptr', 'ibos_ptr'):
-      if hasattr(self, ptr): self.prg.device.allocator.free((x:=getattr(self, ptr)), x.size, BufferOptions(nolru=True, cpu_access=True))
+    # if len(samplers): self.samplers_ptr, self.samplers_cnt = alloc_tex_gpu(samplers, 4)
+    # if len(descriptors): self.descriptors_ptr, self.descriptors_cnt = alloc_tex_gpu(descriptors, 16)
+    # if len(ibos): self.ibos_ptr, self.ibos_cnt = alloc_tex_gpu(ibos, 16)
+
+  # def __del__(self):
+  #   for ptr in ('samplers_ptr', 'descriptors_ptr', 'ibos_ptr'):
+  #     if hasattr(self, ptr): self.prg.device.allocator.free((x:=getattr(self, ptr)), x.size, BufferOptions(nolru=True, cpu_access=True))
 
   def update_buffer(self, index:int, buf:HCQBuffer):
-    if (descr:=self.i2descr.get(index, None)) is not None: to_mv(self.descriptors_ptr.va_addr + 16 * descr + 4 * 4, 8).cast('Q')[0] = buf.va_addr
-    elif (ibo:=self.i2ibo.get(index, None)) is not None: to_mv(self.ibos_ptr.va_addr + 16 * ibo + 4 * 4, 8).cast('Q')[0] = buf.va_addr
-    else: to_mv(self.ptr + self.boffs[index][0], 8).cast('Q')[0] = buf.va_addr
+    if self.buf_info[index].is_texture or self.buf_info[index].is_ibo: to_mv(self.ptr+self.buf_info[index].offset+0x10, 8).cast('Q')[0] = buf.va_addr
+    else: to_mv(self.ptr + self.buf_info[index].offset, 8).cast('Q')[0] = buf.va_addr
 
-  def update_var(self, index:int, val:int): to_mv(self.ptr + self.aoffs[index][0], 8).cast('Q')[0] = val
+  def update_var(self, index:int, val:int): to_mv(self.ptr + self.args_info[index].offset, 8).cast('Q')[0] = val
 
 class QCOMProgram(HCQProgram):
   def __init__(self, device: QCOMDevice, name: str, lib: bytes):
@@ -232,7 +240,7 @@ class QCOMProgram(HCQProgram):
     self.max_threads = min(1024, ((384 * 32) // (max(1, (self.fregs + round_up(self.hregs, 2) // 2)) * 128)) * 128)
     device._ensure_stack_size(self.hw_stack_offset * 4)
 
-    super().__init__(QCOMArgsState, self.device, self.name, kernargs_alloc_size=1024)
+    super().__init__(QCOMArgsState, self.device, self.name, kernargs_alloc_size=1024 + (self.tex_cnt + self.ibo_cnt) * 0x40 + self.samp_cnt * 0x10)
 
   def __call__(self, *bufs, global_size:Tuple[int,int,int]=(1,1,1), local_size:Tuple[int,int,int]=(1,1,1), vals:Tuple[int, ...]=(), wait=False):
     if self.max_threads < prod(local_size): raise RuntimeError("Too many resources requsted for launch")
@@ -253,19 +261,45 @@ class QCOMProgram(HCQProgram):
     self.pvtmem, self.shmem = _read_lib(image_desc_off+0xc8), _read_lib(image_desc_off+0xd8)
 
     # Fill up constants and buffers info
-    self.buffs_info, self.consts_info = [], []
+    self.buf_info, self.consts_info = [], []
 
-    samplers_count = _read_lib(image_desc_off + 0xdc)
-    bdoff = round_up(image_desc_off + 0x158 + len(self.name), 4) + 8 * samplers_count
-    while (bdoff + 16 <= len(self.lib)):
-      length, _, _, offset_words = struct.unpack("I" * 4, self.lib[bdoff:bdoff+16])
+    # samplers_count = _read_lib(image_desc_off + 0xdc)
+    # bdoff = round_up(image_desc_off + 0x158 + len(self.name), 4) + 8 * samplers_count
+    # while bdoff + 16 <= len(self.lib):
+    #   length, _, _, offset_words = struct.unpack("I" * 4, self.lib[bdoff:bdoff+16])
+    #   if length == 0: break
+    #   self.buffs_info.append((offset_words * 4, struct.unpack("I", self.lib[bdoff+0x3c:bdoff+0x40])[0] == 0x0))
+    #   bdoff += length
+
+    # Collect sampler info.
+    self.samp_cnt = _read_lib(image_desc_off + 0xdc)
+    assert self.samp_cnt <= 1, "Up to one sampler supported"
+    if self.samp_cnt:
+      self.samplers = [qreg.a6xx_tex_samp_0(wrap_s=(clamp_mode:=adreno.A6XX_TEX_CLAMP_TO_BORDER), wrap_t=clamp_mode, wrap_r=clamp_mode),
+                       qreg.a6xx_tex_samp_1(unnorm_coords=True, cubemapseamlessfiltoff=True), 0, 0]
+
+    # Collect kernel arguments (buffers) info.
+    bdoff = round_up(image_desc_off + 0x158 + len(self.name), 4) + 8 * self.samp_cnt
+    while bdoff + 16 <= len(self.lib):
+      length, _, _, offset_words = struct.unpack("IIII", self.lib[bdoff:bdoff+16])
       if length == 0: break
-      self.buffs_info.append((offset_words * 4, struct.unpack("I", self.lib[bdoff+0x3c:bdoff+0x40])[0] == 0x0))
+      is_image, is_wronly = offset_words == 0, struct.unpack("I", self.lib[bdoff+0x3c:bdoff+0x40])[0] == 0x0
+      self.buf_info.append(SimpleNamespace(offset=offset_words * 4, is_texture=is_image and (not is_wronly), is_ibo=is_image and is_wronly))
       bdoff += length
+
+    # Setting correct offsets to textures/ibos.
+    self.tex_cnt, self.ibo_cnt = sum(x.is_texture for x in self.buf_info), sum(x.is_ibo for x in self.buf_info)
+    self.samp_off, self.ibo_off, self.tex_off = 1024, 1024 + 0x10 * self.samp_cnt, 1024 + 0x10 * self.samp_cnt + 0x40 * self.ibo_cnt
+    cur_ibo_off, cur_tex_off = self.ibo_off, self.tex_off
+    for x in self.buf_info:
+      if x.is_ibo: x.offset, cur_ibo_off = cur_ibo_off, cur_ibo_off + 0x40
+      elif x.is_texture: x.offset, cur_tex_off = cur_tex_off, cur_tex_off + 0x40
+
+    print(self.buf_info)
 
     if _read_lib(0xb0) != 0: # check if we have constants.
       cdoff = _read_lib(0xac)
-      while (cdoff + 40 <= image_offset):
+      while cdoff + 40 <= image_offset:
         cnst, offset_words, _, is32 = struct.unpack("I", self.lib[cdoff:cdoff+4])[0], *struct.unpack("III", self.lib[cdoff+16:cdoff+28])
         self.consts_info.append((cnst, offset_words * (sz_bytes:=(2 << is32)), sz_bytes))
         cdoff += 40
@@ -291,11 +325,11 @@ class QCOMAllocator(HCQAllocator):
       texture = self.device._gpu_alloc(pitch * round_up(imgh, 16), kgsl.KGSL_MEMTYPE_TEXTURE, map_to_cpu=True)
 
       # Extend HCQBuffer with texture-related info.
-      texture.samplers, texture.descriptor, texture.ibo = [0] * 4, [0] * 16, [0] * 16
+      texture.descriptor, texture.ibo = [0] * 16, [0] * 16
 
       # Compiled sampler (always the same in tinygrad).
-      texture.samplers[0] = qreg.a6xx_tex_samp_0(wrap_s=(clamp_mode:=adreno.A6XX_TEX_CLAMP_TO_BORDER), wrap_t=clamp_mode, wrap_r=clamp_mode)
-      texture.samplers[1] = qreg.a6xx_tex_samp_1(unnorm_coords=True, cubemapseamlessfiltoff=True)
+      # texture.samplers[0] = qreg.a6xx_tex_samp_0(wrap_s=(clamp_mode:=adreno.A6XX_TEX_CLAMP_TO_BORDER), wrap_t=clamp_mode, wrap_r=clamp_mode)
+      # texture.samplers[1] = qreg.a6xx_tex_samp_1(unnorm_coords=True, cubemapseamlessfiltoff=True)
 
       tex_fmt = adreno.FMT6_32_32_32_32_FLOAT if options.image.itemsize == 4 else adreno.FMT6_16_16_16_16_FLOAT
       texture.descriptor[0] = qreg.a6xx_tex_const_0(swiz_x=0, swiz_y=1, swiz_z=2, swiz_w=3, fmt=tex_fmt)
