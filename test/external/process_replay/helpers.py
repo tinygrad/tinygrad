5bafed2f88b5bb817f336eb94aac50806284d3e6
qazal <77887910+Qazalin@users.noreply.github.com>
2024-09-21 16:53:34 +0800

    process replay traceback (#6642)

--- test/external/process_replay/helpers.py
+++ test/external/process_replay/helpers.py
@@ -1,5 +1,7 @@
-import difflib, logging
-from tinygrad.helpers import colored, getenv
+from dataclasses import dataclass
+import difflib, logging, subprocess, traceback
+from typing import Dict, Optional
+from tinygrad.helpers import ContextVar, colored, getenv
 
 def print_diff(s0, s1, unified=getenv("UNIFIED_DIFF",1)):
   if not logging.getLogger().hasHandlers(): logging.basicConfig(level=logging.INFO, format="%(message)s")
@@ -10,3 +12,18 @@ def print_diff(s0, s1, unified=getenv("UNIFIED_DIFF",1)):
     import ocdiff
     diff = ocdiff.console_diff(str(s0), str(s1))
   logging.info(diff)
+
+@dataclass(frozen=True)
+class ProcessReplayContext:
+  ctx_vars: Dict[str, int]
+  loc: str = ""
+  head_sha: Optional[str] = None
+  run_id: Optional[int] = None
+def get_process_replay_ctx() -> ProcessReplayContext:
+  ctx_vars = {k:v.value for k,v in ContextVar._cache.items()}
+  try: head_sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode()
+  except Exception: head_sha = ""
+  stack = filter(lambda x: not any(n in x.filename for n in ["engine/schedule.py", "engine/realize.py", "codegen/kernel.py", "unittest"]),
+                 traceback.extract_stack()[:-1])
+  loc = "\n".join(traceback.format_list(stack))
+  return ProcessReplayContext(ctx_vars, loc, head_sha, getenv("GITHUB_RUN_ID") or None)
