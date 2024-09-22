4940b1230985c8f3e9bdb2115ba6f9bc844175c1
Mesozoic Egg <mesozoic.egg@proton.me>
2024-09-22 17:39:26 +0800

    more mypy error fix

--- tinygrad/runtime/support/metal.py
+++ tinygrad/runtime/support/metal.py
@@ -1,6 +1,6 @@
 from ctypes import CDLL, c_void_p, c_char_p, Structure, c_ulong
 from ctypes.macholib.dyld import dyld_find  # pyright: ignore
-from typing import cast, Any
+from typing import cast, Any, List, Tuple
 
 class objc_id(c_void_p): pass
 
@@ -26,14 +26,14 @@ def send_message(ptr: objc_id, selector: str, /, *args: Any, restype: type = obj
 
 def to_ns_str(s: str) -> objc_id: return send_message(NSString, "stringWithUTF8String:", s.encode())
 
-def to_ns_array(items: list[Any]): return (objc_id * len(items))(*items)
+def to_ns_array(items: List[Any]): return (objc_id * len(items))(*items)
 
-def int_tuple_to_struct(t: tuple[int, ...], _type: type = c_ulong):
+def int_tuple_to_struct(t: Tuple[int, ...], _type: type = c_ulong):
   class Struct(Structure): pass
   Struct._fields_ = [(f"field{i}", _type) for i in range(len(t))]
   return Struct(*t)
 
-def dedup(items: list[objc_id]) -> list[objc_id]:
+def dedup(items: List[objc_id]) -> List[objc_id]:
   ret = []
   seen = set()
   for item in items:
