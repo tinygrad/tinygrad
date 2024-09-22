7cedabfd803170e0c68158d267023778484f8997
Mesozoic Egg <mesozoic.egg@proton.me>
2024-09-22 17:29:30 +0800

    remove unused stuff

--- tinygrad/runtime/support/metal.py
+++ tinygrad/runtime/support/metal.py
@@ -1,4 +1,3 @@
-import os
 from ctypes import CDLL, c_void_p, c_char_p, Structure, c_ulong
 from ctypes.macholib.dyld import dyld_find  # pyright: ignore
 from typing import cast, Any
@@ -7,29 +6,23 @@ class objc_id(c_void_p): pass
 
 def load_library(path: str): return CDLL(cast(str,dyld_find(path)))
 
-libobjc = load_library(os.environ.get("LIBOBJC", "/usr/lib/libobjc.dylib"))
+libobjc = load_library("/usr/lib/libobjc.dylib")
+libmetal = load_library("/Library/Frameworks/Metal.framework/Metal")
+core_graphics = load_library("/Library/Frameworks/CoreGraphics.framework/CoreGraphics")
+libdispatch = load_library("/usr/lib/libSystem.dylib")
 libobjc.objc_msgSend.restype = objc_id
 libobjc.objc_getClass.restype = objc_id
 libobjc.objc_getClass.argtypes = [c_char_p]
 libobjc.sel_registerName.restype = objc_id
 libobjc.sel_registerName.argtypes = [c_char_p]
-
-libmetal = load_library(os.environ.get("METALLIBPATH", "/Library/Frameworks/Metal.framework/Metal"))
 libmetal.MTLCreateSystemDefaultDevice.restype = objc_id
-
-core_graphics = load_library("/Library/Frameworks/CoreGraphics.framework/CoreGraphics")
-libdispatch = load_library("/usr/lib/libSystem.dylib")
 libdispatch.dispatch_data_create.restype = objc_id
+NSString: objc_id = libobjc.objc_getClass(b"NSString")
 
 def send_message(ptr: objc_id, selector: str, /, *args: Any, restype: type = objc_id) -> objc_id:
   sender = libobjc["objc_msgSend"]
   sender.restype = restype
   return sender(ptr, libobjc.sel_registerName(selector.encode()), *args)
-
-NSString: objc_id = libobjc.objc_getClass(b"NSString")
-NSConcreteData: objc_id = libobjc.objc_getClass(b"NSConcreteData")
-NSData: objc_id = libobjc.objc_getClass(b"NSData")
-NSMutableArray: objc_id = libobjc.objc_getClass(b"NSMutableArray")
 
 def to_ns_str(s: str) -> objc_id: return send_message(NSString, "stringWithUTF8String:", s.encode())
 
