253050271aa40de4e5f5eb0cc14b8116eafe4c30
George Hotz <geohot@gmail.com>
2024-09-22 13:12:07 +0800

    it's REDUCE by the time it's in lowerer

--- tinygrad/codegen/lowerer.py
+++ tinygrad/codegen/lowerer.py
@@ -64,10 +64,10 @@ def lower_load_store(ctx: IndependentLowerer, x: UOp):
     barrier = (UOp(UOps.BARRIER, dtypes.void, (x.src[2],)),) if x.src[0].op is UOps.DEFINE_LOCAL else ()
     return UOp(UOps.LOAD, x.dtype, (buf, idx) + ((x.const_like(0), valid) if has_valid else ()) + barrier)
   # NOTE: only store the local reduceop in the threads that are actually doing the reduce
-  store_back = x.src[0].op is UOps.DEFINE_LOCAL and x.src[2].op is UOps.REDUCE_AXIS and \
+  store_back = x.src[0].op is UOps.DEFINE_LOCAL and x.src[2].op is UOps.REDUCE and \
     x.src[2].src[0].op is UOps.LOAD and x.src[2].src[0].src[0].op is UOps.DEFINE_LOCAL
   # NOTE: If we're storing the reduced value back into each thread, need to zero-out the reduced axes
-  if store_back: idx, _ = x.st_arg.to_indexed_uops([u.const_like(0) if i in x.src[2].arg[1] else u for i,u in enumerate(ctx.idxs)])
+  if store_back: idx, _ = x.st_arg.to_indexed_uops([u.const_like(0) if u in x.src[2].src else u for u in ctx.idxs])
   if x.src[0].op is UOps.DEFINE_GLOBAL or store_back:
     for oidx, ridx in zip(ctx.idxs, ctx.ridxs):
       if oidx != ridx: valid = valid * oidx.eq(0)
