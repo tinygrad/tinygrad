752ac2f29de22b8be6fbb12d94aaa50450fddf8e
Andre Daprato <andre.daprato5@gmail.com>
2024-09-22 20:31:18 -0400

    simplify

--- tinygrad/runtime/ops_webgpu.py
+++ tinygrad/runtime/ops_webgpu.py
@@ -33,7 +33,7 @@ class WebGPUProgram:
       query_set = self.device.create_query_set(type=wgpu.QueryType.timestamp, count=2)
       query_buf = self.device.create_buffer(size=16, usage=wgpu.BufferUsage.QUERY_RESOLVE | wgpu.BufferUsage.COPY_SRC)
       timestamp_writes = {"query_set": query_set, "beginning_of_pass_write_index": 0, "end_of_pass_write_index": 1}
-    compute_pass = command_encoder.begin_compute_pass(timestamp_writes=timestamp_writes if wait and self.timestamp_supported else None)
+    compute_pass = command_encoder.begin_compute_pass(timestamp_writes=timestamp_writes if wait else None)
     compute_pass.set_pipeline(compute_pipeline)
     compute_pass.set_bind_group(0, bind_group, [], 0, 999999) # last 2 not used
     compute_pass.dispatch_workgroups(*global_size)  # x y z
