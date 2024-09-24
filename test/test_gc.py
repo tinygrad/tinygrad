033afbf23fbbb5d1a3f7f0c548136023f118c63a
Woze Parrot <wozeparrot@gmail.com>
2024-09-24 04:30:49 +0000

    feat: lower back

--- test/test_gc.py
+++ test/test_gc.py
@@ -15,23 +15,23 @@ class TestGC(unittest.TestCase):
     (a*b).mean().backward()
     assert (tensors_allocated() > 0)
     del a,b
-    assert (tensors_allocated() == 2) # one for Tensor._device_rng_counters
+    assert (tensors_allocated() == 1) # one for Tensor._device_rng_counters
 
   def test_gc_complex(self):
     a = Tensor(np.zeros((4, 4), dtype=np.float32), requires_grad=True)
     b = Tensor.rand(4, 4, requires_grad=True)
-    assert (tensors_allocated() == 4)
+    assert (tensors_allocated() == 3)
     (a*b).mean().backward()
-    assert (tensors_allocated() == 6)
+    assert (tensors_allocated() == 5)
     del b
-    assert (tensors_allocated() == 4)
+    assert (tensors_allocated() == 3)
     b = Tensor(np.zeros((4, 4), dtype=np.float32), requires_grad=True)
     print(tensors_allocated())
     (a*b).mean().backward()
     print(tensors_allocated())
-    assert (tensors_allocated() == 6)
+    assert (tensors_allocated() == 5)
     del b
-    assert (tensors_allocated() == 4)
+    assert (tensors_allocated() == 3)
 
 if __name__ == '__main__':
   unittest.main()
