29eb5323d53663775e380e406121b2094075d5ab
Chen-Yu Yang <chenyu@fastmail.com>
2024-09-25 20:44:46 -0700

    update mlperf bert scripts
    removed DISABLE_DROPOUT=1.
    updated BS to 54 that works on tinyboxes with dropouts.
    used bert's sparse_categorical_crossentropy that takes Tensor ignore_index in accuracy method

--- examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
+++ examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
@@ -3,13 +3,10 @@
 export PYTHONPATH="."
 export MODEL="bert"
 export SUBMISSION_PLATFORM="tinybox_red"
-export DEFAULT_FLOAT="HALF" GPUS=6 BS=84 EVAL_BS=6
+export DEFAULT_FLOAT="HALF" GPUS=6 BS=54 EVAL_BS=6
 
 export BEAM=4
 export BASEDIR="/raid/datasets/wiki"
-
-echo "TODO: DISABLING DROPOUT - UNSET FOR REAL SUBMISSION RUN"
-export DISABLE_DROPOUT=1 # TODO: Unset flag for real submission run.
 
 # pip install -e ".[mlperf]"
 export LOGMLPERF=1
