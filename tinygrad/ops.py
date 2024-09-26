39d6b838d8e67c1874072544e74d3edcbddb8299
qazal <qazal.software@gmail.com>
2024-09-26 12:11:58 +0800

    delete test_variable_const [run_process_replay]

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -169,8 +169,7 @@ class UOp(MathTrait):
   def key(self) -> bytes:
     return hashlib.sha256(str((self.op, self.dtype, self.arg)).encode() + b"".join([s.key for s in self.src])).digest()
   def __repr__(self): return pretty_print(self, lambda x: f"{type(self).__name__}({x.op}, {x.dtype}, arg={x.argstr()}, src=(%s))")
-  def argstr(self):
-    return f'({", ".join(map(str, self.arg))})' if self.op is UOps.REDUCE_AXIS else repr(self.arg) if isinstance(self.arg, Variable) else self.arg
+  def argstr(self): return f'({", ".join(map(str, self.arg))})' if self.op is UOps.REDUCE_AXIS else self.arg
   # *** uop syntactic sugar
   @property
   def st_arg(self) -> ShapeTracker:
