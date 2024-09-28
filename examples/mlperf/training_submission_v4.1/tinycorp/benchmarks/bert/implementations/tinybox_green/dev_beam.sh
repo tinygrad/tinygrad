3211bb198140abcda7bf11873ad5bb2dac5c1bd9
chenyu <chenyu@fastmail.com>
2024-09-28 10:14:27 +0800

    bert BS back to 54
    60 does not run end to end

--- examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_green/dev_beam.sh
+++ examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_green/dev_beam.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH="."
 export MODEL="bert"
-export DEFAULT_FLOAT="HALF" GPUS=6 BS=60 EVAL_BS=6
+export DEFAULT_FLOAT="HALF" GPUS=6 BS=54 EVAL_BS=6
 
 export BEAM=4
 export IGNORE_JIT_FIRST_BEAM=1
