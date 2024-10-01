258571786dd7b2869bcee1b75fbecdb6d59f180a
George Hotz <72895+geohot@users.noreply.github.com>
2024-10-01 23:51:23 +0800

    Update ops.py

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -286,7 +286,8 @@ class UOp(MathTrait):
     if self.op is UOps.SPECIAL: return 0, self.arg[1]-1 if isinstance(self.arg[1], int) else dtypes.max(self.dtype)
     if self.op is UOps.CONST: return self.arg, self.arg
     if self.op is UOps.VCONST: return (min(self.arg), max(self.arg))
-    if self.op is UOps.CAST: return (max(self.src[0].vmin, dtypes.min(self.dtype)), min(self.src[0].vmax, dtypes.max(self.dtype)))
+    # TODO: cast is causing issues
+    #if self.op is UOps.CAST: return (max(self.src[0].vmin, dtypes.min(self.dtype)), min(self.src[0].vmax, dtypes.max(self.dtype)))
     if self.op is UOps.ALU and self.dtype.count == 1:
       s0,s1,s2 = [cast(UOp, self.src[i] if i < len(self.src) else None) for i in range(3)]
       if self.arg is BinaryOps.ADD: return s0.vmin+s1.vmin, s0.vmax+s1.vmax
