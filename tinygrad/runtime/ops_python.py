f6472996057326c90719d211950eec4bbf1d45f1
p4sscode <p4ssenger.developer@gmail.com>
2024-10-01 08:22:15 -0300

    generic wmma rendering for cuda, ptx
    - also adds wmma generic shape ops_python support

--- tinygrad/runtime/ops_python.py
+++ tinygrad/runtime/ops_python.py
@@ -170,7 +170,8 @@ class PythonProgram:
             def b_elem(x, i, j, goff): return x[(j%2)+(j//8)*2][goff+(j//2)%4+(i)*4]
             # (i, j), C, D (4 elements on 32 threads)
             def c_map(lane, elem): return ((elem%2)+(lane%4)*2, (lane//4)+(elem//2)*8)
-            ul[i] = wmma_helper(32, 16, 8, 4, 4, a_elem, b_elem, c_map)
+            sza, szb, szc = (prod(sz for _, sz in upc) for upc in arg[6])
+            ul[i] = wmma_helper(32, arg[1][2], sza, szb, szc, a_elem, b_elem, c_map)
           elif arg[4] == "INTEL":
             # A (16 elements on 8 threads)
             def a_elem(x, i, j, goff): return x[i%2+j*2][goff+i//2]
