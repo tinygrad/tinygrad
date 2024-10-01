0ceac4ded7d963997d47c7b501febac40a047a97
Vladimir Ovechkin <vladov3000@gmail.com>
2024-09-30 23:31:37 -0700

    Add test for unaligned copies into OpenCL buffers.

--- test/test_ocl.py
+++ test/test_ocl.py
@@ -1,5 +1,7 @@
 import unittest
 from tinygrad import Device
+from tinygrad.device import Buffer
+from tinygrad.dtype import dtypes
 from tinygrad.helpers import CI
 from tinygrad.runtime.ops_gpu import CLDevice, CLAllocator, CLCompiler, CLProgram
 
@@ -18,3 +20,12 @@ class TestCLError(unittest.TestCase):
     with self.assertRaises(RuntimeError) as err:
       CLProgram(device, name="", lib=CLCompiler(device, "test").compile("__kernel void test(__global int* a) { a[0] = 1; }"))
     assert str(err.exception) == "OpenCL Error -46: CL_INVALID_KERNEL_NAME"
+
+  def test_unaligned_copy(self):
+    data = list(range(65))
+    unaligned = memoryview(bytearray(data))[1:]
+    buffer = Buffer("GPU", 64, dtypes.uint8).allocate()
+    buffer.copyin(unaligned)
+    result = memoryview(bytearray(len(data) - 1))
+    buffer.copyout(result)
+    assert unaligned == result, "Unaligned data copied in must be equal to data copied out."
