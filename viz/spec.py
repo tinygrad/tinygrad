46c204f37b6078df0aa1002f2c11b70e10402584
qazal <qazal.software@gmail.com>
2024-10-02 08:47:22 +0800

    leaaan

--- viz/spec.py
+++ viz/spec.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional, Tuple
-from tinygrad.ops import UOp
+from typing import Dict, List, Optional, Tuple
 
 @dataclass(frozen=True)
 class GraphRewriteMetadata:
@@ -8,24 +7,20 @@ class GraphRewriteMetadata:
   loc: Tuple[str, int]
   """File_path, Lineno"""
   code_line: str
-  """The python line calling graph_rewrite"""
+  """The Python line calling graph_rewrite"""
   kernel_name: Optional[str]
   """The kernel instance calling graph_rewrite"""
   kernel_code: Optional[str]
   """The final code rendered by the kernel instance connected with this graph_rewrite"""
-  rewrite_count: int
-  """Total number of rewrites on the sink"""
-
-@dataclass(frozen=True)
-class GraphRewriteFullInfo:
-  """
-  Full details about a single call to graph_rewrite
-  NOTE: this is slower to get because it reconstructs the entire graph after every rewrite
-  """
-  graphs: List[UOp]
-  """Snapshot of the SINK at every stage of graph_rewrite"""
   upats: List[Tuple[Tuple[str, int], str]]
   """List of all the UPats that matched and returned a not none value."""
-  diffs: List[str]
+
+@dataclass(frozen=True)
+class GraphRewriteDetails(GraphRewriteMetadata):
+  """Full details about a single call to graph_rewrite"""
+  graphs: List[Dict[int, Tuple[str, str, List[int], str, str]]]
+  """Sink at every stage of graph_rewrite"""
+  diffs: List[List[str]]
   """.diff style before and after of the rewritten UOp child"""
-  metadata: GraphRewriteMetadata
+  changed_nodes: List[List[int]]
+  """Nodes that changed at every stage of graph_rewrite"""
