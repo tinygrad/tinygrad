2786cfa7e151e406387e73addd9e2e5e9164c3c1
chenyu <chenyu@fastmail.com>
2024-10-04 20:07:16 +0800

    fix var_vals in MCTS
    tested with JITBEAM=100 llama

--- extra/mcts_search.py
+++ extra/mcts_search.py
@@ -86,7 +86,7 @@ def mcts_search(lin:Kernel, rawbufs:List[Buffer], amt:int) -> Kernel:
     return ret
 
   rawbufs = _ensure_buffer_alloc(rawbufs)
-  var_vals = {k:(k.max+k.min)//2 for k in lin.ast.variables()}
+  var_vals = {k:(k.vmax+k.vmin)//2 for k in lin.ast.variables()}
   dev = Device[lin.opts.device]
   root = MCTSNode(lin)
 
