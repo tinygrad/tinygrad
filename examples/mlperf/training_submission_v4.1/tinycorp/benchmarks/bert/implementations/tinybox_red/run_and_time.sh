1ff2c98f8a0fcc7e539b586345d491047b32034f
chenyu <chenyu@fastmail.com>
2024-10-08 05:37:52 -0400

    fix logfile name for bert red (#6952)

--- examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
+++ examples/mlperf/training_submission_v4.1/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
@@ -14,7 +14,7 @@ export LOGMLPERF=1
 
 export SEED=$RANDOM
 DATETIME=$(date "+%m%d%H%M")
-LOGFILE="bert_green_${DATETIME}_${SEED}.log"
+LOGFILE="bert_red_${DATETIME}_${SEED}.log"
 
 # init
 BENCHMARK=10 INITMLPERF=1 python3 examples/mlperf/model_train.py | tee $LOGFILE
