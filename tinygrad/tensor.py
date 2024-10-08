82f4d3fa4dc4dbc455bb38d25373b9fb789d1fff
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-10-08 15:32:14 -0400

    implement cummax

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -1793,8 +1793,8 @@ class Tensor:
     print(t.logcumsumexp(axis=1).numpy())
     ```
     """
-    cm = self.cumsum(axis=axis).maximum(self)
-    return (self - cm).exp().cumsum(axis=axis).log() + cm
+    m = self.max(axis=axis, keepdim=True)
+    return (self - m).exp().cumsum(axis=axis).log() + m
 
   def argmax(self, axis=None, keepdim=False):
     """
@@ -2150,6 +2150,35 @@ class Tensor:
     base_add = base_add.unsqueeze(-1).expand(*base_add.shape, ret.shape[-1])
     def fix(x:Tensor): return x.flatten(start_dim=-2)[..., -s:].transpose(axis,-1)
     return fix(ret) + fix(base_add)
+
+  def _cummax(self, axis:int=0, _first_zero=False) -> Tuple[Tensor, Tensor]:
+    assert self.shape[axis] != 0
+    pl_sz = self.shape[axis] - 1
+    values = self.transpose(axis,-1).pad2d((pl_sz, 0), -float("inf"))._pool((self.shape[axis],)).max(-1).transpose(axis,-1)
+    idxs = Tensor.arange(self.shape[axis], dtype=dtypes.int64, device=self.device).reshape(*[1 if i != axis else -1 for i in range(self.ndim)]).expand(self.shape)
+    idx_mask = (self == values) * idxs # index is turned on if values[i] = cummax[i]
+    max_idxs =  idx_mask.transpose(axis,-1).pad2d((self.shape[axis]-1, 0))._pool((self.shape[axis],)).max(-1).transpose(axis,-1) # fill blanks with max_idx
+
+    return values, max_idxs.detach()
+
+  def cummax(self, axis:int=0) -> Tuple[Tensor, Tensor]:
+    """
+    Computes the cumulative maximum of the tensor along the specified axis.
+
+    You can pass in the `axis` keyword argument to control the axis along which the cumulative maximum is computed.
+
+    ```python exec="true" source="above" session="tensor" result="python"
+    t = Tensor([[1, 2, 3], [4, 5, 6]])
+    print(t.numpy())
+    ```
+    ```python exec="true" source="above" session="tensor" result="python"
+    print(t.cummax(1).numpy())
+    ```
+    """
+    axis = self._resolve_dim(axis)
+    if self.ndim == 0 or 0 in self.shape: return self, self.cast(dtypes.int64)
+    return self._cummax(axis)
+
 
   @staticmethod
   def _tri(r:sint, c:sint, diagonal:int=0, **kwargs) -> Tensor:
