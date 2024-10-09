da706fb89b0a563e2cc4a1989d15a6cfa4f57832
Mesozoic Egg <mesozoic.egg@proton.mail>
2024-10-10 06:29:32 +0800

    wip

--- examples/attention_multigpu.py
+++ examples/attention_multigpu.py
@@ -10,14 +10,63 @@ import tiktoken, argparse
 
 SHARD = os.getenv("SHARD", True)
 
+n_embed = 768
+n_head = 12
+block_size = 1024
 
+class CausalSelfAttention:
+  def __init__(self):
+    # key, query, value projections for all heads, but in a batch
+    self.c_attn = nn.Linear(n_embed, 3 * n_embed)
+    # output projection
+    self.c_proj = nn.Linear(n_embed, n_embed)
+    # regularization
+    self.n_head = n_head
+    self.n_embed = n_embed
+    # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
+    self.bias = Tensor.ones(1, 1, block_size, block_size).tril()
+    self.bias.requires_grad = False
+
+  def __call__(self, x:Tensor):
+    B, T, C = x.shape
+    qkv = self.c_attn(x)
+    q, k, v = qkv.split(self.n_embed, dim=2)
+    k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+    q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+    v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+    # manual implementation of attention
+    att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+    att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+    att = att.softmax()
+    y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+    y = y.transpose(1, 2).view(B, T, C) # re-assemble all head outputs side by side
+    # output projection
+    y = self.c_proj(y)
+    return y
+
+class Block:
+  def __init__(self):
+    self.attn = CausalSelfAttention()
+    pass
+  def __call__(self, x):
+    return self.attn(x)
 class GPT:
   def __init__(self):
+    self.wte = nn.Embedding(50304, n_embed)
+    self.wpe = nn.Embedding(block_size, n_embed)
     self.linear = nn.Linear(768, 50257)
+    self.b0 = Block()
 
   def __call__(self, idx, targets):
-    x = idx.reshape((4, 64, 1))
-    x = x.expand((4, 64, 768))
+    b, t = idx.shape
+    tok_emb = self.wte(idx)
+    pos = Tensor.arange(0, t)
+    if SHARD:
+      pos.shard_(GPUS, axis=0)
+    pos_emb = self.wpe(pos)
+    x = tok_emb + pos_emb
+    x = self.b0(x)
     x = self.linear(x)
     loss = x.sparse_categorical_crossentropy(targets)
     return x, loss
