69c1a6469928baa6cfca3837d6875e503842fa56
qazal <qazal.software@gmail.com>
2024-10-12 16:11:55 +0300

    process_replay.py changes

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # compare kernels created by HEAD against master
 import os, multiprocessing, logging, pickle, sqlite3, difflib, functools
-from typing import Callable, List, Tuple, Union, cast
+from typing import List, Tuple, Union, cast
 from tinygrad.engine.schedule import full_ast_rewrite
 from tinygrad.helpers import VERSION, Context, ContextVar, colored, db_connection, getenv, tqdm
-from tinygrad.codegen.kernel import Kernel, Opt
-from test.external.process_replay.helpers import ProcessReplayContext, print_diff
-from tinygrad.ops import UOp
-from tinygrad.renderer import Renderer
+from tinygrad.codegen.kernel import Kernel
+from test.external.process_replay.helpers import print_diff
+from tinygrad.renderer import Program
 
 # *** process replay settings
 
@@ -29,48 +28,52 @@ if REF == "master": SKIP_PROCESS_REPLAY = True
 
 # *** recreators
 
-def recreate_sched(sink:UOp, ctx) -> UOp: return full_ast_rewrite(sink, ctx)
-def recreate_kernel(ast:UOp, opts:Renderer, applied_opts:List[Opt], name:str, ctx:ProcessReplayContext) -> str:
-  with Context(**{k:v for k,v in ctx.ctx_vars.items() if k in ContextVar._cache and k != "DEBUG"}):
-    k = Kernel(ast, opts=opts)
-    for opt in applied_opts: k.apply_opt(opt)
+def recreate(compare, *args, **kwargs) -> str:
+  if isinstance(x:=args[0], Kernel):
+    k = Kernel(x.ast, opts=x.opts)
+    for opt in x.applied_opts: k.apply_opt(opt)
     # NOTE: replay with the captured renderer, not the one in master
-    return k.opts.render(name, cast(List,k.to_program().uops))
+    return k.opts.render(compare.function_name, cast(List,k.to_program().uops))
+  return str(full_ast_rewrite(*args, **kwargs))
 
 # *** diff a "good" recreation against the generated version
 
-def diff(offset:int, name:str, fxn:Callable) -> Union[Tuple[int, int], bool]:
+def diff(offset:int) -> Union[Tuple[int, int], bool]:
   if early_stop.is_set(): return True
   conn = db_connection()
   cur = conn.cursor()
-  cur.execute(f"SELECT val FROM '{name}_{TABLE_NAME}' LIMIT ? OFFSET ?", (PAGE_SIZE, offset))
+  cur.execute(f"SELECT val FROM '{TABLE_NAME}' LIMIT ? OFFSET ?", (PAGE_SIZE, offset))
   additions, deletions, changed = 0, 0, 0
   for row in cur.fetchall():
     # try unpickle
-    try: args = pickle.loads(row[0])
+    try: args, kwargs, ctx, compare = pickle.loads(row[0])
     except Exception as e:
       logging.warning(f"FAILED TO UNPICKLE OBJECTS {e}")
       if ASSERT_DIFF: return True
       continue
     # try recreate
-    try: good = fxn(*args[:-1])
+    try:
+      with Context(**{k:v for k,v in ctx[0].items() if k in ContextVar._cache and k != "DEBUG"}): good = recreate(compare, *args, **kwargs)
+      compare = compare.src if isinstance(compare, Program) else str(compare)
     except Exception as e:
       logging.warning(f"FAILED TO RECREATE KERNEL {e}")
+      if isinstance(x:=args[0], Kernel): args = [x.ast, x.applied_opts] # TODO: can Kernel have __repr__
       for x in args[:-1]: logging.info(x)
       if ASSERT_DIFF: return True
       continue
     # diff kernels
-    try: assert args[-1] == good
+    try: assert compare == good
     except AssertionError:
       logging.info("PROCESS REPLAY DETECTED CHANGE")
-      for x in args[:-1]: logging.info(x)
-      print_diff(good, args[-1])
-      changes = list(difflib.unified_diff(str(good).splitlines(), str(args[-1]).splitlines()))
+      if isinstance(x:=args[0], Kernel): args = [x.ast, x.applied_opts]
+      for x in args: logging.info(x)
+      print_diff(good, compare)
+      changes = list(difflib.unified_diff(good.splitlines(), compare.splitlines()))
       additions += len([x for x in changes if x.startswith("+")])
       deletions += len([x for x in changes if x.startswith("-")])
       if ASSERT_DIFF: return additions, deletions
       if changed > MAX_DIFF_PCT:
-        logging.warning(f"detected changes in over {MAX_DIFF_PCT}% of {name}s. skipping further diff generation.")
+        logging.warning(f"detected changes in over {MAX_DIFF_PCT}% of samples. skipping further diff generation.")
         early_stop.set()
         break
   conn.commit()
@@ -79,18 +82,18 @@ def diff(offset:int, name:str, fxn:Callable) -> Union[Tuple[int, int], bool]:
 
 # *** generic runner for executing fxn across all rows of a table in parallel
 
-def _pmap(name:str, fxn:Callable, maxtasksperchild:int=16) -> None:
+def main() -> None:
   conn = db_connection()
   cur = conn.cursor()
-  try: row_count = cur.execute(f"select count(*) from '{name}_{TABLE_NAME}'").fetchone()[0]
+  try: row_count = cur.execute(f"select count(*) from '{TABLE_NAME}'").fetchone()[0]
   except sqlite3.OperationalError:
-    logging.warning(f"{name}_{TABLE_NAME} isn't accessible in master, did DB_VERSION change?")
+    logging.warning(f"{TABLE_NAME} isn't accessible in master, did DB_VERSION change?")
     return None
   conn.commit()
   cur.close()
-  with multiprocessing.get_context("spawn").Pool(multiprocessing.cpu_count(), maxtasksperchild=maxtasksperchild) as pool:
+  with multiprocessing.get_context("spawn").Pool(multiprocessing.cpu_count(), maxtasksperchild=16) as pool:
     inputs = list(range(0, row_count, PAGE_SIZE))
-    ret: List[Union[bool, Tuple[int, int]]] = list(tqdm(pool.imap_unordered(functools.partial(diff, name=name, fxn=fxn), inputs), total=len(inputs)))
+    ret: List[Union[bool, Tuple[int, int]]] = list(tqdm(pool.imap_unordered(functools.partial(diff), inputs), total=len(inputs)))
     pool.close()
     pool.join()
     pool.terminate()
@@ -108,9 +111,7 @@ if __name__ == "__main__":
     logging.info("skipping process replay.")
     exit(0)
 
-  for name,fxn in [("schedule", recreate_sched), ("kernel", recreate_kernel)]:
-    logging.info(f"***** {name} diff")
-    try: _pmap(name, fxn)
-    except Exception as e:
-      if ASSERT_DIFF: raise e
-      logging.error(f"{name} diff err {e}")
+  try: main()
+  except Exception as e:
+    if ASSERT_DIFF: raise e
+    logging.error(f"diff err {e}")
