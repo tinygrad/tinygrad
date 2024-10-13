2141f4c26df9b95d8489425594f71467cd7fe642
detolly <thomas@detolly.no>
2024-10-13 08:07:46 +0200

    ops_clang: mmap prot_exec and trampoline

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,30 +1,29 @@
 from typing import Optional, List
-import ctypes, subprocess, pathlib, tempfile
+import ctypes, subprocess, mmap, pathlib, tempfile
 from tinygrad.device import Compiled, Compiler, MallocAllocator
 from tinygrad.helpers import cpu_time_execution, DEBUG, cpu_objdump
 from tinygrad.renderer.cstyle import ClangRenderer
+from tinygrad.runtime.autogen import libc
 
 class ClangCompiler(Compiler):
   def __init__(self, cachekey="compile_clang", args:Optional[List[str]]=None):
     self.args = ['-march=native'] if args is None else args
     super().__init__(cachekey)
-
   def compile(self, src:str) -> bytes:
-    # TODO: remove file write. sadly clang doesn't like the use of /dev/stdout here
-    with tempfile.NamedTemporaryFile(delete=True) as output_file:
-      subprocess.check_output(['clang', '-shared', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-fPIC', '-ffreestanding', '-nostdlib',
-                               '-', '-o', str(output_file.name)], input=src.encode('utf-8'))
-      return pathlib.Path(output_file.name).read_bytes()
+    with tempfile.NamedTemporaryFile(delete=True) as file:
+      elf_bytes = subprocess.check_output(['clang', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-c', '-fPIC', '-nostdlib', '-march=native', '-', '-o', pathlib.Path(file.name)], input=src.encode('utf-8'))
+      raw_function_bytes = subprocess.check_output(['objcopy', '-O', 'binary', '--only-section=.text', pathlib.Path(file.name), '/dev/stdout'], input=elf_bytes)
+      return raw_function_bytes
 
 class ClangProgram:
-  def __init__(self, name:str, lib:bytes):
-    if DEBUG >= 6: cpu_objdump(lib)
-    self.name, self.lib = name, lib
-    # write to disk so we can load it
-    with tempfile.NamedTemporaryFile(delete=True) as cached_file_path:
-      pathlib.Path(cached_file_path.name).write_bytes(lib)
-      self.fxn = ctypes.CDLL(str(cached_file_path.name))[name]
-
+  def __init__(self, name:str, obj:bytes):
+    if DEBUG >= 6: cpu_objdump(obj)
+    self.name, self.obj = name, obj
+    self.buf, self.len = libc.mmap(0, len(obj), mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC, mmap.MAP_PRIVATE | mmap.MAP_ANONYMOUS, -1, 0), len(obj)
+    ctypes.memmove(self.buf, (ctypes.c_char * len(obj)).from_buffer_copy(obj), len(obj))
+    self.fxn = lambda *args: ctypes.CFUNCTYPE(None)(self.buf)(*args)
+  def __del__(self):
+    libc.munmap(self.buf, self.len)
   def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: self.fxn(*bufs, *vals), enable=wait)
 
 class ClangDevice(Compiled):
