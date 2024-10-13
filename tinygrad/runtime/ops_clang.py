56523b27dc9d7dade3a237a651527e35f0cd7cf1
detolly <thomas@detolly.no>
2024-10-13 09:40:02 +0200

    clang: fix debug>6 raw bytes disassembly

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 import ctypes, subprocess, mmap, pathlib, tempfile
 from tinygrad.device import Compiled, Compiler, MallocAllocator
-from tinygrad.helpers import cpu_time_execution, DEBUG, cpu_objdump
+from tinygrad.helpers import cpu_time_execution, DEBUG
 from tinygrad.renderer.cstyle import ClangRenderer
 from tinygrad.runtime.autogen import libc
 
@@ -11,16 +11,24 @@ class ClangCompiler(Compiler):
     super().__init__(cachekey)
   def compile(self, src:str) -> bytes:
     with tempfile.NamedTemporaryFile(delete=True) as file:
-      elf_bytes = subprocess.check_output(['clang', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-c', '-fPIC', '-nostdlib', '-march=native', '-', '-o', pathlib.Path(file.name)], input=src.encode('utf-8'))
-      raw_function_bytes = subprocess.check_output(['objcopy', '-O', 'binary', '--only-section=.text', pathlib.Path(file.name), '/dev/stdout'], input=elf_bytes)
+      subprocess.check_output([
+        'clang', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-c',
+        '-fPIC', '-nostdlib', '-', '-o', pathlib.Path(file.name)
+      ], input=src.encode('utf-8'))
+      raw_function_bytes = subprocess.check_output([
+        'objcopy', '-O', 'binary', '--only-section=.text', pathlib.Path(file.name), '/dev/stdout'])
       return raw_function_bytes
 
 class ClangProgram:
-  def __init__(self, name:str, obj:bytes):
-    if DEBUG >= 6: cpu_objdump(obj)
-    self.name, self.obj = name, obj
-    self.buf, self.len = libc.mmap(0, len(obj), mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC, mmap.MAP_PRIVATE | mmap.MAP_ANONYMOUS, -1, 0), len(obj)
-    ctypes.memmove(self.buf, (ctypes.c_char * len(obj)).from_buffer_copy(obj), len(obj))
+  def __init__(self, name:str, code:bytes):
+    if DEBUG >= 6:
+      with tempfile.NamedTemporaryFile(delete=True) as f:
+        pathlib.Path(f.name).write_bytes(code)
+        print(subprocess.check_output(['objdump', '-b', 'binary', '-m', 'i', '--disassembler-color=on', '-D', f.name]).decode('utf-8'))
+    self.name, self.obj = name, code
+    self.buf, self.len = libc.mmap(0, len(code), mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC,
+                                   mmap.MAP_PRIVATE | mmap.MAP_ANONYMOUS, -1, 0), len(code)
+    ctypes.memmove(self.buf, (ctypes.c_char * len(code)).from_buffer_copy(code), len(code))
     self.fxn = lambda *args: ctypes.CFUNCTYPE(None)(self.buf)(*args)
   def __del__(self):
     libc.munmap(self.buf, self.len)
