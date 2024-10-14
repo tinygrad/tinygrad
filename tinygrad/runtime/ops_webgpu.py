714386406b02dfae798a059a932777ffb91cf878
Mesozoic Egg <mesozoic.egg@proton.me>
2024-10-14 17:14:43 +0800

    two way comm

--- tinygrad/runtime/ops_webgpu.py
+++ tinygrad/runtime/ops_webgpu.py
@@ -4,6 +4,7 @@ from base64 import b64encode
 from hashlib import sha1
 from email.parser import Parser
 from io import StringIO
+import io
 import multiprocessing
 import socket
 import time
@@ -155,9 +156,52 @@ def handleExtraConnection(socket: socket.socket):
     finally:
       req.close()
 
+def handshake(req: socket.socket):
+  ws_hash = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+  data = req.recv(1024).strip().decode()
+  headers = Parser().parsestr(data.split("\r\n", 1)[1])
+  if headers.get("Upgrade", None) != "websocket":
+    return
+  print("Handshaking...")
+  key = headers["Sec-WebSocket-Key"]
+  digest = b64encode(sha1((key + ws_hash).encode()).digest()).decode()
+  response = "HTTP/1.1 101 Switching Protocols\r\n"
+  response += "Upgrade: websocket\r\n"
+  response += "Connection: Upgrade\r\n"
+  response += f"Sec-WebSocket-Accept: {digest}\r\n\r\n"
+  req.send(response.encode())
+
+def read(rfile: io.BufferedReader):
+  length = rfile.read(2)[1] & 127
+  if length == 126:
+    length = struct.unpack(">H", rfile.read(2))[0]
+  elif length == 127:
+    length = struct.unpack(">Q", rfile.read(8))[0]
+  masks = [byte for byte in rfile.read(4)]
+  decoded = ""
+  for char in rfile.read(length):
+    decoded += chr(char ^ masks[len(decoded) % 4])
+  print(f"{decoded=}")
+  return decoded
+
+def write(req: socket.socket, message: str):
+  TEXT = bytes([129])
+  BINARY = bytes([130])
+  req.send(TEXT)
+  length = len(message)
+  if length <= 125:
+    req.send(bytes([length]))
+  elif 126 <= length <= 65535:
+    req.send(bytes([126]))
+    req.send(struct.pack(">H", length))
+  else:
+    req.send(bytes([127]))
+    req.send(struct.pack(">Q", length))
+  req.send(message.encode())
+
 class WebDevice:
   def __init__(self, address):
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     self.socket.bind(address)
@@ -167,12 +211,15 @@ class WebDevice:
     t = threading.Thread(target=handleExtraConnection, args=(self.socket,))
     t.daemon = True
     t.start()
-    rfile = req.makefile('rb', 1024)
-    print('req', req)
-    data = rfile.read(4)
-    while data:
-      print('read', data)
-      data = rfile.read(4)
+    print("Con received", address, req)
+    handshake(req)
+    rfile = req.makefile("rb", -1)
+    print("handshake done")
+    print("send instruction")
+    write(req, "message from server")
+    while True:
+      print("waiting to read")
+      content = read(rfile)
 
 
 
