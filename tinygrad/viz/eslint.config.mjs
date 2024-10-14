dc9eb9be397b1b3f7526e96e7985a45485dbd336
qazal <qazal.software@gmail.com>
2024-10-14 18:05:05 +0300

    space

--- tinygrad/viz/eslint.config.mjs
+++ tinygrad/viz/eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 import pluginHtml from "eslint-plugin-html";
 
 export default [
-  {files: ["**/*.html"], plugins: {html: pluginHtml}, rules:{"max-len": ["error", {"code": 150}]}},
+  {files: ["**/*.html"], plugins: {html: pluginHtml}, rules: {"max-len": ["error", {"code": 150}]}},
   {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
 ];
