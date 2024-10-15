de9b6d67ea64e5c813629ce5828cf33764baa9c9
qazal <qazal.software@gmail.com>
2024-10-15 16:46:31 +0300

    test_rewrite_twice

--- test/test_viz.py
+++ test/test_viz.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 import unittest
 from tinygrad.dtype import PtrDType, dtypes
-from tinygrad.ops import TRACK_MATCH_STATS, TrackedPatternMatcher as PatternMatcher, UOp, UOps, UPat, graph_rewrite, contexts, track_rewrites
+from tinygrad.ops import TRACK_MATCH_STATS, BinaryOps, TrackedPatternMatcher as PatternMatcher, UOp, UOps, UPat, graph_rewrite, contexts, track_rewrites
 from tinygrad.viz.serve import _replace_uop
 
 @track_rewrites
@@ -31,10 +31,21 @@ class TestViz(unittest.TestCase):
     pm = PatternMatcher([
       (UPat.var("x")*1, lambda x:x),
     ])
-    ld = UOp(UOps.LOAD, dtypes.int, (UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0), UOp.const(dtypes.int, 0)))
-    uops = viz(sink:=ld*1, pm)
-    assert len(uops) == 1
-    assert uops[0] == graph_rewrite(sink, pm)
+    a = UOp(UOps.LOAD, dtypes.int, (UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0), UOp.const(dtypes.int, 0)))
+    uops = viz(a*1, pm)
+    self.assertEqual(len(uops), 1)
+    self.assertEqual(uops[0], a)
+
+  def test_rewrite_twice(self):
+    pm = PatternMatcher([
+      (UPat.var("x")+UPat.var("x"), lambda x:x*2),
+      (UPat.var("x", dtypes.int)*2, lambda x:x.alu(BinaryOps.SHL, UOp.const(dtypes.int, 1))),
+    ])
+    a = UOp(UOps.LOAD, dtypes.int, (UOp(UOps.DEFINE_GLOBAL, PtrDType(dtypes.int), (), 0), UOp.const(dtypes.int, 0)))
+    uops = viz(a+a, pm)
+    self.assertEqual(len(uops), 2)
+    self.assertEqual(uops[0], a*2)
+    self.assertEqual(uops[1], graph_rewrite(a+a, pm))
 
 if __name__ == "__main__":
   unittest.main()
