a0c2b3e473f2925fb8a591eff5927fb41365347c
detolly <thomas@detolly.no>
2024-10-16 07:41:26 +0200

    clang: separate mac osx and linux. it's not the same at all

--- tinygrad/runtime/graph/clang.py
+++ tinygrad/runtime/graph/clang.py
@@ -1,11 +1,11 @@
 from typing import List, Dict, cast
 import ctypes
-from tinygrad.helpers import cpu_time_execution, DEBUG
+from tinygrad.helpers import OSX, cpu_time_execution, DEBUG, dedup
 from tinygrad.engine.jit import GraphRunner, GraphException
 from tinygrad.device import Buffer, Device
 from tinygrad.engine.realize import ExecItem, CompiledRunner
 from tinygrad.ops import Variable
-from tinygrad.runtime.ops_clang import ClangProgram
+from tinygrad.runtime.ops_clang import ClangProgram, ClangProgramOSX
 from tinygrad.renderer.cstyle import ClangRenderer
 render_dtype = ClangRenderer().render_dtype
 
@@ -16,7 +16,7 @@ class ClangGraph(GraphRunner):
 
     args = [f"{render_dtype(x.dtype)}* arg{i}" for i,x in enumerate(input_rawbuffers)]
     args += sorted([f"int {v.expr}" for v in var_vals])
-    code = ["\nvoid batched("+','.join(args)+") __attribute__((section(\".tinygrad\"))) {"]
+    code = ["\nvoid batched("+','.join(args)+") " + ("__attribute__((section(\".tinygrad\")))" if not OSX else "") + " {"]
     i = 0
     for ji in jit_cache:
       kernel_args = []
@@ -36,6 +36,35 @@ class ClangGraph(GraphRunner):
     compiler = Device["CLANG"].compiler
     assert compiler is not None
     self.clprg = ClangProgram("batched", compiler.compile("\n".join(code))) # no point in caching the pointers
+
+  def __call__(self, rawbufs: List[Buffer], var_vals: Dict[Variable, int], wait=False):
+    return cpu_time_execution(
+    lambda: self.clprg(*[x._buf for x in rawbufs], *[x[1] for x in sorted(var_vals.items(), key=lambda x: x[0].expr)]), enable=wait)
+
+class ClangGraphOSX(GraphRunner):
+  def __init__(self, jit_cache: List[ExecItem], input_rawbuffers: List[Buffer], var_vals: Dict[Variable, int]):
+    super().__init__(jit_cache, input_rawbuffers, var_vals)
+    if not all(isinstance(ji.prg, CompiledRunner) for ji in jit_cache): raise GraphException
+
+    prgs = '\n'.join(dedup([cast(CompiledRunner, ji.prg).p.src for ji in jit_cache]))
+    args = [f"{render_dtype(x.dtype)}* arg{i}" for i,x in enumerate(input_rawbuffers)]
+    args += sorted([f"int {v.expr}" for v in var_vals])
+    code = ["void batched("+','.join(args)+") {"]
+    for ji in jit_cache:
+      args = []
+      for buf in ji.bufs:
+        assert buf is not None
+        if buf in input_rawbuffers:
+          args.append(f"arg{input_rawbuffers.index(buf)}")
+        else:
+          args.append(f"({render_dtype(buf.dtype)}*)0x{ctypes.addressof(buf._buf):X}")
+      args += [x.expr for x in cast(CompiledRunner, ji.prg).p.vars]
+      code.append(f"  {cast(CompiledRunner, ji.prg).p.function_name}({','.join(args)});")
+    code.append("}")
+    if DEBUG >= 4: print("\n".join(code))
+    compiler = Device["CLANG"].compiler
+    assert compiler is not None
+    self.clprg = ClangProgramOSX("batched", compiler.compile(prgs+"\n"+"\n".join(code))) # no point in caching the pointers
 
   def __call__(self, rawbufs: List[Buffer], var_vals: Dict[Variable, int], wait=False):
     return cpu_time_execution(
--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 import ctypes, subprocess, mmap, pathlib, tempfile
 from tinygrad.device import Compiled, Compiler, MallocAllocator
-from tinygrad.helpers import cpu_time_execution, DEBUG
+from tinygrad.helpers import OSX, cpu_objdump, cpu_time_execution, DEBUG
 from tinygrad.renderer.cstyle import ClangRenderer
 from tinygrad.runtime.autogen import libc
 
@@ -42,7 +42,31 @@ class ClangProgram:
     libc.munmap(self.buf, self.len)
   def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: ctypes.CFUNCTYPE(None)(self.buf)(*bufs, *vals), enable=wait)
 
+class ClangCompilerOSX(Compiler):
+  def __init__(self, cachekey="compile_clang", args:Optional[List[str]]=None):
+    self.args = ['-march=native'] if args is None else args
+    super().__init__(cachekey)
+
+  def compile(self, src:str) -> bytes:
+    # TODO: remove file write. sadly clang doesn't like the use of /dev/stdout here
+    with tempfile.NamedTemporaryFile(delete=True) as output_file:
+      subprocess.check_output(['clang', '-shared', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-fPIC', '-ffreestanding', '-nostdlib',
+                               '-', '-o', str(output_file.name)], input=src.encode('utf-8'))
+      return pathlib.Path(output_file.name).read_bytes()
+
+class ClangProgramOSX:
+  def __init__(self, name:str, lib:bytes):
+    if DEBUG >= 6: cpu_objdump(lib)
+    self.name, self.lib = name, lib
+    # write to disk so we can load it
+    with tempfile.NamedTemporaryFile(delete=True) as cached_file_path:
+      pathlib.Path(cached_file_path.name).write_bytes(lib)
+      self.fxn = ctypes.CDLL(str(cached_file_path.name))[name]
+
+  def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: self.fxn(*bufs, *vals), enable=wait)
+
 class ClangDevice(Compiled):
   def __init__(self, device:str):
-    from tinygrad.runtime.graph.clang import ClangGraph
-    super().__init__(device, MallocAllocator, ClangRenderer(), ClangCompiler(), ClangProgram, ClangGraph)
+    from tinygrad.runtime.graph.clang import ClangGraph, ClangGraphOSX
+    if OSX: super().__init__(device, MallocAllocator, ClangRenderer(), ClangCompilerOSX(), ClangProgramOSX, ClangGraphOSX)
+    else:   super().__init__(device, MallocAllocator, ClangRenderer(), ClangCompiler(), ClangProgram, ClangGraph)
