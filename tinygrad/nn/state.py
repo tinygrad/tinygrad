42eb057d4e8777a63801298a5fa712c33875826a
leopf <l.pfob@3-klicks.de>
2024-10-16 11:54:46 +0200

    q6_k simpler

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -244,9 +244,9 @@ def ggml_data_to_tensor(t: Tensor, n: int, ggml_type: int):
     return q_to_uint8(blocks[:,4:], 4).bitcast(dtypes.int8) * d + m
   if ggml_type == 14:
     xl, xh = q_to_uint8(blocks[:,:128].reshape((-1, 2, 64)), 4), q_to_uint8(blocks[:,128:192].reshape((-1, 2, 32)), 2).lshift(4)
-    scales = blocks[:,192:208].bitcast(dtypes.int8).unsqueeze(-1).expand((blocks.shape[0], 16, 16)).reshape((blocks.shape[0], 2, 128))
-    d = blocks[:,-2:].bitcast(dtypes.float16).cast(dtypes.float32).expand((-1, 256)).reshape((-1, 2, 128))
-    return d * (xl.bitwise_or(xh).bitcast(dtypes.int8) - 32) * scales
+    scales = blocks[:,192:208].bitcast(dtypes.int8).unsqueeze(-1).expand((blocks.shape[0], 16, 16)).reshape((-1, 256))
+    d = blocks[:,-2:].bitcast(dtypes.float16).cast(dtypes.float32).expand((-1, 256))
+    return d * (xl.bitwise_or(xh).bitcast(dtypes.int8) - 32).flatten(-2) * scales
 
 def load_gguf(tensor: Tensor) -> Tuple[Dict, Dict[str, Tensor]]:
   """
