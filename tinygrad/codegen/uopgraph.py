e2e367988d480b0fe3356e989c770a239ed66866
ttomsa <tomasvsilva8@gmail.com>
2024-10-18 00:52:52 +0100

    round and round we go

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -564,7 +564,8 @@ int64_idx = PatternMatcher([
   #  lambda rng: UOp(rng.op, dtypes.int64, tuple(s.cast(dtypes.int64) for s in rng.src), rng.arg) if max(rng._min_max, key=abs) > dtypes.max(rng.dtype) else None)
   # cast alu operands if needed (for int64 indexing)
   (UPat(UOps.ALU, (dtypes.int32, dtypes.int64), name="alu"),
-    lambda alu: UOp(alu.op, alu.dtype, tuple(s.cast(alu.dtype) for s in alu.src), alu.arg) if any(s.dtype != alu.dtype for s in alu.src) else None)
+    lambda alu: UOp(alu.op, alu.dtype, tuple(s.cast(alu.dtype) for s in alu.src), alu.arg) if any(s.dtype != alu.dtype for s in alu.src) and \
+    alu.arg in (BinaryOps.ADD, BinaryOps.MUL) else None)
   #(UPat((UOps.CONST, UOps.VCONST, UOps.SPECIAL, UOps.RANGE, UOps.EXPAND, UOps.VECTORIZE, UOps.DEFINE_VAR), dtypes.int32, name="x"),
   #  lambda x: UOp(x.op, dtypes.int64, tuple(s.cast(dtypes.int64) for s in x.src), x.arg) if max(x._min_max, key=abs) > dtypes.max(x.dtype) else None)
 ])
