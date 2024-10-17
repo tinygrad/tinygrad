09a45939ca1f045033a7d322b9dbdeaae6a46b82
detolly <thomas@detolly.no>
2024-10-17 07:52:31 +0200

    clang: cleanup + include tbss

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -5,16 +5,15 @@ from tinygrad.helpers import OSX, cpu_objdump, cpu_time_execution, DEBUG
 from tinygrad.renderer.cstyle import ClangRenderer
 from tinygrad.runtime.autogen import libc
 
-linker_script = '''
-SECTIONS {
-    .text : {
-        . = 0x0;
-        *(.tinygrad);
-        *(.text);
-        *(.rodata*);
-    }
-}
-'''
+linker_script = '''SECTIONS {
+  .text : {
+    . = 0x0;
+    *(.tinygrad);
+    *(.text*);
+    *(.rodata*);
+    *(.tbss*);
+  }
+}'''
 class ClangCompiler(Compiler):
   def __init__(self, cachekey="compile_clang", args:Optional[List[str]]=None):
     self.args = ['-march=native'] if args is None else args
@@ -23,8 +22,8 @@ class ClangCompiler(Compiler):
     with tempfile.NamedTemporaryFile(delete=True) as file, tempfile.NamedTemporaryFile(delete=True) as linker_file:
       pathlib.Path(linker_file.name).write_text(linker_script)
       subprocess.check_output([
-        'clang', *self.args, '-O2', '-Wall', '-Werror', '-Wno-gcc-compat', '-x', 'c', '-', '-fPIE', '-fPIC', '-static',
-        '-', '-o', pathlib.Path(file.name), '-T', pathlib.Path(linker_file.name), '-lc', '-lm', '-nostdlib', '-ffreestanding'
+        'clang',*self.args,'-O2','-Wall','-Werror','-Wno-gcc-compat','-x','c','-','-fPIE','-fPIC','-static','-lc','-lm',
+        '-o',pathlib.Path(file.name),'-T',pathlib.Path(linker_file.name),'-nostdlib','-ffreestanding','-Wl,--build-id=none'
       ], input=src.encode('utf-8'))
       return subprocess.check_output(['objcopy', '-O', 'binary', '--only-section=.text', pathlib.Path(file.name), '/dev/stdout'])
 
@@ -33,13 +32,13 @@ class ClangProgram:
     if DEBUG >= 6:
       with tempfile.NamedTemporaryFile(delete=True) as f:
         pathlib.Path(f.name).write_bytes(code)
-        print(subprocess.check_output(['objdump', '-b', 'binary', '-m', 'i', '--disassembler-color=on', '-D', f.name]).decode('utf-8'))
+        print(subprocess.check_output(['objdump','-b','binary','-m','i','--disassembler-color=on','-D',f.name]).decode('utf-8'))
     self.name, self.obj = name, code
     self.buf, self.len = libc.mmap(0, len(code), mmap.PROT_READ | mmap.PROT_WRITE | mmap.PROT_EXEC,
                                    mmap.MAP_PRIVATE | mmap.MAP_ANONYMOUS, -1, 0), len(code)
     ctypes.memmove(self.buf, (ctypes.c_char * len(code)).from_buffer_copy(code), len(code))
   def __del__(self):
-    libc.munmap(self.buf, self.len)
+    if hasattr(self, "buf") and hasattr(self, 'len'): libc.munmap(self.buf, self.len)
   def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: ctypes.CFUNCTYPE(None)(self.buf)(*bufs, *vals), enable=wait)
 
 class ClangCompilerOSX(Compiler):
