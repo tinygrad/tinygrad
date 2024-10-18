5fb6195cdb08cb3d64414d53fcb5d899687eaeca
ttomsa <tomasvsilva8@gmail.com>
2024-10-18 02:57:38 +0100

    no metal test

--- test/test_int64_idx.py
+++ test/test_int64_idx.py
@@ -2,6 +2,7 @@ import unittest
 from tinygrad import Tensor, dtypes, Context, Device
 
 class TestInt64Indexing(unittest.TestCase):
+  @unittest.skipIf(Device.DEFAULT in ("METAL"), "memory error in METAL")
   def test_int64_indexing(self):
     t = Tensor.ones((1 << 31) + 4, dtype=dtypes.int8).contiguous()
     assert t[-1].item() == 1
