912415c9bb08349b6a51e4b587bc2ce1af1b94ec
nimlgen <lenyaprokh@gmail.com>
2024-10-18 21:39:34 +0300

    cuda on windows

--- autogen_stubs.sh
+++ autogen_stubs.sh
@@ -85,7 +85,8 @@ generate_kfd() {
 generate_cuda() {
   clang2py /usr/include/cuda.h -o $BASE/cuda.py -l /usr/lib/x86_64-linux-gnu/libcuda.so
   sed -i "s\import ctypes\import ctypes, ctypes.util\g" $BASE/cuda.py
-  sed -i "s\ctypes.CDLL('/usr/lib/x86_64-linux-gnu/libcuda.so')\ctypes.CDLL(ctypes.util.find_library('cuda'))\g" $BASE/cuda.py
+  patch_dlopen $BASE/cuda.py cuda "ctypes.CDLL(ctypes.util.find_library('cuda'))" "ctypes.CDLL(ctypes.util.find_library('nvcuda'))"
+  sed -i "s\ctypes.CDLL('/usr/lib/x86_64-linux-gnu/libcuda.so')\_try_dlopen_cuda()\g" $BASE/cuda.py
   fixup $BASE/cuda.py
   python3 -c "import tinygrad.runtime.autogen.cuda"
 }
@@ -93,8 +94,13 @@ generate_cuda() {
 generate_nvrtc() {
   clang2py /usr/local/cuda/include/nvrtc.h /usr/local/cuda/include/nvJitLink.h -o $BASE/nvrtc.py -l /usr/local/cuda/lib64/libnvrtc.so -l /usr/local/cuda/lib64/libnvJitLink.so
   sed -i "s\import ctypes\import ctypes, ctypes.util\g" $BASE/nvrtc.py
-  sed -i "s\ctypes.CDLL('/usr/local/cuda/lib64/libnvrtc.so')\ctypes.CDLL(ctypes.util.find_library('nvrtc'))\g" $BASE/nvrtc.py
-  sed -i "s\ctypes.CDLL('/usr/local/cuda/lib64/libnvJitLink.so')\ctypes.CDLL(ctypes.util.find_library('nvJitLink'))\g" $BASE/nvrtc.py
+
+  patch_dlopen $BASE/nvrtc.py nvrtc "ctypes.CDLL(ctypes.util.find_library('nvrtc'))" "ctypes.CDLL(ctypes.util.find_library('nvrtc64_120_0'))"
+  sed -i "s\ctypes.CDLL('/usr/local/cuda/lib64/libnvrtc.so')\if platform.system() == "Windows": ctypes.CDLL(ctypes.util.find_library('nvrtc-builtins64_126'))\n_try_dlopen_nvrtc()\g" $BASE/nvrtc.py
+
+  patch_dlopen $BASE/nvrtc.py jitlink "ctypes.CDLL(ctypes.util.find_library('nvJitLink'))" "ctypes.CDLL(ctypes.util.find_library('nvJitLink_120_0'))"
+  sed -i "s\ctypes.CDLL('/usr/local/cuda/lib64/libnvJitLink.so')\_try_dlopen_nvrtc()\g" $BASE/nvrtc.py
+
   fixup $BASE/nvrtc.py
   python3 -c "import tinygrad.runtime.autogen.nvrtc"
 }
