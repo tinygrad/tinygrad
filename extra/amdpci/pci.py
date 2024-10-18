3861eefaf6580e67a4c5fd181d0a8fb66a090bd1
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-10-18 07:52:18 -0700

    gart

--- extra/amdpci/pci.py
+++ extra/amdpci/pci.py
@@ -339,19 +339,19 @@ def replay_rlc():
   assert val == 0x1000
 
   amdgpu_wreg(0x3696, 0x40) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x40:
+  while amdgpu_rreg(0x3697) != 0x40: pass
   #   print(amdgpu_rreg(0x3697))
   amdgpu_wreg(0x3696, 0x80) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x80:
+  while amdgpu_rreg(0x3697) != 0x80: pass
   #   print(amdgpu_rreg(0x3697))
   amdgpu_wreg(0x3696, 0xc0) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0xc0:
+  while amdgpu_rreg(0x3697) != 0xc0: pass
   #   print(amdgpu_rreg(0x3697))
   amdgpu_wreg(0x3696, 0x100) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x100:
+  while amdgpu_rreg(0x3697) != 0x100: pass
   #   print(amdgpu_rreg(0x3697))
   amdgpu_wreg(0x3696, 0x140) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x140:
+  while amdgpu_rreg(0x3697) != 0x140: pass
   #   print(amdgpu_rreg(0x3697))
   val = amdgpu_rreg(0x39bc) # amdgpu_cgs_read_register:47:(offset)
   assert val == 0x1000
@@ -895,7 +895,7 @@ def replay_rlc():
   amdgpu_wreg(0x9e94, 0x3e80000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9e8e, 0x81010000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x1c0) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x1c0: pass
+  while amdgpu_rreg(0x3697) != 0x1c0: pass
 
   amdgpu_wreg(0x9ed8, 0x3) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9ed8, 0x1) # amdgpu_cgs_write_register:54:(offset)
@@ -918,7 +918,7 @@ def replay_rlc():
   amdgpu_wreg(0x9ed5, 0x3e80000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9ecf, 0x81010000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x200) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x200: pass
+  while amdgpu_rreg(0x3697) != 0x200: pass
 
   amdgpu_wreg(0x9f19, 0x3) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9f19, 0x1) # amdgpu_cgs_write_register:54:(offset)
@@ -941,7 +941,7 @@ def replay_rlc():
   amdgpu_wreg(0x9f16, 0x3e80000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9f10, 0x81010000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x240) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x240: pass
+  while amdgpu_rreg(0x3697) != 0x240: pass
 
   amdgpu_wreg(0x9f5a, 0x3) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9f5a, 0x1) # amdgpu_cgs_write_register:54:(offset)
@@ -964,7 +964,7 @@ def replay_rlc():
   amdgpu_wreg(0x9f57, 0x3e80000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x9f51, 0x81010000) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x280) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x280: pass
+  while amdgpu_rreg(0x3697) != 0x280: pass
 
   amdgpu_wreg(0x539e, 0x0) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x134, 0x0) # amdgpu_cgs_write_register:54:(offset)
@@ -1048,13 +1048,13 @@ def replay_rlc():
   # assert val == 0x1000
   amdgpu_wreg(0x39bc, 0x1000) # amdgpu_cgs_write_register:54:(offset)
   val = amdgpu_rreg(0x397b) # amdgpu_cgs_read_register:47:(offset)
-  assert val == 0x0
+  # assert val == 0x0
   val = amdgpu_rreg(0x397c) # amdgpu_cgs_read_register:47:(offset)
-  assert val == 0x0
+  # assert val == 0x0
   val = amdgpu_rreg(0x397d) # amdgpu_cgs_read_register:47:(offset)
-  assert val == 0x0
+  # assert val == 0x0
   val = amdgpu_rreg(0x397e) # amdgpu_cgs_read_register:47:(offset)
-  assert val == 0x0
+  # assert val == 0x0
   val = amdgpu_rreg(0x397a) # amdgpu_cgs_read_register:47:(offset)
   assert val == 0x40202
   amdgpu_wreg(0x3984, 0x400100) # amdgpu_cgs_write_register:54:(offset)
@@ -1062,7 +1062,7 @@ def replay_rlc():
   assert val == 0x147faa15
   amdgpu_wreg(0x3985, 0x147faa15) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x2c0) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x2c0: pass
+  while amdgpu_rreg(0x3697) != 0x2c0: pass
 
   val = amdgpu_rreg(0x263e) # amdgpu_dm_plane_add_gfx11_modifiers:664:(adev->reg_offset[GC_HWIP][0][0] + 0x13de)
   assert val == 0x545
@@ -1077,7 +1077,7 @@ def replay_rlc():
   val = amdgpu_rreg(0x263e) # amdgpu_dm_plane_add_gfx11_modifiers:664:(adev->reg_offset[GC_HWIP][0][0] + 0x13de)
   assert val == 0x545
   amdgpu_wreg(0x3696, 0x300) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x300: pass
+  while amdgpu_rreg(0x3697) != 0x300: pass
 
   val = amdgpu_rreg(0x5db4) # amdgpu_cgs_read_register:47:(offset)
   assert val == 0x44440440
@@ -1120,7 +1120,7 @@ def replay_rlc():
   assert val == 0x22220202
   amdgpu_wreg(0x5db6, 0x22220202) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x340) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x340: pass
+  while amdgpu_rreg(0x3697) != 0x340: pass
 
   val = amdgpu_rreg(0x5db4) # amdgpu_cgs_read_register:47:(offset)
   assert val == 0x44440440
@@ -1163,7 +1163,7 @@ def replay_rlc():
   assert val == 0x22220202
   amdgpu_wreg(0x5db6, 0x22220202) # amdgpu_cgs_write_register:54:(offset)
   amdgpu_wreg(0x3696, 0x380) # amdgpu_cgs_write_register:54:(offset)
-  # while amdgpu_rreg(0x3697) != 0x380: pass
+  while amdgpu_rreg(0x3697) != 0x380: pass
 
   val = amdgpu_rreg(0x5db4) # amdgpu_cgs_read_register:47:(offset)
   assert val == 0x44440440
@@ -1462,24 +1462,132 @@ def gfx_v11_0_config_gfx_rs64():
 
   print("done gfx_v11_0_config_gfx_rs64")
 
-# def gfxhub_v3_0_setup_vm_pt_regs():
-#   pass
+gart_base = 0x7fff00000000
+gart_size = 0x20000000
+pt_base = 0x5feb00001 # end of VRAM, 1 is for VALID?
 
-# def gfxhub_v3_0_init_gart_aperture_regs():
-#   gfxhub_v3_0_setup_vm_pt_regs()
+regGCVM_CONTEXT0_PAGE_TABLE_BASE_ADDR_LO32 = 0x2953
+regGCVM_CONTEXT0_PAGE_TABLE_BASE_ADDR_HI32 = 0x2954
+def gfxhub_v3_0_setup_vm_pt_regs(pt_base, vmid):
+  assert vmid == 0, "not suppoer any other"
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_BASE_ADDR_LO32, pt_base & 0xffffffff)
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_BASE_ADDR_HI32, (pt_base >> 32) & 0xffffffff)
 
-# def gfxhub_v3_0_gart_enable():
-#   gfxhub_v3_0_init_gart_aperture_regs()
+regGCVM_CONTEXT0_PAGE_TABLE_START_ADDR_LO32 = 0x2973
+regGCVM_CONTEXT0_PAGE_TABLE_START_ADDR_HI32 = 0x2974
+regGCVM_CONTEXT0_PAGE_TABLE_END_ADDR_LO32 = 0x2993
+regGCVM_CONTEXT0_PAGE_TABLE_END_ADDR_HI32 = 0x2994
+def gfxhub_v3_0_init_gart_aperture_regs():
+  gfxhub_v3_0_setup_vm_pt_regs(pt_base, 0)
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_START_ADDR_LO32, gart_base & 0xffffffff)
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_START_ADDR_HI32, (gart_base >> 32) & 0xffffffff)
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_END_ADDR_LO32, (gart_base + gart_size) & 0xffffffff)
+  amdgpu_wreg(regGCVM_CONTEXT0_PAGE_TABLE_END_ADDR_HI32, ((gart_base + gart_size) >> 32) & 0xffffffff)
 
-# def gfx_v11_0_gfxhub_enable():
-#   print("start gfx_v11_0_gfxhub_enable")
+agp_start = (0xffffff << 24)
+agp_end = (0x0 << 24)
+fb_start = (0x200000 << 18)
+fb_end = (0x217fbf << 18)
 
+regGCMC_VM_AGP_BASE = 0x28dc
+regGCMC_VM_AGP_BOT = 0x28db
+regGCMC_VM_AGP_TOP = 0x28da
+regGCMC_VM_SYSTEM_APERTURE_LOW_ADDR = 0x28dd
+regGCMC_VM_SYSTEM_APERTURE_HIGH_ADDR = 0x28de
+regGCMC_VM_SYSTEM_APERTURE_DEFAULT_ADDR_LSB = 0x2808
+regGCMC_VM_SYSTEM_APERTURE_DEFAULT_ADDR_MSB = 0x2809
+regGCVM_L2_PROTECTION_FAULT_DEFAULT_ADDR_LO32 = 0x282b
+regGCVM_L2_PROTECTION_FAULT_DEFAULT_ADDR_HI32 = 0x282c
+regGCVM_L2_PROTECTION_FAULT_CNTL2 = 0x2825
+def gfxhub_v3_0_init_system_aperture_regs():
+  amdgpu_wreg(regGCMC_VM_AGP_BASE, 0)
+  amdgpu_wreg(regGCMC_VM_AGP_BOT, agp_start >> 24)
+  amdgpu_wreg(regGCMC_VM_AGP_TOP, agp_end >> 24)
+  amdgpu_wreg(regGCMC_VM_AGP_BOT, min(fb_start, agp_start) >> 18)
+  amdgpu_wreg(regGCMC_VM_AGP_TOP, min(fb_end, agp_end) >> 18)
+  amdgpu_wreg(regGCMC_VM_SYSTEM_APERTURE_DEFAULT_ADDR_LSB, 0x5feaff) # gfxhub_v3_0_init_system_aperture_regs:178:((adev->reg_offset[GC_HWIP][0][0] + 0x15a8))
+  amdgpu_wreg(regGCMC_VM_SYSTEM_APERTURE_DEFAULT_ADDR_MSB, 0x0) # gfxhub_v3_0_init_system_aperture_regs:180:((adev->reg_offset[GC_HWIP][0][0] + 0x15a9))
+  amdgpu_wreg(regGCVM_L2_PROTECTION_FAULT_DEFAULT_ADDR_LO32, 0x3008e) # gfxhub_v3_0_init_system_aperture_regs:184:((adev->reg_offset[GC_HWIP][0][0] + 0x15cb))
+  amdgpu_wreg(regGCVM_L2_PROTECTION_FAULT_DEFAULT_ADDR_HI32, 0x0) # gfxhub_v3_0_init_system_aperture_regs:186:((adev->reg_offset[GC_HWIP][0][0] + 0x15cc))
+  val = amdgpu_rreg(regGCVM_L2_PROTECTION_FAULT_CNTL2) # gfxhub_v3_0_init_system_aperture_regs:189:(adev->reg_offset[GC_HWIP][0][0] + 0x15c5)
+  amdgpu_wreg(regGCVM_L2_PROTECTION_FAULT_CNTL2, 0x60000) # gfxhub_v3_0_init_system_aperture_regs:189:(adev->reg_offset[GC_HWIP][0][0] + 0x15c5)
+
+
+regGCMC_VM_MX_L1_TLB_CNTL = 0x28df
+def gfxhub_v3_0_init_tlb_regs():
+  # tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL, ENABLE_L1_TLB, 1);
+	# tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL, SYSTEM_ACCESS_MODE, 3);
+	# tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL,
+	# 		    ENABLE_ADVANCED_DRIVER_MODEL, 1);
+	# tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL,
+	# 		    SYSTEM_APERTURE_UNMAPPED_ACCESS, 0);
+	# tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL, ECO_BITS, 0);
+	# tmp = REG_SET_FIELD(tmp, GCMC_VM_MX_L1_TLB_CNTL,
+	# 		    MTYPE, MTYPE_UC); /* UC, uncached */
+  amdgpu_wreg(regGCMC_VM_MX_L1_TLB_CNTL, 0x1859)
+
+def gfxhub_v3_0_init_cache_regs():
+  # TODO: enable cache
+  pass
+
+regGCVM_CONTEXT0_CNTL = 0x28e8
+def gfxhub_v3_0_enable_system_domain():
+  # ENABLE_CONTEXT=1, PAGE_TABLE_DEPTH=0, RETRY_PERMISSION_OR_INVALID_PAGE_FAULT=0
+  amdgpu_wreg(regGCVM_CONTEXT0_CNTL, 0x1fffe01)
+
+regGCVM_CONTEXT1_CNTL = 0x28e9
+regGCVM_CONTEXT1_PAGE_TABLE_START_ADDR_LO32 = 0x2975
+regGCVM_CONTEXT1_PAGE_TABLE_START_ADDR_HI32 = 0x2976
+regGCVM_CONTEXT1_PAGE_TABLE_END_ADDR_LO32 = 0x2995
+regGCVM_CONTEXT1_PAGE_TABLE_END_ADDR_HI32 = 0x2996
+def gfxhub_v3_0_setup_vmid_config():
+  ctx_distance = 1
+  ctx_addr_distance = 2
+
+  for vmid in range(15):
+    amdgpu_wreg(regGCVM_CONTEXT1_CNTL + ctx_distance * vmid, 0x1fffe07) # gfxhub_v3_0_setup_vmid_config:332:((adev->reg_offset[GC_HWIP][0][0] + 0x1689) + i * hub->ctx_distance)
+    amdgpu_wreg(regGCVM_CONTEXT1_PAGE_TABLE_START_ADDR_LO32 + ctx_addr_distance * vmid, 0x0) # gfxhub_v3_0_setup_vmid_config:334:((adev->reg_offset[GC_HWIP][0][0] + 0x1715) + i * hub->ctx_addr_distance)
+    amdgpu_wreg(regGCVM_CONTEXT1_PAGE_TABLE_START_ADDR_HI32 + ctx_addr_distance * vmid, 0x0) # gfxhub_v3_0_setup_vmid_config:336:((adev->reg_offset[GC_HWIP][0][0] + 0x1716) + i * hub->ctx_addr_distance)
+    amdgpu_wreg(regGCVM_CONTEXT1_PAGE_TABLE_END_ADDR_LO32 + ctx_addr_distance * vmid, 0xffffffff) # gfxhub_v3_0_setup_vmid_config:338:((adev->reg_offset[GC_HWIP][0][0] + 0x1735) + i * hub->ctx_addr_distance)
+    amdgpu_wreg(regGCVM_CONTEXT1_PAGE_TABLE_END_ADDR_HI32 + ctx_addr_distance * vmid, 0xf) # gfxhub_v3_0_setup_vmid_config:341:((adev->reg_offset[GC_HWIP][0][0] + 0x1736) + i * hub->ctx_addr_distance)
+
+regGCVM_INVALIDATE_ENG0_ADDR_RANGE_LO32 = 0x292f
+regGCVM_INVALIDATE_ENG0_ADDR_RANGE_HI32 = 0x2930
+def gfxhub_v3_0_program_invalidation():
+  eng_addr_distance = 2
+
+  for i in range(18):
+    amdgpu_wreg(regGCVM_INVALIDATE_ENG0_ADDR_RANGE_LO32 + eng_addr_distance * i, 0xffffffff)
+    amdgpu_wreg(regGCVM_INVALIDATE_ENG0_ADDR_RANGE_HI32 + eng_addr_distance * i, 0x1f)
+
+def hdp_v6_0_flush_hdp(): amdgpu_wreg(0x1fc00, 0x0)
+def gmc_v11_0_flush_gpu_tlb():
+  amdgpu_wreg(0x291c, 0xf80001)
+  while amdgpu_rreg(0x292e) != 1: pass
+
+def gfxhub_v3_0_gart_enable():
+  gfxhub_v3_0_init_gart_aperture_regs()
+  gfxhub_v3_0_init_system_aperture_regs()
+  gfxhub_v3_0_init_tlb_regs()
+  gfxhub_v3_0_init_cache_regs()
+
+  gfxhub_v3_0_enable_system_domain()
+  # gfxhub_v3_0_disable_identity_aperture()
+  gfxhub_v3_0_setup_vmid_config()
+  gfxhub_v3_0_program_invalidation()
+  print("done gfxhub_v3_0_gart_enable")
+
+
+def gfx_v11_0_gfxhub_enable():
+  print("start gfx_v11_0_gfxhub_enable")
+  gfxhub_v3_0_gart_enable()
+  hdp_v6_0_flush_hdp()
+
+  gmc_v11_0_flush_gpu_tlb()
+  hdp_v6_0_flush_hdp()
 
 gfx_v11_0_config_gfx_rs64()
-
-# def stupid_exec():
-  
-# gfx_v11_0_gfxhub_enable()
+gfx_v11_0_gfxhub_enable()
 
 
 
