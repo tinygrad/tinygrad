e72488597844a72e70abc5a4344f28b9c98df166
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2024-10-22 00:02:57 +0200

    minimal

--- test/test_device_behavior.py
+++ test/test_device_behavior.py
@@ -2,17 +2,8 @@ import unittest
 
 from tinygrad import Tensor
 
-
 class TestDeviceBehavior(unittest.TestCase):
   def test_should_pass(self):
-    queue = Tensor.zeros((3, 1)).contiguous()
-    buffered = Tensor([2, 1]).unsqueeze(-1)
-    replace = buffered.shape[0]
-    queue[replace:] = queue[:-replace]
-    queue[:replace] = buffered
-    buffered = Tensor([3]).unsqueeze(-1)
-    replace = buffered.shape[0]
-    queue[replace:] = queue[:-replace]
-    queue[:replace] = buffered
-    res = queue.tolist()
-    assert res == [[3.0], [2.0], [1.0]], res
+    t = Tensor([[3.0], [2.0], [1.0]]).contiguous()
+    t[1:] = t[:-1]
+    assert t.tolist() == [[3.0], [3.0], [2.0]]
