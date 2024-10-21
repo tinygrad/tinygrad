18f78ef49275c67a36be2d78beeeaf27514a01c0
Maximilian Wolf <69987866+MaxWolf-01@users.noreply.github.com>
2024-10-21 13:40:39 +0200

    tinyfy

--- test/test_device_behavior.py
+++ test/test_device_behavior.py
@@ -1,21 +1,11 @@
-import os
 import unittest
 
 from tinygrad import Tensor
 
 
 class TestDeviceBehavior(unittest.TestCase):
-  def test_device_behavior_gpu(self):
-    os.environ["GPU"] = "1"
-    self._should_pass()
-
-  def test_device_behavior_clang(self):
-    os.environ["CLANG"] = "1"
-    self._should_pass()
-
-  @staticmethod
-  def _should_pass(qlen: int = 3, qsz: int = 1) -> None:
-    queue = Tensor.zeros((qlen, qsz)).contiguous()
+  def test_should_pass(self):
+    queue = Tensor.zeros((3, 1)).contiguous()
     buffered = Tensor.cat(*[Tensor([3]), Tensor([1])]).unsqueeze(-1)
     replace = buffered.shape[0]
     queue[replace:] = queue[:-replace]
@@ -24,5 +14,4 @@ class TestDeviceBehavior(unittest.TestCase):
     replace = buffered.shape[0]
     queue[replace:] = queue[:-replace]
     queue[:replace] = buffered
-    res = queue.tolist()
-    assert res == [[4.0], [3.0], [1.0]], res
+    assert (res := queue.tolist()) == [[4.0], [3.0], [1.0]], res
