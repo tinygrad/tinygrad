ba79165edaef56bb287dce1f0cc266ac7581e2a1
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-10-23 15:38:59 +0200

    Simplify shift emulation in wgsl

--- test/test_rand.py
+++ test/test_rand.py
@@ -2,6 +2,8 @@ from tinygrad import Tensor
 from tinygrad.dtype import dtypes
 
 if __name__ == '__main__':
-    x = Tensor.rand(3)
+    '''x = Tensor.rand(3)
     y = x.numpy()
-    print(y)
+    print(y)'''
+    x = Tensor([4294967296], dtype=dtypes.uint64)
+    print((x << 2).numpy())
