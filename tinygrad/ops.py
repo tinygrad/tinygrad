3899eff4deb41bc382efa3c47450bfcd9ec7794e
George Hotz <geohot@gmail.com>
2024-10-24 15:37:49 +0800

    leftover lru_cache on UPat [pr]

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -522,14 +522,11 @@ class UPat(MathTrait):
   def any(*src): return UPatAny(src=src)
 
   @staticmethod
-  @functools.lru_cache(None)
   def var(name:Optional[str]=None, dtype:Optional[DType]=None): return UPat(dtype=dtype, name=name)
   @staticmethod
-  @functools.lru_cache(None)
   def cvar(name:Optional[str]=None, dtype:Optional[DType]=None, vec=True):
     return UPat((UOps.CONST, UOps.VCONST) if vec else UOps.CONST, dtype=dtype, name=name)
   @staticmethod
-  @functools.lru_cache(None)
   def const(dtype:Optional[DType], b:ConstType): return UPat(UOps.CONST, dtype=dtype, arg=b)
 
   # copied from UOp
