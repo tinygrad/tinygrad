6f02f01fe936ea8e0e5c786752084fbfaeebff0d
p4sscode <p4ssenger.developer@gmail.com>
2024-10-25 14:49:42 -0300

    hotifx: typo

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -83,7 +83,7 @@ class Renderer:
   shared_max: int = 32768
   tensor_cores: List[TensorCore] = []
   extra_matcher: Any = None
-  # different types are neccesary while ptx and llvm are not pm-like
+  # different types are necessary while ptx and llvm are not pm-like
   code_for_op: Union[Dict[Op, Callable], Dict[Union[Op, Tuple[Op, Optional[Tuple[DType, ...]]]], Callable]] = {}
 
   def __reduce__(self): return self.__class__, ()
