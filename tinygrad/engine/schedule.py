7a476821f3bf776b2e7202971838c7fcfafa567b
qazal <qazal.software@gmail.com>
2024-10-29 14:23:43 +0200

    global metadata 3

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -163,6 +163,7 @@ view_right = merge_views+PatternMatcher([
 class ScheduleItemContext:
   var_vals: Dict[Variable, int]
   assigned: Set[UOp]
+  ubuf_metadata: Dict[UOp, Metadata]
   sts: Set[ShapeTracker] = field(default_factory=set)
   bufs: List[UOp] = field(default_factory=list)
   assign_preloads: List[UOp] = field(default_factory=list)
@@ -202,6 +203,7 @@ multioutput = PatternMatcher([
 ])
 
 def full_ast_rewrite(pre:UOp, ctx:ScheduleItemContext) -> UOp:
+  ctx.metadata.update((mx,None) for x in pre.sparents if x.op in BUFFER_UOPS and len(x.src) > 2 and (mx:=ctx.ubuf_metadata.get(x.src[0])))
   # fuse and fold store -> loads
   sink = graph_rewrite(pre, lazy)
   # fuse multi output
@@ -220,7 +222,7 @@ def full_ast_rewrite(pre:UOp, ctx:ScheduleItemContext) -> UOp:
         and ShapeTracker.from_shape(s.shape).shrink(m) == s.shrink(m)) for x in sink.sparents if x.op is UOps.LOAD and x.src[0] in assign_targets):
       raise RuntimeError("self operand of augmented assign must be contiguous.\nhelp: consider using .contiguous():\n"
                          +colored("   - a += a.T\n", "red")+colored("   + a += a.T.contiguous()", "green"))
-  if getenv("RUN_PROCESS_REPLAY"): PROCESS_REPLAY_CAPTURE.append((pre, ScheduleItemContext(ctx.var_vals, ctx.assigned), sink))
+  if getenv("RUN_PROCESS_REPLAY"): PROCESS_REPLAY_CAPTURE.append((pre, ScheduleItemContext(ctx.var_vals, ctx.assigned, ctx.ubuf_metadata), sink))
   return sink
 
 PROCESS_REPLAY_CAPTURE: List[Tuple[UOp, ScheduleItemContext, UOp]] = []
@@ -242,8 +244,7 @@ def create_schedule_with_vars(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem]
     cache: Dict[LazyBuffer, UOp] = {}
     to_store = tuple(to_uop(out, outs, ctx, cache) for out in outs)
     sink = UOp(UOps.SINK, src=tuple(UOp.store(ctx.buf_uops[x.buffer], ShapeTracker.from_shape(x.shape).to_uop(), u) for x,u in zip(outs,to_store)))
-    metadata = {mx:None for x in sink.sparents if x.op in BUFFER_UOPS and len(x.src) > 2 and (mx:=ctx.ubuf_metadata.get(x.src[0]))}
-    si_ctx = ScheduleItemContext(ctx.var_vals, {ubuf for x in assigns if (ubuf:=ctx.buf_uops.get(x.buffer)) is not None}, metadata=metadata)
+    si_ctx = ScheduleItemContext(ctx.var_vals, {ubuf for x in assigns if (ubuf:=ctx.buf_uops.get(x.buffer)) is not None}, ctx.ubuf_metadata)
     small_graphs.append((full_ast_rewrite(sink, si_ctx), si_ctx))
 
   # do BFS
