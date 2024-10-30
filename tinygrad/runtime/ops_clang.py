bbd018c4b312a0e242a94a5296d36eb774327c03
Elisha Hollander <just4now666666@gmail.com>
2024-10-30 17:56:38 +0200

    use `mmap` instead of `CDLL`

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -3,10 +3,12 @@ import ctypes, subprocess, pathlib, tempfile
 from tinygrad.device import Compiled, Compiler, MallocAllocator
 from tinygrad.helpers import cpu_time_execution, DEBUG, cpu_objdump
 from tinygrad.renderer.cstyle import ClangRenderer
+from tinygrad.runtime.autogen.libc import mmap, memcpy
+from mmap import PROT_READ, PROT_WRITE, PROT_EXEC, MAP_ANON, MAP_PRIVATE
 
 class ClangCompiler(Compiler):
   def __init__(self, cachekey="compile_clang", args:Optional[List[str]]=None):
-    self.args = ['-march=native'] if args is None else args
+    self.args = args or []
     super().__init__(cachekey)
 
   def compile(self, src:str) -> bytes:
@@ -14,16 +16,15 @@ class ClangCompiler(Compiler):
     with tempfile.NamedTemporaryFile(delete=True) as output_file:
       subprocess.check_output(['clang', '-shared', *self.args, '-O2', '-Wall', '-Werror', '-x', 'c', '-fPIC', '-ffreestanding', '-nostdlib',
                                '-', '-o', str(output_file.name)], input=src.encode('utf-8'))
+      subprocess.check_output(['objcopy', output_file.name, '-j', '.text', '-O', 'binary'])
       return pathlib.Path(output_file.name).read_bytes()
 
 class ClangProgram:
   def __init__(self, name:str, lib:bytes):
     if DEBUG >= 6: cpu_objdump(lib)
     self.name, self.lib = name, lib
-    # write to disk so we can load it
-    with tempfile.NamedTemporaryFile(delete=True) as cached_file_path:
-      pathlib.Path(cached_file_path.name).write_bytes(lib)
-      self.fxn = ctypes.CDLL(str(cached_file_path.name))[name]
+    flags = MAP_ANON | MAP_PRIVATE | 0x100 # no MAP_FIXED in mmap for some reason
+    self.fxn = ctypes.CFUNCTYPE(None)(memcpy(mmap(0, len(lib), PROT_READ | PROT_WRITE | PROT_EXEC, flags, -1, 0), lib, len(lib)))
 
   def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: self.fxn(*bufs, *vals), enable=wait)
 
