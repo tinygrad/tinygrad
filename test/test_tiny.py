4a5360e521c0cc88fc038ddecf2ae768a33f479c
George Hotz <geohot@gmail.com>
2024-10-30 17:06:25 +0800

    only image test on GPU

--- test/test_tiny.py
+++ test/test_tiny.py
@@ -1,6 +1,6 @@
 # basic self-contained tests of the external functionality of tinygrad
 import unittest
-from tinygrad import Tensor, Context, Variable, TinyJit, dtypes
+from tinygrad import Tensor, Context, Variable, TinyJit, dtypes, Device
 
 class TestTiny(unittest.TestCase):
 
@@ -18,9 +18,9 @@ class TestTiny(unittest.TestCase):
     out = Tensor.cat(Tensor.ones(8).contiguous(), Tensor.ones(8).contiguous())
     self.assertListEqual(out.tolist(), [1]*16)
 
-  def test_gemm(self, N=4, device=None, out_dtype=dtypes.float):
-    a = Tensor.ones(N,N,device=device).contiguous()
-    b = Tensor.eye(N,device=device).contiguous()
+  def test_gemm(self, N=4, out_dtype=dtypes.float):
+    a = Tensor.ones(N,N).contiguous()
+    b = Tensor.eye(N).contiguous()
     self.assertListEqual((out:=a@b).flatten().tolist(), [1.0]*(N*N))
     self.assertEqual(out.dtype, out_dtype)
 
@@ -59,8 +59,9 @@ class TestTiny(unittest.TestCase):
 
   # *** image ***
 
+  @unittest.skipIf(Device.DEFAULT != "GPU", "image only supported on GPU")
   def test_image(self):
-    with Context(IMAGE=2): self.test_gemm(device="GPU", out_dtype=dtypes.imagef((4, 1, 4)))
+    with Context(IMAGE=2): self.test_gemm(out_dtype=dtypes.imagef((4, 1, 4)))
 
   def test_beam_image(self):
     with Context(BEAM=1): self.test_image()
