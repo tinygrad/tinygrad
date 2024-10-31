996152d2de6ab70c12d5c4327a959b4bdd97890b
George Hotz <72895+geohot@users.noreply.github.com>
2024-10-31 15:13:11 +0700

    improve full_graph_rewrite matchers for speed (#7431)
    * remove finalize [pr]
    
    * early transcendental
    
    * fix tests
    
    * load store indexing runs with devectorize
    
    * move delete_redundant_gates
    
    * ptx has to wait for the mask to move

--- test/test_uop_graph.py
+++ test/test_uop_graph.py
@@ -4,8 +4,9 @@ from tinygrad import dtypes, Device
 from tinygrad.helpers import DEBUG
 from tinygrad.ops import BinaryOps, TernaryOps, UnaryOps, UOps, UOp, KernelInfo
 from tinygrad.ops import UPat, PatternMatcher
+from tinygrad.renderer import Renderer
 from tinygrad.codegen.lowerer import rewrite_shapetracker_with_index
-from tinygrad.codegen.uopgraph import full_graph_rewrite, graph_rewrite, expander, reducer, sym, float4_folding, finalize, migrate_indexing
+from tinygrad.codegen.uopgraph import full_graph_rewrite, graph_rewrite, expander, load_store_indexing, sym, float4_folding, migrate_indexing
 from tinygrad.codegen.linearize import linearize_uop
 from tinygrad.shape.shapetracker import ShapeTracker, View
 
@@ -445,12 +446,10 @@ class TestUOpGraph(unittest.TestCase):
 
 def expander_rewrite(sink):
   sink = graph_rewrite(sink, sym + expander)
-  sink = graph_rewrite(sink, sym + reducer)
-  return graph_rewrite(sink, sym + finalize)
+  return graph_rewrite(sink, sym + load_store_indexing)
 def float4_rewrite(sink):
   sink = graph_rewrite(sink, sym + migrate_indexing)
-  sink = graph_rewrite(sink, sym + expander + float4_folding)
-  return graph_rewrite(sink, sym + finalize)
+  return graph_rewrite(sink, sym + expander + float4_folding)
 
 class TestExpander(unittest.TestCase):
   def test_expand_add_broadcast(self):
@@ -618,11 +617,11 @@ class TestLoadStoreFolder(unittest.TestCase):
     buf = UOp(UOps.DEFINE_GLOBAL, dtypes.float.ptr())
     gate = UOp(UOps.DEFINE_VAR, dtypes.bool)
     load = [UOp(UOps.LOAD, dtypes.float, (buf, UOp.const(dtypes.int, i), UOp.const(dtypes.float, 0), gate)) for i in range(4)]
-    sink = UOp(UOps.VECTORIZE, dtypes.float.vec(len(load)), tuple(load))
-    sink = float4_rewrite(sink)
+    sink = UOp(UOps.VECTORIZE, dtypes.float.vec(len(load)), tuple(load)).sink()
+    sink = full_graph_rewrite(sink, Renderer())
     assert len([x for x in sink.sparents if x.op is UOps.LOAD]) == 1
     single_load = [x for x in sink.sparents if x.op is UOps.LOAD][0]
-    self.assertEqual(single_load.src[1].op, UOps.CONST)
+    self.assertEqual(single_load.src[1].op, UOps.VECTORIZE)
 
   def test_simple_load_dont_fold_different_gated(self):
     buf = UOp(UOps.DEFINE_GLOBAL, dtypes.float.ptr())
@@ -637,7 +636,7 @@ class TestLoadStoreFolder(unittest.TestCase):
     buf = UOp(UOps.DEFINE_GLOBAL, dtypes.float.ptr())
     load = [UOp(UOps.STORE, dtypes.float, (buf, UOp.const(dtypes.int, i), UOp.const(dtypes.float, 0))) for i in range(4)]
     sink = UOp(UOps.SINK, dtypes.void, tuple(load))
-    sink = float4_rewrite(sink)
+    sink = full_graph_rewrite(sink, Renderer())
     assert len([x for x in sink.sparents if x.op is UOps.STORE]) == 1
 
   def test_simple_store_fold_gate(self):
@@ -645,7 +644,7 @@ class TestLoadStoreFolder(unittest.TestCase):
     gate = UOp.variable("g1", False, True, dtypes.bool)
     load = [UOp(UOps.STORE, dtypes.float, (buf, UOp.const(dtypes.int, i), UOp.const(dtypes.float, 0), gate)) for i in range(4)]
     sink = UOp(UOps.SINK, dtypes.void, tuple(load))
-    sink = float4_rewrite(sink)
+    sink = full_graph_rewrite(sink, Renderer())
     assert len([x for x in sink.sparents if x.op is UOps.STORE]) == 1
     one_store = [x for x in sink.sparents if x.op is UOps.STORE][0]
     assert len(one_store.src) == 3
@@ -657,8 +656,7 @@ class TestLoadStoreFolder(unittest.TestCase):
     gate2 = UOp.variable("g2", False, True, dtypes.bool)
     load = [UOp(UOps.STORE, dtypes.float, (buf, UOp.const(dtypes.int, i), UOp.const(dtypes.float, i), gate if i == 0 else gate2)) for i in range(4)]
     sink = UOp(UOps.SINK, dtypes.void, tuple(load))
-    sink = float4_rewrite(sink)
-    print(sink)
+    sink = full_graph_rewrite(sink, Renderer())
     assert len([x for x in sink.sparents if x.op is UOps.STORE]) == 3
 
 class TestIFUOps(unittest.TestCase):
