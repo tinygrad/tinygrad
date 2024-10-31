7d70968072184259b72ea5b9e0243d7a130867b5
zibokapi <zibokapi@gmail.com>
2024-10-31 11:28:18 +0800

    move isinf and isnan to new branch

--- test/test_ops.py
+++ test/test_ops.py
@@ -339,6 +339,11 @@ class TestOps(unittest.TestCase):
     helper_test_op(None, lambda x: x.round(), vals=[[1.499, 1.5, 1.501, 1.0, 2.1, 0.0, -5.0, -2.499, -2.5, -2.501]], forward_only=True)
     helper_test_op(None, lambda x: x.round(), vals=[[2.5, -1.5]], forward_only=True)
 
+  def test_isinf(self):
+    helper_test_op(None, torch.isinf, Tensor.isinf, vals=[[float('-inf'), 0., float('inf'), float('nan'), 1.1]], forward_only=True)
+  def test_isnan(self):
+    helper_test_op(None, torch.isnan, Tensor.isnan, vals=[[float('-inf'), 0., float('inf'), float('nan'), 1.1]], forward_only=True)
+
   def test_lerp(self):
     helper_test_op([(45,35), (45,35), (45,35)], lambda x,y,z: x.lerp(y,z))
     helper_test_op(None, lambda x,y,z: x.lerp(y,z), vals=[[1.,2.,3.], [4.,5.,6.], 0.5])
