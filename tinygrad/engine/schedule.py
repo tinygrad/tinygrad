38b17905757045f7e9bacf94404363544386da16
qazal <77887910+Qazalin@users.noreply.github.com>
2024-10-31 13:51:46 +0200

    move image dtype fixup [pr] (#7444)
    * move image dtype fixup [pr]
    
    * more work
    
    * late dtype
    
    * use base

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -49,6 +49,14 @@ def to_uop(buf:LazyBuffer, ctx:ScheduleContext, cache:Dict[LazyBuffer, UOp]) -> 
   if buf is not buf.base:
     cache[buf] = ret = to_uop(buf.base, ctx, cache).view(buf.st)
     return ret
+  # make things that can't be images not images
+  if isinstance(buf.dtype, ImageDType) and (prod(buf.shape) != prod(buf.dtype.shape) or
+                                            not any(buf.shape[x]%4 == 0 for x in buf.st.unit_stride_axes())):
+    if DEBUG >= 2: print(f"forcing image {buf.dtype} with shape {buf.shape} to {buf.dtype.base}")
+    # hack the underlying buffer too
+    buf.dtype = buf.buffer.dtype = buf.dtype.base
+    assert not buf.is_realized(), "can't fixup allocated buffer"
+    buf.buffer.options = None
   dtype = buf.dtype.base if isinstance(buf.dtype, ImageDType) else buf.dtype
   # consts are always fused and generated
   if buf.op is MetaOps.CONST:
