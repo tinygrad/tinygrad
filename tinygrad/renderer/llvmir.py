d8b7222eaa6183d1e00375b83b698593750e7c20
p4sscode <p4ssenger.developer@gmail.com>
2024-10-31 12:35:35 -0300

    add shl and shr to llvmir

--- tinygrad/renderer/llvmir.py
+++ tinygrad/renderer/llvmir.py
@@ -1,7 +1,7 @@
 from typing import Dict, Callable, List, Optional
 from llvmlite import ir
 from tinygrad.dtype import DType, PtrDType, dtypes
-from tinygrad.ops import Op, UnaryOps, BinaryOps, TernaryOps, UOps, UOp
+from tinygrad.ops import Op, UnaryOps, BinaryOps, TernaryOps, UOps, UOp, PatternMatcher, UPat
 from tinygrad.renderer import Renderer
 
 MFLAGS = ('nsz', 'arcp', 'contract', 'afn', 'reassoc') # All from fast math, but nnan and ninf
@@ -63,7 +63,12 @@ class LLVMRenderer(Renderer):
     BinaryOps.MAX: lambda builder, x, y, dtype: builder.select(builder.icmp_unsigned(">", x, y) if is_bool_or_unsigned(dtype) else builder.icmp_signed(">", x, y) if dtypes.is_int(dtype) else builder.fcmp_unordered(">", x, y, flags=MFLAGS), x, y),  # noqa: E501
     BinaryOps.MOD: lambda builder, x, y, dtype: builder.urem(x, y) if is_bool_or_unsigned(dtype) else builder.srem(x, y) if dtypes.is_int(dtype) else builder.frem(x, y),  # noqa: E501
     BinaryOps.XOR: lambda builder, x, y, dtype: builder.xor(x, y), BinaryOps.AND: lambda builder, x, y, dtype: builder.and_(x, y), BinaryOps.OR: lambda builder, x, y, dtype: builder.or_(x, y), # noqa: E501
+    BinaryOps.SHL: lambda builder, x, y, dtype: builder.shl(x,y), BinaryOps.SHR: lambda builder, x, y, dtype: builder.lshr(x,y) if dtypes.is_unsigned(dtype) else builder.ashr(x,y), # noqa: E501
     TernaryOps.WHERE: lambda builder, x, y, z, dtype: builder.select(x, y, z)}
+  extra_matcher = PatternMatcher([
+    (UPat(UOps.ALU, arg=BinaryOps.SHL, src=(UPat.var("x"), UPat.var("y"))), lambda x, y: x << y.cast(x.dtype) if x.dtype != y.dtype else None),
+    (UPat(UOps.ALU, arg=BinaryOps.SHR, src=(UPat.var("x"), UPat.var("y"))), lambda x, y: x >> y.cast(x.dtype) if x.dtype != y.dtype else None),
+  ])
 
   def render(self, name:str, uops:List[UOp]) -> str:
     # all llvm stuff goes into a module
