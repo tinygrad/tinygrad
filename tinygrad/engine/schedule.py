6febd20fcfcf54a86404df0be55fd5281fc71639
qazal <77887910+Qazalin@users.noreply.github.com>
2024-11-01 20:03:12 +0200

    set forced_realize for outputs [pr] (#7469)

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -237,6 +237,7 @@ break_sched = PatternMatcher([(UPat.load(b:=UPat.var("b"), UPat(), UPat.store(b,
 @track_rewrites(named=True)
 def create_schedule_with_vars(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Variable, int]]:
   if len(outs:=dedup(x.base for x in outs if x.realized is None and x.base.op is not MetaOps.CONST)) == 0: return [], {}
+  for out in outs: out.forced_realize = True
   # create the big graph
   ctx = ScheduleContext()
   cache: Dict[LazyBuffer, UOp] = {}
