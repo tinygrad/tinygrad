ce16ff4bc4d5463010909ee55c69700392371502
zibokapi <zibokapi@gmail.com>
2024-11-01 18:55:11 +0800

    update test coverage for detect_positive and detect_negative

--- test/test_ops.py
+++ test/test_ops.py
@@ -340,7 +340,10 @@ class TestOps(unittest.TestCase):
     helper_test_op(None, lambda x: x.round(), vals=[[2.5, -1.5]], forward_only=True)
 
   def test_isinf(self):
-    helper_test_op(None, torch.isinf, Tensor.isinf, vals=[[float('-inf'), 0., float('inf'), float('nan'), 1.1]], forward_only=True)
+    val = [float('-inf'), 0., float('inf'), float('nan'), 1.1]
+    helper_test_op(None, torch.isinf, Tensor.isinf, vals=[val], forward_only=True)
+    np.testing.assert_equal(Tensor(val).isinf(detect_positive=True, detect_negative=False).numpy(), [False, False, True, False, False])
+    np.testing.assert_equal(Tensor(val).isinf(detect_positive=False, detect_negative=True).numpy(), [True, False, False, False, False])
   def test_isnan(self):
     helper_test_op(None, torch.isnan, Tensor.isnan, vals=[[float('-inf'), 0., float('inf'), float('nan'), 1.1]], forward_only=True)
 
