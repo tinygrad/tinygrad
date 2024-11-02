b1866cbfd9d99a464284619423d595d78b21b9e8
geohotstan <135171913+geohotstan@users.noreply.github.com>
2024-11-03 00:13:38 +0800

    failure test case for pool ops (#7483)
    * add failure test case
    
    * minimum case

--- test/test_ops.py
+++ test/test_ops.py
@@ -1831,6 +1831,13 @@ class TestOps(unittest.TestCase):
           lambda x: torch.nn.functional.avg_pool2d(x, kernel_size=ksz),
           lambda x: Tensor.avg_pool2d(x, kernel_size=ksz), rtol=1e-5)
 
+  # TODO fix edge case
+  @unittest.expectedFailure
+  def test_avgpool2d_failure(self):
+    helper_test_op([(1,1,8,8)],
+      lambda x: torch.nn.functional.avg_pool2d(x, kernel_size=(1,2), padding=(0,1), stride=(5,1)),
+      lambda x: Tensor.avg_pool2d(x, kernel_size=(1,2), padding=(0,1), stride=(5,1)), rtol=1e-5)
+
   def test_avgpool2d_padding(self):
     shape = (32,2,111,28)
     for ksz in [(2,2), (3,3), 2, 3, (3,2)]:
