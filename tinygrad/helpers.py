fed87dfcb125ceb96a31ab404bc7788e1724d233
Gabriel Gordbegli <ggordbegli@gmail.com>
2024-11-05 17:11:55 -0500

    update GlobalCounters types

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -122,8 +122,8 @@ _METADATA: contextvars.ContextVar[Optional[Metadata]] = contextvars.ContextVar("
 # **************** global state Counters ****************
 
 class GlobalCounters:
-  global_ops: ClassVar[int] = 0
-  global_mem: ClassVar[int] = 0
+  global_ops: ClassVar[Union[int, float]] = 0
+  global_mem: ClassVar[Union[int, float]] = 0
   time_sum_s: ClassVar[float] = 0.0
   kernel_count: ClassVar[int] = 0
   mem_used: ClassVar[int] = 0   # NOTE: this is not reset
