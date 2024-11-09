25ae02f37745cd14863ded433a870b2389e247f1
ttomsa <tomasvsilva8@gmail.com>
2024-11-09 00:03:10 +0000

    no point in range test

--- test/test_int64_idx.py
+++ test/test_int64_idx.py
@@ -7,11 +7,5 @@ class TestInt64Indexing(unittest.TestCase):
     t = Tensor.ones((1 << 31) + 4, dtype=dtypes.int8).contiguous()
     assert t[-1].item() == 1
 
-  @unittest.skipUnless(Device.DEFAULT in ("CLANG"), "CLANG for int64 max range")
-  def test_int64_indexing_noopt(self):
-    with Context(NOOPT=1):
-      t = Tensor.ones((1 << 31) + 4, dtype=dtypes.int8).contiguous()
-      assert t[-1].item() == 1
-
 if __name__ == '__main__':
   unittest.main()
