c326e62f139f455e2b1d061825ff4cfd046b9b3d
ttomsa <tomasvsilva8@gmail.com>
2024-11-12 23:23:17 +0000

    better

--- tinygrad/renderer/x86asm.py
+++ tinygrad/renderer/x86asm.py
@@ -7,10 +7,6 @@ import struct
 from collections import Counter
 from tinygrad.renderer.cstyle import CStyleLanguage
 
-# 64 bit general registers, rsp/rbp not included, r15 temp register for now
-gen_regs = ["rdi", "rsi", "rdx", "rcx", "r8", "r9", "rax", "rbx"] + ['r'+str(i) for i in range(10,15)]
-float_regs = ["xmm" + str(i) for i in range(0,16)]
-all_regs = gen_regs + float_regs
 size_prefix = {1: "byte", 2: "word", 4: "dword", 8: "qword"}
 mov_sufix = {4: "d", 8: "q"}
 asm_ops = {Ops.STORE: "mov", Ops.LOAD: "mov", Ops.DEFINE_ACC: "mov", Ops.ASSIGN: "mov", Ops.ADD: "add", Ops.SUB: "sub", Ops.MUL: "imul", Ops.IDIV: "idiv",
@@ -28,28 +24,6 @@ def cflag(x:UOp) -> str:
   if x.op is Ops.CMPLT: return "setl" if x.src[0].dtype in dtypes.sints else "setb"
   if x.op is Ops.CMPNE: return "setne"
   assert False
-
-def x86_cast(ctx:Dict[UOp, str], x:UOp, s:UOp) -> str:
-  # NOTE: cast from uint64 to floats is complicated, might not want to allow that
-  # TODO: cast from uint32 to float requires use of 64bit reg (already zero extended)
-  if isinstance(x.dtype.scalar(), PtrDType):
-    assert isinstance(s.dtype.scalar(), PtrDType)
-    return f"mov {ctx[x]}, {ctx[s]}"
-
-  if (dtypes.is_int(s.dtype) or s.dtype is dtypes.bool) and dtypes.is_int(x.dtype):
-    if s.dtype.itemsize < x.dtype.itemsize:
-      if dtypes.is_unsigned(s.dtype): return f"mov{'zx' if s.dtype.itemsize < 4 else ''} {ctx[x]}, {ctx[s]}"
-      else: return f"movsx{'d' if s.dtype.itemsize == 4 else ''} {ctx[x]}, {ctx[s]}"
-    else: return f"mov {ctx[x]}, {ctx[s]}"
-  
-  # here float is present
-  cfrom = "si" if not dtypes.is_float(s.dtype) else "tsd" if s.dtype.itemsize == 8 else "tss"
-  cto = "si" if not dtypes.is_float(x.dtype) else "sd" if x.dtype.itemsize == 8 else "ss"
-  if dtypes.is_int(s.dtype) and s.dtype.itemsize < 4:
-    # need to zero/sign extend to 32bit temp reg before cast
-    return f"mov{'zx' if dtypes.is_unsigned(s.dtype) else 'sx'} r15d, {ctx[s]}\ncvt{cfrom}2{cto} {ctx[x]}, r15d"
-
-  return f"cvt{cfrom}2{cto} {ctx[x]}, {ctx[s]}"
 
 def opc(u:UOp) -> str:
   op = asm_ops[u.op]
@@ -82,9 +56,8 @@ x86_rewrite = PatternMatcher([
   (UPat(Ops.RANGE, name="x"), lambda ctx,x: f"mov {ctx[x]}, {ctx[x.src[0]]}\n.LOOP_{x.arg[0]}:"),
   (UPat(Ops.ENDRANGE, name="x"), lambda ctx,x: f"inc {ctx[x.src[0]]}\ncmp {ctx[x.src[0]]}, {ctx[x.src[0].src[1]]}\njl .LOOP_{x.src[0].arg[0]}"),
   
-  # TODO: instead of in all_regs do is_reg or in regs (pm needs access to regs)
-  (UPat(Ops.BITCAST, name="x"), lambda ctx,x: f"mov{mov_sufix[x.dtype.itemsize] if ctx[x.src[0]] in all_regs else ""} {ctx[x]}, {ctx[x.src[0]]}"),
-  (UPat(Ops.CAST, name="x"), lambda ctx,x: x86_cast(ctx, x, x.src[0])),
+  (UPat(Ops.BITCAST, name="x"), lambda ctx,x: f"mov{mov_sufix[x.dtype.itemsize] if ctx.r[x.src[0]] in ctx.all_regs else ""} {ctx[x]}, {ctx[x.src[0]]}"),
+  (UPat(Ops.CAST, name="x"), lambda ctx,x: ctx.x86_cast(x, x.src[0])),
   # no cmov for floats
   (UPat(Ops.WHERE, dtype=dtypes.floats, name="x"), lambda ctx,x: f"test {ctx[x.src[0]]}, 1\njnz .l{ctx.uop_i[x]}\n{optype("mov", x.dtype)} {ctx[x]}, {ctx[x.src[2]]}\n.l{ctx.uop_i[x]}:"),
   (UPat(Ops.WHERE, dtype=dtypes.ints, name="x"), lambda ctx,x: f"test {ctx[x.src[0]]}, 1\n{opc(x)} {ctx[x]}, {ctx[x.src[2]]}"),
@@ -125,11 +98,35 @@ class X86Renderer(Renderer):
     assert self.r[s] != "rdx", "divisor can't be rdx i think\n"
     l += f"mov {self.regt("rax", x.dtype)}, {self[x]}\n"
     l += "xor rdx, rdx\n" if dtypes.is_unsigned(x.dtype) else "cqo\n" if x.dtype.itemsize == 8 else "cdq\n"
-    l += f"idiv {self[s]}\n"
+    l += f"{opc(x)} {self[s]}\n"
     l += f"mov {self[x]}, {self.regt("rax", x.dtype)}\n"
     l += "pop rdx\n"
     if self.r[x] != "rax": l += "pop rax"
     return l
+  
+  def x86_cast(self, x:UOp, s:UOp) -> str:
+    # NOTE: cast from uint64 to floats is complicated, might not want to allow that
+    # TODO: cast from uint32 to float requires use of 64bit reg (already zero extended)
+    if isinstance(x.dtype.scalar(), PtrDType):
+      assert isinstance(s.dtype.scalar(), PtrDType)
+      return f"mov {self[x]}, {self[s]}"
+
+    if (dtypes.is_int(s.dtype) or s.dtype is dtypes.bool) and dtypes.is_int(x.dtype):
+      if s.dtype.itemsize < x.dtype.itemsize:
+        if s.dtype in dtypes.sints: return f"movs{'x' if s.dtype.itemsize < 4 else 'xd'} {self[x]}, {self[s]}"
+        elif s.dtype.itemsize < 4: return f"movzx {self[x]}, {self[s]}"
+      # cast to smaller int or uint32 to uint64 is just a mov
+      return f"mov {self[x]}, {self.regt(self.r[s], x.dtype)}"
+
+    # here float is present
+    cfrom = "si" if not dtypes.is_float(s.dtype) else "tsd" if s.dtype.itemsize == 8 else "tss"
+    cto = "si" if not dtypes.is_float(x.dtype) else "sd" if x.dtype.itemsize == 8 else "ss"
+    if (dtypes.is_int(s.dtype) or s.dtype is dtypes.bool) and s.dtype.itemsize < 4:
+      # need to zero/sign extend to 32bit temp reg before cast
+      return f"mov{'zx' if dtypes.is_unsigned(s.dtype) else 'sx'} r15d, {self[s]}\ncvt{cfrom}2{cto} {self[x]}, r15d"
+
+    return f"cvt{cfrom}2{cto} {self[x]}, {self[s]}"
+
 
   # 64 bit int reg to lower bit reg
   def regt(self, reg:str, dt:DType) -> str:
@@ -139,8 +136,12 @@ class X86Renderer(Renderer):
     if dt.itemsize == 2: return reg+'w' if reg[-1].isdigit() else reg[1:]
     if dt.itemsize == 1: return reg+'b' if reg[-1].isdigit() else reg[1:]+'l' if reg[-1] == 'i' else reg[1:-1]+'l'
 
-  def __getitem__(self, key:UOp): return self.regt(self.r[key], key.dtype) if self.r[key] in all_regs else self.r[key]  # hacky helper
+  def __getitem__(self, key:UOp): return self.regt(self.r[key], key.dtype) if self.r[key] in self.all_regs else self.r[key]  # hacky helper
   def render(self, name:str, ops:List[UOp]) -> str:
+    # 64 bit general registers, rsp/rbp not included, r15 temp register for now
+    gen_regs = ["rdi", "rsi", "rdx", "rcx", "r8", "r9", "rax", "rbx"] + ['r'+str(i) for i in range(10,15)]
+    float_regs = ["xmm" + str(i) for i in range(0,16)]
+    self.all_regs = gen_regs + float_regs
     # can be a register, memory location or immediate value
     r: Dict[UOp, str] = {}
     self.r = r
@@ -154,12 +155,20 @@ class X86Renderer(Renderer):
     uop_i = {u:i for i,u in enumerate(ops)}
     self.uop_i = uop_i
 
-    # TODO: should organize ops by which need self to be reg, first/second source to be reg
-    # these ops require all operands to be regs
-    srcs_all_regs = (Ops.WHERE, Ops.IDIV, Ops.INDEX)
-    first_src_reg = (Ops.INDEX,)
-    # self_is_reg =
-    sec_src_no_mem = (Ops.INDEX,)
+    def is_imm(u:UOp) -> bool:
+      if u.op is Ops.CONST and not dtypes.is_float(u.dtype) and abs(u.arg) <= dtypes.max(dtypes.int32): return True
+      return False
+
+    def mov_to_reg(u:UOp) -> str:
+      regs[u] = assign_reg(i, u.dtype)
+      kernel.append(f"{optype("mov", u.dtype)} {regs[u]}, {mem[u] if u in mem else to_hex(u.arg)}")
+
+    def mov_to_stack(u:UOp, reg:str) -> str:
+      nonlocal stack_size
+      if u not in mem:
+        mem[u] = f"[rbp - {stack_size}]"
+        stack_size += 8
+      kernel.append(f"{optype("mov", u.dtype if u.op != Ops.CONST else dtypes.int32)} {mem[u]}, {reg}")
     
     def free_reg(reg:str): float_regs.append(reg) if reg.startswith("xmm") else gen_regs.append(reg)
 
@@ -172,10 +181,7 @@ class X86Renderer(Renderer):
       nonlocal stack_size
       # we choose who to spill by looking for the reg whose next instruction is the latest
       chosen = max(candidates, key=lambda u: min(v for v in live_range[u] if v >= i))
-      if chosen not in mem:
-        mem[chosen] = f"[rbp - {stack_size}]"
-        stack_size += 8
-      kernel.append(f"{optype("mov", dt)} {mem[chosen]}, {regs[chosen]}")
+      mov_to_stack(chosen, regs[chosen])
       return regs.pop(chosen)
 
     # do a pass over ops to assign ranges, ranges allow us to get rid of dead regs and pick the best reg to spill
@@ -188,38 +194,30 @@ class X86Renderer(Renderer):
 
     for i,u in enumerate(ops):
       if u.op is Ops.CONST:
-        # consts to stack if they can't be immediate values
-        # pattern match 64bit int consts to 32bit?
-        if dtypes.is_float(u.dtype) or abs(u.arg) > dtypes.max(dtypes.int32):
-          mem[u] = f"[rbp - {stack_size}]"
-          stack_size += 8
+        if not is_imm(u):
           kernel.append(f"mov r15, {to_hex(u.arg)}")
-          kernel.append(f"mov {mem[u]}, r15")
-          r[u] = mem[u]
-        else:
-          r[u] = to_hex(u.arg)
-
-      if u.op is Ops.DEFINE_GLOBAL:
-        #define globals to stack, this frees them for spilling
-        reg = assign_reg(i, u.dtype)
-        mem[u] = f"[rbp - {stack_size}]"
-        stack_size += 8
-        free_reg(reg)
-        kernel.append(f"mov {mem[u]}, {reg}")
-        r[u] = mem[u]
-      
-      if u.op in (Ops.CONST, Ops.DEFINE_GLOBAL): continue
-
+          mov_to_stack(u, "r15")
+        continue
       regs[u] = regs[u.src[0]] if u.op in (Ops.ASSIGN,) else assign_reg(i, u.dtype)
 
-      for s in u.src: 
-        if u.op in srcs_all_regs and s not in regs:
-          if s.op is not Ops.CONST: assert s in mem
-          regs[s] = assign_reg(i, s.dtype)
-          kernel.append(f"{optype("mov", s.dtype)} {regs[s]}, {r[s]}")
+      if u.op is Ops.DEFINE_GLOBAL:
+        mov_to_stack(u, regs[u])
+        free_reg(regs.pop(u))
+      if u.op in (Ops.STORE, Ops.LOAD, Ops.INDEX):
+        if u.src[0] not in regs: mov_to_reg(u.src[0])
+      if u.op is Ops.INDEX:
+        # src[1] can't be mem
+        if not is_imm(u.src[1]) and u.src[1] not in regs: mov_to_reg(u.src[1])
+      if u.op in (Ops.WHERE, Ops.IDIV):
+        # TODO: src[0] in WHERE doesn't need to be reg but requires size prefix
+        for s in u.src:
+          if s not in regs: mov_to_reg(s)
+      if u.op in (Ops.CMPLT, Ops.CMPNE):
+        if is_imm(u.src[0]): mov_to_reg(u.src[0])
+        # only 1 operand can be memory TODO: remove from mem when moved to reg, makes this cleaner by just checking if in mem
+        elif u.src[0] not in regs and u.src[1] not in regs and not is_imm(u.src[1]): mov_to_reg(u.src[0])
 
-      for s in u.src: r[s] = regs[s] if s in regs else mem[s] if s in mem else to_hex(s.arg)
-      r[u] = regs[u]
+      for s in (u,) + u.src: r[s] = regs[s] if s in regs else mem[s] if s in mem else to_hex(s.arg)
 
       if u.op in GroupOp.ALU and u.op not in (Ops.CMPLT, Ops.CMPNE):
         # for cmp nothing to mov as reg depends on flag
@@ -227,6 +225,12 @@ class X86Renderer(Renderer):
       
       l = x86_rewrite.rewrite(u, ctx=self)
       if l: kernel.append(l)
+
+      if u.op is Ops.INDEX:
+        # when this happens INDEX is before range, everything before range that's not define_acc should be moved to the stack
+        if is_imm(u.src[1]):
+          mov_to_stack(u, regs[u])
+          free_reg(regs.pop(u))
           
       if u.op is Ops.GEP: assert u.dtype == dtypes.float32 and u.dtype.count == 1 and len(u.arg) == 1
       if u.op is Ops.VECTORIZE: assert u.dtype.scalar() == dtypes.float32
