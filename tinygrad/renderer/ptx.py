c07d4b2d0694e62963972e0e6916909cd9b8edd3
judy <mesozoic.egg@proton.mail>
2024-11-16 05:22:40 +0800

    space

--- tinygrad/renderer/ptx.py
+++ tinygrad/renderer/ptx.py
@@ -72,7 +72,7 @@ def render_wmma(ctx: "PTXRenderer", x: UOp):
       yield f"mov.b32 {ctx.wmma_r[_i]}, {{{', '.join(ctx.r[vv][i:i+2])}}};"
       _i += 1
   yield f'mma.sync.aligned.m{M}n{N}k{K}.row.col.f32.{dt_map[dtype_in]}.{dt_map[dtype_in]}.f32{" "*12}' +\
-  f'{{{", ".join(ctx.r[x])}}}, {{{", ".join(ctx.wmma_r[:n_operands[0]])}}}, {{{", ".join(ctx.wmma_r[-n_operands[1]:])}}},' + \
+  f'{{{", ".join(ctx.r[x])}}}, {{{", ".join(ctx.wmma_r[:n_operands[0]])}}}, {{{", ".join(ctx.wmma_r[-n_operands[1]:])}}}, ' + \
   f'{{{", ".join(ctx.r[x.src[2]])}}};'
 
 def modifier(a: DType, b: DType): return '.rzi' if dtypes.is_int(a) and dtypes.is_float(b) else '.rn' if dtypes.is_float(a) and \
