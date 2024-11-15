9b1605eef9ef21c6899792837263a417a629cabf
George Hotz <72895+geohot@users.noreply.github.com>
2024-11-15 12:13:04 +0800

    Revert "objdump intel syntax (#7605)" (#7707)
    This reverts commit 8f8e375f276c2ad921d57275cbf2e9a1665aa432.

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 import ctypes, subprocess, pathlib, tempfile
 from tinygrad.device import Compiled, Compiler, MallocAllocator
-from tinygrad.helpers import cpu_time_execution, cpu_objdump
+from tinygrad.helpers import cpu_time_execution, DEBUG, cpu_objdump
 from tinygrad.renderer.cstyle import ClangRenderer
 
 class ClangCompiler(Compiler):
@@ -16,10 +16,9 @@ class ClangCompiler(Compiler):
                                '-', '-o', str(output_file.name)], input=src.encode('utf-8'))
       return pathlib.Path(output_file.name).read_bytes()
 
-  def disassemble(self, lib:bytes): cpu_objdump(lib)
-
 class ClangProgram:
   def __init__(self, name:str, lib:bytes):
+    if DEBUG >= 6: cpu_objdump(lib)
     self.name, self.lib = name, lib
     # write to disk so we can load it
     with tempfile.NamedTemporaryFile(delete=True) as cached_file_path:
