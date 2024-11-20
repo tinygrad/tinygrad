03204f4c42667b00ce4363fb8848153af8242e0d
ttomsa <tomasvsilva8@gmail.com>
2024-11-20 22:48:37 +0000

    print_debug function

--- test/test_prints.py
+++ test/test_prints.py
@@ -7,31 +7,26 @@ def clear_cache():
   method_cache.clear()
   Kernel.kernel_cnt.clear()
 
+def print_debug(level:int) -> str:
+  clear_cache()
+  db_out = io.StringIO()
+  with contextlib.redirect_stdout(db_out):
+    with Context(DEBUG=level):
+      Tensor.ones(3).realize()
+  return db_out.getvalue()
+
 @unittest.skipIf(Device.DEFAULT != "CUDA", "test prints for cuda")
 class TestCUDAPrints(unittest.TestCase):
   def test_cuda_print_order_debug_6(self):
-    clear_cache()
-    db5_out = io.StringIO()
-    with contextlib.redirect_stdout(db5_out):
-      with Context(DEBUG=5):
-        Tensor.ones(3).realize()
-
-    clear_cache()
-    db6_out = io.StringIO()
-    with contextlib.redirect_stdout(db6_out):
-      with Context(DEBUG=6):
-        Tensor.ones(3).realize()
+    db5_str = print_debug(5)
+    db6_str = print_debug(6)
     # get rif of trailing "\n" and final print with kernel time/flops/mem usage
-    db5_str = db5_out.getvalue()[:-1].rpartition("\n")[0]
-    assert db6_out.getvalue().startswith(db5_str), "DEBUG=6 output should start with DEBUG=5 output"
+    db5_str = db5_str[:-1].rpartition("\n")[0]
+    assert db6_str.startswith(db5_str), "DEBUG=6 output should start with DEBUG=5 output"
 
   def test_cuda_disassemble(self):
-    clear_cache()
-    db6_out = io.StringIO()
-    with contextlib.redirect_stdout(db6_out):
-      with Context(DEBUG=6):
-        Tensor.ones(3).realize()
-    assert "returned non-zero exit status 1" not in db6_out.getvalue(), "failed disassembly from ptx"
+    db6_str = print_debug(6)
+    assert "returned non-zero exit status 1" not in db6_str, "failed disassembly from ptx"
 
 if __name__ == "__main__":
   unittest.main()
