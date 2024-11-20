4095451489e8ce4fa25ade2a993856843a0549f6
p4sscode <p4ssenger.developer@gmail.com>
2024-11-20 11:29:37 -0300

    hotfix: add check for tc.dtype_out is bf16 back

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -1036,16 +1036,17 @@ class TestLinearizer(unittest.TestCase):
   @unittest.skipUnless(Device[Device.DEFAULT].renderer.tensor_cores, "test requires tensor cores")
   def test_tensor_cores(self):
     for tc in Device[Device.DEFAULT].renderer.tensor_cores:
-      if (getenv("EMULATE_CUDA") or getenv("EMULATE_INTEL") or getenv("EMULATE_METAL")) and tc.dtype_in == dtypes.bfloat16: continue
-      if CI and Device.DEFAULT == "METAL" and tc.dtype_in == dtypes.bfloat16: continue
+      if (getenv("EMULATE_CUDA") or getenv("EMULATE_INTEL") or getenv("EMULATE_METAL")) and \
+        tc.dtype_in == dtypes.bfloat16 or tc.dtype_out == dtypes.bfloat16: continue
+      if CI and Device.DEFAULT == "METAL" and tc.dtype_in == dtypes.bfloat16 or tc.dtype_out == dtypes.bfloat16: continue
       # for AMX, tc.dims[2] == 1 so reduceop is None thus tensor_cores are not triggered
       helper_tc_allclose(tc.dims[0], tc.dims[1], 2 if AMX else tc.dims[2], tc.dtype_in, tc.dtype_out, axis=0, tc_opt=0)
 
   @unittest.skipUnless(Device[Device.DEFAULT].renderer.tensor_cores, "test requires tensor cores")
   def test_tensor_cores_padded(self):
     for tc in Device[Device.DEFAULT].renderer.tensor_cores:
-      if (getenv("EMULATE_CUDA") or getenv("EMULATE_METAL")) and tc.dtype_in == dtypes.bfloat16: continue
-      if CI and Device.DEFAULT == "METAL" and tc.dtype_in == dtypes.bfloat16: continue
+      if (getenv("EMULATE_CUDA") or getenv("EMULATE_METAL")) and tc.dtype_in == dtypes.bfloat16 or tc.dtype_out == dtypes.bfloat16: continue
+      if CI and Device.DEFAULT == "METAL" and tc.dtype_in == dtypes.bfloat16 or tc.dtype_out == dtypes.bfloat16: continue
       pad = 1
 
       # check that TC is triggered for TC_OPT=2
