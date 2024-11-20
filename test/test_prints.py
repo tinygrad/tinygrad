5b473c2d1ab3c97a204c6b0df6ca3c7749ec17bb
ttomsa <tomasvsilva8@gmail.com>
2024-11-20 20:10:20 +0000

    add ptx disassembly test

--- test/test_prints.py
+++ test/test_prints.py
@@ -3,24 +3,35 @@ from tinygrad import Tensor, Context, Device
 from tinygrad.engine.realize import method_cache
 from tinygrad.codegen.kernel import Kernel
 
+def clear_cache():
+  method_cache.clear()
+  Kernel.kernel_cnt.clear()
+
 @unittest.skipIf(Device.DEFAULT != "CUDA", "test prints for cuda")
-class TestPrints(unittest.TestCase):
-  def test_cuda_print_debug_6(self):
+class TestCUDAPrints(unittest.TestCase):
+  clear_cache()
+  def test_cuda_print_order_debug_6(self):
     db5_out = io.StringIO()
     with contextlib.redirect_stdout(db5_out):
       with Context(DEBUG=5):
-        Tensor.arange(0,6).realize()
-    method_cache.clear()
-    Kernel.kernel_cnt.clear()
+        Tensor.ones(3).realize()
 
+    clear_cache()
     db6_out = io.StringIO()
     with contextlib.redirect_stdout(db6_out):
       with Context(DEBUG=6):
-        Tensor.arange(0,6).realize()
-
+        Tensor.ones(3).realize()
     # get rif of trailing "\n" and final print with kernel time/flops/mem usage
-    db_str = db5_out.getvalue()[:-1].rpartition("\n")[0]
-    assert db6_out.getvalue().startswith(db_str), "DEBUG=6 output should start with DEBUG=5 output"
+    db5_str = db5_out.getvalue()[:-1].rpartition("\n")[0]
+    assert db6_out.getvalue().startswith(db5_str), "DEBUG=6 output should start with DEBUG=5 output"
+
+  def test_cuda_disassemble(self):
+    clear_cache()
+    db6_out = io.StringIO()
+    with contextlib.redirect_stdout(db6_out):
+      with Context(DEBUG=6):
+        Tensor.ones(3).realize()
+    assert "Failed" not in db6_out.getvalue(), "failed disassembly from ptx"
 
 if __name__ == "__main__":
   unittest.main()
