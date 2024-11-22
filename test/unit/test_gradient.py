ad88cc26c68c0b9dfcf07a8add03e063d0e48053
George Hotz <geohot@gmail.com>
2024-11-22 14:47:32 +0800

    more correct

--- test/unit/test_gradient.py
+++ test/unit/test_gradient.py
@@ -1,12 +1,43 @@
-import unittest
+from typing import Callable
+import unittest, math
 import jax
+import jax.numpy as jnp
+from tinygrad.dtype import dtypes
 from tinygrad.ops import UOp
 from tinygrad.gradient import gradient
 
 class TestGradient(unittest.TestCase):
-  def test_add(self):
-    def f(x,y): return x+y
-    UOp.variable()
+  def _test_one_input_function(self, f:Callable, jf:Callable|None):
+    x = UOp.variable('x', -math.inf, math.inf, dtype=dtypes.float)
+    gx = gradient(f(x), [x])[0]
+    gf = jax.grad(f if jf is None else jf)
 
+    for val in [-5., -2.0, 0.0, 2.0, 5.]:
+      tg_out, jax_out = gx.substitute({x: x.const_like(val)}).ssimplify(), gf(val).item()
+      if math.isnan(tg_out) and math.isnan(jax_out): continue
+      self.assertAlmostEqual(tg_out, jax_out, places=5)
 
+  def _test_two_input_function(self, f:Callable):
+    x, y = UOp.variable('x', -math.inf, math.inf), UOp.variable('y', -math.inf, math.inf)
 
+    # compute in tinygrad
+    gx,gy = gradient(f(x,y), [x,y])
+
+    # compute in JAX
+    gf = jax.grad(f)
+    jgx,jgy = gf(f(2.0, 2.0))
+
+    print(jgx)
+
+    self.assertEqual(gx.ssimplify(), 1)
+    self.assertEqual(gy.ssimplify(), 1)
+
+  def test_sin(self): self._test_one_input_function(lambda x: x.sin(), lambda x: jnp.sin(x))
+  def test_sqrt(self): self._test_one_input_function(lambda x: x.sqrt(), lambda x: jnp.sqrt(x))
+  def test_log2(self): self._test_one_input_function(lambda x: x.log2(), lambda x: jnp.log2(x))
+  def test_exp2(self): self._test_one_input_function(lambda x: x.exp2(), lambda x: jnp.exp2(x))
+
+  def test_chain(self): self._test_one_input_function(lambda x: x.sin().sqrt(), lambda x: jnp.sqrt(jnp.sin(x)))
+
+if __name__ == '__main__':
+  unittest.main()
--- tinygrad/gradient.py
+++ tinygrad/gradient.py
@@ -3,12 +3,12 @@ import math
 
 # ctx is grad_output
 pm_gradient = PatternMatcher([
-  (UPat(Ops.CAST, name="ret"), lambda ctx, ret: ctx.cast(ret.src[0].dtype)),
+  (UPat(Ops.CAST, name="ret"), lambda ctx, ret: (ctx.cast(ret.src[0].dtype),)),
   (UPat(Ops.RECIP, name="ret"), lambda ctx, ret: (-ctx * ret * ret,)),
-  (UPat(Ops.SIN, name="ret"), lambda ctx, ret: (math.pi/2 - ret.src[0]).sin() * ctx),
-  (UPat(Ops.LOG2, name="ret"), lambda ctx, ret: ctx / (ret.src[0] * math.log(2))),
-  (UPat(Ops.EXP2, name="ret"), lambda ctx, ret: ret.src[0] * ctx * math.log(2)),
-  (UPat(Ops.SQRT, name="ret"), lambda ctx, ret: ctx / (ret*2)),
+  (UPat(Ops.SIN, name="ret"), lambda ctx, ret: ((math.pi/2 - ret.src[0]).sin() * ctx,)),
+  (UPat(Ops.LOG2, name="ret"), lambda ctx, ret: (ctx / (ret.src[0] * math.log(2)),)),
+  (UPat(Ops.EXP2, name="ret"), lambda ctx, ret: (ret * ctx * math.log(2),)),
+  (UPat(Ops.SQRT, name="ret"), lambda ctx, ret: (ctx / (ret*2),)),
   (UPat((Ops.CMPLT, Ops.CMPNE)), lambda: (None, None)),
   (UPat(Ops.ADD), lambda ctx: (ctx, ctx)),
   (UPat(Ops.MUL, name="ret"), lambda ctx, ret: (ret.src[1]*ctx, ret.src[0]*ctx)),
@@ -16,5 +16,26 @@ pm_gradient = PatternMatcher([
   # TODO: reduce/movement ops once we have the new lazy stuff
 ])
 
-def gradient(root:UOp, t:list[UOp]) -> list[UOp]:
-  root.parents
+# copied from tensor.py, get relevant topospot of gradients
+def _deepwalk(root:UOp, targets:list[UOp]):
+  def _walk(node:UOp, visited:set[UOp]):
+    visited.add(node)
+    if any(x in node.parents for x in targets):
+      for i in node.src:
+        if i not in visited: yield from _walk(i, visited)
+      yield node
+  return list(_walk(root, set()))
+
+def gradient(root:UOp, targets:list[UOp]) -> list[UOp]:
+  # TODO: better error
+  if not all(x in root.sparents for x in targets): raise RuntimeError("some gradient targets not found in parents")
+  grads = {root: root.const_like(1.0)}
+  for t0 in reversed(_deepwalk(root, targets)):
+    lgrads = pm_gradient.rewrite(t0, ctx=grads[t0])
+    if lgrads is None: raise RuntimeError(f"failed to compute gradient for {t0.op}")
+    assert len(lgrads) == len(t0.src)
+    for k,v in zip(t0.src, lgrads):
+      if k in grads: grads[k] += v
+      else: grads[k] = v
+  return [grads[x] for x in targets]
+
