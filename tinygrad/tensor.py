6b6a519bbe85490acd88efdeb483795541783d78
Jakob Spahn <jakob@craalse.de>
2024-11-25 13:35:14 +0100

    remove parantheses

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -273,7 +273,7 @@ class Tensor(SimpleMathTrait):
     """
     assert self.dtype.fmt is not None, f"no fmt dtype for {self.dtype}"
     assert all_int(self.shape), f"no data if shape is symbolic, {self.shape=}"
-    if TYPE_CHECKING or sys.version_info < (3, 12): assert (self.dtype.fmt != "e")
+    if TYPE_CHECKING or sys.version_info < (3, 12): assert self.dtype.fmt != "e"
     return self._data().cast(self.dtype.fmt, self.shape)
 
   def item(self) -> ConstType:
