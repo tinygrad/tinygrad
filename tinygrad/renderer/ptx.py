25ae7ff5c72e79dcd727278e3f27be378cb61422
Mesozoic Egg <mesozoic.egg@proton.mail>
2024-11-25 17:49:47 +0800

    cast, line too long fix

--- tinygrad/renderer/ptx.py
+++ tinygrad/renderer/ptx.py
@@ -189,11 +189,11 @@ class PTXRenderer(Renderer):
       elif u.op is Ops.WMMA:
         self.wmma_r = [ssa("wmma", dtype="b32") for vv in u.src[:2] for i in range(0, len(r[vv]), 2)]
         r[u] = [ssa("wmma", dtype=self.types[u.dtype.scalar()]) for _ in range(u.dtype.count)]
-      prefix, dtype = {Ops.CAST: ("cast", None), Ops.BITCAST: ("cast", None), Ops.ENDRANGE: ("pred", "pred"), Ops.RANGE: ("ridx", None), Ops.DEFINE_ACC: ("acc", None),
-        Ops.DEFINE_VAR: ("dat", None), Ops.CONST: ("const", None), Ops.DEFINE_LOCAL: ("local", "u64"),
+      prefix, dtype = {Ops.CAST: ("cast", None), Ops.BITCAST: ("cast", None), Ops.ENDRANGE: ("pred", "pred"), Ops.RANGE: ("ridx", None),
+        Ops.DEFINE_ACC: ("acc", None), Ops.DEFINE_VAR: ("dat", None), Ops.CONST: ("const", None), Ops.DEFINE_LOCAL: ("local", "u64"),
         Ops.DEFINE_GLOBAL: ("dat", self.types.get(dtypes.ulong if u.dtype.__class__ == PtrDType else u.dtype)),
         **{op: ("alu", None) for op in GroupOp.ALU}}.get(u.op, (None, None))
-      if prefix: r[u] = ssa(cast(str, prefix), u, dtype)
+      if prefix: r[u] = ssa(prefix, u, dtype)
 
       if (l:=cast(Union[str, List[str]], string_rewrite.rewrite(u, ctx=self))) is None:
         raise RuntimeError(f"failed to render {u.op} with {u.dtype} srcs {[x.dtype for x in u.src]}")
