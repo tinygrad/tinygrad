1b41040b80b694bf35b7b1fff574ddd5679da4e9
Jamesb <experimentallearning0@gmail.com>
2024-11-26 08:21:38 +0000

    start adding jit

--- extra/utilities/blake3.py
+++ extra/utilities/blake3.py
@@ -1,30 +1,63 @@
 import math
-from typing import Optional, Tuple
+from typing import Callable, Optional, Tuple
 from tinygrad.dtype import dtypes
+from tinygrad.engine import jit
 from tinygrad.tensor import Tensor
 
+# try adding masks
+# [x] fix chunks.replace
+
 def mix(states: Tensor, chunks: Tensor) -> Tensor:
+  print(states.shape, chunks.shape)
   def rotr(x: Tensor, n: int) -> Tensor: return ((x << (32 - n)) | (x >> n))
   for i, (a,b,c,d) in enumerate([(0,4,8,12), (1,5,9,13), (2,6,10,14), (3,7,11,15), (0,5,10,15), (1,6,11,12), (2,7,8,13), (3,4,9,14)]):
     mx, my = chunks[:, i * 2], chunks[:, i * 2 + 1]
-    for m in (mx, my): # NOTE: 99% of the execution time is spent here
-      states[:, a] = states[:, a] + states[:, b] + m
-      states[:, d] = rotr(states[:, d] ^ states[:, a], 16 if m is mx else 8)
-      states[:, c] = states[:, c] + states[:, d]
-      states[:, b] = rotr(states[:, b] ^ states[:, c], 12 if m is mx else 7)
+    # First iteration with mx
+    new_a = states[:, a]
+    new_b = states[:, b]
+    new_c = states[:, c]
+    new_d = states[:, d]
+
+    new_a = (new_a + new_b + mx).realize()
+    new_d = rotr(new_d ^ new_a, 16)
+    new_c = (new_c + new_d)
+    new_b = rotr(new_b ^ new_c, 12)
+
+    # Second iteration with my
+    new_a = (new_a + new_b + my)
+    new_d = rotr(new_d ^ new_a, 8)
+    new_c = (new_c + new_d)
+    new_b = rotr(new_b ^ new_c, 7)
+
+    states[:, a] = new_a
+    states[:, b] = new_b
+    states[:, c] = new_c
+    states[:, d] = new_d
+  return states.realize()
 
 def compress_chunks(states: Tensor, chunks: Tensor, chain_vals: Tensor, n_end_blocks: int):
-  for i in range(chunks.shape[1]): # parallel over chunks, sequential over blocks
+  print("-" * 100)
+  print("CHUNKS START")
+  print("-" * 100)
+  for i in range(16): # parallel over chunks, sequential over blocks
+    print("chunks iteration", i)
     compressed = compress_blocks(states[:, i].contiguous(), chunks[:, i].contiguous(), chain_vals[:, i].contiguous())
     if i < chunks.shape[1] - 1: states[:, i + 1, :8] = compressed[:, :8] # propagate chain vals to the next block
   end_idx = n_end_blocks - 1 if chunks.shape[0] == 1 else n_end_blocks # handle partial final chunk
+  print("CHUNKS DONE")
+  print("-" * 100)
   return compressed[:, :8] if n_end_blocks == 16 else compressed[:-1, :8].cat(states[-1:, end_idx, :8])
 
 def compress_blocks(states: Tensor, chunks: Tensor, chain_vals: Tensor) -> Tensor:
+  print("-" * 100)
+  print("BLOCKS START")
+  print("-" * 100)
+  mix_jit = jit.TinyJit(mix)
   for _ in range(6):
-    mix(states, chunks)
-    chunks.replace(chunks[:, [2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8]])
-  mix(states, chunks)
+    print("blocks iteration", _)
+    mix_jit(states, chunks)
+    chunks = chunks[:, [2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8]]
+  mix_jit(states, chunks)
   states[:, :8] = states[:, :8] ^ states[:, 8:]
   states[:, 8:] = chain_vals[:, :8] ^ states[:, 8:]
   return states
@@ -56,17 +89,16 @@ def create_state(iv: Tensor, count: Optional[int], end_block_len: Optional[int],
 
 def create_flags(chunks: Tensor, n_end_blocks: Optional[int], root: bool, parent: bool) -> Tensor:
   end_idx = n_end_blocks - 1 if n_end_blocks is not None else -1
-  flags = Tensor.zeros((chunks.shape[0], chunks.shape[1], 1), dtype=dtypes.uint32).contiguous()
-  flags[:, 0] = flags[:, 0] + 1 # chunk start flag
+  flags = Tensor.full((chunks.shape[0], chunks.shape[1], 1), fill_value=4 if parent else 0, dtype=dtypes.uint32).contiguous()
+  flags[:, 0] += flags[:, 0] + 1 # chunk start flag
   flags[:-1, -1] = flags[:-1, -1] + 2 # chunk end flag
   flags[-1, end_idx:] = flags[-1, end_idx:] + 2 # chunk end flag for partial final chunk
-  if parent: flags[:] = 4 # parent flag
   if root: flags[0, end_idx] = flags[0, end_idx] + 8 # root flag
   return flags
 
 def compress(data, compressor, count, end_block_len, n_end_blocks, root, parent) -> Tensor:
-  IV = [0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19]
-  iv = Tensor(IV, dtype=dtypes.uint32).expand(data.shape[0], data.shape[1], -1).contiguous()
+  iv = Tensor([0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19], dtype=dtypes.uint32)
+  iv = iv.expand(data.shape[0], data.shape[1], -1).contiguous()
   states = create_state(iv, count, end_block_len, n_end_blocks, create_flags(data, n_end_blocks, root, parent))
   return compressor(states, data, iv, n_end_blocks)[:, :8]
 
@@ -81,3 +113,14 @@ def blake3(tensor: Tensor) -> str:
     chain_vals = compress(chain_vals, tree_compressor, None, None, None, i == tree_levels - 1, True)
     if leftover_chain_val is not None: chain_vals = chain_vals.cat(leftover_chain_val, dim=0)
   return chain_vals[0].flatten().bitcast(dtypes.uint8).data().tobytes()[:32].hex()
+
+
+if __name__ == "__main__":
+  data = 1024 * 1024 * 1000
+  input = Tensor.full((data,), fill_value=0x41, dtype=dtypes.uint8)
+  import time
+  start = time.monotonic()
+  result = blake3(input)
+  duration = time.monotonic() - start
+  print(f"Hashed {data/1024/1024:.1f}MB in {duration:.3f}s ({data/duration/1024/1024:.1f}MB/s)")
+  print(f"Hash: {result}")
