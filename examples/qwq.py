ac2f338b180388ab203f32c2dbab419be0cf31fa
Francis Lata <francisalbertlata@gmail.com>
2024-12-01 08:23:00 -0500

    add validation for simple prompt

--- examples/qwq.py
+++ examples/qwq.py
@@ -8,24 +8,25 @@ from pathlib import Path
 from extra.models.llama import Transformer, convert_from_huggingface, fix_bf16
 from examples.llama3 import load
 from tinygrad import nn, Tensor
-from tinygrad.helpers import fetch
+from tinygrad.helpers import fetch, colored
 from tinygrad.nn.state import load_state_dict
 
 MODELS = {
   "32B": {
     "model_params": {"dim": 5120, "n_heads": 40, "n_kv_heads": 8, "n_layers": 64, "norm_eps": 1e-5, "rope_theta": 1000000, "vocab_size": 152064, "hidden_dim": 27648},
-    "total_num_weights": 17
+    "total_num_weights": 17,
+    "tokenizer": "Qwen/QwQ-32B-Preview"
   }
 }
 
-def download_weights(total_num_weights:int):
+def download_weights(total_num_weights):
   model = fetch("https://huggingface.co/Qwen/QwQ-32B-Preview/resolve/main/model.safetensors.index.json?download=true", "model.safetensors.index.json", subdir="qwq_32b_preview")
 
   for i in range(1, total_num_weights + 1):
     filename = f"model-{str(i).zfill(5)}-of-{str(total_num_weights).zfill(5)}.safetensors"
     fetch(f"https://huggingface.co/Qwen/QwQ-32B-Preview/resolve/main/{filename}?download=true", filename, subdir="qwq_32b_preview")
 
-  return model
+  return Path(os.path.dirname(model))
 
 def load_model(model_path, model_params):
   # build model
@@ -56,19 +57,19 @@ if __name__ == "__main__":
   parser.add_argument("--size", choices=["32B"], default="32B", help="Model size")
   parser.add_argument("--count", type=int, default=30, help="Max number of tokens to generate")
   parser.add_argument("--temperature", type=float, default=0.7, help="Temperature in the softmax")
-  # parser.add_argument("--weights", type=str, default=None,
-  #                     help="Path to the downloaded weights")
+  parser.add_argument("--prompt", type=str, default="Hello.", help="Phrase to start with")
+  parser.add_argument("--weights", type=str, default=None, help="Path to the downloaded weights")
   args = parser.parse_args()
 
   model_info = MODELS[args.size]
 
-  model_path = download_weights(model_info["total_num_weights"])
-  transformer = load_model(Path(os.path.dirname(model_path)), model_info["model_params"])
-  tokenizer = AutoTokenizer.from_pretrained("Qwen/QwQ-32B-Preview")
+  model_path = Path(args.weights) if args.weights else download_weights(model_info["total_num_weights"])
+  transformer = load_model(model_path, model_info["model_params"])
+  tokenizer = AutoTokenizer.from_pretrained(model_info["tokenizer"])
 
-  outputted = "Hello."
+  outputted = args.prompt
   start_pos, toks = 0, tokenizer(outputted)["input_ids"]
-  print(outputted, end='', flush=True)
+  print(outputted, end="", flush=True)
 
   new_toks = tokenizer(outputted)["input_ids"]
   assert toks == new_toks[:len(toks)]
@@ -87,8 +88,19 @@ if __name__ == "__main__":
     # add the new token
     toks.append(tok)
 
-    # TODO: this is a hack to deal with spaces. i think the decode is fast though, so who cares?
     cur = tokenizer.decode(toks, skip_special_tokens=True)
     sys.stdout.write(cur[len(outputted):])
     sys.stdout.flush()
     outputted = cur
+
+  if args.temperature == 0 and args.count == 10 and args.prompt == "Hello.":
+    text = tokenizer.decode(toks)
+    key = args.size
+    expected = {
+      "32B": "Hello. I'm trying to make a program that will read",
+    }
+    try:
+      assert text == expected[key], f"invalid output: `{colored(text, 'red')}` != `{expected[key]}`"
+      print("\n" + colored("output validated", "green"))  # NOTE: "\n" iside colored does not render the color in github action
+    except KeyError:
+      pass
