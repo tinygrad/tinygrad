6bf1e93c746c890263386b4e6ee5c7890ba9dde9
uuuvn <uuuvn@proton.me>
2024-12-02 20:03:56 +0300

    Typo and comment about stdlib caching

--- tinygrad/runtime/ops_metal.py
+++ tinygrad/runtime/ops_metal.py
@@ -86,11 +86,12 @@ class MetalCompiler(Compiler):
       else:
         ret = CompileError(errorMessage.decode())
     # llvm will create modules.timestamp in cache path and cache compilation of metal stdlib (250ms => 8ms compilation time)
+    # note that llvm won't necessarily create anything else here as apple has prebuilt versions of many standart libraries
     params = f'-fno-fast-math -std=metal3.1 --driver-mode=metal -x metal -fmodules-cache-path="{os.path.join(_cache_dir, "tinygrad")}"'
     # source blob has to be padded to multiple of 4 but at least one 'b\x00' should be added, params blob just has to be null terminated
     src_padded, params_padded = src.encode() + b'\x00'*(round_up(len(src) + 1, 4) - len(src)), params.encode() + b'\x00'
     request = struct.pack('<QQ', len(src_padded), len(params_padded)) + src_padded + params_padded
-    # The callback is actully not a callback but a block which is apple's non-standart extension to add closures to C.
+    # The callback is actually not a callback but a block which is apple's non-standart extension to add closures to C.
     # See https://clang.llvm.org/docs/Block-ABI-Apple.html#high-level for struct layout.
     # Fields other than invoke are unused in this case so we can just use ctypes.byref with negative offset to invoke field, add blockptr as a first
     # argument and pretend it's a normal callback
