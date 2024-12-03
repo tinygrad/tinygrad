08a201f00933005e2c40a2ca1fc5855fcedad4bc
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2024-12-03 01:17:50 +0100

    Happy mypy

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -932,14 +932,14 @@ def div_and_mod_folding(x: UOp, c: int, which: Literal[Ops.MOD, Ops.IDIV], split
     offset -= r * v.vmin  # determine what the new offset would be
   else: # vmin/vmax of the remainder is between 0 and c, we can remove the mod/div
     remainders = [min(r, r-c, key=abs) for r in remainders]
-    if which is Ops.MOD: return functools.reduce(operator.add, [r*v for r,v in zip(remainders,svars)], offset)
-    return functools.reduce(operator.add, [(f-r)//c * v for f,r,v in zip(factors, remainders,svars)], (const-offset)//c)
+    if which is Ops.MOD: return functools.reduce(operator.add, [r*v for r,v in zip(remainders,svars)], x.const_like(offset))
+    return functools.reduce(operator.add, [(f-r)//c * v for f,r,v in zip(factors, remainders,svars)], x.const_like((const-offset)//c))
 
   if gcd != 1: something_changed = True
   if not something_changed:
     if which is Ops.IDIV and (1 < div < c) and (newx:=div_and_mod_folding(x, div, Ops.IDIV)) is not None: return newx//(c//div)
     return None
-  quo, rem = const//c, (const%c)//gcd
+  quo, rem = x.const_like(const//c), x.const_like((const%c)//gcd)
   for q,r,f,v in zip(quotients, remainders, factors, svars):
     if which is Ops.IDIV and (not split_rem) and r!=0:
       rem += f//gcd * v
