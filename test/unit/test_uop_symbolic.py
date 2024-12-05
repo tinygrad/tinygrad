0a73fe3c183f21ed8ad21902f94dbfa3227592d5
chenyu <chenyu@fastmail.com>
2024-12-04 20:13:19 -0500

    one more

--- test/unit/test_uop_symbolic.py
+++ test/unit/test_uop_symbolic.py
@@ -67,6 +67,7 @@ class TestSymbolic(unittest.TestCase):
     self.helper_test_variable(Variable("a", 3, 4) < Variable("b", 5, 6), 1, 1, "True")
     self.helper_test_variable(Variable("a", 3, 5) < Variable("b", 5, 6), 0, 1, "(a<b)")
     self.helper_test_variable(Variable("a", 5, 6) < Variable("b", 3, 5), 0, 0, "False")
+    self.helper_test_variable(Variable("a", 3, 4) < Variable("a", 3, 4), 0, 0, "False")
 
   def test_lt_divides(self):
     expr = (Variable("idx", 0, 511)*4 + Variable("FLOAT4_INDEX", 0, 3)) < 512
