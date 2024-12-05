3c9b34ee7196456d49cac1a0c1543c65a6fdc822
qazal <qazal.software@gmail.com>
2024-12-05 08:29:46 -0800

    linter and missed _device

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -322,7 +322,8 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   def cast(self, dtype:DType, bitcast=False, allow_buffer_view=True):
     shape = self.st.shape if self.st is not None else None
     if bitcast and self.dtype.itemsize != dtype.itemsize:
-      if shape is None or not all_int(shape) or not self.device.startswith("DISK"): raise RuntimeError(f"shape changing bitcast not supported on {self}")
+      if shape is None or not all_int(shape) or not self.device.startswith("DISK"):
+        raise RuntimeError(f"shape changing bitcast not supported on {self}")
       # https://pytorch.org/docs/stable/generated/torch.Tensor.view.html
       if (shape[-1]*self.dtype.itemsize) % dtype.itemsize != 0: raise RuntimeError("unsupported size in bitcast")
       shape = shape[:-1] + ((shape[-1]*self.dtype.itemsize) // dtype.itemsize,)
@@ -358,7 +359,8 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
     return self if len(axis) == 0 else UOp(Ops.REDUCE_AXIS, self.dtype, (self,), (op, axis))
   def assign(self, x:UOp): return UOp(Ops.ASSIGN, self.dtype, (self,x), None if self.st is None or self.st.contiguous else self.st)
   def contiguous(self, allow_buffer_view=True):
-    return UOp.metaop(Ops.BUFFER_VIEW, self.shape, self.dtype, self.device, None, (self,)) if allow_buffer_view and self.can_view() else self.alu(Ops.CONTIGUOUS)
+    if allow_buffer_view and self.can_view(): return UOp.metaop(Ops.BUFFER_VIEW, self.shape, self.dtype, self.device, None, (self,))
+    return UOp(Ops.CONTIGUOUS, self.dtype, (self,))
 
   # *** from LazyBuffer ***
 
@@ -390,8 +392,8 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
     return (s:=self.base).op is Ops.VIEW and len(s.src) == 2 and s.realized is None and s.src[1].op is Ops.CONST and not isinstance(s.src[1].arg, UOp)
   def is_unrealized_unmasked_const(self): return self.is_unrealized_const() and all(v.mask is None for v in unwrap(self.st).views)
   def can_view(self):
-    return (self.st is not None and self.st.consecutive and not self.is_unrealized_const() and not isinstance(self.dtype, ImageDType) and
-            self.device.split(":")[0] in view_supported_devices)
+    return (self.st is not None and self._device is not None and self.st.consecutive and not self.is_unrealized_const() and
+            not isinstance(self.dtype, ImageDType) and self.device.split(":")[0] in view_supported_devices)
   @property
   def srcs(self): return self.src
   @srcs.deleter
