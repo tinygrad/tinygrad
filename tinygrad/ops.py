ade7ba33b4521fefa0d417dad9e11bf9f763b348
qazal <qazal.software@gmail.com>
2024-12-06 13:30:13 +0200

    subbufer everything

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -329,10 +329,8 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
       # https://pytorch.org/docs/stable/generated/torch.Tensor.view.html
       if (shape[-1]*self.dtype.itemsize) % dtype.itemsize != 0: raise RuntimeError("unsupported size in bitcast")
       shape = shape[:-1] + ((shape[-1]*self.dtype.itemsize) // dtype.itemsize,)
-      return UOp.metaop(Ops.BUFFER_VIEW, unwrap(shape), dtype, self.device, None, (self,))
-    if self._device is not None and self.device.startswith("DISK"):
-      if not bitcast: raise RuntimeError("can only bitcast disk")
-      return UOp.metaop(Ops.BUFFER_VIEW, unwrap(shape), dtype, self.device, None, (self,))
+    if bitcast and self.can_view() and allow_buffer_view: return UOp.metaop(Ops.BUFFER_VIEW, unwrap(shape), dtype, self.device, None, (self,))
+    if self._device is not None and self._device.startswith("DISK"): raise RuntimeError("can only bitcast DISK")
     return UOp(Ops.BITCAST if bitcast else Ops.CAST, dtype, (self,))
   def bitcast(self, dtype:DType): return self.cast(dtype, bitcast=True)
   def gep(self, i:Union[Tuple[int, ...], int]):
@@ -360,9 +358,9 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
     return self if len(axis) == 0 else UOp(Ops.REDUCE_AXIS, self.dtype, (self,), (op, axis))
   def assign(self, x:UOp): return UOp(Ops.ASSIGN, self.dtype, (self,x), None if self.st is None or self.st.contiguous else self.st)
   def contiguous(self, allow_buffer_view=True):
-    if self.device.startswith("DISK"): return UOp.metaop(Ops.BUFFER_VIEW, self.shape, self.dtype, self.device, None, (self,))
     if not unwrap(self.st).contiguous or self.size != self.base.size or self.is_unrealized_const():
-      return UOp(Ops.CONTIGUOUS, self.dtype, (self,))
+      if allow_buffer_view and self.can_view(): return self.metaop(Ops.BUFFER_VIEW, self.shape, self.dtype, self.device, None, (self,))
+      return self.alu(Ops.CONTIGUOUS)
     forced_realize.add(self.base)
     return self
   @staticmethod
