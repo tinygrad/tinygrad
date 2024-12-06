e6941a39b8e8306e6d38f87c9341f8f05cbd11b0
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-12-06 11:34:51 +0100

    Typing and cleanup

--- test/testextra/test_f16_decompress.py
+++ test/testextra/test_f16_decompress.py
@@ -8,8 +8,8 @@ import numpy as np
 class TestF16Decompression(unittest.TestCase):
   def test_u32_to_f16(self):
     a = Tensor.randn(50, dtype=dtypes.float16, device=None if is_dtype_supported(dtypes.float16) else "CLANG:0")
-    f16_as_u32 = a.bitcast(dtypes.uint32) if is_dtype_supported(dtypes.float16) else a.bitcast(dtypes.uint32).to(Device.DEFAULT) 
-    f16 = u32_to_f16(f16_as_u32[:50])
+    f16_as_u32 = a.bitcast(dtypes.uint32) if is_dtype_supported(dtypes.float16) else a.bitcast(dtypes.uint32).to(Device.DEFAULT)
+    f16 = u32_to_f16(f16_as_u32)
     ref = a.numpy()
     out = f16.numpy().astype(np.float16)
     np.testing.assert_allclose(out, ref)
