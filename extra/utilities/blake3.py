4f3f2640ba1015c4857ac953c463019bbd032af7
Jamesb <experimentallearning0@gmail.com>
2024-12-07 17:56:12 +0000

    fully jitted

--- extra/utilities/blake3.py
+++ extra/utilities/blake3.py
@@ -1,133 +1,89 @@
 import math
 import random
 import time
-from typing import Callable, Tuple
-from tinygrad import Tensor, TinyJit, Device, GlobalCounters, Variable
+from typing import Tuple
+from tinygrad import Tensor, TinyJit, Variable
 from tinygrad.dtype import dtypes
-from tinygrad.helpers import Context, Profiling, ceildiv
+from tinygrad.helpers import ceildiv
 from tinygrad.tensor import Tensor
 
-PAD, DEFAULT_LEN, PERMS = 66, 65, Tensor([2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8], dtype=dtypes.uint32)
 IV = Tensor([0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19], dtype=dtypes.uint32)
+PAD, DEFAULT_LEN = 66, 65
+PERMUTATIONS = Tensor([2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8], dtype=dtypes.uint32)
 
 def compress_blocks(states: Tensor, data: Tensor, chain_vals: Tensor) -> Tensor:
-    for i in range(7):
-        for j, (a,b,c,d) in enumerate([(0,4,8,12), (1,5,9,13), (2,6,10,14), (3,7,11,15), (0,5,10,15), (1,6,11,12), (2,7,8,13), (3,4,9,14)]):
-            mx, my = data[j * 2], data[j * 2 + 1]
-            for m in (mx, my):
-                states[a] = (states[a] + states[b] + m)
-                states[d] = ((states[d] ^ states[a]) << (32 - (16 if m is mx else 8))) | ((states[d] ^ states[a]) >> (16 if m is mx else 8))
-                states[c] = states[c] + states[d]
-                states[b] = ((states[b] ^ states[c]) << (32 - (12 if m is mx else 7))) | ((states[b] ^ states[c]) >> (12 if m is mx else 7))
-        if i < 6: data = data[PERMS]
-    return (states[:8] ^ states[8:]).cat(chain_vals[:8] ^ states[8:]).realize()
-
-def compress_chunks(states: Tensor, data: Tensor, chain_vals: Tensor, info: Tensor) -> Tensor:
-    for i in range(16):
-        next_state = states[i] if i == 0 else states[i-1, :8].cat(states[i, 8:])
-        states[i] = compress_blocks(next_state, data[i], chain_vals[i])
-    states = states * (info < PAD)
-    end_block = (states * (info < DEFAULT_LEN)).sum(0)
-    return (states[-1, :] | end_block)[:8]
-
-def tensor_to_blake_data(tensor: Tensor, max_memory: int) -> Tuple[Tensor, Tensor]:
-    assert max_memory % 1024 == 0, "max_memory must be divisible by 1024"
-    data = tensor.flatten().pad(((0, (max_memory // tensor.element_size()) - tensor.shape[0],),), value=0)
-    data = data.bitcast(dtypes.uint32).reshape(-1, 16, 16).permute(1, 2, 0).contiguous()
-    final_chunk_len = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 1024 or 1024)
-    n_end_blocks = ceildiv(final_chunk_len, 64) or 1
-    n_chunks = max(1, ceildiv(tensor.nbytes(), 1024))
-    info = Tensor.full((16, 1, data.shape[-1]), fill_value=DEFAULT_LEN, dtype=dtypes.uint32).contiguous()
-    info[n_end_blocks - 1, :, n_chunks - 1] = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 64) or 64
-    info[n_end_blocks:, :, n_chunks - 1:] = info[:, :, n_chunks:] = PAD
-    n_steps = Variable(min_val=0, max_val=54, name="n_steps").bind(math.ceil(math.log2(max(n_chunks, 1))))
-    return data, info, n_steps
+  for i in range(7):
+    for j, (a,b,c,d) in enumerate([(0,4,8,12), (1,5,9,13), (2,6,10,14), (3,7,11,15), (0,5,10,15), (1,6,11,12), (2,7,8,13), (3,4,9,14)]):
+      mx, my = data[j * 2], data[j * 2 + 1]
+      for m in (mx, my):
+        states[a] = (states[a] + states[b] + m)
+        states[d] = ((states[d] ^ states[a]) << (32 - (16 if m is mx else 8))) | ((states[d] ^ states[a]) >> (16 if m is mx else 8))
+        states[c] = states[c] + states[d]
+        states[b] = ((states[b] ^ states[c]) << (32 - (12 if m is mx else 7))) | ((states[b] ^ states[c]) >> (12 if m is mx else 7))
+    if i < 6: data = data[PERMUTATIONS]
+  return (states[:8] ^ states[8:]).cat(chain_vals[:8] ^ states[8:]).realize()
 
 @TinyJit
-def pairwise_concat(chain_vals: Tensor) -> Tuple[Tensor, Tensor]:
-  print(f"--- pairwise_concat ---")
-  cv_pairs_with_leftover = chain_vals.permute(1, 0).reshape(-1, 16).transpose()
-  paired = chain_vals.any(0).reshape(-1, 2)
-  paired_mask, leftover_mask = (paired[:, 0] * paired[:, 1]), (paired[:, 0] ^ paired[:, 1])
-  cv_pairs = (cv_pairs_with_leftover * paired_mask).pad(((0, 0), (0, paired.shape[0])), value=0)
-  leftover = (cv_pairs_with_leftover * leftover_mask).max(1)[:8].reshape(-1, 1)
-  print(f"--- pairwise_concat done ---")
-  return cv_pairs, leftover
-
-def create_state(iv: Tensor, counts: Tensor, info: Tensor, flags: Tensor) -> Tensor:
-    counts = counts.cat(Tensor.zeros(iv.shape[0], 1, iv.shape[-1], dtype=dtypes.uint32), dim=1)
-    lengths = (info == DEFAULT_LEN).where(64, info)
-    return (iv.cat(iv[:, :4], counts, lengths, flags, dim=1) * (info < PAD).cast(dtypes.uint32)).realize()
-
-def create_flags(info: Tensor, parents: bool, final_step: Tensor) -> Tensor:
-    flags = Tensor.zeros((16, 1, info.shape[-1]), dtype=dtypes.uint32).contiguous()
-    flags[-1, 0] = flags[-1, 0] + 2 # chunk end flag
-    flags = (flags + 2 * ((flags != 2) * (info < DEFAULT_LEN))) # chunk end flag for partial final chunk
-    flags[0] = flags[0] + 1 # chunk start flag
-    if parents: flags[:] = 4
-    flags = (flags + (8 * (((info < PAD).sum() <= 16) * (info < DEFAULT_LEN)))) # add root flag if <= 1 chunk
-    flags = final_step.where(12, flags)
-    return flags.cast(dtypes.uint32).realize()
+def init_chain_vals(data: Tensor, info: Tensor) -> Tuple[Tensor, Tensor]:
+  chain_vals = IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1]).contiguous()
+  counts = Tensor.arange(0, data.shape[-1], dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1).permute(1, 2, 0)
+  counts = counts.cat(Tensor.zeros(chain_vals.shape[0], 1, chain_vals.shape[-1], dtype=dtypes.uint32), dim=1)
+  lengths = (info == DEFAULT_LEN).where(64, info)
+  flags = Tensor.zeros((16, 1, info.shape[-1]), dtype=dtypes.uint32).contiguous()
+  flags[-1, 0] = flags[-1, 0] + 2 # chunk end flag
+  flags = (flags + 2 * ((flags != 2) * (info < DEFAULT_LEN))) # chunk end flag for partial final chunk
+  flags[0] = flags[0] + 1 # chunk start flag
+  flags = (flags + (8 * (((info < PAD).sum() <= 16) * (info < DEFAULT_LEN)))).cast(dtypes.uint32) # add root flag if <= 1 chunk
+  states = (chain_vals.cat(chain_vals[:, :4], counts, lengths, flags, dim=1) * (info < PAD).cast(dtypes.uint32))
+  for i in range(16):
+    next_state = states[i] if i == 0 else states[i-1, :8].cat(states[i, 8:])
+    states[i] = compress_blocks(next_state, data[i], chain_vals[i])
+  states = states * (info < PAD)
+  end_block = (states * (info < DEFAULT_LEN)).sum(0)
+  return (states[-1, :] | end_block)[:8].realize()
 
 @TinyJit
-def init_compress(data: Tensor, info: Tensor) -> Tuple[Tensor, Tensor]:
-    iv = IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1]).contiguous()
-    counts = Tensor.arange(0, data.shape[-1], dtype=dtypes.uint32).reshape(-1, 1).expand(-1, 16).reshape(-1, 16, 1).permute(1, 2, 0)
-    states = create_state(iv, counts, info, create_flags(info, False, Tensor.zeros(1, dtype=dtypes.bool)))
-    return compress_chunks(states, data, iv, info).realize()
+def tree_hash(chain_vals: Tensor, n_steps: Variable) -> Tensor:
+  for _ in range(n_steps.val):
+    stacked = chain_vals.transpose().reshape(-1, 16).transpose().reshape(2, 8, -1)
+    stacked_mask = stacked.any(1)
+    final_step = (stacked_mask.sum() <= 2)
+    pair_mask, remainder_mask = (stacked_mask[0] * stacked_mask[1]), (stacked_mask[0] ^ stacked_mask[1])
+    paired, remainder = (stacked * pair_mask).reshape(16, -1), (stacked * remainder_mask).reshape(16, -1)[:8]
+    flags = final_step.where(12, Tensor.full((1, paired.shape[-1]), 4, dtype=dtypes.uint32))
+    iv = IV.reshape(8, 1).expand(8, paired.shape[-1])
+    counts = Tensor.zeros((2, paired.shape[-1]), dtype=dtypes.uint32)
+    lengths = Tensor.full((1, paired.shape[-1]), 64, dtype=dtypes.uint32)
+    states = iv.cat(iv[:4], counts, lengths, flags, dim=0)
+    chain_vals = ((compress_blocks(states, paired, iv) * pair_mask)[:8] + remainder).realize()
+  return chain_vals.realize()
 
-@TinyJit
-def tree_compress(chain_vals: Tensor, info: Tensor) -> Tensor:
-    final_step = chain_vals.any(0).sum(-1) == 2
-    chain_vals, leftover_chain_val = pairwise_concat(chain_vals)
-    valid = chain_vals.any(0)
-    iv = IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1]).contiguous()
-    counts = Tensor.zeros((16, 1, info.shape[-1]), dtype=dtypes.uint32)
-    flags =  create_flags(info, True, final_step)
-    states = create_state(iv, counts, info, flags)
-    chain_vals = compress_blocks(states[-1].contiguous(), chain_vals, iv[0])
-    chain_vals = (chain_vals[:8] * valid)
-    insertion_mask = (valid ^ valid.roll(1, -1))
-    insertion_mask[0] = 0
-    return insertion_mask.where(leftover_chain_val, chain_vals).realize()
-
-compress_blocks_jit = TinyJit(compress_blocks)
+def tensor_to_blake_input(tensor: Tensor, max_memory: int) -> Tuple[Tensor, Tensor, Variable]:
+  assert max_memory % 1024 == 0 and max_memory & (max_memory - 1) == 0, "max_memory must be a power of two divisible by 1024"
+  data = tensor.flatten().pad(((0, (max_memory // tensor.element_size()) - tensor.shape[0],),), value=0)
+  data = data.bitcast(dtypes.uint32).reshape(-1, 16, 16).permute(1, 2, 0).contiguous()
+  final_chunk_len = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 1024 or 1024)
+  n_end_blocks = ceildiv(final_chunk_len, 64) or 1
+  n_chunks = max(1, ceildiv(tensor.nbytes(), 1024))
+  info = Tensor.full((16, 1, data.shape[-1]), fill_value=DEFAULT_LEN, dtype=dtypes.uint32).contiguous()
+  info[n_end_blocks - 1, :, n_chunks - 1] = 0 if tensor.nbytes() == 0 else (tensor.nbytes() % 64) or 64
+  info[n_end_blocks:, :, n_chunks - 1:] = info[:, :, n_chunks:] = PAD
+  n_steps = Variable(min_val=0, max_val=math.log2(max_memory), name="n_steps").bind(math.ceil(math.log2(max(n_chunks, 1))))
+  return data, info, n_steps
 
 def blake3(tensor: Tensor, max_memory: int = 1024**3) -> str:
-    data, info, n_steps = tensor_to_blake_data(tensor, max_memory)
-    start = time.time()
-    print(f"--- init_compress ---")
-    chain_vals = init_compress(data, info)
-    end = time.time()
-    print(f"Time: {end - start:.2f}s")
-    start = time.time()
-    info = (info < DEFAULT_LEN).where(64, info)
-    start = time.time()
-    print(f"--- tree_compress ---")
-    # TODO: try old pairwise_concat etc.
-    for _ in range(n_steps.val):
-        final_step = chain_vals.any(0).sum(-1) == 2
-        chain_vals, leftover_chain_val = pairwise_concat(chain_vals)
-        valid = chain_vals.any(0)
-        iv = IV.reshape(1, 8, 1).expand(16, 8, info.shape[-1])
-        counts = Tensor.zeros((16, 1, info.shape[-1]), dtype=dtypes.uint32)
-        flags =  create_flags(info, True, final_step)
-        states = create_state(iv, counts, info, flags)
-        chain_vals = compress_blocks_jit(states[-1].contiguous(), chain_vals, iv[0])
-        chain_vals = (chain_vals[:8] * valid)
-        insertion_mask = (valid ^ valid.roll(1, -1))
-        insertion_mask[0] = 0
-        chain_vals = insertion_mask.where(leftover_chain_val, chain_vals)
-    end = time.time()
-    print(f"Time: {end - start:.2f}s")
-    return chain_vals[:, 0].flatten().bitcast(dtypes.uint8).data().tobytes().hex()
+  data, info, n_steps = tensor_to_blake_input(tensor, max_memory)
+  chain_vals = init_chain_vals(data, info)
+  chain_vals = tree_hash(chain_vals, n_steps)
+  return chain_vals[:, 0].flatten().bitcast(dtypes.uint8).data().tobytes().hex()
 
 if __name__ == "__main__":
     import time
     import sys
 
     arg = sys.argv[1]
-    max_memory = (1024**2 * 500)
+    max_memory = 2 ** math.ceil(math.log2(1024**3))
+    print(f"Using max_memory: {max_memory / 1024 / 1024:.1f} MB")
 
     if arg == "warmup":
         # warmup the JIT
@@ -158,6 +114,8 @@ if __name__ == "__main__":
         size_mb = float(sys.argv[1])
         size = int(size_mb * 1024 * 1024)
 
-        for i in range(4):
+        for i in range(5):
             randint = random.randint(0, 1024**2 * 20)
+            # if i == 4:
+            #     with Context(DEBUG=2):
             benchmark_size(size - randint)
