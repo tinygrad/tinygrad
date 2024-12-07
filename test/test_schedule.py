39f2fd8787abe794efd6cdb1a798869d5170662d
qazal <qazal.software@gmail.com>
2024-12-07 11:59:06 +0200

    more diff pruning

--- test/test_schedule.py
+++ test/test_schedule.py
@@ -1536,12 +1536,11 @@ class TestIndexing(unittest.TestCase):
     a[0] = 6
     np.testing.assert_equal(a.numpy(), [6., 2., 3., 4.])
 
-  #@unittest.skipUnless(Device.DEFAULT in view_supported_devices, "need view")
-  @unittest.skip("subbuffer on non-disk not supported yet")
+  @unittest.skipUnless(Device.DEFAULT in view_supported_devices, "need view")
   def test_arange_view_op(self):
     a = Tensor.arange(12).reshape(4, 3).shrink(((1, 2), (1, 3))).contiguous()
     assert isinstance(a.lazydata, LazyBuffer)
-    self.assertIs(a.lazydata.base.op, Ops.BUFFER_VIEW)
+    self.assertIs(a.lazydata.base.src[1].op, Ops.BUFFER_VIEW)
     self.check_schedule(a, 1)
     np.testing.assert_equal(a.numpy(), [[4, 5]])
 
