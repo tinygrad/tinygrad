85179bb2ed9dcf8a579a4842accf5f4eb6bb4ebb
qazal <qazal.software@gmail.com>
2024-12-11 16:12:53 +0200

    more

--- test/test_const_folding.py
+++ test/test_const_folding.py
@@ -10,7 +10,7 @@ def _check_ast_count(desired_count:int, t:Tensor):
   # NOTE: this has side effect because everything can be scheduled only once
   schedule = create_schedule(t.lazydata.lbs)
   asts = [s for s in schedule if s.ast.op is Ops.SINK]
-  assert len(asts) == desired_count, f"{len(asts)} != {desired_count}"
+  assert len(asts) == desired_count
 
 class TestUnaryOpsConstFolding(unittest.TestCase):
   def test_all_consts_ops(self):
@@ -99,7 +99,7 @@ class TestBinaryOpsConstFolding(unittest.TestCase):
     _check_ast_count(0, Tensor.ones(4) ** Tensor([1.0, 2, 3, 4]))
 
 # folds advance indexing into basic indexing
-@unittest.expectedFailure
+@unittest.skip("indexing ops folding no longer supported")
 class TestIndexingConstFolding(unittest.TestCase):
   def test_scalar_index(self):
     t = Tensor.arange(16).float().reshape(1,1,4,4).realize()
