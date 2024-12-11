8b0bbeeb28353cebb221a4abfe85aa2e3d4887e1
zibokapi <zibokapi@gmail.com>
2024-12-11 19:58:40 +0800

    maybe something like this?

--- test/external/external_test_onnx_backend.py
+++ test/external/external_test_onnx_backend.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import Any, Tuple
 from onnx.backend.base import Backend, BackendRep
+import onnx
 import onnx.backend.test
 import numpy as np
 from tinygrad import Tensor, Device, dtypes
@@ -38,7 +39,32 @@ class TinygradBackend(Backend):
     # NOTE: this is onnx CPU
     return device == "CPU"
 
-backend_test = onnx.backend.test.BackendTest(TinygradBackend, __name__)
+# extend BackendTest to support in-memory tests
+class TinygradBackendTestRunner(onnx.backend.test.BackendTest):
+  def _add_model_test(self, model_test, kind):
+    if model_test.model is not None and model_test.data_sets is not None:
+      # Handle in-memory model and data
+      model_marker = [model_test.model]
+      def run(test_self, device='CPU', **kwargs):
+        model = model_test.model
+        if hasattr(self.backend, "is_compatible") and not self.backend.is_compatible(model):
+          raise unittest.SkipTest("Not compatible with backend")
+        prepared = self.backend.prepare(model, device, **kwargs)
+        for inputs, ref_outputs in model_test.data_sets:
+          outputs = prepared.run(inputs)
+          self.assert_similar_outputs(ref_outputs, outputs, model_test.rtol, model_test.atol)
+      self._add_test(kind + "Model", model_test.name, run, model_marker)
+    else:
+      # Delegate to the original logic for file-based tests
+      super()._add_model_test(model_test, kind)
+
+
+backend_test = TinygradBackendTestRunner(TinygradBackend, __name__)
+
+from test.external.external_test_onnx_superset import adam_test_case, adagrad_test_case, momentum_test_case
+backend_test._add_model_test(adam_test_case, "Tinygrad")
+backend_test._add_model_test(adagrad_test_case, "Tinygrad")
+backend_test._add_model_test(momentum_test_case, "Tinygrad")
 
 # TODO: there isn't an AttributeProto for `epsilon` in the NodeProto for 'test_adam_multiple_cpu'
 # [x.name for x in n.attribute] -> ['alpha', 'beta', 'norm_coefficient']
---
+++ test/external/external_test_onnx_superset.py
@@ -0,0 +1,73 @@
+import numpy as np
+from onnx import helper
+from onnx.backend.test.case.test_case import TestCase
+from onnx.defs import AI_ONNX_PREVIEW_TRAINING_DOMAIN
+
+def create_testcase(op:str, name:str, inputs:dict[str, np.ndarray], outputs:dict[str, np.ndarray], domain='', **opts):
+  node = helper.make_node(op, list(inputs), outputs, name=f"test_{op}", domain=domain, **opts)
+  inputs_info = [helper.make_tensor_value_info(name, helper.np_dtype_to_tensor_dtype(arr.dtype), arr.shape) for name, arr in inputs.items()]
+  outputs_info = [helper.make_tensor_value_info(name, helper.np_dtype_to_tensor_dtype(arr.dtype), arr.shape) for name, arr in outputs.items()]
+  graph = helper.make_graph([node], name, inputs_info, outputs_info)
+  model = helper.make_model(graph)
+  return TestCase(name=name, model_name=name, model=model, data_sets=[(list(inputs.values()), list(outputs.values()))], kind="node",
+                  rtol=1e-3, atol=1e-7, url=None, model_dir=None)
+
+def test_adam_large_t():
+  from onnx.backend.test.case.node.adam import apply_adam
+  op = "Adam"
+  inputs = {
+  "r": np.array(0.1, dtype=np.float32),
+  "t": np.array(2, dtype=np.int64),
+  "x": np.array([1.2, 2.8], dtype=np.float32),
+  "g": np.array([-0.94, -2.5], dtype=np.float32),
+  "v": np.array([1.7, 3.6], dtype=np.float32),
+  "h": np.array([0.1, 0.1], dtype=np.float32)
+  }
+  outputs = ["x_new", "v_new", "h_new"]
+  opts = { "alpha": 0.95, "beta": 0.1, "epsilon": 1e-7, "norm_coefficient": 0.001, "norm_coefficient_post": 0.0 }
+  onnx_out = apply_adam(**inputs, **opts)
+  outputs = dict([*list(zip(outputs, onnx_out))])
+  # NOTE: fix x_new since np.sqrt turns float32 into float64
+  outputs["x_new"] = outputs["x_new"].astype(np.float32)
+  adam_test_case = create_testcase(op, "test_Adam_internal", inputs, outputs, AI_ONNX_PREVIEW_TRAINING_DOMAIN, **opts)
+  return adam_test_case
+
+def test_adagrad_large_t():
+  from onnx.backend.test.case.node.adagrad import apply_adagrad
+  op = "Adagrad"
+  inputs = {
+    "r": np.array(0.1, dtype=np.float32),
+    "t": np.array(2, dtype=np.int64),
+    "x": np.array([1.0], dtype=np.float32),
+    "g": np.array([-1.0], dtype=np.float32),
+    "h": np.array([2.0], dtype=np.float32)
+  }
+  outputs = ["x_new", "h_new"]
+  opts = { "norm_coefficient": 0.001, "epsilon": 1e-5, "decay_factor": 0.1 }
+  onnx_out = apply_adagrad(**inputs, **opts)
+  outputs = dict([*list(zip(outputs, onnx_out))])
+  # NOTE: fix x_new since np.sqrt turns float32 into float64
+  outputs["x_new"] = outputs["x_new"].astype(np.float32)
+  adagrad_test_case = create_testcase(op, "test_Adagrad_internal", inputs, outputs, AI_ONNX_PREVIEW_TRAINING_DOMAIN, **opts)
+  return adagrad_test_case
+
+def test_momentum_large_t():
+  from onnx.backend.test.case.node.momentum import apply_momentum
+  op = "Momentum"
+  inputs = {
+    "r": np.array(0.1, dtype=np.float32),
+    "t": np.array(2, dtype=np.int64),
+    "x": np.array([1.2, 2.8], dtype=np.float32),
+    "g": np.array([-0.94, -2.5], dtype=np.float32),
+    "v": np.array([1.7, 3.6], dtype=np.float32)
+  }
+  outputs = ["x_new", "v_new"]
+  opts = { "norm_coefficient": 0.001, "alpha": 0.95, "beta": 0.1, "mode": "standard" }
+  onnx_out = apply_momentum(**inputs, **{"norm_coefficient": 0.001, "alpha": 0.95, "beta": 0.1})
+  outputs = dict([*list(zip(outputs, onnx_out))])
+  momentum_test_case = create_testcase(op, "test_Momentum_internal", inputs, outputs, AI_ONNX_PREVIEW_TRAINING_DOMAIN, **opts)
+  return momentum_test_case
+
+momentum_test_case = test_momentum_large_t()
+adam_test_case = test_adam_large_t()
+adagrad_test_case = test_adagrad_large_t()
