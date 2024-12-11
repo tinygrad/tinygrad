f926f9678b5b10a2c871363cd88fbed1557ab24f
qazal <qazal.software@gmail.com>
2024-12-11 22:06:23 +0200

    test_multi_const_folding_tensor

--- test/test_const_folding.py
+++ test/test_const_folding.py
@@ -238,7 +238,6 @@ class TestMultiConstFolding(unittest.TestCase):
     _check_ast_count(0, t ** 1)
     _check_ast_count(0, 1 ** t)
 
-  @unittest.expectedFailure
   def test_multi_const_folding_tensor(self):
     ds = tuple(f"{Device.DEFAULT}:{i}" for i in range(4))
     t = Tensor.arange(16).float().realize().to(ds)
