4396ac96b50d58b967c48dca6b34584b38c5b3a2
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-12-13 12:38:59 +0100

    No NamedTuple in compile.py

--- examples/webgpu/stable_diffusion/compile.py
+++ examples/webgpu/stable_diffusion/compile.py
@@ -2,13 +2,11 @@ import os
 from extra.export_model import export_model
 from extra.f16_decompress import u32_to_f16
 from examples.stable_diffusion import StableDiffusion
-from tinygrad.nn.state import get_state_dict, safe_save, safe_load_metadata, torch_load, load_state_dict
+from tinygrad.nn.state import safe_save, torch_load, load_state_dict
 from tinygrad.tensor import Tensor
 from tinygrad import Device, dtypes
 from tinygrad.helpers import fetch
-from typing import NamedTuple, Any, List
 import requests
-import argparse
 import numpy as np
 from pathlib import Path
 
@@ -33,37 +31,29 @@ def fetch_dep(file, url):
 if __name__ == "__main__":
   fetch_dep(os.path.join(os.path.dirname(__file__), "clip_tokenizer.js"), "https://huggingface.co/wpmed/tinygrad-sd-f16/raw/main/clip_tokenizer.js")
   fetch_dep(os.path.join(os.path.dirname(__file__), "bpe_simple_vocab_16e6.mjs"), "https://huggingface.co/wpmed/tinygrad-sd-f16/raw/main/bpe_simple_vocab_16e6.mjs")
-  parser = argparse.ArgumentParser(description='Run Stable Diffusion', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--remoteweights', action='store_true', help="Use safetensors from Huggingface, or from local")
-  args = parser.parse_args()
-  Device.DEFAULT = "WEBGPU"
 
+  Device.DEFAULT = "WEBGPU"
   Tensor.no_grad = True
   model = StableDiffusion()
   load_state_dict(model, torch_load(fetch('https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt', 'sd-v1-4.ckpt'))['state_dict'], strict=False)
 
-  class ModelPart(NamedTuple):
-    name: str = ""
-    input: List[Tensor] = []
-    forward: Any = None
-
   model_parts = [
-    ModelPart(name = "textModel", input = [Tensor.randn(1, 77)], forward = model.cond_stage_model.transformer.text_model),
-    ModelPart(name = "diffusor", input = [
+    ("textModel", [Tensor.randn(1, 77)], model.cond_stage_model.transformer.text_model),
+    ("diffusor", [
       Tensor.randn(1, 77, 768), Tensor.randn(1, 77, 768), Tensor.randn(1,4,64,64), 
       Tensor.rand(1), Tensor.randn(1), Tensor.randn(1), Tensor.randn(1)
-    ], forward = model),
-    ModelPart(name = "decoder", input = [Tensor.randn(1,4,64,64)], forward = model.decode),
-    ModelPart(name = "f16tof32", input = [Tensor.randn(2097120, dtype=dtypes.uint32)], forward = u32_to_f16)
+    ], model),
+    ("decoder", [Tensor.randn(1,4,64,64)], model.decode),
+    ("f16tof32", [Tensor.randn(2097120, dtype=dtypes.uint32)], u32_to_f16)
   ]
 
   for model in model_parts:
-    prg, inp_sizes, out_sizes, state = export_model(model, Device.DEFAULT.lower(), *model.input, model_name=model.name)
+    prg, inp_sizes, out_sizes, state = export_model(model[2], Device.DEFAULT.lower(), *model[1], model_name=model[0])
     dirname = Path(__file__).parent
-    weight_loc =  (dirname / f"net_{model.name}.safetensors").as_posix()
+    weight_loc =  (dirname / f"net_{model[0]}.safetensors").as_posix()
     safe_save(state, weight_loc)
-    if model.name == "diffusor":
+    if model[0] == "diffusor":
       convert_f32_to_f16(weight_loc, (dirname / f"net_diffusor_f16.safetensors").as_posix())
 
-    with open(dirname / f"net_{model.name}.js", "w") as text_file:
+    with open(dirname / f"net_{model[0]}.js", "w") as text_file:
       text_file.write(prg)
