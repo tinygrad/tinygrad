3d4bbc3a355d597e917cef98a228029622869f20
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2024-12-13 07:17:53 +0100

    WIP

--- examples/webgpu/stable_diffusion/compile.py
+++ examples/webgpu/stable_diffusion/compile.py
@@ -17,18 +17,14 @@ def convert_f32_to_f16(input_file, output_file):
     metadata_length_bytes = f.read(8)
     metadata_length = int.from_bytes(metadata_length_bytes, byteorder='little', signed=False)
     metadata_json_bytes = f.read(metadata_length)
-    float32_values = np.fromfile(f, dtype=np.float32)
+    values = np.fromfile(f, dtype=np.float32)
 
-  first_text_model_offset = 3772703308
-  num_elements = int((first_text_model_offset)/4)
-  front_float16_values = float32_values[:num_elements].astype(np.float16)
-  rest_float32_values = float32_values[num_elements:]
+  f16_values = values.astype(np.float16)
 
   with open(output_file, 'wb') as f:
     f.write(metadata_length_bytes)
     f.write(metadata_json_bytes)
-    front_float16_values.tofile(f)
-    rest_float32_values.tofile(f)
+    f16_values.tofile(f)
 
 def fetch_dep(file, url):
   with open(file, "w", encoding="utf-8") as f:
@@ -44,25 +40,30 @@ if __name__ == "__main__":
 
   Tensor.no_grad = True
   model = StableDiffusion()
-
-  # load in weights
   load_state_dict(model, torch_load(fetch('https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt', 'sd-v1-4.ckpt'))['state_dict'], strict=False)
 
-  class Step(NamedTuple):
+  class ModelPart(NamedTuple):
     name: str = ""
     input: List[Tensor] = []
     forward: Any = None
 
   model_parts = [
-    Step(name = "textModel", input = [Tensor.randn(1, 77)], forward = model.cond_stage_model.transformer.text_model),
-    Step(name = "diffusor", input = [Tensor.randn(1, 77, 768), Tensor.randn(1, 77, 768), Tensor.randn(1,4,64,64), Tensor.rand(1), Tensor.randn(1), Tensor.randn(1), Tensor.randn(1)], forward = model),
-    Step(name = "decoder", input = [Tensor.randn(1,4,64,64)], forward = model.decode),
-    Step(name = "f16tof32", input = [Tensor.randn(2097120, dtype=dtypes.uint32)], forward = u32_to_f16)
+    ModelPart(name = "textModel", input = [Tensor.randn(1, 77)], forward = model.cond_stage_model.transformer.text_model),
+    ModelPart(name = "diffusor", input = [
+      Tensor.randn(1, 77, 768), Tensor.randn(1, 77, 768), Tensor.randn(1,4,64,64), 
+      Tensor.rand(1), Tensor.randn(1), Tensor.randn(1), Tensor.randn(1)
+    ], forward = model),
+    ModelPart(name = "decoder", input = [Tensor.randn(1,4,64,64)], forward = model.decode),
+    ModelPart(name = "f16tof32", input = [Tensor.randn(2097120, dtype=dtypes.uint32)], forward = u32_to_f16)
   ]
 
   for model in model_parts:
     prg, inp_sizes, out_sizes, state = export_model(model, Device.DEFAULT.lower(), *model.input)
     dirname = Path(__file__).parent
-    safe_save(state, (dirname / f"net_{model.name}.safetensors").as_posix())
+    weight_loc =  (dirname / f"net_{model.name}.safetensors").as_posix()
+    safe_save(state, weight_loc)
+    if model.name == "diffusor":
+      convert_f32_to_f16(weight_loc, (dirname / f"net_diffuso_f16.safetensors").as_posix())
+
     with open(dirname / f"net_{model.name}.js", "w") as text_file:
         text_file.write(prg)
