857a14b46ff4c341cbf5ca33f840b7fff2b7caa4
uuuvn <uuuvn@proton.me>
2024-12-15 19:31:05 +0300

    Check for existence of `dlmopen` instead of `not OSX`

--- tinygrad/runtime/ops_clang.py
+++ tinygrad/runtime/ops_clang.py
@@ -1,10 +1,11 @@
 from typing import Optional, List
 import ctypes, subprocess, pathlib, tempfile, os, tinygrad.runtime.autogen.libc as libc
 from tinygrad.device import Compiled, Compiler, MallocAllocator
-from tinygrad.helpers import OSX, getenv, cpu_time_execution, cpu_objdump
+from tinygrad.helpers import getenv, cpu_time_execution, cpu_objdump
 from tinygrad.renderer.cstyle import ClangRenderer
 
-ISOLATE_DLOPEN = getenv("ISOLATE_DLOPEN", int(not OSX))
+# dlmopen is a GNU extension and so it won't exist on OSX or non-glibc linux
+ISOLATE_DLOPEN = getenv("ISOLATE_DLOPEN", int(hasattr(libc, "dlmopen")))
 
 class ClangCompiler(Compiler):
   def __init__(self, cachekey="compile_clang", args:Optional[List[str]]=None, objdump_tool='objdump'):
@@ -24,7 +25,7 @@ class ClangCompiler(Compiler):
 class ClangProgram:
   # We can't use libc.LM_ID_NEWLM for every dlmopen because (at least in glibc) the maximum number of namespaces is limited and too small for us.
   # However having one namespace for all ClangPrograms shouldn't be a problem as long as we use RTLD_LOCAL for every dlmopen.
-  NAMESPACE_ID = libc.LM_ID_NEWLM if ISOLATE_DLOPEN else libc.LM_ID_BASE
+  NAMESPACE_ID = libc.Lmid_t(libc.LM_ID_NEWLM if ISOLATE_DLOPEN else libc.LM_ID_BASE)
   def __init__(self, name:str, lib:bytes):
     # write to disk so we can load it
     with tempfile.NamedTemporaryFile(delete=True) as cached_file_path:
@@ -34,9 +35,8 @@ class ClangProgram:
       else:
         handle = libc.dlopen(str(cached_file_path.name).encode(), os.RTLD_NOW | os.RTLD_LOCAL)
     if handle is None: raise RuntimeError("failed to import dynamic library: try ISOLATE_DLOPEN=0")
-    if ClangProgram.NAMESPACE_ID == libc.LM_ID_NEWLM:
-      assert libc.dlinfo(handle, libc.RTLD_DI_LMID, ctypes.byref(ret:=libc.Lmid_t())) == 0
-      ClangProgram.NAMESPACE_ID = ret.value
+    if ClangProgram.NAMESPACE_ID.value == libc.LM_ID_NEWLM:
+      assert libc.dlinfo(handle, libc.RTLD_DI_LMID, ctypes.byref(ClangProgram.NAMESPACE_ID)) == 0
     self.fxn = ctypes.CDLL(None, handle=handle)[name]
 
   def __call__(self, *bufs, vals=(), wait=False): return cpu_time_execution(lambda: self.fxn(*bufs, *vals), enable=wait)
