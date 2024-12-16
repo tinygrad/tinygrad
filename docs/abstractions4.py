73636d0f6b517c8f4a119797a1b8ab0f0783d764
George Hotz <geohot@gmail.com>
2024-12-16 09:28:58 -0800

    more notes

--- docs/abstractions4.py
+++ docs/abstractions4.py
@@ -22,9 +22,32 @@ for si in c.schedule(): print(si)
 #    make a best effort approach for others
 #    give buffers to all Tensors in scope (can break a lot of folding)
 
+# we can do a substitute to add TENSOR UOps to all UOps we want to potentially get a Buffer
+# however, you need a concept of "strong" and "weak" Tensors if you want to fold.
+# "strong" Tensors are Tensors that are explicitly specified in the realize
+# "weak" Tensors are Tensors that are in scope but not in the realize
+
 # 2. What should the scope policy of Buffers be?
 #    Kept around only if the Tensor is around (can make things unrealizable)
 #    Kept around if any unrealized child Tensor is around
 #    Kept around if any child Tensor (even realized) is around (stuff isn't freed)
 
-# 3. Is UOp mutability okay?
+# it's acceptable for `backward` to only work if the gradient path hasn't been realized
+# that choice allows `no_grad` to be meaningless. if backward worked on realized graphs, you wouldn't be able to free memory
+
+# "Kept around if any unrealized child Tensor is around" is probably the only choice that makes sense
+
+# 3. Is UOp mutability okay? Are there any alternatives that work?
+
+# without UOp mutability, there's no clean solution to this
+a = Tensor([1., 2, 3])
+b = Tensor([4., 5, 6])
+c = a+b
+d = c*2
+c.realize()
+# d has a UOp graph keeping a and b alive, but not if the c UOp is mutable
+
+# 4. How do we represent a CONST Tensor? Currently it uses a fake BUFFER
+#      UOp(Ops.VIEW, dtypes.float, arg=ShapeTracker(views=(View(shape=(), strides=(), offset=0, mask=None, contiguous=True),)), src=(
+#        UOp(Ops.BUFFER, dtypes.float.ptr(), arg=(-1, 'METAL', 1), src=()),
+#        UOp(Ops.CONST, dtypes.float, arg=2.0, src=()),)),)),)),))
