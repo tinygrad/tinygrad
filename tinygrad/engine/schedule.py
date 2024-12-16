02295221f6aba92dfa36c52449dd2bd7b5562524
qazal <qazal.software@gmail.com>
2024-12-16 15:01:23 +0200

    test_plus_big passes

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -1,7 +1,7 @@
 import functools
 from dataclasses import dataclass
 from typing import Tuple, List, Dict
-from tinygrad.ops import GroupOp, UOp, Ops, PatternMatcher, UPat, Variable, graph_rewrite, track_rewrites, symbolic, merge_views
+from tinygrad.ops import GroupOp, UOp, Ops, PatternMatcher, UPat, Variable, graph_rewrite, track_rewrites, merge_views
 from tinygrad.helpers import Metadata, unwrap, unwrap_or
 from tinygrad.device import Buffer
 from tinygrad.shape.shapetracker import ShapeTracker
@@ -26,10 +26,6 @@ class ScheduleItem:
   def output_idxs(self) -> Tuple[int, ...]: return tuple(x.src[0].arg for x in self.ast.src) if self.ast.op is Ops.SINK else (0,)
 
 remove_movement_ops = PatternMatcher([(UPat(GroupOp.Movement, name="x"), lambda x: x.base.view(unwrap(x.st))),])
-prune = PatternMatcher([
-  # root is VIEW, wraps a CONST and a BUFFER, is this fine?
-  (UPat(Ops.VIEW, name="view", src=(UPat(), UPat.cvar("x"))), lambda view,x: x if all(v.mask is None for v in unwrap(view.st).views) else None),
-])
 
 def _bufferize(ctx:Dict[UOp, UOp], x:UOp):
   if x in ctx or x.op is Ops.VIEW and len(x.src) == 1: return None
@@ -41,22 +37,29 @@ realize = PatternMatcher([
    lambda ctx,sink: UOp(Ops.SINK, src=new_src) if (new_src:=tuple(nx for x in sink.src if (nx:=_bufferize(ctx, x)) is not None))!=sink.src else None),
   (UPat(Ops.COPY, name="cp", src=(UPat.var("x"),)), lambda ctx,cp,x: None if (bx:=unwrap_or(_bufferize(ctx, x), x)) is x else cp.replace(src=(bx,))),
   (UPat(Ops.VIEW, name="x", src=(UPat(Ops.BUFFER), UPat(GroupOp.Meta))), _bufferize),
+  (UPat(Ops.CONTIGUOUS, name="x"), _bufferize),
 ])
 
 def add_buf(ctx:List[UOp], b:UOp):
   ctx.append(b)
   return UOp(Ops.DEFINE_GLOBAL, b.dtype, (), len(ctx)-1)
 to_ast = PatternMatcher([
-  # buffer becomes a ptr
-  (UPat(Ops.BUFFER, name="b"), add_buf),
-  # buffer views become global load
+  # ** buffer op rules
+  # const is just const
+  (UPat(Ops.VIEW, name="st", src=(UPat(), UPat.cvar("x"))), lambda st,x: UOp.const_with_shape(x.dtype, x.arg, st.shape)),
+  # buffer view is load
   (UPat(Ops.VIEW, name="st", src=(UPat(Ops.DEFINE_GLOBAL, name="ptr"),)), lambda st,ptr: UOp.load(ptr, st.st.to_uop(), dtype=ptr.dtype.base)),
-  # buffer+op views become global stores
+  # buffer+op view is store
   (UPat(Ops.VIEW, name="st", src=(UPat(Ops.DEFINE_GLOBAL, name="ptr"), UPat.var("v"))), lambda st,ptr,v: UOp.store(ptr, st.st.to_uop(), v)),
-  # sink rules
+  # ** sink rules
   (UPat(Ops.SINK, src=(UPat.store(UPat.var("dest"), UPat(), UPat(GroupOp.Meta, name="metaop")),)),
    lambda metaop,dest: metaop.replace(src=(dest,)+metaop.src)),
   (UPat(Ops.SINK, src=(UPat(Ops.LOAD),)), lambda: UOp(Ops.NOOP)),
+  # ** misc rules
+  # buffer becomes a ptr
+  (UPat(Ops.BUFFER, name="b"), add_buf),
+  # don't need contiguous anymore
+  (UPat(Ops.CONTIGUOUS, src=(UPat.var("x"),)), lambda x:x),
 ])
 
 cut_edges = PatternMatcher([
@@ -72,7 +75,7 @@ def schedule_uop(out:UOp, dest:UOp) -> ScheduleItem:
 
 @track_rewrites(named=True)
 def create_schedule_with_vars(outs:List[UOp]) -> Tuple[List[ScheduleItem], Dict[Variable, int]]:
-  sink = graph_rewrite(UOp.sink(*outs), remove_movement_ops+merge_views+prune+symbolic)
+  sink = graph_rewrite(UOp.sink(*outs), remove_movement_ops+merge_views)
   realizes: Dict[UOp, UOp] = {}
   graph_rewrite(sink, realize, realizes)
   schedule = [schedule_uop(u, b) for b,u in realizes.items()]
