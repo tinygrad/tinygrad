3a65aab75440d5984a05d8a1227b007d6952111d
leopf <l.pfob@3-klicks.de>
2024-12-19 15:50:02 +0100

    extension cleanup

--- docs/mkdocs_exts.py
+++ docs/mkdocs_exts.py
@@ -1,9 +1,8 @@
 import griffe
 
 class StateAcceptFilenameDecorator(griffe.Extension):
-  def on_function_instance(self, *, func: griffe.Function, **kwargs) -> None:
-    def name(n: str): return griffe.ExprName(n, func.parent)
-    for decorator in func.decorators:
-      if decorator.callable_path == "tinygrad.nn.state.accept_filename":
-        for p in func.parameters:
-          p.annotation = griffe.ExprSubscript(name("Union"), griffe.ExprTuple([name("Tensor"), name("str"), name("pathlib.Path")], implicit=True))
+  def on_function_instance(self, *, func: griffe.Function, **_) -> None:
+    if any(d.callable_path == "tinygrad.nn.state.accept_filename" for d in func.decorators):
+      def name(n: str): return griffe.ExprName(n, func.parent)
+      for p in func.parameters:
+        p.annotation = griffe.ExprSubscript(name("Union"), griffe.ExprTuple([name("Tensor"), name("str"), name("pathlib.Path")], implicit=True))
