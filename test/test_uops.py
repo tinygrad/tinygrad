695965123f1058d5e3e02f9ef9539c624060c4b8
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2024-12-25 18:49:55 +0100

    Fix typo in test

--- test/test_uops.py
+++ test/test_uops.py
@@ -367,7 +367,7 @@ class TestAssembly(unittest.TestCase):
     self.assertIn(Ops.IDIV, ops)
 
   def test_mulacc_unrolled(self):
-    # test that     acc = acc + a0*b0 a1*b1 + a2*b2 + a3*b3
+    # test that     acc = acc + a0*b0 + a1*b1 + a2*b2 + a3*b3
     # is not        acc = acc + (a0*b0 + a1*b1 + a2*b2 + a3*b3)
     a = Tensor.empty(1024)
     b = Tensor.empty(1024)
