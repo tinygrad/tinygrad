3e16cb150ee113fcaf886e3c7786925c3fd63433
qazal <qazal.software@gmail.com>
2024-12-28 12:16:59 +0200

    this assert should work now

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -283,6 +283,7 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
     # buffer ops return the ShapeTracker from sources
     if self.op in GroupOp.Buffer: return vsrc[0] if len(vsrc:=[x.st for x in self.src if x.op is Ops.VIEW]) != 0 else None
     src_sts = [x.st for x in self.src if x.st is not None]
+    assert all_same([x.shape for x in src_sts]), f"UOp sources must have the same shape {self} {[x.shape for x in src_sts]}"
     match self.op:
       # only reduce ops are allowed to change shape
       case Ops.REDUCE_AXIS | Ops.WMMA: shape = src_sts[0].reduce(self.axis_arg)
