a1b2ea8506425d8838d70d2b04910d0e074e6eff
qazal <qazal.software@gmail.com>
2025-01-02 03:42:40 +0200

    when const_folding folds

--- test/test_schedule_map.py
+++ test/test_schedule_map.py
@@ -4,7 +4,7 @@ from tinygrad import Tensor
 def unwrap_buf(t) -> list:
   assert t.lazydata.base.realized is not None, f"expected {t.lazydata} to be real"
   return t.tolist()
- 
+
 class TestMap(unittest.TestCase):
   def test_simple_copy(self):
     a = Tensor([11])
@@ -42,6 +42,12 @@ class TestMap(unittest.TestCase):
     b = (a*0).contiguous()
     b.realize()
     self.assertListEqual(unwrap_buf(b), [0])
+
+  def test_const_folding_folds(self):
+    a = Tensor([11])
+    b = (a*0)
+    b.realize()
+    self.assertIsNone(b.lazydata.realized)
 
 if __name__ == "__main__":
   unittest.main()
