e539692d9fd87901d2f8daa2ae2053fdd5c988dd
zibokapi <zibokapi@gmail.com>
2025-01-04 22:40:44 +0800

    use Tensor to get_input

--- examples/benchmark_onnx.py
+++ examples/benchmark_onnx.py
@@ -1,6 +1,6 @@
 import sys, onnx, time, pathlib
 from tinygrad import Tensor, TinyJit, Device, GlobalCounters, fetch, getenv
-from extra.onnx import get_run_onnx
+from extra.onnx import get_run_onnx, dtype_parse
 import onnxruntime as ort
 import numpy as np
 
@@ -25,17 +25,17 @@ def get_input(inp:onnx.ValueInfoProto, config:dict):
       case (False, _): shape.append(1)
   shape = tuple(shape)
   # get dtype
-  dtype = onnx.helper.tensor_dtype_to_np_dtype(inp.type.tensor_type.elem_type)
+  dtype = dtype_parse(inp.type.tensor_type.elem_type)
   # determine value
   match inp.name:
     case "input_ids":
       vocab_size = config.get("text_config", {}).get("vocab_size") or config.get("vocab_size", 50265)
-      val = np.random.randint(0, vocab_size, shape)
-    case "attention_mask": val = np.random.randint(0, 2, shape)
-    case "token_type_ids": val = np.random.randint(0, config.get("type_vocab_size", 2), shape)
-    case "image_tensor": val = np.random.randint(0, 256, shape)
-    case _: val = (np.random.randn(*shape) * 8) if shape else np.array(0)
-  return val.astype(dtype)
+      val = Tensor.randint(*shape, low=0, high=vocab_size, dtype=dtype)
+    case "attention_mask": val = Tensor.randint(*shape, low=0, high=2, dtype=dtype)
+    case "token_type_ids": val = Tensor.randint(*shape, low=0, high=config.get("type_vocab_size", 2), dtype=dtype)
+    case "image_tensor": val = Tensor.randint(*shape, low=0, high=256, dtype=dtype)
+    case _: val = Tensor.randn(*shape, dtype=dtype).mul(8) if shape else Tensor(None, dtype=dtype)
+  return val.realize()
 
 def benchmark(onnx_model_path:pathlib.Path, config:dict={}, test_vs_ort=False):
   print("running benchmark")
@@ -43,7 +43,7 @@ def benchmark(onnx_model_path:pathlib.Path, config:dict={}, test_vs_ort=False):
   Tensor.no_grad = True
   Tensor.training = False
   run_onnx = get_run_onnx(onnx_model)
-  print("loaded into tinygrad onnx runner")
+  print("loaded model")
 
   # find preinitted tensors and ignore them
   initted_tensors = {inp.name for inp in onnx_model.graph.initializer}
@@ -53,14 +53,14 @@ def benchmark(onnx_model_path:pathlib.Path, config:dict={}, test_vs_ort=False):
   run_onnx_jit = TinyJit(lambda **kwargs: tuple(run_onnx({k:v.to(Device.DEFAULT) for k,v in kwargs.items()}).values()), prune=True)
 
   for i in range(3):
-    new_inputs = {inp.name:Tensor(get_input(inp, config)).realize() for inp in expected_inputs}
+    new_inputs = {inp.name:get_input(inp, config) for inp in expected_inputs}
     GlobalCounters.reset()
     print(f"run {i}")
     run_onnx_jit(**new_inputs)
 
   # run 20 times
   for _ in range(20):
-    new_inputs = {inp.name:Tensor(get_input(inp, config)).realize() for inp in expected_inputs}
+    new_inputs = {inp.name:get_input(inp, config) for inp in expected_inputs}
     GlobalCounters.reset()
     st = time.perf_counter()
     out = run_onnx_jit(**new_inputs)
