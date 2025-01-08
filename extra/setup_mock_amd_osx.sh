0724b9450e952e38ec330b7b35e04fc389c55e93
nimlgen <lenyaprokh@gmail.com>
2025-01-09 00:39:05 +0300

    hm

--- extra/setup_mock_amd_osx.sh
+++ extra/setup_mock_amd_osx.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 INSTALL_PATH="/opt/homebrew/lib"
+mkdir -p $INSTALL_PATH
 
 curl -s https://api.github.com/repos/Qazalin/remu/releases/latest | \
     jq -r '.assets[] | select(.name == "libremu.dylib").browser_download_url' | \
     xargs curl -L -o $INSTALL_PATH/libremu.dylib
 
-curl -s https://api.github.com/repos/nimlgen/homebrew-tinygrad_mockgpu_utils/releases/latest | \
+curl -s https://api.github.com/repos/nimlgen/amdcomgr_dylib/releases/latest | \
     jq -r '.assets[] | select(.name == "libamd_comgr.dylib").browser_download_url' | \
     xargs curl -L -o $INSTALL_PATH/libamd_comgr.dylib
