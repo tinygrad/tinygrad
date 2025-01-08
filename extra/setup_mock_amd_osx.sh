1184de77d16c404923550f83c47d8bd1696befa4
nimlgen <lenyaprokh@gmail.com>
2025-01-09 00:43:59 +0300

    hm2

--- extra/setup_mock_amd_osx.sh
+++ extra/setup_mock_amd_osx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-INSTALL_PATH="/opt/homebrew/lib"
-mkdir -p $INSTALL_PATH
+INSTALL_PATH="${1:-/opt/homebrew/lib}"
+[ ! -d "$INSTALL_PATH" ] && sudo mkdir -p "$INSTALL_PATH"
 
 curl -s https://api.github.com/repos/Qazalin/remu/releases/latest | \
     jq -r '.assets[] | select(.name == "libremu.dylib").browser_download_url' | \
