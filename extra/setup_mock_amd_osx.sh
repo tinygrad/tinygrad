321ad6d4a84df0782a6b5eda6498b90a2a4df2fc
nimlgen <lenyaprokh@gmail.com>
2025-01-09 00:53:49 +0300

    small comments

--- extra/setup_mock_amd_osx.sh
+++ extra/setup_mock_amd_osx.sh
@@ -2,10 +2,12 @@
 INSTALL_PATH="${1:-/opt/homebrew/lib}"
 [ ! -d "$INSTALL_PATH" ] && mkdir -p "$INSTALL_PATH"
 
+# Download libremu.dylib
 curl -s https://api.github.com/repos/Qazalin/remu/releases/latest | \
     jq -r '.assets[] | select(.name == "libremu.dylib").browser_download_url' | \
     xargs curl -L -o $INSTALL_PATH/libremu.dylib
 
+# Download libamd_comgr.dylib
 curl -s https://api.github.com/repos/nimlgen/amdcomgr_dylib/releases/latest | \
     jq -r '.assets[] | select(.name == "libamd_comgr.dylib").browser_download_url' | \
     xargs curl -L -o $INSTALL_PATH/libamd_comgr.dylib
