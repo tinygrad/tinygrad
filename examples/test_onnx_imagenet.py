e512531c3e9819aaebec4f5718012493e609c120
George Hotz <geohot@gmail.com>
2025-01-09 19:22:59 -0800

    more work on onnx imagenet [pr]

--- examples/test_onnx_imagenet.py
+++ examples/test_onnx_imagenet.py
@@ -9,10 +9,12 @@ from tinygrad import Tensor, dtypes
 #  ~70% - https://github.com/onnx/models/raw/refs/heads/main/validated/vision/classification/resnet/model/resnet50-v2-7.onnx
 #  ~43% - https://github.com/onnx/models/raw/refs/heads/main/Computer_Vision/alexnet_Opset16_torch_hub/alexnet_Opset16.onnx
 #  ~64% - https://github.com/xamcat/mobcat-samples/raw/refs/heads/master/onnx_runtime/InferencingSample/InferencingSample/mobilenetv2-7.onnx
+#  ~71% - https://github.com/axinc-ai/onnx-quantization/raw/refs/heads/main/models/mobilenetv2_1.0.opt.onnx
 # broken:
 #  https://github.com/MTlab/onnx2caffe/raw/refs/heads/master/model/MobileNetV2.onnx
 #  https://huggingface.co/qualcomm/MobileNet-v2-Quantized/resolve/main/MobileNet-v2-Quantized.onnx
 #  https://github.com/xamcat/mobcat-samples/raw/refs/heads/master/onnx_runtime/InferencingSample/InferencingSample/mobilenetv2-7-quantized.onnx
+#  ~35% - https://github.com/axinc-ai/onnx-quantization/raw/refs/heads/main/models/mobilenev2_quantized.onnx
 
 if __name__ == "__main__":
   run_onnx_jit, input_shapes, input_types = load_onnx_model(sys.argv[1])
@@ -20,7 +22,7 @@ if __name__ == "__main__":
   assert shape[1:] == (3,224,224), f"shape is {shape}"
 
   input_mean = Tensor([0.485, 0.456, 0.406]).reshape(1, -1, 1, 1)
-  input_std = Tensor([0.485, 0.456, 0.406]).reshape(1, -1, 1, 1)
+  input_std = Tensor([0.229, 0.224, 0.225]).reshape(1, -1, 1, 1)
 
   files = get_val_files()
   random.shuffle(files)
