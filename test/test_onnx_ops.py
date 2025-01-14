0d48e67727a628063acc31162e38d0e2060070fa
zibokapi <zibokapi@gmail.com>
2025-01-15 01:44:32 +0800

    simplify bias and c to be minimalistic enough to test correctness

--- test/test_onnx_ops.py
+++ test/test_onnx_ops.py
@@ -34,13 +34,13 @@ def helper_test_op(inputs, model, atol=1e-6, rtol=1e-3):
     np.testing.assert_allclose(tinygrad_val, ort_val, rtol=rtol, atol=atol)
 
 def helper_test_single_op(op:str, inps:dict[str, np.ndarray], opt:dict[str, Any],
-                          outs:dict[str, tuple[list[int], np.dtype]], domain=None, atol=1e-6, rtol=1e-3, tag=""):
+                          outs:dict[str, tuple[list[int], np.dtype]], domain=None, atol=1e-6, rtol=1e-3):
   onnx_inputs = [helper.make_tensor_value_info(name, helper.np_dtype_to_tensor_dtype(arr.dtype), arr.shape) for name, arr in inps.items()]
   onnx_outputs = [helper.make_tensor_value_info(name, helper.np_dtype_to_tensor_dtype(np.dtype(dtype)), shape)
                   for name, (shape, dtype) in outs.items()]
-  nodes = [helper.make_node(op, list(inps.keys()), list(outs), domain=domain, **opt)]
-  graph = helper.make_graph(nodes, f"test_{op.lower()}{tag}", onnx_inputs, onnx_outputs)
-  model = helper.make_model(graph, producer_name=f"test_{op.lower()}{tag}")
+  nodes = [helper.make_node(op, list(inps), list(outs), domain=domain, **opt)]
+  graph = helper.make_graph(nodes, f"test_{op.lower()}", onnx_inputs, onnx_outputs)
+  model = helper.make_model(graph, producer_name=f"test_{op.lower()}")
   helper_test_op(list(inps.values()), model, atol, rtol)
 
 class TestOnnxOps(unittest.TestCase):
@@ -53,22 +53,23 @@ class TestOnnxOps(unittest.TestCase):
 class TestOnnxQuantizedOps(unittest.TestCase):
   def test_qlinear_conv(self):
     for dtype, zero_point in [(np.uint8, 128), (np.int8, 0)]:
-      with self.subTest(dtype=dtype, zero_point=zero_point):
-        dtype_min, dtype_max = np.iinfo(dtype).min, np.iinfo(dtype).max+1
-        inputs = {
-          "x": np.random.randint(dtype_min, dtype_max, [1, 3, 224, 224], dtype=dtype),
-          "x_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-          "x_zero_point": np.array(zero_point, dtype=dtype),
-          "w": np.random.randint(dtype_min, dtype_max, [32, 3, 3, 3], dtype=dtype),
-          "w_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-          "w_zero_point": np.array(zero_point, dtype=dtype),
-          "y_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-          "y_zero_point": np.array(zero_point, dtype=dtype),
-          "b": np.random.randint(-10000, 10000, [32], dtype=np.int32)
-        }
-        attributes = {'auto_pad': 'NOTSET', 'dilations': (1, 1), 'group': 1, 'kernel_shape': (3, 3), 'pads': (1, 1, 1, 1), 'strides': (2, 2)}
-        outputs = {"out": ([1, 32, 112, 112], dtype)}
-        helper_test_single_op("QLinearConv", inputs, attributes, outputs, atol=1)
+      for b in (np.ones([32], dtype=np.int32), np.zeros([32], dtype=np.int32)):
+        with self.subTest(dtype=dtype, zero_point=zero_point):
+          dtype_min, dtype_max = np.iinfo(dtype).min, np.iinfo(dtype).max+1
+          inputs = {
+            "x": np.random.randint(dtype_min, dtype_max, [1, 3, 224, 224], dtype=dtype),
+            "x_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+            "x_zero_point": np.array(zero_point, dtype=dtype),
+            "w": np.random.randint(dtype_min, dtype_max, [32, 3, 3, 3], dtype=dtype),
+            "w_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+            "w_zero_point": np.array(zero_point, dtype=dtype),
+            "y_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+            "y_zero_point": np.array(zero_point, dtype=dtype),
+            "b": b
+          }
+          attributes = {'auto_pad': 'NOTSET', 'dilations': (1, 1), 'group': 1, 'kernel_shape': (3, 3), 'pads': (1, 1, 1, 1), 'strides': (2, 2)}
+          outputs = {"out": ([1, 32, 112, 112], dtype)}
+          helper_test_single_op("QLinearConv", inputs, attributes, outputs, atol=1)
 
   def test_qlinear_matmul(self):
     for dtype, zero_point in [(np.uint8, 128), (np.int8, 0)]:
@@ -126,26 +127,28 @@ class TestOnnxQuantizedOps(unittest.TestCase):
     for dtype, zero_point in [(np.uint8, 128), (np.int8, 0)]:
       for alpha in [0.5, 1.0, 2.0]:
         for transA in [0, 1]:
-          with self.subTest(dtype=dtype, zero_point=zero_point, alpha=alpha, transA=transA):
-            dtype_min, dtype_max = np.iinfo(dtype).min, np.iinfo(dtype).max+1
-            inputs = {
-              "A": np.random.randint(dtype_min, dtype_max, [32, 32], dtype=dtype),
-              "a_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-              "a_zero_point": np.array(zero_point, dtype=dtype),
-              "B": np.random.randint(dtype_min, dtype_max, [32, 32], dtype=dtype),
-              "b_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-              "b_zero_point": np.array(zero_point, dtype=dtype),
-              "C": np.random.randint(-16256, 16256, [32, 32], dtype=np.int32),
-            }
-            attributes = {'alpha': alpha, 'transA': transA, 'transB': 0}
-            outputs = {"Y": ([32,32], np.float32)}
-            helper_test_single_op("QGemm", inputs, attributes, outputs, domain=CONTRIB_OPERATORS, atol=1)
+          # TODO: need a better way for testing optional inputs
+          for C in (np.ones([32,32], dtype=np.int32), np.zeros([32,32], dtype=np.int32)):
+            with self.subTest(dtype=dtype, zero_point=zero_point, alpha=alpha, transA=transA, C=C):
+              dtype_min, dtype_max = np.iinfo(dtype).min, np.iinfo(dtype).max+1
+              inputs = {
+                "A": np.random.randint(dtype_min, dtype_max, [32, 32], dtype=dtype),
+                "a_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+                "a_zero_point": np.array(zero_point, dtype=dtype),
+                "B": np.random.randint(dtype_min, dtype_max, [32, 32], dtype=dtype),
+                "b_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+                "b_zero_point": np.array(zero_point, dtype=dtype),
+                "C": C
+              }
+              attributes = {'alpha': alpha, 'transA': transA, 'transB': 0}
+              outputs = {"Y": ([32,32], np.float32)}
+              helper_test_single_op("QGemm", inputs, attributes, outputs, domain=CONTRIB_OPERATORS, atol=1)
 
-            inputs = {**inputs,
-                      "y_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
-                      "y_zero_point": np.array(zero_point, dtype=dtype)}
-            outputs = {"Y": ([32,32], dtype)}
-            helper_test_single_op("QGemm", inputs, attributes, outputs, domain=CONTRIB_OPERATORS, atol=1)
+              inputs = {**inputs,
+                        "y_scale": np.array(np.random.uniform(0.01, 0.1), dtype=np.float32),
+                        "y_zero_point": np.array(zero_point, dtype=dtype)}
+              outputs = {"Y": ([32,32], dtype)}
+              helper_test_single_op("QGemm", inputs, attributes, outputs, domain=CONTRIB_OPERATORS, atol=1)
 
 if __name__ == "__main__":
   unittest.main()
