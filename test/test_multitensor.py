cdd831a8e95377e93d98834345a253a9e7fc61b3
George Hotz <geohot@gmail.com>
2025-01-17 09:57:22 -0800

    no pathlib stuff

--- test/test_multitensor.py
+++ test/test_multitensor.py
@@ -331,9 +331,7 @@ class TestMultiTensor(unittest.TestCase):
   # NOTE: this is failing on LLVM CI, no idea why. Works locally.
   @unittest.skipIf(CI and Device.DEFAULT in ("CUDA", "NV", "LLVM"), "slow")
   def test_data_parallel_resnet(self):
-    import sys, pathlib
-    sys.path.append((pathlib.Path(__file__).parent.parent / "extra" / "models").as_posix())
-    from resnet import ResNet18
+    from extra.models.resnet import ResNet18
 
     fake_image = Tensor.rand((2, 3, 224//8, 224//8))
     fake_image_sharded = fake_image.shard(devices_2, axis=0)
@@ -350,9 +348,7 @@ class TestMultiTensor(unittest.TestCase):
 
   @unittest.skipIf(CI and Device.DEFAULT in ("CUDA", "NV", "LLVM"), "slow, and flaky on LLVM")
   def test_data_parallel_resnet_train_step(self):
-    import sys, pathlib
-    sys.path.append((pathlib.Path(__file__).parent.parent / "extra" / "models").as_posix())
-    from resnet import ResNet18
+    from extra.models.resnet import ResNet18
     from tinygrad.nn.optim import LARS
 
     fake_image = Tensor.rand((2, 3, 224//8, 224//8))
