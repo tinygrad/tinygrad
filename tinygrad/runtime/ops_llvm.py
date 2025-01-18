c3171df459343f3036b7d4f490afe0b493d80b50
uuuvn <uuuvn@proton.me>
2025-01-18 11:18:20 +0300

    Move things around

--- tinygrad/runtime/ops_llvm.py
+++ tinygrad/runtime/ops_llvm.py
@@ -15,32 +15,22 @@ def expect(x, err, ret=None):
 HOST_ARCH = {'arm64': 'AArch64', 'aarch64': 'AArch64', 'x86_64': 'X86'}[platform.machine()]
 REQUIRED_COMPONENTS = ['Target', 'TargetInfo', 'TargetMC', 'AsmPrinter']
 
-class LLVMDevice(Compiled):
-  def __init__(self, device:str):
-    for component in REQUIRED_COMPONENTS:
-      getattr(llvm, f'LLVMInitialize{HOST_ARCH}{component}')()
-
-    triple = f'{platform.machine()}-none-unknown-elf'.encode()
-    target = expect(llvm.LLVMGetTargetFromTriple(triple, ctypes.pointer(tgt:=llvm.LLVMTargetRef()), err:=cerr()), err, tgt)
-    features = b'+reserve-x18' if platform.machine() == 'arm64' else b''
-    target_machine = llvm.LLVMCreateTargetMachine(target, triple, b'', features, llvm.LLVMCodeGenLevelDefault, llvm.LLVMRelocPIC,
-                                                  llvm.LLVMCodeModelDefault)
-    pbo = llvm.LLVMCreatePassBuilderOptions()
-    if getenv("LLVMOPT"):
-      passes = b'default<O2>'
-      llvm.LLVMPassBuilderOptionsSetLoopUnrolling(pbo, True)
-      llvm.LLVMPassBuilderOptionsSetLoopVectorization(pbo, True)
-      llvm.LLVMPassBuilderOptionsSetSLPVectorization(pbo, True)
-      llvm.LLVMPassBuilderOptionsSetVerifyEach(pbo, True)
-    else:
-      passes = b'default<O0>'
-
-    super().__init__(device, MallocAllocator, LLVMRenderer(), LLVMCompiler(target_machine, passes, pbo), CPUProgram)
-
 class LLVMCompiler(Compiler):
-  def __init__(self, target_machine, passes, pbo):
-    self.target_machine, self.passes, self.pbo = target_machine, passes, pbo
-    super().__init__(f"compile_llvm_{LLVM_VER}_jit_opt" if getenv("LLVMOPT") else f"compile_llvm_{LLVM_VER}_jit")
+  def __init__(self, target_machine, opt):
+    self.pbo = llvm.LLVMCreatePassBuilderOptions()
+    if opt:
+      self.passes = b'default<O2>'
+      llvm.LLVMPassBuilderOptionsSetLoopUnrolling(self.pbo, True)
+      llvm.LLVMPassBuilderOptionsSetLoopVectorization(self.pbo, True)
+      llvm.LLVMPassBuilderOptionsSetSLPVectorization(self.pbo, True)
+      llvm.LLVMPassBuilderOptionsSetVerifyEach(self.pbo, True)
+    else:
+      self.passes = b'default<O0>'
+    self.target_machine, self.opt = target_machine, opt
+    super().__init__(f"compile_llvm_{LLVM_VER}_jit{'_opt' if opt else ''}")
+
+  def __del__(self):
+    llvm.LLVMDisposePassBuilderOptions(self.pbo)
 
   def compile(self, src:str) -> bytes:
     src_buf = llvm.LLVMCreateMemoryBufferWithMemoryRangeCopy(ctypes.create_string_buffer(src_bytes:=src.encode()), len(src_bytes), b'src')
@@ -55,3 +45,16 @@ class LLVMCompiler(Compiler):
     return jit_loader(obj)
 
   def disassemble(self, lib:bytes): capstone_flatdump(lib)
+
+class LLVMDevice(Compiled):
+  def __init__(self, device:str):
+    for component in REQUIRED_COMPONENTS:
+      getattr(llvm, f'LLVMInitialize{HOST_ARCH}{component}')()
+
+    triple = f'{platform.machine()}-none-unknown-elf'.encode()
+    target = expect(llvm.LLVMGetTargetFromTriple(triple, ctypes.pointer(tgt:=llvm.LLVMTargetRef()), err:=cerr()), err, tgt)
+    features = b'+reserve-x18' if platform.machine() == 'arm64' else b''
+    target_machine = llvm.LLVMCreateTargetMachine(target, triple, b'', features, llvm.LLVMCodeGenLevelDefault, llvm.LLVMRelocPIC,
+                                                  llvm.LLVMCodeModelDefault)
+
+    super().__init__(device, MallocAllocator, LLVMRenderer(), LLVMCompiler(target_machine, getenv("LLVMOPT")), CPUProgram)
