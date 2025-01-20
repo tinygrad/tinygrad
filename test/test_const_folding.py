286aa814d539b3b78d3b74af3c23cc0c923ad10c
qazal <qazal.software@gmail.com>
2025-01-20 17:15:32 +0200

    remove that

--- test/test_const_folding.py
+++ test/test_const_folding.py
@@ -215,7 +215,6 @@ class TestReduceOpsConstFolding(unittest.TestCase):
 
 @unittest.skipIf(CI and Device.DEFAULT in {"GPU", "CUDA", "METAL"}, "no GPU CI")
 class TestMultiConstFolding(unittest.TestCase):
-  @unittest.expectedFailure # requires contiguous folding
   def test_multi_const_folding_literal(self):
     ds = tuple(f"{Device.DEFAULT}:{i}" for i in range(4))
     t = Tensor.arange(16).float().realize().to(ds)
