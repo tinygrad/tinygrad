af65331b76650435265438bfe418dc8b2fbbf0e8
chenyu <chenyu@fastmail.com>
2025-01-22 22:00:05 -0500

    update beam params for bert green [pr] (#8726)
    increase BEAM_UPCAST_MAX and BEAM_LOCAL_MAX to default and matched red. 3% faster step

--- examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
+++ examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/bert/implementations/tinybox_red/run_and_time.sh
@@ -5,7 +5,7 @@ export MODEL="bert"
 export SUBMISSION_PLATFORM="tinybox_red"
 export DEFAULT_FLOAT="HALF" GPUS=6 BS=66 EVAL_BS=36
 
-export BEAM=3
+export BEAM=3 BEAM_UOPS_MAX=3000 BEAM_UPCAST_MAX=256 BEAM_LOCAL_MAX=1024
 export IGNORE_JIT_FIRST_BEAM=1
 export BASEDIR="/raid/datasets/wiki"
 
