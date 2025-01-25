87394d100e247c9639e6c82978bf89ae8508410c
c143 <fritter@gmx.com>
2025-01-25 16:24:05 +0100

    fix helpers fetch for windows tests

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -242,7 +242,7 @@ def fetch(url:str, name:Optional[Union[pathlib.Path, str]]=None, subdir:Optional
           allow_caching=not getenv("DISABLE_HTTP_CACHE")) -> pathlib.Path:
   if url.startswith(("/", ".")): return pathlib.Path(url)
   if name is not None and (isinstance(name, pathlib.Path) or '/' in name): fp = pathlib.Path(name)
-  else: fp = _ensure_downloads_dir() / (subdir or "") / ((name or hashlib.md5(url.encode('utf-8')).hexdigest()) + (".gunzip" if gunzip else ""))
+  else: fp = (_ensure_downloads_dir() / (subdir if subdir is not None else "") / name or hashlib.md5(url.encode('utf-8')).hexdigest() + (".gunzip" if gunzip else ""))
   if not fp.is_file() or not allow_caching:
     (_dir := fp.parent).mkdir(parents=True, exist_ok=True)
     with urllib.request.urlopen(url, timeout=10) as r:
@@ -252,10 +252,8 @@ def fetch(url:str, name:Optional[Union[pathlib.Path, str]]=None, subdir:Optional
       progress_bar:tqdm = tqdm(total=length, unit='B', unit_scale=True, desc=f"{url}", disable=CI)
       with tempfile.NamedTemporaryFile(dir=_dir, delete=False) as f:
         while chunk := readfile.read(16384): progress_bar.update(f.write(chunk))
-        f.close()
-        pathlib.Path(f.name).rename(fp)
-      progress_bar.update(close=True)
-      if length and (file_size:=os.stat(fp).st_size) < length: raise RuntimeError(f"fetch size incomplete, {file_size} < {length}")
+      fp = pathlib.Path(f.name).replace(fp)
+      if length and (file_size:=os.path.getsize(fp)) < length: raise RuntimeError(f"fetch size incomplete: {file_size} < {length}")
   return fp
 
 # *** Exec helpers
