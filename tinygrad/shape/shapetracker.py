40ea8c894e1e5737bb681e130f7b00b0a760d595
eliotgolding <177857289+eliotgolding@users.noreply.github.com>
2025-01-26 15:10:09 +0000

    wip

--- tinygrad/shape/shapetracker.py
+++ tinygrad/shape/shapetracker.py
@@ -8,6 +8,8 @@ from tinygrad.shape.view import View, strides_for_shape, unravel
 from tinygrad.dtype import dtypes
 from tinygrad.ops import UOp, Ops, graph_rewrite, split_uop, symbolic_flat, Variable, sint, uop_given_valid, simplify_valid, sint_to_uop, Context
 from tinygrad.codegen.rewriter import sym
+import numpy as np
+import highspy
 
 def overflow(u: UOp): return u.vmax > dtypes.max(dtypes.int) or u.vmin < dtypes.min(dtypes.int)
 
