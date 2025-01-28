42482d1d8e4ef5266e68c7414ac507ff5cb865a4
George Hotz <72895+geohot@users.noreply.github.com>
2025-01-28 18:58:30 +0900

    Revert "move llvm_bf16_cast to renderer for CLANG and LLVM [pr] (#8720)"
    This reverts commit af0452f116529140eba0b53d15663019e33515f2.

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -3725,6 +3725,11 @@ class Tensor(SimpleMathTrait):
 
   # ***** cast ops *****
 
+  def llvm_bf16_cast(self, dtype:DTypeLike):
+    # hack for devices that don't support bfloat16
+    assert self.dtype == dtypes.bfloat16
+    return self.to("LLVM").cast(dtype)
+
   def cast(self, dtype:DTypeLike) -> Tensor:
     """
     Casts `self` to the given `dtype`.
