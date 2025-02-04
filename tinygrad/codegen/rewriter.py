bb5ded85ccb167151fe6ceb91ee76bd35ecc1c99
eliotgolding <177857289+eliotgolding@users.noreply.github.com>
2025-02-04 23:47:33 +0000

    Don't rewrite idiv to rshift when numerator is negative (#8885)
    * more conditions for shift rewrite mul/idiv
    
    * make ptx test uint so the new condition is true
    
    * delete idiv test
    
    * rewrite to 0 is wrong for idiv, as denominator is cast to 0 before division
    
    * mul/div by 2**(large count) is unsupported anyway

--- tinygrad/codegen/rewriter.py
+++ tinygrad/codegen/rewriter.py
@@ -3,7 +3,7 @@ from typing import Optional, Any, Callable
 import functools, itertools, operator
 from collections import defaultdict
 from tinygrad.dtype import dtypes, ImageDType, PtrDType
-from tinygrad.ops import UOp, Ops, UPat, PatternMatcher, symbolic_flat, symbolic_simple
+from tinygrad.ops import UOp, Ops, UPat, PatternMatcher, symbolic_flat, symbolic_simple, resolve
 from tinygrad.ops import graph_rewrite, split_uop, uop_given_valid, parse_valid, is_increasing, simplify_valid, GroupOp
 from tinygrad.helpers import DEBUG, getenv, flatten, dedup, TRANSCENDENTAL, AMX, prod, partition, all_same
 from tinygrad.codegen.transcendental import xexp2, xlog2, xsin, TRANSCENDENTAL_SUPPORTED_DTYPES
@@ -131,7 +131,7 @@ def get_late_rewrite_patterns(ops, force_transcendental=False):
   if Ops.SHL in ops and Ops.SHR in ops:
     pat += [
       (UPat.var("x", dtypes.ints)*UPat.cvar("c"), lambda c,x: x << powers_of_two[c.arg] if c.arg in powers_of_two else None),
-      (UPat.var("x", dtypes.ints)//UPat.cvar("c"), lambda x,c: x >> powers_of_two[c.arg] if c.arg in powers_of_two else None)
+      (UPat.var("x", dtypes.ints)//UPat.cvar("c"), lambda x,c: x >> powers_of_two[c.arg] if c.arg in powers_of_two and resolve(x>=0,False) else None)
     ]
   if Ops.NEG in ops:
     pat += [(UPat.var('x')*-1, lambda x: x.alu(Ops.NEG))]
