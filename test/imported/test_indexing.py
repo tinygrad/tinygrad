4f432a3351970c4631c962a36728f23ffdf66aed
Chen-Yu Yang <chenyu@fastmail.com>
2025-02-05 21:00:04 -0500

    one more

--- test/imported/test_indexing.py
+++ test/imported/test_indexing.py
@@ -1048,6 +1048,7 @@ class TestIndexing(unittest.TestCase):
     a = src[0].mul(src[1])
     self.assertEqual(a[0,1].item(), 2)
 
+  @unittest.skipUnless(is_dtype_supported(dtypes.int64), "need dtypes.int64")
   def test_getitem_scalars(self):
     zero = Tensor(0, dtype=dtypes.int64)
     one = Tensor(1, dtype=dtypes.int64)
