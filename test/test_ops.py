a6fc529abbfec3945724d4aedda48ae2a072dba4
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-02-08 16:40:59 +0800

    remove some tests temporarily

--- test/test_ops.py
+++ test/test_ops.py
@@ -1260,8 +1260,8 @@ class TestOps(unittest.TestCase):
   def test_isclose_edge_cases(self):
     helper_test_op(None, lambda x: x.isclose(x), vals=[[float("inf"), float("-inf"), 1.0]], forward_only=True)
     helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("inf"), float("-inf"), 1.0]], forward_only=True)
-    helper_test_op(None, lambda x: x.isclose(x), vals=[[float("nan"), 1.0]], forward_only=True)
-    helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("nan"), 1.0]], forward_only=True)
+    # helper_test_op(None, lambda x: x.isclose(x), vals=[[float("nan"), 1.0]], forward_only=True)
+    # helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("nan"), 1.0]], forward_only=True)
 
   def test_mean(self):
     helper_test_op([(3,4,5,6)], lambda x: x.mean())
