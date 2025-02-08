6bcd2e5fb5577041977a1f3ac608a8320519b8f9
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-02-08 17:10:41 +0800

    re-enable other test

--- test/test_ops.py
+++ test/test_ops.py
@@ -1261,7 +1261,7 @@ class TestOps(unittest.TestCase):
     helper_test_op(None, lambda x: x.isclose(x), vals=[[float("inf"), float("-inf"), 1.0]], forward_only=True)
     helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("inf"), float("-inf"), 1.0]], forward_only=True)
     helper_test_op(None, lambda x: x.isclose(x), vals=[[float("nan"), 1.0]], forward_only=True)
-    # helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("nan"), 1.0]], forward_only=True)
+    helper_test_op(None, lambda x: x.isclose(x, equal_nan=True), vals=[[float("nan"), 1.0]], forward_only=True)
 
   def test_mean(self):
     helper_test_op([(3,4,5,6)], lambda x: x.mean())
