3f6ff67f41d5bc4232627488853a057d05f75541
Rounak Singh <poppz@poppzs-MacBook-Pro.local>
2025-02-10 09:26:06 +0530

    [bounty][refactor] make tensor inherit from MathTrait instead of SimpleMathTrait

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -17,79 +17,142 @@ class FastEnum(IntEnum):
   @staticmethod
   def _generate_next_value_(_, __, ___, last_values): return 1 + max([0, *last_values, *[max(c) for c in FastEnum.__subclasses__()]])
 
-class SimpleMathTrait:
-  # required to implement
-  def alu(self:T, arg:Ops, *src) -> T: raise NotImplementedError
-  def const_like(self:T, b:ConstLike) -> T: raise NotImplementedError
+class MathTrait:
+  # Required methods that must be implemented by subclasses
+  def alu(self: "MathTrait", arg: Ops, *src) -> "MathTrait":
+    raise NotImplementedError
 
-  # great functions you get!
-  def ufix(self, x): return self.const_like(x) if not isinstance(x, MathTrait) else x
-  def _binop(self, op, x, reverse): return self.ufix(x).alu(op, self) if reverse else self.alu(op, self.ufix(x))
-  def logical_not(self): return self.ne(True)
+  def const_like(self: "MathTrait", b: ConstLike) -> "MathTrait":
+    raise NotImplementedError
+
+  # Basic operations inherited from SimpleMathTrait
+
+  def ufix(self, x):
+    # Wrap x using const_like if x is not already a MathTrait instance.
+    return self.const_like(x) if not isinstance(x, MathTrait) else x
+
+  def _binop(self, op, x, reverse):
+    return self.ufix(x).alu(op, self) if reverse else self.alu(op, self.ufix(x))
+
+  def logical_not(self):
+    return self.ne(True)
+
   def neg(self):
-    if (dtype:=getattr(self, 'dtype')) is None: raise TypeError(f"MathTraits __neg__ requires a dtype, {self=}")
-    return self.logical_not() if dtype.scalar() == dtypes.bool else self*(-1)
-  def add(self, x, reverse=False): return self._binop(Ops.ADD, x, reverse)
-  def mul(self, x, reverse=False): return self._binop(Ops.MUL, x, reverse)
-  def bitwise_and(self, x, reverse=False): return self._binop(Ops.AND, x, reverse)
-  def bitwise_or(self, x, reverse=False): return self._binop(Ops.OR, x, reverse)
-  def xor(self, x, reverse=False): return self._binop(Ops.XOR, x, reverse)
-  def idiv(self, x, reverse=False): return self._binop(Ops.IDIV, x, reverse)
-  def mod(self, x, reverse=False): return self._binop(Ops.MOD, x, reverse)
-  def sub(self, x, reverse=False): return self.ufix(x).alu(Ops.ADD, -self) if reverse else self.alu(Ops.ADD, self.ufix(-x))
-  def div(self, x, reverse=False): return (self.ufix(x)*self.alu(Ops.RECIP)) if reverse else (self*self.ufix(x).alu(Ops.RECIP))
+    if (dtype := getattr(self, 'dtype', None)) is None:
+      raise TypeError(f"MathTrait __neg__ requires a dtype, {self=}")
+    return self.logical_not() if dtype.scalar() == dtypes.bool else self * (-1)
 
-  def __neg__(self): return self.neg()
+  def add(self, x, reverse=False):
+    return self._binop(Ops.ADD, x, reverse)
+  def mul(self, x, reverse=False):
+    return self._binop(Ops.MUL, x, reverse)
+  def bitwise_and(self, x, reverse=False):
+    return self._binop(Ops.AND, x, reverse)
+  def bitwise_or(self, x, reverse=False):
+    return self._binop(Ops.OR, x, reverse)
+  def xor(self, x, reverse=False):
+    return self._binop(Ops.XOR, x, reverse)
+  def idiv(self, x, reverse=False):
+    return self._binop(Ops.IDIV, x, reverse)
+  def mod(self, x, reverse=False):
+    return self._binop(Ops.MOD, x, reverse)
+  def sub(self, x, reverse=False):
+    return self.ufix(x).alu(Ops.ADD, -self) if reverse else self.alu(Ops.ADD, self.ufix(-x))
+  def div(self, x, reverse=False):
+    return (self.ufix(x) * self.alu(Ops.RECIP)) if reverse else (self * self.ufix(x).alu(Ops.RECIP))
 
-  def __add__(self, x): return self.add(x)
-  def __sub__(self, x): return self.sub(x)
-  def __mul__(self, x): return self.mul(x)
-  def __truediv__(self, x): return self.div(x)
-  def __floordiv__(self, x): return self.idiv(x)  # TODO: idiv is trunc div, not floordiv
-  def __mod__(self, x): return self.mod(x)
-  def __and__(self, x): return self.bitwise_and(x)
-  def __or__(self, x): return self.bitwise_or(x)
-  def __xor__(self, x): return self.xor(x)
+  def __neg__(self):
+    return self.neg()
+  def __add__(self, x):
+    return self.add(x)
+  def __sub__(self, x):
+    return self.sub(x)
+  def __mul__(self, x):
+    return self.mul(x)
+  def __truediv__(self, x):
+    return self.div(x)
+  def __floordiv__(self, x):
+    # Note: idiv currently implements truncation division, not floor division.
+    return self.idiv(x)
+  def __mod__(self, x):
+    return self.mod(x)
+  def __and__(self, x):
+    return self.bitwise_and(x)
+  def __or__(self, x):
+    return self.bitwise_or(x)
+  def __xor__(self, x):
+    return self.xor(x)
+  def __radd__(self, x):
+    return self.add(x, True)
+  def __rsub__(self, x):
+    return self.sub(x, True)
+  def __rmul__(self, x):
+    return self.mul(x, True)
+  def __rtruediv__(self, x):
+    return self.div(x, True)
+  def __rfloordiv__(self, x):
+    return self.idiv(x, True)
+  def __rand__(self, x):
+    return self.bitwise_and(x, True)
+  def __ror__(self, x):
+    return self.bitwise_or(x, True)
+  def __rxor__(self, x):
+    return self.xor(x, True)
+  def __rmod__(self, x):
+    return self.mod(x, True)
+  def __lt__(self, x):
+    return self.alu(Ops.CMPLT, self.ufix(x))
+  def __gt__(self, x):
+    return self.ufix(x).alu(Ops.CMPLT, self)
+  def __ge__(self, x):
+    return (self < x).logical_not()
+  def __le__(self, x):
+    return (self > x).logical_not()
 
-  def __radd__(self, x): return self.add(x, True)
-  def __rsub__(self, x): return self.sub(x, True)
-  def __rmul__(self, x): return self.mul(x, True)
-  def __rtruediv__(self, x): return self.div(x, True)
-  def __rfloordiv__(self, x): return self.idiv(x, True)
-  def __rand__(self, x): return self.bitwise_and(x, True)
-  def __ror__(self, x): return self.bitwise_or(x, True)
-  def __rxor__(self, x): return self.xor(x, True)
-  def __rmod__(self, x): return self.mod(x, True)
+  def ne(self, x):
+    return self.alu(Ops.CMPNE, self.ufix(x))
+  def eq(self, x):
+    return self.ne(x).logical_not()
 
-  def __lt__(self, x): return self.alu(Ops.CMPLT, self.ufix(x))
-  def __gt__(self, x): return self.ufix(x).alu(Ops.CMPLT, self)
-  def __ge__(self, x): return (self < x).logical_not()
-  def __le__(self, x): return (self > x).logical_not()
+  def __ne__(self, x):
+    return self.ne(x)
 
-  def ne(self, x): return self.alu(Ops.CMPNE, self.ufix(x))
-  def eq(self, x): return self.ne(x).logical_not()
-  def __ne__(self, x): return self.ne(x)
-  # NOTE: __eq__ isn't overridden, and means the same thing as is by default
+  # Additional operations from the original MathTrait
 
-class MathTrait(SimpleMathTrait):
-  # TODO: move to Tensor when new backward is done
-  def lshift(self, x, reverse=False): return self._binop(Ops.SHL, x, reverse)
-  def rshift(self, x, reverse=False): return self._binop(Ops.SHR, x, reverse)
-  def __lshift__(self, x): return self.lshift(x)
-  def __rshift__(self, x): return self.rshift(x)
-  def __rlshift__(self, x): return self.lshift(x, True)
-  def __rrshift__(self, x): return self.rshift(x, True)
+  def lshift(self, x, reverse=False):
+    return self._binop(Ops.SHL, x, reverse)
+  def rshift(self, x, reverse=False):
+    return self._binop(Ops.SHR, x, reverse)
 
-  def maximum(self, x): return self.alu(Ops.MAX, self.ufix(x))
-  def minimum(self, x): return -(-self).maximum(-x)
-  def where(self, x, y): return self.alu(Ops.WHERE, x, x.ufix(y))
-  def threefry(self, seed): return self.alu(Ops.THREEFRY, seed)
-  def reciprocal(self): return self.alu(Ops.RECIP)
-  def sqrt(self): return self.alu(Ops.SQRT)
-  def sin(self): return self.alu(Ops.SIN)
-  def log2(self): return self.alu(Ops.LOG2)
-  def exp2(self): return self.alu(Ops.EXP2)
-  def pow(self, x): return self.alu(Ops.POW, self.ufix(x))
+  def __lshift__(self, x):
+    return self.lshift(x)
+  def __rshift__(self, x):
+    return self.rshift(x)
+  def __rlshift__(self, x):
+    return self.lshift(x, True)
+  def __rrshift__(self, x):
+    return self.rshift(x, True)
+
+  def maximum(self, x):
+    return self.alu(Ops.MAX, self.ufix(x))
+  def minimum(self, x):
+    return -(-self).maximum(-x)
+  def where(self, x, y):
+    return self.alu(Ops.WHERE, x, x.ufix(y))
+  def threefry(self, seed):
+    return self.alu(Ops.THREEFRY, seed)
+  def reciprocal(self):
+    return self.alu(Ops.RECIP)
+  def sqrt(self):
+    return self.alu(Ops.SQRT)
+  def sin(self):
+    return self.alu(Ops.SIN)
+  def log2(self):
+    return self.alu(Ops.LOG2)
+  def exp2(self):
+    return self.alu(Ops.EXP2)
+  def pow(self, x):
+    return self.alu(Ops.POW, self.ufix(x))
 
 # the order of these Ops controls the order of the toposort
 class Ops(FastEnum):
