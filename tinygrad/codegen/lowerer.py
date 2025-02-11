e3e67cf06434afa21cd5e2544107256a33cbe915
George Hotz <geohot@gmail.com>
2025-02-11 18:01:33 +0800

    handle the other case

--- tinygrad/codegen/lowerer.py
+++ tinygrad/codegen/lowerer.py
@@ -101,6 +101,8 @@ def lower_reduce_axis(ctx: IndexContext, x: UOp):
   if len(contract_axis:=flatten(x.arg for x in reduce_expand)):
     ret = UOp(Ops.CONTRACT, x.dtype.vec(prod(x[1] for x in contract_axis)), (ret,), tuple(contract_axis))
     ret = functools.reduce(lambda x,y: x.alu(alu_op, y), [acc]+[ret.gep(i) for i in range(ret.dtype.count)])
+  else:
+    ret = acc.alu(alu_op, ret)
   if not len(reduce_range): return ret
   # create ACC and assign
   return acc.assign(ret)
