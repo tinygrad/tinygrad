40d538b16be78f734aafb1c148f7ce73a4c9315b
Marcello <marcellofuschi1@gmail.com>
2025-02-13 04:35:01 +0100

    Refactor

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -1911,19 +1911,20 @@ class Tensor(SimpleMathTrait):
     print(t.logcumsumexp(axis=1).numpy())
     ```
     """
+    axis = self._resolve_dim(axis)
     x = self.unsqueeze(0) if self.ndim == 0 else self
     if axis not in (-1, x.ndim - 1):
       x = x.transpose(axis, -1)
     last_dim_size = x.shape[-1]
     x_reshaped = x.reshape(-1, last_dim_size)
-    x_cummax = x_reshaped.cummax(-1).reshape(*x_reshaped.shape, 1)
+    x_cummax = x_reshaped.cummax(-1).unsqueeze(-1)
     x_expand = x_reshaped.unsqueeze(1).expand(*x_reshaped.shape, last_dim_size)
     mask = Tensor.ones(last_dim_size, last_dim_size).tril().unsqueeze(0)
-    ret = ((x_expand - x_cummax).exp() * mask).sum(-1).log() + x_cummax.reshape(*x_reshaped.shape)
+    ret = ((x_expand - x_cummax).exp() * mask).sum(-1).log() + x_cummax.squeeze(-1)
     ret = ret.reshape(*x.shape)
     if axis not in (-1, x.ndim - 1):
       ret = ret.transpose(-1, axis)
-    return ret if self.ndim != 0 else ret.squeeze(0)
+    return ret if self.ndim != 0 else ret.squeeze()
 
   def argmax(self, axis=None, keepdim=False):
     """
