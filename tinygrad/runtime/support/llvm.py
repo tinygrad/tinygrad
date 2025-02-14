4dcfece5cdf6676583be148be26081c37e124fff
b1tg <b1tg@users.noreply.github.com>
2025-02-13 18:54:14 -0800

    ci llvm trunk

--- tinygrad/runtime/support/llvm.py
+++ tinygrad/runtime/support/llvm.py
@@ -16,7 +16,8 @@ elif OSX:
   LLVM_PATH: str|None = os.path.join(brew_prefix, 'lib', 'libLLVM.dylib')
 else:
   LLVM_PATH = ctypes.util.find_library('LLVM')
-  for ver in range(17, 19+1):
+  # use newer LLVM if possible
+  for ver in reversed(range(14, 19+1)):
     if LLVM_PATH is not None: break
     LLVM_PATH = ctypes.util.find_library(f'LLVM-{ver}')
   if LLVM_PATH is None:
