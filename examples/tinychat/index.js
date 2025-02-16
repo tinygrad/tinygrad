442586420bf3a8e406879170143eb5128a0d01b3
hooved <172129504+hooved@users.noreply.github.com>
2025-02-16 14:44:46 -0500

    make loading messages more concise

--- examples/tinychat/index.js
+++ examples/tinychat/index.js
@@ -27,11 +27,11 @@ async function getDevice() {
   try {
     adapter = await navigator.gpu.requestAdapter();
     if (!adapter) {
-      this.loadingMessage = "Loading WASM model (enable WebGPU for more speed):";
+      this.loadingMessage = "Loading WASM (WebGPU not enabled):";
       throw new Error("No WebGPU adapter found");
     }
   } catch(error) {
-    this.loadingMessage = "Loading WASM model (enable WebGPU for more speed):";
+      this.loadingMessage = "Loading WASM (WebGPU not enabled):";
     throw error;
   }
   const requiredLimits = {};
@@ -42,7 +42,7 @@ async function getDevice() {
   try {
     return await adapter.requestDevice({ requiredLimits });
   } catch(error) {
-    this.loadingMessage = "Loading WASM model (WebGPU launch failed):";
+    this.loadingMessage = "Loading WASM (WebGPU error):";
     throw error;
   }
 };
@@ -676,7 +676,7 @@ document.addEventListener("alpine:init", () => {
       this.lastSeenToks = prefillToks.slice(0, startPos);
 
       this.progress = makeProgress(unprocessedPrefillToks.length);
-      this.loadingMessage = `Reading input:`;
+      this.loadingMessage = (window.BACKEND === "WebGPU") ? "Reading input:" : "Loading (enable WebGPU for speed):";
       this.progress(0, this.loadingMessage);
       for (const tok of unprocessedPrefillToks) {
         if (this.cancelGeneration) {this.loadingMessage=""; return;}
