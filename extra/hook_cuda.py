d32ee9a14e9d4d9ee1b830ab89d2bf00bec0901e
George Hotz <geohot@gmail.com>
2025-02-19 22:27:37 -0800

    progress

--- extra/hook_cuda.py
+++ extra/hook_cuda.py
@@ -1,7 +1,17 @@
 import ctypes, struct, platform, pathlib, os
 from hexdump import hexdump
 from tinygrad.helpers import to_mv
-from tinygrad.runtime.autogen import libc
+from tinygrad.runtime.autogen import libc, cuda
+from tinygrad.device import CPUProgram
+
+def _hook(fxn_address_value, tramp):
+  page_address = (fxn_address_value//0x1000)*0x1000
+  ret = libc.mprotect(page_address, 0x2000, 7)
+  assert ret == 0
+  libc.memcpy(fxn_address_value, tramp, len(tramp))
+  ret = libc.mprotect(page_address, 0x2000, 5)
+  assert ret == 0
+  CPUProgram.rt_lib["__clear_cache"](fxn_address_value, fxn_address_value + len(tramp))
 
 def install_hook(c_function, python_function):
   python_function_addr = ctypes.cast(ctypes.byref(python_function), ctypes.POINTER(ctypes.c_ulong)).contents.value
@@ -18,46 +28,62 @@ def install_hook(c_function, python_function):
     tramp = b"\x49\xBB" + struct.pack("Q", python_function_addr) + b"\x41\xFF\xE3"
   else:
     raise Exception(f"processor {processor} not supported")
+  tramp = ctypes.create_string_buffer(tramp)
 
   # get real function address
   fxn_address = ctypes.cast(ctypes.byref(c_function), ctypes.POINTER(ctypes.c_ulong))
-  page_address = (fxn_address.contents.value//0x1000)*0x1000
-  print(f"** hooking function at 0x{fxn_address.contents.value:X}")
+  fxn_address_value = fxn_address.contents.value
+  #print(f"** hooking function at 0x{fxn_address_value}")
 
-  # hook function
-  ret = libc.mprotect(page_address, 0x2000, 7)
-  assert ret == 0
-  libc.memcpy(fxn_address.contents.value, ctypes.create_string_buffer(tramp), len(tramp))
-  ret = libc.mprotect(page_address, 0x2000, 5)
-  assert ret == 0
+  orig_save = (ctypes.c_char*len(tramp))()
+  libc.memcpy(orig_save, fxn_address_value, len(tramp))
+  _hook(fxn_address_value, tramp)
 
-@ctypes.CFUNCTYPE(ctypes.c_uint)
-def hook(flags):
-  print("called cuInit")
-  return 0
+  def original(*args):
+    _hook(fxn_address_value, orig_save)
+    ret = c_function(*args)
+    _hook(fxn_address_value, tramp)
+    return ret
+  return original
+
+hooked = {}
+
+function_names = {}
+
+@ctypes.CFUNCTYPE(*([cuda.cuModuleGetFunction.restype] + cuda.cuModuleGetFunction.argtypes))
+def cuModuleGetFunction(hfunc, hmod, name):
+  ret = hooked["cuModuleGetFunction"](hfunc, hmod, name)
+  python_name = ctypes.string_at(name).decode()
+  print(f"called cuModuleGetFunction {python_name}")
+  function_names[ctypes.addressof(hfunc.contents.contents)] = python_name
+  return ret
+
+@ctypes.CFUNCTYPE(*([cuda.cuMemAlloc_v2.restype] + cuda.cuMemAlloc_v2.argtypes))
+def cuMemAlloc_v2(dptr, bytesize):
+  print(f"called cuMemAlloc_v2 {bytesize}")
+  return hooked["cuMemAlloc_v2"](dptr, bytesize)
+
+@ctypes.CFUNCTYPE(*([cuda.cuLaunchKernel.restype] + cuda.cuLaunchKernel.argtypes))
+def cuLaunchKernel(f, gridDimX, gridDimY, gridDimZ, blockDimX, blockDimY, blockDimZ, sharedMemBytes, hStream, kernelParams, extra):
+  name = function_names[ctypes.addressof(f.contents)]
+  print(f"called cuLaunchKernel {name} <<{gridDimX}, {gridDimY}, {gridDimZ}>> <<{blockDimX}, {blockDimY}, {blockDimZ}>>")
+  return hooked["cuLaunchKernel"](f, gridDimX, gridDimY, gridDimZ, blockDimX, blockDimY, blockDimZ, sharedMemBytes, hStream, kernelParams, extra)
 
 if __name__ == "__main__":
-  cuda = ctypes.CDLL("/usr/lib/x86_64-linux-gnu/libcuda.so.550.90.07")
-  #install_hook(cuda.cuInit, hook)
-  install_hook(cuda.cuLaunchKernel, hook)
-  #install_hook(cuda.cuLaunchKernel_ptsz, hook)
-  #install_hook(cuda.cuLaunchKernelEx, hook)
-  #install_hook(cuda.cuLaunchKernelEx_ptsz, hook)
+  hooked['cuModuleGetFunction'] = install_hook(cuda.cuModuleGetFunction, cuModuleGetFunction)
+  hooked['cuMemAlloc_v2'] = install_hook(cuda.cuMemAlloc_v2, cuMemAlloc_v2)
+  hooked['cuLaunchKernel'] = install_hook(cuda.cuLaunchKernel, cuLaunchKernel)
 
   print("importing torch...")
   import torch
-  print("torch", torch.__version__, torch.__file__)
 
-  #from extra.nv_gpu_driver import nv_ioctl
-
-  maps = pathlib.Path("/proc/self/maps").read_text()
-  print('\n'.join([x for x in maps.split("\n") if 'libcuda' in x]))
-
+  # confirm cuda library is right
+  #print("torch", torch.__version__, torch.__file__)
+  #maps = pathlib.Path("/proc/self/maps").read_text()
+  #print('\n'.join([x for x in maps.split("\n") if 'libcuda' in x]))
 
   a = torch.zeros(10).cuda()
   print("tensor created")
   a += 1
   print("tensor math done", a.cpu().numpy())
-
-  install_hook(cuda.cuLaunchKernel, hook)
 
