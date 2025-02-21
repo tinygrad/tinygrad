f81e0f69a1598cf71d5ebe89739f896e687fcd57
George Hotz <geohot@gmail.com>
2025-02-21 09:07:41 +0800

    progress

--- extra/torch_backend.py
+++ extra/torch_backend.py
@@ -1,25 +1,57 @@
+from typing import cast
 from tinygrad import Tensor
+import traceback
 import torch
 
+def fallback_kernel(op, *args, **kwargs):
+  print("FALLBACK", op)
+
 # register device
-aten = torch._ops.ops.aten
 lib = torch.library.Library("aten", "IMPL", "PrivateUse1")
-def impl(x):
-  def _wrap(y): lib.impl(x, y)
-  return _wrap
-torch.utils.rename_privateuse1_backend("tiny")
+#lib.fallback(fallback_kernel)
+#torch.utils.rename_privateuse1_backend("tiny")
 torch._register_device_module('privateuseone', lib)
 
-
 # implement functions
-@impl(aten.empty.memory_format)
-def empty_memory_format(size, dtype=None, layout=None, device=None, pin_memory=False, memory_format=None):
-  print("Custom PrivateUse1 empty called!")
-  print(size)
-  return torch.empty(size, dtype=dtype, layout=layout, device="meta")
-  #return Tensor.empty(*size)
+#@torch.library.impl("aten::fill_.Scalar", "PrivateUse1")
+#def fill_scalar():
+#  print("fill scalar")
 
-a = torch.ones(4, 4, device='tiny:0')
-b = torch.ones(4, 4, device='tiny:0')
+class PrivateUseOneTensor(torch.Tensor):
+  @property
+  def device(self): return 'privateuseone:0'
+
+@torch.library.impl("aten::add.Tensor", "privateuseone")
+def add(x, y):
+  print("HERE")
+
+"""
+class TinyGradDLpackWrapper:
+  def __init__(self, tinygrad_tensor):
+    self.tg_t = tinygrad_tensor
+
+  def __dlpack_device__(self):
+    return (12, 0)
+
+  def __dlpack__(self, stream=None):
+    print("DLPACK")
+    return None
+
+"""
+@torch.library.impl("aten::empty.memory_format", "privateuseone")
+def empty_memory_format(size, dtype=None, layout=None, device=None, pin_memory=False, memory_format=None):
+  print("Custom PrivateUse1 empty called!", size, dtype, layout, device, pin_memory, memory_format)
+  # TODO: return Tensor with device privateuseone:0
+  #ret = Tensor.empty(*size)
+  #return torch.from_dlpack(TinyGradDLpackWrapper(ret))
+  #t = torch.empty(size, dtype=dtype, layout=layout, device="PrivateUse1")
+  #t = t.as_subclass(PrivateUseOneTensor)
+  #print(type(t), t.device, device)
+  #assert t.device == device, "THIS FAILS"
+  #return t
+
+
+a = torch.ones(4, 4, device='privateuseone')
+b = torch.ones(4, 4, device='privateuseone')
 c = a+b
 print(c.cpu().numpy())
