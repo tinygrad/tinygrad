8aff17010b894f66b4362549f1cb646c2e8b413b
Damoon <damoon.shah3@gmail.com>
2025-02-23 17:07:44 +0300

    invert is problematic

--- extra/torch_backend/backend2.py
+++ extra/torch_backend/backend2.py
@@ -1,6 +1,7 @@
 from tinygrad import Tensor, dtypes
 import torch, contextlib
 from torch.utils._python_dispatch import TorchDispatchMode
+import numpy as np
 
 torch_to_tiny_dtype = {
   torch.float32: dtypes.float32,
@@ -15,26 +16,45 @@ def empty_memory_format(size, dtype=None, layout=None, device=None, pin_memory=F
   return TTensor(Tensor.empty(*size))
 
 def to_copy_impl(input, dtype=None, device=None, layout=None, non_blocking=False, **kwargs):
-    input_data = input.tiny.numpy()  
-    if dtype is not None: return TTensor(Tensor(input_data, dtype=torch_to_tiny_dtype[dtype]))
-    return TTensor(Tensor(input_data))
+  input_data = input.tiny.numpy()  
+  if dtype is not None: return TTensor(Tensor(input_data, dtype=torch_to_tiny_dtype[dtype]))
+  return TTensor(Tensor(input_data))
 
 def detach_impl(input):
-    input_data = input.tiny.numpy()
-    new_tensor = TTensor(Tensor(input_data))
-    new_tensor.requires_grad = False 
-    return new_tensor
+  input_data = input.tiny.numpy()
+  new_tensor = TTensor(Tensor(input_data))
+  new_tensor.requires_grad = False 
+  return new_tensor
 
 def uniform_impl(input, from_=0.0, to=1.0):
-    random_values = torch.rand(input.shape, dtype=input.dtype, device=input.device)
-    uniform_values = from_ + (to - from_) * random_values
-    input.copy_(uniform_values) 
-    return input  
+  random_values = torch.rand(input.shape, dtype=input.dtype, device=input.device)
+  uniform_values = from_ + (to - from_) * random_values
+  input.copy_(uniform_values) 
+  return input  
 
 def copy_impl(self, src):
-    src_data = src.numpy()  
-    self.tiny.numpy()[:] = src_data  
-    return self 
+  src_data = src.numpy()  
+  self.tiny.numpy()[:] = src_data  
+  return self 
+
+def zeros_impl(size, dtype=None, layout=None, device=None, pin_memory=False):
+  if dtype is None: return TTensor(Tensor.zeros(size), dtype=torch.float32)
+  return TTensor(Tensor.zeros(size), dtype=dtype)
+
+def ones_impl(size, dtype=None, layout=None, device=None, pin_memory=False):
+  if dtype is None: return TTensor(Tensor.ones(size), dtype=torch.float32)
+  return TTensor(Tensor.ones(size), dtype=dtype)
+
+def zero_impl(input):
+  if input.dim() == 0:
+    input.tiny.numpy()[()] = 0  
+  else:
+    input.tiny.numpy()[:] = 0
+  return TTensor(input)
+
+def fill_scalar_impl(input, value):
+  input.tiny.numpy()[:] = value
+  return TTensor(input)
 
 tiny_backend = {
   "aten.empty.memory_format": empty_memory_format,
@@ -42,7 +62,7 @@ tiny_backend = {
   "aten.abs.default": lambda x: TTensor(x.tiny.abs()),
   "aten.eq.Tensor": lambda x,y: TTensor(x.tiny == y.tiny),
   "aten.bitwise_and.Tensor": lambda x,y: TTensor(x.tiny & y.tiny),
-  "aten.ne.Scalar": lambda x,y: TTensor(x.tiny != y),
+  "aten.ne.Scalar": lambda x,y: TTenso1r(x.tiny != y),
   "aten.mul.Tensor": lambda x,y: TTensor(x.tiny * y.tiny),
   "aten.masked_select.default": lambda x,y: TTensor(Tensor(x.tiny.numpy()[y.tiny.numpy()])),
   "aten.lift_fresh.default": lambda x: TTensor(x),
@@ -50,6 +70,15 @@ tiny_backend = {
   "aten.detach.default": detach_impl,
   "aten.uniform_.default": uniform_impl,
   "aten.copy_.default": copy_impl,
+  "aten.zeros.default": zeros_impl,
+  "aten.ones.default": ones_impl,
+  "aten.zero_.default": zero_impl,
+  "aten.fill_.Scalar": fill_scalar_impl,
+  "aten.neg.default": lambda x: TTensor(-x.tiny),
+  "aten._local_scalar_dense.default": lambda x: TTensor(x.tiny.numpy(), dtype=x.dtype).item(),
+  "aten.item.default": lambda x: x.tiny.item() if isinstance(x.tiny, (np.ndarray)) else TTensor(x.tiny.numpy(), dtype=x.dtype).item(),
+  "aten.invert": lambda x: TTensor(~x.tiny) if x.tiny.dtype.is_int() or x.tiny.dtype == dtypes.bool else RuntimeError('Only integers and booleans are supported'),
+  # "aten.bitwise_not.default": lambda x: TTensor(~x.tiny) if x.tiny.dtype.is_int() or x.tiny.dtype == dtypes.bool else raise(),
 }
 
 class TTensor(torch.Tensor):
