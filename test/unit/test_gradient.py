91356230b1b5d522c67b637ae2fca320ae7fa9ed
qazal <qazal.software@gmail.com>
2025-02-24 11:50:08 +0100

    lint

--- test/unit/test_gradient.py
+++ test/unit/test_gradient.py
@@ -3,7 +3,7 @@ import unittest, math
 import torch
 from tinygrad import Tensor
 from tinygrad.dtype import dtypes
-from tinygrad.ops import UOp, Ops
+from tinygrad.ops import UOp
 from tinygrad.gradient import compute_gradient
 
 class TestGradient(unittest.TestCase):
