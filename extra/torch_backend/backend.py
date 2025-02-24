40b698733d614d2a0a3c155c7deea339c8578bfc
Damoon <damoon.shah3@gmail.com>
2025-02-24 20:18:27 +0300

    identically named functions are added in least amount of code

--- extra/torch_backend/backend.py
+++ extra/torch_backend/backend.py
@@ -36,21 +36,60 @@ def masked_select(self, mask):
   # err, bad
   return wrap(Tensor(self.cpu().numpy()[mask.cpu().numpy()]))
 
+# @torch.library.impl("aten::as_strided", "privateuseone")
+# def as_strided(tensor:torch.Tensor, size, stride, storage_offset=None):
+#   storage_offset_int = storage_offset if storage_offset is not None else tensor.storage_offset()
+
+#   #return tensor.cpu().as_strided(size, stride).tiny()
+#   if TORCH_DEBUG >= 1: print("** NOTE: this as_strided is wrong", tensor.shape, size, stride, storage_offset)
+
+#   if tuple(x for x in tensor.shape if x != 1) == tuple(x for x in size if x != 1):
+#     # this is squeeze/unsqueeze
+#     return tensor.reshape(size)
+
+#   # TODO: how do i know this is permute?
+#   if len(size) == 2 and size == [tensor.shape[1], tensor.shape[0]] and stride == [0, 1]: return wrap(unwrap(tensor).permute(1,0))
+
+#   #print(tensor.cpu().numpy())
+#   raise NotImplementedError("fix as_strided")
+
 @torch.library.impl("aten::as_strided", "privateuseone")
-def as_strided(tensor:torch.Tensor, size, stride, storage_offset=None):
-  #return tensor.cpu().as_strided(size, stride).tiny()
+def as_strided(tensor, size, stride, storage_offset=None):
   if TORCH_DEBUG >= 1: print("** NOTE: this as_strided is wrong", tensor.shape, size, stride, storage_offset)
+  tg_tensor = unwrap(tensor)
+  if len(size) != len(stride): raise ValueError
+  storage_offset = storage_offset if storage_offset is not None else tensor.storage_offset()
+  tg_tensor.realize()
+  # print(size, stride, tg_tensor.shape)
+  # TODO: May need to use lazydata.buffer to generate a new tensor, Stride needed here
+  if size != []: tg_tensor = tg_tensor.reshape(size)
+  if storage_offset: tg_tensor = tg_tensor[storage_offset:]
+  return wrap(tg_tensor)
 
-  if tuple(x for x in tensor.shape if x != 1) == tuple(x for x in size if x != 1):
-    # this is squeeze/unsqueeze
-    return tensor.reshape(size)
+@torch.library.impl("aten::nll_loss_forward", "privateuseone")
+def nll_loss_forward_output(self, target, weight=None, reduction=1, ignore_index=-100):
+  if TORCH_DEBUG >= 1: print(f"nll_loss_forward: {self.shape=}, {target.shape=}, {weight=}, {reduction=}, {ignore_index=}")
+  input_tg = unwrap(self)
+  target_tg = unwrap(target) if isinstance(target, torch.Tensor) else target
+  weight_tg = unwrap(weight) if weight is not None else None
+  reduction_str = {0: "none", 1: "mean", 2: "sum"}.get(reduction, "mean")
+  loss = input_tg.nll_loss(target_tg, weight=weight_tg, ignore_index=ignore_index, reduction=reduction_str)
+  total_weight = Tensor.ones(1) if weight_tg is None else weight_tg.sum()
+  return wrap(loss), wrap(total_weight)
 
-  # TODO: how do i know this is permute?
-  if tensor.shape == (1000, 512) and size == [512, 1000] and stride == [0, 1]:
-    return wrap(unwrap(tensor).permute(1,0))
-
-  #print(tensor.cpu().numpy())
-  raise NotImplementedError("fix as_strided")
+@torch.library.impl("aten::nll_loss_backward.grad_input", "privateuseone")
+def nll_loss_backward_grad_input(grad_output, self, target, weight=None, reduction=1, ignore_index=-100, total_weight=None, *, grad_input):
+  if TORCH_DEBUG >= 1: print(f"nll_loss_backward: {grad_output.shape=}, {self.shape=}, {target.shape=}, {weight=}, {reduction=}, {ignore_index=}")
+  grad_output_tg = unwrap(grad_output)
+  input_tg = unwrap(self).requires_grad_(True)
+  target_tg = unwrap(target) if isinstance(target, torch.Tensor) else target
+  weight_tg = unwrap(weight) if weight is not None else None
+  total_weight_tg = unwrap(total_weight) if total_weight is not None else Tensor.ones(1)
+  reduction_str = {0: "none", 1: "mean", 2: "sum"}.get(reduction, "mean")
+  loss = input_tg.nll_loss(target_tg, weight=weight_tg, ignore_index=ignore_index, reduction=reduction_str)
+  grad = input_tg.backward(grad_output_tg if reduction_str == "none" else grad_output_tg * total_weight_tg)
+  unwrap(grad_input).replace(grad)
+  return grad_input
 
 @torch.library.impl("aten::empty_strided", "privateuseone")
 def empty_strided(size, stride, dtype, layout=None, device=None, pin_memory=False):
@@ -115,6 +154,17 @@ for k,v in get_decompositions(decomps).items():
   if TORCH_DEBUG >= 2: print("register decomp for", k)
   torch.library.impl(key, "privateuseone")(v)
 
+def call_func(obj, func_str):
+    func = getattr(obj, func_str, None)
+    if callable(func):
+        func()
+    else:
+        print(f"Function {func_str} not found")
+
+# TODO: Categorize these functions and move them to a separate file
+IDENTICAL_FUNCS = ["flip", "tril", "triu", "acos", "acosh", "asin", "asinh", "atan", "atanh", "ceil", "celu", "clamp", "cos", "cosh", "elu", "erf", "hardsigmoid", "hardswish", "hardtanh", "tan", "tanh", "trunc", "sigmoid", "sign", "silu", "sin", "sinh", "softplus", "round", "log", "log2", "min", "mish", "gather", "argmax", "reciprocal", "mean", "sqrt", "rsqrt", "neg", "atan", "acos", "acosh", "asin", "asinh", "atanh", "ceil", "celu", "clamp", "cos", "cosh", "elu", "erf", "abs", "exp", "exp2", "min", "max", "relu"]
+identical_dict = {f"aten.{func}": getattr(Tensor, func) for func in IDENTICAL_FUNCS}
+
 tiny_backend = {
   "aten.view": Tensor.reshape,
   "aten.add.Tensor": Tensor.add,
@@ -129,19 +179,8 @@ tiny_backend = {
   "aten.gt.Tensor": Tensor.__gt__, "aten.gt.Scalar": Tensor.__gt__,
   "aten.lt.Tensor": Tensor.__lt__, "aten.lt.Scalar": Tensor.__lt__,
   "aten.le.Tensor": Tensor.__le__, "aten.le.Scalar": Tensor.__le__,
-  "aten.abs": Tensor.abs,
-  "aten.exp": Tensor.exp,
-  "aten.exp2": Tensor.exp2,
-  "aten.min": Tensor.min,
-  "aten.max": Tensor.max,
-  "aten.relu": Tensor.relu,
   "aten.relu_": lambda x: x.assign(x.relu()),
-  "aten.mean": Tensor.mean,
   "aten.mean.dim": Tensor.mean,
-  "aten.neg": Tensor.neg,
-  "aten.reciprocal": Tensor.reciprocal,
-  "aten.sqrt": Tensor.sqrt,
-  "aten.rsqrt": Tensor.rsqrt,
   "aten.mm": Tensor.matmul,
   "aten.var.correction": Tensor.var,
   # TODO: support var_mean in tinygrad
@@ -149,9 +188,7 @@ tiny_backend = {
   # NOTE: axis=[] in torch means all, change tinygrad?
   "aten.sum.IntList_out": lambda self,axis,keepdim=False,out=None:
     out.replace(Tensor.sum(self, axis if len(axis) else None, keepdim), allow_shape_mismatch=True),
-  "aten.argmax": Tensor.argmax,
   "aten.scatter.value": Tensor.scatter,
-  "aten.gather": Tensor.gather,
   "aten.where.self": Tensor.where,
   "aten._log_softmax": lambda self,dim,half_to_float: self.softmax(dim),
   "aten.random_": lambda self:
@@ -159,6 +196,8 @@ tiny_backend = {
   "aten.uniform_": lambda self, low=0, high=1: self.assign(Tensor.uniform(*self.shape, low=low, high=high)),
   "aten.normal_": lambda self, low=0, high=1: self.assign(Tensor.normal(*self.shape, low=low, high=high)),
 }
+
+tiny_backend.update(identical_dict)
 
 # NOTE: there's earlier things to hook these, so the .out form isn't needed
 #"aten.add.out": lambda x,y,out: out.replace(x+y, allow_shape_mismatch=True),
