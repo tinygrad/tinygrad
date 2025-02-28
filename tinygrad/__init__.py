fae916db35012765c61d58ada758ac1707860af5
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-02-28 21:21:18 +0530

    only decorator approach

--- tinygrad/__init__.py
+++ tinygrad/__init__.py
@@ -1,4 +1,9 @@
 import os
+if int(os.getenv("TYPED", "0")):
+  from typeguard import typechecked
+else: 
+  def typechecked(obj): return obj
+  
 from tinygrad.tensor import Tensor                                    # noqa: F401
 from tinygrad.engine.jit import TinyJit                               # noqa: F401
 from tinygrad.ops import UOp
@@ -6,10 +11,3 @@ Variable = UOp.variable
 from tinygrad.dtype import dtypes                                     # noqa: F401
 from tinygrad.helpers import GlobalCounters, fetch, Context, getenv   # noqa: F401
 from tinygrad.device import Device                                    # noqa: F401
-
-if int(os.getenv("TYPED", "0")):
-  from typeguard import typechecked, install_import_hook
-  install_import_hook(packages=['tinygrad'])
-else:
-  def typechecked(obj):
-    return obj
