1872ad72355b3e60a335b2ce6f8d2f7d2f97a006
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-02-28 21:21:18 +0530

    only decorator approach

--- tinygrad/__init__.py
+++ tinygrad/__init__.py
@@ -9,10 +9,3 @@ Variable = UOp.variable
 from tinygrad.dtype import dtypes                                     # noqa: F401
 from tinygrad.helpers import GlobalCounters, fetch, Context, getenv   # noqa: F401
 from tinygrad.device import Device                                    # noqa: F401
-
-if int(os.getenv("TYPED", "0")):
-  from typeguard import typechecked, install_import_hook
-  install_import_hook(packages=['tinygrad'])
-else:
-  def typechecked(obj):
-    return obj
