8631f55f4e9b52b78faf08138e2c126592882548
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-02-28 11:15:15 +0530

    fix TRACEMETA=0 not working in context

--- tinygrad/__init__.py
+++ tinygrad/__init__.py
@@ -7,7 +7,7 @@ from tinygrad.engine.jit import TinyJit                               # noqa: F4
 from tinygrad.ops import UOp
 Variable = UOp.variable
 from tinygrad.dtype import dtypes                                     # noqa: F401
-from tinygrad.helpers import GlobalCounters, fetch, Context, getenv   # noqa: F401
+from tinygrad.helpers import GlobalCounters, fetch, Context, getenv, TRACEMETA   # noqa: F401
 from tinygrad.device import Device                                    # noqa: F401
 
 if int(os.getenv("TYPED", "0")):
