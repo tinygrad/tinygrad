36948b9964d5254569a9f9435334e598082c2b1c
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-02-28 13:31:22 +0530

    catching type errors

--- tinygrad/__init__.py
+++ tinygrad/__init__.py
@@ -1,7 +1,5 @@
 import os
-if int(os.getenv("TYPED", "0")):
-  from typeguard import install_import_hook
-  install_import_hook(__name__)
+
 from tinygrad.tensor import Tensor                                    # noqa: F401
 from tinygrad.engine.jit import TinyJit                               # noqa: F401
 from tinygrad.ops import UOp
@@ -9,3 +7,10 @@ Variable = UOp.variable
 from tinygrad.dtype import dtypes                                     # noqa: F401
 from tinygrad.helpers import GlobalCounters, fetch, Context, getenv   # noqa: F401
 from tinygrad.device import Device                                    # noqa: F401
+
+if int(os.getenv("TYPED", "0")):
+  from typeguard import typechecked, install_import_hook
+  install_import_hook(packages=['tinygrad'])
+else:
+  def typechecked(obj):
+    return obj
