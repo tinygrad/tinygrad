fba82ae6b1782b80090dfb9cb59d74476be8f36a
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-02-28 00:39:18 -0800

    failed test

--- extra/torch_backend/test.py
+++ extra/torch_backend/test.py
@@ -87,5 +87,12 @@ class TestTorchBackend(unittest.TestCase):
     a = torch.ones(4, device=device)
     print(str(a))
 
+  @unittest.skip("failed")
+  def test_floor_div(self):
+    a = torch.tensor([10., 7., 5.], device=device)
+    b = torch.tensor([3., 2., 2.], device=device)
+    result = a // b
+    np.testing.assert_equal(result.cpu().numpy(), [3., 3., 2.])
+
 if __name__ == "__main__":
   unittest.main()
