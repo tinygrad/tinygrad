f4148ac46ae096afac3a926d68ba8d25af1e7341
Priyank Patel <tocubed@gmail.com>
2025-03-01 05:49:10 -0800

    torch fix casting and add ops for sd vae(s) (#9297)
    * torch fix copy casting and add upsample op
    
    * update cast and add test
    
    * fix lint
    
    * add pad for sdxl vae to work

--- extra/torch_backend/backend.py
+++ extra/torch_backend/backend.py
@@ -140,15 +140,16 @@ def convolution_backward_overrideable(grad_out, input, weight, stride, padding, 
   return tuple([wrap(grads.pop(0)) if m else None for m in output_mask])
 
 @torch.library.impl("aten::_copy_from", "privateuseone")
-def _copy_from(src, dest, non_blocking=False):
+def _copy_from(src: torch.Tensor, dest, non_blocking=False):
+  cast_dtype = _from_torch_dtype(dest.dtype)
   if str(src.device) == "tiny" and str(dest.device) == "tiny":
-    unwrap(dest).replace(unwrap(src), allow_shape_mismatch=True)
+    unwrap(dest).replace(unwrap(src).cast(cast_dtype), allow_shape_mismatch=True)
   elif str(src.device) == "tiny" and str(dest.device) == "cpu":
     # TODO: is there a better way?
     dest.resize_(src.numel()).resize_(src.shape)
-    dest.copy_(torch.from_numpy(unwrap(src).numpy()))
+    dest.copy_(torch.from_numpy(unwrap(src).cast(cast_dtype).numpy()))
   elif str(src.device) == "cpu" and str(dest.device) == "tiny":
-    unwrap(dest).assign(Tensor(src.numpy()))
+    unwrap(dest).assign(Tensor(src.numpy()).cast(cast_dtype))
   else:
     raise NotImplementedError(f"can't copy from {src.device} -> {dest.device}")
 
@@ -191,6 +192,7 @@ decomps = [
   aten._softmax_backward_data, aten.embedding_dense_backward,
   aten.linalg_vector_norm,
   aten.binary_cross_entropy, aten.binary_cross_entropy_backward,
+  aten.upsample_nearest2d.out,
   # activations
   aten.hardswish, aten.hardswish_backward,
   aten.hardtanh, aten.hardtanh_backward,
@@ -322,6 +324,7 @@ tiny_backend = {**{k:wrap_out(v) for k,v in tiny_backend_out.items()}, **{
   "aten.logical_not": Tensor.logical_not,
   "aten.masked_fill_.Scalar": lambda self,mask,value: self.assign(mask.where(self, value)),
   "aten.multinomial": Tensor.multinomial,
+  "aten.pad": Tensor.pad,
   "aten.reflection_pad2d": functools.partial(Tensor.pad, mode="reflect"),
 }}
 
