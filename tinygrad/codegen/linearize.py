6a9c03995a9548f3ae3e7fbb102f0d1a8086511e
Tzvetomir Kaydzhiev <tzwetomir@gmail.com>
2025-03-03 13:36:49 +0000

    WIP: Adding types to new linearizer

--- tinygrad/codegen/linearize.py
+++ tinygrad/codegen/linearize.py
@@ -1,28 +1,26 @@
 from __future__ import annotations
-import collections, heapq, functools
-from dataclasses import dataclass
+import functools
 from tinygrad.ops import UOp, Ops, PatternMatcher, UPat, graph_rewrite, GroupOp
-from tinygrad.spec import type_verify
-from tinygrad.dtype import dtypes, PtrDType
-from tinygrad.helpers import dedup, flatten, partition
+from tinygrad.helpers import dedup
 
 DONT_PLACE_IN_BLOCK = {Ops.NAME, Ops.DEFINE_GLOBAL, Ops.DEFINE_LOCAL, Ops.DEFINE_VAR, Ops.SPECIAL, Ops.CONST, *GroupOp.Block}
 
+
 # root_uop -> sink
-def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
-  scope_blocks = {}
+def linearize_uop(root_uop: UOp, skip_check: bool = not __debug__) -> list[UOp]:
+  scope_blocks: dict[UOp, LinearizeScope] = {}
 
   class LinearizeScope:
     def __init__(self):
-      self.define_acc = []
-      self.start = []
-      self.end = []
-      self.context = []
-      self.body = []
-      self.subgraph_nodes = set()
-    
-    def fuse(self, other):
-      scope = LinearizeScope()
+      self.define_acc: list[UOp] = []
+      self.start: list[UOp] = []
+      self.end: list[UOp] = []
+      self.context: list[UOp] = []
+      self.body: list[UOp] = []
+      self.subgraph_nodes: set[UOp] = set()
+
+    def fuse(self, other: LinearizeScope) -> LinearizeScope:
+      scope: LinearizeScope = LinearizeScope()
       scope.define_acc = dedup(self.define_acc + other.define_acc)
       scope.start = dedup(self.start + other.start)
       scope.end = dedup(self.end + other.end)
@@ -34,37 +32,39 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
           if src not in scope.subgraph_nodes:
             scope.context.append(src)
 
-      body_set = set(scope.body)
-      for i in scope.start + scope.end:
-        assert i not in body_set, "Broken skopes"
+      if not skip_check:
+        body_set: set[UOp] = set(scope.body)
+        for i in scope.start + scope.end:
+          assert i not in body_set, "Broken skopes"
 
       return scope
 
     @functools.cached_property
     def linear(self):
-      linear = []
+      linear: list[UOp] = []
 
       for elem in self.define_acc:
         linear.append(elem)
 
       for elem in self.end:
         linear.append(elem)
-      
-      curr_context = set(self.context + self.define_acc + self.end)
-      simple_set = set([elem for elem in self.body if elem.op not in {Ops.ASSIGN, Ops.STORE}])
-      scope_set = set([elem for elem in self.body if elem.op in {Ops.ASSIGN, Ops.STORE}])
-      fusion_set = set()
-      poped = True
+
+      curr_context: set[UOp] = set(self.context + self.define_acc + self.end)
+      simple_set: set[UOp] = set([elem for elem in self.body if elem.op not in {Ops.ASSIGN, Ops.STORE}])
+      scope_set: set[UOp] = set([elem for elem in self.body if elem.op in {Ops.ASSIGN, Ops.STORE}])
+      fusion_set: set[UOp] = set()
+      poped: bool = True
+
       while True:
         poped = False
-        pop_list = []
+        pop_list: list[UOp] = []
         for elem in simple_set:
-          ready = True
+          ready: bool = True
           for cont in elem.src:
             if cont not in curr_context:
               ready = False
               break
-          
+
           if ready:
             pop_list.append(elem)
             curr_context.add(elem)
@@ -76,13 +76,13 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
 
         for elem in pop_list:
           simple_set.remove(elem)
-        
+
         if poped:
           continue
 
         for elem in scope_set:
-          elem_scope = scope_blocks[elem]
-          ready = True
+          elem_scope: LinearizeScope = scope_blocks[elem]
+          ready: bool = True
           for cont in elem_scope.context:
             if cont not in curr_context:
               ready = False
@@ -90,22 +90,22 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
 
           if ready:
             fusion_set.add(elem)
-        
+
         for elem in fusion_set:
           scope_set.discard(elem)
-        
+
         if not fusion_set:
           assert not simple_set and not scope_set, "Linearization Failed"
           break
 
-        fusion_elem = fusion_set.pop()
-        fusion_elem = scope_blocks[fusion_elem]
+        fusion_elem: UOp = fusion_set.pop()
+        fusion_elem: LinearizeScope = scope_blocks[fusion_elem]
         for cont_elem in fusion_elem.subgraph_nodes:
           curr_context.add(cont_elem)
-        pop_list = []
+        pop_list: list[UOp] = []
         for elem in fusion_set:
-          scope_elem = scope_blocks[elem]
-          fusable = True
+          scope_elem: LinearizeScope = scope_blocks[elem]
+          fusable: bool = True
           for i in scope_elem.end:
             if i not in fusion_elem.end:
               fusable = False
@@ -113,7 +113,7 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
           for i in fusion_elem.end:
             if i not in scope_elem.end:
               fusable = False
-          
+
           if not fusable:
             continue
 
@@ -122,10 +122,10 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
             curr_context.add(cont_elem)
 
           pop_list.append(elem)
-        
+
         for elem in pop_list:
           fusion_set.remove(elem)
-        
+
         linear = linear + fusion_elem.linear
 
         for e in fusion_elem.subgraph_nodes:
@@ -143,38 +143,37 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
           for s in o.src:
             assert o.op is Ops.DEFINE_ACC or s in linear[:i]
 
-      linear = sorted([elem for elem in root_uop.toposort if elem.op is Ops.DEFINE_GLOBAL], key=lambda x: x.arg) + [elem for elem in linear if elem.op is not Ops.DEFINE_GLOBAL]
+      linear = sorted([elem for elem in root_uop.toposort if elem.op is Ops.DEFINE_GLOBAL], key=lambda x: x.arg) + [
+        elem for elem in linear if elem.op is not Ops.DEFINE_GLOBAL
+      ]
       if self.start[0].op is Ops.SINK:
         assert root_uop.arg is not None
         linear = [UOp(Ops.NAME, arg=root_uop.arg.name)] + linear
 
       return linear
 
-  def _make_scope(scope_start:UOp):
-    scope = LinearizeScope()
+  def _make_scope(scope_start: UOp):
+    scope: LinearizeScope = LinearizeScope()
     scope.start.append(scope_start)
-    stack = [(scope_start, None, False)]
+    stack: list[tuple[UOp, UOp | None, bool]] = [(scope_start, None, False)]
     scope.subgraph_nodes.add(scope_start)
-    visited = set()
+    visited: set[UOp] = set()
     while stack:
       curr_uop, pare_uop, bubbling = stack.pop()
       if curr_uop in visited:
         if curr_uop in scope.subgraph_nodes and pare_uop is not None:
           scope.subgraph_nodes.add(pare_uop)
         continue
-      elif bubbling:
-        visited.add(curr_uop)
 
       if bubbling:
-        if curr_uop.op in {Ops.RANGE, Ops.IF} and scope_start.op in {Ops.ASSIGN, Ops.STORE} or \
-           curr_uop.src == () and scope_start.op == Ops.SINK:
+        visited.add(curr_uop)
+        if curr_uop.op in {Ops.RANGE, Ops.IF} and scope_start.op in {Ops.ASSIGN, Ops.STORE} or curr_uop.src == () and scope_start.op == Ops.SINK:
           scope.end.append(curr_uop)
           scope.subgraph_nodes.add(curr_uop)
 
         if curr_uop in scope.subgraph_nodes and pare_uop is not None:
           scope.subgraph_nodes.add(pare_uop)
-          if curr_uop.key != scope_start.key and curr_uop.op not in {Ops.RANGE, Ops.IF} and \
-            not (curr_uop.src == () and scope_start.op == Ops.SINK):
+          if curr_uop.key != scope_start.key and curr_uop.op not in {Ops.RANGE, Ops.IF} and not (curr_uop.src == () and scope_start.op == Ops.SINK):
             if curr_uop.op is Ops.DEFINE_ACC:
               scope.define_acc.append(curr_uop)
             else:
@@ -187,7 +186,7 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
         elif curr_uop.op in {Ops.ASSIGN, Ops.STORE}:
           for uop_src in scope_blocks[curr_uop].context:
             stack.append((uop_src, curr_uop, False))
-    
+
     for elem in scope.body:
       assert elem.op not in {Ops.RANGE, Ops.IF}, "Colliding scopes"
       if elem.op in {Ops.ASSIGN, Ops.STORE}:
@@ -197,46 +196,15 @@ def linearize_uop(root_uop:UOp, skip_check:bool=not __debug__) -> list[UOp]:
       for src in elem.src:
         if src not in scope.subgraph_nodes:
           scope.context.append(src)
-    
+
     scope.context = dedup(scope.context)
 
     return scope
 
-  scopes_list = [elem for elem in root_uop.toposort if elem.op in {Ops.ASSIGN, Ops.STORE}]
+  scopes_list: list[UOp] = [elem for elem in root_uop.toposort if elem.op in {Ops.ASSIGN, Ops.STORE}]
   for scope_start in scopes_list:
-    scope = _make_scope(scope_start)
+    scope: LinearizeScope = _make_scope(scope_start)
     scope_blocks[scope_start] = scope
-    print(" ----- ")
-    for i in scope.start:
-      print(f"scope start {i.op}")
-    print(" -   - ")
-    for i in scope.body:
-      print(f"scope elem {i.op}")
-    print(" -   - ")
-    for i in scope.end:
-      print(f"scope end {i.op}")
-    print(" -   - ")
-    for i in scope.context:
-      print(f"scope context {i.op}")
-    print(" ----- ")
 
-  scope = _make_scope(root_uop)
-  print(" --- global --- ")
-  for i in scope.start:
-    print(f"scope start {i.op}")
-  print(" -   - ")
-  for i in scope.body:
-    print(f"scope elem {i.op}")
-  print(" -   - ")
-  for i in scope.end:
-    print(f"scope end {i.op}")
-  print(" -   - ")
-  for i in scope.context:
-    print(f"scope context {i.op}")
-  print(" --- global --- ")
-
-  print(" --- final --- ")
-  for i in scope.linear:
-    print(f"scope linear --> {i.op}")
-  print(" --- final --- ")
+  scope: LinearizeScope = _make_scope(root_uop)
   return scope.linear[:-1]
