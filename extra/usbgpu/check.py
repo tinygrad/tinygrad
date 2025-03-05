b538cf27215a3a40b83c2b31ad5ccda271ddfa89
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-03-05 06:35:18 -0800

    and gpu shoudl be there

--- extra/usbgpu/check.py
+++ extra/usbgpu/check.py
@@ -21,14 +21,43 @@ def rescan_bus(bus, gpu_bus):
   usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_BUS_RESET, size=1)
   usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_PARITY|pci.PCI_BRIDGE_CTL_SERR, size=1)
 
+  usb.pcie_cfg_req(pci.PCI_MEMORY_BASE, bus=bus, dev=0, fn=0, value=0x1000, size=2)
+  usb.pcie_cfg_req(pci.PCI_MEMORY_LIMIT, bus=bus, dev=0, fn=0, value=0x2000, size=2)
+  usb.pcie_cfg_req(pci.PCI_PREF_MEMORY_BASE, bus=bus, dev=0, fn=0, value=0x2000, size=2)
+  usb.pcie_cfg_req(pci.PCI_PREF_MEMORY_LIMIT, bus=bus, dev=0, fn=0, value=0xffff, size=2)
+
 try: print_cfg(0, 0)
 except: print("bus=0, dev=0 failed")
 
 try: print_cfg(1, 0)
 except: print("bus=1, dev=0 failed")
 
-rescan_bus(0, gpu_bus=2)
-rescan_bus(1, gpu_bus=2)
+rescan_bus(0, gpu_bus=4)
+rescan_bus(1, gpu_bus=4)
 
 try: print_cfg(2, 0)
 except: print("bus=2, dev=0 failed")
+
+def setup_bus(bus, gpu_bus):
+  print("setup bus={}".format(bus))
+  usb.pcie_cfg_req(pci.PCI_MEMORY_BASE, bus=bus, dev=0, fn=0, value=0x1000, size=2)
+  usb.pcie_cfg_req(pci.PCI_MEMORY_LIMIT, bus=bus, dev=0, fn=0, value=0x2000, size=2)
+
+  usb.pcie_cfg_req(pci.PCI_PREF_MEMORY_BASE, bus=bus, dev=0, fn=0, value=0x2000, size=2)
+  usb.pcie_cfg_req(pci.PCI_PREF_MEMORY_LIMIT, bus=bus, dev=0, fn=0, value=0xffff, size=2)
+
+  usb.pcie_cfg_req(pci.PCI_SUBORDINATE_BUS, bus=bus, dev=0, fn=0, value=gpu_bus, size=1)
+  usb.pcie_cfg_req(pci.PCI_SECONDARY_BUS, bus=bus, dev=0, fn=0, value=bus+1, size=1)
+  usb.pcie_cfg_req(pci.PCI_PRIMARY_BUS, bus=bus, dev=0, fn=0, value=max(0, bus-1), size=1)
+
+  usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_BUS_RESET, size=1)
+  usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_PARITY|pci.PCI_BRIDGE_CTL_SERR, size=1)
+  usb.pcie_cfg_req(pci.PCI_COMMAND, bus=bus, dev=0, fn=0, value=pci.PCI_COMMAND_IO | pci.PCI_COMMAND_MEMORY | pci.PCI_COMMAND_MASTER, size=1)
+
+setup_bus(2, gpu_bus=4)
+try: print_cfg(3, 0)
+except: print("bus=3, dev=0 failed")
+
+setup_bus(3, gpu_bus=4)
+try: print_cfg(4, 0)
+except: print("bus=4, dev=0 failed")
