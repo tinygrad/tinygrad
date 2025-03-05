2605b15f386be934f294d029195cd1942ba6711d
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-03-05 06:02:00 -0800

    rescan

--- extra/usbgpu/check.py
+++ extra/usbgpu/check.py
@@ -1,6 +1,7 @@
 import array
 from hexdump import hexdump
 from tinygrad.runtime.support.am.usb import USBConnector
+from tinygrad.runtime.autogen import pci
 
 usb = USBConnector("")
 
@@ -8,15 +9,26 @@ def print_cfg(bus, dev):
   cfg = []
   for i in range(0, 256, 4):
     cfg.append(usb.pcie_cfg_req(i, bus=bus, dev=dev, fn=0, value=None, size=4))
-  
+
   print("bus={}, dev={}".format(bus, dev))
   hexdump(bytearray(array.array('I', cfg)))
+
+def rescan_bus(bus, gpu_bus):
+  print("set PCI_SUBORDINATE_BUS bus={} to {}".format(bus, gpu_bus))
+  usb.pcie_cfg_req(pci.PCI_SUBORDINATE_BUS, bus=bus, dev=0, fn=0, value=gpu_bus, size=1)
+
+  print("rescan bus={}".format(bus))
+  usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_BUS_RESET, size=1)
+  usb.pcie_cfg_req(pci.PCI_BRIDGE_CONTROL, bus=bus, dev=0, fn=0, value=pci.PCI_BRIDGE_CTL_PARITY|pci.PCI_BRIDGE_CTL_SERR, size=1)
 
 try: print_cfg(0, 0)
 except: print("bus=0, dev=0 failed")
 
 try: print_cfg(1, 0)
 except: print("bus=1, dev=0 failed")
+
+rescan_bus(0, gpu_bus=2)
+rescan_bus(1, gpu_bus=2)
 
 try: print_cfg(2, 0)
 except: print("bus=2, dev=0 failed")
