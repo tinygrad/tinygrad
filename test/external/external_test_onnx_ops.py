b53fbfcfa172333e4db5ee5e6cd4a5c95b92ecd9
zibokapi <zibokapi@gmail.com>
2025-03-07 19:07:57 +0800

    fix gather and auto_pad

--- test/external/external_test_onnx_ops.py
+++ test/external/external_test_onnx_ops.py
@@ -27,6 +27,27 @@ class TestMainOnnxOps(TestOnnxOps):
     outputs = ["out"]
     self.helper_test_single_op("Reshape", inputs, attributes, outputs)
 
+  def test_conv(self):
+    # test VALID auto_pad
+    inputs = {
+      "x": np.random.randn(1, 3, 384, 384).astype(np.float32),
+      "w": np.random.randn(1152, 3, 14, 14).astype(np.float32),
+      "b": np.random.randn(1152).astype(np.float32)
+    }
+    attributes = {'auto_pad': 'VALID', 'dilations': (1, 1), 'group': 1, 'kernel_shape': (14, 14), 'strides': (14, 14)}
+    outputs = ["y"]
+    self.helper_test_single_op("Conv", inputs, attributes, outputs, atol=1e-4)
+
+  def test_gather(self):
+    # test const negative indices
+    inputs = {
+      "input": np.random.randn(1, 3, 3).astype(np.float32),
+      "indices": np.array(-2, dtype=np.long),
+    }
+    attributes = {'axis': 1}
+    outputs = ["y"]
+    self.helper_test_single_op("Gather", inputs, attributes, outputs)
+
   def test_quantize_linear(self):
     test_cases = [
       {"test_case": "round_half_to_even", "qdtype": np.int8, "qzero_point": 0, "x": [-1.5, -0.5, 0.5, 1.5], "scale": 1.0},
