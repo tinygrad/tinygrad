f2a2d1bcbc2eff01f7b843b08399805b1b63b279
zibokapi <zibokapi@gmail.com>
2025-03-10 13:21:06 +0800

    increase atol rtol

--- test/models/test_onnx.py
+++ test/models/test_onnx.py
@@ -14,7 +14,7 @@ from tinygrad.helpers import CI, fetch, temp
 def run_onnx_torch(onnx_model, inputs):
   import torch
   from onnx2torch import convert
-  torch_model = convert(onnx_model).float()
+  torch_model = convert(onnx_model)
   with torch.no_grad():
     torch_out = torch_model(*[torch.tensor(x) for x in inputs.values()])
   return torch_out
@@ -36,7 +36,7 @@ class TestOnnxModel(unittest.TestCase):
         "nav_features": np.zeros((1, 256)),
         "features_buffer": np.zeros((1, 99, 128)),
     }
-      inputs = {k:Tensor(v.astype(np.float32), requires_grad=False) for k,v in np_inputs.items()}
+      inputs = {k:Tensor(v.astype(np.float16), requires_grad=False) for k,v in np_inputs.items()}
       return inputs
 
     for _ in range(7):
@@ -71,7 +71,6 @@ class TestOnnxModel(unittest.TestCase):
   def test_openpilot_model(self):
     onnx_model = onnx.load(fetch(OPENPILOT_MODEL))
     run_onnx = OnnxRunner(onnx_model)
-    print("got run_onnx")
     inputs = {
       "input_imgs": np.random.randn(*(1, 12, 128, 256)),
       "big_input_imgs": np.random.randn(*(1, 12, 128, 256)),
@@ -80,7 +79,7 @@ class TestOnnxModel(unittest.TestCase):
       "nav_features": np.zeros((1, 256)),
       "features_buffer": np.zeros((1, 99, 128)),
     }
-    inputs = {k:v.astype(np.float32) for k,v in inputs.items()}
+    inputs = {k:v.astype(np.float16) for k,v in inputs.items()}
 
     st = time.monotonic()
     print("****** run onnx ******")
@@ -97,7 +96,8 @@ class TestOnnxModel(unittest.TestCase):
     torch_out = run_onnx_torch(onnx_model, inputs).numpy()
     Tensor.no_grad = False
     print(tinygrad_out, torch_out)
-    np.testing.assert_allclose(tinygrad_out, torch_out, atol=1e-4, rtol=1e-2)
+    assert tinygrad_out.dtype == torch_out.dtype, f"tinygrad dtype {tinygrad_out.dtype}, torch dtype {torch_out.dtype}"
+    np.testing.assert_allclose(tinygrad_out, torch_out, atol=1e-2, rtol=5e-2)
 
   @unittest.skip("slow")
   def test_efficientnet(self):
