0b27ae7c8b1180a7adc087a1f9981f0fe50aca76
George Hotz <geohot@gmail.com>
2025-03-13 13:12:08 +0800

    more

--- extra/replay_pkl.py
+++ extra/replay_pkl.py
@@ -37,11 +37,15 @@ if __name__ == "__main__":
             k.apply_opt(Opt(OptOps.PADTO, 2, 128))
             k.apply_opt(Opt(OptOps.UPCAST, 2, 128))
             #k.apply_opt(Opt(op=OptOps.UPCAST, axis=1, arg=4))
+          elif knum == 1:
+            k.apply_opt(Opt(op=OptOps.UNROLL, axis=2, arg=0))
+            k.apply_opt(Opt(op=OptOps.UNROLL, axis=1, arg=0))
+            k.apply_opt(Opt(op=OptOps.UNROLL, axis=0, arg=0))
+            k.apply_opt(Opt(OptOps.PADTO, 2, 128))
+            k.apply_opt(Opt(OptOps.UPCAST, 2, 128))
           else:
             k.hand_coded_optimizations()
-          if knum in [5]:
-            k.apply_opt(Opt(OptOps.UPCAST, 1, 2))
-            pass
+          #if knum in [5]: k.apply_opt(Opt(OptOps.UPCAST, 1, 2))
         p2 = k.to_program()
         new_ei = replace(ei, prg=CompiledRunner(p2), bufs=[Buffer("DSP", 128+b.size*2, b.dtype).view(b.size, b.dtype, 128) for b in ei.bufs])
         new_ei.run()
