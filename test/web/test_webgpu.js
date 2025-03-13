9110f219520a9d4cee26dd207ce9fc4809671a54
hooved <172129504+hooved@users.noreply.github.com>
2025-03-13 09:51:51 -0400

    puppeteer test: refactor device, improve debugging

--- test/web/test_webgpu.js
+++ test/web/test_webgpu.js
@@ -1,5 +1,4 @@
-const disableShaderF16 = process.argv.includes('--disableShaderF16');
-const query = disableShaderF16 ? "?useShaderF16=false" : "";
+const disableTimeout = process.argv.includes('--disableTimeout');
 const puppeteer = require("puppeteer");
 const { spawn } = require("child_process");
 const child = spawn("python", ["-m", "http.server", "8000"], { shell: true, detached: true });
@@ -26,7 +25,7 @@ async function waitForText(selector, text) {
   let n = 0;
   let ready = false;
   while (n < 30) {
-    const res = await (await selector.getProperty("textContent")).jsonValue();
+    const res = await selector.evaluate(el => el.textContent);
     console.log(`waiting for text ${text} got ${res}`);
     if (res == text) {
       ready = true;
@@ -41,7 +40,7 @@ async function waitForText(selector, text) {
 async function runTest() {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ["--enable-unsafe-webgpu"],
+    args: ["--enable-unsafe-webgpu", "--remote-debugging-port=9222"],
   });
   const page = await browser.newPage();
 
@@ -53,7 +52,7 @@ async function runTest() {
       console.log(`error from page ${message}`),
     );
 
-  const res = await page.goto(`http://localhost:8000/examples/webgpu/efficientnet/index.html${query}`);
+  const res = await page.goto("http://localhost:8000/examples/webgpu/efficientnet/index.html");
   if (res.status() !== 200) throw new Error("Failed to load page");
 
   const textSelector = await page.waitForSelector("#result");
