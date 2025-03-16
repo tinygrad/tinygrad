180e25e023cb4cc28da096a381e8099240f360e0
amarmemic <amar.memic@cc-isobus.com>
2025-03-16 11:25:44 +0100

    remove trailing whitespaces

--- tinygrad/runtime/ops_gpu.py
+++ tinygrad/runtime/ops_gpu.py
@@ -28,9 +28,9 @@ gpu_arch: xe-hpc(Max 1100), acm-g10(A770) (for more information see ocloc compil
         this format as input to clCreateProgramWithBinary function call.
         ocloc does not require Intel GPU device to be present in the system nor does it
         depend on Intel Compute GPU runtime driver to be installed. It does however rely
-        on the same set of compilers (IGC, common_clang) as the runtime driver. 
+        on the same set of compilers (IGC, common_clang) as the runtime driver.
 """
-class IntelOfflineCompiler: 
+class IntelOfflineCompiler:
   def __init__(self):
    pass
   def compile(self, cl_kernel:str, gpu_arch:str) -> bytes:
@@ -39,7 +39,7 @@ class IntelOfflineCompiler:
     ocloc_arguments_carray = (ctypes.c_char_p * (len(ocloc_arguments)))(*ocloc_arguments)
     ocloc_arguments_len_param = ctypes.c_uint32(len(ocloc_arguments))
     ocloc_arguments_param = ctypes.cast(ocloc_arguments_carray, (ctypes.POINTER(ctypes.POINTER(ctypes.c_char))))
-    # create byte array for (in-memory) kernel source code which is directly provided to ocloc lib 
+    # create byte array for (in-memory) kernel source code which is directly provided to ocloc lib
     cl_kernel_bytes = (cl_kernel + "\0").encode('utf-8')
     cl_kernel_carray = (ctypes.c_ubyte * (len(cl_kernel_bytes)))(*cl_kernel_bytes)
     cl_kernel_param = (ctypes.cast(cl_kernel_carray, (ctypes.POINTER((ctypes.c_ubyte)))))
@@ -47,23 +47,24 @@ class IntelOfflineCompiler:
     cl_kernel_len_param = ctypes.byref(cl_kernel_len)
     # create array with all provided (in-memory) kernel files which are matched with in args provided name (see -file kernel.cl)
     # provided_kernels_param shall match kernel.cl which is provided in ocloc_arguments (it's because libocloc api)
-    provided_kernels_carray = (ctypes.c_char_p * 1)(b"kernel.cl") 
+    provided_kernels_carray = (ctypes.c_char_p * 1)(b"kernel.cl")
     provided_kernels_param = ctypes.cast(provided_kernels_carray, (ctypes.POINTER(ctypes.POINTER(ctypes.c_char))))
-    # create output paramters 
+    # create output paramters
     num_outputs_param = ctypes.c_uint32(0)
     data_outputs_param = ctypes.POINTER(ctypes.POINTER(ctypes.c_uint8))()
     len_outputs_param = ctypes.POINTER(ctypes.c_uint64)()
     name_outputs_param = ctypes.POINTER(ctypes.POINTER(ctypes.c_char))()
-    # compile and check result/output 
-    ocloc_retcode = ocloc.oclocInvoke(ocloc_arguments_len_param, ocloc_arguments_param, ctypes.c_uint32(1), 
-                            cl_kernel_param, cl_kernel_len_param, provided_kernels_param, 
+    # compile and check result/output
+    ocloc_retcode = ocloc.oclocInvoke(ocloc_arguments_len_param, ocloc_arguments_param, ctypes.c_uint32(1),
+                            cl_kernel_param, cl_kernel_len_param, provided_kernels_param,
                             0, None, None, None, ctypes.byref(num_outputs_param), ctypes.byref(data_outputs_param),
                             ctypes.byref(len_outputs_param), ctypes.byref(name_outputs_param))
     if ocloc_retcode != ocloc.OCLOC_SUCCESS:
       raise CompileError(f"Intel OpenCL Offline Compiler (ocloc) Error\n\n{ocloc._ocloc_error_t__enumvalues[ocloc_retcode]}")
     binary = bytes(ctypes.string_at(data_outputs_param[0], len_outputs_param[0]))
     # free memory which was internally allocated for output buffers
-    ocloc_retcode = ocloc.oclocFreeOutput(ctypes.byref(num_outputs_param), ctypes.byref(data_outputs_param), ctypes.byref(len_outputs_param), ctypes.byref(name_outputs_param))
+    ocloc_retcode = ocloc.oclocFreeOutput(ctypes.byref(num_outputs_param), ctypes.byref(data_outputs_param), 
+                                          ctypes.byref(len_outputs_param), ctypes.byref(name_outputs_param))
     if ocloc_retcode != ocloc.OCLOC_SUCCESS:
       print("Error: ocloc freeing memory failed!")
     return binary
