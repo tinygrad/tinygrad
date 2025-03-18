588849b702fc02a97b341fcd9ab1b60f50f226ca
Comma Device <device@comma.ai>
2025-03-18 07:57:51 +0000

    dump input buffers

--- extra/dsp/hook.py
+++ extra/dsp/hook.py
@@ -16,6 +16,10 @@ def _mmap(addr, length, prot, flags, fd, offset):
 #install_hook(libc.ioctl, ioctl)
 #orig_mmap_mv = install_hook(libc.mmap, _mmap)
 print("import done")
+import mmap
+
+alloc_sizes = {}
+mmaped = {}
 
 def handle_ioctl(fd, request, argp, ret):
   fn = os.readlink(f"/proc/self/fd/{fd}")
@@ -25,12 +29,16 @@ def handle_ioctl(fd, request, argp, ret):
     if nr == 0:
       st = get_struct(argp, qcom_dsp.struct_ion_allocation_data)
       print(ret, "ION_IOC_ALLOC", format_struct(st))
+      alloc_sizes[st.handle] = st.len
     elif nr == 1:
       st = get_struct(argp, qcom_dsp.struct_ion_handle_data)
       print(ret, "ION_IOC_FREE", format_struct(st))
+      if st.handle in alloc_sizes: del alloc_sizes[st.handle]
+      if st.handle in mmaped: del mmaped[st.handle]
     elif nr == 2:
       st = get_struct(argp, qcom_dsp.struct_ion_fd_data)
       print(ret, "ION_IOC_MAP", format_struct(st))
+      mmaped[st.handle] = mmap.mmap(st.fd, alloc_sizes[st.handle])
   elif fn == "/dev/adsprpc-smd":
     assert chr(itype) == 'R'
     if nr == 8:
@@ -55,12 +63,14 @@ def handle_ioctl(fd, request, argp, ret):
       in_h = (st.sc>>4) & 0xF
       out_h = (st.sc>>0) & 0xF
       print(f"\tm:{method} ia:{in_args} oa:{out_args} ih:{in_h} oh:{out_h}")
+      """
       if in_args or out_args:
         for arg in range(in_args+out_args):
           print(arg, format_struct(st.pra[arg]))
           if st.pra[arg].buf.pv is not None:
             ww = to_mv(st.pra[arg].buf.pv, st.pra[arg].buf.len)
             hexdump(to_mv(st.pra[arg].buf.pv, st.pra[arg].buf.len)[:0x40])
+      """
     elif nr == 6:
       print(ret, "FASTRPC_IOCTL_INIT", format_struct(ini:=get_struct(argp, qcom_dsp.struct_fastrpc_ioctl_init)))
       print(os.readlink(f"/proc/self/fd/{ini.filefd}"))
@@ -68,6 +78,22 @@ def handle_ioctl(fd, request, argp, ret):
     elif nr == 7:
       print(ret, "FASTRPC_IOCTL_INVOKE_ATTRS", format_struct(ini:=get_struct(argp, qcom_dsp.struct_fastrpc_ioctl_invoke_attrs)))
     elif nr == 12: print(ret, "FASTRPC_IOCTL_CONTROL", format_struct(get_struct(argp, qcom_dsp.struct_fastrpc_ioctl_control)))
+    elif nr == 4:
+      st_fd = get_struct(argp, qcom_dsp.struct_fastrpc_ioctl_invoke_fd)
+      st = st_fd.inv
+      print(ret, "FASTRPC_IOCTL_INVOKE_FD", format_struct(st))
+
+      method = (st.sc>>24) & 0xFF
+      in_args = (st.sc>>16) & 0xFF
+      out_args = (st.sc>>8) & 0xFF
+      in_h = (st.sc>>4) & 0xF
+      out_h = (st.sc>>0) & 0xF
+      print(f"\tm:{method} ia:{in_args} oa:{out_args} ih:{in_h} oh:{out_h}")
+
+      if st.sc in [0x2030200, 0x3040300]:
+        for handle, mapped in mmaped.items():
+          print(f" buffer {handle} {alloc_sizes[handle]:X}")
+          with open(f"/tmp/buf_{st.sc:X}_{handle}_{alloc_sizes[handle]:X}", "wb") as f: f.write(mapped)
     else:
       print(f"{ret} UNPARSED {nr}")
   else:
