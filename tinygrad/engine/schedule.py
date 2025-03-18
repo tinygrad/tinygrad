a5a5aebdac9b40a15c57812cbfc0aba7231a8203
qazal <qazal.software@gmail.com>
2025-03-18 17:07:52 +0800

    recompute arange + threefry

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -161,7 +161,9 @@ def apply_swizzle(u:UOp) -> UOp:
 
 def swizzle_reduceop(ctx, r:UOp, src:UOp, view:UOp):
   ctx.update_children()
-  if r in ctx.children and len(ctx.children[r]) > 1: return r.contiguous().view(view.arg)
+  # NOTE: if the reduceop is not an arange and has more than one path, we realize it
+  # if it's an arange just recompute it
+  if r in ctx.children and len(ctx.children[r]) > 1 and src.base.op is not Ops.CONST: return r.contiguous().view(view.arg)
   if (st:=unwrap(view.st)).contiguous: return None
   input_st = ShapeTracker.from_shape(src.shape)
   tmp = input_st.permute(tuple(i for i in range(len(input_st.shape)) if i not in r.axis_arg)+r.axis_arg)
@@ -177,7 +179,7 @@ def swizzle_reduceop(ctx, r:UOp, src:UOp, view:UOp):
 
 def reduceop_view_right(src:UOp, v:UOp, r:UOp):
   assert unwrap(v.st).contiguous and v.size == src.size, f"can't compute new axis for {src.shape} -> {r.shape}"
-  return src.r(r.arg[0], tuple(i for i,(s,u) in enumerate(zip(src.shape, r.shape)) if s != u)).view(ShapeTracker.from_shape(r.shape))
+  return src.r(r.arg[0], tuple(i for i,(s,u) in enumerate(zip(src.shape, r.shape)) if resolve(s != u))).view(ShapeTracker.from_shape(r.shape))
 
 def elementwise_view_right(root:UOp):
   if not (swizzles:=[x for x in root.src if x.op is Ops.VIEW and x.base.op not in DONT_PUSH_VIEWS]): return None
