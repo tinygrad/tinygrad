26abd7ffaa5662edb6635ec1347cfebea74e08d4
amarmemic <amar.memic@cc-isobus.com>
2025-03-18 22:44:56 +0100

    add first simple compilation test

--- test/test_intel_ocloc.py
+++ test/test_intel_ocloc.py
@@ -1,32 +1,36 @@
+import unittest
 from tinygrad.tensor import Tensor
 from tinygrad.codegen.kernel import Kernel
-from tinygrad.runtime.ops_gpu import CLDevice, IntelOfflineCompiler
-import numpy as np
-import pickle
-import unittest
+from tinygrad import intel_offline_compiler as ioc
+from tinygrad.renderer.cstyle import OpenCLRenderer, IntelRenderer
+from tinygrad.runtime.ops_gpu import CLDevice, CLProgram, CLAllocator
+from tinygrad.device import BufferSpec
 
 # TODO: skip if not intel hardware
 class TestIntelOcloc(unittest.TestCase):
-  def test_compare_binary(self):
-    a = Tensor.ones((1, 3, 4096))
-    b = Tensor.ones((1, 3, 4096))
-    c = Tensor.conv2d(a,b,padding=1)
+  def test_simple_compilation(self):
+    cl_kernel = """__kernel void test(__global int* data0) {
+                      int gidx0 = get_group_id(0);
+                      *(data0+gidx0) =  get_group_id(0);
+                    }"""
 
-    # TODO: determine target ip version (via opencl possibel ?)
-
-    # runtime binary 
-    sched = c.schedule()
-    kernel = Kernel(sched[-1].ast)
-    kernel.linearize()
-
-    # render 
-
-    # compile 
-
-    print("kernel uops ", kernel.uops)
-
-    # ocloc binary 
-   
+    def compile(src, ioc_compile_func):
+      device = CLDevice()
+      array_len = 100
+      # compile
+      buf = device.allocator.alloc(array_len*4, BufferSpec(host=True, cpu_access=True))
+      binary = device.compiler.compile(cl_kernel) if ioc_compile_func is None else ioc_compile_func(cl_kernel)
+      prog = device.runtime("test", binary)
+      res = prog(buf, global_size = (array_len,1,1), local_size=(4,1,1), wait=True)
+      # read buf ou
+      array = bytearray(array_len*4)
+      mv_array = memoryview(array)
+      device.allocator._copyout(mv_array, buf)
+      device.allocator._free(buf, BufferSpec(host=True, cpu_access=True))
+      return res, binary, list(bytearray(mv_array))
+    cl_time, cl_binary, cl_result = compile(cl_kernel, None)
+    ioc_time, ioc_binary, ioc_result = compile(cl_kernel, lambda x: ioc.IntelOfflineCompiler().compile(x, "xe"))
+    assert(cl_result == ioc_result)
 
 if __name__ == '__main__':
   unittest.main()
