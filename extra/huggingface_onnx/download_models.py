4951851a8a3c849eeac990848eb96927c4ccc0b7
zibokapi <zibokapi@gmail.com>
2025-03-19 14:08:53 +0800

    add retry mechanism for model download

--- extra/huggingface_onnx/download_models.py
+++ extra/huggingface_onnx/download_models.py
@@ -1,6 +1,19 @@
-import yaml, argparse
+import yaml, argparse, time
+from requests.exceptions import ConnectionError, Timeout
 from pathlib import Path
 from huggingface_hub import snapshot_download
+
+def snapshot_with_retries(model_id, allow_patterns, cache_dir, retries=3, timeout=5):
+  for _ in range(retries):
+    try:
+      snapshot_path = snapshot_download(repo_id=model_id, resume_download=True, allow_patterns=allow_patterns, cache_dir=cache_dir)
+      return snapshot_path
+    except ConnectionError as e:
+      exception = e
+      print(f"Encountered timeout while downloading `{model_id}` with patterns {allow_patterns}, retrying.")
+      time.sleep(timeout)
+  raise Timeout( f"Failed to download model '{model_id}' with patterns {allow_patterns} after {retries} retries. Aborting CI process.") \
+    from exception
 
 def download_models(yaml_file: str, download_dir: str) -> None:
   with open(yaml_file, 'r') as f: metadata = yaml.safe_load(f)
@@ -9,9 +22,9 @@ def download_models(yaml_file: str, download_dir: str) -> None:
   for i, (model_id, model_data) in enumerate(metadata["repositories"].items()):
     print(f"Downloading {i+1}/{n}: {model_id}...")
     allow_patterns = [file_info["file"] for file_info in model_data["files"]]
-    root_path = Path(snapshot_download(repo_id=model_id, allow_patterns=allow_patterns, cache_dir=download_dir))
     # download configs too (the sizes are small)
-    snapshot_download(repo_id=model_id, allow_patterns=["*config.json"], cache_dir=download_dir)
+    allow_patterns = allow_patterns + ["*config.json"]
+    root_path = Path(snapshot_with_retries(model_id, allow_patterns, download_dir))
     print(f"Downloaded model files to: {root_path}")
     model_data["download_path"] = str(root_path)
 
