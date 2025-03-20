c867a48ab4b8eb8c39fec060cfe300964c9c0ffa
George Hotz <geohot@gmail.com>
2025-03-20 20:02:35 +0800

    custom

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -76,6 +76,9 @@ class Estimates:
       elif u.op is Ops.STORE: lds += u.src[1].dtype.itemsize * mults
       elif u.op in GroupOp.ALU and u not in dont_count: flops += (mults * (2 if u.op is Ops.MULACC else 1)) * u.dtype.count
       elif u.op is Ops.WMMA and u not in dont_count: flops += 2 * prod(u.arg[1]) // u.arg[5] * mults
+      elif u.op in {Ops.CUSTOM, Ops.CUSTOMI} and u not in dont_count:
+        if u.arg.startswith("__builtin_HEXAGON_V6_vrmpybus_acc_128B"): flops += 128*2*mults
+        if u.arg.startswith("__builtin_HEXAGON_V6_vrmpybus_128B"): flops += 128*mults
     return Estimates(flops, lds, lds) # TODO: properly track memory, lds is always a high estimate
 
 @dataclass
