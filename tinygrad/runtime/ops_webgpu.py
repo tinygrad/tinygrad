e7c7342f448241e3c372b61f6a285698f07f6403
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2025-03-22 23:09:32 +0100

    Refactor webgpu_dawn lib finding

--- tinygrad/runtime/ops_webgpu.py
+++ tinygrad/runtime/ops_webgpu.py
@@ -9,11 +9,7 @@ import os
 
 backend_types = {v: k for k, v in webgpu.WGPUBackendType__enumvalues.items() }
 
-try:
-  instance = webgpu.wgpuCreateInstance(webgpu.WGPUInstanceDescriptor(features = webgpu.WGPUInstanceFeatures(timedWaitAnyEnable = True)))
-except AttributeError:
-  raise RuntimeError("Cannot find dawn library. Install it with: " + ("brew tap wpmed92/dawn && brew install dawn" if OSX else
-  "sudo curl -L https://github.com/wpmed92/pydawn/releases/download/v0.1.6/libwebgpu_dawn.so -o /usr/lib/libwebgpu_dawn.so"))
+instance = webgpu.wgpuCreateInstance(webgpu.WGPUInstanceDescriptor(features = webgpu.WGPUInstanceFeatures(timedWaitAnyEnable = True)))
 
 def to_c_string(_str): return ctypes.create_string_buffer(_str.encode('utf-8'))
 
