f2a58cb9208c0c2ff7a9a6a1bc5d787caaf1f724
yvon <yvonmanzi@gmail.com>
2025-03-22 16:11:17 +0200

    one little issue

--- tinygrad/codegen/linearize.py
+++ tinygrad/codegen/linearize.py
@@ -222,7 +222,7 @@ def linearize_uop(sink:UOp, skip_check:bool=not __debug__) -> list[UOp]:
     # NOTE: if any BLOCKEND is the parent of any other with the same arg, this algo fails
     if len(v) > 1:
       out = UOp(Ops.BLOCKFORK, src=(UOp(Ops.BLOCKEND, src=tuple(flatten(x.src for x in v)),
-                                        arg=BasicBlock(tuple(dedup(flatten([y.arg.ctx for y in v]))), v[0].arg.lst, k.arg.end)),), arg=len(v))
+                                        arg=BasicBlock(tuple(dedup(flatten([y.arg.ctx for y in v]))), v[0].arg.lst, k.end)),), arg=len(v))
       for u in v: new_forks[u] = out
   sink = sink.substitute(new_forks)
 
