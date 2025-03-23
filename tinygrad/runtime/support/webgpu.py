d734e24c012ba463343ee1a5c8fd9f8ffb3f64d6
chenyu <chenyu@fastmail.com>
2025-03-23 09:10:02 -0400

    minor WEBGPU_PATH cleanup [pr] (#9552)
    also mypy recognizes `sys.platform == 'win32'` but does not recognizes it if wrapped inside a helper...

--- tinygrad/runtime/support/webgpu.py
+++ tinygrad/runtime/support/webgpu.py
@@ -2,14 +2,10 @@ import ctypes, ctypes.util, os, subprocess
 from tinygrad.helpers import OSX
 
 if OSX:
-  brew_prefix = subprocess.check_output(['brew', '--prefix', 'dawn']).decode().strip()
-
-  if not os.path.exists(brew_prefix):
+  if not os.path.exists(brew_prefix:=subprocess.check_output(['brew', '--prefix', 'dawn']).decode().strip()):
     raise FileNotFoundError('dawn library not found. Install it with `brew tap wpmed92/dawn && brew install dawn`')
   WEBGPU_PATH: str|None = os.path.join(brew_prefix, 'lib', 'libwebgpu_dawn.dylib')
 else:
-  WEBGPU_PATH = ctypes.util.find_library('webgpu_dawn')
-
-  if WEBGPU_PATH is None:
+  if (WEBGPU_PATH:=ctypes.util.find_library('webgpu_dawn')) is None:
     raise FileNotFoundError("dawn library not found. " +
     "Install it with `sudo curl -L https://github.com/wpmed92/pydawn/releases/download/v0.1.6/libwebgpu_dawn.so -o /usr/lib/libwebgpu_dawn.so`")
