f4a87ac212b9c1a00a0ace864e370b74b61edbb9
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-03-24 13:23:04 +0000

    fix

--- tinygrad/engine/memory.py
+++ tinygrad/engine/memory.py
@@ -10,12 +10,13 @@ from tinygrad.runtime.support.allocator import TLSFAllocator
 
 def _internal_memory_planner(buffers:list[list[Buffer]|tuple[Buffer, ...]], noopt_buffers=None, debug_prefix="") -> dict[Buffer, Buffer]:
   if NO_MEMORY_PLANNER: return {}
-  first_appearance, last_appearance = {}, {}
+  first_appearance, last_appearance, buf_to_opt = {}, {}, set()
   for i,u in enumerate(buffers):
     for buf in u:
-      if buf.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers): continue
+      if buf.is_allocated() or buf.base.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers): continue
       if buf.base not in first_appearance: first_appearance[buf.base] = i
       last_appearance[buf.base] = i
+      buf_to_opt.add(buf)
 
   # Sort buffers operations in timeline order. 2 events: buffer is allocted and buffer is freed.
   buffer_requests = sorted([((first_appearance[buf], True), buf) for buf in first_appearance.keys()] + \
@@ -25,30 +26,31 @@ def _internal_memory_planner(buffers:list[list[Buffer]|tuple[Buffer, ...]], noop
   reuse_buffers:dict[tuple[str], list[Buffer]] = defaultdict(list)
   global_planner:dict[tuple[str], tuple[int, TLSFAllocator]] = defaultdict(lambda: (0, TLSFAllocator(256 << 30, block_size=0x100, lv2_cnt=32)))
   for (time, is_open_ev), buf in buffer_requests:
-    if can_suballoc:=hasattr(Device[buf.device].allocator, "offset") and not isinstance(buf.dtype, ImageDType):
+    # Check if can suballoc.
+    if hasattr(Device[buf.device].allocator, "_offset") and not isinstance(buf.dtype, ImageDType):
       if is_open_ev: buffer_replace[buf] = (None, global_planner[buf.device][1].alloc(buf.nbytes))
       else: global_planner[buf.device][1].free(buffer_replace[buf][1])
-      global_planner[buf.device][0] = (max(global_planner[buf.device][0], [buffer_replace[buf][1] + buf.nbytes]), global_planner[buf.device][1])
+      global_planner[buf.device] = (max(global_planner[buf.device][0], buffer_replace[buf][1] + buf.nbytes), global_planner[buf.device][1])
     else:
       key = (buf.device, buf.dtype, buf.options, buf.nbytes)
       if is_open_ev: buffer_replace[buf] = (reuse_buffers[key].pop(), 0) if key in reuse_buffers and len(reuse_buffers[key]) > 0 else (buf, 0)
       else: reuse_buffers[key].append((buffer_replace[buf][0], None))
 
   global_buffers = {dev: Buffer(dev, round_up(sz, 0x1000), dtypes.int8) for dev, (sz, _) in global_planner.items()}
-  buffer_replace = {dev: (global_buffers[buf.device] if buf is None else buf, offset) for dev, (sz, buf) in global_planner.items()}
+  buffer_replace = {buf: (base or global_buffers[buf.device], off) for buf, (base, off) in buffer_replace.items()}
 
-  # assign all known root buffers first
-  assigned = {buf: Buffer(buf.device, buf.size, buf.dtype, base=base, offset=off) for buf, (base, offset) in buffer_replace.items() if buf != base}
+  assigned = {buf: Buffer(buf.device, buf.size, buf.dtype, base=base, offset=off) for buf, (base, off) in buffer_replace.items() if buf != base}
 
   # and now subbuffers
-  for i,u in enumerate(buffers):
-    for buf in u:
-      if buf.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers) or buf._base is None: continue
-      assigned[buf] = Buffer(buf.device, buf.size, buf.dtype, base=(pbuf:=assigned.get(buf.base, buf.base)).base, offset=pbuf.offset+buf.offset)
+  for buf in buf_to_opt:
+    if buf._base is None: continue
+    assigned[buf] = Buffer(buf.device, buf.size, buf.dtype, base=(pbuf:=assigned.get(buf.base, buf.base)).base, offset=pbuf.offset+buf.offset)
 
-  if DEBUG >= 1 and len(ak:=dedup(x for x in assigned.keys() if x._base is None)) != len(av:=dedup(x for x in assigned.values() if x._base is None)):
-    print(debug_prefix+f"memory reduced from {sum([x.nbytes for x in ak])/1e6:.2f} MB -> {sum([x.nbytes for x in av+list(global_buffers.values())])/1e6:.2f} MB,",
-          f"{len(ak)} -> {len(av)} bufs")
+  if DEBUG >= 1:
+    ak=dedup(x for x in assigned.keys() if x._base is None)
+    av=dedup(x for x in assigned.values() if x._base is None)
+    print(f"{debug_prefix} memory reduced from {sum([x.nbytes for x in ak])/1e6:.2f} MB ->",
+          f"{sum([x.nbytes for x in av+list(global_buffers.values())])/1e6:.2f} MB,", f"{len(ak)} -> {len(av)} bufs")
 
   return assigned
 
