0a0fb145eb19049f15918498cb2b1525e304742c
ignaciosica <mignacio.sica@gmail.com>
2025-03-26 18:15:42 +0800

    focus only on amt > 0

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -340,6 +340,12 @@ class Kernel:
 
   def apply_opt(self, opt:Opt, append_opt:bool=True):
     if self.dont_use_locals: check(opt.op not in {OptOps.LOCAL, OptOps.GROUP, OptOps.GROUPTOP}, "not using locals")
+    if getenv("LDS") and opt.op in (OptOps.UNROLL, OptOps.LOCAL) and opt.arg is not None and not isinstance(opt.arg, tuple) and int(opt.arg) > 2 \
+      and opt.arg % 2 == 0:
+      for i in range(int(math.log2(opt.arg))):
+        self.apply_opt((new_opt:=Opt(opt.op, opt.axis, 2)))
+        self.applied_opts.append(new_opt)
+      return
 
     if opt.op is OptOps.TC:
       check(len(self.applied_opts) == 0, "tensor core opts must be first") # TODO: things like PADTO might be fine
@@ -354,14 +360,6 @@ class Kernel:
     axis = self.real_axis(opt)
     check(axis < len(self.full_shape), "invalid axis")
 
-    if getenv("LDS") and opt.op in (OptOps.UNROLL, OptOps.LOCAL) and opt.arg is not None and not isinstance(opt.arg, tuple) and int(opt.arg) > 2 \
-      and opt.arg % 2 == 0:
-      if (opt_amt := opt.arg) == 0: opt_amt = self.full_unupcasted_shape[-1]
-      for i in range(int(math.log2(opt_amt))):
-        new_opt = Opt(opt.op, opt.axis, 2)
-        self.apply_opt(new_opt)
-        self.applied_opts.append(new_opt)
-      return
 
     if opt.op is OptOps.SWAP: amt = cast(int, opt.arg)  # arg is an axis in the SWAPs
     elif opt.arg is not None:
