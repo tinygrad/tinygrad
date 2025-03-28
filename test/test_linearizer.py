5596c1bf00e33904f7df0e7e687662787888c535
ignaciosica <mignacio.sica@gmail.com>
2025-03-28 19:41:19 +0800

    rename helper function

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -2199,7 +2199,7 @@ class TestKernelOpts(unittest.TestCase):
     ]
     helper_linearizer_opt(r, [x[0] for x in opts_shapes], color_sizes=[x[1] for x in opts_shapes])
 
-def test_lds_helper(opts:list[Opt], expected_bufs, N=16, M=16, K=16):
+def helper_lds_allclose(opts:list[Opt], expected_bufs, N=16, M=16, K=16):
   a, b = Tensor.rand(M, K), Tensor.rand(K, N)
   realized_ast, bufs = helper_realized_ast(a @ b)
   k = Kernel(realized_ast)
@@ -2224,66 +2224,66 @@ class TestLDS(unittest.TestCase):
   # test lds 0 with TC padded
 
   def test_lds_output_basic(self):
-    test_lds_helper(opts=[Opt(OptOps.LDS, 0, None)], expected_bufs=[(0,1)])
+    helper_lds_allclose(opts=[Opt(OptOps.LDS, 0, None)], expected_bufs=[(0,1)])
 
   @unittest.expectedFailure
   def test_lds_input_basic(self):
-    test_lds_helper(opts=[Opt(OptOps.LDS, 1, None)], expected_bufs=[(1,16)])
-    test_lds_helper(opts=[Opt(OptOps.LDS, 2, None)], expected_bufs=[(2,16)])
+    helper_lds_allclose(opts=[Opt(OptOps.LDS, 1, None)], expected_bufs=[(1,16)])
+    helper_lds_allclose(opts=[Opt(OptOps.LDS, 2, None)], expected_bufs=[(2,16)])
 
   @unittest.expectedFailure
   def test_lds_multi_basic(self):
-    test_lds_helper(opts=[Opt(OptOps.LDS, 0, None), Opt(OptOps.LDS, 1, None)], expected_bufs=[(1,1),(1,16)])
-    test_lds_helper(opts=[Opt(OptOps.LDS, 0, None), Opt(OptOps.LDS, 1, None), Opt(OptOps.LDS, 2, None)], expected_bufs=[(0,16),(1,16),(2,16)])
+    helper_lds_allclose(opts=[Opt(OptOps.LDS, 0, None), Opt(OptOps.LDS, 1, None)], expected_bufs=[(1,1),(1,16)])
+    helper_lds_allclose(opts=[Opt(OptOps.LDS, 0, None), Opt(OptOps.LDS, 1, None), Opt(OptOps.LDS, 2, None)], expected_bufs=[(0,16),(1,16),(2,16)])
 
   def test_lds_output_unroll(self):
     # unroll doesn't change local output buffer size
     for sz in [0,2,4,8]:
-      test_lds_helper(opts=[Opt(OptOps.UNROLL, 0, sz), Opt(OptOps.LDS, 0, None)], expected_bufs=[(0,1)])
+      helper_lds_allclose(opts=[Opt(OptOps.UNROLL, 0, sz), Opt(OptOps.LDS, 0, None)], expected_bufs=[(0,1)])
 
   def test_lds_output_local(self):
     # if only locals are applied, local buffer size for output should be prod(locals)
 
     basic_local_opts = [Opt(OptOps.LOCAL, 0, 2),
                         Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=basic_local_opts, expected_bufs=[(0,2)])
+    helper_lds_allclose(opts=basic_local_opts, expected_bufs=[(0,2)])
 
     multi_local_opts = [Opt(OptOps.LOCAL, 0, 2),
                         Opt(OptOps.LOCAL, 0, 8),
                         Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=multi_local_opts, expected_bufs=[(0,16)])
+    helper_lds_allclose(opts=multi_local_opts, expected_bufs=[(0,16)])
 
     multi_axis_local_opts = [Opt(OptOps.LOCAL, 1, 2),
                              Opt(OptOps.LOCAL, 0, 2),
                              Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=multi_axis_local_opts, expected_bufs=[(0,4)])
+    helper_lds_allclose(opts=multi_axis_local_opts, expected_bufs=[(0,4)])
 
     full_local_opts = [Opt(OptOps.LOCAL, 0, 16),
                        Opt(OptOps.LOCAL, 0, 16),
                        Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=full_local_opts, expected_bufs=[(0,256)])
+    helper_lds_allclose(opts=full_local_opts, expected_bufs=[(0,256)])
 
   def test_lds_output_upcast(self):
     # if only upcasts are applied, local buffer size for output should be prod(locals)
 
     basic_upcast_opts = [Opt(OptOps.UPCAST, 0, 2),
                         Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=basic_upcast_opts, expected_bufs=[(0,2)])
+    helper_lds_allclose(opts=basic_upcast_opts, expected_bufs=[(0,2)])
 
     multi_upcast_opts = [Opt(OptOps.UPCAST, 0, 2),
                         Opt(OptOps.UPCAST, 0, 8),
                         Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=multi_upcast_opts, expected_bufs=[(0,16)])
+    helper_lds_allclose(opts=multi_upcast_opts, expected_bufs=[(0,16)])
 
     multi_axis_upcast_opts = [Opt(OptOps.UPCAST, 1, 2),
                              Opt(OptOps.UPCAST, 0, 2),
                              Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=multi_axis_upcast_opts, expected_bufs=[(0,4)])
+    helper_lds_allclose(opts=multi_axis_upcast_opts, expected_bufs=[(0,4)])
 
     full_upcast_opts = [Opt(OptOps.UPCAST, 1, 16),
                        Opt(OptOps.UPCAST, 0, 16),
                        Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=full_upcast_opts, expected_bufs=[(0,256)])
+    helper_lds_allclose(opts=full_upcast_opts, expected_bufs=[(0,256)])
 
   def test_lds_output_local_upcast(self):
     # for locals and upcasts size is no longer product as upcast can be applied to local dimensions
@@ -2293,23 +2293,23 @@ class TestLDS(unittest.TestCase):
     opts = [Opt(OptOps.LOCAL, 0, 2),
             Opt(OptOps.UPCAST, 1, 2),
             Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=opts, expected_bufs=[(0,4)])
+    helper_lds_allclose(opts=opts, expected_bufs=[(0,4)])
 
     opts = [Opt(OptOps.LOCAL, 0, 2),
             Opt(OptOps.UPCAST, 0, 4),
             Opt(OptOps.LOCAL, 1, 8),
             Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=opts, expected_bufs=[(0,64)])
+    helper_lds_allclose(opts=opts, expected_bufs=[(0,64)])
 
     opts = [Opt(OptOps.LOCAL, 0, 16),
             Opt(OptOps.UPCAST, 1, 2),
             Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=opts, expected_bufs=[(0,16)]) # upcasting local
+    helper_lds_allclose(opts=opts, expected_bufs=[(0,16)]) # upcasting local
 
     opts = [Opt(OptOps.LOCAL, 0, 16),
             Opt(OptOps.UPCAST, 0, 16),
             Opt(OptOps.LDS, 0, None)]
-    test_lds_helper(opts=opts, expected_bufs=[(0,256)])
+    helper_lds_allclose(opts=opts, expected_bufs=[(0,256)])
 
 if __name__ == "__main__":
   unittest.main()
