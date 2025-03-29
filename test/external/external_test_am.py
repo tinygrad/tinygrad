118bd1cbed8dd7a582a4e8a1e48589329293a522
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-03-29 20:19:53 +0700

    hotfix: amd imports (#9620)

--- test/external/external_test_am.py
+++ test/external/external_test_am.py
@@ -1,6 +1,7 @@
 import unittest
 from tinygrad.runtime.support.am.amdev import AMMemoryManager, AMPageTableTraverseContext
 from tinygrad.runtime.support.am.ip import AM_GMC
+from tinygrad.runtime.support.amd import import_module
 from tinygrad.helpers import mv_address
 
 class FakeGMC(AM_GMC):
@@ -170,6 +171,20 @@ class TestAMPageTable(unittest.TestCase):
                    (0x0, 0x4000), (0x10000, 0x4000), (0x10000, 0x40000), (0x10001000, 0x40000), (0x100001000, 0x3000)]:
       must_cover_checker(va, sz)
       not_cover_checker(va, sz)
+
+class TestAM(unittest.TestCase):
+  def test_imports(self):
+    with self.assertRaises(ImportError): import_module("gc", (7, 0, 0))
+    x = import_module("gc", (11, 0, 0))
+    assert x.__name__ == "tinygrad.runtime.autogen.am.gc_11_0_0"
+    x = import_module("gc", (11, 6, 0))
+    assert x.__name__ == "tinygrad.runtime.autogen.am.gc_11_0_0"
+    x = import_module("gc", (12, 0, 0))
+    assert x.__name__ == "tinygrad.runtime.autogen.am.gc_12_0_0"
+    x = import_module("gc", (10, 3, 0))
+    assert x.__name__ == "tinygrad.runtime.autogen.am.gc_10_3_0"
+    x = import_module("gc", (10, 3, 3))
+    assert x.__name__ == "tinygrad.runtime.autogen.am.gc_10_3_0"
 
 if __name__ == "__main__":
   unittest.main()
