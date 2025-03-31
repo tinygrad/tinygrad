3e36b23e95b92e6f647edda3fcc76bb49bb6ee92
yvon <yvonmanzi@gmail.com>
2025-03-31 16:42:16 +0200

    more testing

--- test/test_ops.py
+++ test/test_ops.py
@@ -2961,15 +2961,15 @@ class TestOps(unittest.TestCase):
   def test_nonzero(self):
     helper_test_op([(3,3)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True)
     helper_test_op([(3, 3)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True, vals=[[0, 1, 0],[2, 0, 3],[0, 0, 4]])
-    helper_test_op(
-        [(3, 3)],
-        lambda x: torch.nonzero(x),
-        lambda x: x.nonzero(),
-        forward_only=True,
-        vals=[[0, 0, 0],
-              [0, 0, 0],
-              [0, 0, 0]]
-    )
+    helper_test_op([(3, 3)],lambda x: torch.nonzero(x),lambda x: x.nonzero(),forward_only=True,vals=[[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+    helper_test_op([(0,3)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True)
+    helper_test_op([()], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True)
+    helper_test_op([()], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True, vals=[0])
+    helper_test_op([(5,)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True)
+    helper_test_op([(5,)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True, vals=[0, 1, 0, 2, 0])
+    helper_test_op([(2,2,2)], lambda x: torch.nonzero(x), lambda x: x.nonzero(), forward_only=True, vals=[[[0,1],[0,0]],[[1,0],[0,1]]])
+    helper_test_op([(3,3)], lambda x: torch.nonzero(x > 0.5), lambda x: (x > 0.5).nonzero(), forward_only=True)
+    
   @unittest.skipIf(Device.DEFAULT == "QCOM", "OpenCL fails to compile this (both on GPU(qcom)/QCOM backends)")
   def test_cast(self):
     helper_test_op([(3, 3)], lambda x: x.float())
