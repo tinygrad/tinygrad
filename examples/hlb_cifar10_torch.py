576bb4bbca1c5e778bd2614a2c216cddb39d0dcd
ankit <ankit.avinash@gmail.com>
2025-04-02 11:29:15 +0530

    use tinygrad datasets

--- examples/hlb_cifar10_torch.py
+++ examples/hlb_cifar10_torch.py
@@ -1,8 +1,7 @@
-print("start")
-
 # pylint: skip-file
 # https://github.com/tysam-code/hlb-CIFAR10/blob/main/main.py with optional tiny backend
-from tinygrad import getenv
+from tinygrad import getenv, dtypes
+from tinygrad.nn.datasets import cifar
 if getenv("TINY_BACKEND"): import tinygrad.frontend.torch
 
 
@@ -87,36 +86,20 @@ hyp = {
     }
 }
 
-print("device is", hyp['misc']['device'])
-
-from tinygrad.nn.datasets import cifar
-print(cifar())
-
 #############################################
 #                Dataloader                 #
 #############################################
 
 if not os.path.exists(hyp['misc']['data_location']):
 
-        transform = transforms.Compose([
-            transforms.ToTensor()])
-
-        print("loading data")
-
-        cifar10      = torchvision.datasets.CIFAR10('extra/datasets/', download=True,  train=True,  transform=transform)
-        cifar10_eval = torchvision.datasets.CIFAR10('extra/datasets/', download=False, train=False, transform=transform)
-
-        # use the dataloader to get a single batch of all of the dataset items at once.
-        train_dataset_gpu_loader = torch.utils.data.DataLoader(cifar10, batch_size=len(cifar10), drop_last=True,
-                                                  shuffle=True, num_workers=2, persistent_workers=False)
-        eval_dataset_gpu_loader = torch.utils.data.DataLoader(cifar10_eval, batch_size=len(cifar10_eval), drop_last=True,
-                                                  shuffle=False, num_workers=1, persistent_workers=False)
-
         train_dataset_gpu = {}
         eval_dataset_gpu = {}
 
-        train_dataset_gpu['images'], train_dataset_gpu['targets'] = [item.to(device=hyp['misc']['device'], non_blocking=True) for item in next(iter(train_dataset_gpu_loader))]
-        eval_dataset_gpu['images'],  eval_dataset_gpu['targets']  = [item.to(device=hyp['misc']['device'], non_blocking=True) for item in next(iter(eval_dataset_gpu_loader)) ]
+        X_train, Y_train, X_test, Y_test = cifar()
+        train_dataset_gpu['images'] = torch.tensor(X_train.float().numpy(), device=hyp['misc']['device'])
+        train_dataset_gpu['targets'] = torch.tensor(Y_train.cast(dtypes.int64).numpy(), device=hyp['misc']['device'])
+        eval_dataset_gpu['images'] = torch.tensor(X_test.float().numpy(), device=hyp['misc']['device'])
+        eval_dataset_gpu['targets'] = torch.tensor(Y_test.cast(dtypes.int64).numpy(), device=hyp['misc']['device'])
 
         cifar10_std, cifar10_mean = torch.std_mean(train_dataset_gpu['images'], dim=(0, 2, 3)) # dynamically calculate the std and mean from the data. this shortens the code and should help us adapt to new datasets!
 
@@ -145,15 +128,13 @@ if not os.path.exists(hyp['misc']['data_location']):
 
         torch.save(data, hyp['misc']['data_location'])
 
-        print("saved")
-
 else:
     ## This is effectively instantaneous, and takes us practically straight to where the dataloader-loaded dataset would be. :)
     ## So as long as you run the above loading process once, and keep the file on the disc it's specified by default in the above
     ## hyp dictionary, then we should be good. :)
     data = torch.load(hyp['misc']['data_location'])
 
-    print("loaded")
+print("done with data")
 
 ## As you'll note above and below, one difference is that we don't count loading the raw data to GPU since it's such a variable operation, and can sort of get in the way
 ## of measuring other things. That said, measuring the preprocessing (outside of the padding) is still important to us.
