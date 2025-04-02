f72a87fd0e2120abc92c88fa56bf909e2bd88776
George Hotz <72895+geohot@users.noreply.github.com>
2025-04-02 16:38:01 +0800

    add proper support for Ops.IGNORE to remove store masks (#9692)
    * add proper support for Ops.IGNORE to remove store masks
    
    * remove useless NHWC
    
    * revert that

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -111,8 +111,7 @@ class ProgramSpec:
           # NOTE: you have to set local_size and global_size to the base [1,1,1] outside this
           if u.arg[0][0] == 'i': self.local_size = None
           special_size = self.local_size if u.arg[0][0] == 'l' else self.global_size
-          assert special_size is not None
-          special_size[int(u.arg[0][-1])] = u.arg[1]
+          if special_size is not None: special_size[int(u.arg[0][-1])] = u.arg[1]
       self.vars = sorted(self.vars, key=lambda v: v.arg)
       self.outs = sorted(dedup(self.outs))
       self.ins = sorted(dedup(self.ins))
