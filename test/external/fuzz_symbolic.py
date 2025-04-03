087219aae971e5f4551bd939f665849347be1c07
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-04-03 13:57:36 +0200

    First version, caught a bug?

--- test/external/fuzz_symbolic.py
+++ test/external/fuzz_symbolic.py
@@ -1,13 +1,20 @@
-import itertools
 import random
+from z3 import IntVal, Solver, substitute, unsat, sat, ArithRef, Ints, simplify, If
 from tinygrad import Variable, dtypes
 from tinygrad.ops import UOp
 from tinygrad.helpers import DEBUG
 random.seed(42)
 
+# z3 division on integers is floored, but tinygrad idiv is truncated division
+ArithRef.__floordiv__ = lambda self, other: If(self<0, (self+(other-1))/other, self/other)
+
 def add_v(expr, rng=None):
   if rng is None: rng = random.randint(0,2)
   return expr + v[rng], rng
+
+def mul_v(expr, rng=None):
+  if rng is None: rng = random.randint(0,2)
+  return expr * v[rng], rng
 
 def div(expr, rng=None):
   if rng is None: rng = random.randint(1,9)
@@ -41,18 +48,20 @@ def gt(expr, rng=None):
   if rng is None: rng = random.randint(-4,4)
   return expr > rng, rng
 
-# NOTE: you have to replace these for this test to pass
-from tinygrad.ops import python_alu, Ops
-python_alu[Ops.MOD] = lambda x,y: x%y
-python_alu[Ops.IDIV] = lambda x,y: x//y
 
 if __name__ == "__main__":
-  ops = [add_v, div, mul, add_num, mod]
-  for _ in range(1000):
-    upper_bounds = [*list(range(1, 10)), 16, 32, 64, 128, 256]
+  ops = [add_v, mul, add_num, div]
+  for i in range(1000):
+    solver = Solver()
+    if i % 100 == 0:
+      print(f"Running test {i}")
+    upper_bounds = [*list(range(1, 10)), 16, 32, 64, 128, 256, 512, 1024]
+    v1, v2, v3 = Ints("v1 v2 v3")
     u1 = Variable("v1", 0, random.choice(upper_bounds))
     u2 = Variable("v2", 0, random.choice(upper_bounds))
     u3 = Variable("v3", 0, random.choice(upper_bounds))
+    solver.add(v1 >= 0, v2 >= 0, v3 >= 0)
+    solver.add(v1 <= u1.arg[2], v2 <= u2.arg[2], v3 <= u3.arg[2])
     v = [u1,u2,u3]
     tape = [random.choice(ops) for _ in range(random.randint(2, 30))]
     # 10% of the time, add one of lt, le, gt, ge
@@ -64,15 +73,23 @@ if __name__ == "__main__":
       if DEBUG >= 1: print(t.__name__, rng)
       rngs.append(rng)
     if DEBUG >=1: print(expr)
-    space = list(itertools.product(range(u1.vmin, u1.vmax+1), range(u2.vmin, u2.vmax+1), range(u3.vmin, u3.vmax+1)))
-    volume = len(space)
-    for (v1, v2, v3) in random.sample(space, min(100, volume)):
-      v = [v1,v2,v3]
-      rn = 0
-      for t,r in zip(tape, rngs): rn, _ = t(rn, r)
-      num = eval(expr.render(simplify=False))
-      if num != rn:
-        unsimplified_num = eval(expr.render(simplify=False))
-        assert unsimplified_num == rn, "UNSIMPLIFIED MISMATCH!"
-        assert num == rn, f"mismatched {expr.render()} at {v1=} {v2=} {v3=} = {num} != {rn}\n{expr.render(simplify=False)}"
-      if DEBUG >= 1: print(f"matched {expr.render()} at {v1=} {v2=} {v3=} = {num} == {rn}")
+
+    v = [v1,v2,v3]
+    z3_expr = IntVal(0)
+    for t,r in zip(tape, rngs): z3_expr, _ = t(z3_expr, r)
+    simplified_expr = eval(expr.render())
+    if solver.check(simplified_expr != z3_expr) == sat:
+      m = solver.model()
+      unsimplified_expr = eval(expr.render(simplify=False))
+      assert solver.check(unsimplified_expr != z3_expr) == unsat, f"UNSIMPLIFIED MISMATCH!, {expr.render(simplify=False)} != {z3_expr}"
+      n1, n2, n3 = m[v1], m[v2], m[v3]
+      # print(f"{u1=}\n{u2=}\n{u3=}\n")
+      u1_val, u2_val, u3_val = u1.const_like(n1.as_long()), u2.const_like(n2.as_long()), u3.const_like(n3.as_long())
+      num_tiny = expr.simplify().substitute({u1:u1_val, u2:u2_val, u3:u3_val}).ssimplify()
+      rn_tiny = expr.substitute({u1:u1_val, u2:u2_val, u3:u3_val}).ssimplify()
+
+      # num = simplify(substitute(simplified_expr, (v1, IntVal(n1)), (v2, IntVal(n2)), (v3, IntVal(n3)))) if not isinstance(simplified_expr, int) else simplified_expr
+      # rn = simplify(substitute(unsimplified_expr, (v1, IntVal(n1)), (v2, IntVal(n2)), (v3, IntVal(n3))))
+      assert False, f"mismatched {expr.render()} at v1={m[v1]}; v2={m[v2]}; v3={m[v3]} = {num_tiny} != {rn_tiny}\n{expr.render(simplify=False)}"
+
+    if DEBUG >= 1: print(f"validated {expr.render()}")
