a66d9120b1d7dd13a5a0c3f251811290c7055279
George Hotz <geohot@gmail.com>
2025-04-03 12:49:59 +0800

    more gc tests

--- test/external/external_uop_gc.py
+++ test/external/external_uop_gc.py
@@ -1,5 +1,6 @@
 import gc
 from tinygrad import Tensor, UOp, Device
+from tinygrad.shape.shapetracker import views_to_indexed_uops
 from tinygrad.engine.realize import method_cache, get_kernel
 
 def uops_allocated(): return sum([isinstance(x, UOp) for x in gc.get_objects()])
@@ -26,8 +27,30 @@ def gradient_test():
   y = Tensor([[2.0,0,-2.0]], requires_grad=True)
   z = y.matmul(x).sum()
   z.backward()
+def realized_eye():
+  Tensor.eye(3, requires_grad=True).realize()
+def realized_list():
+  Tensor([[2.0,0,-2.0]], requires_grad=True).realize()
+def kernel_matmul():
+  x = Tensor.eye(3, requires_grad=True)
+  y = Tensor([[2.0,0,-2.0]], requires_grad=True)
+  z = y.matmul(x)
+  si = z.schedule()[-1]
+  get_kernel(Device.default.renderer, si.ast)
+def realized_matmul():
+  x = Tensor.eye(3, requires_grad=True)
+  y = Tensor([[2.0,0,-2.0]], requires_grad=True)
+  z = y.matmul(x)
+  Tensor.realize(z)
+def realized_gradient():
+  x = Tensor.eye(3, requires_grad=True)
+  y = Tensor([[2.0,0,-2.0]], requires_grad=True)
+  z = y.matmul(x).sum()
+  z.backward()
+  Tensor.realize(x, y, z, x.grad, y.grad)
 tests = [start, single_tensor, two_plus_two, two_plus_two_schedule, two_plus_two_kernel,
-         two_plus_two_linearize, two_plus_two_realize, two_plus_two_item, gradient_test]
+         two_plus_two_linearize, two_plus_two_realize, two_plus_two_item, gradient_test,
+         realized_eye, realized_list, kernel_matmul, realized_matmul, realized_gradient]
 
 if __name__ == "__main__":
   gc.disable()
@@ -36,7 +59,11 @@ if __name__ == "__main__":
   print_uops()
   for t in tests:
     t()
-    method_cache.clear()  # this will keep uops alive
+
+    # these caches will keep uops alive
+    method_cache.clear()
+    views_to_indexed_uops.cache_clear()
+
     new_uops = uops_allocated()
     gc.collect()
     new_uops_gc = uops_allocated()
