fe49ce87d9626be4ec0054e45723e03a4db771cc
qazal <qazal.software@gmail.com>
2025-04-04 12:49:21 +0800

    not solved

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -79,8 +79,13 @@ def get_test_method() -> str|None:
   import unittest
   frm = sys._getframe(1)
   while (f_back:=frm.f_back) is not None and "unittest" not in f_back.f_code.co_filename: frm = f_back
-  if (s:=frm.f_locals.get("self")) is not None and isinstance(s, unittest.TestCase):
-    return f"{frm.f_code.co_filename}_{type(s).__name__}.{s._testMethodName}"
+  all_locals = frm.f_locals
+  s = all_locals.get("self")
+  if s is not None and isinstance(s, unittest.TestCase): ret = f"{frm.f_code.co_filename}_{type(s).__name__}.{s._testMethodName}"
+  else: ret = None
+  del all_locals
+  del s
+  return ret
 
 # for length N coefficients `p`, returns p[0] * x**(N-1) + p[1] * x**(N-2) + ... + p[-2] * x + p[-1]
 def polyN(x:T, p:list[float]) -> T: return functools.reduce(lambda acc,c: acc*x+c, p, 0.0)  # type: ignore
