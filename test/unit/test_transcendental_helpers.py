c67169d920bead3979957a83ab08f65ade1f4ae3
Eitan Turok <eitan.turok@gmail.com>
2025-04-08 12:02:05 +0300

    cleanup more

--- test/unit/test_transcendental_helpers.py
+++ test/unit/test_transcendental_helpers.py
@@ -7,6 +7,19 @@ from tinygrad.ops import UOp, Ops
 from tinygrad.codegen.transcendental import TRANSCENDENTAL_SUPPORTED_DTYPES, payne_hanek_reduction, cody_waite_reduction, frexp, rintk, pow2if, xpow, xexp2, xlog2, trig_poly
 from test.helpers import eval_uop
 
+def uops_equal(u1:UOp|tuple, u2:Optional[UOp|tuple]=None, cmp_op:bool=False, cmp_scalar_dtype:bool|DType=False, cmp_vcount:bool|int=False):
+  # instead of comparing u1 to u2, compare u1 to expected op, scalar_dtype, or vcount
+  if u2 == None:
+    return uops_equal(u1, u1, cmp_op=cmp_op, cmp_scalar_dtype=cmp_scalar_dtype, cmp_vcount=cmp_vcount)
+  # compare u1 to u2
+  if isinstance(u1, UOp) and isinstance(u2, UOp):
+    if cmp_op: assert u1.op == u2.op if isinstance(cmp_op, bool) else u1.op == u2.op == cmp_op, f'ops must match:\n{u1=}\n{u2=}\n{u1.op=}\n{u2.op=}\n{cmp_op=}'
+    if cmp_scalar_dtype: assert u1.dtype.scalar() == u2.dtype.scalar() if isinstance(cmp_scalar_dtype, bool) else u1.dtype.scalar() == u2.dtype.scalar() == cmp_scalar_dtype, f'dtype must match:\n{u1=}\n{u2=}\n{u1.dtype.scalar()=}\n{u2.dtype.scalar()=}\n{cmp_scalar_dtype=}'
+    if cmp_vcount: assert u1.dtype.vcount == u2.dtype.vcount if isinstance(cmp_vcount, bool) else u1.dtype.vcount == u2.dtype.vcount == cmp_vcount, f'vcount must match:\n{u1=}\n{u2=}\n{u1.dtype.vcount=}\n{u2.dtype.vcount=}\n{cmp_vcount=}'
+  # recursive call
+  for x1, x2 in zip((u1 if isinstance(u1, tuple) else u1.src), (u2 if isinstance(u2, tuple) else u2.src)):
+    uops_equal(x1, x2, cmp_op=cmp_op, cmp_scalar_dtype=cmp_scalar_dtype, cmp_vcount=cmp_vcount)
+
 class TestTranscendentalFunctions(unittest.TestCase):
   def test_payne_hanek_reduction(self):
     # TODO: Test constant input when constant folding is fixed (or maybe test both variants)
@@ -72,42 +85,10 @@ class TestTranscendentalFunctions(unittest.TestCase):
     np.testing.assert_allclose(eval_uop(pow2if(UOp.const(dtypes.int, -10), dtypes.float)), 2**-10)
     np.testing.assert_allclose(eval_uop(pow2if(UOp.const(dtypes.int, -63), dtypes.float)), 2**-63)
 
-# class TestVectorizedTranscendetalFunctions(unittest.TestCase):
-
-#   def _test_vectorization_preserved(self, fxn, in_vec, vcount):
-#     out_vec = fxn(in_vec)
-#     self.uops_equal(out_vec, cmp_vcount=vcount)
-
-#   def test_vectorization_preserved(self):
-#     # given a vectorized input, check that the fxn output is vectorized with the same vcount
-#     for dtype_scalar in TRANSCENDENTAL_SUPPORTED_DTYPES:
-#       for val in [-2,1.3,194]:
-#         for vcount in [1,4,19]:
-#           if dtype_scalar == dtypes.float16:
-#             continue
-#           in_vec = UOp.const(dtype_scalar.vec(vcount), val)
-#           self._test_vectorization_preserved(payne_hanek_reduction, in_vec, vcount)
-#           self._test_vectorization_preserved(lambda x: xpow(x, x), in_vec, vcount)
-#           self._test_vectorization_preserved(xexp2, in_vec, vcount)
-#           self._test_vectorization_preserved(cody_waite_reduction, in_vec, vcount)
-
-def uops_equal(u1:UOp|tuple, u2:Optional[UOp|tuple]=None, cmp_op:bool=False, cmp_scalar_dtype:bool|DType=False, cmp_vcount:bool|int=False):
-  # instead of comparing u1 to u2, compare u1 to expected op, scalar_dtype, or vcount
-  if u2 == None:
-    return uops_equal(u1, u1, cmp_op=cmp_op, cmp_scalar_dtype=cmp_scalar_dtype, cmp_vcount=cmp_vcount)
-  # compare u1 to u2
-  if isinstance(u1, UOp) and isinstance(u2, UOp):
-    if cmp_op: assert u1.op == u2.op if isinstance(cmp_op, bool) else u1.op == u2.op == cmp_op, f'ops must match:\n{u1=}\n{u2=}\n{u1.op=}\n{u2.op=}\n{cmp_op=}'
-    if cmp_scalar_dtype: assert u1.dtype.scalar() == u2.dtype.scalar() if isinstance(cmp_scalar_dtype, bool) else u1.dtype.scalar() == u2.dtype.scalar() == cmp_scalar_dtype, f'dtype must match:\n{u1=}\n{u2=}\n{u1.dtype.scalar()=}\n{u2.dtype.scalar()=}\n{cmp_scalar_dtype=}'
-    if cmp_vcount: assert u1.dtype.vcount == u2.dtype.vcount if isinstance(cmp_vcount, bool) else u1.dtype.vcount == u2.dtype.vcount == cmp_vcount, f'vcount must match:\n{u1=}\n{u2=}\n{u1.dtype.vcount=}\n{u2.dtype.vcount=}\n{cmp_vcount=}'
-  # recursive call
-  for x1, x2 in zip((u1 if isinstance(u1, tuple) else u1.src), (u2 if isinstance(u2, tuple) else u2.src)):
-    uops_equal(x1, x2, cmp_op=cmp_op, cmp_scalar_dtype=cmp_scalar_dtype, cmp_vcount=cmp_vcount)
-
-class TestTranscendetalVectorizationPreserved(unittest.TestCase):
+class TestTranscendentalVectorizationPreserved(unittest.TestCase):
+  # given a vectorized input, check that the fxn output is vectorized with the same vcount
 
   def _test_vectorization_preserved(self, fxn, scalar_dtypes=TRANSCENDENTAL_SUPPORTED_DTYPES, vals=[-2,1.3,194], vcounts=[1,4,19]):
-    # given a vectorized input, check that the fxn output is vectorized with the same vcount
     for scalar_dtype in scalar_dtypes:
       for val in vals:
         for vcount in vcounts:
@@ -120,12 +101,11 @@ class TestTranscendetalVectorizationPreserved(unittest.TestCase):
   def test_payne_hanek_reduction(self): return self._test_vectorization_preserved(payne_hanek_reduction)
   def test_cody_waite_reduction(self): return self._test_vectorization_preserved(cody_waite_reduction)
 
-
-class TestTranscendetalScalarVectorInputs(unittest.TestCase):
+class TestTranscendentalScalarVectorInputs(unittest.TestCase):
+  # given a scalar and vectorized input, check that the fxn outputs have the same ops, scalar_dtypes, and evaluate to the same value
+  # the vectorization stuff (vcount, __eq__) can differ
 
   def _test_scalar_vec_equality(self, fxn, scalar_dtypes=TRANSCENDENTAL_SUPPORTED_DTYPES, vals=[-2,1.3,194], vcounts=[1,4,19]):
-    # given a scalar and vectorized input, check that the fxn outputs have the same ops, scalar_dtypes, and evaluate to the same value
-    # the vectorization stuff (vcount, __eq__) can differ
     for scalar_dtype in scalar_dtypes:
       for val in vals:
         for vcount in vcounts:
