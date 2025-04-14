fc993ba9a188e756fcf577e95284a869fd2f13b5
Chen-Yu Yang <chenyu@fastmail.com>
2025-04-14 16:15:12 -0400

    fine

--- tinygrad/codegen/kernel.py
+++ tinygrad/codegen/kernel.py
@@ -88,7 +88,7 @@ class Kernel:
     if isinstance(self.membufs[0].dtype, ImageDType):
       unit_stride_axes_mul_4 = [i for i in self.sts[0].unit_stride_axes(ignore_valid=True) if self.sts[0].shape[i]%4 == 0]
       assert unit_stride_axes_mul_4, f"needs a unit stride axis in {self.bufs[0]}"
-      if all(x < self.first_upcast for x in unit_stride_axes_mul_4): self.apply_opt(Opt(OptOps.UPCAST, unit_stride_axes_mul_4[0], 4), False)
+      if all(x < self.first_upcast for x in unit_stride_axes_mul_4): self.apply_opt(Opt(OptOps.UPCAST, unit_stride_axes_mul_4[0], 4))
 
   def copy(self):
     ret = type(self).__new__(type(self))
