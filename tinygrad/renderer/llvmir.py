054fbbf4dd462f7b953217c7ed0e33461fb79b34
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-04-14 19:42:03 +0300

    __reduce__ to match others

--- tinygrad/renderer/llvmir.py
+++ tinygrad/renderer/llvmir.py
@@ -234,4 +234,4 @@ class AMDLLVMRenderer(LLVMRenderer):
      lambda x: UOp(Ops.WMMA, dtypes.half.vec(16), (x.src[0], x.src[1], x.src[2].cast(dtypes.half.vec(16))), (*x.arg,)).cast(dtypes.half.vec(8)))
   ]) + LLVMRenderer.extra_matcher
   def __init__(self, arch:str): self.arch = arch
-  def __reduce__(self): return (AMDLLVMRenderer, (self.arch,))
+  def __reduce__(self): return self.__class__, (self.arch,)
