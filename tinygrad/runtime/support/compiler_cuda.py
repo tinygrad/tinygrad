07394fac4e96167c0ed45f1479570453889bcf74
jmaczan <jedrzejpawel@maczan.pl>
2025-04-16 22:32:10 +0200

    Fallback to sm_89 only if nvrtc < 12.8

--- tinygrad/runtime/support/compiler_cuda.py
+++ tinygrad/runtime/support/compiler_cuda.py
@@ -41,11 +41,11 @@ def cuda_disassemble(lib, arch):
 class CUDACompiler(Compiler):
   def __init__(self, arch:str, cache_key:str="cuda"):
     self.arch = arch
-    if arch == "sm_120": # fallback for rtx 5090 until nvrtc supports sm_120 
+    nvrtc_check(nvrtc.nvrtcVersion((nvrtcMajor := ctypes.c_int()), (nvrtcMinor := ctypes.c_int())))
+    if arch == "sm_120" and (nvrtcMajor.value, nvrtcMinor.value) < (12, 8):
       self.arch = "sm_89"
     self.compile_options = [f'--gpu-architecture={self.arch}']
     self.compile_options += [f"-I{CUDA_PATH}/include"] if CUDA_PATH else ["-I/usr/local/cuda/include", "-I/usr/include", "-I/opt/cuda/include"]
-    nvrtc_check(nvrtc.nvrtcVersion((nvrtcMajor := ctypes.c_int()), (nvrtcMinor := ctypes.c_int())))
     if (nvrtcMajor.value, nvrtcMinor.value) >= (12, 4): self.compile_options.append("--minimal")
     super().__init__(f"compile_{cache_key}_{self.arch}")
   def _compile_program(self, src:str, nvrtc_get_content:Callable, nvrtc_get_size:Callable) -> bytes:
