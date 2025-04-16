608e193983110944d5e778bd3411b3da0aa8e78c
nimlgen <lenyaprokh@gmail.com>
2025-04-16 21:26:02 +0300

    patched for 24

--- extra/usbgpu/patch.py
+++ extra/usbgpu/patch.py
@@ -1,78 +1,70 @@
 #!/usr/bin/env python3
 import sys
+import zlib
 
 def patch(input_filepath, output_filepath, patches):
   with open(input_filepath, 'rb') as infile: data = bytearray(infile.read())
 
   for offset, expected_bytes, new_bytes in patches:
-    # if len(expected_bytes) != len(new_bytes):
-    #   raise ValueError("Expected bytes and new bytes must be the same length")
+    if len(expected_bytes) != len(new_bytes):
+      print(len(expected_bytes), len(new_bytes))
+      raise ValueError("Expected bytes and new bytes must be the same length")
 
     if offset + len(new_bytes) > len(data): return False
     current_bytes = data[offset:offset + len(expected_bytes)]
-    assert bytes(current_bytes) == expected_bytes
+    assert bytes(current_bytes) == expected_bytes, f"Expected {expected_bytes} at offset {offset:x}, but got {current_bytes}"
     data[offset:offset + len(new_bytes)] = new_bytes
+
+  checksum = sum(data[4:-6]) & 0xff
+  crc32 = zlib.crc32(data[4:-6]).to_bytes(4, 'little')
+  data[-5] = checksum
+  data[-4] = crc32[0]
+  data[-3] = crc32[1]
+  data[-2] = crc32[2]
+  data[-1] = crc32[3]
 
   with open(output_filepath, 'wb') as outfile:
     outfile.write(data)
 
   return True
 
-# function verifier, old asm
-patched_pci_bar_write_old = bytes([
-    0x90, 0x00, 0x06,  # MOV DPTR,#0x6
-    0xE0,              # MOVX A,@DPTR
-    0xFB,              # MOV R3,A
-    0xA3,              # INC DPTR
-    0xE0,              # MOVX A,@DPTR
-    0xFA,              # MOV R2,A
-    0xA3,              # INC DPTR
-    0xE0,              # MOVX A,@DPTR
-    0xF9,              # MOV R1,A
-    0x90, 0x00, 0x10,  # MOV DPTR,#0x10
-    0x12, 0x71, 0x3F,  # LCALL mb_read_from_addrspace
-    0x54, 0x01,        # ANL A,#0x1
-    0xF5, 0x4C,        # MOV DAT_INTMEM_4c,A
-    0x90, 0x90, 0x00,  # MOV DPTR,#0x9000
-    0xE0,              # MOVX A,@DPTR
-    0x30])
-
-patched_pci_bar_write = bytes([
-    0x90, 0x00, 0x07,  # MOV DPTR,#0x7
-    0xE0,              # MOVX A,@DPTR
-    0xFA,              # MOV R2,A
-    0xA3,              # INC DPTR
-    0xE0,              # MOVX A,@DPTR
-    0x24, 0x10,        # ADD A,#0x10
-    0xF9,              # MOV R1,A
-    0xE4,              # CLR A
-    0x3A,              # ADDC A,R2
-    0xF5, 0x83,        # MOV DPH,A
-    0x89, 0x82,        # MOV DPL,R1
-    0x12, 0x74, 0xD7,  # LCALL copy4_from_dptr_into_r4_r7
-    0x12, 0x75, 0x09,  # LCALL copy4_into_b220
-    0x90, 0x00, 0x07,  # MOV DPTR,#0x7
-    0xE0,              # MOVX A,@DPTR
-    0xFA,              # MOV R2,A
-    0xA3,              # INC DPTR
-    0xE0,              # MOVX A,@DPTR
-    0x24, 0x14,        # ADD A,#0x14
-    0xF9,              # MOV R1,A
-    0xE4,              # CLR A
-    0x3A,              # ADDC A,R2
-    0xF5, 0x83,        # MOV DPH,A
-    0x89, 0x82,        # MOV DPL,R1
-    0x12, 0x74, 0xD7,  # LCALL copy4_from_dptr_into_r4_r7
-    0x90, 0x05, 0xF9,  # MOV DPTR,#0x5f9
-    0x12, 0x72, 0x8D,  # LCALL copy_4_into_ptr
-    0x12, 0xA3, 0x99,  # LCALL mb_tlp_mem_req
-    0x12, 0xB6, 0xE3,  # LCALL SUB_CODE_b6e3
-    0x22               # RET
-])
-
 patches = [
-  (0x7a63, b'\xC6\x4B', b'\xB6\xB3'),
-  (0x9f0c, patched_pci_bar_write_old, patched_pci_bar_write),
+  (0x3903 + 1 + 4, b'\x8a', b'\x8b'),
+  (0x3cf9 + 1 + 4, b'\x8a', b'\x8b'), # this is the one which triggered...
+
+  (0x2a0d + 1 + 4, b'\x0a', b'\x05'), # write handle exit with code 5 (?)
+  (0x29ad + 1 + 4, b'\x09', b'\x08'), # write handle exit with code 5 (?)
+  # (0x40ef + 0 + 4, b'\x60', b'\x70'), # jz -> jnz
+  # (0x40e1 + 0 + 4, b'\x90', b'\x22'), # jmp -> ret
+  # (0x40fa + 0 + 4, b'\x80', b'\x22'),
+  # (0x40e1 + 0 + 4, b'\x90\x06\xe6\x04\xf0', b'\x7f\x00\x02\x41\x7c'), # jmp -> ret
 ]
+
+next_traphandler = 0
+def add_traphandler(addr, sec):
+  global next_traphandler, patches
+
+  trap_addr = 0x6000 + next_traphandler * 0x20
+  return_addr = addr + len(sec)
+  cntr_addr = 0x3000 + next_traphandler
+  patches += [
+    (addr + 4, sec, b'\x02' + trap_addr.to_bytes(2, 'big') + b'\x22'*(len(sec)-3)),
+    (trap_addr + 4, b'\x00' * (21 + len(sec)), 
+      b'\xc0\xe0\xc0\x82\xc0\x83\x90' + cntr_addr.to_bytes(2, 'big') + b'\xe0\x04\xf0\xd0\x83\xd0\x82\xd0\xe0' + sec + b'\x02' + return_addr.to_bytes(2, 'big')),
+  ]
+  next_traphandler += 1
+
+# add_traphandler(0x0206, b'\xed\x54\x06') # fill_scsi_resp
+# add_traphandler(0x40d9, b'\x78\x6a\xe6') # fill_scsi_to_usb_transport
+# add_traphandler(0x4d44, b'\x78\x6a\xe6') # FUN_CODE_4d44
+# add_traphandler(0x4784, b'\x78\x6a\xe6') # FUN_CODE_4784
+# add_traphandler(0x3e81, b'\x90\xc5\x16') # FUN_CODE_3e81
+# add_traphandler(0x32a5, b'\x78\x6a\xe6') # FUN_CODE_32a5
+# add_traphandler(0x2a10, b'\x90\xc4\x51') # FUN_CODE_2a10
+# add_traphandler(0x2608, b'\x12\x16\x87') # FUN_CODE_2608
+# add_traphandler(0x180d, b'\x90\x0a\x7d') # close_to_scsi_parse
+# add_traphandler(0x10ee, b'\x90\x04\x64') # iniside trap handler
+# add_traphandler(0x10e0, b'\x90\xc8\x06') # iniside trap handler
+# add_traphandler(0x4977, b'\x90\x0a\xa8') # waiter for nvme???
 
 assert patch(sys.argv[1], sys.argv[2], patches) is True
