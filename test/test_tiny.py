f1c189bf8cc60d69f28896634d891da3dcfff4ae
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-04-19 17:05:52 +0200

    Remove CHECK_OOB import

--- test/test_tiny.py
+++ test/test_tiny.py
@@ -1,7 +1,7 @@
 # basic self-contained tests of the external functionality of tinygrad
 import unittest, random
 from tinygrad import Tensor, Context, Variable, TinyJit, dtypes, Device, nn
-from tinygrad.helpers import CHECK_OOB, IMAGE, CI
+from tinygrad.helpers import IMAGE, CI
 
 class TestTiny(unittest.TestCase):
 
