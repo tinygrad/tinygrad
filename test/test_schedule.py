5fb61550e1666610cdf87fa8a3b19b5507e40d4f
qazal <qazal.software@gmail.com>
2025-04-20 21:07:47 +0300

    test_kernelize_bw

--- test/test_schedule.py
+++ test/test_schedule.py
@@ -87,6 +87,25 @@ class TestSchedule(unittest.TestCase):
     run_schedule(check_schedule(b, 1))
     np.testing.assert_allclose(b.numpy(), np.broadcast_to(a.numpy().astype(np.float16), (2, 4, 4))+2)
 
+  def test_kernelize(self):
+    a = Tensor([1])
+    b = a+2
+    b.kernelize()
+    c = b*2
+    c.kernelize()
+    self.assertEqual(c.item(), 6)
+
+  @unittest.expectedFailure # TODO: this should pass
+  def test_kernelize_bw(self):
+    a = Tensor.full((3,), 2.0, requires_grad=True).contiguous()
+    b = Tensor.full((3,), 3.0, requires_grad=True).contiguous()
+    x = (a*b).kernelize()
+    y = Tensor.eye(3, requires_grad=True)
+    z = y.matmul(x).sum()
+    if getenv("VIZ"):
+      graph_rewrite(z.lazydata, PatternMatcher([]), name="y.matmul(x).sum()")
+    z.backward()
+
   def test_empty_is_not_realized(self):
     a = Tensor.empty(10)
     child = a+2
