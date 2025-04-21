c1597b0f06e5b86852da7183e27154c0bff4d2a8
leopf <l.pfob@3-klicks.de>
2025-04-21 11:19:40 +0200

    one loop

--- tinygrad/runtime/graph/cpu.py
+++ tinygrad/runtime/graph/cpu.py
@@ -10,47 +10,47 @@ from tinygrad.renderer.cstyle import ClangRenderer
 
 class CPUGraph(GraphRunner):
   def __init__(self, device, jit_cache: list[ExecItem], input_rawbuffers: list[Buffer], var_vals: dict[Variable, int]):
-    cpu_progs: list[ExecItem] = []
-    if issubclass(device.runtime, CPUProgram):
-      device = Device["CPU"]
-      cpu_progs.extend(dict((cast(CompiledRunner, item.prg).p.name, item) for item in jit_cache).values()) # dedupe by name
+    if issubclass(device.runtime, CPUProgram): device = Device["CPU"]
     elif not isinstance(device.renderer, ClangRenderer): raise GraphException
     super().__init__(jit_cache, input_rawbuffers, var_vals)
 
     self.base_bufs = dedup(b.base for ji in jit_cache for b in ji.bufs if b is not None and b not in input_rawbuffers)
-    self.base_rawbufs = [b._buf for b in self.base_bufs] + [cast(CompiledRunner, ji.prg)._prg.fxn for ji in cpu_progs]
-
-    targs = [(f"arg{i}", (x.dtype.ptr(), False)) for i,x in enumerate(input_rawbuffers)] + \
-            [(f"cbuf{i}", (dtypes.char.ptr(), False)) for i in range(len(self.base_bufs))] + \
-            [(f"fxnp{i}", (dtypes.void.ptr(), False)) for i in range(len(cpu_progs))] + \
-            sorted([(f"{v.expr}", (dtypes.int, False)) for v in var_vals])
+    self.rawbufs = [b._buf for b in self.base_bufs]
 
     def render_arg(buf):
       if buf in input_rawbuffers: return f"arg{input_rawbuffers.index(buf)}"
       return f"({device.renderer.render_dtype(buf.dtype)}*)(cbuf{self.base_bufs.index(buf.base)} + {buf.offset})"
 
-    batched = ["void batched("+','.join([f"{device.renderer.render_dtype(x[1][0])} {x[0]}" for x in targs])+") {"]
-    for i, ji in enumerate(cpu_progs):
-      runner = cast(CompiledRunner, ji.prg)
-      arg_dtypes = tuple(buf.dtype.ptr() for buf in ji.bufs if buf is not None) + tuple(v.dtype for v in runner.p.vars)
-      s_args = ','.join([device.renderer.render_dtype(dt) for dt in arg_dtypes])
-      batched.append(f"  void (*{to_function_name(runner.p.name)})({s_args}) = (void (*)({s_args}))fxnp{i};")
-
+    defines, funcs, batched_inner  = [], [], []
+    rendered_casts: set[str] = set()
     for i, ji in enumerate(jit_cache):
-      args = [render_arg(buf) for buf in ji.bufs] + [x.expr for x in cast(CompiledRunner, ji.prg).p.vars]
-      batched.append(f"  {to_function_name(cast(CompiledRunner, ji.prg).p.name)}({','.join(args)});")
-    batched.append("}")
+      runner = cast(CompiledRunner, ji.prg)
+      fn_name = to_function_name(runner.p.name)
 
-    if not cpu_progs:
-      prep = [device.renderer._render(cast(CompiledRunner, ji.prg).p.uops) for i,ji in enumerate(jit_cache)]
-      funcs = dedup(device.renderer._render_body(prep[i][0], *prep[i][1:], cast(CompiledRunner, ji.prg).p.uops) for i,ji in enumerate(jit_cache))
-      defines = dedup(itertools.chain.from_iterable(device.renderer._render_defines(cast(CompiledRunner, ji.prg).p.uops) for ji in jit_cache))
-    else: funcs, defines = [], []
+      if isinstance(runner._prg, CPUProgram) and fn_name not in rendered_casts:
+        arg_dtypes = tuple(buf.dtype.ptr() for buf in ji.bufs if buf is not None) + tuple(v.dtype for v in runner.p.vars)
+        s_args = ','.join([device.renderer.render_dtype(dt) for dt in arg_dtypes])
+        batched_inner.append(f"  void (*{fn_name})({s_args}) = (void (*)({s_args}))fxnp{len(rendered_casts)};")
+        self.rawbufs.append(runner._prg.fxn)
+        rendered_casts.add(fn_name)
+
+      if not isinstance(runner._prg, CPUProgram):
+        defines.extend(device.renderer._render_defines(runner.p.uops))
+        funcs.append(device.renderer._render_body(*device.renderer._render(runner.p.uops), runner.p.uops))
+
+      batched_inner.append(f"  {fn_name}({','.join([render_arg(buf) for buf in ji.bufs] + [x.expr for x in runner.p.vars])});")
+
+    targs = [(f"arg{i}", (x.dtype.ptr(), False)) for i,x in enumerate(input_rawbuffers)] + \
+            [(f"cbuf{i}", (dtypes.char.ptr(), False)) for i in range(len(self.base_bufs))] + \
+            [(f"fxnp{i}", (dtypes.void.ptr(), False)) for i in range(len(rendered_casts))] + \
+            sorted([(f"{v.expr}", (dtypes.int, False)) for v in var_vals])
+
+    batched = ["void batched("+','.join([f"{device.renderer.render_dtype(x[1][0])} {x[0]}" for x in targs])+") {", *batched_inner, "}"]
     entry = device.renderer._render_entry("batched", targs)
-    code = '\n'.join(itertools.chain(defines, funcs, batched, (entry,)))
+    code = '\n'.join(itertools.chain(dedup(defines), dedup(funcs), batched, (entry,)))
 
     if DEBUG >= 4: print(code)
     self.clprg = device.runtime("batched", device.compiler.compile_cached(code))
 
   def __call__(self, rawbufs: list[Buffer], var_vals: dict[Variable, int], wait=False):
-    return self.clprg(*[x._buf for x in rawbufs], *self.base_rawbufs, *[x[1] for x in sorted(var_vals.items(), key=lambda x: x[0].expr)], wait=wait)
+    return self.clprg(*[x._buf for x in rawbufs], *self.rawbufs, *[x[1] for x in sorted(var_vals.items(), key=lambda x: x[0].expr)], wait=wait)
