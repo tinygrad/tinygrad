963aae9be6a832dc401d9deda9f5ec9da9c82f40
Francis Lata <francisalbertlata@gmail.com>
2025-04-21 13:26:46 -0700

    a start in rewriting postprocessing step

--- examples/mlperf/model_eval.py
+++ examples/mlperf/model_eval.py
@@ -115,27 +115,30 @@ def eval_retinanet():
     try: next_proc = data_get()
     except StopIteration: next_proc = None
     nd = time.perf_counter()
-    predictions, img_ids = mdl.postprocess_detections(proc[0].numpy(), orig_image_sizes=proc[2]), proc[1]
-    coco_results  = [{"image_id": img_ids[i], "category_id": label, "bbox": box.tolist(), "score": score}
-      for i, prediction in enumerate(predictions) for box, score, label in zip(*prediction.values())]
-    with redirect_stdout(None):
-      coco_eval.cocoDt = coco.loadRes(coco_results)
-      coco_eval.params.imgIds = img_ids
-      coco_eval.evaluate()
-    evaluated_imgs.extend(img_ids)
-    coco_evalimgs.append(np.array(coco_eval.evalImgs).reshape(ncats, narea, len(img_ids)))
-    n += len(proc[0])
-    et = time.perf_counter()
-    tlog(f"****** {(run-st)*1000:7.2f} ms to enqueue, {(et-run)*1000:7.2f} ms to realize ({(nd-run)*1000:7.2f} ms fetching). {(len(proc))/(et-st):8.2f} examples/sec. {GlobalCounters.global_ops*1e-12/(et-st):5.2f} TFLOPS")
-    st = et
-    proc, next_proc = next_proc, None
+    split_idx = mdl.postprocess_detections2(proc[0], orig_image_sizes=proc[2]), proc[1]
+    print(f"--act: {split_idx=}")
+    exp_split_idx = mdl.postprocess_detections(proc[0].numpy(), orig_image_sizes=proc[2]), proc[1]
+    print(f"--exp: {exp_split_idx=}")
+  #   coco_results  = [{"image_id": img_ids[i], "category_id": label, "bbox": box.tolist(), "score": score}
+  #     for i, prediction in enumerate(predictions) for box, score, label in zip(*prediction.values())]
+  #   with redirect_stdout(None):
+  #     coco_eval.cocoDt = coco.loadRes(coco_results)
+  #     coco_eval.params.imgIds = img_ids
+  #     coco_eval.evaluate()
+  #   evaluated_imgs.extend(img_ids)
+  #   coco_evalimgs.append(np.array(coco_eval.evalImgs).reshape(ncats, narea, len(img_ids)))
+  #   n += len(proc[0])
+  #   et = time.perf_counter()
+  #   tlog(f"****** {(run-st)*1000:7.2f} ms to enqueue, {(et-run)*1000:7.2f} ms to realize ({(nd-run)*1000:7.2f} ms fetching). {(len(proc))/(et-st):8.2f} examples/sec. {GlobalCounters.global_ops*1e-12/(et-st):5.2f} TFLOPS")
+  #   st = et
+  #   proc, next_proc = next_proc, None
 
-  coco_eval.params.imgIds = evaluated_imgs
-  coco_eval._paramsEval.imgIds = evaluated_imgs
-  coco_eval.evalImgs = list(np.concatenate(coco_evalimgs, -1).flatten())
-  coco_eval.accumulate()
-  coco_eval.summarize()
-  tlog("done")
+  # coco_eval.params.imgIds = evaluated_imgs
+  # coco_eval._paramsEval.imgIds = evaluated_imgs
+  # coco_eval.evalImgs = list(np.concatenate(coco_evalimgs, -1).flatten())
+  # coco_eval.accumulate()
+  # coco_eval.summarize()
+  # tlog("done")
 
 def eval_rnnt():
   # RNN-T
--- extra/models/retinanet.py
+++ extra/models/retinanet.py
@@ -1,7 +1,7 @@
 import math
 from tinygrad import Tensor, dtypes
 from tinygrad.helpers import flatten, get_child
-from examples.mlperf.helpers import generate_anchors, BoxCoder
+from examples.mlperf.helpers import generate_anchors, BoxCoder, generate_anchors2
 from examples.mlperf.losses import sigmoid_focal_loss, l1_loss
 from extra.models.resnet import ResNet
 import tinygrad.nn as nn
@@ -68,68 +68,78 @@ class RetinaNet:
       obj.assign(dat)
 
   # predictions: (BS, (H1W1+...+HmWm)A, 4 + K)
+  def postprocess_detections2(self, predictions:Tensor, input_size:tuple[int, int]=(800, 800), image_sizes:list[tuple[int, int]]|None=None,
+                              orig_image_sizes:list[tuple[int, int]]|None=None, score_thresh=0.05, topk_candidates=1000, nms_thresh=0.5):
+    anchors = generate_anchors2(input_size)
+    grid_sizes = self.backbone.compute_grid_sizes2(input_size)
+    split_idx = [self.num_anchors * sz[0] * sz[1] for sz in grid_sizes[:-1]]
+    return split_idx
+    # split_idx = np.cumsum([int(self.num_anchors * sz[0] * sz[1]) for sz in grid_sizes[:-1]])
+
+  # predictions: (BS, (H1W1+...+HmWm)A, 4 + K)
   def postprocess_detections(self, predictions, input_size=(800, 800), image_sizes=None, orig_image_sizes=None, score_thresh=0.05, topk_candidates=1000, nms_thresh=0.5):
     anchors = generate_anchors(input_size)
     grid_sizes = self.backbone.compute_grid_sizes(input_size)
     split_idx = np.cumsum([int(self.num_anchors * sz[0] * sz[1]) for sz in grid_sizes[:-1]])
-    detections = []
-    for i, predictions_per_image in enumerate(predictions):
-      h, w = input_size if image_sizes is None else image_sizes[i]
+    return split_idx
+    # detections = []
+    # for i, predictions_per_image in enumerate(predictions):
+    #   h, w = input_size if image_sizes is None else image_sizes[i]
 
-      predictions_per_image = np.split(predictions_per_image, split_idx)
-      offsets_per_image = [br[:, :4] for br in predictions_per_image]
-      scores_per_image = [cl[:, 4:] for cl in predictions_per_image]
+    #   predictions_per_image = np.split(predictions_per_image, split_idx)
+    #   offsets_per_image = [br[:, :4] for br in predictions_per_image]
+    #   scores_per_image = [cl[:, 4:] for cl in predictions_per_image]
 
-      image_boxes, image_scores, image_labels = [], [], []
-      for offsets_per_level, scores_per_level, anchors_per_level in zip(offsets_per_image, scores_per_image, anchors):
-        # remove low scoring boxes
-        scores_per_level = scores_per_level.flatten()
-        keep_idxs = scores_per_level > score_thresh
-        scores_per_level = scores_per_level[keep_idxs]
+    #   image_boxes, image_scores, image_labels = [], [], []
+    #   for offsets_per_level, scores_per_level, anchors_per_level in zip(offsets_per_image, scores_per_image, anchors):
+    #     # remove low scoring boxes
+    #     scores_per_level = scores_per_level.flatten()
+    #     keep_idxs = scores_per_level > score_thresh
+    #     scores_per_level = scores_per_level[keep_idxs]
 
-        # keep topk
-        topk_idxs = np.where(keep_idxs)[0]
-        num_topk = min(len(topk_idxs), topk_candidates)
-        sort_idxs = scores_per_level.argsort()[-num_topk:][::-1]
-        topk_idxs, scores_per_level = topk_idxs[sort_idxs], scores_per_level[sort_idxs]
+    #     # keep topk
+    #     topk_idxs = np.where(keep_idxs)[0]
+    #     num_topk = min(len(topk_idxs), topk_candidates)
+    #     sort_idxs = scores_per_level.argsort()[-num_topk:][::-1]
+    #     topk_idxs, scores_per_level = topk_idxs[sort_idxs], scores_per_level[sort_idxs]
 
-        # bbox coords from offsets
-        anchor_idxs = topk_idxs // self.num_classes
-        labels_per_level = topk_idxs % self.num_classes
-        boxes_per_level = decode_bbox(offsets_per_level[anchor_idxs], anchors_per_level[anchor_idxs])
-        # clip to image size
-        clipped_x = boxes_per_level[:, 0::2].clip(0, w)
-        clipped_y = boxes_per_level[:, 1::2].clip(0, h)
-        boxes_per_level = np.stack([clipped_x, clipped_y], axis=2).reshape(-1, 4)
+    #     # bbox coords from offsets
+    #     anchor_idxs = topk_idxs // self.num_classes
+    #     labels_per_level = topk_idxs % self.num_classes
+    #     boxes_per_level = decode_bbox(offsets_per_level[anchor_idxs], anchors_per_level[anchor_idxs])
+    #     # clip to image size
+    #     clipped_x = boxes_per_level[:, 0::2].clip(0, w)
+    #     clipped_y = boxes_per_level[:, 1::2].clip(0, h)
+    #     boxes_per_level = np.stack([clipped_x, clipped_y], axis=2).reshape(-1, 4)
 
-        image_boxes.append(boxes_per_level)
-        image_scores.append(scores_per_level)
-        image_labels.append(labels_per_level)
+    #     image_boxes.append(boxes_per_level)
+    #     image_scores.append(scores_per_level)
+    #     image_labels.append(labels_per_level)
 
-      image_boxes = np.concatenate(image_boxes)
-      image_scores = np.concatenate(image_scores)
-      image_labels = np.concatenate(image_labels)
+    #   image_boxes = np.concatenate(image_boxes)
+    #   image_scores = np.concatenate(image_scores)
+    #   image_labels = np.concatenate(image_labels)
 
-      # nms for each class
-      keep_mask = np.zeros_like(image_scores, dtype=bool)
-      for class_id in np.unique(image_labels):
-        curr_indices = np.where(image_labels == class_id)[0]
-        curr_keep_indices = nms(image_boxes[curr_indices], image_scores[curr_indices], nms_thresh)
-        keep_mask[curr_indices[curr_keep_indices]] = True
-      keep = np.where(keep_mask)[0]
-      keep = keep[image_scores[keep].argsort()[::-1]]
+    #   # nms for each class
+    #   keep_mask = np.zeros_like(image_scores, dtype=bool)
+    #   for class_id in np.unique(image_labels):
+    #     curr_indices = np.where(image_labels == class_id)[0]
+    #     curr_keep_indices = nms(image_boxes[curr_indices], image_scores[curr_indices], nms_thresh)
+    #     keep_mask[curr_indices[curr_keep_indices]] = True
+    #   keep = np.where(keep_mask)[0]
+    #   keep = keep[image_scores[keep].argsort()[::-1]]
 
-      # resize bboxes back to original size
-      image_boxes = image_boxes[keep]
-      if orig_image_sizes is not None:
-        resized_x = image_boxes[:, 0::2] * orig_image_sizes[i][1] / w
-        resized_y = image_boxes[:, 1::2] * orig_image_sizes[i][0] / h
-        image_boxes = np.stack([resized_x, resized_y], axis=2).reshape(-1, 4)
-      # xywh format
-      image_boxes = np.concatenate([image_boxes[:, :2], image_boxes[:, 2:] - image_boxes[:, :2]], axis=1)
+    #   # resize bboxes back to original size
+    #   image_boxes = image_boxes[keep]
+    #   if orig_image_sizes is not None:
+    #     resized_x = image_boxes[:, 0::2] * orig_image_sizes[i][1] / w
+    #     resized_y = image_boxes[:, 1::2] * orig_image_sizes[i][0] / h
+    #     image_boxes = np.stack([resized_x, resized_y], axis=2).reshape(-1, 4)
+    #   # xywh format
+    #   image_boxes = np.concatenate([image_boxes[:, :2], image_boxes[:, 2:] - image_boxes[:, :2]], axis=1)
 
-      detections.append({"boxes":image_boxes, "scores":image_scores[keep], "labels":image_labels[keep]})
-    return detections
+    #   detections.append({"boxes":image_boxes, "scores":image_scores[keep], "labels":image_labels[keep]})
+    # return detections
 
 class ClassificationHead:
   def __init__(self, in_channels:int, num_anchors:int, num_classes:int):
@@ -207,6 +217,10 @@ class ResNetFPN:
   # this is needed to decouple inference from postprocessing (anchors generation)
   def compute_grid_sizes(self, input_size):
     return np.ceil(np.array(input_size)[None, :] / 2 ** np.arange(3, 8)[:, None])
+  
+  # this is needed to decouple inference from postprocessing (anchors generation)
+  def compute_grid_sizes2(self, input_size:tuple[int, int]) -> Tensor:
+    return (Tensor(input_size)[None, :] / 2.0 ** Tensor.arange(3, 8)[:, None]).ceil()
 
   def __call__(self, x:Tensor):
     out = self.body.bn1(self.body.conv1(x)).relu()
