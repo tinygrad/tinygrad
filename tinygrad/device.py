104a8384bc9d45b2660f0618b48dbbf1f593cd7d
ignaciosica <mignacio.sica@gmail.com>
2025-04-24 18:14:11 -0300

    minor cleanup

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -333,7 +333,7 @@ def is_dtype_supported(dtype:DType, device:Optional[str]=None) -> bool:
     if device == "METAL": return not CI
     if device in {"CUDA", "NV"}: return not CI and not getenv("PTX")
     if device in {"CPU", "LLVM"}: return not CI and platform.machine() in {"arm", "arm64", "aarch64", "x86_64", "amd64"}
-    return device in {"AMD"}
+    return device == "AMD"
   if dtype in dtypes.fp8s:
     # not supported yet - in progress
     return False
