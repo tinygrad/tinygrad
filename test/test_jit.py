754d789f510a8b5d2468600fec56e765dd360ea6
uuuvn <83587632+uuuvn@users.noreply.github.com>
2025-04-24 20:39:31 +0500

    Fix and enable jit tests on CLOUD (#10031)

--- test/test_jit.py
+++ test/test_jit.py
@@ -456,7 +456,7 @@ class TestJit(unittest.TestCase):
       np.testing.assert_allclose(a.numpy(), xc.numpy(), atol=1e-4, rtol=1e-5)
       np.testing.assert_allclose(b.numpy(), yc.numpy(), atol=1e-4, rtol=1e-5)
 
-  @unittest.skipIf(CI and Device.DEFAULT in {"GPU", "CUDA", "METAL"}, "no GPU/CUDA/METAL in CI, fine to run on AMD/NV")
+  @unittest.skipIf(not_support_multi_device(), "no multi")
   def test_jitted_view(self):
     d0, d1 = f"{Device.DEFAULT}:0", f"{Device.DEFAULT}:1"
 
