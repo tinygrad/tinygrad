c3ad7b2a84b73125468b985c434b6a7517c54eb8
Park Jun <pj2417@gmail.com>
2025-04-24 20:29:02 +0900

    create randperm and support pytorch backend (#10019)

--- extra/torch_backend/backend.py
+++ extra/torch_backend/backend.py
@@ -112,7 +112,8 @@ def index_put(self, indices, values, accumulate=False):
 def isin_tensor_tensor_out(x, y, *, assume_unique=False, invert=False, out=None): return out.copy_(aten.isin(x.cpu(), y.cpu(), assume_unique=assume_unique, invert=invert).tiny())
 
 @torch.library.impl("aten::randperm.generator_out", "privateuseone")
-def randperm_generator(n, generator=None, out=None): out.copy_(torch.randperm(n, generator=generator, device="cpu").tiny())
+def randperm_generator(n, generator=None, out=None):
+  return out.copy_(wrap(Tensor.randperm(n, generator=generator, device=unwrap(out).device)))
 
 @torch.library.impl("aten::cummax", "privateuseone")
 def cummax(self, dim):
