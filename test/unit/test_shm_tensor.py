6f6226ca355318f563700edc327397ec2c4a83f5
leopf <l.pfob@3-klicks.de>
2025-04-24 12:18:43 +0200

    check rlimit, skip if too low

--- test/unit/test_shm_tensor.py
+++ test/unit/test_shm_tensor.py
@@ -1,12 +1,14 @@
-import unittest
+import unittest, sys
 import multiprocessing.shared_memory as shared_memory
-from tinygrad.helpers import CI
 from tinygrad.tensor import Tensor, Device
 import numpy as np
 
 class TestRawShmBuffer(unittest.TestCase):
-  def test_e2e(self):
-    t = Tensor.randn(2, 2, 2).realize()
+  def _test_e2e(self, t: Tensor):
+    if sys.platform != "win32":
+      import resource
+      rlimit = resource.getrlimit(resource.RLIMIT_MEMLOCK)[0]
+      if rlimit < t.nbytes(): raise unittest.SkipTest(f"This test requires a rlimit of at least {t.nbytes()//1024} KiB.")
 
     # copy to shm
     shm_name = (s := shared_memory.SharedMemory(create=True, size=t.nbytes())).name
@@ -19,20 +21,8 @@ class TestRawShmBuffer(unittest.TestCase):
     assert np.allclose(t.numpy(), t2.numpy())
     s.unlink()
 
-  @unittest.skipIf(CI, "CI doesn't like big shared memory")
-  def test_e2e_big(self):
-    t = Tensor.randn(2048, 2048, 8).realize()
-
-    # copy to shm
-    shm_name = (s := shared_memory.SharedMemory(create=True, size=t.nbytes())).name
-    s.close()
-    t_shm = t.to(f"disk:shm:{shm_name}").realize()
-
-    # copy from shm
-    t2 = t_shm.to(Device.DEFAULT).realize()
-
-    assert np.allclose(t.numpy(), t2.numpy())
-    s.unlink()
+  def test_e2e_small(self): self._test_e2e(Tensor.randn(2, 2, 2).realize())
+  def test_e2e_big(self): self._test_e2e(Tensor.randn(2048, 2048, 8).realize())
 
 if __name__ == "__main__":
   unittest.main()
