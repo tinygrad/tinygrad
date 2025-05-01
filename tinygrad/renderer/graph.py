034fa1afd7e68afc7aacf882816c75ad80a65ec5
hooved <172129504+hooved@users.noreply.github.com>
2025-05-01 10:16:24 -0400

    refactor graph definition

--- tinygrad/renderer/graph.py
+++ tinygrad/renderer/graph.py
@@ -1,5 +1,6 @@
 # render the kernel graph for execution outside tinygrad
 from tinygrad.tensor import Tensor
+from tinygrad.device import Buffer
 from tinygrad.ops import UOp, Variable
 from tinygrad.renderer import Renderer
 from tinygrad.runtime.ops_webgpu import JavaScriptRenderer
@@ -9,30 +10,34 @@ from tinygrad.engine.realize import lower_schedule
 from tinygrad.nn.state import get_parameters
 from typing import Callable, Sequence
 
+# Common logic regardless of target (e.g. WebGPU-JS, C)
 class GraphRenderer(Renderer):
-  def __init__(self, model:Callable, inputs:Sequence, device:str):
-    # Inputs
-    # JS model input args will have same relative positions as Tensors/Variables within above inputs
-    #   only Tensor and Variable args will be captured as inputs, the rest is baked into the model if relevant
-    #   Tensor input becomes JS typed array of closest type, e.g. Uint8Array, Int32Array
-    #   Variable input becomes JS number type
-    # TODO: don't use all positional args? instead use Object w/ names for Variable args, or for everything?
-    # TODO: don't realize input Tensors?
-    in_args = [(x.realize() if isinstance(x, Tensor) else x) for x in inputs if isinstance(x, (Tensor, Variable))]
-  
-    # Outputs
-    # JS model output is an array of typed arrays whose order matches get_parameters output
-    # TODO: if only one output, return a typed array instead of array of typed arrays?
-    # TODO: assert no realizes happen within model, only kernelize is allowed
-    output = [t.kernelize() for t in get_parameters(model(*inputs))]
-
-    schedule, var_vals, _ = create_schedule_with_vars(UOp.sink(*[t.lazydata for t in output]))
+  def __init__(self, inputs:list[UOp], outputs:list[UOp]):
+    schedule, var_vals, _ = create_schedule_with_vars(UOp.sink(*outputs))
     for si, ei in lower_schedule(memory_planner(schedule)):
       continue
+
+def define_graph(fxn:Callable, inputs:Sequence) -> tuple[list[UOp], list[UOp]]:
+  # Inputs
+  # JS model input args will have same relative positions as Tensors/Variables within above inputs
+  #   only Tensor and Variable args will be captured as inputs, the rest is baked into the model if relevant
+  #   Tensor input becomes JS typed array of closest type, e.g. Uint8Array, Int32Array
+  #   Variable input becomes JS number type
+  # TODO: don't have all args be positional? instead use Object w/ names for Variable args, or for everything?
+  dynamic_inputs = [(x.realize().lazydata if isinstance(x, Tensor) else x) for x in inputs if isinstance(x, (Tensor, Variable))]
+  
+  # Outputs
+  # JS model output is an array of typed arrays whose order matches outputs
+  # TODO: assert no realizes happen within model, only kernelize is allowed
+  # TODO: if only one output, return a typed array instead of array of typed arrays?
+  outputs = [t.kernelize().lazydata for t in get_parameters(fxn(*inputs))]
+
+  # NOTE: kernel graph is implicit from outputs
+  return dynamic_inputs, outputs
 
 def export_webgpu(model:Callable, inputs:Sequence) -> tuple[str, dict[str, Tensor]]:
   """
   Renders the model's kernel graph into JavaScript and exports the model's state.
   """
-  graph = GraphRenderer(model, inputs, "WEBGPU")
+  inputs, outputs = define_graph(model, inputs)
   return
