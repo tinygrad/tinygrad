d65a91649f100f3406cc193fdd716ebc6fab7c33
quortus <156855065+quortus@users.noreply.github.com>
2025-05-01 19:41:20 +0000

    Rollback parameter name change

--- tinygrad/runtime/graph/cpu.py
+++ tinygrad/runtime/graph/cpu.py
@@ -11,29 +11,29 @@ from tinygrad.renderer.llvmir import LLVMRenderer, ldt
 
 T = TypeVar('T')
 class BatchedGraph(Generic[T], GraphRunner):
-  def __init__(self, device, jit_cache: list[ExecItem], input_rawbufs: list[Buffer], var_vals: dict[Variable, int]):
+  def __init__(self, device, jit_cache: list[ExecItem], input_rawbuffers: list[Buffer], var_vals: dict[Variable, int]):
     renderer_class = get_typing_args(getattr(self, "__orig_bases__")[0])[0]
     if not issubclass(type(device.renderer), renderer_class) and not isinstance(device.renderer, renderer_class): raise GraphException
 
-    super().__init__(jit_cache, input_rawbufs, var_vals)
-    self.base_bufs = dedup(b.base for ji in jit_cache for b in ji.bufs if b is not None and b not in input_rawbufs)
+    super().__init__(jit_cache, input_rawbuffers, var_vals)
+    self.base_bufs = dedup(b.base for ji in jit_cache for b in ji.bufs if b is not None and b not in input_rawbuffers)
     self.base_rawbufs = [b._buf for b in self.base_bufs]
 
-    targs = [(f"arg{i}", x.dtype.ptr()) for i,x in enumerate(input_rawbufs)] + \
+    targs = [(f"arg{i}", x.dtype.ptr()) for i,x in enumerate(input_rawbuffers)] + \
             [(f"cbuf{i}", dtypes.char.ptr()) for i in range(len(self.base_bufs))] + \
             sorted([(f"{v.expr}", dtypes.int) for v in var_vals])
-    code = self._prepare_code(device.renderer, jit_cache, input_rawbufs, targs)
+    code = self._prepare_code(device.renderer, jit_cache, input_rawbuffers, targs)
     if DEBUG >= 4: print(code)
     self.clprg = device.runtime("batched", device.compiler.compile_cached(code))
 
-  def _prepare_code(self, renderer:T, jit_cache:list[ExecItem], input_rawbufs:list[Buffer], targs:list[tuple[str, DType]]) -> str: return ""
+  def _prepare_code(self, renderer:T, jit_cache:list[ExecItem], input_rawbuffers:list[Buffer], targs:list[tuple[str, DType]]) -> str: return ""
   def __call__(self, rawbufs: list[Buffer], var_vals: dict[Variable, int], wait=False):
     return self.clprg(*[x._buf for x in rawbufs], *self.base_rawbufs, *[x[1] for x in sorted(var_vals.items(), key=lambda x: x[0].expr)], wait=wait)
 
 class CPUGraph(BatchedGraph[ClangRenderer]):
-  def _prepare_code(self, renderer:ClangRenderer, jit_cache:list[ExecItem], input_rawbufs:list[Buffer], targs:list[tuple[str, DType]]) -> str:
+  def _prepare_code(self, renderer:ClangRenderer, jit_cache:list[ExecItem], input_rawbuffers:list[Buffer], targs:list[tuple[str, DType]]) -> str:
     def render_arg(buf):
-      if buf in input_rawbufs: return f"arg{input_rawbufs.index(buf)}"
+      if buf in input_rawbuffers: return f"arg{input_rawbuffers.index(buf)}"
       return f"({renderer.render_dtype(buf.dtype)}*)(cbuf{self.base_bufs.index(buf.base)} + {buf.offset})"
 
     batched = ["void batched("+','.join([f"{renderer.render_dtype(x[1])} {x[0]}" for x in targs])+") {"]
@@ -50,13 +50,13 @@ class CPUGraph(BatchedGraph[ClangRenderer]):
     return '\n'.join(defines) + '\n' + '\n'.join([''.join(f) for f in funcs]) + '\n'.join(batched) + '\n' + entry
 
 class LLVMGraph(BatchedGraph[LLVMRenderer]):
-  def _prepare_code(self, renderer, jit_cache:list[ExecItem], input_rawbufs:list[Buffer], targs:list[tuple[str, DType]]) -> str:
+  def _prepare_code(self, renderer, jit_cache:list[ExecItem], input_rawbuffers:list[Buffer], targs:list[tuple[str, DType]]) -> str:
     out = []
     for i,ji in enumerate(jit_cache):
       args = []
       for j,buf in enumerate(cast(list[Buffer], ji.bufs)):
-        arg = f"%arg{input_rawbufs.index(buf)}" if buf in input_rawbufs else f"%b{i}_{j}"
-        if buf not in input_rawbufs: out.append(f"  {arg} = getelementptr inbounds i8, ptr %cbuf{self.base_bufs.index(buf.base)}, i64 {buf.offset}")
+        arg = f"%arg{input_rawbuffers.index(buf)}" if buf in input_rawbuffers else f"%b{i}_{j}"
+        if buf not in input_rawbuffers: out.append(f"  {arg} = getelementptr inbounds i8,ptr %cbuf{self.base_bufs.index(buf.base)},i64 {buf.offset}")
         args.append(f"{ldt(buf.dtype.ptr())} {arg}")
       args += [f"{ldt(x.dtype)} %{x.expr}" for x in cast(CompiledRunner, ji.prg).p.vars]
       out.append(f"  call void @{to_function_name(cast(CompiledRunner, ji.prg).p.name)}({','.join(args)})")
