bfa9a759e601941f3d42e2842654988b3f1ba7bf
George Hotz <geohot@gmail.com>
2025-05-04 16:16:45 -0700

    once is fine

--- test/test_opt_gemm.py
+++ test/test_opt_gemm.py
@@ -11,18 +11,18 @@ class TestOptGemm(unittest.TestCase):
     N = 64
     cls.a = Tensor.randn(N, N).contiguous().realize()
     cls.b = Tensor.randn(N, N).contiguous().realize()
+    cls.res = cls.a.T.numpy() @ cls.b.T.numpy()
 
   def _test_gemm_unrolled_permute_l(self, opts=[]):
     t = self.a.T @ self.b.T
-    res = self.a.T.numpy() @ self.b.T.numpy()
     # TODO: this should be a generic test helper
     si = get_single_element(t.schedule())
     k = Kernel(si.ast)
     k.apply_opts(opts)
     run = CompiledRunner(k.to_program())
     ExecItem(run, si.bufs).run()
-    test = si.bufs[0].numpy().reshape(res.shape)
-    np.testing.assert_allclose(res, test, atol=1e-4)
+    test = si.bufs[0].numpy().reshape(self.res.shape)
+    np.testing.assert_allclose(self.res, test, atol=1e-4)
 
   def test_gemm_unrolled_permute_l_44(self):
     opts = [Opt(op=OptOps.UPCAST, axis=0, arg=4), Opt(op=OptOps.UPCAST, axis=1, arg=4)]
