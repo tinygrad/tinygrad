5f3f162606f7f147067ac9c3893bafe64669a02a
George Hotz <72895+geohot@users.noreply.github.com>
2025-05-04 16:45:15 -0400

    cache rewrites for renderer [pr] (#10155)
    * add caching to rewrites for renderer [pr]
    
    * remove that
    
    * update ebs

--- tinygrad/codegen/flow.py
+++ tinygrad/codegen/flow.py
@@ -24,10 +24,15 @@ class RewriteStep:
 
 def apply_rewrites(sink:UOp, rewrites:list[RewriteStep]): return functools.reduce(lambda x,f: f(x), rewrites, sink)
 
-def get_rewrites_for_renderer(opts:Renderer, linearizer=True) -> list[RewriteStep]:
+def get_rewrites_for_renderer(opts:Renderer, linearizer:bool=True) -> list[RewriteStep]:
+  # cache with the values of the context vars
+  return _get_rewrites_for_renderer(opts, linearizer, QUANTIZE.value, DEVECTORIZE.value, TRANSCENDENTAL.value)
+
+@functools.cache
+def _get_rewrites_for_renderer(opts:Renderer, linearizer:bool, _QUANTIZE, _DEVECTORIZE, _TRANSCENDENTAL) -> list[RewriteStep]:
   # ** lowerer (rewrite_shapetracker_with_index) **
   ret: list[RewriteStep] = []
-  if QUANTIZE and opts.device in {"CPU", "DSP"}: ret.append(RewriteStep(pm_quant, name="quantize"))
+  if _QUANTIZE and opts.device in {"CPU", "DSP"}: ret.append(RewriteStep(pm_quant, name="quantize"))
   ret.append(RewriteStep(pm_lowerer, lambda ast: get_index(ast, opts), name="lowerer"))
 
   # ** expander (expand_rewrite) **
@@ -45,8 +50,8 @@ def get_rewrites_for_renderer(opts:Renderer, linearizer=True) -> list[RewriteSte
   ret.append(RewriteStep(pm_reduce+gep_pushing, lambda _: ReduceContext(), name="remove_reduce"))
 
   # devectorize (TODO: does this need opts?)
-  if DEVECTORIZE >= 2: pm_devectorize = sym+load_store_folding+load_store_indexing
-  elif DEVECTORIZE: pm_devectorize = sym+devectorize+load_store_folding+correct_load_store+load_store_indexing
+  if _DEVECTORIZE >= 2: pm_devectorize = sym+load_store_folding+load_store_indexing
+  elif _DEVECTORIZE: pm_devectorize = sym+devectorize+load_store_folding+correct_load_store+load_store_indexing
   else: pm_devectorize = sym+load_store_folding+correct_load_store+load_store_indexing
   ret.append(RewriteStep(pm_devectorize, lambda _: opts, name="devectorize"))
 
@@ -57,7 +62,7 @@ def get_rewrites_for_renderer(opts:Renderer, linearizer=True) -> list[RewriteSte
   if opts.pre_matcher is not None: ret.append(RewriteStep(opts.pre_matcher, name="pre_matcher"))
 
   # final rules for the renderer (without sym)
-  pm_final_rewrite = symbolic_simple+get_late_rewrite_patterns(supported_ops, TRANSCENDENTAL>=2)+pm_render+extra_matcher
+  pm_final_rewrite = symbolic_simple+get_late_rewrite_patterns(supported_ops, _TRANSCENDENTAL>=2)+pm_render+extra_matcher
   ret.append(RewriteStep(pm_final_rewrite, lambda _: opts, name="final rewrite"))
 
   # ** linearizer **
