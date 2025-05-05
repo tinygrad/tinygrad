4889cebb2b2e1989abbb4e36029abf606b703b7a
zibokapi <zibokapi@gmail.com>
2025-05-05 17:13:40 +0800

    bless FUSE_ARANGE

--- examples/olmoe.py
+++ examples/olmoe.py
@@ -1,9 +1,11 @@
 # https://arxiv.org/pdf/2409.02060
-import time
+import time, os
+if "FLOAT16" not in os.environ: os.environ["FUSE_ARANGE"] = "1"
+if "FUSE_ARANGE_UINT" not in os.environ: os.environ["FUSE_ARANGE_UINT"] = "0"
 import numpy as np
 np.set_printoptions(suppress=True, linewidth=1000)
 import functools
-from tinygrad import Tensor, nn, Device, GlobalCounters, Context
+from tinygrad import Tensor, nn, Device, GlobalCounters
 from tinygrad.helpers import Timing, getenv
 from extra.models.llama import Transformer, convert_from_huggingface
 
@@ -22,19 +24,15 @@ class MixtureFeedForward:
     g = g.squeeze() # (BS, length, num_experts) -> (num_experts,)
     probs, sel = g.topk(self.activated_experts)
 
-    # print(f"11111 mem_used: {GlobalCounters.global_mem/1e9:.2f} GB")
-    with Context(FUSE_ARANGE=1):
-      selected_gate_projs = self.gate_proj[sel]
-      selected_up_projs = self.up_proj[sel]
-      selected_down_projs = self.down_proj[sel]
-      selected_gate_projs.realize(selected_up_projs, selected_down_projs)
-    # print(f"222222 mem_used: {GlobalCounters.global_mem/1e9:.2f} GB")
+    sel = sel.kernelize()
+    selected_gate_projs = self.gate_proj[sel].kernelize()
+    selected_up_projs = self.up_proj[sel].kernelize()
+    selected_down_projs = self.down_proj[sel].kernelize()
 
     # run MoE
     x_up_gate = x.dot(selected_gate_projs.permute(0,2,1)).silu() * x.dot(selected_up_projs.permute(0,2,1))
     x_down = x_up_gate.dot(selected_down_projs.permute(0,2,1))
     ret = (x_down.float() * probs.reshape(self.activated_experts, 1, 1)).sum(axis=0)
-    # print(f"333333 mem_used: {GlobalCounters.global_mem/1e9:.2f} GB")
     return ret
 
 # model is bf16, 1.3B active, 6.9B total
@@ -47,12 +45,7 @@ def fetch_weights() -> dict[str, Tensor]:
   m3 = Tensor.from_url("https://huggingface.co/allenai/OLMoE-1B-7B-0924/resolve/main/model-00003-of-00003.safetensors").to(Device.DEFAULT)
   return {**nn.state.safe_load(m1), **nn.state.safe_load(m2), **nn.state.safe_load(m3)}
 
-def filter_layers(state, layers):
-  lay = [f"model.layers.{i}." for i in range(layers)]
-  return {k: v for k, v in state.items() if not k.startswith("model.layers.") or any(k.startswith(l) for l in lay)}
-
 if __name__ == "__main__":
-  LAYERS = 2
   if getenv("TORCH"):
     from transformers import OlmoeForCausalLM, AutoTokenizer
     model = OlmoeForCausalLM.from_pretrained("allenai/OLMoE-1B-7B-0924")
@@ -64,13 +57,13 @@ if __name__ == "__main__":
     exit(0)
 
   with Timing("create model: "):
-    model = Transformer(n_layers=LAYERS, dim=2048, hidden_dim=1024, n_heads=16, norm_eps=1e-5, qk_norm=1e-5, max_context=1024,
+    model = Transformer(n_layers=16, dim=2048, hidden_dim=1024, n_heads=16, norm_eps=1e-5, qk_norm=1e-5, max_context=1024,
                         vocab_size=50304, feed_forward=functools.partial(MixtureFeedForward, 64, 8))
     model_state_dict = nn.state.get_state_dict(model)
     del model_state_dict['freqs_cis']
 
   with Timing("load weights to GPU: "):
-    nhf_state = convert_from_huggingface(filter_layers(fetch_weights(), LAYERS), model, 16, 16)
+    nhf_state = convert_from_huggingface(fetch_weights(), 16, 16, 16)
     # NOTE: i'm not sure this actually needs float32, it may just change the type of things downstream from it. but doesn't match torch w/o this
     for needs_float32 in ['tok_embeddings.weight']: nhf_state[needs_float32] = nhf_state[needs_float32].float()
   print(f"ram used: {GlobalCounters.mem_used/1e9:.2f} GB")
@@ -98,28 +91,9 @@ if __name__ == "__main__":
     timings.append(time.perf_counter()-st)
     toks.append(tok)
     start_pos += 1
-    print(toks)
-    print(tokenizer.decode(toks))
-    print(f"global_mem: {GlobalCounters.global_mem/1e9:.2f} GB")
-    print(f"global_ops: {GlobalCounters.global_ops:,} ops")
-    print(f"mem_used: {GlobalCounters.mem_used/1e9:.2f} GB")
-    print(f"kernel_count: {GlobalCounters.kernel_count:,} kernels")
   print(f"fastest token {min(timings)*1e3:.2f} ms, {1/min(timings):.1f} tok/s")
 
-  # if temperature == 0:
-  #   # Hello, I am a newbie to this forum and I am trying to get a better understanding of the different types of data that can be stored in a
-  #   assert toks == [12092, 13, 309, 717, 247, 747, 17782, 281, 436, 12209, 285, 309, 717, 2820, 281, 755,
-  #                   247, 1805, 4685, 273, 253, 1027, 3510, 273, 941, 326, 476, 320, 7141, 275, 247], "BAD OUTPUT!"
-
-
-# global_mem: 2.80 GB
-# global_ops: 3,663,325,624 ops
-# mem_used: 16.78 GB
-
-# global_mem: 2.80 GB
-# global_ops: 4,810,369,464 ops
-# mem_used: 16.78 GB
-
-# global_mem: 2.80 GB
-# global_ops: 13,624,553,216 ops
-# mem_used: 16.58 GB
+  if temperature == 0:
+    # Hello, I am a newbie to this forum and I am trying to get a better understanding of the different types of data that can be stored in a
+    assert toks == [12092, 13, 309, 717, 247, 747, 17782, 281, 436, 12209, 285, 309, 717, 2820, 281, 755,
+                    247, 1805, 4685, 273, 253, 1027, 3510, 273, 941, 326, 476, 320, 7141, 275, 247], "BAD OUTPUT!"
