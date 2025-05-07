685d5c46df5bd7a14af7d495a6273575e1124d94
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-05-07 14:41:56 +0300

    usbgpu: send pci write in batches (#10190)
    * usbgpu: send pci write in batches
    
    * mock

--- test/mockgpu/usb.py
+++ test/mockgpu/usb.py
@@ -11,3 +11,6 @@ class MockUSB:
   def pcie_mem_req(self, address, value=None, size=1):
     if value is None: return int.from_bytes(self.mem[address:address+size], "little")
     else: self.mem[address:address+size] = value.to_bytes(size, "little")
+
+  def pcie_mem_write(self, address, values, size):
+    for i, value in enumerate(values): self.pcie_mem_req(address + i * size, value, size)
