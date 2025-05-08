7511cd8e1fab512a140cd7382ddc4db8fbf6055c
hooved <172129504+hooved@users.noreply.github.com>
2025-05-08 13:48:02 -0400

    render webgpu compute

--- tinygrad/renderer/graph/webgpu.py
+++ tinygrad/renderer/graph/webgpu.py
@@ -1,13 +1,11 @@
 from tinygrad import Tensor
-from tinygrad.ops import Ops
+from tinygrad.ops import Ops, Variable
 from tinygrad.runtime.ops_webgpu import js_init_device, js_init_encoder, js_alloc, js_copyin, js_copy, js_copyout, js_create_layout, \
   js_create_pipeline, js_create_bind_group, js_begin_compute_pass
-from tinygrad.renderer import ProgramSpec
 from tinygrad.renderer.graph import GraphRenderer
 from tinygrad.engine.realize import CompiledRunner
+from tinygrad.helpers import merge_dicts
 from typing import Callable, Sequence
-
-def declare_kernel(p:ProgramSpec) -> str: return f"const {p.function_name} = `{p.src.replace(p.function_name, 'main')}`;"
 
 safe_load_state_dict = f"""const safeLoadStateDict = async (modelStateDict, safetensorPath) => {{
   const safetensorBuffer = await fetch(safetensorPath).then(x => x.arrayBuffer()).then(x => new Uint8Array(x));
@@ -24,28 +22,26 @@ empty = (state:="GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST") + " | GPUBuf
 uniform, copyout = "GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST", "GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ"
 buf_usages = [f"const {label} = {usage};" for label, usage in zip(("empty", "state", "uniform", "copyout"), (empty, state, uniform, copyout))]
 
-add_compute_pass = f"""const addComputePass = (commandEncoder, layout, pipeline, buffers, globalDims, infinityBuf) => {{
-  const entries = [...[infinityBuf].concat(buffers).map((buffer, index) => ({{ binding: index, resource: {{ buffer }} }})))];
-  const bindGroup = {js_create_bind_group("layout", "entries")};
-  {"  \n".join(js_begin_compute_pass("commandEncoder", "pipeline", "bindGroup", "globalDims"))}
-}};"""
-
 class WebGPUJSRenderer(GraphRenderer):
   def render_graph(self) -> str:
-    prg: list[str] = js_init_device + safe_load_state_dict.split("\n") + buf_usages
-    prg.append("const setupNet = async (safetensorPath) => {")
 
     # render I/O between host/WebGPU
-    input_bufs, input_copyins, output_bufs, output_read_bufs, output_copies, ret_bufs, output_copyouts = [], [], [], [], [], [], []
+    kernel_bufs = merge_dicts(self.empty_bufs, self.state_bufs)
+    arg_names, input_bufs, input_copyins, output_bufs, output_read_bufs, output_copies, ret_bufs, output_copyouts = [], [], [], [], [], [], [], []
+    # render inputs
     for i, uop in enumerate(self.inputs):
       if uop.base.op is Ops.BUFFER: # from Tensor input
-        input_bufs.append(f"const input_{i} = {js_alloc(str(uop.base.buffer.nbytes), "empty")};")
-        input_copyins.append(js_copyin(f"input_{i}", f"_input_{i}"))
-
+        arg_names.append(f"bufArg_{i}")
+        input_bufs.append(f"const inputBuf_{i} = {js_alloc(str(uop.buffer.nbytes), "empty")};")
+        input_copyins.append(js_copyin(f"inputBuf_{i}", f"bufArg_{i}"))
+        kernel_bufs[uop.buffer] = f"inputBuf_{i}"
       elif uop.op is Ops.BIND: # from symbolic variable input
-        input_bufs.append(f"const input_{i} = {js_alloc("4", "uniform")};")
-        input_copyins.append(js_copyin(f"input_{i}", f"new Int32Array([{uop.unbind()[0].arg[0]}])"))
+        arg_names.append(f"intArg_{i}")
+        input_bufs.append(f"const inputBuf_{i} = {js_alloc("4", "uniform")};")
+        input_copyins.append(js_copyin(f"inputBuf_{i}", f"new Int32Array([{f"intArg_{i}"}])"))
+        kernel_bufs[uop.unbind()[0]] = f"inputBuf_{i}"
 
+    # render outputs
     command_encoder = f"const commandEncoder = {js_init_encoder};"
     for i, uop in enumerate(self.outputs):
       output_bufs.append(f"const output_{i} = {js_alloc(str(uop.base.buffer.nbytes), "empty")};")
@@ -54,7 +50,7 @@ class WebGPUJSRenderer(GraphRenderer):
       ret_bufs.append(f"const ret_{i} = new Uint8Array(output_{i}.size);")
       output_copyouts.append(js_copyout(f"ret_{i}", f"outputReader_{i}"))
 
-    # render WebGPU buffer setup
+    # render setup of buffers used only by WebGPU kernels
     empty_bufs = [f"const {name} = {js_alloc(str(buf.nbytes), "empty")};" for buf, name in self.empty_bufs.items()]
     state_dict_kv_pairs = [f'"{name}": {js_alloc(str(buf.nbytes), "state")}' for buf, name in self.state_bufs.items()]
     state_dict = f"const stateDict = {{ {",\n".join(state_dict_kv_pairs)} }};"
@@ -64,17 +60,36 @@ class WebGPUJSRenderer(GraphRenderer):
     write_infinity = f'{js_copyin(f"infinityBuf", f"new Float32Array([Infinity])")}'
 
     # render WebGPU compute
-    declare_kernels = list(set(declare_kernel(ei.prg.p) for ei in self.eis if isinstance(ei.prg, CompiledRunner)))
-    call_order = f"const kernels = [{", ".join(ei.prg.p.function_name for ei in self.eis if isinstance(ei.prg, CompiledRunner))}];"
-    layouts = f'const layouts = [{", ".join(js_create_layout(["uniform"] + ["storage"]*len(ei.bufs) + ["uniform"]*len(ei.prg.p.vars))
-                                            for ei in self.eis if isinstance(ei.prg, CompiledRunner))}]'
-    # group pipelines by kernel name, so developers can see a kernel name on every added compute pass, making debugging easier
-    pipelines = [f'const pending = kernels.reduce((r, k, i) => ((r[k] = r[k] || []).push({js_create_pipeline("layouts[i]", "k")})), r), {{}});',
-      "const pipelines = Object.fromEntries(await Promise.all(Object.entries(pending).map(async([k,ps]) => [k, await Promise.all(ps)])));"]
-    calls = [f"addComputePass(commandEncoder, ...)"]
-    
+    add_compute_pass = ["const addComputePass = (pipeline, buffers, workgroupDims, commandEncoder, layout) => {",
+      "  const entries = [...[infinityBuf].concat(buffers).map((buffer, index) => ({ binding: index, resource: { buffer } })))];",
+      f'  const bindGroup = {js_create_bind_group("layout", "entries")};', 
+      "  \n".join(js_begin_compute_pass("commandEncoder", "pipeline", "bindGroup", "workgroupDims")),
+    "};\n"]
 
+    layouts, kernels, kernel_name_sequence, compute_passes = [], {}, [], []
+    for i, (ei, p) in enumerate((ei, ei.prg.p) for ei in self.eis if isinstance(ei.prg, CompiledRunner)):
+
+      # first buf in every kernel is infinity_buf, a uniform buffer
+      layouts.append(js_create_layout(["uniform"] + ["storage"] * len(ei.bufs) + ["uniform"] * len(p.vars)))
+      kernels[p.function_name] = p.src.replace(p.function_name, "main")
+      # kernel_name_sequence becomes pipelines: a JS array of {p.function_name: GPUComputePipeline}
+      kernel_name_sequence.append(f'"{p.function_name}"') 
+      buf_names = ", ".join(kernel_bufs[arg] for arg in ei.bufs + p.vars)
+      global_size = ', '.join(idx.simplify().render() if isinstance(idx, Variable) else str(idx) for idx in p.global_size)
+
+      # deliberately display p.function_name in every addComputePass for easier debugging/understanding
+      compute_passes.append(f'addComputePass(pipelines[{i}]["{p.function_name}"], [{buf_names}], [{global_size}], commandEncoder, layouts[{i}]);')
+    
+    layouts = f'const layouts = [{", ".join(layouts)}];'
+    kernels = ["const kernels = {"] + [f'  "{k}": {v}' for k,v in kernels.items()] + ["};\n"]
+    make_pipelines = [f'const kernelNameSequence = [{", ".join(kernel_name_sequence)}];',
+      f'let pipelines = await Promise.all(kernelNameSequence.map((name, i) => {js_create_pipeline("layouts[i]", "kernels[name]")}));',
+      'pipelines = pipelines.map((pipeline, i) => { kernelNameSequence[i] : pipeline });']
+
+    prg: list[str] = js_init_device + safe_load_state_dict.split("\n") + buf_usages
+    prg.append("const setupNet = async (safetensorPath) => {")
     # TODO: complete rendering
+
     return "\n".join(prg)
 
 def export_webgpu(fxn:Callable, args:Sequence) -> tuple[str, dict[str, Tensor]]:
