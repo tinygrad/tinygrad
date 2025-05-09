cedc1bb47324aa70f4707e12e1ac6bbf6aa97212
qazal <qazal.software@gmail.com>
2025-05-09 16:47:49 +0300

    merge loops

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -991,9 +991,9 @@ def graph_rewrite(sink:UOp, pm:PatternMatcher, ctx=None, bottom_up=False, name=N
 def graph_rewrite_map(sink:UOp, pm:PatternMatcher, ctx=None, bottom_up=False, name=None, input_map:dict[UOp, UOp]|None=None) -> dict[UOp, UOp]:
   rewrite_ctx = RewriteContext(pm, ctx)
   new_map = {k:(rewrite_ctx.bottom_up_rewrite(k) if bottom_up else rewrite_ctx.top_down_rewrite(k)) for k in list(sink.toposort())[::-1]}
+  all_metadata.update((v, k.metadata) for k,v in new_map.items() if isinstance(k.metadata, Metadata))
   if input_map is not None:
     for k,v in input_map.items(): new_map[k] = new_map.get(v,v)
-  all_metadata.update((v, k.metadata) for k,v in new_map.items() if isinstance(k.metadata, Metadata))
   return new_map
 
 def sint_to_uop(x:sint, dtype:DType=dtypes.int) -> UOp: return UOp.const(dtype, x) if isinstance(x, int) else x
