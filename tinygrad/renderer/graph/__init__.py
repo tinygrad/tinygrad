7ef432ae5994a3d065fa1b47511bdd1fc85a60e7
hooved <172129504+hooved@users.noreply.github.com>
2025-05-10 11:49:48 -0400

    ruff, mypy

--- tinygrad/renderer/graph/__init__.py
+++ tinygrad/renderer/graph/__init__.py
@@ -1,19 +1,19 @@
 # render the kernel graph for execution outside tinygrad
-from tinygrad import Tensor
+from tinygrad import Tensor, dtype
+from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.device import Buffer
 from tinygrad.ops import UOp, Variable, Ops
 from tinygrad.renderer import Renderer
 from tinygrad.nn.state import get_parameters, get_state_dict
 from tinygrad.engine.schedule import create_schedule_with_vars
 from tinygrad.engine.memory import memory_planner
-from tinygrad.engine.realize import lower_schedule, ExecItem, BufferCopy
-from typing import Callable, Sequence
+from tinygrad.engine.realize import lower_schedule, ExecItem, CompiledRunner
+from typing import Callable, Sequence, cast
 import itertools
 
-def is_partial_write(ast:UOp, buf_idx:int) -> bool:
-  for u in ast.toposort():
-    if u.op is Ops.STORE and (buf:=u.src[0]).arg == buf_idx and buf.dtype.size > u.src[1].arg.size: return True
-  return False
+def is_partial_write(ast:UOp, i:int) -> bool:
+  stores = [u for u in ast.toposort() if u.op is Ops.STORE and isinstance(u.src[0].dtype, dtype.PtrDType) and isinstance(u.src[1].arg, ShapeTracker)]
+  return True if any((b:=u.src[0]).arg == i and cast(dtype.PtrDType, b.dtype).size > cast(ShapeTracker, u.src[1].arg).size for u in stores) else False
 
 # Common logic regardless of render target (e.g. JavaScript, C)
 class GraphRenderer(Renderer):
@@ -42,17 +42,16 @@ class GraphRenderer(Renderer):
     self.state_bufs: dict[Buffer, str] = dict()
     seen = set(self.empty_bufs.keys())
     for si, ei in lower_schedule(memory_planner(schedule)):
-      if isinstance(ei.prg, BufferCopy):
-        ei.run()
-        continue
+      assert isinstance(ei.prg, CompiledRunner), "BufferCopy not yet supported, ensure all data is realized on device."
       self.eis.append(ei)
       for i, buf in enumerate(ei.bufs):
+        assert buf is not None
         if buf not in seen and (i not in ei.prg.p.outs or i in ei.prg.p.ins or is_partial_write(si.ast, i)): self.state_bufs[buf] = f"buf_{next(ctr)}"
         elif buf not in seen: self.empty_bufs[buf] = f"buf_{next(ctr)}"
         seen.add(buf)
 
     self.state_bufs.update({k: v for k,v in buf_names.items() if k in self.state_bufs})
     # TODO: we need to ensure the self.state_bufs have been realized with actual data, before now
-    self.state_dict: dict[str, Tensor] = {v: Tensor(bytes(k.as_buffer()), dtype=k.dtype, device=k.device).realize() for k,v in self.state_bufs.items()}
+    self.state_dict: dict[str, Tensor] = {v:Tensor(bytes(k.as_buffer()), dtype=k.dtype, device=k.device).realize() for k,v in self.state_bufs.items()}
 
   def render_graph(self) -> str: raise NotImplementedError("Implement a language-specific GraphRenderer")
--- tinygrad/renderer/graph/webgpu.py
+++ tinygrad/renderer/graph/webgpu.py
@@ -1,10 +1,12 @@
 from tinygrad import dtypes
+from tinygrad.device import Buffer
 from tinygrad.ops import Ops, Variable
 from tinygrad.runtime.ops_webgpu.js import init_device, init_encoder, alloc, copyin, copy, copyout, create_layout, create_pipeline, \
   create_bind_group, begin_compute_pass
 from tinygrad.renderer.graph import GraphRenderer
 from tinygrad.engine.realize import CompiledRunner
 from tinygrad.helpers import merge_dicts
+from typing import cast
 
 safe_load_state_dict = f"""const safeLoadStateDict = async (modelStateDict, safetensorPath) => {{
   const safetensorBuffer = await fetch(safetensorPath).then(x => x.arrayBuffer()).then(x => new Uint8Array(x));
@@ -27,7 +29,7 @@ class WebGPUJSRenderer(GraphRenderer):
   def render_graph(self) -> str:
     # render I/O between host/WebGPU
     arg_names, input_copyins, sym_var_bufs, output_read_bufs, output_copies, ret_bufs, output_copyouts, ret_names = [], [], [], [], [], [], [], []
-    kernel_bufs = merge_dicts([self.empty_bufs, {k: f'stateDict["{v}"]' for k,v in self.state_bufs.items()}])
+    kernel_bufs = cast(dict[Buffer|Variable, str], merge_dicts([self.empty_bufs, {k: f'stateDict["{v}"]' for k,v in self.state_bufs.items()}]))
     # render inputs
     for i, uop in enumerate(self.inputs):
       if uop.base.op is Ops.BUFFER: # from Tensor input
@@ -56,12 +58,12 @@ class WebGPUJSRenderer(GraphRenderer):
     state_dict = ["const stateDict = {"] + indent(state_dict_kv_pairs, 1) + ["};"]
     # representing Infinity with a runtime buffer is the most correct way known, see https://github.com/tinygrad/tinygrad/pull/10179
     declare_infinity = f'const infinityBuf = {alloc("4", "uniform")};'
-    write_infinity = f'{copyin(f"infinityBuf", f"new Float32Array([Infinity])")}'
+    write_infinity = f'{copyin("infinityBuf", "new Float32Array([Infinity])")}'
 
     # render WebGPU compute
     add_compute_pass = ["const addComputePass = (pipeline, buffers, workgroupDims, commandEncoder, layout) => {",
       "  const entries = [...[infinityBuf].concat(buffers).map((buffer, index) => ({ binding: index, resource: { buffer } }))];",
-      f'  const bindGroup = {create_bind_group("layout", "entries")};', 
+      f'  const bindGroup = {create_bind_group("layout", "entries")};',
       "  \n".join(begin_compute_pass("commandEncoder", "pipeline", "bindGroup", "workgroupDims")),
     "};\n"]
 
@@ -72,20 +74,21 @@ class WebGPUJSRenderer(GraphRenderer):
       kernels[p.function_name] = p.src.replace(p.function_name, "main")
       # kernel_name_sequence becomes pipelines: a JS array of {p.function_name: GPUComputePipeline}
       kernel_name_sequence.append(f'"{p.function_name}"')
-      buf_names = ", ".join(kernel_bufs[arg] for arg in ei.bufs + p.vars)
+      buf_names = ", ".join(kernel_bufs[cast(Buffer|Variable, arg)] for arg in ei.bufs + p.vars)
+      assert p.global_size is not None and len(p.global_size) == 3
       global_size = ', '.join(idx.simplify().render() if isinstance(idx, Variable) else str(idx) for idx in p.global_size)
       # deliberately display p.function_name in every addComputePass for easier debugging/understanding
       compute_passes.append(f'addComputePass(pipelines[{i}]["{p.function_name}"], [{buf_names}], [{global_size}], commandEncoder, layouts[{i}]);')
 
     layouts = [f'const layouts = [{", ".join(layouts)}];']
-    kernels = ["const kernels = {"] + indent([f'"{k}": `{v}`,' for k,v in kernels.items()], 1) + ["};\n"]
+    kernel_obj = ["const kernels = {"] + indent([f'"{k}": `{v}`,' for k,v in kernels.items()], 1) + ["};\n"]
     make_pipelines = [f'const kernelNameSequence = [{", ".join(kernel_name_sequence)}];',
       f'let pipelines = await Promise.all(kernelNameSequence.map((name, i) => {create_pipeline("layouts[i]", "kernels[name]")}));',
       'pipelines = pipelines.map((pipeline, i) => { return {[kernelNameSequence[i]] : pipeline} });']
     trigger_gpu = ["const gpuCommands = commandEncoder.finish();", "device.queue.submit([gpuCommands]);"]
     load = ["const load = async (fn) => { await safeLoadStateDict(stateDict, fn); };"]
 
-    prg: list[str] = init_device + safe_load_state_dict.split("\n") + buf_usages + kernels
+    prg: list[str] = init_device + safe_load_state_dict.split("\n") + buf_usages + kernel_obj
     prg += ["const createGraph = async () => {"]
     prg += indent(sym_var_bufs + empty_bufs + state_dict + output_read_bufs + ret_bufs + [declare_infinity, write_infinity], 1)
     prg += indent(add_compute_pass + layouts + make_pipelines + load, 1)
