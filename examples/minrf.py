2d28cf64cc9c03483acfded56672c718da8b00d9
George Hotz <geohot@gmail.com>
2025-05-11 16:19:37 -0700

    t

--- examples/minrf.py
+++ examples/minrf.py
@@ -14,13 +14,14 @@ def timestep_embedding(t:Tensor, dim:int, max_period=10000):
 
 class TimestepEmbedder:
   def __init__(self, hidden_size, frequency_embedding_size=256):
-    self.mlp = [nn.Linear(frequency_embedding_size, hidden_size), Tensor.silu, nn.Linear(hidden_size, hidden_size)]
+    self.mlp = [nn.Linear(1, hidden_size), Tensor.silu, nn.Linear(hidden_size, hidden_size)]
     self.frequency_embedding_size = frequency_embedding_size
 
   def __call__(self, t:Tensor):
     # TODO: why is this required!!!
-    if getenv("MASK_T", 1): t = t * 0
-    return timestep_embedding(t, self.frequency_embedding_size).sequential(self.mlp)
+    return t.reshape(-1, 1).sequential(self.mlp)
+    #if getenv("MASK_T", 1): t = t * 0
+    #return timestep_embedding(t, self.frequency_embedding_size).sequential(self.mlp)
 
 class TransformerBlock:
   def __init__(self, dim, n_heads, norm_eps=1e-5):
