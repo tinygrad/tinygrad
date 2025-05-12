271155f6b33046af24666d5040b8aee86925e074
hooved <172129504+hooved@users.noreply.github.com>
2025-05-12 10:46:45 -0400

    mypy

--- tinygrad/renderer/graph/webgpu.py
+++ tinygrad/renderer/graph/webgpu.py
@@ -56,25 +56,23 @@ def indent(strings:list[str], indents:int) -> list[str]: return [indents*"  " + 
 class WebGPUJSRenderer(GraphRenderer):
   def render_graph(self) -> str:
     # render I/O between host/WebGPU
-    arg_names, copyins, sym_var_bufs, validators, copyout_bufs, output_copies, ret_bufs, copyouts, ret_names = [], [], [], [], [], [], [], [], []
-    kernel_bufs = cast(dict[Buffer|Variable, str], self.bufs) # Variable args will map to WebGPU uniform buffers
+    arg_names, copyins, sym_bufs, validators, copyout_bufs, output_copies, ret_bufs, copyouts, ret_names = [], [], {}, [], [], [], [], [], []
     # render inputs
     for i, uop in enumerate(self.inputs):
       if uop.base.op is Ops.BUFFER: # from Tensor input
         arg_names.append(f"bufArg_{i}")
-        copyins.append(copyin((buf:=self.bufs[uop.base.buffer]), f"bufArg_{i}"))
-        validators.append(f"if (bufArg_{i}.byteLength !== {buf}.size) throw new Error(`bufArg_{i} does not have expected number of bytes`);")
+        copyins.append(copyin((dest_buf:=self.bufs[uop.base.buffer]), f"bufArg_{i}"))
+        validators.append(f"if (bufArg_{i}.byteLength !== {dest_buf}.size) throw new Error(`bufArg_{i} does not have expected number of bytes`);")
       elif uop.op is Ops.BIND: # from symbolic variable input
         arg_names.append(f"intArg_{i}")
-        kernel_bufs[uop.unbind()[0]] = f"input_{i}"
-        sym_var_bufs.append(f'const input_{i} = {alloc("4", "uniform")};')
+        sym_bufs[uop.unbind()[0]] = f"input_{i}"
         copyins.append(copyin(f"input_{i}", f'new Int32Array([{f"intArg_{i}"}])'))
 
     # render outputs
     command_encoder = ["const commandEncoder = device.createCommandEncoder();"]
     for i, uop in enumerate(self.outputs):
       copyout_bufs.append(f'const gpuReadBuffer{i} = {alloc(str(uop.base.buffer.nbytes), "map_read")};')
-      output_copies.append(f"commandEncoder.copyBufferToBuffer({(out_buf:=kernel_bufs[uop.base.buffer])}, 0, gpuReadBuffer{i}, 0, {out_buf}.size);")
+      output_copies.append(f"commandEncoder.copyBufferToBuffer({(out_buf:=self.bufs[uop.base.buffer])}, 0, gpuReadBuffer{i}, 0, {out_buf}.size);")
       array_type = f"{'Uint' if (dt:=uop.dtype) in dtypes.uints else 'Int' if dt in (dtypes.sints+(dtypes.bool,)) else 'Float'}{8*dt.itemsize}Array"
       ret_bufs.append(f"const resultBuffer{i} = new {array_type}(gpuReadBuffer{i}.size / {dt.itemsize});")
       copyouts += [f"await gpuReadBuffer{i}.mapAsync(GPUMapMode.READ);",
@@ -88,6 +86,7 @@ class WebGPUJSRenderer(GraphRenderer):
     for buf, name in self.bufs.items():
       if buf in self.state_bufs: state_buf_allocs.append(f'const {name} = stateDict["{self.state_bufs[buf]}"] = {alloc(str(buf.nbytes), "state")};')
       else: empty_buf_allocs.append(f'const {name} = {alloc(str(buf.nbytes), "empty")};')
+    sym_uniforms = [f'const {name} = {alloc("4", "uniform")};' for name in sym_bufs.values()]
     # representing Infinity with a runtime buffer is the most correct way known, see https://github.com/tinygrad/tinygrad/pull/10179
     create_infinity = [f'const infinityBuf = {alloc("4", "uniform")};']
     create_infinity += [f'{copyin("infinityBuf", "new Float32Array([Infinity])")}']
@@ -101,7 +100,8 @@ class WebGPUJSRenderer(GraphRenderer):
       kernels[p.function_name] = p.src.replace(p.function_name, "main")
       # kernel_sequence becomes pipelines: a JS array of {p.function_name: GPUComputePipeline}
       kernel_sequence.append(f'"{p.function_name}"')
-      buf_names = ", ".join(kernel_bufs[cast(Buffer|Variable, arg)] for arg in ei.bufs + p.vars)
+      for arg in ei.bufs + p.vars: assert arg is not None
+      buf_names = ", ".join(self.bufs[arg] if isinstance(arg, Buffer) else sym_bufs[cast(Variable, arg)] for arg in ei.bufs + p.vars)
       assert p.global_size is not None and len(p.global_size) == 3
       global_size = ', '.join(idx.simplify().render() if isinstance(idx, Variable) else str(idx) for idx in p.global_size)
       # deliberately display p.function_name in every addComputePass for easier debugging/understanding
@@ -113,7 +113,7 @@ class WebGPUJSRenderer(GraphRenderer):
 
     prg: list[str] = buf_usages + kernel_obj
     prg += ["const createGraph = async () => {"]
-    prg += indent(init_device + sym_var_bufs + empty_buf_allocs + state_dict + state_buf_allocs + copyout_bufs + ret_bufs + create_infinity, 1)
+    prg += indent(init_device + sym_uniforms + empty_buf_allocs + state_dict + state_buf_allocs + copyout_bufs + ret_bufs + create_infinity, 1)
     prg += indent(kernel_sequence + layouts + pipes + add_compute_pass + safe_load_state_dict + load, 1)
     prg += indent([f"const run = async ({args}) => {{"], 1)
     prg += indent(validators + command_encoder + copyins + compute_passes + output_copies + trigger_gpu + copyouts + [f"return [{ret}];"], 2)
