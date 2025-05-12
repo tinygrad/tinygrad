f019b05e30ac23e5a8dbd0be743b0e879aaf4fcc
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-05-12 17:11:28 +0200

    more tests

--- test/unit/test_simplify_valid_idx.py
+++ test/unit/test_simplify_valid_idx.py
@@ -177,6 +177,18 @@ class TestValidIdxSimplification(unittest.TestCase):
       "1",
       "((((ridx0+ridx1)<1)!=True)&(((ridx2+ridx3)<1)!=True))")
 
+  def test_valid_becomes_const3(self):
+    ridx0 = Range(0, 4)
+    ridx1 = Range(1, 4)
+    ridx2 = Range(2, 4)
+    ridx3 = Range(3, 4)
+    idx= ((ridx0+ridx1+ridx2+ridx3+27)//30)
+    valid = (ridx0<1).ne(True) & (ridx1<2).ne(True) & ((ridx2+ridx3)<1).ne(True)
+    load = get_gated_load_uop(valid, idx)
+    self.check(load,
+      "1",
+      "((((ridx0<1)!=True)&((ridx1<2)!=True))&(((ridx2+ridx3)<1)!=True))")
+
 class TestImageSimplification(unittest.TestCase):
   def check(self, load, svalid, sidx0, sidx1):
     load = full_rewrite_to_sink(load.sink()).src[0]
@@ -390,6 +402,15 @@ class TestImageSimplification(unittest.TestCase):
     valid = (((idx2+ridx4)<1)!=1)&(((idx1+ridx5)<1)!=1)
     load = get_load_image_uop((128, 768, 4), valid, (alu0, alu1))
     self.check(load, None, "((((idx1*24)+ridx3)+(ridx5*3))+-3)", "(((idx2*2)+ridx4)+-1)")
+
+  def test_simplify7(self):
+    idx0 = Special("idx0", 4)
+    idx1 = Special("idx1", 32)
+    idx2 = Special("idx2", 64)
+    valid = (((idx2<1).ne(True))&((idx1<1).ne(True)))
+    alu = (idx0+(idx2*(2**9))+(idx1*(2**4))+-516)
+    load = get_load_image_uop((128, 768, 4), valid, (alu//512, alu%512))
+    self.check(load, None, "(idx2+-1)", "((idx0+(idx1*16))+-4)")
 
 if __name__ == '__main__':
   unittest.main()
