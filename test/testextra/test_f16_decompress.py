fce416a921d304ab7062bbb185ca1d853818ad69
Chen-Yu Yang <chenyu@fastmail.com>
2025-05-13 22:56:24 +0800

    cleanup

--- test/testextra/test_f16_decompress.py
+++ test/testextra/test_f16_decompress.py
@@ -1,12 +1,12 @@
 import unittest
 from extra.f16_decompress import u32_to_f16
 from tinygrad.tensor import Tensor
-from tinygrad.device import Device, is_dtype_supported
+from tinygrad.device import is_dtype_supported
 from tinygrad import dtypes
 import numpy as np
 
 class TestF16Decompression(unittest.TestCase):
-  @unittest.skipUnless(is_dtype_supported(dtypes.float16))
+  @unittest.skipUnless(is_dtype_supported(dtypes.float16), "need float16")
   def test_u32_to_f16(self):
     a = Tensor.randn(50, dtype=dtypes.float16)
     f16_as_u32 = a.bitcast(dtypes.uint32)
