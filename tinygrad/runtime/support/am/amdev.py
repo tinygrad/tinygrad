17414b7302bd9bda1fdb991e40a8e6ae3e26ecd4
nimlgen <lenyaprokh@gmail.com>
2025-05-14 13:39:52 +0300

    cleaner

--- tinygrad/runtime/support/am/amdev.py
+++ tinygrad/runtime/support/am/amdev.py
@@ -104,8 +104,8 @@ class AMPageTableEntry:
     self.entries[entry_id] = self.adev.gmc.get_pte_flags(self.lv, table, frag, uncached, system, snooped, valid) | (paddr & 0x0000FFFFFFFFF000)
 
 class AMPageTableTraverseContext:
-  def __init__(self, adev, pt, vaddr, create_pts=False, free_pts=False):
-    self.adev, self.vaddr, self.create_pts, self.free_pts = adev, vaddr - adev.gmc.vm_base, create_pts, free_pts
+  def __init__(self, adev, pt, vaddr, create_pts=False, free_pts=False, boot=False):
+    self.adev, self.vaddr, self.create_pts, self.free_pts, self.boot = adev, vaddr - adev.gmc.vm_base, create_pts, free_pts, boot
     self.pt_stack:list[tuple[AMPageTableEntry, int, int]] = [(pt, self._pt_pte_idx(pt, vaddr), self._pt_pte_size(pt))]
 
   def _pt_pte_size(self, pt): return (1 << ((9 * (3-pt.lv)) + 12))
@@ -115,7 +115,7 @@ class AMPageTableTraverseContext:
     pt, pte_idx, _ = self.pt_stack[-1]
     if (entry:=pt.entries[pte_idx]) & am.AMDGPU_PTE_VALID == 0:
       assert self.create_pts, "Not allowed to create new page table"
-      pt.set_entry(pte_idx, self.adev.mm.palloc(0x1000, zero=True), table=True, valid=True)
+      pt.set_entry(pte_idx, self.adev.mm.palloc(0x1000, zero=True, boot=self.boot), table=True, valid=True)
       entry = pt.entries[pte_idx]
 
     assert not self.adev.gmc.is_pte_huge_page(entry), f"Must be table pt={pt.paddr:#x}, {pte_idx=} {entry=:#x}"
@@ -172,12 +172,12 @@ class AMMemoryManager:
     va_pwr2_div, sz_pwr2_div, sz_pwr2_max = va & -(va) if va > 0 else (1 << 63), sz & -(sz), (1 << (sz.bit_length() - 1))
     return (min(va_pwr2_div, sz_pwr2_div) if must_cover else min(va_pwr2_div, sz_pwr2_max)).bit_length() - 1 - 12
 
-  def map_range(self, vaddr:int, size:int, paddrs:list[tuple[int, int]], uncached=False, system=False, snooped=False) -> AMMapping:
+  def map_range(self, vaddr:int, size:int, paddrs:list[tuple[int, int]], uncached=False, system=False, snooped=False, boot=False) -> AMMapping:
     if AM_DEBUG >= 2: print(f"am {self.adev.devfmt}: mapping {vaddr=:#x} ({size=:#x})")
 
     assert size == sum(p[1] for p in paddrs), f"Size mismatch {size=} {sum(p[1] for p in paddrs)=}"
 
-    ctx = AMPageTableTraverseContext(self.adev, self.root_page_table, vaddr, create_pts=True)
+    ctx = AMPageTableTraverseContext(self.adev, self.root_page_table, vaddr, create_pts=True, boot=boot)
     for paddr, psize in paddrs:
       for off, pt, pte_idx, pte_cnt, pte_covers in ctx.next(psize):
         for pte_off in range(pte_cnt):
@@ -235,7 +235,7 @@ class AMMemoryManager:
     for paddr, _ in vm.paddrs: self.pa_allocator.free(paddr)
 
   def palloc(self, size:int, align:int=0x1000, zero=True, boot=False) -> int:
-    # assert self.adev.is_booting == boot, "During booting, only boot memory can be allocated"
+    assert self.adev.is_booting == boot, "During booting, only boot memory can be allocated"
     paddr = (self.boot_allocator if boot else self.pa_allocator).alloc(round_up(size, 0x1000), align)
     if zero: self.adev.vram[paddr:paddr+size] = bytes(size)
     return paddr
@@ -269,7 +269,7 @@ class AMDev:
     # all blocks that are initialized only during the initial AM boot.
     # To determine if the GPU is in the third state, AM uses regSCRATCH_REG7 as a flag.
     self.is_booting, self.smi_dev = True, False # During boot only boot memory can be allocated. This flag is to validate this.
-    self.partial_boot = (self.reg("regSCRATCH_REG7").read() == (am_version:=0xA0000003)) and (getenv("AM_RESET", 0) != 1)
+    self.partial_boot = (self.reg("regSCRATCH_REG7").read() == (am_version:=0xA0000004)) and (getenv("AM_RESET", 0) != 1)
 
     # Memory manager & firmware
     self.mm = AMMemoryManager(self, self.vram_size)
@@ -284,9 +284,9 @@ class AMDev:
     self.gfx:AM_GFX = AM_GFX(self)
     self.sdma:AM_SDMA = AM_SDMA(self)
 
-    # if self.partial_boot and (self.reg("regGCVM_CONTEXT0_CNTL").read() != 0):
-    #   if DEBUG >= 2: print(f"am {self.devfmt}: MEC is active. Issue a full reset.")
-    #   self.partial_boot = False
+    if self.partial_boot and (self.reg("regGCVM_CONTEXT0_CNTL").read() != 0):
+      if DEBUG >= 2: print(f"am {self.devfmt}: MEC is active. Issue a full reset.")
+      self.partial_boot = False
 
     # Init sw for all IP blocks
     for ip in [self.soc, self.gmc, self.ih, self.psp, self.smu, self.gfx, self.sdma]: ip.init_sw()
--- tinygrad/runtime/support/am/ip.py
+++ tinygrad/runtime/support/am/ip.py
@@ -369,18 +369,14 @@ class AM_PSP(AM_IP):
     from tinygrad.runtime.support.usb import USBMMIOInterface
     self.reg_pref = "regMP0_SMN_C2PMSG" if self.adev.ip_ver[am.MP0_HWIP] < (14,0,0) else "regMPASP_SMN_C2PMSG"
 
-    sys_addr=0x200000
-    size=0x10000 * 8
-    self.adev.mm.map_range(vaddr:=self.adev.mm.alloc_vaddr(size=size, align=am.PSP_1_MEG), size,
-      [(sys_addr, size)], system=True, snooped=False, uncached=True)
+    msg1_region = next((reg for reg in self.adev.dma_regions or [] if reg[1].nbytes >= (512 << 10)), None)
+    if msg1_region is not None:
+      self.msg1_addr, self.msg1_view = self.adev.mm.alloc_vaddr(size=msg1_region[1].nbytes, align=am.PSP_1_MEG), msg1_region[1]
+      self.adev.mm.map_range(self.msg1_addr, msg1_region[1].nbytes, [(msg1_region[0], msg1_region[1].nbytes)], system=True, uncached=True, boot=True)
+    else:
+      self.msg1_paddr = self.adev.mm.palloc(am.PSP_1_MEG, align=am.PSP_1_MEG, zero=False, boot=True)
+      self.msg1_addr, self.msg1_view = self.adev.paddr2mc(self.msg1_paddr), self.adev.vram.view(self.msg1_paddr, am.PSP_1_MEG, 'B')
 
-    # self.adev.
-    # self.copy_bufs = [self._new_dma_region(ctrl_addr=0xf000, sys_addr=0x200000, size=0x10000 * 8)]
-
-    self.msg1_va = vaddr
-    self.msg1_view = USBMMIOInterface(self.adev.vram.usb, 0xf000, 0x80000, 'B', pcimem=False)
-
-    # self.msg1_paddr = self.adev.mm.palloc(am.PSP_1_MEG, align=am.PSP_1_MEG, zero=False, boot=True)
     self.cmd_paddr = self.adev.mm.palloc(am.PSP_CMD_BUFFER_SIZE, zero=False, boot=True)
     self.fence_paddr = self.adev.mm.palloc(am.PSP_FENCE_BUFFER_SIZE, zero=not self.adev.partial_boot, boot=True)
 
@@ -418,15 +414,7 @@ class AM_PSP(AM_IP):
   def _wait_for_bootloader(self): self.adev.wait_reg(self.adev.reg(f"{self.reg_pref}_35"), mask=0x80000000, value=0x80000000)
 
   def _prep_msg1(self, data):
-    # usb hacks
-    # from tinygrad.runtime.support.usb import USBMMIOInterface
-    # USBMMIOInterface(vram_bar.usb, 0xf000, 0x80000, 'B', pcimem=False)
-
-    assert len(data) < 0x80000
-    dt = bytes(data) + b'\x00' * (0x80000 - len(data))
-    self.msg1_view[:] = dt
-    # self.adev.vram.view(self.msg1_paddr, len(data))[:] = data
-    # self.adev.vram[self.msg1_paddr + len(data)] = 0
+    self.msg1_view[:] = bytes(data) + b'\x00' * 4
     self.adev.gmc.flush_hdp()
 
   def _bootloader_load_component(self, fw, compid):
@@ -437,7 +425,7 @@ class AM_PSP(AM_IP):
     if DEBUG >= 2: print(f"am {self.adev.devfmt}: loading sos component: {am.psp_fw_type__enumvalues[fw]}")
 
     self._prep_msg1(self.adev.fw.sos_fw[fw])
-    self.adev.reg(f"{self.reg_pref}_36").write(self.msg1_va >> 20)
+    self.adev.reg(f"{self.reg_pref}_36").write(self.msg1_addr >> 20)
     self.adev.reg(f"{self.reg_pref}_35").write(compid)
 
     return self._wait_for_bootloader() if compid != am.PSP_BL__LOAD_SOSDRV else 0
@@ -492,7 +480,7 @@ class AM_PSP(AM_IP):
     for fw_type in fw_types:
       if DEBUG >= 2: print(f"am {self.adev.devfmt}: loading fw: {am.psp_gfx_fw_type__enumvalues[fw_type]}")
       cmd = am.struct_psp_gfx_cmd_resp(cmd_id=am.GFX_CMD_ID_LOAD_IP_FW)
-      cmd.cmd.cmd_load_ip_fw.fw_phy_addr_hi, cmd.cmd.cmd_load_ip_fw.fw_phy_addr_lo = data64(self.msg1_va)
+      cmd.cmd.cmd_load_ip_fw.fw_phy_addr_hi, cmd.cmd.cmd_load_ip_fw.fw_phy_addr_lo = data64(self.msg1_addr)
       cmd.cmd.cmd_load_ip_fw.fw_size = len(fw_bytes)
       cmd.cmd.cmd_load_ip_fw.fw_type = fw_type
       self._ring_submit(cmd)
@@ -507,7 +495,7 @@ class AM_PSP(AM_IP):
 
   def _load_toc_cmd(self, toc_size):
     cmd = am.struct_psp_gfx_cmd_resp(cmd_id=am.GFX_CMD_ID_LOAD_TOC)
-    cmd.cmd.cmd_load_toc.toc_phy_addr_hi, cmd.cmd.cmd_load_toc.toc_phy_addr_lo = data64(self.msg1_va)
+    cmd.cmd.cmd_load_toc.toc_phy_addr_hi, cmd.cmd.cmd_load_toc.toc_phy_addr_lo = data64(self.msg1_addr)
     cmd.cmd.cmd_load_toc.toc_size = toc_size
     return self._ring_submit(cmd)
 
