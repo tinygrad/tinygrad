e1a40e8040c20dd8af15251af096f901219a5aae
George Hotz <72895+geohot@users.noreply.github.com>
2025-05-16 22:05:53 -0700

    add hcq fixedvars support [pr] (#10356)
    * add hcq fixedvars support [pr]
    
    * different test
    
    * fixedvars are only for comp_queues
    
    * fix hcq varvals

--- test/test_multitensor.py
+++ test/test_multitensor.py
@@ -218,6 +218,14 @@ class TestMultiTensor(unittest.TestCase):
       out = f(tt)
       assert out.item() == 1+2+3+4
 
+  def test_multitensor_inside_jit(self):
+    @TinyJit
+    def f(x): return (x.shard((d1,d2), 0)+1).contiguous().sum()
+    for _ in range(5):
+      tt = Tensor.arange(0, 4).contiguous().realize()
+      out = f(tt)
+      assert out.item() == 1+2+3+4
+
   @unittest.skip("slow")
   def test_fuzz_allreduce(self):
     random.seed(41)
