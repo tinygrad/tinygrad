c9bd045d785eff0f9b6fac18ed1422643d01380f
V-E-D <vedantthote2019@gmail.com>
2025-05-17 13:32:38 +0530

    :D

--- test/test_ops.py
+++ test/test_ops.py
@@ -1255,7 +1255,9 @@ class TestOps(unittest.TestCase):
                                                                          np.arange(64,128,dtype=np.float32).reshape(8,8)])
   def test_small_gemm_eye(self):
     helper_test_op(None, lambda x,y: x.matmul(y), lambda x,y: x@y, vals=[np.eye(8).astype(np.float32), np.eye(8).astype(np.float32)])
-  @unittest.skipIf(CI and Device.DEFAULT in ["NV", "LLVM", "GPU", "CUDA"] or (IMAGE and getenv("FLOAT16", 0)==0), "not supported on these in CI or IMAGE without FLOAT16")
+  @unittest.skipIf(CI and Device.DEFAULT in ["NV", "LLVM", "GPU", "CUDA"] or
+                   (IMAGE and getenv("FLOAT16", 0)==0),
+                   "not supported on these in CI or IMAGE without FLOAT16")
   def test_gemm_fp16(self):
     helper_test_op([(64,64), (64,64)], lambda x,y: x.half().matmul(y.half()), atol=5e-2, rtol=5e-2)
   def test_gemm(self):
