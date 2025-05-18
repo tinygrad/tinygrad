79046dcb738f1a5494ba1fa2305c212710592413
roryclear <roryclear.rc@gmail.com>
2025-05-18 14:25:12 +0100

    log2 hack, 66 failing ops

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -96,7 +96,7 @@ class CStyleLanguage(Renderer):
   nan: str = "0.0/0.0"
   code_for_op: dict = {
     Ops.SQRT: lambda x,dtype: f"v_sqrt({x})", Ops.RECIP: lambda x,dtype: f"(1/{x})", Ops.NEG: lambda x,dtype: f"-{x}",
-    Ops.EXP2: lambda x,dtype: f"exp2({x})", Ops.LOG2: lambda x,dtype: f"log2({x})", Ops.SIN: lambda x,dtype: f"sin({x})",
+    Ops.EXP2: lambda x,dtype: f"exp2({x})", Ops.LOG2: lambda x,dtype: f"v_log2({x})", Ops.SIN: lambda x,dtype: f"sin({x})",
     Ops.AND: lambda a,b,dtype: f"({a}&{b})" if dtype in [dtypes.int, dtypes.uint] else f"({a}&&{b})", Ops.XOR: lambda a,b,dtype: f"({a}^{b})", Ops.OR: lambda a,b,dtype: f"({a}|{b})",
     Ops.ADD: lambda a,b,dtype: f"({a}+{b})", Ops.SUB: lambda a,b,dtype: f"({a}-{b})", Ops.MUL: lambda a,b,dtype: f"({a}*{b})",
     Ops.MOD: lambda a,b,dtype: f"({a}%{b})", Ops.IDIV: lambda a,b,dtype: f"({a}/{b})", Ops.CMPNE: lambda a,b,dtype: f"({a}!={b})",
@@ -109,8 +109,9 @@ class CStyleLanguage(Renderer):
   def get_kernel_modifier(self, uops:list[UOp]) -> str: return ""
   def render_kernel(self, function_name:str, kernel:list[str], bufs:list[tuple[str,tuple[DType,bool]]], uops:list[UOp], prefix=None) -> str:
     if self.device == "METAL":
+      custom_log2 = "float v_log2(float x) {\nif (x <= 0.0f) { const uint nan_bits = 0x7FC00000u; return uintBitsToFloat(nan_bits); }\nreturn log2(x);\n}"
       custom_sqrt = "float v_sqrt(float x) {\nif (x < 0.0f) { const uint nan_bits = 0x7FC00000u; return uintBitsToFloat(nan_bits); }\nreturn sqrt(x);\n}"
-      prg = "#version 450\n"+custom_sqrt+"\nvoid main() {\n" + ''.join(['\n'.join(kernel), "\n}"])
+      prg = "#version 450\n"+custom_sqrt+"\n"+custom_log2+"\nvoid main() {\n" + ''.join(['\n'.join(kernel), "\n}"])
 
       #todo ....
       # moves shared vars
