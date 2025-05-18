a3308e145de8dbd8e36017cdc556a4a99697755e
George Hotz <geohot@gmail.com>
2025-05-18 09:09:04 -0700

    hotfix: remote print -> DEBUG=3

--- tinygrad/runtime/ops_remote.py
+++ tinygrad/runtime/ops_remote.py
@@ -290,7 +290,7 @@ class RemoteConnection:
 
   def batch_submit(self):
     data = self.req.serialize()
-    with Timing(f"*** send {len(self.req._q):-3d} requests {len(self.req._h):-3d} hashes with len {len(data)/1024:.2f} kB in ", enabled=DEBUG>=1):
+    with Timing(f"*** send {len(self.req._q):-3d} requests {len(self.req._h):-3d} hashes with len {len(data)/1024:.2f} kB in ", enabled=DEBUG>=3):
       self.conn.request("POST", "/batch", data)
       response = self.conn.getresponse()
       assert response.status == 200, f"POST /batch failed: {response}"
