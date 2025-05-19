00bea66d26d4842410276ccc9eafe285a712a618
hooved <172129504+hooved@users.noreply.github.com>
2025-05-19 11:46:30 -0400

    test state init in graph that mutates state

---
+++ test/test_export.py
@@ -0,0 +1,17 @@
+import unittest
+from tinygrad import Tensor, Device, TinyJit
+
+class Model:
+  def __init__(self):
+    self.weight1 = Tensor([1,2,3])
+
+  def mutate_weight(self, x:Tensor):
+    self.weight1 = self.weight1 + x
+    return self.weight1
+
+@unittest.skipUnless(Device.DEFAULT == "WEBGPU", "only used for WebGPU export currently")
+class TestExportWebGPU(unittest.TestCase):
+  def test_export_mutate_weight(self):
+    model = Model()
+    _, state_dict = TinyJit(model.mutate_weight).export_webgpu(Tensor([7]))
+    assert len(state_dict) == 1 and list(state_dict.values())[0].tolist() == [1,2,3]
