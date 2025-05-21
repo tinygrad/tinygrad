ab0659f59c89f6326a289787d74fa88321c9fa29
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2025-05-21 19:24:43 +0100

    Add sparse wmma instructions

--- extra/mmapeak/mmapeak.py
+++ extra/mmapeak/mmapeak.py
@@ -13,11 +13,17 @@ INSTRUCTIONS_PER_LOOP = 1_000
 
 assemblyTemplate = (pathlib.Path(__file__).parent / "template.s").read_text()
 
-def launchBenchmark(instruction, vgprIndices):
-  src = assemblyTemplate.replace("INSTRUCTION",
-                                  "{} v[0:{}], v[{}:{}], v[{}:{}], 1\n"
-                                  .format(instruction, vgprIndices[0], vgprIndices[1], vgprIndices[2],
-                                           vgprIndices[1], vgprIndices[2]) * INSTRUCTIONS_PER_LOOP)
+def launchBenchmark(instruction, vgprIndices, dense = True):
+  if dense:
+    instructions = "{} v[0:{}], v[{}:{}], v[{}:{}], 1\n".format(instruction, vgprIndices[0],
+                                                                vgprIndices[1], vgprIndices[2],
+                                                                vgprIndices[1], vgprIndices[2]) * INSTRUCTIONS_PER_LOOP
+  else:
+    instructions = "{} v[0:{}], v[{}:{}], v[{}:{}], v{}\n".format(instruction, vgprIndices[0],
+                                                                vgprIndices[1], vgprIndices[2],
+                                                                vgprIndices[3], vgprIndices[4],
+                                                                vgprIndices[5]) * INSTRUCTIONS_PER_LOOP
+  src = assemblyTemplate.replace("INSTRUCTION", instructions)
   lib = COMPILER.compile(src)
   fxn = AMDProgram(DEV, "matmul", lib)
   start = time.perf_counter()
@@ -25,7 +31,7 @@ def launchBenchmark(instruction, vgprIndices):
   end = time.perf_counter()
   elapsed = end-start
   FLOPs = FLOPS_PER_MATMUL * NUM_WAVES * NUM_WORKGROUPS * INTERNAL_LOOP * INSTRUCTIONS_PER_LOOP
-  print("{:<27} : {} T(FL)OPS".format(instruction, round(FLOPs/elapsed/10**12, 2)))
+  print("{:<29} : {} T(FL)OPS".format(instruction, round(FLOPs/elapsed/10**12, 2)))
 
 if __name__=="__main__":
   DEVICENUM = os.getenv("DEVICENUM", "0")
@@ -53,9 +59,21 @@ if __name__=="__main__":
     launchBenchmark("v_wmma_f32_16x16x16_f16", (7,8,11))
     launchBenchmark("v_wmma_i32_16x16x16_iu4", (7,8,8))
     launchBenchmark("v_wmma_i32_16x16x16_iu8", (7,8,9))
-    launchBenchmark("v_wmma_f32_16X16X16_fp8_fp8", (7,8,9))
-    launchBenchmark("v_wmma_f32_16X16X16_fp8_bf8", (7,8,9))
-    launchBenchmark("v_wmma_f32_16X16X16_bf8_fp8", (7,8,9))
-    launchBenchmark("v_wmma_f32_16X16X16_bf8_bf8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16x16x16_fp8_fp8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16x16x16_fp8_bf8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16x16x16_bf8_fp8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16x16x16_bf8_bf8", (7,8,9))
     FLOPS_PER_MATMUL = 16*16*32*2
     launchBenchmark("v_wmma_i32_16X16X32_iu4", (7,8,9))
+    launchBenchmark("v_swmmac_f32_16x16x32_f16", (7,8,11,12,19,20), False)
+    launchBenchmark("v_swmmac_f32_16x16x32_bf16", (7,8,11,12,19,20), False)
+    launchBenchmark("v_swmmac_f16_16x16x32_f16", (3,4,7,8,15,16), False)
+    launchBenchmark("v_swmmac_bf16_16x16x32_bf16", (3,4,7,8,15,16), False)
+    launchBenchmark("v_swmmac_i32_16x16x32_iu8", (7,8,9,10,13,14), False)
+    launchBenchmark("v_swmmac_i32_16x16x32_iu4", (7,8,8,9,10,11), False)
+    launchBenchmark("v_swmmac_f32_16x16x32_fp8_fp8", (7,8,9,10,13,14), False)
+    launchBenchmark("v_swmmac_f32_16x16x32_fp8_bf8", (7,8,9,10,13,14), False)
+    launchBenchmark("v_swmmac_f32_16x16x32_bf8_fp8", (7,8,9,10,13,14), False)
+    launchBenchmark("v_swmmac_f32_16x16x32_bf8_bf8", (7,8,9,10,13,14), False)
+    FLOPS_PER_MATMUL = 16*16*64*2
+    launchBenchmark("v_swmmac_i32_16x16x64_iu4", (7,8,9,10,13,14), False)
