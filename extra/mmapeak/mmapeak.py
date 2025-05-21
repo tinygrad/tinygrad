568ed95d486776d68bae5d453bee2fa2ad44fbb9
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2025-05-21 11:13:09 +0100

    RDNA4 wmma requires less VGRPs

--- extra/mmapeak/mmapeak.py
+++ extra/mmapeak/mmapeak.py
@@ -13,10 +13,11 @@ INSTRUCTIONS_PER_LOOP = 1_000
 
 assemblyTemplate = (pathlib.Path(__file__).parent / "template.s").read_text()
 
-def launchBenchmark(instruction, vgprCount):
+def launchBenchmark(instruction, vgprIndices):
   src = assemblyTemplate.replace("INSTRUCTION",
-                                  "{} v[0:7], v[8:{}], v[8:{}], 1\n"
-                                  .format(instruction, 8+vgprCount-1, 8+vgprCount-1) * INSTRUCTIONS_PER_LOOP)
+                                  "{} v[0:{}], v[{}:{}], v[{}:{}], 1\n"
+                                  .format(instruction, vgprIndices[0], vgprIndices[1], vgprIndices[2],
+                                           vgprIndices[1], vgprIndices[2]) * INSTRUCTIONS_PER_LOOP)
   lib = COMPILER.compile(src)
   fxn = AMDProgram(DEV, "matmul", lib)
   start = time.perf_counter()
@@ -32,17 +33,27 @@ if __name__=="__main__":
     DEV = Device['AMD:' + DEVICENUM]
   except:
     raise RuntimeError("Error while initiating AMD device")
-  
+
   if (ARCH := DEV.arch) not in ['gfx1100', 'gfx1201']:
     raise RuntimeError("only gfx1100 and gfx1201 supported")
   COMPILER = HIPCompiler(ARCH)
 
+  if ARCH == 'gfx1100':
+    launchBenchmark("v_wmma_bf16_16x16x16_bf16", (7,8,15))
+    launchBenchmark("v_wmma_f16_16x16x16_f16", (7,8,15))
+    launchBenchmark("v_wmma_f32_16x16x16_bf16", (7,8,15))
+    launchBenchmark("v_wmma_f32_16x16x16_f16", (7,8,15))
+    launchBenchmark("v_wmma_i32_16x16x16_iu4", (7,8,9))
+    launchBenchmark("v_wmma_i32_16x16x16_iu8", (7,8,11))
   if ARCH == 'gfx1201':
     NUM_WORKGROUPS = 64
-   
-  launchBenchmark("v_wmma_bf16_16x16x16_bf16", 8)
-  launchBenchmark("v_wmma_f16_16x16x16_f16", 8)
-  launchBenchmark("v_wmma_f32_16x16x16_bf16", 8)
-  launchBenchmark("v_wmma_f32_16x16x16_f16", 8)
-  launchBenchmark("v_wmma_i32_16x16x16_iu4", 2)
-  launchBenchmark("v_wmma_i32_16x16x16_iu8", 4)
+    launchBenchmark("v_wmma_bf16_16x16x16_bf16", (3,4,7))
+    launchBenchmark("v_wmma_f16_16x16x16_f16", (3,4,7))
+    launchBenchmark("v_wmma_f32_16x16x16_bf16", (7,8,11))
+    launchBenchmark("v_wmma_f32_16x16x16_f16", (7,8,11))
+    launchBenchmark("v_wmma_i32_16x16x16_iu4", (7,8,8))
+    launchBenchmark("v_wmma_i32_16x16x16_iu8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16X16X16_fp8_fp8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16X16X16_fp8_bf8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16X16X16_bf8_fp8", (7,8,9))
+    launchBenchmark("v_wmma_f32_16X16X16_bf8_bf8", (7,8,9))
