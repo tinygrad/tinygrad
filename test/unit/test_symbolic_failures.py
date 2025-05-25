80c8c2fd39da6461a747f82b601853b2134abc22
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-05-25 22:51:32 +0200

    more tests

--- test/unit/test_symbolic_failures.py
+++ test/unit/test_symbolic_failures.py
@@ -1,7 +1,17 @@
 import unittest
-from tinygrad import Variable
+from tinygrad import Variable, dtypes
+from tinygrad.helpers import Context
+from tinygrad.uop.ops import Ops, UOp
+
 
 class TestFuzzFailure(unittest.TestCase):
+  def setUp(self):
+    self.context = Context(CORRECT_DIVMOD_FOLDING=1)
+    self.context.__enter__()
+
+  def tearDown(self):
+    self.context.__exit__(None, None, None)
+
   def test_fuzz_failure1(self):
     v1=Variable('v1', 0, 8)
     v2=Variable('v2', 0, 2)
@@ -90,6 +100,62 @@ class TestFuzzFailure(unittest.TestCase):
     v3=Variable('v3', 0, 8)
     expr = (((((((((((((((((((((((((((((0*-2)//1)+3)*-2)+-3)*-4)*1)+v1)+0)%2)%8)%9)+v2)%9)+-4)//4)+-1)*-2)+0)+v1)+v1)+3)+v1)+4)+-4)+0)*2)+-3)%6)
     v1_val, v2_val, v3_val = v1.const_like(0), v2.const_like(1), v3.const_like(0)
+    num = expr.simplify().substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()
+    rn = expr.substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()
+    assert num==rn, f"{num} != {rn}"
+
+  def test_fuzz_failure10(self):
+    v1=Variable("v1", 0, 256)
+    v2=Variable("v2", 0, 32)
+    v3=Variable("v3", 0, 32)
+    expr = UOp(Ops.MUL, dtypes.int, arg=None, src=(
+      UOp(Ops.MAX, dtypes.int, arg=None, src=(
+        UOp(Ops.MUL, dtypes.int, arg=None, src=(
+          UOp(Ops.WHERE, dtypes.int, arg=None, src=(
+            UOp(Ops.CMPNE, dtypes.bool, arg=None, src=(
+              UOp(Ops.CMPLT, dtypes.bool, arg=None, src=(
+                x5:=UOp(Ops.IDIV, dtypes.int, arg=None, src=(
+                  UOp(Ops.WHERE, dtypes.int, arg=None, src=(
+                    UOp(Ops.CMPNE, dtypes.bool, arg=None, src=(
+                      UOp(Ops.CMPLT, dtypes.bool, arg=None, src=(
+                        x9:=UOp(Ops.CONST, dtypes.int, arg=9, src=()),
+                        x10:=UOp(Ops.DEFINE_VAR, dtypes.int, arg=('v1', 0, 256), src=()),)),
+                      x11:=UOp(Ops.CONST, dtypes.bool, arg=True, src=()),)),
+                    UOp(Ops.ADD, dtypes.int, arg=None, src=(
+                      UOp(Ops.MUL, dtypes.int, arg=None, src=(
+                         x10,
+                        x14:=UOp(Ops.CONST, dtypes.int, arg=-4, src=()),)),
+                       x14,)),
+                    UOp(Ops.IDIV, dtypes.int, arg=None, src=(
+                       x10,
+                       x9,)),)),
+                   x9,)),
+                 x14,)),
+               x11,)),
+             x5,
+            UOp(Ops.IDIV, dtypes.int, arg=None, src=(
+              UOp(Ops.ADD, dtypes.int, arg=None, src=(
+                UOp(Ops.MOD, dtypes.int, arg=None, src=(
+                  x19:=UOp(Ops.DEFINE_VAR, dtypes.int, arg=('v2', 0, 32), src=()),
+                  UOp(Ops.CONST, dtypes.int, arg=3, src=()),)),
+                 x19,)),
+              UOp(Ops.CONST, dtypes.int, arg=5, src=()),)),)),
+          x22:=UOp(Ops.CONST, dtypes.int, arg=-1, src=()),)),
+        UOp(Ops.MUL, dtypes.int, arg=None, src=(
+          UOp(Ops.ADD, dtypes.int, arg=None, src=(
+            UOp(Ops.ADD, dtypes.int, arg=None, src=(
+              UOp(Ops.MOD, dtypes.int, arg=None, src=(
+                UOp(Ops.MUL, dtypes.int, arg=None, src=(
+                   x10,
+                  UOp(Ops.CONST, dtypes.int, arg=-2, src=()),)),
+                UOp(Ops.CONST, dtypes.int, arg=6, src=()),)),
+              UOp(Ops.MOD, dtypes.int, arg=None, src=(
+                UOp(Ops.DEFINE_VAR, dtypes.int, arg=('v3', 0, 32), src=()),
+                UOp(Ops.CONST, dtypes.int, arg=1, src=()),)),)),
+            UOp(Ops.CONST, dtypes.int, arg=0, src=()),)),
+           x22,)),)),
+       x22,))
+    v1_val, v2_val, v3_val = UOp.const(dtypes.int, 9), UOp.const(dtypes.int, 0),UOp.const(dtypes.int, 0)
     num = expr.simplify().substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()
     rn = expr.substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()
     assert num==rn, f"{num} != {rn}"
