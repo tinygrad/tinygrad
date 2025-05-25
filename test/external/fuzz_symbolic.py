5bda110920f4b3add3ba0bb4a567b8292b89726b
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-05-25 19:46:02 +0200

    Cleaned up printing and debugging

--- test/external/fuzz_symbolic.py
+++ test/external/fuzz_symbolic.py
@@ -44,7 +44,6 @@ def gt(expr, rng=None):
   if rng is None: rng = random.randint(-4,4)
   return expr > rng, rng
 
-
 if __name__ == "__main__":
   ops = [add_v, mul, add_num, div, mod]
   for i in range(50000):
@@ -59,20 +58,25 @@ if __name__ == "__main__":
     # 10% of the time, add one of lt, le, gt, ge
     if random.random() < 0.1: tape.append(random.choice([lt, le, gt, ge]))
     expr = UOp.const(dtypes.int, 0)
-    rngs = []
     for t in tape:
-      expr, rng = t(expr)
-      if DEBUG >= 1: print(t.__name__, rng)
-      rngs.append(rng)
-    if DEBUG >=1: print(expr)
+      expr, rng = t[0](expr)
+      if DEBUG >= 3: print(t[0].__name__, rng)
+    if DEBUG >=3: print(expr)
     with Context(CORRECT_DIVMOD_FOLDING=1):
       simplified_expr = expr.simplify()
 
     solver = z3.Solver()
-    solver.set(timeout=1000)  # some expressions take very long, but its very unlikely they actually return sat
+    solver.set(timeout=1000)  # some expressions take very long verify, but its very unlikely they actually return sat
     z3_sink = graph_rewrite(expr.sink(simplified_expr, u1, u2, u3), z3_renderer, ctx=(solver, {}))
     z3_expr, z3_simplified_expr = z3_sink.src[0].arg, z3_sink.src[1].arg
-    if solver.check(z3_simplified_expr != z3_expr) == z3.sat:
+    check = solver.check(z3_simplified_expr != z3_expr)
+    if check == z3.unknown and DEBUG>=1:
+      print("Skipped due to timeout:\n" +
+            f"v1=Variable(\"{u1.arg[0]}\", {u1.arg[1]}, {u1.arg[2]})\n" +
+            f"v2=Variable(\"{u2.arg[0]}\", {u2.arg[1]}, {u2.arg[2]})\n" +
+            f"v3=Variable(\"{u3.arg[0]}\", {u3.arg[1]}, {u3.arg[2]})\n" +
+            f"expr = {expr.render(simplify=False)}\n")
+    elif check == z3.sat:
       m = solver.model()
       v1, v2, v3 = z3_sink.src[2].arg, z3_sink.src[3].arg, z3_sink.src[4].arg
       n1, n2, n3 = m[v1], m[v2], m[v3]
@@ -81,17 +85,15 @@ if __name__ == "__main__":
         num = expr.simplify().substitute({u1:u1_val, u2:u2_val, u3:u3_val}).ssimplify()
         rn = expr.substitute({u1:u1_val, u2:u2_val, u3:u3_val}).ssimplify()
         if num==rn: print("z3 found a mismatch but the expressions are equal!!")
-      print(f"""
-mismatched {expr.render()} at v1={m[v1]}; v2={m[v2]}; v3={m[v3]} = {num} != {rn}
-Reproduce with:
-v1=Variable(\"{u1.arg[0]}\", {u1.arg[1]}, {u1.arg[2]})
-v2=Variable(\"{u2.arg[0]}\", {u2.arg[1]}, {u2.arg[2]})
-v3=Variable(\"{u3.arg[0]}\", {u3.arg[1]}, {u3.arg[2]})
-expr = {expr.render(simplify=False)}
-v1_val, v2_val, v3_val = v1.const_like({n1.as_long()}), v2.const_like({n2.as_long()}), v3.const_like({n3.as_long()})
-num = expr.simplify().substitute({{v1:v1_val, v2:v2_val, v3:v3_val}}).ssimplify()
-rn = expr.substitute({{v1:v1_val, v2:v2_val, v3:v3_val}}).ssimplify()
-assert num==rn, f"{{num}} != {{rn}}"
-""")
+      print("mismatched {expr.render()} at v1={m[v1]}; v2={m[v2]}; v3={m[v3]} = {num} != {rn}\n" +
+            "Reproduce with:\n" +
+            f"v1=Variable(\"{u1.arg[0]}\", {u1.arg[1]}, {u1.arg[2]})\n" +
+            f"v2=Variable(\"{u2.arg[0]}\", {u2.arg[1]}, {u2.arg[2]})\n" +
+            f"v3=Variable(\"{u3.arg[0]}\", {u3.arg[1]}, {u3.arg[2]})\n" +
+            f"expr = {expr.render(simplify=False)}\n" +
+            f"v1_val, v2_val, v3_val = v1.const_like({n1.as_long()}), v2.const_like({n2.as_long()}), v3.const_like({n3.as_long()})\n" +
+            "num = expr.simplify().substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()\n" +
+            "rn = expr.substitute({v1:v1_val, v2:v2_val, v3:v3_val}).ssimplify()\n" +
+            f"assert num==rn, {num} != {rn}\n")
 
-    if DEBUG >= 1: print(f"validated {expr.render()}")
+    if DEBUG >= 2: print(f"validated {expr.render()}")
