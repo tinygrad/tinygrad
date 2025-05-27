be3b27ea337a70adc47087b52c9fc0203b47db9c
Eitan Turok <eitan.turok@gmail.com>
2025-05-27 23:09:21 +0300

    better

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from icecream import install
 install()
 
 tensor_size, idx_size = 50257, 324
-mask = Tensor.rand(1, tensor_size) > 0.5
+mask = Tensor.ones(1, tensor_size, dtype=dtypes.bool).contiguous()
 idx1 = Tensor.zeros(idx_size, dtype=dtypes.int)
 idx2 = Tensor.randint(idx_size, low=0, high=tensor_size, dtype=dtypes.int)
 ic(mask, idx1, idx1.numpy(), idx2, idx2.numpy())
