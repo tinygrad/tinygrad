993bb68eec60ff4376dc9e36ed14eb13512ec8e1
ttomsa <tomasvsilva8@gmail.com>
2025-05-30 18:46:58 +0100

    add arm64 to devices

---
+++ tinygrad/runtime/ops_arm64.py
@@ -0,0 +1,8 @@
+from tinygrad.device import Compiled, MallocAllocator
+from tinygrad.renderer.asm import Arm64Renderer
+from tinygrad.runtime.ops_cpu import ClangJITCompiler, CPUProgram
+
+class ARM64Device(Compiled):
+  def __init__(self, device:str):
+    super().__init__(device, MallocAllocator, Arm64Renderer(),
+                      ClangJITCompiler(cachekey="compile_arm64", lang_args=['assembler']), CPUProgram)
