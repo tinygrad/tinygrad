fdf907686906ab77a686fe4132fe83cf52974a46
hooved <172129504+hooved@users.noreply.github.com>
2025-05-30 08:24:42 -0400

    new graph capture passes all test_jit_cases

--- test/test_jit_cases.py
+++ test/test_jit_cases.py
@@ -1,5 +1,10 @@
 import unittest
 from tinygrad import TinyJit, Tensor
+from tinygrad.renderer.graph import GraphRenderer
+from tinygrad.uop.ops import UOp, Ops
+from tinygrad.engine.jit import get_input_replace
+from tinygrad.device import Buffer
+from typing import cast
 
 # The JIT functions as a "capturing" JIT.
 # Whatever kernels ran in the JIT the second run through the function will be the kernels that will run from then on.
@@ -14,27 +19,65 @@ from tinygrad import TinyJit, Tensor
 # explicit inputs and outputs are realized on their way in and out of the JIT
 # there's a whole bunch of edge cases and weirdness here that needs to be tested and clarified.
 
+# NOTE: This is a very rough/unoptimized jit_v2 prototype, because GraphRenderer is currently designed for model export
+# The purpose is to show that the new graph capture method works for all the TestJitCases
+def jit_v2(fxn, *args, **kwargs):
+  r = GraphRenderer(fxn, *args, **kwargs)
+
+  r_args = [arg.buffer if arg.op is Ops.BUFFER else arg for arg in r.inputs]
+  r_buf_args = [arg for arg in r_args if isinstance(arg, Buffer)]
+  map_input_replace_idx_to_original_idx = {i: r_args.index(buf) for i, buf in enumerate(r_buf_args)}
+  input_replace = get_input_replace(r.eis, r_buf_args)
+  input_replace.update({k: map_input_replace_idx_to_original_idx[v] for k,v in input_replace.items()})
+
+  explicit_output_j_i: dict[int, tuple[int, int]] = dict()
+  for idx, out in enumerate(r.outputs):
+    for j, ei in enumerate(r.eis):
+      for i, buf in enumerate(ei.bufs):
+        if buf == out.buffer:
+          explicit_output_j_i[idx] = (j, i)
+
+  # NOTE: To make model export to other languages simple, Tensor and Variable args must be positional (not kwargs)
+  def captured_jit_v2(*args: Tensor|UOp):
+    if args: Tensor.realize(*[arg for arg in args if isinstance(arg, Tensor)])
+    var_vals = dict(v.unbind() for v in args if isinstance(v, UOp))
+    for (j,i),input_idx in input_replace.items():
+      assert isinstance(args[input_idx], Tensor)
+      b = cast(Tensor, args[input_idx]).lazydata.base.realized
+      assert isinstance(b, Buffer)
+      r.eis[j].bufs[i] = b
+    for ei in r.eis: ei.run(var_vals)
+    # NOTE: for model export we don't need to return a Tensor, we do the below just for these tests
+    assert all(isinstance(b, Buffer) for ei in r.eis for b in ei.bufs)
+    ret = [Tensor(bytes((b:=cast(Buffer, r.eis[j].bufs[i])).as_buffer()), b.device, b.dtype) for j,i in explicit_output_j_i.values()]
+    return ret[0] if len(ret) == 1 else ret
+
+  return captured_jit_v2
+
 class TestJitCases(unittest.TestCase):
-  def test_explicit(self):
+  def test_explicit(self, v2=False):
     # this function has an explicit input and an explicit output
-    @TinyJit
     def f(x:Tensor):
       ret:Tensor = x*2
       return ret
+    f = TinyJit(f) if not v2 else jit_v2(f, Tensor([0]))
 
     for i in range(5):
       out = f(Tensor([i]))
       self.assertEqual(out.item(), i*2)
 
-  def test_implicit_input(self):
+  def test_explicit_v2(self):
+    self.test_explicit(v2=True)
+
+  def test_implicit_input(self, v2=False):
     # x is the implicit input (like a weight)
     x = Tensor([0])
 
     # this function has an implicit input and an explicit output
-    @TinyJit
     def f():
       ret:Tensor = x*2
       return ret
+    f = TinyJit(f) if not v2 else jit_v2(f)
 
     for i in range(5):
       # NOTE: this must be realized here, otherwise the update doesn't happen
@@ -43,36 +86,49 @@ class TestJitCases(unittest.TestCase):
       out = f()
       self.assertEqual(out.item(), i*2)
 
-  def test_implicit_output(self):
+  def test_implicit_input_v2(self):
+    self.test_implicit_input(v2=True)
+
+  def test_implicit_output(self, v2=False):
     # out is the implicit output (it's assigned to)
     out = Tensor([0])
 
     # this function has an explicit input and an implicit output
-    @TinyJit
     def f(x:Tensor):
       # NOTE: this must be realized here
       # if we were explicitly tracking the implicit output Tensors, we might not need this realize
-      out.assign(x*2).realize()
+      if not v2: out.assign(x*2).realize()
+
+      # realize within jitted function is banned in v2
+      else: out.assign(x*2)
+    f = TinyJit(f) if not v2 else jit_v2(f, Tensor([0]))
 
     for i in range(5):
       f(Tensor([i]))
       self.assertEqual(out.item(), i*2)
 
-  def test_implicit_io(self):
+  def test_implicit_output_v2(self):
+    self.test_implicit_output(v2=True)
+
+  def test_implicit_io(self, v2=False):
     # x is the implicit input (like a weight)
     # out is the implicit output (it's assigned to)
     x = Tensor([0])
     out = Tensor([0])
 
     # this function has an implicit input and an implicit output
-    @TinyJit
     def f():
-      out.assign(x*2).realize() # NOTE: this must be realized here
+      if not v2: out.assign(x*2).realize() # NOTE: this must be realized here
+      else: out.assign(x*2) # realize within jitted function is banned in v2
+    f = TinyJit(f) if not v2 else jit_v2(f)
 
     for i in range(5):
       x.assign(Tensor([i])).realize()
       f()
       self.assertEqual(out.item(), i*2)
+
+  def test_implicit_io_v2(self):
+    self.test_implicit_io(v2=True)
 
 if __name__ == '__main__':
   unittest.main()
