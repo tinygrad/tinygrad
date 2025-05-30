2c485728283a96a95b6697ace5bb00460f753269
hooved <172129504+hooved@users.noreply.github.com>
2025-05-30 13:57:48 -0400

    debug test

--- test/test_export.py
+++ test/test_export.py
@@ -1,5 +1,6 @@
 import unittest
 from tinygrad import Tensor, Device, TinyJit
+from tinygrad.nn.state import get_state_dict
 
 class Model:
   def __init__(self):
@@ -12,8 +13,17 @@ class Model:
 class TestExportWebGPU(unittest.TestCase):
   def test_export_unmutated_implicit_input(self):
     model = Model()
-    _, state_dict = TinyJit(model.mutate_implicit_input).export_webgpu(Tensor([7]))
-    self.assertEqual(len(state_dict), 1)
+    _, state_dict = TinyJit(model.mutate_implicit_input).export_webgpu(Tensor([7]), tensor_names=get_state_dict(model))
+
+    name_t_u_b = "\n"
+    # debugging test failure in CI that doesn't reproduce locally
+    for name, t in state_dict.items():
+      name_t_u_b += str(name) + "\n"
+      name_t_u_b += str(t) + "\n"
+      name_t_u_b += str(t.lazydata) + "\n"
+      name_t_u_b += str(t.lazydata.base.realized) + "\n\n"
+    assert len(state_dict) == 1, name_t_u_b
+    #self.assertEqual(len(state_dict), 1)
     self.assertEqual(list(state_dict.values())[0].tolist(), [0])
     self.assertEqual(model.w.tolist(), [7])
 
