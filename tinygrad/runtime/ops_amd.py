685b27c0410e9671f0ac22cd461c1fde144d4b6c
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-06-03 13:05:22 +0000

    fix check

--- tinygrad/runtime/ops_amd.py
+++ tinygrad/runtime/ops_amd.py
@@ -666,9 +666,9 @@ class PCIIface:
     except OSError as e: raise RuntimeError(f"Cannot resize BAR: {e}. Ensure the resizable BAR option is enabled on your system.") from e
 
     # Disable migartion of locked pages.
-    if first_dev and FileIOInterface(reloc_sysfs:="/proc/sys/vm/compact_unevictable_allowed", os.O_RDONLY).read() != "0":
+    if first_dev and FileIOInterface(reloc_sysfs:="/proc/sys/vm/compact_unevictable_allowed", os.O_RDONLY).read()[0] != "0":
       os.system(cmd:=f"sudo sh -c 'echo 0 > {reloc_sysfs}'")
-      assert FileIOInterface(reloc_sysfs, os.O_RDONLY).read() == "0", f"Failed to disable migration of locked pages. Please run {cmd} manually."
+      assert FileIOInterface(reloc_sysfs, os.O_RDONLY).read()[0] == "0", f"Failed to disable migration of locked pages. Please run {cmd} manually."
 
     # Try to init vfio. Use it if success.
     if getenv("VFIO", 0):
