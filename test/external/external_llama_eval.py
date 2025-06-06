ba0ac20867ec0eb7f93c1bce997bf1a9ba568878
patrini32 <171664803+patrini32@users.noreply.github.com>
2025-06-06 10:48:05 +0300

    fix?

--- test/external/external_llama_eval.py
+++ test/external/external_llama_eval.py
@@ -39,7 +39,7 @@ class LLaMaAdaptor(LM):
       max_gen_toks = args.get("max_gen_toks") or args.get("max_length") or self.max_length-prompt_len
       assert self.max_length >= max_gen_toks, "This eval needs a longer context length"
       for i in range(0,max_gen_toks+prompt_len+1):
-        next_tok = self.model(Tensor([toks[i]]), i, args.get("temperature", 0.0)).item()
+        next_tok = self.model(Tensor([[toks[i]]]), i, args.get("temperature", 0.0)).item()
         if next_tok in self.tokenizer.stop_tokens or next_tok in until: break
         toks.append(next_tok)
       continuations.append(self.tokenizer.decode(toks[prompt_len:]))
