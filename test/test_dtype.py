286564798b21c64d93639a0c48b2b8f98e6f7aab
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-06-10 13:16:55 +0200

    cleaner test

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -469,26 +469,18 @@ class TestHelpers(unittest.TestCase):
       if dtypes.is_float(dt):
         np.testing.assert_equal(dtypes.min(dt), -math.inf)
         np.testing.assert_equal(dtypes.max(dt), math.inf)
-      elif dtypes.is_int(dt):
-        info = np.iinfo(_to_np_dtype(dt))
-        np.testing.assert_equal(dtypes.min(dt), info.min)
-        np.testing.assert_equal(dtypes.max(dt), info.max)
-      else:
-        assert dt == dtypes.bool, dt
-        np.testing.assert_equal(dtypes.min(dt), False)
-        np.testing.assert_equal(dtypes.max(dt), True)
-
-  def test_dtype_range_property(self):
-    for dt in core_dtypes:
-      if dtypes.is_float(dt):
         np.testing.assert_equal(dt.min, -math.inf)
         np.testing.assert_equal(dt.max, math.inf)
       elif dtypes.is_int(dt):
         info = np.iinfo(_to_np_dtype(dt))
+        np.testing.assert_equal(dtypes.min(dt), info.min)
+        np.testing.assert_equal(dtypes.max(dt), info.max)
         np.testing.assert_equal(dt.min, info.min)
         np.testing.assert_equal(dt.max, info.max)
       else:
         assert dt == dtypes.bool, dt
+        np.testing.assert_equal(dtypes.min(dt), False)
+        np.testing.assert_equal(dtypes.max(dt), True)
         np.testing.assert_equal(dt.min, False)
         np.testing.assert_equal(dt.max, True)
 
