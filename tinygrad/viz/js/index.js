822e2dcb205f8586cc3841ca9b39a18488327473
qazal <77887910+Qazalin@users.noreply.github.com>
2025-06-12 15:19:48 +0300

    viz: back button returns to the kernel graph (#10790)
    * create space
    
    * viz: back button returns to the kernel graph

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -38,7 +38,14 @@ async function renderDag(graph, additions, recenter=false) {
     const STROKE_WIDTH = 1.4;
     const nodes = d3.select("#nodes").selectAll("g").data(g.nodes().map(id => g.node(id)), d => d).join("g")
       .attr("transform", d => `translate(${d.x},${d.y})`).classed("clickable", d => d.ref != null)
-      .on("click", (_,d) => d.ref != null && setState({ expandSteps: true, currentCtx:d.ref, currentStep:0, currentRewrite:0 }));
+      .on("click", (_,d) => {
+        if (d.ref != null) {
+          // NOTE: browser does a structured clone, passing a mutable object is safe.
+          history.replaceState(state, "");
+          history.pushState(state, "");
+          setState({ expandSteps: true, currentCtx:d.ref, currentStep:0, currentRewrite:0 });
+        }
+      });
     nodes.selectAll("rect").data(d => [d]).join("rect").attr("width", d => d.width).attr("height", d => d.height).attr("fill", d => d.color)
       .attr("x", d => -d.width/2).attr("y", d => -d.height/2).attr("style", d => d.style ?? `stroke:#4a4b57; stroke-width:${STROKE_WIDTH}px;`);
     nodes.selectAll("g.label").data(d => [d]).join("g").attr("class", "label").attr("transform", d => {
@@ -278,6 +285,10 @@ function setState(ns) {
   Object.assign(state, ns);
   main();
 }
+window.addEventListener("popstate", (e) => {
+  if (e.state != null) setState(e.state);
+});
+
 async function main() {
   const { currentCtx, currentStep, currentRewrite, expandSteps } = state;
   // ** left sidebar context list
