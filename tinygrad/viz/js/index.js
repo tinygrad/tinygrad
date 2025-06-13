484442f27cc0667b5571c5fe15110058f2451d0b
qazal <qazal.software@gmail.com>
2025-06-13 18:21:05 +0300

    Divide timelines by device on y axis

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -252,6 +252,14 @@ async function renderProfiler() {
   }
   const root = document.querySelector(".profiler");
   root.innerHTML = "";
+  const Y_OFFSET = 20;
+  // ** devices on the y axis
+  const procList = d3.select(root).append("div").attr("style", `width: 20%; height: 100%; padding-top: ${Y_OFFSET}px;`);
+  for (const e of traceEvents) {
+    if (e.name === "process_name") {
+      procList.append("div").text(e.args.name).attr("id", `proc-${e.pid}`);
+    }
+  }
   // ** time axis
   // 1. time counters
   // NOTE: Using Math.min/max can lead to "max callstack size exceeded" errors if there are a lot of traceEvents
@@ -306,7 +314,8 @@ async function renderProfiler() {
         const x = scale(e.ts-st);
         const width = scale(e.ts+e.dur-st)-x;
         const height = 20;
-        const y = 20;
+        const procRect = rect(`#proc-${e.pid}`);
+        const y = procRect.y-(Y_OFFSET*2);
         if (width < 0.5) continue;
         ctx.fillStyle = `#${colors[i%colors.length]}`;
         ctx.fillRect(x, y, width, height);
