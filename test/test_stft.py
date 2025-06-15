86c057102e5b48387251a6c1e36190867c1f1dc2
Irwin1138 <irwin1139@gmail.com>
2025-06-15 23:18:28 +0300

    Add initial stft test comparing against torch and librosa

---
+++ test/test_stft.py
@@ -0,0 +1,45 @@
+import unittest
+import numpy as np
+import torch
+from tinygrad import Tensor, Variable, Device
+from tinygrad.helpers import OSX
+
+
+class TestSample(unittest.TestCase):
+  def test_hann(self):
+    ref = torch.hann_window(256, False)
+    result = Tensor.hann_tg(256, False)
+    np.testing.assert_allclose(result.numpy(), ref.numpy(), atol=1e-6, rtol=1e-5)
+
+  def test_hann_periodic(self):
+    ref = torch.hann_window(256)
+    result = Tensor.hann_tg(256)
+    np.testing.assert_allclose(result.numpy(), ref.numpy(), atol=1e-7)
+
+  def test_stft(self):
+    N_FFT = 400
+    HOP_LENGTH = 160
+    BS = 16
+
+    Tensor.manual_seed(42)
+    X = Tensor.rand(BS, 2400).realize()
+    reference = torch.stft(torch.Tensor(X.numpy()), N_FFT, HOP_LENGTH, center=False, return_complex=True, window=torch.hann_window(N_FFT))
+    reference = reference.abs()
+    result = Tensor.stft_full(X, N_FFT, HOP_LENGTH, (0, 0))
+    np.testing.assert_allclose(result.numpy(), reference.numpy(), atol=1e-4, rtol=1e-2)
+
+  def test_stft_librosa(self):
+    import librosa
+    N_FFT = 400
+    HOP_LENGTH = 160
+    BS = 16
+
+    Tensor.manual_seed(42)
+    X = Tensor.rand(BS, 2400).realize()
+    reference = librosa.stft(X.numpy(), n_fft=N_FFT, hop_length=HOP_LENGTH, center=False, window="hann", dtype=np.csingle)
+    reference = np.abs(reference)
+    result = Tensor.stft_full(X, N_FFT, HOP_LENGTH, (0, 0))
+    np.testing.assert_allclose(result.numpy(), reference, atol=1e-7, rtol=1e-2)
+
+if __name__ == '__main__':
+  unittest.main()
