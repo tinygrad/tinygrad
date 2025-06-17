58d1346a62b6e5c1922a252a318ec0b4e3d18847
kevvz <92408564+kevvz@users.noreply.github.com>
2025-06-16 21:58:09 -0700

    add qr tests

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -2,15 +2,12 @@ import numpy as np
 import unittest
 from tinygrad import Tensor
 
- #TODO: create orthogonality test. create reconstruction test if needed
-def test_reconstruction(U):
-  pass
 class TestLinAlg(unittest.TestCase):
   def test_svd_general(self): 
     sizes = [(2,2),(3,3),(3,6),(6,3)]
     #sizes = [(2,2),(4,2),(2,4),(5,3),(3,5),(2,2,2),(2,2,4),(2,4,2)]
     for size in sizes:
-      a = Tensor.randn(size[0],size[1]).realize()
+      a = Tensor.randn(size).realize()
       U,S,V = Tensor.svd(a)
       num_single_values= min(size[0],size[1])
       m,n = size
@@ -19,12 +16,16 @@ class TestLinAlg(unittest.TestCase):
       np.testing.assert_allclose(reconstructed_tensor.numpy(), a.numpy() , atol=1e-5, rtol=1e-5)
       np.testing.assert_allclose((U @ U.transpose()).numpy(),Tensor.eye(m).numpy(),atol=1e-5,rtol=1e-5)
       np.testing.assert_allclose((V @ V.transpose()).numpy(),Tensor.eye(n).numpy(),atol=1e-5,rtol=1e-5)
-  # def test_singular_matrix(): #test nans
-  #   pass
-  # def test_large_matrix():
-  #   pass
-  # def test_qr_general():
-  #   pass
+  def test_qr_general(self):
+    sizes = [(3,3),(3,6),(6,3),(2,2,2,2,2)]
+    for size in sizes:
+      a = Tensor.randn(size).realize()
+      Q,R = Tensor.qr(a)
+      reconstructed_tensor = Q @ R
+      Q_size = size[-2]
+      identity = Tensor.eye(Q_size).reshape((1,)*(len(size)-2)+(Q_size,Q_size)).expand(size[0:-2]+(Q_size,Q_size))
+      np.testing.assert_allclose(reconstructed_tensor.numpy(), a.numpy() , atol=1e-5, rtol=1e-5)
+      np.testing.assert_allclose((Q @ Q.transpose(-2,-1)).numpy(),identity.numpy(),atol=1e-5,rtol=1e-5)
 
 if __name__ == "__main__":
   unittest.main() 
