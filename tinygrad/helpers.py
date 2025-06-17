c0d2e6a4b498e65c0e317857a51acc30117c4e2c
noahk <noahskusaba@gmail.com>
2025-06-17 07:06:13 -0400

    bugfix

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -349,5 +349,4 @@ def _serialize_code(code:types.CodeType):
 copyreg.pickle(types.CodeType, _serialize_code)
 
 def _serialize_module(module:types.ModuleType): return importlib.import_module, (module.__name__,)
-
 copyreg.pickle(types.ModuleType, _serialize_module)
