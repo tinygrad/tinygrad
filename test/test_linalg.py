5272c32bfd10ebb30ae982ca2651231757168625
kevvz <92408564+kevvz@users.noreply.github.com>
2025-06-17 12:24:17 -0700

    little less verbose nonfull svd test

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -12,7 +12,7 @@ class TestLinAlg(unittest.TestCase):
       k= min(m,n)
       s_diag = (S.unsqueeze(-2) * Tensor.eye(k).reshape((1,)*len(b_shape)+(k,k)))
       s_diag= s_diag.expand(b_shape+(k,k)).pad(tuple([(0,0) for i in range(len(size)-2)] + [(0,m-k), (0,n-k)]))
-      reconstructed_tensor = U @ s_diag @ V.transpose(-2,-1)
+      reconstructed_tensor = U @ s_diag @ V
       u_identity = (Tensor.eye(m).reshape((1,)*len(b_shape)+(m,m)).expand(b_shape+(m,m)))
       v_identity = (Tensor.eye(n).reshape((1,)*len(b_shape)+(n,n)).expand(b_shape+(n,n)))
       np.testing.assert_allclose(reconstructed_tensor.numpy(), a.numpy() , atol=1e-5, rtol=1e-5)
@@ -26,17 +26,14 @@ class TestLinAlg(unittest.TestCase):
       b_shape,m,n = size[0:-2],size[-2],size[-1]
       k = min(m,n)
       s_diag = (S.unsqueeze(-2) * Tensor.eye(k).reshape((1,)*len(b_shape)+(k,k)).expand(b_shape+(k,k)))
-      reconstructed_tensor = U @ s_diag @ V.transpose(-2,-1)
+      reconstructed_tensor = U @ s_diag @ V
       u_identity = (Tensor.eye(k).reshape((1,)*len(b_shape)+(k,k)).expand(b_shape+(k,k)))
       v_identity = (Tensor.eye(k).reshape((1,)*len(b_shape)+(k,k)).expand(b_shape+(k,k)))
-
+      if (m < n): U_r,V_r = U @ U.transpose(-2,-1),V @ V.transpose(-2,-1)
+      else: U_r,V_r = U.transpose(-2,-1) @ U, V.transpose(-2,-1) @ V
       np.testing.assert_allclose(reconstructed_tensor.numpy(), a.numpy() , atol=1e-5, rtol=1e-5)
-      if (m < n): #u,v is only orthogonal along the smaller dimension for non full matrices
-        np.testing.assert_allclose((U @ U.transpose(-2,-1)).numpy(),u_identity.numpy(),atol=1e-5,rtol=1e-5)
-        np.testing.assert_allclose((V.transpose(-2,-1) @ V).numpy(),v_identity.numpy(),atol=1e-5,rtol=1e-5)
-      else:
-        np.testing.assert_allclose((U.transpose(-2,-1) @ U).numpy(),u_identity.numpy(),atol=1e-5,rtol=1e-5)
-        np.testing.assert_allclose((V @ V.transpose(-2,-1)).numpy(),v_identity.numpy(),atol=1e-5,rtol=1e-5)
+      np.testing.assert_allclose(U_r.numpy(),u_identity.numpy(),atol=1e-5,rtol=1e-5)
+      np.testing.assert_allclose(V_r.numpy(),v_identity.numpy(),atol=1e-5,rtol=1e-5)
   def test_qr_general(self):
     sizes = [(3,3),(3,6),(6,3),(2,2,2,2,2)]
     for size in sizes:
