1318a2de5b76468da5e6652ff1aec292535cc308
Fang-Pen Lin <hello@fangpenlin.com>
2025-06-18 00:54:24 -0700

    Rename

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -3890,13 +3890,13 @@ class Tensor(MathTrait):
     print(t.binary_crossentropy_logits(Y).item())
     ```
     """
-    log_val = (1 + self.abs().neg().exp()).log()
+    log_exp = (1 + self.abs().neg().exp()).log()
     base = self.maximum(0) - Y * self
     if pos_weight is None:
-      return (base + log_val)._do_reduction(reduction)
+      return (base + log_exp)._do_reduction(reduction)
     pos_scalar = 1 + Y * (pos_weight - 1)
     pos_addition = Y * (pos_weight - 1) * self.neg().maximum(0)
-    return (base + pos_addition + (pos_scalar * log_val))._do_reduction(reduction)
+    return (base + pos_addition + (pos_scalar * log_exp))._do_reduction(reduction)
 
   def sparse_categorical_crossentropy(self, Y:Tensor, ignore_index:int=-1, label_smoothing=0.0, reduction:ReductionStr="mean") -> Tensor:
     """
