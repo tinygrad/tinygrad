427dd94545ef8b3f2c14c288053cb5280ba69fb0
qazal <qazal.software@gmail.com>
2025-06-20 12:47:50 +0300

    one place

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -799,11 +799,12 @@ if TRACK_MATCH_STATS or PROFILE:
   import atexit
   @atexit.register
   def print_match_stats():
+    if not TRACK_MATCH_STATS: return None
     if TRACK_MATCH_STATS >= 2:
       with open(fn:=temp("rewrites.pkl", append_user=True), "wb") as f:
         print(f"rewrote {len(tracked_ctxs)} graphs and matched {sum(len(r.matches) for x in tracked_ctxs for r in x)} times, saved to {fn}")
         with Context(PICKLE_BUFFERS=0): pickle.dump((tracked_keys, tracked_ctxs), f)
-      if getenv("VIZ"): launch_viz("VIZ", temp("rewrites.pkl", append_user=True))
+    if getenv("VIZ"): launch_viz("VIZ", temp("rewrites.pkl", append_user=True))
     if getenv("PRINT_MATCH_STATS", 1):
       ret = [0,0,0.0,0.0]
       for k,v in sorted(list(match_stats.items()), key=lambda x: x[1][2]+x[1][3]):
