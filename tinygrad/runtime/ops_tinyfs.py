0d530cd5197522b90b63f4fd76e8fe5c6572d170
Woze Parrot <wozeparrot@gmail.com>
2025-06-20 22:55:52 +0000

    feat: send request_id

--- tinygrad/runtime/ops_tinyfs.py
+++ tinygrad/runtime/ops_tinyfs.py
@@ -1,7 +1,6 @@
 import socket, uuid
-from tinygrad.device import Compiled, Compiler, Allocator
+from tinygrad.device import Compiled, Allocator
 from tinygrad.helpers import DEBUG, getenv
-from tinygrad.runtime.ops_null import NullRenderer, NullProgram
 
 TINYFS_ENDPOINT = getenv("TINYFS_ENDPOINT", "localhost:6767")
 
@@ -11,9 +10,9 @@ class TinyFSDevice(Compiled):
     super().__init__(device, TinyFSAllocator(self), None, None, None)
 
 class TinyFSBuffer:
-  def __init__(self, device:TinyFSDevice, size:int, offset=0, sock=None):
+  def __init__(self, device:TinyFSDevice, size:int, offset=0, sock=None, request_id=None):
     self.device, self.size, self.offset = device, size, offset
-    self.request_id: uuid.UUID|None = None
+    self.request_id: uuid.UUID|None = request_id
     if sock is None:
       self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.sock.connect((TINYFS_ENDPOINT.split(":")[0], int(TINYFS_ENDPOINT.split(":")[1])))
@@ -33,18 +32,21 @@ class TinyFSAllocator(Allocator[TinyFSDevice]):
     dest.sock.send(f"{dest.device.op}_IN {dest.size}\r\n".encode())
 
     # read the response uuid
-    if dest.device.op == "STORE":
-      dest.request_id = uuid.UUID(bytes=dest.sock.recv(16))
-      print(f"Request ID: {dest.request_id}")
+    dest.request_id = uuid.UUID(bytes=dest.sock.recv(16))
+    if DEBUG >= 2: print(f"Request ID: {dest.request_id}")
 
     dest.sock.sendall(src)
 
   def _copyout(self, dest:memoryview, src:TinyFSBuffer):
-    src.sock.send(f"{src.device.op}_OUT {src.size}\r\n".encode())
+    src.sock.send(f"{src.device.op}_OUT {src.size} {src.request_id}\r\n".encode())
+
+    src.request_id = uuid.UUID(bytes=src.sock.recv(16))
+    if DEBUG >= 2: print(f"Request ID: {src.request_id}")
+
     recv = 0
     while recv < src.size:
       recv += src.sock.recv_into(dest[recv:], src.size - recv)
 
   def _offset(self, buf:TinyFSBuffer, size:int, offset:int):
     assert offset == 0, f"only offset 0 supported, found offset {offset}"
-    return TinyFSBuffer(buf.device, size, offset, buf.sock)
+    return TinyFSBuffer(buf.device, size, offset, buf.sock, buf.request_id)
