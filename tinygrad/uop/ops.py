fa52bdb50faee65824d3264feb4e6f75141da227
George Hotz <72895+geohot@users.noreply.github.com>
2025-06-20 18:56:23 -0700

    applied_opts is in the optimized ast] (#10906)

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -518,6 +518,7 @@ class KernelInfo:
   local_dims: int = 0           # number of local dimensions  (this is remapping RANGE to SPECIAL)
   upcasted: int = 0             # count that are upcasted     (this is remapping RANGE to UNROLL)
   dont_use_locals: bool = False # don't use local indexing
+  applied_opts: tuple = tuple()
 
 # ******** ops in python ********
 
