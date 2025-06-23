fbc6dcd61ef84a8d9135fc9a7bd496b839fd6e24
kevvz <92408564+kevvz@users.noreply.github.com>
2025-06-23 12:53:38 -0700

    wrap tests

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -1,6 +1,17 @@
 import numpy as np
 import unittest
 from tinygrad import Tensor
+from typing import List
+import functools
+
+def orthogonality_helper(A:Tensor,tolerance=1.0e-5):
+  b_shape,m = A.shape[0:-2],A.shape[-2]  #outer dimension should be the dim along orthogonality
+  A_identity = (Tensor.eye(m).reshape((1,) * len(b_shape)+(m,m)).expand(b_shape+(m,m)))
+  np.testing.assert_allclose((A @ A.transpose(-2,-1)).numpy(),A_identity.numpy(),atol=tolerance,rtol=tolerance)
+
+def reconstruction_helper(A:List[Tensor],B:Tensor, tolerance=1.0e-5):
+  reconstructed_tensor = functools.reduce(Tensor.matmul, A)
+  np.testing.assert_allclose(reconstructed_tensor.numpy(),B.numpy(),atol=tolerance,rtol=tolerance)
 
 class TestLinAlg(unittest.TestCase):
 
@@ -13,12 +24,9 @@ class TestLinAlg(unittest.TestCase):
       k = min(m,n)
       s_diag = (S.unsqueeze(-2) * Tensor.eye(k).reshape((1,) * len(b_shape) + (k,k)))
       s_diag = s_diag.expand(b_shape + (k,k)).pad(tuple([(0,0) for _ in range(len(size)-2)] + [(0,m-k), (0,n-k)]))
-      reconstructed_tensor = U @ s_diag @ V
-      u_identity = (Tensor.eye(m).reshape((1,) * len(b_shape)+(m,m)).expand(b_shape+(m,m)))
-      v_identity = (Tensor.eye(n).reshape((1,) * len(b_shape)+(n,n)).expand(b_shape+(n,n)))
-      np.testing.assert_allclose(reconstructed_tensor.numpy(),a.numpy(),atol=1e-5,rtol=1e-5)
-      np.testing.assert_allclose((U @ U.transpose(-2,-1)).numpy(),u_identity.numpy(),atol=1e-5,rtol=1e-5)
-      np.testing.assert_allclose((V @ V.transpose(-2,-1)).numpy(),v_identity.numpy(),atol=1e-5,rtol=1e-5)
+      orthogonality_helper(U)
+      orthogonality_helper(V)
+      reconstruction_helper([U,s_diag,V],a)
 
   def test_svd_nonfull(self):
     sizes = [(2,2),(5,3),(3,5),(2,2,2,2,3)]
@@ -28,15 +36,10 @@ class TestLinAlg(unittest.TestCase):
       b_shape,m,n = size[0:-2],size[-2],size[-1]
       k = min(m,n)
       s_diag = (S.unsqueeze(-2) * Tensor.eye(k).reshape((1,) * len(b_shape) + (k,k)).expand(b_shape + (k,k)))
-      reconstructed_tensor = U @ s_diag @ V
-      u_identity = (Tensor.eye(k).reshape((1,) * len(b_shape)+(k,k)).expand(b_shape + (k,k)))
-      v_identity = (Tensor.eye(k).reshape((1,) * len(b_shape)+(k,k)).expand(b_shape + (k,k)))
       #reduced U,V is only orthogonal along smaller dim
-      if (m < n): U_r,V_r = U @ U.transpose(-2,-1), V @ V.transpose(-2,-1)
-      else: U_r,V_r = U.transpose(-2,-1) @ U, V.transpose(-2,-1) @ V
-      np.testing.assert_allclose(reconstructed_tensor.numpy(),a.numpy(),atol=1e-5,rtol=1e-5)
-      np.testing.assert_allclose(U_r.numpy(),u_identity.numpy(),atol=1e-5,rtol=1e-5)
-      np.testing.assert_allclose(V_r.numpy(),v_identity.numpy(),atol=1e-5,rtol=1e-5)
+      if (m < n): orthogonality_helper(U),orthogonality_helper(V)
+      else: orthogonality_helper(U.transpose(-2,-1)),orthogonality_helper(V.transpose(-2,-1))
+      reconstruction_helper([U,s_diag,V],a)
 
   @unittest.skip("very big. recommend wrapping with TinyJit around inner function")
   def test_svd_large(self):
@@ -47,23 +50,17 @@ class TestLinAlg(unittest.TestCase):
     k = min(m,n)
     s_diag = (S.unsqueeze(-2) * Tensor.eye(k).reshape((1,) * len(b_shape) + (k,k)))
     s_diag = s_diag.expand(b_shape + (k,k)).pad(tuple([(0,0) for _ in range(len(size)-2)] + [(0,m-k), (0,n-k)]))
-    reconstructed_tensor = U @ s_diag @ V
-    u_identity = (Tensor.eye(m).reshape((1,) * len(b_shape)+(m,m)).expand(b_shape+(m,m)))
-    v_identity = (Tensor.eye(n).reshape((1,) * len(b_shape)+(n,n)).expand(b_shape+(n,n)))
-    np.testing.assert_allclose(reconstructed_tensor.numpy(),a.numpy(),atol=1e-3,rtol=1e-3)
-    np.testing.assert_allclose((U @ U.transpose(-2,-1)).numpy(),u_identity.numpy(),atol=1e-3,rtol=1e-3)
-    np.testing.assert_allclose((V @ V.transpose(-2,-1)).numpy(),v_identity.numpy(),atol=1e-3,rtol=1e-3)
+    orthogonality_helper(U,tolerance=1.0e-3)
+    orthogonality_helper(V,tolerance=1.0e-3)
+    reconstruction_helper([U,s_diag,V],a,tolerance=1.0e-3)
 
   def test_qr_general(self):
     sizes = [(3,3),(3,6),(6,3),(2,2,2,2,2)]
     for size in sizes:
       a = Tensor.randn(size).realize()
       Q,R = Tensor.qr(a)
-      reconstructed_tensor = Q @ R
-      Q_size = size[-2]
-      identity = Tensor.eye(Q_size).reshape((1,) * (len(size)-2) + (Q_size,Q_size)).expand(size[0:-2] + (Q_size,Q_size))
-      np.testing.assert_allclose(reconstructed_tensor.numpy(),a.numpy(),atol=1e-5, rtol=1e-5)
-      np.testing.assert_allclose((Q @ Q.transpose(-2,-1)).numpy(),identity.numpy(),atol=1e-5,rtol=1e-5)
+      orthogonality_helper(Q)
+      reconstruction_helper([Q,R],a)
 
 if __name__ == "__main__":
   unittest.main()
