ea140a761bc8dd3bae016133ffc1230ceb5e537b
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-06-23 15:53:45 +0000

    unified script

--- extra/nvpci/gpu_reset.sh
+++ extra/nvpci/gpu_reset.sh
@@ -1,7 +1,2 @@
-GPU=0000:01:00.0              # your PCI address
-echo 1 | sudo tee /sys/bus/pci/devices/$GPU/reset 2>/dev/null || {
-    # FLR not supported – fall back to remove/rescan
-    echo 1 | sudo tee /sys/bus/pci/devices/$GPU/remove
-    sleep 1
-    echo 1 | sudo tee /sys/bus/pci/rescan
-}
+GPU="$1"
+echo 1 | sudo tee /sys/bus/pci/devices/$GPU/reset 2>/dev/null
