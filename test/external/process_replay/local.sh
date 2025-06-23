96a6f59f57e7dfbe02ebb2bcfe1c86db860cc460
qazal <qazal.software@gmail.com>
2025-06-23 12:41:21 +0300

    back

--- test/external/process_replay/local.sh
+++ test/external/process_replay/local.sh
@@ -2,27 +2,9 @@
 
 set -e
 HEAD=$(git rev-parse --abbrev-ref HEAD)
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <command to process replay>"
-  exit 1
-fi
-
-# reset the exisitng replay cache
 python test/external/process_replay/reset.py
-
-# run the command just passed in with CAPTURE_PROCESS_REPLAY=1
-CAPTURE_PROCESS_REPLAY=1 "$@"
-
-# checkout master
-GIT=${GIT:-1}
-if [ "$GIT" -eq 1 ]; then
-  git checkout master
-  git checkout "$HEAD" -- test/external/process_replay/process_replay.py
-fi
-
-# run process replay
+CAPTURE_PROCESS_REPLAY=1 python test/test_ops.py TestOps.test_add
+git checkout master
+git checkout $HEAD -- test/external/process_replay/process_replay.py
 ASSERT_PROCESS_REPLAY=${ASSERT_PROCESS_REPLAY:-1} python test/external/process_replay/process_replay.py
-
-# install as pr util:
-# sudo ln -s "$(pwd)/test/external/process_replay/local2.sh" /usr/local/bin/pr && hash -r
-# pr python test/test_ops.py TestOps.test_add
+git checkout $HEAD
