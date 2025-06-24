700de611685df630dff51999c865097386f7ec3d
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-06-24 07:31:49 +0000

    xx

--- tinygrad/runtime/support/system.py
+++ tinygrad/runtime/support/system.py
@@ -54,7 +54,7 @@ class _System:
     else: self.lock_fd = os.open(lock_name, os.O_RDWR | os.O_CREAT | os.O_CLOEXEC, 0o666)
 
     try: fcntl.flock(self.lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except OSError: raise RuntimeError(f"Failed to open AM device {self.devfmt}. It's already in use.")
+    except OSError: raise RuntimeError(f"Failed to take lock file {name}. It's already in use.")
 
     return self.lock_fd
 
