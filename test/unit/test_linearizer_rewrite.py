5025683e3c7c6083cdc8756fde7e2f2de414e43f
qazal <qazal.software@gmail.com>
2025-06-24 17:57:13 +0300

    state in uops

--- test/unit/test_linearizer_rewrite.py
+++ test/unit/test_linearizer_rewrite.py
@@ -2,6 +2,7 @@ import unittest
 from tinygrad import Tensor, Context, Device
 from tinygrad.engine.realize import get_program
 from tinygrad.renderer import Opt, OptOps
+from tinygrad.uop.ops import KernelInfo
 
 class TestLinearizerRewrite(unittest.TestCase):
   def test_reduction(self):
@@ -9,20 +10,22 @@ class TestLinearizerRewrite(unittest.TestCase):
     out = (t*2).sum(axis=1)
     with Context(SPLIT_REDUCEOP=0, DEVECTORIZE=0):
       si = out.schedule()[-1]
-      opts_override = []
-      opts_override.append(Opt(OptOps.UPCAST, 0, 4))
-      opts_override.append(Opt(OptOps.UNROLL, 0, 4))
-      prg = get_program(si.ast, Device["CPU"].renderer, opts_override=opts_override)
+      opts_to_apply = []
+      opts_to_apply.append(Opt(OptOps.UPCAST, 0, 4))
+      opts_to_apply.append(Opt(OptOps.UNROLL, 0, 4))
+      ast = si.ast.replace(arg=KernelInfo(opts_to_apply=tuple(opts_to_apply)))
+      prg = get_program(ast, Device["CPU"].renderer)
       print(prg.src)
 
   def test_arange(self):
     out = Tensor.arange(32, device="NULL")
     with Context(SPLIT_REDUCEOP=0, DEVECTORIZE=0):
       si = out.schedule()[-1]
-      opts_override = []
-      opts_override.append(Opt(OptOps.UPCAST, 0, 4))
-      opts_override.append(Opt(OptOps.UNROLL, 0, 4))
-      prg = get_program(si.ast, Device["CPU"].renderer, opts_override=opts_override)
+      opts_to_apply = []
+      opts_to_apply.append(Opt(OptOps.UPCAST, 0, 4))
+      opts_to_apply.append(Opt(OptOps.UNROLL, 0, 4))
+      ast = si.ast.replace(arg=KernelInfo(opts_to_apply=tuple(opts_to_apply)))
+      prg = get_program(ast, Device["CPU"].renderer)
       print(prg.src)
 
 if __name__ == '__main__':
--- tinygrad/opt/__init__.py
+++ tinygrad/opt/__init__.py
@@ -1,28 +1,25 @@
 # opt opinionatedly transforms an ast into an optimized ast using either heuristics or beam search
 
-from typing import Sequence
 from tinygrad.opt.kernel import Kernel
 from tinygrad.opt.heuristic import hand_coded_optimizations
 from tinygrad.uop.ops import UOp
 from tinygrad.helpers import NOOPT, BEAM, USE_TC, getenv
-from tinygrad.renderer import Renderer, Opt
+from tinygrad.renderer import Renderer
 
-def get_optimized_ast(ast:UOp, renderer:Renderer, opts_override:Sequence[Opt]|None=None, name_override:str|None=None) -> UOp:
+def get_optimized_ast(ast:UOp, renderer:Renderer) -> UOp:
   """
   Optimize an AST based on heuristics or BEAM search.
 
   Args:
     ast: The Ops.SINK rooted AST
     renderer: The renderer used to generate the code
-    opts_override: Optionally override the opts used to transform the AST
-    name_override: Optionally override the name of the output AST
 
   Returns:
     The Ops.SINK rooted AST transformed to apply the opts and with a KernelInfo in the arg.
   """
 
   k = Kernel(ast, opts=renderer)
-  if opts_override is not None: k.apply_opts(opts_override)
+  if ast.arg is not None and ast.arg.opts_to_apply is not None: k.apply_opts(ast.arg.opts_to_apply)
   elif not NOOPT:
     if not k.apply_tensor_cores(USE_TC.value): k.apply_opts(hand_coded_optimizations(k))
     if BEAM >= 1:
@@ -30,4 +27,4 @@ def get_optimized_ast(ast:UOp, renderer:Renderer, opts_override:Sequence[Opt]|No
       kb = Kernel(ast, opts=renderer)
       rawbufs = bufs_from_lin(kb, allocate=False)
       k = beam_search(kb, rawbufs, BEAM.value, bool(getenv("BEAM_ESTIMATE", 1)))
-  return k.get_optimized_ast(name_override=name_override)
+  return k.get_optimized_ast()
