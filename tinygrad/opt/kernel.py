5a1911b7c46275064fbff32d93439f31b9ee4b94
George Hotz <72895+geohot@users.noreply.github.com>
2025-06-27 09:54:34 -0700

    apply the global dims late (#11002)
    * apply the global dims late [pr]
    
    * late gpudims
    
    * tests passing
    
    * remove the random local_dims inc
    
    * simpler

--- tinygrad/opt/kernel.py
+++ tinygrad/opt/kernel.py
@@ -454,8 +454,10 @@ class Kernel:
         # otherwise we just replace the VIEW source
         return ret.replace(src=(ret.src[0].replace(arg=st),)+ret.src[1:])
       if op.op is Ops.SINK:
+        # NOTE: should group_for_reduces be added to the local_dims?
         return ret.replace(arg = KernelInfo(ret.arg.name if ret.arg is not None else self.name if name_override is None else name_override,
-                                            self.local_dims, self.upcasted, self.dont_use_locals, tuple(self.applied_opts)))
+                                            self.global_dims if self.opts.has_local else 0, self.local_dims+self.group_for_reduces,
+                                            self.upcasted, self.dont_use_locals, tuple(self.applied_opts)))
       if op.op is Ops.REDUCE_AXIS:
         reduce_idx = len(self.bufs) + self.reduceops.index(op) * 2
 
