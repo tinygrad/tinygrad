39dfb8036e82c569a02fcc750b939a06e5a7d21b
George Hotz <geohot@gmail.com>
2025-07-02 14:33:40 -0700

    setitem has one less realize, why broken

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -1268,7 +1268,7 @@ class Tensor(MathTrait):
     if not isinstance(v, Tensor): raise TypeError(f"can't set a {type(v).__name__} to a Tensor")
     if self.requires_grad or v.requires_grad: raise NotImplementedError("setitem with requires_grad is not supported")
 
-    res = self.realize()._getitem(indices, v)
+    res = self._getitem(indices, v)
     # if shapes match and data is not shared it's a copy and we assign to self
     if res.shape == self.shape and res.uop is not self.uop:
       self.assign(res).realize()
