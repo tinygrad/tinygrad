3b85534df036696c9eea701ab9f3c3964d4cb4f0
George Hotz <72895+geohot@users.noreply.github.com>
2025-07-02 14:28:44 -0700

    outerworld range test [pr] (#11059)
    * outerworld range test [pr]
    
    * bound range
    
    * grad acc test
    
    * more tests
    
    * 5 steps is fine

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -412,17 +412,18 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   # *** uop Variable stuff ***
 
   @staticmethod
-  def variable(name:str, min_val:ConstType, max_val:ConstType, dtype:DType=dtypes.int):
+  def variable(name:str, min_val:ConstType, max_val:ConstType, dtype:DType=dtypes.int) -> UOp:
     assert not isinstance(min_val, UOp) and not isinstance(max_val, UOp), f"can't create Variable {name} with {min_val}/{max_val}"
     return UOp(Ops.DEFINE_VAR, dtype, arg=(name, min_val, max_val))
   @property
   def expr(self):
     assert self.op is Ops.DEFINE_VAR, f"op is {self.op}, need DEFINE_VAR"
     return self.arg[0]
-  def bind(self, val:int):
+  def bind(self, val:int|UOp):
     assert self.op is Ops.DEFINE_VAR, f"op is {self.op}, need DEFINE_VAR"
-    assert self.arg[1] <= val and val <= self.arg[2], f"bind {val} not in range [{self.arg[1]}, {self.arg[2]}]"
-    return UOp(Ops.BIND, self.dtype, (self, self.const_like(val)))
+    uval = self.const_like(val) if isinstance(val, int) else val
+    assert self.arg[1] <= uval.vmin and uval.vmax <= self.arg[2], f"bind {val} not in range [{self.arg[1]}, {self.arg[2]}]"
+    return UOp(Ops.BIND, self.dtype, (self, uval))
   def unbind(self) -> tuple[Variable, int]:
     assert self.op is Ops.BIND and self.src[0].op is Ops.DEFINE_VAR and self.src[1].op is Ops.CONST, f"can't unbind {self}"
     return self.src[0], self.src[1].arg
