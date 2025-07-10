29c321ca63bd933f70d2d709f95436eff444232e
George Hotz <geohot@gmail.com>
2025-07-10 14:29:57 -0700

    fix local shape

--- tinygrad/opt/kernel.py
+++ tinygrad/opt/kernel.py
@@ -494,10 +494,9 @@ class Kernel:
 
         ret = ret.replace(arg = (op.arg[0], axes))
         if self.group_for_reduces and grouped_axes:
-          local_shape = (1,) * self.global_dims + self.full_shape[self.global_dims:self.global_dims+self.local_dims] + \
-            tuple([self.full_shape[i] if self.sts[reduce_idx].shape[i] != self.sts[reduce_idx+1].shape[i] else 1 \
-              for i in range(self.first_reduce, self.first_reduce+self.group_for_reduces)]) + \
-            (1,) * (self.shape_len - self.upcasted - self.group_for_reduces - self.first_reduce) + tuple([x[0] for x in self.upcasted_axis(0)])
+          local_shape = tuple([s if self.axis_types[i] not in (AxisType.GLOBAL, AxisType.REDUCE) and \
+                               (self.axis_types[i] is not AxisType.GROUP_REDUCE or self.sts[reduce_idx].shape[i] != self.sts[reduce_idx+1].shape[i]) \
+                               else 1 for i,s in enumerate(self.full_shape)])
           st = ShapeTracker.from_shape(local_shape).expand(self.full_shape[:self.global_dims]+local_shape[self.global_dims:])
           local_size = st.real_size()
           local_buffer = UOp(Ops.DEFINE_LOCAL, op.dtype.ptr(local_size, local=True), (), f"temp{self.reduceops.index(op)}")
