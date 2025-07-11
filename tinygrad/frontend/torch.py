f1fa599976f626f647d2e830d81786442a701c22
mpai17 <mohnishpai7@gmail.com>
2025-07-10 22:22:19 -0500

    torch.compile TinyJIT for beautiful_mnist_torch
    Removed whitespace

--- tinygrad/frontend/torch.py
+++ tinygrad/frontend/torch.py
@@ -1,7 +1,7 @@
 # type: ignore
 import sys, pathlib
 sys.path.append(pathlib.Path(__file__).parent.parent.as_posix())
-try: 
+try:
   import extra.torch_backend.backend  # noqa: F401 # pylint: disable=unused-import
   import extra.torch_backend.test_compile  # noqa: F401 # Register the tiny backend for torch.compile
 except ImportError as e: raise ImportError("torch frontend not in release\nTo fix, install tinygrad from a git checkout with pip install -e .") from e
