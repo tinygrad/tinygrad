6da2973dd8d7c831581966326107447ffab48b52
George Hotz <geohot@gmail.com>
2025-07-11 13:31:04 -0700

    local

--- extra/metal_local_matmul.py
+++ extra/metal_local_matmul.py
@@ -4,6 +4,8 @@ from tinygrad.opt.kernel import Opt, OptOps
 from tinygrad.uop.ops import UOp, Ops, KernelInfo
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.engine.realize import get_program, CompiledRunner, ExecItem
+from tinygrad.uop.ops import graph_rewrite
+from tinygrad.kernelize.kernelize import merge_views
 
 N = 1024
 L = 8
@@ -16,23 +18,40 @@ if __name__ == "__main__":
   b2 = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=2)
 
   # TODO: this should not be a string, just a number
-  l1 = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(L, local=True), arg=f"temp0")
-  l2 = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(L, local=True), arg=f"temp1")
+  l1 = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(L*L, local=True), arg=f"temp0")
+  l2 = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(L*L, local=True), arg=f"temp1")
 
-  bs1 = b1.view(ShapeTracker.from_shape((N//L, N//L, L, L, N), (N*L, 0, N, 0, 1))).load()
-  bs2 = b2.view(ShapeTracker.from_shape((N//L, N//L, L, L, N), (0,   L, 0, 1, N))).load()
+  # global
+  gv1 = ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (N*L, 0, N, 0, L,   1))
+  gv2 = ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (0,   L, 0, 1, N*L, N))
+  # local-write
+  lwv1 = l1.view(ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (0, 0, L, 0, 0, 1)))
+  lwv2 = l2.view(ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (0, 0, 0, 1, 0, L)))
+  # local-read
+  lrv1 = l1.view(ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (0, 0, L, 0, 0, 1)))
+  lrv2 = l2.view(ShapeTracker.from_shape((N//L, N//L, L, L, N//L, L), (0, 0, 0, 1, 0, L)))
 
-  lv1 = l1.view(ShapeTracker.from_shape((N//L, N//L, L, L, N), (0, 0, 1, 0, 0)))
-  lv2 = l2.view(ShapeTracker.from_shape((N//L, N//L, L, L, N), (0, 0, 0, 1, 0)))
+  # global_load-local_store optimizations. you have to apply the same permutation to both sides of the load/store
+  gv1 = gv1.permute((0,1,2,5,4,3))
+  lwv1 = lwv1.permute((0,1,2,5,4,3))
+  gv2 = gv2.permute((0,1,2,5,4,3))
+  lwv2 = lwv2.permute((0,1,2,5,4,3))
 
-  lbs1 = lv1.load(lv1.store(bs1))
-  lbs2 = lv2.load(lv2.store(bs2))
+  # local_store-local_load optimizations. you have to apply the same permutation to both sides of the load/store
+  lwv2 = lwv2.permute((0,1,2,5,4,3))
+  lrv2 = lrv2.permute((0,1,2,5,4,3))
 
-  mat = (lbs1*lbs2).r(Ops.ADD, (4,))
-  st =  b0.view(ShapeTracker.from_shape((N//L, N//L, L, L, 1), (N*L, L, N, 1, 0))).store(mat)
+  bs1 = b1.view(gv1).load()
+  bs2 = b2.view(gv2).load()
+  lbs1 = lrv1.load(lwv1.store(bs1))
+  lbs2 = lrv2.load(lwv2.store(bs2))
+  #lbs1, lbs2 = bs1, bs2
 
-  ast = st.sink(arg=KernelInfo(global_dims=2, local_dims=2))
+  mat = (lbs1*lbs2).r(Ops.ADD, (4, 5))
+  st =  b0.view(ShapeTracker.from_shape((N//L, N//L, L, L, 1, 1), (N*L, L, N, 1, 0, 0))).store(mat)
 
+  ast = st.sink(arg=KernelInfo(global_dims=2, local_dims=2, upcasted=1))
+  ast = graph_rewrite(ast, merge_views)
   prg = get_program(ast, Device.default.renderer)
   print(prg.src)
 
