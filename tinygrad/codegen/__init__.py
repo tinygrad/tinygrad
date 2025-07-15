8198655e669f7afbf0602450e117f1c6c12d79f9
ttomsa <tomasvsilva8@gmail.com>
2025-07-15 22:29:59 +0100

    combine late rewrite pats with pre matcher to split vectors

--- tinygrad/codegen/__init__.py
+++ tinygrad/codegen/__init__.py
@@ -64,14 +64,14 @@ def _get_rewrites_for_renderer(opts:Renderer, linearizer:bool, _QUANTIZE, _DEVEC
   ret.append(RewriteStep(pm_devectorize, lambda _: opts, name="devectorize"))
 
   supported_ops = tuple(opts.code_for_op.keys())
+  pre_matcher = opts.pre_matcher if opts.pre_matcher is not None else PatternMatcher([])
   extra_matcher = opts.extra_matcher if opts.extra_matcher is not None else PatternMatcher([])
 
-  # optional pre matcher
-  if opts.pre_matcher is not None: ret.append(RewriteStep(opts.pre_matcher, name="pre_matcher"))
+  # late rewrite patterns (without sym)
+  ret.append(RewriteStep(symbolic_simple+get_late_rewrite_patterns(supported_ops, _TRANSCENDENTAL>=2)+pre_matcher, lambda _: opts, name="late rewrite"))
 
   # final rules for the renderer (without sym)
-  pm_final_rewrite = symbolic_simple+get_late_rewrite_patterns(supported_ops, _TRANSCENDENTAL>=2)+pm_render+extra_matcher
-  ret.append(RewriteStep(pm_final_rewrite, lambda _: opts, name="final rewrite"))
+  ret.append(RewriteStep(symbolic_simple+pm_render+extra_matcher, name="renderer rewrite"))
 
   # return the list (with optional linearizer)
   return ret + (rewrites_for_linearizer if linearizer else [])
