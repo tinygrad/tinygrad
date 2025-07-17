2fa8694c81173855fae2fd3feaf7a64c19fc972b
hooved <172129504+hooved@users.noreply.github.com>
2025-07-17 11:07:54 -0400

    update dev_run / dependencies

--- examples/mlperf/training_submission_v5.1/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_green/dev_run.sh
+++ examples/mlperf/training_submission_v5.1/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_green/dev_run.sh
@@ -5,23 +5,26 @@
 #   The focus is on correctness, not speed - I use CPU because my local GPU isn't big enough for holding all the training params.
 # TODO: next steps: train to convergence on one big NVIDIA GPU (cloud rented), then on tinybox
 
-# dependencies
-# pip install tqdm
+# *** dependencies
+#pip install tqdm
+#pip install numpy
+
+## to match mlperf reference clip tokenizer behavior
+#pip install ftfy
+#pip install regex
+
+## to use mlperf reference dataloader
+#pip install webdataset
+#pip install torch # for torch.utils.data.DataLoader, which webdataset depends on
 
 export PYTHONPATH="." NV=1
 
-#export MODEL="bert"
 export MODEL="stable_diffusion"
 
-#export DEFAULT_FLOAT="HALF" SUM_DTYPE="HALF" GPUS=6 BS=96 EVAL_BS=96
-export BS=1 EVAL_BS=1
+# https://github.com/mlcommons/training_policies/blob/master/training_rules.adoc#benchmark_specific_rules
+# Checkpoint must be collected every 512,000 images. CEIL(512000 / global_batch_size) if 512000 is not divisible by GBS.
+export BS=1 EVAL_BS=512000
 
-#export FUSE_ARANGE=1 FUSE_ARANGE_UINT=0
-
-#export BEAM=8 BEAM_UOPS_MAX=10000 BEAM_UPCAST_MAX=256 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=5
-#export IGNORE_JIT_FIRST_BEAM=1
-
-#export BASEDIR="/raid/datasets/wiki"
 # TODO: this will change when training on a tinybox
 export BASEDIR="/home/hooved/train-sd/training/stable_diffusion"
 
