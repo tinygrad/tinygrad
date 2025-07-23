fad5496c8a7e41054a23f55f869d5390d4fa0e68
b1tg <b1tg@users.noreply.github.com>
2025-07-23 11:50:24 +0800

    fix tests

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -254,14 +254,13 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   @staticmethod
   def range(dtype:DType, end:sint, idx:int): return UOp(Ops.RANGE, dtype=dtype, src=(sint_to_uop(end),), arg=idx)
   def r(self, op:Ops, axis:tuple[int, ...], permute=True):
-    axis = tuple(sorted([x for x in axis if resolve(self.shape[x] != 1)]))
     if len(axis) == 0: return self
     # move any non reduce axis before the first reduce axis
-    move_early, rest = partition(range(axis[0], len(self.shape)), lambda i: i not in axis and resolve(self.shape[i] != 1))
+    move_early, rest = partition(range(axis[0], len(self.shape)), lambda i: i not in axis)
     if move_early and permute:
       permaxis = tuple(range(axis[0])) + tuple(move_early) + tuple(rest)
       ret = self.permute(permaxis)
-      new_axis = tuple([x for x in range(axis[0]+len(move_early), len(self.shape)) if resolve(ret.shape[x] != 1)])
+      new_axis = tuple([x for x in range(axis[0]+len(move_early), len(self.shape))])
       assert len(axis) == len(new_axis)
     else:
       ret, new_axis = self, axis
