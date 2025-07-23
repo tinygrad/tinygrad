ece46048259d51929bce5a869709a64d67936589
George Hotz <geohot@gmail.com>
2025-07-23 16:36:37 -0700

    cleanups

--- extra/gemm/amd_uop_matmul.py
+++ extra/gemm/amd_uop_matmul.py
@@ -1,154 +1,13 @@
 from tinygrad import Tensor, Device, Context, GlobalCounters, dtypes
-from tinygrad.helpers import prod, unwrap
 from tinygrad.uop.ops import UOp, Ops, KernelInfo
-from tinygrad.opt.kernel import AxisType
 from tinygrad.engine.realize import CompiledRunner, ExecItem, get_program
-from tinygrad.uop.ops import graph_rewrite, PatternMatcher, UPat, Ops, UOp, GroupOp, track_matches
-from tinygrad.shape.shapetracker import ShapeTracker, strides_for_shape
-from tinygrad.schedule.kernelize import merge_views
-from tinygrad.shape.view import View
+from tinygrad.uop.ops import Ops, UOp
 from tinygrad.dtype import AddrSpace
 
 N = 4096
 run_count = 5
 
-# change reduceop axes and input ShapeTrackers, view gets replaced with a reshape.
-# src->r->view  -->   src->view->r
-def swizzle_reduceop(src:UOp, r:UOp, view:UOp):
-  if r.tag is not None: return None
-  # confirm the input is in order
-  # TODO: replace this with a UOp that allows for nothing else then remove this
-  permute = tuple(i for i in range(len(src.shape)) if i not in r.axis_arg)+r.axis_arg
-  assert permute == tuple(range(len(permute))), f"reduce axis must already be in order, {permute} isn't"
-
-  # append the reduce shape to each of the views
-  reduce_count = len(r.axis_arg)
-  prshape = prod(rshape:=src.shape[-reduce_count:])
-  rstrides = strides_for_shape(rshape)
-  nv = [View.create(v.shape[:-reduce_count]+rshape, tuple(x*prshape for x in v.strides[:-reduce_count])+rstrides, v.offset*prshape,
-                    v.mask[:-reduce_count]+tuple((0,s) for s in rshape) if v.mask is not None else None) for v in unwrap(view.st).views]
-
-  # no reshape required with shrinking REDUCE_AXIS
-  return UOp(Ops.REDUCE_AXIS, r.dtype, (src.view(ShapeTracker(tuple(nv))),),
-             (r.arg[0], tuple(range(len(view.shape)-reduce_count, len(view.shape)))))
-
-early_view_left = merge_views+PatternMatcher([
-  # view before elementwise and buffer ops
-  (UPat(Ops.VIEW, src=(UPat({*GroupOp.ALU, Ops.CAST, Ops.BITCAST, Ops.BIND, Ops.VALID, Ops.STORE, Ops.LOAD}, name="e"),), name="view"),
-   lambda e,view: e.replace(src=tuple(s.view(view.st) for s in e.src)) if e.tag is None else None),
-  # push a non contiguous ShapeTracker through reduceop
-  (UPat(Ops.VIEW, src=(UPat(Ops.REDUCE_AXIS, src=(UPat.var("src"),), name="r"),), name="view"), swizzle_reduceop),
-])
-
-def hand_spec():
-  # Block Tile size  . 128x128
-  # Thread Tile size . 4x4
-  # Wave Tile size   . 128x32
-  # A wave is        . 8x4
-  # ────── problem size and tiling params (mirror the C kernel) ───────────────────
-  BK = 8             # depth of K-tile
-  BN = BM = 128      # block-tile (output) sizes
-  # the real thread is 16x8 = 128 regs
-  TM = 4
-  nbIterWaveM = 2
-  TN = 4
-  nbIterWaveN = 4
-
-  # ────── shared-memory tile sizes (unchanged) ───────────────────────────────────
-  LDS_A_SZ = BK * BM          # 1024 floats
-  LDS_B_SZ = BK * BN          # 1024 floats
-
-  bC = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=0)   # output C
-  bA = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=1)   # input A
-  bB = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=2)   # input B
-
-  # TODO: this should not be a string, just a number
-  lAs = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(LDS_A_SZ, addrspace=AddrSpace.LOCAL), arg="As")
-  lBs = UOp(Ops.DEFINE_LOCAL, dtypes.float.ptr(LDS_B_SZ, addrspace=AddrSpace.LOCAL), arg="Bs")
-
-  s0 = ShapeTracker.from_shape((N, N, N), (N, 0, 1))
-  s1 = ShapeTracker.from_shape((N, N, N), (0, 1, N))
-  s2 = ShapeTracker.from_shape((N, N, 1), (N, 1, 0))
-
-  ls0 = ShapeTracker.from_shape((BM, BK))
-  ls1 = ShapeTracker.from_shape((BN, BK))
-
-  buf_at = [AxisType.GLOBAL, AxisType.UPCAST, AxisType.LOCAL, AxisType.LOCAL, AxisType.LOCAL, AxisType.LOCAL, AxisType.UPCAST, AxisType.UPCAST]
-  buf_bt = [AxisType.GLOBAL, AxisType.UPCAST, AxisType.LOCAL, AxisType.LOCAL, AxisType.LOCAL, AxisType.LOCAL, AxisType.UPCAST, AxisType.UPCAST]
-  axis_types = buf_at + buf_bt + [AxisType.REDUCE, AxisType.UNROLL, AxisType.UNROLL, AxisType.UNROLL]
-
-  # 128 x 128 x 8
-  full_shape = (N//BM, 2, 2, 2, 2, 2, 2, 2, N//BN, 2, 2, 2, 2, 2, 2, 2, N//BK, 2, 2, 2)
-
-  s0 = s0.reshape(full_shape)
-  s1 = s1.reshape(full_shape)
-  s2 = s2.reshape(full_shape[:-4] + (1,)*4)
-
-  ls0 = ls0.reshape((1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2)).expand(s0.shape)
-  ls1 = ls1.reshape((1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2)).expand(s1.shape)
-  assert ls0.real_size() == LDS_A_SZ
-  assert ls1.real_size() == LDS_B_SZ
-
-  # BK is a loop of 8
-  #  each loop reads 8 in A, 16 in B
-
-  print(ls0)
-  print(ls1)
-
-  permaxis = []
-  for axis_order in [AxisType.GLOBAL, AxisType.LOCAL, AxisType.LOOP, AxisType.UPCAST, AxisType.GROUP_REDUCE, AxisType.REDUCE, AxisType.UNROLL]:
-    permaxis += [i for i,a in enumerate(axis_types) if a == axis_order]
-  axis_types = [axis_types[x] for x in permaxis]
-  s0, s1, s2, ls0, ls1 = [x.permute(tuple(permaxis)) for x in [s0, s1, s2, ls0, ls1]]
-  print(axis_types)
-
-  lw0, lr0 = ls0, ls0
-  lw1, lr1 = ls1, ls1
-
-  # first round of permutes
-
-  permaxis = (0, 1, 19, 18, 17, 12, 11, 10, 5, 4, 3, 2, 6, 7, 8, 9, 16, 13, 14, 15)
-  s0 = s0.permute(permaxis)
-  lw0 = lw0.permute(permaxis)
-
-  permaxis = (0, 1, 15, 14, 9, 8, 7, 6, 13, 19, 18, 17, 5, 4, 3, 2, 16, 12, 11, 10)
-  s1 = s1.permute(permaxis)
-  lw1 = lw1.permute(permaxis)
-
-  # second round of permutes
-  #permaxis = (0, 1, 12, 11, 5, 4, 3, 2, 10, 6, 7, 8, 9, 13, 14, 15, 16, 17, 18, 19)
-  #lw0 = lw0.permute(permaxis)
-  #lr0 = lr0.permute(permaxis)
-
-  from tinygrad.opt.kernel import axis_colors, colored
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(s0.shape, s0.views[0].strides, axis_types)]))
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(s1.shape, s1.views[0].strides, axis_types)]))
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(s2.shape, s2.views[0].strides, axis_types)]))
-  print("lw")
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(lw0.shape, lw0.views[0].strides, axis_types)]))
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(lw1.shape, lw1.views[0].strides, axis_types)]))
-  print("lr")
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(lr0.shape, lr0.views[0].strides, axis_types)]))
-  print('_'.join([colored(f"{s}({st})", axis_colors[x]) for s,st,x in zip(lr1.shape, lr1.views[0].strides, axis_types)]))
-
-  # loads and stores
-  bs0 = bA.view(s0).load()
-  bs1 = bB.view(s1).load()
-  bs0 = lAs.view(lr0).load(lAs.view(lw0).store(bs0))
-  bs1 = lBs.view(lr1).load(lBs.view(lw1).store(bs1))
-
-  mat = (bs0 * bs1).r(Ops.ADD, tuple([i for i,a in enumerate(axis_types) if a in (AxisType.REDUCE, AxisType.UNROLL)]), permute=False)
-  st = bC.view(s2).store(mat)
-
-  ast = st.sink(arg=KernelInfo(axis_types=tuple(axis_types), name="tinygemm"))
-  ast = graph_rewrite(ast, merge_views)
-  prg = get_program(ast, Device.default.renderer)
-  print(prg.src)
-  return prg
-
-def hand_spec_2():
-  N = 4096
-
+def hand_spec_kernel3():
   BLOCK_SIZE = 256
 
   BN = 128
@@ -165,7 +24,7 @@ def hand_spec_2():
   nbWaveX = BN // WN
   nbWaveY = BM // WM
 
-  threadIdx_x = UOp(Ops.SPECIAL, dtypes.int, arg=("lidx0", 256))
+  threadIdx_x = UOp(Ops.SPECIAL, dtypes.int, arg=("lidx0", BLOCK_SIZE))
   waveIndex = threadIdx_x // 32
   waveIdx = waveIndex % nbWaveX
   waveIdy = waveIndex // nbWaveX
@@ -195,8 +54,8 @@ def hand_spec_2():
   nbReadsB = BN * BK // BLOCK_SIZE
   nbReadsA = BM * BK // BLOCK_SIZE
 
-  blockIdx_x = UOp(Ops.SPECIAL, dtypes.int, arg=("gidx0", N//128))
-  blockIdx_y = UOp(Ops.SPECIAL, dtypes.int, arg=("gidx1", N//128))
+  blockIdx_x = UOp(Ops.SPECIAL, dtypes.int, arg=("gidx0", N//BN))
+  blockIdx_y = UOp(Ops.SPECIAL, dtypes.int, arg=("gidx1", N//BM))
 
   a = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=0)
   b = UOp(Ops.DEFINE_GLOBAL, dtypes.float.ptr(N*N), arg=1)
@@ -214,40 +73,44 @@ def hand_spec_2():
   kId_range = UOp.range(dtypes.int, N//BK, 0)
   kId = kId_range*BK
 
+  # load from globals into locals
   i = UOp.range(dtypes.int, nbReadsB, 1)
   index_x = BN * blockIdx_x + rBIdx
   index_y = rBIdy + i * strideReadB + kId
-  Bs_store = Bs.index((index_y % BK) * BN + index_x % BN).store(b.index(N * index_y + index_x).load(), i)
+  Bs_store = Bs[(index_y % BK) * BN + index_x % BN].store(b[N * index_y + index_x].load(), i)
 
   i = UOp.range(dtypes.int, nbReadsA, 2)
   index_x = rAIdx + kId
   index_y = BM * blockIdx_y + rAIdy + i * strideReadA
-  As_store = As.index((index_x % BK) * BM + index_y % BM).store(a.index(N * index_y + index_x).load(), i)
+  As_store = As[(index_x % BK) * BM + index_y % BM].store(a[N * index_y + index_x].load(), i)
 
   barrier = UOp(Ops.BARRIER, src=(As_store, Bs_store))
 
   k = UOp.range(dtypes.int, BK, 3)
 
+  # load from locals into registers
   iterWave = UOp.range(dtypes.int, nbIterWaveN, 4)
   i = UOp.range(dtypes.int, TN, 5)
   index = waveIdx * WN + iterWave * SUBWN + TN * idxInWave + i
-  B_row_store = B_row.index(iterWave*TN + i).store(Bs.index(k*BN + index).load(barrier), iterWave, i)
+  B_row_store = B_row[iterWave*TN + i].store(Bs[k*BN + index].load(barrier), iterWave, i)
 
   iterWave = UOp.range(dtypes.int, nbIterWaveM, 6)
   i = UOp.range(dtypes.int, TM, 7)
   index = waveIdy * WM + iterWave * SUBWM + TM * idyInWave + i
-  A_col_store = A_col.index(iterWave*TM + i).store(As.index(k*BM + index).load(barrier), iterWave, i)
+  A_col_store = A_col[iterWave*TM + i].store(As[k*BM + index].load(barrier), iterWave, i)
 
+  # do the GEMM math
   iterWaveM = UOp.range(dtypes.int, nbIterWaveM, 8)
   iterWaveN = UOp.range(dtypes.int, nbIterWaveN, 9)
   yt = UOp.range(dtypes.int, TM, 10)
   xt = UOp.range(dtypes.int, TN, 11)
   x = iterWaveN * TN + xt
   y = iterWaveM * TM + yt
-  c_regs_idx = c_regs.index(y * TN * nbIterWaveN + x)
-  sink = c_regs_idx.store(c_regs_idx.load() + A_col.index(y).load(A_col_store) * B_row.index(x).load(B_row_store),
+  c_regs_idx = c_regs[y * TN * nbIterWaveN + x]
+  sink = c_regs_idx.store(c_regs_idx.load() + A_col[y].load(A_col_store) * B_row[x].load(B_row_store),
                           iterWaveM, iterWaveN, yt, xt, k, kId_range)
 
+  # store c_regs into c
   iterWaveM = UOp.range(dtypes.int, nbIterWaveM, 12)
   iterWaveN = UOp.range(dtypes.int, nbIterWaveN, 13)
   xOut = blockIdx_x * BN + waveIdx * WN + iterWaveN * SUBWN + TN * idxInWave
@@ -255,13 +118,12 @@ def hand_spec_2():
   yt = UOp.range(dtypes.int, TM, 14)
   xt = UOp.range(dtypes.int, TN, 15)
   indexC = N * (yOut + yt) + xOut + xt
-  sink = c.index(indexC).store(c_regs.index(TN * nbIterWaveN * (iterWaveM * TM + yt) + (iterWaveN * TN + xt)).load(sink),
-                               iterWaveM, iterWaveN, yt, xt)
+  sink = c[indexC].store(c_regs[TN * nbIterWaveN * (iterWaveM * TM + yt) + (iterWaveN * TN + xt)].load(sink), iterWaveM, iterWaveN, yt, xt)
 
   return sink.sink(arg=KernelInfo(name="tinygemm"))
 
 if __name__ == "__main__":
-  hprg = hand_spec_2()
+  hprg = hand_spec_kernel3()
   prg = get_program(hprg, Device.default.renderer)
   print(prg.src)
   hrunner = CompiledRunner(prg)
