65d07d2d744a8fa37e5fd50510aa58ad3726ef18
qazal <qazal.software@gmail.com>
2025-07-25 17:49:35 +0300

    also print stats

--- test.py
+++ test.py
@@ -8,27 +8,33 @@ from tinygrad.helpers import diskcache
 def get(s):
   return list(parse_xml(xctrace_export(s).stdout))
 
-with open(temp("profile.pkl", append_user=True), "rb") as f: w = pickle.load(f)
-commands = [e for e in w if isinstance(e, ProfileRangeEvent) and e.device == "METAL"]
-tinygemm = next(iter([c for c in commands if c.name == "tinygemm"]))
-
 time_info = get("time-info")[0]
 num, denom = [int(field.text) for field in time_info["timebase-info"].findall("mach-timebase-info-field")]
 mabs_epoch, timebase = int(time_info["mabs-epoch"]), num/denom
 
-counter_values:dict[int, float] = {}
-for row in get("gpu-counter-value"):
-  sample_ts = xctrace_to_cpu_time(int(row["timestamp"]), mabs_epoch, timebase)
-  if sample_ts < tinygemm.st: continue
-  if sample_ts > tinygemm.en: break
-  counter_values.setdefault(row.pop("counter-id"), []).append(float(row["value"]))
-
+counter_info = {row.pop("counter-id"):row for row in get("gpu-counter-info")}
 COUNTER_GROUPS = {"ALU":[11, 13, 15, 17, 19, 21, 23], "DRAM":[62, 64], "SRAM":[25]}
-counter_info = {}
-for row in get("gpu-counter-info"): counter_info[row.pop("counter-id")] = row
 
-ret = {}
-for k,v in counter_values.items():
-  if (group:=next((g for g,lst in COUNTER_GROUPS.items() if int(k) in lst), None)) is None: continue
-  ret.setdefault(group, {})[counter_info[k]["name"]] = statistics.mean(v)
-print(ret)
+def get_stats(st:float, en:float):
+  counter_values:dict[int, float] = {}
+  for row in get("gpu-counter-value"):
+    sample_ts = xctrace_to_cpu_time(int(row["timestamp"]), mabs_epoch, timebase)
+    if sample_ts < st: continue
+    if sample_ts > en: break
+    counter_values.setdefault(row.pop("counter-id"), []).append(float(row["value"]))
+
+  ret = {g:{} for g in COUNTER_GROUPS}
+  for k,v in counter_values.items():
+    if (group:=next((g for g,lst in COUNTER_GROUPS.items() if int(k) in lst), None)) is None: continue
+    ret[group][counter_info[k]["name"]] = statistics.mean(v)
+  return ret
+
+with open(temp("profile.pkl", append_user=True), "rb") as f: profile = pickle.load(f)
+for e in profile:
+  if isinstance(e, ProfileRangeEvent) and e.device == "METAL":
+    if not (ret:=get_stats(e.st, e.en)): continue
+    print(e.name)
+    for group, vals in ret.items():
+      print(f"** {group}")
+      long = max((len(k) for k in vals), default=0)
+      for k, v in vals.items(): print(f"{k:<{long}} {v:>6.2f}%")
