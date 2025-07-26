83dd5e4c1f2dc72897b4e376d7eec9045847919f
hooved <172129504+hooved@users.noreply.github.com>
2025-07-26 11:52:08 -0400

    add data/checkpoint download script

--- globvars.py
+++ globvars.py
@@ -6,9 +6,6 @@ def md(a:Tensor, b:Tensor):
   max_diff = diff.max()
   mean_diff = diff.mean()
   assert a.dtype in (dtypes.float16, dtypes.float32)
-  eps = 2e-5 if a.dtype is dtypes.float16 else 1e-8
-  eps_str = "2e-5" if a.dtype is dtypes.float16 else "1e-8"
-  ratio = (diff / (a.abs() + eps)).mean()
   print(f"diff.abs().mean(): {mean_diff.item()}")
   print(f"a.abs().mean(): {a.abs().mean().item()}")
   print(f"diff.abs().max(): {max_diff.item()}")
@@ -17,6 +14,3 @@ def md(a:Tensor, b:Tensor):
 
 d = safe_load("/home/hooved/train-sd/training/stable_diffusion/checkpoints/mixed.safetensors")
 for v in d.values(): v.to_("NV").realize()
-
-measure_layernorm=True
-measure_softmax=True
