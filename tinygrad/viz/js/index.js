6174cfa8280c9fee820bdd74e5e0307e9d16e56c
qazal <77887910+Qazalin@users.noreply.github.com>
2025-07-28 00:25:00 +0800

    viz: only show match counts greater than 0 (#11394)

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -456,7 +456,7 @@ async function main() {
       for (const [j,u] of steps.entries()) {
         const inner = ul.appendChild(document.createElement("ul"));
         inner.id = `step-${i}-${j}`;
-        inner.innerText = `${u.name ?? u.loc[0].replaceAll("\\", "/").split("/").pop()+':'+u.loc[1]}`+(u.match_count != null ? `- ${u.match_count}` : '');
+        inner.innerText = `${u.name ?? u.loc[0].replaceAll("\\", "/").split("/").pop()+':'+u.loc[1]}`+(u.match_count ? ` - ${u.match_count}` : '');
         inner.style.marginLeft = `${8*u.depth}px`;
         inner.onclick = (e) => {
           e.stopPropagation();
