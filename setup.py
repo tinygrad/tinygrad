e0fa35363050d080e4bc51814e2260fcb49bec2c
cypix <thesgadyal@gmail.com>
2025-07-29 03:37:42 +0530

    dep fix

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(name='tinygrad',
             "boto3",
             "pandas",
             "influxdb3-python",
-            "muon @ git+https://github.com/KellerJordan/Muon",
+            "muon-optimizer @ git+https://github.com/KellerJordan/muon.git@master",
         ],
         'docs': [
             "mkdocs",
