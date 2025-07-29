f3a6d98dde7e3480ae27357076c464ea285f1f65
kevvz <92408564+kevvz@users.noreply.github.com>
2025-07-29 00:19:02 -0700

    correct comment

--- tinygrad/nn/optim.py
+++ tinygrad/nn/optim.py
@@ -81,7 +81,7 @@ def SGD(params: list[Tensor], lr=0.001, momentum=0.0, weight_decay=0.0, nesterov
   """
   return LARS(params, lr, momentum, weight_decay, nesterov, classic, tcoef=0.0, fused=fused)
 
-# Muon applies the newton schulz algorithm on b. also can include momentum, nesterov, and weight decay
+# Muon applies the newton schulz algorithm on gradient. also can include momentum, nesterov, and weight decay
 def Muon(params: list[Tensor], lr=0.02, momentum=0.95, weight_decay=0.0, nesterov=True, \
          classic=False, fused=FUSE_OPTIM, ns_params=(3.4445, -4.7750, 2.0315)):
   """
