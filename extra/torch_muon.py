bf7164d7fbf0ff991e499d2bcc452be1525fe4ec
kevvz <92408564+kevvz@users.noreply.github.com>
2025-07-29 13:49:22 -0700

    spacing

--- extra/torch_muon.py
+++ extra/torch_muon.py
@@ -2,72 +2,72 @@ import torch
 
 #credit to KellerJordan at https://github.com/KellerJordan/Muon/tree/master
 def zeropower_via_newtonschulz5(G:torch.tensor, steps: int):
-    """
-    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
-    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
-    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
-    zero even beyond the point where the iteration no longer converges all the way to one everywhere
-    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
-    where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
-    performance at all relative to UV^T, where USV^T = G is the SVD.
-    """
-    assert G.ndim >= 2 # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
+  """
+  Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+  quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+  of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+  zero even beyond the point where the iteration no longer converges all the way to one everywhere
+  on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+  where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
+  performance at all relative to UV^T, where USV^T = G is the SVD.
+  """
+  assert G.ndim >= 2 # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
 
-    a, b, c = (3.4445, -4.7750,  2.0315)
-    # X = G.bfloat16()
-    X = G
-    if G.size(-2) > G.size(-1):
-        X = X.mT
+  a, b, c = (3.4445, -4.7750,  2.0315)
+  # X = G.bfloat16()
+  X = G
+  if G.size(-2) > G.size(-1):
+      X = X.mT
 
-    # Ensure spectral norm is at most 1
-    X = X / (X.norm(dim=(-2, -1), keepdim=True) + 1e-5)
-    # Perform the NS iterations
-    for _ in range(steps):
-        A = X @ X.mT
-        B = b * A + c * A @ A # quintic computation strategy adapted from suggestion by @jxbz, @leloykun, and @YouJiacheng
-        X = a * X + B @ X
+  # Ensure spectral norm is at most 1
+  X = X / (X.norm(dim=(-2, -1), keepdim=True) + 1e-5)
+  # Perform the NS iterations
+  for _ in range(steps):
+    A = X @ X.mT
+    B = b * A + c * A @ A # quintic computation strategy adapted from suggestion by @jxbz, @leloykun, and @YouJiacheng
+    X = a * X + B @ X
     
-    if G.size(-2) > G.size(-1):
-        X = X.mT
+  if G.size(-2) > G.size(-1):
+    X = X.mT
 
-    return X
+  return X
 
 def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True):
 
-    momentum.lerp_(grad, beta)
-    update = grad.lerp_(momentum, beta) if nesterov else momentum
-    if update.ndim == 4: # for the case of conv filters
-        update = update.view(len(update), -1)
-    update = zeropower_via_newtonschulz5(update, steps=ns_steps)
-    # update *= max(1, grad.size(-2) / grad.size(-1))**0.5
-    return update
+  momentum.lerp_(grad, beta)
+  update = grad.lerp_(momentum, beta) if nesterov else momentum
+  if update.ndim == 4: # for the case of conv filters
+    update = update.view(len(update), -1)
+  update = zeropower_via_newtonschulz5(update, steps=ns_steps)
+  # update *= max(1, grad.size(-2) / grad.size(-1))**0.5
+  return update
 
 class SingleDeviceMuon(torch.optim.Optimizer):
-    """
-    Muon variant for usage in non-distributed settings.
-    """
-    def __init__(self, params, lr=0.02, weight_decay=0, momentum=0.95):
-        defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum)
-        super().__init__(params, defaults)
+  """
+  Muon variant for usage in non-distributed settings.
+  """
+  def __init__(self, params, lr=0.02, weight_decay=0, momentum=0.95):
+    defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum)
+    super().__init__(params, defaults)
 
-    @torch.no_grad()
-    def step(self, closure=None):
+  @torch.no_grad()
+  def step(self, closure=None):
 
-        loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
+    loss = None
+    if closure is not None:
+      with torch.enable_grad():
+        loss = closure()
 
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
-                    p.grad = torch.zeros_like(p)  # Force synchronization
-                state = self.state[p]
-                if len(state) == 0:
-                    state["momentum_buffer"] = torch.zeros_like(p)
-                update = muon_update(p.grad, state["momentum_buffer"], beta=group["momentum"])
-                p.mul_(1 - group["lr"] * group["weight_decay"])
+    for group in self.param_groups:
+      for p in group["params"]:
+        if p.grad is None:
+          p.grad = torch.zeros_like(p)  # Force synchronization
+        state = self.state[p]
+        if len(state) == 0:
+          state["momentum_buffer"] = torch.zeros_like(p)
+        update = muon_update(p.grad, state["momentum_buffer"], beta=group["momentum"])
+        p.mul_(1 - group["lr"] * group["weight_decay"])
 
-                p.add_(update.reshape(p.shape), alpha=-group["lr"])
+        p.add_(update.reshape(p.shape), alpha=-group["lr"])
 
-        return loss
+    return loss
