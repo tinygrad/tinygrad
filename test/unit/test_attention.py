cc928c927fc7f71540c0782a3d1534c16444fbda
Jonathan Coletti <88168630+JonathanColetti@users.noreply.github.com>
2025-07-30 00:13:49 -0400

    adhere to pylint
    verified by the command `python -m pylint --disable=all -e W0311 -e C0303 --jobs=0 --indent-string='  ' --recursive=y test_attention.py`

--- test/unit/test_attention.py
+++ test/unit/test_attention.py
@@ -9,7 +9,6 @@ class TestAttention(unittest.TestCase):
     attn = q.scaled_dot_product_attention(k, v)
     sched = attn.schedule()
     return attn, sched
-    
   def test_half_qkv_buffers(self):
     _, sched = self._get_attn_and_scheduler()
     # attention has 5 kernels now
@@ -17,15 +16,12 @@ class TestAttention(unittest.TestCase):
     softmax_inputs = sched[1:4]
     for si in softmax_inputs:
       assert all(b.dtype == dtypes.half for b in si.bufs), f"non half {si.bufs=}"
-
   def test_uint_qkv_buffers(self):
     _, sched = self._get_attn_and_scheduler(dtype=dtypes.uint)
     # attention has 6 kernels now
     self.assertEqual(len(sched), 6)
-
     qkv_uint_scheds = [si for si in sched if all(b.dtype == dtypes.uint for b in si.bufs)]
     assert len(qkv_uint_scheds) >= 1, f"no uint found in scheduler buffers {sched[0].bufs=} "
-
     found_cast_uint_to_float = any(
         any(b.dtype == dtypes.uint for b in si.bufs) and any(b.dtype == dtypes.float for b in si.bufs)
         for si in sched
