67126c8cc5f1243d3b73b0ae4fe9ec57c65f0ecd
kevvz <92408564+kevvz@users.noreply.github.com>
2025-08-05 11:24:34 -0700

    better tests

--- extra/torch_muon.py
+++ extra/torch_muon.py
@@ -2,7 +2,8 @@ import torch
 
 #credit to KellerJordan at https://github.com/KellerJordan/Muon/tree/master
 #some changes: classic momentum instead of weighting gradient
-def zeropower_via_newtonschulz5(G:torch.tensor, steps: int):
+#added ns_steps, ns_params, nesterov as hyperparams
+def zeropower_via_newtonschulz5(G:torch.tensor, steps:int, params:tuple[int, ...]):
   """
   Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
   quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
@@ -14,8 +15,7 @@ def zeropower_via_newtonschulz5(G:torch.tensor, steps: int):
   """
   assert G.ndim >= 2 # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
 
-  a, b, c = (3.4445, -4.7750,  2.0315)
-  # X = G.bfloat16()
+  a, b, c = params
   X = G
   if G.size(-2) > G.size(-1):
     X = X.mT
@@ -33,23 +33,22 @@ def zeropower_via_newtonschulz5(G:torch.tensor, steps: int):
 
   return X
 
-def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True):
+def muon_update(grad, momentum, beta=0.95, ns_steps=5, ns_params=(3.4445, -4.7750,  2.0315), nesterov=True):
   if beta:
     momentum.mul_(beta).add_(grad)
     update = grad.add(momentum,alpha=beta) if nesterov else momentum
   else: update = grad
   if update.ndim == 4: # for the case of conv filters
     update = update.view(len(update), -1)
-  update = zeropower_via_newtonschulz5(update, steps=ns_steps)
-  # update *= max(1, grad.size(-2) / grad.size(-1))**0.5
+  update = zeropower_via_newtonschulz5(update, steps=ns_steps, params=ns_params)
   return update
 
 class SingleDeviceMuon(torch.optim.Optimizer):
   """
   Muon variant for usage in non-distributed settings.
   """
-  def __init__(self, params, lr=0.02, weight_decay=0.0,pre_wd=False, momentum=0.95, nesterov=True,fused=False):
-    defaults = dict(lr=lr, weight_decay=weight_decay, pre_wd=pre_wd, momentum=momentum, nesterov=nesterov)
+  def __init__(self, params, lr=0.02, weight_decay=0.0, momentum=0.95, ns_steps=5, ns_params=(3.4445, -4.7750,  2.0315), nesterov=True):
+    defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, ns_steps=ns_steps, ns_params=ns_params, nesterov=nesterov)
     super().__init__(params, defaults)
 
   @torch.no_grad()
@@ -67,7 +66,8 @@ class SingleDeviceMuon(torch.optim.Optimizer):
         state = self.state[p]
         if len(state) == 0:
           state["momentum_buffer"] = torch.zeros_like(p)
-        update = muon_update(p.grad, state["momentum_buffer"], beta=group["momentum"], nesterov=group["nesterov"])
+        update = muon_update(p.grad, state["momentum_buffer"], beta=group["momentum"], ns_steps=group["ns_steps"],
+                             ns_params=group["ns_params"], nesterov=group["nesterov"])
         p.mul_(1.0 - group["lr"] * group["weight_decay"])
 
         p.add_(update.reshape(p.shape), alpha=-group["lr"])
