c00f00f541b03946f6657d40276d25b9fbad4971
zibokapi <zibokapi@gmail.com>
2025-08-05 18:49:23 +0800

    Revert "test segfault"
    This reverts commit 4e1aaf41e767e1b24a7c2561c9049428836eb3da.

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -791,7 +791,6 @@ class TestInferenceMode(unittest.TestCase):
       assert W.grad is None
     f(x, m, W)
 
-@unittest.skip("a")
 class TestTensorMetadata(unittest.TestCase):
   def setUp(self) -> None: _METADATA.set(None)
 
