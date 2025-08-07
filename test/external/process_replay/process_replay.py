a1ca72e3f5c661fcf15ff77288eca0b3786c95c3
qazal <qazal.software@gmail.com>
2025-08-08 01:31:57 +0300

    can use opts_to_apply

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -49,8 +49,9 @@ def replay_kernelize(ret:dict[UOp, UOp], big_sink:UOp) -> tuple[str, str, tuple[
   return to_str(new_sink), to_str(ret[big_sink]), (big_sink,)
 
 def replay_get_program(p:ProgramSpec, ast:UOp, renderer:Renderer|None=None, opts:list[Opt]|None=None) -> tuple[str, str, tuple[Any, ...]]:
-  input_ast = ast.replace(arg=KernelInfo(opts_to_apply=p.applied_opts, name=p.name)) if ast.arg is None and opts is None else ast
-  p2 = get_program(input_ast, renderer=renderer, opts=opts)
+  # NOTE: this always uses the opts_to_apply path
+  input_ast = ast.replace(arg=KernelInfo(opts_to_apply=p.applied_opts, name=p.name)) if ast.arg is None or opts is not None else ast
+  p2 = get_program(input_ast, renderer=renderer)
   def to_str(ret:ProgramSpec) -> str:
     # PYTHON renderer pickles UOps, first unpickle and decode here
     if p.device.startswith("PYTHON"): return "\n".join([str(x) for x in pickle.loads(base64.b64decode(ret.src))])
