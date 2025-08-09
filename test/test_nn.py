f2f9883bc8f8896c6f66ecf45a6a87c744ef9681
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-08-09 17:19:26 +0200

    add regression test

--- test/test_nn.py
+++ test/test_nn.py
@@ -440,6 +440,13 @@ class TestNN(unittest.TestCase):
       result = layer(a)
       self.assertEqual(result.shape, shp + (embed_size,))
 
+  def test_embedding_regression(self):
+    # used to fail bounds check
+    with Context(FUSE_ARANGE=1):
+      embedding = Embedding(30522, 1024)
+      input_ids = Tensor.empty(96, 512)
+      embedding(input_ids).realize()
+
   def test_load_state_dict(self):
     layer = Conv2d(3, 5, kernel_size=3)
 
