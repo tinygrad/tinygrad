00391db628f365e9c02fec3b8d48688e839d6a30
George Hotz <72895+geohot@users.noreply.github.com>
2025-08-19 20:18:45 -0700

    no ast for mem estimate (#11744)
    * no ast for mem estimate
    
    * skip for webgpu

--- test/test_uops_stats.py
+++ test/test_uops_stats.py
@@ -20,6 +20,7 @@ def get_stats(x:Tensor):
   ei = lower_schedule_item(si)
   return ei.prg.estimates.ops, ei.prg.estimates.mem
 
+@unittest.skipIf(Device.DEFAULT == "WEBGPU", "webgpu does extra load/store for packed types")
 class TestMemoryCount(unittest.TestCase):
   def test_add(self):
     a = Tensor.empty(1024, 1024, dtype=dtypes.uint8)
