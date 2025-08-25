e01cc1ebae54c186c043a916e41cfbffe06285e2
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-08-24 19:12:59 -0700

    adjust commment

--- tinygrad/uop/symbolic.py
+++ tinygrad/uop/symbolic.py
@@ -33,10 +33,11 @@ symbolic_simple = PatternMatcher([
   (UPat.var("x") / UPat.var("x"), lambda x: x.const_like(1)), # x/x -> 1
   ((UPat.var("x") * UPat.var("x2")) / UPat.var("x2"), lambda x,x2: x), # (x*x2)/x2 -> x
   ((UPat.var() % UPat.var("y")).named("base") % UPat.var("y"), lambda base,y: base),  # (x%y)%y = -> x%y (rewritten with base for speed)
-  (UPat.var("x")%UPat.cvar("d")+(UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c"), lambda x,d,c: x+(c-d)*(x//d)),  # (x%c)+(x//c)*c = x
+  # 4 variations of (x%c)+(x//c)*c = x
+  (UPat.var("x")%UPat.cvar("d")+(UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c"), lambda x,d,c: x+(c-d)*(x//d)),
   ((UPat.var("y")+UPat.var("x")%UPat.cvar("d")) + (UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c"),
     lambda y,x,d,c: (y+x)+(c-d)*(x//d)),
-  (UPat.var("x")%UPat.cvar("d")*UPat.cvar("c1") + (UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c2"), lambda x,d,c1,c2: x*c1+(c2-c1*d)*(x//d)),  # (x%c)+(x//c)*c = x
+  (UPat.var("x")%UPat.cvar("d")*UPat.cvar("c1") + (UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c2"), lambda x,d,c1,c2: x*c1+(c2-c1*d)*(x//d)),
   ((UPat.var("y")+UPat.var("x")%UPat.cvar("d")*UPat.cvar("c1"))+(UPat.var("x")//UPat.cvar("d"))*UPat.cvar("c2"),
     lambda y,x,d,c1,c2: y+x*c1+(c2-c1*d)*(x//d)),
   (UPat.var("x", dtype=dtypes.bool) & UPat.cvar("c", vec=False), lambda x,c: x if c.arg else c),
