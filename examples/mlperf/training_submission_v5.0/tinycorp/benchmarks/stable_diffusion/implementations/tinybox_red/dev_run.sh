5bb18919c29e4b19c0774af1cd0d0bd8bf0b9282
hooved <172129504+hooved@users.noreply.github.com>
2025-08-25 13:30:31 +0000

    eval BEAM config on mi300x and red

--- examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_red/dev_run.sh
+++ examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_red/dev_run.sh
@@ -18,9 +18,9 @@
 #pip install --index-url https://download.pytorch.org/whl/cpu torch # for torch.utils.data.DataLoader, which webdataset depends on
 #pip install webdataset
 source venv/bin/activate
-#export DEBUG=2
-#export BEAM=5 BEAM_UOPS_MAX=8000 BEAM_UPCAST_MAX=256 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=5
-#export IGNORE_JIT_FIRST_BEAM=1
+#export DEBUG=6
+export BEAM=5 BEAM_UOPS_MAX=8000 BEAM_UPCAST_MAX=256 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=5
+export IGNORE_JIT_FIRST_BEAM=1
 
 #export SEED=$RANDOM
 DATETIME=$(date "+%m%d%H%M")
@@ -30,30 +30,52 @@ export HCQDEV_WAIT_TIMEOUT_MS=300000
 
 export PYTHONPATH="."
 export MODEL="stable_diffusion"
-#export GPUS=8 BS=248
-export GPUS=6
 
-#export EVAL_BS=192
+#export RESUME_CKPTDIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/08221929"
+#export RESUME_ITR=8260
+#export BACKUP_INTERVAL=2065
+export BACKUP_INTERVAL=413
+
+# mi300x
 # use separate BS for the various jits in eval to maximize throughput
-export CONTEXT_BS=6
-export DENOISE_BS=6
-export DECODE_BS=6
-export INCEPTION_BS=6
-export CLIP_BS=6
+export GPUS=8 BS=248
+export CONTEXT_BS=816
+export DENOISE_BS=600
+export DECODE_BS=384
+export INCEPTION_BS=560
+export CLIP_BS=240
+
+# tinybox red
+#export GPUS=6 BS=12
+#export CONTEXT_BS=600
+#export DENOISE_BS=96
+#export DECODE_BS=90
+#export INCEPTION_BS=480
+#export CLIP_BS=84
+####export DENOISE_BS=144
+####export DECODE_BS=138
 
 export RUN_EVAL=1
-export EVAL_OVERFIT_SET=1
-#export EVAL_INTERVAL=4000
+#export EVAL_OVERFIT_SET=1
+#export EVAL_INTERVAL=2065
 export EVAL_ONLY=1
-export EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/08142156/run_eval_88000"
+export EVAL_CKPT_DIR="/home/hooved/stable_diffusion/checkpoints/training_checkpoints/08241246/run_eval"
+#export LIMIT_EVAL_SAMPLES=600
 
 export BASEDIR="/home/hooved/stable_diffusion"
+
+# mi300x
 export DATADIR="/raid/datasets/stable_diffusion"
 export CKPTDIR="/raid/weights/stable_diffusion"
+
+# tinybox red
+#export DATADIR="/home/hooved/stable_diffusion/datasets"
+#export CKPTDIR="/home/hooved/stable_diffusion/checkpoints"
 export UNET_CKPTDIR="${BASEDIR}/checkpoints/training_checkpoints/${DATETIME}"
 mkdir -p $UNET_CKPTDIR
 
 export WANDB=1
-#export PARALLEL=16
+#export PARALLEL=0
+export PARALLEL=16
 
 RUNMLPERF=1 python3 examples/mlperf/model_train.py
