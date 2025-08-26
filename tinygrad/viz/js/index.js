5c50916677d9f4ca35de18631cfdb113b3164280
qazal <qazal.software@gmail.com>
2025-08-26 08:17:34 +0300

    safari doesn't have context-stroke

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -60,7 +60,7 @@ async function renderDag(graph, additions, recenter=false) {
       .attr("transform", d => `translate(${d.x},${d.y})`).classed("clickable", d => d.ref != null).on("click", (_,d) => {
         if (d.ref != null) return setCtxWithHistory(d.ref);
         const src = g.predecessors(d.id) || [];
-        nodes.classed("highlight", n => (src || []).includes(n.id));
+        nodes.classed("highlight", n => src.includes(n.id));
         d3.select("#edges").selectAll("path.edgePath").classed("highlight", e => src.includes(e.v) && e.w===d.id);
       });
     nodes.selectAll("rect").data(d => [d]).join("rect").attr("width", d => d.width).attr("height", d => d.height).attr("fill", d => d.color)
