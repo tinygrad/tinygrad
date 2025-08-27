323f8a9ea783ace81d4d76290ab57d74c08b3bb6
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-08-27 12:12:28 +0000

    hcq: finalize without synchronization when in error state

--- tinygrad/runtime/support/hcq.py
+++ tinygrad/runtime/support/hcq.py
@@ -383,15 +383,20 @@ class HCQCompiled(Compiled, Generic[SignalType]):
     self.kernargs_buf:HCQBuffer = self.allocator.alloc(kernargs_size, BufferSpec(cpu_access=True))
     self.kernargs_offset_allocator:BumpAllocator = BumpAllocator(self.kernargs_buf.size, wrap=True)
 
+    self.is_in_error_state = False # True if error is unrecoverable and sync will always fail
+
     if self._is_cpu(): HCQCompiled.cpu_devices.append(self)
 
   def synchronize(self):
+    if self.is_in_error_state and sys.is_finalizing(): return
+
     # If we have any work on CPU devices, need to synchronize them. This is just an optimization to release GIL allowing to finish faster.
     if not self._is_cpu():
       for dev in HCQCompiled.cpu_devices: dev.synchronize()
 
     try: self.timeline_signal.wait(self.timeline_value - 1)
     except RuntimeError as e:
+      self.is_in_error_state = True
       if hasattr(self, 'on_device_hang'): self.on_device_hang()
       else: raise e
 
