f2cf4ab8b2a84cbbb144e6bd86b321baa644a556
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-08-31 15:55:09 -0700

    shorten line and make mypy happy

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -301,10 +301,7 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   def range(end:sint, *arg):
     if len(arg) == 0: raise RuntimeError("range needs an arg")
     if len(arg) == 1: arg = arg+(AxisType.LOOP,)
-    end = sint_to_uop(end, dtype=dtypes.index)
-    if end.dtype is not dtypes.index:
-      end = end.cast(dtypes.index)
-    return UOp(Ops.RANGE, dtype=dtypes.index, src=(end,), arg=arg)
+    return UOp(Ops.RANGE, dtype=dtypes.index, src=(sint_to_uop(end, dtype=dtypes.index).cast(dtypes.index),), arg=arg)
   def r(self, op:Ops, axis:tuple[int, ...]):
     axis = tuple(sorted([x for x in axis if resolve(self.shape[x] != 1)]))
     if len(axis) == 0: return self
