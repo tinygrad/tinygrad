a3a3c9a105fd68845dc1294360b8586f603af380
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-09-03 02:12:20 -0700

    remove import

--- tinygrad/codegen/lowerer.py
+++ tinygrad/codegen/lowerer.py
@@ -1,7 +1,6 @@
 # the job of the lowerer is to do indexing
 from dataclasses import dataclass
 from tinygrad.dtype import dtypes, least_upper_dtype
-from tinygrad.device import is_dtype_supported
 from tinygrad.uop.ops import KernelInfo, UOp, Ops, PatternMatcher, UPat, sint_to_uop, AxisType, graph_rewrite, resolve, GroupOp
 
 # ***** indexing *****
