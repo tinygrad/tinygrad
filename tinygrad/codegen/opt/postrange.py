d4f70e580cc6f09a5f4b15c6be4d040f880ae1d2
George Hotz <geohot@gmail.com>
2025-09-03 18:52:41 -0700

    fix rangeify

--- tinygrad/codegen/opt/postrange.py
+++ tinygrad/codegen/opt/postrange.py
@@ -322,7 +322,9 @@ def apply_opts(ctx:Renderer, ast:UOp):
   elif not NOOPT:
     k.simplify_merge_adjacent()
     from tinygrad.codegen.opt.heuristic import hand_coded_optimizations
-    for opt in hand_coded_optimizations(k): k.apply_opt(opt)
+    # NOTE: hand_coded_optimizations doesn't support multiblock opts yet
+    if all(len(u.src) == 1 for u in ast.parents if u.op is Ops.LOAD):
+      for opt in hand_coded_optimizations(k): k.apply_opt(opt)
   return k.get_optimized_ast(name_override=ast.arg.name if ast.arg is not None and ast.arg.name != "test" else None)
 
 pm_postrange_opt = PatternMatcher([
