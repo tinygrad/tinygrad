d627f34a62787fe691d6826480b3d6e729dccf06
Shun Usami <usatie@gmail.com>
2025-09-04 13:43:42 -0700

    [wip] Just update setitem.py

--- setitem.py
+++ setitem.py
@@ -1,7 +1,7 @@
 from tinygrad import Tensor
 import functools
 
-def gen_mask(self: Tensor, indices):
+def gen_mask(self, indices):
     masks = []
     for dim,(dim_size, idx) in enumerate(zip(self.shape, indices)):
         if isinstance(idx, int):
@@ -11,6 +11,7 @@ def gen_mask(self: Tensor, indices):
         elif isinstance(idx, slice):
             # e.g. dim=3, idx=slice(1,12,3) = [1,4,7,10], shape=(10,20,30,40,50)
             #              () -> (4,) -> (4,20) -> (20,) -> (1,20,1,1,1) -> (10,20,30,40,50)
+            # TODO: Negative start/stop/step, but mask should be correct
             # Negative start/stop is handled by slice.indices, but negative step could be tricky
             start, stop, step = idx.indices(dim_size)
             mask = Tensor.arange(start, stop, step).one_hot(dim_size).sum(0).reshape((1,)*dim + (dim_size,) + (1,)*(self.ndim-dim-1)).expand(self.shape)
@@ -27,48 +28,52 @@ x : (10,20,30,40,50)
 v : (10, 1, 30, 4, 50) or broadcastable to this shape
 x[:, 3, ..., 1:12:3] = v
 """
-def gen_index_shape(shape: tuple[int,...], indices):
+def gen_index_shape(self, indices):
     masks = []
-    ndim = len(shape)
-    indices = list(indices) + [None]*(ndim - len(indices))
+    indices = list(indices) + [None]*(self.ndim - len(indices))
     res_shape = []
-    for (dim_size, idx) in zip(shape, indices):
+    for (dim_size, idx) in zip(self.shape, indices):
         if isinstance(idx, int):
             res_shape.append(1)
         elif isinstance(idx, slice):
-            start = idx.start or 0
-            stop = idx.stop or dim_size
-            step = idx.step or 1
-            res_shape.append((stop - start + (step or 1) - 1) // (step or 1))
+            start, stop, step = idx.indices(dim_size)
+            size = (abs(stop - start) + abs(step) - 1) // abs(step)
+            print(f"{start=}, {stop=}, {step=}, {size=}")
+            res_shape.append(size)
         elif idx is None:
             res_shape.append(dim_size)
         else:
             raise NotImplementedError(f"Indexing with {type(idx)} not supported")
     return tuple(res_shape)
 
-def pad_values(v: Tensor, shape: tuple[int,...], indices):
-    vshape = gen_index_shape(shape, indices)
+def pad_values(self, v: Tensor, indices):
+    vshape = gen_index_shape(self, indices)
     print(f"{vshape=}")
-    ndim = len(shape)
     # e.g.
     # v.shape = (1, 1, 4, 1) -> (10, 1, 30, 4, 50)
     vb = v._broadcast_to(vshape)
     padding = []
-    for dim,(dim_size, idx) in enumerate(zip(shape, indices)):
+    for dim,(dim_size, idx) in enumerate(zip(self.shape, indices)):
         print(f"dim={dim}, idx={idx}, dim_size={dim_size}")
         if isinstance(idx, int):
             pass
         elif isinstance(idx, slice):
-            start = idx.start or 0
-            stop = idx.stop or dim_size
-            step = idx.step or 1
-            if step > 1:
+            start, stop, step = idx.indices(dim_size)
+            if step < 0:
+                vb = vb.flip(dim)
+            if abs(step) > 1:
                 # (10,1,30,4,50) -> (10,1,30,12,50) -> (10,1,30,(1+12+17),50)
-                vb = vb.repeat_interleave(step, dim=dim)
+                vb = vb.repeat_interleave(abs(step), dim=dim)
                 # e.g. dim=3, idx=slice(1,12,3) = [1,4,7,10], shape=(10,20,30,40,50)
             # pads = (None, None, (1, 17), 0, None)
-            pad = (start, dim_size - (start + vshape[dim]*step ))
-            pads = (None,) * dim + (pad, ) + (None,) * (ndim - dim - 1)
+            if step > 0:
+                pad = (start, dim_size - (start + vshape[dim]*step ))
+            else:
+                print(f"{dim_size=}, {stop=}, {vshape[dim]=}, {step=}")
+                right_pad = dim_size - start - 1
+                left_pad = dim_size - vb.shape[dim] - right_pad
+                pad = (left_pad, right_pad)
+            pads = (None,) * dim + (pad, ) + (None,) * (self.ndim - dim - 1)
             print(f"{pads=}")
             vb = vb.pad(pads)
         elif idx is None:
@@ -77,13 +82,13 @@ def pad_values(v: Tensor, shape: tuple[int,...], indices):
             raise NotImplementedError(f"Indexing with {type(idx)} not supported")
     return vb
 
-def setitem(x: Tensor, indices, v: Tensor):
-    shape = x.shape
-    mask = gen_mask(x, indices)
-    vb = pad_values(v, shape, indices)
+def setitem(self: Tensor, indices, v: Tensor):
+    print(f"setitem: {self.shape=}, {indices=}, {v.shape=}")
+    mask = gen_mask(self, indices)
     print(f"{mask.numpy()=}")
+    vb = pad_values(self, v, indices)
     print(f"{vb.numpy()=}")
-    return mask.where(vb, x)
+    return mask.where(vb, self)
 
 def test_gen_mask():
     # single element
@@ -114,10 +119,40 @@ def test_gen_mask():
     assert (mask == Tensor([[[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[1,0,1,0],[1,0,1,0]]])).sum().item() == 24
     
 def test_setitem():
-    x = Tensor.zeros(2, 3, 4)
-    v = Tensor.arange(1, 5).reshape((2,2))
+    def initial():
+        x = Tensor.zeros(2,3,4)
+        v = Tensor.arange(1,5).reshape((2,2))
+        return x, v
+    print("========= setitem (Slice Indexing) =========")
+    x, v = initial()
     print(f"{x.numpy()=}")
     print(f"{v.numpy()=}")
+    x = setitem(x, (None, slice(1,3), slice(1,3)), v)
+    print(f"{x.numpy()=}")
+
+    print("========= setitem (Slice Indexing - Strided) =========")
+    x, v = initial()
     x = setitem(x, (None, slice(1,3), slice(1,4,2)), v)
+    print(f"{x.numpy()=}")
+
+    print("========= setitem (Slice Indexing - Negative Step) =========")
+    x, v = initial()
+    x = setitem(x, (None, slice(1,3), slice(3,1,-1)), v)
+    print(f"{x.numpy()=}")
+
+    print("========= setitem (Slice Indexing - Negative Step 2) =========")
+    x, v = initial()
+    x = setitem(x, (None, slice(1,None,-1), slice(3,1,-1)), v)
+    print(f"{x.numpy()=}")
+
+    print("========= setitem (Slice Indexing - Negative Strided) =========")
+    x, v = initial()
+    x = setitem(x, (None, slice(1,3), slice(3,0,-2)), v)
+    print(f"{x.numpy()=}")
+
+    print("========= setitem (Slice Indexing - Negative Stop) =========")
+    x = Tensor([[3.0], [2.0], [1.0]]).contiguous()
+    #x[:-1] = t[1:]
+    x = setitem(x, (slice(None, -1), ), x[1:])
     print(f"{x.numpy()=}")
 
