e5cb72b1f20707168587ce9caadbf4c50bdeb0f9
b1tg <b1tg@users.noreply.github.com>
2025-09-04 12:30:57 +0800

    inf + 0.0

--- test/test_dtype_alu.py
+++ test/test_dtype_alu.py
@@ -66,7 +66,7 @@ def universal_test(a, b, dtype, op):
   tensor_value = (op[0](ta, tb)).numpy()
   numpy_value = op[1](ta.numpy(), tb.numpy())
   if dtype in dtypes.fp8s: # TODO: this can be removed?
-    numpy_value = fp8_to_float(float_to_fp8(numpy_value, dtype), dtype)
+    numpy_value = fp8_to_float(float_to_fp8(numpy_value, dtype), dtype) if math.isfinite(numpy_value) else numpy_value
   if dtype in dtypes.floats:
     atol, rtol = {dtypes.bfloat16:(1e-3, 1e-2), dtypes.fp8e4m3:(1e-1, 1e-1), dtypes.fp8e5m2: (1.0, 5e-1)}.get(dtype, (1e-10, 1e-7))
     np.testing.assert_allclose(tensor_value, numpy_value, atol=atol, rtol=rtol)
