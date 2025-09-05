64093b35705f59ee8faa0ce57a36b2e280f68535
qazal <qazal.software@gmail.com>
2025-09-05 17:24:31 +0300

    int

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -380,8 +380,8 @@ async function renderProfiler() {
 
   function resize() {
     const profiler = document.querySelector(".profiler");
-    const { height, width:sideWidth } = rect("#device-list");
-    const width = profiler.clientWidth-(sideWidth+padding);
+    let { height, width:sideWidth } = rect("#device-list");
+    width = profiler.clientWidth-(sideWidth+padding), height = Math.round(height);
     if (canvas.width === width*dpr && canvas.height === height*dpr) return;
     canvas.width = width*dpr;
     canvas.height = height*dpr;
