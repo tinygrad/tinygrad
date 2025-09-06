5d75d0f0f5c364afa63b0416f5ae594085fef6e6
ttomsa <tomasvsilva8@gmail.com>
2025-09-06 20:04:23 +0100

    Revert "let's try this"
    This reverts commit 9a2b21e07a62f00f7089163fc8547695ed4d6d4d.

--- tinygrad/runtime/ops_x86.py
+++ tinygrad/runtime/ops_x86.py
@@ -3,11 +3,9 @@ from tinygrad.helpers import capstone_flatdump
 from tinygrad.renderer.isa import X86Renderer
 from tinygrad.runtime.support.hcq import HCQCompiled
 from tinygrad.runtime.ops_cpu import CPUWorker, CPUAllocator, CPUProgram, Compiler, CPUSignal, CPUComputeQueue
-from tinygrad.muop import MUOp
 
 class X86Compiler(Compiler):
   def __init__(self): super().__init__(None)
-  def compile(self, src:list[MUOp]): return MUOp.assemble(src)
   def disassemble(self, lib:bytes): return capstone_flatdump(lib)
 
 class X86Device(HCQCompiled):
