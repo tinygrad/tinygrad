478e7587557b2c65ed288255c00664e0efc6742e
George Hotz <geohot@gmail.com>
2025-09-05 18:21:55 -0700

    Revert "fix scheduler copy (#12048)"
    This reverts commit 51b7c407887d9e7ded6f1d792e5745a1a59b8b9c.

--- tinygrad/codegen/opt/postrange.py
+++ tinygrad/codegen/opt/postrange.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import math, itertools
 from collections import defaultdict
-from typing import cast, Final
+from typing import cast, Final, Sequence
 from tinygrad.uop.ops import PatternMatcher, UPat, Ops, UOp, KernelInfo, graph_rewrite, AxisType, ssimplify, can_pad
 from tinygrad.device import Buffer
 from tinygrad.dtype import AddrSpace, dtypes, ImageDType
@@ -45,10 +45,8 @@ class Scheduler:
   def shape_str_to_axis(self, nms:list[str]) -> tuple[int, ...]: return tuple([self.shape_str().index(x) for x in nms])
 
   def copy(self):
-    ret = Scheduler(self.ast, self.opts)
-    ret.dont_use_locals = self.dont_use_locals
-    ret.applied_opts = self.applied_opts[:]
-    return ret
+    # TODO: this is spamming the many ns on the names
+    return Scheduler(self.get_optimized_ast(), self.opts)
 
   kernel_cnt: Final[defaultdict[str, int]] = defaultdict(int)
   def get_optimized_ast(self, name_override:str|None=None):
@@ -107,6 +105,10 @@ class Scheduler:
       check(axis < self.shape_len, f"invalid axis on {axis=} {op=} {self.shape_len=}")
       return axis
     except IndexError as e: raise KernelOptError from e
+
+  def apply_opts(self, opts:Sequence[Opt]) -> Scheduler:
+    for opt in opts: self.apply_opt(opt)
+    return self
 
   def apply_opt(self, opt:Opt, append_opt:bool=True):
     if opt.op is OptOps.NOLOCALS:
@@ -312,10 +314,10 @@ def apply_opts(ctx:Renderer, ast:UOp):
   elif ast.arg is not None and ast.arg.opts_to_apply is not None:
     for opt in ast.arg.opts_to_apply: k.apply_opt(opt)
   elif not NOOPT and (ast.arg is None or ast.arg.applied_opts == ()):
+    from tinygrad.codegen.opt.heuristic import hand_coded_optimizations
+    # NOTE: hand_coded_optimizations doesn't support multiblock opts yet
     if all(len(u.src) == 1 for u in ast.parents if u.op is Ops.LOAD):
-      # NOTE: hand_coded_optimizations doesn't support multiblock opts yet
-      from tinygrad.codegen.opt.heuristic import hand_coded_optimizations
-      k = hand_coded_optimizations(k)
+      for opt in hand_coded_optimizations(k): k.apply_opt(opt)
   return k.get_optimized_ast(name_override=ast.arg.name if ast.arg is not None and ast.arg.name != "test" else None)
 
 pm_postrange_opt = PatternMatcher([
