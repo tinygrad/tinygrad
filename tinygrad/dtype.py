a4b82550af290f65011989ec007626ec3e2a5f50
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-09-07 21:30:21 -0700

    indentation

--- tinygrad/dtype.py
+++ tinygrad/dtype.py
@@ -6,9 +6,9 @@ from tinygrad.helpers import getenv, prod
 from enum import Enum, auto
 
 class InvalidType:
-    def __eq__(self, other): return self is other
-    def __hash__(self): return id(self)
-    def __repr__(self): return "Invalid"
+  def __eq__(self, other): return self is other
+  def __hash__(self): return id(self)
+  def __repr__(self): return "Invalid"
 Invalid = InvalidType()
 
 ConstType = float|int|bool
