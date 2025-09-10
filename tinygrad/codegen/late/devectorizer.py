208d376b9e80822caebb074d7ce6949d6a7b5168
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-09-09 23:15:21 -0700

    cleanup

--- tinygrad/codegen/late/devectorizer.py
+++ tinygrad/codegen/late/devectorizer.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 from tinygrad.dtype import dtypes, ImageDType, DType, AddrSpace, Invalid
 from tinygrad.uop.ops import UOp, Ops, UPat, PatternMatcher, graph_rewrite, GroupOp, identity_element
-from tinygrad.uop.symbolic import uop_given_valid, parse_valid, sym, symbolic_flat, cast_folding, invalid_gate, invalid_pat
+from tinygrad.uop.symbolic import uop_given_valid, parse_valid, sym, symbolic_flat, cast_folding, invalid_gate
 from tinygrad.helpers import getenv, flatten, AMX, prod, partition
 from tinygrad.renderer import Renderer
 
