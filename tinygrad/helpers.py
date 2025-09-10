8d5ebeea8be497add5440189eb7a0afeb4ec40ef
ttomsa <tomasvsilva8@gmail.com>
2025-09-10 20:13:11 +0100

    rename to CPU_X86

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -146,7 +146,7 @@ RANGEIFY, FUSE_ATTENTION = ContextVar("RANGEIFY", 0), ContextVar("FUSE_ATTENTION
 EMULATE = ContextVar("EMULATE", "")
 CPU_COUNT = ContextVar("CPU_COUNT", max(1, (os.cpu_count() or 1) // (4 if ARCH_X86 else 2))) # take 1/2 of the cores, accounting HT
 CPU_LLVM, AMD_LLVM = ContextVar("CPU_LLVM", 0), ContextVar("AMD_LLVM", 1)
-X86 = ContextVar("X86", 0)
+X86 = ContextVar("CPU_X86", 0)
 
 @dataclass(frozen=True)
 class Metadata:
