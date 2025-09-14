d1cac4537582124e4a5e4641eaf0cd6568be41d5
hooved <172129504+hooved@users.noreply.github.com>
2025-09-14 17:00:20 +0000

    cleanup/validate dataloader

--- examples/mlperf/dataloader.py
+++ examples/mlperf/dataloader.py
@@ -513,7 +513,7 @@ def batch_load_retinanet(dataset, val:bool, base_dir:Path, batch_size:int=32, sh
 
 # stable diffusion callbacks to match mlperf ref; declared here because they're pickled
 def filter_dataset(sample:dict): return {k:v for k,v in sample.items() if k in {'npy', 'txt'}}
-def collate(batch:list[dict], device:str|None=None):
+def collate(batch:list[dict]):
   # mlperf ref uses torch.utils.data.default_collate
   ret = {"npy": [], "txt": [], "__key__": []}
   for sample in batch:
@@ -524,18 +524,20 @@ def collate_fn(batch): return batch
 
 # Reference (code): https://github.com/mlcommons/training/blob/2f4a93fb4888180755a8ef55f4b977ef8f60a89e/stable_diffusion/ldm/data/webdatasets.py, Line 55
 # Reference (params): https://github.com/mlcommons/training/blob/ab4ae1ca718d7fe62c369710a316dff18768d04b/stable_diffusion/configs/train_01x08x08.yaml, Line 107
-def batch_load_train_stable_diffusion(BS:int, device:str|None=None):
+def batch_load_train_stable_diffusion(urls:str, BS:int):
   # webdataset depends on torch.utils.data.DataLoader
   import webdataset
-  dataset = webdataset.WebDataset(urls=f'{os.getenv("DATADIR")}/laion-400m/webdataset-moments-filtered/{{00000..00831}}.tar', resampled=True,
-                                  cache_size=-1, cache_dir=None)
+  dataset = webdataset.WebDataset(urls=urls, resampled=True, cache_size=-1, cache_dir=None)
   dataset = dataset.shuffle(size=1000)
   dataset = dataset.decode()
   dataset = dataset.map(filter_dataset)
-  dataset = dataset.batched(BS, partial=False, collation_fn=functools.partial(collate, device=device))
+  dataset = dataset.batched(BS, partial=False, collation_fn=collate)
   dataset = webdataset.WebLoader(dataset, batch_size=None, shuffle=False, num_workers=1, persistent_workers=True, collate_fn=collate_fn)
 
   for x in dataset:
+    assert isinstance(x, dict) and all(isinstance(k, str) for k in x.keys()) and all(isinstance(v, list) for v in x.values())
+    assert all(isinstance(moment_mean_logvar, np.ndarray) and moment_mean_logvar.shape==(1,8,64,64) for moment_mean_logvar in x["npy"])
+    assert all(isinstance(caption, str) for caption in x["txt"])
     yield x
 
 # llama3
