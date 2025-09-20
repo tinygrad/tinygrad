19493ba4c1ab535c2ec96c375a222d9f76bc4b83
nimlgen <lenyaprokh@gmail.com>
2025-09-20 18:01:32 +0300

    xx

--- test/test_jit.py
+++ test/test_jit.py
@@ -8,7 +8,7 @@ from tinygrad.tensor import Tensor
 from tinygrad.engine.jit import TinyJit, GraphRunner, MultiGraphRunner, graph_class
 from tinygrad.engine.realize import CompiledRunner, BufferCopy, BufferXfer
 from tinygrad.device import Device
-from tinygrad.helpers import Context, JIT, RANGEIFY, GlobalCounters, getenv
+from tinygrad.helpers import Context, JIT, GlobalCounters, getenv
 from tinygrad.dtype import dtypes
 from extra.models.unet import ResBlock
 
