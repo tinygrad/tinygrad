63be3cd8e4aea57bd1f3f57a83ebbd97f7947749
Woze Parrot <wozeparrot@gmail.com>
2025-09-23 17:21:58 -0700

    feat: fetch mapping in its own process

--- extra/tinyfs/fetch_raid.py
+++ extra/tinyfs/fetch_raid.py
@@ -21,12 +21,19 @@ def fetch_file(item):
 
   pt.uop.realized.deallocate()
 
-if __name__ == "__main__":
-  with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-    mapping_tensor = Tensor(bytes.fromhex("d734f5e3be9f1e9d863bfaa4fc6c1ef2")).load(175866113).realize()
-    mapping = mapping_tensor.data().tobytes().decode()
-    mapping = json.loads(mapping)
-    mapped_files = mapping.items()
+def fetch_mapping():
+  mapping_tensor = Tensor(bytes.fromhex("d734f5e3be9f1e9d863bfaa4fc6c1ef2")).load(175866113).realize()
+  mapping = mapping_tensor.data().tobytes().decode()
+  mapping = json.loads(mapping)
+  mapped_files = mapping.items()
+  return list(mapped_files)
 
+if __name__ == "__main__":
+  with multiprocessing.Pool(processes=1) as pool:
+    mapped_files = pool.apply(fetch_mapping)
+
+  print(f"fetched mapping for {len(mapped_files)} files")
+
+  with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
     for _ in tqdm(pool.imap_unordered(fetch_file, mapped_files), total=len(mapped_files)):
       pass
