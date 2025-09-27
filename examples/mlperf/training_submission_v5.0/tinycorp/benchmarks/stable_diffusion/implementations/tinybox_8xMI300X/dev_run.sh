695efee87139a49c268b27d6b3baa9e0aa846c3e
hooved <172129504+hooved@users.noreply.github.com>
2025-09-27 11:01:01 +0000

    fix venv name

--- examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_8xMI300X/dev_run.sh
+++ examples/mlperf/training_submission_v5.0/tinycorp/benchmarks/stable_diffusion/implementations/tinybox_8xMI300X/dev_run.sh
@@ -18,7 +18,7 @@ fi
 if [[ -d .venv-sd-mlperf ]]; then
   . .venv-sd-mlperf/bin/activate
 else
-  python3 -m venv .venv-sd-mlperf && . venv-sd-mlperf/bin/activate
+  python3 -m venv .venv-sd-mlperf && . .venv-sd-mlperf/bin/activate
   pip install --index-url https://download.pytorch.org/whl/cpu torch && pip install tqdm numpy ftfy regex pillow scipy wandb webdataset
 fi
 pip list
