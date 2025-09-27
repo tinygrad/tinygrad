41e66d7ef81aa1c4f2b551a981d6325339681f29
hooved <172129504+hooved@users.noreply.github.com>
2025-09-27 17:04:52 +0000

    factor out GPUS

--- test/external/mlperf_stable_diffusion/external_test_models.py
+++ test/external/mlperf_stable_diffusion/external_test_models.py
@@ -4,6 +4,7 @@ from tinygrad.nn.state import get_parameters
 from extra.models import clip
 from examples.mlperf.initializers import gelu_erf
 Device.DEFAULT="NULL"
+GPUS = [f"NULL:{i}" for i in range(8)]
 
 clip_params = {"dims": 1024, "n_heads": 16, "layers": 24, "return_pooled": False, "ln_penultimate": True, "clip_tokenizer_version": "sd_mlperf_v5_0"}
 def get_cond_stage_model(GPUS:list[str]|None=None) -> clip.FrozenOpenClipEmbedder:
@@ -14,7 +15,7 @@ def get_cond_stage_model(GPUS:list[str]|None=None) -> clip.FrozenOpenClipEmbedde
   return model
 def get_tokens(BS:int) -> Tensor: return Tensor([0] * 77 * BS, dtype=dtypes.int32).reshape(-1, 77)
 
-class TestFrozenOpenClip(unittest.TestCase):
+class TestOpenClip(unittest.TestCase):
   def test_tokenizer(self):
     prompt = "Beautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated."
     model = get_cond_stage_model()
@@ -28,7 +29,6 @@ class TestFrozenOpenClip(unittest.TestCase):
       self.assertEqual(resblock.mlp.gelu, gelu_erf)
 
   def test_multigpu_clip_embed(self):
-    GPUS = [f"NULL:{i}" for i in range(8)]
     BS = 304
     model = get_cond_stage_model(GPUS)
     tokens = get_tokens(BS)
@@ -37,7 +37,6 @@ class TestFrozenOpenClip(unittest.TestCase):
     self.assertEqual(embeds.dtype, dtypes.float32)
 
   def test_multigpu_clip_score(self):
-    GPUS = [f"NULL:{i}" for i in range(8)]
     BS = 240
     vision_cfg = {'width': 1280, 'layers': 32, 'd_head': 80, 'image_size': 224, 'patch_size': 14}
     text_cfg = {'width': 1024, 'n_heads': 16, 'layers': 24, 'vocab_size': 49408, 'ctx_length': 77}
