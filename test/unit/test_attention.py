109c63b904aaa7061741c057e5c50ef090206e6c
qazal <77887910+Qazalin@users.noreply.github.com>
2025-09-30 11:17:21 +0300

    update Tensor unit tests for RANGEIFY (#12359)
    * update test_kernelize for RANGEIFY
    
    * also kernelizes user contiguous
    
    * skip that test
    
    * tensor uop repr
    
    * 4 kernels, still realizes a float

--- test/unit/test_attention.py
+++ test/unit/test_attention.py
@@ -1,5 +1,6 @@
 import unittest
 from tinygrad import Tensor, dtypes, TinyJit, UOp
+from tinygrad.helpers import RANGEIFY
 from tinygrad.apps.llm import apply_rope
 
 # TODO: test_scheduler, but just in uint
@@ -12,7 +13,7 @@ class TestAttention(unittest.TestCase):
     attn = q.scaled_dot_product_attention(k, v)
     sched = attn.schedule()
     # attention has 5 kernels now
-    self.assertEqual(len(sched), 5)
+    self.assertEqual(len(sched), 4 if RANGEIFY else 5)
     softmax_inputs = sched[1:4]
     for si in softmax_inputs:
       assert all(b.dtype == dtypes.half for b in si.bufs), f"non half {si.bufs=}"
