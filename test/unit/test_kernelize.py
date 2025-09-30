224d0fb3bb3b34d8eb6c60b78f0efd264e286194
qazal <qazal.software@gmail.com>
2025-09-30 15:52:11 +0800

    also kernelizes user contiguous

--- test/unit/test_kernelize.py
+++ test/unit/test_kernelize.py
@@ -14,14 +14,15 @@ class TestKernelize(unittest.TestCase):
     self.assertIs(ret_reshaped_1.uop.src[0], ret_reshaped_2.uop.src[0])
 
   def test_two_reduce(self):
-    a = Tensor.ones(16,16).contiguous().realize()
+    a = Tensor.ones(16,16).contiguous()
     a1 = a.sum(axis=1)
     a0 = a1.sum(axis=0)
     a0.kernelize()
-    self.assertEqual(len([s for s in a0.uop.toposort() if s.op is Ops.KERNEL]), 1 if RANGEIFY else 2)
+    self.assertEqual(len([s for s in a0.uop.toposort() if s.op is Ops.KERNEL]), 2 if RANGEIFY else 3)
     self.assertIs(a1.uop.base.op, Ops.REDUCE_AXIS if RANGEIFY else Ops.ASSIGN)
-    # input Tensor always kernelizes
+    # input Tensor and user contiguous kernelize
     self.assertIs(a0.uop.base.op, Ops.ASSIGN)
+    self.assertIs(a.uop.base.op, Ops.ASSIGN)
 
   def test_two_reduce_w_add(self):
     a = Tensor.ones(16,16).contiguous()
