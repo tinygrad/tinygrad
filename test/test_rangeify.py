3dc1b2e98eb66ace4fb9cf8715f355b01c94aed9
George Hotz <geohot@gmail.com>
2025-10-01 17:27:17 +0800

    broken

--- test/test_rangeify.py
+++ test/test_rangeify.py
@@ -84,13 +84,13 @@ class TestRangeify(unittest.TestCase):
     C = Tensor.empty(N, N)
     (A@B@C).realize()
 
-  @unittest.skip("gemm tc")
+  #@unittest.skip("gemm tc")
   def test_double_gemm_tc(self):
     with Context(DEBUG=0):
       A, B, C = [Tensor.randn(N, N) for _ in range(3)]
       Tensor.realize(A, B, C)
-    args = (Opt(OptOps.TC, 0, (0,0,1,1))), Opt(OptOps.TC, 0, (0,0,1,0))
-    #args = (Opt(OptOps.TC, 0, (0,0,1,0)),)
+    #args = (Opt(OptOps.TC, 0, (0,0,1,1))), Opt(OptOps.TC, 0, (0,0,1,0))
+    args = (Opt(OptOps.TC, 0, (0,0,1,0)),)
     #args = (Opt(OptOps.TC, 0, (0,0,1,1)),)
     tst = (A@B@C).contiguous(arg=args).realize()
     assert tst.uop.base.op is Ops.BUFFER, "buffer"
