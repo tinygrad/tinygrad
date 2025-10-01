8ab0d05ef36ccc41fa4859d70911e59be30cac9d
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-09-30 22:00:10 -0700

    move that to pm_render

--- tinygrad/uop/symbolic.py
+++ tinygrad/uop/symbolic.py
@@ -505,11 +505,6 @@ sym = symbolic_flat+PatternMatcher([
   # fold gated LOAD/STORE
   (UPat((Ops.LOAD, Ops.STORE), src=(UPat().index(UPat.const(dtypes.index, Invalid)).or_casted(),), allow_any_len=True, name="x"),
     lambda x: UOp(Ops.NOOP) if x.op is Ops.STORE else x.const_like(0)), # invalid store does nothing. invalid load produces 0
-  # Where after gated load becomes alt value
-  (UPat.var("c").where(UPat(Ops.LOAD, src=(UPat().index(UPat.var("c").where(UPat(), invalid_pat)).or_casted(),), allow_any_len=True,
-    name="l").or_casted(), UPat.var("a")), lambda c,l,a,i: l.replace(src=(l.src[0], a.cast(l.dtype))+l.src[1:]).cast(a.dtype)),
-  (UPat.var("c").where(UPat.var("a"), UPat(Ops.LOAD, src=(UPat().index(UPat.var("c").logical_not().where(UPat(), invalid_pat)).or_casted(),),
-    allow_any_len=True, name="l").or_casted()), lambda c,l,a,i: l.replace(src=(l.src[0], a.cast(l.dtype))+l.src[1:]).cast(a.dtype)),
   # remove VECTORIZE from SINK/BARRIER. TODO: SINK/BARRIER are really the same thing at GLOBAL/LOCAL levels
   (UPat(Ops.BARRIER, name="root"),
     lambda root: UOp(Ops.BARRIER, root.dtype, tuple(flatten(x.src if x.op in REMOVE_FROM_BARRIER else (x,) for x in root.src)), root.arg)
