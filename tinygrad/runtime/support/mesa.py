bacf5c91cc5881cdcd610dd0933ff9b96a0e8273
Christopher Milan <chrismilan@ucla.edu>
2025-10-02 08:31:35 -0700

    mypy

--- tinygrad/runtime/support/mesa.py
+++ tinygrad/runtime/support/mesa.py
@@ -2,5 +2,5 @@ import os, ctypes, ctypes.util
 from tinygrad.helpers import getenv
 
 MESA_PATH = getenv('MESA_PATH', '/usr/lib')
-found = (os.path.exists(path:=f"{MESA_PATH}/libtinymesa_cpu.so") or (path:=ctypes.util.find_library('tinymesa_cpu')) or
-         os.path.exists(path:=f"{MESA_PATH}/libtinymesa.so") or (path:=ctypes.util.find_library('tinymesa')))
+found = (os.path.exists(path:=f"{MESA_PATH}/libtinymesa_cpu.so") or (path:=ctypes.util.find_library('tinymesa_cpu') or '') or
+         os.path.exists(path:=f"{MESA_PATH}/libtinymesa.so") or (path:=ctypes.util.find_library('tinymesa') or ''))
