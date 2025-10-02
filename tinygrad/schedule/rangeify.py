bbc0fee96aedd7a4a8fd7cee0426b7729a9a2a49
qazal <qazal.software@gmail.com>
2025-10-02 17:59:54 +0800

    don't tag MSTACK/MSELECT

--- tinygrad/schedule/rangeify.py
+++ tinygrad/schedule/rangeify.py
@@ -692,7 +692,8 @@ def tag_uop(ctx:list[UOp], x:UOp):
   return x.replace(tag=(len(ctx)-1,))
 add_tags = PatternMatcher([
   # don't tag BUFFERs, they are global
-  (UPat(GroupOp.All-{Ops.BUFFER, Ops.CONST, Ops.DEVICE, Ops.UNIQUE, Ops.DEFINE_VAR, Ops.BIND}.union(GroupOp.Movement), name="x"), tag_uop),
+  (UPat(GroupOp.All-{Ops.BUFFER, Ops.CONST, Ops.DEVICE, Ops.UNIQUE, Ops.DEFINE_VAR, Ops.BIND, Ops.MSTACK,
+                     Ops.MSELECT}.union(GroupOp.Movement), name="x"), tag_uop),
 ])
 
 # support for using a contiguous permuted view instead of the parent view if one exists
