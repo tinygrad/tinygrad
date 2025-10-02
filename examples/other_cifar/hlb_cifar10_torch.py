c518f8828f27a0e2f3460de47f3b33a3cfa2d811
0xbeedee <81985269+0xbeedee@users.noreply.github.com>
2025-10-02 21:11:10 +0200

    further torchify the code and cleanup

--- examples/other_cifar/hlb_cifar10_torch.py
+++ examples/other_cifar/hlb_cifar10_torch.py
@@ -2,7 +2,7 @@ import random, time
 import numpy as np
 from typing import Optional, Tuple
 
-from tinygrad import dtypes, Tensor, Device, GlobalCounters
+from tinygrad import dtypes, Device, GlobalCounters
 from tinygrad.helpers import Context, BEAM, WINO, getenv, colored, DEBUG
 from tinygrad.nn.datasets import cifar
 from extra.bench_log import BenchEvent, WallTimeEvent
@@ -71,7 +71,6 @@ class SpeedyResNet(nn.Module):
     self.linear = nn.Linear(512, 10, bias=False)
 
   def forward(self, x):
-    # TODO kept it to one line as in the original, perhaps split into two lines for clarity?
     x = F.pad(F.conv2d(x, self.whitening), ((1, 0, 0, 1)))
     x = self.net(x)
     x = torch.amax(x, dim=(2, 3))
@@ -139,21 +138,6 @@ def train_cifar():
     W = V / np.sqrt(Λ + 1e-2)[:, None, None, None]
 
     return torch.tensor((W.astype(np.float32)), dtype=torch.float32, requires_grad=False)
-
-  # ========== Loss ==========
-  # TODO do i keep the below code? test to make sure the outputs are the same
-  # def cross_entropy(x: torch.Tensor, y: torch.Tensor, reduction: str = "mean", label_smoothing: float = 0.0) -> torch.Tensor:
-  #   divisor = y.shape[1]
-  #   assert isinstance(divisor, int), "only supported int divisor"
-  #   y = (1 - label_smoothing) * y + label_smoothing / divisor
-  #   ret = -x.log_softmax(axis=1).mul(y).sum(axis=1)
-  #   if reduction == "none":
-  #     return ret
-  #   if reduction == "sum":
-  #     return ret.sum()
-  #   if reduction == "mean":
-  #     return ret.mean()
-  #   raise NotImplementedError(reduction)
 
   # ========== Preprocessing ==========
   # NOTE: this only works for RGB in format of NxCxHxW and pads the HxW
@@ -235,14 +219,15 @@ def train_cifar():
       self.net_ema = SpeedyResNet(w)
       for net_ema_param, net_param in zip(self.net_ema.state_dict().values(), net.state_dict().values()):
         net_ema_param.requires_grad = False
-        net_ema_param.assign(net_param.numpy())
+        net_ema_param.copy_(net_param.numpy())
 
     def update(self, net, decay):
       for net_ema_param, (param_name, net_param) in zip(self.net_ema.state_dict().values(), net.state_dict().items()):
         # batchnorm currently is not being tracked
         if not ("num_batches_tracked" in param_name) and not ("running" in param_name):
-          net_ema_param.assign(net_ema_param.detach() * decay + net_param.detach() * (1.0 - decay)).realize()
+          net_ema_param.copy_(net_ema_param.detach() * decay + net_param.detach() * (1.0 - decay)).realize()
 
+  # ========== Setup ==========
   set_seed(getenv("SEED", hyp["seed"]))
 
   if getenv("TINY_BACKEND"):
@@ -335,88 +320,88 @@ def train_cifar():
       lr_schedulers[1].step()
     return loss
 
+  @torch.no_grad()
   def eval_step(model, X, Y):
-    out = model(X, training=False)
+    out = model(X)
     loss_fn = nn.CrossEntropyLoss(reduction="mean")
     loss = loss_fn(out, Y)
     correct = out.argmax(axis=1) == Y.argmax(axis=1)
     return correct.realize(), loss.realize()
 
+  # ========== Training ==========
   step_times = []
   model_ema: Optional[modelEMA] = None
   projected_ema_decay_val = hyp["ema"]["decay_base"] ** hyp["ema"]["every_n_steps"]
   i = 0
   eval_acc_pct = 0.0
   batcher = fetch_batches(X_train, Y_train, BS=BS, is_train=True)
-  with Tensor.train():
-    st = time.monotonic()
-    while i <= STEPS:
-      if i % getenv("EVAL_STEPS", STEPS) == 0 and i > 1 and not getenv("DISABLE_BACKWARD"):
-        # Use Tensor.training = False here actually bricks batchnorm, even with track_running_stats=True
-        corrects = []
-        corrects_ema = []
-        losses = []
-        losses_ema = []
-        for Xt, Yt in fetch_batches(X_test, Y_test, BS=EVAL_BS, is_train=False):
-          # TODO sharding temporarily disabled
-          # if len(GPUS) > 1:
-          #   Xt.shard_(GPUS, axis=0)
-          #   Yt.shard_(GPUS, axis=0)
-
-          correct, loss = eval_step(model, Xt, Yt)
-          losses.append(loss.numpy().tolist())
-          corrects.extend(correct.numpy().tolist())
-          if model_ema:
-            correct_ema, loss_ema = eval_step(model_ema.net_ema, Xt, Yt)
-            losses_ema.append(loss_ema.numpy().tolist())
-            corrects_ema.extend(correct_ema.numpy().tolist())
-
-        # collect accuracy across ranks
-        correct_sum, correct_len = sum(corrects), len(corrects)
-        if model_ema:
-          correct_sum_ema, correct_len_ema = sum(corrects_ema), len(corrects_ema)
-
-        eval_acc_pct = correct_sum / correct_len * 100.0
-        if model_ema:
-          acc_ema = correct_sum_ema / correct_len_ema * 100.0
-        print(
-          f"eval     {correct_sum}/{correct_len} {eval_acc_pct:.2f}%, {(sum(losses) / len(losses)):7.2f} val_loss STEP={i} (in {(time.monotonic() - st) * 1e3:.2f} ms)"
-        )
-        if model_ema:
-          print(f"eval ema {correct_sum_ema}/{correct_len_ema} {acc_ema:.2f}%, {(sum(losses_ema) / len(losses_ema)):7.2f} val_loss STEP={i}")
-
-      if STEPS == 0 or i == STEPS:
-        break
-
-      GlobalCounters.reset()
-
-      with WallTimeEvent(BenchEvent.STEP):
-        X, Y = next(batcher)
+  model.train()
+  st = time.monotonic()
+  while i <= STEPS:
+    if i % getenv("EVAL_STEPS", STEPS) == 0 and i > 1 and not getenv("DISABLE_BACKWARD"):
+      corrects = []
+      corrects_ema = []
+      losses = []
+      losses_ema = []
+      for Xt, Yt in fetch_batches(X_test, Y_test, BS=EVAL_BS, is_train=False):
         # TODO sharding temporarily disabled
         # if len(GPUS) > 1:
-        #   X.shard_(GPUS, axis=0)
-        #   Y.shard_(GPUS, axis=0)
+        #   Xt.shard_(GPUS, axis=0)
+        #   Yt.shard_(GPUS, axis=0)
 
-        with Context(BEAM=getenv("LATEBEAM", BEAM.value), WINO=getenv("LATEWINO", WINO.value)):
-          loss = train_step(model, [opt_bias, opt_non_bias], [lr_sched_bias, lr_sched_non_bias], X, Y)
-          et = time.monotonic()
-          loss_cpu = loss.detach().numpy()
-        # EMA for network weights
-        if getenv("EMA") and i > hyp["ema"]["steps"] and (i + 1) % hyp["ema"]["every_n_steps"] == 0:
-          if model_ema is None:
-            model_ema = modelEMA(W, model)
-          model_ema.update(model, Tensor([projected_ema_decay_val * (i / STEPS) ** hyp["ema"]["decay_pow"]]))
+        correct, loss = eval_step(model, Xt, Yt)
+        losses.append(loss.numpy().tolist())
+        corrects.extend(correct.numpy().tolist())
+        if model_ema:
+          correct_ema, loss_ema = eval_step(model_ema.net_ema, Xt, Yt)
+          losses_ema.append(loss_ema.numpy().tolist())
+          corrects_ema.extend(correct_ema.numpy().tolist())
 
-      cl = time.monotonic()
-      step_times.append((cl - st) * 1000.0)
-      device_str = loss.device if isinstance(loss.device, str) else f"{loss.device}"
-      #  53  221.74 ms run,    2.22 ms python,  219.52 ms CL,  803.39 loss, 0.000807 LR, 4.66 GB used,   3042.49 GFLOPS,    674.65 GOPS
+      # collect accuracy across ranks
+      correct_sum, correct_len = sum(corrects), len(corrects)
+      if model_ema:
+        correct_sum_ema, correct_len_ema = sum(corrects_ema), len(corrects_ema)
+
+      eval_acc_pct = correct_sum / correct_len * 100.0
+      if model_ema:
+        acc_ema = correct_sum_ema / correct_len_ema * 100.0
       print(
-        # TODO is the method to get the LR correct? check this!
-        f"{i:3d} {(cl - st) * 1000.0:7.2f} ms run, {(et - st) * 1000.0:7.2f} ms python, {(cl - et) * 1000.0:7.2f} ms {device_str}, {loss_cpu:7.2f} loss, {lr_sched_non_bias.get_last_lr()[0]:.6f} LR, {GlobalCounters.mem_used / 1e9:.2f} GB used, {GlobalCounters.global_ops * 1e-9 / (cl - st):9.2f} GFLOPS, {GlobalCounters.global_ops * 1e-9:9.2f} GOPS"
+        f"eval     {correct_sum}/{correct_len} {eval_acc_pct:.2f}%, {(sum(losses) / len(losses)):7.2f} val_loss STEP={i} (in {(time.monotonic() - st) * 1e3:.2f} ms)"
       )
-      st = cl
-      i += 1
+      if model_ema:
+        print(f"eval ema {correct_sum_ema}/{correct_len_ema} {acc_ema:.2f}%, {(sum(losses_ema) / len(losses_ema)):7.2f} val_loss STEP={i}")
+
+    if STEPS == 0 or i == STEPS:
+      break
+
+    GlobalCounters.reset()
+
+    with WallTimeEvent(BenchEvent.STEP):
+      X, Y = next(batcher)
+      # TODO sharding temporarily disabled
+      # if len(GPUS) > 1:
+      #   X.shard_(GPUS, axis=0)
+      #   Y.shard_(GPUS, axis=0)
+
+      with Context(BEAM=getenv("LATEBEAM", BEAM.value), WINO=getenv("LATEWINO", WINO.value)):
+        loss = train_step(model, [opt_bias, opt_non_bias], [lr_sched_bias, lr_sched_non_bias], X, Y)
+        et = time.monotonic()
+        loss_cpu = loss.detach().numpy()
+      # EMA for network weights
+      if getenv("EMA") and i > hyp["ema"]["steps"] and (i + 1) % hyp["ema"]["every_n_steps"] == 0:
+        if model_ema is None:
+          model_ema = modelEMA(W, model)
+        model_ema.update(model, torch.tensor([projected_ema_decay_val * (i / STEPS) ** hyp["ema"]["decay_pow"]]))
+
+    cl = time.monotonic()
+    step_times.append((cl - st) * 1000.0)
+    device_str = loss.device if isinstance(loss.device, str) else f"{loss.device}"
+    #  53  221.74 ms run,    2.22 ms python,  219.52 ms CL,  803.39 loss, 0.000807 LR, 4.66 GB used,   3042.49 GFLOPS,    674.65 GOPS
+    print(
+      f"{i:3d} {(cl - st) * 1000.0:7.2f} ms run, {(et - st) * 1000.0:7.2f} ms python, {(cl - et) * 1000.0:7.2f} ms {device_str}, {loss_cpu:7.2f} loss, {lr_sched_non_bias.get_last_lr()[0]:.6f} LR, {GlobalCounters.mem_used / 1e9:.2f} GB used, {GlobalCounters.global_ops * 1e-9 / (cl - st):9.2f} GFLOPS, {GlobalCounters.global_ops * 1e-9:9.2f} GOPS"
+    )
+    st = cl
+    i += 1
 
   if assert_time := getenv("ASSERT_MIN_STEP_TIME"):
     min_time = min(step_times)
