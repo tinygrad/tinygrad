4083cbd6c48075d7d9877f797f1f1ba8a972092d
qazal <qazal.software@gmail.com>
2025-10-03 16:45:09 +0800

    alt

--- tinygrad/schedule/rangeify.py
+++ tinygrad/schedule/rangeify.py
@@ -72,8 +72,8 @@ earliest_rewrites = PatternMatcher([
 
   # assign only to buffer, otherwise make it a CONTIGUOUS
   (UPat(Ops.ASSIGN, src=(UPat(GroupOp.All-{Ops.BUFFER}, name="target"), UPat(name="x")), name="assign"),
-   lambda x,target,assign: x.f(Ops.CONTIGUOUS, tag=assign.tag) if (((t:=target.base).op is not Ops.BUFFER and target.tag is None) and \
-       not (t.op is Ops.MSTACK and all(s.op is Ops.BUFFER for s in t.src))) else None),
+   lambda x,target,assign: x.f(Ops.CONTIGUOUS, tag=assign.tag) if (((t:=target.base).op is not Ops.BUFFER) and \
+       not (t.op is Ops.MSTACK and all(s.op is Ops.BUFFER for s in t.src))) or target.tag is not None else None),
 
    # realize before assign if input permutes the target buffer
    (UPat(Ops.ASSIGN, src=(UPat.var("a"), UPat.var("b")), name="assign"), find_permutes),
