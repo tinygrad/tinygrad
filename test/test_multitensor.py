92b482284050b69cdc3328c578700435a97432be
Chen-Yu Yang <chenyu@fastmail.com>
2025-10-06 10:54:57 +0800

    multi test case for sharded ring allreduce
    triggers `children not making progress` with RANGEIFY

--- test/test_multitensor.py
+++ test/test_multitensor.py
@@ -201,6 +201,13 @@ class TestMultiTensor(unittest.TestCase):
     fn = f(n)
     np.testing.assert_allclose(fX.numpy(), fn, rtol=1e-6, atol=1e-6)
 
+  def test_allreduce_shard_ring_sum(self):
+    for axis in (0, 1, None):
+      for use_ring in (0, 2):
+        t = Tensor([1, 2, 3, 4]).reshape(2, 2)
+        with Context(RING=use_ring):
+          np.testing.assert_equal(t.shard(devices_2, axis=axis).sum().item(), 10)
+
   def test_allreduce_naive(self):
     with Context(RING=0):
       a,b = _test_allreduce(Tensor.rand(256, 256))
