1da6ee823cbb2156918997d5026b264e1dd9e4d9
qazal <qazal.software@gmail.com>
2025-10-08 12:06:11 +0800

    alt also passes

--- test/test_schedule.py
+++ test/test_schedule.py
@@ -120,7 +120,6 @@ class TestSchedule(unittest.TestCase):
     with self.assertRaisesRegex(RuntimeError, "all buffers must be on the same device"): check_schedule(c, 1)
 
   @unittest.skipIf(Device.DEFAULT == "CPU", "devices must mismatch")
-  @expect_rangeify_fails
   def test_error_on_device_mismatch_alt(self):
     a = Tensor.empty(10)
     b = Tensor.empty((1,), device="CPU").expand(10).contiguous()
