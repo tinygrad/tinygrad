4300ebc455c91812c265cff9ff9b7f8ec50b6a69
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-10-11 08:53:10 +0200

    cache apply_movement_op (#12609)
    * cache apply_movement_op
    
    * pyling and clear cache
    
    * fix types
    
    * ignore
    
    * cleanup

--- tinygrad/schedule/rangeify.py
+++ tinygrad/schedule/rangeify.py
@@ -103,7 +103,7 @@ earliest_rewrites = PatternMatcher([
 # movement op on INDEX as a PatternMatcher
 pm_mops = PatternMatcher([
   (UPat(GroupOp.Movement, name="r").f(Ops.INDEX, allow_any_len=True, name="idx"),
-   lambda r,idx: r.src[0].index(*apply_movement_op(r, idx.src[1:]), dtype=idx.dtype, arg=idx.arg)),
+   lambda r,idx: r.src[0].index(*apply_movement_op(r.op, r.src[0].shape, r.arg, idx.src[1:]), dtype=idx.dtype, arg=idx.arg)),  # type: ignore
 ])
 
 # *****************
