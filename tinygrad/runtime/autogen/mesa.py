04e24e03c749dca62819bafed6b6835013bbc073
Christopher Milan <chrismilan@ucla.edu>
2025-10-16 08:15:19 -0700

    check both brew paths

--- tinygrad/runtime/autogen/mesa.py
+++ tinygrad/runtime/autogen/mesa.py
@@ -7,15 +7,14 @@
 # LONGDOUBLE_SIZE is: 16
 #
 import ctypes, ctypes.util, os, gzip, base64, subprocess, tinygrad.helpers as helpers
-def brew_path():
-  try: return f"{subprocess.check_output(['brew', '--prefix', 'tinymesa_cpu']).decode().strip()}/lib/libtinymesa_cpu.dylib"
-  except Exception:
-    try: return f"{subprocess.check_output(['brew', '--prefix', 'tinymesa']).decode().strip()}/lib/libtinymesa.dylib"
-    except Exception: return ''
+def brew_path(nm):
+  try: return f"{subprocess.check_output(['brew', '--prefix', nm]).decode().strip()}/lib/lib{nm}.dylib"
+  except Exception: return ''
 PATHS_TO_TRY = [
   (BASE:=os.getenv('MESA_PATH', f"/usr{'/local/' if helpers.OSX else '/'}lib"))+'/libtinymesa_cpu'+(EXT:='.dylib' if helpers.OSX else '.so'),
   f'{BASE}/libtinymesa{EXT}',
-  brew_path(),
+  brew_path('tinymesa_cpu'),
+  brew_path('tinymesa'),
 ]
 def _try_dlopen_tinymesa_cpu():
   library = ctypes.util.find_library("tinymesa_cpu")
