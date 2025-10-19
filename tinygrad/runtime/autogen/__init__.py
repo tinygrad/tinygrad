51a6cf20eaa154a4b81690ca7e596dedf24fd6e2
Christopher Milan <chrismilan@ucla.edu>
2025-10-19 08:29:21 -0700

    opencl

--- tinygrad/runtime/autogen/__init__.py
+++ tinygrad/runtime/autogen/__init__.py
@@ -4,3 +4,5 @@ from tinygrad.runtime.autogen.autogen import Autogen
 libc = Autogen("libc", "None if (libc_path:=ctypes.util.find_library('c')) is None else ctypes.CDLL(libc_path, use_errno=True)",
   lambda: ([i for i in subprocess.check_output("dpkg -L libc6-dev".split()).decode().split() if 'sys/mman.h' in i or 'sys/syscall.h' in i] +
            ["/usr/include/string.h", "/usr/include/elf.h", "/usr/include/unistd.h", "/usr/include/asm-generic/mman-common.h"]))
+
+opencl = Autogen("opencl", "ctypes.CDLL(ctypes.util.find_library('OpenCL'))", ["/usr/include/CL/cl.h"])
