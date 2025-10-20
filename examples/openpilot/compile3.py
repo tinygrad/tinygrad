ae8b5258bcada844b39a379050685fd415a823d9
Woze Parrot <wozeparrot@gmail.com>
2025-10-20 23:08:06 +0000

    feat: compile3 bench

--- examples/openpilot/compile3.py
+++ examples/openpilot/compile3.py
@@ -1,3 +1,4 @@
+from ast import Global
 import os, sys, pickle, time, re
 import numpy as np
 
@@ -121,6 +122,13 @@ def test_vs_onnx(new_inputs, test_val, onnx_file, tol):
   print("test vs onnx passed")
   return timings
 
+def time(run, inputs):
+  from extra.bench_log import WallTimeEvent, KernelTimeEvent, BenchEvent
+  for _ in range(10):
+    with WallTimeEvent(BenchEvent.STEP):
+      with KernelTimeEvent(BenchEvent.STEP):
+        run(**inputs).numpy()
+
 if __name__ == "__main__":
   onnx_file = fetch(OPENPILOT_MODEL)
   inputs, outputs = compile(onnx_file)
@@ -131,3 +139,5 @@ if __name__ == "__main__":
   if not getenv("FLOAT16"):
     test_vs_onnx(inputs, outputs, onnx_file, 1e-4)
 
+  if getenv("BENCHMARK_LOG"):
+    time(pickle_loaded, inputs)
