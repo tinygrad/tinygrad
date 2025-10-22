109b994aa392338ed101a468879626fc0fab8815
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-10-22 16:05:12 +0800

    split ranges but only on cpu

--- tinygrad/codegen/simplify.py
+++ tinygrad/codegen/simplify.py
@@ -1,6 +1,6 @@
 from tinygrad.uop.ops import UOp, PatternMatcher, UPat, Ops, graph_rewrite, _substitute, range_start, ImageDType
 from tinygrad.uop.symbolic import symbolic_flat
-from tinygrad.helpers import partition
+from tinygrad.helpers import partition, dedup
 from tinygrad.dtype import dtypes
 
 def flatten_range(r:UOp):
@@ -129,4 +129,18 @@ def reduce_collapse(red:UOp):
 pm_reduce_simplify = pm_reduce_unparented + PatternMatcher([
   # remove REDUCE without loads (generic arange opt / indexing). TODO: support multi range
   (UPat(Ops.REDUCE, src=(UPat(), UPat()), name="red"), reduce_collapse),
+])
+
+def cut_store_range(ctx, store:UOp):
+  # only cut ranges on CPU for now
+  if store.src[2].src[0].op is not Ops.CONST or ctx!="CPU": return None
+  idx, val, r = store.src
+  if not (cuts:=[c.src[1].arg for c in store.get_consumer_map()[r] if c.op is Ops.CMPLT and r is c.src[0] and c.src[1].op is Ops.CONST]): return None
+  cuts = sorted(dedup([0] + cuts + [r.src[0].arg]))
+
+  return UOp.sink(*[store.substitute({r: UOp.range((end-start), *(r.arg[0:-1]+(i,r.arg[-1])))+start})
+    for i,(start,end) in enumerate(zip(cuts[:-1], cuts[1:]))])
+
+pm_split_store = pm_flatten_range+PatternMatcher([
+  (UPat(Ops.STORE, src=(UPat(), UPat(), UPat()), name="store").sink(), cut_store_range),
 ])
