7cbf21a4977b66856f2ba3470471a6a4a35114c2
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2025-10-22 20:35:18 +0800

    try giving endrange priority

--- tinygrad/codegen/late/control_flow.py
+++ tinygrad/codegen/late/control_flow.py
@@ -45,7 +45,8 @@ def linearize(u:UOp) -> list[UOp]:
     if u.op is Ops.LOAD: priority.append(-1000)
     if u.op is Ops.BARRIER: priority.append(-1500)
     # ranges are scheduled as late as possible so anything that can be outside is
-    if u.op is Ops.RANGE: priority = [2000]
+    # if u.op is Ops.RANGE: priority = [2000]
+    if u.op is Ops.END: priority = [-1000]
     # move defines and consts to the top
     if u.op in {Ops.DEFINE_GLOBAL, Ops.DEFINE_LOCAL, Ops.DEFINE_REG, Ops.DEFINE_VAR, Ops.SPECIAL, Ops.CONST}: priority.append(-2000)
     priorities[u] = min(priority)
