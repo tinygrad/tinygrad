b66bbcd072ef9e0cc467542925b8708ebdedfa95
Tyler Guest <tylerguest@users.noreply.github.com>
2025-10-23 23:15:09 -0400

    * updated colored int instance, could be better

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -28,7 +28,7 @@ def argsort(x): return type(x)(sorted(range(len(x)), key=x.__getitem__))
 def all_same(items:tuple[T, ...]|list[T]): return all(x == items[0] for x in items)
 def all_int(t: Sequence[Any]) -> TypeGuard[tuple[int, ...]]: return all(isinstance(s, int) for s in t)
 def colored(st, color:str|int|None, background=False): # replace the termcolor library
-  if isinstance(color, int): return f"\u001b[{48 if background else 38};5;{int(color)}m{st}\u001b[0m"
+  if isinstance(color, int): return f"\u001b[{48 if background else 38};5;{int(color)}m{st}\u001b[0m" if color is not None else st
   colors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
   return f"\u001b[{10*background+60*(color.upper() == color)+30+colors.index(color.lower())}m{st}\u001b[0m" if color is not None else st
 def colorize_float(x: float): return colored(f"{x:7.2f}x", 'green' if x < 0.75 else 'red' if x > 1.15 else 'yellow')
