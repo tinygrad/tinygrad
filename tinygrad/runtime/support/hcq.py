b95a533662fd15c5ac253f8047fa07a105899945
nimlgen <lenyaprokh@gmail.com>
2025-10-28 02:17:02 +0800

    f

--- tinygrad/runtime/support/hcq.py
+++ tinygrad/runtime/support/hcq.py
@@ -57,7 +57,7 @@ if MOCKGPU:=getenv("MOCKGPU"): from test.mockgpu.mockgpu import MockFileIOInterf
 
 # **************** for HCQ Compatible Devices ****************
 
-def hcq_filter_visible_devices(dev:Sequence[Any]):
+def hcq_filter_visible_devices(dev):
   return [dev[x] for x in ids] if (ids:=[int(x) for x in (getenv('HCQ_VISIBLE_DEVICES', '')).split(',') if x.strip()]) else dev
 
 SignalType = TypeVar('SignalType', bound='HCQSignal')
