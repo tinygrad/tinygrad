babbd4151bbcb6e6b2f616cf2c2b5148db4650a0
Chen-Yu Yang <chenyu@fastmail.com>
2025-10-29 14:35:51 -0400

    empty change [pr]

--- examples/whisper.py
+++ examples/whisper.py
@@ -219,6 +219,7 @@ MODEL_URLS = {
   "large-v2": "https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt",
   "large": "https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt",
 }
+
 def init_whisper(model_name="tiny.en", batch_size=1):
   assert MODEL_URLS[model_name] is not None
 
