4f7205775b917958265e16b302f7355ee2eaa722
Chen-Yu Yang <chenyu@fastmail.com>
2025-10-31 18:23:35 -0400

    only float

--- test/test_custom_kernel.py
+++ test/test_custom_kernel.py
@@ -90,7 +90,8 @@ class TestCustomKernel(unittest.TestCase):
     self.assertTrue((b_p1 == 3).all().item())
 
   def test_sum(self):
-    a = Tensor([1, 2, 3, 4, 5])
+    # TODO: this only works for float, and silently fails with int
+    a = Tensor([1.0, 2, 3, 4, 5])
     tst = Tensor.empty(1)
     b = Tensor.custom_kernel(tst, a, fxn=custom_sum)[0]
     self.assertEqual(b.item(), 15)
