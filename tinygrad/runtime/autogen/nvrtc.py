6d2df88bbaa4975071d1c7f07883a0c44b9efa54
Christopher Milan <chrismilan@ucla.edu>
2025-11-01 09:13:15 -0700

    regen nvrtc/nvjitlink

--- tinygrad/runtime/autogen/nvrtc.py
+++ tinygrad/runtime/autogen/nvrtc.py
@@ -20,7 +20,6 @@ NVRTC_ERROR_NO_NAME_EXPRESSIONS_AFTER_COMPILATION = nvrtcResult.define('NVRTC_ER
 NVRTC_ERROR_NO_LOWERED_NAMES_BEFORE_COMPILATION = nvrtcResult.define('NVRTC_ERROR_NO_LOWERED_NAMES_BEFORE_COMPILATION', 9)
 NVRTC_ERROR_NAME_EXPRESSION_NOT_VALID = nvrtcResult.define('NVRTC_ERROR_NAME_EXPRESSION_NOT_VALID', 10)
 NVRTC_ERROR_INTERNAL_ERROR = nvrtcResult.define('NVRTC_ERROR_INTERNAL_ERROR', 11)
-NVRTC_ERROR_TIME_FILE_WRITE_FAILED = nvrtcResult.define('NVRTC_ERROR_TIME_FILE_WRITE_FAILED', 12)
 
 # const char *nvrtcGetErrorString(nvrtcResult result)
 try: (nvrtcGetErrorString:=dll.nvrtcGetErrorString).restype, nvrtcGetErrorString.argtypes = ctypes.POINTER(ctypes.c_char), [nvrtcResult]
