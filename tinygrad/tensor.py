729a806314e2fdaf90f021a51bc3087ea818aad6
Duy Le <71242963+duyle-py@users.noreply.github.com>
2025-11-06 23:04:56 +0700

    refactor Tensor class to improve dimension resolution and enhance offset handling in the diagonal method

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -1505,11 +1505,11 @@ class Tensor(OpMixin):
     """
     if offset == 0 and dim1 == 0 and dim2 == 1 and self.ndim == 2 and self.shape[0] == self.shape[1]:
       return self.flatten().pad(((0, self.shape[0]))).reshape(self.shape[0], self.shape[0]+1)[:, 0]
-    d1, d2 = sorted((dim1 % self.ndim, dim2 % self.ndim))
+    d1, d2 = sorted((self._resolve_dim(dim1), self._resolve_dim(dim2)))
     x = self.permute([i for i in range(self.ndim) if i not in (d1, d2)] + [d1, d2])
-    size = max(0, min(x.shape[-2] - max(0, -offset), x.shape[-1] - max(0, offset)))
+    size = smax(0, smin(x.shape[-2] - smax(0, -offset), x.shape[-1] - smax(0, offset)))
     return Tensor.empty(*x.shape[:-2], 0, dtype=self.dtype, device=self.device) if size == 0 else \
-           x[..., (idx := Tensor.arange(size, device=self.device)) + max(0, -offset), idx + max(0, offset)]
+           x[..., (idx := Tensor.arange(size, device=self.device)) + smax(0, -offset), idx + smax(0, offset)]
 
   def roll(self, shifts:int|tuple[int, ...], dims:int|tuple[int, ...]|None=None) -> Tensor:
     """
