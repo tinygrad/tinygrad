8dd777b8936d3c5468eeafe40db7117df10ff7b3
nimlgen <lenyaprokh@gmail.com>
2025-11-07 22:42:16 +0800

    mypy

--- tinygrad/runtime/support/hcq.py
+++ tinygrad/runtime/support/hcq.py
@@ -439,7 +439,7 @@ class HCQCompiled(Compiled, Generic[SignalType]):
 
   def _select_iface(self, *ifaces:Type):
     if val:=getenv(f'{type(self).__name__[:-6].upper()}_IFACE', ""): ifaces = tuple(x for x in ifaces if x.__name__.startswith(val.upper()))
-    return select_first_inited([functools.partial(iface, self, self.device_id) for iface in ifaces],
+    return select_first_inited([functools.partial(cast(Callable, iface), self, self.device_id) for iface in ifaces],
                                f"No interface for {type(self).__name__[:-6]}:{self.device_id} is available")
 
   def _is_cpu(self) -> bool: return hasattr(self, 'device') and self.device.split(":")[0] == "CPU"
