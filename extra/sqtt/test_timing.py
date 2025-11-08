f40d937465bfbfd346e630093f4b65530a58370b
qazal <qazal.software@gmail.com>
2025-11-08 12:11:15 +0000

    work

--- extra/sqtt/test_timing.py
+++ extra/sqtt/test_timing.py
@@ -108,9 +108,11 @@ class TestTiming(unittest.TestCase):
       op = custom(f"*((volatile unsigned int*)scratch) = *((volatile unsigned int*)scratch);", op)
       op = custom('asm volatile("" :: "v"(scratch) : "memory");', op)
       # move v10's value to scratch
-      op = custom(f'asm volatile("v_mov_b32_e32 v10 100");', op)
+      op = custom('asm volatile("s_mov_b32_e32 s10 200");', op)
+      op = custom('asm volatile("v_writelane_b32 v10 s10 0");', op)
       op = custom(f'asm volatile("scratch_store_b32 off v10 off offset:0");', op)
-      op = custom(f'asm volatile("v_mov_b32_e32 v10 0");', op)
+      op = custom('asm volatile("s_mov_b32_e32 s10 0");', op)
+      op = custom('asm volatile("v_writelane_b32 v10 s10 0");', op)
       op = custom('asm volatile("s_waitcnt_vscnt null, 0");', op)
       # read back and store
       op = custom(f'asm volatile("scratch_load_b32 v10 off off offset:0");', op)
@@ -120,7 +122,7 @@ class TestTiming(unittest.TestCase):
     ret = Tensor([0,0,0,0], dtype=dtypes.uint32).realize()
     ret = Tensor.custom_kernel(ret, fxn=custom_spill)[0]
     ret.realize()
-    self.assertListEqual(ret.tolist(), [100, 0, 0, 0])
+    self.assertListEqual(ret.tolist(), [200, 0, 0, 0])
 
 if __name__ == "__main__":
   unittest.main()
