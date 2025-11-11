e606ba9d0986d76a56f75ca4b442d9f526eda6cb
Chen-Yu Yang <chenyu@fastmail.com>
2025-11-11 20:09:20 +0000

    interesting

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -4119,10 +4119,10 @@ class Tensor(OpMixin):
 
     # NOTE: with NHWC we can remove the transposes
     # bs x groups*cin x H x W
-    cx = self.transpose(-1, -2).reshape(bs//groups, groups*cin, -1, 1)
+    cx = self.transpose(self.ndim-1, self.ndim-2).reshape(bs//groups, groups*cin, -1, 1)
     # groups*cout x cin x H, W
-    cw = w.transpose(-1, -2).reshape(groups*cout, cin, 1, 1)
-    return cx.image_conv2d(cw, groups=groups, dtype=dtype).reshape(out_shape_t).transpose(-1, -2)
+    cw = w.transpose(w.ndim-1, w.ndim-2).reshape(groups*cout, cin, 1, 1)
+    return cx.image_conv2d(cw, groups=groups, dtype=dtype).reshape(out_shape_t).transpose(self.ndim-1, self.ndim-2)
 
   def image_conv2d(self, weight:Tensor, bias:Tensor|None=None, groups=1, stride=1, dilation=1, padding=0, dtype=None) -> Tensor:
     base_image_type = dtypes.imageh if getenv("FLOAT16", 0) else dtypes.imagef
