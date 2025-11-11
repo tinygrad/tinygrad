b463f7763f017d2a236cbf650ff116bb69d1dafa
Christopher Milan <chrismilan@ucla.edu>
2025-11-11 14:51:10 -0500

    move sys to system in helpers.py

--- tinygrad/runtime/autogen/__init__.py
+++ tinygrad/runtime/autogen/__init__.py
@@ -1,6 +1,5 @@
-import importlib, pathlib, subprocess
-
-def sys(cmd): return subprocess.check_output(cmd.split()).decode().strip()
+import importlib, pathlib
+from tinygrad.helpers import system
 
 root = (here:=pathlib.Path(__file__).parent).parents[2]
 
@@ -13,6 +12,6 @@ def load(name, dll, files, **kwargs):
 def __getattr__(nm):
   match nm:
     case "libc": return load("libc", ["find_library('c')"], lambda: (
-      [i for i in sys("dpkg -L libc6-dev").split() if 'sys/mman.h' in i or 'sys/syscall.h' in i] +
+      [i for i in system("dpkg -L libc6-dev").split() if 'sys/mman.h' in i or 'sys/syscall.h' in i] +
       ["/usr/include/string.h", "/usr/include/elf.h", "/usr/include/unistd.h", "/usr/include/asm-generic/mman-common.h"]), use_errno=True)
     case _: raise AttributeError(f"no such autogen: {nm}")
