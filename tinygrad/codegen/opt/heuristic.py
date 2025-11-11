9166e5e6c6339fbc7f5ed788657d684969c84b80
Chen-Yu Yang <chenyu@fastmail.com>
2025-11-10 20:46:56 -0800

    test delete more stuff in heuristic

--- tinygrad/codegen/opt/heuristic.py
+++ tinygrad/codegen/opt/heuristic.py
@@ -93,17 +93,6 @@ def hand_coded_optimizations(k:Scheduler) -> Scheduler:
 
   # **** below this line need to be optional and benchmarked ****
 
-  # if there are small dims with lots of valid masks, upcast them (they might be from Tensor.stack)
-  to_upcast: list[int] = []
-  # upcast leading axes first (hack-ish for winograd; we actually want to upcast masked axes with low stride first)
-  for axis in k.upcastable_dims:
-    # for Schedule, we check if the range is used in INDEX gates or WHERE gates
-    is_masked = any(any(o is k.rngs[axis] for o in u.src[0].backward_slice) for u in k.ast.backward_slice if u.op is Ops.WHERE)
-    if k.full_shape[axis] <= 7 and is_masked and prod(k.full_shape[j] for j in to_upcast) * k.full_shape[axis] <= 7 * 7:
-      if DEBUG >= 4: print(f"upcasting masked axis : {axis}")
-      to_upcast.append(axis)
-  for axis in to_upcast[::-1]: k.apply_opt(Opt(OptOps.UPCAST, axis, 0))
-
   # potentially do more upcasts of non reduce axes based on a heuristic
   is_dsp = k.ren is not None and k.ren.device == "DSP"
   upcasted_axis: set[int] = set()
