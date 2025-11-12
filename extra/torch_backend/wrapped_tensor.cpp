d86befe89de5ef1e2cb7bb48a4117a46aa972880
Duy Le <71242963+duyle-py@users.noreply.github.com>
2025-11-12 09:59:10 +0700

    fix: update stride calculation in wrap_tensor function to handle sizes correctly

--- extra/torch_backend/wrapped_tensor.cpp
+++ extra/torch_backend/wrapped_tensor.cpp
@@ -126,8 +126,10 @@ at::Tensor wrap_tensor(py::object &py_obj, c10::ScalarType dtype, c10::DeviceInd
     strides.resize(sizes.size());
     int64_t stride = 1;
     for (int i = sizes.size() - 1; i >= 0; i--) {
-      strides[i] = sizes[i] == 1 ? 0 : stride;
-      stride *= sizes[i];
+        strides[i] = stride;
+        if (sizes[i] > 1) {
+            stride *= sizes[i];
+        }
     }
   }
 
