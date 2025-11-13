759557f63328e3f8c0fd00d6a7d89ae3127e257f
wozeparrot <wozeparrot@gmail.com>
2025-11-12 17:06:53 -0800

    feat: move tk tests to testextra (#13242)

--- test/testextra/test_tk.py
+++ test/testextra/test_tk.py
@@ -2,12 +2,17 @@ import unittest, math
 
 from tinygrad import Tensor, Device, dtypes, Context
 from tinygrad.engine.realize import ExecItem, get_runner
+from tinygrad.helpers import CI
+from tinygrad.renderer.ptx import PTXRenderer
 import numpy as np
 
 from extra.thunder.tiny.tk import WARP_THREADS
 from extra.thunder.tiny.tk.kernel import Kernel
 
+@unittest.skipUnless(Device.DEFAULT in ["CUDA", "NV"], "only cuda")
+@unittest.skipIf(isinstance(Device[Device.DEFAULT].renderer, PTXRenderer), "no ptx")
 class TestTK(unittest.TestCase):
+  @unittest.skipIf(CI, "no wmma in ci")
   def test_simple_matmul(self):
     N = 32
     BLOCK_SIZE = 16
@@ -58,6 +63,7 @@ class TestTK(unittest.TestCase):
 
     np.testing.assert_allclose(c.numpy(), ref.numpy())
 
+  @unittest.skipIf(CI, "no wmma in ci")
   def test_simple_matmul_transposed(self):
     N = 32
     BLOCK_SIZE = 16
@@ -334,6 +340,7 @@ class TestTK(unittest.TestCase):
 
     np.testing.assert_allclose(b.numpy(), ref.numpy(), atol=1e-5, rtol=1e-5)
 
+  @unittest.skip("fake range not ended")
   def test_softmax(self):
     N = 32
     BLOCK_SIZE = 16
@@ -389,10 +396,8 @@ class TestTK(unittest.TestCase):
     ei = ExecItem(get_runner(Device.DEFAULT, sink), [t.uop.buffer for t in (b, a)])
     for _ in range(5): ei.run(wait=True)
     b = b.float()
-    print(b.tolist())
 
     ref = a.float().softmax(axis=3)
-    print(ref.tolist())
 
     np.testing.assert_allclose(b.numpy(), ref.numpy(), atol=1e-5, rtol=1e-5)
 
