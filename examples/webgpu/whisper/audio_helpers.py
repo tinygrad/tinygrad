c5709d869e7943d593d0452626d48fd4a9e5fbfb
Irwin1138 <irwin1139@gmail.com>
2025-11-14 16:11:41 +0200

    cleanup

--- examples/webgpu/whisper/audio_helpers.py
+++ examples/webgpu/whisper/audio_helpers.py
@@ -11,7 +11,7 @@ def stft(x:Tensor, weight:Tensor, n_fft, stride, pad, pad_mode="constant")->Tens
   x_padded = x.pad(pad, mode=pad_mode)
   stft_raw = x_padded.unsqueeze(1).conv2d(weight, stride=stride)
 
-  # NOTE(irwin): magnitudes only atm
+  # magnitudes only atm
   magnitudes = (stft_raw[:, :cutoff, :]**2 + stft_raw[:, cutoff:, :]**2).sqrt()
   return magnitudes
 
@@ -45,8 +45,7 @@ def make_basis_buffers(N_FFT: int, k_freq_bin: int|Tensor, window:Tensor) -> tup
 
   w = window
   cos_basis = w * angle.cos()
-  # NOTE(irwin): negate sin_basis to match torch
-  sin_basis = w * -angle.sin()
+  sin_basis = w * -angle.sin() # negate sin_basis to match torch
   return cos_basis, sin_basis
 
 def stft_full(x:Tensor, n_fft:int, stride:int, pad:tuple[int, int], window="hann", pad_mode="constant") -> Tensor:
@@ -67,13 +66,13 @@ def fft_frequencies(sr:float, n_fft:int):
   return rfftfreq(n=n_fft, d=1.0 / sr)
 
 def hz_to_mel(freq:Tensor)->Tensor:
-  # Fill in the linear part
+  # linear part
   f_min = 0.0
   f_sp = 200.0 / 3
 
   mels = (freq - f_min) / f_sp
 
-  # Fill in the log-scale part
+  # log-scale part
 
   min_log_hz = 1000.0  # beginning of log region (Hz)
 
@@ -81,24 +80,23 @@ def hz_to_mel(freq:Tensor)->Tensor:
   return mask.where(((min_log_hz - f_min) / f_sp) + (freq / min_log_hz).log() / (Tensor(6.4).log() / 27.0), mels)
 
 def mel_to_hz(mels:Tensor)->Tensor:
-  # Fill in the linear scale
+  # linear scale
   f_min = 0.0
   f_sp = 200.0 / 3
   freqs = f_min + f_sp * mels
 
-  # And now the nonlinear scale
+  # nonlinear scale
   min_log_hz = 1000.0  # beginning of log region (Hz)
   min_log_mel = (min_log_hz - f_min) / f_sp  # same (Mels)
   logstep = Tensor(6.4).log() / 27.0  # step size for log region
 
-  # If we have vector data, vectorize
   log_t = mels >= min_log_mel
   freqs = log_t.where(min_log_hz * ((logstep * (mels - min_log_mel)).exp()), freqs)
 
   return freqs
 
 def mel_frequencies(n_mels: int = 128, *, fmin: float = 0.0, fmax: float = 11025.0)->Tensor:
-  # 'Center freqs' of mel bands - uniformly spaced between limits
+  # center freqs of mel bands - uniformly spaced between limits
   min_max_mel = hz_to_mel(Tensor([fmin, fmax]))
 
   mels = Tensor.linspace(min_max_mel[0], min_max_mel[1], n_mels)
@@ -118,15 +116,11 @@ def mel(
   if fmax is None:
     fmax = float(sr) / 2
 
-  # Initialize the weights
   n_mels = int(n_mels)
   weights = Tensor.zeros((n_mels, int(1 + n_fft // 2)), dtype=dtype).contiguous()
 
-  # Center freqs of each FFT bin
-  fftfreqs = fft_frequencies(sr=sr, n_fft=n_fft)
-
-  # 'Center freqs' of mel bands - uniformly spaced between limits
-  mel_f = mel_frequencies(n_mels + 2, fmin=fmin, fmax=fmax)
+  fftfreqs = fft_frequencies(sr=sr, n_fft=n_fft) # center freqs of each FFT bin
+  mel_f = mel_frequencies(n_mels + 2, fmin=fmin, fmax=fmax) # center freqs of mel bands
 
   fdiff = mel_f[1:] - mel_f[:-1]
   ramps = mel_f[None].T.expand(-1, fftfreqs.shape[-1]) - fftfreqs
@@ -144,16 +138,17 @@ def mel(
 def resample(x, L, M, num_taps=64):
   fc = 0.5 / max(L, M)
   t = Tensor.arange(-num_taps//2, num_taps//2 + 1)
-  # NOTE(irwin): poor man's sinc
+
+  # poor man's sinc
   h:Tensor = (t * fc * math.pi).sin() / (t * fc * math.pi)
-  # hack fix NaN
-  h[num_taps//2] = 1.0
-  h *= 0.54 - 0.46 * (2 * math.pi * (t + num_taps//2) / num_taps).cos()  # hamming
+  h[num_taps//2] = 1.0 # hack fix NaN
+
+  h *= 0.54 - 0.46 * (2 * math.pi * (t + num_taps//2) / num_taps).cos() # hamming
   h /= h.sum()
-  # TODO(irwin): contiguous sped up things before replacing decimation with stride=M, retest if still relevant
+
   upsampled = x.reshape(-1, 1, x.shape[-1]).pad((None, (0, L-1), None)).transpose(1, 2).flatten(1).unsqueeze(1)
-  # upsampled = xx.cat(Tensor.zeros(L-1, *x.shape)).T.flatten().contiguous()
-  padding = (len(h) // 2)
+
+  padding = len(h) // 2
   filtered = upsampled.conv2d(h.reshape(1, 1, -1), stride=M, padding=padding).flatten(1)
   return filtered
 
@@ -166,9 +161,7 @@ def resample2(samples, source, target):
   gcd = math.gcd(source, target)
   M = source // gcd
   L = target // gcd
-  # NOTE(irwin): overkill but works
-  taps = next_power_of_2(max(M, L))*2
-  #print(M, L, taps)
+  taps = next_power_of_2(max(M, L))*2 # overkill but works
   return resample(samples, L, M, taps)
 
 def resample_batched(samples, source, target):
