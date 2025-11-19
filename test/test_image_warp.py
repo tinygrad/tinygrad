a40f80fd06036fa0c17b2bc5d5012122ce356f05
Bruce Wayne <harald.the.engineer@gmail.com>
2025-11-19 14:44:04 -0800

    fix test architecture

--- test/test_image_warp.py
+++ test/test_image_warp.py
@@ -56,32 +56,33 @@ def warp_perspective_numpy(src, M_inv):
   dst[dst_y, dst_x] = src[src_y, src_x]
   return dst.ravel()
 
-@unittest.skipIf(Device.DEFAULT == "WEBGPU", "Precision error")
-class TestImageWarpJit(unittest.TestCase):
-  update_img_jit = TinyJit(warp_perspective_tinygrad, prune=True)
+class TestImageWarp(unittest.TestCase):
+  @unittest.skipIf(Device.DEFAULT == "WEBGPU", "Precision error")
+  def test_image_warp_jit(self):
+    update_img_jit = TinyJit(warp_perspective_tinygrad, prune=True)
 
-  # run 20 times
-  step_times = []
-  for _ in range(20):
-    inputs = [(32*Tensor.randn(H,W) + 128).cast(dtype='uint8').contiguous().realize(),
-                  Tensor.randn(3,3).contiguous().realize()]
-    Device.default.synchronize()
-    inputs_np = [x.numpy() for x in inputs]
+    # run 20 times
+    step_times = []
+    for _ in range(20):
+      inputs = [(32*Tensor.randn(H,W) + 128).cast(dtype='uint8').contiguous().realize(),
+                    Tensor.randn(3,3).contiguous().realize()]
+      Device.default.synchronize()
+      inputs_np = [x.numpy() for x in inputs]
 
-    # do warp
-    st = time.perf_counter()
-    out = update_img_jit(*inputs)
-    mt = time.perf_counter()
-    Device.default.synchronize()
-    et = time.perf_counter()
-    step_times.append((et-st)*1e3)
-    print(f"enqueue {(mt-st)*1e3:6.2f} ms -- total run {step_times[-1]:6.2f} ms")
-    out_np = warp_perspective_numpy(*inputs_np)
+      # do warp
+      st = time.perf_counter()
+      out = update_img_jit(*inputs)
+      mt = time.perf_counter()
+      Device.default.synchronize()
+      et = time.perf_counter()
+      step_times.append((et-st)*1e3)
+      print(f"enqueue {(mt-st)*1e3:6.2f} ms -- total run {step_times[-1]:6.2f} ms")
+      out_np = warp_perspective_numpy(*inputs_np)
 
-    mismatch = np.abs(out.numpy() - out_np) > 0
-    mismatch_percent = sum(mismatch.flatten()) / len(mismatch.flatten()) * 100
-    mismatch_percent_tol = 1e-2
-    assert mismatch_percent < mismatch_percent_tol, f"input mismatch percent {mismatch_percent} exceeds tolerance {mismatch_percent_tol}"
+      mismatch = np.abs(out.numpy() - out_np) > 0
+      mismatch_percent = sum(mismatch.flatten()) / len(mismatch.flatten()) * 100
+      mismatch_percent_tol = 1e-2
+      assert mismatch_percent < mismatch_percent_tol, f"input mismatch percent {mismatch_percent} exceeds tolerance {mismatch_percent_tol}"
 
 
 if __name__ == '__main__':
