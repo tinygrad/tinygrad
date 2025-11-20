88a416a622c31347a39c9fa40f4b407e2ab1359c
qazal <qazal.software@gmail.com>
2025-11-20 11:17:24 +0200

    can ctrlc it

--- test/external/external_benchmark_pyrender.py
+++ test/external/external_benchmark_pyrender.py
@@ -1,15 +1,9 @@
 # benchmark speed of pyrender for all created UOps saved with TRACK_MATCH_STATS=2
 import functools, pickle, time
 from tinygrad.uop.ops import UOp, Ops
-from tinygrad.helpers import tqdm, temp, time_to_str
+from tinygrad.helpers import tqdm, temp, time_to_str, cpu_profile
 
 BENCHMARK_OPS = {Ops.INDEX, Ops.BUFFERIZE}
-
-def time_render(u:UOp) -> tuple[str, int]:
-  st = time.perf_counter()
-  try: ren = u.render()
-  except Exception: return "PYRENDER_ERR", -1
-  return ren, time.perf_counter()-st
 
 @functools.cache
 def create_uop(a:int) -> UOp:
@@ -23,14 +17,18 @@ if __name__ == "__main__":
 
   # benchmark
   result:list[tuple[str, int]] = []
-  for steps in tqdm(trace.rewrites):
-    for r in steps:
-      for _,yn,_,__ in r.matches:
-        y = create_uop(yn)
-        if y.op in BENCHMARK_OPS:
-          result.append(time_render(y))
-
-  N = 10
-  print(f"Slowst {N} renders from {len(result)} samples:")
-  for ren,tm in sorted(result, key=lambda x:x[1], reverse=True)[:N]:
-    print(f"{time_to_str(tm).strip():<10s} {ren}")
+  try:
+    for steps in tqdm(trace.rewrites):
+      for r in steps:
+        for _,yn,_,__ in r.matches:
+          y = create_uop(yn)
+          if y.op in BENCHMARK_OPS:
+            with cpu_profile("pyrender") as e:
+              try: ren = y.render()
+              except Exception: ren = "PYRENDER_ERR"
+            result.append((ren, float(e.en-e.st)/1e6))
+  finally:
+    N = 10
+    print(f"Slowst {N} renders from {len(result)} samples:")
+    for ren,tm in sorted(result, key=lambda x:x[1], reverse=True)[:N]:
+      print(f"{time_to_str(tm).strip():<10s} {ren}")
