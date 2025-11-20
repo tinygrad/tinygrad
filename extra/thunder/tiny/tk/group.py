67b846ea4a92128fa3cd7fb5024746586d3a002d
Woze Parrot <wozeparrot@gmail.com>
2025-11-20 03:58:33 +0000

    feat: flat registers and register to global stores

--- extra/thunder/tiny/tk/group.py
+++ extra/thunder/tiny/tk/group.py
@@ -34,7 +34,7 @@ class Group:
     rngs_for_shape = tuple(UOp.range(dim, Group.clear_rid + i) for i, dim in enumerate(reg.shape))
     Group.clear_rid += len(reg.shape)
 
-    reg_store = reg[*rngs_for_shape].store(UOp.const(reg.dtype.base, value)).end(*rngs_for_shape)
+    reg_store = reg[*rngs_for_shape].store(value).end(*rngs_for_shape)
 
     self.ker.push_store(reg_store, reg)
     return reg.after(reg_store).reshape(reg.shape)
@@ -62,15 +62,16 @@ class Group:
 
     wmma_arg = ('WMMA_16_16_16___bf16_float', (16, 16, 16), dtypes.bfloat16, dtypes.float, 'AMD', 64, (((4, 2), (3, 2)), ((4, 2), (3, 2)), ((4, 2), (3, 2))), ())
 
-    for height in self.ker.range(c.shape[-2], track=False):
-      for width in self.ker.range(c.shape[-1], track=False):
-        for inner in self.ker.range(a.shape[-1], AxisType.REDUCE, track=False):
-          a_in = a[height, inner]
-          b_in = b[inner, width]
-          d_in = c[height, width]
+    for height in self.ker.range(c.shape[-3], track=False):
+      for width in self.ker.range(c.shape[-2], track=False):
+        for inner in self.ker.range(a.shape[-2], AxisType.REDUCE, track=False):
+          a_in = UOp.vectorize(*[a[height, inner, i] for i in range(4)])
+          b_in = UOp.vectorize(*[b[inner, width, i] for i in range(4)])
+          d_in = UOp.vectorize(*[c[height, width, i] for i in range(4)])
 
           out = UOp(Ops.WMMA, dtypes.float32.vec(4), (a_in, b_in, d_in), arg=wmma_arg)
-          c_store = c[height, width].store(out).end(height, width, inner)
+          c_i = [c[height, width, i].store(out.gep(i)) for i in range(4)]
+          c_store = UOp.group(*c_i).end(height, width, inner)
 
     self.ker.push_store(c_store, c)
     return c.after(c_store).reshape(c.shape)
@@ -81,9 +82,9 @@ class Group:
 
     wmma_arg = ('WMMA_16_16_16___bf16_float', (16, 16, 16), dtypes.bfloat16, dtypes.float, 'AMD', 64, (((4, 2), (3, 2)), ((4, 2), (3, 2)), ((4, 2), (3, 2))), ())
 
-    for height in self.ker.range(c.shape[-2], track=False):
-      for width in self.ker.range(c.shape[-1], track=False):
-        for inner in self.ker.range(a.shape[-1], AxisType.REDUCE, track=False):
+    for height in self.ker.range(c.shape[-3], track=False):
+      for width in self.ker.range(c.shape[-2], track=False):
+        for inner in self.ker.range(a.shape[-2], AxisType.REDUCE, track=False):
           a_in = UOp.vectorize(*[a[height, inner, i] for i in range(4)])
           b_in = UOp.vectorize(*[b[width, inner, i] for i in range(4)])
           d_in = UOp.vectorize(*[c[height, width, i] for i in range(4)])
@@ -160,39 +161,29 @@ class Group:
     assert isinstance(dst.dtype, PtrDType) and isinstance(src.dtype, PtrDType)
     dst_dtype, src_dtype = cast(PtrDType, dst.dtype), cast(PtrDType, src.dtype)
     if dst_dtype.addrspace == AddrSpace.REG and src_dtype.addrspace == AddrSpace.LOCAL:
-      srcf = src.flatten(-2)
+      laneid = self.ker.laneid
+      rt_layout = cast(RT, dst).layout
+      elements_per_thread = rt_layout.elements_per_thread
 
-      if self.warps % 4 == 0: local_warpid = (self.warpid // 4) + (self.warpid % 4) * (self.warps // 4)
-      else: local_warpid = self.warpid
-      warp_laneid = self.threadIdx_x % WARP_THREADS
-      elements_per_thread = cast(ST, src).layout.elements_per_thread
-
-      for height in self.ker.range(dst.shape[-2], track=False):
-        for width in self.ker.range(dst.shape[-1], track=False):
+      for height in self.ker.range(dst.shape[-3], track=False):
+        for width in self.ker.range(dst.shape[-2], track=False):
           for inner in self.ker.range(elements_per_thread, AxisType.UPCAST, track=False):
-            base_row = (local_warpid * dst.shape[-2] + height) * cast(RT, dst).layout.rows
-            base_col = width * cast(RT, dst).layout.cols
+            base_row = height * rt_layout.rows
+            base_col = width * rt_layout.cols
 
             if not transpose:
-              row = warp_laneid % 16
-              col = 4 * (warp_laneid // 16)
+              row = laneid % rt_layout.rows
+              col = rt_layout.stride * (laneid // rt_layout.rows) + inner
             else:
-              row = 4 * (warp_laneid // 16)
-              col = warp_laneid % 16
+              row = rt_layout.stride * (laneid // rt_layout.cols) + inner
+              col = laneid % rt_layout.cols
 
             srow, scol = cast(ST, src).swizzle(row, col)
             srow, scol = base_row + srow, base_col + scol
-            src_i_last = srow * src.shape[-1] + scol
-            if not transpose:
-              src_i_last += inner
-            else:
-              src_i_last += inner * src.shape[-1]
 
-            dst_store = dst[*dst_idxs, height, width].store(srcf[*idxs[:-2], src_i_last].contract(inner))
-            dst_store = dst_store.end(height, width)
+            dst_store = dst[*dst_idxs, height, width, inner].store(src[*idxs[:-2], srow, scol])
+            dst_store = dst_store.end(height, width, inner)
     elif dst_dtype.addrspace == AddrSpace.LOCAL and src_dtype.addrspace == AddrSpace.GLOBAL:
-      dstf = dst.flatten(-2)
-
       srcf = src.flatten()
       row_stride = prod(src.shape[axis+1:])
 
@@ -208,14 +199,13 @@ class Group:
         for inner in self.ker.range(elements_per_thread, AxisType.UPCAST, track=False):
           load_idx = outer * self.group_threads + self.laneid
           row = load_idx // memcpy_per_row
-          col = (load_idx * elements_per_thread) % dst.shape[-1]
+          col = (load_idx * elements_per_thread) % dst.shape[-1] + inner
 
           srow, scol = cast(ST, dst).swizzle(row, col)
-          dst_i = srow * dst.shape[-1] + scol + inner
 
-          src_i += row * row_stride + col + inner
+          src_i += row * row_stride + col
 
-          dst_store = dstf[*dst_idxs, dst_i].store(srcf[src_i]).end(outer, inner)
+          dst_store = dst[*dst_idxs, srow, scol].store(srcf[src_i]).end(outer, inner)
     else:
       raise NotImplementedError(f"load from {src_dtype.addrspace} to {dst_dtype.addrspace} not implemented")
 
@@ -226,38 +216,28 @@ class Group:
     assert isinstance(dst.dtype, PtrDType) and isinstance(src.dtype, PtrDType)
     dst_dtype, src_dtype = cast(PtrDType, dst.dtype), cast(PtrDType, src.dtype)
     if src_dtype.addrspace == AddrSpace.REG and dst_dtype.addrspace == AddrSpace.LOCAL:
-      dstf = dst.flatten(-2)
+      laneid = self.ker.laneid
+      rt_layout = cast(RT, src).layout
+      elements_per_thread = rt_layout.elements_per_thread
 
-      if self.warps % 4 == 0: local_warpid = (self.warpid // 4) + (self.warpid % 4) * (self.warps // 4)
-      else: local_warpid = self.warpid
-      warp_laneid = self.threadIdx_x % WARP_THREADS
-      elements_per_thread = cast(ST, src).layout.elements_per_thread
+      for height in self.ker.range(src.shape[-3], track=False):
+        for width in self.ker.range(src.shape[-2], track=False):
+          for inner in self.ker.range(elements_per_thread, track=False):
+            base_row = height * rt_layout.rows
+            base_col = width * rt_layout.cols
 
-      for height in self.ker.range(src.shape[-2], track=False):
-        for width in self.ker.range(src.shape[-1], track=False):
-          base_row = (local_warpid * src.shape[-2] + height) * cast(RT, dst).layout.rows
-          base_col = width * cast(RT, dst).layout.cols
-
-          if not transpose:
-            row = warp_laneid % 16
-            col = 4 * (warp_laneid // 16)
-          else:
-            row = 4 * (warp_laneid // 16)
-            col = warp_laneid % 16
-
-          srow, scol = cast(ST, dst).swizzle(row, col)
-          srow, scol = base_row + srow, base_col + scol
-          dst_i_last = srow * dst.shape[-1] + scol
-
-          src_load = src[*src_idxs, height, width]
-          dst_stores = []
-          for inner in range(elements_per_thread):
             if not transpose:
-              dst_stores.append(dstf[*idxs[:-2], dst_i_last + inner].store(src_load.gep(inner)))
+              row = laneid % rt_layout.rows
+              col = rt_layout.stride * (laneid // rt_layout.rows) + inner
             else:
-              dst_stores.append(dstf[*idxs[:-2], dst_i_last + inner * dst.shape[-1]].store(src_load.gep(inner)))
-          dst_store = UOp.group(*dst_stores)
-          dst_store = dst_store.end(height, width)
+              row = rt_layout.stride * (laneid // rt_layout.cols) + inner
+              col = laneid % rt_layout.cols
+
+            srow, scol = cast(ST, dst).swizzle(row, col)
+            srow, scol = base_row + srow, base_col + scol
+
+            dst_store = dst[*idxs[:-2], srow, scol].store(src[*src_idxs, height, width, inner])
+            dst_store = dst_store.end(height, width, inner)
     elif src_dtype.addrspace == AddrSpace.LOCAL and dst_dtype.addrspace == AddrSpace.GLOBAL:
       dstf = dst.flatten()
       row_stride = prod(dst.shape[axis+1:])
@@ -265,8 +245,6 @@ class Group:
       idxs = tuple(idx * src.shape[-2] if i == axis else idx for i, idx in enumerate(idxs))
       idxs = tuple(idx * src.shape[-1] if i == 3 else idx for i, idx in enumerate(idxs))
       dst_i = ((idxs[0] * dst.shape[-3] + idxs[1]) * dst.shape[-2] + idxs[2]) * dst.shape[-1] + idxs[3]
-
-      srcf = src.flatten(-2)
 
       elements_per_thread = cast(ST, src).layout.elements_per_thread
       memcpy_per_row = src.shape[-1] // elements_per_thread
@@ -276,14 +254,43 @@ class Group:
         for inner in self.ker.range(elements_per_thread, AxisType.UPCAST, track=False):
           load_idx = outer * self.group_threads + self.laneid
           row = load_idx // memcpy_per_row
-          col = (load_idx * elements_per_thread) % src.shape[-1]
+          col = (load_idx * elements_per_thread) % src.shape[-1] + inner
 
           srow, scol = cast(ST, src).swizzle(row, col)
-          src_i = srow * src.shape[-1] + scol + inner
 
-          dst_i += row * row_stride + col + inner
+          dst_i += row * row_stride + col
 
-          dst_store = dstf[dst_i].store(srcf[*src_idxs, src_i]).end(outer, inner)
+          dst_store = dstf[dst_i].store(src[*src_idxs, srow, scol]).end(outer, inner)
+    elif src_dtype.addrspace == AddrSpace.REG and dst_dtype.addrspace == AddrSpace.GLOBAL:
+      dstf = dst.flatten()
+      row_stride = prod(dst.shape[axis+1:])
+
+      idxs = tuple(idx * src.shape[-2] if i == axis else idx for i, idx in enumerate(idxs))
+      idxs = tuple(idx * src.shape[-1] if i == 3 else idx for i, idx in enumerate(idxs))
+      dst_i = ((idxs[0] * dst.shape[-3] + idxs[1]) * dst.shape[-2] + idxs[2]) * dst.shape[-1] + idxs[3]
+
+      laneid = self.ker.laneid
+      rt_layout = cast(RT, src).layout
+      elements_per_thread = rt_layout.elements_per_thread
+
+      for height in self.ker.range(src.shape[-3], track=False):
+        for width in self.ker.range(src.shape[-2], track=False):
+          for inner in self.ker.range(elements_per_thread, track=False):
+            base_row = height * rt_layout.rows
+            base_col = width * rt_layout.cols
+
+            if not transpose:
+              row = laneid % rt_layout.rows
+              col = rt_layout.stride * (laneid // rt_layout.rows) + inner
+            else:
+              row = rt_layout.stride * (laneid // rt_layout.cols) + inner
+              col = laneid % rt_layout.cols
+
+            srow, scol = base_row + row, base_col + col
+
+            dst_i += srow * row_stride + scol
+
+            dst_store = dstf[dst_i].store(src[*src_idxs, height, width, inner]).end(height, width, inner)
     else:
       raise NotImplementedError(f"store from {src_dtype.addrspace} to {dst_dtype.addrspace} not implemented")
 
