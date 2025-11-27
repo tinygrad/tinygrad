e693093118ed7a7e4dd90f6199dbff80e34f1fff
George Hotz <geohot@gmail.com>
2025-11-26 16:52:51 -0800

    add DEBUG_GC

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -291,6 +291,20 @@ def cpu_profile(name:str|TracingKey, device="TINY", is_copy=False, display=True)
 def profile_marker(name:str, color="gray") -> None:
   cpu_events.append(ProfilePointEvent("TINY", "marker", None, {"name":name, "color":color}))
 
+if getenv("DEBUG_GC"):
+  gc_event: ProfileRangeEvent|None = None
+  def my_gc_callback(phase, stats):
+    global gc_event
+    if phase == 'start':
+      assert gc_event is None
+      gc_event = ProfileRangeEvent("GC", "collect", perf_counter_us())
+    elif phase == "stop":
+      assert gc_event is not None
+      gc_event.en = perf_counter_us()
+      cpu_events.append(gc_event)
+      gc_event = None
+  if PROFILE: gc.callbacks.append(my_gc_callback)
+
 # *** universal database cache ***
 
 cache_dir: str = os.path.join(getenv("XDG_CACHE_HOME", os.path.expanduser("~/Library/Caches" if OSX else "~/.cache")), "tinygrad")
