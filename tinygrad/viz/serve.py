ec9344c3a6ec903091b6ba514a3a1e6f4ceb88eb
qazal <qazal.software@gmail.com>
2025-12-04 20:52:47 +0200

    pmc comes before waves

--- tinygrad/viz/serve.py
+++ tinygrad/viz/serve.py
@@ -262,6 +262,20 @@ def load_sqtt(profile:list[ProfileEvent]) -> None:
     prg_cu = sorted(cu_events, key=row_tuple)
     if cu_events: summary.append(f"Scheduled on {len(prg_cu)} CUs")
     steps.append(create_step(program.name if program else key[0], ("/counters", len(ctxs), len(steps)), {"src":"\n\n".join(summary)}, depth=1))
+    # ** PMC events
+    if (pmc_event:=next((e for e in counters if isinstance(e, ProfilePMCEvent)), None)) is not None:
+      agg_cols = ["Name", "Sum"]
+      sample_cols = ["XCC", "INST", "SE", "SA", "WGP", "Value"]
+      rows:list[list] = []
+      view, ptr = memoryview(pmc_event.blob).cast('Q'), 0
+      for s in pmc_event.sched:
+        row:list = [s.name, 0, {"cols":sample_cols, "rows":[]}]
+        for sample in itertools.product(range(s.xcc), range(s.inst), range(s.se), range(s.sa), range(s.wgp)):
+          row[1] += (val:=int(view[ptr]))
+          row[2]["rows"].append(sample+(val,))
+          ptr += 1
+        rows.append(row)
+      steps.append(create_step("PMC", ("/pmc", len(ctxs), len(steps)), {"rows":rows, "cols":agg_cols, "summary":[]}, depth=2))
     for cu in prg_cu:
       events = [ProfilePointEvent(unit, "start", unit, ts=Decimal(0)) for unit in units]+cu_events[cu]
       steps.append(create_step(f"{cu} {len(cu_events[cu])}", ("/counters", len(ctxs), len(steps)),
@@ -269,20 +283,6 @@ def load_sqtt(profile:list[ProfileEvent]) -> None:
       for k in sorted(wave_insts.get(cu, []), key=row_tuple):
         data = wave_insts[cu][k]
         steps.append(create_step(k.replace(cu, ""), ("/sqtt-insts", len(ctxs), len(steps)), data, loc=data["loc"], depth=3))
-    # ** PMC events
-    if (pmc_event:=next((e for e in counters if isinstance(e, ProfilePMCEvent)), None)) is None: continue
-    agg_cols = ["Name", "Sum"]
-    sample_cols = ["XCC", "INST", "SE", "SA", "WGP", "Value"]
-    rows:list[list] = []
-    view, ptr = memoryview(pmc_event.blob).cast('Q'), 0
-    for s in pmc_event.sched:
-      row:list = [s.name, 0, {"cols":sample_cols, "rows":[]}]
-      for sample in itertools.product(range(s.xcc), range(s.inst), range(s.se), range(s.sa), range(s.wgp)):
-        row[1] += (val:=int(view[ptr]))
-        row[2]["rows"].append(sample+(val,))
-        ptr += 1
-      rows.append(row)
-    steps.append(create_step("PMC", ("/pmc", len(ctxs), len(steps)), {"rows":rows, "cols":agg_cols, "summary":[]}, depth=2))
   ctxs.append({"name":"Counters", "steps":steps})
 
 def device_sort_fn(k:str) -> tuple[int, str, int]:
