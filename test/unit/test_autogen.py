5c2849856cfa50b945a4b0bf6250be0d5bc346fa
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-12-05 07:07:07 -0800

    autogen: raise when unknown field is set

--- test/unit/test_autogen.py
+++ test/unit/test_autogen.py
@@ -110,4 +110,16 @@ class TestAutogen(unittest.TestCase):
     assert frts_cmd.readVbiosDesc.__class__ is FWSECLIC_READ_VBIOS_DESC
     assert frts_cmd.frtsRegionDesc.__class__ is FWSECLIC_FRTS_REGION_DESC
 
+  def test_unknown_field_error(self):
+    class MyStruct(Struct): pass
+    MyStruct._packed_ = True
+    MyStruct._fields_ = [('a', ctypes.c_int), ('b', ctypes.c_int)]
+    with self.assertRaises(AssertionError) as cm:
+      MyStruct(a=1, b=2, c=3)
+    with self.assertRaises(AssertionError) as cm:
+      MyStruct(A=1, b=2)
+    x = MyStruct(a=1, b=2)
+    with self.assertRaises(AssertionError) as cm:
+      x.c = 3
+
 if __name__ == "__main__": unittest.main()
