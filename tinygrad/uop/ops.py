bff8754513429bc4879699380ff540e10b1d73ef
Nino Risteski <ninoristeski@gmail.com>
2025-12-07 21:18:13 +0100

    reset shard

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -529,7 +529,6 @@ class UOp(OpMixin, metaclass=UOpMetaClass):
     if self.shape[axis] % dcount != 0: raise RuntimeError(f"multi axis uneven: {self.shape[axis]=} {axis=} {dcount=}")
     sz = self.shape[axis] // dcount
     return self.shrink(tuple((0,s) if i != axis else (dnum*sz,dnum*sz+sz) for i,s in enumerate(self.shape)))
-
   def shard(self, devices:tuple[str, ...], axis:int) -> UOp: return self.copy_to_device(devices)._shard(axis).multi(axis)
 
   # *** from LazyBuffer ***
