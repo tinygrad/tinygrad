1c16b6e0825784efffbf96a1badf24abf79f9173
Christopher Milan <chrismilan@ucla.edu>
2025-12-08 11:02:08 -0800

    Mesa: freedreno (#12746)
    * ir3 init
    
    * got a program
    
    * 1 + 1 works
    
    * use isa_disasm instead of shader_disasm
    
    * wip
    
    * matmul works
    
    * works on py3.14
    
    * fix const loading
    
    * skip QCOM failing tests
    
    * cleanup
    
    * args actually work
    
    * add compile-only tests
    
    * fix typo and install tinymesa
    
    * IR3 NULL backend
    
    * (float32) images work
    
    * autogen fix
    
    * fix compile only test
    
    * typo
    
    * mypy happy
    
    * compile-only uses py3.14
    
    * bump mesa
    
    * unify qcom disassembler
    
    * float16 works
    
    * disasm shows in viz
    
    * save a line
    
    * add real del
    
    * variable workgroup sizes
    
    * simplify diff
    
    * bump line count
    
    * properly set wgsz
    
    * regen mesa
    
    * no preamble
    
    * bump lines

--- tinygrad/runtime/ops_null.py
+++ tinygrad/runtime/ops_null.py
@@ -5,6 +5,8 @@ from tinygrad.renderer.cstyle import Renderer, CStyleLanguage
 from tinygrad.renderer.llvmir import AMDLLVMRenderer
 from tinygrad.uop.ops import Ops
 from tinygrad.helpers import cpu_profile, EMULATE
+from tinygrad.renderer.nir import IR3Renderer
+from tinygrad.runtime.support.compiler_mesa import IR3Compiler
 
 class NullRenderer(CStyleLanguage):
   device = "NULL"
@@ -37,4 +39,6 @@ class NullDevice(Compiled):
       case "AMD_RDNA4": renderer = functools.partial(AMDLLVMRenderer, "gfx1201")
       case "": renderer = NullRenderer
       case _: raise RuntimeError(f"can't EMULATE device: {EMULATE.value}")
-    super().__init__(device, NullAllocator(self), CompilerSet([CompilerPair(renderer, Compiler)]), functools.partial(NullProgram, device), NullGraph)
+    compilers = CompilerSet([CompilerPair(renderer, Compiler),
+                             CompilerPair(functools.partial(IR3Renderer, self), functools.partial(IR3Compiler, 0x6030001))]) # adreno 630
+    super().__init__(device, NullAllocator(self), compilers, functools.partial(NullProgram, device), NullGraph)
