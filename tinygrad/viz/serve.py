70a04ef656f616c4f685744f9cc0aa99e14333d9
qazal <qazal.software@gmail.com>
2025-12-15 18:36:57 +0900

    don't need that

--- tinygrad/viz/serve.py
+++ tinygrad/viz/serve.py
@@ -305,7 +305,7 @@ def get_profile(profile:list[ProfileEvent], sort_fn:Callable[[str], Any]=device_
   # start by getting the time diffs
   device_decoders:dict[str, Callable[[list[ProfileEvent]], None]] = {}
   for ev in profile:
-    if isinstance(ev, ProfileDeviceEvent) and ev.device not in device_ts_diffs:
+    if isinstance(ev, ProfileDeviceEvent):
       device_ts_diffs[ev.device] = (ev.comp_tdiff,ev.copy_tdiff if ev.copy_tdiff is not None else ev.comp_tdiff)
       if (d:=ev.device.split(":")[0]) == "AMD": device_decoders[d] = load_counters
   # load device specific counters
