e39464e933f393eb0a1e1370bb281ebd47bee017
qazal <qazal.software@gmail.com>
2025-12-16 06:43:45 -0800

    target a simpler init

--- extra/gemm/asm/test.py
+++ extra/gemm/asm/test.py
@@ -5,35 +5,23 @@ from tinygrad import Tensor, Device, dtypes
 from tinygrad.engine.realize import lower_schedule_item, ExecItem, CompiledRunner
 
 N = 8192
-
-# ** pre generate random inputs with torch
-
-root = pathlib.Path("/tmp/gemm_inputs")
-if not (root/"A").exists():
-  import torch
-  scale = 10.0
-  dtype = torch.bfloat16
-  torch.manual_seed(0)
-  A = (torch.randn(N, N, dtype=torch.float32, device='cpu') / scale).to(dtype)
-  B = (torch.randn(N, N, dtype=torch.float32, device='cpu') / scale).to(dtype)
-  C = A@B.t().contiguous()
-  A.contiguous().view(torch.uint16).cpu().numpy().tofile(root/"A")
-  B.contiguous().view(torch.uint16).cpu().numpy().tofile(root/"B")
-  C.contiguous().view(torch.uint16).cpu().numpy().tofile(root/"C")
-  print("Saved tensors to", root)
-
+scale = 10.0
 dtype = dtypes.bfloat16
-A = Tensor(np.fromfile(root/"A", dtype=np.uint16)).bitcast(dtype).reshape(N, N).contiguous().realize()
-B = Tensor(np.fromfile(root/"B", dtype=np.uint16)).bitcast(dtype).reshape(N, N).contiguous().realize()
-C = Tensor(np.fromfile(root/"C", dtype=np.uint16)).bitcast(dtype).reshape(N, N).contiguous().realize()
+
+Tensor.manual_seed(0)
+
+A = (Tensor.randn(N, N) / scale).cast(dtypes.bfloat16).contiguous()
+B = (Tensor.randn(N, N) / scale).cast(dtypes.bfloat16).contiguous()
+Bt = B.T.contiguous()
+Tensor.realize(A, B, Bt)
 
 # ** tinygrad gemm
 
-C_tiny = Tensor.matmul(A, B.T, dtype=dtypes.float32).cast(dtype)
+C_tiny = Tensor.matmul(A, Bt, dtype=dtypes.float32).cast(dtype)
 si = C_tiny.schedule()[-1]
 eis:list[ExecItem] = [lower_schedule_item(si)]
 
-C_asm = Tensor.empty_like(C)
+C_asm = Tensor.empty_like(A)
 C_asm.uop.buffer.allocate()
 
 # ** raw assembly gemm
