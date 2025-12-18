77191fb744b47cb71dcad732027aa7e6cf8c78e0
nimlgen <138685161+nimlgen@users.noreply.github.com>
2025-12-18 12:02:28 +0300

    hive_reset for mi350 (#13746)

--- extra/amdpci/hive_reset.py
+++ extra/amdpci/hive_reset.py
@@ -5,7 +5,7 @@ from tinygrad.runtime.support.system import System, PCIDevice, PCIDevImplBase
 from tinygrad.runtime.support.am.amdev import AMDev
 
 if __name__ == "__main__":
-  gpus = System.pci_scan_bus(0x1002, [(0xffff, [0x74a1])])
+  gpus = System.pci_scan_bus(0x1002, [(0xffff, [0x74a1, 0x75a0])])
   pcidevs = [PCIDevice(f"reset:{gpu}", gpu, bars=[0, 2, 5]) for gpu in gpus]
   amdevs = []
   with Context(DEBUG=2):
