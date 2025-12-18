c51893be6b19e5d716c60debe084ed128603d6da
qazal <qazal.software@gmail.com>
2025-12-18 02:42:57 -0800

    can viz this, validate with torch

--- extra/gemm/asm/hip/test.py
+++ extra/gemm/asm/hip/test.py
@@ -1,4 +1,4 @@
-import pathlib
+import pathlib, ctypes
 from tinygrad import Tensor, Device, dtypes
 from tinygrad.helpers import system, temp
 
@@ -24,21 +24,28 @@ A = (torch.randn(N, N, dtype=torch.float32, device='cpu') / scale).to(dtype)
 B = (torch.randn(N, N, dtype=torch.float32, device='cpu') / scale).to(dtype)
 ref_out = A@B.t().contiguous()
 
-# bitcast to uint16 since there's no bf16 on numpy
-A, B = [t.view(torch.uint16).numpy() for t in [A, B]]
+A = A.contiguous()
+B = B.contiguous()
 
-out = Tensor.empty(N, N, dtype=dtypes.uint16).uop.buffer.allocate()
-bufs = [b._buf for b in [out, Tensor(A).realize().uop.buffer, Tensor(B).realize().uop.buffer]]
+# ** tinygrad
+
+A_tiny = [Tensor.from_blob(A.data_ptr(), A.shape, dtype=dtypes.bfloat16, device="cpu").to(Device.DEFAULT).realize() for _ in range(2)]
+B_tiny = [Tensor.from_blob(B.data_ptr(), B.shape, dtype=dtypes.bfloat16, device="cpu").to(Device.DEFAULT).realize() for _ in range(2)]
+C_tiny = Tensor.matmul(A_tiny[0], B_tiny[0].T.contiguous(), dtype=dtypes.float32).cast(dtypes.bfloat16)
+C_asm = Tensor.empty_like(C_tiny)
 
 # ** run
 
 prg = dev.runtime(name, lib)
-et = prg(*bufs, global_size=[128, 86, 1], local_size=[256, 1, 1], wait=True)
+et = prg(*[b._buf for b in [C_asm.uop.buffer.ensure_allocated(), A_tiny[1].uop.buffer, B_tiny[1].uop.buffer]], global_size=[128, 86, 1], local_size=[256, 1, 1], wait=True)
 print(f"gemm finished in {et*1e3:9.2f} ms")
 
 # ** correctness
 
-import torch
-asm_out = torch.from_numpy(out.numpy()).view(torch.bfloat16).reshape(ref_out.shape)
-print(asm_out)
-assert torch.allclose(asm_out, ref_out, rtol=1e-2, atol=1e-3)
+def torch_bf16(t:Tensor):
+  asm_out = t.to("cpu").realize().uop.buffer._buf
+  buf = (ctypes.c_uint16*C_asm.uop.size).from_address(asm_out.va_addr)
+  return torch.frombuffer(buf, dtype=torch.bfloat16, count=C_asm.uop.size).reshape(C_asm.shape)
+
+assert torch.allclose(torch_bf16(C_asm), ref_out, rtol=1e-2, atol=1e-3)
+assert torch.allclose(torch_bf16(C_tiny), ref_out, rtol=1e-2, atol=1e-3)
