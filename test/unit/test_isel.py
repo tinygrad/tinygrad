678a6b36898a11f1a49f1fb1ef619d20b735b82a
ttomsa <tomasvsilva8@gmail.com>
2025-12-20 20:22:35 +0000

    cleanup test_isel

--- test/unit/test_isel.py
+++ test/unit/test_isel.py
@@ -5,18 +5,18 @@ from tinygrad.renderer.x86 import X86Renderer
 from tinygrad.renderer.isa import IselContext, Register
 from tinygrad import dtypes
 
-def isel_rewrite(x:UOp):
-  x = graph_rewrite(x, X86Renderer().pre_isel_matcher)
-  return graph_rewrite(x, X86Renderer().isel_matcher, IselContext(x), bottom_up=True)
-
 class TestIselX86(unittest.TestCase):
+  def isel_rewrite(self, x:UOp):
+    x = graph_rewrite(x, X86Renderer().pre_isel_matcher)
+    return graph_rewrite(x, X86Renderer().isel_matcher, IselContext(x), bottom_up=True)
+
   def test_cmove(self):
     a = UOp.variable("a", 0, 0, dtypes.int32)
     b = UOp.variable("b", 0, 0, dtypes.int32)
     c = (a < b).where(a, b)
     d = (a != b).where(a, b)
     f = c + d
-    n = isel_rewrite(f)
+    n = self.isel_rewrite(f)
     self.assertTrue(n.src[0].op is X86Ops.CMOVL and n.src[1].op is X86Ops.CMOVNE)
     # both comparisons become the same X86Ops.CMP
     self.assertTrue(n.src[0].src[2] == n.src[1].src[2] and n.src[0].src[2].op is X86Ops.CMP)
@@ -25,14 +25,14 @@ class TestIselX86(unittest.TestCase):
   def test_vshufps_same_src(self):
     a = UOp.variable("a", 0, 0, dtypes.float32.vec(4))
     vec = UOp(Ops.VECTORIZE, a.dtype, (a.gep(3), a.gep(2), a.gep(1), a.gep(0)))
-    n = isel_rewrite(vec)
+    n = self.isel_rewrite(vec)
     self.assertTrue(n.op is X86Ops.VSHUFPS and n.src[0] is a and n.src[1] is a and n.src[2].arg == 27)
 
   def test_vshufps_diff_src(self):
     a = UOp.variable("a", 0, 0, dtypes.float32.vec(4))
     b = UOp.variable("b", 0, 0, dtypes.float32)
     vec = UOp(Ops.VECTORIZE, a.dtype, (a.gep(2), a.gep(3), b, b))
-    n = isel_rewrite(vec)
+    n = self.isel_rewrite(vec)
     self.assertTrue(n.op is X86Ops.VSHUFPS and n.src[0] is a and n.src[1] is b and n.src[2].arg == 14)
 
   def test_vinsertps(self):
@@ -41,7 +41,7 @@ class TestIselX86(unittest.TestCase):
     c = UOp.variable("c", 0, 0, dtypes.float32.vec(4))
     d = UOp.variable("d", 0, 0, dtypes.float32)
     vec = UOp(Ops.VECTORIZE, dtypes.float32.vec(4), (a.gep(0), b.gep(0), c.gep(0), d))
-    n = isel_rewrite(vec)
+    n = self.isel_rewrite(vec)
     self.assertTrue(n.op is X86Ops.VINSERTPS and len(n.src) == 3)
     self.assertTrue(n.src[0].op is X86Ops.VINSERTPS and n.src[1] is d and n.src[2].arg == 48)
     n = n.src[0]
@@ -55,7 +55,7 @@ class TestIselX86(unittest.TestCase):
     offset = UOp.variable("a", 0, 0, dtypes.int32) + UOp.const(dtypes.int32, 1)
     index = UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0).index(offset, ptr=True)
     load = index.load()
-    n = isel_rewrite(load)
+    n = self.isel_rewrite(load)
     self.assertTrue(n.src[2].op is X86Ops.IMM and n.src[2].dtype is dtypes.int8)
 
   def test_fuse_index(self):
@@ -63,7 +63,7 @@ class TestIselX86(unittest.TestCase):
     offset = var + UOp.const(dtypes.int32, 1)
     index = UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0).index(offset, ptr=True)
     load = index.load()
-    n = isel_rewrite(load)
+    n = self.isel_rewrite(load)
     self.assertTrue(n.src[1] is var)
 
   # don't fuse when used multiple times
@@ -72,7 +72,7 @@ class TestIselX86(unittest.TestCase):
     index = UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0).index(offset, ptr=True)
     load = index.load()
     store = index.store(load)
-    n = isel_rewrite(store)
+    n = self.isel_rewrite(store)
     self.assertTrue(n.src[1].op is Ops.NOOP)
 
   def test_fuse_load(self):
@@ -80,7 +80,7 @@ class TestIselX86(unittest.TestCase):
     index = UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0).index(offset, ptr=True)
     load = index.load()
     add = offset + load
-    n = isel_rewrite(add)
+    n = self.isel_rewrite(add)
     self.assertTrue(len(n.src) == 4)
 
   # don't fuse when used multiple times
@@ -90,7 +90,7 @@ class TestIselX86(unittest.TestCase):
     load = index.load()
     add1 = offset + load
     add2 = add1 + load
-    n = isel_rewrite(add2)
+    n = self.isel_rewrite(add2)
     self.assertTrue(len(n.src) == 2)
 
   # TODO: get_consumer_map() uses dict causing this
@@ -100,7 +100,7 @@ class TestIselX86(unittest.TestCase):
     index = UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0).index(offset, ptr=True)
     load = index.load()
     add = load + load
-    n = isel_rewrite(add)
+    n = self.isel_rewrite(add)
     self.assertTrue(len(n.src) == 2)
 
   # test noop has same reg as src, this is because noops aren't instructions but still need to be part of the graph
@@ -108,7 +108,7 @@ class TestIselX86(unittest.TestCase):
   # by giving them the same reg as src we ensure they share the same live range
   def test_noop(self):
     noop = UOp(Ops.NOOP, dtypes.int32, (UOp(Ops.DEFINE_GLOBAL, dtypes.int32.ptr(), arg=0),))
-    n = isel_rewrite(noop)
+    n = self.isel_rewrite(noop)
     self.assertTrue(isinstance(n.arg, Register) and n.arg == n.src[0].arg)
 
   # TODO: don't use fmadd if uop used multiple times
