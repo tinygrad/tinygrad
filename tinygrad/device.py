863ce5874f9c7bfa2511a3dc3b7775b7b75002a3
George Hotz <geohot@gmail.com>
2025-12-22 18:49:16 -0500

    revert

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -319,10 +319,7 @@ class Compiled:
     for en, rc in self.comp_sets.values():
       if en is not None and en.value == 0 and rc in comps: comps.remove(rc)
 
-    ret = select_first_inited(list(forced_comps) if len(forced_comps)>0 else comps, f"No compiler for {self.device} is available", self.cached_pair)
-    # NOTE: some devices (e.g. DISK) don't specify compilers and get a default Renderer with empty device.
-    if not ret[0].device: ret[0].device = self.device
-    return ret
+    return select_first_inited(list(forced_comps) if len(forced_comps)>0 else comps, f"No compiler for {self.device} is available", self.cached_pair)
 
   def synchronize(self):
     """
