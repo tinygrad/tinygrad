a81b0673dc80ca02a90b02a3278ac7e2feec5381
Irwin1138 <irwin1139@gmail.com>
2025-12-24 16:18:30 +0200

    Revert "fix linter errors"
    This reverts commit 00aa943e91d6da0de309fcc26a231366878aa86a.

--- tinygrad/runtime/support/cuda.py
+++ tinygrad/runtime/support/cuda.py
@@ -1,4 +1,4 @@
-import os, platform
+import ctypes.util, os, platform
 from ctypes.util import find_library
 
 CUDA_PATH: str | None
@@ -10,8 +10,8 @@ if platform.system() == "Windows":
   def find_nv_dll(glob_pattern):
     cuda_bin = os.path.join(os.environ.get("CUDA_PATH", ""), "bin")
     matches = glob.glob(os.path.join(cuda_bin, glob_pattern))
-    return matches[0] if matches else None
-
+    return matches[0] if matches else None  
+  
   CUDA_PATH = find_library('nvcuda')
   NVRTC_PATH = find_nv_dll("nvrtc64_*.dll")
   NVJITLINK_PATH = find_nv_dll("nvJitLink_*.dll")
