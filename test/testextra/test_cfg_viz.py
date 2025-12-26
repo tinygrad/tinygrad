957ffcdb4aeccb19881ee6caee59f6a2970ace82
qazal <qazal.software@gmail.com>
2025-12-27 01:56:04 +0900

    labeled basic blocks also work

--- test/testextra/test_cfg_viz.py
+++ test/testextra/test_cfg_viz.py
@@ -55,7 +55,7 @@ amdhsa.kernels:
 
 @track_rewrites(name=lambda *args,ret,**kwargs: TracingKey(ret.name, ret=ret))
 def run_asm(name:str, insts:list) -> ProgramSpec:
-  src = "\n".join([inst.disasm() for inst in insts])
+  src = "\n".join([inst if isinstance(inst, str) else inst.disasm() for inst in insts])
   prg = ProgramSpec(name, template.replace("fn_name", name).replace("INSTRUCTION", textwrap.dedent(src)), Device.DEFAULT, UOp(Ops.SINK),
                     global_size=[1, 1, 1], local_size=[1, 1, 1], globals=[0])
   ei = ExecItem(UOp(Ops.SINK), [Tensor.empty(1).uop.buffer.ensure_allocated()], prg=CompiledRunner(prg))
@@ -71,80 +71,103 @@ class TestCfg(unittest.TestCase):
 
   def test_simple(self):
     run_asm("simple", [
-      s_branch(0),
+      s_branch("bb1"),
+      "bb1:",
       s_endpgm(),
     ])
 
   def test_diamond(self):
     run_asm("diamond", [
+      "entry:",
       s_cmp_eq_i32(s[0], 0),
-      s_cbranch_scc1(1),
-      s_branch(2),
+      s_cbranch_scc1("if"),
+      s_branch("else"),
+      "if:",
       s_nop(1),
-      s_branch(1),
+      s_branch("end"),
+      "else:",
       s_nop(0),
+      "end:",
       s_endpgm(),
     ])
 
   def test_loop(self):
     run_asm("simple_loop", [
+      "entry:",
       s_mov_b32(s[1], 4),
+      "loop:",
       s_add_u32(s[1], s[1], -1),
       s_cmp_eq_i32(s[1], 0),
-      s_cbranch_scc0(-3),
+      s_cbranch_scc0("loop"),
       s_endpgm(),
     ])
 
   def test_loop_branch(self):
     run_asm("loop_if", [
+      "entry:",
       s_mov_b32(s[1], 4),
+      "loop:",
       s_add_u32(s[1], s[1], -1),
       s_cmp_eq_i32(s[1], 2),
-      s_cbranch_scc1(1),
-      s_branch(1),
+      s_cbranch_scc1("cond"),
+      s_branch("cont"),
+      "cond:",
       s_add_u32(s[1], s[1], -2),
+      "cont:",
       s_cmp_eq_i32(s[1], 0),
-      s_cbranch_scc0(-7),
+      s_cbranch_scc0("loop"),
       s_endpgm(),
     ])
 
   def test_loop_break(self):
     run_asm("loop_break", [
+      "entry:",
       s_mov_b32(s[1], 8),
+      "loop:",
       s_add_u32(s[1], s[1], -1),
       s_cmp_eq_i32(s[1], 5),
-      s_cbranch_scc1(2),
+      s_cbranch_scc1("break"),
       s_cmp_eq_i32(s[1], 0),
-      s_cbranch_scc0(-5),
+      s_cbranch_scc0("loop"),
+      "break:",
       s_endpgm(),
     ])
 
   def test_switch(self):
     run_asm("switch_case", [
+      "entry:",
       s_cmp_eq_i32(s[0], 0),
-      s_cbranch_scc1(3),
+      s_cbranch_scc1("case0"),
       s_cmp_eq_i32(s[0], 1),
-      s_cbranch_scc1(3),
-      s_branch(4),
+      s_cbranch_scc1("case1"),
+      s_branch("case2"),
+      "case0:",
       s_nop(0),
-      s_branch(4),
+      s_branch("join"),
+      "case1:",
       s_nop(1),
-      s_branch(2),
+      s_branch("join"),
+      "case2:",
       s_nop(2),
-      s_branch(0),
+      s_branch("join"),
+      "join:",
       s_endpgm(),
     ])
 
   def test_ping_pong(self):
     run_asm("ping_pong", [
+      "entry:",
       s_cmp_eq_i32(s[0], 0),
-      s_cbranch_scc1(1),
-      s_branch(3),
+      s_cbranch_scc1("ping"),
+      s_branch("pong"),
+      "ping:",
       s_cmp_eq_i32(s[1], 0),
-      s_cbranch_scc1(1),
-      s_branch(2),
+      s_cbranch_scc1("pong"),
+      s_branch("end"),
+      "pong:",
       s_cmp_eq_i32(s[2], 0),
-      s_cbranch_scc1(-5),
+      s_cbranch_scc1("ping"),
+      "end:",
       s_endpgm(),
     ])
 
