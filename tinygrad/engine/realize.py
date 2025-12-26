3a90511cc6e4a3e8532def7d64ad06554d21badd
Chen-Yu Yang <chenyu@fastmail.com>
2025-12-26 11:17:49 -0500

    remove redudant check in disk_supports_fast_copyout

--- tinygrad/engine/realize.py
+++ tinygrad/engine/realize.py
@@ -73,7 +73,7 @@ class BufferCopy(Runner):
   def copy(self, dest, src):
     disk_supports_fast_copyout = src.device.startswith("DISK") and hasattr(src.allocator.dev, 'io_uring') and \
       getattr(src.allocator.dev, 'fd', None) is not None and dest.allocator.supports_copy_from_disk
-    if src.device.startswith("DISK") and hasattr(dest.allocator, 'copy_from_disk') and disk_supports_fast_copyout and src.nbytes >= 4096:
+    if disk_supports_fast_copyout and hasattr(dest.allocator, 'copy_from_disk') and src.nbytes >= 4096:
       dest.allocator.copy_from_disk(dest._buf, src._buf, src.nbytes)
     elif (src.device.startswith("DISK") or src.device.startswith("TINYFS")) and hasattr(dest.allocator, '_as_buffer'):
       # fast(ish) path, uses readinto in diskbuffers
