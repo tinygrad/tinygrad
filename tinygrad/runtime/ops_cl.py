98263c26c1a2407160f6a818b983084bac825470
Christopher Milan <chrismilan@ucla.edu>
2026-01-03 17:17:16 +0000

    cleanup

--- tinygrad/runtime/ops_cl.py
+++ tinygrad/runtime/ops_cl.py
@@ -55,8 +55,7 @@ class CLProgram:
       if isinstance(dt:=self.buf_dtypes[i], ImageDType):
         fmt = cl.cl_image_format(cl.CL_RGBA, {2:cl.CL_HALF_FLOAT, 4:cl.CL_FLOAT}[dt.itemsize])
         desc = cl.cl_image_desc(cl.CL_MEM_OBJECT_IMAGE2D, dt.shape[1], dt.shape[0], image_row_pitch=dt.pitch, buffer=b)
-        try: b = checked(cl.clCreateImage(self.dev.context, cl.CL_MEM_READ_WRITE, fmt, desc, None, status:=ctypes.c_int32()), status)
-        except RuntimeError: raise Exception(f"{dt=}, {dt.pitch=}")
+        b = checked(cl.clCreateImage(self.dev.context, cl.CL_MEM_READ_WRITE, fmt, desc, None, status:=ctypes.c_int32()), status)
       check(cl.clSetKernelArg(self.kernel, i, ctypes.sizeof(b), ctypes.byref(b)))
     for i,v in enumerate(vals,start=len(bufs)): check(cl.clSetKernelArg(self.kernel, i, 4, ctypes.byref(ctypes.c_int32(v))))
     if local_size is not None: global_size = cast(tuple[int,int,int], tuple(int(g*l) for g,l in zip(global_size, local_size)))
