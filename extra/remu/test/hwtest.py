b437fb51d4a51c11706755e6f2b9b2d8ad264aa3
qazal <qazal.software@gmail.com>
2026-01-03 10:52:47 +0900

    simpler asm tests

--- extra/remu/test/hwtest.py
+++ extra/remu/test/hwtest.py
@@ -3,9 +3,8 @@
 
 import numpy as np
 import unittest
-import subprocess, struct, math, textwrap, functools
-from tinygrad import Tensor, dtypes, Device, UOp
-from tinygrad.uop.ops import Ops, KernelInfo
+import subprocess, struct, math, functools
+from tinygrad import Tensor, dtypes, Device
 from tinygrad.helpers import getenv
 
 from extra.assembly.amd.autogen.rdna3.ins import *
@@ -15,7 +14,7 @@ from test.testextra.test_cfg_viz import asm_kernel
 
 def get_output(asm:list, n_threads:int=1, vdst:VGPR=v[1]):
   out = Tensor([0]*n_threads, dtype=dtypes.uint32).realize()
-  src = "\n".join(inst.disasm() for inst in [
+  insts = [
     s_load_b64(s[0:1], s[0:1], NULL),
     *asm,
     v_lshlrev_b32_e32(v[0], 2, v[0]),
@@ -23,9 +22,8 @@ def get_output(asm:list, n_threads:int=1, vdst:VGPR=v[1]):
     #global_store_b32(v[0], v[1], s[0:1]),
     global_store_b32(addr=v[0], data=vdst, saddr=s[0:1]),
     s_endpgm()
-  ])
-  src = template.replace("fn_name", "test").replace("INSTRUCTION", textwrap.dedent(src))
-  out = Tensor.custom_kernel(out, fxn=functools.partial(asm_kernel, src=src, device=out.device, n_threads=n_threads))[0]
+  ]
+  out = Tensor.custom_kernel(out, fxn=functools.partial(asm_kernel, name="test", insts=insts, device=out.device, n_threads=n_threads))[0]
   out.realize()
   return out.tolist()
 
