8b5ff403fa516ac3238dd79ac50b6a96a20b985c
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-01-11 16:24:53 +0300

    am: flag successful finalization (#14097)
    * am: flag successful finalization
    
    * import

--- tinygrad/runtime/support/am/ip.py
+++ tinygrad/runtime/support/am/ip.py
@@ -280,7 +280,6 @@ class AM_GFX(AM_IP):
         self._grbm_select(me=1, pipe=0, queue=q, inst=xcc)
         if self.adev.regCP_HQD_ACTIVE.read(inst=xcc) & 1: self.adev.regCP_HQD_DEQUEUE_REQUEST.write(0x2, inst=xcc) # 1 - DRAIN_PIPE; 2 - RESET_WAVES
         self._grbm_select(inst=xcc)
-    for xcc in range(self.xccs): self.adev.regGCVM_CONTEXT0_CNTL.write(0, inst=xcc)
 
   def setup_ring(self, ring_addr:int, ring_size:int, rptr_addr:int, wptr_addr:int, eop_addr:int, eop_size:int, idx:int, aql:bool) -> tuple[int, int]:
     pipe, queue, doorbell = idx // 4, idx % 4, am.AMDGPU_NAVI10_DOORBELL_MEC_RING0
