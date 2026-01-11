9522a20ac4d89afbe18e85055ed57723da892780
qazal <qazal.software@gmail.com>
2026-01-11 06:13:34 -0800

    cdna

--- extra/mmapeak/mmapeak.py
+++ extra/mmapeak/mmapeak.py
@@ -6,27 +6,30 @@ os.environ["AMD_AQL"] = "1"
 from tinygrad.device import Device
 from tinygrad.runtime.support.compiler_amd import HIPCompiler
 from tinygrad.runtime.ops_amd import AMDProgram
+from tinygrad.helpers import unwrap
 
 NUM_WORKGROUPS = 96
 WAVE_SIZE = 32
 NUM_WAVES = 2
 FLOPS_PER_MATMUL = 16*16*16*2
-INTERNAL_LOOP = 1_000_00
-INSTRUCTIONS_PER_LOOP = 200
+INTERNAL_LOOP = 1
+INSTRUCTIONS_PER_LOOP = 1
 DIRECTIVE = ".amdhsa_wavefront_size32 1"
+INST_ADD_I32 = None
 
 assemblyTemplate = (pathlib.Path(__file__).parent / "template.s").read_text()
 
 def launchBenchmark(instruction, vgprIndices, dense=True, accum=False, **kwargs):
   if accum:
-    instructions = instruction(a[0:vgprIndices[0]], v[vgprIndices[1]:vgprIndices[2]], v[vgprIndices[1]:vgprIndices[2]], **kwargs)
+    instructions = instruction(v[0:vgprIndices[0]], v[vgprIndices[1]:vgprIndices[2]], v[vgprIndices[1]:vgprIndices[2]], **kwargs)
   elif dense:
     instructions = instruction(v[0:vgprIndices[0]], v[vgprIndices[1]:vgprIndices[2]], v[vgprIndices[1]:vgprIndices[2]], 1)
   else:
     instructions = instruction(v[0:vgprIndices[0]], v[vgprIndices[1]:vgprIndices[2]], v[vgprIndices[3]:vgprIndices[4]], v[vgprIndices[5]])
 
   insts = [s_mov_b32(s[1], INTERNAL_LOOP), s_mov_b32(s[2], 0)]
-  insts += ["loop:"]+[instructions]*INSTRUCTIONS_PER_LOOP+[s_addk_co_i32(s[1], 0xffff), s_cmp_lg_i32(s[1], s[2]), s_cbranch_scc1("loop"), s_endpgm()]
+  insts += ["loop:"]+[instructions]*INSTRUCTIONS_PER_LOOP+[unwrap(INST_ADD_I32)(s[1], 0xffff), s_cmp_lg_i32(s[1], s[2]),
+                                                           s_cbranch_scc1("loop"), s_endpgm()]
   src = assemblyTemplate.replace("INSTRUCTIONS", "\n".join([i if isinstance(i, str) else i.disasm() for i in insts]))
   src = src.replace("DIRECTIVE", DIRECTIVE)
   lib = COMPILER.compile(src)
@@ -44,7 +47,8 @@ if __name__=="__main__":
 
   COMPILER = HIPCompiler(DEV.arch)
   if DEV.arch in {'gfx1100', 'gfx1103', 'gfx1151'}:
-    from extra.assembly.amd.autogen.rdna3 import *
+    from extra.assembly.amd.autogen.rdna3.ins import *
+    INST_ADD_I32 = s_add_i32
     if DEV.arch == 'gfx1103': NUM_WORKGROUPS = 8
     if DEV.arch == 'gfx1151': NUM_WORKGROUPS = 32
     launchBenchmark(v_wmma_bf16_16x16x16_bf16, (7,8,15))
@@ -55,6 +59,7 @@ if __name__=="__main__":
     launchBenchmark(v_wmma_i32_16x16x16_iu8, (7,8,11))
   elif DEV.arch in {'gfx1200', 'gfx1201'}:
     from extra.assembly.amd.autogen.rdna4.ins import *
+    INST_ADD_I32 = s_addk_co_i32
     NUM_WORKGROUPS = 64
     launchBenchmark(v_wmma_bf16_16x16x16_bf16, (3,4,7))
     launchBenchmark(v_wmma_f16_16x16x16_f16, (3,4,7))
@@ -81,7 +86,8 @@ if __name__=="__main__":
     FLOPS_PER_MATMUL = 16*16*64*2
     launchBenchmark(v_swmmac_i32_16x16x64_iu4, (7,8,9,10,13,14), False)
   elif DEV.arch == 'gfx950':
-    from extra.assembly.amd.autogen.cdna import *
+    from extra.assembly.amd.autogen.cdna.ins import *
+    INST_ADD_I32 = s_add_i32
     DIRECTIVE = ".amdhsa_accum_offset 4"
     NUM_WORKGROUPS = 256
     WAVE_SIZE = 64
