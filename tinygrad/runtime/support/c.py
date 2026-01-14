398adbb4947a4d8bcf39bf0830dcc6f1388eb260
Christopher Milan <chrismilan@ucla.edu>
2026-01-14 14:56:04 -0800

    del_an in init_c_struct_t

--- tinygrad/runtime/support/c.py
+++ tinygrad/runtime/support/c.py
@@ -124,7 +124,7 @@ class Field(property):
 @functools.cache
 def init_c_struct_t(sz:int, fields: tuple[tuple, ...]):
   CStruct = type("CStruct", (Struct,), {'_fields_': [('_mem_', ctypes.c_byte * sz)]})
-  for nm,*args in fields: setattr(CStruct, nm, Field(*args))
+  for nm,ty,*args in fields: setattr(CStruct, nm, Field(del_an(ty), *args))
   return CStruct
 def init_c_var(ty, creat_cb): return (creat_cb(v:=del_an(ty)()), v)[1]
 
