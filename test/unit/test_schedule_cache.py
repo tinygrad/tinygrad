708ac6d726ff022fe857c8a8d859d245479785a8
qazal <qazal.software@gmail.com>
2026-01-15 13:59:54 +0900

    simplest failing test

--- test/unit/test_schedule_cache.py
+++ test/unit/test_schedule_cache.py
@@ -1,6 +1,11 @@
 import unittest
-from tinygrad import Tensor, Variable
+import functools
+from tinygrad import Tensor, Variable, UOp
+from tinygrad.uop.ops import KernelInfo
 from tinygrad.engine.schedule import schedule_cache
+
+def custom_set0_kernel(A:UOp, num:int) -> UOp:
+  return A[0].set(num).sink(arg=KernelInfo(f"custom_set0_{num}"))
 
 class TestScheduleCache(unittest.TestCase):
   def test_bound_variable_reuses_cache(self):
@@ -25,6 +30,13 @@ class TestScheduleCache(unittest.TestCase):
     t = x + Tensor(v.bind(42))
     _, var_vals = t.schedule_with_vars()
     self.assertEqual(var_vals, {'pos': 42})
+
+  def test_custom_kernel(self):
+    for i in range(4):
+      a = Tensor.empty(1)
+      a = Tensor.custom_kernel(a, fxn=functools.partial(custom_set0_kernel, num=i))[0]
+      a.realize()
+      self.assertEqual(a.item(), i)
 
   def test_simple(self):
     a = Tensor.ones(10).contiguous()
