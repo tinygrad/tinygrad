ec9319479e1a56aaa96038407641a9679f16c005
qazal <qazal.software@gmail.com>
2026-01-16 15:12:33 -0800

    viz: show dimmer ALT/OTHER packets

--- tinygrad/viz/serve.py
+++ tinygrad/viz/serve.py
@@ -290,14 +290,12 @@ def sqtt_timeline(e) -> list[ProfileEvent]:
     if len(ret) > 50_000: break
     if isinstance(p, INST):
       op_name = p.op.name if isinstance(p.op, InstOp) else f"0x{p.op:02x}"
-      # skip OTHER_* packets
-      if "OTHER" in op_name: continue
       name, width = (op_name, 10 if "BARRIER" in op_name else 1)
       # Wave SALU op, not to be confused with the global SALU
       if op_name == "SALU": name = "WAVE_SALU"
-      add(name, p, width=width)
+      add(name, p, width=width, idx=int("OTHER" in name))
     if isinstance(p, (VALUINST, IMMEDIATE)): add(p.__class__.__name__, p)
-    if isinstance(p, (VMEMEXEC, ALUEXEC)): add(str(p.src).split('.')[1], p)
+    if isinstance(p, (VMEMEXEC, ALUEXEC)): add((name:=str(p.src).split('.')[1]), p, idx=int("ALT" in name))
   return [ProfilePointEvent(r, "start", r, ts=Decimal(0)) for r in rows]+ret
 
 # ** SQTT OCC only unpacks wave start, end time and SIMD location
