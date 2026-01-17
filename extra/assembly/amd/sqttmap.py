ed3256624ccfd52b7c89ec9580e81b8d90034f36
qazal <qazal.software@gmail.com>
2026-01-17 14:59:51 +0900

    start inst trace

--- extra/assembly/amd/sqttmap.py
+++ extra/assembly/amd/sqttmap.py
@@ -1,31 +1,32 @@
 # maps trace packets to instructions.
 from extra.assembly.amd.sqtt import decode, print_packets
+from extra.assembly.amd.dsl import Inst
 from extra.assembly.amd.decode import decode_inst
 from tinygrad.runtime.support.elf import elf_loader
 
-def disasm_llvm(lib:bytes):
+def get_rocprof_output(sqtt, prg, target):
   from tinygrad.viz.serve import llvm_disasm
-  return llvm_disasm(target, lib)
+  from extra.sqtt.roc import decode as roc_decode
+  disasm = {addr+prg.base:inst_disasm for addr, inst_disasm in llvm_disasm(target, prg.lib).items()}
+  rctx = roc_decode([sqtt], {prg.name:disasm})
+  return rctx.inst_execs[(sqtt.kern, sqtt.exec_tag)], {k:v[0] for k,v in disasm.items()}
 
 def map_insts(data:bytes, lib:bytes):
+  # rocprof to compare
+  #rwaves, rpc_table = get_rocprof_output(e, prg, target)
+  #for inst in rwaves[0].unpack_insts(): print(rpc_table[inst.pc])
+
+  # get pc table
+  pc_table:dict[int, Inst] = {}
   image, sections, _ = elf_loader(lib)
   text = next((sh for sh in sections if sh.name == ".text"), None)
   assert text is not None, "no .text section found"
   text_off, text_size = text.header.sh_addr, text.header.sh_size
-
-  pc_table:dict[int, str] = {}
-
-  # disassemble all instructions in .text section
   offset = text_off
   while offset < text_off + text_size:
     inst = decode_inst(image[offset:])
     pc_table[offset] = inst
     offset += inst.size()
-
-  # compare llvm
-  llvm_pc_table = disasm_llvm(lib)
-  assert len(pc_table) == len(llvm_pc_table)
-  assert list(pc_table) == list(llvm_pc_table)
 
 if __name__ == "__main__":
   import sys, pickle
