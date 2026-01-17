1ae0d4e2fd0423ce04242aff04e6f548f228f608
qazal <qazal.software@gmail.com>
2026-01-17 16:52:24 +0900

    work

--- extra/assembly/amd/sqttmap.py
+++ extra/assembly/amd/sqttmap.py
@@ -2,6 +2,8 @@
 from extra.assembly.amd.sqtt import decode, print_packets, INST, VALUINST, IMMEDIATE, WAVESTART, WAVEEND, InstOp
 from extra.assembly.amd.dsl import Inst
 from extra.assembly.amd.decode import decode_inst
+from extra.assembly.amd.autogen.rdna3.ins import SOPP
+from extra.assembly.amd.autogen.rdna3.enum import SOPPOp
 from tinygrad.runtime.support.elf import elf_loader
 
 def get_rocprof_output(sqtt, prg, target):
@@ -14,6 +16,7 @@ def get_rocprof_output(sqtt, prg, target):
 def map_insts(data:bytes, lib:bytes):
   # rocprof to compare
   rwaves, rpc_table = get_rocprof_output(e, prg, target)
+  rwaves = {w.wave_id:w for w in rwaves}
 
   # map pcs to insts
   pc_map:dict[int, Inst] = {}
@@ -29,20 +32,26 @@ def map_insts(data:bytes, lib:bytes):
 
   rwaves_iter, rwaves_base = {}, {}
   wave_pc:dict[int, int] = {}
+  simd_sel = (0, 0)
   for p in decode(data):
-    if isinstance(p, WAVESTART):
+    if isinstance(p, WAVESTART) and (p.cu, p.simd) == simd_sel:
+      assert p.wave not in wave_pc
       wave_pc[p.wave] = 0
       rwaves_iter[p.wave] = rwaves[p.wave].unpack_insts()
       rwaves_base[p.wave] = next(rwaves[p.wave].unpack_insts()).pc
-    if isinstance(p, (INST, VALUINST, IMMEDIATE)) and "OTHER_" not in type(p).__name__:
+    if isinstance(p, WAVEEND) and (p.cu, p.simd) == simd_sel:
+      wave_pc.pop(p.wave)
+    if isinstance(p, INST) and ("OTHER_" not in p.op.name):
       inst = pc_map[pc:=wave_pc[p.wave]]
-      if isinstance(p, INST) and p.op == InstOp.JUMP:
+      # s_delay_alu doesn't get a packet
+      if isinstance(inst, SOPP) and inst.op is SOPPOp.S_DELAY_ALU: continue
+      if isinstance(inst, SOPP) and (inst.op is SOPPOp.S_BRANCH or (inst.op.name.startswith("S_CBRANCH") and p.op is InstOp.JUMP)):
         x = inst.simm16 & 0xffff
         wave_pc[p.wave] += inst.size() + (x - 0x10000 if x & 0x8000 else x)*4
       else:
         wave_pc[p.wave] += inst.size()
 
-      print(f"{pc:012X} {inst.disasm()}")
+      print(f"{pc:012X} {inst.disasm()} wave={p.wave}")
       if (ref:=next(rwaves_iter[p.wave], None)) is None and inst.disasm() in {"s_endpgm", "s_code_end"}: break
       rpc = ref.pc-rwaves_base[p.wave]
       assert rpc == pc, f"{rpc}:{rpc_table[ref.pc]} != {pc}:{inst.disasm()}"
