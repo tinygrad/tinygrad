708c6ac24e2f6baff708977b9e43b4c08913d064
qazal <qazal.software@gmail.com>
2026-01-17 15:29:07 +0900

    base tests pass

--- extra/assembly/amd/sqttmap.py
+++ extra/assembly/amd/sqttmap.py
@@ -1,5 +1,5 @@
 # maps trace packets to instructions.
-from extra.assembly.amd.sqtt import decode, print_packets
+from extra.assembly.amd.sqtt import decode, print_packets, INST, VALUINST, IMMEDIATE, WAVESTART, WAVEEND
 from extra.assembly.amd.dsl import Inst
 from extra.assembly.amd.decode import decode_inst
 from tinygrad.runtime.support.elf import elf_loader
@@ -13,11 +13,10 @@ def get_rocprof_output(sqtt, prg, target):
 
 def map_insts(data:bytes, lib:bytes):
   # rocprof to compare
-  #rwaves, rpc_table = get_rocprof_output(e, prg, target)
-  #for inst in rwaves[0].unpack_insts(): print(rpc_table[inst.pc])
+  rwaves, rpc_table = get_rocprof_output(e, prg, target)
 
-  # get pc table
-  pc_table:dict[int, Inst] = {}
+  # map pcs to insts
+  pc_map:dict[int, Inst] = {}
   image, sections, _ = elf_loader(lib)
   text = next((sh for sh in sections if sh.name == ".text"), None)
   assert text is not None, "no .text section found"
@@ -25,8 +24,22 @@ def map_insts(data:bytes, lib:bytes):
   offset = text_off
   while offset < text_off + text_size:
     inst = decode_inst(image[offset:])
-    pc_table[offset] = inst
+    pc_map[offset-text_off] = inst
     offset += inst.size()
+
+  rwaves_iter, rwaves_base = {}, {}
+  wave_pc:dict[int, int] = {}
+  for p in decode(data):
+    if isinstance(p, WAVESTART):
+      wave_pc[p.wave] = 0
+      rwaves_iter[p.wave] = rwaves[p.wave].unpack_insts()
+      rwaves_base[p.wave] = next(rwaves[p.wave].unpack_insts()).pc
+    if isinstance(p, (INST, VALUINST, IMMEDIATE)) and "OTHER_" not in type(p).__name__:
+      inst = pc_map[wave_pc[p.wave]]
+      if (ref:=next(rwaves_iter[p.wave], None)) is None and inst.disasm() in {"s_endpgm", "s_code_end"}: break
+      assert ref.pc-rwaves_base[p.wave] == wave_pc[p.wave]
+      #print(inst.disasm(), rpc_table[ref.pc])
+      wave_pc[p.wave] += inst.size()
 
 if __name__ == "__main__":
   import sys, pickle
