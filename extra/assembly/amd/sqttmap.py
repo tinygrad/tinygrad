2227c575e7603eaf591f7c965183ee6d1506f99f
qazal <qazal.software@gmail.com>
2026-01-17 19:03:12 -0800

    work

--- extra/assembly/amd/sqttmap.py
+++ extra/assembly/amd/sqttmap.py
@@ -6,6 +6,7 @@ from extra.assembly.amd.decode import decode_inst
 from extra.assembly.amd.autogen.rdna3.ins import SOPP, s_endpgm
 from extra.assembly.amd.autogen.rdna3.enum import SOPPOp
 from tinygrad.runtime.support.elf import elf_loader
+from tinygrad.helpers import DEBUG, colored
 
 def map_insts(data:bytes, lib:bytes) -> Iterator[tuple[PacketType, int, Inst]]:
   # map pcs to insts
@@ -22,19 +23,26 @@ def map_insts(data:bytes, lib:bytes) -> Iterator[tuple[PacketType, int, Inst]]:
 
   wave_pc:dict[int, int] = {}
   simd_sel = (0, 0)
-  def wave_focus(p) -> bool: return getattr(p, "cu", 0) == 0 and getattr(p, "simd", 0) == 0 and getattr(p, "wave", None) == 0
+  def wave_focus(p) -> bool: return getattr(p, "cu", 0) == 0 and getattr(p, "simd", 0) == 0 and getattr(p, "wave", None) is not None
   for p in decode(data):
     if not wave_focus(p): continue
-    print_packets([p])
+    if DEBUG >= 2: print_packets([p])
     if isinstance(p, WAVESTART):
       assert p.wave not in wave_pc
       wave_pc[p.wave] = 0
-      continue
-    if isinstance(p, WAVEEND): break
-    pc = wave_pc[p.wave]
-    inst = pc_map[pc]
-    wave_pc[p.wave] += inst.size()
-    yield (p, pc, inst)
+    elif isinstance(p, WAVEEND):
+      pc = wave_pc.pop(p.wave)
+      yield (p, pc, s_endpgm())
+    elif isinstance(p, INST) and p.op.name.startswith("OTHER_"): continue
+    elif isinstance(p, (VALUINST, INST, IMMEDIATE)):
+      inst = pc_map[pc:=wave_pc[p.wave]]
+      wave_pc[p.wave] += inst.size()
+      if DEBUG >= 2: print(f"{' '*29}{colored(inst.disasm(), 'BLACK')}")
+      if isinstance(inst, SOPP) and inst.op is SOPPOp.S_DELAY_ALU:
+        inst = pc_map[pc:=wave_pc[p.wave]]
+        wave_pc[p.wave] += inst.size()
+        if DEBUG >= 2: print(f"{' '*29}{colored(inst.disasm(), 'BLACK')}")
+      yield (p, pc, inst)
 
 def test_rocprof_inst_traces_match(sqtt, prg, target):
   from tinygrad.viz.serve import llvm_disasm
@@ -42,42 +50,45 @@ def test_rocprof_inst_traces_match(sqtt, prg, target):
   disasm = {addr+prg.base:inst_disasm for addr, inst_disasm in llvm_disasm(target, prg.lib).items()}
   rctx = roc_decode([sqtt], {prg.name:disasm})
   rwaves = rctx.inst_execs[(sqtt.kern, sqtt.exec_tag)]
-  rwaves_iter:dict[int, list[Iterator[InstExec]]] = {}
+  rwaves_iter:dict[int, list[Iterator[InstExec]]] = {} # wave unit (0-15) -> list of inst trace iterators for all executions on that unit
   for w in rwaves: rwaves_iter.setdefault(w.wave_id, []).append(w.unpack_insts())
-  rwaves_base = next(iter(disasm))
-  wave_n:dict[int, int] = {}
+  rwaves_base = next(iter(disasm)) # base program counter
 
-  total_waves = sum(len(v) for v in rwaves_iter.values())
-  insts = 0
-
+  passed_insts = 0
   for pkt, pc, inst in map_insts(sqtt.blob, prg.lib):
     rocprof_inst = next(rwaves_iter[pkt.wave][0])
     ref_pc = rocprof_inst.pc-rwaves_base
-    if inst.disasm() == "s_endpgm":
-      wave_n[pkt.wave] = wave_n.get(pkt.wave, 0)+1
-      last = list(rwaves_iter[pkt.wave].pop(0))
-      try:
-        assert len(last) == 1 and disasm[last[0].pc][0] == "s_endpgm"
-      except AssertionError as e:
-        print("-- not in pkt")
-        for l in last: print(l)
-        raise e
-    else:
-      assert pkt._time == rocprof_inst.time+rocprof_inst.stall
-      assert ref_pc == pc, f"pc mismatch {ref_pc}:{disasm[rocprof_inst.pc][0]} != {pc}:{inst.disasm()} {pkt._time} {rocprof_inst.time+rocprof_inst.stall}"
-      insts += 1
+    # always check pc matches
+    assert ref_pc == pc, f"pc mismatch {ref_pc}:{disasm[rocprof_inst.pc][0]} != {pc}:{inst.disasm()}"
+    # special handling for s_endpgm, it marks the wave completion.
+    if inst == s_endpgm():
+      completed_wave = list(rwaves_iter[pkt.wave].pop(0))
+      assert len(completed_wave) == 0, f"incomplete instructions in wave {pkt.wave}"
+    else: assert pkt._time == rocprof_inst.time+rocprof_inst.stall
+    passed_insts += 1
 
-  print(f"passed for {insts} instructions across {total_waves} waves scheduled on {len(rwaves_iter)} wave units")
+  for k,v in rwaves_iter.items():
+    assert len(v) == 0, f"incomplete wave {k}"
+
+  print(f"passed for {passed_insts} instructions across {len(rwaves)} waves scheduled on {len(rwaves_iter)} wave units")
 
 if __name__ == "__main__":
-  import sys, pickle
+  import argparse, pickle, pathlib
   from tinygrad.helpers import temp
-  fp = temp("profile.pkl", append_user=True) if len(sys.argv) < 2 else sys.argv[1]
-  with open(fp, "rb") as f:
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--profile', type=pathlib.Path, metavar="PATH", help='Path to profile (optional file, default: latest profile)',
+                      default=pathlib.Path(temp("profile.pkl", append_user=True)))
+  parser.add_argument('--kernel', type=str, default=None, metavar="NAME", help='Kernel to focus on (optional name, default: all kernels)')
+  args = parser.parse_args()
+  with open(args.profile, "rb") as f:
     data = pickle.load(f)
   sqtt_events = [e for e in data if type(e).__name__ == "ProfileSQTTEvent"]
   kern_events = {e.name:e for e in data if type(e).__name__ == "ProfileProgramEvent"}
   target = next((e for e in data if type(e).__name__ == "ProfileDeviceEvent" and e.device.startswith("AMD"))).props["gfx_target_version"]
-  e =sqtt_events[1]
-  kern_events[e.kern]
-  test_rocprof_inst_traces_match(e, kern_events[e.kern], target)
+  for e in sqtt_events:
+    if args.kernel is not None and args.kernel != e.kern: continue
+    if not e.itrace: continue
+    # for small kernels, se0 itrace is empty
+    if e.se != 1: continue
+    print(f"==== {e.kern}")
+    test_rocprof_inst_traces_match(e, kern_events[e.kern], target)
