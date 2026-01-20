3a8304d54d4e066577b6fe3a1d159168af630eca
nimlgen <lenyaprokh@gmail.com>
2026-01-20 12:23:39 +0300

    um

--- tinygrad/runtime/support/system.py
+++ tinygrad/runtime/support/system.py
@@ -321,7 +321,7 @@ class APLRemotePCIDevice(RemotePCIDevice):
   @staticmethod
   def install_tinygpu():
     print("Downloading TinyGPU.app...")
-    system(f"ditto -xk {fetch("https://github.com/nimlgen/tinygpu_releases/raw/5b6ff43c8dffc43f57b29677e85664f667633359/TinyGPU.zip")} /Applications")
+    system(f"ditto -xk {fetch('https://github.com/nimlgen/tinygpu_releases/raw/5b6ff43c8dffc43f57b29677e85664f667633359/TinyGPU.zip')} /Applications")
     print(system(f"{APLRemotePCIDevice.APP_PATH} install"))
 
   def __init__(self, devpref:str, pcibus:str, bars:list[int], resize_bars:list[int]|None=None):
