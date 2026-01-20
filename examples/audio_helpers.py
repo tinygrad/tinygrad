a6239e5b19ffca03f65077d7516225365019c7d8
Irwin1138 <irwin1139@gmail.com>
2026-01-20 18:38:11 +0200

    consistent whitespace

--- examples/audio_helpers.py
+++ examples/audio_helpers.py
@@ -16,15 +16,12 @@ def stft(
   magnitudes = (stft_raw[:, :cutoff, :] ** 2 + stft_raw[:, cutoff:, :] ** 2).sqrt()
   return magnitudes
 
-
 def hann_window(N: int, periodic=True) -> Tensor:
   M = N + (periodic * 1)
   return ((1.0 - (Tensor.arange(M) * 2.0 * math.pi / (M - 1)).cos()) * 0.5)[:N]
 
-
 def make_stft_basis_buffers(n_fft: int, window: Tensor) -> Tensor:
   return Tensor.cat(*make_basis_buffers(n_fft, Tensor.arange((n_fft // 2) + 1)[None].T, window)).reshape(n_fft + 2, 1, n_fft)
-
 
 def make_basis_buffers(N_FFT: int, k_freq_bin: int | Tensor, window: Tensor) -> tuple[Tensor, Tensor]:
   n = Tensor.arange(N_FFT)
@@ -35,13 +32,11 @@ def make_basis_buffers(N_FFT: int, k_freq_bin: int | Tensor, window: Tensor) -> 
   sin_basis = w * -angle.sin()  # negate sin_basis to match torch
   return cos_basis, sin_basis
 
-
 def stft_full(x: Tensor, n_fft: int, stride: int, pad: tuple[int, int], window="hann", pad_mode="constant") -> Tensor:
   assert window == "hann", "other window types not implemented yet"
   bb = make_stft_basis_buffers(n_fft, hann_window(n_fft))
   res = stft(x, bb, n_fft, stride, pad, pad_mode)
   return res
-
 
 # rewritten from numpy
 def rfftfreq(n: int, d: float = 1.0, device=None) -> Tensor:
