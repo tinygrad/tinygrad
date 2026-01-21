d5e546759a1932be9b89c773eab65cf8563184ce
Vixel2006 <yusufshihata2006@gmail.com>
2026-01-21 11:44:38 +0200

    custom sharding for fsdp modules

--- tinygrad/nn/distributed.py
+++ tinygrad/nn/distributed.py
@@ -1,14 +1,40 @@
+from tinygrad import Tensor
 from tinygrad import nn
 import functools
+import math
+from typing import Any
 
-def fsdp(devices:tuple[str, ...], shard_axis:int=0):
-    def decorator(cls):
-        init = cls.__init__
-        @functools.wraps(init)
-        def wrap(self, *args, **kwargs):
-            init(self, *args, **kwargs)
-            for param in nn.state.get_parameters(self):
-                param = param.shard_(devices, shard_axis)
-        cls.__init__ = wrap
-        return cls
-    return decorator
+
+class FSDP:
+    def __init__(self, module:Any, devices:tuple[str, ...], axis:int=0):
+        self.module = module
+        self.devices = devices
+        self.axis = axis
+        self.ndev = len(self.devices)
+        self.sharded_params = []
+
+        self.shard_params()
+    
+    def shard_params(self):
+        for name, param in nn.state.get_state_dict(self.module).items():
+            rem = param.shape[self.axis] % self.ndev
+            pad_width = (self.ndev - rem) % self.ndev
+            padding = [
+                (0, pad_width) if i == self.axis else (0, 0)
+                for i in range(param.ndim)
+            ]
+            param_padded = param.pad(padding)
+            param_sharded = param_padded.shard_(self.devices, self.axis)
+
+            parts = name.split(".")
+            submod = self.module
+            for p in parts[:-1]:
+                submod = getattr(submod, p)
+            setattr(submod, parts[-1], param_sharded)
+
+
+    def gather_params(self):
+        pass
+
+    def sync_gradient(self):
+        pass
