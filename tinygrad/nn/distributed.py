c68374aa8793fa9478fa2f2bb42afe0c1dc0286b
Vixel2006 <yusufshihata2006@gmail.com>
2026-01-22 11:30:09 +0200

    fsdp forward pass done

--- tinygrad/nn/distributed.py
+++ tinygrad/nn/distributed.py
@@ -10,12 +10,15 @@ class FSDP:
 		self.devices:tuple[str, ...] = devices
 		self.axis = axis
 		self.ndev = len(self.devices)
-		self.units =  []
+		self.units = []
+		self.logical_shapes:dict[str, tuple] = {}
+		self.unit_prefix:dict[Any, str] = {}
 
 		for name, param in nn.state.get_state_dict(self.module).items(): self.shard_param(name, param)
 		for unit in self.units: self.wrap_call(unit)
 	
 	def shard_param(self, name:str, param:Tensor):
+		self.logical_shapes[name] = param.shape
 		# get the padding width needed to be able to shard the tensor around the sharding axis
 		rem = param.shape[self.axis] % self.ndev
 		pad_width = (self.ndev - rem) % self.ndev
@@ -32,30 +35,37 @@ class FSDP:
 			# if this is a leaf module in the model, append to the units to modify its __call__ using wrappers
 			if i == len(parts) - 2 and submod not in self.units:
 				self.units.append(submod)
+				self.unit_prefix[submod] = ".".join(parts[:-1])
 		setattr(submod, parts[-1], param_sharded)
 
-	def gather_param(self, param:Tensor) -> Tensor:
-		# Creating a full copy of the param on all devices and do one tensor that have copies across devices
-		return Tensor.cat([param.to(device) for device in self.devices])
+	def gather_param(self, shape:tuple[int], param:Tensor) -> Tensor:
+		# creating a full copy of the param on all devices and do one tensor that have copies across devices
+		# we use logical shapes to get the correct tensors without the padding for shape matching and avoiding unnessecary math ops on padded elements
+		gathered = param.to(self.devices[0])
+		slices = tuple(slice(0, s) for s in shape)
+		return gathered[slices].shard(self.devices, axis=None)
 
 	def wrap_call(self, unit:Any):
-		orig_call = getattr(unit, "__call__")	
-		@wraps(orig_call)
-		def wrapped(*args, **kwargs):
-			sharded_params = {}
-			unit_params = nn.state.get_state_dict(unit).items()
-			# gathering the params of the unit before doing the call
-			for name, param in unit_params:
-				sharded_params[name] = param.clone()
-				setattr(unit, name, self.gather_param(param))
-			out = orig_call(*args, **kwargs)
-			# sharding the params of the units again
-			for name, param in unit_params: setattr(unit, name, sharded_params[name])
-			return out
-		setattr(unit, "__call__", wrapped)
+		ctx = self
+
+		class Wrapper(unit.__class__):
+			def __call__(self, *args, **kwargs):
+				sharded_refs = {}
+				unit_params = nn.state.get_state_dict(self).items()
+				for name, param in unit_params:
+					key = f"{ctx.unit_prefix[self]}.{name}"
+					sharded_refs[name] = param
+					shape = ctx.logical_shapes[key]
+					setattr(self, name, ctx.gather_param(shape, param))
+				try:
+					return super().__call__(*args, **kwargs)
+				finally:
+					for name, param in sharded_refs.items(): setattr(self, name, param)
+		unit.__class__ = Wrapper
+					
 
 	def __call__(self, x:Tensor) -> Tensor:
 		return self.module(x)
 
-	def sync_gradient(self):
+	def sync_grad(self):
 		pass
