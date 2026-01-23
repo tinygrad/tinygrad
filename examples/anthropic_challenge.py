d9c4d46d0cbe6f2c4c3fdf4b0caa93867274ad3d
George Hotz <geohot@gmail.com>
2026-01-23 10:43:38 +0900

    lil clean

--- examples/anthropic_challenge.py
+++ examples/anthropic_challenge.py
@@ -4,7 +4,7 @@ from tinygrad.uop.symbolic import symbolic
 from tinygrad.codegen import Renderer
 from tinygrad.codegen.opt import Opt, OptOps
 
-# ************************* implementation of problem (replace with anything!) ************************
+# ************************* implementation of the problem ************************
 
 def myhash(a: Tensor) -> Tensor:
   a = (a + 0x7ED55D16) + (a << 12)
@@ -69,7 +69,7 @@ global_addrs = []
 vliw_prepare = PatternMatcher([
   # loop unrolling (should be a part of tinygrad)
   (UPat(Ops.SINK, name="sink"), loop_unrolling),
-  # cast is fake (TODO: why is this needed?)
+  # cast is fake
   (UPat(Ops.CAST, name="c"), lambda c: c.src[0]),
   # rewrites to hardcode the addresses in memory
   (UPat(Ops.DEFINE_GLOBAL, name="dg"), lambda dg: UOp.const(dtypes.uint, global_addrs[dg.arg])),
@@ -87,24 +87,52 @@ class VLIWRenderer(Renderer):
   pre_matcher = vliw_prepare
 
   def render(self, uops:list[UOp]):
-    # NOTE: this is a minimal renderer. to get speed, you need VLIW. to get under 1536 regs, you need a reg allocator
+
+    # TODO: this is a minimal renderer. for low cycle count, make it good
+    # to get speed, you need to add VLIW packing
+    # to get under 1536 regs, you need to add a register allocator
+    # we left the fun parts to you
+
     print(f"rendering with {len(uops)} uops")
     reg, uop_reg, instrs = 0, {}, []
     def r(u): return uop_reg[u]
     for u in uops:
+      assert u.dtype.count in (1,8), "dtype count must be 1 or 8"
+
+      # dumb register allocator
       if u.op not in {Ops.STORE, Ops.SINK, Ops.GEP}: uop_reg[u] = reg; reg += u.dtype.count
-      if u.op is Ops.CONST: instrs.append({"load": [("const", r(u), u.arg)]})
-      elif u.op is Ops.VECTORIZE:
-        if all(s == u.src[0] for s in u.src): instrs.append({"valu": [("vbroadcast", r(u), r(u.src[0]))]})
-        else: instrs.extend({"flow": [("add_imm", r(u)+i, r(s), 0)]} for i, s in enumerate(u.src) if r(s) != r(u)+i)
-      elif u.op is Ops.GEP: uop_reg[u] = r(u.src[0]) + u.arg[0]  # extract element from vector
-      elif u.op is Ops.LOAD: instrs.append({"load": [("vload", r(u), r(u.src[0]))] if u.dtype.count > 1 else [("load", r(u), r(u.src[0]))]})
-      elif u.op is Ops.STORE: instrs.append({"store": [("vstore", r(u.src[0]), r(u.src[1]))] if u.src[1].dtype.count > 1 else [("store", r(u.src[0]), r(u.src[1]))]})
-      elif u.op is Ops.MULACC: assert u.dtype.count == 8; instrs.append({"valu": [("multiply_add", r(u), r(u.src[0]), r(u.src[1]), r(u.src[2]))]})
-      elif u.op is Ops.WHERE: assert u.dtype.count == 8; instrs.append({"flow": [("vselect", r(u), r(u.src[0]), r(u.src[1]), r(u.src[2]))]})
-      elif u.op in self.code_for_op: instrs.append({("valu" if u.dtype.count > 1 else "alu"): [(self.code_for_op[u.op], r(u), r(u.src[0]), r(u.src[1]))]})
-      elif u.op is Ops.SINK: instrs.append({"flow": [("halt",)]})
-      else: assert False, f"unhandled op {u.op}"
+
+      # render UOps to instructions
+      match u.op:
+        case Ops.SINK:
+          instrs.append({"flow": [("halt",)]})
+        case Ops.CONST:
+          instrs.append({"load": [("const", r(u), u.arg)]})
+        case Ops.GEP:
+          uop_reg[u] = r(u.src[0]) + u.arg[0]
+        case Ops.VECTORIZE:
+          if all(s == u.src[0] for s in u.src):
+            instrs.append({"valu": [("vbroadcast", r(u), r(u.src[0]))]})
+          else:
+            # this is a copy into a contiguous chunk of registers
+            instrs.extend({"flow": [("add_imm", r(u)+i, r(s), 0)]} for i,s in enumerate(u.src) if r(s) != r(u)+i)
+        case Ops.LOAD:
+          op = "vload" if u.dtype.count > 1 else "load"
+          instrs.append({"load": [(op, r(u), r(u.src[0]))]})
+        case Ops.STORE:
+          op = "vstore" if u.src[1].dtype.count > 1 else "store"
+          instrs.append({"store": [(op, r(u.src[0]), r(u.src[1]))]})
+        case Ops.MULACC:
+          assert u.dtype.count == 8
+          instrs.append({"valu": [("multiply_add", r(u), r(u.src[0]), r(u.src[1]), r(u.src[2]))]})
+        case Ops.WHERE:
+          assert u.dtype.count == 8
+          instrs.append({"flow": [("vselect", r(u), r(u.src[0]), r(u.src[1]), r(u.src[2]))]})
+        case _ if u.op in self.code_for_op:
+          cat = "valu" if u.dtype.count > 1 else "alu"
+          instrs.append({cat: [(self.code_for_op[u.op], r(u), r(u.src[0]), r(u.src[1]))]})
+        case _:
+          raise NotImplementedError(f"unhandled op {u.op}")
     return repr(instrs)
 
 # ************************* test and render *************************
@@ -152,10 +180,10 @@ if __name__ == "__main__":
   # NOTE: the scratch size needs to be reduced to 1536 when you have a register allocator
   src = eval(prg.src)
   max_regs = max(t[1] for instr in src for v in instr.values() for t in v if len(t) > 1) + 8
-  print(f"{max_regs} regs used" + ("" if max_regs < 1536 else " <-- WARNING: TOO MANY REGISTERS, MUST BE < 1536"))
+  print(f"{max_regs:5d} regs used" + ("" if max_regs <= 1536 else "       <-- WARNING: TOO MANY REGISTERS, MUST BE <= 1536"))
   machine = problem.Machine(mem, src, problem.DebugInfo(scratch_map={}), n_cores=1, trace=False, scratch_size=max_regs)
   machine.run()
-  print(f"ran for {machine.cycle} cycles")
+  print(f"ran for {machine.cycle:5d} cycles" + ("" if machine.cycle <= 1363 else "  <-- EVEN CLAUDE GOT 1363"))
 
   # compare to reference
   ref_mem = mem.copy()
