9aebd0914a3693576638e02a6b90b081d974035d
Vixel2006 <yusufshihata2006@gmail.com>
2026-01-23 18:00:37 +0200

    simple tests for fsdp module

--- tinygrad/nn/distributed.py
+++ tinygrad/nn/distributed.py
@@ -1,8 +1,6 @@
 from tinygrad import Tensor
 from tinygrad import nn
 from typing import Any
-from functools import wraps
-
 
 class FSDP:
 	def __init__(self, module:Any, devices:tuple[str, ...], axis:int=0):
@@ -25,7 +23,8 @@ class FSDP:
 		padding = [(0, pad_width) if i == self.axis else (0, 0) for i in range(param.ndim)]
 		# pad the tensor then replace it with the sharded version
 		param_padded = param.pad(padding)
-		param_sharded = param_padded.shard_(self.devices, self.axis)
+		param_sharded = param_padded.shard(self.devices, self.axis)
+		param_sharded.requires_grad = True
 		# get parameters and submodules of the model
 		parts = name.split(".")
 		submod = self.module
@@ -43,7 +42,7 @@ class FSDP:
 		# we use logical shapes to get the correct tensors without the padding for shape matching and avoiding unnessecary math ops on padded elements
 		gathered = param.to(self.devices[0])
 		slices = tuple(slice(0, s) for s in shape)
-		return gathered[slices].shard(self.devices, axis=None)
+		return gathered[slices].contiguous().shard(self.devices, axis=None)
 
 	def wrap_call(self, unit:Any):
 		ctx = self
@@ -62,7 +61,6 @@ class FSDP:
 				finally:
 					for name, param in sharded_refs.items(): setattr(self, name, param)
 		unit.__class__ = Wrapper
-					
 
 	def __call__(self, x:Tensor) -> Tensor:
 		return self.module(x)
