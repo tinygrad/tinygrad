e8efe80b59250d91468daf1692e56a53708b36d5
qazal <qazal.software@gmail.com>
2026-01-23 06:28:51 -0800

    asm_gemm in tensor

--- extra/gemm/asm/cdna/asm_gemm.py
+++ extra/gemm/asm/cdna/asm_gemm.py
@@ -1,6 +1,6 @@
 import pathlib
 from tinygrad import Tensor, UOp, dtypes
-from tinygrad.helpers import all_same
+from tinygrad.helpers import all_same, Context
 from tinygrad.engine.realize import Estimates
 from tinygrad.uop.ops import Ops, KernelInfo
 
@@ -20,9 +20,9 @@ def asm_gemm(A:Tensor, B:Tensor) -> Tensor:
   assert can_use_asm_gemm(A, B), f"{_stats['errs'][0]}"
   _stats["used"] += 1
 
+  N = A.shape[0]
   dname = A.device
   def custom_gemm_kernel(C:UOp, A:UOp, B:UOp, params:UOp) -> UOp:
-    N = A.shape[0]
     lidx = UOp.special(THREADS_PER_WG, "lidx0")
     gidx = UOp.special(N//THREADS_PER_WG * N//THREADS_PER_WG, "gidx0")
 
@@ -31,14 +31,15 @@ def asm_gemm(A:Tensor, B:Tensor) -> Tensor:
     sink = UOp.sink(C.base, A.base, B.base, params.base, lidx, gidx, arg=KernelInfo(name="gemm", estimates=Estimates(ops=N*N*N*2, mem=N*N*4*3)))
     return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.DEVICE, arg=dname), UOp(Ops.LINEAR, src=(*sink.src, sink)), UOp(Ops.SOURCE, arg=src)))
   
-  params = Tensor.full((N, N), N).realize()
+  params = Tensor.full((N, N), N).contiguous()
+  Bt = B.T.contiguous()
   C = Tensor.empty(N, N, dtype=dtypes.half)
-  C = Tensor.custom_kernel(C, A, B.T, params, fxn=custom_gemm_kernel)[0]
+  C = Tensor.custom_kernel(C, A, Bt, params, fxn=custom_gemm_kernel)[0]
   return C
 
 if __name__ == "__main__":
   import numpy as np
-  from tinygrad.helpers import getenv, Context
+  from tinygrad.helpers import getenv
   N = getenv("N", 4096)
 
   rng = np.random.default_rng(0)
