3b9dea5e6a340748bc6a2ce3a3225b3e4d9962fb
jIab-b <sam.mac446@gmail.com>
2026-01-24 18:46:08 +1000

    updated speed_compare_cuda_ptx to work with new cuda compile format

--- test/external/speed_compare_cuda_ptx.py
+++ test/external/speed_compare_cuda_ptx.py
@@ -1,18 +1,24 @@
 import itertools
 from tinygrad import Device
-from tinygrad.engine.realize import CompiledRunner, get_program
+from tinygrad.engine.realize import CompiledRunner
+from tinygrad.codegen import get_program
 from tinygrad.codegen.opt.heuristic import hand_coded_optimizations
 from tinygrad.helpers import getenv, colorize_float
 from extra.optimization.helpers import load_worlds, ast_str_to_lin, ast_str_to_ast
 from tinygrad.codegen.opt.postrange import bufs_from_ast
-from tinygrad.runtime.ops_cuda import PTXCompiler, PTXRenderer, CUDACompiler
+from tinygrad.runtime.ops_cuda import PTXCompiler, PTXRenderer, CUDACompiler, CUDARenderer
 
 if __name__ == "__main__":
   ast_strs = load_worlds(filter_reduce=False, filter_novariable=True)
-  # no bfloat16 for ptx at the moment
-  ast_strs = [x for x in ast_strs if "dtypes.bfloat16" not in x]
+  # no bfloat16 for ptx at the moment, also skip malformed ASTs with Invalid
+  ast_strs = [x for x in ast_strs if "dtypes.bfloat16" not in x and "Invalid" not in x]
   dev = Device["CUDA"]
-  ptx = PTXRenderer(dev.arch)
+
+  cuda_renderer = CUDARenderer(dev.arch)
+  cuda_renderer.compiler = CUDACompiler(dev.arch)
+
+  ptx_renderer = PTXRenderer(dev.arch)
+  ptx_renderer.compiler = PTXCompiler(dev.arch)
 
   # NUM=112 python3 test/external/speed_compare_cuda_ptx.py
 
@@ -20,20 +26,16 @@ if __name__ == "__main__":
   if single != -1: ast_strs = ast_strs[single:single+1]
 
   average_tm_cuda, average_tm_ptx = 0, 0
-  for num,ast in enumerate(ast_strs):
+  for num, ast in enumerate(ast_strs):
     # cuda compile
-    dev.compiler = CUDACompiler(dev.arch)
-    lin = ast_str_to_lin(ast, opts=dev.renderer)
-    lin.apply_opts(hand_coded_optimizations(lin))
-    cuda_prg = CompiledRunner(get_program(lin.get_optimized_ast(), lin.opts))
+    lin = hand_coded_optimizations(ast_str_to_lin(ast, opts=cuda_renderer))
+    cuda_prg = CompiledRunner(get_program(lin.get_optimized_ast(), lin.ren))
 
-    bufs = bufs_from_ast(ast_str_to_ast(ast), "CUDA")
+    bufs = [b.allocate() for b in bufs_from_ast(ast_str_to_ast(ast), "CUDA")]
 
     # ptx compile
-    dev.compiler = PTXCompiler(dev.arch)
-    lin = ast_str_to_lin(ast, opts=ptx)
-    lin.apply_opts(hand_coded_optimizations(lin))
-    ptx_prg = CompiledRunner(get_program(lin.get_optimized_ast(), lin.opts))
+    lin = hand_coded_optimizations(ast_str_to_lin(ast, opts=ptx_renderer))
+    ptx_prg = CompiledRunner(get_program(lin.get_optimized_ast(), lin.ren))
 
     # warmup
     try:
@@ -50,10 +52,10 @@ if __name__ == "__main__":
     average_tm_cuda += min(tm_cuda)
     average_tm_ptx += min(tm_ptx)
     ratio = min(tm_ptx)/min(tm_cuda)
-    print(f"{average_tm_ptx/average_tm_cuda:5.2f}x -- {num:4d} {colorize_float(ratio)}  {min(tm_ptx)*1e6:7.2f} us", lin.name)
+    print(f"{average_tm_ptx/average_tm_cuda:5.2f}x -- {num:4d} {colorize_float(ratio)}  {min(tm_ptx)*1e6:7.2f} us", ptx_prg.p.name)
     if ratio > 1.5:
       def fix(x): return x.replace('\t', ' ').strip()
-      ll1, ll2 = cuda_prg.lib.decode().split('\n'), ptx_prg.lib.decode().split('\n')
+      ll1, ll2 = cuda_prg.p.lib.decode().split('\n'), ptx_prg.p.lib.decode().split('\n')
       if single != -1:
         for ln, (l1, l2) in enumerate(itertools.zip_longest(ll1, ll2, fillvalue='')):
           print(f"{ln:5d} | {fix(l1):80s} | {fix(l2):80s}")
