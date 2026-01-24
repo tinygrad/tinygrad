47bf8758cead4e7b70c762c67e4e4c2ef43da36a
qazal <qazal.software@gmail.com>
2026-01-24 16:32:56 +0900

    store refactor

--- extra/gemm/asm/cdna/lib.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-from extra.gemm.asm.cdna.gemm import asm_gemm_kernel
-
-_, lib = asm_gemm_kernel(4096).to_asm()
-print(len(lib))
-with open(pathlib.Path(__file__).parent/"lib", "wb") as f: f.write(lib)
