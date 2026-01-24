3eb3acd98f7579031a1477c43951ed4456b30eda
qazal <qazal.software@gmail.com>
2026-01-24 12:36:49 +0900

    everything compiled to dsl

--- extra/gemm/asm/cdna/to_dsl.py
+++ extra/gemm/asm/cdna/to_dsl.py
@@ -4,9 +4,20 @@ from extra.assembly.amd.dsl import s, v, Reg, VCC_LO, VCC_HI, VCC, EXEC_LO, EXEC
 from extra.assembly.amd.autogen.cdna.ins import *
 
 lines = (pathlib.Path(__file__).parent/"gemm.s").read_text().split("\n")
-dsl:list[Inst] = []
+dsl:list[Inst|str] = []
 for line in lines:
   asm = line.split("//")[0].lstrip()
   if not asm: continue
+  # label
+  if asm.endswith(":"):
+    dsl.append(asm)
+    continue
   inst, *rest = asm.split(" ")
-  print(eval(inst))
+  if inst == "v_accvgpr_write_b32": inst = "v_accvgpr_write"
+  if inst == "v_accvgpr_read_b32": inst = "v_accvgpr_read"
+  try:
+    inst = eval(inst)
+  except NameError:
+    if "e64" in inst: inst = eval(inst.replace("_e64", ""))
+    else: inst = eval(inst+"_e64")
+  print(inst)
