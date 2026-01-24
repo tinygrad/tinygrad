57a4ddcfaa175fdf0c7e80d0c6255a3f35675218
jIab-b <sam.mac446@gmail.com>
2026-01-24 19:28:18 +1000

    got rid of deleted test from generate dataset

--- extra/optimization/generate_dataset.sh
+++ extra/optimization/generate_dataset.sh
@@ -6,7 +6,7 @@ export CAPTURE_PROCESS_REPLAY=1
 rm $LOGOPS
 test/external/process_replay/reset.py
 
-CI=1 python3 -m pytest -n=auto test/test_ops.py test/test_nn.py test/test_winograd.py test/models/test_real_world.py --durations=20
+CI=1 python3 -m pytest -n=auto test/test_ops.py test/test_nn.py test/models/test_real_world.py --durations=20
 CL=1 python3 -m pytest test/test_tiny.py
 
 # extract, sort and uniq
@@ -14,4 +14,4 @@ extra/optimization/extract_dataset.py
 sort -u /tmp/ops > /tmp/sops
 ls -lh /tmp/ops /tmp/sops
 gzip -k /tmp/sops
-# mv /tmp/sops.gz extra/datasets/
+  # mv /tmp/sops.gz extra/datasets/
