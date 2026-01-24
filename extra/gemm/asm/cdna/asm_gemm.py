844ea7ef1e00f3ce61d783696fdb8c6f8c4b0e00
qazal <qazal.software@gmail.com>
2026-01-24 15:56:53 +0900

    work

--- extra/gemm/asm/cdna/asm_gemm.py
+++ extra/gemm/asm/cdna/asm_gemm.py
@@ -1,10 +1,10 @@
 import pathlib, atexit, functools
-from tinygrad import Tensor, UOp, dtypes
+from tinygrad import Tensor, UOp, dtypes, Device
 from tinygrad.helpers import all_same, dedup
 from tinygrad.engine.realize import Estimates
 from tinygrad.uop.ops import Ops, KernelInfo
 # note: this should be in the same file
-from extra.gemm.asm.cdna.gemm import k
+from extra.gemm.asm.cdna.gemm import asm_gemm_kernel
 
 THREADS_PER_WG = 256
 
@@ -18,11 +18,11 @@ def can_use_asm_gemm(A:Tensor, B:Tensor) -> bool:
   if A.shape[0] % THREADS_PER_WG != 0: return todo(f"N must be divisable by {THREADS_PER_WG}")
   return True
 
-def custom_gemm_kernel(C:UOp, A:UOp, B:UOp, params:UOp, dname:str) -> UOp:
+def custom_gemm_kernel(C:UOp, A:UOp, B:UOp, params:UOp, dname:str, arch:str) -> UOp:
   N = A.shape[0]
   lidx = UOp.special(THREADS_PER_WG, "lidx0")
   gidx = UOp.special(N//THREADS_PER_WG * N//THREADS_PER_WG, "gidx0")
-  src, lib = k.to_asm()
+  src, lib = asm_gemm_kernel(arch).to_asm()
   sink = UOp.sink(C.base, A.base, B.base, params.base, lidx, gidx, arg=KernelInfo(name="gemm", estimates=Estimates(ops=N*N*N*2, mem=N*N*4*3)))
   return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.DEVICE, arg=dname), UOp(Ops.LINEAR, src=(*sink.src, sink)), UOp(Ops.SOURCE, arg=src),
                                UOp(Ops.BINARY, arg=lib)))
@@ -39,10 +39,12 @@ def asm_gemm(A:Tensor, B:Tensor) -> Tensor:
   stats["used"] += 1
 
   N = A.shape[0]
+  # may open the device to get arch
+  arch = Device[A.device].renderer.arch
   params = Tensor.full((N, N), N).contiguous()
   Bt = B.T.contiguous()
   C = Tensor.empty(N, N, dtype=dtypes.half)
-  C = Tensor.custom_kernel(C, A, Bt, params, fxn=functools.partial(custom_gemm_kernel, dname=A.device))[0]
+  C = Tensor.custom_kernel(C, A, Bt, params, fxn=functools.partial(custom_gemm_kernel, dname=A.device, arch=arch))[0]
   return C
 
 if __name__ == "__main__":
