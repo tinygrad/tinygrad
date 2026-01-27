6720a94997c719828303315618ace02d1f336e14
qazal <qazal.software@gmail.com>
2026-01-27 21:44:46 +0900

    don't need those

--- extra/amdgpu_elf.py
+++ extra/amdgpu_elf.py
@@ -10,22 +10,20 @@ def put(dst:bytearray, off:int, data:bytes) -> None:
   dst[off:end] = data
 
 def pack_hsaco(prg:bytes, kd:dict) -> bytes:
-  # match LLVM's layout: rodata at 0x3c0, text at 0x400, text_vaddr at 0x1400
-  # note: don't need this, just keep it for the rodata byte to byte compare with llvm for now, test_vaddr is in rodata
-  rodata_offset = 0x3c0
-  text_offset = 0x400
-  text_vaddr = 0x1400  # gives kernel_code_entry_byte_offset = 0x1040
+  text = prg + b'\x00' * ((hsa.AMD_ISA_ALIGN_BYTES - len(prg) % hsa.AMD_ISA_ALIGN_BYTES) % hsa.AMD_ISA_ALIGN_BYTES)
+  text_offset = align_up(ctypes.sizeof(libc.Elf64_Ehdr), hsa.AMD_ISA_ALIGN_BYTES)
+  rodata_offset = text_offset + len(text)
 
-  # ** pack rodata object (kernel descriptor)
+  # ** pack rodata object
   desc = amdgpu_kd.llvm_amdhsa_kernel_descriptor_t()
   desc.group_segment_fixed_size = kd.get('group_segment_fixed_size', 0)
   desc.private_segment_fixed_size = kd.get('private_segment_fixed_size', 0)
   desc.kernarg_size = kd.get('kernarg_size', 0)
-  desc.kernel_code_entry_byte_offset = text_vaddr - rodata_offset
+  desc.kernel_code_entry_byte_offset = text_offset-rodata_offset
   # rsrc1
   vgpr_granule = max(0, (kd['next_free_vgpr'] + 7) // 8 - 1)
   sgpr_granule = kd.get('sgpr_granule', 0)  # GFX9: 2*max(0,ceil(sgprs/16)-1), GFX10+: reserved=0
-  reserved1 = (kd.get('workgroup_processor_mode', 0) << 3) | (kd.get('memory_ordered', 0) << 4)  # GFX10+ only
+  reserved1 = (kd.get('workgroup_processor_mode', 1) << 3) | (kd.get('memory_ordered', 1) << 4)
   desc.compute_pgm_rsrc1 = (vgpr_granule << hsa.AMD_COMPUTE_PGM_RSRC_ONE_GRANULATED_WORKITEM_VGPR_COUNT_SHIFT |
                             sgpr_granule << hsa.AMD_COMPUTE_PGM_RSRC_ONE_GRANULATED_WAVEFRONT_SGPR_COUNT_SHIFT |
                             3 << hsa.AMD_COMPUTE_PGM_RSRC_ONE_FLOAT_DENORM_MODE_16_64_SHIFT |
@@ -43,25 +41,22 @@ def pack_hsaco(prg:bytes, kd:dict) -> bytes:
   # code properties, different for every arch
   desc.kernel_code_properties = (kd.get('user_sgpr_kernarg_segment_ptr', 0) << hsa.AMD_KERNEL_CODE_PROPERTIES_ENABLE_SGPR_KERNARG_SEGMENT_PTR_SHIFT |
                                  kd.get('uses_dynamic_stack', 0) << hsa.AMD_KERNEL_CODE_PROPERTIES_IS_DYNAMIC_CALLSTACK_SHIFT |
-                                 kd.get('wavefront_size32', 0) << hsa.AMD_KERNEL_CODE_PROPERTIES_ENABLE_WAVEFRONT_SIZE32_SHIFT)
+                                 kd.get('wavefront_size32', 1) << hsa.AMD_KERNEL_CODE_PROPERTIES_ENABLE_WAVEFRONT_SIZE32_SHIFT)
   rodata = bytes(desc)
 
   # ** pack elf sections
   sh_names:list[int] = []
   strtab = bytearray(b"\x00")
-  for name in [".rodata", ".text", ".shstrtab"]:
+  for name in [".text", ".rodata", ".strtab"]:
     sh_names.append(len(strtab))
     strtab += name.encode("ascii") + b"\x00"
 
-  text_size = len(prg)
-  rodata_size = len(rodata)
-  strtab_offset = align_up(text_offset + text_size, 4)  # align strtab to 4 bytes
-  strtab_size = len(strtab)
-  shdr_offset = strtab_offset + strtab_size
+  rodata_offset = align_up(text_offset+(text_size:=len(text)), hsa.AMD_KERNEL_CODE_ALIGN_BYTES)
+  strtab_offset = rodata_offset+(rodata_size:=len(rodata))
+  shdr_offset   = strtab_offset+(strtab_size:=len(strtab))
 
-  # sections: (sh_type, sh_flags, sh_addr, sh_offset, sh_size)
-  sections = [(libc.SHT_PROGBITS, libc.SHF_ALLOC, rodata_offset, rodata_offset, rodata_size),
-              (libc.SHT_PROGBITS, libc.SHF_ALLOC | libc.SHF_EXECINSTR, text_vaddr, text_offset, text_size),
+  sections = [(libc.SHT_PROGBITS, libc.SHF_ALLOC | libc.SHF_EXECINSTR, text_offset, text_offset, text_size),
+              (libc.SHT_PROGBITS, libc.SHF_ALLOC, rodata_offset, rodata_offset, rodata_size),
               (libc.SHT_STRTAB, 0, 0, strtab_offset, strtab_size)]
   shdrs = (libc.Elf64_Shdr * len(sections))()
   for i,s in enumerate(sections): shdrs[i] = libc.Elf64_Shdr(sh_names[i], *s)
@@ -71,8 +66,8 @@ def pack_hsaco(prg:bytes, kd:dict) -> bytes:
 
   elf = bytearray(shdr_offset + ctypes.sizeof(shdrs))
   put(elf, 0, bytes(ehdr))
+  put(elf, text_offset, text)
   put(elf, rodata_offset, rodata)
-  put(elf, text_offset, prg)
   put(elf, strtab_offset, strtab)
   put(elf, shdr_offset, bytes(shdrs))
   return bytes(elf)
