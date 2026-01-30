3f8d6bc25b74ecaa37a6b26889b245c9c691c2fc
Chen-Yu Yang <chenyu@fastmail.com>
2026-01-30 11:28:43 -0500

    flip unique const src order [pr]
    matches buffer, simplifies replace_input_buffer

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -99,12 +99,8 @@ from tinygrad.schedule.multi import get_multi_map
 
 def replace_input_buffer(ctx:tuple[dict[UOp, UOp], dict[str, int]], b:UOp):
   if (ret:=ctx[0].get(b, None)) is None:
-    if b.op is Ops.BUFFER:
-      ctx[0][b] = ret = b.replace(src=(UOp(Ops.LUNIQUE, arg=len(ctx[0])), b.src[1]))
-    else:
-      # TODO: flip args in CONST
-      assert b.op is Ops.CONST
-      ctx[0][b] = ret = b.replace(src=(b.src[0], UOp(Ops.LUNIQUE, arg=len(ctx[0]))))
+    # both BUFFER and CONST have src=(UNIQUE, DEVICE), replace UNIQUE with LUNIQUE
+    ctx[0][b] = ret = b.replace(src=(UOp(Ops.LUNIQUE, arg=len(ctx[0])), b.src[1]))
   return ret
 
 def strip_bind(ctx:tuple[dict[UOp, UOp], dict[str, int]], b:UOp):
@@ -117,7 +113,7 @@ pm_pre_sched_cache = PatternMatcher([
   # replace input buffers
   (UPat(Ops.BUFFER, src=(UPat(Ops.UNIQUE), UPat(Ops.DEVICE)), name="b"), replace_input_buffer),
   # remove unique consts
-  (UPat(Ops.CONST, src=(UPat(Ops.DEVICE), UPat(Ops.UNIQUE)), name="b"), replace_input_buffer),
+  (UPat(Ops.CONST, src=(UPat(Ops.UNIQUE), UPat(Ops.DEVICE)), name="b"), replace_input_buffer),
   # strip value from BIND for cache key normalization, so different values hit same cache
   (UPat(Ops.BIND, src=(UPat(Ops.DEFINE_VAR), UPat(Ops.CONST)), name="b"), strip_bind),
 ])
@@ -131,7 +127,7 @@ def replace_input_buffer_back(ctx:dict[UOp, UOp], b:UOp):
 
 pm_post_sched_cache = PatternMatcher([
   (UPat(Ops.BUFFER, src=(UPat(Ops.LUNIQUE), UPat(Ops.DEVICE)), name="b"), replace_input_buffer_back),
-  (UPat(Ops.CONST, src=(UPat(Ops.DEVICE), UPat(Ops.LUNIQUE)), name="b"), replace_input_buffer_back),
+  (UPat(Ops.CONST, src=(UPat(Ops.LUNIQUE), UPat(Ops.DEVICE)), name="b"), replace_input_buffer_back),
   # restore BIND value stripped in pm_pre_sched_cache
   (UPat(Ops.BIND, src=(UPat(Ops.DEFINE_VAR),), name="b"), lambda ctx,b: ctx.get(b)),
 ])
