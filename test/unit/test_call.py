2c7bf0694599d69e81d979752220f0b09a127d02
George Hotz <geohot@gmail.com>
2026-01-30 14:27:08 +0800

    skip that for now

--- test/unit/test_call.py
+++ test/unit/test_call.py
@@ -4,8 +4,6 @@ from tinygrad import Tensor
 from tinygrad.dtype import dtypes
 from tinygrad.uop.ops import UOp, Ops
 
-# we define a plus function
-plus_fxn = UOp.param(0, dtypes.float, (10,10)) + UOp.param(1, dtypes.float, (10,10))
 
 class TestCall(unittest.TestCase):
   def test_call_plus(self):
@@ -13,8 +11,25 @@ class TestCall(unittest.TestCase):
     b = Tensor.randn(10, 10)
     Tensor.realize(a,b)
 
+    # we define a plus function
+    plus_fxn = UOp.param(0, dtypes.float, (10,10)) + UOp.param(1, dtypes.float, (10,10))
+
     c = Tensor.call(a, b, fxn=plus_fxn)
     np.testing.assert_equal(c.numpy(), (a+b).numpy())
+
+  @unittest.skip("needs GEMM on mixins")
+  def test_call_gemm(self):
+    M, K, N = 4, 8, 4
+    a = Tensor.randn(M, K)
+    b = Tensor.randn(K, N)
+    Tensor.realize(a, b)
+
+    # we define a gemm function
+    x = UOp.param(0, dtypes.float, shape=(M, K))
+    y = UOp.param(1, dtypes.float, shape=(K, N))
+    c = Tensor.call(a, b, fxn=x@y)
+
+    np.testing.assert_allclose(c.numpy(), a.numpy() @ b.numpy(), rtol=1e-5)
 
 if __name__ == '__main__':
   unittest.main()
