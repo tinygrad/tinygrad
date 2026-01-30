242b4c47d00e41ff164543db17e7c629edb53d58
George Hotz <geohot@gmail.com>
2026-01-30 12:50:37 +0800

    resolve call

--- test/unit/test_call.py
+++ test/unit/test_call.py
@@ -4,14 +4,14 @@ from tinygrad import Tensor
 from tinygrad.dtype import dtypes
 from tinygrad.uop.ops import UOp, Ops
 
+# we define a plus function
+plus_fxn = UOp.param(0, dtypes.float, (10,10)) + UOp.param(1, dtypes.float, (10,10))
+
 class TestCall(unittest.TestCase):
   def test_call_plus(self):
     a = Tensor.randn(10, 10)
     b = Tensor.randn(10, 10)
     Tensor.realize(a,b)
-
-    # we define a plus function
-    plus_fxn = UOp.param(0, dtypes.float, (10,10)) + UOp.param(1, dtypes.float, (10,10))
 
     c = Tensor.call(a, b, fxn=plus_fxn)
     np.testing.assert_equal(c.numpy(), (a+b).numpy())
