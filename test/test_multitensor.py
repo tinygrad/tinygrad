2111762a48887db8f934df50d1918865e6c05283
imaolo <56898718+imaolo@users.noreply.github.com>
2026-02-01 11:48:43 -0500

    failed test case for RING output device (#14191)
    * Add enable/disable scheduler cache ContextVar
    
    * add allreduce ring and naive to() tests
    
    * clearer test comparing native vs ring allreduce
    
    * split tests, add helper
    
    * removing trailing whitespace
    
    ---------
    
    Co-authored-by: chenyu <chenyu@fastmail.com>

--- test/test_multitensor.py
+++ test/test_multitensor.py
@@ -33,6 +33,10 @@ def _test_allreduce(t:Tensor):
   b.realize()
   return aa, b
 
+def _test_multiple_to_single_device(ring:int) -> Tensor:
+  t = Tensor.empty(32).shard(devices_4, 0).to(Device.DEFAULT)
+  with Context(RING=ring, SCACHE=0): return t.realize()
+
 @unittest.skipIf(not_support_multi_device(), "no multi")
 class TestMultiTensor(unittest.TestCase):
   @needs_second_gpu
@@ -255,6 +259,15 @@ class TestMultiTensor(unittest.TestCase):
     with Context(RING=2):
       a,b = _test_allreduce(Tensor.rand(256, 256))
       np.testing.assert_almost_equal(a.numpy(), b.numpy(), decimal=5)
+
+  def test_multiple_to_single_device_naive(self):
+    t = _test_multiple_to_single_device(0)
+    self.assertEqual(t.device, Device.DEFAULT)
+
+  @unittest.skip("TODO: ring allreduce ignores target device")
+  def test_multiple_to_single_device_ring(self):
+    t = _test_multiple_to_single_device(2)
+    self.assertEqual(t.device, Device.DEFAULT)
 
   def test_allreduce_all2all(self):
     with Context(ALL2ALL=2):
