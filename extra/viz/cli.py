f2e93938f0da82e229871e4808bfc4b8870aeddf
qazal <qazal.software@gmail.com>
2026-02-03 17:49:16 +0900

    first 10 samples of that --kernel

--- extra/viz/cli.py
+++ extra/viz/cli.py
@@ -3,7 +3,7 @@ import argparse, pathlib
 from typing import Iterator
 from tinygrad.viz import serve as viz
 from tinygrad.uop.ops import RewriteTrace
-from tinygrad.helpers import temp, ansistrip, colored, time_to_str
+from tinygrad.helpers import temp, ansistrip, colored, time_to_str, ansilen
 from test.null.test_viz import load_profile
 
 def optional_eq(val:dict, arg:str|None) -> bool: return arg is None or ansistrip(val["name"]) == arg
@@ -30,8 +30,6 @@ if __name__ == "__main__":
   parser.add_argument('--select', type=str, default=None, metavar="NAME",
                       help='Rewrites: Select an item within the chosen kernel (optional name, default: only list names)')
   parser.add_argument('--profile', action="store_true", help="View profiling trace (default: views rewrites)")
-  parser.add_argument('--agg', action="store_true", default=True,
-                      help="Profile only: View aggregate table or all kernels (like DEBUG=2) (default: true)")
   parser.add_argument('--device', type=str, default=None, metavar="NAME", help="Profile only: Select a device (default: prints all devices)")
   args = parser.parse_args()
 
@@ -41,20 +39,23 @@ if __name__ == "__main__":
   if args.profile:
     from tabulate import tabulate
     profile = load_profile(viz.load_pickle(pathlib.Path(temp("profile.pkl", append_user=True)), default=[]))
-    agg, total = {}, 0
+    agg, total, n = {}, 0, 0
     for k,v in profile["layout"].items():
       if not optional_eq({"name":k}, args.device): continue
       print(k)
       if args.device is None: continue
       for e in v.get("events", []):
         et = e["dur"]*1e-6
-        if not args.agg:
-          ptm = colored(time_to_str(et, w=9), "yellow" if et > 0.01 else None) if et is not None else ""
-          print(e["name"], ptm,  " "+e["fmt"].replace("\n", " | ")+"  ")
-        a = agg.setdefault(e["name"], [0.0, 0])
-        a[0] += et
-        a[1] += 1
-        total += et
+        if args.kernel is not None:
+          if ansistrip(e["name"]) == args.kernel and n < 10:
+            ptm = colored(time_to_str(et, w=9), "yellow" if et > 0.01 else None) if et is not None else ""
+            print(f"{e['name']+" "*(46-ansilen(e['name']))} {ptm}/{(et or 0)*1e3:9.2f}ms  {e['fmt'].replace('\n', ' | ')}  ")
+            n += 1
+        else:
+          a = agg.setdefault(e["name"], [0.0, 0])
+          a[0] += et
+          a[1] += 1
+          total += et
 
     if agg:
       rows = [[n, t, time_to_str(t, w=9), t / c if c else 0.0, c, (t / total * 100.0) if total else 0.0] for n, (t, c) in agg.items()]
