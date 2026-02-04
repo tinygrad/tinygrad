43ef24a8afbe07095a581e1aeaae57006657f7e3
chenyu <chenyu@fastmail.com>
2026-02-04 15:03:47 -0500

    remove buf_target [pr] (#14540)
    not really needed

--- tinygrad/codegen/gpudims.py
+++ tinygrad/codegen/gpudims.py
@@ -88,8 +88,7 @@ def add_gpudims(ctx:Renderer, s:UOp):
   subs = {}
   for r in s_topo:
     # look for local INDEXes that are not used in the GLOBAL store, then add them as an INVALID
-    if r.op is Ops.STORE and r.buf_target().ptrdtype.addrspace == AddrSpace.GLOBAL:
-      idx = r.src[0]
+    if r.op is Ops.STORE and (idx := r.src[0]).src[0].ptrdtype.addrspace == AddrSpace.GLOBAL:
       missing_locals = [all_ranges[rng] for rng in local_dims if all_ranges[rng] not in idx.ranges]
       if len(missing_locals):
         assert len(idx.src) == 2, "index has 2 sources"
