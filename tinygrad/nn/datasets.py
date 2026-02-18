9c06898222695bcea75ce6521504f76d5b357f73
George Hotz <geohot@gmail.com>
2026-02-18 18:10:02 +0800

    mnist should be contiguious

--- tinygrad/nn/datasets.py
+++ tinygrad/nn/datasets.py
@@ -4,8 +4,10 @@ from tinygrad.nn.state import tar_extract
 def mnist(device=None, fashion=False):
   base_url = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/" if fashion else "https://storage.googleapis.com/cvdf-datasets/mnist/"
   def _mnist(file): return Tensor.from_url(base_url+file, gunzip=True)
-  return _mnist("train-images-idx3-ubyte.gz")[0x10:].reshape(-1,1,28,28).to(device), _mnist("train-labels-idx1-ubyte.gz")[8:].to(device), \
-         _mnist("t10k-images-idx3-ubyte.gz")[0x10:].reshape(-1,1,28,28).to(device), _mnist("t10k-labels-idx1-ubyte.gz")[8:].to(device)
+  return _mnist("train-images-idx3-ubyte.gz")[0x10:].reshape(-1,1,28,28).to(device).contiguous(), \
+         _mnist("train-labels-idx1-ubyte.gz")[8:].to(device).contiguous(), \
+         _mnist("t10k-images-idx3-ubyte.gz")[0x10:].reshape(-1,1,28,28).to(device).contiguous(), \
+         _mnist("t10k-labels-idx1-ubyte.gz")[8:].to(device).contiguous()
 
 def cifar(device=None):
   tt = tar_extract(Tensor.from_url('https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz', gunzip=True))
