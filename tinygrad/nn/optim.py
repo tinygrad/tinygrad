9fe0bd4459733bce87602a6e149c87ede4b8bc16
George Hotz <geohot@gmail.com>
2026-02-20 12:15:01 +0800

    Revert "no contigs there"
    This reverts commit c45bb9fcfd5b5ee7a7cee7fc6b247cdbb56852ff.

--- tinygrad/nn/optim.py
+++ tinygrad/nn/optim.py
@@ -27,8 +27,8 @@ class Optimizer:
 
   def _new_optim_param(self) -> list[Tensor]:
     param_dtype = to_dtype(getenv("OPTIM_DTYPE", "float32"))
-    if self.fused: return [Tensor.zeros(self.pos_params[-1], dtype=param_dtype, device=self.device, requires_grad=False).contiguous()]
-    return [Tensor.zeros_like(t, dtype=param_dtype, requires_grad=False).contiguous() for t in self.params]
+    if self.fused: return [Tensor.zeros(self.pos_params[-1], dtype=param_dtype, device=self.device, requires_grad=False)]
+    return [Tensor.zeros_like(t, dtype=param_dtype, requires_grad=False) for t in self.params]
 
   def zero_grad(self):
     """
@@ -154,7 +154,7 @@ class LAMB(Optimizer):
   def __init__(self, params: list[Tensor], lr=0.001, b1=0.9, b2=0.999, eps=1e-6, weight_decay=0.0, adam=False, fused=FUSE_OPTIM):
     super().__init__(params, lr, fused)
     self.b1, self.b2, self.eps, self.wd, self.adam = b1, b2, eps, weight_decay, adam
-    self.b1_t, self.b2_t = (Tensor.ones((1,), dtype=dtypes.float32, device=self.device, requires_grad=False).contiguous() for _ in [b1, b2])
+    self.b1_t, self.b2_t = (Tensor.ones((1,), dtype=dtypes.float32, device=self.device, requires_grad=False) for _ in [b1, b2])
     self.m = self._new_optim_param()
     self.v = self._new_optim_param()
 
