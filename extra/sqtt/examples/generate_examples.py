a5c0bdfca8fc35ebf848ccebc7e806f7852a74ce
qazal <qazal.software@gmail.com>
2026-02-23 13:06:12 +0800

    sqtt examples generator changes from inst_discovery

--- extra/sqtt/examples/generate_examples.py
+++ extra/sqtt/examples/generate_examples.py
@@ -1,4 +1,4 @@
-import os, subprocess
+import os, subprocess, sys
 from pathlib import Path
 from tinygrad.helpers import temp
 
@@ -6,9 +6,9 @@ EXAMPLES_DIR = Path(__file__).parent
 PROFILE_PATH = Path(temp("profile.pkl", append_user=True))
 
 EXAMPLES = [
-  "test.backend.test_custom_kernel.TestCustomKernel.test_empty",
-  "test.test_tiny.TestTiny.test_plus",
-  "test.test_tiny.TestTiny.test_gemm",
+  "test/backend/test_custom_kernel.py TestCustomKernel.test_empty",
+  "test/test_tiny.py TestTiny.test_plus",
+  "test/test_tiny.py TestTiny.test_gemm",
 ]
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
   (EXAMPLES_DIR/arch).mkdir(exist_ok=True)
   for test in EXAMPLES:
     for i in range(2):
-      subprocess.run(["python", "-m", "unittest", test], cwd=EXAMPLES_DIR.parent.parent.parent,
-                     env={**os.environ, "AMD":"1", "SQTT_LIMIT_SE":"-1", "VIZ":"-2"}, check=True)
+      subprocess.run([sys.executable, *test.split()], cwd=EXAMPLES_DIR.parent.parent.parent,
+                     env={**os.environ, "AMD":"1", "AM_RESET":"1", "VIZ":"-2", "PYTHONPATH":"."})
       PROFILE_PATH.rename(dest:=EXAMPLES_DIR/arch/f"profile_{test.split('.')[-1].replace('test_', '')}_run_{i}.pkl")
       print(f"saved SQTT trace to {dest}")
