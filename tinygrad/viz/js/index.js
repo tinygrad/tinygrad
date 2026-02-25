49ef135a8b7f1c12d6c53aab3894ed99a89aaaec
qazal <qazal.software@gmail.com>
2026-02-25 15:01:46 +0200

    str

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -52,7 +52,7 @@ function intersectRect(r1, r2) {
 
 function addTags(root) {
   root.selectAll("circle").data(d => [d]).join("circle").attr("r", 5);
-  root.selectAll("text").data(d => [d]).join("text").text(d => d).attr("dy", `0.35em`);
+  root.selectAll("text").data(d => [d]).join("text").text(d => d).attr("dy", "0.35em");
 }
 
 const drawGraph = (data) => {
