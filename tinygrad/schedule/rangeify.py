876b264e06425bad5c357d6d238bc38abdb8b4f6
Chen-Yu Yang <chenyu@fastmail.com>
2026-02-27 15:05:19 -0500

    replaced

--- tinygrad/schedule/rangeify.py
+++ tinygrad/schedule/rangeify.py
@@ -230,8 +230,8 @@ def remove_bufferize(src:UOp, buf:UOp, idx:UOp):
   # if it makes it here, the bufferize is removed
   # this is the ranges replaced
   # NOTE: if buf src is a const, we don't replace it. if idx is Invalid (dead load), don't replace it either
-  subs = {k:v for k,v in zip(buf.src[1:], idx.src[1:]) if k.op is not Ops.CONST and not (v.op is Ops.CONST and v.arg is Invalid)}
-  return src.substitute(subs, extra_pm=pm_gate_substitute)
+  replaced = {k:v for k,v in zip(buf.src[1:], idx.src[1:]) if k.op is not Ops.CONST and not (v.op is Ops.CONST and v.arg is Invalid)}
+  return src.substitute(replaced, extra_pm=pm_gate_substitute)
 
 def remove_noop_bufferize(idx,b2):
   if idx.src[1:] != b2.src[1:] or idx.src[0].op is Ops.BUFFER_VIEW: return None
