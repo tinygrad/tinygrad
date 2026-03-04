9c9f8a1d8e699d861d3ed5691a81c72143b4d147
Roelof van Dijk <3604013+roelofvandijk@users.noreply.github.com>
2026-03-04 18:57:23 +0100

    fix: can't import on non-metal

--- test/unit/test_metal_graph.py
+++ test/unit/test_metal_graph.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import MagicMock
 from tinygrad import Device
 from tinygrad.engine.realize import CompiledRunner
-from tinygrad.runtime.graph.metal import MetalGraph
 
 def _ei(*offsets):
   ei = MagicMock()
@@ -12,14 +11,16 @@ def _ei(*offsets):
 
 @unittest.skipUnless(Device.DEFAULT == "METAL", "Metal device required to run")
 class TestMetalGraph(unittest.TestCase):
-  def setUp(self): self.dev = Device[Device.DEFAULT]
+  def setUp(self):
+    from tinygrad.runtime.graph.metal import MetalGraph
+    self.MetalGraph = MetalGraph
+    self.dev = Device[Device.DEFAULT]
 
   def test_supports_exec_item_normal_offset(self):
-    assert MetalGraph.supports_exec_item([self.dev], _ei(0, 100, 0xFFFFFFFF)) is True
+    assert self.MetalGraph.supports_exec_item([self.dev], _ei(0, 100, 0xFFFFFFFF)) is True
 
   def test_supports_exec_item_overflow_offset(self):
-    # overflow is rejected before reaching GraphRunner.supports_exec_item, so devs doesn't matter
-    assert MetalGraph.supports_exec_item([], _ei(0, 0x100000000)) is False
+    assert self.MetalGraph.supports_exec_item([], _ei(0, 0x100000000)) is False
 
 if __name__ == "__main__":
   unittest.main()
