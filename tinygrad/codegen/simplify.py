f237322b3271672a61621ff8ba37ae183b375968
Christopher Milan <chrismilan@ucla.edu>
2026-03-12 14:02:59 +0800

    only replace if range is always guarded

--- tinygrad/codegen/simplify.py
+++ tinygrad/codegen/simplify.py
@@ -18,7 +18,7 @@ pm_flatten_range = PatternMatcher([
 ])
 
 def count_divmod(x:UOp) -> int: return sum(u.op in {Ops.IDIV, Ops.MOD} for u in x.backward_slice)
-def simplify_merge_adjacent(u:UOp) -> UOp|None:
+def simplify_merge_adjacent(ctx, u:UOp) -> UOp|None:
   reduce_ranges = [x.ranges for x in u.backward_slice_with_self if x.op is Ops.REDUCE]
   # on END we only want to merge adjacent ranges, on REDUCE we want to try all combinations
   for r0, r1 in (zip(u.ended_ranges, u.ended_ranges[1:]) if u.op is Ops.END else itertools.permutations(u.ended_ranges, 2)):
