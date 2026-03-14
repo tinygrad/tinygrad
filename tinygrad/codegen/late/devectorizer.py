060cd86030265197a1e103bd9bc266cac57448b8
Christopher Milan <chrismilan@ucla.edu>
2026-03-14 10:07:03 +0000

    a lot neater

--- tinygrad/codegen/late/devectorizer.py
+++ tinygrad/codegen/late/devectorizer.py
@@ -188,17 +188,12 @@ def _do_image_fixup(dt:ImageDType, idx:UOp) -> tuple[UOp, UOp]:
   x, valid = idx.src[1].get_idx(), idx.src[1].get_valid()
   h, w = dt.shape[0], dt.shape[1]
   if IMAGE == 1 and valid is not None:
-    print(f"IMAGE SHAPE SEARCH: ({h=} {w=})")
-    max_dropped, min_complexity = -1, float("inf")
-    for h_, w_ in ImageDType.valid_dims(dt):
-      dropped = len(_drop_valid_stmts(valid, _idx:=uop_given_valid(valid, UOp.vectorize((x//4)%w_, x//(4*w_)).simplify()), h_, w_))
-      complexity = len(_idx.gep(1).backward_slice)
-      if dropped > max_dropped or (dropped == max_dropped and complexity < min_complexity):
-        print(f"  . {h_}x{w_} -- {dropped=} {complexity=}")
-        h, w, max_dropped, min_complexity = h_, w_, dropped, complexity
-      else: print(f"  x {h_}x{w_} -- {dropped=} {complexity=}")
-    print(f"IMAGE SHAPE SELECTED: {h=} {w=} (dropped={max_dropped}, complexity={min_complexity})")
-    buf = buf.replace(dtype=(dtypes.imageh if dt.itemsize==2 else dtypes.imagef)((h, w, 4), w * 4 * dt.itemsize))
+    h, w = max(ImageDType.valid_dims(dt), key=lambda hw:
+                # maximize number of valids removed
+               (len(_drop_valid_stmts(valid, idx:=uop_given_valid(valid, UOp.vectorize((x//4)%hw[1], x//(4*hw[1])).simplify()), *hw)),
+                # and minimize idx complexity (number of nodes)
+                -len(idx.gep(1).backward_slice)))
+    buf = buf.replace(dtype=(dtypes.imageh if dt.itemsize == 2 else dtypes.imagef)((h, w, 4), w * 4 * dt.itemsize))
   oidx = UOp(Ops.VECTORIZE, dtypes.index.vec(2), ((x // 4) % w, (x // (4*w))))
   return x, idx.replace(src=(buf, oidx.valid(valid)))
 
