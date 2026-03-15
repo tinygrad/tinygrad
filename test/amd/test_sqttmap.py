4445f503567171e2cc6820a3726aab8018e8d095
qazal <77887910+Qazalin@users.noreply.github.com>
2026-03-15 23:06:19 +0200

    viz: variable duration rdna barriers (#15277)
    * viz: variable length rdna barriers
    
    * work
    
    * tiny changes
    
    * simple wave simd test
    
    * small wave sync test
    
    * good multi barrier bug find
    
    * simple fix
    
    * wave_sync asserts
    
    * rdna4 work
    
    * more rdna4
    
    * find more bugs in my model
    
    * it's so much simpler
    
    * wave_sync tests duration
    
    * r4
    
    * should just call this rdna4

--- test/amd/test_sqttmap.py
+++ test/amd/test_sqttmap.py
@@ -64,6 +64,8 @@ class TestSQTTMapBase(unittest.TestCase):
 
   def test_rocprof_inst_traces_match(self):
     for name, (events, kern_events, target) in self.examples.items():
+      if "sync" in name and self.target.startswith("gfx12"):
+        self.skipTest("our timestamps are off by a few cycles because rocprof patches timestamps for rdna4 barriers")
       for event in events:
         if not event.itrace: continue
         if event.kern not in kern_events: continue
@@ -81,7 +83,6 @@ class TestSQTTMapBase(unittest.TestCase):
           mean = sum(frequency) / len(frequency)
           variance = sum((v - mean) ** 2 for v in frequency) / len(frequency)
           self.assertGreater(mean, 0)
-          self.assertGreater(variance, 0)
           if DEBUG >= 2: print(f"{name:20s} SE:{event.se} {mean/1e9:.2f} GHz mean, {variance/1e18:.2f} GHz^2 variance")
           events = [e for e in timeline if type(e).__name__ == "ProfileRangeEvent"]
           insts, execs = 0, 0
@@ -90,9 +91,20 @@ class TestSQTTMapBase(unittest.TestCase):
               if "ALT" not in e.name.display_name: execs += 1
             elif "WAVE" in e.device:
               # sopk/immediates don't get ALU/MEM EXEC
-              if e.name.display_name not in {"IMMEDIATE", "IMMEDIATE_MASK", "JUMP", "JUMP_NO", "MESSAGE"}: insts += 1
+              if e.name.display_name not in {"IMMEDIATE", "IMMEDIATE_MASK", "JUMP", "JUMP_NO", "MESSAGE", "BARRIER", "BARRIER_SIGNAL"}: insts += 1
             else: raise Exception(f"timeline row must be INST or EXEC, got {e.device}")
           self.assertEqual(execs, insts)
+
+  def test_wave_sync(self):
+    for name, (events, kern_events, target) in self.examples.items():
+      for event in events:
+        wave_barriers = {}
+        for e in sqtt_timeline(event.blob, kern_events[event.kern].lib, target):
+          if type(e).__name__ == "ProfileRangeEvent" and e.name.display_name == "BARRIER": wave_barriers.setdefault(e.device, []).append(e)
+        if not wave_barriers: continue
+        for row, events in wave_barriers.items():
+          for e in events:
+            assert e.en-e.st > 1, f"all barriers must have a duration greater than 1, got {e}"
 
 class TestSQTTMapRDNA3(TestSQTTMapBase): target = "gfx1100"
 
