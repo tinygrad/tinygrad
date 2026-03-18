2f7c6eea26a857440365ebadb8b68925cd37fda2
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-03-18 04:20:09 -0700

    test

--- test/null/test_tensor.py
+++ test/null/test_tensor.py
@@ -170,5 +170,9 @@ class TestRand(unittest.TestCase):
     Tensor.rand(2**17, 2**17).schedule()
     Tensor.rand(2**17, 2**17).schedule()
 
+class TestTensorDevice(unittest.TestCase):
+  def test_create_from_single_device_tuple(self):
+    (Tensor([1.0, 2.0, 3.0], device=(Device.DEFAULT,)).reshape(1, -1, 1, 1) + Tensor.rand(1, 3, 4, 4)).realize()
+
 if __name__ == '__main__':
   unittest.main()
