b7f7dd4764ffd34a4c9084ef8f6bda0d4e0afb67
Christopher Milan <chrismilan@ucla.edu>
2026-03-19 14:31:08 +0800

    unfoldable image loads are buffer loads

--- tinygrad/codegen/late/devectorizer.py
+++ tinygrad/codegen/late/devectorizer.py
@@ -202,24 +202,13 @@ def get_image_idx(idx:UOp, width:int):
 
 def image_fixup(ls:UOp):
   # normal image load or store, with the CAST from expand_index
-  if ls.src[0].op is Ops.CAST and isinstance(image_dtype:=ls.src[0].src[0].dtype, ImageDType):
+  if isinstance(dt:=ls.src[0].src[0].dtype, ImageDType) and ls.src[0].op is Ops.CAST:
     assert ls.src[0].dtype.count == 4, "image must be casted to 4"
-    idx = get_image_idx(ls.src[0].src[0], image_dtype.shape[1])
-    return ls.replace(src=(idx,)+ls.src[1:])
+    return ls.replace(src=(get_image_idx(ls.src[0].src[0], dt.shape[1]),)+ls.src[1:])
 
-  # this is an unprocessed image without a cast, aka unfoldable image load. this doesn't work for stores
-  if isinstance(image_dtype:=ls.src[0].src[0].dtype, ImageDType) and ls.src[0].src[1].get_idx().dtype != dtypes.weakint.vec(2):
-    assert ls.op is Ops.LOAD, "if an image store isn't upcasted to 4, we can't store it"
-    x, idx = ls.src[0].src[1].get_idx(), get_image_idx(ls.src[0], image_dtype.shape[1])
-    vec_load = ls.replace(dtype=ls.dtype.vec(4), src=(idx,)+ls.src[1:])
-    # image pixels have 4 channels (.xyzw), select channel based on x % 4
-    x_mod_4 = x % 4
-    def sel(ret, i): return x_mod_4.ne(i).where(ret, vec_load.gep(i))
-    # if x is non-negative, x % 4 is in [0, 3] and we can skip NAN fallback
-    if x_mod_4.vmin >= 0: return functools.reduce(sel, range(int(x_mod_4.vmin)+1, int(x_mod_4.vmax)+1), vec_load.gep(int(x_mod_4.vmin)))
-    return functools.reduce(sel, range(4), ls.const_like(float('nan')))
-
-  return None
+  # this is an unprocessed image without a cast, we should just make it a buffer
+  if isinstance(dt, ImageDType) and (off:=ls.src[0].src[1]).get_idx().dtype != dtypes.weakint.vec(2):
+    return ls.replace(src=(ls.src[0].src[0].replace(dtype=(dtypes.half if dt.itemsize == 2 else dtypes.float).ptr(dt.size)).index(off), *ls.src[1:]))
 
 correct_load_store = PatternMatcher([
   # split LOAD/STORE
