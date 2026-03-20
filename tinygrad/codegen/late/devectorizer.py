397fa9bfc9c2286366e79651469baf94438fdba0
Christopher Milan <chrismilan@ucla.edu>
2026-03-20 15:15:12 +0800

    properly replace unfoldable loads

--- tinygrad/codegen/late/devectorizer.py
+++ tinygrad/codegen/late/devectorizer.py
@@ -208,8 +208,8 @@ def image_fixup(ls:UOp):
 
   # this is an unprocessed image without a cast, we should just make it a buffer
   if isinstance(dt, ImageDType) and (off:=ls.src[0].src[1]).get_idx().dtype != dtypes.weakint.vec(2):
-    idx = ls.src[0].src[0].replace(dtype=(dtypes.half if dt.itemsize == 2 else dtypes.float).ptr(dt.size)).index(off)
-    return ls.replace(src=(idx,)).cast(dtypes.float) if ls.op is Ops.LOAD else ls.replace(src=(idx, ls[1].cast(dtypes.float)))
+    idx = ls.src[0].src[0].replace(dtype=(new_dt:=dtypes.half if dt.itemsize == 2 else dtypes.float).ptr(dt.size)).index(off)
+    return ls.replace(src=(idx,), dtype=new_dt).cast(dtypes.float) if ls.op is Ops.LOAD else ls.replace(src=(idx, ls[1].cast(dtypes.float)))
 
 correct_load_store = PatternMatcher([
   # split LOAD/STORE
