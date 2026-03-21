1d879e46f62df940f1aec65f16e1f483cbf77877
George Hotz <geohot@gmail.com>
2026-03-21 11:40:45 +0800

    cleanups

--- extra/gemm/amd_copy_matmul.py
+++ extra/gemm/amd_copy_matmul.py
@@ -63,23 +63,23 @@ def block_128x128_gemm(c:UOp, a:UOp, b:UOp) -> UOp:
   acc = acc.after(acc.store(UOp.const(dtypes.float, 0).reshape((1,)*len(acc.shape)).expand(acc.shape)))
 
   if use_wmma:
-    A_tiles = A_local.reshape(WAVES_M, TM // WMMA_ACC, WMMA_M, BLOCK_K // WMMA_K, WMMA_K)
-    B_tiles = B_local.reshape(WAVES_N, TN, WMMA_N, BLOCK_K // WMMA_K, WMMA_K)
-
     k = UOp.range(BLOCK_K // WMMA_K, 4, AxisType.REDUCE)
     tile_m = UOp.range(TM // WMMA_ACC, 5, AxisType.LOOP)
     tile_n = UOp.range(TN, 6, AxisType.LOOP)
 
-    k_upcast_a = UOp.range(WMMA_K, 301, axis_type=AxisType.UPCAST)
-    a_frag = A_tiles[wave_m, tile_m, lane_n, k, k_upcast_a].contract(k_upcast_a)
-    k_upcast_b = UOp.range(WMMA_K, 311, axis_type=AxisType.UPCAST)
-    b_frag = B_tiles[wave_n, tile_n, lane_n, k, k_upcast_b].contract(k_upcast_b)
+    a_frag = A_local.reshape(WAVES_M, TM // WMMA_ACC, WMMA_M, BLOCK_K // WMMA_K, WMMA_K)[wave_m, tile_m, lane_n, k]
+    b_frag = B_local.reshape(WAVES_N, TN, WMMA_N, BLOCK_K // WMMA_K, WMMA_K)[wave_n, tile_n, lane_n, k]
+    acc_load = acc.reshape(TM // WMMA_ACC, WMMA_ACC, TN).permute(0,2,1).after(k_tile, k, tile_m, tile_n)[tile_m, tile_n]
 
-    acc_ref = acc.reshape(TM // WMMA_ACC, WMMA_ACC, TN).after(k_tile, k, tile_m, tile_n)
-    acc_load = UOp(Ops.VECTORIZE, dtypes.float.vec(WMMA_ACC), tuple(acc_ref[tile_m, e, tile_n] for e in range(WMMA_ACC)))
+    # TODO: remove unneeded CONTRACTS
+    k_upcast_a = UOp.range(WMMA_K, 301, axis_type=AxisType.UPCAST)
+    k_upcast_b = UOp.range(WMMA_K, 311, axis_type=AxisType.UPCAST)
+    acc_upcast = UOp.range(WMMA_ACC, 302, axis_type=AxisType.UPCAST)
     wmma_arg = ('WMMA_16_16_16_half_float', (16, 16, 16), dtypes.half, dtypes.float, 'AMD', 32,
                 (((301, 16),), ((311, 16),), ((302, WMMA_ACC),)), ())
-    out = UOp(Ops.WMMA, dtypes.float.vec(WMMA_ACC), (a_frag, b_frag, acc_load), arg=wmma_arg)
+    out = UOp(Ops.WMMA, dtypes.float.vec(WMMA_ACC), (a_frag[k_upcast_a].contract(k_upcast_a),
+                                                     b_frag[k_upcast_b].contract(k_upcast_b),
+                                                     acc_load[acc_upcast].contract(acc_upcast)), arg=wmma_arg)
 
     acc_wr = acc.reshape(TM // WMMA_ACC, WMMA_ACC, TN)
     acc_store = UOp.group(*[acc_wr[tile_m, e, tile_n].store(out.gep(e)) for e in range(WMMA_ACC)]).end(tile_m, tile_n)
