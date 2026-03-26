e2bf34f166e3096a1b1f66fd24ef73a40f5fd65c
Chen-Yu Yang <chenyu@fastmail.com>
2026-03-26 08:37:23 +0800

    Tensor.sub to mixin
    also _broadcasted skipped broadcasting shape if it does not have shape

--- tinygrad/mixin/__init__.py
+++ tinygrad/mixin/__init__.py
@@ -9,5 +9,9 @@ class OpMixin(ElementwiseMixin, MovementMixin):
   def _broadcasted(self, y, reverse=False) -> tuple[Self, Self]:
     if not isinstance(y, type(self)): y = self.ufix(y)
     x, y = (self, y) if not reverse else (y, self)
-    out_shape, out_dtype = _broadcast_shape(x.shape, y.shape), least_upper_dtype(x.dtype, y.dtype)
-    return x._broadcast_to(out_shape).cast(out_dtype), y._broadcast_to(out_shape).cast(out_dtype)
+    try:
+      out_shape = _broadcast_shape(x.shape, y.shape)
+      x, y = x._broadcast_to(out_shape), y._broadcast_to(out_shape)
+    except RuntimeError: pass
+    out_dtype = least_upper_dtype(x.dtype, y.dtype)
+    return x.cast(out_dtype), y.cast(out_dtype)
