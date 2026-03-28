d4848fea68dd752ca181bb3cf38f14abf4aa9da5
qazal <qazal.software@gmail.com>
2026-03-28 12:22:10 +0000

    work

--- tinygrad/viz/js/index.js
+++ tinygrad/viz/js/index.js
@@ -947,7 +947,7 @@ async function main() {
       }
       appendSteps(ul, i, steps);
     }
-    return setState({ currentCtx:-1 });
+    return setState({ currentCtx: 5, currentStep: 2, currentRewrite: 0, expandSteps: true });
   }
   // ** center graph
   const { currentCtx, currentStep, currentRewrite, expandSteps } = state;
