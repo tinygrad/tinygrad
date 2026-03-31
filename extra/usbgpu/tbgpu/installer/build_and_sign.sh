a03988501cf993733a222573beb5bb6c98abc84f
nimlgen <lenyaprokh@gmail.com>
2026-03-31 11:43:20 +0300

    new profiles

--- extra/usbgpu/tbgpu/installer/build_and_sign.sh
+++ extra/usbgpu/tbgpu/installer/build_and_sign.sh
@@ -3,8 +3,8 @@ set -e
 
 xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -alltargets -configuration Release build
 
-cp "../profiles/driver2_release.provisionprofile" "./build/Release/TinyGPU.app/Contents/Library/SystemExtensions/org.tinygrad.tinygpu.driver2.dext/embedded.provisionprofile"
-cp "../profiles/installer_provisioning.provisionprofile" "./build/Release/TinyGPU.app/Contents/embedded.provisionprofile"
+cp "../profiles/driver_release_0431.provisionprofile" "./build/Release/TinyGPU.app/Contents/Library/SystemExtensions/org.tinygrad.tinygpu.driver2.dext/embedded.provisionprofile"
+cp "../profiles/installer_release_0431.provisionprofile" "./build/Release/TinyGPU.app/Contents/embedded.provisionprofile"
 
 codesign \
     --sign "Developer ID Application: tinygrad, Corp. (9YG3G8543N)" \
