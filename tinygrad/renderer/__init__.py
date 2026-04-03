b8be46529e01c21758ca5f0f1372f1328c88a244
Christopher Milan <chrismilan@ucla.edu>
2026-04-02 21:20:03 -0700

    oops

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Callable, cast
 import functools
 from dataclasses import dataclass, field
-from tinygrad.helpers import to_function_name, dedup, prod, DEBUG
+from tinygrad.helpers import to_function_name, dedup, prod, Target, DEBUG
 from tinygrad.uop.ops import Ops, UOp, sym_infer, sint, Variable, ssimplify, smin, GroupOp, PatternMatcher, print_uops
 from tinygrad.dtype import AddrSpace, PtrDType
 from tinygrad.codegen.opt.tc import TensorCore
