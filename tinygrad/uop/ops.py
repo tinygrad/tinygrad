cd783f0f64752da86d8cc7480c50f5f3bb179db0
qazal <qazal.software@gmail.com>
2026-04-17 17:03:16 +0300

    tms also 0

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -1310,7 +1310,7 @@ if TRACK_MATCH_STATS or PROFILE:
   def launch_viz(env_str:str, data:str):
     os.environ[f"{env_str}_DATA"] = data
     if not TRACK_MATCH_STATS and not PROFILE:
-      os.environ["VIZ"], os.environ["PROFILE"] = "0", "0"
+      os.environ["VIZ"], os.environ["PROFILE"], os.environ["TRACK_MATCH_STATS"] = "0", "0", "0"
       args = ['--rewrites-path', os.getenv("REWRITE_DATA", "")] if os.getenv("REWRITE_DATA", "") else []
       args += ['--profile-path', os.getenv("PROFILE_DATA", "")] if os.getenv("PROFILE_DATA", "") else []
       viz_path = pathlib.Path(__file__).resolve().parent.parent / "viz" / "serve.py"
