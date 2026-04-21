d110e9899e6b60f3317418509fc8ec6f9f32eb8b
Christopher Milan <chrismilan@ucla.edu>
2026-04-21 11:39:57 -0700

    alive?

--- test/mockgpu/cuda/cuda.py
+++ test/mockgpu/cuda/cuda.py
@@ -167,5 +167,5 @@ def cuGetErrorString(error: int, pStr) -> int:
   error_str = orig_cuda.enum_cudaError_enum.get(error, "Unknown CUDA error").encode()
   buf = ctypes.create_string_buffer(error_str)
   # Set the pointer to point to our error string buffer
-  pStr.value = ctypes.cast(buf, ctypes.POINTER(ctypes.c_char))
+  pStr.value, pStr._obj = ctypes.cast(buf, ctypes.POINTER(ctypes.c_char)), buf
   return orig_cuda.CUDA_SUCCESS
