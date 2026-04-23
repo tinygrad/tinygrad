0920934ec79025c73f253d3ee74d52da8a6aa3ef
Nino Risteski <ninwork1@gmail.com>
2026-04-23 10:27:54 +0200

    test

--- test/unit/test_realize_stats.py
+++ test/unit/test_realize_stats.py
@@ -3,16 +3,14 @@ from tinygrad.helpers import Context, GlobalCounters
 from tinygrad.engine.realize import update_stats
 from tinygrad.renderer import Estimates
 
-def _stats(mem, lds=0):
-  GlobalCounters.reset()
-  out = io.StringIO()
-  with Context(DEBUG=2, NO_COLOR=1), contextlib.redirect_stdout(out):
-    update_stats("copy", "TEST", Estimates(mem=mem, lds=lds), {}, 1.0, 2)
-  return out.getvalue()
-
 def test_bandwidth_units():
   for mem, lds, expected in [
     (0, 0, "0|0      GB/s"),
     (200_000_000, 0, "200|0      MB/s"),
     (10_000_000_000_000, 0, "TB/s"),
-  ]: assert expected in _stats(mem, lds)
+  ]:
+    GlobalCounters.reset()
+    out = io.StringIO()
+    with Context(DEBUG=2, NO_COLOR=1), contextlib.redirect_stdout(out):
+      update_stats("copy", "TEST", Estimates(mem=mem, lds=lds), {}, 1.0, 2)
+    assert expected in out.getvalue()
