68c9b55e0938e0147b85b345ef47e4625956717f
qazal <qazal.software@gmail.com>
2026-04-23 21:17:24 +0300

    -t default

--- tinygrad/viz/cli.py
+++ tinygrad/viz/cli.py
@@ -203,11 +203,10 @@ def get_arg_parser() -> argparse.ArgumentParser:
   g_opts = parser.add_argument_group("optional args")
   g_opts.add_argument("-s", "--src", type=str, default=None, metavar="NAME", help="Select a data source (default: list all sources)")
   g_opts.add_argument("-i", "--item", type=str, default=None, metavar="NAME", help="Select an item within the source (default: list all items)")
-  g_opts.add_argument("-t", "--top", type=int, default=None, metavar="COUNT",
-                      help="Number of top kernels to aggregate (default: do not aggregate, set -1 to aggregate all)")
+  g_opts.add_argument("-t", "--top", nargs="?", type=int, const=20, metavar="COUNT", help="Aggregate top kernels (optional count, default 20)")
+  g_opts.add_argument("--json", action="store_true", help="Emit profiler output as JSON")
   g_opts.add_argument("--path", type=pathlib.Path, metavar="PATH", help="Optional path to saved pickle (default: latest pkl)",
                       default=pathlib.Path(temp("rewrites.pkl", append_user=True)))
-  g_opts.add_argument("--json", action="store_true", help="Emit profiler output as JSON")
   g_opts.add_argument("-h", "--help", action="help", help="show this help message and exit")
   return parser
 
