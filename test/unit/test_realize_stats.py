7364918366ff0af7451702176e95c413c0bc723b
Nino Risteski <ninwork1@gmail.com>
2026-04-23 10:11:21 +0200

    cleaner test

--- test/unit/test_realize_stats.py
+++ test/unit/test_realize_stats.py
@@ -1,22 +1,18 @@
-import io, unittest, contextlib
+import io, contextlib
 from tinygrad.helpers import Context, GlobalCounters
 from tinygrad.engine.realize import update_stats
 from tinygrad.renderer import Estimates
 
-class TestUpdateStats(unittest.TestCase):
-  def _stats(self, mem, lds=0):
-    GlobalCounters.reset()
-    out = io.StringIO()
-    with Context(DEBUG=2, NO_COLOR=1), contextlib.redirect_stdout(out):
-      update_stats("copy", "TEST", Estimates(mem=mem, lds=lds), {}, 1.0, 2)
-    return out.getvalue()
+def _stats(mem, lds=0):
+  GlobalCounters.reset()
+  out = io.StringIO()
+  with Context(DEBUG=2, NO_COLOR=1), contextlib.redirect_stdout(out):
+    update_stats("copy", "TEST", Estimates(mem=mem, lds=lds), {}, 1.0, 2)
+  return out.getvalue()
 
-  def test_bandwidth_units(self):
-    for mem, lds, expected in [
-      (0, 0, "0|0      GB/s"),
-      (200_000_000, 0, "200|0      MB/s"),
-      (10_000_000_000_000, 0, "TB/s"),
-    ]: assert expected in self._stats(mem, lds)
-
-if __name__ == '__main__':
-  unittest.main()
+def test_bandwidth_units():
+  for mem, lds, expected in [
+    (0, 0, "0|0      GB/s"),
+    (200_000_000, 0, "200|0      MB/s"),
+    (10_000_000_000_000, 0, "TB/s"),
+  ]: assert expected in _stats(mem, lds)
