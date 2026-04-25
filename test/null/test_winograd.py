dcdeea3ccefe428355b790a4a848e8abb2db0f2a
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-04-25 00:29:12 -0700

    batch 4

--- test/null/test_winograd.py
+++ test/null/test_winograd.py
@@ -18,14 +18,14 @@ class TestWinograd(unittest.TestCase):
   def test_forward_kernels(self):
     x,w = Tensor.rand(1,4,9,9).realize(), Tensor.rand(4,4,3,3).realize()
     out = Tensor.conv2d(x,w)
-    self.assertEqual(len(out.schedule()), 2)
+    self.assertEqual(len(out.schedule_linear().src), 2)
 
   def test_backward_kernels(self):
     x,w = Tensor.empty(1,4,9,9,requires_grad=True).realize(), Tensor.empty(4,4,3,3,requires_grad=True).realize()
     out = Tensor.conv2d(x,w, padding=1)
     out.mean().backward()
-    backward_schedule = Tensor.schedule(x.grad, w.grad)
-    self.assertEqual(len(backward_schedule), 4)
+    backward_schedule = x.grad.schedule_linear(w.grad)
+    self.assertEqual(len(backward_schedule.src), 4)
 
   def test_counters(self):
     IC, OC, X, Y = 4,4,9,9
