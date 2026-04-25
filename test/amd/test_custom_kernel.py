6ebd53aa919e39da70a55615334ce14c35d441f1
qazal <qazal.software@gmail.com>
2026-04-26 03:40:13 +0800

    VALU pipe 1

--- test/amd/test_custom_kernel.py
+++ test/amd/test_custom_kernel.py
@@ -117,13 +117,12 @@ def custom_handwritten(A:UOp, arch:str) -> UOp:
       k.emit(r4.s_mov_b32(s[30+i], i))
       k.emit(r4.s_mov_b32(s[40+i], i))
       k.emit(r4.s_mul_i32(s[14+i], s[12+i], 32))
-  # saturate vector ALU pipe
+  # saturate vector ALU pipe operating on 32 and 64 bits
   if "VALU" in pipes:
     for i in range(UNROLL_N):
       k.emit(r4.v_mov_b32_e32(v[20+i], i))
-      k.emit(r4.v_mov_b32_e32(v[30+i], i))
-      k.emit(r4.v_mov_b32_e32(v[40+i], i))
-      k.emit(r4.v_rcp_f32_e32(v[14+i], v[12+i]))
+      k.emit(r4.v_lshlrev_b64_e32(v[30+2*i:31+2*i], 2, v[12+i:13+i])) # 2c
+      k.emit(r4.v_mad_co_u64_u32(v[40+2*i:41+2*i], NULL, v[12+i], v[13+i], v[14+i:15+i])) # 4c
   k.emit(r4.s_add_co_i32(s[1], s[1], -1))
   k.emit(r4.s_cmp_eq_i32(s[1], 0))
   k.emit(r4.s_cbranch_scc0(), target="loop")
