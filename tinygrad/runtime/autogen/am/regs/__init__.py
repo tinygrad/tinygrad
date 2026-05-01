664b36734d9fba19aadb1c4d920d612bac7f9cd6
Christopher Milan <chrismilan@ucla.edu>
2026-04-30 19:52:51 -0700

    works on 9060 xt

---
+++ tinygrad/runtime/autogen/am/regs/__init__.py
@@ -0,0 +1,40 @@
+import pathlib, re, itertools
+from tinygrad.runtime.autogen import load
+from tinygrad.runtime.autogen.am import am_src
+
+# 9060xt
+__all__ = ["gc_12_0_0", "hdp_7_0_0", "mmhub_4_1_0", "mp_11_0", "mp_14_0_2", "nbif_6_3_1", "osssys_7_0_0"]
+
+patterns = {
+  # FIXME: CDNA4 naming
+  "gc": ["GCVM", "GCMC_VM", "CP_(HQD|MQD|MEC|ME_CNTL|PERFMON|RB_WPTR_POLL_CNTL|INT_CNTL|STAT|PFP_PRGRM|ME_PRGRM|COHER_START)", "COMPUTE_",
+         "SQ_PERFCOUNTER", "GL2C_PERFCOUNTER", "SQ_THREAD_TRACE", "SPI_(CONFIG_CNTL|COMPUTE_QUEUE_RESET)", "GRBM", "SH_MEM", "RLC", "TCP", "GB_ADDR_CONFIG",
+         "SDMA[01]_(WATCHDOG_CNTL|UTCL1_(CNTL|PAGE)|MCU_CNTL|F32_CNTL|CNTL($|_BASE_IDX$)|QUEUE0_|RLC_CGCG_CTRL)", "SCRATCH_REG[67]"],
+  # FIXME: CDNA4 naming
+  "mmhub": ["MMVM", "MMMC_VM", "MM_ATC_L2_MISC_CG"],
+  "nbif": ["BIF_BX_PF[01]_GPU_HDP_FLUSH", "BIF_BX_PF0_RSMU", "BIF_BX0_(REMAP_HDP_MEM_FLUSH_CNTL|BIF_DOORBELL_INT_CNTL|PCIE_INDEX2|PCIE_DATA2)",
+           "BIFC_(DOORBELL_ACCESS_EN_PF|GFX_INT_MONITOR_MASK)", "XCC_DOORBELL_FENCE", "DOORBELL0_CTRL_ENTRY", "GDC_S2A0_S2A_DOORBELL_ENTRY",
+           "S2A_DOORBELL_ENTRY", "RCC_DEV0_EPF0_RCC_DOORBELL_APER_EN", "RCC_DEV0_EPF2_STRAP2($|_BASE_IDX$)"],
+  "mp": ["MP([01]|ASP)_SMN_C2PMSG"],
+  "hdp": ["HDP_MEM_POWER_CTRL"],
+  "oss": ["IH_"]
+}
+
+def __getattr__(nm):
+  prefix = {"osssys": "oss"}.get(x:=nm.split("_", 1)[0], x)
+  def genreg(name, files, patterns=[], **kwargs):
+    def extract(lines, pat): return ((m.group(1), int(m.group(2), 0)) for l in lines if (m:=re.match(pat, l)))
+    def clean_name(name): return name[next((i for i,c in enumerate(name) if c.isupper()), len(name)):]
+
+    offset, sh_mask = pathlib.Path(f"{files[0]}_offset.h").read_text().splitlines(), pathlib.Path(f"{files[0]}_sh_mask.h").read_text().splitlines()
+    defs = {k:v for k,v in extract(offset, r'#define\s+((?:mm|reg)\S+)\s+(0x[\da-fA-F]+|\d+)') if any(re.match("(mm|reg)" + p, k) for p in patterns)}
+    fields = {reg: {name.split('__')[1].lower(): ((mask & -mask).bit_length() - 1, mask.bit_length() - 1) for name, mask in fields}
+              for reg, fields in itertools.groupby(extract(sh_mask, r'#define\s+(\S+)_MASK\s+(0x[\da-fA-F]+|\d+)'), lambda x: x[0].split('__')[0])}
+
+    regs = {reg: (off, defs[f"{reg}_BASE_IDX"], fields.get(clean_name(reg), {})) for reg,off in defs.items() if not reg.endswith("_BASE_IDX") and f"{reg}_BASE_IDX" in defs}
+    print(f"defined {len(regs)} registers for {name}")
+    return "\n".join(["regs = {"] + [f"{k!r}: {v!r}," for k,v in regs.items()] + ["}"])
+
+  assert prefix in patterns, f"Failed to load ASIC registers for {prefix.upper()}"
+  return load(f"am/regs/{nm}", [f"../amdgpu-33970e1351f5e511029602454979f3de7e22260f/drivers/gpu/drm/amd/include/asic_reg/{prefix}/{nm}"],
+                           patterns=patterns[prefix], gen=genreg)
--- tinygrad/runtime/support/amd.py
+++ tinygrad/runtime/support/amd.py
@@ -1,5 +1,4 @@
-import functools, re, urllib, tinygrad.runtime.autogen
-from collections import defaultdict
+import functools, re, tinygrad.runtime.autogen.am.regs
 from dataclasses import dataclass
 from tinygrad.helpers import getbits, fetch
 
@@ -76,35 +75,8 @@ def import_pmc(ip) -> dict[str, tuple[str, int]]:
   return res
 
 def import_asic_regs(prefix:str, version:tuple[int, ...], cls=AMDReg) -> dict[str, AMDReg]:
-  def _split_name(name): return name[:(pos:=next((i for i,c in enumerate(name) if c.isupper()), len(name)))], name[pos:]
-  def _extract_regs(txt):
-    x = {}
-    for k,v in {m.group(1): int(m.group(2), 0) for line in txt.splitlines() if (m:=re.match(r'#define\s+(\S+)\s+(0x[\da-fA-F]+|\d+)', line))}.items():
-      if k.startswith('VM_') or k.startswith('MC_'): x[prefix.upper()[:2]+k] = v
-      elif k.startswith('regVM_') or k.startswith('regMC_'): x["reg"+prefix.upper()[:2]+k[3:]] = v
-      else: x[k] = v
-    return x
-  def _download_file(ver, suff) -> str:
-    dir_prefix = {"osssys": "oss"}.get(prefix, prefix)
-    fetch_name = f"{prefix}_{'_'.join(map(str, ver))}_{suff}.h"
-    return header_download(f"include/asic_reg/{dir_prefix}/{fetch_name}", name=fetch_name, subdir="asic_regs")
-
   for ver in fixup_ip_version(prefix, version):
-    try: offs, sh_masks = _extract_regs(_download_file(ver, "offset")), _extract_regs(_download_file(ver, "sh_mask"))
-    except urllib.error.HTTPError as e:
-      if e.code == 404: continue
-      raise
-
-    offsets = {k:v for k,v in offs.items() if _split_name(k)[0] in {'reg', 'mm'} and not k.endswith('_BASE_IDX')}
-    bases = {k[:-len('_BASE_IDX')]:v for k,v in offs.items() if _split_name(k)[0] in {'reg', 'mm'} and k.endswith('_BASE_IDX')}
-
-    fields: defaultdict[str, dict[str, tuple[int, int]]] = defaultdict(dict)
-    for field_name, field_mask in sh_masks.items():
-      if not ('__' in field_name and field_name.endswith('_MASK')): continue
-      reg_name, reg_field_name = field_name[:-len('_MASK')].split('__')
-      if reg_name.startswith('MC_') or reg_name.startswith('VM_'): reg_name = f"{prefix.upper()[:2]}{reg_name}"
-      fields[reg_name][reg_field_name.lower()] = ((field_mask & -field_mask).bit_length()-1, field_mask.bit_length()-1)
-
-    # NOTE: Some registers like regGFX_IMU_FUSESTRAP in gc_11_0_0 are missing base idx, just skip them
-    return {reg:cls(name=reg, offset=off, segment=bases[reg], fields=fields[_split_name(reg)[1]]) for reg,off in offsets.items() if reg in bases}
+    if (nm:=f"{prefix}_{'_'.join(map(str, ver))}") not in tinygrad.runtime.autogen.am.regs.__all__: continue
+    regs = getattr(tinygrad.runtime.autogen.am.regs, nm).regs
+    return {reg:cls(name=reg, offset=off, segment=seg, fields=fields) for reg,(off,seg,fields) in regs.items()}
   raise ImportError(f"Failed to load ASIC registers for {prefix.upper()} {'.'.join(map(str, version))}")
