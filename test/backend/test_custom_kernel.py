2f0460ab507bb0e75535c79b341ace742cabf4e4
qazal <qazal.software@gmail.com>
2026-05-01 16:27:23 +0300

    kernel count

--- test/backend/test_custom_kernel.py
+++ test/backend/test_custom_kernel.py
@@ -324,7 +324,6 @@ class TestCustomKernel(unittest.TestCase):
   @unittest.expectedFailure
   def test_custom_kernel_sched_copy(self): self.test_custom_kernel_sched(use_custom=True)
 
-  @unittest.expectedFailure
   def test_sliced_buffer_function(self):
     x = Tensor.arange(32).reshape(8, 4).realize()
     from tinygrad import function
@@ -334,8 +333,8 @@ class TestCustomKernel(unittest.TestCase):
       return Tensor.custom_kernel(y, x, fxn=custom_add_one_kernel)[0]
     GlobalCounters.reset()
     y = run(x[0]).realize()
-    # it's copying the input and the output
-    self.assertEqual(GlobalCounters.kernel_count, 1)
+    # TODO: it's copying the output
+    self.assertEqual(GlobalCounters.kernel_count, 2)
     self.assertEqual(y.tolist(), [1, 2, 3, 4])
 
 class TestUOpReduce(unittest.TestCase):
