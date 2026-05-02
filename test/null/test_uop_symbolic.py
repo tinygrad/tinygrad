559af3df847e19e98bd2f6e1efdd8c4689222840
Bautista <bautax1234@gmail.com>
2026-05-02 11:58:21 -0300

    test for masked cast branch in redundant mask folding

--- test/null/test_uop_symbolic.py
+++ test/null/test_uop_symbolic.py
@@ -428,6 +428,10 @@ class TestSymbolic(unittest.TestCase):
     x = UOp.variable('x', 0, 255, dtype=dtypes.uint32)
     self.helper_test_variable((x & -4) >> 2, 0, 63, "(x>>2)", test_z3=False)
 
+  def test_masked_cast_fold(self):
+    x = UOp.variable('x', 0, 0xFFFFFFFF, dtype=dtypes.uint64)
+    self.assertEqual((x & 0xFFFFFFFF).cast(dtypes.uint32).simplify(), x.cast(dtypes.uint32))
+
   def test_bool_or_not_tautology(self):
     a = Variable("a", 0, 10)
     c = a<10
