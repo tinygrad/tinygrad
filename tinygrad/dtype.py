485fe1980de583ede167211d2bcbb7a843c27513
Christopher Milan <chrismilan@ucla.edu>
2026-05-08 12:19:34 -0700

    retain osx behavior

--- tinygrad/dtype.py
+++ tinygrad/dtype.py
@@ -139,11 +139,11 @@ class ImageDType(PtrDType):
   # get list of (height, width) that do not require pitch padding
   @staticmethod
   def valid_dims(ptr:PtrDType, arch:str) -> list[tuple[int,int]]:
-    ALIGN = next((int(p.split('=')[1]) for p in arch.split(',') if p.startswith("IMAGE_PITCH_ALIGNMENT="))) # 256 if OSX else 64
+    ALIGN = next((int(p.split('=')[1]) for p in arch.split(',') if p.startswith("IMAGE_PITCH_ALIGNMENT=")))
     MAXW, pxls = 16384, ptr.size // 4
     if ptr.base not in (dtypes.half, dtypes.float) or ptr.size > 4*MAXW*MAXW: return []
     # height=1 images just need to abide by alignment requirements in bytes, not pixels!
-    if ptr.size % (ALIGN * 4) != 0: return [] if ptr.nbytes() % ALIGN != 0 or pxls > MAXW else [(1, pxls)]
+    if ptr.size % (ALIGN * 4) != 0: return [] if ptr.nbytes() % (64 if OSX else ALIGN) != 0 or pxls > MAXW else [(1, pxls)]
     return [(pxls//ALIGN//k, ALIGN*k) for k in range(ceildiv(pxls//ALIGN, MAXW), min(pxls//ALIGN, MAXW//ALIGN)+1) if (pxls//ALIGN)%k == 0]
 
 class dtypes:
