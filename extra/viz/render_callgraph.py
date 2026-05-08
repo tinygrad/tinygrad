a02c7d778fc531aad863488796b14b798466c917
qazal <qazal.software@gmail.com>
2026-05-08 22:33:01 +0300

    better

--- extra/viz/render_callgraph.py
+++ extra/viz/render_callgraph.py
@@ -1,45 +1,38 @@
-# usage: DEBUG=5 python -m tinygrad.viz.cli --json | python ./extra/viz/render_callgraph.py
-import json, sys
+import json, sys, argparse
 from tinygrad.uop.ops import UOp, Ops
 from tinygrad.uop.spec import eval_pyrender
-from tinygrad.helpers import ansistrip, prod
-from tinygrad.viz.serve import uop_to_json, VizData
+from tinygrad.helpers import ansistrip, prod, temp, TracingKey, DEBUG
+from tinygrad.viz import serve as viz
+from tinygrad.renderer import Renderer
+from tinygrad.viz.cli import fmt_colored
 
-# decode the viz.cli UOp stream
-# TODO: using pickle is better here
-pending:str|None = None
-data:list[dict] = []
-for line in sys.stdin:
-  r = json.loads(line)
-  if pending:
-    if (v:=r.get("value")):
-      if v.startswith("UOp("): uop = eval_pyrender("ast = "+r["value"])
-      elif "ast = " in v: uop = eval_pyrender(r["value"])
-      else: continue
-      data.append({"name":pending, "uop":uop})
-    pending = None
-  if r.get("device", "") == "TINY" and (r["name"].startswith("Schedule") or r["name"].startswith("do_to_program")): pending = r["name"]
+parser = argparse.ArgumentParser()
+parser.add_argument("--rewrites-path", type=str, metavar="PATH", help="Optional path to rewrites.pkl (default: latest rewrites)",
+                    default=temp("rewrites.pkl", append_user=True))
+parser.add_argument("--query", type=str, metavar="NAME", help="kernel name")
+args = parser.parse_args()
+data = viz.VizData(viz.load_pickle(args.rewrites_path, default=None))
+ast = next(viz._reconstruct(data, data.trace.rewrites[i][0].sink) for i,k in enumerate(data.trace.keys) if ansistrip(k.display_name) == args.query)
+uop_names = {viz._reconstruct(data, data.trace.rewrites[i][0].sink):k.display_name for i,k in enumerate(data.trace.keys) if isinstance(k.ret, Renderer)}
 
-# map calls to programs / buffers
-uop_names = {v["uop"]:v["name"] for v in data}
-ast = next(v["uop"] for v in data if ansistrip(v["name"]).endswith(sys.argv[1])) if len(sys.argv) > 1 else None
-if ast is None: ast = [v["uop"] for v in data if v["name"].startswith("do_to_program")][-4]
-root = next(s for v in data for s in v["uop"].toposort() if s.op is Ops.CALL and s.src[0] is ast)
-op_w, buf_w = 4, 16
-viz_data = VizData() # TODO: it should not need this
-bufs:list[UOp] = []
-for c in root.toposort(enter_calls=False):
-  if c.op is not Ops.CALL: continue
-  buf_ids:dict[int, str] = {}
-  for i,u in enumerate(c.src[1:]):
-    while u.op is Ops.AFTER: u = u.src[0]
-    # TODO: this can always be the BUFFER once it reconstructs the param -> buffer mapping
-    assert u.op in {Ops.BUFFER, Ops.PARAM}
-    op_name = str(u.op).split(".")[1]
-    buf_ids[i] = st = f"{op_name[0].lower()}{u.arg}"
-    print(f"{op_name[:3]:<{op_w}} {st:<{buf_w}} {str(u.dtype):<8} {prod(u.size())}")
-  body = c.src[0]
-  print(f"{'CALL':<{op_w}} {uop_names.get(body, '<unknown>').removeprefix('do_to_program for ')}")
-  for u in body.toposort():
-    if u.op is Ops.INDEX:
-      print(f"{'I':<{op_w}} {buf_ids[(p:=u.src[0].arg)]:<{buf_w}} {p:<2} {' '.join(uop_to_json(viz_data, u)[id(u)]['label'].split('\n')[4:])}")
+for i,k in enumerate(data.trace.keys):
+  if not k.display_name.startswith("Schedule"): continue
+  if (sink:=next((viz._reconstruct(data, s.sink) for s in data.trace.rewrites[i] if s.name == "View Kernel Graph"), None)) is None: continue
+  if (root:=next((u for u in sink.toposort(enter_calls=False) if u.op is Ops.CALL and u.src[0] is ast), None)) is None: continue
+  for c in root.toposort(enter_calls=False):
+    if c.op is not Ops.CALL: continue
+    arg_str:list[str] = []
+    op_w, buf_w = 4, 16
+    for u in c.src[1:]:
+      while u.op is Ops.AFTER: u = u.src[0]
+      # TODO: this can always be the BUFFER UNIQUE once it reconstructs the param -> buffer mapping
+      assert u.op in {Ops.BUFFER, Ops.PARAM}
+      op_name = str(u.op).split(".")[1]
+      arg_str.append(st:=f"{op_name[0].lower()}{u.arg}")
+      print(f"{op_name[:3]:<{op_w}} {st:<{buf_w}} {str(u.dtype):<8} {prod(u.size())}")
+    body = c.src[0]
+    print(f"{'CALL':<{op_w}} {fmt_colored(uop_names.get(body, '<unknown>').removeprefix('do_to_program for '))}")
+    if DEBUG >= 3: print(body.pyrender())
+    for u in body.toposort():
+      if u.op is Ops.INDEX:
+        print(f"{'I':<{op_w}} {arg_str[(p:=u.src[0].arg)]:<{buf_w}} {p:<2} {' '.join(viz.uop_to_json(data, u)[id(u)]['label'].split('\n')[4:])}")
