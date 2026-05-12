00dcc1905955c0171974bab4969cc948791ea39e
mad360 <matt@dldipaolo.com>
2026-05-12 18:32:40 -0400

    fixup

--- examples/mlperf/scripts/download_llama2_70b_lora_dataset.py
+++ examples/mlperf/scripts/download_llama2_70b_lora_dataset.py
@@ -1,5 +1,4 @@
+from tinygrad.helpers import getenv
+from examples.mlperf.dataloader import download_llama2_70b_lora_dataset
 if __name__ == '__main__':
-  from tinygrad.helpers import getenv
-  from examples.mlperf.dataloader import download_llama2_70b_lora_dataset
-
   download_llama2_70b_lora_dataset(getenv("BASEDIR", "/raid/datasets/c4-70b-lora/"))
