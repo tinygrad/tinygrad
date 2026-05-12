86c94ea884341e1bd1a750ced67b4a1df3d040ec
mad360 <matt@dldipaolo.com>
2026-05-12 16:10:58 -0400

    remove unused, transfer AMD settings, touchups

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
+set -e  # Exit on any error
+set -o pipefail  # Make pipeline fail if any command fails
 
 export PYTHONPATH="."
 export DEFAULT_FLOAT="bfloat16" OPTIM_DTYPE="bfloat16"
 export MODEL="llama2_70b_lora"
 export NULL_ALLOW_COPYOUT=1
 export OFFLOAD_OPTIM=${OFFLOAD_OPTIM:-0}
-export FP8=${FP8:-0}
 export MASTER_WEIGHTS=${MASTER_WEIGHTS:-1}
 export LORA=1
-export WQKV=1
 
-export DEV="${DEV:-NULL}"
-export BS="${BS:-1}"
+export HK_FLASH_ATTENTION=${HK_FLASH_ATTENTION:-1}
+export LATE_ALLREDUCE=${LATE_ALLREDUCE:-0}
+export USE_ATOMICS=${USE_ATOMICS:-1}
+export ASM_GEMM=${ASM_GEMM:-1}
+export ALLREDUCE_CAST=${ALLREDUCE_CAST:-1}
+export FAST_CE=${FAST_CE:-1}
+export FUSED_INPUT_QUANTIZE=${FUSED_INPUT_QUANTIZE:-1}
+export FUSED_ADD_NORM_MUL_QUANTIZE=${FUSED_ADD_NORM_MUL_QUANTIZE:-1}
+export FUSED_SILU_W13=${FUSED_SILU_W13:-1}
+export FUSED_PAD_GRAD_ACCUM=${FUSED_PAD_GRAD_ACCUM:-1}
+
+export DEV="${DEV:-AMD}"
+export BS="${BS:-8}"
 export MP="${MP:-1}"
 export DP="${DP:-1}"
+export FSDP="${FSDP:-1}"
 export VIZ=${VIZ:-0}
 export JITBEAM=${JITBEAM:-3}
 export WANDB=${WANDB:-0}
@@ -31,7 +43,7 @@ export MAX_STEPS=${MAX_STEPS:-1024}
 export WARMUP_STEPS=${WARMUP_STEPS:-1}
 export SAMPLES=${SAMPLES:-$((MAX_STEPS * GBS))}
 
-export EVAL_BS=${EVAL_BS:-1}
+export EVAL_BS=${EVAL_BS:-8}
 export EVAL_FREQ=${EVAL_FREQ:-384}
 
 export ADAM_BETA_1=${ADAM_BETA_1:-0.9}
