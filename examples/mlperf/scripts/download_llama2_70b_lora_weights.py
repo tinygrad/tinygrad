1a1ff39cf75bd22b6b44a43183b4585388c7ac2d
mad360 <matt@dldipaolo.com>
2026-05-13 00:40:29 -0400

    cleanups

--- examples/mlperf/scripts/download_llama2_70b_lora_weights.py
+++ examples/mlperf/scripts/download_llama2_70b_lora_weights.py
@@ -5,15 +5,13 @@ from examples.mlperf.hash import hash_directory
 from extra.huggingface_onnx.huggingface_manager import snapshot_download_with_retry
 
 MODEL_PATH = getenv("MODEL_PATH", "/raid/weights/c4-llama2-70b-lora/")
-LLAMA2_70B_REPO_ID = "imaolo/llama2-70b-fused-qkv-flat-mlperf"
-
-# TODO
-HF_FLAT_EXPECTED_HASH = "9931046352d60e1687bef38094899e99"
-HF_FLAT_REVISION = ""
+REPO_ID = "imaolo/llama2-70b-fused-qkv-flat-mlperf"
+EXPECTED_HASH = "9931046352d60e1687bef38094899e99"
+REVISION = "5ef07c7bdfffa493f16d7ffa394a203fc5ee8734"
 
 if __name__ == '__main__':
-  weights_path = Path(snapshot_download_with_retry(repo_id=LLAMA2_70B_REPO_ID, local_dir=MODEL_PATH, allow_patterns=["*safetensors*", "*.json", "*.md"]))
+  weights_path = Path(snapshot_download_with_retry(repo_id=REPO_ID, local_dir=MODEL_PATH, revision=REVISION, allow_patterns=["*safetensors*", "*.json", "*.md"]))
 
   clean_dir(weights_path, ['.safetensors'])
 
-  assert hash_directory(weights_path) == HF_FLAT_EXPECTED_HASH
+  assert hash_directory(weights_path) == EXPECTED_HASH
