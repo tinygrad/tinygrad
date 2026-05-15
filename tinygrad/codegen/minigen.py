fede39db53575a5fa4857d80f2b17c6f2e639c57
George Hotz <geohot@gmail.com>
2026-05-15 09:07:20 -0700

    minigen can render more

--- tinygrad/codegen/minigen.py
+++ tinygrad/codegen/minigen.py
@@ -1,25 +1,56 @@
-from tinygrad.uop.ops import UOp, graph_rewrite, PatternMatcher, GroupOp, UPat
+from tinygrad.uop.ops import UOp, graph_rewrite, PatternMatcher, GroupOp, UPat, Ops
 from tinygrad.uop.spec import type_verify, spec_program
-from tinygrad.dtype import dtypes
+from tinygrad.dtype import dtypes, Invalid
 from tinygrad.helpers import SPEC
 from tinygrad.renderer import Renderer
-from tinygrad.codegen.late.devectorizer import pm_add_loads
+from tinygrad.codegen.late.devectorizer import pm_add_loads, reduce_to_acc, ReduceContext
+from tinygrad.codegen.late.linearizer import pm_split_ends
+from tinygrad.uop.decompositions import get_late_rewrite_patterns, get_transcendental_patterns
 
 pm_lower_weakint = PatternMatcher([
   (UPat(GroupOp.All, dtypes.weakint, name="x"), lambda x: x.replace(dtype=dtypes.int))
 ])
 
+pm_minimal_reduce = PatternMatcher([
+  (UPat(Ops.REDUCE, name="red"), reduce_to_acc),
+])
+
+pm_minimal_move_gates_from_index = PatternMatcher([
+  # here we create the alt value for load to be 0s and remove the where Invalid
+  (UPat.var("buf").index(UPat.var("gate").where(UPat.var("idx"), UPat(arg=Invalid))).or_casted(name="cast").load(name="l"),
+   lambda buf,gate,idx,cast,l: buf.index(idx, ptr=True).cast(cast.dtype).load(l.const_like(0), gate, dtype=l.dtype)),
+  (UPat.var("buf").index(UPat.var("gate").where(UPat.var("idx"), UPat(arg=Invalid))).or_casted(name="cast").store(UPat.var("data")),
+   lambda buf,gate,idx,cast,data: buf.index(idx, ptr=True).cast(cast.dtype).store(data, gate)),
+])
+
+
 def minigen_to_sink(ast:UOp, ren:Renderer, optimize:bool) -> UOp:
   sink = ast
 
-  # we need to lower weakint for the program
-  # this will be simpler when we have implicit dtype
-  sink = graph_rewrite(sink, pm_lower_weakint)
+  # REDUCE is not allowed in programs, we need to do that REDUCE somewhere
+  # this creates a register where the reduce happens
+  sink = graph_rewrite(sink, pm_minimal_reduce, ctx=ReduceContext(), name="remove reduce")
 
   # we need to add loads
   # this is really a store to DEFINE_REG, but load is simpler
-  # LOAD is kind of anonymous store
-  sink = graph_rewrite(sink, pm_add_loads)
+  # LOAD(DATA) is anonymous store -> AFTER(anon_buf, STORE(anon_buf, DATA))
+  sink = graph_rewrite(sink, pm_add_loads, name="add loads")
+
+  # we need to lower weakint for the program
+  # this will be simpler when we have implicit dtype
+  sink = graph_rewrite(sink, pm_lower_weakint, name="remove weakint")
+
+  # move gates from INDEX to LOAD/STORE
+  sink = graph_rewrite(sink, pm_minimal_move_gates_from_index, name="move gates")
+
+  # split ENDs, renderable ENDs can only end one RANGE
+  sink = graph_rewrite(sink, pm_split_ends, name="split ends")
+
+  # decompose ops like SIN/THREEFRY into renderable versions
+  supported_ops = tuple(ren.code_for_op.keys())
+  pm_decomp = get_late_rewrite_patterns(supported_ops, disable_fast_idiv=True) + \
+              get_transcendental_patterns(supported_ops, force_transcendental=False)
+  sink = graph_rewrite(sink, pm_decomp, ctx=ren.target, name="decompose ops to renderable")
 
   if SPEC: type_verify(sink, spec_program)
   return sink
