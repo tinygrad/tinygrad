9e408e239f9a70c18fe2304eac2b329c66eccc9e
George Hotz <geohot@gmail.com>
2026-05-18 15:56:57 -0700

    fix rangeify tests

--- tinygrad/codegen/minigen.py
+++ tinygrad/codegen/minigen.py
@@ -1,19 +1,32 @@
+from dataclasses import dataclass
 from tinygrad.uop.ops import UOp, graph_rewrite, PatternMatcher, GroupOp, UPat, Ops
 from tinygrad.uop.spec import type_verify, spec_program
-from tinygrad.dtype import dtypes, Invalid
+from tinygrad.dtype import dtypes, Invalid, AddrSpace
 from tinygrad.helpers import SPEC
 from tinygrad.renderer import Renderer
-from tinygrad.codegen.late.devectorizer import pm_add_loads, reduce_to_acc, ReduceContext
+from tinygrad.codegen.late.devectorizer import pm_add_loads, reduce_to_acc
 from tinygrad.codegen.late.linearizer import CFGContext, pm_split_ends, pm_add_control_flow
-from tinygrad.uop.decompositions import get_late_rewrite_patterns, get_transcendental_patterns
+from tinygrad.uop.decompositions import get_late_rewrite_patterns, get_transcendental_patterns, pm_dtype_decomps
 from tinygrad.uop.symbolic import sym
 
 pm_lower_weakint = PatternMatcher([
   (UPat(GroupOp.All, dtypes.weakint, name="x"), lambda x: x.replace(dtype=dtypes.int))
 ])
 
+@dataclass
+class ReduceContext:
+  acc_num: int = 0
+  local_num: int = 0
+
+def stage_to_local(ctx:ReduceContext, x:UOp):
+  # TODO: addrspace shouldn't be on dtype
+  ret = UOp(Ops.DEFINE_LOCAL, x.dtype.ptr(size=x.numel(), addrspace=AddrSpace.LOCAL), (), arg=ctx.local_num)
+  ctx.local_num += 1
+  return ret.after(ret.reshape(*[u.vmax+1 for u in x.src[1:]]).index(*x.src[1:]).store(x.src[0]).end(*x.src[1:]))
+
 pm_minimal_reduce = PatternMatcher([
   (UPat(Ops.REDUCE, name="red"), reduce_to_acc),
+  (UPat(Ops.STAGE, name="x"), stage_to_local),
 ])
 
 pm_minimal_move_gates_from_index = PatternMatcher([
@@ -28,9 +41,13 @@ pm_minimal_move_gates_from_index = PatternMatcher([
 def minigen_to_sink(ast:UOp, ren:Renderer, optimize:bool) -> UOp:
   sink = ast
 
+  # do single symbolic (this rewrites POW)
+  sink = graph_rewrite(sink, sym, name="symbolic")
+
   # REDUCE is not allowed in programs, we need to do that REDUCE somewhere
   # this creates a register where the reduce happens
-  sink = graph_rewrite(sink, pm_minimal_reduce, ctx=ReduceContext(), name="remove reduce")
+  # STAGE is also not allowed in programs, this is similar to REDUCE
+  sink = graph_rewrite(sink, pm_minimal_reduce, ctx=ReduceContext(), name="remove reduce/stage")
 
   # we need to add loads
   # this is really a store to DEFINE_REG, but load is simpler
@@ -41,25 +58,25 @@ def minigen_to_sink(ast:UOp, ren:Renderer, optimize:bool) -> UOp:
   # this will be simpler when we have implicit dtype
   sink = graph_rewrite(sink, pm_lower_weakint, name="remove weakint")
 
-  # move gates from INDEX to LOAD/STORE
+  # move gates from INDEX to LOAD/STORE (Invalid isn't renderable)
   sink = graph_rewrite(sink, pm_minimal_move_gates_from_index, name="move gates")
 
   # split ENDs, renderable ENDs can only end one RANGE
   sink = graph_rewrite(sink, pm_split_ends, name="split ends")
 
-  # do single symbolic (this rewrites POW)
-  sink = graph_rewrite(sink, sym, name="symbolic")
-
   # **** enter decanonicalize *****
-
-  # this was the linearizer, add control flow edges where they are needed on RANGEs
-  sink = graph_rewrite(sink, pm_add_control_flow, ctx=CFGContext(sink), name="add control flow", bottom_up=True)
 
   # decompose ops like SIN/THREEFRY into renderable versions
   supported_ops = tuple(ren.code_for_op.keys())
   pm_decomp = get_late_rewrite_patterns(supported_ops, disable_fast_idiv=True) + \
               get_transcendental_patterns(supported_ops, force_transcendental=False)
   sink = graph_rewrite(sink, pm_decomp, ctx=ren.target, name="decompose ops to renderable")
+
+  # decompose dtypes we don't support into renderable versions
+  sink = graph_rewrite(sink, pm_dtype_decomps, ctx=(set(), ren.target), name="decomp dtypes")
+
+  # this was the linearizer, add control flow edges where they are needed on RANGEs
+  sink = graph_rewrite(sink, pm_add_control_flow, ctx=CFGContext(sink), name="add control flow", bottom_up=True)
 
   if SPEC: type_verify(sink, spec_program)
   return sink
