55409e39a833ca8c6a8ccf658bce05f8dbf13c3e
qazal <qazal.software@gmail.com>
2026-05-26 17:36:53 +0300

    simple isel test

---
+++ test/amd/test_isel.py
@@ -0,0 +1,19 @@
+import unittest
+import numpy as np
+from tinygrad import Tensor, Device, UOp
+from tinygrad.codegen import to_program
+from tinygrad.viz.serve import amd_decode
+from tinygrad.renderer.amd.dsl import Inst
+
+def get_insts(t:Tensor) -> list[Inst]:
+  prg_uop = to_program(t.schedule_linear().src[-1].src[0], Device[t.device].renderer)
+  return list(amd_decode(prg_uop.src[4].arg, Device[t.device].renderer.target.arch).values())
+
+class TestIsel(unittest.TestCase):
+  def test_v_max(self):
+    a = Tensor(np.arange(2, dtype=np.uint32)).realize()
+    insts = get_insts(a.max())
+    assert any(getattr(i, "op_name", "").startswith("v_max") for i in insts)
+
+if __name__ == "__main__":
+  unittest.main()
