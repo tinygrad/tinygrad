3a72fd5116f99ffd40656e828b25e7c77dc36de2
qazal <qazal.software@gmail.com>
2026-05-26 14:47:46 +0300

    keep changes

--- tinygrad/viz/js/worker.js
+++ tinygrad/viz/js/worker.js
@@ -45,21 +45,18 @@ const layoutUOp = (g, { graph, change }, opts) => {
   const lineHeight = 14;
   g.setGraph({ rankdir: "LR", font:"sans-serif", lh:lineHeight });
   ctx.font = `350 ${lineHeight}px ${g.graph().font}`;
-  const labelDims = (label) => {
+  if (change?.length) g.setNode("overlay", {label:"", labelWidth:0, labelHeight:0, className:"overlay"});
+  let callCount = 0;
+  for (const [k, {label, src, ref, color, tag, exclude }] of Object.entries(graph)) {
+    // adjust node dims by label size (excluding escape codes) + add padding
     let [width, height] = [0, 0];
     for (line of label.replace(/\u001B\[(?:K|.*?m)/g, "").split("\n")) {
       width = Math.max(width, ctx.measureText(line).width);
       height += lineHeight;
     }
-    return rectDims(width, height);
-  }
-  if (change?.length) g.setNode("overlay", {label:"", labelWidth:0, labelHeight:0, className:"overlay"});
-  let callCount = 0;
-  for (const [k, {label, src, ref, color, tag }] of Object.entries(graph)) {
-    // adjust node dims by label size (excluding escape codes) + add padding
     const callNode = label.startsWith("CALL\n") || label.startsWith("FUNCTION\n");
     if (callNode) callCount++;
-    g.setNode(k, {...labelDims(label), label, ref, id:k, color, tag, callNode});
+    g.setNode(k, {...rectDims(width, height), label, ref, id:k, color, tag, callNode, exclude});
     // add edges
     const edgeCounts = {};
     for (const [_, s] of src) edgeCounts[s] = (edgeCounts[s] || 0)+1;
@@ -82,28 +79,15 @@ const layoutUOp = (g, { graph, change }, opts) => {
   }
   // optionally remove node srcs, track affected nodes
   const disconnected = new Set();
-  // node op comes from label
-  const nodeOp = (n) => g.node(n)?.label.split("\n", 1)[0];
-  // TODO: can we detect movement op nodes in a better way?
-  const movementOps = new Set(["RESHAPE", "EXPAND", "PERMUTE", "PAD", "SHRINK", "FLIP"]);
   for (const n of g.nodes()) {
-    const op = nodeOp(n), src = g.predecessors(n) || [];
-    const collapsibleSrc = ["DEVICE", "UNIQUE", "LUNIQUE"].includes(op) || (op === "STACK" && src.length === 0) ||
-      (op === "CONST" && !["UNIQUE", "LUNIQUE"].includes(nodeOp(src[0])));
+    const node = g.node(n);
     for (const consumerId of (g.successors(n) || [])) {
       const consumer = g.node(consumerId);
       // add +- toggle if this consumer has collapsible sources
       const edge = g.edge(n, consumerId);
-      const collapsible = consumer.callNode ? edge?.label?.text === 0 : collapsibleSrc || (op === "STACK" && movementOps.has(nodeOp(consumerId)));
+      const collapsible = consumer.callNode ? edge?.label?.text === 0 : node.exclude;
       if (!collapsible) continue;
       consumer.collapsible = true;
-      const consumerOp = nodeOp(consumerId);
-      if (!consumer.callNode && consumerOp !== "END") for (const [port, s] of graph[consumerId].src) {
-        if (String(s) !== String(n) || (port !== 0 && ["STAGE", "INDEX"].includes(consumerOp))) continue;
-        const [op, arg=""] = g.node(n).label.split("\n"), extra = src.length ? ` ${nodeOp(src[0])}` : "";
-        consumer.label += `\n${op}${port} ${arg}${extra}`;
-        ({width:consumer.width, height:consumer.height, labelWidth:consumer.labelWidth, labelHeight:consumer.labelHeight} = labelDims(consumer.label));
-      }
       // make sources invisible if UI has toggled it off
       const collapsed = consumer.callNode ? opts.showCallSrc === opts.callSrcMask.has(consumerId) : !opts.nodeSrcMask.has(consumerId);
       if (!collapsed) continue;
