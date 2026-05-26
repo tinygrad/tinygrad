eb4e470a3a64d0ba9d5857a965ce6a21d8403f6f
qazal <qazal.software@gmail.com>
2026-05-26 17:45:09 +0300

    use native max

--- test/amd/test_isel.py
+++ test/amd/test_isel.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from tinygrad import Tensor, Device, UOp
+from tinygrad import Tensor, Device, Context
 from tinygrad.codegen import to_program
 from tinygrad.viz.serve import amd_decode
 from tinygrad.renderer.amd.dsl import Inst
@@ -10,10 +10,11 @@ def get_insts(t:Tensor) -> list[Inst]:
   return list(amd_decode(prg_uop.src[4].arg, Device[t.device].renderer.target.arch).values())
 
 class TestIsel(unittest.TestCase):
+  @Context(NOOPT=1)
   def test_v_max(self):
     a = Tensor(np.arange(2, dtype=np.uint32)).realize()
     insts = get_insts(a.max())
-    assert any(getattr(i, "op_name", "").startswith("v_max") for i in insts)
+    assert any((op:=getattr(i, "op_name", "")).startswith("V_MAX") or op.startswith("S_MAX") for i in insts)
 
 if __name__ == "__main__":
   unittest.main()
