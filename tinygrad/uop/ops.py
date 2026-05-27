c88941a9571f7af8cff1776cda7d85f65e012ec2
George Hotz <geohot@gmail.com>
2026-05-26 19:19:44 -0700

    bugfix

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -1077,9 +1077,8 @@ class ProgramInfo:
     for u in sink.toposort():
       if u.op is Ops.DEFINE_VAR: _vars.append(u)
       if u.op is Ops.PARAM: _globals.append(u.arg)
-      if u.op in (Ops.STORE, Ops.LOAD):
-        if (idx:=u.src[0]).op is Ops.INDEX or (u.src[0].op is Ops.CAST and (idx:=u.src[0].src[0]).op is Ops.INDEX):
-          if (buf:=idx.src[0]).op is Ops.PARAM: (outs if u.op is Ops.STORE else ins).append(buf.arg)
+      if u.op in (Ops.STORE, Ops.LOAD) and (idx:=u.src[0]).op in (Ops.INDEX, Ops.SLICE) and (buf:=idx.src[0]).op is Ops.PARAM:
+        (outs if u.op is Ops.STORE else ins).append(buf.arg)
       if u.op is Ops.SPECIAL:
         if u.arg[0] == 'i': local_size = None
         special_size = local_size if u.arg[0] == 'l' else global_size
