22c835bddc5fec4e650dcd5fbfa6231f7a7be005
Christopher Milan <chrismilan@ucla.edu>
2026-06-02 23:36:58 +0000

    hmm

--- tinygrad/runtime/autogen/__init__.py
+++ tinygrad/runtime/autogen/__init__.py
@@ -50,7 +50,8 @@ def load(name, files, **kwargs):
 def __getattr__(nm):
   match nm:
     case "libc":
-      return load("libc", ["/usr/include/sys/mman.h", "/usr/include/bits/mman-shared.h", "/usr/include/string.h", "/usr/include/elf.h", "/usr/include/unistd.h", "/usr/include/asm-generic/mman-common.h"],
+      return load("libc", lambda: ([i for i in system("dpkg -L libc6-dev").split() if 'sys/mman.h' in i or 'bits/mman-shared.h' in i] +
+                                   ["/usr/include/string.h", "/usr/include/elf.h", "/usr/include/unistd.h", "/usr/include/asm-generic/mman-common.h"]),
                   args=["-D__USE_GNU", "-D_GNU_SOURCE"], dll="'c'", errno=True, recsym=True)
     case "avcodec": return load("avcodec", ["{}/libavcodec/hevc/hevc.h", "{}/libavcodec/cbs_h265.h"], srcs=ffmpeg_src)
     case "opencl": return load("opencl", ["{}/CL/cl.h"], dll="'OpenCL'", args=["-I{}"], srcs=opencl_src)
