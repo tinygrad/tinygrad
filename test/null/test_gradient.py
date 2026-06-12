762f50bd52954587c55d4343c0fc111cf2198677
chenyu <chenyu@fastmail.com>
2026-06-11 23:58:21 -0400

    move gradient.py to mixin/ [PR] (#16583)

--- test/null/test_gradient.py
+++ test/null/test_gradient.py
@@ -4,7 +4,7 @@ import torch
 from tinygrad import Tensor
 from tinygrad.dtype import dtypes
 from tinygrad.uop.ops import UOp
-from tinygrad.gradient import compute_gradient
+from tinygrad.mixin.gradient import compute_gradient
 
 class TestGradient(unittest.TestCase):
   def _cmp_nan_okay(self, x, y):
