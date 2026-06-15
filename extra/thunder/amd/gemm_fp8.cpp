f8f976d9c4e7661ebe0b3875842b75ae5abc901a
qazal <qazal.software@gmail.com>
2026-06-15 01:29:06 -0700

    Revert "c"
    This reverts commit 8941fec7ca03755372d8464cf513bd9843e75411.

--- extra/thunder/amd/gemm_fp8.cpp
+++ extra/thunder/amd/gemm_fp8.cpp
@@ -359,7 +359,7 @@ __global__ __launch_bounds__(512, 2) void hk_fp8_gemm(bf16 *C_ptr, fp8e4m3 *A_pt
 #endif
 #if SCALE_MODE & 4
     float g_scale = (*g_scale_ptr + 1e-08f) * (1.0f / 448.0f);
-    scale *= g_scale;
+    scale *= *g_scale;
 #endif
 
     mul(cA, cA, scale);
