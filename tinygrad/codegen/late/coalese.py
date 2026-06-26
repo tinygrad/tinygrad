3c2df81e0e4dee0bc33c36482efd243edfb0f9cd
George Hotz <geohot@gmail.com>
2026-06-25 22:01:57 -0700

    always simplify

--- tinygrad/codegen/late/coalese.py
+++ tinygrad/codegen/late/coalese.py
@@ -25,26 +25,21 @@ devectorizer2 = PatternMatcher([
 
 def transform_to_image(ctx, buf:UOp, x:UOp) -> UOp|None:
   if not IMAGE or ctx.target.device not in {"QCOM", "CL", "PYTHON", "NULL"}: return None
-  valid = None
+  valid = UOp.const(dtypes.bool, True)
   if x.op == Ops.WHERE and x.src[2].op == Ops.CONST and x.src[2].arg == Invalid: valid,x,_= x.src
   # search for dims that drop the most valid statements
   best_drop, cands = -1, []
   for ch, cw in image_valid_dims(buf.dtype.base, buf.max_numel(), ctx.target.arch):
-    cidx = UOp.vectorize((x//4)%cw, x//(4*cw))
-    dropped = 0
-    if valid is not None:
-      cidx = uop_given_valid(valid, cidx)
-      dropped = len(_drop_valid_stmts(valid, cidx, ch, cw))
+    cidx = uop_given_valid(valid, UOp.vectorize((x//4)%cw, x//(4*cw)))
+    dropped = len(_drop_valid_stmts(valid, cidx, ch, cw))
     if dropped > best_drop: best_drop, cands = dropped, [(ch, cw, cidx)]
     elif dropped == best_drop: cands.append((ch, cw, cidx))
-    # exit early without valid for speed
-    if valid is None: break
   # if no candidates, we don't rewrite
   if len(cands) == 0: return None
   # and tiebreak with indexing complexity (ie. number of nodes)
   h, w, cidx = cands[0] if len(cands) == 1 else min(cands, key=lambda cand: len(cand[2].gep(1).simplify().backward_slice))
   buf = buf.replace(dtype=(dtypes.imageh if buf.dtype.itemsize == 2 else dtypes.imagef)((h, w, 4)))
-  if valid is not None:
+  if valid.op is not Ops.CONST or valid.arg is not True:
     return buf.index(valid.where(cidx.src[1], cidx.src[1].const_like(Invalid)),
                      valid.where(cidx.src[0], cidx.src[0].const_like(Invalid)))
   else:
