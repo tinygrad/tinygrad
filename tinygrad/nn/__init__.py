bee150d4a9c2cb81954d71d2b8586005bdd30095
George Hotz <geohot@gmail.com>
2026-06-28 13:13:49 -0700

    correct fix

--- tinygrad/nn/__init__.py
+++ tinygrad/nn/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import math, functools
 from tinygrad.tensor import Tensor
-from tinygrad.dtype import dtypes, AddrSpace
+from tinygrad.dtype import dtypes
 from tinygrad.helpers import prod, make_tuple, flatten, USE_ATOMICS, TRAINING
 from tinygrad.nn import optim, state, datasets  # noqa: F401
 
@@ -350,16 +350,14 @@ def _embedding_bwd(grad_emb:UOp, call:UOp) -> tuple:
       global_token_id = idx_flat[i].cast(dtypes.weakint)
       local_token_id = (global_token_id - offset).clip(0, grad_weight.shape[0]-1)
       in_range = (global_token_id >= offset) & (global_token_id < (offset + local_vocab_size))
-      grad_val = in_range.where(grad_emb_flat[i, j].cast(dtypes.float), 0.0)
+      grad_val = in_range.where(grad_emb_flat[i, j].load().cast(dtypes.float), 0.0)
     else:
       local_token_id = idx_flat[i].clip(0, grad_weight.shape[0]-1).cast(dtypes.weakint)
-      grad_val = grad_emb_flat[i, j].cast(dtypes.float)
+      grad_val = grad_emb_flat[i, j].load().cast(dtypes.float)
     # atomic scatter-add: grad_weight[token_id, j] += grad_emb_flat[i, j]
     if device in ("CPU", "NULL"): atomic_arg = "__atomic_fetch_add({0}, {1}, __ATOMIC_RELAXED);"
     elif device == "AMD": atomic_arg = "__hip_atomic_fetch_add({0}, {1}, __ATOMIC_RELAXED, __HIP_MEMORY_SCOPE_AGENT);"
     else: raise NotImplementedError(f"no atomics for device {device}")
-    # NOTE: we need .load() here to bring the val to ALU because CUSTOM doesn't do that automatically
-    if grad_val.addrspace != AddrSpace.ALU: grad_val = grad_val.load()
     atomic = UOp(Ops.CUSTOM, dtypes.void, (grad_weight.index(local_token_id, j, ptr=True), grad_val), arg = atomic_arg)
     return atomic.end(i, j_outer, j_inner).sink(arg=KernelInfo(name="embedding_bwd", opts_to_apply=()))
 
