8f10fd37e6324e352ad729ffb70019d9fd8515e1
Rishav Sanjay <rishavnps@gmail.com>
2026-06-29 20:55:13 +0530

    fix missign comment

--- tinygrad/uop/validate.py
+++ tinygrad/uop/validate.py
@@ -53,6 +53,7 @@ z3_renderer = PatternMatcher([
 ])
 
 def uops_to_z3(solver:z3.Solver, *uops: UOp) -> list[z3.ExprRef]:
+  # gate on upstream AFTER/BUFFER as a replacement for PtrDType, but keep INDEX as an unknown LOAD
   lst = list(UOp.sink(*uops).toposort(gate=lambda x: x.op not in {Ops.AFTER, Ops.BUFFER} and
                                       (x.dtype.scalar() in dtypes.ints+(dtypes.bool, dtypes.weakint) or x.op is Ops.SINK)))[:-1]
   z3map: dict[UOp, z3.ExprRef] = {}
