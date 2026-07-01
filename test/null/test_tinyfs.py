c8720e9d38575d8d79cb359f5e09a8d8add86f1a
Chen-Yu Yang <chenyu@fastmail.com>
2026-07-01 15:14:28 -0400

    move fs_load and fs_store to nn
    similar to safe_load and safe_save

--- test/null/test_tinyfs.py
+++ test/null/test_tinyfs.py
@@ -1,24 +1,25 @@
 import unittest
 from tinygrad import Tensor
+from tinygrad.nn.state import fs_store, fs_load
 
 class TestLoadStore(unittest.TestCase):
   def test_load_shape(self):
-    t = Tensor(bytes(16)).fs_load(1024)
+    t = fs_load(Tensor(bytes(16)), 1024)
     assert t.shape == (1024,), t.shape
     t.schedule_linear()
 
   def test_store_shape(self):
-    t = Tensor.zeros(1024).fs_store()
+    t = fs_store(Tensor.zeros(1024))
     assert t.shape == (16,), t.shape
     t.schedule_linear()
 
   def test_load_large_shape(self):
-    t = Tensor(bytes(16)).fs_load(10_000_000)
+    t = fs_load(Tensor(bytes(16)), 10_000_000)
     assert t.shape == (10_000_000,), t.shape
     t.schedule_linear()
 
   def test_store_large_shape(self):
-    t = Tensor.zeros(10_000_000).fs_store()
+    t = fs_store(Tensor.zeros(10_000_000))
     assert t.shape == (16,), t.shape
     t.schedule_linear()
 
