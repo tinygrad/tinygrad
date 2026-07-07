125c68cd74dc153eb15abf4f00fa82ea26671565
George Hotz <geohot@gmail.com>
2026-07-07 15:03:26 -0700

    generic const INDEX into STACK is src

--- tinygrad/uop/movement.py
+++ tinygrad/uop/movement.py
@@ -14,6 +14,7 @@ mop_cleanup = PatternMatcher([
   # STACK on INDEX CONST
   (UPat(Ops.STACK, src=UPat(Ops.INDEX, src=(UPat.var("src"), UPat(Ops.CONST))), name="stk"),
    lambda src,stk: src if stk.shape == src.shape and list(range(len(stk.src))) == [x.src[1].arg for x in stk.src] else None),
-  # INDEX on STACK (simple)
-  (UPat(Ops.INDEX, src=(UPat(Ops.STACK, name="stk"), UPat(Ops.CONST, name="c"))), lambda stk,c: stk.src[c.arg]),
+  # const INDEX into STACK is src
+  (UPat(Ops.INDEX, src=(UPat(Ops.STACK, name="a"), UPat.cvar("i")), name="idx", allow_any_len=True),
+   lambda a,i,idx: a.src[i.arg].index(*idx.src[2:])),
 ])
