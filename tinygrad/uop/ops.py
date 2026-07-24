041e4c965863d2862b55a01569d95c57af8bdbbe
Christopher Milan <chrismilan@ucla.edu>
2026-07-24 16:14:23 -0700

    fix x86

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -1143,7 +1143,8 @@ class UOp(RandMixin, metaclass=UOpMetaClass):
 
   def to_elf(self) -> TinyELF:
     assert self.op is Ops.PROGRAM and isinstance(self.arg, ProgramInfo), "to_elf should only be called on a PROGRAM ast"
-    sig = tuple((u.arg.name, u.arg.slot, u.dtype, u._shape) for u in self.src[1].src if u.op is Ops.PARAM)
+    sig = tuple((u.arg.name, u.arg.slot, u.dtype, u._shape)
+                for u in tuple(filter(lambda u: u.op is Ops.PARAM and u.addrspace != AddrSpace.ALU, self.src[1].src)) + self.arg.vars)
     return TinyELF(self.src[3].arg, self.arg.function_name, self.arg.target, sig)
 
 @dataclass(frozen=True)
