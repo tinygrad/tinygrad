8de6e36c9625cedcecb1ba4cfba27e5c43e13cb7
Christopher Milan <chrismilan@ucla.edu>
2026-07-24 16:28:33 -0700

    windows x86?

--- tinygrad/runtime/ops_cpu.py
+++ tinygrad/runtime/ops_cpu.py
@@ -94,7 +94,7 @@ class CPUProgram(HCQProgram['CPUDevice']):
   except OSError: pass
 
   def __init__(self, dev:CPUDevice, obj:TinyELF):
-    self.runtimevars = {name:i for i,(name,*_) in enumerate(obj.signature) if name == 'core_id'}
+    self.runtimevars = {name:slot for name,slot,*_ in obj.signature if name == 'core_id'}
 
     LVP = obj.target.renderer == "LVP"
     if sys.platform == "win32": # mypy doesn't understand when WIN is used here
