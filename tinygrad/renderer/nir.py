bec229ed5fad849e080a1b671ba8f4e11d9bcbd0
Christopher Milan <chrismilan@ucla.edu>
2026-07-27 20:31:29 -0700

    nv: mockgpu

--- tinygrad/renderer/nir.py
+++ tinygrad/renderer/nir.py
@@ -246,14 +246,14 @@ class NIRRenderer(Renderer):
 
   def supported_dtypes(self): return {d for d in Renderer.supported_dtypes(self) if d not in dtypes.fp8s+(dtypes.bfloat16,)}
 
+def padded_idx(param_idx:int, size:int): return round_up(param_idx, size) + size
+
 class NAKRenderer(NIRRenderer):
-  param = nir_instr(nc=1, num_components=1, bs=lambda sz:sz*8, also=lambda self,sz: setattr(self, "param_idx", self.param_idx + sz),
+  param = nir_instr(nc=1, num_components=1, bs=lambda sz:sz*8, also=lambda self,sz: setattr(self, "param_idx", padded_idx(self.param_idx, sz)),
     intrins={"ALIGN_MUL":lambda sz:sz}, srcs=lambda self,b: [nsrc(nimm(b, 0, dtypes.int)), nsrc(nimm(b, self.param_idx, dtypes.int))])(
        lambda self, b, x, sz: mesa.nir_intrinsic_instr_create(b.shader, mesa.nir_intrinsic_ldc_nv))
 
   def supported_dtypes(self): return {d for d in super().supported_dtypes() if (d != dtypes.half or int(self.target.arch[3:]) >= 53)}
-
-def padded_idx(param_idx:int, size:int): return round_up(param_idx, size) + size
 
 class LVPRenderer(NIRRenderer):
   has_local = False
@@ -267,7 +267,7 @@ class LVPRenderer(NIRRenderer):
   param = nir_instr(nc=1, bs=lambda sz: sz * 8, num_components=1, intrins={"ALIGN_MUL":lambda sz: sz, "RANGE":lambda self: self.param_sz},
     srcs=lambda b,self,sz: [nsrc(nimm(b, 0, dtypes.int)), nsrc(nimm(b, round_up(self.param_idx, sz), dtypes.int))], also=lambda self, sz:
     setattr(self, "param_idx", padded_idx(self.param_idx, sz)))(lambda self,b,x,sz:
-                                                                      mesa.nir_intrinsic_instr_create(b.shader, mesa.nir_intrinsic_load_ubo))
+                                                                mesa.nir_intrinsic_instr_create(b.shader, mesa.nir_intrinsic_load_ubo))
 
   def prerender(self, uops:list[UOp]):
     super().prerender(uops)
