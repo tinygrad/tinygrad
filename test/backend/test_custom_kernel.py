d1c8635497b979ed60e2c1eb0a88421e15001667
qazal <qazal.software@gmail.com>
2026-07-30 17:58:35 +0800

    update tests

--- test/backend/test_custom_kernel.py
+++ test/backend/test_custom_kernel.py
@@ -433,7 +433,7 @@ class TestCustomKernel(unittest.TestCase):
     a = Tensor.custom_kernel(a.reshape(2, 2).T, fxn=custom_src_kernel)[0]
     self.assertEqual(a.tolist(), [[1, 2], [1, 3]])
 
-  def test_mop_input(self, mop_fxn=lambda x: x.reshape(16, 2), kcount:int=0):
+  def _test_mop_input(self, mop_fxn, pre_kernel=False):
     # default: input is BUFFER
     x = mop_fxn(Tensor.arange(32).clone("CPU").realize())
     y = Tensor.custom_kernel(Tensor.empty_like(x), x, fxn=custom_add_one_kernel)[0]
@@ -441,7 +441,7 @@ class TestCustomKernel(unittest.TestCase):
     y.realize()
     kernel_count = GlobalCounters.kernel_count
     self.assertEqual(y.tolist(), x.add(1).tolist())
-    self.assertEqual(kernel_count, 1+kcount)
+    self.assertEqual(kernel_count, 1+int(pre_kernel))
     # same test with @function, input is PARAM
     from tinygrad import function
     x0 = Tensor.arange(32).clone("CPU").realize()
@@ -454,20 +454,18 @@ class TestCustomKernel(unittest.TestCase):
     y = run(x0).realize()
     kernel_count = GlobalCounters.kernel_count
     self.assertEqual(y.tolist(), mop_fxn(x0).add(1).tolist())
-    # TODO: another +1 is because function copies the output
-    self.assertEqual(kernel_count, 1+kcount+1)
+    self.assertEqual(kernel_count, 1+int(pre_kernel))
 
-  def test_shrink_input(self): self.test_mop_input(lambda x: x[:4], kcount=0)
+  def test_shrink_input(self): self._test_mop_input(lambda x: x[:4])
   @unittest.expectedFailure
-  def test_double_permute_input(self): self.test_mop_input(lambda x: x.reshape(4, 8).T.T, kcount=0)
-  def test_permute_input(self): self.test_mop_input(lambda x: x.reshape(4, 8).T, kcount=1)
-  def test_offset_shrink_input(self): self.test_mop_input(lambda x: x[4:8], kcount=0)
-  def test_2d_shrink_input(self): self.test_mop_input(lambda x: x.reshape(4, 8)[:, 2:6], kcount=1)
+  def test_double_permute_input(self): self._test_mop_input(lambda x: x.reshape(4, 8).T.T)
   @unittest.expectedFailure
-  def test_pad_input(self): self.test_mop_input(lambda x: x[:4].pad(((0, 4),)), kcount=1)
-  @unittest.expectedFailure
-  def test_flip_input(self): self.test_mop_input(lambda x: x.flip(0), kcount=1)
-  def test_expand_input(self): self.test_mop_input(lambda x: x.reshape(16, 2)[:, :1].expand(16, 2), kcount=1)
+  def test_reshape_input(self): self._test_mop_input(lambda x: x.reshape(16, 2))
+  def test_permute_input(self): self._test_mop_input(lambda x: x.reshape(4, 8).T, pre_kernel=True)
+  def test_offset_shrink_input(self): self._test_mop_input(lambda x: x[4:8])
+  def test_2d_shrink_input(self): self._test_mop_input(lambda x: x.reshape(4, 8)[:, 2:6], pre_kernel=True)
+  def test_pad_input(self): self._test_mop_input(lambda x: x[:4].pad(((0, 4),)), pre_kernel=True)
+  def test_flip_input(self): self._test_mop_input(lambda x: x.flip(0), pre_kernel=True)
 
 class TestUnshardIndex(unittest.TestCase):
   """Regression tests for INDEX on UNSHARD (fragment) resolution in schedule/multi.py.
