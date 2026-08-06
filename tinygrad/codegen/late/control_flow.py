4297b77a795a6cc5d7630ec75b59d9f4b00448ee
ttomsa <tomasvsilva8@gmail.com>
2026-08-06 22:07:13 +0100

    i guess

--- tinygrad/codegen/late/control_flow.py
+++ tinygrad/codegen/late/control_flow.py
@@ -1,9 +1,9 @@
 from tinygrad.uop.ops import PatternMatcher, UOp, Ops, UPat, dtypes, GroupOp, AddrSpace
 
 def do_split_ends(e:UOp):
-  ret = e.src[0]
-  for r in sorted(UOp.sink(*e.src[1:]).ranges, key=lambda x: x.arg, reverse=True): ret = ret.end(r)
-  return ret
+  ret, backedge = e.src[0], tuple(x for x in e.src[1:] if x.dtype in (dtypes.void, dtypes.bool))
+  for r in sorted(UOp.sink(*[x for x in e.src[1:] if x not in backedge]).ranges, key=lambda x: x.arg, reverse=True): ret = ret.end(r)
+  return ret.end(*backedge) if len(backedge) else ret
 
 pm_split_ends = PatternMatcher([
   # split the ends
