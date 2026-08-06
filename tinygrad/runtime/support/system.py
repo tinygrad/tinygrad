38e131e796de555392a5ad27c195299cf8fbd1f3
George Hotz <geohot@gmail.com>
2026-08-06 11:29:52 -0700

    pci: use VFIO_DEVICE_RESET for device resets when the device is bound to vfio

--- tinygrad/runtime/support/system.py
+++ tinygrad/runtime/support/system.py
@@ -270,7 +270,9 @@ class PCIDevice:
       vfio.VFIO_IOMMU_UNMAP_DMA(unwrap(System.vfio), du)
       i = j + 1
 
-  def reset(self): os.system(f"sudo sh -c 'echo 1 > /sys/bus/pci/devices/{self.pcibus}/reset'")
+  def reset(self):
+    if getattr(self, 'vfio_dev', None) is not None: vfio.VFIO_DEVICE_RESET(self.vfio_dev)
+    else: os.system(f"sudo sh -c 'echo 1 > /sys/bus/pci/devices/{self.pcibus}/reset'")
   def read_config(self, offset:int, size:int): return int.from_bytes(self.cfg_fd.read(size, binary=True, offset=offset), byteorder='little')
   def write_config(self, offset:int, value:int, size:int): self.cfg_fd.write(value.to_bytes(size, byteorder='little'), binary=True, offset=offset)
   def write_config_flush(self, offset:int, value:int, size:int):
