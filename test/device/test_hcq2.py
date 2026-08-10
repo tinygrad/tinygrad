29341d74499fe8f4fe1c85538180e939d6e9f98a
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-08-10 07:02:13 -0700

    test

--- test/device/test_hcq2.py
+++ test/device/test_hcq2.py
@@ -1,36 +1,14 @@
 import unittest, numpy as np
 from unittest.mock import patch
 from tinygrad import Device, Tensor
-from tinygrad.uop.ops import UOp, Ops
-from tinygrad.dtype import dtypes
-from tinygrad.runtime.support import hcq2
+from tinygrad.helpers import getenv
+from tinygrad.runtime.support.hcq2 import HCQ_DEVS, all_devices_in
 
-class TestKernelCopy(unittest.TestCase):
-  def test_copy_lowers_to_kernel(self):
-    dst, src = (UOp.new_buffer("CPU", 16, dtypes.float) for _ in range(2))
-    call = src.copy_to_device("CPU").call(dst, src)
-
-    with patch.object(hcq2, "HCQ_DEVS", frozenset(("CPU",))), patch.object(type(Device["CPU"]), "has_copy_queue", False):
-      lowered = hcq2.pm_insert_copy_staging.rewrite(call)
-    self.assertIs(lowered.src[0].op, Ops.PROGRAM)
-    self.assertEqual(lowered.src[1:], call.src[1:]) # same dst and src, the copy is just a kernel now
-
-  def test_copy_keeps_copy_queue(self):
-    dst, src = (UOp.new_buffer("CPU", 16, dtypes.float) for _ in range(2))
-    call = src.copy_to_device("CPU").call(dst, src)
-
-    with patch.object(hcq2, "HCQ_DEVS", frozenset(("CPU",))), patch.object(type(Device["CPU"]), "has_copy_queue", True):
-      self.assertIsNone(hcq2.pm_insert_copy_staging.rewrite(call))
-
-@unittest.skipIf(Device[Device.DEFAULT].has_copy_queue, "device has a copy queue, run with AMD_DISABLE_SDMA=1")
-class TestKernelCopyExec(unittest.TestCase):
-  def test_copy_across_devices(self):
-    a = (Tensor.ones(64) * 2).to(Device.DEFAULT).contiguous().realize()
-    np.testing.assert_equal(a.to(f"{Device.DEFAULT.split(':')[0]}:1").contiguous().realize().numpy(), np.full(64, 2))
-
-  def test_copy_from_host(self):
-    a = Tensor(np.arange(64, dtype=np.float32)).to(Device.DEFAULT).contiguous().realize()
-    np.testing.assert_equal(a.numpy(), np.arange(64))
+@unittest.skipUnless(getenv("HCQ2") and all_devices_in(Device.DEFAULT, HCQ_DEVS), "hcq2 device required")
+class TestHCQ2(unittest.TestCase):
+  def test_copy_without_copy_queue(self):
+    with patch.object(Device[Device.DEFAULT], "has_copy_queue", False):
+      np.testing.assert_equal(Tensor(np.arange(61, dtype=np.float32)).to(Device.DEFAULT).contiguous().realize().numpy(), np.arange(61))
 
 if __name__ == "__main__":
   unittest.main()
