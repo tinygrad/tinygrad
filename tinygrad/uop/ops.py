ba346a6780d6c2c1e47bc56c369730152228a10b
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-12 12:32:44 -0300

    quick tuplize patch ugh

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -328,8 +328,8 @@ class UOp(RandMixin, metaclass=UOpMetaClass):
 
   @functools.cached_property
   def tuplize(self:UOp) -> tuple:
-    # NOTE: find better comparison for Ops.INS
-    return (self.op.value, self.arg.__hash__() if self.op is Ops.INS else self.arg, self.dtype,)+tuple([x.tuplize for x in self.src])
+    # NOTE: find better comparison for RDNA3 ops
+    return (self.op.value, str(self.arg.args) if isinstance(self.arg, functools.partial) else self.arg, self.dtype,)+tuple([x.tuplize for x in self.src])
 
   # *** uop shape stuff ***
 
