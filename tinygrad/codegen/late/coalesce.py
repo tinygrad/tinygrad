57d6520e0023348e93c41179061d5d01796cd1c0
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-12 08:42:23 -0300

    more coalesce

--- tinygrad/codegen/late/coalesce.py
+++ tinygrad/codegen/late/coalesce.py
@@ -136,6 +136,11 @@ def memory_coalescing(sink:UOp, ctx:Renderer) -> UOp:
       pass
     elif is_image_shape(buf._shape):
       lengths = [4]
+    elif ctx.target.device == "AMD":
+      must_divide = False
+      lengths = [4,2]
+      if buf.dtype == dtypes.half: lengths.extend(8,6)
+      elif buf.dtype.itemsize == 4: lengths.append(3)
     elif ctx is not None and ctx.supports_float4:
       # TODO: a better way to get this than ctx
       lengths = [8,4,2] if buf.dtype == dtypes.half and getenv("ALLOW_HALF8") else [4,2]
