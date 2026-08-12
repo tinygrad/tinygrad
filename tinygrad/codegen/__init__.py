86aca81d3b168b3ca3fb7bd107d82bcb0b4a275b
qazal <qazal.software@gmail.com>
2026-08-12 12:08:21 -0700

    pm_reduce_invalid (gpt)

--- tinygrad/codegen/__init__.py
+++ tinygrad/codegen/__init__.py
@@ -8,7 +8,7 @@ from tinygrad.uop.render import pyrender
 from tinygrad.uop.spec import type_verify, spec_tensor, spec_program
 from tinygrad.renderer import Renderer, Estimates
 from tinygrad.renderer.isa import ISARenderer, IselContext, PreRegAllocContext
-from tinygrad.dtype import dtypes, AddrSpace
+from tinygrad.dtype import dtypes, AddrSpace, Invalid
 
 # import all pattern matchers here
 from tinygrad.codegen.gpudims import pm_add_gpudims
@@ -219,8 +219,13 @@ def reduce_ranges_to_acc(ctx:ReduceContext, r:UOp):
   acc_out = acc_initted.store(acc_initted.alu(r.arg[0], inp)).end(*r.src[1:]).rtag("mergeable")
   return acc.after(acc_out)
 
+pm_reduce_invalid = PatternMatcher([
+  (UPat.var("gate").where(UPat.var("value"), UPat(Ops.CONST, arg=Invalid)),
+   lambda ctx,gate,value: gate.where(value, value.const_like(ctx))),
+])
+
 def expand_horizontal_reduce(r:UOp):
-  inp = r.src[0]
+  inp = graph_rewrite(r.src[0], pm_reduce_invalid, ctx=identity_element(r.arg[0], r.dtype))
   vals = [inp.index(*idx) for idx in itertools.product(*[range(inp.max_shape[a]) for a in range(r.arg[1])])]
   return functools.reduce(lambda x,y: x.alu(r.arg[0], y), vals)
 
