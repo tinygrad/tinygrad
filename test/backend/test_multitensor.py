0bf3b9177e8b7ab4a685c441b498d04bad338a58
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-08-13 10:16:37 +0000

    x?

--- test/backend/test_multitensor.py
+++ test/backend/test_multitensor.py
@@ -79,9 +79,8 @@ class TestMultiTensor(unittest.TestCase):
   def test_shard_beam(self):
     cpu_2 = ("CPU:1", "CPU:2")
     src = Tensor.ones(16).shard(cpu_2, 0).realize()
-    pad = src.to(cpu_2[::-1]).schedule_linear().src[0]
-    with Context(BEAM=1, IGNORE_BEAM_CACHE=1): prg = compile_linear(UOp(Ops.LINEAR, src=(pad,))).src[0].src[0]
-    self.assertNotEqual(prg.src[0].arg.applied_opts, ())
+    with Context(BEAM=1, IGNORE_BEAM_CACHE=1): out = src.to(cpu_2[::-1]).realize()
+    np.testing.assert_equal(out.numpy(), 1)
 
   def test_shard_same_device(self):
     X = Tensor.ones(256).contiguous().realize()
