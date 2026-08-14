a42f995a805acb6e882e363871f1434d6dd797f2
amoghmunikote <amogh.munikote@gmail.com>
2026-08-14 11:18:51 -0700

    Add Turing support for signaling PCAS in NVComputeQueue

--- tinygrad/runtime/ops_nv.py
+++ tinygrad/runtime/ops_nv.py
@@ -149,7 +149,10 @@ class NVComputeQueue(NVCommandQueue):
     if self.active_qmd is None:
       if prg.dev.pma_enabled: self.nvm(1, nv_gpu.NVC6C0_PM_TRIGGER, 0)
       self.nvm(1, nv_gpu.NVC6C0_SEND_PCAS_A, qmd_buf.va_addr >> 8)
-      self.nvm(1, nv_gpu.NVC6C0_SEND_SIGNALING_PCAS2_B, 9)
+      if prg.dev.iface.compute_class <= nv_gpu.TURING_COMPUTE_A:
+        self.nvm(1, nv_gpu.NVC6C0_SEND_SIGNALING_PCAS_B, nv_flags("NVC6C0_SEND_SIGNALING_PCAS_B", invalidate="true", schedule="true"))
+      else:
+        self.nvm(1, nv_gpu.NVC6C0_SEND_SIGNALING_PCAS2_B, 9)
     else:
       self.active_qmd.write(dependent_qmd0_pointer=qmd_buf.va_addr >> 8, dependent_qmd0_action=1, dependent_qmd0_prefetch=1, dependent_qmd0_enable=1)
 
