7583a6679563259c2137280f99ed3d093ab2a0df
amoghmunikote <amogh.munikote@gmail.com>
2026-08-14 09:17:26 -0700

    Recognize Turing

--- tinygrad/runtime/support/nv/nvdev.py
+++ tinygrad/runtime/support/nv/nvdev.py
@@ -9,6 +9,9 @@ from tinygrad.runtime.support.hcq import MMIOInterface
 
 NV_DEBUG = getenv("NV_DEBUG", 0)
 
+CHIP_ARCH_NAMES = {0x16: "TU1", 0x17: "GA1", 0x19: "AD1", 0x1b: "GB2"}
+CHIP_FW_NAMES = {"TU1": "tu102", "GA1": "ga102", "AD1": "ad102", "GB2": "gb202"}
+
 class NVReg:
   def __init__(self, nvdev, base, off, fields=None): self.nvdev, self.base, self.off, self.fields = nvdev, base, off, fields
 
@@ -111,8 +114,8 @@ class NVDev:
     self.pci_dev.write_config_flush(pci.PCI_COMMAND, self.pci_dev.read_config(pci.PCI_COMMAND, 2) | pci.PCI_COMMAND_MASTER, 2)
     self.chip_id = self.reg("NV_PMC_BOOT_0").read()
     self.chip_details = self.reg("NV_PMC_BOOT_42").read_bitfields()
-    self.chip_name = {0x17: "GA1", 0x19: "AD1", 0x1b: "GB2"}[self.chip_details['architecture']] + f"{self.chip_details['implementation']:02d}"
-    self.fw_name = {"GB2": "gb202", "AD1": "ad102", "GA1": "ga102"}[self.chip_name[:3]]
+    self.chip_name = CHIP_ARCH_NAMES[self.chip_details['architecture']] + f"{self.chip_details['implementation']:02d}"
+    self.fw_name = CHIP_FW_NAMES[self.chip_name[:3]]
     self.mmu_ver, self.fmc_boot = (3, True) if self.chip_details['architecture'] >= 0x1a else (2, False)
 
     self.flcn:NV_FLCN|NV_FLCN_COT = NV_FLCN_COT(self) if self.fmc_boot else NV_FLCN(self)
