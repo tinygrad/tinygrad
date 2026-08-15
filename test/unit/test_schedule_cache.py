1095e24a330ea7319dd94465c374937706dfa354
qazal <qazal.software@gmail.com>
2026-08-14 23:06:17 -0700

    work

--- test/unit/test_schedule_cache.py
+++ test/unit/test_schedule_cache.py
@@ -4,13 +4,13 @@ from tinygrad import Tensor, Variable, UOp, function
 from tinygrad.uop.ops import KernelInfo
 from tinygrad.schedule import schedule_cache
 
-def custom_set0_kernel(A:UOp, _B:UOp=None, num:int=0) -> UOp:
-  return A[0].set(num).sink(arg=KernelInfo(f"custom_set0_{num}"))
+def custom_add_kernel(A:UOp, B:UOp, num:int=0) -> UOp:
+  return A[0].set(B[0] + num).sink(arg=KernelInfo(f"custom_add_{num}"))
 
-def custom_set0_backward(grad_a:UOp, _) -> tuple[None, UOp]:
-  x = Tensor.invalids(*grad_a.shape, dtype=grad_output.dtype, device=grad_output.device)
-  x = Tensor.custom_kernel(x, fxn=functools.partial(custom_set0_kernel, num=0))[0]
-  return None, (x * Tensor(grad_a, device=grad_output.device)).uop
+def custom_add_backward(grad_output:UOp, _) -> tuple[None, UOp]:
+  grad = Tensor.invalids(*grad_output.shape, dtype=grad_output.dtype, device=grad_output.device)
+  grad = Tensor.custom_kernel(grad, Tensor(grad_output, device=grad_output.device), fxn=functools.partial(custom_add_kernel, num=0))[0]
+  return None, grad.uop
 
 class TestScheduleCache(unittest.TestCase):
   def test_bound_variable_reuses_cache(self):
@@ -30,27 +30,27 @@ class TestScheduleCache(unittest.TestCase):
 
   def test_custom_kernel(self):
     for i in range(4):
-      a = Tensor.empty(1)
-      a = Tensor.custom_kernel(a, fxn=functools.partial(custom_set0_kernel, num=i))[0]
+      a, b = Tensor.empty(1), Tensor.ones(1)
+      a = Tensor.custom_kernel(a, b, fxn=functools.partial(custom_add_kernel, num=i))[0]
       a.realize()
-      self.assertEqual(a.item(), i)
+      self.assertEqual(a.item(), i+1)
 
   def test_same_custom_function_reuses_cache(self):
     schedule_cache.clear()
-    fxn = functools.partial(custom_set0_kernel, num=10)
+    fxn = functools.partial(custom_add_kernel, num=10)
 
     # first run
-    a = Tensor.empty(1)
-    a = Tensor.custom_kernel(a, fxn=fxn)[0]
+    a, x = Tensor.empty(1), Tensor.ones(1)
+    a = Tensor.custom_kernel(a, x, fxn=fxn)[0]
     a.realize()
-    self.assertEqual(a.item(), 10)
+    self.assertEqual(a.item(), 11)
     cache_size_after_first = len(schedule_cache)
 
     # second run with same function should reuse cache
-    b = Tensor.empty(1)
-    b = Tensor.custom_kernel(b, fxn=fxn)[0]
+    b, x = Tensor.empty(1), Tensor.ones(1)
+    b = Tensor.custom_kernel(b, x, fxn=fxn)[0]
     b.realize()
-    self.assertEqual(b.item(), 10)
+    self.assertEqual(b.item(), 11)
     self.assertEqual(len(schedule_cache), cache_size_after_first)
 
   def test_simple(self):
@@ -75,24 +75,24 @@ class TestScheduleCache(unittest.TestCase):
     @function(precompile=True, precompile_backward=True)
     def f(x:Tensor) -> Tensor:
       out = Tensor.invalids(*x.shape, dtype=x.dtype, device=x.device)
-      out = Tensor.custom_kernel(out, x, fxn=functools.partial(custom_set0_kernel, num=10), grad_fxn=custom_set0_backward)[0]
-      return out + x
+      out = Tensor.custom_kernel(out, x, fxn=functools.partial(custom_add_kernel, num=10), grad_fxn=custom_add_backward)[0]
+      return out * x
 
     # warmup
     x = Tensor.ones(1).realize()
     out = f(x)
-    out.sum().backward()
+    out.backward(x)
     self.assertEqual(out.item(), 11)
-    self.assertEqual(x.grad.item(), 1)
+    self.assertEqual(x.grad.item(), 12)
 
     # use the cache next time function is called
     start_len_schedule_cache = len(schedule_cache)
     for _ in range(3):
       x = Tensor.ones(1).realize()
       out = f(x)
-      out.sum().backward()
+      out.backward(x)
       self.assertEqual(out.item(), 11)
-      self.assertEqual(x.grad.item(), 1)
+      self.assertEqual(x.grad.item(), 12)
     self.assertEqual(len(schedule_cache), start_len_schedule_cache)
 
 if __name__ == "__main__":
