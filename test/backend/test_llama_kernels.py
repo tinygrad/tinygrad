2900620da1c7eab366600f6096994332f936152d
qazal <qazal.software@gmail.com>
2026-08-19 16:16:18 +0800

    add uop kernel, has_hipcc is cached

--- test/backend/test_llama_kernels.py
+++ test/backend/test_llama_kernels.py
@@ -131,6 +131,9 @@ class TestFusedQKVRoPE(unittest.TestCase):
 
   def test_forward(self): self._test_fused_qkv_fw((1, 32, 8, 2, 16))
 
+  def test_llama31_8b_forward(self): self._test_fused_qkv_fw(self.SHAPE)
+
+  @unittest.skipUnless(has_hipcc(), "backward kernel requires hipcc to compile")
   def test_llama31_8b_backward(self):
     Tensor.manual_seed(1)
     B, N, H, H_KV, D = self.SHAPE
