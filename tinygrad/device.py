e884252cfe5eb699a340bd6bb09c22ad84138dbb
George Hotz <geohot@gmail.com>
2026-08-20 14:20:44 -0700

    fix compile server to be fork safe

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -301,6 +301,7 @@ class DepsTracker:
 class CompileError(Exception): pass
 
 class Compiler:
+  compiler_process: subprocess.Popen
   def __init__(self, cachekey:str|None=None): self.cachekey = cachekey if CCACHE else None
   def compile(self, src:str) -> bytes: return src.encode()   # NOTE: empty compiler is the default
   def compile_cached(self, src:str) -> bytes:
@@ -312,8 +313,15 @@ class Compiler:
   def disassemble(self, lib:bytes): pass
   def server(self, cmd:str, arch:str, *args) -> subprocess.Popen:
     argv = f"{cmd} {pathlib.Path(__file__).parent}/runtime/support/compileserver.py {type(self).__module__}:{type(self).__name__} {arch}"
-    return subprocess.Popen(argv.split() + [str(a) for a in args], stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=0)
+    self.compile_server_argv, self.compile_server_pid = argv.split() + [str(a) for a in args], os.getpid()
+    return self.spawn_compile_server()
+  def spawn_compile_server(self) -> subprocess.Popen:
+    return subprocess.Popen(self.compile_server_argv, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=0)
   def compile_server(self, src:str, proc:subprocess.Popen) -> bytes:
+    # the compile server is per-process. if the pid changed (this process forked after starting it), start a new one for this process
+    if self.compile_server_pid != os.getpid():
+      self.compile_server_pid, self.compiler_process = os.getpid(), self.spawn_compile_server()
+      proc = self.compiler_process
     unwrap(proc.stdin).write(struct.pack("I", len(src.encode())) + src.encode())
     if (lib:=unwrap(proc.stdout).read(struct.unpack("I", unwrap(proc.stdout).read(4))[0])): return lib
     raise CompileError("Compilation Error")
