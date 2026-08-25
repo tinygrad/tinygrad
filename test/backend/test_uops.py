469a362730e3f9abb7579bc84a19bfa1a8d29136
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-25 14:18:39 +0000

    remove div by 0

--- test/backend/test_uops.py
+++ test/backend/test_uops.py
@@ -371,11 +371,10 @@ class TestUOpPrograms(unittest.TestCase):
     self._run(prog.sink(arg=KernelInfo(opts_to_apply=())), a, b, c)
     with Context(DEBUG=0): self.assertLessEqual((c-ref).square().mean().item(), 1e-6)
 
-  @unittest.skipIf(Device.DEFAULT == "PYTHON", "PYTHON division by 0 overflows")
   def test_xidiv32(self):
     from tinygrad.codegen.decomp.op import xidiv32
-    for dt,n_data,d_data in [(dtypes.uint32, [0,27,2**31,2**32-1],   [35, 0, 9, 2**30-1]),
-                             (dtypes.int32,  [0,-340,2**31, -2**32], [35, 0, -9, -3**10])]:
+    for dt,n_data,d_data in [(dtypes.uint32, [0,27,2**31,2**32-1],   [35, 3, 9, 2**30-1]),
+                             (dtypes.int32,  [0,-340,2**31, -2**32], [35, 3, -9, -3**10])]:
       with self.subTest(dtype=dt):
         num, denom, out = Tensor(n_data, dtype=dt), Tensor(d_data, dtype=dt), Tensor.empty(4, dtype=dt)
         with Context(DEBUG=0): Tensor.realize(num, denom, out)
