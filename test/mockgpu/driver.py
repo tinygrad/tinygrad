176a4762ae1e6fc464c91f2c0575b0150e9f75c3
Hang Su <tonyfloater@gmail.com>
2026-08-26 04:59:37 -0400

    init commit

--- test/mockgpu/driver.py
+++ test/mockgpu/driver.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 class VirtFileDesc:
   def __init__(self, fd): self.fd, self.off = fd, 0
-  def ioctl(self, fd, req, argp): raise NotImplementedError()
-  def mmap(self, st, sz, prot, flags, fd, off): raise NotImplementedError()
+  def ioctl(self, fd, request, argp): raise NotImplementedError()
+  def mmap(self, start, sz, prot, flags, fd, offset): raise NotImplementedError()
   def close(self, fd): return 0
 
 class TextFileDesc(VirtFileDesc):
@@ -12,7 +12,7 @@ class TextFileDesc(VirtFileDesc):
     super().__init__(fd)
     self.content = text
 
-  def ioctl(self, fd, req, argp): return 0
+  def ioctl(self, fd, request, argp): return 0
   def read_contents(self, size=None):
     ret = self.content[self.off:self.off+(size or len(self.content))]
     self.off += (size or len(self.content))
@@ -23,7 +23,7 @@ class DirFileDesc(VirtFileDesc):
     super().__init__(fd)
     self.child_names = child_names
 
-  def ioctl(self, fd, req, argp): return 0
+  def ioctl(self, fd, request, argp): return 0
   def list_contents(self): return self.child_names
 
 @dataclass(frozen=True)
@@ -36,4 +36,4 @@ class VirtDriver:
     self.tracked_files = []
     self.tracked_addresses = []
   def track_address(self, staddr, enaddr, rcb, wcb): self.tracked_addresses.append((staddr, enaddr, rcb, wcb))
-  def open(self, name, flags, mode, fdcls): raise NotImplementedError()
+  def open(self, name, flags, mode, virtfile): raise NotImplementedError()
