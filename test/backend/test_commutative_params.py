6927b3f5d97b7c57c60e08c2759586c7c172d002
jadidbourbaki <tprod4242@gmail.com>
2026-08-28 16:43:55 -0400

    clean up tests, add abi stack and image view regressions

--- test/backend/test_commutative_params.py
+++ test/backend/test_commutative_params.py
@@ -1,123 +1,57 @@
-
 import numpy as np
 import unittest
-from tinygrad.device import Device
-from tinygrad.uop.ops import UOp, KernelInfo
-from tinygrad import dtypes
-from tinygrad.codegen import to_program
 from dataclasses import replace
-from tinygrad.device import Buffer
+from tinygrad import Tensor, Context, dtypes
+from tinygrad.device import Device, Buffer
+from tinygrad.uop.ops import UOp, KernelInfo
+from tinygrad.codegen import to_program
 from tinygrad.engine.realize import get_runtime
 
 class TestCommutativeParams(unittest.TestCase):
-  def _test_template(self,
-           out_slot: int,
-           in_slot: int,
-           variable_slots: tuple[int, ...],
-           values: tuple[int, ...]):
-    # let's first build a micro operation
+  def _test_template(self, out_slot:int, in_slot:int, variable_slots:tuple[int, ...], values:tuple[int, ...]):
     output_uop = UOp.param(out_slot, dtypes.float32, shape=(4,))
     input_uop = UOp.param(in_slot, dtypes.float32, shape=(4,))
 
     variables = []
-
     for index, slot in enumerate(variable_slots):
-      variable = UOp.variable(f"var_{index}", min_val = 0, max_val = 42,
-                  dtype=dtypes.int32, param = True)
-      # my idea here to create a variable to break the assumption
-      # in codegen that buffers are always before variables
-      replaced_arg = replace(variable.arg, slot = slot)
-      variable = variable.replace(arg = replaced_arg)
-      variables.append(variable)
-
-    assert len(variables) == len(variable_slots)
+      variable = UOp.variable(f"var_{index}", 0, 42, dtypes.int32, param=True)
+      variables.append(variable.replace(arg=replace(variable.arg, slot=slot)))
 
     v_range = UOp.range(4, 0)
-
-    value = input_uop.index(v_range).load()
-    value = value * variables[0].cast(dtypes.float32)
-
+    value = input_uop.index(v_range).load() * variables[0].cast(dtypes.float32)
     for variable in variables[1:]:
       value = value + variable.cast(dtypes.float32)
+    value = output_uop.index(v_range).store(value).end(v_range)
 
-    value = output_uop.index(v_range).store(value)
-    value = value.end(v_range)
-
-    kernel_name = f"commutative_params_order_{out_slot}_{in_slot}_"
-    kernel_name += "_".join(str(slot) for slot in variable_slots)
-
+    kernel_name = f"commutative_params_order_{out_slot}_{in_slot}_" + "_".join(str(slot) for slot in variable_slots)
     uop_graph_root = value.sink(arg=KernelInfo(name=kernel_name))
-
-    # self.assertTrue(Device.DEFAULT in Device)
-    default_renderer = Device[Device.DEFAULT].renderer
-
-    # okay, so now we have the program generated
-    program = to_program(ast = uop_graph_root, renderer = default_renderer)
+    program = to_program(uop_graph_root, Device[Device.DEFAULT].renderer)
 
     program_signature = program.to_elf().signature
-
-    self.assertIsNotNone(program_signature)
-
-    # we need to make sure every parameter is in the right order
     check_slots = [element[1] for element in program_signature]
     self.assertEqual(check_slots, list(range(len(program_signature))))
 
-    # does the pre-linearized UOp graph align? note that
-    # from what I can tell the globals and vars are in from_sink
     check_tuple = (in_slot, out_slot)
     if out_slot < in_slot:
       check_tuple = (out_slot, in_slot)
-
     self.assertEqual(program.arg.globals, check_tuple)
-
-    program_arg_variable_splots = tuple(var.arg.slot for var in program.arg.vars)
-    self.assertEqual(program_arg_variable_splots, variable_slots)
-
-    # now we are done with "compile time" checks time to run the kernel
+    self.assertEqual(tuple(var.arg.slot for var in program.arg.vars), variable_slots)
 
     input_data = np.arange(4, dtype=np.float32)
+    output_buf = Buffer(Device.DEFAULT, 4, dtypes.float32).allocate()
+    input_buf = Buffer(Device.DEFAULT, 4, dtypes.float32, initial_value=input_data.tobytes())
 
-    output_buf = Buffer(device = Device.DEFAULT,
-              size = 4,
-              dtype = dtypes.float32).allocate()
-
-    # because we are passing initial_value, the allocation
-    # happens automatically
-    # todo(hayder): double check this later
-    input_buf = Buffer(device = Device.DEFAULT,
-             size = 4,
-             dtype = dtypes.float32,
-             initial_value = input_data.tobytes())
-
-    runtime = get_runtime(device = Device.DEFAULT, ast = program)
-
-    global_size, local_size = program.arg.launch_dims(var_vals = {})
-
+    runtime = get_runtime(Device.DEFAULT, program)
+    global_size, local_size = program.arg.launch_dims({})
     slot_to_buffer_map = {in_slot: input_buf, out_slot: output_buf}
-
-    # put the buffers in the order of the slots
     device_specific_buffers = [slot_to_buffer_map[slot]._buf for slot in program.arg.globals]
+    runtime(*device_specific_buffers, global_size=global_size, local_size=local_size, vals=values, wait=True)
 
-    runtime(*device_specific_buffers,
-        global_size = global_size,
-        local_size = local_size,
-        vals = values,
-        wait = True)
-
-    # we computed this above using the kernel
-    # now doing it in plain ol' python
     expected_answer = input_data * values[0] + sum(values[1:])
-
-    # the output should be stored in... your guessed it!... the output buffer
     actual_answer = np.frombuffer(output_buf.as_memoryview(), dtype=np.float32)
-
-    # todo: assert_equal instead of assert_allclose *should* be the right move
-    # here since we are storing an integer as a float and
-    # we are not testing between multiple backends, but confirm this
     np.testing.assert_equal(actual_answer, expected_answer)
 
   def test_commutative_params(self):
-    # now for some table driven tests (yes, i used to code in go)
     cases = [
       (0, 1, (2,), (3,)),
       (1, 0, (2,), (2,)),
@@ -126,10 +60,42 @@ class TestCommutativeParams(unittest.TestCase):
       (0, 2, (1, 3), (4, 2)),
       (3, 2, (0, 1), (2, 4)),
     ]
-
     for case in cases:
       with self.subTest(case=case):
         self._test_template(*case)
+
+  def test_params_beyond_abi_registers(self):
+    # x86 passes the first 6 params in registers, the rest go on the stack
+    buffer_uops = [UOp.param(i, dtypes.float32, shape=(4,)) for i in range(8)]
+    v_range = UOp.range(4, 0)
+    value = buffer_uops[1].index(v_range).load()
+    for buffer_uop in buffer_uops[2:]:
+      value = value + buffer_uop.index(v_range).load()
+    value = buffer_uops[0].index(v_range).store(value).end(v_range)
+    program = to_program(value.sink(arg=KernelInfo(name="params_beyond_abi_registers")), Device[Device.DEFAULT].renderer)
+    self.assertEqual([element[1] for element in program.to_elf().signature], list(range(8)))
+
+    input_data = [np.full(4, i, dtype=np.float32) for i in range(1, 8)]
+    buffers = [Buffer(Device.DEFAULT, 4, dtypes.float32).allocate()]
+    buffers += [Buffer(Device.DEFAULT, 4, dtypes.float32, initial_value=data.tobytes()) for data in input_data]
+    global_size, local_size = program.arg.launch_dims({})
+    get_runtime(Device.DEFAULT, program)(*[b._buf for b in buffers], global_size=global_size, local_size=local_size, wait=True)
+    np.testing.assert_equal(np.frombuffer(buffers[0].as_memoryview(), dtype=np.float32), sum(input_data))
+
+  @unittest.skipUnless(Device.DEFAULT == "CL", "images need CL")
+  def test_image_and_flat_view_of_one_buffer(self):
+    x = Tensor.rand(16, 10, 27).realize()
+    weight = Tensor.rand(10).realize()
+    target = Tensor.uniform(16, 27, low=0, high=10, dtype=dtypes.int32).realize()
+    x_np = x.numpy()
+    target_np = target.numpy()
+    weight_np = weight.numpy()
+    picked = np.take_along_axis(x_np, target_np[:,None,:], axis=1).squeeze(1)
+    expected = -(picked * weight_np[target_np]).sum() / weight_np[target_np].sum()
+
+    with Context(IMAGE=1):
+      loss = x.nll_loss(target, weight=weight).numpy()
+    np.testing.assert_allclose(loss, expected, atol=1e-5, rtol=1e-5)
 
 if __name__ == "__main__":
   unittest.main()
