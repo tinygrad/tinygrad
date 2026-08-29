e8d1363938c35fb2e1668a4f1db77f9cf485324b
Chen-Yu Yang <chenyu@fastmail.com>
2026-08-28 21:47:30 -0400

    some tri cleanups [PR]

--- tinygrad/nn/onnx.py
+++ tinygrad/nn/onnx.py
@@ -1037,7 +1037,7 @@ def get_onnx_ops() -> dict[str, types.FunctionType|dict[OpSetId, types.FunctionT
       attn_scores = mask.where(attn_scores, mask_filter_value)
 
     if unidirectional:
-      causal_mask = Tensor.ones((seq_len, seq_len), dtype=dtypes.bool, buffer=False).tril()
+      causal_mask = Tensor._tri(seq_len, seq_len, 1).logical_not()
       attn_scores = causal_mask.where(attn_scores, mask_filter_value)
 
     output = attn_scores.softmax(-1) @ v
@@ -1069,8 +1069,7 @@ def get_onnx_ops() -> dict[str, types.FunctionType|dict[OpSetId, types.FunctionT
     qk_matmul_return_val = scores
 
     if is_causal:
-      causal_mask = Tensor.ones(Q.shape[-2], K.shape[-2], dtype=dtypes.bool, buffer=False).tril(0)
-      scores = scores.masked_fill(causal_mask.logical_not(), -float("inf"))
+      scores = scores.masked_fill(Tensor._tri(Q.shape[-2], K.shape[-2], 1), -float("inf"))
 
     if attn_mask is not None:
       mask_to_add = attn_mask.where(0, -float("inf")) if attn_mask.dtype == dtypes.bool else attn_mask
