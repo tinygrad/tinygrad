ebb8daab983bd578a66860612122add3899e10c1
George Hotz <geohot@gmail.com>
2026-09-02 16:40:02 -0700

    just label that

--- tinygrad/viz/serve.py
+++ tinygrad/viz/serve.py
@@ -163,7 +163,7 @@ def uop_to_json(data:VizData, x:UOp) -> dict[int, dict]:
     # limit SOURCE labels line count
     if u.op is Ops.SOURCE and len(lines:=label.split("\n")) > 40:
       label = "\n".join(lines[:30]) + "\n..."
-    if u.op is Ops.BUFFER: label += "\nUNBOUND" if u.is_unbound else "\nBOUND"
+    if u.is_unbound: label += "\nUNBOUND"
     addrspace_color:str|None = None
     with soft_err(): addrspace_color = addrspace_colors.get(u.addrspace, None) if u.addrspace is not None else None
     color = "#C07788" if u.is_unbound else uops_colors.get(u.op, "#ffffff")
