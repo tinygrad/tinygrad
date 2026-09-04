5b72133849216c0432e7f06e98434a752ec9a0cf
Ramy Fakhoury <li0nr208@gmail.com>
2026-09-04 22:34:22 +0300

    remove hardcoded cuda arch

--- test/mockgpu/cuda/cuda.py
+++ test/mockgpu/cuda/cuda.py
@@ -3,7 +3,7 @@ from typing import Any
 import ctypes, time
 from test.mockgpu.helpers import ptx_run
 from tinygrad.runtime.autogen import cuda as orig_cuda
-from tinygrad.helpers import mv_address
+from tinygrad.helpers import mv_address, DEV
 
 for attr in dir(orig_cuda):
   if not attr.startswith('__'):
@@ -25,6 +25,7 @@ class CUDAState:
     self.next_context_id = 1
 
 cuda_state = CUDAState()
+MOCKGPU_ARCH = DEV.arch or "sm_86"
 
 # Helper functions
 def check_context():
@@ -40,7 +41,8 @@ def cuDeviceGet(device, ordinal: int) -> int:
   if ordinal < 0:
     return orig_cuda.CUDA_ERROR_INVALID_VALUE
   device._obj.value = ordinal
-  cuda_state.devices[ordinal] = {"compute_capability": (8, 6)}
+  gpu_arch = int(MOCKGPU_ARCH[3:])
+  cuda_state.devices[ordinal] = {"compute_capability": divmod(gpu_arch, 10)}
   return orig_cuda.CUDA_SUCCESS
 
 def cuCtxCreate_v2(pctx, flags: int, dev: int) -> int:
@@ -136,8 +138,7 @@ def cuLaunchKernel(f, gx: int, gy: int, gz: int, lx: int, ly: int, lz: int, shar
 def cuDeviceComputeCapability(major, minor, dev: int) -> int:
   if dev not in cuda_state.devices:
     return orig_cuda.CUDA_ERROR_INVALID_VALUE
-  major._obj.value = 8
-  minor._obj.value = 6
+  major._obj.value, minor._obj.value = cuda_state.devices[dev]["compute_capability"]
   return orig_cuda.CUDA_SUCCESS
 
 def cuDeviceCanAccessPeer(canAccessPeer, dev: int, peerDev: int) -> int:
