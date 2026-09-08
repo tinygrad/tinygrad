a0c54ab886827935f63753375adfdf7e4cad5978
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-09-08 11:16:03 -0700

    remove test_import_private_buffer

--- test/device/test_metal.py
+++ test/device/test_metal.py
@@ -11,13 +11,6 @@ class TestMetal(unittest.TestCase):
     with self.assertRaises(MemoryError):
       device.allocator.alloc(10000000000000000000)
 
-  def test_import_private_buffer(self):
-    device = Device['METAL']
-    private = device.sysdevice.newBufferWithLength_options(4, metal.MTLResourceStorageModePrivate)
-    (buf, _), host = device.allocator.alloc(4, BufferSpec(external_ptr=private.value))
-    self.assertEqual(buf.buf.value, private.value)
-    self.assertIsNone(host)
-
   def test_compile_error(self):
     compiler = MetalCompiler()
     with self.assertRaises(CompileError):
