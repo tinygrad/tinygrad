1c05207d717261f7267bbb8632b5660d80e1e525
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-09-10 12:13:18 +0000

    x

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -220,9 +220,10 @@ class Buffer:
     return None
 
   def as_memoryview(self, allow_zero_copy=False) -> memoryview:
-    if allow_zero_copy and (mv:=self._host_mv()) is not None:
+    if (mv:=self._host_mv()) is not None:
       self.allocator.dev.synchronize()
-      return mv
+      if allow_zero_copy: return mv
+      with cpu_profile(f"{self.device} -> TINY", f"{self.device}:COPY"): return memoryview(bytearray(mv))
     Buffer("PYTHON", self.size, self.dtype, opaque=(mv:=memoryview(bytearray(self.nbytes)))).copy_from(self)
     return mv
 
