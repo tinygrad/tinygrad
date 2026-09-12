f758e15f24f6c3062bc11a5e84fdf0bd07eb7493
BFA <82632764+borjamoskv@users.noreply.github.com>
2026-09-12 21:08:11 +0200

    use setattr for subclass method override in Program

--- tinygrad/device.py
+++ tinygrad/device.py
@@ -389,7 +389,7 @@ class Program(Generic[DeviceType]):
       def _init(self, dev:DeviceType, obj:TinyELF, *a, **kw):
         init(self, dev, obj, *a, **kw)
         if not hasattr(self, 'signature'): self.signature = obj.signature
-      cls.__init__ = _init
+      setattr(cls, '__init__', _init)
     if (call:=cls.__dict__.get('__call__')) is not None:
       def _call(self, *args, global_size:tuple[int,int,int]=(1,1,1), local_size:tuple[int,int,int]=(1,1,1), vals:tuple[int, ...]=(),
                 wait=False, **kw) -> float|None:
@@ -398,7 +398,7 @@ class Program(Generic[DeviceType]):
           vals = tuple(a for a, (_,_,_,s) in zip(args, self.signature) if s == ())
           return call(self, *bufs, global_size=global_size, local_size=local_size, vals=vals, wait=wait, **kw)
         return call(self, *args, global_size=global_size, local_size=local_size, vals=vals, wait=wait, **kw)
-      cls.__call__ = _call
+      setattr(cls, '__call__', _call)
   def __init__(self, dev:DeviceType, obj:TinyELF): self.signature = obj.signature
   def __call__(self, *bufs, global_size:tuple[int,int,int]=(1,1,1), local_size:tuple[int,int,int]=(1,1,1), vals:tuple[int, ...]=(),
                wait=False) -> float|None: pass
