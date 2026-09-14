6151f3cdd4dff0f19d8fbae81a6506b02db54033
qazal <qazal.software@gmail.com>
2026-09-14 10:05:34 +0800

    cleanup

--- extra/llama_kernels/quantize_mxfp4/__init__.py
+++ extra/llama_kernels/quantize_mxfp4/__init__.py
@@ -27,9 +27,9 @@ def _custom_quantize_mxfp4(row_fp4:UOp, row_scale:UOp, col_fp4:UOp, col_scale:UO
                   *(UOp(Ops.CUSTOM, src=(o.base.index(0),), arg=("", dtypes.void)) for o in outputs),
                   UOp.special(256, "lidx0"), UOp.special(M//128, "gidx0"), UOp.special(N//64, "gidx1"),
                   arg=KernelInfo(name, estimates=Estimates(ops=12*M*N, mem=mem)))
-  src = (pathlib.Path(__file__).parent/"quantize_mxfp4.cpp").read_text()
+  src = ((cpp_dir:=pathlib.Path(__file__).parent)/"quantize_mxfp4.cpp").read_text()
   return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.LINEAR, src=(*sink.src, sink)), UOp(Ops.SOURCE, arg=src),
-    UOp(Ops.BINARY, arg=compile_hip(src, [f"-DKERNEL_NAME={name}", f"-DM_DIM={M}", f"-DN_DIM={N}",
+    UOp(Ops.BINARY, arg=compile_hip(src, [f"-I{cpp_dir}", f"-DKERNEL_NAME={name}", f"-DM_DIM={M}", f"-DN_DIM={N}",
                                              f"-DSHUFFLE_ROWWISE_FP4_VALUE={int(shuffle_row)}",
                                              f"-DSHUFFLE_COLWISE_FP4_VALUE={int(shuffle_col)}"]))))
 
