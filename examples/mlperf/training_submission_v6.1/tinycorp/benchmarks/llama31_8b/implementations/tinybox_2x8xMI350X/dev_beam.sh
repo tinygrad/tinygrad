3dd577b51b2162ddd06b560e115de2ec1de346b9
nimlgen <138685161+nimlgen@users.noreply.github.com>
2026-09-20 13:30:40 -0700

    mlperf: llama31_8b on tinybox_2x8xMI350X

---
+++ examples/mlperf/training_submission_v6.1/tinycorp/benchmarks/llama31_8b/implementations/tinybox_2x8xMI350X/dev_beam.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# two boxes, a serve.py on each: PYTHONPATH=. DEV=PCI+AMD python extra/remote/serve.py 6667. run this on either, the far one goes first
+export NODES=${NODES:-"tinyamd3 tinyamd4"}
+export REMOTE=${REMOTE:-"$(echo $NODES | tr ' ' '\n' | grep -vx "$(hostname)" | head -1):6667,127.0.0.1:6667"}
+export DEV=PCI+AMD REMOTE_TIMEOUT=600 ALLREDUCE_NODE_NDEVS=8 DP=16 BS=32 EVAL_BS=16
+exec bash "$(dirname "$0")/../tinybox_8xMI350X/dev_beam.sh"
