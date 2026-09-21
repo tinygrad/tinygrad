9df061b6667a863e7c24c4cd01f57f3e4b9fab81
qazal <qazal.software@gmail.com>
2026-09-21 18:22:55 +0800

    simpler

--- test/amd/test_asm_kernel.py
+++ test/amd/test_asm_kernel.py
@@ -13,6 +13,7 @@ import tinygrad.runtime.autogen.amd.rdna4.ins as r4
 from tinygrad.renderer.amd.dsl import s, v, NULL, Reg
 from extra.gemm.amd_asm_matmul import Kernel
 
+# small pattern matcher converting CALL to INS
 from tinygrad.uop.ops import PatternMatcher, UPat, graph_rewrite, rewrite_group
 
 def assemble_inst(call:UOp) -> UOp|None:
@@ -22,13 +23,11 @@ def assemble_inst(call:UOp) -> UOp|None:
   args = [Reg(u.arg.slot + (256 if u.tag == "v" else 0), size) for u, size in regs]
   return UOp(Ops.INS, src=call.src[1:], arg=(call.arg.op(*args), dtypes.void))
 
-assemble_sink_pm = PatternMatcher([
-  (UPat(Ops.CALL, name="call"), assemble_inst),
-])
+assemble_sink_pm = PatternMatcher([(UPat(Ops.CALL, name="call"), assemble_inst),])
 
-@rewrite_group("asm_sink")
-def asm_sink(sink:UOp) -> UOp:
-  ins = graph_rewrite(sink, assemble_sink_pm, name="assemble instructions")
+@rewrite_group("call_to_ins")
+def call_to_ins(sink:UOp) -> UOp:
+  ins = graph_rewrite(sink, assemble_sink_pm, name="convert CALL to INS")
   lin = UOp(Ops.LINEAR, src=tuple(u for u in ins.toposort() if u.op is Ops.INS))
   return UOp(Ops.PROGRAM, src=(sink.replace(src=tuple(u for u in sink.src if u.op not in {Ops.CALL, Ops.AFTER})), lin))
 
@@ -38,28 +37,24 @@ def custom_add_one(A:UOp) -> UOp:
   threads = UOp.special(A.numel(), "lidx0")
   dest = tuple(UOp.param(i, dtypes.int32, (1,), addrspace=AddrSpace.REG).rtag("s") for i in range(2))
   kernarg = UOp.stack(*dest)
+  # NOTE: this call body doesn't implement the instruction, this exists in the emulator
   kernarg_load = UOp(Ops.CALL, src=(UOp.sink(), UOp.stack(*dest), kernarg), arg=InstInfo(functools.partial(s_load_b64, soffset=NULL)))
-  kernarg_wait = UOp(Ops.CALL, src=(UOp.sink(), kernarg_load),
-                    arg=InstInfo(functools.partial(s_waitcnt_lgkmcnt, sdst=NULL, simm16=0)))
+  kernarg_wait = UOp(Ops.CALL, src=(UOp.sink(), kernarg_load), arg=InstInfo(functools.partial(s_waitcnt_lgkmcnt, sdst=NULL, simm16=0)))
   saddr_after = UOp.stack(*(d.after(kernarg_wait) for d in dest))
   offset_val = UOp.param(0, dtypes.int32, (32,), addrspace=AddrSpace.REG).rtag("v")
-  lane_id = offset_val
-  offset_call = UOp(Ops.CALL, src=(UOp.sink(), offset_val, lane_id), arg=InstInfo(functools.partial(v_lshlrev_b32_e32, src0=2)))
+  offset_call = UOp(Ops.CALL, src=(UOp.sink(), offset_val, offset_val), arg=InstInfo(functools.partial(v_lshlrev_b32_e32, src0=2)))
   offset_after = offset_val.after(offset_call)
   val = UOp.param(1, dtypes.float32, (32,), addrspace=AddrSpace.REG).rtag("v")
   load_call = UOp(Ops.CALL, src=(UOp.sink(), val, offset_after, offset_val, saddr_after), arg=InstInfo(global_load_b32))
   wait_call = UOp(Ops.CALL, src=(UOp.sink(), load_call), arg=InstInfo(functools.partial(s_waitcnt_vmcnt, sdst=NULL, simm16=0)))
-  val_after = val.after(wait_call)
   c1_dest = UOp.param(2, dtypes.float32, (32,), addrspace=AddrSpace.REG).rtag("v")
   mov_call = UOp(Ops.CALL, src=(UOp.sink(), c1_dest), arg=InstInfo(functools.partial(v_mov_b32_e32, src0=1.0)))
-  c1_after = c1_dest.after(mov_call)
   add_dest = val
-  add_call = UOp(Ops.CALL, src=(UOp.sink(), add_dest, val_after, c1_after), arg=InstInfo(v_add_f32_e32))
-  add_after = add_dest.after(add_call)
+  add_after = add_dest.after(UOp(Ops.CALL, src=(UOp.sink(), add_dest, val.after(wait_call), c1_dest.after(mov_call)), arg=InstInfo(v_add_f32_e32)))
   store_to_global = UOp(Ops.CALL, src=(UOp.sink(), offset_val, offset_after, add_after, saddr_after), arg=InstInfo(global_store_b32))
   end_call = UOp(Ops.CALL, src=(UOp.sink(), store_to_global), arg=InstInfo(s_endpgm))
   sink = UOp.sink(A.base, threads, end_call, arg=KernelInfo(f"custom_add_one_{A.numel()}", estimates=Estimates(ops=A.numel(), mem=A.numel()*4*2)))
-  return asm_sink(sink)
+  return call_to_ins(sink)
 
 def custom_add_var(A:UOp, B:UOp) -> UOp:
   A,B = A.flatten(), B.flatten()
