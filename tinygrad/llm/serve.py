578a78b1d3d0a2c583a6506cecd582722c23598c
b1tg <b1tg@protonmail.ch>
2026-09-22 06:42:48 +0000

    fix

--- tinygrad/llm/serve.py
+++ tinygrad/llm/serve.py
@@ -14,9 +14,15 @@ def parse_tool_call(s:str) -> tuple[str, typing.Any]|None:
       call = json.loads(s)
       return call["name"], call.get("arguments", call.get("parameters", {}))
     except (json.JSONDecodeError, KeyError): return None
+  args = {}
+  # GLM format: name<arg_key>key</arg_key><arg_value>value</arg_value>...
+  if (gm := re.fullmatch(r"([\w.-]+)\s*((?:<arg_key>[^<]+</arg_key>\s*<arg_value>.*?</arg_value>\s*)*)", s, re.DOTALL)):
+    for key, value in re.findall(r"<arg_key>([^<]+)</arg_key>\s*<arg_value>(.*?)</arg_value>", gm.group(2), re.DOTALL):
+      try: args[key] = json.loads(value)
+      except json.JSONDecodeError: args[key] = value
+    return gm.group(1), args
   # XML format: <function=name>\n<parameter=key>\nvalue\n</parameter>...</function>
   if (fm := re.match(r"<function=([^>]+)>\s*(.*?)\s*(?:</function>)?$", s, re.DOTALL)):
-    args = {}
     for pm in re.finditer(r"<parameter=([^>]+)>(.*?)</parameter>", fm.group(2), re.DOTALL):
       value = re.sub(r"^\r?\n|\r?\n\Z", "", pm.group(2))
       try: args[pm.group(1)] = json.loads(value)
