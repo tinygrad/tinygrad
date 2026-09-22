bf7c0c85941177aca0f1fa9073404fb46f9d06e0
qazal <qazal.software@gmail.com>
2026-09-22 17:51:33 +0800

    ASM_CALL

---
+++ test/mockgpu/amd/call.py
@@ -0,0 +1,3 @@
+def run_asm(lib: int, lib_sz: int, gx: int, gy: int, gz: int, lx: int, ly: int, lz: int, args_ptr: int, rsrc2: int = 0x19c,
+            scratch_size: int = 0, arch: str = "rdna3", user_data: list[int]|None = None) -> int:
+  return 0
