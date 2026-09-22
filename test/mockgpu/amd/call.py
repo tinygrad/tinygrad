8ea14d0b6a4e98db182a5fa9471f3b71307dabf0
qazal <qazal.software@gmail.com>
2026-09-22 18:12:58 +0800

    fmt

--- test/mockgpu/amd/call.py
+++ test/mockgpu/amd/call.py
@@ -1,8 +1,8 @@
 import ctypes
 from tinygrad.renderer.amd import InstDecodeError, decode_inst
 
-def run_asm(lib: int, lib_sz: int, gx: int, gy: int, gz: int, lx: int, ly: int, lz: int, args_ptr: int, rsrc2: int = 0x19c,
-            scratch_size: int = 0, arch: str = "rdna3", user_data: list[int]|None = None) -> int:
+def run_asm(lib:int, lib_sz:int, gx:int, gy:int, gz:int, lx:int, ly:int, lz:int, args_ptr:int, rsrc2:int=0x19c,
+            scratch_size:int=0, arch:str="rdna3", user_data:list[int]|None=None) -> int:
   code = ctypes.string_at(lib, lib_sz)
   offset = 0
   while offset < len(code):
