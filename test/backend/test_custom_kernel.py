f5a017a109bed1e17bc0b4cc08b9e5fc1c4daa7a
qazal <qazal.software@gmail.com>
2026-09-24 10:15:39 +0800

    remove binary todo

--- test/backend/test_custom_kernel.py
+++ test/backend/test_custom_kernel.py
@@ -602,24 +602,18 @@ class TestCustomKernel(unittest.TestCase):
   def test_simple_from_source(self):
     a = Tensor.arange(4).clone().realize()
     src = "void test_src(int* restrict a) { a[0] = 1; }"
-    # TODO: it currently requires a compiler for Ops.BINARY
-    from tinygrad.device import Device
-    binary = Device[a.device].renderer.compiler.compile(src)
     def custom_src_kernel(A:UOp, B:UOp) -> UOp:
       sink = UOp.sink(A, arg=KernelInfo(name="test_src"))
-      return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.LINEAR, src=tuple(sink.toposort())), UOp(Ops.SOURCE, arg=src), UOp(Ops.BINARY, arg=binary)))
+      return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.LINEAR, src=tuple(sink.toposort())), UOp(Ops.SOURCE, arg=src),))
     a = Tensor.custom_kernel(a.reshape(2, 2).clone(), a.reshape(2, 2).T, fxn=custom_src_kernel)[0]
     self.assertEqual(a.tolist(), [[1, 1], [2, 3]])
 
-  @Context(DEV="CPU")
   def test_simple_from_source_alt(self):
-    a = Tensor.arange(4).clone().realize()
+    a = Tensor.arange(4).clone("CPU").realize()
     src = "void copy(int* restrict out, int* restrict in) { for (int i = 0; i < 4; i++) out[i] = in[i]; }"
-    from tinygrad.device import Device
-    binary = Device[a.device].renderer.compiler.compile(src)
     def custom_src_kernel(out:UOp, inp:UOp) -> UOp:
       sink = UOp.sink(out, inp, arg=KernelInfo(name="copy"))
-      return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.LINEAR, src=tuple(sink.toposort())), UOp(Ops.SOURCE, arg=src), UOp(Ops.BINARY, arg=binary)))
+      return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.LINEAR, src=tuple(sink.toposort())), UOp(Ops.SOURCE, arg=src),))
     out = Tensor.custom_kernel(Tensor.empty_like(a), a+1, fxn=custom_src_kernel)[0]
     GlobalCounters.reset()
     out.realize()
