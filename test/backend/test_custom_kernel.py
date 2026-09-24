b38661426577e4f392298371bd67ecfc1d916c41
qazal <qazal.software@gmail.com>
2026-09-24 10:22:09 +0800

    ctx

--- test/backend/test_custom_kernel.py
+++ test/backend/test_custom_kernel.py
@@ -598,8 +598,9 @@ class TestCustomKernel(unittest.TestCase):
     assert_kernel_count(2 if x[0].uop.contiguous_view() is None else 1)
     self.assertEqual(y.tolist(), [1, 2, 3, 4])
 
+  @Context(DEV="CPU")
   def test_simple_from_source(self):
-    a = Tensor.arange(4).clone("CPU").realize()
+    a = Tensor.arange(4).clone().realize()
     src = "void test_src(int* restrict a) { a[0] = 1; }"
     def custom_src_kernel(A:UOp, B:UOp) -> UOp:
       sink = UOp.sink(A, arg=KernelInfo(name="test_src"))
@@ -607,8 +608,9 @@ class TestCustomKernel(unittest.TestCase):
     a = Tensor.custom_kernel(a.reshape(2, 2).clone(), a.reshape(2, 2).T, fxn=custom_src_kernel)[0]
     self.assertEqual(a.tolist(), [[1, 1], [2, 3]])
 
+  @Context(DEV="CPU")
   def test_simple_from_source_alt(self):
-    a = Tensor.arange(4).clone("CPU").realize()
+    a = Tensor.arange(4).clone().realize()
     src = "void copy(int* restrict out, int* restrict in) { for (int i = 0; i < 4; i++) out[i] = in[i]; }"
     def custom_src_kernel(out:UOp, inp:UOp) -> UOp:
       sink = UOp.sink(out, inp, arg=KernelInfo(name="copy"))
