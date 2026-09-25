84bb5c12400ff19c01ee7d752a2f4c9a2cb5f6cf
George Hotz <geohot@gmail.com>
2026-09-25 15:04:58 +0800

    multinomial: 100 samples is enough for the KS test

--- test/backend/test_randomness.py
+++ test/backend/test_randomness.py
@@ -312,8 +312,8 @@ class TestRandomness(unittest.TestCase):
     @TinyJit
     def sample_one(): return Tensor(w).multinomial(1, replacement=False).realize()
 
-    tiny_samples = [sample_one().item() for _ in range(200)]
-    torch_samples = [torch.tensor(w).multinomial(1, replacement=False).item() for _ in range(200)]
+    tiny_samples = [sample_one().item() for _ in range(100)]
+    torch_samples = [torch.tensor(w).multinomial(1, replacement=False).item() for _ in range(100)]
     self.assertTrue(equal_distribution(lambda *_: Tensor(tiny_samples), lambda _: torch.tensor(torch_samples)))
 
     w = list(range(32))
