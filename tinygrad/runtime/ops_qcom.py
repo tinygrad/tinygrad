82d3f6a2d217d7b55faeba58984506f080fe4b39
Cintu07 <pawankalyan1892@gmail.com>
2026-09-26 04:58:38 +0000

    qcom: symbolic launch sizes dont crash the dims check

--- tinygrad/runtime/ops_qcom.py
+++ tinygrad/runtime/ops_qcom.py
@@ -116,7 +116,8 @@ class QCOMComputeQueue(HWQueue):
     data, lib = qcom_build_program(self.dev, prg, self.devs)
     global_size, local_size = prg.arg.global_size, prg.arg.local_size
     if data.max_threads < prod(local_size): raise RuntimeError("Too many resources requested for launch")
-    if any(g*l>mx for g,l,mx in zip(global_size, local_size, [65536, 65536, 65536])) and any(l>mx for l,mx in zip(local_size, [1024, 1024, 1024])):
+    if any(g*l>mx for g,l,mx in zip(global_size, local_size, [65536, 65536, 65536]) if isinstance(g, int)) and \
+       any(l>mx for l,mx in zip(local_size, [1024, 1024, 1024])):
       raise RuntimeError(f"Invalid global/local dims {global_size=}, {local_size=}")
 
     def cast_int(x, ceil=False): return (math.ceil(x) if ceil else int(x)) if isinstance(x, float) else x
